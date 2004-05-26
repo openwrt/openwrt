fdfb2210e6e673d89b75b0db43dbc03ec80e7426
Mike Baker <mbm@openwrt.org>
2004-05-26 01:28:41 +0000

    squashfs-2.0
    SVN-Revision: 37

--- obsolete-buildroot/make/squashfsroot.mk
+++ obsolete-buildroot/make/squashfsroot.mk
@@ -3,9 +3,9 @@
 # mksquashfs to build to target squashfs filesystems
 #
 #############################################################
-SQUASHFS_DIR=$(BUILD_DIR)/squashfs1.3r3
-SQUASHFS_SOURCE=squashfs1.3r3.tar.gz
-SQUASHFS_SITE=http://aleron.dl.sourceforge.net/sourceforge/squashfs
+SQUASHFS_DIR=$(BUILD_DIR)/squashfs2.0
+SQUASHFS_SOURCE=squashfs2.0-ALPHA.tar.gz
+SQUASHFS_SITE=http://dl.sourceforge.net/sourceforge/squashfs
 
 $(DL_DIR)/$(SQUASHFS_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(SQUASHFS_SITE)/$(SQUASHFS_SOURCE)
