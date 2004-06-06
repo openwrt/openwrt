04a27a0829be3312e6fe0d117d1a29ef6efe0ae7
Mike Baker <mbm@openwrt.org>
2004-06-06 04:52:51 +0000

    cleanup, remove whitespace at end of lines
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@61 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- root/etc/functions.sh
+++ root/etc/functions.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/ash
 
-debug () {
- [ -z "$DEBUG" ] || echo $1
-}
+alias debug=${DEBUG:-true}
 
-# allow env to override nvram 
+# allow env to override nvram
 nvram_get () {
  eval "echo \${$1:-\$(nvram get $1)}"
 }
@@ -13,7 +11,7 @@ nvram_get () {
 # valid interface?
 if_valid () (
   [ "${1%%[0-9]}" = "vlan" ] && {
-    i=${1#vlan} 
+    i=${1#vlan}
     hwname=$(nvram_get vlan${i}hwname)
     hwaddr=$(nvram_get ${hwname}macaddr)
     [ -z "$hwaddr" ] && return 1
@@ -24,27 +22,27 @@ if_valid () (
     $DEBUG ifconfig $vif up
     $DEBUG vconfig add $vif $i 2>/dev/null
   }
-  ifconfig "$1" >/dev/null 2>&1 || [ "${1%%[0-9]}" = "br" ] 
+  ifconfig "$1" >/dev/null 2>&1 || [ "${1%%[0-9]}" = "br" ]
   return $?
 )
 
 wifi () (
   debug "### wifi $1 ###"
   if=$(awk 'gsub(":","") {print $1}' /proc/net/wireless)
-  $DEBUG wlconf $if $1 
+  $DEBUG wlconf $if $1
 )
 
 ifup () (
   type=$1
   debug "### ifup $type ###"
-  
+
   if=$(nvram_get ${type}_ifname)
   if [ "${if%%[0-9]}" = "ppp" ]; then
-    if=$(nvram_get pppoe_ifname) 
+    if=$(nvram_get pppoe_ifname)
   fi
-  
+
   if_valid $if || return
-  
+
   $DEBUG ifconfig $if down
   if [ "${if%%[0-9]}" = "br" ]; then
     stp=$(nvram_get ${type}_stp)
@@ -56,25 +54,25 @@ ifup () (
     for sif in $if_list; do {
       if_valid $sif || continue
       $DEBUG ifconfig $sif 0.0.0.0 up
-      $DEBUG brctl addif $if $sif 
+      $DEBUG brctl addif $if $sif
     } done
   fi
 
   if_mac=$(nvram_get ${type}_hwaddr)
   [ -z "$if_mac" ] || $DEBUG ifconfig $if hw ether $if_mac
- 
+
   if_proto=$(nvram_get ${type}_proto)
   case "$if_proto" in
     static)
       if_ip=$(nvram_get ${type}_ipaddr)
       if_netmask=$(nvram_get ${type}_netmask)
       if_gateway=$(nvram_get ${type}_gateway)
-      
-      ipcalc -s "$if_ip"      || return 
-      ipcalc -s "$if_netmask" || return 
+
+      ipcalc -s "$if_ip"      || return
+      ipcalc -s "$if_netmask" || return
       $DEBUG ifconfig $if $if_ip netmask $if_netmask up
 
-      ipcalc -s "$if_gateway" || return 
+      ipcalc -s "$if_gateway" || return
       $DEBUG route add default gw $if_gateway
 
       [ -f /etc/resolv.conf ] && return
@@ -97,9 +95,9 @@ ifup () (
       if_password=$(nvram_get ppp_passwd)
       if_redial=$(nvram_get ppp_redialperiod)
       if_idletime=$(nvram_get ppp_idletime)
-      
+
       $DEBUG ifconfig $if 0.0.0.0 up
-      
+
       $DEBUG /sbin/pppoecd $if -u $if_username -p $if_password -i 0 -I $if_redial -T $if_idletime -k
     ;;
     *)
@@ -108,10 +106,10 @@ ifup () (
   esac
 )
 
-ifdown () (                                                                       
+ifdown () (
   type=$1
   debug "### ifdown $type ###"
-  if=$(nvram_get ${type}_ifname)                                                
-  if_valid $if || return                                                        
+  if=$(nvram_get ${type}_ifname)
+  if_valid $if || return
   $DEBUG ifdown $if
 )
