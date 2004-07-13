9ac0b84f406a3df6c2e347f363b2535a56f9dacf
Mike Baker <mbm@openwrt.org>
2004-07-13 02:51:21 +0000

    split ifup/ifdown/wifi into standalone scripts
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@94 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- root/etc/functions.sh
+++ root/etc/functions.sh
@@ -16,97 +16,11 @@ if_valid () (
     hwaddr=$(nvram_get ${hwname}macaddr)
     [ -z "$hwaddr" ] && return 1
 
-    vif=$(ifconfig -a | awk '{IGNORECASE=1} /^eth.*'$hwaddr'/ {print $1; exit}')
+    vif=$(ifconfig -a | awk '/^eth.*'$hwaddr'/ {print $1; exit}' IGNORECASE=1)
     debug "# vlan$i: $hwname $hwaddr => $vif"
 
     $DEBUG ifconfig $vif up
     $DEBUG vconfig add $vif $i 2>/dev/null
   }
   ifconfig "$1" >/dev/null 2>&1 || [ "${1%%[0-9]}" = "br" ]
-)
-
-wifi () (
-  debug "### wifi $1 ###"
-  if=$(awk 'gsub(":","") {print $1}' /proc/net/wireless)
-  $DEBUG wlconf $if $1
-)
-
-ifup () (
-  type=$1
-  debug "### ifup $type ###"
-
-  if=$(nvram_get ${type}_ifname)
-  if [ "${if%%[0-9]}" = "ppp" ]; then
-    if=$(nvram_get pppoe_ifname)
-  fi
-
-  if_valid $if || return
-
-  $DEBUG ifconfig $if down
-  if [ "${if%%[0-9]}" = "br" ]; then
-    stp=$(nvram_get ${type}_stp)
-    $DEBUG brctl delbr $if
-    $DEBUG brctl addbr $if
-    $DEBUG brctl setfd $if 0
-    $DEBUG brctl stp $if ${stp:-0}
-    if_list=$(nvram_get ${type}_ifnames)
-    for sif in $if_list; do {
-      if_valid $sif || continue
-      $DEBUG ifconfig $sif 0.0.0.0 up
-      $DEBUG brctl addif $if $sif
-    } done
-  fi
-
-  if_mac=$(nvram_get ${type}_hwaddr)
-  ${if_mac:+$DEBUG ifconfig $if hw ether $if_mac}
-
-  if_proto=$(nvram_get ${type}_proto)
-  case "$if_proto" in
-    static)
-      if_ip=$(nvram_get ${type}_ipaddr)
-      if_netmask=$(nvram_get ${type}_netmask)
-      if_gateway=$(nvram_get ${type}_gateway)
-
-      $DEBUG ifconfig $if $if_ip ${if_netmask:+netmask $if_netmask} broadcast + up
-      ${if_gateway:+$DEBUG route add default gw $if_gateway}
-
-      [ -f /etc/resolv.conf ] && return
-
-      debug "# --- creating /etc/resolv.conf ---"
-      for dns in $(nvram_get ${type}_dns); do {
-	echo "nameserver $dns" >> /etc/resolv.conf
-      } done
-    ;;
-    dhcp)
-      pidfile=/tmp/dhcp-${type}.pid
-      if [ -f $pidfile ]; then
-        $DEBUG kill $(cat $pidfile)
-      fi
-      ${DEBUG:-eval} "udhcpc -i $if -b -p $pidfile &" 
-    ;;
-    pppoe)
-      if_username=$(nvram_get ppp_username)
-      if_password=$(nvram_get ppp_passwd)
-      if_redial=$(nvram_get ppp_redialperiod)
-      if_idletime=$(nvram_get ppp_idletime)
-      if_mtu=$(nvram_get wan_mtu)
-
-      $DEBUG ifconfig $if 0.0.0.0 up
-
-      $DEBUG /sbin/pppoecd $if -u $if_username -p $if_password \
-        -i 0 -I $if_redial -T $if_idletime -t $if_mtu -k
-    ;;
-    *)
-      echo "### WARNING $if: $if_proto is not supported"
-    ;;
-  esac
-)
-
-ifdown () (
-  type=$1
-  debug "### ifdown $type ###"
-  if=$(nvram_get ${type}_ifname)
-  if_valid $if || return
-  kill $(cat /var/run/${if}.pid 2>-) 2>-
-  $DEBUG ifconfig $if down
 )
