9ccc100b0e9734dc5c20cc2e65bd3385608eaeda
Imre Kaloz <kaloz@openwrt.org>
2004-11-15 08:19:53 +0000

    upgrade to bridge-utils 1.0.4
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@176 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- obsolete-buildroot/make/bridge.mk
+++ obsolete-buildroot/make/bridge.mk
@@ -8,7 +8,7 @@ ifeq ($(strip $(USE_BRIDGE_VERSION)),)
 USE_BRIDGE_VERSION=0.9.7
 endif
 BRIDGE_SOURCE_URL=http://dl.sourceforge.net/sourceforge/bridge/
-BRIDGE_SOURCE=bridge-utils-$(USE_BRIDGE_VERSION).tgz
+BRIDGE_SOURCE=bridge-utils-$(USE_BRIDGE_VERSION).tar.gz
 BRIDGE_BUILD_DIR=$(BUILD_DIR)/bridge-utils-$(USE_BRIDGE_VERSION)
 BRIDGE_TARGET_BINARY:=usr/sbin/brctl
 
@@ -40,7 +40,7 @@ $(BRIDGE_BUILD_DIR)/.configured: $(BRIDGE_BUILD_DIR)/.unpacked
 		--mandir=/usr/man \
 		--infodir=/usr/info \
 		$(DISABLE_NLS) \
-		--with-linux=$(LINUX_DIR) \
+		--with-linux-headers=$(LINUX_DIR) \
 	);
 	touch  $(BRIDGE_BUILD_DIR)/.configured
 
