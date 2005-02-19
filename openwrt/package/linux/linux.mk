a7e7276e228c6e01e7fe067d72161b34d090cd7c
Waldemar Brodkorb <wbx@openwrt.org>
2005-02-19 07:42:15 +0000

    sync with buildroot2 from uclibc, cleanup, extract linuxsource in kernel-headers
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@264 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- openwrt/package/linux/linux.mk
+++ openwrt/package/linux/linux.mk
@@ -26,47 +26,37 @@ LINUX_SITE=http://www.kernel.org/pub/linux/kernel/v2.4
 LINUX_SOURCE=linux-$(LINUX_VERSION).tar.bz2
 LINUX_KCONFIG=package/linux/linux.config
 LINUX_KERNEL=$(BUILD_DIR)/buildroot-kernel
+LINUX_PATCHES=$(PACKAGE_DIR)/linux/kernel-patches
 LINUX_BINLOC=arch/$(LINUX_KARCH)/brcm-boards/bcm947xx/compressed/vmlinuz
 # Used by pcmcia-cs and others
 LINUX_SOURCE_DIR=$(LINUX_DIR)
 
 # kernel stuff extracted from linksys firmware GPL sourcetree
 # WRT54GS_3_37_2_1109_US (shared,include,wl,et)
+LINKSYS_KERNEL_SITE=http://openwrt.openbsd-geek.de
 LINKSYS_KERNEL_TGZ=linksys-kernel.tar.gz
 
 TARGET_MODULES_DIR=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
 
-$(DL_DIR)/$(LINUX_SOURCE):
-	-mkdir -p $(DL_DIR)
-	$(WGET) -P $(DL_DIR) $(LINUX_SITE)/$(LINUX_SOURCE)
-
 $(DL_DIR)/$(LINKSYS_KERNEL_TGZ):
-	$(WGET) -P $(DL_DIR) $(LINKSYS_TGZ_SITE)/$(LINKSYS_KERNEL_TGZ)
+	$(WGET) -P $(DL_DIR) $(LINKSYS_KERNEL_SITE)/$(LINKSYS_KERNEL_TGZ)
 
 $(LINUX_DIR)/.unpacked: $(DL_DIR)/$(LINUX_SOURCE) $(DL_DIR)/$(LINKSYS_KERNEL_TGZ)
 	-mkdir -p $(BUILD_DIR)
-	(cd $(BUILD_DIR); tar jxvf $(DL_DIR)/$(LINUX_SOURCE))
-	-mkdir -p $(TOOL_BUILD_DIR)
-	-(cd $(TOOL_BUILD_DIR); ln -sf $(LINUX_DIR) linux)
-	toolchain/patch-kernel.sh $(LINUX_DIR) package/linux/kernel-patches
-ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS_LZMA),y)
-	toolchain/patch-kernel.sh $(LINUX_DIR) target/squashfs-lzma/kernel-patch
-endif
-	-cp $(LINUX_KCONFIG) $(LINUX_DIR)/.config
+	bzcat $(DL_DIR)/$(LINUX_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	# extract linksys binary kernel stuff and include/shared files
 	zcat $(DL_DIR)/$(LINKSYS_KERNEL_TGZ) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	touch $(LINUX_DIR)/.unpacked
 
-$(LINUX_KCONFIG):
-	@if [ ! -f "$(LINUX_KCONFIG)" ] ; then \
-		echo ""; \
-		echo "You should create a .config for your kernel"; \
-		echo "and install it as $(LINUX_KCONFIG)"; \
-		echo ""; \
-		sleep 5; \
-	fi;
+$(LINUX_DIR)/.patched: $(LINUX_DIR)/.unpacked
+	$(PATCH) $(LINUX_DIR) $(LINUX_PATCHES)
+ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS_LZMA),y)
+	$(PATCH) $(LINUX_DIR) target/squashfs-lzma/kernel-patch
+endif
+	touch $(LINUX_DIR)/.patched
 
-$(LINUX_DIR)/.configured:  $(LINUX_DIR)/.unpacked  $(LINUX_KCONFIG)
+$(LINUX_DIR)/.configured:  $(LINUX_DIR)/.patched
+	-cp $(LINUX_KCONFIG) $(LINUX_DIR)/.config
 	$(SED) "s,^CROSS_COMPILE.*,CROSS_COMPILE=$(KERNEL_CROSS),g;" $(LINUX_DIR)/Makefile
 	$(SED) "s,^CROSS_COMPILE.*,CROSS_COMPILE=$(KERNEL_CROSS),g;" $(LINUX_DIR)/arch/mips/Makefile
 	$(SED) "s,\-mcpu=,\-mtune=,g;" $(LINUX_DIR)/arch/mips/Makefile
@@ -91,19 +81,18 @@ $(LINUX_DIR)/.modules_done: $(LINUX_KERNEL)
 	tar -C $(BUILD_DIR)/modules/lib -cjf openwrt-kmodules.tar.bz2 modules
 	touch $(LINUX_DIR)/.modules_done
 
-
 $(STAGING_DIR)/include/linux/version.h: $(LINUX_DIR)/.configured
 	mkdir -p $(STAGING_DIR)/include
 	tar -ch -C $(LINUX_DIR)/include -f - linux | tar -xf - -C $(STAGING_DIR)/include/
 	tar -ch -C $(LINUX_DIR)/include -f - asm | tar -xf - -C $(STAGING_DIR)/include/
-
-linux: $(LINUX_DIR)/.modules_done $(TARGET_MODULES_DIR)
 
 $(TARGET_MODULES_DIR): 
 	-mkdir -p $(TARGET_MODULES_DIR)
 	cp $(LINUX_DIR)/drivers/net/wl/wl.o $(TARGET_MODULES_DIR)
 	cp $(LINUX_DIR)/drivers/net/et/et.o $(TARGET_MODULES_DIR)
 	cp $(LINUX_DIR)/drivers/net/diag/diag.o $(TARGET_MODULES_DIR)
+
+linux: $(LINUX_DIR)/.modules_done $(TARGET_MODULES_DIR)
 
 linux-source: $(DL_DIR)/$(LINUX_SOURCE)
 
@@ -115,6 +104,13 @@ linuxclean: clean
 linux-dirclean:
 	rm -rf $(LINUX_DIR)
 
-linux-headers: $(LINUX_DIR)/.configured 
+endif
 
+#############################################################
+#
+# Toplevel Makefile options
+#
+##############################################################
+ifeq ($(strip $(BR2_PACKAGE_LINUX)),y)
+TARGETS+=linux
 endif
