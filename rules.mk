3d1af731a6291ec1c845e5280f1f5bfd00a95b91
Nicolas Thill <nico@openwrt.org>
2005-03-06 11:02:08 +0000

    Change ipkg invocation and add -force-defaults option
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@313 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- rules.mk
+++ rules.mk
@@ -112,7 +112,8 @@ endif
 
 
 # invoke ipkg with configuration in $(STAGING_DIR)/etc/ipkg.conf 
-IPKG := PATH="$(TARGET_PATH)" IPKG_CONF_DIR=$(STAGING_DIR)/etc ipkg -force-depends
+IPKG := IPKG_CONF_DIR=$(STAGING_DIR)/etc $(TARGET_DIR)/bin/ipkg -force-defaults -force-depends
+IPKG_STATE_DIR := $(TARGET_DIR)/usr/lib/ipkg
 
 # invoke ipkg-build with some default options
 IPKG_BUILD := PATH="$(TARGET_PATH)" ipkg-build -c -o root -g root
