84c5431da3397174beb07f6170ab22a40accb32d
Felix Fietkau <nbd@openwrt.org>
2005-03-21 14:27:53 +0000

    clean linux kernel, when linux.config changes
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@408 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- openwrt/scripts/configtest.pl
+++ openwrt/scripts/configtest.pl
@@ -1,12 +1,26 @@
 #!/usr/bin/perl
-
 my %change = (
 	'BUSYBOX' => 'make -C package busybox-clean',
 	'' => 'make target_clean'
 );
 
+my @configfiles = (
+	['package/linux/linux.config', 'build_mipsel/linux/.config' =>
+		'make -C package linux-clean']
+);
+
+
+
 foreach my $change (keys %change) {
 	my $v1 = `grep '$change' .config.test`;
 	my $v2 = `grep '$change' .config`;
 	$v1 eq $v2 or system($change{$change});
+}
+
+foreach my $file (@configfiles) {
+	if ((-f $file->[0]) and (-f $file->[1])) {
+		my @s1 = stat $file->[0];
+		my @s2 = stat $file->[1];
+		$s1[9] > $s2[9] and system($file->[2]);
+	}
 }
