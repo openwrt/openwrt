b347ad8870f77f57b78581a0bff7a675cd8e2981
Felix Fietkau <nbd@openwrt.org>
2005-04-10 20:28:17 +0000

    add openwrt sdk
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@613 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ openwrt/package/sdk/files/depend.mk
@@ -0,0 +1,6 @@
+# You can put your package dependencies in here
+# Example (make openvpn depend on openssl):
+#	openvpn-compile: openssl-compile
+#
+# Note: This file is not present in the full buildroot. There you
+# have to put your package dependencies in buildroot/package/Makefile
