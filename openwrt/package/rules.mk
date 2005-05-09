f608b954528840d086c1aeb9d07cd2d73bd769de
Felix Fietkau <nbd@openwrt.org>
2005-05-09 09:33:23 +0000

    merge nico's package/rules.mk and package/templates.mk into one file, update packages that use this
    SVN-Revision: 817

--- openwrt/package/rules.mk
+++ openwrt/package/rules.mk
@@ -1,3 +1,30 @@
+define PKG_template
+IPKG_$(1):=$(PACKAGE_DIR)/$(2)_$(3)_$(4).ipk
+IDIR_$(1):=$(PKG_BUILD_DIR)/ipkg/$(2)
+INFO_$(1):=$(IPKG_STATE_DIR)/info/$(2).list
+
+ifneq ($(BR2_PACKAGE_$(1)),)
+compile: $$(IPKG_$(1))
+endif
+ifeq ($(BR2_PACKAGE_$(1)),y)
+install: $$(INFO_$(1))
+endif
+
+$$(IDIR_$(1))/CONTROL/control: $(PKG_BUILD_DIR)/.prepared
+	$(SCRIPT_DIR)/make-ipkg-dir.sh $$(IDIR_$(1)) ./ipkg/$(2).control $(3) $(4)
+	for file in conffiles preinst postinst prerm postrm; do \
+		[ -f ./ipkg/$(2).$$$$file ] && cp ./ipkg/$(2).$$$$file $$(IDIR_$(1))/CONTROL/$$$$file || true; \
+	done
+
+$$(INFO_$(1)): $$(IPKG_$(1))
+	$(IPKG) install $$(IPKG_$(1))
+
+$(2)-clean:
+	rm -f $$(IPKG_$(1))
+clean: $(2)-clean
+
+endef
+
 ifneq ($(strip $(PKG_SOURCE)),)
 $(DL_DIR)/$(PKG_SOURCE):
 	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(PKG_SOURCE) $(PKG_MD5SUM) $(PKG_SOURCE_URL)
@@ -17,10 +44,10 @@ all: compile
 
 source: $(DL_DIR)/$(PKG_SOURCE)
 prepare: $(PKG_BUILD_DIR)/.prepared
-compile: $(COMPILE_TARGETS)
-install: $(INSTALL_TARGETS)
+compile:
+install:
 
-clean: $(CLEAN_TARGETS)
-	rm -rf $(PKG_FILES) $(PKG_BUILD_DIR)
+clean: 
+	rm -rf $(PKG_BUILD_DIR)
 
 .PHONY: all source prepare compile install clean
