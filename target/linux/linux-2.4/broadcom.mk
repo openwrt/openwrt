d703a241d3e23944af9d50a9abcbe635f443ceb2
Felix Fietkau <nbd@openwrt.org>
2005-06-05 14:42:57 +0000

    add rebuild target for the linux directory
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1149 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/linux-2.4/broadcom.mk
+++ target/linux/linux-2.4/broadcom.mk
@@ -10,8 +10,8 @@ LINUX_BINARY_DRIVER_SITE=http://openwrt.org/downloads/
 # proprietary driver, extracted from asus GPL sourcetree GPL_1941.zip
 LINUX_BINARY_WL_DRIVER=kernel-binary-wl-0.3.tar.gz
 LINUX_BINARY_WL_MD5SUM=cc45df670bcfb4e74a709b9d7beba636
-LINUX_ET_DRIVER=kernel-source-et-0.9.tar.gz
-LINUX_ET_MD5SUM=eba31483e5ff4f8420827538ab452b7c
+LINUX_ET_DRIVER=kernel-source-et-0.10.tar.gz
+LINUX_ET_MD5SUM=408901f0b3c672ea0f353795391f07f6
 
 $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER):
 	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_BINARY_WL_DRIVER) $(LINUX_BINARY_WL_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
@@ -19,24 +19,21 @@ $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER):
 $(DL_DIR)/$(LINUX_ET_DRIVER):
 	$(SCRIPT_DIR)/download.pl $(DL_DIR) $(LINUX_ET_DRIVER) $(LINUX_ET_MD5SUM) $(LINUX_BINARY_DRIVER_SITE)
 	
-$(LINUX_DIR)/.patched: $(LINUX_DIR)/.drivers-unpacked
+$(LINUX_DIR)/.depend_done $(LINUX_DIR)/.modules_done: drivers-unpacked
 
-$(LINUX_DIR)/.drivers-unpacked:
+drivers-unpacked:
 	-mkdir -p $(BUILD_DIR)
 	zcat $(DL_DIR)/$(LINUX_BINARY_WL_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	zcat $(DL_DIR)/$(LINUX_ET_DRIVER) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	# copy binary wlan driver
-	mkdir $(LINUX_DIR)/drivers/net/{et,wl}
+	mkdir -p $(LINUX_DIR)/drivers/net/{et,wl}
 	cp -a $(BUILD_DIR)/wl/*.o $(LINUX_DIR)/drivers/net/wl
 	# copy proprietary et source
 	cp -a $(BUILD_DIR)/et/* $(LINUX_DIR)/drivers/net/et
 	mkdir -p $(LINUX_DIR)/arch/mips/bcm947xx/include/
 	cp -a $(BUILD_DIR)/et/*.h $(LINUX_DIR)/arch/mips/bcm947xx/include/
-	touch $@
 
 linux-dirclean: drivers-clean
 
 drivers-clean:
-	rm -rf $(BUILD_DIR)/wl
-	rm -rf $(BUILD_DIR)/et
-
+	rm -rf $(BUILD_DIR)/{wl,et}
