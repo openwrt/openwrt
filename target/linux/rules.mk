bc4ba23f31108bcbc47e24851f3ddd60aa20ffa7
Felix Fietkau <nbd@openwrt.org>
2005-06-11 15:36:31 +0000

    add board tag to kernel module version and add versioned depend (does not work with current ipkg yet)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1202 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/rules.mk
+++ target/linux/rules.mk
@@ -5,7 +5,13 @@ else
 KDEPEND_$(1):=$($(4))
 endif
 
-PKG_$(1) := $(PACKAGE_DIR)/kmod-$(2)_$(LINUX_VERSION)-$(PKG_RELEASE)_$(ARCH).ipk
+ifeq ($$(strip $(5)),)
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE))
+else
+IDEPEND_$(1):=kernel ($(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)), $(5)
+endif
+
+PKG_$(1) := $(PACKAGE_DIR)/kmod-$(2)_$(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE)_$(ARCH).ipk
 I_$(1) := $(PKG_BUILD_DIR)/ipkg/$(2)
 
 ifeq ($$(KDEPEND_$(1)),m)
@@ -18,8 +24,10 @@ endif
 endif
 
 $$(PKG_$(1)): $(LINUX_DIR)/.modules_done
+	rm -rf $$(I_$(1))
 	mkdir -p $$(I_$(1))/lib/modules/$(LINUX_VERSION)
-	$(SCRIPT_DIR)/make-ipkg-dir.sh $$(I_$(1)) ../control/kmod-$(2).control $(LINUX_VERSION)-$(PKG_RELEASE) $(ARCH)
+	$(SCRIPT_DIR)/make-ipkg-dir.sh $$(I_$(1)) ../control/kmod-$(2).control $(LINUX_VERSION)-$(BOARD)-$(PKG_RELEASE) $(ARCH)
+	echo "Depends: $$(IDEPEND_$(1))" >> $$(I_$(1))/CONTROL/control
 	cp $(3) $$(I_$(1))/lib/modules/$(LINUX_VERSION)
 	$(IPKG_BUILD) $$(I_$(1)) $(PACKAGE_DIR)
 
