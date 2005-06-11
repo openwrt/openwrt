f48b8046f0949f5d3efe1e79793e94821228e752
Felix Fietkau <nbd@openwrt.org>
2005-06-11 21:18:26 +0000

    cleanup in target/
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1205 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/image/jffs2.mk
@@ -0,0 +1,30 @@
+JFFS2OPTS :=  --pad --little-endian --squash
+#JFFS2OPTS += -Xlzo -msize -Xlzari
+
+jffs2-prepare:
+	$(MAKE) -C jffs2 prepare
+
+jffs2-compile:
+	$(MAKE) -C jffs2 compile
+
+jffs2-clean:
+	$(MAKE) -C jffs2 clean
+	rm -f $(KDIR)/root.jffs2*
+
+$(KDIR)/root.jffs2-4MB:
+	@rm -rf $(KDIR)/root/jffs
+	$(STAGING_DIR)/bin/mkfs.jffs2 $(JFFS2OPTS) -e 0x10000 -o $@ -d $(KDIR)/root
+
+$(KDIR)/root.jffs2-8MB:
+	@rm -rf $(KDIR)/root/jffs
+	$(STAGING_DIR)/bin/mkfs.jffs2 $(JFFS2OPTS) -e 0x20000 -o $@ -d $(KDIR)/root
+
+jffs2-install: $(KDIR)/root.jffs2-4MB $(KDIR)/root.jffs2-8MB
+	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="jffs2-4MB"
+	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="jffs2-8MB"
+
+prepare: jffs2-prepare
+compile: jffs2-compile
+install: jffs2-install
+clean: jffs2-clean
+
