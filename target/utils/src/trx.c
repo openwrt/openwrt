d1117f0919d903cf7c76ad09c4bda9c16ee2a363
Waldemar Brodkorb <wbx@openwrt.org>
2005-07-06 10:38:52 +0000

    allow bigger firmware, for customized firmware images
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1359 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/utils/src/trx.c
+++ target/utils/src/trx.c
@@ -62,7 +62,7 @@ uint32_t crc32buf(char *buf, size_t len);
 
 #define TRX_MAGIC	0x30524448	/* "HDR0" */
 #define TRX_VERSION	1
-#define TRX_MAX_LEN	0x3A0000
+#define TRX_MAX_LEN	0x5A0000
 #define TRX_NO_HEADER	1		/* Do not write TRX header */	
 
 struct trx_header {
