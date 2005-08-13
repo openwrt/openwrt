46752c424c55212cf0232aafc665d8af271979d5
Felix Fietkau <nbd@openwrt.org>
2005-08-13 16:15:24 +0000

    fix tgz building
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1632 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/image/tgz.mk
+++ target/linux/image/tgz.mk
@@ -1,9 +1,14 @@
 $(KDIR)/root.tar.gz:
 	tar -zcf $@ --owner=root --group=root -C $(KDIR)/root/ .
 
+ifeq ($(BOARD),soekris)
+install: tgz-install
+
 tgz-install: $(KDIR)/root.tar.gz
 	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="tgz"
+endif
 
 prepare:
 compile:
-install: tgz-install
+install: $(KDIR)/root.tar.gz
+
