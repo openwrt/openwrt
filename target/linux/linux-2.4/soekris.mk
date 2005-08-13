3ec350b6691c72eff44bb81024e70220c2e8314d
Nicolas Thill <nico@openwrt.org>
2005-08-13 07:34:47 +0000

    build a compressed kernel image to use with i386 bootloaders
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1619 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/linux-2.4/soekris.mk
+++ target/linux/linux-2.4/soekris.mk
@@ -9,3 +9,10 @@ $(eval $(call KMOD_template,WD1100,wd1100,\
 	$(MODULES_DIR)/kernel/drivers/char/wd1100.o \
 ,CONFIG_WD1100))
 
+
+$(LINUX_BUILD_DIR)/bzImage: $(LINUX_DIR)/vmlinux
+	$(MAKE) -C $(LINUX_DIR) TOPDIR=$(LINUX_DIR) ARCH=$(LINUX_KARCH) PATH=$(TARGET_PATH) bzImage
+	cp -f $(LINUX_DIR)/arch/i386/boot/bzImage $@
+	touch $@
+
+compile: $(LINUX_BUILD_DIR)/bzImage
