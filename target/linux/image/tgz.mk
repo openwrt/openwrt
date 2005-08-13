30bb8abe7e526545a6191ce22346cbbb6c902602
Nicolas Thill <nico@openwrt.org>
2005-08-13 07:30:53 +0000

    remove kernel image from rootfs archive, invoke image install like jffs2 and squashfs do
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@1617 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/image/tgz.mk
+++ target/linux/image/tgz.mk
@@ -1,14 +1,8 @@
-$(KDIR)/vmlinux.gz: $(KDIR)/vmlinux
-	gzip -c -vf9 < $< > $@
+$(KDIR)/root.tar.gz:
+	tar -zcf $@ --owner=root --group=root -C $(KDIR)/root/ .
 
-$(KDIR)/rootfs.tar.gz: $(KDIR)/vmlinux.gz
-	tar -cf $(KDIR)/rootfs.tar --owner=root --group=root -C $(KDIR)/ ./vmlinux.gz
-	tar -rf $(KDIR)/rootfs.tar --owner=root --group=root -C $(KDIR)/root/ .
-	gzip -f9 $(KDIR)/rootfs.tar
-
-tgz-install: $(KDIR)/rootfs.tar.gz
-	cp $(KDIR)/vmlinux.gz $(BIN_DIR)/openwrt-$(BOARD)-$(KERNEL)-vmlinux.gz
-	cp $(KDIR)/rootfs.tar.gz $(BIN_DIR)/openwrt-$(BOARD)-$(KERNEL)-rootfs.tar.gz
+tgz-install: $(KDIR)/root.tar.gz
+	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="tgz"
 
 prepare:
 compile:
