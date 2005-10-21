6a0c6639bec28210a2e277561849e30055433aa8
Felix Fietkau <nbd@openwrt.org>
2005-10-21 17:20:24 +0000

    update make-ipkg-dir.sh
    SVN-Revision: 2209

--- openwrt/scripts/make-ipkg-dir.sh
+++ openwrt/scripts/make-ipkg-dir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BASE=http://svn.openwrt.org/openwrt/trunk/openwrt
 TARGET=$1
 CONTROL=$2
 VERSION=$3
@@ -6,6 +7,13 @@ ARCH=$4
 
 mkdir -p "$TARGET/CONTROL"
 grep '^[^(Version|Architecture)]' "$CONTROL" > "$TARGET/CONTROL/control"
+grep '^Maintainer' "$CONTROL" 2>&1 >/dev/null || \
+        echo "Maintainer: OpenWrt Developers Team <openwrt-devel@openwrt.org>" >> "$TARGET/CONTROL/control"
+grep '^Source' "$CONTROL" 2>&1 >/dev/null || {
+        pkgbase=$(pwd | sed -e "s|^$TOPDIR/||g")
+        [ "$pkgbase" = "$WD" ] && src="N/A" || src="$BASE/$pkgbase"
+        echo "Source: $src" >> "$TARGET/CONTROL/control"
+}
 echo "Version: $VERSION" >> "$TARGET/CONTROL/control"
 echo "Architecture: $ARCH" >> "$TARGET/CONTROL/control"
 chmod 644 "$TARGET/CONTROL/control"
