970b7bcc234e88327961c1dbb7544452b4f2f528
Felix Fietkau <nbd@openwrt.org>
2005-10-22 20:47:19 +0000

    sync base-files with whiterussian
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@2260 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/default/etc/functions.sh
+++ package/base-files/default/etc/functions.sh
@@ -24,6 +24,56 @@ if_valid () (
   { debug "# missing interface '$1' ignored"; false; }
 )
 
+do_ifup() {
+	if_proto=$(nvram get ${2}_proto)
+	if=$(nvram get ${2}_ifname)
+	[ "${if%%[0-9]}" = "ppp" ] && if=$(nvram get ${if_proto}_ifname)
+	
+	pidfile=/var/run/${if}.pid
+	[ -f $pidfile ] && $DEBUG kill $(cat $pidfile)
+
+	case "$1" in
+	static)
+		ip=$(nvram get ${2}_ipaddr)
+		netmask=$(nvram get ${2}_netmask)
+		gateway=$(nvram get ${2}_gateway)
+
+		$DEBUG ifconfig $if $ip ${netmask:+netmask $netmask} broadcast + up
+		${gateway:+$DEBUG route add default gw $gateway}
+
+		[ -f /etc/resolv.conf ] && return
+
+		debug "# --- creating /etc/resolv.conf ---"
+		for dns in $(nvram get ${2}_dns); do
+			echo "nameserver $dns" >> /etc/resolv.conf
+		done
+	;;
+	dhcp)
+		DHCP_IP=$(nvram get ${2}_ipaddr)
+		DHCP_NETMASK=$(nvram get ${2}_netmask)
+		$DEBUG ifconfig $if $ip ${netmask:+netmask $netmask} broadcast + up
+
+		DHCP_ARGS="-i $if ${DHCP_IP:+-r $DHCP_IP} -b -p $pidfile"
+		DHCP_HOSTNAME=$(nvram get ${2}_hostname)
+		DHCP_HOSTNAME=${DHCP_HOSTNAME%%.*}
+		[ -z $DHCP_HOSTNAME ] || DHCP_ARGS="$DHCP_ARGS -H $DHCP_HOSTNAME"
+		[ "$if_proto" = "pptp" ] && DHCP_ARGS="$DHCP_ARGS -n -q" || DHCP_ARGS="$DHCP_ARGS -R &"
+		oldpid=$(cat $pidfile)
+		${DEBUG:-eval} "udhcpc $DHCP_ARGS"
+		pidof udhcpc | grep "$oldpid" >&- 2>&- && {
+			sleep 1
+			kill -9 $oldpid
+		}
+	;;
+	none|"")
+	;;
+	*)
+		[ -x "/sbin/ifup.$1" ] && { $DEBUG /sbin/ifup.$1 ${2}; exit; }
+		echo "### ifup ${2}: ignored ${2}_proto=\"$1\" (not supported)"
+	;;
+	esac
+}
+
 bitcount () {
   local c=$1
   echo $((
