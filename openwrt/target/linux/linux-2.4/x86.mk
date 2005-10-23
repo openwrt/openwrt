ed59c2703c1a57880a8c5054483617744848cf1d
Nicolas Thill <nico@openwrt.org>
2005-10-23 23:45:39 +0000

    add support for more network devices, start menuconfig reorg.
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@2294 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- openwrt/target/linux/linux-2.4/x86.mk
+++ openwrt/target/linux/linux-2.4/x86.mk
@@ -1,10 +1,7 @@
 # $Id$
 
-# soekris specific kmod packages
+# x86 specific kmod packages
 
-$(eval $(call KMOD_template,NATSEMI,natsemi,\
-	$(MODULES_DIR)/kernel/drivers/net/natsemi.o \
-,CONFIG_NATSEMI))
 $(eval $(call KMOD_template,WD1100,wd1100,\
 	$(MODULES_DIR)/kernel/drivers/char/wd1100.o \
 ,CONFIG_WD1100))
