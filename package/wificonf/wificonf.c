0df4877bd36602f5f6921c64da09a4a150b0ae8e
Felix Fietkau <nbd@openwrt.org>
2005-11-14 02:07:33 +0000

    add support for the new broadcom driver to wificonf. wep is currently broken. will fix that later, when i find out what's wrong with it
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk/openwrt@2480 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/wificonf/wificonf.c
+++ package/wificonf/wificonf.c
@@ -1,7 +1,7 @@
 /*
  * Wireless Network Adapter configuration utility
  *
- * Copyright (C) 2005 Felix Fietkau <nbd@vd-s.ath.cx>
+ * Copyright (C) 2005 Felix Fietkau <nbd@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,6 +21,9 @@
 #include <shutils.h>
 #include <wlioctl.h>
 #include <signal.h>
+
+#define	ADD_VIF_RETRIES	5
+#define DEBUG
 
 /*------------------------------------------------------------------*/
 /*
@@ -85,8 +88,17 @@ static int wpa_enc = 0;
 
 static char *wl_var(char *name)
 {
-	strcpy(buffer, prefix);
-	strcat(buffer, name);
+	sprintf(buffer, "%s_%s", prefix, name);
+	return buffer;
+}
+
+static char *vif_var(int vif, char *name)
+{
+	if (vif == 0)
+		return wl_var(name);
+	
+	sprintf(buffer, "%s.%d_%s", prefix, vif, name);
+	return buffer;
 }
 
 static int nvram_enabled(char *name)
@@ -114,11 +126,16 @@ static int bcom_ioctl(int skfd, char *ifname, int cmd, void *buf, int len)
 	strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
 
 	ret = ioctl(skfd, SIOCDEVPRIVATE, &ifr);
+	
+#ifdef DEBUG
+	if (ret < 0)
+		fprintf(stderr, "IOCTL %d failed: %d\n", cmd, ret);
+#endif
 
 	return ret;
 }
 
-static int bcom_set_val(int skfd, char *ifname, char *var, void *val, int len)
+static int bcom_set_var(int skfd, char *ifname, char *var, void *val, int len)
 {
 	char buf[8192];
 	int ret;
@@ -126,18 +143,128 @@ static int bcom_set_val(int skfd, char *ifname, char *var, void *val, int len)
 	if (strlen(var) + 1 > sizeof(buf) || len > sizeof(buf))
 		return -1;
 
+	bzero(buf, sizeof(buf));
 	strcpy(buf, var);
 	memcpy(&buf[strlen(var) + 1], val, len);
 	
-	if ((ret = bcom_ioctl(skfd, ifname, WLC_SET_VAR, buf, sizeof(buf))))
-		return ret;
+	ret = bcom_ioctl(skfd, ifname, WLC_SET_VAR, buf, sizeof(buf));
 
-	return 0;	
+#ifdef DEBUG
+	if (ret < 0)
+		fprintf(stderr, "SET_VAR %s failed: %d\n", var, ret);
+#endif
+	
+	return ret;
+}
+
+static int bcom_get_var(int skfd, char *ifname, char *var, void *buf, int len)
+{
+	int ret;
+	
+	if (strlen(var) + 1 > sizeof(buf) || len > sizeof(buf))
+		return -1;
+
+	bzero(buf, sizeof(buf));
+	strcpy(buf, var);
+	
+	ret = bcom_ioctl(skfd, ifname, WLC_GET_VAR, buf, sizeof(buf));
+
+#ifdef DEBUG
+	if (ret < 0)
+		fprintf(stderr, "GET_VAR %s failed: %d\n", var, ret);
+#endif
+	
+	return ret;
+}
+
+static int bcom_set_bss_var(int skfd, char *ifname, int bss, char *var, void *val, int len)
+{
+	char buf[8192];
+	int i = 0, ret;
+
+	bzero(buf, sizeof(buf));
+	if (strlen(var) + len + 8 > sizeof(buf) || len > sizeof(buf))
+		return -1;
+	
+	// "bsscfg:<name>\x00" <bss> <data>
+	i = sprintf(buf, "bsscfg:%s", var);
+	buf[i++] = 0;
+			
+	memcpy(buf + i, &bss, sizeof(bss));
+	i += sizeof(bss);
+	
+	memcpy(buf + i, val, len);
+	i += len;
+	
+	ret = bcom_ioctl(skfd, ifname, WLC_SET_VAR, buf, i);
+
+#ifdef DEBUG
+	if (ret < 0)
+		fprintf(stderr, "SET_BSS_VAR %s failed: %d\n", var, ret);
+#endif
+
+	return ret;
 }
 
 static int bcom_set_int(int skfd, char *ifname, char *var, int val)
 {
-	return bcom_set_val(skfd, ifname, var, &val, sizeof(val));
+	return bcom_set_var(skfd, ifname, var, &val, sizeof(val));
+}
+
+static int bcom_set_bss_int(int skfd, char *ifname, int bss, char *var, int val)
+{
+	return bcom_set_bss_var(skfd, ifname, bss, var, &val, sizeof(val));
+}
+
+static int is_new_bcom(int skfd, char *ifname)
+{
+	char buf[8192];
+	
+	bzero(buf, 8192);
+	bcom_ioctl(skfd, ifname, WLC_DUMP, buf, 8192);
+
+	if (strstr(buf, "3.130"))
+		return 1;
+
+	return 0;
+}
+
+static int bcom_get_wsec(int vif)
+{
+	int val; 
+
+	if (nvram_match(vif_var(vif, "crypto"), "tkip"))
+		val = TKIP_ENABLED;
+	else if (nvram_match(vif_var(vif, "crypto"), "aes"))
+		val = AES_ENABLED;
+	else if (nvram_match(vif_var(vif, "crypto"), "tkip+aes") || nvram_match(vif_var(vif, "crypto"), "aes+tkip"))
+		val = TKIP_ENABLED | AES_ENABLED;
+	else
+		val = 0;
+	
+	return val;
+}
+
+static int bcom_get_wauth(int vif)
+{
+	char *v, *next, var[80];
+	int res = 0;
+	
+	if (!(v = nvram_get(vif_var(vif, "akm"))))
+		v = nvram_safe_get(vif_var(vif, "auth_mode"));
+
+	foreach(var, v, next) {
+		if (strcmp(var, "psk") == 0)
+			res |= WPA_AUTH_PSK;
+		else if (strcmp(var, "psk2") == 0)
+			res |= WPA2_AUTH_PSK;
+		else if (strcmp(var, "wpa") == 0)
+			res |= WPA_AUTH_UNSPECIFIED;
+		else if (strcmp(var, "wpa2") == 0)
+			res |= WPA2_AUTH_UNSPECIFIED;
+	}
+
+	return res;
 }
 
 static void stop_bcom(int skfd, char *ifname)
@@ -163,7 +290,6 @@ static void start_bcom(int skfd, char *ifname)
 		return;
 
 	bcom_ioctl(skfd, ifname, WLC_UP, &val, sizeof(val));
-	set_wext_ssid(skfd, ifname);
 }
 
 static int setup_bcom_wds(int skfd, char *ifname)
@@ -191,10 +317,26 @@ static int setup_bcom_wds(int skfd, char *ifname)
 	return wds_enabled;
 }
 
+static void set_wext_mode(skfd, ifname)
+{
+	struct iwreq wrq;
+	int ap = 0, infra = 0, wet = 0;
+	
+	/* Set operation mode */
+	ap = !nvram_match(wl_var("mode"), "sta") && !nvram_match(wl_var("mode"), "wet");
+	infra = !nvram_disabled(wl_var("infra"));
+	wet = !ap && nvram_match(wl_var("mode"), "wet");
+
+	wrq.u.mode = (!infra ? IW_MODE_ADHOC : (ap ? IW_MODE_MASTER : (wet ? IW_MODE_REPEAT : IW_MODE_INFRA)));
+	IW_SET_EXT_ERR(skfd, ifname, SIOCSIWMODE, &wrq, "Set Mode");
+}
+
+
 void start_watchdog(int skfd, char *ifname)
 {
 	FILE *f;
-	unsigned char buf[8192], buf2[8192], wbuf[80], *v, *p, *next, *tmp;
+	char *v, *next;
+	unsigned char buf[8192], buf2[8192], wbuf[80], *p, *tmp;
 	int wds = 0, i, restart_wds;
 
 	if (fork())
@@ -242,20 +384,139 @@ void start_watchdog(int skfd, char *ifname)
 	}
 }
 
-static void setup_bcom(int skfd, char *ifname)
+static void setup_bcom_vif_sec(int skfd, char *ifname, int vif)
+{
+	int val, wep, wsec, i;
+	char *v;
+
+	wsec = bcom_get_wsec(vif);
+	if (wsec)
+		val = bcom_get_wauth(vif);
+	else
+		val = 0;
+
+	bcom_set_bss_int(skfd, ifname, vif, "wpa_auth", val);
+
+	if (val) {
+		if (WPA_AUTH_PSK | WPA2_AUTH_PSK) {
+			v = nvram_safe_get(wl_var("wpa_psk"));
+			if ((strlen(v) >= 8) && (strlen(v) < 63) && nvram_match(wl_var("mode"), "wet") && (vif == 0)) {
+				/* Enable in-driver WPA supplicant */
+				wsec_pmk_t pmk;
+			
+				pmk.key_len = (unsigned short) strlen(v);
+				pmk.flags = WSEC_PASSPHRASE;
+				strcpy(pmk.key, v);
+				bcom_ioctl(skfd, ifname, WLC_SET_WSEC_PMK, &pmk, sizeof(pmk));
+				bcom_set_int(skfd, ifname, "sup_wpa", 1);
+			} else {
+				bcom_set_int(skfd, ifname, "sup_wpa", 0);
+			}
+		}
+		bcom_set_bss_int(skfd, ifname, vif, "eap_restrict", 1);
+		bcom_set_bss_int(skfd, ifname, vif, "wsec", wsec);
+		bcom_set_bss_int(skfd, ifname, vif, "wsec_restrict", 1);
+	} else {
+		bcom_set_bss_int(skfd, ifname, vif, "eap_restrict", 0);
+		if (wep = nvram_enabled(vif_var(vif, "wep"))) {
+			wep = atoi(nvram_safe_get(vif_var(vif, "key")));
+			if ((wep >= 1) && (wep <= 4)) {
+				for (i = 1; i < 4; i++) {
+					wl_wsec_key_t k;
+					char name[5] = "key0";
+					unsigned char *kdata = k.data;
+					unsigned char *kstr;
+
+					bzero(&k, sizeof(k));
+					name[3] += i;
+					kstr = nvram_safe_get(vif_var(vif, name));
+					k.len = strlen(kstr);
+					if ((k.len == 10) || (k.len == 26)) {
+						k.index = i - 1;
+#ifdef DEBUG
+						fprintf(stderr, "Adding WEP key %d to VIF %d: ", i, vif);
+#endif
+						k.len = 0;
+						while (*kstr != 0) {
+							strncpy(name, kstr, 2);
+							name[2] = 0;
+							*kdata = (unsigned char) strtoul(name, NULL, 16);
+#ifdef DEBUG
+							fprintf(stderr, "%02x", *kdata);
+#endif
+							kstr += 2;
+							kdata++;
+							k.len++;
+						}
+#ifdef DEBUG
+						fprintf(stderr, "\n");
+#endif
+					} else {
+						k.len = 0;
+					}
+					if ((k.len > 0) && (i == wep))
+						k.flags = WL_PRIMARY_KEY;
+
+					bcom_set_bss_var(skfd, ifname, vif, "wsec_key", &k, sizeof(k));
+				}
+				wep = 1;
+				bcom_set_bss_int(skfd, ifname, vif, "wsec", WEP_ENABLED);
+				bcom_set_bss_int(skfd, ifname, vif, "wsec_restrict", 1);
+				bcom_set_bss_int(skfd, ifname, vif, "auth", 1);
+			} else {
+				wep = 0;
+			}
+		}
+	}
+	
+	if (!wep && !val) {
+		bcom_set_bss_int(skfd, ifname, vif, "wsec", 0);
+		bcom_set_bss_int(skfd, ifname, vif, "wsec_restrict", 0);
+	}
+	
+//	bcom_set_bss_int(skfd, ifname, vif, "auth", atoi(nvram_safe_get(vif_var(vif, "auth"))));
+}
+
+static void setup_bcom_vif(int skfd, char *ifname, int vif)
+{
+	int val, wep, wsec, i;
+	char *s, *v;
+	wlc_ssid_t ssid;
+	
+	s = nvram_safe_get(vif_var(vif, "ssid"));
+	strncpy(ssid.SSID, s, sizeof(ssid.SSID));
+	ssid.SSID_len = strlen(ssid.SSID);
+	ssid.SSID_len = ((ssid.SSID_len > sizeof(ssid.SSID)) ? sizeof(ssid.SSID) : ssid.SSID_len);
+	bcom_set_bss_var(skfd, ifname, vif, "ssid", &ssid, sizeof(ssid));
+	
+	val = nvram_enabled(vif_var(vif, "closed"));
+	bcom_set_bss_int(skfd, ifname, vif, "closednet", val);
+
+	val = nvram_enabled(wl_var("ap_isolate"));
+	bcom_set_bss_int(skfd, ifname, vif, "ap_isolate", val);
+
+}
+	
+static void start_bcom_vif(int skfd, char *ifname, int vif)
+{
+	int cfg[2];
+	int i;
+	
+	cfg[0] = vif;
+	cfg[1] = 1;
+	for (i = 0; i < ADD_VIF_RETRIES; i++) {
+		if (bcom_set_var(skfd, ifname, "bss" , cfg, sizeof(cfg)) == 0)
+			break;
+		usleep(1000 * 1000);
+	}
+}
+
+static void setup_bcom_common(int skfd, char *ifname)
 {
 	int val = 0, ap;
-	char buf[8192];
-	char wbuf[80];
-	char *v;
-	int wds_enabled = 0;
-	
-	if (bcom_ioctl(skfd, ifname, WLC_GET_MAGIC, &val, sizeof(val)) < 0)
-		return;
-	
+	char buf[8192], wbuf[80], *v;
+
 	nvram_set(wl_var("ifname"), ifname);
-	
-	stop_bcom(skfd, ifname);
 
 	/* Set Country */
 	strncpy(buf, nvram_safe_get(wl_var("country_code")), 4);
@@ -264,13 +525,8 @@ static void setup_bcom(int skfd, char *ifname)
 	
 	/* Set other options */
 	val = nvram_enabled(wl_var("lazywds"));
-	wds_enabled = val;
 	bcom_ioctl(skfd, ifname, WLC_SET_LAZYWDS, &val, sizeof(val));
-	
-	if (v = nvram_get(wl_var("frag"))) {
-		val = atoi(v);
-		bcom_ioctl(skfd, ifname, WLC_SET_FRAG, &val, sizeof(val));
-	}
+
 	if (v = nvram_get(wl_var("dtim"))) {
 		val = atoi(v);
 		bcom_ioctl(skfd, ifname, WLC_SET_DTIMPRD, &val, sizeof(val));
@@ -278,10 +534,6 @@ static void setup_bcom(int skfd, char *ifname)
 	if (v = nvram_get(wl_var("bcn"))) {
 		val = atoi(v);
 		bcom_ioctl(skfd, ifname, WLC_SET_BCNPRD, &val, sizeof(val));
-	}
-	if (v = nvram_get(wl_var("rts"))) {
-		val = atoi(v);
-		bcom_ioctl(skfd, ifname, WLC_SET_RTS, &val, sizeof(val));
 	}
 	if (v = nvram_get(wl_var("antdiv"))) {
 		val = atoi(v);
@@ -291,15 +543,34 @@ static void setup_bcom(int skfd, char *ifname)
 		val = atoi(v);
 		bcom_ioctl(skfd, ifname, WLC_SET_TXANT, &val, sizeof(val));
 	}
+	if (v = nvram_get(wl_var("maxassoc"))) {
+		val = atoi(v);
+		bcom_set_int(skfd, ifname, "maxassoc", val);
+	}
 	
-	val = nvram_enabled(wl_var("closed"));
-	bcom_ioctl(skfd, ifname, WLC_SET_CLOSED, &val, sizeof(val));
-
-	val = nvram_enabled(wl_var("ap_isolate"));
-	bcom_set_int(skfd, ifname, "ap_isolate", val);
-
 	val = nvram_enabled(wl_var("frameburst"));
 	bcom_ioctl(skfd, ifname, WLC_SET_FAKEFRAG, &val, sizeof(val));
+
+	ap = !nvram_match(wl_var("mode"), "sta") && !nvram_match(wl_var("mode"), "wet");
+
+	if (ap)
+		val = setup_bcom_wds(skfd, ifname);
+
+	if ((!ap || val) && is_new_bcom(skfd, ifname))
+		start_watchdog(skfd, ifname);
+	
+	/* Set up afterburner, disabled it if WDS is enabled */
+	if (val || nvram_enabled(wl_var("lazywds"))) {
+		val = ABO_OFF;
+	} else {
+		val = ABO_AUTO;
+		if (nvram_enabled(wl_var("afterburner")))
+			val = ABO_ON;
+		if (nvram_disabled(wl_var("afterburner")))
+			val = ABO_OFF;
+	}
+	
+	bcom_set_var(skfd, ifname, "afterburner_override", &val, sizeof(val));
 
 	/* Set up MAC list */
 	if (nvram_match(wl_var("macmode"), "allow"))
@@ -330,25 +601,6 @@ static void setup_bcom(int skfd, char *ifname)
 	}
 	bcom_ioctl(skfd, ifname, WLC_SET_MACMODE, &val, sizeof(val));
 
-	if (ap = !nvram_match(wl_var("mode"), "sta") && !nvram_match(wl_var("mode"), "wet"))
-		wds_enabled = setup_bcom_wds(skfd, ifname);
-
-	if (!ap || wds_enabled)	
-		start_watchdog(skfd, ifname);
-	
-	/* Set up afterburner, disabled it if WDS is enabled */
-	if (wds_enabled) {
-		val = ABO_OFF;
-	} else {
-		val = ABO_AUTO;
-		if (nvram_enabled(wl_var("afterburner")))
-			val = ABO_ON;
-		if (nvram_disabled(wl_var("afterburner")))
-			val = ABO_OFF;
-	}
-	
-	bcom_set_val(skfd, ifname, "afterburner_override", &val, sizeof(val));
-	
 	/* Set up G mode */
 	bcom_ioctl(skfd, ifname, WLC_GET_PHYTYPE, &val, sizeof(val));
 	if (val == 2) {
@@ -385,53 +637,139 @@ static void setup_bcom(int skfd, char *ifname)
 			bcom_ioctl(skfd, ifname, WLC_SET_PLCPHDR, &val, sizeof(val));
 		}
 	}
+}
 
+static void setup_bcom_new(int skfd, char *ifname)
+{
+	int val = 0, i;
+	int iface[16], ifaces = 1;
+	int ap, apsta, sta, wet;
+	char *v;
+
+	if (bcom_ioctl(skfd, ifname, WLC_GET_MAGIC, &val, sizeof(val)) < 0) 
+		return;
+	
+	/* Clear all VIFs */
+	iface[0] = 0;
+	for (i = 0; i < 16; i++) {
+		int cfg[2]; /* index, enabled */
+		
+		cfg[0] = i;
+		cfg[1] = 0;
+		
+		bcom_set_var(skfd, ifname, "bss", cfg, sizeof(cfg));
+		
+		if ((i > 0) && nvram_enabled(vif_var(i, "enabled")) && (i == 0 || nvram_get(vif_var(i, "ssid")))) {
+			iface[ifaces] = i;
+			ifaces++;
+		}
+	}
+
+	set_wext_mode(skfd, ifname);
+	
+	ap = nvram_match(wl_var("mode"), "ap") || nvram_match(wl_var("mode"), "apsta");
+	apsta = nvram_match(wl_var("mode"), "apsta");
+	sta = nvram_match(wl_var("mode"), "sta");
+
+	bcom_set_int(skfd, ifname, "apsta", apsta);
+	bcom_set_int(skfd, ifname, "mssid", (ifaces > 1));
+
+	for (i = 0; i < (sta ? 0 : ifaces); i++) {
+#ifdef DEBUG
+		fprintf(stderr, "setup_bcom_vif(%d) start\n", iface[i]);
+#endif
+		setup_bcom_vif(skfd, ifname, iface[i]);
+#ifdef DEBUG
+		fprintf(stderr, "setup_bcom_vif(%d) end\n", iface[i]);
+#endif
+	}
+
+	
+	if (v = nvram_get(wl_var("rts"))) {
+		val = atoi(v);
+		bcom_set_int(skfd, ifname, "rtsthresh", val);
+	}
+	if (v = nvram_get(wl_var("frag"))) {
+		val = atoi(v);
+		bcom_set_int(skfd, ifname, "fragthresh", val);
+	}
+
+	val = (nvram_disabled(wl_var("radio")) ? (1 | (1 << 16)) : 0);
+	bcom_ioctl(skfd, ifname, WLC_SET_RADIO, &val, sizeof(val));
+
+	setup_bcom_common(skfd, ifname);
 	start_bcom(skfd, ifname);
 
-	if (!(v = nvram_get(wl_var("akm"))))
-		v = nvram_safe_get(wl_var("auth_mode"));
+	val = atoi(nvram_safe_get(wl_var("channel")));
+	if (val > 0)
+		bcom_ioctl(skfd, ifname, WLC_SET_CHANNEL, &val, sizeof(val));
 	
-	if (strstr(v, "wpa") || strstr(v, "psk")) {
-		wpa_enc = 1;
+	val = (ap ? 15 : 0);
+	bcom_ioctl(skfd, ifname, WLC_SET_CS_SCAN_TIMER, &val, sizeof(val));
+	
+	for (i = 0; i < (sta ? 0 : ifaces); i++) {
+		setup_bcom_vif_sec(skfd, ifname, iface[i]);
+	}
 
-		/* Set up WPA */
-		if (nvram_match(wl_var("crypto"), "tkip"))
-			val = TKIP_ENABLED;
-		else if (nvram_match(wl_var("crypto"), "aes"))
-			val = AES_ENABLED;
-		else if (nvram_match(wl_var("crypto"), "tkip+aes") || nvram_match(wl_var("crypto"), "aes+tkip"))
-			val = TKIP_ENABLED | AES_ENABLED;
-		else
-			val = 0;
-		bcom_ioctl(skfd, ifname, WLC_SET_WSEC, &val, sizeof(val));
+	for (i = 0; i < (sta ? 0 : ifaces); i++) {
+		start_bcom_vif(skfd, ifname, iface[i]);
+	}
+}
 
-		if (val && strstr(v, "psk")) {
-			val = (strstr(v, "psk2") ? 0x84 : 0x4);
-			v = nvram_safe_get(wl_var("wpa_psk"));
-			if ((strlen(v) >= 8) && (strlen(v) < 63)) {
-				
-				bcom_ioctl(skfd, ifname, WLC_SET_WPA_AUTH, &val, sizeof(val));
-				
-				if (!nvram_match(wl_var("mode"), "wet")) {
-					/* Enable in-driver WPA supplicant */
-					wsec_pmk_t pmk;
-					
-					pmk.key_len = (unsigned short) strlen(v);
-					pmk.flags = WSEC_PASSPHRASE;
-					strcpy(pmk.key, v);
-					bcom_ioctl(skfd, ifname, WLC_SET_WSEC_PMK, &pmk, sizeof(pmk));
-					bcom_set_int(skfd, ifname, "sup_wpa", 1);
-				}
-			}
-		} else  {
-			val = 1;
-			bcom_ioctl(skfd, ifname, WLC_SET_EAP_RESTRICT, &val, sizeof(val));
+static void setup_bcom_old(int skfd, char *ifname)
+{
+	int val = 0, i;
+	char buf[8192];
+	char wbuf[80];
+	char *v;
+	
+	if (bcom_ioctl(skfd, ifname, WLC_GET_MAGIC, &val, sizeof(val)) < 0)
+		return;
+	
+	setup_bcom_common(skfd, ifname);
+
+	if (v = nvram_get(wl_var("frag"))) {
+		val = atoi(v);
+		bcom_ioctl(skfd, ifname, WLC_SET_FRAG, &val, sizeof(val));
+	}
+	if (v = nvram_get(wl_var("rts"))) {
+		val = atoi(v);
+		bcom_ioctl(skfd, ifname, WLC_SET_RTS, &val, sizeof(val));
+	}
+	
+	val = nvram_enabled(wl_var("closed"));
+	bcom_ioctl(skfd, ifname, WLC_SET_CLOSED, &val, sizeof(val));
+
+	val = nvram_enabled(wl_var("ap_isolate"));
+	bcom_set_int(skfd, ifname, "ap_isolate", val);
+
+	start_bcom(skfd, ifname);
+	set_wext_ssid(skfd, ifname);
+
+	val = bcom_get_wauth(0);
+	bcom_ioctl(skfd, ifname, WLC_SET_WPA_AUTH, &val, sizeof(val));
+
+	if (val & (WPA_AUTH_PSK | WPA2_AUTH_PSK)) {
+		v = nvram_safe_get(wl_var("wpa_psk"));
+		if ((strlen(v) >= 8) && (strlen(v) < 63) && nvram_match(wl_var("mode"), "wet")) {
+			/* Enable in-driver WPA supplicant */
+			wsec_pmk_t pmk;
+			
+			pmk.key_len = (unsigned short) strlen(v);
+			pmk.flags = WSEC_PASSPHRASE;
+			strcpy(pmk.key, v);
+			bcom_ioctl(skfd, ifname, WLC_SET_WSEC_PMK, &pmk, sizeof(pmk));
+			bcom_set_int(skfd, ifname, "sup_wpa", 1);
 		}
+	}
+	if (val) {
+		val = 1;
+		bcom_ioctl(skfd, ifname, WLC_SET_EAP_RESTRICT, &val, sizeof(val));
+		val = bcom_get_wsec(0);
+		bcom_ioctl(skfd, ifname, WLC_SET_WSEC, &val, sizeof(val));
 	} else {
 		val = 0;
-
 		bcom_ioctl(skfd, ifname, WLC_SET_WSEC, &val, sizeof(val));
-		bcom_ioctl(skfd, ifname, WLC_SET_WPA_AUTH, &val, sizeof(val));
 		bcom_ioctl(skfd, ifname, WLC_SET_EAP_RESTRICT, &val, sizeof(val));
 		bcom_set_int(skfd, ifname, "sup_wpa", 0);
 	}
@@ -487,20 +825,6 @@ static void setup_wext_wep(int skfd, char *ifname)
 	}
 }
 
-static void set_wext_mode(skfd, ifname)
-{
-	struct iwreq wrq;
-	int ap = 0, infra = 0, wet = 0;
-	
-	/* Set operation mode */
-	ap = !nvram_match(wl_var("mode"), "sta") && !nvram_match(wl_var("mode"), "wet");
-	infra = !nvram_disabled(wl_var("infra"));
-	wet = !ap && nvram_match(wl_var("mode"), "wet");
-
-	wrq.u.mode = (!infra ? IW_MODE_ADHOC : (ap ? IW_MODE_MASTER : (wet ? IW_MODE_REPEAT : IW_MODE_INFRA)));
-	IW_SET_EXT_ERR(skfd, ifname, SIOCSIWMODE, &wrq, "Set Mode");
-}
-
 static void setup_wext(int skfd, char *ifname)
 {
 	char *buffer;
@@ -518,7 +842,6 @@ static void setup_wext(int skfd, char *ifname)
 		wrq.u.freq.m = channel;
 		IW_SET_EXT_ERR(skfd, ifname, SIOCSIWFREQ, &wrq, "Set Frequency");
 	}
-
 
 	/* Disable radio if wlX_radio is set and not enabled */
 	wrq.u.txpower.disabled = nvram_disabled(wl_var("radio"));
@@ -552,10 +875,27 @@ static int setup_interfaces(int skfd, char *ifname, char *args[], int count)
 		set_wext_mode(skfd, ifname);
 		setup_wext(skfd, ifname);
 	} else {
-		stop_bcom(skfd, ifname);
-		set_wext_mode(skfd, ifname);
-		setup_bcom(skfd, ifname);
-		setup_wext(skfd, ifname);
+		if (is_new_bcom(skfd, ifname)) {
+#ifdef DEBUG
+			fprintf(stderr, "New Broadcom driver detected.\n");
+#endif
+			stop_bcom(skfd, ifname);
+#ifdef DEBUG
+			fprintf(stderr, "Setup start.\n");
+#endif
+			setup_bcom_new(skfd, ifname);
+#ifdef DEBUG
+			fprintf(stderr, "Setup done.\n");
+#endif
+		} else {
+#ifdef DEBUG
+			fprintf(stderr, "Old Broadcom driver detected.\n");
+#endif
+			stop_bcom(skfd, ifname);
+			set_wext_mode(skfd, ifname);
+			setup_bcom_old(skfd, ifname);
+			setup_wext(skfd, ifname);
+		}
 	}
 	
 	prefix[2]++;
@@ -569,7 +909,7 @@ int main(int argc, char **argv)
 		exit(-1);
 	}
 
-	prefix = strdup("wl0_");
+	prefix = strdup("wl0");
 	iw_enum_devices(skfd, &setup_interfaces, NULL, 0);
 	
 	return 0;
