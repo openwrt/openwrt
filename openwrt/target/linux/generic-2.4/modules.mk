ecc5aefc05f8ae64ea419b1fe203961e81b2d301
Imre Kaloz <kaloz@openwrt.org>
2006-01-13 10:02:03 +0000

    clean up kernel modules, remove b44 references, as it's built-in now
    SVN-Revision: 2945

--- openwrt/target/linux/generic-2.4/modules.mk
+++ openwrt/target/linux/generic-2.4/modules.mk
@@ -201,10 +201,6 @@ $(eval $(call KMOD_template,NET_AIRO,net-airo,\
 	$(MODULES_DIR)/kernel/drivers/net/wireless/airo.o \
 ,CONFIG_AIRO,,10,airo))
 
-$(eval $(call KMOD_template,NET_B44,net-b44,\
-	$(MODULES_DIR)/kernel/drivers/net/b44.o \
-,CONFIG_B44,,10,b44))
-
 $(eval $(call KMOD_template,NET_E100,net-e100,\
 	$(MODULES_DIR)/kernel/drivers/net/e100.o \
 ,CONFIG_E100,,10,e100))
