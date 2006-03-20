1563d7a9c5db8ec48985ec1d9360d10cc45549d9
Waldemar Brodkorb <wbx@openwrt.org>
2006-03-20 18:08:46 +0000

    add mirror1 again. sponsored by FH BRS, Lab for telecommunications
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@3418 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- openwrt/scripts/download.pl
+++ openwrt/scripts/download.pl
@@ -84,8 +84,7 @@ foreach my $mirror (@ARGV) {
 	}
 }
 
-#push @mirrors, 'http://mirror1.openwrt.org/';
-#push @mirrors, 'http://mirror2.openwrt.org/';
+push @mirrors, 'http://mirror1.openwrt.org/';
 push @mirrors, 'http://downloads.openwrt.org/sources/';
 
 while (!$ok) {
