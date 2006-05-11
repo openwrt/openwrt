3b1260256e6390404239df879f3ff0ff70ce65ca
Mike Baker <mbm@openwrt.org>
2006-05-11 22:36:20 +0000

    cleanup & add whitespace
    SVN-Revision: 3764

--- openwrt/package/rules.mk
+++ openwrt/package/rules.mk
@@ -1,117 +1,115 @@
 ifneq ($(DUMP),)
-all: dumpinfo
+  all: dumpinfo
 else
-all: compile
+  all: compile
 endif
 
 define Build/DefaultTargets
-$(PKG_BUILD_DIR)/.prepared: FORCE $(DL_DIR)/$(PKG_SOURCE)
-ifeq ($(shell $(SCRIPT_DIR)/timestamp.pl -p $(PKG_BUILD_DIR) .),.)
+  prepared: FORCE $(DL_DIR)/$(PKG_SOURCE)
+   ifeq ($(shell $(SCRIPT_DIR)/timestamp.pl -p $(PKG_BUILD_DIR) .),.)
 	@-rm -rf $(PKG_BUILD_DIR)
 	@mkdir -p $(PKG_BUILD_DIR)
 	$(call Build/Prepare)
-	@touch $$@
-endif
+    endif
 
-$(PKG_BUILD_DIR)/.configured: $(PKG_BUILD_DIR)/.prepared
+  $(PKG_BUILD_DIR)/.configured: prepared
 	$(call Build/Configure)
 	touch $$@
 
-$(PKG_BUILD_DIR)/.built: FORCE $(PKG_BUILD_DIR)/.configured
-#$#$#(error $$(shell $(SCRIPT_DIR)/timestamp.pl -p -x ipkg $$(IPKG_$(1)) $(PKG_BUILD_DIR)))
-ifeq ($$(shell $(SCRIPT_DIR)/timestamp.pl -p -x ipkg $$(IPKG_$(1)) $(PKG_BUILD_DIR)),$(PKG_BUILD_DIR))
+  built: FORCE $(PKG_BUILD_DIR)/.configured
+    ifneq ($$(shell $(SCRIPT_DIR)/timestamp.pl -p -x ipkg $(IPKG_$(1)) $(PKG_BUILD_DIR)),$(IPKG_$(1)))
 	$(call Build/Compile)
-	touch $$@
-endif
+    endif
 
-package-clean: FORCE
+  package-clean: FORCE
 	$(call Build/Clean)
-	rm -f $(PKG_BUILD_DIR)/.built
 
-define Build/DefaultTargets
-endef
+  define Build/DefaultTargets
+  endef
 endef
 
 define Package/Default
-CONFIGFILE:=
-SECTION:=opt
-CATEGORY:=Extra packages
-DEPENDS:=
-MAINTAINER:=OpenWrt Developers Team <openwrt-devel@openwrt.org>
-SOURCE:=$(patsubst $(TOPDIR)/%,%,${shell pwd})
-VERSION:=$(PKG_VERSION)-$(PKG_RELEASE)
-PKGARCH:=$(ARCH)
-PRIORITY:=optional
-DEFAULT:=
-MENU:=
-TITLE:=
-DESCRIPTION:=
-endef
-
-define RequiredField
-ifeq ($$($(1)),)
-$$(error Package/$$(1) is missing the $(1) field)
-endif
+  CONFIGFILE:=
+  SECTION:=opt
+  CATEGORY:=Extra packages
+  DEPENDS:=
+  MAINTAINER:=OpenWrt Developers Team <openwrt-devel@openwrt.org>
+  SOURCE:=$(patsubst $(TOPDIR)/%,%,${shell pwd})
+  VERSION:=$(PKG_VERSION)-$(PKG_RELEASE)
+  PKGARCH:=$(ARCH)
+  PRIORITY:=optional
+  DEFAULT:=
+  MENU:=
+  TITLE:=
+  DESCRIPTION:=
 endef
 
 define BuildPackage
-$(eval $(call Package/Default))
-$(eval $(call Package/$(1)))
+  $(eval $(call Package/Default))
+  $(eval $(call Package/$(1)))
 
-$(foreach FIELD, TITLE CATEGORY PRIORITY VERSION, $(eval $(call RequiredField,$(FIELD))))
+  $(foreach FIELD, TITLE CATEGORY PRIORITY VERSION,
+    ifeq ($($(FIELD)),)
+      $$(error Package/$(1) is missing the $(FIELD) field)
+    endif
+  )
 
-ifeq ($(PKGARCH),)
-PKGARCH:=$(ARCH)
-endif
+  ifeq ($(PKGARCH),)
+    PKGARCH:=$(ARCH)
+  endif
 
-ifeq ($(DESCRIPTION),)
-$(eval DESCRIPTION:=$(TITLE))
-endif
+  ifeq ($(DESCRIPTION),)
+    $(eval DESCRIPTION:=$(TITLE))
+  endif
 
-IPKG_$(1):=$(PACKAGE_DIR)/$(1)_$(VERSION)_$(PKGARCH).ipk
-IDIR_$(1):=$(PKG_BUILD_DIR)/ipkg/$(1)
-INFO_$(1):=$(IPKG_STATE_DIR)/info/$(1).list
+  IPKG_$(1):=$(PACKAGE_DIR)/$(1)_$(VERSION)_$(PKGARCH).ipk
+  IDIR_$(1):=$(PKG_BUILD_DIR)/ipkg/$(1)
+  INFO_$(1):=$(IPKG_STATE_DIR)/info/$(1).list
 
-ifneq ($(CONFIG_PACKAGE_$(1))$(DEVELOPER),)
-COMPILE_$(1):=1
-endif
+  ifneq ($(CONFIG_PACKAGE_$(1))$(DEVELOPER),)
+    COMPILE_$(1):=1
+  endif
 
-ifeq ($(CONFIG_PACKAGE_$(1)),y)
-install-targets: $$(INFO_$(1))
-endif
+  ifeq ($(CONFIG_PACKAGE_$(1)),y)
+    install-targets: $$(INFO_$(1))
+  endif
 
-ifneq ($(COMPILE_$(1)),)
-compile-targets: $(IPKG_$(1))
-endif
+  ifneq ($(COMPILE_$(1)),)
+    compile-targets: $(IPKG_$(1))
+  endif
 
-IDEPEND_$(1):=$$(strip $$(DEPENDS))
+  IDEPEND_$(1):=$$(strip $$(DEPENDS))
 
-DUMPINFO += \
+  DUMPINFO += \
 	echo "Package: $(1)"; 
-ifneq ($(MENU),)
-DUMPINFO += \
+
+  ifneq ($(MENU),)
+    DUMPINFO += \
 	echo "Menu: $(MENU)";
-endif
-ifneq ($(DEFAULT),)
-DUMPINFO += \
+  endif
+
+  ifneq ($(DEFAULT),)
+    DUMPINFO += \
 	echo "Default: $(DEFAULT)";
-endif
-DUMPINFO += \
+  endif
+
+  DUMPINFO += \
 	echo "Version: $(VERSION)"; \
 	echo "Depends: $$(IDEPEND_$(1))"; \
 	echo "Category: $(CATEGORY)"; \
 	echo "Title: $(TITLE)"; \
 	echo "Description: $(DESCRIPTION)" | sed -e 's,\\,\n,g';
-ifneq ($(URL),)
-DUMPINFO += \
+
+  ifneq ($(URL),)
+    DUMPINFO += \
 	echo; \
 	echo "$(URL)";
-endif
-DUMPINFO += \
+  endif
+
+  DUMPINFO += \
 	echo "@@";
 
-
-$$(IDIR_$(1))/CONTROL/control: $(PKG_BUILD_DIR)/.prepared
+  $$(IDIR_$(1))/CONTROL/control: prepared
 	mkdir -p $$(IDIR_$(1))/CONTROL
 	echo "Package: $(1)" > $$(IDIR_$(1))/CONTROL/control
 	echo "Version: $(VERSION)" >> $$(IDIR_$(1))/CONTROL/control
@@ -127,32 +125,33 @@ $$(IDIR_$(1))/CONTROL/control: $(PKG_BUILD_DIR)/.prepared
 		[ -f ./ipkg/$(1).$$$$file ] && cp ./ipkg/$(1).$$$$file $$(IDIR_$(1))/CONTROL/$$$$file || true; \
 	done
 
-$$(IPKG_$(1)): $$(IDIR_$(1))/CONTROL/control $(PKG_BUILD_DIR)/.built
+  $$(IPKG_$(1)): $$(IDIR_$(1))/CONTROL/control built
 	$(call Package/$(1)/install,$$(IDIR_$(1)))
 	mkdir -p $(PACKAGE_DIR)
 	$(RSTRIP) $$(IDIR_$(1))
 	$(IPKG_BUILD) $$(IDIR_$(1)) $(PACKAGE_DIR)
 
-$$(INFO_$(1)): $$(IPKG_$(1))
+  $$(INFO_$(1)): $$(IPKG_$(1))
 	$(IPKG) install $$(IPKG_$(1))
 
-$(1)-clean:
+  $(1)-clean:
 	rm -f $(PACKAGE_DIR)/$(1)_*
-clean: $(1)-clean
 
-ifneq ($(__DEFAULT_TARGETS),1)
-$$(eval $$(call Build/DefaultTargets,$(1)))
-endif
+  clean: $(1)-clean
+
+  ifneq ($(__DEFAULT_TARGETS),1)
+    $$(eval $$(call Build/DefaultTargets,$(1)))
+  endif
 
 endef
 
 ifneq ($(strip $(PKG_SOURCE)),)
-$(DL_DIR)/$(PKG_SOURCE):
+  $(DL_DIR)/$(PKG_SOURCE):
 	$(SCRIPT_DIR)/download.pl "$(DL_DIR)" "$(PKG_SOURCE)" "$(PKG_MD5SUM)" $(PKG_SOURCE_URL)
 endif
 
 ifneq ($(strip $(PKG_CAT)),)
-define Build/Prepare/Default
+  define Build/Prepare/Default
 	@if [ "$(PKG_CAT)" = "unzip" ]; then \
 		unzip -d $(PKG_BUILD_DIR) $(DL_DIR)/$(PKG_SOURCE) ; \
 	else \
@@ -161,11 +160,11 @@ define Build/Prepare/Default
 	@if [ -d ./patches ]; then \
 		$(PATCH) $(PKG_BUILD_DIR) ./patches ; \
 	fi
-endef
+  endef
 endif
 
 define Build/Prepare
-$(call Build/Prepare/Default)
+  $(call Build/Prepare/Default)
 endef
 
 define Build/Configure/Default
@@ -193,7 +192,7 @@ define Build/Configure/Default
 endef
 
 define Build/Configure
-$(call Build/Configure/Default,)
+  $(call Build/Configure/Default,)
 endef
 
 define Build/Compile/Default
@@ -207,7 +206,7 @@ define Build/Compile/Default
 endef
 
 define Build/Compile
-$(call Build/Compile/Default)
+  $(call Build/Compile/Default)
 endef
 
 define Build/Clean
@@ -215,25 +214,25 @@ define Build/Clean
 endef
 
 ifneq ($(DUMP),)
-dumpinfo:
+  dumpinfo:
 	$(DUMPINFO)
 else
 		
-$(PACKAGE_DIR):
+  $(PACKAGE_DIR):
 	mkdir -p $@
 
-source: FORCE $(DL_DIR)/$(PKG_SOURCE)
-prepare: FORCE $(PKG_BUILD_DIR)/.prepared
-configure: FORCE $(PKG_BUILD_DIR)/.configured
+  source: FORCE $(DL_DIR)/$(PKG_SOURCE)
+  prepare: FORCE prepared
+  configure: FORCE $(PKG_BUILD_DIR)/.configured
 
-compile-targets: FORCE
-compile: FORCE compile-targets
+  compile-targets: FORCE
+  compile: FORCE compile-targets
 
-install-targets: FORCE
-install: FORCE install-targets
+  install-targets: FORCE
+  install: FORCE install-targets
 
-clean-targets: FORCE
-clean: FORCE
+  clean-targets: FORCE
+  clean: FORCE
 	@$(MAKE) clean-targets
 	rm -rf $(PKG_BUILD_DIR)
 endif
