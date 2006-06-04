bd5e0bc352fdafd494be6f403a8a075ec1dcdbf8
Felix Fietkau <nbd@openwrt.org>
2006-06-04 21:57:59 +0000

    target/linux/image rewrite
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/buildroot-ng/openwrt@3905 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/image/image.mk
@@ -0,0 +1,53 @@
+include $(TOPDIR)/rules.mk
+
+KDIR:=$(BUILD_DIR)/linux-$(KERNEL)-$(BOARD)
+
+ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
+ifeq ($(CONFIG_TARGET_ROOTFS_JFFS2),y)
+include $(TOPDIR)/target/linux/image/jffs2.mk
+endif
+
+ifeq ($(CONFIG_TARGET_ROOTFS_SQUASHFS_LZMA),y)
+include $(TOPDIR)/target/linux/image/squashfs.mk
+endif
+
+ifeq ($(CONFIG_TARGET_ROOTFS_TGZ),y)
+include $(TOPDIR)/target/linux/image/tgz.mk
+endif
+endif
+
+define Image/mkfs/prepare/default
+	find $(BUILD_DIR)/root -type f -not -perm +0100 | xargs chmod 0644
+	find $(BUILD_DIR)/root -type f -perm +0100 | xargs chmod 0755
+	find $(BUILD_DIR)/root -type d | xargs chmod 0755
+	mkdir -p $(BUILD_DIR)/root/tmp
+	chmod 0777 $(BUILD_DIR)/root/tmp
+endef
+
+define Image/mkfs/prepare
+	$(call Image/mkfs/prepare/default)
+endef
+
+define BuildImage
+compile:
+	$(call Build/Compile)
+
+install:
+	$(call Image/Prepare)
+	$(call Image/mkfs/prepare)
+	$(call Image/mkfs/jffs2)
+	$(call Image/mkfs/squashfs)
+	$(call Image/mkfs/tgz)
+	
+clean:
+	$(call Build/Clean)
+endef
+
+compile-targets:
+install-targets:
+clean-targets:
+
+prepare:
+compile: compile-targets
+install: compile install-targets
+clean: clean-targets
--- target/linux/image/squashfs.mk
+++ target/linux/image/squashfs.mk
@@ -1,36 +1,22 @@
 ifneq ($(CONFIG_BIG_ENDIAN),y)
-endian := le
+ENDIAN := le
 else
-endian := be
+ENDIAN := be
 endif
 
-squashfs-prepare: FORCE
-	$(MAKE) -C squashfs prepare
-
-squashfs-compile: prepare-targets
-	$(MAKE) -C squashfs compile
+$(STAGING_DIR)/bin/mksquashfs-lzma:
+	$(MAKE) -C $(TOPDIR)/target/linux/image/squashfs compile
 	
 squashfs-clean: FORCE
-	$(MAKE) -C squashfs clean
+	$(MAKE) -C $(TOPDIR)/target/linux/image/squashfs clean
 	rm -f $(KDIR)/root.squashfs
 
-$(KDIR)/root.squashfs: install-prepare
+define Image/mkfs/squashfs
 	@mkdir -p $(BUILD_DIR)/root/jffs
-	$(STAGING_DIR)/bin/mksquashfs-lzma $(BUILD_DIR)/root $@ -nopad -noappend -root-owned -$(endian)
-	
-ifeq ($(IB),)
-squashfs-install: compile-targets $(BOARD)-compile
-endif
+	$(STAGING_DIR)/bin/mksquashfs-lzma $(BUILD_DIR)/root $(KDIR)/root.squashfs -nopad -noappend -root-owned -$(ENDIAN)
+	$(call Image/Build,squashfs)
+endef
 
-squashfs-install: $(KDIR)/root.squashfs FORCE
-	$(MAKE) -C $(BOARD) install KERNEL="$(KERNEL)" FS="squashfs"
-
-squashfs-install-ib: compile-targets
-	mkdir -p $(IB_DIR)/staging_dir_$(ARCH)/bin
-	$(CP) $(STAGING_DIR)/bin/mksquashfs-lzma $(IB_DIR)/staging_dir_$(ARCH)/bin
-	
-prepare-targets: squashfs-prepare
-compile-targets: squashfs-compile
-install-targets: squashfs-install
-install-ib: squashfs-install-ib
-clean: squashfs-clean
+FILESYSTEMS += squashfs
+compile-targets: $(STAGING_DIR)/bin/mksquashfs-lzma
+clean-targets: squashfs-clean
