6243bf96493fb54ce5bd02592115d53e1fad0495
Felix Fietkau <nbd@openwrt.org>
2006-06-20 23:49:23 +0000

    add toolchain/download target
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/buildroot-ng/openwrt@4029 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- toolchain/sed/sedcheck.sh
+++ toolchain/sed/sedcheck.sh
@@ -11,11 +11,20 @@ fi;
 echo "HELLO" > .sedtest
 $SED -i -e "s/HELLO/GOODBYE/" .sedtest >/dev/null 2>&1
 
-if [ $? != 0 ] ; then
-	echo build-sed-host-binary
-else
-	echo use-sed-host-binary
-fi;
+case "$1" in
+	download)
+		if [ $? != 0 ] ; then
+			echo download-sed-binary
+		fi;
+	;;
+	*)
+		if [ $? != 0 ] ; then
+			echo build-sed-host-binary
+		else
+			echo use-sed-host-binary
+		fi;
+	;;
+esac
 rm -f .sedtest
 
 
