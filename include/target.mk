913e09274d6e6056319d307a841ce9554085fb03
Felix Fietkau <nbd@openwrt.org>
2006-06-21 14:02:29 +0000

    move target/linux/image to target/image
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/buildroot-ng/openwrt@4040 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ include/target.mk
@@ -0,0 +1,23 @@
+define kernel_template
+ifeq ($(CONFIG_LINUX_$(3)),y)
+KERNEL:=$(1)
+BOARD:=$(2)
+endif
+endef
+
+$(eval $(call kernel_template,2.4,brcm,2_4_BRCM))
+$(eval $(call kernel_template,2.4,ar7,2_4_AR7))
+$(eval $(call kernel_template,2.4,x86,2_4_X86))
+$(eval $(call kernel_template,2.6,brcm,2_6_BRCM))
+$(eval $(call kernel_template,2.6,rb532,2_6_RB532))
+$(eval $(call kernel_template,2.6,x86,2_6_X86))
+$(eval $(call kernel_template,2.4,ar531x,2_4_AR531X))
+$(eval $(call kernel_template,2.6,aruba,2_6_ARUBA))
+$(eval $(call kernel_template,2.6,au1000,2_6_AU1000))
+$(eval $(call kernel_template,2.6,xscale,2_6_XSCALE))
+$(eval $(call kernel_template,2.6,sibyte,2_6_SIBYTE))
+
+export BOARD
+export KERNEL
+
+
