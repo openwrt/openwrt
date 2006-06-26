b33647bf27f5b28c83398e96ad213c24302f5fd8
Felix Fietkau <nbd@openwrt.org>
2006-06-26 01:55:47 +0000

    add support for new modules.mk format (no autogenerated Config.in yet)
    SVN-Revision: 4083

---
+++ openwrt/include/modules.mk
@@ -0,0 +1,27 @@
+include $(TOPDIR)/include/kernel.mk
+
+define KernelPackage/ide-core
+TITLE:=Kernel support for IDE
+DESCRIPTION:=Kernel modules for IDE support\\\
+	useful for usb mass storage devices (e.g. on WL-HDD)\\\
+	\\\
+	Includes: \\\
+	    - ide-core \\\
+	    - ide-detect \\\
+	    - ide-disk \\\
+	    - pdc202xx_old
+KCONFIG:=CONFIG_IDE
+FILES:=$(MODULES_DIR)/kernel/drivers/ide/*.$(LINUX_KMOD_SUFFIX)
+MODULES:=$(call AutoLoad,20,ide-core) $(call AutoLoad,90,ide-detect ide-disk)
+endef
+$(eval $(call KernelPackage,ide-core))
+
+define KernelPackage/ide-pdc202xx
+TITLE:=PDC202xx IDE driver
+DESCRIPTION:=PDC202xx IDE driver
+KCONFIG:=CONFIG_BLK_DEV_PDC202XX_OLD
+FILES:=$(MODULES_DIR)/kernel/drivers/ide/pci/pdc202xx_old.$(LINUX_KMOD_SUFFIX)
+MODULES:=$(call AutoLoad,30,pdc202xx_old)
+endef
+$(eval $(call KernelPackage,ide-pdc202xx))
+
