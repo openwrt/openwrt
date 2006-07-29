d030852b98b1913e024c4a57fbfc75ba9b7cbffe
Felix Fietkau <nbd@openwrt.org>
2006-07-29 11:30:06 +0000

    add support for per-package prereq checks, run global prereq checks before (menu-)config
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/buildroot-ng/openwrt@4318 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/prereq.mk
+++ include/prereq.mk
@@ -5,14 +5,12 @@
 # See /LICENSE for more information.
 #
 
-include $(TOPDIR)/rules.mk
-
 $(TMP_DIR):
 	mkdir -p $@
 
 prereq:
-	echo
 	if [ -f $(TMP_DIR)/.prereq-error ]; then \
+		echo; \
 		cat $(TMP_DIR)/.prereq-error; \
 		echo; \
 		rm -rf $(TMP_DIR); \
@@ -29,7 +27,7 @@ define Require
 
     prereq-$(1): $(TMP_DIR) FORCE
 		echo -n "Checking '$(1)'... "
-		if $(NO_TRACE_MAKE) -f $(INCLUDE_DIR)/prereq.mk check-$(1) >/dev/null 2>/dev/null; then \
+		if $(NO_TRACE_MAKE) -f $(firstword $(MAKEFILE_LIST)) check-$(1) >/dev/null 2>/dev/null; then \
 			echo 'ok.'; \
 		else \
 			echo 'failed.'; \
@@ -53,81 +51,3 @@ define RequireCommand
   $$(eval $$(call Require,$(1),$(2)))
 endef
 
-# Required for the toolchain
-define Require/working-make
-	echo 'all: test' > $(TMP_DIR)/check.mk
-	echo 'e0 = $$$$(foreach s,foobar,$$$$(eval $$$$s:))' >> $(TMP_DIR)/check.mk
-	echo 'e1 = $$$$(foreach s,foobar, $$$$(eval $$$$s:))' >> $(TMP_DIR)/check.mk
-	echo 'test: $$$$(strip $$$$(e0)) $$$$(strip $$$$(e1))' >> $(TMP_DIR)/check.mk
-	$(NO_TRACE_MAKE) -f $(TMP_DIR)/check.mk
-endef
-
-$(eval $(call Require,working-make, \
-	Your make version is buggy. Please install GNU make v3.81 or later. \
-))
-
-define Require/working-gcc
-	echo 'int main(int argc, char **argv) { return 0; }' | \
-		gcc -x c -o $(TMP_DIR)/a.out -
-endef
-
-$(eval $(call Require,working-gcc, \
-	No working GNU C Compiler (gcc) was found on your system. \
-))
-
-define Require/working-g++
-	echo 'int main(int argc, char **argv) { return 0; }' | \
-		g++ -x c++ -o $(TMP_DIR)/a.out -lstdc++ -
-endef
-
-$(eval $(call Require,working-g++, \
-	No working GNU C++ Compiler (g++) was found on your system. \
-))
-
-define Require/ncurses
-	echo 'int main(int argc, char **argv) { initscr(); return 0; }' | \
-		gcc -include ncurses.h -x c -o $(TMP_DIR)/a.out -lncurses -
-endef
-
-$(eval $(call Require,ncurses, \
-	No ncurses development files were not found on your system. \
-))
-
-
-define Require/zlib
-	echo 'int main(int argc, char **argv) { gzdopen(0, "rb"); return 0; }' | \
-		gcc -include zlib.h -x c -o $(TMP_DIR)/a.out -lz -
-endef
-
-$(eval $(call Require,zlib, \
-	No zlib development files were not found on your system. \
-))
-
-
-$(eval $(call RequireCommand,bison, \
-	Please install GNU bison. \
-))
-
-$(eval $(call RequireCommand,flex, \
-	Please install flex. \
-))
-
-$(eval $(call RequireCommand,python, \
-	Please install python. \
-))
-
-$(eval $(call RequireCommand,unzip, \
-	Please install unzip. \
-))
-
-$(eval $(call RequireCommand,bzip2, \
-	Please install bzip2. \
-))
-
-$(eval $(call RequireCommand,patch, \
-	Please install patch. \
-))
-
-$(eval $(call RequireCommand,perl, \
-	Please install perl. \
-))
