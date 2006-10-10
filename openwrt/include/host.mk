b43be2f7275632c1c3263aaf9d1f44347b77130a
Felix Fietkau <nbd@openwrt.org>
2006-10-10 18:31:39 +0000

    fix HOST_ARCH and GNU_HOST_NAME for freebsd and other operating systems
    SVN-Revision: 5027

--- openwrt/include/host.mk
+++ openwrt/include/host.mk
@@ -8,21 +8,20 @@
 include $(TOPDIR)/.host.mk
 
 $(TOPDIR)/.host.mk: $(INCLUDE_DIR)/host.mk
-	@echo "HOST_OS:=`uname`" > $@
-	@echo "HOST_ARCH:=` \
-		$(HOSTCC) -dumpmachine | sed -e s'/-.*//' \
-			-e 's/sparc.*/sparc/' \
-			-e 's/arm.*/arm/' \
-			-e 's/m68k.*/m68k/' \
-			-e 's/ppc/powerpc/' \
-			-e 's/v850.*/v850/' \
-			-e 's/sh[234]/sh/' \
-			-e 's/mips-.*/mips/' \
-			-e 's/mipsel-.*/mipsel/' \
-			-e 's/cris.*/cris/' \
-			-e 's/i[3-9]86/i386/'`" >> $@
-	@echo "GNU_HOST_NAME:=`$(HOSTCC) -dumpmachine`" >> $@
-	@if tar --version 2>&1 | grep 'GNU' >/dev/null; then \
-		echo "TAR_WILDCARDS:=--wildcards" >> $@; \
-	fi
+	@( \
+		HOST_OS=`uname`; \
+		case "$$HOST_OS" in \
+			Linux) HOST_ARCH=`uname -m`;; \
+			*) HOST_ARCH=`uname -p`;; \
+		esac; \
+		GNU_HOST_NAME=`gcc -dumpmachine`; \
+		[ -n "$$GNU_HOST_NAME" ] || \
+			GNU_HOST_NAME=`$(SCRIPT_DIR)/config.guess`; \
+		echo "HOST_OS:=$$HOST_OS" > $@; \
+		echo "HOST_ARCH:=$$HOST_ARCH" >> $@; \
+		echo "GNU_HOST_NAME:=$$GNU_HOST_NAME" >> $@; \
+		if tar --version 2>&1 | grep 'GNU' >/dev/null; then \
+			echo "TAR_WILDCARDS:=--wildcards" >> $@; \
+		fi; \
+	)
 
