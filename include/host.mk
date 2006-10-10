1da2c3fe51cd152336e4a57879faf53cc94fe3dd
Felix Fietkau <nbd@openwrt.org>
2006-10-10 16:58:47 +0000

    move the common variables from host-build.mk to host.mk
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/buildroot-ng/openwrt@5021 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ include/host.mk
@@ -0,0 +1,24 @@
+# 
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/.host.mk
+
+$(TOPDIR)/.host.mk: $(INCLUDE_DIR)/host.mk
+	echo "HOST_OS:=`uname`" > $@
+	echo "HOST_ARCH:=` \
+		$(HOSTCC) -dumpmachine | sed -e s'/-.*//' \
+			-e 's/sparc.*/sparc/' \
+			-e 's/arm.*/arm/' \
+			-e 's/m68k.*/m68k/' \
+			-e 's/ppc/powerpc/' \
+			-e 's/v850.*/v850/' \
+			-e 's/sh[234]/sh/' \
+			-e 's/mips-.*/mips/' \
+			-e 's/mipsel-.*/mipsel/' \
+			-e 's/cris.*/cris/' \
+			-e 's/i[3-9]86/i386/'`" >> $@
+	echo "GNU_HOST_NAME:=`$(HOSTCC) -dumpmachine`" >> $@
