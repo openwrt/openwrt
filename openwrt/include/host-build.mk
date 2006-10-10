6d7097f89976fee9da23ef798d687e9b0d57f276
Felix Fietkau <nbd@openwrt.org>
2006-10-10 16:58:47 +0000

    move the common variables from host-build.mk to host.mk
    SVN-Revision: 5021

--- openwrt/include/host-build.mk
+++ openwrt/include/host-build.mk
@@ -5,24 +5,7 @@
 # See /LICENSE for more information.
 #
 
-include $(TOPDIR)/.host.mk
-
-$(TOPDIR)/.host.mk: $(INCLUDE_DIR)/host-build.mk
-	echo "HOST_OS:=`uname`" > $@
-	echo "HOST_ARCH:=` \
-		$(HOSTCC) -dumpmachine | sed -e s'/-.*//' \
-			-e 's/sparc.*/sparc/' \
-			-e 's/arm.*/arm/' \
-			-e 's/m68k.*/m68k/' \
-			-e 's/ppc/powerpc/' \
-			-e 's/v850.*/v850/' \
-			-e 's/sh[234]/sh/' \
-			-e 's/mips-.*/mips/' \
-			-e 's/mipsel-.*/mipsel/' \
-			-e 's/cris.*/cris/' \
-			-e 's/i[3-9]86/i386/'`" >> $@
-	echo "GNU_HOST_NAME:=`$(HOSTCC) -dumpmachine`" >> $@
-
+include $(INCLUDE_DIR)/host.mk
 
 ifneq ($(strip $(PKG_CAT)),)
   ifeq ($(PKG_CAT),unzip)
