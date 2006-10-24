343ad07ed87d0001bcd771af7fa8e4644398a8dd
Felix Fietkau <nbd@openwrt.org>
2006-10-24 14:15:14 +0000

    remove an obsolete function
    SVN-Revision: 5280

--- package/base-files/default/etc/functions.sh
+++ package/base-files/default/etc/functions.sh
@@ -10,13 +10,6 @@ N="
 
 _C=0
 
-# valid interface?
-if_valid () (
-  ifconfig "$1" >&- 2>&- ||
-  [ "${1%%[0-9]}" = "br" ] ||
-  { debug "# missing interface '$1' ignored"; false; }
-)
-
 hotplug_dev() {
 	env -i ACTION=$1 INTERFACE=$2 /sbin/hotplug net
 }
