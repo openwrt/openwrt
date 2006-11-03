e9ce379d25a368a9c1867aaa05f4e0dd4b5d977d
Felix Fietkau <nbd@openwrt.org>
2006-11-03 02:08:17 +0000

    fix missing variable assignment (found by db90h)
    SVN-Revision: 5395

--- package/base-files/default/lib/config/uci.sh
+++ package/base-files/default/lib/config/uci.sh
@@ -19,6 +19,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 uci_load() {
+	local PACKAGE="$1"
 	config_load "$PACKAGE"
 	local PACKAGE_BASE="$(basename "$PACKAGE")"
 	[ -f "/tmp/.uci/${PACKAGE_BASE}" ] && {
