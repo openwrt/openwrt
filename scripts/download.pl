5b524506f890ad112ddd797607b592d7846d32ee
Tim Yardley <lst@openwrt.org>
2006-11-26 20:40:10 +0000

    correct trailing slash (cosmetic rather than functional)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@5647 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/download.pl
+++ scripts/download.pl
@@ -107,9 +107,9 @@ foreach my $mirror (@ARGV) {
 	}
 }
 
-#push @mirrors, 'http://mirror1.openwrt.org/';
+#push @mirrors, 'http://mirror1.openwrt.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
-push @mirrors, 'http://downloads.openwrt.org/sources/';
+push @mirrors, 'http://downloads.openwrt.org/sources';
 
 while (!$ok) {
 	my $mirror = shift @mirrors;
