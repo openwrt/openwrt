387570cc06ab3aca5824f1856b50a5e3ae5c2dc7
Felix Fietkau <nbd@openwrt.org>
2006-11-27 04:25:34 +0000

    trap chmod errors in tar unpack on bsd - modes like 02755 fail
    SVN-Revision: 5656

--- include/unpack.mk
+++ include/unpack.mk
@@ -1,9 +1,12 @@
 ifeq ($(strip $(PKG_UNPACK)),)
+  ifneq ($(HOST_OS),Linux)
+	TAR := trapret 2 $(PKG_UNPACK) $(TAR)
+  endif
   ifneq ($(strip $(PKG_CAT)),)
     # use existing PKG_CAT
     PKG_UNPACK:=$(PKG_CAT) $(DL_DIR)/$(PKG_SOURCE) | $(TAR) -C $(PKG_BUILD_DIR)/.. $(TAR_OPTIONS)
     ifeq ($(PKG_CAT),unzip)
-      PKG_UNPACK=unzip -d $(PKG_BUILD_DIR) $(DL_DIR)/$(PKG_SOURCE)
+      PKG_UNPACK:=unzip -d $(PKG_BUILD_DIR) $(DL_DIR)/$(PKG_SOURCE)
     endif
     # replace zcat with $(ZCAT), because some system have it as gzcat
     ifeq ($(PKG_CAT),zcat)
