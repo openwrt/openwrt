8d144586eabbf4b205971099c01faaac0c228ee9
Imre Kaloz <kaloz@openwrt.org>
2006-12-19 15:06:32 +0000

    next time I should sleep more
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@5858 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/arm-magic.sh
+++ scripts/arm-magic.sh
@@ -25,13 +25,13 @@
 for board in "avila 526" "gateway7001 731" "nslu2 597" "nas100d 865"
 do
   set -- $board
+  hexid=$(printf %x\\n $2)
   if [ "$2" -lt "256" ]; then
     # we have a low machtypeid, we just need a "mov" (e3a)
-    echo -en "\xe3\xa0\x10\x$(printf %x\\n $2)" > $BIN_DIR/openwrt-$1-2.6-zImage
+    printf "\xe3\xa0\x10\x$hexid" > $BIN_DIR/openwrt-$1-2.6-zImage
   else
     # we have a high machtypeid, we need a "mov" (e3a) and an "orr" (e38)
-    hexid=$(printf %x\\n $2)
-    echo -en "\xe3\xa0\x10\x$(echo $hexid|cut -b "2 3")\xe3\x81\x1c\x$(echo $hexid|cut -b 1)" > $BIN_DIR/openwrt-$1-2.6-zImage
+    printf "\xe3\xa0\x10\xx$(echo $hexid|cut -b "2 3")\xe3\x81\x1c\x$(echo $hexid|cut -b 1)" > $BIN_DIR/openwrt-$1-2.6-zImage
   fi
     # generate the image
     cat $BIN_DIR/openwrt-ixp4xx-2.6-zImage >> $BIN_DIR/openwrt-$1-2.6-zImage
