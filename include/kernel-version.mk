35bccdd4baa9ee6378762ed5b16c01a2543d0dcc
Felix Fietkau <nbd@openwrt.org>
2006-12-25 09:12:57 +0000

    store the default kernel version for our ports in a single place (target makefiles can still override it if necessary)
    SVN-Revision: 5899

---
+++ include/kernel-version.mk
@@ -0,0 +1,15 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+ifeq ($(LINUX_VERSION),)
+  ifeq ($(KERNEL),2.4)
+    LINUX_VERSION:=2.4.33.4
+    LINUX_RELEASE:=1
+    LINUX_KERNEL_MD5SUM:=aca0257549c385a227088ad8853a72a2
+  else
+    LINUX_VERSION:=2.6.19.1
+    LINUX_RELEASE:=1
+    LINUX_KERNEL_MD5SUM:=2ab08fdfddc00e09b3d5bc7397d3c8be
+  endif
+endif
+KERNEL:=2.$(word 2,$(subst ., ,$(strip $(LINUX_VERSION))))
+
