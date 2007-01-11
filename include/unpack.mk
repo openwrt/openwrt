5605b82835d762cac910dd640ade13f8c4a3668e
Felix Fietkau <nbd@openwrt.org>
2007-01-11 19:07:12 +0000

    fix unpacking of .zip files
    SVN-Revision: 6083

--- include/unpack.mk
+++ include/unpack.mk
@@ -8,7 +8,7 @@ ifeq ($(strip $(PKG_UNPACK)),)
     # use existing PKG_CAT
     PKG_UNPACK:=$(PKG_CAT) $(DL_DIR)/$(PKG_SOURCE) | $(HOST_TAR) -C $(PKG_BUILD_DIR)/.. $(TAR_OPTIONS)
     ifeq ($(PKG_CAT),unzip)
-      PKG_UNPACK:=unzip -d $(PKG_BUILD_DIR) $(DL_DIR)/$(PKG_SOURCE)
+      PKG_UNPACK:=unzip -d $(PKG_BUILD_DIR)/.. $(DL_DIR)/$(PKG_SOURCE)
     endif
     # replace zcat with $(ZCAT), because some system have it as gzcat
     ifeq ($(PKG_CAT),zcat)
