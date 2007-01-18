4c1346198c65405322812bcd09e0d3bb0001e9b6
Tim Yardley <lst@openwrt.org>
2007-01-18 18:58:24 +0000

    bring in 2.6.19.2 fixes, primarily for the fixed sata support. see http://www.kernel.org/pub/linux/kernel/v2.6/ChangeLog-2.6.19.2 for full details of the changes
    SVN-Revision: 6128

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifeq ($(LINUX_VERSION),)
     LINUX_RELEASE:=1
     LINUX_KERNEL_MD5SUM:=f59665540a7f3351ea416a0dad104b55
   else
-    LINUX_VERSION:=2.6.19.1
+    LINUX_VERSION:=2.6.19.2
     LINUX_RELEASE:=1
-    LINUX_KERNEL_MD5SUM:=2ab08fdfddc00e09b3d5bc7397d3c8be
+    LINUX_KERNEL_MD5SUM:=ca0ce8f288e8ae93ac243b568f906bf8
   endif
 endif
 KERNEL:=2.$(word 2,$(subst ., ,$(strip $(LINUX_VERSION))))
