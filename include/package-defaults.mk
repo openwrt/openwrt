0241b19768532635b521bf35f7746d4fc37ae91e
Felix Fietkau <nbd@openwrt.org>
2007-03-15 23:57:18 +0000

    split package.mk and clean up build system code (based on patch by mbm), makes the code more readable and speeds up metadata scan
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@6571 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ include/package-defaults.mk
@@ -0,0 +1,106 @@
+# 
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Package/Default
+  CONFIGFILE:=
+  SECTION:=opt
+  CATEGORY:=Extra packages
+  DEPENDS:=
+  PROVIDES:=
+  EXTRA_DEPENDS:=
+  MAINTAINER:=OpenWrt Developers Team <openwrt-devel@openwrt.org>
+  SOURCE:=$(patsubst $(TOPDIR)/%,%,${shell pwd})
+  ifneq ($(PKG_VERSION),)
+    ifneq ($(PKG_RELEASE),)
+      VERSION:=$(PKG_VERSION)-$(PKG_RELEASE)
+    else
+      VERSION:=$(PKG_VERSION)
+    endif
+  else
+    VERSION:=$(PKG_RELEASE)
+  endif
+  PKGARCH:=$(ARCH)
+  PRIORITY:=optional
+  DEFAULT:=
+  MENU:=
+  SUBMENU:=
+  SUBMENUDEP:=
+  TITLE:=
+  DESCRIPTION:=
+endef
+
+ifneq ($(strip $(PKG_UNPACK)),)
+  define Build/Prepare/Default
+  	$(PKG_UNPACK)
+	@if [ -d ./patches -a "$$$$(ls ./patches | wc -l)" -gt 0 ]; then \
+		$(PATCH) $(PKG_BUILD_DIR) ./patches; \
+	fi
+  endef
+endif
+
+TARGET_CPPFLAGS=-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/include
+TARGET_LDFLAGS=-L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib
+
+CONFIGURE_ARGS := \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--program-prefix="" \
+		--program-suffix="" \
+		--prefix=/usr \
+		--exec-prefix=/usr \
+		--bindir=/usr/bin \
+		--sbindir=/usr/sbin \
+		--libexecdir=/usr/lib \
+		--sysconfdir=/etc \
+		--datadir=/usr/share \
+		--localstatedir=/var \
+		--mandir=/usr/man \
+		--infodir=/usr/info \
+		$(DISABLE_NLS)
+
+CONFIGURE_VARS:= \
+		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+		CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+		CPPFLAGS="$(TARGET_CPPFLAGS) $(EXTRA_CPPFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)" \
+		PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
+		PKG_CONFIG_LIBDIR="$(STAGING_DIR)/usr/lib/pkgconfig"
+
+CONFIGURE_PATH:=.
+
+define Build/Configure/Default
+	(cd $(PKG_BUILD_DIR)/$(strip $(3)); \
+	if [ -x configure ]; then \
+		$(CONFIGURE_VARS) \
+		$(2) \
+		$(CONFIGURE_PATH)/configure \
+		$(CONFIGURE_ARGS) \
+		$(1); \
+	fi; \
+	)
+endef
+
+MAKE_VARS := \
+	CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+	CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+	LDFLAGS="$(EXTRA_LDFLAGS) "
+
+MAKE_FLAGS := \
+	$(TARGET_CONFIGURE_OPTS) \
+	CROSS="$(TARGET_CROSS)" \
+	ARCH="$(ARCH)"
+
+define Build/Compile/Default
+	$(MAKE_VARS) \
+	$(MAKE) -C $(PKG_BUILD_DIR) \
+		$(MAKE_FLAGS) \
+		$(1);
+endef
+
+
---
+++ include/package-dumpinfo.mk
@@ -0,0 +1,32 @@
+# 
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ifneq ($(DUMP),)
+define Dumpinfo 
+  dumpinfo: dumpinfo-$(1)
+  dumpinfo-$(1): FORCE
+	@echo "Package: $(1)" ; \
+	 $(if $(MENU),echo "Menu: $(MENU)";) \
+	 $(if $(SUBMENU),echo "Submenu: $(SUBMENU)";) \
+	 $(if $(SUBMENUDEP),echo "Submenu-Depends: $(SUBMENUDEP)";) \
+	 $(if $(DEFAULT),echo "Default: $(DEFAULT)";)  \
+	 if [ "$$$$PREREQ_CHECK" = 1 ]; then echo "Prereq-Check: 1"; fi; \
+	 echo "Version: $(VERSION)"; \
+	 echo "Depends: $(DEPENDS)"; \
+	 echo "Provides: $(PROVIDES)"; \
+	 echo "Build-Depends: $(PKG_BUILD_DEPENDS)"; \
+	 echo "Section: $(SECTION)"; \
+	 echo "Category: $(CATEGORY)"; \
+	 echo "Title: $(TITLE)"; \
+	 echo "Maintainer: $(MAINTAINER)"; \
+	 echo -n "Description: "; \
+	 getvar $(call shvar,Package/$(1)/description); \
+	 $(if $(URL),echo;echo "$(URL)";) \
+	 echo "@@" ; \
+	 $$(if $$(Package/$(1)/config),echo "Config: "; getvar $(call shvar,Package/$(1)/config); echo "@@")
+endef
+endif
