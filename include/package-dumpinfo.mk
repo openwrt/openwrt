199cce687af2f9f603baf1bc24127bfa42ec8a82
Felix Fietkau <nbd@openwrt.org>
2007-03-16 12:22:11 +0000

    revert part of [6573] to keep the speedup of metadata scanning
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@6574 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/package-dumpinfo.mk
+++ include/package-dumpinfo.mk
@@ -14,25 +14,25 @@ ifneq ($(DUMP),)
     dumpinfo: dumpinfo-$(1)
     .SILENT: dumpinfo-$(1)
     dumpinfo-$(1): FORCE
-	echo "Package: $(1)" ;
-	$(if $(MENU),echo "Menu: $(MENU)")
-	$(if $(SUBMENU),echo "Submenu: $(SUBMENU)")
-	$(if $(SUBMENUDEP),echo "Submenu-Depends: $(SUBMENUDEP)")
-	$(if $(DEFAULT),echo "Default: $(DEFAULT)") 
-	if [ "$$$$PREREQ_CHECK" = 1 ]; then echo "Prereq-Check: 1"; fi;
-	echo "Version: $(VERSION)"
-	echo "Depends: $(DEPENDS)"
-	echo "Provides: $(PROVIDES)"
-	echo "Build-Depends: $(PKG_BUILD_DEPENDS)"
-	echo "Section: $(SECTION)"
-	echo "Category: $(CATEGORY)"
-	echo "Title: $(TITLE)"
-	echo "Maintainer: $(MAINTAINER)"
-	echo -n "Description: "
-	getvar $(call shvar,Package/$(1)/description)
-	$(if $(URL),echo;echo "$(URL)")
-	echo "@@"
-	$$(if $$(Package/$(1)/config),echo "Config: "; getvar $(call shvar,Package/$(1)/config); echo "@@")
-	$(if $$(preconfig_$(1)),$$(preconfig_$(1)) echo "")
+	  @echo "Package: $(1)" ; \
+		$(if $(MENU),echo "Menu: $(MENU)";) \
+		$(if $(SUBMENU),echo "Submenu: $(SUBMENU)";) \
+		$(if $(SUBMENUDEP),echo "Submenu-Depends: $(SUBMENUDEP)";) \
+		$(if $(DEFAULT),echo "Default: $(DEFAULT)";)  \
+		if [ "$$$$PREREQ_CHECK" = 1 ]; then echo "Prereq-Check: 1"; fi; \
+		echo "Version: $(VERSION)"; \
+		echo "Depends: $(DEPENDS)"; \
+		echo "Provides: $(PROVIDES)"; \
+		echo "Build-Depends: $(PKG_BUILD_DEPENDS)"; \
+		echo "Section: $(SECTION)"; \
+		echo "Category: $(CATEGORY)"; \
+		echo "Title: $(TITLE)"; \
+		echo "Maintainer: $(MAINTAINER)"; \
+		echo -n "Description: "; \
+		getvar $(call shvar,Package/$(1)/description); \
+		$(if $(URL),echo;echo "$(URL)";) \
+		echo "@@" ; \
+		$$(if $$(Package/$(1)/config),echo "Config: "; getvar $(call shvar,Package/$(1)/config); echo "@@"; ) \
+		$$(if $$(preconfig_$(1)),$$(preconfig_$(1)) echo "")
   endef
 endif
