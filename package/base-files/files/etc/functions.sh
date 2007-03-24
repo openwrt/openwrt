7f0dd3fa4549cbc4f74407b5fe93a1604f6219b8
Felix Fietkau <nbd@openwrt.org>
2007-03-24 13:43:04 +0000

    rename uci-update.awk to uci.awk and add a few config_* functions
    SVN-Revision: 6666

--- package/base-files/files/etc/functions.sh
+++ package/base-files/files/etc/functions.sh
@@ -120,7 +120,11 @@ config_set() {
 	local section="$1"
 	local option="$2"
 	local value="$3"
-	export ${NO_EXPORT:+-n} "CONFIG_${section}_${option}=$value"
+	local old_section="$CONFIG_SECTION"
+
+	CONFIG_SECTION="$section"
+	option "$option" "$value"
+	CONFIG_SECTION="$old_section"
 }
 
 config_foreach() {
