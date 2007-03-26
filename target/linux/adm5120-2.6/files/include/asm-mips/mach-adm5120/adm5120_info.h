0fcd634965f73ce8072e65d07b02cf9b77886bbc
Florian Fainelli <florian@openwrt.org>
2007-03-26 19:09:16 +0000

    Forgot adding infos header
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@6715 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/adm5120-2.6/files/include/asm-mips/mach-adm5120/adm5120_info.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2007 OpenWrt.org
+ * Copyright (C) Gabor Juhos
+ * 
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#ifndef _ADM5120_INFO_H
+#define _ADM5120_INFO_H
+
+#include <linux/types.h>
+
+struct adm5120_info {
+	unsigned long cpu_speed;
+	unsigned int cpu_package;
+	unsigned int boot_loader;
+	unsigned int board_type;
+};
+
+#define CPU_SPEED_175		175000000
+#define CPU_SPEED_200		200000000
+#define CPU_SPEED_225		225000000
+#define CPU_SPEED_250		250000000
+
+#define CPU_PACKAGE_PQFP	0
+#define CPU_PACKAGE_BGA		1
+
+#define BOOT_LOADER_UNKNOWN	0
+#define BOOT_LOADER_CFE		1
+#define BOOT_LOADER_UBOOT	2
+#define BOOT_LOADER_MYLOADER	3
+#define BOOT_LOADER_LAST	3
+
+#define BOARD_TYPE_UNKNOWN	0
+#define BOARD_TYPE_WP54G_WRT	1
+#define BOARD_TYPE_WP54G	2
+#define BOARD_TYPE_WP54AG	3
+#define BOARD_TYPE_WPP54G	4
+#define BOARD_TYPE_WPP54AG	5
+#define BOARD_TYPE_NP28G	6
+#define BOARD_TYPE_NP28GHS	7
+#define BOARD_TYPE_NP27G	8
+#define BOARD_TYPE_WP54Gv1C	9
+#define BOARD_TYPE_RB_1xx	10
+
+extern struct adm5120_info adm5120_info;
+extern void adm5120_info_init(void);
+
+#endif /* _ADM5120_INFO_H */
