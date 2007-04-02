8c7c1075f8e870acc77817063f9a473d33608def
Florian Fainelli <florian@openwrt.org>
2007-04-02 16:07:40 +0000

    Use sn9 mtd driver
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@6835 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/rdc-2.6/files/drivers/mtd/maps/rdc3210.c
+++ target/linux/rdc-2.6/files/drivers/mtd/maps/rdc3210.c
@@ -1,115 +1,390 @@
-/*
- * $Id$
- * Copyright (C) 2005 Gemtek Corporation, Dante Su (dante_su@gemtek.com.tw)
- * Copyright (C) 2006  Florian Fainelli <florian@openwrt.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/*******************************************************************
+ * Simple Flash mapping for RDC3210                                *
+ *                                                                 *
+ *                                                     2005.03.23  *
+ *                              Dante Su (dante_su@gemtek.com.tw)  *
+ *                          Copyright (C) 2005 Gemtek Corporation  *
+ *******************************************************************/
 
-#include <asm/io.h>
-#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
 #include <linux/kernel.h>
-#include <linux/mtd/map.h>
+#include <asm/io.h>
 #include <linux/mtd/mtd.h>
+#include <linux/mtd/map.h>
 #include <linux/mtd/partitions.h>
-#include <linux/vmalloc.h>
+#include <linux/autoconf.h>
+#include <linux/squashfs_fs.h>
+#include <linux/jffs2.h>
+//#include <linux/crc32.h>
 
-#define WINDOW_ADDR             0xFFC00000
-#define WINDOW_SIZE             0x00400000
-#define BUSWIDTH 		2
+#define WINDOW_ADDR		0xFFC00000
+#define WINDOW_SIZE		0x00400000
 
-extern int parse_redboot_partitions(struct mtd_info *master, struct mtd_partition **pparts, unsigned long fis_origin);
+#define BUSWIDTH 2
 
-static struct mtd_partition *parsed_parts;
-static struct mtd_info *rdc3210_mtd_info;
+/* Dante: linked from linux-2.4.x/drivers/mtd/chips/flashdrv.c */
+extern int flashdrv_get_size(void);
+extern int flashdrv_get_sector(int addr);
+extern int flashdrv_get_sector_addr(int sector);
+extern int flashdrv_get_sector_size(int sector);
 
-static struct map_info rdc3210_map = {
-	.name = "rdc3210",
-	.size = WINDOW_SIZE,
-	.bankwidth = BUSWIDTH,
-	.phys = WINDOW_ADDR,
-};
+static struct mtd_info		*rdc3210_mtd;
 
-static struct mtd_partition rdc3210_parts[] = {
-	{ name: "linux",   offset:  0,          size: 0x003C0000 },     /* 3840 KB = (Kernel + ROMFS) = (768 KB + 3072 KB) */
-	{ name: "romfs",   offset:  0x000C0000, size: 0x00300000 },     /* 3072 KB */
-	{ name: "nvram",   offset:  0x003C0000, size: 0x00010000 },     		/*   64 KB */
-	{ name: "factory", offset:  0x003D0000, size: 0x00010000 },     		/*   64 KB */
-	{ name: "bootldr", offset:  0x003E0000, size: 0x00020000, mask_flags: MTD_WRITEABLE },/*  128 KB */
-};
-
-static int __init rdc3210_mtd_init(void)
+__u8  rdc3210_map_read8(struct map_info *map, unsigned long ofs)
 {
-	printk(KERN_INFO "rdc3210: 0x%08x at 0x%08x\n", WINDOW_SIZE, WINDOW_ADDR);
-	rdc3210_map.virt = ioremap_nocache(WINDOW_ADDR, WINDOW_SIZE);
+	return *(__u8 *)(map->map_priv_1 + ofs);
+}
 
-	if (!rdc3210_map.virt) {
-		printk(KERN_ERR "rdc3210: failed to ioremap\n");
-		return -EIO;
+__u16 rdc3210_map_read16(struct map_info *map, unsigned long ofs)
+{
+	return *(__u16 *)(map->map_priv_1 + ofs);
+}
+
+__u32 rdc3210_map_read32(struct map_info *map, unsigned long ofs)
+{
+	return *(__u32 *)(map->map_priv_1 + ofs);
+}
+
+void rdc3210_map_write8(struct map_info *map, __u8 d, unsigned long adr)
+{
+	*(__u8 *)(map->map_priv_1 + adr) = d;
+}
+
+void rdc3210_map_write16(struct map_info *map, __u16 d, unsigned long adr)
+{
+	*(__u16 *)(map->map_priv_1 + adr) = d;
+}
+
+void rdc3210_map_write32(struct map_info *map, __u32 d, unsigned long adr)
+{
+	*(__u32 *)(map->map_priv_1 + adr) = d;
+}
+
+void rdc3210_map_copy_from(struct map_info *map, void *to, unsigned long from, ssize_t len)
+{
+	int	i;
+	u16	*dst = (u16 *)(to);
+	u16	*src = (u16 *)(map->map_priv_1 + from);
+
+	for(i = 0; i < (len / 2); ++i)
+		dst[i] = src[i];
+
+	if(len & 1)
+	{
+		printk("# WARNNING!!! rdc3210_map_copy_from has odd length\n");
+		//dst[len - 1] = B0(src[i]);
+	}
+}
+
+void rdc3210_map_copy_to(struct map_info *map, unsigned long to, const void *from, ssize_t len)
+{
+	int	i;
+	u16	*dst = (u16 *)(map->map_priv_1 + to);
+	u16	*src = (u16 *)(from);
+	
+	for(i = 0; i < (len / 2); ++i)
+		dst[i] = src[i];
+
+	if(len & 1)
+	{
+		printk("# WARNNING!!! rdc3210_map_copy_from has odd length\n");
+		//dst[len - 1] = B0(src[i]);
+	}
+}
+
+struct map_info rdc3210_map = 
+{
+	.name =		"RDC3210 Flash",
+	.size =		WINDOW_SIZE,
+	.phys = 	WINDOW_ADDR,
+	.bankwidth =	BUSWIDTH,
+};
+
+/* Dante: This is the default static mapping, however this is nothing but a hint. (Say dynamic mapping) */
+static struct mtd_partition rdc3210_parts[] = 
+{
+	{ name: "linux",   offset:  0,          size: 0x003C0000 },	/* 3840 KB = (Kernel + ROMFS) = (768 KB + 3072 KB) */
+	{ name: "romfs",   offset:  0x000C0000, size: 0x00300000 },	/* 3072 KB */
+	{ name: "nvram",   offset:  0x003C0000, size: 0x00010000 },	/*   64 KB */
+#if RDC3210_STATIC_MAP || RDC3210_FACTORY_PRESENT
+	{ name: "factory", offset:  0x003D0000, size: 0x00010000 },	/*   64 KB */
+#endif
+	{ name: "bootldr", offset:  0x003E0000, size: 0x00020000 },	/*  128 KB */
+};
+
+static __u32 crctab[257] = {
+	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
+	0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
+	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+	0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
+	0x1db71064, 0x6ab020f2, 0xf3b97148, 0x84be41de,
+	0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+	0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec,
+	0x14015c4f, 0x63066cd9, 0xfa0f3d63, 0x8d080df5,
+	0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+	0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,
+	0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6, 0xacbcf940,
+	0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+	0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116,
+	0x21b4f4b5, 0x56b3c423, 0xcfba9599, 0xb8bda50f,
+	0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+	0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,
+	0x76dc4190, 0x01db7106, 0x98d220bc, 0xefd5102a,
+	0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+	0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818,
+	0x7f6a0dbb, 0x086d3d2d, 0x91646c97, 0xe6635c01,
+	0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+	0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457,
+	0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea, 0xfcb9887c,
+	0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+	0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2,
+	0x4adfa541, 0x3dd895d7, 0xa4d1c46d, 0xd3d6f4fb,
+	0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+	0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9,
+	0x5005713c, 0x270241aa, 0xbe0b1010, 0xc90c2086,
+	0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+	0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4,
+	0x59b33d17, 0x2eb40d81, 0xb7bd5c3b, 0xc0ba6cad,
+	0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+	0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683,
+	0xe3630b12, 0x94643b84, 0x0d6d6a3e, 0x7a6a5aa8,
+	0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+	0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe,
+	0xf762575d, 0x806567cb, 0x196c3671, 0x6e6b06e7,
+	0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+	0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5,
+	0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4, 0x4fdff252,
+	0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+	0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60,
+	0xdf60efc3, 0xa867df55, 0x316e8eef, 0x4669be79,
+	0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+	0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f,
+	0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92, 0x5cb36a04,
+	0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+	0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a,
+	0x9c0906a9, 0xeb0e363f, 0x72076785, 0x05005713,
+	0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+	0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21,
+	0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8, 0x1fda836e,
+	0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+	0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c,
+	0x8f659eff, 0xf862ae69, 0x616bffd3, 0x166ccf45,
+	0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+	0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db,
+	0xaed16a4a, 0xd9d65adc, 0x40df0b66, 0x37d83bf0,
+	0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+	0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6,
+	0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
+	0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+	0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d,
+	0
+};
+
+static __u32 crc32(__u8 * buf, __u32 len)
+{
+	register int i;
+	__u32 sum;
+	register __u32 s0;
+	s0 = ~0;
+	for (i = 0; i < len; i++) {
+		s0 = (s0 >> 8) ^ crctab[(__u8) (s0 & 0xFF) ^ buf[i]];
+	}
+	sum = ~s0;
+	return sum;
+}
+
+static void erase_callback(struct erase_info *done)
+{
+	wait_queue_head_t *wait_q = (wait_queue_head_t *)done->priv;
+	wake_up(wait_q);
+}
+
+static int erase_write (struct mtd_info *mtd, unsigned long pos, 
+			int len, const char *buf)
+{
+	struct erase_info erase;
+	DECLARE_WAITQUEUE(wait, current);
+	wait_queue_head_t wait_q;
+	size_t retlen;
+	int ret;
+
+	/*
+	 * First, let's erase the flash block.
+	 */
+
+	init_waitqueue_head(&wait_q);
+	erase.mtd = mtd;
+	erase.callback = erase_callback;
+	erase.addr = pos;
+	erase.len = len;
+	erase.priv = (u_long)&wait_q;
+
+	set_current_state(TASK_INTERRUPTIBLE);
+	add_wait_queue(&wait_q, &wait);
+
+	ret = mtd->erase(mtd, &erase);
+	if (ret) {
+		set_current_state(TASK_RUNNING);
+		remove_wait_queue(&wait_q, &wait);
+		printk (KERN_WARNING "erase of region [0x%lx, 0x%x] "
+				     "on \"%s\" failed\n",
+			pos, len, mtd->name);
+		return ret;
 	}
 
-	simple_map_init(&rdc3210_map);
+	schedule();  /* Wait for erase to finish. */
+	remove_wait_queue(&wait_q, &wait);
 
-	rdc3210_mtd_info = do_map_probe("cfi_probe", &rdc3210_map);
+	/*
+	 * Next, writhe data to flash.
+	 */
 
-	if (rdc3210_mtd_info)
+	ret = mtd->write (mtd, pos, len, &retlen, buf);
+	if (ret)
+		return ret;
+	if (retlen != len)
+		return -EIO;
+	return 0;
+}
+
+static int __init init_rdc3210_map(void)
+{
+       	printk(KERN_NOTICE "flash device: %x at %x\n", WINDOW_SIZE, WINDOW_ADDR);
+       	
+	rdc3210_map.map_priv_1 = (unsigned long)ioremap(WINDOW_ADDR, WINDOW_SIZE);
+
+	if (!rdc3210_map.map_priv_1) 
 	{
-		rdc3210_mtd_info->owner = THIS_MODULE;
-		int parsed_nr_parts = 0;
-		char * part_type;
+		printk("Failed to ioremap\n");
+		return -EIO;
+	}
+	rdc3210_mtd = do_map_probe("cfi_probe", &rdc3210_map);
+#if RDC3210_STATIC_MAP	/* Dante: This is for fixed map */
+	if (rdc3210_mtd) 
+	{
+		rdc3210_mtd->module = THIS_MODULE;
+		add_mtd_partitions(rdc3210_mtd, rdc3210_parts, sizeof(rdc3210_parts)/sizeof(rdc3210_parts[0]));
+		return 0;
+	}
+#else	/* Dante: This is for dynamic mapping */
 
-#ifdef CONFIG_MTD_REDBOOT_PARTS
-		if (parsed_nr_parts == 0)
+#include "imghdr.h"
+
+	if (rdc3210_mtd) 
+	{	// Dante
+		gt_imghdr_t	*hdr = (gt_imghdr_t *)(rdc3210_map.map_priv_1), *ptmp;
+		unsigned int	tmp = hdr->kernelsz + sizeof(gt_imghdr_t), tmp2 = rdc3210_mtd->erasesize;
+		unsigned int	tmp3 = ((tmp / 32) + ((tmp % 32) ? 1 : 0)) * 32;
+		unsigned int	tmp4 = ((tmp / tmp2) + ((tmp % tmp2) ? 1 : 0)) * tmp2;
+		int	len;
+		
+		if(memcmp(hdr->magic, GTIMG_MAGIC, 4))
 		{
-			int ret = parse_redboot_partitions(rdc3210_mtd_info, &parsed_parts, 0);
-			if (ret > 0)
-			{
-				part_type = "RedBoot";
-				parsed_nr_parts = ret;
-			} else {
-				printk(KERN_ERR "rdc3210: failed to parse RedBoot partitions, using static mapping\n");
-				add_mtd_partitions(rdc3210_mtd_info, rdc3210_parts, sizeof(rdc3210_parts)/sizeof(rdc3210_parts[0]));
-				return -ENXIO;
-			}
+			printk("Invalid MAGIC for Firmware Image!!!\n");
+			return -EIO;
 		}
 
-		add_mtd_partitions(rdc3210_mtd_info, parsed_parts, parsed_nr_parts);
+#if 0
+		/* 1. Adjust Redboot */
+		tmp2 = flashdrv_get_size() - rdc3210_parts[4].size;
+		rdc3210_parts[4].offset = flashdrv_get_sector_addr(flashdrv_get_sector(tmp2));
+		rdc3210_parts[4].size   = flashdrv_get_size() - rdc3210_parts[4].offset;
+		
+		/* 2. Adjust Factory Default */
+		tmp2 -= rdc3210_parts[3].size;
+		rdc3210_parts[3].offset = flashdrv_get_sector_addr(flashdrv_get_sector(tmp2));
+		rdc3210_parts[3].size   = rdc3210_parts[4].offset - rdc3210_parts[3].offset;
+		/* 1. Adjust Redboot */
+		tmp2 = flashdrv_get_size() - rdc3210_parts[3].size;
+		rdc3210_parts[3].offset = flashdrv_get_sector_addr(flashdrv_get_sector(tmp2));
+		rdc3210_parts[3].size   = flashdrv_get_size() - rdc3210_parts[3].offset;
+#endif
+#if RDC3210_NVRAM_IS_JFFS2
+		/* 3. Adjust NVRAM */
+		tmp = hdr->reserved;
+		if (!tmp) tmp = hdr->imagesz;
+		tmp2 = rdc3210_mtd->erasesize;
+		rdc3210_parts[2].offset = rdc3210_parts[0].offset + (((tmp / tmp2) + ((tmp % tmp2) ? 1 : 0)) * tmp2);
+		rdc3210_parts[2].size   = rdc3210_parts[3].offset - rdc3210_parts[2].offset;
+		
+		/* 4. Adjust Linux (Kernel + ROMFS) */
+		rdc3210_parts[0].size   = rdc3210_parts[3].offset - rdc3210_parts[0].offset;
+
+		/* 5. Adjust ROMFS */
+		tmp = hdr->kernelsz + sizeof(gt_imghdr_t);
+#else
+		/* 3. Adjust NVRAM */
+		tmp2 -= rdc3210_parts[2].size;
+		rdc3210_parts[2].offset = flashdrv_get_sector_addr(flashdrv_get_sector(tmp2));
+		rdc3210_parts[2].size   = rdc3210_parts[3].offset - rdc3210_parts[2].offset;
+				
+		/* 4. Adjust Linux (Kernel + ROMFS) */
+		rdc3210_parts[0].size   = rdc3210_parts[2].offset - rdc3210_parts[0].offset;
+
+		/* 5. Adjust ROMFS */
+		tmp2 = rdc3210_mtd->erasesize;
+#endif
+		if ((ptmp = (gt_imghdr_t *)kmalloc(tmp4, GFP_KERNEL)) == NULL)
+		{
+			iounmap((void *)rdc3210_map.map_priv_1);
+			return -ENOMEM;
+		}
+		if (rdc3210_mtd->read(rdc3210_mtd, 0, tmp4, &len, (__u8 *)ptmp) || len != tmp4)
+		{
+			kfree(ptmp);
+			goto oh_snap;
+		}
+		if (*(__u32 *)(((unsigned char *)ptmp)+tmp3) == SQUASHFS_MAGIC)
+			tmp4 = tmp3;
+		else if (!hdr->reserved)
+		{
+			__u8	buf[1024];
+			ptmp->reserved = hdr->imagesz;
+			ptmp->imagesz = tmp4;
+			ptmp->checksum = ptmp->fastcksum = 0;
+			memcpy(buf, ptmp, 0x100);
+			memcpy(buf + 0x100, ((__u8 *)ptmp) + ((tmp4 >> 1) - ((tmp4 & 0x6) >> 1)), 0x100);
+			memcpy(buf + 0x200, ((__u8 *)ptmp) + (tmp4 - 0x200), 0x200);
+			ptmp->fastcksum = crc32(buf, sizeof(buf));
+			ptmp->checksum = crc32((__u8 *)ptmp, tmp4);
+			if (rdc3210_mtd->unlock) rdc3210_mtd->unlock(rdc3210_mtd, 0, tmp2);
+			if (len == erase_write(rdc3210_mtd, 0, tmp2, (char *)ptmp))
+			{
+				kfree(ptmp);
+				iounmap((void *)rdc3210_map.map_priv_1);
+				return len;
+			}
+			if (rdc3210_mtd->sync) rdc3210_mtd->sync(rdc3210_mtd);
+		}
+		kfree(ptmp);
+		rdc3210_parts[1].offset = rdc3210_parts[0].offset + tmp4;
+		rdc3210_parts[1].size   = rdc3210_parts[2].offset - rdc3210_parts[1].offset;
+		
+		rdc3210_mtd->owner = THIS_MODULE;
+		add_mtd_partitions(rdc3210_mtd, rdc3210_parts, sizeof(rdc3210_parts)/sizeof(rdc3210_parts[0]));
 		return 0;
 	}
 #endif
-	iounmap(rdc3210_map.virt);
+oh_snap:
+	iounmap((void *)rdc3210_map.map_priv_1);
 	return -ENXIO;
 }
 
-static void __exit rdc3210_mtd_cleanup(void)
+static void __exit cleanup_rdc3210_map(void)
 {
-	if (rdc3210_mtd_info)
+	if (rdc3210_mtd) 
 	{
-		del_mtd_partitions(rdc3210_mtd_info);
-		map_destroy(rdc3210_mtd_info);
+		del_mtd_partitions(rdc3210_mtd);
+		map_destroy(rdc3210_mtd);
 	}
-
-	if (rdc3210_map.virt) {
-		iounmap(rdc3210_map.virt);
-		rdc3210_map.virt = 0;
+	
+	if (rdc3210_map.map_priv_1) 
+	{
+		iounmap((void *)rdc3210_map.map_priv_1);
+		rdc3210_map.map_priv_1 = 0L;
 	}
 }
 
-module_init(rdc3210_mtd_init);
-module_exit(rdc3210_mtd_cleanup);
-
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("RDC3210 flash map driver");
-MODULE_AUTHOR("Florian Fainelli <florian@openwrt.org>");
+module_init(init_rdc3210_map);
+module_exit(cleanup_rdc3210_map);
