8ff49aed398fe2682088d02f38838d703ced0746
Florian Fainelli <florian@openwrt.org>
2007-04-09 08:13:58 +0000

    Fresh fixes for rdc from sn9, thanks !
    SVN-Revision: 6911

--- target/linux/rdc-2.6/files/drivers/mtd/maps/imghdr.h
+++ target/linux/rdc-2.6/files/drivers/mtd/maps/imghdr.h
@@ -12,14 +12,14 @@
 /* Gemtek */
 typedef struct
 {
-	UINT8		magic[4];		/* ASICII: GMTK */
-	UINT32		checksum;		/* CRC32 */
-	UINT32		version;		/* x.x.x.x */
-	UINT32		kernelsz;		/* The size of the kernel image */
-	UINT32		imagesz;		/* The length of this image file ( kernel + romfs + this header) */
-	UINT32		pid;			/* Product ID */
-	UINT32		fastcksum;		/* Partial CRC32 on (First(256), medium(256), last(512)) */
-	UINT32		reserved;
+	u8		magic[4];		/* ASICII: GMTK */
+	u32		checksum;		/* CRC32 */
+	u32		version;		/* x.x.x.x */
+	u32		kernelsz;		/* The size of the kernel image */
+	u32		imagesz;		/* The length of this image file ( kernel + romfs + this header) */
+	u32		pid;			/* Product ID */
+	u32		fastcksum;		/* Partial CRC32 on (First(256), medium(256), last(512)) */
+	u32		reserved;
 }gt_imghdr_t;
 
 #endif
