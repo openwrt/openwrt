c3c0f64f9ddec7c1aef49f2570bde0d63cb3acdb
Florian Fainelli <florian@openwrt.org>
2007-04-16 12:45:37 +0000

    Add wireless profiles for adm5120eb
    SVN-Revision: 6969

---
+++ target/linux/adm5120eb-2.6/profiles/105-Texas.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Texas
+  NAME:=Texas Instruments WiFi (default)
+  PACKAGES:=kmod-acx
+endef
+
+define Profile/Texas/Description
+	Package set compatible with hardware using Texas Instruments WiFi cards
+endef
+$(eval $(call Profile,Texas))
+
