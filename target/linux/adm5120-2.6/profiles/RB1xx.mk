88ac1b73412434629b528089be83d553b6f48176
Florian Fainelli <florian@openwrt.org>
2007-05-11 22:27:16 +0000

    Correction, there is no USB for RB1xx targets
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@7195 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/adm5120-2.6/profiles/RB1xx.mk
+++ target/linux/adm5120-2.6/profiles/RB1xx.mk
@@ -7,11 +7,11 @@
 
 define Profile/RouterBoard
   NAME:=RouterBoard RB1xx
-  PACKAGES:=kmod-madwifi kmod-usb-core kmod-usb-ohci kmod-usb2 patch-cmdline wget2nand
+  PACKAGES:=kmod-madwifi patch-cmdline wget2nand
 endef
 
 define Profile/RouterBoard/Description
-  Package set compatible with the RouterBoard RB1xx devices. Contains USB support and RouterOS to OpenWrt\\\
+  Package set compatible with the RouterBoard RB1xx devices. Contains RouterOS to OpenWrt\\\
   installation scripts.
 endef
 $(eval $(call Profile,RouterBoard))
