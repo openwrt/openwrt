ee1c03f7d650faca6c9ed6c0efed3e4f19def878
Rod Whitby <rod@whitby.id.au>
2007-05-20 12:17:44 +0000

    Support for the ARTOP IDE chipset on IOMega NAS100d (pata-artop).
    SVN-Revision: 7279

--- package/kernel/modules/block.mk
+++ package/kernel/modules/block.mk
@@ -19,3 +19,14 @@ define KernelPackage/ata-piix
 	DEPENDS:=+kmod-libata +kmod-ide-core +kmod-scsi-core
 endef
 $(eval $(call KernelPackage,ata-piix))
+
+define KernelPackage/pata-artop
+	TITLE:=pata-artop
+	DESCRIPTION:=Kernel module for ARTOP PATA controller
+	KCONFIG:=$(CONFIG_PATA_ARTOP)
+	SUBMENU:=$(BLMENU)
+	AUTOLOAD:=$(call AutoLoad,30,libata pata_artop)
+	FILES:=$(LINUX_DIR)/drivers/ata/pata_artop.$(LINUX_KMOD_SUFFIX)
+	DEPENDS:=+kmod-libata +kmod-ide-core +kmod-scsi-core
+endef
+$(eval $(call KernelPackage,pata-artop))
