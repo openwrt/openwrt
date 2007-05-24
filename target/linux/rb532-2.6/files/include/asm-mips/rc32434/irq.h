780472d02f4ae2b25b1a5e489dbb7be1d06f9b13
Felix Fietkau <nbd@openwrt.org>
2007-05-24 14:52:12 +0000

    don't redefine MIPS_CPU_IRQ_BASE
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@7330 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/rb532-2.6/files/include/asm-mips/rc32434/irq.h
+++ target/linux/rb532-2.6/files/include/asm-mips/rc32434/irq.h
@@ -1,9 +1,7 @@
 #ifndef __ASM_MACH_MIPS_IRQ_H
 #define __ASM_MACH_MIPS_IRQ_H
 
-#include <linux/autoconf.h>
-
-#define MIPS_CPU_IRQ_BASE 0
 #define NR_IRQS	256
+#include_next <irq.h>
 
 #endif /* __ASM_MACH_MIPS_IRQ_H */
