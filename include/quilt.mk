9cc960cad96fcef65fd8cf9a0ed8f41c65cfad97
Felix Fietkau <nbd@openwrt.org>
2007-06-03 19:52:54 +0000

    more fixes for the patching stuff (was testing it on the wrong target)
    SVN-Revision: 7483

--- include/quilt.mk
+++ include/quilt.mk
@@ -49,8 +49,9 @@ define Kernel/Patch/Default
 		if [ -d $(GENERIC_PLATFORM_DIR)/patches ]; then $(PATCH) $(LINUX_DIR) $(GENERIC_PLATFORM_DIR)/patches; fi \
 	)
 	$(if $(strip $(QUILT)),$(call Quilt/Patch,./patches,platform/), \
-		if [ -d ./files ]; then $(CP) ./files/* $(LINUX_DIR)/; fi \
+		if [ -d ./patches ]; then $(PATCH) $(LINUX_DIR) ./patches; fi \
 	)
+	$(if $(strip $(QUILT)),touch $(PKG_BUILD_DIR)/.quilt_used)
 endef
 
 $(STAMP_PATCHED): $(STAMP_PREPARED)
