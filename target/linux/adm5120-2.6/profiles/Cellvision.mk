b187a7e8871ee2c20007cd975815b43a6a92c1d3
Florian Fainelli <florian@openwrt.org>
2007-06-11 21:35:12 +0000

    Add a cellvision profile for ADM5120 IP based cameras, build several modules for it : cpia2, usbaudio
    SVN-Revision: 7575

---
+++ target/linux/adm5120-2.6/profiles/Cellvision.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Cellvision
+  NAME:=Cellvision CAS-63x/77x cameras
+  PACKAGES:=kmod-rt2500 kmod-cpia2 kmod-usb-audio
+endef
+
+define Profile/Cellvision/Description
+  Package set compatible with the Cellvision CAS devices.
+endef
+$(eval $(call Profile,Cellvision))
