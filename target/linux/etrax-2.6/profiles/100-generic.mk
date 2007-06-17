f11bf0d9779495c0549f975b3c24e9ec70788094
John Crispin <blogic@openwrt.org>
2007-06-17 02:53:53 +0000

    initial merge for vhdl support on the foxboard
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@7652 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/etrax-2.6/profiles/100-generic.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/default
+  NAME:=Normal (default)
+endef
+
+define Profile/default/Description
+	Normal Foxboard setup (no vhdl)
+endef
+$(eval $(call Profile,default))
+
---
+++ target/linux/etrax-2.6/profiles/101-vhdl-nofb.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/vhdl_no_fb
+  NAME:=FOXVHDL no fb
+#  PACKAGES:=kmod-madwifi
+endef
+
+define Profile/vhdl_no_fb/Description
+	Setup the Foxboard for FOXVHDL support with no framebuffer
+endef
+$(eval $(call Profile,vhdl_no_fb))
+
