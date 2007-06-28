41af082667ac750886f29f3f8338d877567b303f
Felix Fietkau <nbd@openwrt.org>
2007-06-28 05:52:00 +0000

    fill in the kernel version info for every target, move the kernel tarball md5sums to a central place
    SVN-Revision: 7748

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -3,13 +3,24 @@
 ifeq ($(LINUX_VERSION),)
   ifeq ($(KERNEL),2.4)
     LINUX_VERSION:=2.4.34
-    LINUX_RELEASE:=1
-    LINUX_KERNEL_MD5SUM:=f59665540a7f3351ea416a0dad104b55
   else
     LINUX_VERSION:=2.6.21.5
-    LINUX_RELEASE:=1
-    LINUX_KERNEL_MD5SUM:=2e9a302b5d514b231640227d6a2ab7bf
   endif
 endif
+LINUX_RELEASE?=1
+
+ifeq ($(LINUX_VERSION),2.4.34)
+  LINUX_KERNEL_MD5SUM:=f59665540a7f3351ea416a0dad104b55
+endif
+ifeq ($(LINUX_VERSION),2.6.22-rc6)
+  LINUX_KERNEL_MD5SUM:=37adac36f3928f5e05dd2704c083620d
+endif
+ifeq ($(LINUX_VERSION),2.6.21.5)
+  LINUX_KERNEL_MD5SUM:=2e9a302b5d514b231640227d6a2ab7bf
+endif
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM?=x
+
 KERNEL:=2.$(word 2,$(subst ., ,$(strip $(LINUX_VERSION))))
 
