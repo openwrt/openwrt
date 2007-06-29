0751feea41efa3c7e823a698b863d136bb619124
Rod Whitby <rod@whitby.id.au>
2007-06-29 13:48:05 +0000

    Added more essential NAS modules for NSLU2, NAS100d and DSMG600RevA profiles
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@7780 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ixp4xx-2.6/profiles/300-NAS100d.mk
+++ target/linux/ixp4xx-2.6/profiles/300-NAS100d.mk
@@ -7,7 +7,11 @@
 
 define Profile/NAS100d
   NAME:=Iomega NAS 100d
-  PACKAGES:=kmod-ixp4xx-npe kmod-pata-artop kmod-usb2
+  PACKAGES:=kmod-ixp4xx-npe \
+	kmod-madwifi wireless-tools \
+	kmod-scsi-core kmod-libata kmod-pata-artop \
+	kmod-usb-core kmod-usb2 kmod-usb-storage \
+	kmod-fs-ext2 kmod-fs-ext3
 endef
 
 define Profile/NAS100d/Description
