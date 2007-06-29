de21258fc8674018eb0472190362cec6c735dd02
Rod Whitby <rod@whitby.id.au>
2007-06-29 10:47:27 +0000

    Create various profiles for consumer IXP4xx devices
    SVN-Revision: 7774

---
+++ target/linux/ixp4xx-2.6/profiles/400-DSMG600RevA.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/DSMG600RevA
+  NAME:=DSM-G600 Rev A
+  PACKAGES:=kmod-via-velocity -kmod-ixp4xx-npe kmod-pata-artop madwifi kmod-usb2
+endef
+
+define Profile/DSMG600RevA/Description
+	Package set optimized for the DSM-G600 Rev A
+endef
+$(eval $(call Profile,DSMG600RevA))
+
