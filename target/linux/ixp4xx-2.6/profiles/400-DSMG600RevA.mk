1449d4ce2853d299262e971da68837cd0610ce13
Rod Whitby <rod@whitby.id.au>
2007-06-29 14:12:42 +0000

    Added uhci support to dsmg600 profile
    SVN-Revision: 7781

--- target/linux/ixp4xx-2.6/profiles/400-DSMG600RevA.mk
+++ target/linux/ixp4xx-2.6/profiles/400-DSMG600RevA.mk
@@ -10,7 +10,7 @@ define Profile/DSMG600RevA
   PACKAGES:=kmod-via-velocity \
 	kmod-madwifi wireless-tools \
 	kmod-scsi-core kmod-libata kmod-pata-artop \
-	kmod-usb-core kmod-usb2 kmod-usb-storage \
+	kmod-usb-core kmod-usb-uhci kmod-usb2 kmod-usb-storage \
 	kmod-fs-ext2 kmod-fs-ext3
 endef
 
