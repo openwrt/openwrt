648cf7c524aa6e06214121cb7de77f240c4ae11a
John Crispin <john@openwrt.org>
2007-07-08 16:11:40 +0000

    added i686 support and profile for t-vision s100
    SVN-Revision: 7890

---
+++ target/linux/x86-2.6/profiles/s100.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/s100
+  NAME:=T-Vision S-100
+  PACKAGES:=pciutils usbutils vim kmod-usb-uhci kmod-usb2 kmod-soundcore
+  ARCH:=i686
+endef
+
+define Profile/s100/Description
+	T-Online streaming client.
+endef
+$(eval $(call Profile,s100))
