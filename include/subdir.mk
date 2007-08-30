485f2e16f4da17126fb20deb11fdd71535f97f23
Felix Fietkau <nbd@openwrt.org>
2007-08-30 22:43:25 +0000

    fix a dependency bug
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@8561 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/subdir.mk
+++ include/subdir.mk
@@ -38,7 +38,7 @@ endef
 define stampfile
   $(1)/stamp-$(3):=$(STAGING_DIR)/stamp/.$(2)_$(3)
   $$($(1)/stamp-$(3)): $(TMP_DIR)/.build
-	@+$(SCRIPT_DIR)/timestamp.pl -n $$($(1)/stamp-$(3)) $(1) || \
+	@+$(SCRIPT_DIR)/timestamp.pl -n $$($(1)/stamp-$(3)) $(1) $(4) || \
 		$(MAKE) $$($(1)/flags-$(3)) $(1)/$(3)
 	@mkdir -p $$$$(dirname $$($(1)/stamp-$(3)))
 	@touch $$($(1)/stamp-$(3))
