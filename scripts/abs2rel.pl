b518cc2c00e024805c749da5e153f6fca5ca8c30
Nicolas Thill <nico@openwrt.org>
2007-09-01 15:20:33 +0000

    add an abs2rel.pl helper script (returns a path relative to another)
    SVN-Revision: 8570

---
+++ scripts/abs2rel.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# 
+# Copyright (C) 2007 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+# $Id$
+
+use strict;
+require File::Spec;
+
+my $source = shift @ARGV;
+my $target = shift @ARGV;
+my $result = File::Spec->abs2rel($source, $target);
+
+print "$result\n";
