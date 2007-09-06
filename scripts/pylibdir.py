e812d1ff9252fdf8582fba1674cb362b4c60dc1f
Nicolas Thill <nico@openwrt.org>
2007-09-06 02:57:06 +0000

    remove uneeded stuff after python changes in [8639]
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@8640 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/pylibdir.py
+++
@@ -1,5 +0,0 @@
-# Note #! intentionally left out.  This should be called with the version of 
-# python you care about.
-
-import sys
-print ("/usr/lib/python%s.%s/site-packages" % (sys.version_info[0], sys.version_info[1],))
