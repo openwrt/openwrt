8d169cc962910f9f00e83effcb37772e8100e2d2
Felix Fietkau <nbd@openwrt.org>
2007-09-29 00:05:48 +0000

    Refactor downloading code into download.mk Support multiple file downloads Support svn downloads
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@9057 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/unpack.mk
+++ include/unpack.mk
@@ -1,4 +1,9 @@
-ext=$(word $(words $(subst ., ,$(1))),$(subst ., ,$(1)))
+# 
+# Copyright (C) 2006-2007 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
 
 # unpacking files with +s may break on some platforms. this typically emits error code 2
 ifneq ($(HOST_OS),Linux)
