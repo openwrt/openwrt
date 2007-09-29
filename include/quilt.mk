6e3b88381a800b3cbf6d69979841d42d827688ac
Felix Fietkau <nbd@openwrt.org>
2007-09-29 03:28:03 +0000

    refactor quilt patching code
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@9061 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/quilt.mk
+++ include/quilt.mk
@@ -17,7 +17,11 @@ ifeq ($(MAKECMDGOALS),refresh)
   override QUILT=1
 endif
 
-define Quilt/Patch
+define PatchDir/Quilt
+	@if [ -s $(1)/series ]; then \
+		mkdir -p $(PKG_BUILD_DIR)/patches$(if $(2),/$(2)); \
+		cp $(1)/series $(PKG_BUILD_DIR)/patches$(if $(2),/$(2))/; \
+	fi
 	@for patch in $$$$( (cd $(1) && if [ -f series ]; then grep -v '^#' series; else ls; fi; ) 2>/dev/null ); do ( \
 		cp "$(1)/$$$$patch" $(PKG_BUILD_DIR); \
 		cd $(PKG_BUILD_DIR); \
@@ -25,75 +29,69 @@ define Quilt/Patch
 		quilt push -f >/dev/null 2>/dev/null; \
 		rm -f "$$$$patch"; \
 	); done
+	$(if $(2),@echo $(2) >> $(PKG_BUILD_DIR)/patches/.subdirs)
+endef
+
+define PatchDir/Default
+	@if [ -d "$(1)" -a "$$$$(ls $(1) | wc -l)" -gt 0 ]; then \
+		if [ -s "$(1)/series" ]; then \
+			grep -vE '^#' $(1)/series | xargs -n1 \
+				$(PATCH) $(PKG_BUILD_DIR) "$(1)"; \
+		else \
+			$(PATCH) $(PKG_BUILD_DIR) "$(1)"; \
+		fi; \
+	fi
+endef
+
+define PatchDir
+$(call PatchDir/$(if $(strip $(QUILT)),Quilt,Default),$(strip $(1)),$(strip $(2)))
 endef
 
 QUILT?=$(strip $(shell test -f $(PKG_BUILD_DIR)/.quilt_used && echo y))
 ifneq ($(QUILT),)
   STAMP_PATCHED:=$(PKG_BUILD_DIR)/.quilt_patched
   override CONFIG_AUTOREBUILD=
-  define Build/Patch/Default
-	rm -rf $(PKG_BUILD_DIR)/patches
-	mkdir -p $(PKG_BUILD_DIR)/patches
-	$(call Quilt/Patch,$(PATCH_DIR),)
-	@echo
-	touch $(PKG_BUILD_DIR)/.quilt_used
-  endef
   $(STAMP_CONFIGURED): $(STAMP_PATCHED) FORCE
   prepare: $(STAMP_PATCHED)
   quilt-check: $(STAMP_PATCHED)
-else
-  define Build/Patch/Default
-	@if [ -d $(PATCH_DIR) -a "$$$$(ls $(PATCH_DIR) | wc -l)" -gt 0 ]; then \
-		if [ -f $(PATCH_DIR)/series ]; then \
-			grep -vE '^#' $(PATCH_DIR)/series | xargs -n1 \
-				$(PATCH) $(PKG_BUILD_DIR) $(PATCH_DIR); \
-		else \
-			$(PATCH) $(PKG_BUILD_DIR) $(PATCH_DIR); \
-		fi; \
-	fi
-  endef
 endif
 
+define Build/Patch/Default
+	$(if $(QUILT),rm -rf $(PKG_BUILD_DIR)/patches; mkdir -p $(PKG_BUILD_DIR)/patches)
+	$(call PatchDir,$(PATCH_DIR),)
+	$(if $(QUILT),touch $(PKG_BUILD_DIR)/.quilt_used)
+endef
+
 define Kernel/Patch/Default
+	$(if $(QUILT),rm -rf $(PKG_BUILD_DIR)/patches; mkdir -p $(PKG_BUILD_DIR)/patches)
 	if [ -d $(GENERIC_PLATFORM_DIR)/files ]; then $(CP) $(GENERIC_PLATFORM_DIR)/files/* $(LINUX_DIR)/; fi
 	if [ -d ./files ]; then $(CP) ./files/* $(LINUX_DIR)/; fi
-	$(if $(strip $(QUILT)),$(call Quilt/Patch,$(GENERIC_PATCH_DIR),generic/), \
-		if [ -d $(GENERIC_PATCH_DIR) ]; then $(PATCH) $(LINUX_DIR) $(GENERIC_PATCH_DIR); fi \
-	)
-	$(if $(strip $(QUILT)),$(call Quilt/Patch,$(PATCH_DIR),platform/), \
-		if [ -d $(PATCH_DIR) ]; then $(PATCH) $(LINUX_DIR) $(PATCH_DIR); fi \
-	)
+	$(call PatchDir,$(GENERIC_PATCH_DIR),generic/)
+	$(call PatchDir,$(PATCH_DIR),platform/)
 	$(if $(strip $(QUILT)),touch $(PKG_BUILD_DIR)/.quilt_used)
 endef
 
-ifeq ($(TARGET_BUILD),1)
 $(STAMP_PATCHED): $(STAMP_PREPARED)
-	@cd $(PKG_BUILD_DIR); quilt pop -a -f >/dev/null 2>/dev/null || true
-	(\
+	@( \
 		cd $(PKG_BUILD_DIR)/patches; \
-		rm -f series; \
-		for file in *; do \
-			if [ -f $$file/series ]; then \
-				echo "Converting $$file/series"; \
-				awk -v file="$$file/" '$$0 !~ /^#/ { print file $$0 }' $$file/series >> series; \
-			else \
-				echo "Sorting patches in $$file"; \
-				find $$file/* -type f \! -name series | sort >> series; \
-			fi; \
-		done; \
+		quilt pop -a -f >/dev/null 2>/dev/null; \
+		if [ -s ".subdirs" ]; then \
+			rm -f series; \
+			for file in $$(cat .subdirs); do \
+				if [ -f $$file/series ]; then \
+					echo "Converting $$file/series"; \
+					awk -v file="$$file/" '$$0 !~ /^#/ { print file $$0 }' $$file/series >> series; \
+				else \
+					echo "Sorting patches in $$file"; \
+					find $$file/* -type f \! -name series | sort >> series; \
+				fi; \
+			done; \
+		else \
+			find * -type f \! -name series | sort > series; \
+		fi; \
 	)
 	if [ -s "$(PKG_BUILD_DIR)/patches/series" ]; then (cd $(PKG_BUILD_DIR); quilt push -a); fi
 	touch $@
-else
-$(STAMP_PATCHED): $(STAMP_PREPARED)
-	@cd $(PKG_BUILD_DIR); quilt pop -a -f >/dev/null 2>/dev/null || true
-	(\
-		cd $(PKG_BUILD_DIR)/patches; \
-		find * -type f \! -name series | sort > series; \
-	)
-	if [ -s "$(PKG_BUILD_DIR)/patches/series" ]; then (cd $(PKG_BUILD_DIR); quilt push -a); fi
-	touch $@
-endif
 
 define Quilt/RefreshDir
 	mkdir -p $(1)
