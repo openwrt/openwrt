9e8f2ca1f0d1fe2ea5688c466f2f8b40a5e870c2
John Crispin <john@openwrt.org>
2007-10-04 20:58:11 +0000

    added subtargets to x86
    SVN-Revision: 9128

---
+++ target/linux/x86/mediacenter/target.mk
@@ -0,0 +1,9 @@
+ARCH:=i686
+SUBTARGET:=Mediacenter
+BOARDNAME:=X86 (Mediacenter)
+
+define Target/Description
+	Build firmware images for i686 based Mediacenters
+	(e.g : Custom, T-Vision S100 ...)
+endef
+
