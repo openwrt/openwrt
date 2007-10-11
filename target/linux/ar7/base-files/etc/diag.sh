824c1abfe8b96c5e0731e0cf4af968f20fd88ac0
Eugene Konev <ejka@openwrt.org>
2007-10-11 01:39:24 +0000

    we are using leds-gpio now
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@9252 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar7/base-files/etc/diag.sh
+++ target/linux/ar7/base-files/etc/diag.sh
@@ -4,13 +4,13 @@
 set_led() {
 	local led="$1"
 	local state="$2"
-	[ -d "/sys/class/leds/ar7:$led" ] && echo "$state" > "/sys/class/leds/ar7:$led/brightness"
+	[ -d "/sys/class/leds/$led" ] && echo "$state" > "/sys/class/leds/$led/brightness"
 }
 
 set_state() {
 	case "$1" in
 		preinit)
-			set_led status 1
+			set_led status 255
 		;;
 		done)
 			set_led status 0
