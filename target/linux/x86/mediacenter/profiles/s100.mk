1aaf14613a8be0b0282f1767b37b35990fb7da52
Florian Fainelli <florian@openwrt.org>
2007-10-29 10:09:45 +0000

    Update S100 profile (#2587)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@9459 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/x86/mediacenter/profiles/s100.mk
+++ target/linux/x86/mediacenter/profiles/s100.mk
@@ -8,9 +8,10 @@
 define Profile/s100
   NAME:=T-Vision S-100
   FEATURES+=usb
+  PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb-hid kmod-p54-pci kmod-8139too kmod-ata-core kmod-ata-piix
 endef
 
 define Profile/s100/Description
-	T-Online streaming client.
+	T-Online streaming client. Contains WLAN and USB support.
 endef
 $(eval $(call Profile,s100))
