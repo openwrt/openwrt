38101a0cdc903b6f07bea96bea1fa8853ef0a787
Travis Kemen <thepeople@openwrt.org>
2007-11-28 04:07:24 +0000

    add support for the PCEngines Alix
    SVN-Revision: 9611

---
+++ target/linux/x86/generic/profiles/Alix.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Alix
+  NAME:=PCEngines Alix
+  PACKAGES:=kmod-via-rhine
+endef
+
+define Profile/Alix/Description
+        Package set compatible with the PCEngines Alix. Contains VIA Rhine III VT6105M support
+endef
+$(eval $(call Profile,Alix))
