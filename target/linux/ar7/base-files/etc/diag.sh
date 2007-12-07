eeebde136dabb336e85491057fe8af8c351e9c04
Felix Fietkau <nbd@openwrt.org>
2007-12-07 00:03:14 +0000

    Minor patch to the AR7 diag.sh to make the status LED a bit more useful. From the patch:
    This setup gives us 3.5 distinguishable states:
    
    - Solid OFF:  Bootloader running, or kernel hung (timer task stalled)
    - Solid ON:   Kernel hung (timer task stalled)
    - 5Hz blink:  preinit
    - Heartbeat:  normal operation
    
    Signed-off-by: oliver@opencloud.com
    
    SVN-Revision: 9666

--- target/linux/ar7/base-files/etc/diag.sh
+++ target/linux/ar7/base-files/etc/diag.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 # Copyright (C) 2007 OpenWrt.org
 
-set_led() {
-	local led="$1"
-	local state="$2"
-	[ -d "/sys/class/leds/$led" ] && echo "$state" > "/sys/class/leds/$led/brightness"
-}
+# This setup gives us 3.5 distinguishable states:
+#
+# Solid OFF:  Bootloader running, or kernel hung (timer task stalled)
+# Solid ON:   Kernel hung (timer task stalled)
+# 5Hz blink:  preinit
+# Heartbeat:  normal operation
 
 set_state() {
-	case "$1" in
-		preinit)
-			set_led status 255
-		;;
-		done)
-			set_led status 0
-		;;
-	esac
+        case "$1" in
+                preinit)
+                        [ -d /sys/class/leds/status ] && {
+                                echo timer >/sys/class/leds/status/trigger
+                                echo 100 >/sys/class/leds/status/delay_on
+                                echo 100 >/sys/class/leds/status/delay_off
+                        }
+                ;;
+                done)
+                        [ -d /sys/class/leds/status ] && {
+                                echo heartbeat >/sys/class/leds/status/trigger
+                        }
+                ;;
+        esac
 }
