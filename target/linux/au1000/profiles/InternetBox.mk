dac2f8878af3dbc78a2fb167a1ce36e1a9f39c7f
John Crispin <john@openwrt.org>
2007-12-20 22:17:41 +0000

    add new au1000 profiles
    SVN-Revision: 9812

---
+++ target/linux/au1000/profiles/InternetBox.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2007 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/InternetBox
+  NAME:=T-Mobile "InternetBox"
+  PACKAGES:=kmod-madwifi \
+		ldconfig ldd \
+		hostapd hostapd-utils ip tc wpa-supplicant wpa-cli
+endef
+
+define Profile/InternetBox/Description
+	Package set for the T-Mobile "InternetBox" (TMD SB1-S)
+endef
+
+$(eval $(call Profile,InternetBox))
---
+++ target/linux/au1000/profiles/MeshCube.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2007 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/MeshCube
+  NAME:=MeshCube MTX-1
+  PACKAGES:=kmod-madwifi kmod-hostap kmod-hostap-pci kmod-net-prism54 \
+	kmod-usb-core kmod-usb-ohci kmod-usb-storage kmod-fs-ext3 kmod-fs-vfat \
+	ldconfig ldd \
+	hostap-utils hostapd hostapd-utils ip tc wpa-supplicant wpa-cli
+endef
+
+define Profile/MeshCube/Description
+	Package set for the 4G MeshCube (MTX-1)
+endef
+
+$(eval $(call Profile,MeshCube))
