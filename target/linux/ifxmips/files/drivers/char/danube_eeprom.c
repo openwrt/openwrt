726b82e53124812a26dc9b3a4afd64f676fbf0ff
John Crispin <blogic@openwrt.org>
2007-12-20 22:29:45 +0000

    move danube folder to ifxmips, to allow future integration of the other chips
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@9815 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ifxmips/files/drivers/char/danube_eeprom.c
@@ -0,0 +1,541 @@
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *   This driver was originally based on the INCA-IP driver, but due to
+ *   fundamental conceptual drawbacks there has been changed a lot.
+ *
+ *   Based on INCA-IP driver Copyright (c) 2003 Gary Jennejohn <gj@denx.de>
+ *   Based on the VxWorks drivers Copyright (c) 2002, Infineon Technologies.
+ *
+ *   Copyright (C) 2006 infineon
+ *   Copyright (C) 2007 John Crispin <blogic@openwrt.org> 
+ *
+ */
+
+#define IFAP_EEPROM_DRV_VERSION "0.0.1"
+
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/signal.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/interrupt.h>
+#include <linux/major.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/fcntl.h>
+#include <linux/ptrace.h>
+#include <linux/mm.h>
+#include <linux/ioport.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/spinlock.h>
+#include <linux/slab.h>
+#include <asm/system.h>
+#include <asm/io.h>
+#include <asm/irq.h>
+#include <asm/uaccess.h>
+#include <asm/bitops.h>
+
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/version.h>
+
+#include <asm/danube/danube.h>
+#include <asm/danube/danube_irq.h>
+#include <asm/danube/ifx_ssc_defines.h>
+#include <asm/danube/ifx_ssc.h>
+
+/* allow the user to set the major device number */
+static int danube_eeprom_maj = 0;
+
+extern int ifx_ssc_init (void);
+extern int ifx_ssc_open (struct inode *inode, struct file *filp);
+extern int ifx_ssc_close (struct inode *inode, struct file *filp);
+extern void ifx_ssc_cleanup_module (void);
+extern int ifx_ssc_ioctl (struct inode *inode, struct file *filp,
+			  unsigned int cmd, unsigned long data);
+extern ssize_t ifx_ssc_kwrite (int port, const char *kbuf, size_t len);
+extern ssize_t ifx_ssc_kread (int port, char *kbuf, size_t len);
+
+extern int ifx_ssc_cs_low (unsigned int pin);
+extern int ifx_ssc_cs_high (unsigned int pin);
+extern int ifx_ssc_txrx (char *tx_buf, unsigned int tx_len, char *rx_buf, unsigned int rx_len);
+extern int ifx_ssc_tx (char *tx_buf, unsigned int tx_len);
+extern int ifx_ssc_rx (char *rx_buf, unsigned int rx_len);
+
+#define EEPROM_CS IFX_SSC_WHBGPOSTAT_OUT0_POS
+
+/* commands for EEPROM, x25160, x25140 */
+#define EEPROM_WREN			((unsigned char)0x06)
+#define EEPROM_WRDI			((unsigned char)0x04)
+#define EEPROM_RDSR			((unsigned char)0x05)
+#define EEPROM_WRSR			((unsigned char)0x01)
+#define EEPROM_READ			((unsigned char)0x03)
+#define EEPROM_WRITE			((unsigned char)0x02)
+#define EEPROM_PAGE_SIZE		4
+#define EEPROM_SIZE			512
+
+static int
+eeprom_rdsr (void)
+{
+	int ret = 0;
+	unsigned char cmd = EEPROM_RDSR;
+	unsigned long flag;
+	char status;
+
+	local_irq_save(flag);
+
+	if ((ret = ifx_ssc_cs_low(EEPROM_CS)) == 0)
+		if ((ret = ifx_ssc_txrx(&cmd, 1, &status, 1)) >= 0)
+			ret = status & 1;
+
+	ifx_ssc_cs_high(EEPROM_CS);
+	local_irq_restore(flag);
+
+	return ret;
+}
+
+void
+eeprom_wip_over (void)
+{
+	while (eeprom_rdsr())
+		printk("waiting for eeprom\n");
+}
+
+static int
+eeprom_wren (void)
+{
+	unsigned char cmd = EEPROM_WREN;
+	int ret = 0;
+	unsigned long flag;
+
+	local_irq_save(flag);
+	if ((ret = ifx_ssc_cs_low(EEPROM_CS)) == 0)
+		if ((ret = ifx_ssc_tx(&cmd, 1)) >= 0)
+			ret = 0;
+
+	ifx_ssc_cs_high(EEPROM_CS);
+	local_irq_restore(flag);
+
+	if (!ret)
+		eeprom_wip_over();
+
+	return ret;
+}
+
+static int
+eeprom_wrsr (void)
+{
+	int ret = 0;
+	unsigned char cmd[2];
+	unsigned long flag;
+
+	cmd[0] = EEPROM_WRSR;
+	cmd[1] = 0;
+
+	if ((ret = eeprom_wren()))
+	{
+		printk ("eeprom_wren fails\n");
+		goto out1;
+	}
+
+	local_irq_save(flag);
+
+	if ((ret = ifx_ssc_cs_low(EEPROM_CS)))
+		goto out;
+
+	if ((ret = ifx_ssc_tx(cmd, 2)) < 0) {
+		ifx_ssc_cs_high(EEPROM_CS);
+		goto out;
+	}
+
+	if ((ret = ifx_ssc_cs_low(EEPROM_CS)))
+		goto out;
+
+	local_irq_restore(flag);
+	eeprom_wip_over();
+
+	return ret;
+
+out:
+	local_irq_restore (flag);
+	eeprom_wip_over ();
+
+out1:
+	return ret;
+}
+
+static int
+eeprom_read (unsigned int addr, unsigned char *buf, unsigned int len)
+{
+	int ret = 0;
+	unsigned char write_buf[2];
+	unsigned int eff = 0;
+	unsigned long flag;
+
+	while (1)
+	{
+		eeprom_wip_over();
+		eff = EEPROM_PAGE_SIZE - (addr % EEPROM_PAGE_SIZE);
+		eff = (eff < len) ? eff : len;
+		local_irq_save(flag);
+
+		if ((ret = ifx_ssc_cs_low(EEPROM_CS)) < 0)
+			goto out;
+
+		write_buf[0] = EEPROM_READ | ((unsigned char) ((addr & 0x100) >> 5));
+		write_buf[1] = (addr & 0xff);
+
+		if ((ret = ifx_ssc_txrx (write_buf, 2, buf, eff)) != eff)
+		{
+			printk("ssc_txrx fails %d\n", ret);
+			ifx_ssc_cs_high (EEPROM_CS);
+			goto out;
+		}
+
+		buf += ret;
+		len -= ret;
+		addr += ret;
+
+		if ((ret = ifx_ssc_cs_high(EEPROM_CS)))
+			goto out;
+
+		local_irq_restore(flag);
+
+		if (len <= 0)
+			goto out2;
+	}
+
+out:
+	local_irq_restore (flag);
+out2:
+	return ret;
+}
+
+static int
+eeprom_write (unsigned int addr, unsigned char *buf, unsigned int len)
+{
+	int ret = 0;
+	unsigned int eff = 0;
+	unsigned char write_buf[2];
+	int i;
+	unsigned char rx_buf[EEPROM_PAGE_SIZE];
+
+	while (1)
+	{
+		eeprom_wip_over();
+
+		if ((ret = eeprom_wren()))
+		{
+			printk("eeprom_wren fails\n");
+			goto out;
+		}
+
+		write_buf[0] = EEPROM_WRITE | ((unsigned char) ((addr & 0x100) >> 5));
+		write_buf[1] = (addr & 0xff);
+
+		eff = EEPROM_PAGE_SIZE - (addr % EEPROM_PAGE_SIZE);
+		eff = (eff < len) ? eff : len;
+
+		printk("EEPROM Write:\n");
+		for (i = 0; i < eff; i++) {
+			printk("%2x ", buf[i]);
+			if ((i % 16) == 15)
+				printk("\n");
+		}
+		printk("\n");
+
+		if ((ret = ifx_ssc_cs_low(EEPROM_CS)))
+			goto out;
+
+		if ((ret = ifx_ssc_tx (write_buf, 2)) < 0)
+		{
+			printk("ssc_tx fails %d\n", ret);
+			ifx_ssc_cs_high(EEPROM_CS);
+			goto out;
+		}
+
+		if ((ret = ifx_ssc_tx (buf, eff)) != eff)
+		{
+			printk("ssc_tx fails %d\n", ret);
+			ifx_ssc_cs_high(EEPROM_CS);
+			goto out;
+		}
+
+		buf += ret;
+		len -= ret;
+		addr += ret;
+
+		if ((ret = ifx_ssc_cs_high (EEPROM_CS)))
+			goto out;
+
+		printk ("<==");
+		eeprom_read((addr - eff), rx_buf, eff);
+		for (i = 0; i < eff; i++)
+		{
+			printk ("[%x]", rx_buf[i]);
+		}
+		printk ("\n");
+
+		if (len <= 0)
+			break;
+	}
+
+out:
+	return ret;
+}
+
+int
+danube_eeprom_open (struct inode *inode, struct file *filp)
+{
+	filp->f_pos = 0;
+	return 0;
+}
+
+int
+danube_eeprom_close (struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+int
+danube_eeprom_ioctl (struct inode *inode, struct file *filp, unsigned int cmd, unsigned long data)
+{
+	return 0;
+}
+
+ssize_t
+danube_eeprom_read (char *buf, size_t len, unsigned int addr)
+{
+	int ret = 0;
+	unsigned int data;
+
+	printk("addr:=%d\n", addr);
+	printk("len:=%d\n", len);
+
+	if ((addr + len) > EEPROM_SIZE)
+	{
+		printk("invalid len\n");
+		addr = 0;
+		len = EEPROM_SIZE / 2;
+	}
+
+	if ((ret = ifx_ssc_open((struct inode *) 0, NULL)))
+	{
+		printk("danube_eeprom_open fails\n");
+		goto out;
+	}
+
+	data = (unsigned int)IFX_SSC_MODE_RXTX;
+
+	if ((ret = ifx_ssc_ioctl((struct inode *) 0, NULL, IFX_SSC_RXTX_MODE_SET, (unsigned long) &data)))
+	{
+		printk("set RXTX mode fails\n");
+		goto out;
+	}
+
+	if ((ret = eeprom_wrsr()))
+	{
+		printk("EEPROM reset fails\n");
+		goto out;
+	}
+
+	if ((ret = eeprom_read(addr, buf, len)))
+	{
+		printk("eeprom read fails\n");
+		goto out;
+	}
+
+out:
+	if (ifx_ssc_close((struct inode *) 0, NULL))
+		printk("danube_eeprom_close fails\n");
+
+	return len;
+}
+EXPORT_SYMBOL(danube_eeprom_read);
+
+static ssize_t
+danube_eeprom_fops_read (struct file *filp, char *ubuf, size_t len, loff_t * off)
+{
+	int ret = 0;
+	unsigned char ssc_rx_buf[EEPROM_SIZE];
+	long flag;
+
+	if (*off >= EEPROM_SIZE)
+		return 0;
+
+	if (*off + len > EEPROM_SIZE)
+		len = EEPROM_SIZE - *off;
+
+	if (len == 0)
+		return 0;
+
+	local_irq_save(flag);
+
+	if ((ret = danube_eeprom_read(ssc_rx_buf, len, *off)) < 0)
+	{
+		printk("read fails, err=%x\n", ret);
+		local_irq_restore(flag);
+		return ret;
+	}
+
+	if (copy_to_user((void*)ubuf, ssc_rx_buf, ret) != 0)
+	{
+		local_irq_restore(flag);
+		ret = -EFAULT;
+	}
+
+	local_irq_restore(flag);
+	*off += len;
+
+	return len;
+}
+
+ssize_t
+danube_eeprom_write (char *buf, size_t len, unsigned int addr)
+{
+	int ret = 0;
+	unsigned int data;
+
+	if ((ret = ifx_ssc_open ((struct inode *) 0, NULL)))
+	{
+		printk ("danube_eeprom_open fails\n");
+		goto out;
+	}
+
+	data = (unsigned int) IFX_SSC_MODE_RXTX;
+
+	if ((ret = ifx_ssc_ioctl ((struct inode *) 0, NULL, IFX_SSC_RXTX_MODE_SET, (unsigned long) &data)))
+	{
+		printk ("set RXTX mode fails\n");
+		goto out;
+	}
+
+	if ((ret = eeprom_wrsr ())) {
+		printk ("EEPROM reset fails\n");
+		goto out;
+	}
+
+	if ((ret = eeprom_write (addr, buf, len))) {
+		printk ("eeprom write fails\n");
+		goto out;
+	}
+
+out:
+	if (ifx_ssc_close ((struct inode *) 0, NULL))
+		printk ("danube_eeprom_close fails\n");
+
+	return ret;
+}
+EXPORT_SYMBOL(danube_eeprom_write);
+
+static ssize_t
+danube_eeprom_fops_write (struct file *filp, const char *ubuf, size_t len, loff_t * off)
+{
+	int ret = 0;
+	unsigned char ssc_tx_buf[EEPROM_SIZE];
+
+	if (*off >= EEPROM_SIZE)
+		return 0;
+
+	if (len + *off > EEPROM_SIZE)
+		len = EEPROM_SIZE - *off;
+
+	if ((ret = copy_from_user (ssc_tx_buf, ubuf, len)))
+		return EFAULT;
+
+	ret = danube_eeprom_write (ssc_tx_buf, len, *off);
+
+	if (ret > 0)
+		*off = ret;
+
+	return ret;
+}
+
+loff_t
+danube_eeprom_llseek (struct file * filp, loff_t off, int whence)
+{
+	loff_t newpos;
+	switch (whence) {
+	case SEEK_SET:
+		newpos = off;
+		break;
+
+	case SEEK_CUR:
+		newpos = filp->f_pos + off;
+		break;
+
+	default:
+		return -EINVAL;
+	}
+
+	if (newpos < 0)
+		return -EINVAL;
+
+	filp->f_pos = newpos;
+
+	return newpos;
+}
+
+static struct file_operations danube_eeprom_fops = {
+      owner:THIS_MODULE,
+      llseek:danube_eeprom_llseek,
+      read:danube_eeprom_fops_read,
+      write:danube_eeprom_fops_write,
+      ioctl:danube_eeprom_ioctl,
+      open:danube_eeprom_open,
+      release:danube_eeprom_close,
+};
+
+int __init
+danube_eeprom_init (void)
+{
+	int ret = 0;
+
+	danube_eeprom_maj = register_chrdev(0, "eeprom", &danube_eeprom_fops);
+
+	if (danube_eeprom_maj < 0)
+	{
+		printk("failed to register eeprom device\n");
+		ret = -EINVAL;
+		
+		goto out;
+	}
+
+	printk("danube_eeprom : /dev/eeprom mayor %d\n", danube_eeprom_maj);
+
+out:
+	return ret;
+}
+
+void __exit
+danube_eeprom_cleanup_module (void)
+{
+	/*if (unregister_chrdev (danube_eeprom_maj, "eeprom")) {
+		printk ("Unable to unregister major %d for the EEPROM\n",
+			maj);
+	}*/
+}
+
+module_exit (danube_eeprom_cleanup_module);
+module_init (danube_eeprom_init);
+
+MODULE_LICENSE ("GPL");
+MODULE_AUTHOR ("Peng Liu");
+MODULE_DESCRIPTION ("IFAP EEPROM driver");
+MODULE_SUPPORTED_DEVICE ("danube_eeprom");
+
+
---
+++ target/linux/ifxmips/files/drivers/net/danube_mii0.c
@@ -0,0 +1,433 @@
+/*
+ *   drivers/net/danube_mii0.c
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *   Copyright (C) 2005 Infineon
+ *
+ *   Rewrite of Infineon Danube code, thanks to infineon for the support,
+ *   software and hardware
+ *
+ *   Copyright (C) 2007 John Crispin <blogic@openwrt.org> 
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/interrupt.h>
+#include <asm/uaccess.h>
+#include <linux/in.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/skbuff.h>
+#include <linux/mm.h>
+#include <linux/ethtool.h>
+#include <asm/checksum.h>
+#include <linux/init.h>
+#include <asm/delay.h>
+#include <asm/danube/danube.h>
+#include <asm/danube/danube_mii0.h>
+#include <asm/danube/danube_dma.h>
+#include <asm/danube/danube_pmu.h>
+
+static struct net_device danube_mii0_dev;
+static unsigned char u_boot_ethaddr[MAX_ADDR_LEN];
+
+void
+danube_write_mdio (u32 phy_addr, u32 phy_reg, u16 phy_data)
+{
+	u32 val = MDIO_ACC_REQUEST |
+		((phy_addr & MDIO_ACC_ADDR_MASK) << MDIO_ACC_ADDR_OFFSET) |
+		((phy_reg & MDIO_ACC_REG_MASK) << MDIO_ACC_REG_OFFSET) |
+		phy_data;
+
+	while (readl(DANUBE_PPE32_MDIO_ACC) & MDIO_ACC_REQUEST);
+	writel(val, DANUBE_PPE32_MDIO_ACC);
+}
+
+unsigned short
+danube_read_mdio (u32 phy_addr, u32 phy_reg)
+{
+	u32 val = MDIO_ACC_REQUEST | MDIO_ACC_READ |
+		((phy_addr & MDIO_ACC_ADDR_MASK) << MDIO_ACC_ADDR_OFFSET) |
+		((phy_reg & MDIO_ACC_REG_MASK) << MDIO_ACC_REG_OFFSET);
+
+	writel(val, DANUBE_PPE32_MDIO_ACC);
+	while (readl(DANUBE_PPE32_MDIO_ACC) & MDIO_ACC_REQUEST){};
+	val = readl(DANUBE_PPE32_MDIO_ACC) & MDIO_ACC_VAL_MASK;
+
+	return val;
+}
+
+int
+danube_switch_open (struct net_device *dev)
+{
+	struct switch_priv* priv = (struct switch_priv*)dev->priv;
+	struct dma_device_info* dma_dev = priv->dma_device;
+	int i;
+
+	for (i = 0; i < dma_dev->max_rx_chan_num; i++)
+	{
+		if ((dma_dev->rx_chan[i])->control == DANUBE_DMA_CH_ON)
+			(dma_dev->rx_chan[i])->open(dma_dev->rx_chan[i]);
+	}
+
+	netif_start_queue(dev);
+
+	return 0;
+}
+
+int
+switch_release (struct net_device *dev){
+	struct switch_priv* priv = (struct switch_priv*)dev->priv;
+	struct dma_device_info* dma_dev = priv->dma_device;
+	int i;
+
+	for (i = 0; i < dma_dev->max_rx_chan_num; i++)
+		dma_dev->rx_chan[i]->close(dma_dev->rx_chan[i]);
+
+	netif_stop_queue(dev);
+
+	return 0;
+}
+
+int
+switch_hw_receive (struct net_device* dev,struct dma_device_info* dma_dev)
+{
+	struct switch_priv *priv = (struct switch_priv*)dev->priv;
+	unsigned char* buf = NULL;
+	struct sk_buff *skb = NULL;
+	int len = 0;
+
+	len = dma_device_read(dma_dev, &buf, (void**)&skb);
+
+	if (len >= ETHERNET_PACKET_DMA_BUFFER_SIZE)
+	{
+		printk("packet too large %d\n",len);
+		goto switch_hw_receive_err_exit;
+	}
+
+	/* remove CRC */
+	len -= 4;
+	if (skb == NULL )
+	{
+		printk("cannot restore pointer\n");
+		goto switch_hw_receive_err_exit;
+	}
+
+	if (len > (skb->end - skb->tail))
+	{
+		printk("BUG, len:%d end:%p tail:%p\n", (len+4), skb->end, skb->tail);
+		goto switch_hw_receive_err_exit;
+	}
+
+	skb_put(skb, len);
+	skb->dev = dev;
+	skb->protocol = eth_type_trans(skb, dev);
+	netif_rx(skb);
+
+	priv->stats.rx_packets++;
+	priv->stats.rx_bytes += len;
+
+	return 0;
+
+switch_hw_receive_err_exit:
+	if (len == 0)
+	{
+		if(skb)
+			dev_kfree_skb_any(skb);
+		priv->stats.rx_errors++;
+		priv->stats.rx_dropped++;
+
+		return -EIO;
+	} else {
+		return len;
+	}
+}
+
+int
+switch_hw_tx (char *buf, int len, struct net_device *dev)
+{
+	int ret = 0;
+	struct switch_priv *priv = dev->priv;
+	struct dma_device_info* dma_dev = priv->dma_device;
+
+	ret = dma_device_write(dma_dev, buf, len, priv->skb);
+
+	return ret;
+}
+
+int
+switch_tx (struct sk_buff *skb, struct net_device *dev)
+{
+	int len;
+	char *data;
+	struct switch_priv *priv = dev->priv;
+	struct dma_device_info* dma_dev = priv->dma_device;
+
+	len = skb->len < ETH_ZLEN ? ETH_ZLEN : skb->len;
+	data = skb->data;
+	priv->skb = skb;
+	dev->trans_start = jiffies;
+	// TODO we got more than 1 dma channel, so we should do something intelligent
+	// here to select one
+	dma_dev->current_tx_chan = 0;
+
+	wmb();
+
+	if (switch_hw_tx(data, len, dev) != len)
+	{
+		dev_kfree_skb_any(skb);
+		priv->stats.tx_errors++;
+		priv->stats.tx_dropped++;
+	} else {
+		priv->stats.tx_packets++;
+		priv->stats.tx_bytes+=len;
+	}
+
+	return 0;
+}
+
+void
+switch_tx_timeout (struct net_device *dev)
+{
+	int i;
+	struct switch_priv* priv = (struct switch_priv*)dev->priv;
+
+	priv->stats.tx_errors++;
+
+	for (i = 0; i < priv->dma_device->max_tx_chan_num; i++)
+	{
+		priv->dma_device->tx_chan[i]->disable_irq(priv->dma_device->tx_chan[i]);
+	}
+
+	netif_wake_queue(dev);
+
+	return;
+}
+
+int
+dma_intr_handler (struct dma_device_info* dma_dev, int status)
+{
+	int i;
+
+	switch (status)
+	{
+	case RCV_INT:
+		switch_hw_receive(&danube_mii0_dev, dma_dev);
+		break;
+
+	case TX_BUF_FULL_INT:
+		printk("tx buffer full\n");
+		netif_stop_queue(&danube_mii0_dev);
+		for (i = 0; i < dma_dev->max_tx_chan_num; i++)
+		{
+			if ((dma_dev->tx_chan[i])->control==DANUBE_DMA_CH_ON)
+				dma_dev->tx_chan[i]->enable_irq(dma_dev->tx_chan[i]);
+		}
+		break;
+
+	case TRANSMIT_CPT_INT:
+		for (i = 0; i < dma_dev->max_tx_chan_num; i++)
+			dma_dev->tx_chan[i]->disable_irq(dma_dev->tx_chan[i]);
+
+		netif_wake_queue(&danube_mii0_dev);
+		break;
+	}
+
+	return 0;
+}
+
+unsigned char*
+danube_etop_dma_buffer_alloc (int len, int *byte_offset, void **opt)
+{
+	unsigned char *buffer = NULL;
+	struct sk_buff *skb = NULL;
+
+	skb = dev_alloc_skb(ETHERNET_PACKET_DMA_BUFFER_SIZE);
+	if (skb == NULL)
+		return NULL;
+
+	buffer = (unsigned char*)(skb->data);
+	skb_reserve(skb, 2);
+	*(int*)opt = (int)skb;
+	*byte_offset = 2;
+
+	return buffer;
+}
+
+void
+danube_etop_dma_buffer_free (unsigned char *dataptr, void *opt)
+{
+	struct sk_buff *skb = NULL;
+
+	if(opt == NULL)
+	{
+		kfree(dataptr);
+	} else {
+		skb = (struct sk_buff*)opt;
+		dev_kfree_skb_any(skb);
+	}
+}
+
+static struct net_device_stats*
+danube_get_stats (struct net_device *dev)
+{
+	return (struct net_device_stats *)dev->priv;
+}
+
+static int
+switch_init (struct net_device *dev)
+{
+	u64 retval = 0;
+	int i;
+	struct switch_priv *priv;
+
+	ether_setup(dev);
+
+	printk("%s up\n", dev->name);
+
+	dev->open = danube_switch_open;
+	dev->stop = switch_release;
+	dev->hard_start_xmit = switch_tx;
+	dev->get_stats = danube_get_stats;
+	dev->tx_timeout = switch_tx_timeout;
+	dev->watchdog_timeo = 10 * HZ;
+	dev->priv = kmalloc(sizeof(struct switch_priv), GFP_KERNEL);
+
+	if (dev->priv == NULL)
+		return -ENOMEM;
+
+	memset(dev->priv, 0, sizeof(struct switch_priv));
+	priv = dev->priv;
+
+	priv->dma_device = dma_device_reserve("PPE");
+
+	if (!priv->dma_device){
+		BUG();
+		return -ENODEV;
+	}
+
+	priv->dma_device->buffer_alloc = &danube_etop_dma_buffer_alloc;
+	priv->dma_device->buffer_free = &danube_etop_dma_buffer_free;
+	priv->dma_device->intr_handler = &dma_intr_handler;
+	priv->dma_device->max_rx_chan_num = 4;
+
+	for (i = 0; i < priv->dma_device->max_rx_chan_num; i++)
+	{
+		priv->dma_device->rx_chan[i]->packet_size = ETHERNET_PACKET_DMA_BUFFER_SIZE;
+		priv->dma_device->rx_chan[i]->control = DANUBE_DMA_CH_ON;
+	}
+
+	for (i = 0; i < priv->dma_device->max_tx_chan_num; i++)
+	{
+		if(i == 0)
+			priv->dma_device->tx_chan[i]->control = DANUBE_DMA_CH_ON;
+		else
+			priv->dma_device->tx_chan[i]->control = DANUBE_DMA_CH_OFF;
+	}
+
+	dma_device_register(priv->dma_device);
+
+	/*read the mac address from the mac table and put them into the mac table.*/
+	for (i = 0; i < 6; i++)
+	{
+		retval += u_boot_ethaddr[i];
+	}
+
+	//TODO
+	/* ethaddr not set in u-boot ? */
+	if (retval == 0)
+	{
+		printk("use default MAC address\n");
+		dev->dev_addr[0] = 0x00;
+		dev->dev_addr[1] = 0x11;
+		dev->dev_addr[2] = 0x22;
+		dev->dev_addr[3] = 0x33;
+		dev->dev_addr[4] = 0x44;
+		dev->dev_addr[5] = 0x55;
+	} else {
+		for (i = 0; i < 6; i++)
+			dev->dev_addr[i] = u_boot_ethaddr[i];
+	}
+
+	return 0;
+}
+
+static void
+danube_sw_chip_init (int mode)
+{
+	danube_pmu_enable(DANUBE_PMU_PWDCR_DMA);
+	danube_pmu_enable(DANUBE_PMU_PWDCR_PPE);
+
+	if(mode == REV_MII_MODE)
+		writel((readl(DANUBE_PPE32_CFG) & PPE32_MII_MASK) | PPE32_MII_REVERSE, DANUBE_PPE32_CFG);
+	else if(mode == MII_MODE)
+		writel((readl(DANUBE_PPE32_CFG) & PPE32_MII_MASK) | PPE32_MII_NORMAL, DANUBE_PPE32_CFG);
+
+	writel(PPE32_PLEN_UNDER | PPE32_PLEN_OVER, DANUBE_PPE32_IG_PLEN_CTRL);
+
+	writel(PPE32_CGEN, DANUBE_PPE32_ENET_MAC_CFG);
+
+	wmb();
+}
+
+int __init
+switch_init_module(void)
+{
+	int result = 0;
+
+	danube_mii0_dev.init = switch_init;
+
+	strcpy(danube_mii0_dev.name, "eth%d");
+	SET_MODULE_OWNER(dev);
+
+	result = register_netdev(&danube_mii0_dev);
+	if (result)
+	{
+		printk("error %i registering device \"%s\"\n", result, danube_mii0_dev.name);
+		goto out;
+	}
+
+	/* danube eval kit connects the phy/switch in REV mode */
+	danube_sw_chip_init(REV_MII_MODE);
+	printk("danube MAC driver loaded!\n");
+
+out:
+	return result;
+}
+
+static void __exit
+switch_cleanup(void)
+{
+	struct switch_priv *priv = (struct switch_priv*)danube_mii0_dev.priv;
+
+	printk("danube_mii0 cleanup\n");
+
+	dma_device_unregister(priv->dma_device);
+	dma_device_release(priv->dma_device);
+	kfree(priv->dma_device);
+	kfree(danube_mii0_dev.priv);
+	unregister_netdev(&danube_mii0_dev);
+
+	return;
+}
+
+module_init(switch_init_module);
+module_exit(switch_cleanup);
