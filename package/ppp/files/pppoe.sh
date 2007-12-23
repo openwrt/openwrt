8c871a5f6b2be19e3c67f5c10907fd1ebcf82064
Felix Fietkau <nbd@openwrt.org>
2007-12-23 00:41:38 +0000

    fix pppoe on dynamically assigned interfaces
    SVN-Revision: 9852

--- package/ppp/files/pppoe.sh
+++ package/ppp/files/pppoe.sh
@@ -6,8 +6,6 @@ setup_interface_pppoe() {
 	local iface="$1"
 	local config="$2"
 	
-	config_get device "$config" device
-
 	for module in slhc ppp_generic pppox pppoe; do
 		/sbin/insmod $module 2>&- >&-
 	done
@@ -22,5 +20,5 @@ setup_interface_pppoe() {
 	start_pppd "$config" \
 		plugin rp-pppoe.so \
 		mtu $mtu mru $mtu \
-		"nic-$device"
+		"nic-$iface"
 }
