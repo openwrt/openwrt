40cedc29dfd488a14343b73a99601365bdb22b58
Gabor Juhos <juhosg@openwrt.org>
2008-01-01 14:37:50 +0000

    [adm5120] usb-driver: add preliminary support of ISOCHRONOUS transfer
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@10077 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/adm5120/files/drivers/usb/host/adm5120-hcd.c
+++ target/linux/adm5120/files/drivers/usb/host/adm5120-hcd.c
@@ -1,7 +1,7 @@
 /*
  * ADM5120 HCD (Host Controller Driver) for USB
  *
- * Copyright (C) 2007 Gabor Juhos <juhosg at openwrt.org>
+ * Copyright (C) 2007,2008 Gabor Juhos <juhosg at openwrt.org>
  *
  * This file was derived from: drivers/usb/host/ohci-hcd.c
  *   (C) Copyright 1999 Roman Weissgaerber <weissg@vienna.at>
@@ -12,7 +12,10 @@
  *   [ (C) Copyright 1999 Linus Torvalds  ]
  *   [ (C) Copyright 1999 Gregory P. Smith]
  *
- * This file is licenced under the GPL.
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ *
  */
 
 #include <linux/module.h>
@@ -42,7 +45,7 @@
 #include "../core/hcd.h"
 #include "../core/hub.h"
 
-#define DRIVER_VERSION	"0.14.1"
+#define DRIVER_VERSION	"0.16.0"
 #define DRIVER_AUTHOR	"Gabor Juhos <juhosg at openwrt.org>"
 #define DRIVER_DESC	"ADMtek USB 1.1 Host Controller Driver"
 
