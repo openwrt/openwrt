fb3305367a1a214d7e2e0d35b6d8ce53e1bd0a2d
Gabor Juhos <juhosg@openwrt.org>
2008-01-29 10:33:34 +0000

    image generation cleanups * make image generation makefiles simpler * remove unused profiles * generate images for the Cellvision CAS-xxx/NFS-xxx devices
    SVN-Revision: 10309

--- target/linux/adm5120/router_le/profiles/Cellvision.mk
+++ target/linux/adm5120/router_le/profiles/Cellvision.mk
@@ -5,8 +5,44 @@
 # See /LICENSE for more information.
 #
 
+define Profile/CAS630
+	NAME:=Cellvision CAS-630 IP camera (Experimental)
+	# TODO: add default packages
+endef
+
+define Profile/CAS630/Description
+	Package set optimized for the Cellvision CAS-630 device.
+endef
+
+define Profile/CAS630W
+	NAME:=Cellvision CAS-630W IP camera (Experimental)
+	# TODO: add default packages
+endef
+
+define Profile/CAS630W/Description
+	Package set optimized for the Cellvision CAS-630W device.
+endef
+
+define Profile/CAS670
+	NAME:=Cellvision CAS-670 IP camera (Experimental)
+	# TODO: add default packages
+endef
+
+define Profile/CAS670/Description
+	Package set optimized for the Cellvision CAS-670 device.
+endef
+
+define Profile/CAS670W
+	NAME:=Cellvision CAS-670W IP camera (Experimental)
+	# TODO: add default packages
+endef
+
+define Profile/CAS670W/Description
+	Package set optimized for the Cellvision CAS-670 device.
+endef
+
 define Profile/CAS700
-	NAME:=Cellvision CAS-700 IP camera (Untested)
+	NAME:=Cellvision CAS-700 IP camera (Experimental)
 	# TODO: add default packages
 endef
 
@@ -15,7 +51,7 @@ define Profile/CAS700/Description
 endef
 
 define Profile/CAS700W
-	NAME:=Cellvision CAS-700W IP camera (Untested)
+	NAME:=Cellvision CAS-700W IP camera (Experimental)
 	# TODO: add default packages
 endef
 
@@ -24,7 +60,7 @@ define Profile/CAS700W/Description
 endef
 
 define Profile/CAS771
-	NAME:=Cellvision CAS-771 IP camera (Untested)
+	NAME:=Cellvision CAS-771 IP camera (Experimental)
 	PACKAGES:=kmod-video-cpia2 kmod-usb-ohci kmod-usb2 kmod-usb-audio
 endef
 
@@ -33,7 +69,7 @@ define Profile/CAS771/Description
 endef
 
 define Profile/CAS771W
-	NAME:=Cellvision CAS-771W IP camera (Untested)
+	NAME:=Cellvision CAS-771W IP camera (Experimental)
 	PACKAGES:=kmod-video-cpia2 kmod-usb-ohci kmod-usb2 kmod-usb-audio kmod-rt2500
 endef
 
@@ -42,7 +78,7 @@ define Profile/CAS771W/Description
 endef
 
 define Profile/CAS790
-	NAME:=Cellvision CAS-790 IP camera (Untested)
+	NAME:=Cellvision CAS-790 IP camera (Experimental)
 	# TODO: add default packages
 endef
 
@@ -51,7 +87,7 @@ define Profile/CAS790/Description
 endef
 
 define Profile/CAS861
-	NAME:=Cellvision CAS-861 IP camera (Untested)
+	NAME:=Cellvision CAS-861 IP camera (Experimental)
 	# TODO: add default packages
 endef
 
@@ -60,7 +96,7 @@ define Profile/CAS861/Description
 endef
 
 define Profile/CAS861W
-	NAME:=Cellvision CAS-861W IP camera (Untested)
+	NAME:=Cellvision CAS-861W IP camera (Experimental)
 	PACKAGES:=kmod-rt2500
 endef
 
@@ -68,6 +104,26 @@ define Profile/CAS861W/Description
 	Package set optimized for the Cellvision CAS-861W device.
 endef
 
+define Profile/NFS101U
+	NAME:=Cellvision NFS-101U Network File Server (Experimental)
+endef
+
+define Profile/NFS101U/Description
+	Package set optimized for the Cellvision NFS-101U device.
+endef
+
+define Profile/NFS101WU
+	NAME:=Cellvision NFS-101WU Network File Server (Experimental)
+endef
+
+define Profile/NFS101WU/Description
+	Package set optimized for the Cellvision NFS-101WU device.
+endef
+
+$(eval $(call Profile,CAS630))
+$(eval $(call Profile,CAS630W))
+$(eval $(call Profile,CAS670))
+$(eval $(call Profile,CAS670W))
 $(eval $(call Profile,CAS700))
 $(eval $(call Profile,CAS700W))
 $(eval $(call Profile,CAS771))
@@ -75,3 +131,6 @@ $(eval $(call Profile,CAS771W))
 $(eval $(call Profile,CAS790))
 $(eval $(call Profile,CAS861))
 $(eval $(call Profile,CAS861W))
+$(eval $(call Profile,NFS101U))
+$(eval $(call Profile,NFS101WU))
+
