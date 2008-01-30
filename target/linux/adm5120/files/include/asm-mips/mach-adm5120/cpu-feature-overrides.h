c7e43709f447da2c16340ca51d3e874c1263853b
Gabor Juhos <juhosg@openwrt.org>
2008-01-30 13:15:51 +0000

    more license cleanups
    SVN-Revision: 10327

--- target/linux/adm5120/files/include/asm-mips/mach-adm5120/cpu-feature-overrides.h
+++ target/linux/adm5120/files/include/asm-mips/mach-adm5120/cpu-feature-overrides.h
@@ -10,20 +10,10 @@
  *	Copyright (C) 2003, 2004 Ralf Baechle
  *	Copyright (C) 2004 Maciej W. Rozycki
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the
- *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA  02110-1301, USA.
  */
 #ifndef __ASM_MACH_ADM5120_CPU_FEATURE_OVERRIDES_H
 #define __ASM_MACH_ADM5120_CPU_FEATURE_OVERRIDES_H
