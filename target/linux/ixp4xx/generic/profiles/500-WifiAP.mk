2b79a8b83d7a9a18c50915fc244db2a52f6c1474
John Crispin <blogic@openwrt.org>
2008-01-31 19:46:17 +0000

    update default packages
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@10343 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ixp4xx/generic/profiles/500-WifiAP.mk
+++ target/linux/ixp4xx/generic/profiles/500-WifiAP.mk
@@ -11,7 +11,9 @@ define Profile/WifiAP
 	kmod-scsi-core \
 	kmod-ata-core kmod-ata-artop \
 	kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb-storage kmod-nls-iso8859-1 kmod-nls-cp437 \
-	kmod-fs-ext2 kmod-fs-ext3 kmod-fs-vfat gpioctl openslp hostapd wpa-supplicant
+	kmod-fs-ext2 kmod-fs-ext3 kmod-fs-vfat gpioctl openslp hostapd wpa-supplicant vim \
+	libnetsnmp snmp-utils snmpd olsrd olsrd-mod-nameservice olsrd-mod-dyn-gw olsrd-mod-httpinfo \
+	iperf
 endef
 
 define Profile/WifiAP/Description
