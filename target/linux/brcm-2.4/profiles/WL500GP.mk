ceb7720c8dd3f47f7682a16be606745866a7cdbe
Andy Boyett <agb@openwrt.org>
2008-03-31 08:05:13 +0000

    Change WL500gP profile to use uhci-iv (#1589)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@10694 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm-2.4/profiles/WL500GP.mk
+++ target/linux/brcm-2.4/profiles/WL500GP.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006-2008 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -7,7 +7,7 @@
 
 define Profile/WL500GP
   NAME:=ASUS WL-500g Premium
-  PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-uhci kmod-usb2 wlc nas kmod-wlcompat
+  PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-uhci-iv kmod-usb2 wlc nas kmod-wlcompat
 endef
 
 define Profile/WL500GP/Description
