c2ebe229348a85b3cbc36ee5a6f6be89f2f918fe
Matteo Croce <matteo@openwrt.org>
2008-04-02 11:18:00 +0000

    Let authors holds copyright of the AR7 code (closes #2369)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@10708 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar7/files/drivers/char/ar7_gpio.c
+++ target/linux/ar7/files/drivers/char/ar7_gpio.c
@@ -1,7 +1,5 @@
 /*
- * linux/drivers/char/ar7_gpio.c
- *
- * Copyright (C) 2007 OpenWrt.org
+ * Copyright (C) 2007 Nicolas Thill <nico@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- target/linux/ar7/files/include/asm-mips/ar7/prom.h
+++ target/linux/ar7/files/include/asm-mips/ar7/prom.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007 OpenWrt.org
+ * Copyright (C) 2006, 2007 Florian Fainelli <florian@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
