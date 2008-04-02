af4c2c041f2226619e105e97bad2c8d1e1c7d892
Matteo Croce <rootkit85@yahoo.it>
2008-04-02 11:45:18 +0000

    add proper email addresses to the comment headers
    SVN-Revision: 10709

--- target/linux/ar7/files/arch/mips/ar7/irq.c
+++ target/linux/ar7/files/arch/mips/ar7/irq.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2006, 2007 Felix Fietkau, Eugene Konev
+ * Copyright (C) 2006,2007 Felix Fietkau <nbd@openwrt.org>
+ * Copyright (C) 2006,2007 Eugene Konev <ejka@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- target/linux/ar7/files/include/linux/vlynq.h
+++ target/linux/ar7/files/include/linux/vlynq.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007 Eugene Konev
+ * Copyright (C) 2006, 2007 Eugene Konev <ejka@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
