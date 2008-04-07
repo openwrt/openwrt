2b9c372346eda49110fb236ba7ef6a36dcc2f52b
Felix Fietkau <nbd@openwrt.org>
2008-04-07 01:07:50 +0000

    fix the route to the pptp server when using pptp as wan interface
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@10751 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/pptp/files/pptp.sh
+++ package/pptp/files/pptp.sh
@@ -2,12 +2,18 @@ scan_pptp() {
 	scan_ppp "$@"
 }
 
+find_gw() {
+	route -n | awk '$1 == "0.0.0.0" { print $2; exit }'
+}
+
+
 setup_interface_pptp() {
 	local config="$2"
 	local ifname
 	
 	config_get device "$config" device
 	config_get ipproto "$config" ipproto
+	config_get server "$cfg" server
 
 	for module in slhc ppp_generic ppp_async ip_gre; do
 		/sbin/insmod $module 2>&- >&-
@@ -15,6 +21,11 @@ setup_interface_pptp() {
 	sleep 1
 
 	setup_interface "$device" "$config" "${ipproto:-dhcp}"
+	local gw="$(find_gw)"
+	[ -n "$gw" ] && {
+		route delete "$server" 2>/dev/null >/dev/null
+		route add "$server" gw "$gw"
+	}
 
 	# fix up the netmask
 	config_get netmask "$config" netmask
@@ -26,7 +37,6 @@ setup_interface_pptp() {
 	uci_set_state network "$config" ifname "$ifname"
 
 	config_get mtu "$cfg" mtu
-	config_get server "$cfg" server
 	mtu=${mtu:-1452}
 	start_pppd "$config" \
 		pty "/usr/sbin/pptp $server --loglevel 0 --nolaunchpppd" \
