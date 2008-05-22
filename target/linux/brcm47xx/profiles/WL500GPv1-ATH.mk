8a8e4997baf38953328075724b7faa3460acd103
Florian Fainelli <florian@openwrt.org>
2008-05-22 20:29:47 +0000

    Add a WL500GPv1 profile (#3395)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@11243 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm47xx/profiles/WL500GPv1-ATH.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2008 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/WL500GPv1
+  NAME:=ASUS WL-500g Premium v1 (Atheros WiFi)
+  PACKAGES:=kmod-madwifi kmod-usb-core kmod-usb-uhci kmod-usb2
+endef
+
+define Profile/WL500GPv1/Description
+	Package set optimized for the WL-500g Premium v1 with USB and Atheros WiFi card support
+endef
+$(eval $(call Profile,WL500GPv1))
