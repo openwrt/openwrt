3cc6f9bd75df92bf8c559e10e35be2670f3bd263
Gabor Juhos <juhosg@openwrt.org>
2008-06-22 19:08:19 +0000

    use __raw_{readl,writel} routines in the GPIO code
    SVN-Revision: 11560

--- target/linux/adm5120/files/arch/mips/adm5120/gpio.c
+++ target/linux/adm5120/files/arch/mips/adm5120/gpio.c
@@ -3,8 +3,8 @@
  *
  *  ADM5120 GPIO support
  *
- *  Copyright (C) 2007 OpenWrt.org
- *  Copyright (C) 2007 Gabor Juhos <juhosg at openwrt.org>
+ *  Copyright (C) 2007-2008 OpenWrt.org
+ *  Copyright (C) 2007-2008 Gabor Juhos <juhosg at openwrt.org>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
@@ -28,8 +28,8 @@
 #include <adm5120_switch.h>
 #include <adm5120_irq.h>
 
-#define GPIO_READ(r)		readl((r))
-#define GPIO_WRITE(v, r)	writel((v), (r))
+#define GPIO_READ(r)		__raw_readl((r))
+#define GPIO_WRITE(v, r)	__raw_writel((v), (r))
 #define GPIO_REG(r)	(void __iomem *)(KSEG1ADDR(ADM5120_SWITCH_BASE)+r)
 
 struct adm5120_gpio_line {
