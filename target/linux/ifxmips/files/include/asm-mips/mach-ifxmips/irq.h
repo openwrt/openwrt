b7a2f20f31f71ab281e01f99841f070e72d5a763
John Crispin <blogic@openwrt.org>
2008-06-24 19:24:22 +0000

    cleanup ifxmips headers
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@11573 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ifxmips/files/include/asm-mips/mach-ifxmips/irq.h
+++ target/linux/ifxmips/files/include/asm-mips/mach-ifxmips/irq.h
@@ -1,6 +1,4 @@
 /*
- *   include/asm-mips/mach-ifxmips/irq.h 
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
@@ -16,7 +14,6 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
  *   Copyright (C) 2007 John Crispin <blogic@openwrt.org> 
- *
  */
 
 #ifndef __IFXMIPS_IRQ_H
