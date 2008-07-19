2dfea2a7ed8eed59de22a93fc77ad2fece5543d2
John Crispin <john@openwrt.org>
2008-07-19 13:12:47 +0000

    adds ifxmips profiles
    SVN-Revision: 11871

---
+++ target/linux/ifxmips/profiles/200-Ralink.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2008 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Ralink
+  NAME:=Ralink RT61 Wifi (ARV452)
+  PACKAGES:=kmod-rt61-pci
+endef
+
+define Profile/Ralink/Description
+	Package set compatible with hardware using Ralink WiFi cards
+endef
+$(eval $(call Profile,Ralink))
+
