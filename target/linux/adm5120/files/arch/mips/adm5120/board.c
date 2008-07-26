3185e8d071bf2b3a83b8c9312c88baf272c9aa73
Gabor Juhos <juhosg@openwrt.org>
2008-07-26 18:05:38 +0000

    [adm5120] make boot quiet a bit
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@11939 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/adm5120/files/arch/mips/adm5120/board.c
+++ target/linux/adm5120/files/arch/mips/adm5120/board.c
@@ -3,8 +3,8 @@
  *
  *  ADM5120 generic board code
  *
- *  Copyright (C) 2007 OpenWrt.org
- *  Copyright (C) 2007 Gabor Juhos <juhosg at openwrt.org>
+ *  Copyright (C) 2007,2008 OpenWrt.org
+ *  Copyright (C) 2007,2008 Gabor Juhos <juhosg at openwrt.org>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
@@ -28,7 +28,8 @@
 
 #define PFX	"adm5120: "
 
-static LIST_HEAD(adm5120_boards);
+static struct list_head adm5120_boards __initdata =
+			LIST_HEAD_INIT(adm5120_boards);
 static char adm5120_board_name[ADM5120_BOARD_NAMELEN];
 
 const char *get_system_type(void)
@@ -61,14 +62,14 @@ static int __init adm5120_board_setup(void)
 
 	board = adm5120_board_find(mips_machtype);
 	if (board == NULL) {
-		printk(KERN_ALERT PFX"no board registered for "
+		printk(KERN_ALERT PFX "no board registered for "
 			"machtype %lu, trying generic\n", mips_machtype);
 		board = adm5120_board_find(MACH_ADM5120_GENERIC);
 		if (board == NULL)
 			panic(PFX "unsupported board\n");
 	}
 
-	printk(KERN_INFO PFX "setting up board '%s'\n", board->name);
+	printk(KERN_INFO PFX "board is '%s'\n", board->name);
 
 	memcpy(&adm5120_board_name, board->name, ADM5120_BOARD_NAMELEN);
 
@@ -109,5 +110,4 @@ arch_initcall(adm5120_board_setup);
 void __init adm5120_board_register(struct adm5120_board *board)
 {
 	list_add_tail(&board->list, &adm5120_boards);
-	printk(KERN_INFO PFX "registered board '%s'\n", board->name);
 }
