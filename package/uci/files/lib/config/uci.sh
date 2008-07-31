434f4269ea51f04428380fd0099fbefe76955430
Felix Fietkau <nbd@openwrt.org>
2008-07-31 21:11:15 +0000

    initialize the CONFIG_APPEND variable
    SVN-Revision: 12050

--- package/uci/files/lib/config/uci.sh
+++ package/uci/files/lib/config/uci.sh
@@ -17,6 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+CONFIG_APPEND=
 uci_load() {
 	local PACKAGE="$1"
 	local DATA
