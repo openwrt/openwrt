ab8c8b019fbde4d6d4e2ccd3db414f701903739c
Felix Fietkau <nbd@openwrt.org>
2008-08-27 14:14:00 +0000

    enable quilt by default for the kernel tree
    SVN-Revision: 12400

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -37,7 +37,7 @@ ifeq ($(strip $(CONFIG_EXTERNAL_KERNEL_TREE)),"")
 define Kernel/Prepare/Default
 	bzcat $(DL_DIR)/$(LINUX_SOURCE) | $(TAR) -C $(KERNEL_BUILD_DIR) $(TAR_OPTIONS)
 	$(Kernel/Patch)
-	$(if $(QUILT),touch $(LINUX_DIR)/.quilt_used)
+	touch $(LINUX_DIR)/.quilt_used
 endef
 else
 define Kernel/Prepare/Default
