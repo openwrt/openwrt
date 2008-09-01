54588a7ade4279007abede431fb39365b456012f
Nicolas Thill <nico@openwrt.org>
2008-09-01 14:18:32 +0000

    Change even more dependencies on kmod-video-core after [12460]
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@12462 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/video.mk
+++ package/kernel/modules/video.mk
@@ -48,7 +48,7 @@ $(eval $(call KernelPackage,video-core))
 define KernelPackage/video-cpia2
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=CPIA2 video driver
-  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core kmod-video-core
+  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core +kmod-video-core
   KCONFIG:=CONFIG_VIDEO_CPIA2
   FILES:=$(LINUX_DIR)/drivers/media/video/cpia2/cpia2.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,70,cpia2)
@@ -64,7 +64,7 @@ $(eval $(call KernelPackage,video-cpia2))
 define KernelPackage/video-konica
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Konica USB webcam support
-  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-video kmod-video-core
+  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-video +kmod-video-core
   KCONFIG:=CONFIG_USB_KONICAWC
   FILES:=$(LINUX_DIR)/drivers/media/video/usbvideo/konicawc.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,70,konicawc)
@@ -81,7 +81,7 @@ $(eval $(call KernelPackage,video-konica))
 define KernelPackage/video-ov511
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=OV511 USB webcam support
-  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core kmod-video-core
+  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core +kmod-video-core
   KCONFIG:=CONFIG_VIDEO_OV511
   FILES:=$(LINUX_DIR)/drivers/media/video/ov511.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,70,ov511)
@@ -98,7 +98,7 @@ $(eval $(call KernelPackage,video-ov511))
 define KernelPackage/video-ovcamchip
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=OV6xxx/OV7xxx Camera Chip support
-  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-i2c-core kmod-video-core
+  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-i2c-core +kmod-video-core
   KCONFIG:=CONFIG_VIDEO_OVCAMCHIP
   FILES:=$(LINUX_DIR)/drivers/media/video/ovcamchip/ovcamchip.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,70,ovcamchip)
@@ -116,7 +116,7 @@ $(eval $(call KernelPackage,video-ovcamchip))
 define KernelPackage/video-pwc
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Philips USB webcam support
-  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core kmod-video-core
+  DEPENDS:=@LINUX_2_6 @USB_SUPPORT +kmod-usb-core +kmod-video-core
   KCONFIG:= \
 	CONFIG_USB_PWC \
 	CONFIG_USB_PWC_DEBUG=n
