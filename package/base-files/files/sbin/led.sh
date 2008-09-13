3476a63cd2b1d7ae40a29aafc72a5bdc41397cf7
Travis Kemen <thepeople@openwrt.org>
2008-09-13 00:08:39 +0000

    make option name optional
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@12585 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/sbin/led.sh
+++ package/base-files/files/sbin/led.sh
@@ -9,7 +9,7 @@ do_led() {
 	local sysfs
 	config_get name $1 name
 	config_get sysfs $1 sysfs
-	[ "$name" == "$NAME" -a -e "/sys/class/leds/${sysfs}" ] && {
+	[ "$name" == "$NAME" -o "$sysfs" = "$NAME" -a -e "/sys/class/leds/${sysfs}" ] && {
 		[ "$ACTION" == "set" ] &&
 			echo 1 >/sys/class/leds/${sysfs}/brightness \
 			|| echo 0 >/sys/class/leds/${sysfs}/brightness
