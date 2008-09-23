3f105986a888f65ce2b18e4e4b96544fe2be6a1e
Felix Fietkau <nbd@openwrt.org>
2008-09-23 11:05:54 +0000

    fix sysupgrade support for brcm-2.4 and brcm47xx make mtd refresh based config append optional (leave enabled on x86)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@12657 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm-2.4/base-files/lib/upgrade/platform.sh
+++ target/linux/brcm-2.4/base-files/lib/upgrade/platform.sh
@@ -1,24 +1,16 @@
+PART_NAME=linux
+
 platform_check_image() {
 	[ "$ARGC" -gt 1 ] && return 1
 
 	case "$(get_magic_word "$1")" in
 		# .trx files
 		4844) return 0;;
-		# .bin files
-		5735) return 0;;
 		*)
-			echo "Invalid image type"
+			echo "Invalid image type. Please use only .trx files"
 			return 1
 		;;
 	esac
 }
 
-platform_do_upgrade() {
-	get_image "$1" > $(find_mtd_part "linux")
-	sync
-}
-
-brcm_prepare_mtd() {
-	[ "$SAVE_CONFIG" -eq 1 ] && return 0
-}
-append sysupgrade_pre_upgrade brcm_prepare_mtd
+# use default for platform_do_upgrade()
