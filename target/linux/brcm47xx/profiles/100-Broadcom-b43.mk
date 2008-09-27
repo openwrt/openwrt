74a598a4263606240ab548af065df26fc16a08e6
Andy Boyett <agb@openwrt.org>
2008-09-27 20:15:51 +0000

    make b43 the default profile
    SVN-Revision: 12743

--- target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
+++ target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2007 OpenWrt.org
+# Copyright (C) 2007-2008 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Broadcom-b43
-  NAME:=Broadcom BCM43xx WiFi
+  NAME:=Broadcom BCM43xx WiFi (default)
   PACKAGES:=kmod-b43
 endef
 
--- target/linux/brcm47xx/profiles/105-Atheros.mk
+++ target/linux/brcm47xx/profiles/105-Atheros.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006-2008 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Atheros
-  NAME:=Atheros WiFi (default)
+  NAME:=Atheros WiFi
   PACKAGES:=kmod-madwifi
 endef
 
