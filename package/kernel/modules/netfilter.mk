215ed4cae6c145cb4afb7a87bd6b19b5bf217494
Gabor Juhos <juhosg@openwrt.org>
2008-10-15 10:56:13 +0000

    [package] netfilter/iptables: disable IMQ on 2.6.27
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@12984 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/netfilter.mk
+++ package/kernel/modules/netfilter.mk
@@ -237,7 +237,7 @@ define KernelPackage/ipt-imq
 	imq \
 	$(IPT_IMQ-m) \
   ))
-  DEPENDS:= kmod-ipt-core
+  DEPENDS:= kmod-ipt-core @!LINUX_2_6_27
 endef
 
 define KernelPackage/ipt-imq/description
@@ -354,10 +354,11 @@ endef
 $(eval $(call KernelPackage,ipt-extra))
 
 
+# XXX: disabled on 2.6.27 due to broken IMQ
 define KernelPackage/ip6tables
   SUBMENU:=$(NF_MENU)
   TITLE:=IPv6 modules
-  DEPENDS:=+kmod-ipv6
+  DEPENDS:=+kmod-ipv6 @!LINUX_2_6_27
   KCONFIG:=CONFIG_IP6_NF_IPTABLES
   FILES:=$(foreach mod,$(IPT_IPV6-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
   AUTOLOAD:=$(call AutoLoad,49,$(notdir $(IPT_IPV6-m)))
