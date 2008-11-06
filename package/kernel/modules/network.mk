openwrt/openwrt

commit 58c80864478f69f941bd85f9355b80a33054d0d5

split out crc-ccitt
git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13115 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/kernel/modules/network.mk
@@ -319,28 +319,16 @@ $(eval $(call KernelPackage,tun))
 define KernelPackage/ppp
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=PPP modules
+  DEPENDS:=+LINUX_2_6:kmod-crc-ccitt
   KCONFIG:= \
 	CONFIG_PPP \
 	CONFIG_PPP_ASYNC \
 	CONFIG_SLHC \
-	CONFIG_CRC_CCITT
   FILES:= \
 	$(LINUX_DIR)/drivers/net/ppp_async.$(LINUX_KMOD_SUFFIX) \
 	$(LINUX_DIR)/drivers/net/ppp_generic.$(LINUX_KMOD_SUFFIX) \
   	$(LINUX_DIR)/drivers/net/slhc.$(LINUX_KMOD_SUFFIX)
-endef
-
-define KernelPackage/ppp/2.4
   AUTOLOAD:=$(call AutoLoad,30,slhc ppp_generic ppp_async)
-endef
-
-define KernelPackage/ppp/2.6
-#  KCONFIG+= \
-#	CONFIG_SLHC \
-#	CONFIG_CRC_CCITT
-  FILES+= \
-	$(LINUX_DIR)/lib/crc-ccitt.$(LINUX_KMOD_SUFFIX)
-  AUTOLOAD:=$(call AutoLoad,30,crc-ccitt slhc ppp_generic ppp_async)
 endef
 
 define KernelPackage/ppp/description

package/kernel/modules/other.mk
@@ -22,6 +22,20 @@ endef
 
 $(eval $(call KernelPackage,crc-itu-t))
 
+define KernelPackage/crc-ccitt
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=CRC-CCITT support
+  KCONFIG:=CONFIG_CRC_CCITT
+  FILES:=$(LINUX_DIR)/lib/crc-ccitt.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,20,crc-ccitt)
+endef
+
+define KernelPackage/crc-ccitt/description
+ Kernel module for CRC-CCITT support
+endef
+
+$(eval $(call KernelPackage,crc-ccitt))
+
 
 define KernelPackage/crc7
   SUBMENU:=$(OTHER_MENU)

commit 03daa9a28d311595f3d6c95768d2e953f31c4601

fix trailing backslash error introduced in r13115 - fixes kernel config issues
SVN-Revision: 13131

package/kernel/modules/network.mk
@@ -323,7 +323,7 @@ define KernelPackage/ppp
   KCONFIG:= \
 	CONFIG_PPP \
 	CONFIG_PPP_ASYNC \
-	CONFIG_SLHC \
+	CONFIG_SLHC
   FILES:= \
 	$(LINUX_DIR)/drivers/net/ppp_async.$(LINUX_KMOD_SUFFIX) \
 	$(LINUX_DIR)/drivers/net/ppp_generic.$(LINUX_KMOD_SUFFIX) \