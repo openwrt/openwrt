834c2de24a9eac8363f51f3a46390ebb1406eeb1
Gabor Juhos <juhosg@openwrt.org>
2008-11-25 19:19:55 +0000

    [ar71xx] add dummy code for the Atheros AP83 board
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13354 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-ap83.c
@@ -0,0 +1,24 @@
+/*
+ *  Atheros AP83 board support
+ *
+ *  Copyright (C) 2008 Gabor Juhos <juhosg@openwrt.org>
+ *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#include <linux/platform_device.h>
+#include <linux/input.h>
+
+#include <asm/mips_machine.h>
+#include <asm/mach-ar71xx/ar71xx.h>
+#include <asm/mach-ar71xx/pci.h>
+#include <asm/mach-ar71xx/platform.h>
+
+static void __init ap83_setup(void)
+{
+}
+
+MIPS_MACHINE(MACH_AR71XX_AP83, "Atheros AP83", ap83_setup);
