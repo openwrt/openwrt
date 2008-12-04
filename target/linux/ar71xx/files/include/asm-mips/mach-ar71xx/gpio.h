6e121e498e4d7d6304c2feaf1faa90e34e847728
Gabor Juhos <juhosg@openwrt.org>
2008-12-04 12:17:14 +0000

    [ar71xx] make all AR913x GPIO lines usable
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13509 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/include/asm-mips/mach-ar71xx/gpio.h
+++ target/linux/ar71xx/files/include/asm-mips/mach-ar71xx/gpio.h
@@ -18,6 +18,7 @@
 
 #include <asm/mach-ar71xx/ar71xx.h>
 
+extern unsigned long ar71xx_gpio_count;
 extern void __ar71xx_gpio_set_value(unsigned gpio, int value);
 extern int __ar71xx_gpio_get_value(unsigned gpio);
 
@@ -33,7 +34,7 @@ static inline int irq_to_gpio(unsigned irq)
 
 static inline int gpio_get_value(unsigned gpio)
 {
-	if (gpio < AR71XX_GPIO_COUNT)
+	if (gpio < ar71xx_gpio_count)
 		return __ar71xx_gpio_get_value(gpio);
 
 	return __gpio_get_value(gpio);
@@ -41,7 +42,7 @@ static inline int gpio_get_value(unsigned gpio)
 
 static inline void gpio_set_value(unsigned gpio, int value)
 {
-	if (gpio < AR71XX_GPIO_COUNT)
+	if (gpio < ar71xx_gpio_count)
 		__ar71xx_gpio_set_value(gpio, value);
 	else
 		__gpio_set_value(gpio, value);
