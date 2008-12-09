d31fe48ea00ef3679b705f84bf86f0642a4bc01e
Gabor Juhos <juhosg@openwrt.org>
2008-12-09 18:49:01 +0000

    register eth0 device on the TEW-632BRP board
    SVN-Revision: 13566

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-tew-632brp.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-tew-632brp.c
@@ -10,10 +10,20 @@
  */
 
 #include <asm/mips_machine.h>
+
 #include <asm/mach-ar71xx/ar71xx.h>
+#include <asm/mach-ar71xx/platform.h>
 
 static void __init tew_632brp_setup(void)
 {
+	ar71xx_add_device_mdio(0xfffffffe);
+
+	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
+	ar71xx_eth0_data.phy_mask = 0x0;
+	ar71xx_eth0_data.speed = SPEED_100;
+	ar71xx_eth0_data.duplex = DUPLEX_FULL;
+
+	ar71xx_add_device_eth(0);
 }
 
 MIPS_MACHINE(AR71XX_MACH_TEW_632BRP, "TRENDnet TEW-632BRP", tew_632brp_setup);
