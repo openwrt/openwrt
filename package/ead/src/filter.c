073a2a82844a0ff3fbe844c9c19be258bfb4d7fc
Felix Fietkau <nbd@openwrt.org>
2008-12-24 10:42:12 +0000

    add the 'ead' package (emergency access daemon), which can provide remote access to your device, even if the ip and firewall settings are broken
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13738 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ package/ead/src/filter.c
@@ -0,0 +1,23 @@
+/* precompiled expression: udp and dst host 255.255.255.255 and dst port 56026 */
+
+static struct bpf_insn pktfilter_insns[] = {
+	{ .code = 0x0028, .jt = 0x00, .jf = 0x00, .k = 0x0000000c },
+	{ .code = 0x0015, .jt = 0x0b, .jf = 0x00, .k = 0x000086dd },
+	{ .code = 0x0015, .jt = 0x00, .jf = 0x0a, .k = 0x00000800 },
+	{ .code = 0x0030, .jt = 0x00, .jf = 0x00, .k = 0x00000017 },
+	{ .code = 0x0015, .jt = 0x00, .jf = 0x08, .k = 0x00000011 },
+	{ .code = 0x0020, .jt = 0x00, .jf = 0x00, .k = 0x0000001e },
+	{ .code = 0x0015, .jt = 0x00, .jf = 0x06, .k = 0xffffffff },
+	{ .code = 0x0028, .jt = 0x00, .jf = 0x00, .k = 0x00000014 },
+	{ .code = 0x0045, .jt = 0x04, .jf = 0x00, .k = 0x00001fff },
+	{ .code = 0x00b1, .jt = 0x00, .jf = 0x00, .k = 0x0000000e },
+	{ .code = 0x0048, .jt = 0x00, .jf = 0x00, .k = 0x00000010 },
+	{ .code = 0x0015, .jt = 0x00, .jf = 0x01, .k = 0x0000dada },
+	{ .code = 0x0006, .jt = 0x00, .jf = 0x00, .k = 0x000005dc },
+	{ .code = 0x0006, .jt = 0x00, .jf = 0x00, .k = 0x00000000 },
+};
+
+static struct bpf_program pktfilter = {
+	.bf_len = 14,
+	.bf_insns = pktfilter_insns,
+};
