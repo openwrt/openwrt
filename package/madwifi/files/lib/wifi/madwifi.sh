18eae705065b3b80dab43d420d8a6d450a1193f2
Jo-Philipp Wich <jow@openwrt.org>
2009-01-01 17:29:58 +0000

    package/madwifi: add uci support for the propereq iwpriv setting
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13795 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/madwifi/files/lib/wifi/madwifi.sh
+++ package/madwifi/files/lib/wifi/madwifi.sh
@@ -250,6 +250,9 @@ enable_atheros() {
 		config_get_bool doth "$vif" doth 0
 		[ -n "$doth" ] && iwpriv "$ifname" doth "$doth"
 
+		config_get_bool probereq "$vif" probereq
+		[ -n "$probereq" ] && iwpriv "$ifname" probereq "$probereq"
+
 		config_get maclist "$vif" maclist
 		[ -n "$maclist" ] && {
 			# flush MAC list
