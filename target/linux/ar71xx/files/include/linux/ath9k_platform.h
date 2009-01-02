3fc9a88fa180ec992cb288deff820d1007bbc4e0
Gabor Juhos <juhosg@openwrt.org>
2009-01-02 18:54:33 +0000

    [ar71xx] prepare for ath9k support
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@13825 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ar71xx/files/include/linux/ath9k_platform.h
@@ -0,0 +1,20 @@
+/*
+ * ath9k platform data defines
+ *
+ * Copyright (C) 2008 Gabor Juhos <juhosg@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ */
+
+#ifndef _LINUX_ATH9K_PLATFORM_H
+#define _LINUX_ATH9L_PLATFORM_H
+
+#define ATH9K_PLAT_EEP_MAX_WORDS	2048
+
+struct ath9k_platform_data {
+	u16 eeprom_data[ATH9K_PLAT_EEP_MAX_WORDS];
+};
+
+#endif /* _LINUX_ATH9K_PLATFORM_H */
