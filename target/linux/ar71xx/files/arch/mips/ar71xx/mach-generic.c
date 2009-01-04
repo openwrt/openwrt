3fa9ee3bfaf1e9047c44ef3438bf69be403df0cc
Gabor Juhos <juhosg@openwrt.org>
2009-01-04 19:07:13 +0000

    remove board specific stuff from the generic machine setup
    SVN-Revision: 13861

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-generic.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-generic.c
@@ -1,5 +1,5 @@
 /*
- *  Generic AR71xx machine setup
+ *  Generic AR71xx machine support
  *
  *  Copyright (C) 2008 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
@@ -10,60 +10,13 @@
  */
 
 #include <linux/init.h>
-#include <linux/platform_device.h>
-#include <linux/spi/spi.h>
-#include <linux/spi/flash.h>
 
 #include <asm/mips_machine.h>
 #include <asm/mach-ar71xx/ar71xx.h>
-#include <asm/mach-ar71xx/pci.h>
-#include <asm/mach-ar71xx/platform.h>
-
-static struct spi_board_info ar71xx_generic_spi_info[] = {
-	{
-		.bus_num	= 0,
-		.chip_select	= 0,
-		.max_speed_hz	= 25000000,
-		.modalias	= "m25p80",
-	}
-};
-
-static struct ar71xx_pci_irq ar71xx_generic_pci_irqs[] __initdata = {
-	{
-		.slot	= 0,
-		.pin	= 1,
-		.irq	= AR71XX_PCI_IRQ_DEV0,
-	}, {
-		.slot	= 1,
-		.pin	= 1,
-		.irq	= AR71XX_PCI_IRQ_DEV1,
-	}, {
-		.slot	= 2,
-		.pin	= 1,
-		.irq	= AR71XX_PCI_IRQ_DEV2,
-	}
-};
 
 static void __init ar71xx_generic_init(void)
 {
-	ar71xx_add_device_spi(NULL, ar71xx_generic_spi_info,
-				ARRAY_SIZE(ar71xx_generic_spi_info));
-
-	ar71xx_add_device_mdio(0xffe0ffff);
-
-	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
-	ar71xx_eth0_data.phy_mask = 0x000f0000;
-
-	ar71xx_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
-	ar71xx_eth1_data.phy_mask = 0x00100000;
-
-	ar71xx_add_device_eth(0);
-	ar71xx_add_device_eth(1);
-
-	ar71xx_add_device_usb();
-
-	ar71xx_pci_init(ARRAY_SIZE(ar71xx_generic_pci_irqs),
-			ar71xx_generic_pci_irqs);
+	/* Nothing to do */
 }
 
 MIPS_MACHINE(AR71XX_MACH_GENERIC, "Generic AR71xx board", ar71xx_generic_init);
