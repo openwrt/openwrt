32dce30e261bd6b9487efbc2e38bf0b82fc77828
Andy Boyett <agb@openwrt.org>
2009-01-31 16:08:19 +0000

    broadcom-wl: move txpower back to wifi-device section, but remain backwards compatible
    SVN-Revision: 14306

--- package/broadcom-wl/files/lib/wifi/broadcom.sh
+++ package/broadcom-wl/files/lib/wifi/broadcom.sh
@@ -123,7 +123,8 @@ enable_broadcom() {
 	config_get macfilter "$device" macfilter
 	config_get maclist "$device" maclist
 	config_get macaddr "$device" macaddr
-	local vif_pre_up vif_post_up vif_do_up txpower
+	config_get txpower "$device" txpower
+	local vif_pre_up vif_post_up vif_do_up vif_txpower
 
 	_c=0
 	nas="$(which nas)"
@@ -152,7 +153,7 @@ enable_broadcom() {
 	esac
 
 	for vif in $vifs; do
-		config_get txpower "$vif" txpower
+		config_get vif_txpower "$vif" txpower
 
 		config_get mode "$vif" mode
 		append vif_pre_up "vif $_c" "$N"
@@ -305,7 +306,11 @@ EOF
 	wlc stdin <<EOF
 $vif_do_up
 EOF
-	[ -z "$txpower" ] || iwconfig $device txpower ${txpower}dBm 
+
+	# use vif_txpower (from last wifi-iface) instead of txpower (from
+	# wifi-device) if the latter does not exist
+	txpower=${txpower:-$vif_txpower}
+	[ -z "$txpower" ] || iwconfig $device txpower ${txpower}dBm
 
 	eval "$nas_cmd"
 }
