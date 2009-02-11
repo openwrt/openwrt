df77d3b06d061404cf3210c4da185b65f77e42e6
Michael Büsch <mb@bu3sch.de>
2009-02-11 18:56:51 +0000

    ucmb: Experimental SPI-GPIO based communication channel
    SVN-Revision: 14478

---
+++ package/ucmb/src/ucmb.h
@@ -0,0 +1,13 @@
+#ifndef LINUX_UCMB_H_
+#define LINUX_UCMB_H_
+
+struct ucmb_platform_data {
+	const char *name;
+	unsigned int gpio_sck;
+	unsigned int gpio_miso;
+	unsigned int gpio_mosi;
+
+	struct platform_device *pdev; /* internal */
+};
+
+#endif /* LINUX_UCMB_H_ */
