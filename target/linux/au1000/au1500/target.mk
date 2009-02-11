908c078bd43b7300fd1dbedba8c7703d0d6ab9ea
Florian Fainelli <florian@openwrt.org>
2009-02-11 15:47:01 +0000

    [au1000] prepare to support more au1x00 targets
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@14473 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/au1000/au1500/target.mk
@@ -0,0 +1,7 @@
+BOARDNAME:=Au1500
+
+DEFAULT_PACKAGES+= hostapd-mini
+
+define Target/Description
+        Build firmware images for Au1500 based boards
+endef
---
+++ target/linux/au1000/au1550/target.mk
@@ -0,0 +1,7 @@
+BOARDNAME:=Au1550
+
+DEFAULT_PACKAGES+= hostapd-mini
+
+define Target/Description
+        Build firmware images for Au1550 based boards
+endef
