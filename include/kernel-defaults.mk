55349584aaaa1c5156be7eecdfd4a155052b22da
Felix Fietkau <nbd@openwrt.org>
2009-02-14 14:10:44 +0000

    kernel: add missing definition for CONFIG_KALLSYMS_ALL
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@14507 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -89,6 +89,7 @@ define Kernel/Configure/Default
 	echo "$(if $(CONFIG_KERNEL_KALLSYMS),CONFIG_KALLSYMS=y,# CONFIG_KALLSYMS is not set)" >> $(LINUX_DIR)/.config.target
 	echo "$(if $(CONFIG_KERNEL_PROFILING),CONFIG_PROFILING=y,# CONFIG_PROFILING is not set)" >> $(LINUX_DIR)/.config.target
 	echo "# CONFIG_KALLSYMS_EXTRA_PASS is not set" >> $(LINUX_DIR)/.config.target
+	echo "# CONFIG_KALLSYMS_ALL is not set" >> $(LINUX_DIR)/.config.target
 	echo "# CONFIG_KPROBES is not set" >> $(LINUX_DIR)/.config.target
 	$(SCRIPT_DIR)/metadata.pl kconfig $(TMP_DIR)/.packageinfo $(TOPDIR)/.config > $(LINUX_DIR)/.config.override
 	$(SCRIPT_DIR)/kconfig.pl 'm+' $(LINUX_DIR)/.config.target $(LINUX_DIR)/.config.override > $(LINUX_DIR)/.config
