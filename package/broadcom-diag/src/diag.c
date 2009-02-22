f5fba5bc3ccf493289396deaa64a7ce7385d8590
Felix Fietkau <nbd@openwrt.org>
2009-02-22 18:45:39 +0000

    Asus WL-330gE Support
    Here is support for the compact Asus WL-330gE. It uses that all-in-one
    5354 Broadcom chip that's also in the WL-520gU, so I assume USB
    support can be soldered on. I've yet to open up the device, though.
    Initial flashing is done through the CFE failsafe mode like on other
    Asus devices, where you boot holding the reset button, the power LED
    flashes, and you tftp an image.
    
    Signed-off-by: David Cooper <dave@kupesoft.com>
    
    SVN-Revision: 14624

--- package/broadcom-diag/src/diag.c
+++ package/broadcom-diag/src/diag.c
@@ -68,6 +68,7 @@ enum {
 	WLHDD,
 	WL300G,
 	WL320GE,
+	WL330GE,
 	WL500G,
 	WL500GD,
 	WL500GP,
@@ -305,6 +306,15 @@ static struct platform_t __initdata platforms[] = {
 			{ .name = "wlan",	.gpio = 1 << 0, .polarity = REVERSE },
 			{ .name = "power",	.gpio = 1 << 2, .polarity = REVERSE },
 			{ .name = "link",	.gpio = 1 << 11, .polarity = REVERSE },
+		},
+	},
+	[WL330GE] = {
+		.name		= "ASUS WL-330gE",
+		.buttons	= {
+			{ .name = "reset",	.gpio = 1 << 2 },
+		},
+		.leds		= {
+			{ .name = "power",	.gpio = 1 << 0, .polarity = REVERSE },
 		},
 	},
 	[WL500G] = {
@@ -764,6 +774,8 @@ static struct platform_t __init *platform_detect(void)
 			return &platforms[WL520GC];
 		if (startswith(buf,"WL520GU-")) /* WL520GU-* */
 			return &platforms[WL520GU];
+		if (startswith(buf,"WL330GE-")) /* WL330GE-* */
+			return &platforms[WL330GE];
 	}
 
 	/* Based on "ModelId" */
