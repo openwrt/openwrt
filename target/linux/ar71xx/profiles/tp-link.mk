16c23fa914d523ed9dfd3777671fdf078617982c
Andy Boyett <agb@openwrt.org>
2009-03-01 10:34:52 +0000

    tl-wr941nd: change profile name to what new ar71xx image generation code expects
    SVN-Revision: 14705

--- target/linux/ar71xx/profiles/tp-link.mk
+++ target/linux/ar71xx/profiles/tp-link.mk
@@ -5,13 +5,13 @@
 # See /LICENSE for more information.
 #
 
-define Profile/TLWR941ND
+define Profile/TLWR941NDV2
 	NAME:=TP-LINK TL-WR941ND
 	PACKAGES:=kmod-ath9k hostapd-mini
 endef
 
-define Profile/TLWR941ND/Description
+define Profile/TLWR941NDV2/Description
 	Package set optimized for the TP-LINK TL-WR941ND.
 endef
 
-$(eval $(call Profile,TLWR941ND))
+$(eval $(call Profile,TLWR941NDV2))
