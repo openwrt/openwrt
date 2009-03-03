285b593164e1512ca7e76a847f69a1b16249fa16
Felix Fietkau <nbd@openwrt.org>
2009-03-03 14:07:39 +0000

    avoid using implicit rules during metadata dump to speed up the process even more
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@14736 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/scan.mk
+++ include/scan.mk
@@ -27,7 +27,7 @@ define PackageDir
 	{ \
 		$$(call progress,Collecting $(SCAN_NAME) info: $(SCAN_DIR)/$(2)) \
 		echo Source-Makefile: $(SCAN_DIR)/$(2)/Makefile; \
-		$(NO_TRACE_MAKE) --no-print-dir DUMP=1 -C $(SCAN_DIR)/$(2) $(SCAN_MAKEOPTS) 2>/dev/null || echo "ERROR: please fix $(SCAN_DIR)/$(2)/Makefile" >&2; \
+		$(NO_TRACE_MAKE) --no-print-dir -r DUMP=1 -C $(SCAN_DIR)/$(2) $(SCAN_MAKEOPTS) 2>/dev/null || echo "ERROR: please fix $(SCAN_DIR)/$(2)/Makefile" >&2; \
 		echo; \
 	} > $$@ || true
 endef
