ef537a68da702c907eef6f02069fc85540167aaf
Gabor Juhos <juhosg@openwrt.org>
2009-03-17 18:07:16 +0000

    ap81: register ethernet devices, and add default network configuration file
    SVN-Revision: 14921

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-ap81.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-ap81.c
@@ -16,6 +16,20 @@
 
 static void __init ap81_setup(void)
 {
+	ar71xx_add_device_mdio(0x0);
+
+	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
+	ar71xx_eth0_data.phy_mask = 0xf;
+	ar71xx_eth0_data.speed = SPEED_100;
+	ar71xx_eth0_data.duplex = DUPLEX_FULL;
+	ar71xx_eth0_data.has_ar8216 = 1;
+
+	ar71xx_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
+	ar71xx_eth1_data.phy_mask = 0x10;
+
+	ar71xx_add_device_eth(0);
+	ar71xx_add_device_eth(1);
+
 	ar91xx_add_device_wmac();
 }
 
