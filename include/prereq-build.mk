4182dc4f75df525414cd971f42b526c438b5df02
Ralph Hempel <ralph.hempel@lantiq.com>
2009-03-25 19:51:32 +0000

    added m4 to prereq check, thanks to Gerph
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15043 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -148,3 +148,7 @@ endef
 $(eval $(call Require,getopt-extended, \
 	Please install an extended getopt version that supports --long \
 ))
+
+$(eval $(call RequireCommand,m4, \
+	Please install GNU m4 \
+))
