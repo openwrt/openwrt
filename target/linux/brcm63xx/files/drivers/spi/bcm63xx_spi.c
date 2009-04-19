b1b869ec9960762adf57519ec360304f46ac0675
Florian Fainelli <florian@openwrt.org>
2009-04-19 13:32:54 +0000

    [brcm63xx] fix redundant driver name printing in probe function
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15254 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/files/drivers/spi/bcm63xx_spi.c
+++ target/linux/brcm63xx/files/drivers/spi/bcm63xx_spi.c
@@ -350,7 +350,7 @@ static int __init bcm63xx_spi_probe(struct platform_device *pdev)
 	bcm_spi_writew(SPI_CMD_HARD_RESET, bs->regs, SPI_CMD);
 	bcm_spi_writeb(SPI_INTR_CLEAR_ALL, bs->regs, SPI_INT_MASK);
 	
-	dev_info(&pdev->dev, PFX " at 0x%08x (irq %d, FIFOs size %d) v%s\n",
+	dev_info(&pdev->dev, " at 0x%08x (irq %d, FIFOs size %d) v%s\n",
 				r->start, irq, bs->fifo_size, DRV_VER);
 
 	ret = spi_bitbang_start(&bs->bitbang);
