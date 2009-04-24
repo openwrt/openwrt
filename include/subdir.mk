5e1e868230e4778ad2482b8ce68f3c3ef6bf9253
Felix Fietkau <nbd@openwrt.org>
2009-04-24 01:04:50 +0000

    add BUILD_LOG option to let the build system log all subdir make command output to individual files (useful for debugging parallel builds)
    SVN-Revision: 15362

--- include/subdir.mk
+++ include/subdir.mk
@@ -34,7 +34,8 @@ define subdir
         $(if $(call diralias,$(bd)),$(call warn_eval,$(1)/$(bd),l,T,$(1)/$(call diralias,$(bd))/$(btype)/$(target): $(1)/$(bd)/$(btype)/$(target)))
       )
       $(call warn_eval,$(1)/$(bd),t,T,$(1)/$(bd)/$(target): $(if $(QUILT),,$($(1)/$(bd)/$(target)) $(call $(1)//$(target),$(1)/$(bd))))
-		+$$(SUBMAKE) -C $(1)/$(bd) $(target) $(if $(findstring $(bd),$($(1)/builddirs-ignore-$(target))), || $(call MESSAGE,   ERROR: $(1)/$(bd) failed to build.))
+	  	$(if $(BUILD_LOG),@mkdir -p $(TOPDIR)/logs/$(1)/$(bd))
+		+$$(SUBMAKE) -C $(1)/$(bd) $(target) SILENT= 2>&1 | tee $(TOPDIR)/logs/$(1)/$(bd)/$(target).txt $(if $(findstring $(bd),$($(1)/builddirs-ignore-$(target))), || $(call MESSAGE,   ERROR: $(1)/$(bd) failed to build.))
         $$(if $(call debug,$(1)/$(bd),v),,.SILENT: $(1)/$(bd)/$(target))
 
       # legacy targets
