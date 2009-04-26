51837a4e11a3ee9f0b7749e6ffe2335cb2ff7d72
Nicolas Thill <nico@openwrt.org>
2009-04-26 12:49:11 +0000

    [package] kernel: disable kmod-rfkill on UML
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15425 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/other.mk
+++ package/kernel/modules/other.mk
@@ -855,7 +855,7 @@ $(eval $(call KernelPackage,textsearch))
 define KernelPackage/rfkill
   SUBMENU:=$(OTHER_MENU)
   TITLE:=RF switch subsystem support
-  DEPENDS:=@LINUX_2_6 @!LINUX_2_6_21 @!TARGET_rb532 @!TARGET_avr32 @!TARGET_brcm47xx @!TARGET_s3c24xx @!TARGET_ifxmips @!TARGET_atheros @!TARGET_adm5120 @!TARGET_ar7 @!TARGET_ppc40x @!TARGET_ixp4xx @!TARGET_rdc
+  DEPENDS:=@LINUX_2_6 @!LINUX_2_6_21 @!TARGET_rb532 @!TARGET_avr32 @!TARGET_brcm47xx @!TARGET_s3c24xx @!TARGET_ifxmips @!TARGET_atheros @!TARGET_adm5120 @!TARGET_ar7 @!TARGET_ppc40x @!TARGET_ixp4xx @!TARGET_rdc @!TARGET_uml
   KCONFIG:= \
     CONFIG_RFKILL \
     CONFIG_RFKILL_INPUT \
