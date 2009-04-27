414ea4bc49181656bf5f8a8bce60cdbe7a229e17
Nicolas Thill <nico@openwrt.org>
2009-04-27 16:02:21 +0000

    add missing bits from [15451]
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15453 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/package.mk
+++ include/package.mk
@@ -42,7 +42,7 @@ ifeq ($(DUMP)$(filter prereq clean refresh update,$(MAKECMDGOALS)),)
   ifneq ($(if $(QUILT),,$(CONFIG_AUTOREBUILD)),)
     define Build/Autoclean
       $(PKG_BUILD_DIR)/.dep_files: $(STAMP_PREPARED)
-      $(call rdep,${CURDIR} $(PKG_FILE_DEPEND),$(STAMP_PREPARED),$(PKG_BUILD_DIR)/.dep_files,-x "*/.dep_*")
+      $(call rdep,${CURDIR} $(PKG_FILE_DEPENDS),$(STAMP_PREPARED),$(PKG_BUILD_DIR)/.dep_files,-x "*/.dep_*")
       $(if $(filter prepare,$(MAKECMDGOALS)),,$(call rdep,$(PKG_BUILD_DIR),$(STAMP_BUILT),,-x "*/.dep_*" -x "*/ipkg*"))
     endef
   endif
