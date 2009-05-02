2f7b060aad72322ab1e297d26265f6550a97bf33
Jo-Philipp Wich <jow@openwrt.org>
2009-05-02 23:51:05 +0000

    [package] kernel: fix dependencies for kmod-ipt-filter (required by qos-scripts)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15563 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/netfilter.mk
+++ package/kernel/modules/netfilter.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2006-2008 OpenWrt.org
 #
@@ -78,7 +79,7 @@ define KernelPackage/ipt-filter
   KCONFIG:=$(KCONFIG_IPT_FILTER)
   FILES:=$(foreach mod,$(IPT_FILTER-m),$(LINUX_DIR)/net/$(mod).$(LINUX_KMOD_SUFFIX))
   AUTOLOAD:=$(call AutoLoad,45,$(notdir $(IPT_FILTER-m)))
-  DEPENDS:= kmod-ipt-core @LINUX_2_4||+kmod-textsearch
+  DEPENDS:=kmod-ipt-core +LINUX_2_6:kmod-textsearch
 endef
 
 define KernelPackage/ipt-filter/description
