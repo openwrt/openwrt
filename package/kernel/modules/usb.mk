882a21e5bee34de5ce40cf1ca50849d84c50521b
Gabor Juhos <juhosg@openwrt.org>
2009-05-07 19:13:44 +0000

    [package] kernel/modules: add ar71xx specific KCONFIG options for the OHCI/EHCI drivers
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@15695 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/usb.mk
+++ package/kernel/modules/usb.mk
@@ -97,7 +97,8 @@ define KernelPackage/usb-ohci
   TITLE:=Support for OHCI controllers
   KCONFIG:= \
 	CONFIG_USB_OHCI \
-	CONFIG_USB_OHCI_HCD
+	CONFIG_USB_OHCI_HCD \
+	CONFIG_USB_OHCI_AR71XX=y
 endef
 
 define KernelPackage/usb-ohci/2.4
@@ -138,7 +139,8 @@ define KernelPackage/usb2
   $(call usbdep,)
   TITLE:=Support for USB2 controllers
   KCONFIG:=CONFIG_USB_EHCI_HCD \
-    CONFIG_USB_EHCI_HCD_SSB=y
+    CONFIG_USB_EHCI_HCD_SSB=y \
+    CONFIG_USB_EHCI_AR71XX=y
   FILES:=$(LINUX_DIR)/drivers/usb/host/ehci-hcd.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,40,ehci-hcd)
 endef
