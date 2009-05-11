050d860d653574622f51ecb7aeaeda9492b76e80
Hauke Mehrtens <hauke@hauke-m.de>
2009-05-11 20:41:15 +0000

    kmod-libphy also needed with kernel 2.6.30
    SVN-Revision: 15780

--- package/kernel/modules/netdevices.mk
+++ package/kernel/modules/netdevices.mk
@@ -273,7 +273,7 @@ define KernelPackage/tg3
   TITLE:=Broadcom Tigon3 Gigabit Ethernet
   FILES:=$(LINUX_DIR)/drivers/net/tg3.$(LINUX_KMOD_SUFFIX)
   KCONFIG:=CONFIG_TIGON3
-  DEPENDS:=@LINUX_2_6 +LINUX_2_6_27||LINUX_2_6_28||LINUX_2_6_29:kmod-libphy
+  DEPENDS:=@LINUX_2_6 +LINUX_2_6_27||LINUX_2_6_28||LINUX_2_6_29||LINUX_2_6_30:kmod-libphy
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   AUTOLOAD:=$(call AutoLoad,50,tg3)
 endef
