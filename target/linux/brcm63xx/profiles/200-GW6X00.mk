90a0ecd2752272e90c6208350790b86cb2d0856f
Florian Fainelli <florian@openwrt.org>
2009-05-25 16:14:13 +0000

    [brcm63xx] add the Tecom GW6x00 (6000 and 6200) board profile
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@16059 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm63xx/profiles/200-GW6X00.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/GW6X00
+  NAME:=Tecom GW6X00
+  PACKAGES:=kmod-b43 kmod-usb-core kmod-usb-ohci \
+	kmod-fs-ext2 kmod-nls-cp437 kmod-nls-iso8859-1 kmod-fs-vfat \
+	kmod-loop e2fsprogs
+endef
+
+define Profile/GW6X00/Description
+	Package set compatible with the Tecom GW6000 and GW6200 based
+	on the BCM96348GW reference design.
+endef
+$(eval $(call Profile,GW6X00))
+
