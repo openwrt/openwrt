733b0a1ff824ebb72efb96b32878253756bd6f10
Florian Fainelli <florian@openwrt.org>
2009-05-30 08:54:55 +0000

    rework ar7 profiles to separate dsl firmwares, annex-a being now the default and most widespread
    SVN-Revision: 16201

---
+++ target/linux/ar7/profiles/100-Annex-A.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Annex-A
+  NAME:=Annex-A DSL firmware (default)
+  PACKAGES:=kmod-sangam-atm-annex-a
+endef
+
+define Profile/Annex-A/Description
+	Package set compatible with Annex-A DSL lines (most countries).
+endef
+$(eval $(call Profile,Annex-A))
+
