d0b60d14ea3ced4a60015cc4d6443369c749d342
Jo-Philipp Wich <jow@openwrt.org>
2009-05-30 23:47:50 +0000

    Typo in r8610.mk signed-off by: Jac Goudsmit <jac@goudsm.it>
    SVN-Revision: 16223

--- target/linux/rdc/profiles/R8610.mk
+++ target/linux/rdc/profiles/R8610.mk
@@ -6,7 +6,7 @@
 #
 
 define Profile/r8610
-  NAME:=RDC R8160 Evaluation board
+  NAME:=RDC R8610 Evaluation board
   PACKAGES:=kmod-r6040 kmod-usb-core kmod-usb-ohci kmod-usb2 \
 	kmod-hwmon-core kmod-hwmon-w83627hf kmod-ide-core kmod-ide-it821x \
 	kmod-rtc-core kmod-rtc-m48t86 \
