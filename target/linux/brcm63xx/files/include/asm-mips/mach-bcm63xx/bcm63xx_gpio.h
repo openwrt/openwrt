1208ffaedb5e7472a15427dd91ae237146305ab5
Florian Fainelli <florian@openwrt.org>
2009-06-04 10:01:03 +0000

    use the correct number of GPIOs for bcm6358
    SVN-Revision: 16329

--- target/linux/brcm63xx/files/include/asm-mips/mach-bcm63xx/bcm63xx_gpio.h
+++ target/linux/brcm63xx/files/include/asm-mips/mach-bcm63xx/bcm63xx_gpio.h
@@ -5,8 +5,16 @@
 
 int __init bcm63xx_gpio_init(void);
 
-/* all helpers will BUG() if gpio count is >= 37. */
-#define BCM63XX_GPIO_COUNT	37
+static inline unsigned long bcm63xx_gpio_count(void)
+{
+	switch (bcm63xx_get_cpu_id()) {
+	case BCM6358_CPU_ID:
+		return 40;
+	case BCM6348_CPU_ID:
+	default:
+		return 37;
+	}
+}
 
 #define GPIO_DIR_OUT	0x0
 #define GPIO_DIR_IN	0x1
