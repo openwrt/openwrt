227e69c0f4cf6b349be1c9624f04007cadfb8337
Gabor Juhos <juhosg@openwrt.org>
2009-06-06 17:44:49 +0000

    add experimental sysupgrade support
    SVN-Revision: 16364

---
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+
+. /lib/ar71xx.sh
+
+PART_NAME=firmware
+
+platform_check_image() {
+	local board=$(ar71xx_board_name)
+	local magic="$(get_magic_word "$1")"
+
+	[ "$ARGC" -gt 1 ] && return 1
+
+	case "$board" in
+	ap83 | mzk-w04nu | mzk-w300nh | tew-632brp | wrt-400n)
+		[ "$magic" != "2705" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	tl-wr941nd)
+		[ "$magic" != "0100" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	echo "Sysupgrade is not yet supported on $board."
+	return 1
+}
+
+# use default for platform_do_upgrade()
+
+disable_watchdog() {
+	killall watchdog
+	( ps | grep -v 'grep' | grep '/dev/watchdog' ) && {
+		echo 'Could not disable watchdog'
+		return 1
+	}
+}
+append sysupgrade_pre_upgrade disable_watchdog
