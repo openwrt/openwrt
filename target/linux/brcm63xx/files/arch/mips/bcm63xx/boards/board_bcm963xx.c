d1867359ac4d8e0aecc5832c09ae04fb22a15ab1
Florian Fainelli <florian@openwrt.org>
2009-06-27 18:23:58 +0000

    [brcm63xx] prevent bcm6338 and bcm6345 from crashing very early
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@16585 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/files/arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ target/linux/brcm63xx/files/arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -323,9 +323,15 @@ void __init board_prom_init(void)
 	char cfe_version[32];
 	u32 val;
 
-	/* read base address of boot chip select (0) */
-	val = bcm_mpi_readl(MPI_CSBASE_REG(0));
-	val &= MPI_CSBASE_BASE_MASK;
+	/* read base address of boot chip select (0) 
+	 * 6338 does not have MPI but boots from standard
+	 * MIPS Flash address */
+	if (BCMCPU_IS_6338() || BCMCPU_IS_6345())
+		val = 0x1fc00000;
+	else {
+		val = bcm_mpi_readl(MPI_CSBASE_REG(0));
+		val &= MPI_CSBASE_BASE_MASK;
+	}
 	boot_addr = (u8 *)KSEG1ADDR(val);
 
 	/* dump cfe version */
