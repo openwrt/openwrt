4096da301d1bd8c6b51123e1c157a634022ab845
Florian Fainelli <florian@openwrt.org>
2009-06-28 04:23:11 +0000

    off-by-one gpio numbers for bcm6338
    SVN-Revision: 16597

--- target/linux/brcm63xx/files/include/asm-mips/mach-bcm63xx/bcm63xx_gpio.h
+++ target/linux/brcm63xx/files/include/asm-mips/mach-bcm63xx/bcm63xx_gpio.h
@@ -11,7 +11,7 @@ static inline unsigned long bcm63xx_gpio_count(void)
 	case BCM6358_CPU_ID:
 		return 40;
 	case BCM6338_CPU_ID:
-		return 7;
+		return 8;
 	case BCM6345_CPU_ID:
 		return 16;
 	case BCM6348_CPU_ID:
