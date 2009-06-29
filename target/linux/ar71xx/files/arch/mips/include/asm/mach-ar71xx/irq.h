d9259b25bfc89e408b1c8bd17b028f6c08840d16
Gabor Juhos <juhosg@openwrt.org>
2009-06-29 20:01:22 +0000

    increase NR_IRQS
    SVN-Revision: 16623

--- target/linux/ar71xx/files/arch/mips/include/asm/mach-ar71xx/irq.h
+++ target/linux/ar71xx/files/arch/mips/include/asm/mach-ar71xx/irq.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2008 Gabor Juhos <juhosg@openwrt.org>
+ *  Copyright (C) 2008-2009 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
  *
  *  This program is free software; you can redistribute it and/or modify it
@@ -10,7 +10,7 @@
 #define __ASM_MACH_AR71XX_IRQ_H
 
 #define MIPS_CPU_IRQ_BASE	0
-#define NR_IRQS			36
+#define NR_IRQS			56
 
 #include_next <irq.h>
 
