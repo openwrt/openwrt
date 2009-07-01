60d9465d365eecb20348fafc8bee58cf74a706f8
Gabor Juhos <juhosg@openwrt.org>
2009-07-01 19:38:26 +0000

    [ar71xx] initialize GPIO for the AR7240 SoC
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@16647 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/include/asm/mach-ar71xx/ar71xx.h
+++ target/linux/ar71xx/files/arch/mips/include/asm/mach-ar71xx/ar71xx.h
@@ -238,6 +238,8 @@ static inline u32 ar71xx_usb_ctrl_rr(unsigned reg)
 
 #define AR71XX_GPIO_COUNT	16
 
+#define AR724X_GPIO_COUNT	16
+
 #define AR91XX_GPIO_FUNC_WMAC_LED_EN	BIT(22)
 #define AR91XX_GPIO_FUNC_EXP_PORT_CS_EN	BIT(21)
 #define AR91XX_GPIO_FUNC_I2S_REFCLKEN	BIT(20)
