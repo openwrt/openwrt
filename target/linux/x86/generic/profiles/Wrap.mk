7c68d57ce42996d5f5ebd7e6d2366261ba66e8e5
Andy Boyett <agb@openwrt.org>
2009-07-02 07:24:38 +0000

    rename Pcengines profile to Wrap, PCEngines also makes the ALIX series
    SVN-Revision: 16651

--- target/linux/x86/generic/profiles/Wrap.mk
+++ target/linux/x86/generic/profiles/Wrap.mk
@@ -1,16 +1,16 @@
 #
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006,2009 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
-define Profile/Pcengines
+define Profile/Wrap
   NAME:=PCEngines WRAP
   PACKAGES:=kmod-i2c-scx200 kmod-natsemi kmod-leds-wrap kmod-scx200-gpio kmod-scx200-wdt kmod-hwmon-pc87360
 endef
 
-define Profile/Pcengines/Description
+define Profile/Wrap/Description
 	Package set compatible with the PCEngines WRAP. Contains I2C/LEDS/GPIO/Sensors support
 endef
-$(eval $(call Profile,Pcengines))
+$(eval $(call Profile,Wrap))
