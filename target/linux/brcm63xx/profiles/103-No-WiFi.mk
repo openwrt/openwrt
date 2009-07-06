9a387036d26d42c29bce628af4cf58f3f3cd32dc
Florian Fainelli <florian@openwrt.org>
2009-07-06 11:21:22 +0000

    add a profile with no Wi-Fi drivers enabled, useful for bcm6338
    SVN-Revision: 16708

---
+++ target/linux/brcm63xx/profiles/103-No-WiFi.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/NoWiFi
+  NAME:=No WiFi (BCM6338)
+  PACKAGES:=kmod-spi-bitbang kmod-bcm63xx-spi
+endef
+
+define Profile/NoWiFi/description
+  Package set compatible with BCM63xx routers without Wi-Fi (e.g: BCM6338-based).
+endef
+
+$(eval $(call Profile,NoWiFi))
