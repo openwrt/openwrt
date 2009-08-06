1bfab34d40bc89f021091df56b0bb2e43b7fba8d
Florian Fainelli <florian@openwrt.org>
2009-08-06 18:44:20 +0000

    [brcm63xx] disable watchdog for bcm6345 until it gets fixed
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@17148 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/files/arch/mips/bcm63xx/dev-wdt.c
+++ target/linux/brcm63xx/files/arch/mips/bcm63xx/dev-wdt.c
@@ -31,5 +31,8 @@ int __init bcm63xx_wdt_register(void)
 	wdt_resources[0].start = bcm63xx_regset_address(RSET_WDT);
 	wdt_resources[0].end = wdt_resources[0].start;
 	wdt_resources[0].end += RSET_WDT_SIZE - 1;
-	return platform_device_register(&bcm63xx_wdt_device);
+
+	/* Disable watchdog for 6345 until we fix it */
+	if (!BCMCPU_IS_6345())
+		return platform_device_register(&bcm63xx_wdt_device);
 }
