cef3731e4a96b08c9ca22b2cf9a02a173ca886be
Florian Fainelli <florian@openwrt.org>
2009-08-14 13:56:17 +0000

    [kernel] use 2.4.37.5
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@17257 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -1,14 +1,14 @@
 # Use the default kernel version if the Makefile doesn't override it
 
 ifeq ($(KERNEL),2.4)
-  LINUX_VERSION?=2.4.37.4
+  LINUX_VERSION?=2.4.37.5
 else
   LINUX_VERSION?=2.6.21.7
 endif
 LINUX_RELEASE?=1
 
-ifeq ($(LINUX_VERSION),2.4.37.4)
-  LINUX_KERNEL_MD5SUM:=
+ifeq ($(LINUX_VERSION),2.4.37.5)
+  LINUX_KERNEL_MD5SUM:=cb221187422acaf6c63a40c646e5e476
 endif
 ifeq ($(LINUX_VERSION),2.6.21.7)
   LINUX_KERNEL_MD5SUM:=bc15fad1487336d5dcb0945cd039d8ed
