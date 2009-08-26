71a3d54218dea60f77e5452f7d99f62cbdceb3a0
Gabor Juhos <juhosg@openwrt.org>
2009-08-26 09:52:25 +0000

    create v4 squashfs image for 2.6.31
    SVN-Revision: 17406

--- include/image.mk
+++ include/image.mk
@@ -23,7 +23,7 @@ JFFS2OPTS     :=  --pad --big-endian --squash
 SQUASHFS_OPTS :=  -be
 endif
 
-ifneq ($(CONFIG_LINUX_2_6_30),)
+ifneq ($(CONFIG_LINUX_2_6_30)$(CONFIG_LINUX_2_6_31),)
 USE_SQUASHFS4 := y
 endif
 
