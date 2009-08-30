8049111b19caae31a458ebfb635e93b48b23d23c
Gabor Juhos <juhosg@openwrt.org>
2009-08-30 19:55:55 +0000

    rt288x: generic machine code cleanup
    SVN-Revision: 17442

--- target/linux/ramips/files/arch/mips/ralink/rt288x/mach-generic.c
+++ target/linux/ramips/files/arch/mips/ralink/rt288x/mach-generic.c
@@ -10,56 +10,11 @@
  */
 
 #include <linux/init.h>
-#include <linux/platform_device.h>
-#include <linux/mtd/mtd.h>
-#include <linux/mtd/partitions.h>
-#include <linux/mtd/physmap.h>
 
 #include <asm/mips_machine.h>
-#include <asm/mach-ralink/rt288x.h>
-#include <asm/mach-ralink/platform.h>
-
-#ifdef CONFIG_MTD_PARTITIONS
-static struct mtd_partition generic_partitions[] = {
-	{
-		.name	= "u-boot",
-		.offset	= 0,
-		.size	= 0x030000,
-		.mask_flags = MTD_WRITEABLE,
-	}, {
-		.name	= "config",
-		.offset	= 0x030000,
-		.size	= 0x010000,
-		.mask_flags = MTD_WRITEABLE,
-	}, {
-		.name	= "defconfig",
-		.offset	= 0x040000,
-		.size	= 0x010000,
-		.mask_flags = MTD_WRITEABLE,
-	}, {
-		.name	= "kernel",
-		.offset	= 0x050000,
-		.size	= 0x100000,
-	}, {
-		.name	= "rootfs",
-//		.offset = MTDPART_OFS_NXTBLK,
-//		.size   = MTDPART_SIZ_FULL,
-		.offset	= 0x150000,
-		.size	= 0x2B0000,
-	}
-};
-#endif /* CONFIG_MTD_PARTITIONS */
-
-static struct physmap_flash_data generic_flash_data = {
-#ifdef CONFIG_MTD_PARTITIONS
-	.nr_parts	= ARRAY_SIZE(generic_partitions),
-	.parts		= generic_partitions,
-#endif
-};
 
 static void __init rt288x_generic_init(void)
 {
-	rt288x_register_flash(0, &generic_flash_data);
 }
 
 MIPS_MACHINE(RT288X_MACH_GENERIC, "Generic RT288x board", rt288x_generic_init);
