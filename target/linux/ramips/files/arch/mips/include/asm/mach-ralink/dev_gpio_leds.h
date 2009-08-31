19a462c9e37ab229a3bc2c90e479d142bd63178e
Gabor Juhos <juhosg@openwrt.org>
2009-08-31 09:14:14 +0000

    add common GPIO LED support
    SVN-Revision: 17448

---
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/dev_gpio_leds.h
@@ -0,0 +1,26 @@
+/*
+ *  Ralink SoC GPIO LED device support
+ *
+ *  Copyright (C) 2009 Gabor Juhos <juhosg@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#ifndef __ASM_MACH_RALINK_DEV_GPIO_LEDS_H
+#define __ASM_MACH_RALINK_DEV_GPIO_LEDS_H
+
+struct gpio_led;
+
+#ifdef CONFIG_RALINK_DEV_GPIO_LEDS
+__init void ramips_register_gpio_leds(int id, unsigned num_leds,
+				      struct gpio_led *leds);
+#else
+static inline void ramips_register_gpio_leds(int id, unsigned num_leds,
+					     struct gpio_led *leds)
+{
+}
+#endif
+
+#endif /* __ASM_MACH_RALINK_DEV_GPIO_LEDS_H */
---
+++ target/linux/ramips/files/arch/mips/ralink/common/dev-gpio-leds.c
@@ -0,0 +1,54 @@
+/*
+ *  Ralink SoC GPIO LED device support
+ *
+ *  Copyright (C) 2009 Gabor Juhos <juhosg@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/leds.h>
+
+#include <asm/mach-ralink/dev_gpio_leds.h>
+
+void __init ramips_register_gpio_leds(int id, unsigned num_leds,
+				      struct gpio_led *leds)
+{
+	struct platform_device *pdev;
+	struct gpio_led_platform_data pdata;
+	struct gpio_led *p;
+	int err;
+
+	p = kmalloc(num_leds * sizeof(*p), GFP_KERNEL);
+	if (!p)
+		return;
+
+	memcpy(p, leds, num_leds * sizeof(*p));
+
+	pdev = platform_device_alloc("leds-gpio", id);
+	if (!pdev)
+		goto err_free_leds;
+
+	memset(&pdata, 0, sizeof(pdata));
+	pdata.num_leds = num_leds;
+	pdata.leds = p;
+
+	err = platform_device_add_data(pdev, &pdata, sizeof(pdata));
+	if (err)
+		goto err_put_pdev;
+
+	err = platform_device_add(pdev);
+	if (err)
+		goto err_put_pdev;
+
+	return;
+
+err_put_pdev:
+	platform_device_put(pdev);
+
+err_free_leds:
+	kfree(p);
+}
