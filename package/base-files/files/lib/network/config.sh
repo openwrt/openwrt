2aac835be8b41fc6cb0f279bedef625d666aae5c
Travis Kemen <thepeople@openwrt.org>
2009-09-06 17:02:14 +0000

    fix nonhotpluggable networks not being brought up. Closes #2781
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@17527 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/network/config.sh
+++ package/base-files/files/lib/network/config.sh
@@ -153,6 +153,7 @@ set_interface_ifname() {
 	config_get device "$1" device
 	uci_set_state network "$config" ifname "$ifname"
 	uci_set_state network "$config" device "$device"
+	uci_set_state network "$config" started 1
 }
 
 setup_interface_none() {
