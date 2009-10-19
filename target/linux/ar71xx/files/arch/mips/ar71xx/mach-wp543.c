b1cd54b6b934c7b594cfe8600680c61b7ccfa42c
Gabor Juhos <juhosg@openwrt.org>
2009-10-19 08:07:32 +0000

    ar71xx: reset the phy chip along with the ethernet mac on the WP543 board
    SVN-Revision: 18072

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-wp543.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-wp543.c
@@ -101,6 +101,8 @@ static void __init wp543_setup(void)
 
 	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
 	ar71xx_eth0_data.phy_mask = 0x00000008;
+	ar71xx_eth0_data.reset_bit = RESET_MODULE_GE0_MAC |
+				     RESET_MODULE_GE0_PHY;
 	ar71xx_add_device_eth(0);
 
 	ar71xx_add_device_usb();
