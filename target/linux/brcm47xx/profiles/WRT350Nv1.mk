a1021e8e647496664313389706865ccbb9927225
Travis Kemen <thepeople@openwrt.org>
2009-10-30 01:05:37 +0000

    add WRT350Nv1 target, thanks sn9
    SVN-Revision: 18218

---
+++ target/linux/brcm47xx/profiles/WRT350Nv1.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/WRT350Nv1
+  NAME:=Linksys WRT350Nv1
+  PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ssb-gige kmod-ocf-ubsec-ssb
+endef
+
+define Profile/WRT350Nv1/Description
+	Package set compatible with the Linksys WRT350Nv1. Contains USB support
+endef
+$(eval $(call Profile,WRT350Nv1)
