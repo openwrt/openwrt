10752b10dfa64e4c38c59afbaa2545792a7f369c
Florian Fainelli <florian@openwrt.org>
2009-11-10 00:22:07 +0000

    convert profiles to become subtargets, compile tested only
    SVN-Revision: 18354

---
+++ target/linux/rdc/amit/target.mk
@@ -0,0 +1,2 @@
+BOARDNAME:=Devices from AMIT
+DEFAULT_PACKAGE+= kmod-usb-core kmod-usb-ohci kmod-usb2
---
+++ target/linux/rdc/r8610/target.mk
@@ -0,0 +1,5 @@
+BOARDNAME:=RDC R8160 Evaluation Board
+DEFAULT_PACKAGES += kmod-r6040 kmod-usb-core kmod-usb-ohci kmod-usb2 \
+		    kmod-hwmon-core kmod-hwmon-w83627hf kmod-ide-core kmod-ide-it821x \
+		    kmod-rtc-core kmod-rtc-m48t86 \
+		    kmod-fs-ext2 kmod-fs-ext3
