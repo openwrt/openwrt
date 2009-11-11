a228d3f366975e263d3acb7ab6596dbf0fb1f73b
Felix Fietkau <nbd@openwrt.org>
2009-11-11 01:25:02 +0000

    automatically adjust CONFIG_BLK_DEV_INITRD in the kernel config (thx, sn9)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18361 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -67,7 +67,8 @@ ifeq ($(KERNEL),2.6)
   ifeq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
     define Kernel/SetInitramfs
 		mv $(LINUX_DIR)/.config $(LINUX_DIR)/.config.old
-		grep -v -e INITRAMFS -e CONFIG_RD_ $(LINUX_DIR)/.config.old > $(LINUX_DIR)/.config
+		grep -v -e INITRAMFS -e CONFIG_RD_ -e CONFIG_BLK_DEV_INITRD $(LINUX_DIR)/.config.old > $(LINUX_DIR)/.config
+		echo 'CONFIG_BLK_DEV_INITRD=y' >> $(LINUX_DIR)/.config
 		echo 'CONFIG_INITRAMFS_SOURCE="$(strip $(TARGET_DIR) $(INITRAMFS_EXTRA_FILES))"' >> $(LINUX_DIR)/.config
 		echo 'CONFIG_INITRAMFS_ROOT_UID=$(shell id -u)' >> $(LINUX_DIR)/.config
 		echo 'CONFIG_INITRAMFS_ROOT_GID=$(shell id -g)' >> $(LINUX_DIR)/.config
