f92973072d9e58503a70ff48e2e41fb47a9bcbb8
Gabor Juhos <juhosg@openwrt.org>
2009-11-13 17:11:29 +0000

    ar71xx: add preliminary support for the Netgear WNDR3700
    Based on patches by Marco Porsch from:
    https://forum.openwrt.org/viewtopic.php?id=22311
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18411 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ar71xx/profiles/netgear.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/WNDR3700
+	NAME:=NETGEAR WNDR3700
+	PACKAGES:=kmod-ath9k hostapd-mini kmod-usb-core kmod-usb-ohci kmod-usb2
+endef
+
+define Profile/WNDR3700/Description
+	Package set optimized for the NETGEAR WNDR3700
+endef
+
+$(eval $(call Profile,WNDR3700))
