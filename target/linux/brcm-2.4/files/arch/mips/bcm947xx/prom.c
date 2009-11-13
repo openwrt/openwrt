f43d5f8d1aa23667620c3c1c29f336b21a4c3495
Hauke Mehrtens <hauke@openwrt.org>
2009-11-13 20:32:47 +0000

    [brcm47xx] fix problem with 128MB ram.
    This patch is out of broadcoms official source code
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18413 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm-2.4/files/arch/mips/bcm947xx/prom.c
+++ target/linux/brcm-2.4/files/arch/mips/bcm947xx/prom.c
@@ -31,6 +31,15 @@ prom_init(int argc, const char **argv)
 		    *(unsigned long *)(prom_init))
 			break;
 	}
+
+	/* Ignoring the last page when ddr size is 128M. Cached
+	 * accesses to last page is causing the processor to prefetch
+	 * using address above 128M stepping out of the ddr address
+	 * space.
+	 */
+	if (mem == 0x8000000)
+		mem -= 0x1000;
+
 	add_memory_region(0, mem, BOOT_MEM_RAM);
 }
 
