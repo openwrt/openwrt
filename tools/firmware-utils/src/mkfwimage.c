e8da3793a2db6012f36e6d1552e2260b0ceeda2a
Gabor Juhos <juhosg@openwrt.org>
2009-11-13 17:11:23 +0000

    ar71xx: increase max size of RouterStation and RouterStation Pro firmware images
    Thanks to riskable! (closes #5860)
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18410 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mkfwimage.c
+++ tools/firmware-utils/src/mkfwimage.c
@@ -55,13 +55,13 @@ fw_layout_t fw_layout_data[] = {
 		.name		=	"RS",
 		.kern_start	=	0xbf030000,
 		.kern_entry	=	0x80060000,
-		.firmware_max_length=	0x00640000,
+		.firmware_max_length=	0x00B00000,
 	},
 	{
 		.name		=	"RSPRO",
 		.kern_start	=	0xbf030000,
 		.kern_entry	=	0x80060000,
-		.firmware_max_length=	0x00640000,
+		.firmware_max_length=	0x00B00000,
 	},
 	{
 		.name		=	"LS-SR71",
