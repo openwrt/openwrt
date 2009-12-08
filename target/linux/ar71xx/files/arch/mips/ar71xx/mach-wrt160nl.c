fec8e48603f885f8e70ae67a7f4e71a3b53e292d
Felix Fietkau <nbd@openwrt.org>
2009-12-08 14:06:18 +0000

    ar71xx: make the rtl8306 switch attach to eth0 instead of eth1 on the wrt160nl, fixes wan port detection (#6309)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18699 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-wrt160nl.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-wrt160nl.c
@@ -124,7 +124,7 @@ static void __init wrt160nl_setup(void)
 	ar71xx_add_device_mdio(0x0);
 
 	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
-	ar71xx_eth0_data.phy_mask = 0xf;
+	ar71xx_eth0_data.phy_mask = 0x01;
 	ar71xx_eth0_data.speed = SPEED_100;
 	ar71xx_eth0_data.duplex = DUPLEX_FULL;
 
