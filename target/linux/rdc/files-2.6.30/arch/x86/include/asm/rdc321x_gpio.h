95fa0702028d0aa0ffb625e5fd08e90d76d4290f
Florian Fainelli <florian@openwrt.org>
2009-12-11 19:32:52 +0000

    convert gpio code to use gpiolib, make rdc321x:dmz led work again
    SVN-Revision: 18754

--- target/linux/rdc/files-2.6.30/arch/x86/include/asm/rdc321x_gpio.h
+++ target/linux/rdc/files-2.6.30/arch/x86/include/asm/rdc321x_gpio.h
@@ -3,55 +3,12 @@
 
 #include <linux/kernel.h>
 
-extern int rdc_gpio_get_value(unsigned gpio);
-extern void rdc_gpio_set_value(unsigned gpio, int value);
-extern int rdc_gpio_direction_input(unsigned gpio);
-extern int rdc_gpio_direction_output(unsigned gpio, int value);
-extern int rdc_gpio_request(unsigned gpio, const char *label);
-extern void rdc_gpio_free(unsigned gpio);
+#define gpio_to_irq(gpio)       NULL
 
-/* Wrappers for the arch-neutral GPIO API */
+#define gpio_get_value __gpio_get_value
+#define gpio_set_value __gpio_set_value
 
-static inline int gpio_request(unsigned gpio, const char *label)
-{
-	return rdc_gpio_request(gpio, label);
-}
-
-static inline void gpio_free(unsigned gpio)
-{
-	might_sleep();
-	rdc_gpio_free(gpio);
-}
-
-static inline int gpio_direction_input(unsigned gpio)
-{
-	return rdc_gpio_direction_input(gpio);
-}
-
-static inline int gpio_direction_output(unsigned gpio, int value)
-{
-	return rdc_gpio_direction_output(gpio, value);
-}
-
-static inline int gpio_get_value(unsigned gpio)
-{
-	return rdc_gpio_get_value(gpio);
-}
-
-static inline void gpio_set_value(unsigned gpio, int value)
-{
-	rdc_gpio_set_value(gpio, value);
-}
-
-static inline int gpio_to_irq(unsigned gpio)
-{
-	return gpio;
-}
-
-static inline int irq_to_gpio(unsigned irq)
-{
-	return irq;
-}
+#define gpio_cansleep __gpio_cansleep
 
 /* For cansleep */
 #include <asm-generic/gpio.h>
