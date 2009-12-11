f4e935e035e343953959038446e47901428c05dd
Gabor Juhos <juhosg@openwrt.org>
2009-12-11 19:32:58 +0000

    ar71xx: fix NULL pointer dereference in the ethernet driver
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18755 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/drivers/net/ag71xx/ag71xx_phy.c
+++ target/linux/ar71xx/files/drivers/net/ag71xx/ag71xx_phy.c
@@ -309,9 +309,11 @@ int ag71xx_phy_connect(struct ag71xx *ag)
 	}
 
 	/* Reset the mdio bus explicitly */
-	mutex_lock(&ag->mii_bus->mdio_lock);
-	ag->mii_bus->reset(ag->mii_bus);
-	mutex_unlock(&ag->mii_bus->mdio_lock);
+	if (ag->mii_bus->reset) {
+		mutex_lock(&ag->mii_bus->mdio_lock);
+		ag->mii_bus->reset(ag->mii_bus);
+		mutex_unlock(&ag->mii_bus->mdio_lock);
+	}
 
 	if (pdata->phy_mask)
 		return ag71xx_phy_connect_multi(ag);
