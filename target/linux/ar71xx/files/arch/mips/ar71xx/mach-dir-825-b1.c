37b67cc5d265c316c86b6866cfe9d9f0223bac0e
Gabor Juhos <juhosg@openwrt.org>
2009-12-18 12:57:05 +0000

    ar71xx: move AP94 specific PCI init code into a separate file
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18819 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-dir-825-b1.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-dir-825-b1.c
@@ -14,17 +14,15 @@
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
 #include <linux/input.h>
-#include <linux/pci.h>
-#include <linux/ath9k_platform.h>
 #include <linux/delay.h>
 #include <linux/rtl8366_smi.h>
 
 #include <asm/mips_machine.h>
 #include <asm/mach-ar71xx/ar71xx.h>
-#include <asm/mach-ar71xx/pci.h>
 
 #include "devices.h"
 #include "dev-m25p80.h"
+#include "dev-ap94-pci.h"
 
 #define DIR825B1_GPIO_LED_BLUE_USB		0
 #define DIR825B1_GPIO_LED_ORANGE_POWER		1
@@ -46,11 +44,6 @@
 
 #define DIR825B1_MAC_LOCATION_0			0x2ffa81b8
 #define DIR825B1_MAC_LOCATION_1			0x2ffa8370
-
-static struct ath9k_platform_data dir825b1_wmac0_data;
-static struct ath9k_platform_data dir825b1_wmac1_data;
-static char dir825b1_wmac0_mac[6];
-static char dir825b1_wmac1_mac[6];
 
 #ifdef CONFIG_MTD_PARTITIONS
 static struct mtd_partition dir825b1_partitions[] = {
@@ -143,124 +136,6 @@ static struct platform_device dir825b1_rtl8366_smi_device = {
 	}
 };
 
-#ifdef CONFIG_PCI
-static struct ar71xx_pci_irq dir825b1_pci_irqs[] __initdata = {
-        {
-                .slot   = 0,
-                .pin    = 1,
-                .irq    = AR71XX_PCI_IRQ_DEV0,
-        }, {
-                .slot   = 1,
-                .pin    = 1,
-                .irq    = AR71XX_PCI_IRQ_DEV1,
-        }
-};
-
-static int dir825b1_pci_plat_dev_init(struct pci_dev *dev)
-{
-	switch(PCI_SLOT(dev->devfn)) {
-	case 17:
-		dev->dev.platform_data = &dir825b1_wmac0_data;
-		break;
-
-	case 18:
-		dev->dev.platform_data = &dir825b1_wmac1_data;
-		break;
-	}
-
-	return 0;
-}
-
-static void dir825b1_pci_fixup(struct pci_dev *dev)
-{
-	void __iomem *mem;
-	u16 *cal_data;
-	u16 cmd;
-	u32 bar0;
-	u32 val;
-
-	if (ar71xx_mach != AR71XX_MACH_DIR_825_B1)
-		return;
-
-	dir825b1_pci_plat_dev_init(dev);
-	cal_data = dev->dev.platform_data;
-
-	if (*cal_data != 0xa55a) {
-		printk(KERN_ERR "PCI: no calibration data found for %s\n",
-		       pci_name(dev));
-		return;
-	}
-
-	mem = ioremap(AR71XX_PCI_MEM_BASE, 0x10000);
-	if (!mem) {
-		printk(KERN_ERR "PCI: ioremap error for device %s\n",
-		       pci_name(dev));
-		return;
-	}
-
-	printk(KERN_INFO "PCI: fixup device %s\n", pci_name(dev));
-
-	pci_read_config_dword(dev, PCI_BASE_ADDRESS_0, &bar0);
-
-	/* Setup the PCI device to allow access to the internal registers */
-	pci_write_config_dword(dev, PCI_BASE_ADDRESS_0, AR71XX_PCI_MEM_BASE);
-	pci_read_config_word(dev, PCI_COMMAND, &cmd);
-	cmd |= PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY;
-	pci_write_config_word(dev, PCI_COMMAND, cmd);
-
-	/* set pointer to first reg address */
-	cal_data += 3;
-	while (*cal_data != 0xffff) {
-		u32 reg;
-		reg = *cal_data++;
-		val = *cal_data++;
-		val |= (*cal_data++) << 16;
-
-		__raw_writel(val, mem + reg);
-		udelay(100);
-	}
-
-	pci_read_config_dword(dev, PCI_VENDOR_ID, &val);
-	dev->vendor = val & 0xffff;
-	dev->device = (val >> 16) & 0xffff;
-
-	pci_read_config_dword(dev, PCI_CLASS_REVISION, &val);
-	dev->revision = val & 0xff;
-	dev->class = val >> 8; /* upper 3 bytes */
-
-	pci_read_config_word(dev, PCI_COMMAND, &cmd);
-	cmd &= ~(PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY);
-	pci_write_config_word(dev, PCI_COMMAND, cmd);
-
-	pci_write_config_dword(dev, PCI_BASE_ADDRESS_0, bar0);
-
-	iounmap(mem);
-}
-DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_ATHEROS, PCI_ANY_ID,
-			dir825b1_pci_fixup);
-
-static void __init dir825b1_pci_init(void)
-{
-	memcpy(dir825b1_wmac0_data.eeprom_data,
-	       (u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_0),
-	       sizeof(dir825b1_wmac0_data.eeprom_data));
-
-	memcpy(dir825b1_wmac1_data.eeprom_data,
-	       (u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_1),
-	       sizeof(dir825b1_wmac1_data.eeprom_data));
-
-	memcpy(dir825b1_wmac0_mac, (u8 *)KSEG1ADDR(DIR825B1_MAC_LOCATION_0), 6);
-	dir825b1_wmac0_data.macaddr = dir825b1_wmac0_mac;
-	memcpy(dir825b1_wmac1_mac, (u8 *)KSEG1ADDR(DIR825B1_MAC_LOCATION_1), 6);
-	dir825b1_wmac1_data.macaddr = dir825b1_wmac1_mac;
-
-	ar71xx_pci_plat_dev_init = dir825b1_pci_plat_dev_init;
-	ar71xx_pci_init(ARRAY_SIZE(dir825b1_pci_irqs), dir825b1_pci_irqs);
-}
-#else
-static void __init dir825b1_pci_init(void) { }
-#endif /* CONFIG_PCI */
-
 static void __init dir825b1_setup(void)
 {
         u8 mac[6], i;
@@ -299,7 +174,11 @@ static void __init dir825b1_setup(void)
 	ar71xx_add_device_usb();
 
 	platform_device_register(&dir825b1_rtl8366_smi_device);
-	dir825b1_pci_init();
+
+	ap94_pci_init((u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_0),
+		      (u8 *) KSEG1ADDR(DIR825B1_MAC_LOCATION_0),
+		      (u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_1),
+		      (u8 *) KSEG1ADDR(DIR825B1_MAC_LOCATION_1));
 }
 
 MIPS_MACHINE(AR71XX_MACH_DIR_825_B1, "D-Link DIR-825 rev. B1", dir825b1_setup);
