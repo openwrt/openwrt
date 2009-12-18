60eea9284bdbd01721f077a663334b25d8b66a8d
Gabor Juhos <juhosg@openwrt.org>
2009-12-18 12:57:12 +0000

    tl-wr1043nd: Register missing leds.
    This adds the folowing for the TP-Link wr1043nd board
    1. USB and WLAN LEDS
    2. Change QSS LED from red to green
    3. Fix polarity of the System LED
    
    Signed-off-by: Andrew Tarabaras <andrew@advancedtechnologies.com.au>
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18821 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-tl-wr1043nd.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-tl-wr1043nd.c
@@ -21,8 +21,10 @@
 #include "dev-m25p80.h"
 #include "dev-ar913x-wmac.h"
 
+#define TL_WR1043ND_GPIO_LED_USB        1
 #define TL_WR1043ND_GPIO_LED_SYSTEM     2
 #define TL_WR1043ND_GPIO_LED_QSS        5
+#define TL_WR1043ND_GPIO_LED_WLAN       9
 
 #define TL_WR1043ND_GPIO_BTN_RESET      3
 #define TL_WR1043ND_GPIO_BTN_QSS        7
@@ -66,13 +68,21 @@ static struct flash_platform_data tl_wr1043nd_flash_data = {
 
 static struct gpio_led tl_wr1043nd_leds_gpio[] __initdata = {
 	{
+		.name		= "tl-wr1043nd:green:usb",
+		.gpio		= TL_WR1043ND_GPIO_LED_USB,
+		.active_low	= 0,
+	}, {
 		.name		= "tl-wr1043nd:green:system",
 		.gpio		= TL_WR1043ND_GPIO_LED_SYSTEM,
-		.active_low	= 1,
+		.active_low	= 0,
 	}, {
-		.name		= "tl-wr1043nd:red:qss",
+		.name		= "tl-wr1043nd:green:qss",
 		.gpio		= TL_WR1043ND_GPIO_LED_QSS,
 		.active_low	= 1,
+	}, {
+		.name		= "tl-wr1043nd:green:wlan",
+		.gpio		= TL_WR1043ND_GPIO_LED_WLAN,
+		.active_low	= 0,
 	}
 };
 
