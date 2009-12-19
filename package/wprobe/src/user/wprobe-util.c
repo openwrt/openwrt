36422504da9128ee65183cc9a8b4e94dc252a5c2
Felix Fietkau <nbd@openwrt.org>
2009-12-19 22:19:26 +0000

    wprobe-util: always print regular output to stdout instead of stderr
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18854 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/wprobe/src/user/wprobe-util.c
+++ package/wprobe/src/user/wprobe-util.c
@@ -82,7 +82,7 @@ wprobe_dump_data(struct wprobe_iface *dev)
 	bool first = true;
 
 	if (!simple_mode)
-		fprintf(stderr, "\n");
+		fprintf(stdout, "\n");
 	wprobe_request_data(dev, NULL);
 	list_for_each_entry(attr, &dev->global_attr, list) {
 		if (simple_mode) {
@@ -90,7 +90,7 @@ wprobe_dump_data(struct wprobe_iface *dev)
 				fprintf(stdout, "[global]\n");
 			fprintf(stdout, "%s=%s\n", attr->name, wprobe_dump_value(attr));
 		} else {
-			fprintf(stderr, (first ?
+			fprintf(stdout, (first ?
 				"Global:            %s=%s\n" :
 				"                   %s=%s\n"),
 				attr->name,
@@ -105,7 +105,7 @@ wprobe_dump_data(struct wprobe_iface *dev)
 		wprobe_request_data(dev, link->addr);
 		list_for_each_entry(attr, &dev->link_attr, list) {
 			if (first) {
-				fprintf((simple_mode ? stdout : stderr),
+				fprintf(stdout,
 					(simple_mode ? 
 					 "[%02x:%02x:%02x:%02x:%02x:%02x]\n%s=%s\n" :
 					 "%02x:%02x:%02x:%02x:%02x:%02x: %s=%s\n"),
@@ -115,7 +115,7 @@ wprobe_dump_data(struct wprobe_iface *dev)
 					wprobe_dump_value(attr));
 				first = false;
 			} else {
-				fprintf((simple_mode ? stdout : stderr),
+				fprintf(stdout,
 					(simple_mode ? "%s=%s\n" :
 					 "                   %s=%s\n"),
 					attr->name,
@@ -171,11 +171,11 @@ static void show_attributes(struct wprobe_iface *dev)
 	if (simple_mode)
 		return;
 	list_for_each_entry(attr, &dev->global_attr, list) {
-		fprintf(stderr, "Global attribute: '%s' (%s)\n",
+		fprintf(stdout, "Global attribute: '%s' (%s)\n",
 			attr->name, attr_typestr[attr->type]);
 	}
 	list_for_each_entry(attr, &dev->link_attr, list) {
-		fprintf(stderr, "Link attribute: '%s' (%s)\n",
+		fprintf(stdout, "Link attribute: '%s' (%s)\n",
 			attr->name, attr_typestr[attr->type]);
 	}
 }
@@ -196,9 +196,9 @@ static void show_filter_simple(void *arg, const char *group, struct wprobe_filte
 static void show_filter(void *arg, const char *group, struct wprobe_filter_item *items, int n_items)
 {
 	int i;
-	fprintf(stderr, "Filter group: '%s' (tx/rx)\n", group);
+	fprintf(stdout, "Filter group: '%s' (tx/rx)\n", group);
 	for (i = 0; i < n_items; i++) {
-		fprintf(stderr, " - %s (%lld/%lld)\n",
+		fprintf(stdout, " - %s (%lld/%lld)\n",
 			items[i].name, items[i].tx, items[i].rx);
 	}
 }
@@ -415,7 +415,7 @@ int main(int argc, char **argv)
 	if (!dev || (list_empty(&dev->global_attr) &&
 		list_empty(&dev->link_attr))) {
 		if (err)
-			fprintf(stderr, "%s\n", err);
+			fprintf(stdout, "%s\n", err);
 		else
 			fprintf(stderr, "Interface '%s' not found\n", ifname);
 		return 1;
