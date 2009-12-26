64e26215eee9aca32f7c1b409e7acb974f25e2f9
Gabor Juhos <juhosg@openwrt.org>
2009-12-26 14:19:05 +0000

    ar71xx: use the generic m25p80 registration code for the MZK-W04NU
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@18938 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-mzk-w04nu.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-mzk-w04nu.c
@@ -11,8 +11,6 @@
 #include <linux/platform_device.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
-#include <linux/spi/spi.h>
-#include <linux/spi/flash.h>
 
 #include <asm/mips_machine.h>
 #include <asm/mach-ar71xx/ar71xx.h>
@@ -21,6 +19,7 @@
 #include "dev-ar913x-wmac.h"
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
+#include "dev-m25p80.h"
 
 #define MZK_W04NU_GPIO_LED_USB		0
 #define MZK_W04NU_GPIO_LED_STATUS	1
@@ -72,16 +71,6 @@ static struct flash_platform_data mzk_w04nu_flash_data = {
         .parts          = mzk_w04nu_partitions,
         .nr_parts       = ARRAY_SIZE(mzk_w04nu_partitions),
 #endif
-};
-
-static struct spi_board_info mzk_w04nu_spi_info[] = {
-	{
-		.bus_num	= 0,
-		.chip_select	= 0,
-		.max_speed_hz	= 25000000,
-		.modalias	= "m25p80",
-		.platform_data  = &mzk_w04nu_flash_data,
-	}
 };
 
 static struct gpio_led mzk_w04nu_leds_gpio[] __initdata = {
@@ -157,8 +146,7 @@ static void __init mzk_w04nu_setup(void)
 	ar71xx_add_device_eth(0);
 	ar71xx_add_device_eth(1);
 
-	ar71xx_add_device_spi(NULL, mzk_w04nu_spi_info,
-					ARRAY_SIZE(mzk_w04nu_spi_info));
+	ar71xx_add_device_m25p80(&mzk_w04nu_flash_data);
 
 	ar71xx_add_device_leds_gpio(-1, ARRAY_SIZE(mzk_w04nu_leds_gpio),
 					mzk_w04nu_leds_gpio);
