e16f20b51813c4ef7d2519235d45fa37bd09e2ac
Gabor Juhos <juhosg@openwrt.org>
2010-01-02 15:52:30 +0000

    adm5120: use enum for MACH_ADM5120_ definitions
    SVN-Revision: 19004

--- target/linux/adm5120/files/arch/mips/include/asm/mach-adm5120/adm5120_info.h
+++ target/linux/adm5120/files/arch/mips/include/asm/mach-adm5120/adm5120_info.h
@@ -37,52 +37,54 @@ extern unsigned int adm5120_package;
 
 extern unsigned long adm5120_memsize;
 
-#define MACH_ADM5120_GENERIC	0	/* Generic board */
-#define MACH_ADM5120_WP54G_WRT	1	/* Compex WP54G-WRT */
-#define MACH_ADM5120_WP54	2	/* Compex WP54G/WP54AG/WPP54G/WPP54AG */
-#define MACH_ADM5120_NP28G	3	/* Compex NP28G */
-#define MACH_ADM5120_NP28GHS	4	/* Compex NP28G HotSpot */
-#define MACH_ADM5120_NP27G	5	/* Compex NP27G */
-#define MACH_ADM5120_WP54Gv1C	6	/* Compex WP54G version 1C */
-#define MACH_ADM5120_RB_11X	7	/* Mikrotik RouterBOARD 111/112 */
-#define MACH_ADM5120_RB_133	8	/* Mikrotik RouterBOARD 133 */
-#define MACH_ADM5120_RB_133C	9	/* Mikrotik RouterBOARD 133c */
-#define MACH_ADM5120_RB_150	10	/* Mikrotik RouterBOARD 150 */
-#define MACH_ADM5120_RB_153	11	/* Mikrotik RouterBOARD 153 */
-#define MACH_ADM5120_RB_192	12	/* Mikrotik RouterBOARD 192 */
-#define MACH_ADM5120_HS100	13	/* ZyXEL HomeSafe 100/100W */
-#define MACH_ADM5120_P334U	14	/* ZyXEL Prestige 334U */
-#define MACH_ADM5120_P334W	15	/* ZyXEL Prestige 334W */
-#define MACH_ADM5120_P334WH	16	/* ZyXEL Prestige 334WH */
-#define MACH_ADM5120_P334WHD	17	/* ZyXEL Prestige 334WHD */
-#define MACH_ADM5120_P334WT	18	/* ZyXEL Prestige 334WT */
-#define MACH_ADM5120_P335	19	/* ZyXEL Prestige 335/335WT */
-#define MACH_ADM5120_P335PLUS	20	/* ZyXEL Prestige 335Plus */
-#define MACH_ADM5120_P335U	21	/* ZyXEL Prestige 335U */
-#define MACH_ADM5120_ES2108	22	/* ZyXEL Ethernet Switch 2108 */
-#define MACH_ADM5120_ES2108F	23	/* ZyXEL Ethernet Switch 2108-F */
-#define MACH_ADM5120_ES2108G	24	/* ZyXEL Ethernet Switch 2108-G */
-#define MACH_ADM5120_ES2108LC	25	/* ZyXEL Ethernet Switch 2108-LC */
-#define MACH_ADM5120_ES2108PWR	26	/* ZyXEL Ethernet Switch 2108-PWR */
-#define MACH_ADM5120_ES2024A	27	/* ZyXEL Ethernet Switch 2024A */
-#define MACH_ADM5120_ES2024PWR	28	/* ZyXEL Ethernet Switch 2024PWR */
-#define MACH_ADM5120_CAS630	29	/* Cellvision CAS-630/630W */
-#define MACH_ADM5120_CAS670	30	/* Cellvision CAS-670/670W */
-#define MACH_ADM5120_CAS700	31	/* Cellvision CAS-700/700W */
-#define MACH_ADM5120_CAS771	32	/* Cellvision CAS-771/771W */
-#define MACH_ADM5120_CAS790	33	/* Cellvision CAS-790 */
-#define MACH_ADM5120_CAS861	34	/* Cellvision CAS-861/861W */
-#define MACH_ADM5120_NFS101U	35	/* Cellvision NFS-101U/101WU */
-#define MACH_ADM5120_NFS202U	36	/* Cellvision NFS-202U/202WU */
-#define MACH_ADM5120_EASY5120PATA 37	/* Infineon EASY 5120P-ATA */
-#define MACH_ADM5120_EASY5120RT 38	/* Infineon EASY 5120-RT */
-#define MACH_ADM5120_EASY5120WVOIP 39	/* Infineon EASY 5120-WVoIP */
-#define MACH_ADM5120_EASY83000	40	/* Infineon EASY-83000 */
-#define MACH_ADM5120_BR6104K	41	/* Edimax BR-6104K */
-#define MACH_ADM5120_BR6104KP	42	/* Edimax BR-6104KP */
-#define MACH_ADM5120_BR61X4WG	43	/* Edimax BR-6104Wg/BR-6114WG */
-#define MACH_ADM5120_PMUGW	44	/* Motorola Powerline MU Gateway */
-#define MACH_ADM5120_5GXI	45	/* OSBRiDGE 5GXi/5XLi */
+enum {
+	MACH_ADM5120_GENERIC = 0,	/* Generic board */
+	MACH_ADM5120_5GXI,		/* OSBRiDGE 5GXi/5XLi */
+	MACH_ADM5120_BR6104K,		/* Edimax BR-6104K */
+	MACH_ADM5120_BR6104KP,		/* Edimax BR-6104KP */
+	MACH_ADM5120_BR61X4WG,		/* Edimax BR-6104Wg/BR-6114WG */
+	MACH_ADM5120_CAS630,		/* Cellvision CAS-630/630W */
+	MACH_ADM5120_CAS670,		/* Cellvision CAS-670/670W */
+	MACH_ADM5120_CAS700,		/* Cellvision CAS-700/700W */
+	MACH_ADM5120_CAS771,		/* Cellvision CAS-771/771W */
+	MACH_ADM5120_CAS790,		/* Cellvision CAS-790 */
+	MACH_ADM5120_CAS861,		/* Cellvision CAS-861/861W */
+	MACH_ADM5120_EASY5120PATA,	/* Infineon EASY 5120P-ATA */
+	MACH_ADM5120_EASY5120RT,	/* Infineon EASY 5120-RT */
+	MACH_ADM5120_EASY5120WVOIP,	/* Infineon EASY 5120-WVoIP */
+	MACH_ADM5120_EASY83000,		/* Infineon EASY-83000 */
+	MACH_ADM5120_ES2108,		/* ZyXEL Ethernet Switch 2108 */
+	MACH_ADM5120_ES2108F,		/* ZyXEL Ethernet Switch 2108-F */
+	MACH_ADM5120_ES2108G,		/* ZyXEL Ethernet Switch 2108-G */
+	MACH_ADM5120_ES2108LC,		/* ZyXEL Ethernet Switch 2108-LC */
+	MACH_ADM5120_ES2108PWR,		/* ZyXEL Ethernet Switch 2108-PWR */
+	MACH_ADM5120_ES2024A,		/* ZyXEL Ethernet Switch 2024A */
+	MACH_ADM5120_ES2024PWR,		/* ZyXEL Ethernet Switch 2024PWR */
+	MACH_ADM5120_HS100,		/* ZyXEL HomeSafe 100/100W */
+	MACH_ADM5120_NFS101U,		/* Cellvision NFS-101U/101WU */
+	MACH_ADM5120_NFS202U,		/* Cellvision NFS-202U/202WU */
+	MACH_ADM5120_NP28G,		/* Compex NP28G */
+	MACH_ADM5120_NP28GHS,		/* Compex NP28G HotSpot */
+	MACH_ADM5120_NP27G,		/* Compex NP27G */
+	MACH_ADM5120_RB_11X,		/* Mikrotik RouterBOARD 111/112 */
+	MACH_ADM5120_RB_133,		/* Mikrotik RouterBOARD 133 */
+	MACH_ADM5120_RB_133C,		/* Mikrotik RouterBOARD 133c */
+	MACH_ADM5120_RB_150,		/* Mikrotik RouterBOARD 150 */
+	MACH_ADM5120_RB_153,		/* Mikrotik RouterBOARD 153 */
+	MACH_ADM5120_RB_192,		/* Mikrotik RouterBOARD 192 */
+	MACH_ADM5120_P334U,		/* ZyXEL Prestige 334U */
+	MACH_ADM5120_P334W,		/* ZyXEL Prestige 334W */
+	MACH_ADM5120_P334WH,		/* ZyXEL Prestige 334WH */
+	MACH_ADM5120_P334WHD,		/* ZyXEL Prestige 334WHD */
+	MACH_ADM5120_P334WT,		/* ZyXEL Prestige 334WT */
+	MACH_ADM5120_P335,		/* ZyXEL Prestige 335/335WT */
+	MACH_ADM5120_P335PLUS,		/* ZyXEL Prestige 335Plus */
+	MACH_ADM5120_P335U,		/* ZyXEL Prestige 335U */
+	MACH_ADM5120_PMUGW,		/* Motorola Powerline MU Gateway */
+	MACH_ADM5120_WP54,		/* Compex WP54G/WP54AG/WPP54G/WPP54AG */
+	MACH_ADM5120_WP54G_WRT,		/* Compex WP54G-WRT */
+	MACH_ADM5120_WP54Gv1C,		/* Compex WP54G version 1C */
+};
 
 /*
  * TODO:remove adm5120_eth* variables when the switch driver will be
