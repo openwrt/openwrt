ba7bfddaaed9bc067199b1e3208b1c634ebc7b5f
Felix Fietkau <nbd@openwrt.org>
2010-01-20 02:26:05 +0000

    mac80211: restructure /lib/wifi/mac80211.sh use the new multi-bss single instance hostapd mode move mac80211 specific bits out of /lib/wifi/hostapd.sh add a new option 'htmode' for switching between HT20 and HT40+,HT40-
    SVN-Revision: 19235

--- package/hostapd/files/hostapd.sh
+++ package/hostapd/files/hostapd.sh
@@ -1,7 +1,10 @@
-hostapd_setup_vif() {
-	local vif="$1"
-	local driver="$2"
-	local hostapd_cfg=
+hostapd_set_bss_options() {
+	local var="$1"
+	local vif="$2"
+	local enc wpa_group_rekey
+
+	config_get enc "$vif" encryption
+	config_get wpa_group_rekey "$vif" wpa_group_rekey
 
 	# Examples:
 	# psk-mixed/tkip 	=> WPA1+2 PSK, TKIP
@@ -14,7 +17,7 @@ hostapd_setup_vif() {
 
 	# crypto defaults: WPA2 vs WPA1
 	case "$enc" in
-		wpa2*|WPA2*|*PSK2*|*psk2*)
+		wpa2*|*psk2*)
 			wpa=2
 			crypto="CCMP"
 		;;
@@ -30,137 +33,82 @@ hostapd_setup_vif() {
 
 	# explicit override for crypto setting
 	case "$enc" in
-		*tkip+aes|*TKIP+AES|*tkip+ccmp|*TKIP+CCMP) crypto="CCMP TKIP";;
-		*tkip|*TKIP) crypto="TKIP";;
-		*aes|*AES|*ccmp|*CCMP) crypto="CCMP";;
+		*tkip+aes|*tkip+ccmp) crypto="CCMP TKIP";;
+		*aes|*ccmp) crypto="CCMP";;
+		*tkip) crypto="TKIP";;
 	esac
 
 	# use crypto/auth settings for building the hostapd config
 	case "$enc" in
-		*psk*|*PSK*)
+		*psk*)
 			config_get psk "$vif" key
 			if [ ${#psk} -eq 64 ]; then
-				append hostapd_cfg "wpa_psk=$psk" "$N"
+				append "$var" "wpa_psk=$psk" "$N"
 			else
-				append hostapd_cfg "wpa_passphrase=$psk" "$N"
+				append "$var" "wpa_passphrase=$psk" "$N"
 			fi
 		;;
-		*wpa*|*WPA*)
+		*wpa*)
 			# required fields? formats?
 			# hostapd is particular, maybe a default configuration for failures
 			config_get server "$vif" server
-			append hostapd_cfg "auth_server_addr=$server" "$N"
+			append "$var" "auth_server_addr=$server" "$N"
 			config_get port "$vif" port
 			port=${port:-1812}
-			append hostapd_cfg "auth_server_port=$port" "$N"
+			append "$var" "auth_server_port=$port" "$N"
 			config_get secret "$vif" key
-			append hostapd_cfg "auth_server_shared_secret=$secret" "$N"
+			append "$var" "auth_server_shared_secret=$secret" "$N"
 			config_get nasid "$vif" nasid
-			append hostapd_cfg "nas_identifier=$nasid" "$N"
-			append hostapd_cfg "eapol_key_index_workaround=1" "$N"
-			append hostapd_cfg "radius_acct_interim_interval=300" "$N"
-			append hostapd_cfg "ieee8021x=1" "$N"
-			append hostapd_cfg "auth_algs=1" "$N"
-			append hostapd_cfg "wpa_key_mgmt=WPA-EAP" "$N"
-			append hostapd_cfg "wpa_group_rekey=300" "$N"
-			append hostapd_cfg "wpa_gmk_rekey=640" "$N"
+			append "$var" "nas_identifier=$nasid" "$N"
+			append "$var" "eapol_key_index_workaround=1" "$N"
+			append "$var" "radius_acct_interim_interval=300" "$N"
+			append "$var" "ieee8021x=1" "$N"
+			append "$var" "auth_algs=1" "$N"
+			append "$var" "wpa_key_mgmt=WPA-EAP" "$N"
+			append "$var" "wpa_group_rekey=300" "$N"
+			append "$var" "wpa_gmk_rekey=640" "$N"
 		;;
 		*)
 			wpa=0
 			crypto=
 		;;
 	esac
-	config_get ifname "$vif" ifname
-	config_get bridge "$vif" bridge
+	append "$var" "wpa=$wpa" "$N"
+	[ -n "$crypto" ] && append "$var" "wpa_pairwise=$crypto" "$N"
+	[ -n "$wpa_group_rekey" ] && append "$var" "wpa_group_rekey=$wpa_group_rekey" "$N"
+
 	config_get ssid "$vif" ssid
+	config_get bridge "$vif" bridge
+	config_get ieee80211d "$vif" ieee80211d
+
+	append "$var" "ssid=$ssid" "$N"
+	[ -n "$bridge" ] && append "$var" "bridge=$bridge" "$N"
+	[ -n "$ieee80211d" ] && append "$var" "ieee80211d=$ieee80211d" "$N"
+}
+
+hostapd_setup_vif() {
+	local vif="$1"
+	local driver="$2"
+	hostapd_cfg=
+
+	hostapd_set_bss_options hostapd_cfg "$vif"
+	config_get ifname "$vif" ifname
 	config_get device "$vif" device
 	config_get channel "$device" channel
 	config_get hwmode "$device" hwmode
-	config_get wpa_group_rekey "$vif" wpa_group_rekey
-	config_get ieee80211d "$vif" ieee80211d
-	config_get_bool hidden "$vif" hidden 0
-	config_get_bool wds "$vif" wds 0
-	[ "$wds" -gt 0 -a "$driver" = "nl80211" ] && wds="wds_sta=1" || wds=""
 	case "$hwmode" in
 		bg) hwmode=g;;
 	esac
-	config_get country "$device" country
 	[ "$channel" = auto ] && channel=
 	[ -n "$channel" -a -z "$hwmode" ] && wifi_fixup_hwmode "$device"
-	[ -n "$hwmode" ] && {
-		config_get hwmode_11n "$device" hwmode_11n
-		[ -n "$hwmode_11n" ] && {
-			hwmode="$hwmode_11n"
-			config_get ht_capab "$device" ht_capab
-			[ -n "$ht_capab" -a -n "${ht_capab%%\[*}" ] && {
-				ht_capab=`echo "[$ht_capab]" | sed -e 's, ,][,g'`
-			}
-		}
-	}
 	cat > /var/run/hostapd-$ifname.conf <<EOF
 ctrl_interface=/var/run/hostapd-$ifname
 driver=$driver
 interface=$ifname
 ${hwmode:+hw_mode=$hwmode}
 ${channel:+channel=$channel}
-${bridge:+bridge=$bridge}
-ssid=$ssid
-wpa=$wpa
-${crypto:+wpa_pairwise=$crypto}
-${country:+country_code=$country}
-${hwmode_11n:+ieee80211n=1}
-${ht_capab:+ht_capab=$ht_capab}
-${wpa_group_rekey:+wpa_group_rekey=$wpa_group_rekey}
-${ieee80211d:+ieee80211d=$ieee80211d}
-$wds
 $hostapd_cfg
 EOF
-	case "$driver" in
-		madwifi)
-		;;
-		*) 
-			cat >> /var/run/hostapd-$ifname.conf <<EOF
-ignore_broadcast_ssid=$hidden
-wmm_enabled=1
-wmm_ac_bk_cwmin=4
-wmm_ac_bk_cwmax=10
-wmm_ac_bk_aifs=7
-wmm_ac_bk_txop_limit=0
-wmm_ac_bk_acm=0
-wmm_ac_be_aifs=3
-wmm_ac_be_cwmin=4
-wmm_ac_be_cwmax=10
-wmm_ac_be_txop_limit=0
-wmm_ac_be_acm=0
-wmm_ac_vi_aifs=2
-wmm_ac_vi_cwmin=3
-wmm_ac_vi_cwmax=4
-wmm_ac_vi_txop_limit=94
-wmm_ac_vi_acm=0
-wmm_ac_vo_aifs=2
-wmm_ac_vo_cwmin=2
-wmm_ac_vo_cwmax=3
-wmm_ac_vo_txop_limit=47
-wmm_ac_vo_acm=0
-tx_queue_data3_aifs=7
-tx_queue_data3_cwmin=15
-tx_queue_data3_cwmax=1023
-tx_queue_data3_burst=0
-tx_queue_data2_aifs=3
-tx_queue_data2_cwmin=15
-tx_queue_data2_cwmax=63
-tx_queue_data2_burst=0
-tx_queue_data1_aifs=1
-tx_queue_data1_cwmin=7
-tx_queue_data1_cwmax=15
-tx_queue_data1_burst=3.0
-tx_queue_data0_aifs=1
-tx_queue_data0_cwmin=3
-tx_queue_data0_cwmax=7
-tx_queue_data0_burst=1.5
-EOF
-		;;
-	esac
 	hostapd -P /var/run/wifi-$ifname.pid -B /var/run/hostapd-$ifname.conf
 }
 
