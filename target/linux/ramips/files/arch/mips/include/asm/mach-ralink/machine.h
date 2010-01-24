6351d88e29a6e0ffc96d11d33d22eafdbfd1a41b
Jo-Philipp Wich <jow@openwrt.org>
2010-01-24 23:31:49 +0000

    add initial support for the WZR-AGL300NH - patch by Joonas Lahtinen <joonas.lahtinen@gmail.com>
    SVN-Revision: 19321

--- target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
@@ -1,6 +1,7 @@
 /*
  * Ralink machine types
  *
+ * Copyright (C) 2010 Joonas Lahtinen <joonas.lahtinen@gmail.com>
  * Copyright (C) 2009 Gabor Juhos <juhosg@openwrt.org>
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -12,6 +13,7 @@ enum ramips_mach_type {
 	RAMIPS_MACH_GENERIC,
 	/* RT2880 based machines */
 	RAMIPS_MACH_RT_N15,		/* Asus RT-N15 */
+	RAMIPS_MACH_WZR_AGL300NH,	/* Buffalo WZR-AGL300NH */
 
 	/* RT3050 based machines */
 	RAMIPS_MACH_DIR_300_REVB,	/* D-Link DIR-300 rev B */
---
+++ target/linux/ramips/files/arch/mips/ralink/rt288x/mach-wzr-agl300nh.c
@@ -0,0 +1,105 @@
+/*
+ *  Buffalo WZR-AGL300NH board support
+ *
+ *  Copyright (C) 2010 Joonas Lahtinen <joonas.lahtinen@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
+#include <linux/mtd/physmap.h>
+#include <linux/leds.h>
+
+#include <asm/mips_machine.h>
+#include <asm/mach-ralink/machine.h>
+#include <asm/mach-ralink/dev_gpio_leds.h>
+#include <asm/mach-ralink/rt288x.h>
+#include <asm/mach-ralink/rt288x_regs.h>
+
+#include "devices.h"
+
+#if 0
+#define WZR_AGL300NH_GPIO_LED_POWER	XX
+#define WZR_AGL300NH_GPIO_BUTTON_AOSS	XX
+#define WZR_AGL300NH_GPIO_BUTTON_RESET	XX
+#endif // 0
+
+#ifdef CONFIG_MTD_PARTITIONS
+
+/*
+From stock firmware:
+mtd0: 00030000 00010000 "uboot"
+mtd1: 00010000 00010000 "uboot_environ"
+mtd2: 00010000 00010000 "factory_default"
+mtd3: 000b0000 00010000 "linux"
+mtd4: 002f0000 00010000 "rootfs"
+mtd5: 00010000 00010000 "user_property"
+*/
+
+static struct mtd_partition wzr_agl300nh_partitions[] = {
+	{
+		.name	= "uboot",
+		.offset	= 0,
+		.size	= 0x030000,
+		.mask_flags = MTD_WRITEABLE,
+	}, {
+		.name	= "uboot_environ",
+		.offset	= 0x030000,
+		.size	= 0x010000,
+		.mask_flags = MTD_WRITEABLE,
+	}, {
+		.name	= "factory_default",
+		.offset	= 0x040000,
+		.size	= 0x010000,
+		.mask_flags = MTD_WRITEABLE,
+	}, {
+		.name	= "linux",
+		.offset	= 0x050000,
+		.size	= 0x0b0000,
+	}, {
+		.name	= "rootfs",
+		.offset	= 0x100000,
+		.size	= 0x2f0000,
+	}, {
+		.name	= "user_property",
+		.offset	= 0x3f0000,
+		.size	= 0x010000,
+	}
+};
+#endif /* CONFIG_MTD_PARTITIONS */
+
+static struct physmap_flash_data wzr_agl300nh_flash_data = {
+#ifdef CONFIG_MTD_PARTITIONS
+	.nr_parts	= ARRAY_SIZE(wzr_agl300nh_partitions),
+	.parts		= wzr_agl300nh_partitions,
+#endif
+};
+
+#if 0
+static struct gpio_led wzr_agl300nh_leds_gpio[] __initdata = {
+	{
+		.name		= "wzr-agl300nh:green:power",
+		.gpio		= WZR_AGL300NH_GPIO_LED_POWER,
+		.active_low	= ??,
+	}
+};
+#endif
+
+static void __init wzr_agl300nh_init(void)
+{
+	rt288x_gpio_init(RT2880_GPIO_MODE_UART0);
+
+	rt288x_register_flash(0, &wzr_agl300nh_flash_data);
+
+/*
+	ramips_register_gpio_leds(-1, ARRAY_SIZE(wzr_agl300nh_leds_gpio),
+				  wzr_agl300nh_leds_gpio);
+*/
+}
+
+MIPS_MACHINE(RAMIPS_MACH_WZR_AGL300NH, "Buffalo WZR-AGL300NH", wzr_agl300nh_init);
