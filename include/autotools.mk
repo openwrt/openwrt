e201d1990d7d603b04ae41e9afbc90691a14ed1b
Lars-Peter Clausen <lars@metafoo.de>
2010-01-30 14:51:22 +0000

    autotools.mk: Add libstdc++ quirk to libtool_fixup_libdir. Fixes build of packages which use libtool and link against packages which link against libstdc++
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@19399 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/autotools.mk
+++ include/autotools.mk
@@ -22,9 +22,11 @@ update_libtool_ucxx=$(call replace,libtool,$(STAGING_DIR)/host/bin,$(CONFIGURE_P
 # prevent libtool from linking against host development libraries
 define libtool_fixup_libdir
 	find $(1) -name '*.la' | $(XARGS) \
-		$(SED) "s,\(^libdir='\| \|-L\|^dependency_libs='\)/usr/lib,\1$(STAGING_DIR)/usr/lib,g"
+		$(SED) "s,\(^libdir='\| \|-L\|^dependency_libs='\)/usr/lib,\1$(STAGING_DIR)/usr/lib,g" \
+		    -e "s,$(STAGING_DIR)/usr/lib/\(libstdc++\|libsupc++\).la,$(TOOLCHAIN_DIR)/usr/lib/\1.la,g"
 	find $(2) -name '*.la' | $(XARGS) \
-		$(SED) "s,\(^libdir='\| \|-L\|^dependency_libs='\)/usr/lib,\1$(STAGING_DIR)/usr/lib,g"
+		$(SED) "s,\(^libdir='\| \|-L\|^dependency_libs='\)/usr/lib,\1$(STAGING_DIR)/usr/lib,g" \
+		    -e "s,$(STAGING_DIR)/usr/lib/\(libstdc++\|libsupc++\).la,$(TOOLCHAIN_DIR)/usr/lib/\1.la,g"
 endef
 
 define remove_version_check
