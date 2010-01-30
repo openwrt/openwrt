30a397fd5e056d5c61c1b71f646d5097a521dd85
Gabor Juhos <juhosg@openwrt.org>
2010-01-30 15:25:44 +0000

    ramips: register gpio buttons on the DIR-300 revB board
    SVN-Revision: 19410

--- target/linux/ramips/files/arch/mips/ralink/rt305x/mach-dir-300-revb.c
+++ target/linux/ramips/files/arch/mips/ralink/rt305x/mach-dir-300-revb.c
@@ -1,7 +1,7 @@
 /*
  *  D-Link DIR-300 rev B board support
  *
- *  Copyright (C) 2009 Gabor Juhos <juhosg@openwrt.org>
+ *  Copyright (C) 2009-2010 Gabor Juhos <juhosg@openwrt.org>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
@@ -15,6 +15,7 @@
 #include <linux/mtd/physmap.h>
 
 #include <asm/mach-ralink/machine.h>
+#include <asm/mach-ralink/dev-gpio-buttons.h>
 #include <asm/mach-ralink/dev-gpio-leds.h>
 #include <asm/mach-ralink/rt305x.h>
 #include <asm/mach-ralink/rt305x_regs.h>
@@ -27,6 +28,8 @@
 
 #define DIR_300B_GPIO_BUTTON_WPS	0	/* active low */
 #define DIR_300B_GPIO_BUTTON_RESET	10	/* active low */
+
+#define DIR_300B_BUTTONS_POLL_INTERVAL	20
 
 #ifdef CONFIG_MTD_PARTITIONS
 static struct mtd_partition dir_300b_partitions[] = {
@@ -84,6 +87,24 @@ static struct gpio_led dir_300b_leds_gpio[] __initdata = {
 	}
 };
 
+static struct gpio_button dir_300b_gpio_buttons[] __initdata = {
+	{
+		.desc		= "reset",
+		.type		= EV_KEY,
+		.code		= BTN_0,
+		.threshold	= 3,
+		.gpio		= DIR_300B_GPIO_BUTTON_RESET,
+		.active_low	= 1,
+	}, {
+		.desc		= "wps",
+		.type		= EV_KEY,
+		.code		= BTN_1,
+		.threshold	= 3,
+		.gpio		= DIR_300B_GPIO_BUTTON_WPS,
+		.active_low	= 1,
+	}
+};
+
 static void __init dir_300b_init(void)
 {
 	rt305x_gpio_init(RT305X_GPIO_MODE_GPIO << RT305X_GPIO_MODE_UART0_SHIFT);
@@ -92,6 +113,9 @@ static void __init dir_300b_init(void)
 	rt305x_register_ethernet();
 	ramips_register_gpio_leds(-1, ARRAY_SIZE(dir_300b_leds_gpio),
 				  dir_300b_leds_gpio);
+	ramips_register_gpio_buttons(-1, DIR_300B_BUTTONS_POLL_INTERVAL,
+				     ARRAY_SIZE(dir_300b_gpio_buttons),
+				     dir_300b_gpio_buttons);
 }
 
 MIPS_MACHINE(RAMIPS_MACH_DIR_300_REVB, "DIR-300-revB", "D-Link DIR-300 revB",
