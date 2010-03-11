2b84aa9043fd0bc319d15186a2c09d16e2a0e488
Lars-Peter Clausen <lars@metafoo.de>
2010-03-11 02:00:59 +0000

    [xburst] Add kernel sound packages
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20132 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/xburst/n516/target.mk
+++ target/linux/xburst/n516/target.mk
@@ -1,2 +1,8 @@
 BOARDNAME:=Hanvon N516 e-book reader
-#DEFAULT_PACKAGES += uboot-xburst-n516
+DEFAULT_PACKAGES += \
+	kmod-sound-core \
+	kmod-sound-soc-core \
+	kmod-sound-soc-jz4740 \
+	kmod-sound-soc-jzcodec \
+	kmod-sound-soc-n516 \
+
--- target/linux/xburst/n526/target.mk
+++ target/linux/xburst/n526/target.mk
@@ -1,2 +1,7 @@
 BOARDNAME:=Hanvon N526 e-book reader
-DEFAULT_PACKAGES += uboot-xburst-n526
+DEFAULT_PACKAGES += \
+	kmod-sound-core \
+	kmod-sound-soc-core \
+	kmod-sound-soc-jz4740 \
+	kmod-sound-soc-jzcodec \
+	kmod-sound-soc-n516 \
