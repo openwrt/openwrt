256f938e5a93c219ee8301bf6da615a37b50bfc6
Lars-Peter Clausen <lars@metafoo.de>
2010-03-11 02:00:59 +0000

    Add kernel sound packages
    SVN-Revision: 20132

--- target/linux/xburst/modules.mk
+++ target/linux/xburst/modules.mk
@@ -1,4 +1,49 @@
-ifeq ($(CONFIG_TARGET_xburst),y)
-	SOUNDCORE_FILES = 
-	SOUNDCORE_LOAD = 
-endif
+
+
+SOUND_MENU:=Sound Support
+
+define KernelPackage/sound-soc-jz4740
+  SUBMENU:=$(SOUND_MENU)
+  DEPENDS:=kmod-sound-soc-core @TARGET_xburst
+  TITLE:=JZ4740 SoC sound support
+  KCONFIG:=CONFIG_SND_JZ4740_SOC CONFIG_SND_JZ4740_SOC_I2S @TARGET_xburst
+  FILES:=$(LINUX_DIR)/sound/soc/jz4740/snd-soc-jz4740.$(LINUX_KMOD_SUFFIX) \
+  $(LINUX_DIR)/sound/soc/jz4740/snd-soc-jz4740-i2s.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,40,snd-soc-jz4740 snd-soc-jz4740-pcm)
+endef
+
+define KernelPackage/sound-soc-jzcodec
+  SUBMENU:=$(SOUND_MENU)
+  DEPENDS:=kmod-sound-soc-core @TARGET_xburst
+  TITLE:=JZ4740 SoC internal codec support
+  KCONFIG:=CONFIG_SND_SOC_JZCODEC
+  FILES:=$(LINUX_DIR)/sound/soc/codecs/snd-soc-jzcodec.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,40,snd-soc-jzcodec)
+endef
+
+define KernelPackage/sound-soc-xburst/default
+  SUBMENU:=$(SOUND_MENU)
+  DEPENDS:=kmod-sound-soc-jz4740 kmod-sound-soc-jzcodec @TARGET_xburst_$(if $(4),$(4),$(3))
+  TITLE:=$(1) sound support
+  KCONFIG:=CONFIG_SND_JZ4740_SOC_$(2)
+  FILES:=$(LINUX_DIR)/sound/soc/jz4740/snd-soc-$(3).$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,45,snd-soc-$(3))
+endef
+
+define KernelPackage/sound-soc-qi_lb60
+$(call KernelPackage/sound-soc-xburst/default,QI NanoNote,QI_LB60,qi-lb60,qi_lb60)
+endef
+
+define KernelPackage/sound-soc-n516
+$(call KernelPackage/sound-soc-xburst/default,Hanvon N516,N516,n516)
+endef
+
+define KernelPackage/sound-soc-n526
+$(call KernelPackage/sound-soc-xburst/default,Hanvon N526,N526,n526)
+endef
+
+$(eval $(call KernelPackage,sound-soc-jz4740))
+$(eval $(call KernelPackage,sound-soc-jzcodec))
+$(eval $(call KernelPackage,sound-soc-qi_lb60))
+$(eval $(call KernelPackage,sound-soc-n516))
+$(eval $(call KernelPackage,sound-soc-n526))
--- target/linux/xburst/qi_lb60/target.mk
+++ target/linux/xburst/qi_lb60/target.mk
@@ -1,2 +1,8 @@
 BOARDNAME:=QI Ben Nanonote (qi_lb60)
-#DEFAULT_PACKAGES += uboot-xburst-qi_lb60
+DEFAULT_PACKAGES += \
+	kmod-sound-core \
+	kmod-sound-soc-core \
+	kmod-sound-soc-jz4740 \
+	kmod-sound-soc-jzcodec \
+	kmod-sound-soc-qi_lb60 \
+
