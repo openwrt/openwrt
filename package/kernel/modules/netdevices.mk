8356fc0b8e69beed2c95b3652bb1db59dba43a8f
Hamish Guthrie <hcg@openwrt.org>
2010-03-26 14:11:01 +0000

    Create kernel module for PS3 networking
    SVN-Revision: 20447

--- package/kernel/modules/netdevices.mk
+++ package/kernel/modules/netdevices.mk
@@ -375,3 +375,19 @@ define KernelPackage/macvlan/description
 endef
 
 $(eval $(call KernelPackage,macvlan))
+
+define KernelPackage/ps3lan
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=PS3-Lan support
+  DEPENDS:=@LINUX_2_6
+  KCONFIG:=CONFIG_GELIC_NET \
+           CONFIG_GELIC_WIRELESS=y
+  FILES:=$(LINUX_DIR)/drivers/net/ps3_gelic.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,50,ps3_gelic)
+endef
+
+define KernelPackage/ps3lan/description
+  Kernel module for PS3 Networking. Includes Gb Ethernet and WLAN
+endef
+
+$(eval $(call KernelPackage,ps3lan))
