9da0228c9e8067c2e199bb2ce8c8f1043a92f067
Gabor Juhos <juhosg@openwrt.org>
2010-03-27 17:38:30 +0000

    package/mtd: reuse existing crc32 stuff in the trx code
     * patch by Bernhard Loos
     * note: fixed compiler error on brcm47xx
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20523 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/mtd/src/crc32.h
+++ package/mtd/src/crc32.h
@@ -7,7 +7,7 @@ extern const uint32_t crc32_table[256];
 
 /* Return a 32-bit CRC of the contents of the buffer. */
 
-	static inline uint32_t
+static inline uint32_t
 crc32(uint32_t val, const void *ss, int len)
 {
 	const unsigned char *s = ss;
@@ -15,5 +15,12 @@ crc32(uint32_t val, const void *ss, int len)
 		val = crc32_table[(val ^ *s++) & 0xff] ^ (val >> 8);
 	return val;
 }
+
+static inline unsigned int crc32buf(char *buf, size_t len)
+{
+	return crc32(0xFFFFFFFF, buf, len);
+}
+
+
 
 #endif
