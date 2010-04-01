508ed126fce54e34107dc4335fe1639d5f57a098
Imre Kaloz <kaloz@openwrt.org>
2010-04-01 12:05:27 +0000

    [x86]: add a subtarget and drivers for the Intel Tolapai SoC
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20648 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/x86/ep80579/target.mk
@@ -0,0 +1,9 @@
+BOARDNAME:=Intel EP80579
+DEFAULT_PACKAGES += kmod-ep80579-can kmod-ep80579-eth kmod-ep80579-misc \
+			kmod-usb-core kmod-usb2 kmod-usb-hid kmod-usb-uhci
+
+define Target/Description
+	Build firmware images for Intel EP80579 based boards
+	(e.g : ADI Engineering Ocracoke Island)
+endef
+
