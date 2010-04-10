f4d71f5e8167a72e086dee9ab5f872240bdf2a17
Florian Fainelli <florian@openwrt.org>
2010-04-10 21:02:51 +0000

    create two endian-specific subtargets, as malta can run both
    SVN-Revision: 20790

---
+++ target/linux/malta/be/target.mk
@@ -0,0 +1,9 @@
+ARCH:=mips
+SUBTARGET:=be
+BOARDNAME:=Big Endian
+FEATURES:=ramdisk
+
+define Target/Description
+	Build BE firmware images for MIPS Malta CoreLV board running in
+	big-endian mode
+endef
---
+++ target/linux/malta/le/target.mk
@@ -0,0 +1,9 @@
+ARCH:=mipsel
+SUBTARGET:=le
+BOARDNAME:=Little Endian
+FEATURES:=ramdisk
+
+define Target/Description
+	Build LE firmware images for MIPS Malta CoreLV board running in
+	little-endian mode
+endef
