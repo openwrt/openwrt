bd9c7b83ae98978fb444d48798e117c52fe56166
Gabor Juhos <juhosg@openwrt.org>
2010-04-11 17:47:25 +0000

    ramips: add sysupgrade support
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20800 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+
+. /lib/ramips.sh
+
+PART_NAME=firmware
+RAMFS_COPY_DATA=/lib/ramips.sh
+
+platform_check_image() {
+	local board=$(ramips_board_name)
+	local magic="$(get_magic_word "$1")"
+
+	[ "$ARGC" -gt 1 ] && return 1
+
+	case "$board" in
+	dir-300-b1 | fonera20n | v22rw-2x2 | whr-g300n)
+		[ "$magic" != "2705" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	echo "Sysupgrade is not yet supported on $board."
+	return 1
+}
+
+platform_do_upgrade() {
+	local board=$(ramips_board_name)
+
+	case "$board" in
+	*)
+		default_do_upgrade "$ARGV"
+		;;
+	esac
+}
