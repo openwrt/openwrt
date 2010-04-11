43878f9c0fd94678581c951e6fa8f344ae09bb91
Gabor Juhos <juhosg@openwrt.org>
2010-04-11 17:47:23 +0000

    ramips: add diag support
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20799 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/base-files/etc/diag.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+#
+
+. /lib/ramips.sh
+
+status_led=""
+
+led_set_attr() {
+	[ -f "/sys/class/leds/$1/$2" ] && echo "$3" > "/sys/class/leds/$1/$2"
+}
+
+status_led_set_timer() {
+	led_set_attr $status_led "trigger" "timer"
+	led_set_attr $status_led "delay_on" "$1"
+	led_set_attr $status_led "delay_off" "$2"
+}
+
+status_led_on() {
+	led_set_attr $status_led "trigger" "none"
+	led_set_attr $status_led "brightness" 255
+}
+
+status_led_off() {
+	led_set_attr $status_led "trigger" "none"
+	led_set_attr $status_led "brightness" 0
+}
+
+get_status_led() {
+	case $(ramips_board_name) in
+	dir-300-b1)
+		status_led="dir-300b:green:status"
+		;;
+	fonera20n)
+		status_led="fonera20n:green:power"
+		;;
+	v22rw-2x2)
+		status_led="v22rw-2x2:green:security"
+		;;
+	whr-g300n)
+		status_led="whr-g300n:red:diag"
+		;;
+	esac;
+}
+
+set_state() {
+	get_status_led
+
+	case "$1" in
+	preinit)
+		insmod leds-gpio
+		status_led_set_timer 200 200
+		;;
+	failsafe)
+		status_led_set_timer 50 50
+		;;
+	done)
+		status_led_on
+		;;
+	esac
+}
