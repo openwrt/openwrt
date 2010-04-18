b6bd0ef6673964e5215d3c66de248f77e99b40e9
Gabor Juhos <juhosg@openwrt.org>
2010-04-18 16:24:12 +0000

    ar71xx: adding support for zyxel nbg460n/550n/550nh boards
    This patch adds support for the zyxel nbg460n/550n/550nh routers.
    Currently this only works if a second stage bootloader is also flashed,
    as the used switch needs extra initialization.
    
    Signed-off-by: Michael Kurz <michi.kurz@googlemail.com>
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@20990 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ar71xx/profiles/zyxel.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/NBG_460N_550N_550NH
+	NAME:=Zyxel NBG 460N/550N/550NH
+	PACKAGES:=kmod-ath9k swconfig
+endef
+
+define Profile/NBG_460N_550N_550NH/Description
+	Package set optimized for the Zyxel NBG 460N/550N/550NH Routers.
+endef
+
+$(eval $(call Profile,NBG_460N_550N_550NH))
