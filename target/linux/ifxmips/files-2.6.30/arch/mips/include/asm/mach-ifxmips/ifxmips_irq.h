c8d93ffc3f37a97d1e9066c443aa1978aa6c4042
Ralph Hempel <ralph.hempel@lantiq.com>
2010-04-27 15:46:45 +0000

    [ifxmips] add external irq
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21201 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ifxmips/files-2.6.30/arch/mips/include/asm/mach-ifxmips/ifxmips_irq.h
+++ target/linux/ifxmips/files-2.6.30/arch/mips/include/asm/mach-ifxmips/ifxmips_irq.h
@@ -72,6 +72,10 @@
 #define IFXMIPS_USB_INT				(INT_NUM_IM4_IRL0 + 22)
 #define IFXMIPS_USB_OC_INT			(INT_NUM_IM4_IRL0 + 23)
 
+#define IFXMIPS_EIU_IR0				(INT_NUM_IM4_IRL0 + 30)
+#define IFXMIPS_EIU_IR1				(INT_NUM_IM3_IRL0 + 31)
+#define IFXMIPS_EIU_IR2				(INT_NUM_IM1_IRL0 + 26)
+
 
 extern void ifxmips_mask_and_ack_irq(unsigned int irq_nr);
 
