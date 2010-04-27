f9f11f950d5caf9876fd8f04b674f57aeed939c8
Felix Fietkau <nbd@openwrt.org>
2010-04-27 20:16:27 +0000

    ar71xx: add a new default profile containing all drivers for all boards
    SVN-Revision: 21209

--- target/linux/ar71xx/profiles/00-default.mk
+++ target/linux/ar71xx/profiles/00-default.mk
@@ -6,8 +6,8 @@
 #
 
 define Profile/Default
-	NAME:=Default Profile (no WiFi)
-	PACKAGES:=
+	NAME:=Default Profile (all drivers)
+	PACKAGES:=kmod-ath9k kmod-madwifi kmod-usb-core kmod-usb-ohci kmod-usb2 swconfig vsc7385-ucode-ap83 vsc7385-ucode-pb44 vsc7395-ucode-ap83 vsc7395-ucode-pb44 wpad-mini
 endef
 
 define Profile/Default/Description
