ba1debe042e190adc85932dc06f1e51f01959302
Nicolas Thill <nico@openwrt.org>
2010-04-29 00:44:26 +0000

    include: rearrange image.mk, indent, reorder, change some ifeq/ifneq tests
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21244 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/image.mk
+++ include/image.mk
@@ -17,42 +17,42 @@ KDIR=$(KERNEL_BUILD_DIR)
 
 IMG_PREFIX:=openwrt-$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))
 
-ifneq ($(CONFIG_BIG_ENDIAN),y)
-JFFS2OPTS     :=  --pad --little-endian --squash -v
-SQUASHFS_OPTS :=  -le
+ifneq ($(CONFIG_BIG_ENDIAN),)
+  JFFS2OPTS     :=  --pad --big-endian --squash -v
+  SQUASHFS_OPTS :=  -be
 else
-JFFS2OPTS     :=  --pad --big-endian --squash -v
-SQUASHFS_OPTS :=  -be
+  JFFS2OPTS     :=  --pad --little-endian --squash -v
+  SQUASHFS_OPTS :=  -le
 endif
 
 ifeq ($(CONFIG_JFFS2_RTIME),y)
-JFFS2OPTS+= -X rtime
+  JFFS2OPTS += -X rtime
 endif
 ifeq ($(CONFIG_JFFS2_ZLIB),y) 
-JFFS2OPTS+= -X zlib
+  JFFS2OPTS += -X zlib
 endif
 ifeq ($(CONFIG_JFFS2_LZMA),y)
-JFFS2OPTS+= -X lzma --compression-mode=size
+  JFFS2OPTS += -X lzma --compression-mode=size
 endif
 ifneq ($(CONFIG_JFFS2_RTIME),y)
-JFFS2OPTS+=  -x rtime
+  JFFS2OPTS +=  -x rtime
 endif
 ifneq ($(CONFIG_JFFS2_ZLIB),y)
-JFFS2OPTS+= -x zlib
+  JFFS2OPTS += -x zlib
 endif
 ifneq ($(CONFIG_JFFS2_LZMA),y)
-JFFS2OPTS+= -x lzma
+  JFFS2OPTS += -x lzma
 endif
 
 ifneq ($(CONFIG_LINUX_2_4)$(CONFIG_LINUX_2_6_25),)
-USE_SQUASHFS3 := y
+  USE_SQUASHFS3 := y
 endif
 
 ifneq ($(USE_SQUASHFS3),)
-MKSQUASHFS_CMD := $(STAGING_DIR_HOST)/bin/mksquashfs-lzma
+  MKSQUASHFS_CMD := $(STAGING_DIR_HOST)/bin/mksquashfs-lzma
 else
-MKSQUASHFS_CMD := $(STAGING_DIR_HOST)/bin/mksquashfs4
-SQUASHFS_OPTS  := -comp lzma -processors 1
+  MKSQUASHFS_CMD := $(STAGING_DIR_HOST)/bin/mksquashfs4
+  SQUASHFS_OPTS  := -comp lzma -processors 1
 endif
 
 JFFS2_BLOCKSIZE ?= 64k 128k
@@ -74,8 +74,17 @@ define prepare_generic_squashfs
 	rm -f $(KDIR)/tmpfile.*
 endef
 
-ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
-  ifeq ($(CONFIG_TARGET_ROOTFS_JFFS2),y)
+
+ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
+
+  define Image/BuildKernel
+		cp $(KDIR)/vmlinux.elf $(BIN_DIR)/$(IMG_PREFIX)-vmlinux.elf
+		$(call Image/Build/Initramfs)
+  endef
+
+else
+
+  ifneq ($(CONFIG_TARGET_ROOTFS_JFFS2),)
     define Image/mkfs/jffs2/sub
 		# FIXME: removing this line will cause strange behaviour in the foreach loop below
 		$(STAGING_DIR_HOST)/bin/mkfs.jffs2 $(JFFS2OPTS) -e $(patsubst %k,%KiB,$(1)) -o $(KDIR)/root.jffs2-$(1) -d $(TARGET_DIR) -v 2>&1 1>/dev/null | awk '/^.+$$$$/'
@@ -87,7 +96,7 @@ ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
     endef
   endif
 
-  ifeq ($(CONFIG_TARGET_ROOTFS_SQUASHFS),y)
+  ifneq ($(CONFIG_TARGET_ROOTFS_SQUASHFS),)
     define Image/mkfs/squashfs
 		@mkdir -p $(TARGET_DIR)/overlay
 		$(MKSQUASHFS_CMD) $(TARGET_DIR) $(KDIR)/root.squashfs -nopad -noappend -root-owned $(SQUASHFS_OPTS)
@@ -95,7 +104,7 @@ ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
     endef
   endif
 
-  ifeq ($(CONFIG_TARGET_ROOTFS_UBIFS),y)
+  ifneq ($(CONFIG_TARGET_ROOTFS_UBIFS),)
     define Image/mkfs/ubifs
 		$(CP) ./ubinize.cfg $(KDIR)
 		$(STAGING_DIR_HOST)/bin/mkfs.ubifs $(UBIFS_OPTS) -o $(KDIR)/root.ubifs -d $(TARGET_DIR)
@@ -104,34 +113,22 @@ ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),y)
 		$(call Image/Build,ubi)
     endef
   endif
-else
-  define Image/BuildKernel
-	cp $(KDIR)/vmlinux.elf $(BIN_DIR)/$(IMG_PREFIX)-vmlinux.elf
-	$(call Image/Build/Initramfs)
-  endef
+
 endif
 
-define Image/Checksum
-	( cd ${BIN_DIR} ; \
-		$(FIND) -maxdepth 1 -type f \! -name 'md5sums'  -printf "%P\n" | sort | xargs \
-		md5sum --binary > md5sums \
-	)
-endef
-
-
-ifeq ($(CONFIG_TARGET_ROOTFS_CPIOGZ),y)
+ifneq ($(CONFIG_TARGET_ROOTFS_CPIOGZ),)
   define Image/mkfs/cpiogz
 		( cd $(TARGET_DIR); find . | cpio -o -H newc | gzip -9 >$(BIN_DIR)/$(IMG_PREFIX)-rootfs.cpio.gz )
   endef
 endif
 
-ifeq ($(CONFIG_TARGET_ROOTFS_TARGZ),y)
+ifneq ($(CONFIG_TARGET_ROOTFS_TARGZ),)
   define Image/mkfs/targz
 		$(TAR) -zcf $(BIN_DIR)/$(IMG_PREFIX)-rootfs.tar.gz --numeric-owner --owner=0 --group=0 -C $(TARGET_DIR)/ .
   endef
 endif
 
-ifeq ($(CONFIG_TARGET_ROOTFS_EXT2FS),y)
+ifneq ($(CONFIG_TARGET_ROOTFS_EXT2FS),)
   E2SIZE=$(shell echo $$(($(CONFIG_TARGET_ROOTFS_FSPART)*1024)))
 
   define Image/mkfs/ext2
@@ -140,7 +137,7 @@ ifeq ($(CONFIG_TARGET_ROOTFS_EXT2FS),y)
   endef
 endif
 
-ifeq ($(CONFIG_TARGET_ROOTFS_ISO),y)
+ifneq ($(CONFIG_TARGET_ROOTFS_ISO),)
   define Image/mkfs/iso
 		$(call Image/Build,iso)
   endef
@@ -159,52 +156,62 @@ define Image/mkfs/prepare
 	$(call Image/mkfs/prepare/default)
 endef
 
-define BuildImage
-download:
-prepare:
-ifneq ($(IB),1)
-  compile: compile-targets FORCE
-	$(call Build/Compile)
-else
-  compile:
-endif
 
-ifneq ($(IB),1)
-  install: compile install-targets FORCE
-	$(call Image/Prepare)
-	$(call Image/mkfs/prepare)
-	$(call Image/BuildKernel)
-	$(call Image/mkfs/jffs2)
-	$(call Image/mkfs/squashfs)
-	$(call Image/mkfs/targz)
-	$(call Image/mkfs/cpiogz)
-	$(call Image/mkfs/ext2)
-	$(call Image/mkfs/iso)
-	$(call Image/mkfs/ubifs)
-	$(call Image/Checksum)
-else
-  install: compile install-targets
-	$(call Image/BuildKernel)
-	$(call Image/mkfs/jffs2)
-	$(call Image/mkfs/squashfs)
-	$(call Image/mkfs/targz)
-	$(call Image/mkfs/cpiogz)
-	$(call Image/mkfs/ext2)
-	$(call Image/mkfs/iso)
-	$(call Image/mkfs/ubifs)
-	$(call Image/Checksum)
-endif
-
-ifneq ($(IB),1)
-  clean: clean-targets
-	$(call Build/Clean)
-else
-  clean:
-endif
-
-compile-targets:
-install-targets:
-clean-targets:
+define Image/Checksum
+	( cd ${BIN_DIR} ; \
+		$(FIND) -maxdepth 1 -type f \! -name 'md5sums'  -printf "%P\n" | sort | xargs \
+		md5sum --binary > md5sums \
+	)
 endef
 
 
+define BuildImage
+
+  download:
+  prepare:
+
+  ifeq ($(IB),)
+    compile: compile-targets FORCE
+		$(call Build/Compile)
+  else
+    compile:
+  endif
+
+  ifeq ($(IB),)
+    install: compile install-targets FORCE
+		$(call Image/Prepare)
+		$(call Image/mkfs/prepare)
+		$(call Image/BuildKernel)
+		$(call Image/mkfs/cpiogz)
+		$(call Image/mkfs/targz)
+		$(call Image/mkfs/ext2)
+		$(call Image/mkfs/iso)
+		$(call Image/mkfs/jffs2)
+		$(call Image/mkfs/squashfs)
+		$(call Image/mkfs/ubifs)
+		$(call Image/Checksum)
+  else
+    install: compile install-targets
+		$(call Image/BuildKernel)
+		$(call Image/mkfs/cpiogz)
+		$(call Image/mkfs/targz)
+		$(call Image/mkfs/ext2)
+		$(call Image/mkfs/iso)
+		$(call Image/mkfs/jffs2)
+		$(call Image/mkfs/squashfs)
+		$(call Image/mkfs/ubifs)
+		$(call Image/Checksum)
+  endif
+
+  ifeq ($(IB),)
+    clean: clean-targets
+		$(call Build/Clean)
+  else
+    clean:
+  endif
+
+  compile-targets:
+  install-targets:
+  clean-targets:
+
+endef
