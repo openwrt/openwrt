36f84f9f36276500e5eda275f7589c5022a406c6
Nicolas Thill <nico@openwrt.org>
2010-04-29 03:57:55 +0000

    move kmod-lp package defition specific to brcm-2.4 to package/kernel
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21247 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/other.mk
+++ package/kernel/modules/other.mk
@@ -507,6 +507,19 @@ define KernelPackage/lp
   AUTOLOAD:=$(call AutoLoad,50,parport lp)
 endef
 
+define KernelPackage/lp/2.4
+  FILES:= \
+	$(LINUX_DIR)/drivers/parport/parport.$(LINUX_KMOD_SUFFIX) \
+	$(LINUX_DIR)/drivers/parport/parport_*.$(LINUX_KMOD_SUFFIX) \
+	$(LINUX_DIR)/drivers/char/lp.$(LINUX_KMOD_SUFFIX) \
+	$(LINUX_DIR)/drivers/char/ppdev.$(LINUX_KMOD_SUFFIX)
+  AUTOLOAD:=$(call AutoLoad,50, \
+  	parport \
+  	parport_splink \
+  	lp \
+  )
+endef
+
 $(eval $(call KernelPackage,lp))
 
 
