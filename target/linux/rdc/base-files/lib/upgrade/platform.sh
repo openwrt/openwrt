5dc1ce4ccbb5874e971ede60eb38a8c51fd6720b
Florian Fainelli <florian@openwrt.org>
2010-05-02 19:30:59 +0000

    fix upgrade detection logic, patch from Bernhard Loos
    SVN-Revision: 21310

--- target/linux/rdc/base-files/lib/upgrade/platform.sh
+++ target/linux/rdc/base-files/lib/upgrade/platform.sh
@@ -1,16 +1,10 @@
-PART_NAME=linux
+PART_NAME=firmware
 
 platform_check_image() {
-	[ "$ARGC" -gt 1 ] && return 1
+	[ "$(get_magic_word $(find_mtd_part firmware))" -eq "$(get_magic_word "$1")" ] && return 0
 
-	case "$(get_magic_word "$1")" in
-		# rdc .bin files for ar360w3g
-		474d) return 0;;
-		*)
-			echo "Invalid image type. Please use an appropriate .bin file"
-			return 1
-		;;
-	esac
+	echo "Invalid image type. Please use an appropriate .bin file"
+	return 1
 }
 
 # use default for platform_do_upgrade()
