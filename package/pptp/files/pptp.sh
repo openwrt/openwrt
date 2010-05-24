a93571825f040d24c12c6071dab32a5d98018091
Jo-Philipp Wich <jow@openwrt.org>
2010-05-24 16:28:46 +0000

    pptp: add "buffering" uci option to enable --nobuffer if needed (#7080)
    SVN-Revision: 21563

--- package/pptp/files/pptp.sh
+++ package/pptp/files/pptp.sh
@@ -14,9 +14,18 @@ setup_interface_pptp() {
 	local config="$2"
 	local ifname
 
+	local device
 	config_get device "$config" device
+
+	local ipproto
 	config_get ipproto "$config" ipproto
+
+	local server
 	config_get server "$config" server
+
+	local buffering
+	config_get_bool buffering "$config" buffering 1
+	[ "$buffering" == 0 ] && buffering="--nobuffer" || buffering=
 
 	for module in slhc ppp_generic ppp_async ip_gre; do
 		/sbin/insmod $module 2>&- >&-
@@ -37,7 +46,7 @@ setup_interface_pptp() {
 	config_get mtu "$config" mtu
 	mtu=${mtu:-1452}
 	start_pppd "$config" \
-		pty "/usr/sbin/pptp $server --loglevel 0 --nolaunchpppd" \
+		pty "/usr/sbin/pptp $server --loglevel 0 --nolaunchpppd $buffering" \
 		file /etc/ppp/options.pptp \
 		mtu $mtu mru $mtu
 }
