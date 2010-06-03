ebfb87169d2562a1684ca3882802e878df8c4339
Claudio Mignanti <c.mignanti@gmail.com>
2010-06-03 09:52:34 +0000

    [etrax] Force usb features, fix depends
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21664 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/etrax/modules.mk
+++ target/linux/etrax/modules.mk
@@ -8,12 +8,11 @@
 define KernelPackage/usb-etrax
   SUBMENU:=$(USB_MENU)
   TITLE:=Support for the ETRAX USB host controller
-  DEPENDS:=@TARGET_etrax
   KCONFIG:=CONFIG_ETRAX_USB_HOST \
 	CONFIG_ETRAX_USB_HOST_PORT1=y CONFIG_ETRAX_USB_HOST_PORT2=y
   FILES:=$(LINUX_DIR)/drivers/usb/host/hc-crisv10.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,50,hc-crisv10,1)
-  $(call AddDepends/usb)
+  $(call AddDepends/usb,@TARGET_etrax)
 endef
 
 define KernelPackage/usb-etrax/description
