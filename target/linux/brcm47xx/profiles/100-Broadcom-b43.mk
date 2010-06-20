da6e39e59246bb37658ab48fcc059b7f8ec35bad
Jo-Philipp Wich <jow@openwrt.org>
2010-06-20 13:32:40 +0000

    add a profile using proprietary wl instead of b43
    SVN-Revision: 21851

--- target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
+++ target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
@@ -1,17 +1,19 @@
 #
-# Copyright (C) 2007-2008 OpenWrt.org
+# Copyright (C) 2007-2010 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Broadcom-b43
-  NAME:=Broadcom BCM43xx WiFi (default)
-  PACKAGES:=kmod-b43 kmod-b43legacy
+  NAME:=Broadcom BCM43xx WiFi - "b43" driver (default)
+  PACKAGES:=wpad-mini kmod-b43 kmod-b43legacy
 endef
 
 define Profile/Broadcom-b43/Description
 	Package set compatible with hardware using Broadcom BCM43xx cards
+	using the MAC80211 b43 and b43legacy drivers.
 endef
+
 $(eval $(call Profile,Broadcom-b43))
 
