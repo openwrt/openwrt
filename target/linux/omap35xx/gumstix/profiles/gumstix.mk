4e37ed81844a9e3943c3af5075408bcf16aecd9a
Hamish Guthrie <hcg@openwrt.org>
2010-06-21 11:59:59 +0000

    Refactor omap35xx sub-targets to cater for beagleboard and gumstix overo
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21858 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/omap35xx/gumstix/profiles/gumstix.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2006 - 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Gumstix
+  NAME:=gumstix
+  PACKAGES:=-iptables -ppp -ppp-mod-pppoe -kmod-ipt-nathelper \
+		-firewall -mtd -hotplug2 -udevtrigger \
+		ext2 usb udev kmod-bluetooth ntpclient wpa-supplicant \
+		wireless-tools wpa-cli \
+		bluez-utils bluez-hcidump hegw usbutils python pyserial \
+		python-mini python-openssl python-sqlite3
+endef
+
+define Profile/Gumstix/Description
+        Gumstix www.gumstix.com
+endef
+$(eval $(call Profile,Gumstix))
