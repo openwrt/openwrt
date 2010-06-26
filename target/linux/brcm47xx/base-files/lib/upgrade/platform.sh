19a9284b9a6fd60ac78a129052cc677a39abf3cb
Felix Fietkau <nbd@openwrt.org>
2010-06-26 20:41:49 +0000

    add back the base-files/image code from brcm-2.4 to brcm47xx
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@21945 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm47xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,16 @@
+PART_NAME=linux
+
+platform_check_image() {
+	[ "$ARGC" -gt 1 ] && return 1
+
+	case "$(get_magic_word "$1")" in
+		# .trx files
+		4844) return 0;;
+		*)
+			echo "Invalid image type. Please use only .trx files"
+			return 1
+		;;
+	esac
+}
+
+# use default for platform_do_upgrade()
