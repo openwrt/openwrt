3c64ce369f9e5b7a263ee90c9e5bfae1b7dba3b5
Claudio Mignanti <c.mignanti@gmail.com>
2010-07-11 12:27:09 +0000

    [etrax] enable usb support by default.
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@22129 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/etrax/files/drivers/usb/host/hc-crisv10.c
+++ target/linux/etrax/files/drivers/usb/host/hc-crisv10.c
@@ -33,7 +33,7 @@
 /***************************************************************************/
 /***************************************************************************/
 
-#define VERSION			"1.00-openwrt_diff"
+#define VERSION			"1.00-openwrt_diff-v1"
 #define COPYRIGHT		"(c) 2005, 2006 Axis Communications AB"
 #define DESCRIPTION             "ETRAX 100LX USB Host Controller"
 
