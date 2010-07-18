1dd824a358ca3f33bbc3f53ef021b638eb8d0d3a
Jo-Philipp Wich <jow@openwrt.org>
2010-07-18 18:36:12 +0000

    base-files, busybox: fix 22279, simplify code and get rid of awk
    SVN-Revision: 22282

--- package/base-files/files/bin/login.sh
+++ package/base-files/files/bin/login.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
-# Copyright (C) 2006 OpenWrt.org
+# Copyright (C) 2006-2010 OpenWrt.org
 
-
-regpassresult="0"
-grep '^root:[^!]' /etc/passwd >&- 2>&-
-regpassresult="$?"
-shadowresult="0"
-if [ -e "/etc/shadow" ]; then
-	grep '^root:[^!]' /etc/shadow >&- 2>&-
-        shadowresult="$?"
-fi
-[ "$regpassresult" = "0" ] && [ "$shadowresult" = "0" ] && [ -z "$FAILSAFE" ] && {
-    echo "Login failed."
-    exit 0
-} || {
+if grep -qs '^root:[^!]' /etc/passwd /etc/shadow && [ -z "$FAILSAFE" ]; then
+	echo "Login failed."
+	exit 0
+else
 cat << EOF
  === IMPORTANT ============================
   Use 'passwd' to set your login password
   this will disable telnet and enable SSH
  ------------------------------------------
 EOF
-}
+fi
 
 exec /bin/ash --login
