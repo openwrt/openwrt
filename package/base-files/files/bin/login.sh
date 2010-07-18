b98d2df29574ff92346b7aa05da132be39c8daaa
Daniel Dickinson <daniel@cshore.neomailbox.net>
2010-07-18 15:18:38 +0000

    [package] base-files & telnet: Fixed telnet starting even with root password when shadow passwords in use.
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@22279 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/bin/login.sh
+++ package/base-files/files/bin/login.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # Copyright (C) 2006 OpenWrt.org
 
+
+regpassresult="0"
 grep '^root:[^!]' /etc/passwd >&- 2>&-
-[ "$?" = "0" -a -z "$FAILSAFE" ]  &&  
-{
+regpassresult="$?"
+shadowresult="0"
+if [ -e "/etc/shadow" ]; then
+	grep '^root:[^!]' /etc/shadow >&- 2>&-
+        shadowresult="$?"
+fi
+[ "$regpassresult" = "0" ] && [ "$shadowresult" = "0" ] && [ -z "$FAILSAFE" ] && {
     echo "Login failed."
     exit 0
 } || {
