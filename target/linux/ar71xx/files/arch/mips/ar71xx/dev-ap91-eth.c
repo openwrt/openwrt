65899cfa4946f945d1a91eab21b73f7534d5baf8
Felix Fietkau <nbd@openwrt.org>
2010-07-27 21:04:43 +0000

    ar71xx: remove the fifo cfg overrides for the ap91 ethernet code - these values have been wrong on every single device i've tested, falling back to the atheros values seems to be the right thing to do
    SVN-Revision: 22401

--- target/linux/ar71xx/files/arch/mips/ar71xx/dev-ap91-eth.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/dev-ap91-eth.c
@@ -50,17 +50,11 @@ void __init ap91_eth_init(u8 *mac_addr, const char *port_names[])
 	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
 	ar71xx_eth0_data.speed = SPEED_100;
 	ar71xx_eth0_data.duplex = DUPLEX_FULL;
-	ar71xx_eth0_data.fifo_cfg1 = 0x0fff0000;
-	ar71xx_eth0_data.fifo_cfg2 = 0x00001fff;
-	ar71xx_eth0_data.fifo_cfg3 = 0x008001ff;
 
 	/* LAN ports */
 	ar71xx_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_RMII;
 	ar71xx_eth1_data.speed = SPEED_1000;
 	ar71xx_eth1_data.duplex = DUPLEX_FULL;
-	ar71xx_eth1_data.fifo_cfg1 = 0x0fff0000;
-	ar71xx_eth1_data.fifo_cfg2 = 0x00001fff;
-	ar71xx_eth1_data.fifo_cfg3 = 0x008001ff;
 
 	ar71xx_add_device_mdio(0x0);
 	ar71xx_add_device_eth(1);
