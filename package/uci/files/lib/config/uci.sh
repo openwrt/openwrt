63991ba875e46f40324352a5317d144465567833
Jo-Philipp Wich <jow@openwrt.org>
2010-08-14 00:45:15 +0000

    base-files, uci: properly revert list state on config_load This fixes a bug where lists end up with duplicate values if config_load was invoked multiple times.
    SVN-Revision: 22629

--- package/uci/files/lib/config/uci.sh
+++ package/uci/files/lib/config/uci.sh
@@ -23,9 +23,15 @@ uci_load() {
 	local PACKAGE="$1"
 	local DATA
 	local RET
+	local VAR
 
 	_C=0
 	if [ -z "$CONFIG_APPEND" ]; then
+		for VAR in $CONFIG_LIST_STATE; do
+			export ${NO_EXPORT:+-n} CONFIG_${VAR}=
+			export ${NO_EXPORT:+-n} CONFIG_${VAR}_LENGTH=
+		done
+		export ${NO_EXPORT:+-n} CONFIG_LIST_STATE=
 		export ${NO_EXPORT:+-n} CONFIG_SECTIONS=
 		export ${NO_EXPORT:+-n} CONFIG_NUM_SECTIONS=0
 		export ${NO_EXPORT:+-n} CONFIG_SECTION=
