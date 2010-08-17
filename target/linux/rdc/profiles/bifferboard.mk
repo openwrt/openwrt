af0ecdebfdd43aa46bf624d5fa67a868e2f14857
Florian Fainelli <florian@openwrt.org>
2010-08-17 15:31:27 +0000

    allow 1MB extra space for JFFS and other enhancements (#7706)
    SVN-Revision: 22690

--- target/linux/rdc/profiles/bifferboard.mk
+++ target/linux/rdc/profiles/bifferboard.mk
@@ -7,5 +7,7 @@
 
 define Profile/bifferboard
 	NAME:=Bifferboard
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 \
+		kmod-usb-storage kmod-scsi-core kmod-fs-ext3
 endef
 $(eval $(call Profile,bifferboard))
