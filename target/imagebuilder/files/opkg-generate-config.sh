699876fa6b30be6a3fa11db92bce249d090aeb85
Jo-Philipp Wich <jow@openwrt.org>
2010-09-08 00:36:35 +0000

    - add a helper script to generate opkg.conf, attempt to detect package architecture from packages/ - fix package defaults when no package override is given, IB previously aggregated the defautls of all profiles - introduce a repositories.conf, allows using remote opkg repositories in imagebuilder
    SVN-Revision: 22978

---
+++ target/imagebuilder/files/opkg-generate-config.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+TOPDIR="$(pwd)"
+TARGETDIR="${1}"
+
+[ -f "$TOPDIR/scripts/${0##*/}" ] || {
+	echo "Please execute within the toplevel directory" >&2
+	exit 1
+}
+
+# Try to find package architecture from packages directory
+PKGARCH=
+for pkg in $TOPDIR/packages/*.ipk; do
+	if [ -f "$pkg" ]; then
+		PKGARCH="${pkg##*_}"
+		PKGARCH="${PKGARCH%.ipk}"
+		[ "$PKGARCH" = all ] || break
+	fi
+done
+
+# Try to find package architecture from the target directory
+[ -n "$PKGARCH" ] || {
+	PKGARCH="${TARGETDIR##*/root-}"
+	[ "$PKGARCH" != "$TARGETDIR" ] || {
+		echo "Cannot determine package architecture" >&2
+		exit 1
+	}
+}
+
+rm -f $TOPDIR/tmp/opkg.conf
+
+[ -f $TOPDIR/repositories.conf ] && \
+	$TOPDIR/staging_dir/host/bin/sed \
+		-n -e "s/\$A/$PKGARCH/g" -e "/^[[:space:]*]src/p" \
+		$TOPDIR/repositories.conf > $TOPDIR/tmp/opkg.conf
+
+cat <<EOT >> $TOPDIR/tmp/opkg.conf
+dest root /
+arch all 100
+arch $PKGARCH 200
+option offline_root $TARGETDIR
+src imagebuilder file:$TOPDIR/packages
+EOT
+
+exit 0
