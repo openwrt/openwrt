e2ca2a1d17cb5777fb8a3d2ef01bdb6024f746f8
Lars-Peter Clausen <lars@metafoo.de>
2010-09-28 16:32:08 +0000

    [kirkwood] Fix mv_cesa module dependencies and .ko file location Thanks KanjiMonster & Memphiz
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@23145 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/kirkwood/modules.mk
+++ target/linux/kirkwood/modules.mk
@@ -2,10 +2,10 @@
 define KernelPackage/crypto-mv_cesa
    TITLE:=Marvell crypto engine
    KCONFIG:=CONFIG_CRYPTO_DEV_MV_CESA
-   FILES:=$(LINUX_DIR)/crypto/mv_cesa.ko
+   FILES:=$(LINUX_DIR)/drivers/crypto/mv_cesa.ko
    AUTOLOAD:=$(call AutoLoad,09,mv_cesa)
    SUBMENU:=Cryptographic API modules
-   DEPENDS:=kmod-crypto-core
+   DEPENDS:=kmod-crypto-core @TARGET_kirkwood
 endef
 
 $(eval $(call KernelPackage,crypto-mv_cesa))
