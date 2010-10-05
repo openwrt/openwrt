a1973192c68002f2c2361aa323946e5b70924587
Ralph Hempel <ralph.hempel@lantiq.com>
2010-10-05 20:20:13 +0000

    add configure as package target
    SVN-Revision: 23262

--- include/subdir.mk
+++ include/subdir.mk
@@ -5,7 +5,7 @@
 # See /LICENSE for more information.
 #
 
-SUBTARGETS:=clean download prepare compile install update refresh prereq dist distcheck
+SUBTARGETS:=clean download prepare compile install update refresh prereq dist distcheck configure
 
 subtarget-default = $(filter-out ., \
 	$(if $($(1)/builddirs-$(2)),$($(1)/builddirs-$(2)), \
