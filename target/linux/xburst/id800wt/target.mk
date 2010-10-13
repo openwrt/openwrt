6144179d612a4b1e127f30db8f2817c6e5f6712b
Lars-Peter Clausen <lars@metafoo.de>
2010-10-13 01:55:44 +0000

    Add 2.6.36 patches
    SVN-Revision: 23421

---
+++ target/linux/xburst/id800wt/target.mk
@@ -0,0 +1,8 @@
+BOARDNAME:=Sungale Pictureframe (id800wt)
+DEFAULT_PACKAGES += \
+	kmod-sound-core \
+	kmod-sound-soc-core \
+	kmod-sound-soc-jz4740 \
+	kmod-sound-soc-jz4740-codec \
+	kmod-sound-soc-id800wt \
+
