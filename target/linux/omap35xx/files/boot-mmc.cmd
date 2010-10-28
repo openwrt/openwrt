a78ed6322ee4a2ab93b64ed048bec7a653eb2fe0
Hamish Guthrie <hcg@openwrt.org>
2010-10-28 08:54:52 +0000

    [omap35xx] Update u-boot command-line parameters
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@23691 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/omap35xx/files/boot-mmc.cmd
+++ target/linux/omap35xx/files/boot-mmc.cmd
@@ -5,6 +5,8 @@ echo  RootFS: MMC0P2
 echo  VideoMode: 640x480@60
 echo  VideoSpec: omapfb:vram:2M,vram:4M
 echo -----------------------------
-setenv bootargs 'console=ttyS2,115200n8 console=tty0 root=/dev/mmcblk0p2 rw rootwait nohz=off video=omapfb:vram:1M,vxres:640,vyres:480'
+setenv mmcargs setenv bootargs earlyprintk=ttyS2,115200n8 console=ttyS2,115200n8 console=tty0 root=/dev/mmcblk0p2 rw rootwait init=/etc/preinit nohz=off video=omapfb:vram:1M,vxres:640,vyres:480
+printenv
 fatload mmc 0 83000000 uImage
+run mmcargs
 bootm 83000000
