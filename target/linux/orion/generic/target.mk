4ec905f3c9325be3086d35e4ec9567f77db6bf6a
Alexandros C. Couloumbis <alex@ozo.com>
2010-11-04 22:10:11 +0000

    target/linux/orion: create subtargets (closes #8154)
    SVN-Revision: 23883

--- target/linux/orion/generic/target.mk
+++ target/linux/orion/generic/target.mk
@@ -1,6 +1,16 @@
+#
+# Copyright (C) 2008-2010 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
 BOARDNAME:=Generic
+FEATURES:=squashfs jffs2
+
+DEFAULT_PACKAGES += kmod-ath9k wpad-mini
 
 define Target/Description
-	Build firmware images for orion based boards that boot from internal flash
+	Build firmware images for Marvell Orion based boards that boot from internal flash.
+	(e.g.: Linksys WRT350N v2, Netgear WNR854T, ...)
 endef
-
