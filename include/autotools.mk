07d0739c17e1ca2004d6a4b08c5bb388fd0274a7
Jo-Philipp Wich <jow@openwrt.org>
2010-12-08 00:54:14 +0000

    [include] deploy the steamroller tactics and autoreconfigure any package carring the libtool fixup, this should solve a number of issues ranging from linking into the host, syntax errors in libtool and missing make targets
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@24317 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/autotools.mk
+++ include/autotools.mk
@@ -5,32 +5,7 @@
 # See /LICENSE for more information.
 #
 
-define replace
-	if [ -f "$(PKG_BUILD_DIR)/$(3)$(1)" -a -e "$(2)/$(if $(4),$(4),$(1))" ]; then \
-		rm -f $(PKG_BUILD_DIR)/$(3)$(1); \
-		ln -s $(2)/$(if $(4),$(4),$(1)) $(PKG_BUILD_DIR)/$(3)$(1); \
-	fi
-	
-endef
-
 PKG_LIBTOOL_PATHS?=$(CONFIGURE_PATH)
-
-# replace copies of ltmain.sh with the build system's version
-update_libtool_common = \
-	$(foreach p,$(LIBTOOL_PATHS), \
-		$(call replace,ltmain.sh,$(STAGING_DIR)/host/share/libtool,$(p)/) \
-		$(call replace,libtool.m4,$(STAGING_DIR)/host/share/aclocal,$(p)/) \
-	)
-update_libtool = \
-	$(foreach p,$(PKG_LIBTOOL_PATHS), \
-		$(call replace,libtool,$(STAGING_DIR)/host/bin,$(p)/) \
-	) \
-	$(call update_libtool_common)
-update_libtool_ucxx = \
-	$(foreach p,$(PKG_LIBTOOL_PATHS), \
-		$(call replace,libtool,$(STAGING_DIR)/host/bin,$(p)/,libtool-ucxx) \
-	) \
-	$(call update_libtool_common)
 
 autoconf_bool = $(patsubst %,$(if $($(1)),--enable,--disable)-%,$(2))
 
@@ -38,32 +13,30 @@ autoconf_bool = $(patsubst %,$(if $($(1)),--enable,--disable)-%,$(2))
 define libtool_fixup_libdir
 	find $(1) -name '*.la' | $(XARGS) \
 		$(SED) "s,\(^libdir='\| \|-L\|^dependency_libs='\)/usr/lib,\1$(STAGING_DIR)/usr/lib,g" \
-		    -e "s,$(STAGING_DIR)/usr/lib/\(libstdc++\|libsupc++\).la,$(TOOLCHAIN_DIR)/lib/\1.la,g"
-endef
-
-define remove_version_check
-	if [ -f "$(PKG_BUILD_DIR)/$(CONFIGURE_PATH)/configure" ]; then \
-		$(SED) \
-			's,\(gentoo\|pardus\)_ltmain_version=.*,\1_ltmain_version="$$$$\1_lt_version",' \
-			$(PKG_BUILD_DIR)/$(CONFIGURE_PATH)/configure; \
-	fi
+		    -e "s,$(STAGING_DIR)/usr/lib/\(libstdc++\|libsupc++\).la,$(TOOLCHAIN_DIR)/lib/\1.la,g";
 endef
 
 # delete *.la-files from staging_dir - we can not yet remove respective lines within all package
 # Makefiles, since backfire still uses libtool v1.5.x which (may) require those files
 define libtool_remove_files
-	find $(1) -name '*.la' | $(XARGS) rm -f
+	find $(1) -name '*.la' | $(XARGS) rm -f;
+endef
+
+define autoreconf
+	(cd $(PKG_BUILD_DIR); $(STAGING_DIR_HOST)/bin/autoreconf -v -f -i -s $(patsubst %,-I %,$(PKG_LIBTOOL_PATHS)) $(PKG_LIBTOOL_PATHS));
 endef
 
 Hooks/InstallDev/Post += libtool_remove_files
 
 ifneq ($(filter libtool,$(PKG_FIXUP)),)
-  # WARNING: the use of PKG_FIXUP is DEPRECATED
   PKG_BUILD_DEPENDS += libtool
+  Hooks/Configure/Pre += autoreconf
+  Hooks/InstallDev/Post += libtool_fixup_libdir
 endif
  
 ifneq ($(filter libtool-ucxx,$(PKG_FIXUP)),)
-  # WARNING: the use of PKG_FIXUP is DEPRECATED
   PKG_BUILD_DEPENDS += libtool
+  Hooks/Configure/Pre += autoreconf
+  Hooks/InstallDev/Post += libtool_fixup_libdir
 endif
 
