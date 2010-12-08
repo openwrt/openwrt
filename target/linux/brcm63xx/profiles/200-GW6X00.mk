de920641344350465a1599c99acac7b62b10ea3b
Daniel Dickinson <daniel@cshore.neomailbox.net>
2010-12-08 00:40:28 +0000

    [brcm63xx] Profiles: Make buttons and led modules included by default so that failsafe will work in the case of builds for releases.
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@24315 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/profiles/200-GW6X00.mk
+++ target/linux/brcm63xx/profiles/200-GW6X00.mk
@@ -10,9 +10,7 @@ define Profile/GW6X00
   PACKAGES:=kmod-brcm-wl kmod-usb-core kmod-usb-ohci kmod-usb-storage \
 	kmod-fs-ext2 kmod-nls-cp437 kmod-nls-iso8859-1 kmod-fs-vfat \
 	kmod-loop e2fsprogs kmod-spi-bitbang kmod-bcm63xx-spi \
-	kmod-ipt-nathelper-extra kmod-input-gpio-buttons \
-	kmod-leds-gpio kmod-button-hotplug \
-	wl wlc
+	kmod-ipt-nathelper-extra wl wlc
 endef
 
 define Profile/GW6X00/Description
