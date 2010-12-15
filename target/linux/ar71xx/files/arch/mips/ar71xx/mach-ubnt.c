e421d2575f5ba29ac2e9ba52d13d0803125d7df8
Gabor Juhos <juhosg@openwrt.org>
2010-12-15 19:55:47 +0000

    ar71xx: UBNT M-Series MAC address fix
    UBNT M-Series devices have 2 MAC addresses in board data.
    Fix is to use both entries from board data instead of one entry.
    
    Signed-off-by: Kestutis Barkauskas<keba@devint.net>
    
    SVN-Revision: 24611

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-ubnt.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-ubnt.c
@@ -225,15 +225,16 @@ MIPS_MACHINE(AR71XX_MACH_UBNT_LSSR71, "UBNT-LS-SR71", "Ubiquiti LS-SR71",
 
 static void __init ubnt_m_setup(void)
 {
-	u8 *mac = (u8 *) KSEG1ADDR(0x1fff0000);
+	u8 *mac1 = (u8 *) KSEG1ADDR(0x1fff0000);
+	u8 *mac2 = (u8 *) KSEG1ADDR(0x1fff0000 + ETH_ALEN);
 	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
 
 	ar71xx_add_device_m25p80(NULL);
 
 	ar71xx_add_device_mdio(~0);
 
-	ar71xx_init_mac(ar71xx_eth0_data.mac_addr, mac, 0);
-	ar71xx_init_mac(ar71xx_eth1_data.mac_addr, mac, 1);
+	ar71xx_init_mac(ar71xx_eth0_data.mac_addr, mac1, 0);
+	ar71xx_init_mac(ar71xx_eth1_data.mac_addr, mac2, 0);
 	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
 	ar71xx_eth0_data.speed = SPEED_100;
 	ar71xx_eth0_data.duplex = DUPLEX_FULL;
