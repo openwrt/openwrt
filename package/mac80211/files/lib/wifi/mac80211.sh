3347f8e360195ecccc2e8cb482b4d3efb40035f7
Felix Fietkau <nbd@openwrt.org>
2010-12-19 11:56:56 +0000

    hostapd: add support for specifying the maximum number of clients
    Signed-off-by: Alexander Couzens <lynxis@c-base.org>
    
    SVN-Revision: 24725

--- package/mac80211/files/lib/wifi/mac80211.sh
+++ package/mac80211/files/lib/wifi/mac80211.sh
@@ -105,11 +105,13 @@ mac80211_hostapd_setup_bss() {
 
 	config_get macaddr "$vif" macaddr
 	config_get_bool hidden "$vif" hidden 0
+	config_get max_clients "$vif" max_clients
 	cat >> /var/run/hostapd-$phy.conf <<EOF
 $hostapd_cfg
 wmm_enabled=1
 bssid=$macaddr
 ignore_broadcast_ssid=$hidden
+${max_clients:+max_num_sta=$max_clients}
 EOF
 }
 
