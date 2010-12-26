dda75774e9b94c0ca362b1447fc4176823f269da
Daniel Dickinson <daniel@cshore.neomailbox.net>
2010-12-26 04:18:04 +0000

    base-files: Added uci-defaults script to run mtd fixtrx on firstboot for devices that need it.
    Signed-off-by: Daniel Dickinson <daniel@cshore.neomailbox.net>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@24836 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm63xx/base-files/etc/uci-defaults/brcm63xx_fixcrc.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Copyright (C) 2007 OpenWrt.org
+#
+#
+
+. /lib/brcm63xx.sh
+
+do_fixcrc() {
+	mtd fixtrx linux
+}
+
+brcm63xx_detect
+
+case "$board_name" in
+	"bcm63xx/CPVA642 "*)
+		do_fixcrc
+		;;
+esac
+
