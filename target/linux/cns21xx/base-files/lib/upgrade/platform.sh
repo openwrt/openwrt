0d46434b606a53fc835cbb70cad4bf03e99f613d
Gabor Juhos <juhosg@openwrt.org>
2010-12-30 17:19:16 +0000

    cns21xx: add initial support for the Cavium CNS21xx SoC
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@24859 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/cns21xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+
+. /lib/cns21xx.sh
+
+PART_NAME=firmware
+RAMFS_COPY_DATA=/lib/cns21xx.sh
+
+platform_check_image() {
+	local board=$(get_board_name)
+	local magic="$(get_magic_word "$1")"
+
+	[ "$ARGC" -gt 1 ] && return 1
+
+	case "$board" in
+	nsb3ast)
+		[ "$magic" != "0b1c" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	ns-k330)
+		[ "$magic" != "0c1c" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	echo "Sysupgrade is not yet supported on $board."
+	return 1
+}
+
+disable_watchdog() {
+	killall watchdog
+	( ps | grep -v 'grep' | grep '/dev/watchdog' ) && {
+		echo 'Could not disable watchdog'
+		return 1
+	}
+}
+
+append sysupgrade_pre_upgrade disable_watchdog
