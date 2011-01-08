b16b9edaed84e7454d676d405f7863936dad71be
Gabor Juhos <juhosg@openwrt.org>
2011-01-08 20:24:22 +0000

    generic: rtl8366: add enable_port helper
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@24937 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/generic/files/drivers/net/phy/rtl8366s.c
+++ target/linux/generic/files/drivers/net/phy/rtl8366s.c
@@ -605,6 +605,12 @@ static int rtl8366s_is_vlan_valid(struct rtl8366_smi *smi, unsigned vlan)
 	return 1;
 }
 
+static int rtl8366s_enable_port(struct rtl8366_smi *smi, int port, int enable)
+{
+	return rtl8366_smi_rmwr(smi, RTL8366S_PECR, (1 << port),
+				(enable) ? 0 : (1 << port));
+}
+
 static int rtl8366s_sw_reset_mibs(struct switch_dev *dev,
 				  const struct switch_attr *attr,
 				  struct switch_val *val)
@@ -1032,6 +1038,7 @@ static struct rtl8366_smi_ops rtl8366s_smi_ops = {
 	.is_vlan_valid	= rtl8366s_is_vlan_valid,
 	.enable_vlan	= rtl8366s_enable_vlan,
 	.enable_vlan4k	= rtl8366s_enable_vlan4k,
+	.enable_port	= rtl8366s_enable_port,
 };
 
 static int __devinit rtl8366s_probe(struct platform_device *pdev)
