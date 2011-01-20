cb175f3466aa182682a4245a3b9b90d051382c5a
Hamish Guthrie <hcg@openwrt.org>
2011-01-20 11:26:47 +0000

    [gumstix] update package dependencies
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25050 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/omap35xx/gumstix/profiles/gumstix.mk
+++ target/linux/omap35xx/gumstix/profiles/gumstix.mk
@@ -11,17 +11,14 @@ define Profile/Gumstix
 		-firewall -mtd -hotplug2 -udevtrigger -dropbear \
 		ext4 usb udev kmod-bluetooth ntpclient wpa-supplicant \
 		wireless-tools wpa-cli \
-		bluez-utils bluez-hcidump usbutils python pyserial \
-		python-mini python-openssl python-cjson libstdcpp \
+		bluez-utils bluez-hcidump usbutils \
 		kmod-usb-serial kmod-usb-serial-cp210x kmod-usb-serial-ftdi \
 		kmod-usb-serial-pl2303 kmod-usb-uhci kmod-cfg80211 \
 		kmod-lib80211 kmod-libertas-sd kmod-mac80211 libnl-tiny \
 		crda iw kmod-usb-hid \
-		openssh-client openssh-client-utils openssh-keygen \
-		openssh-server kmod-usb-storage kmod-usb-storage-extras \
-		kmod-fs-vfat avahi-autoipd avahi-daemon avahi-dnsconfd \
-		hegw hegwtest \
-		dbus-python python-expat python-ifconfig
+		kmod-usb-storage kmod-usb-storage-extras \
+		kmod-fs-vfat \
+		hegw hegwtest
 endef
 
 define Profile/Gumstix/Description
