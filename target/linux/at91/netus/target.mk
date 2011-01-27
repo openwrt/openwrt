d851aeec39a295fbeaad969d5da9bc0cf460f081
Jo-Philipp Wich <jow@openwrt.org>
2011-01-27 20:57:01 +0000

    [at91] move Netus G20 into a subtarget and rework toplevel config and makefile
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25165 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/at91/netus/target.mk
@@ -0,0 +1,10 @@
+# 
+# Copyright (C) 2006-2011 OpenWrt.org
+#
+
+SUBTARGET:=netus
+BOARDNAME:=Acmesystems NetusG20 board
+
+define Target/Description
+	Build firmware images for Acmesystems NetusG20 board
+endef
