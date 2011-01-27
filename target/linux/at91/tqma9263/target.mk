53d234602aec246c68f134a82d745336d3d29c3e
Jo-Philipp Wich <jow@openwrt.org>
2011-01-27 20:55:53 +0000

    [at91] prepare TQMa9263 subtarget
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25164 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/at91/tqma9263/target.mk
@@ -0,0 +1,10 @@
+# 
+# Copyright (C) 2011 OpenWrt.org
+#
+
+SUBTARGET:=tqma9263
+BOARDNAME:=TQ Components TQMa9263
+
+define Target/Description
+	Build firmware images for TQ Components TQMa9263 board.
+endef
