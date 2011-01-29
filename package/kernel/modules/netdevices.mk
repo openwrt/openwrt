95b2c75e548720559583943f4628c7af468c8fd7
Florian Fainelli <florian@openwrt.org>
2011-01-29 22:06:23 +0000

    [package] kmod-via-velocity depends on kmod-crc-ccitt (#8730)
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25244 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/netdevices.mk
+++ package/kernel/modules/netdevices.mk
@@ -158,7 +158,7 @@ $(eval $(call KernelPackage,via-rhine))
 define KernelPackage/via-velocity
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=VIA Velocity Gigabit Ethernet Adapter kernel support
-  DEPENDS:=@TARGET_ixp4xx||TARGET_mpc83xx||TARGET_x86
+  DEPENDS:=@TARGET_ixp4xx||TARGET_mpc83xx||TARGET_x86 +kmod-crc-ccitt
   KCONFIG:=CONFIG_VIA_VELOCITY
   FILES:=$(LINUX_DIR)/drivers/net/via-velocity.ko
   AUTOLOAD:=$(call AutoLoad,50,via-velocity)
