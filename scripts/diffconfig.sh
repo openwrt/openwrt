b0359432ae5bc18aa90a7e40e5e72e0df73ad955
Felix Fietkau <nbd@openwrt.org>
2011-01-29 02:35:28 +0000

    scripts/diffconfig.sh: use the new kconfig mode to remove some more unnecessary entries in the generated output
    SVN-Revision: 25233

--- scripts/diffconfig.sh
+++ scripts/diffconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 grep \^CONFIG_TARGET_ .config | head -n3 > tmp/.diffconfig.head
 ./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage1 Config.in >/dev/null
-./scripts/kconfig.pl '>' tmp/.diffconfig.stage1 .config >> tmp/.diffconfig.head
+./scripts/kconfig.pl '>+' tmp/.diffconfig.stage1 .config >> tmp/.diffconfig.head
 ./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage2 Config.in >/dev/null
 ./scripts/kconfig.pl '>' tmp/.diffconfig.stage2 .config >> tmp/.diffconfig.head
 cat tmp/.diffconfig.head
