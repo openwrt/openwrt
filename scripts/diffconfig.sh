73ea5d23f2c7218b73f1455da207eac3ab90604c
Felix Fietkau <nbd@openwrt.org>
2011-01-29 02:22:45 +0000

    add scripts/diffconfig.sh: a tool for making reduced .config files these files contain only the target selection and options that differ from the default settings this makes it much easier to review .config files
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25231 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ scripts/diffconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+grep \^CONFIG_TARGET_ .config | head -n3 > tmp/.diffconfig.head
+./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage1 Config.in >/dev/null
+./scripts/kconfig.pl '>' tmp/.diffconfig.stage1 .config >> tmp/.diffconfig.head
+./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage2 Config.in >/dev/null
+./scripts/kconfig.pl '>' tmp/.diffconfig.stage2 .config >> tmp/.diffconfig.head
+cat tmp/.diffconfig.head
+rm -f tmp/.diffconfig tmp/.diffconfig.head
