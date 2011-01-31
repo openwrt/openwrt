ef8a192e42823cbf87af5bdbe1378eb9e2e71836
Daniel Dickinson <crazycshore@gmail.com>
2011-01-31 05:11:38 +0000

    block-extroot: Fixed hang when no modules in on either squashfs or jffs2 and suppressed extraneous not found error message when no modules on jffs2.
    SVN-Revision: 25259

--- package/block-extroot/files/extmount.sh
+++ package/block-extroot/files/extmount.sh
@@ -17,8 +17,8 @@ er_load_modules() {
 	mkdir -p /tmp/extroot_modules/modules
 	ln -sf /etc/modules.d/* /tmp/overlay/etc/modules.d/* /tmp/extroot_modules/modules.d
 	ln -sf /lib/modules/*/* /tmp/overlay/lib/modules/*/* /tmp/extroot_modules/modules
-    	local modules="$(grep -l '# May be required for rootfs' /tmp/extroot_modules/modules.d/*)"
-	cd /tmp/extroot_modules/modules && {
+    	local modules="$(grep -l '# May be required for rootfs' /tmp/extroot_modules/modules.d/* 2>/dev/null)"
+	cd /tmp/extroot_modules/modules && [ -n "$modules" ] && {
 		cat $modules | sed -e 's/^\([^#].*\)/insmod \.\/\1.ko/'| sh 2>&- || :
 	}
 	rm -rf /tmp/extroot_modules
