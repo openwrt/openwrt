e919bd002a7f08d1c893dc3272f1f6332442fc35
John Crispin <john@openwrt.org>
2011-02-01 14:35:36 +0000

    * drop codebase in favour of lantiq target
    SVN-Revision: 25277

--- target/linux/ifxmips/files/arch/mips/ifxmips/danube/arcaydian.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ARCAYDIAN_H__
-#define _ARCAYDIAN_H__
-
-int __init ifxmix_detect_brn_block(unsigned int offset);
-int __init ifxmips_find_brn_mac(unsigned int offset, unsigned char *ifxmips_ethaddr);
-
-#endif
