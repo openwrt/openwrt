7e4a29fbe2dd57584b54e367574d6bcb72614db8
Felix Fietkau <nbd@openwrt.org>
2011-02-06 16:04:33 +0000

    use := instead of = for PKG_CONFIG_PATH to prevent recursion
    SVN-Revision: 25388

--- include/package.mk
+++ include/package.mk
@@ -81,8 +81,8 @@ define Build/Exports/Default
   $(1) : export STAGING_PREFIX=$$(STAGING_DIR)/usr
   $(1) : export PATH=$$(TARGET_PATH_PKG)
   $(1) : export CONFIG_SITE:=$$(CONFIG_SITE)
-  $(1) : export PKG_CONFIG_PATH=$$(PKG_CONFIG_PATH)
-  $(1) : export PKG_CONFIG_LIBDIR=$$(PKG_CONFIG_PATH)
+  $(1) : export PKG_CONFIG_PATH:=$$(PKG_CONFIG_PATH)
+  $(1) : export PKG_CONFIG_LIBDIR:=$$(PKG_CONFIG_PATH)
 endef
 Build/Exports=$(Build/Exports/Default)
 
