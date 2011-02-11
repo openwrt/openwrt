bd511bbd5399f4667066b973dcb6220b7367fbbe
Florian Fainelli <florian@openwrt.org>
2011-02-11 17:37:51 +0000

    [package] use i2c_defaults for kmod-i2c-at91
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25468 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/modules/i2c.mk
+++ package/kernel/modules/i2c.mk
@@ -184,13 +184,14 @@ endef
 
 $(eval $(call KernelPackage,i2c-mv64xxx))
 
+
+I2C_AT91_MODULES:=\
+  CONFIG_I2C_AT91:drivers/i2c/busses/i2c-at91
+
 define KernelPackage/at91-i2c
-  SUBMENU:=$(I2C_MENU)
+  $(call i2c_defaults,$(I2C_AT91_MODULES),55)
   TITLE:=I2C (TWI) master driver for Atmel AT91
-  DEPENDS:=@TARGET_at91 kmod-i2c-core
-  KCONFIG:=CONFIG_I2C_AT91
-  FILES:=$(LINUX_DIR)/drivers/i2c/busses/i2c-at91.ko
-  AUTOLOAD:=$(call AutoLoad,55,i2c-at91)
+  DEPENDS:=@TARGET_at91 +kmod-i2c-core
 endef
 
 define KernelPackage/at91-i2c/description
