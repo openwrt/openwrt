afee24f0870b872515f0da93fb218e0dd5b60b71
Felix Fietkau <nbd@openwrt.org>
2011-02-13 16:08:36 +0000

    ar71xx: remove vsc73xx ucodes from the default profile - these are only used on some atheros reference designs which are unlikely to have many users
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25522 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/generic/profiles/00-default.mk
+++ target/linux/ar71xx/generic/profiles/00-default.mk
@@ -9,9 +9,7 @@ define Profile/Default
 	NAME:=Default Profile (all drivers)
 	PACKAGES:= \
 		kmod-ath9k kmod-usb-core kmod-usb-ohci kmod-usb2 swconfig \
-		vsc7385-ucode-ap83 vsc7385-ucode-pb44 vsc7395-ucode-ap83 \
-		vsc7395-ucode-pb44 wpad-mini uboot-ar71xx-nbg460n_550n_550nh \
-		kmod-ledtrig-usbdev
+		wpad-mini uboot-ar71xx-nbg460n_550n_550nh kmod-ledtrig-usbdev
 endef
 
 define Profile/Default/Description
