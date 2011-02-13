04ec55e7120eb1cb97b120a84a5c29ea6fc7f429
Felix Fietkau <nbd@openwrt.org>
2011-02-13 01:44:57 +0000

    base-files: /usr/lib/common.awk is only used by /bin/ipcalc.sh, move the code there
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@25495 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/bin/ipcalc.sh
+++ package/base-files/files/bin/ipcalc.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
 awk -f /usr/lib/common.awk -f - $* <<EOF
+function bitcount(c) {
+	c=and(rshift(c, 1),0x55555555)+and(c,0x55555555)
+	c=and(rshift(c, 2),0x33333333)+and(c,0x33333333)
+	c=and(rshift(c, 4),0x0f0f0f0f)+and(c,0x0f0f0f0f)
+	c=and(rshift(c, 8),0x00ff00ff)+and(c,0x00ff00ff)
+	c=and(rshift(c,16),0x0000ffff)+and(c,0x0000ffff)
+	return c
+}
+
+function ip2int(ip) {
+	for (ret=0,n=split(ip,a,"\."),x=1;x<=n;x++) ret=or(lshift(ret,8),a[x]) 
+	return ret
+}
+
+function int2ip(ip,ret,x) {
+	ret=and(ip,255)
+	ip=rshift(ip,8)
+	for(;x<3;ret=and(ip,255)"."ret,ip=rshift(ip,8),x++);
+	return ret
+}
+
 BEGIN {
 	ipaddr=ip2int(ARGV[1])
 	netmask=ip2int(ARGV[2])
