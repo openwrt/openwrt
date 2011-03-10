e7c5412ff0275f29e94609c1faaa513b36b4573b
Hamish Guthrie <hcg@openwrt.org>
2011-03-10 10:37:02 +0000

    Add new profile for vpp platform
    SVN-Revision: 25997

---
+++ target/linux/omap35xx/gumstix/profiles/001-vpp.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2011 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+FEATURES:=ubifs
+
+define Profile/Vpp
+  NAME:=vpp
+  PACKAGES:=-iptables -ppp -ppp-mod-pppoe -kmod-ipt-nathelper \
+		-firewall -mtd -hotplug2 -udevtrigger -dropbear \
+		ext4 ubifs usb udev kmod-bluetooth ntpclient wpa-supplicant \
+		wireless-tools wpa-cli \
+		bluez-utils bluez-hcidump usbutils \
+		kmod-usb-serial kmod-usb-serial-cp210x kmod-usb-serial-ftdi \
+		kmod-usb-serial-pl2303 kmod-usb-uhci kmod-cfg80211 \
+		kmod-lib80211 kmod-libertas-sd kmod-mac80211 libnl-tiny \
+		crda iw kmod-usb-hid \
+		kmod-usb-storage kmod-usb-storage-extras kmod-fs-vfat \
+		uboot-omap35xx-omap3_overo \
+		hegw hegwtest mtd-utils ejre
+endef
+
+define Profile/Vpp/Description
+        Gumstix www.gumstix.com
+endef
+$(eval $(call Profile,Vpp))
