1dbabd309b922bb5782755f11615317068915da3
Felix Fietkau <nbd@openwrt.org>
2011-03-28 23:25:18 +0000

    scripts/diffconfig.sh: handle CONFIG_ALL, CONFIG_DEVEL and CONFIG_TOOLCHAINOPTS properly
    SVN-Revision: 26349

--- scripts/diffconfig.sh
+++ scripts/diffconfig.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 grep \^CONFIG_TARGET_ .config | head -n3 > tmp/.diffconfig.head
+grep '^CONFIG_ALL=y' .config >> tmp/.diffconfig.head
+grep '^CONFIG_DEVEL=y' .config >> tmp/.diffconfig.head
+grep '^CONFIG_TOOLCHAINOPTS=y' .config >> tmp/.diffconfig.head
 ./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage1 Config.in >/dev/null
 ./scripts/kconfig.pl '>+' tmp/.diffconfig.stage1 .config >> tmp/.diffconfig.head
 ./scripts/config/conf -D tmp/.diffconfig.head -w tmp/.diffconfig.stage2 Config.in >/dev/null
