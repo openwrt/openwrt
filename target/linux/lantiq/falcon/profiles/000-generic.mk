ba4dd89063210b5e8019a41996575f8780a1bef9
John Crispin <john@openwrt.org>
2011-04-02 12:56:11 +0000

    remove unknown package names from DEFAULT:=
    SVN-Revision: 26403

--- target/linux/lantiq/falcon/profiles/000-generic.mk
+++ target/linux/lantiq/falcon/profiles/000-generic.mk
@@ -5,8 +5,7 @@ define Profile/Generic
 	kmod-dm9000 \
 	kmod-i2c-core kmod-i2c-algo-bit kmod-i2c-gpio kmod-eeprom-at24 \
 	kmod-spi-bitbang kmod-spi-gpio kmod-eeprom-at25 \
-	gpon-dti-agent \
-	uboot-easy980x0_norflash uboot-easy980x0_serialflash uboot-easy98020
+	gpon-dti-agent
 endef
 
 $(eval $(call Profile,Generic))
--- target/linux/lantiq/falcon/profiles/001-lantiq.mk
+++ target/linux/lantiq/falcon/profiles/001-lantiq.mk
@@ -3,8 +3,7 @@ define Profile/EASY98000
   PACKAGES:= \
 	kmod-dm9000 \
 	kmod-i2c-core kmod-i2c-algo-bit kmod-i2c-gpio kmod-eeprom-at24 \
-	kmod-spi-bitbang kmod-spi-gpio kmod-eeprom-at25 \
-	uboot-easy980x0_norflash uboot-easy980x0_serialflash
+	kmod-spi-bitbang kmod-spi-gpio kmod-eeprom-at25
 endef
 
 define Profile/EASY98000/Description
