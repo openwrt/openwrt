624526ae1196f8d76e7012ee3b3ec0f6cace5041
Felix Fietkau <nbd@openwrt.org>
2011-04-02 12:43:21 +0000

    ar71xx: use DEFAULT:= in the uboot-ar71xx package instead of adding uboot to the profiles, fixes imagebuilder issues
    SVN-Revision: 26401

--- target/linux/ar71xx/generic/profiles/00-default.mk
+++ target/linux/ar71xx/generic/profiles/00-default.mk
@@ -9,7 +9,7 @@ define Profile/Default
 	NAME:=Default Profile (all drivers)
 	PACKAGES:= \
 		kmod-ath9k kmod-usb-core kmod-usb-ohci kmod-usb2 swconfig \
-		wpad-mini uboot-ar71xx-nbg460n_550n_550nh kmod-ledtrig-usbdev
+		wpad-mini kmod-ledtrig-usbdev
 endef
 
 define Profile/Default/Description
