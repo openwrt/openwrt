2e4239ef70303996ff296302fdc1b63943218500
Felix Fietkau <nbd@openwrt.org>
2011-04-03 23:30:26 +0000

    x86/geos: redux of cs5535 version
    The previous technique didn't work with "make defconfig" correctly.
    
    Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@26458 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/x86/geos/target.mk
+++ target/linux/x86/geos/target.mk
@@ -1,6 +1,6 @@
 BOARDNAME:=Traverse Geos
 FEATURES:=squashfs jffs2 ext4 pci usb gpio
-GEOS_GPIO = $(if $(findstring 1,$(call CompareKernelPatchVer,$(KERNEL_PATCHVER),ge,2.6.38)),gpio-cs5535-new,gpio-cs5535)
+GEOS_GPIO = $(if $(findstring 2.6.32,$(LINUX_VERSION)),gpio-cs5535,gpio-cs5535-new)
 DEFAULT_PACKAGES += kmod-ata-via-sata \
             kmod-crypto-hw-geode kmod-crypto-ocf \
             kmod-$(GEOS_GPIO) kmod-gpio-nsc \
