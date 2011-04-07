f6ae4bf15f2cd26a8e95f14e63f479dd72ef27d9
Gabor Juhos <juhosg@openwrt.org>
2011-04-07 20:53:14 +0000

    ar71xx: indicate AR934x support in menuconfig
    Signed-off-by: Jaiganesh Narayanan <jnarayanan@atheros.com>
    
    SVN-Revision: 26517

--- target/linux/ar71xx/generic/target.mk
+++ target/linux/ar71xx/generic/target.mk
@@ -1,7 +1,7 @@
 BOARDNAME:=Generic
 
 define Target/Description
-	Build firmware images for generic Atheros AR71xx/AR913x based boards.
+	Build firmware images for generic Atheros AR71xx/AR913x/AR934x based boards.
 endef
 
 
