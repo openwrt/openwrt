54ede248290368aadbd2012a3ca564e2e42bfef9
Gabor Juhos <juhosg@openwrt.org>
2011-04-12 09:29:10 +0000

    ar71xx: Add support for WZR-HP-G301NH
    Add support for the Buffalo WZR-HP-G301NH. The only difference between it
    and the WZR-HP-G00NH is that it has a RTL8366RB instead of a RTL8366S.
    Since we don't do runtime detection of the switch, we need a separate
    machine definition for it.
    
    While we are at it, also rename the profile to reflect that it now is for
    more than one device.
    
    Signed-off-by: Jonas Gorski <jonas.gorski+openwrt@gmail.com>
    
    SVN-Revision: 26601

--- target/linux/ar71xx/base-files/etc/diag.sh
+++ target/linux/ar71xx/base-files/etc/diag.sh
@@ -123,7 +123,7 @@ get_status_led() {
 	wrt160nl)
 		status_led="wrt160nl:blue:wps"
 		;;
-	wzr-hp-g300nh)
+	wzr-hp-g300nh | wzr-hp-g301nh)
 		status_led="wzr-hp-g300nh:green:router"
 		;;
 	zcn-1523h-2 | zcn-1523h-5)
