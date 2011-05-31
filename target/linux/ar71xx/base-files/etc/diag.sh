650c7cc2fdf1b658be22d970e5d55859a726f0ac
Gabor Juhos <juhosg@openwrt.org>
2011-05-31 22:53:13 +0000

    ar71xx: use the diag LED for diagnostic on the WZR-HP-G30XNH boards
    SVN-Revision: 27043

--- target/linux/ar71xx/base-files/etc/diag.sh
+++ target/linux/ar71xx/base-files/etc/diag.sh
@@ -124,7 +124,7 @@ get_status_led() {
 		status_led="wrt160nl:blue:wps"
 		;;
 	wzr-hp-g300nh | wzr-hp-g301nh)
-		status_led="wzr-hp-g300nh:green:router"
+		status_led="wzr-hp-g300nh:red:diag"
 		;;
 	zcn-1523h-2 | zcn-1523h-5)
 		status_led="zcn-1523h:amber:init"
