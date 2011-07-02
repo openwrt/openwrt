c163347f4b237de68e0fc431ee35912ce416d96d
Felix Fietkau <nbd@openwrt.org>
2011-07-02 06:49:09 +0000

    add support for hidden packages that get selected/built but do not show up in menuconfig
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@27344 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/metadata.pm
+++ scripts/metadata.pm
@@ -114,6 +114,7 @@ sub parse_package_metadata($) {
 		};
 		/^Menu-Depends: \s*(.+)\s*$/ and $pkg->{mdepends} = [ split /\s+/, $1 ];
 		/^Depends: \s*(.+)\s*$/ and $pkg->{depends} = [ split /\s+/, $1 ];
+		/^Hidden: \s*(.+)\s*$/ and $pkg->{hidden} = 1;
 		/^Build-Variant: \s*([\w\-]+)\s*/ and $pkg->{variant} = $1;
 		/^Build-Only: \s*(.+)\s*$/ and $pkg->{buildonly} = 1;
 		/^Build-Depends: \s*(.+)\s*$/ and $pkg->{builddepends} = [ split /\s+/, $1 ];
