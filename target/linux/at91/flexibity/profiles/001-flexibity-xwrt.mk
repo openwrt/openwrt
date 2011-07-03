98eec3e323322222a9a4a6439c6dbd5b11a6ed42
John Crispin <blogic@openwrt.org>
2011-07-03 18:51:59 +0000

    AT91: Add flexibity board support
    The patch below adds AT91 Flexibity Connect board support.
    
    Signed-off-by: Maxim Osipov <maxim.osipov@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@27412 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/at91/flexibity/profiles/001-flexibity-xwrt.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2008 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/flexibity-xwrt
+  NAME:=Flexibity Connect (XWrt)
+  PACKAGES:=bridge wireless-tools block-mount modprobe dnsmasq hotplug2 udev webif webif-applications webif-flexibity \
+	syslog-ng sudo transmission-web miniupnpd nmap-ssl ip ntpclient ntpdate chat crda e169-stats genl hostapd-utils \
+	iw madwimax netcat portmap ppp ppp-mod-pppoe pptp tcpdump usb-modem usb-modem-huawei-e175x usb-modem-huawei-eg162 \
+	usb-modem-nokia-5800 wpa-cli wpa-supplicant motion badblocks blkid cifsmount disktype dosfsck dosfslabel e2fsprogs \
+	fuse-utils mkdosfs nfs-utils ntfs-3g ntfs-3g-utils reiserfsprogs resize2fs sysfsutils tune2fs uuidgen certtool \
+	gnutls-utils picocom setterm unrar unzip sqlite3-cli alsa-utils anyremote bluez-utils bzip2 comgt crypto-tools \
+	file flock gdbserver gnupg gpioctl gsm-utils gzip huaweiaktbbo hwclock i2c-tools input-utils ldd lsof mdadm \
+	module-init-tools mount-utils openssl-util procps psmisc px5g screen strace stress sysstat uboot-envtools \
+	usb-modeswitch usb-modeswitch-data usbutils lua
+endef
+
+define Profile/flexibity-xwrt/Description
+	Complete packages set for the Flexibity Connect device (XWrt).
+endef
+
+$(eval $(call Profile,flexibity-xwrt))
+
