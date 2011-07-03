c9f3e7082ff7d08851e71bacbebaf4c2ae5be340
Gabor Juhos <juhosg@openwrt.org>
2011-07-03 15:01:43 +0000

    ramips: disable watchdog on sysupgrade
    SVN-Revision: 27380

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -36,3 +36,13 @@ platform_do_upgrade() {
 		;;
 	esac
 }
+
+disable_watchdog() {
+	killall watchdog
+	( ps | grep -v 'grep' | grep '/dev/watchdog' ) && {
+		echo 'Could not disable watchdog'
+		return 1
+	}
+}
+
+append sysupgrade_pre_upgrade disable_watchdog
