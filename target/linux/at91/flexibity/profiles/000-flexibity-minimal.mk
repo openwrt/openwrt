349e97f87939460050352725ff6c6be6962aa2ab
John Crispin <john@openwrt.org>
2011-07-03 18:51:59 +0000

    AT91: Add flexibity board support
    The patch below adds AT91 Flexibity Connect board support.
    
    Signed-off-by: Maxim Osipov <maxim.osipov@gmail.com>
    
    SVN-Revision: 27412

---
+++ target/linux/at91/flexibity/profiles/000-flexibity-minimal.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2008 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/flexibity-minimal
+  NAME:=Flexibity Connect (minimal)
+  PACKAGES:=
+endef
+
+define Profile/flexibity-minimal/Description
+	Minimal packages set for the Flexibity Connect device.
+endef
+
+$(eval $(call Profile,flexibity-minimal))
+
---
+++ target/linux/at91/flexibity/target.mk
@@ -0,0 +1,16 @@
+# 
+# Copyright (C) 2011 Flexibity
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+SUBTARGET:=flexibity
+BOARDNAME:=Flexibity Connect (AT91)
+FEATURES:=ext2 tgz usb
+LINUX_VERSION:=2.6.39
+
+define Target/Description
+  Build fimware images for Flexibity Connect board.
+endef
+
