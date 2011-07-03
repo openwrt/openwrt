521e495f4bd2315f12b2dfc4b690e57b1f0fee6e
Gabor Juhos <juhosg@openwrt.org>
2011-07-03 15:01:56 +0000

    ramips: add all supported boards to lib/ramips.sh
    SVN-Revision: 27389

--- target/linux/ramips/base-files/lib/ramips.sh
+++ target/linux/ramips/base-files/lib/ramips.sh
@@ -10,11 +10,29 @@ ramips_board_name() {
 	machine=$(awk 'BEGIN{FS="[ \t]+:[ \t]"} /machine/ {print $2}' /proc/cpuinfo)
 
 	case "$machine" in
+	*"Aztech HW550-3G")
+		name="hw550-3g"
+		;;
 	*"DIR-300 revB")
 		name="dir-300-b1"
 		;;
+	*"F5D8235 v2")
+		name="f5d8235-v2"
+		;;
 	*"La Fonera 2.0N")
 		name="fonera20n"
+		;;
+	*"MoFi Network MOFI3500-3GN")
+		name="mofi3500-3gn"
+		;;
+	*"PWH2004")
+		name="pwh2004"
+		;;
+	*"RT-N15")
+		name="rt-n15"
+		;;
+	*"WCR-150GN")
+		name="wcr-150gn"
 		;;
 	*"V22RW-2X2")
 		name="v22rw-2x2"
@@ -22,11 +40,8 @@ ramips_board_name() {
 	*"WHR-G300N")
 		name="whr-g300n"
 		;;
-	*"Aztech HW550-3G")
-		name="hw550-3g"
-		;;
-	*"MoFi Network MOFI3500-3GN")
-		name="mofi3500-3gn"
+	*"WZR-AGL300NH")
+		name="wzr-agl300nh"
 		;;
 	*)
 		name="generic"
