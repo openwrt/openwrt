43771202d35da68615a4af0743816aabe97a85e4
John Crispin <john@openwrt.org>
2011-07-03 17:38:11 +0000

    update lantiq/falcon patches
    SVN-Revision: 27406

---
+++ target/linux/lantiq/falcon-stable/target.mk
@@ -0,0 +1,11 @@
+
+include $(PLATFORM_SUBDIR)/../falcon/target.mk
+
+LINUX_VERSION:=2.6.32.33
+
+SUBTARGET:=falcon-stable
+BOARDNAME:=Falcon-Stable
+
+define Target/Description
+	Lantiq Falcon (Stable Kernel)
+endef
