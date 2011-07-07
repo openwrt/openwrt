f177052c9d80b02308f5f86ae53fea4b3c078c1a
John Crispin <john@openwrt.org>
2011-07-07 18:20:16 +0000

    remove ar9 board from xway profiles
    SVN-Revision: 27534

--- target/linux/lantiq/xway/profiles/001-lantiq.mk
+++ target/linux/lantiq/xway/profiles/001-lantiq.mk
@@ -8,15 +8,3 @@ define Profile/EASY50712/Description
 endef
 
 $(eval $(call Profile,EASY50712))
-
-define Profile/EASY50812
-  NAME:=EASY50812
-  PACKAGES:= kmod-usb-core kmod-usb-dwc-otg kmod-leds-gpio
-endef
-
-define Profile/EASY50812/Description
-	Lantiq EASY50812 evalkit
-endef
-
-$(eval $(call Profile,EASY50812))
-
