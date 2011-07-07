09d547eae40e6ee47990121e9d7450e677b0f1dd
John Crispin <john@openwrt.org>
2011-07-07 18:20:21 +0000

    rename xway profile to danube and remove ar9 specifics
    SVN-Revision: 27535

--- target/linux/lantiq/danube/target.mk
+++ target/linux/lantiq/danube/target.mk
@@ -1,10 +1,10 @@
 ARCH:=mips
-SUBTARGET:=xway
-BOARDNAME:=Xway
+SUBTARGET:=danube
+BOARDNAME:=Danube
 FEATURES:=squashfs jffs2 atm
 
 DEFAULT_PACKAGES+=kmod-pppoa ppp-mod-pppoa linux-atm atm-tools br2684ctl kmod-ltq-dsl ltq-dsl-app swconfig
 
 define Target/Description
-	Lantiq XWAY (danube/twinpass/ar9)
+	Lantiq Danube/Twinpass
 endef
