2fd0b76c6be538662aafbf25d2c7969b69090739
Daniel Dickinson <daniel@cshore.neomailbox.net>
2011-07-07 03:09:20 +0000

    [ar7] base-files: diag.sh: added failsafe led definition
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@27502 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar7/base-files/etc/diag.sh
+++ target/linux/ar7/base-files/etc/diag.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Copyright (C) 2007 OpenWrt.org
 
-# This setup gives us 3.5 distinguishable states:
+# This setup gives us 4.5 distinguishable states:
 #
 # Solid OFF:  Bootloader running, or kernel hung (timer task stalled)
 # Solid ON:   Kernel hung (timer task stalled)
 # 5Hz blink:  preinit
+# 10Hz blink: failsafe
 # Heartbeat:  normal operation
 
 set_state() {
@@ -15,6 +16,13 @@ set_state() {
                                 echo timer >/sys/class/leds/status/trigger
                                 echo 100 >/sys/class/leds/status/delay_on
                                 echo 100 >/sys/class/leds/status/delay_off
+                        }
+                ;;
+                failsafe)
+                        [ -d /sys/class/leds/status ] && {
+                                echo timer >/sys/class/leds/status/trigger
+                                echo 50 >/sys/class/leds/status/delay_on
+                                echo 50 >/sys/class/leds/status/delay_off
                         }
                 ;;
                 done)
