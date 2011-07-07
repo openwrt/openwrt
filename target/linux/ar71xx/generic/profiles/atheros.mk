e9e6aeacc0072399742464d9126c93bba9cee9ef
Gabor Juhos <juhosg@openwrt.org>
2011-07-07 13:01:48 +0000

    ar71xx: add separate profiles for the AP121 and AP121-MINI boards
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@27522 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/generic/profiles/atheros.mk
+++ target/linux/ar71xx/generic/profiles/atheros.mk
@@ -5,6 +5,28 @@
 # See /LICENSE for more information.
 #
 
+define Profile/AP121
+	NAME:=Atheros AP121 reference board
+	PACKAGES:=wpad-mini kmod-ath9k kmod-usb-core kmod-usb2
+endef
+
+define Profile/AP121/Description
+	Package set optimized for the Atheros AP121 reference board.
+endef
+
+$(eval $(call Profile,AP121))
+
+define Profile/AP121MINI
+	NAME:=Atheros AP121-MINI reference board
+	PACKAGES:=wpad-mini kmod-ath9k
+endef
+
+define Profile/AP121MINI/Description
+	Package set optimized for the Atheros AP121-MINI reference board.
+endef
+
+$(eval $(call Profile,AP121MINI))
+
 define Profile/AP81
 	NAME:=Atheros AP81 reference board
 	PACKAGES:=wpad-mini kmod-ath9k kmod-usb-core kmod-usb2
