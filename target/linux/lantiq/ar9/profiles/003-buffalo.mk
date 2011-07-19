3833b7be24ba9f8cb847de8197e60147068afe67
John Crispin <john@openwrt.org>
2011-07-19 18:06:33 +0000

    add support for more lantiq AR9 boards
    SVN-Revision: 27694

---
+++ target/linux/lantiq/ar9/profiles/003-buffalo.mk
@@ -0,0 +1,11 @@
+define Profile/WBMR
+  NAME:=WBMR - Buffalo WBMR-HP-G300H
+  PACKAGES:= kmod-usb-core kmod-usb-dwc-otg kmod-leds-gpio \
+	 kmod-ltq-dsl-firmware-b kmod-ledtrig-usbdev 
+endef
+
+define Profile/WBMR/Description
+	Package set optimized for the Buffalo WBMR routers
+endef
+
+$(eval $(call Profile,WBMR))
