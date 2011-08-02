61e2e0331160559bd82469944e4f0a17f95f2935
Jonas Gorski <jogo@openwrt.org>
2011-08-02 09:53:19 +0000

    kernel: Fix firewire for 2.6.37+
    The old ieee1394 stack was removed in 2.6.37. The new firewire stack is
    available for all kernel versions, but experimental for the older one, so
    make both available where appropriate.
    
    SVN-Revision: 27874

--- package/kernel/modules/firewire.mk
+++ package/kernel/modules/firewire.mk
@@ -15,15 +15,15 @@ endef
 
 define KernelPackage/ieee1394
   SUBMENU:=$(FIREWIRE_MENU)
-  TITLE:=Support for FireWire
-  DEPENDS:=@PCI_SUPPORT
+  TITLE:=Support for FireWire (old stack)
+  DEPENDS:=@PCI_SUPPORT @LINUX_2_6_30||LINUX_2_6_31||LINUX_2_6_32||LINUX_2_6_36
   KCONFIG:=CONFIG_IEEE1394
   FILES:=$(LINUX_DIR)/drivers/ieee1394/ieee1394.ko
   AUTOLOAD:=$(call AutoLoad,20,ieee1394)
 endef
 
 define KernelPackage/ieee1394/description
- Kernel support for FireWire
+ Kernel support for FireWire (old stack)
 endef
 
 $(eval $(call KernelPackage,ieee1394))
@@ -89,4 +89,72 @@ define KernelPackage/video1394/description
 endef
 
 $(eval $(call KernelPackage,video1394))
+
+
+define KernelPackage/firewire
+  SUBMENU:=$(FIREWIRE_MENU)
+  TITLE:=Support for FireWire (new stack)
+  DEPENDS:=@PCI_SUPPORT
+  KCONFIG:=CONFIG_FIREWIRE
+  FILES:=$(LINUX_DIR)/drivers/firewire/firewire-core.ko
+  AUTOLOAD:=$(call AutoLoad,20,firewire-core)
+endef
+
+define KernelPackage/firewire/description
+  Kernel support for FireWire (new stack)
+endef
+
+$(eval $(call KernelPackage,firewire))
+
+
+define KernelPackage/firewire-ohci
+  SUBMENU:=$(FIREWIRE_MENU)
+  TITLE:=Support for OHCI-1394 controllers
+  DEPENDS:=kmod-firewire
+  KCONFIG:= \
+	CONFIG_FIREWIRE_OHCI \
+	CONFIG_FIREWIRE_OHCI_DEBUG=n \
+	CONFIG_FIREWIRE_OHCI_REMOTE_DMA=n
+  FILES:=$(LINUX_DIR)/drivers/firewire/firewire-ohci.ko
+  AUTOLOAD:=$(call AutoLoad,50,firewire-ohci)
+endef
+
+
+define KernelPackage/firewire-ohci/description
+ Kernel support for FireWire OHCI-1394 controllers
+endef
+
+$(eval $(call KernelPackage,firewire-ohci))
+
+
+define KernelPackage/firewire-sbp2
+  SUBMENU:=$(FIREWIRE_MENU)
+  TITLE:=Support for SBP-2 devices over FireWire
+  DEPENDS:=kmod-firewire +!TARGET_x86:kmod-scsi-core
+  KCONFIG:=CONFIG_FIREWIRE_SBP2
+  FILES:=$(LINUX_DIR)/drivers/firewire/firewire-sbp2.ko
+  AUTOLOAD:=$(call AutoLoad,50,firewire-sbp2)
+endef
+
+define KernelPackage/firewire-sbp2/description
+ Kernel support for SBP-2 devices over FireWire
+endef
+
+$(eval $(call KernelPackage,firewire-sbp2))
+
+
+define KernelPackage/firewire-net
+  SUBMENU:=$(FIREWIRE_MENU)
+  TITLE:=Support for IP networking over FireWire
+  DEPENDS:=kmod-firewire @!LINUX_2_6_30
+  KCONFIG:=CONFIG_FIREWIRE_NET
+  FILES:=$(LINUX_DIR)/drivers/firewire/firewire-net.ko
+  AUTOLOAD:=$(call AutoLoad,50,firewire-net)
+endef
+
+define KernelPackage/firewire-net/description
+ Kernel support for IPv4 over FireWire
+endef
+
+$(eval $(call KernelPackage,firewire-net))
 
