a5f2fd63598414246a497e08a4433a0116f2f606
Jonas Gorski <jogo@openwrt.org>
2011-08-09 12:19:38 +0000

    include/kernel.mk: fix download path for linux 3.x
    SVN-Revision: 27939

--- include/kernel.mk
+++ include/kernel.mk
@@ -48,7 +48,11 @@ else
   LINUX_SOURCE:=linux-$(LINUX_VERSION).tar.bz2
   TESTING:=$(if $(findstring -rc,$(LINUX_VERSION)),/testing,)
   ifeq ($(call qstrip,$(CONFIG_EXTERNAL_KERNEL_TREE)),)
-    LINUX_SITE:=@KERNEL/linux/kernel/v$(KERNEL)$(TESTING)
+    ifeq ($(word 1,$(subst ., ,$(KERNEL_BASE))),3)
+      LINUX_SITE:=@KERNEL/linux/kernel/v3.x$(TESTING)
+    else
+      LINUX_SITE:=@KERNEL/linux/kernel/v$(KERNEL)$(TESTING)
+    endif
   endif
 
   ifneq ($(TARGET_BUILD),1)
