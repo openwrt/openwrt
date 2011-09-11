1bdf65f580b50bacc4637abbb3409546e92653e9
Felix Fietkau <nbd@openwrt.org>
2011-09-11 17:44:12 +0000

    ar71xx: fix register range check for DMA stuck checks (thx, Frédéric Moulins)
    SVN-Revision: 28213

--- target/linux/ar71xx/files/drivers/net/ag71xx/ag71xx.h
+++ target/linux/ar71xx/files/drivers/net/ag71xx/ag71xx.h
@@ -354,7 +354,7 @@ static inline void ag71xx_check_reg_offset(struct ag71xx *ag, unsigned reg)
 {
 	switch (reg) {
 	case AG71XX_REG_MAC_CFG1 ... AG71XX_REG_MAC_MFL:
-	case AG71XX_REG_MAC_IFCTL ... AG71XX_REG_INT_STATUS:
+	case AG71XX_REG_MAC_IFCTL ... AG71XX_REG_TX_SM:
 	case AG71XX_REG_MII_CFG:
 		break;
 
