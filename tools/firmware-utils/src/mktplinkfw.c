9faace43279c56b213651b560dd1d763ba728da5
Gabor Juhos <juhosg@openwrt.org>
2011-09-21 11:47:39 +0000

    firmware-utils/mktplinkfw: add support for TL-WR740N v3
    Signed-off-by: Martin Raykov <martin.raykov@gmail.com>
    
    SVN-Revision: 28268

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -39,6 +39,7 @@
 #define HWID_TL_WA901ND_V2	0x09010002
 #define HWID_TL_WR741ND_V1	0x07410001
 #define HWID_TL_WR740N_V1	0x07400001
+#define HWID_TL_WR740N_V3	0x07400300
 #define HWID_TL_WR841N_V1_5	0x08410002
 #define HWID_TL_WR841ND_V3	0x08410003
 #define HWID_TL_WR841ND_V5	0x08410005
@@ -163,6 +164,14 @@ static struct board_info boards[] = {
 	}, {
 		.id		= "TL-WR740Nv1",
 		.hw_id		= HWID_TL_WR740N_V1,
+		.hw_rev		= 1,
+		.fw_max_len	= 0x3c0000,
+		.kernel_la	= 0x80060000,
+		.kernel_ep	= 0x80060000,
+		.rootfs_ofs	= 0x140000,
+	}, {
+		.id		= "TL-WR740Nv3",
+		.hw_id		= HWID_TL_WR740N_V3,
 		.hw_rev		= 1,
 		.fw_max_len	= 0x3c0000,
 		.kernel_la	= 0x80060000,
