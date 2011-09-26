32b5ea459c24eb0448dce2d8f8a7daffd39c7a2b
Jo-Philipp Wich <jow@openwrt.org>
2011-09-26 02:02:35 +0000

    [include] image.mk: pass through $(CONFIG_PKG_BUILD_JOBS) to the mksquashfs -processors option
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@28305 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/image.mk
+++ include/image.mk
@@ -87,7 +87,7 @@ else
   ifneq ($(CONFIG_TARGET_ROOTFS_SQUASHFS),)
     define Image/mkfs/squashfs
 		@mkdir -p $(TARGET_DIR)/overlay
-		$(STAGING_DIR_HOST)/bin/mksquashfs4 $(TARGET_DIR) $(KDIR)/root.squashfs -nopad -noappend -root-owned -comp $(SQUASHFSCOMP) -processors 1
+		$(STAGING_DIR_HOST)/bin/mksquashfs4 $(TARGET_DIR) $(KDIR)/root.squashfs -nopad -noappend -root-owned -comp $(SQUASHFSCOMP) -processors $(if $(CONFIG_PKG_BUILD_JOBS),$(CONFIG_PKG_BUILD_JOBS),1)
 		$(call Image/Build,squashfs)
     endef
   endif
