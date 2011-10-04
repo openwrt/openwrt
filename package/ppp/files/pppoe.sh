64f4a89e7c4f3e0ad20eec5c6ca96746438d342c
Jo-Philipp Wich <jow@openwrt.org>
2011-10-04 23:37:53 +0000

    ppp: implement pppoe specific uci options "ac" and "service" which map to "rp_pppoe_ac" and "rp_pppoe_service"
    SVN-Revision: 28366

--- package/ppp/files/pppoe.sh
+++ package/ppp/files/pppoe.sh
@@ -5,15 +5,26 @@ stop_interface_pppoe() {
 setup_interface_pppoe() {
 	local iface="$1"
 	local config="$2"
-	
+
 	for module in slhc ppp_generic pppox pppoe; do
 		/sbin/insmod $module 2>&- >&-
 	done
 
-	config_get mtu "$config" mtu
-	mtu=${mtu:-1492}
+	local mtu
+	config_get mtu "$config" mtu 1492
+
+	local ac
+	config_get ac "$config" ac
+
+	local service
+	config_get service "$config" service
+
+	# NB: the first nic-* argument will be moved to the
+	#     end of the argument list by start_pppd()
 	start_pppd "$config" \
 		"nic-$iface" \
+		${ac:+rp_pppoe_ac "$ac"} \
+		${service:+rp_pppoe_service "$service"} \
 		plugin rp-pppoe.so \
 		mtu $mtu mru $mtu
 }
