f8cfea80ae98ed9da3aa19f58182ce677872e372
Gabor Juhos <juhosg@openwrt.org>
2011-11-01 11:20:54 +0000

    firmware-utils/mktplinkfw: add TL-WR741ND v4
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@28707 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -39,6 +39,7 @@
 #define HWID_TL_WA901ND_V2	0x09010002
 #define HWID_TL_WR703N_V1	0x07030101
 #define HWID_TL_WR741ND_V1	0x07410001
+#define HWID_TL_WR741ND_V4	0x07410004
 #define HWID_TL_WR740N_V1	0x07400001
 #define HWID_TL_WR740N_V3	0x07400003
 #define HWID_TL_WR743ND_V1	0x07430001
@@ -163,6 +164,14 @@ static struct board_info boards[] = {
 		.kernel_la	= 0x80060000,
 		.kernel_ep	= 0x80060000,
 		.rootfs_ofs	= 0x140000,
+	}, {
+		.id		= "TL-WR741NDv4",
+		.hw_id		= HWID_TL_WR741ND_V4,
+		.hw_rev		= 1,
+		.fw_max_len	= 0x3c0000,
+		.kernel_la	= 0x80060000,
+		.kernel_ep	= 0x80060000,
+		.rootfs_ofs	= 0x100000,
 	}, {
 		.id		= "TL-WR740Nv1",
 		.hw_id		= HWID_TL_WR740N_V1,
