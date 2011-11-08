daca6a6a97f89214b6fbfccf7989b4c17bde3da6
Jo-Philipp Wich <jow@openwrt.org>
2011-11-08 18:48:38 +0000

    base-files: do not assume that "top" is busybox' top, but try to call the applet explicitely
    SVN-Revision: 28862

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -80,7 +80,7 @@ run_ramfs() { # <command> [...]
 kill_remaining() { # [ <signal> ]
 	local sig="${1:-TERM}"
 	echo -n "Sending $sig to remaining processes ... "
-	top -bn1 | while read pid ppid user stat vsz pvsz pcpu cmd args; do
+	/bin/busybox top -bn1 2>/dev/null | while read pid ppid user stat vsz pvsz pcpu cmd args; do
 		case "$pid" in
 			[0-9]*) : ;;
 			*) continue ;;
