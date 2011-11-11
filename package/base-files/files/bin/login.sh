20ff304ed90da0fa22b3d550fe1045d53eed5a0e
Jo-Philipp Wich <jow@openwrt.org>
2011-11-11 13:25:31 +0000

    base-files: now that the initial password is completely empty (and not primed with "!") adjust login.sh for it to make telnetd available again
    SVN-Revision: 28943

--- package/base-files/files/bin/login.sh
+++ package/base-files/files/bin/login.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Copyright (C) 2006-2010 OpenWrt.org
+# Copyright (C) 2006-2011 OpenWrt.org
 
-if grep -qs '^root:[^!]' /etc/passwd /etc/shadow && [ -z "$FAILSAFE" ]; then
+if grep -qs '^root:[^!:]' /etc/passwd /etc/shadow && [ -z "$FAILSAFE" ]; then
 	echo "Login failed."
 	exit 0
 else
