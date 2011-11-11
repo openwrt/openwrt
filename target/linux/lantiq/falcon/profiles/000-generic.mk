fda4ee22a04670489b3a4254783d8075fece9878
John Crispin <john@openwrt.org>
2011-11-11 21:59:11 +0000

    lantiq: improve falcon package selection
    SVN-Revision: 28962

--- target/linux/lantiq/falcon/profiles/000-generic.mk
+++ target/linux/lantiq/falcon/profiles/000-generic.mk
@@ -1,9 +1,8 @@
 define Profile/Generic
   NAME:=Generic - all boards
   PACKAGES:= \
-	kmod-leds-gpio \
 	kmod-dm9000 \
-	kmod-i2c-core kmod-i2c-algo-bit kmod-i2c-gpio kmod-eeprom-at24 \
+	kmod-i2c-core kmod-i2c-falcon-lantiq kmod-eeprom-at24 \
 	kmod-spi-bitbang kmod-spi-gpio kmod-eeprom-at25 \
 	gpon-dti-agent
 endef
--- target/linux/lantiq/falcon/target.mk
+++ target/linux/lantiq/falcon/target.mk
@@ -4,7 +4,7 @@ BOARDNAME:=Falcon
 FEATURES:=squashfs jffs2
 DEVICE_TYPE:=other
 
-DEFAULT_PACKAGES+= kmod-ifxos gpon-base-files kmod-leds-gpio \
+DEFAULT_PACKAGES+= kmod-ifxos gpon-base-files kmod-leds-gpio kmod-ledtrig-heartbeat \
 	kmod-gpon-optic-drv gpon-optic-drv kmod-gpon-onu-drv gpon-onu-drv \
 	gpon-pe-firmware gpon-omci-api gpon-omci-onu gpon-luci
 
