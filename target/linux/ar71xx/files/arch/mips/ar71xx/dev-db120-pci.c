81359fd6752ffcb8f8b801268066e2f8c85c8b77
Gabor Juhos <juhosg@openwrt.org>
2011-11-13 12:48:03 +0000

    ar71xx: fixup onboard PCIe chip registration on the DB120 board
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29022 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/dev-db120-pci.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/dev-db120-pci.c
@@ -2,6 +2,7 @@
  *  Atheros db120 reference board PCI initialization
  *
  *  Copyright (C) 2010-2011 Jaiganesh Narayanan <jnarayanan@atheros.com>
+ *  Copyright (C) 2011 Gabor Juhos <juhosg@openwrt.org>
  *
  *  Parts of this file are based on Atheros linux 2.6.31 BSP
  *
@@ -11,11 +12,17 @@
  */
 
 #include <linux/pci.h>
+#include <linux/ath9k_platform.h>
 
 #include <asm/mach-ar71xx/ar71xx.h>
 #include <asm/mach-ar71xx/pci.h>
 
 #include "dev-db120-pci.h"
+
+static struct ath9k_platform_data db120_wmac_data = {
+	.led_pin = -1,
+};
+static char db120_wmac_mac[6];
 
 static struct ar71xx_pci_irq db120_pci_irqs[] __initdata = {
 	{
@@ -25,7 +32,28 @@ static struct ar71xx_pci_irq db120_pci_irqs[] __initdata = {
 	}
 };
 
-void __init db120_pci_init(void)
+static int db120_pci_plat_dev_init(struct pci_dev *dev)
 {
+	switch (PCI_SLOT(dev->devfn)) {
+	case 0:
+		dev->dev.platform_data = &db120_wmac_data;
+		break;
+	}
+
+	return 0;
+}
+
+void __init db120_pci_init(u8 *cal_data, u8 *mac_addr)
+{
+	if (cal_data)
+		memcpy(db120_wmac_data.eeprom_data, cal_data,
+		       sizeof(db120_wmac_data.eeprom_data));
+
+	if (mac_addr) {
+		memcpy(db120_wmac_mac, mac_addr, sizeof(db120_wmac_mac));
+		db120_wmac_data.macaddr = db120_wmac_mac;
+	}
+
+	ar71xx_pci_plat_dev_init = db120_pci_plat_dev_init;
 	ar71xx_pci_init(ARRAY_SIZE(db120_pci_irqs), db120_pci_irqs);
 }
