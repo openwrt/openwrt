dcab0017e49c438fbbf4fd4690b15ff232c9a3ac
Gabor Juhos <juhosg@openwrt.org>
2011-11-24 20:09:43 +0000

    ar71xx: modify phy mask of the WP543 board
    This fixes ethernet on newer boards. Based on a patch from #9702.
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29328 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ar71xx/mach-wp543.c
+++ target/linux/ar71xx/files/arch/mips/ar71xx/mach-wp543.c
@@ -77,11 +77,11 @@ static void __init wp543_setup(void)
 {
 	ar71xx_add_device_m25p80(NULL);
 
-	ar71xx_add_device_mdio(0, 0xfffffff7);
+	ar71xx_add_device_mdio(0, 0xfffffff0);
 
 	ar71xx_init_mac(ar71xx_eth0_data.mac_addr, ar71xx_mac_base, 0);
 	ar71xx_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
-	ar71xx_eth0_data.phy_mask = 0x08;
+	ar71xx_eth0_data.phy_mask = 0x0f;
 	ar71xx_eth0_data.reset_bit = RESET_MODULE_GE0_MAC |
 				     RESET_MODULE_GE0_PHY;
 	ar71xx_add_device_eth(0);
