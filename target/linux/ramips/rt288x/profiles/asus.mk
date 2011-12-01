85c9f9860b673adeb3e4b4b573ba0789c3db45b3
Gabor Juhos <juhosg@openwrt.org>
2011-12-01 22:48:55 +0000

    ramips: add profile for the Asus RT-N15
    This uses previously submitted rtl8366 kernel packages.
    
    [juhosg: The kmod-gpio-dev package has been removed from the package list.]
    
    Signed-off-by: Roman Yeryomin <roman@advem.lv>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29391 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/rt288x/profiles/asus.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2011 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/RTN15
+       NAME:=Asus RT-N15
+       PACKAGES:=kmod-switch-rtl8366s kmod-swconfig swconfig
+endef
+
+define Profile/RTN15/Description
+       Package set for Asus RT-N15 board
+endef
+
+$(eval $(call Profile,RTN15))
+
