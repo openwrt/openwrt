e3017bc05e316ade69484923c2ea9c26cdc4802e
Felix Fietkau <nbd@openwrt.org>
2011-12-14 20:21:36 +0000

    ath9k: add support for registering extra leds connected to the wmac gpio lines
    SVN-Revision: 29530

--- target/linux/generic/files/include/linux/ath9k_platform.h
+++ target/linux/generic/files/include/linux/ath9k_platform.h
@@ -32,6 +32,9 @@ struct ath9k_platform_data {
 	bool is_clk_25mhz;
 	int (*get_mac_revision)(void);
 	int (*external_reset)(void);
+
+	int num_leds;
+	const struct gpio_led *leds;
 };
 
 #endif /* _LINUX_ATH9K_PLATFORM_H */
