dde2004075d9aa93c91b65964f0d1417b9ec1347
Gabor Juhos <juhosg@openwrt.org>
2012-01-04 17:30:53 +0000

    firmware-utils/mktplinkfw: add support for TL-WA701N/ND
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29655 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -33,6 +33,7 @@
 #define HWID_TL_MR3020_V1	0x30200001
 #define HWID_TL_MR3220_V1	0x32200001
 #define HWID_TL_MR3420_V1	0x34200001
+#define HWID_TL_WA701N_V1	0x07010001
 #define HWID_TL_WA901ND_V1	0x09010001
 #define HWID_TL_WA901ND_V2	0x09010002
 #define HWID_TL_WR703N_V1	0x07030101
@@ -174,6 +175,11 @@ static struct board_info boards[] = {
 	}, {
 		.id		= "TL-MR3420v1",
 		.hw_id		= HWID_TL_MR3420_V1,
+		.hw_rev		= 1,
+		.layout_id	= "4M",
+	}, {
+		.id		= "TL-WA701Nv1",
+		.hw_id		= HWID_TL_WA701N_V1,
 		.hw_rev		= 1,
 		.layout_id	= "4M",
 	}, {
