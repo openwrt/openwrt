df72f38897c73736e2bb7d00861f07fdf9bee67e
Gabor Juhos <juhosg@openwrt.org>
2012-01-04 21:13:57 +0000

    ar71xx: create profile and build image for the ALFA AP96 board
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29663 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/generic/profiles/alfa.mk
+++ target/linux/ar71xx/generic/profiles/alfa.mk
@@ -5,6 +5,18 @@
 # See /LICENSE for more information.
 #
 
+define Profile/ALFAAP96
+	NAME:=ALFA Network AP96  board
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-rtc-pcf2123
+endef
+
+define Profile/ALFAAP96/Description
+	Package set optimized for the ALFA Network AP96 board.
+endef
+
+$(eval $(call Profile,ALFAAP96))
+
+
 define Profile/HORNETUB
 	NAME:=ALFA Network Hornet-UB board
 	PACKAGES:=kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev
