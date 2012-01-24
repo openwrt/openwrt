1932a7205c4497e6120534fd94087669dba1a398
Gabor Juhos <juhosg@openwrt.org>
2012-01-24 11:48:45 +0000

    ramips: add kernel support for ALL0256N
    Signed-off-by: Daniel Golle <dgolle@allnet.de>
    
    SVN-Revision: 29882

--- target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
@@ -20,6 +20,7 @@ enum ramips_mach_type {
 	RAMIPS_MACH_WZR_AGL300NH,	/* Buffalo WZR-AGL300NH */
 
 	/* RT3050 based machines */
+	RAMIPS_MACH_ALL0256N,		/* Allnet ALL0256N */
 	RAMIPS_MACH_DIR_300_B1,		/* D-Link DIR-300 B1 */
 	RAMIPS_MACH_DIR_600_B1,		/* D-Link DIR-600 B1 */
 	RAMIPS_MACH_DIR_600_B2,		/* D-Link DIR-600 B2 */
---
+++ target/linux/ramips/files/arch/mips/ralink/rt305x/mach-all0256n.c
@@ -0,0 +1,128 @@
+/*
+ *  Allnet ALL0256N board support
+ *
+ *  Copyright (C) 2012 Daniel Golle <dgolle@allnet.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/spi/spi.h>
+#include <linux/spi/flash.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
+#include <linux/mtd/physmap.h>
+
+#include <asm/mach-ralink/machine.h>
+#include <asm/mach-ralink/dev-gpio-buttons.h>
+#include <asm/mach-ralink/dev-gpio-leds.h>
+#include <asm/mach-ralink/rt305x.h>
+#include <asm/mach-ralink/rt305x_regs.h>
+
+#include "devices.h"
+
+#define ALL0256N_GPIO_BUTTON_RESET 0
+#define ALL0256N_GPIO_LED_RSSI_LOW 14
+#define ALL0256N_GPIO_LED_RSSI_MED 12
+#define ALL0256N_GPIO_LED_RSSI_HIGH 13
+#define ALL0256N_BUTTONS_POLL_INTERVAL 20
+
+#ifdef CONFIG_MTD_PARTITIONS
+static struct mtd_partition all0256n_partitions[] = {
+	{
+		.name	= "u-boot",
+		.offset	= 0,
+		.size	= 0x030000,
+		.mask_flags = MTD_WRITEABLE,
+	}, {
+		.name	= "u-boot-env",
+		.offset	= 0x030000,
+		.size	= 0x010000,
+	}, {
+		.name	= "factory",
+		.offset	= 0x040000,
+		.size	= 0x010000,
+	}, {
+		.name	= "kernel",
+		.offset	= 0x050000,
+		.size	= 0x0D0000,
+	}, {
+		.name	= "rootfs",
+		.offset	= 0x120000,
+		.size	= 0x2E0000,
+	}, {
+		.name	= "firmware",
+		.offset	= 0x050000,
+		.size	= 0x3B0000,
+	}
+};
+#endif /* CONFIG_MTD_PARTITIONS */
+
+
+const struct flash_platform_data all0256n_flash = {
+	.type		= "mx25l3205d",
+#ifdef CONFIG_MTD_PARTITIONS
+	.parts		= all0256n_partitions,
+	.nr_parts	= ARRAY_SIZE(all0256n_partitions),
+#endif
+};
+
+struct spi_board_info all0256n_spi_slave_info[] __initdata = {
+	{
+		.modalias	= "m25p80",
+		.platform_data	= &all0256n_flash,
+		.irq		= -1,
+		.max_speed_hz	= 10000000,
+		.bus_num	= 0,
+		.chip_select	= 0,
+	},
+};
+
+static struct gpio_button all0256n_gpio_buttons[] __initdata = {
+	{
+		.desc		= "reset",
+		.type		= EV_KEY,
+		.code		= KEY_RESTART,
+		.threshold	= 3,
+		.gpio		= ALL0256N_GPIO_BUTTON_RESET,
+		.active_low	= 1,
+	}
+};
+
+static struct gpio_led all0256n_leds_gpio[] __initdata = {
+	{
+		.name		= "all0256n:green:rssilow",
+		.gpio		= ALL0256N_GPIO_LED_RSSI_LOW,
+		.active_low	= 1,
+	}, {
+		.name		= "all0256n:green:rssimed",
+		.gpio		= ALL0256N_GPIO_LED_RSSI_MED,
+		.active_low	= 1,
+	}, {
+		.name		= "all0256n:green:rssihigh",
+		.gpio		= ALL0256N_GPIO_LED_RSSI_HIGH,
+		.active_low	= 1,
+	}
+};
+
+static void __init all0256n_init(void)
+{
+	rt305x_gpio_init(RT305X_GPIO_MODE_GPIO << RT305X_GPIO_MODE_UART0_SHIFT);
+	rt305x_register_spi(all0256n_spi_slave_info,
+			    ARRAY_SIZE(all0256n_spi_slave_info));
+	rt305x_esw_data.vlan_config = RT305X_ESW_VLAN_CONFIG_WLLLL;
+	rt305x_register_ethernet();
+	ramips_register_gpio_leds(-1, ARRAY_SIZE(all0256n_leds_gpio),
+				  all0256n_leds_gpio);
+	ramips_register_gpio_buttons(-1, ALL0256N_BUTTONS_POLL_INTERVAL,
+				     ARRAY_SIZE(all0256n_gpio_buttons),
+				     all0256n_gpio_buttons);
+	rt305x_register_wifi();
+	rt305x_register_wdt();
+}
+
+MIPS_MACHINE(RAMIPS_MACH_ALL0256N, "ALL0256N", "Allnet ALL0256N",
+	     all0256n_init);
