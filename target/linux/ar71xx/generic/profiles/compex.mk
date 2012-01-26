d8bb980f470ec34b7309ecd65d3223263777fd92
Gabor Juhos <juhosg@openwrt.org>
2012-01-26 17:01:44 +0000

    ar71xx: create a profile and build images for Compex WPE72
    [juhosg: remove ath9k from the the profile package list, build WPE72
    image with the Default profile as well]
    
    Signed-off-by: Johnathan Boyce <jon.boyce@globalreach.eu.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29914 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/generic/profiles/compex.mk
+++ target/linux/ar71xx/generic/profiles/compex.mk
@@ -15,3 +15,14 @@ define Profile/WP543/Description
 endef
 
 $(eval $(call Profile,WP543))
+
+define Profile/WPE72
+	NAME:=Compex WPE72/WPE72NX
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2
+endef
+
+define Profile/WPE72/Description
+	Package set optimized for the Compex WPE72 boards including Compex WPE72NX Indoor Access Point.
+endef
+
+$(eval $(call Profile,WPE72))
