a32fd3f98a762833babf5768b68eb44ed70f4105
Gabor Juhos <juhosg@openwrt.org>
2012-01-26 17:01:43 +0000

    firmware-utils/mkmylofw: add WPE72 support
    Signed-off-by: Johnathan Boyce <jon.boyce@globalreach.eu.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29913 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mkmylofw.c
+++ tools/firmware-utils/src/mkmylofw.c
@@ -154,6 +154,9 @@ struct cpx_board boards[] = {
 	CPX_BOARD_AR71XX(DEVID_COMPEX_WP543, 2,
 		"WP543", "Compex WP543",
 		"BareBoard"),
+	CPX_BOARD_AR71XX(DEVID_COMPEX_WPE72, 8,
+		"WPE72", "Compex WPE72",
+		"BareBoard"),
 
 	CPX_BOARD_AR23XX(DEVID_COMPEX_NP25G, 4,
 		"NP25G", "Compex NetPassage 25G",
