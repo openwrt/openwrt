76b8adb1875baabb1c0e9699f6b465a63fc31d7a
Jo-Philipp Wich <jow@openwrt.org>
2012-02-02 18:41:10 +0000

    [PATCH] net5501: correct net5501 h/w configuration Bump to version 3.2. Simplify and correct kernel config (based on x86/config-3.2). Designate eth0 as wan interface, and bridge eth1/eth2/eth3. Add heartbeat LED trigger. Use correct CS5535 GPIO driver. Use correct USB drivers (OHCI not UHCI). Add MSR and HW clock support. Add GPIO mask definition. Add correct SATA drivers. Add LED triggers. [Patch from Philipp Prindeville, via  http://patchwork.openwrt.org/patch/1802/]
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@29994 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/x86/net5501/target.mk
+++ target/linux/x86/net5501/target.mk
@@ -1,12 +1,29 @@
+LINUX_VERSION:=3.2.1
 BOARDNAME:=Soekris net5501
-FEATURES:=squashfs pci usb gpio
-DEFAULT_PACKAGES += kmod-ata-via-sata kmod-crypto-hw-geode \
-			kmod-gpio-cs5535 kmod-gpio-nsc kmod-gpio-pc8736x \
-			kmod-wdt-geode kmod-hwmon-core kmod-hwmon-pc87360 \
-			kmod-via-rhine kmod-leds-net5501 \
+FEATURES:=squashfs jffs2 ext4 pci usb gpio
+DEFAULT_PACKAGES += kmod-crypto-hw-geode kmod-crypto-ocf \
+			kmod-gpio-cs5535-new kmod-gpio-nsc \
+			kmod-wdt-geode kmod-cs5535-clockevt kmod-cs5535-mfgpt \
+			kmod-cs5536 \
+			kmod-hwmon-core kmod-hwmon-pc87360 kmod-gpio-pc8736x \
+			kmod-via-rhine kmod-solos-pci \
 			kmod-i2c-core kmod-i2c-gpio \
 			kmod-i2c-algo-bit kmod-i2c-algo-pca kmod-i2c-algo-pcf \
-			kmod-usb-core kmod-usb2 kmod-usb-uhci
+			kmod-i2c-scx200-acb \
+			kmod-usb-core kmod-usb2 kmod-usb-ohci \
+			kmod-cfg80211 kmod-mac80211 \
+			kmod-mppe kmod-pppoe kmod-pppoa kmod-pppo2ltp \
+			kmod-ath kmod-ath5k kmod-ath9k \
+			kmod-leds-gpio kmod-input-gpio-keys-polled \
+			kmod-button-hotplug \
+			kmod-ledtrig-heartbeat kmod-ledtrig-gpio \
+			kmod-ledtrig-netdev kmod-ledtrig-netfilter \
+			kmod-cpu-msr \
+			soloscli linux-atm br2684ctl bridge ppp ppp-mod-pppoa \
+			pppdump pppstats \
+			libopenssl ocf-crypto-headers zlib hwclock hostapd
+
+CS5535_MASK:=0x01000040
 
 CFLAGS += -Os -pipe -march=k6-2 -fno-align-functions -fno-align-loops -fno-align-jumps \
 	  -fno-align-labels
@@ -15,3 +32,6 @@ define Target/Description
 	Build firmware images for Soekris net5501 board
 endef
 
+define KernelPackage/gpio-cs5535-new/install
+	sed -i -r -e 's/$$$$$$$$/ mask=$(CS5535_MASK)/' $(1)/etc/modules.d/??-gpio-cs5535-new
+endef
