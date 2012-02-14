ed79921fe516cfe1c51442bf906f10244f8279a8
Gabor Juhos <juhosg@openwrt.org>
2012-02-14 17:32:40 +0000

    ramips: use SoC specific irq.h
    SVN-Revision: 30525

--- target/linux/ramips/files/arch/mips/include/asm/mach-ralink/rt288x/irq.h
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/rt288x/irq.h
@@ -6,12 +6,12 @@
  *  under the terms of the GNU General Public License version 2 as published
  *  by the Free Software Foundation.
  */
-#ifndef __ASM_MACH_RT288X_IRQ_H
-#define __ASM_MACH_RT288X_IRQ_H
+#ifndef __ASM_MACH_RALINK_RT288X_IRQ_H
+#define __ASM_MACH_RALINK_RT288X_IRQ_H
 
 #define MIPS_CPU_IRQ_BASE	0
 #define NR_IRQS			48
 
 #include_next <irq.h>
 
-#endif /* __ASM_MACH_RT288X_IRQ_H */
+#endif /* __ASM_MACH_RALINK_RT288X_IRQ_H */
---
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/rt3883/irq.h
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (C) 2012 Gabor Juhos <juhosg@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+#ifndef __ASM_MACH_RALINK_RT3883_IRQ_H
+#define __ASM_MACH_RALINK_RT3883_IRQ_H
+
+#define MIPS_CPU_IRQ_BASE	0
+#define NR_IRQS			140
+
+#include_next <irq.h>
+
+#endif /* __ASM_MACH_RALINK_RT3883_IRQ_H */
