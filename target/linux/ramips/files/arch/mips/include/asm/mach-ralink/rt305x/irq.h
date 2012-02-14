5b15128f6654d13a2b7eee0f0ae45b0ba0c88430
Gabor Juhos <juhosg@openwrt.org>
2012-02-14 17:32:40 +0000

    ramips: use SoC specific irq.h
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@30525 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/rt305x/irq.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright (C) 2008 Gabor Juhos <juhosg@openwrt.org>
+ *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+#ifndef __ASM_MACH_RALINK_RT305X_IRQ_H
+#define __ASM_MACH_RALINK_RT305X_IRQ_H
+
+#define MIPS_CPU_IRQ_BASE	0
+#define NR_IRQS			48
+
+#include_next <irq.h>
+
+#endif /* __ASM_MACH_RALINK_RT305X_IRQ_H */
