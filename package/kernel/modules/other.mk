f39459d60505ac4563d6cb6b9daaa3957222fb0d
Gabor Juhos <juhosg@openwrt.org>
2012-03-08 09:00:16 +0000

    package/kernel: add package for the ath79 watchdog
    SVN-Revision: 30851

--- package/kernel/modules/other.mk
+++ package/kernel/modules/other.mk
@@ -703,6 +703,23 @@ endef
 
 $(eval $(call KernelPackage,wdt-scx200))
 
+
+define KernelPackage/wdt-ath79
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Atheros AR7XXX/AR9XXX watchdog timer
+  DEPENDS:=@TARGET_ar71xx
+  KCONFIG:=CONFIG_ATH79_WDT
+  FILES:=$(LINUX_DIR)/drivers/$(WATCHDOG_DIR)/ath79_wdt.ko
+  AUTOLOAD:=$(call AutoLoad,50,ath79_wdt)
+endef
+
+define KernelPackage/wdt-ath79/description
+  Kernel module for AR7XXX/AR9XXX watchdog timer.
+endef
+
+$(eval $(call KernelPackage,wdt-ath79))
+
+
 define KernelPackage/pwm
   SUBMENU:=$(OTHER_MENU)
   TITLE:=PWM generic API
