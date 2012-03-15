a86e95542d27a6cc5a179fbbd98e020bcb9420f0
Felix Fietkau <nbd@openwrt.org>
2012-03-15 19:32:31 +0000

    swconfig: use a mutex instead of a spinlock, many swconfig calls sleep
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@30952 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/generic/files/include/linux/switch.h
+++ target/linux/generic/files/include/linux/switch.h
@@ -191,7 +191,7 @@ struct switch_dev {
 	struct list_head dev_list;
 	unsigned long def_global, def_port, def_vlan;
 
-	spinlock_t lock;
+	struct mutex sw_mutex;
 	struct switch_port *portbuf;
 
 	char buf[128];
