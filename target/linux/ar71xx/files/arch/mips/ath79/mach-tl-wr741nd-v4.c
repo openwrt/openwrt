f4d1e3d147e42efbdf5cae7d89b367200fddd135
Gabor Juhos <juhosg@openwrt.org>
2012-03-27 19:37:51 +0000

    ar71xx: fix button polarity on TL-WR703N and TL-WR741N v4
    SVN-Revision: 31102

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr741nd-v4.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr741nd-v4.c
@@ -89,14 +89,14 @@ static struct gpio_keys_button tl_wr741ndv4_gpio_keys[] __initdata = {
 		.code		= KEY_RESTART,
 		.debounce_interval = TL_WR741NDV4_KEYS_DEBOUNCE_INTERVAL,
 		.gpio		= TL_WR741NDV4_GPIO_BTN_RESET,
-		.active_low	= 1,
+		.active_low	= 0,
 	}, {
 		.desc		= "WPS",
 		.type		= EV_KEY,
 		.code		= KEY_WPS_BUTTON,
 		.debounce_interval = TL_WR741NDV4_KEYS_DEBOUNCE_INTERVAL,
 		.gpio		= TL_WR741NDV4_GPIO_BTN_WPS,
-		.active_low	= 1,
+		.active_low	= 0,
 	}
 };
 
