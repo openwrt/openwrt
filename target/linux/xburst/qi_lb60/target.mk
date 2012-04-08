b0971e69f2ff110d6fc6e52a601e4e87d707e758
Mirko Vogt <mirko@openwrt.org>
2012-04-08 12:09:52 +0000

    level up target xburst to linux kernel version 3.2.1
    Thanks to the qi-hardware project - especially to Lars, Maarten and Xiangfu
    
    SVN-Revision: 31218

--- target/linux/xburst/qi_lb60/target.mk
+++ target/linux/xburst/qi_lb60/target.mk
@@ -1,8 +1,1 @@
 BOARDNAME:=QI Ben Nanonote (qi_lb60)
-DEFAULT_PACKAGES += \
-	kmod-sound-core \
-	kmod-sound-soc-core \
-	kmod-sound-soc-jz4740 \
-	kmod-sound-soc-jz4740-codec \
-	kmod-sound-soc-qilb60 \
-
