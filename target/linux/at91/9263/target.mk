701309ec11337125fa2a6e61739ffe4d1c43854e
Claudio Mignanti <c.mignanti@gmail.com>
2012-04-20 13:33:01 +0000

    Reorganize the subtargets, group subtargets per cpu family
    Signed-off-by: Claudio Mignanti <c.mignanti@gmail.com>
    
    SVN-Revision: 31372

---
+++ target/linux/at91/9263/target.mk
@@ -0,0 +1,11 @@
+# 
+# Copyright (C) 2011-2012 OpenWrt.org
+#
+
+SUBTARGET:=9263
+BOARDNAME:=AT91SAM9263 Based board
+
+define Target/Description
+	Build images for AT91SAM9263 based board, supported board:
+		* TQ Components TQMa9263
+endef
---
+++ target/linux/at91/9g20/target.mk
@@ -0,0 +1,12 @@
+# 
+# Copyright (C) 2006-2012 OpenWrt.org
+#
+
+SUBTARGET:=9g20
+BOARDNAME:=AT91SAM9g20 Based board
+
+define Target/Description
+	Build images for AT91SAM9g20 based board, supported board:
+		* Acmesystems NetusG20
+		* Taskit Stamp9G20 Evaluation Board
+endef
