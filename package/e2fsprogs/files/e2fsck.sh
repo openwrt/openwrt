2d19ca6925f229e7e1dc8398baf3a702a05ed56f
Jo-Philipp Wich <jow@openwrt.org>
2012-04-20 15:18:17 +0000

    e2fsprogs: fix status reaping with fsck piped to logger, based on patch by Lukasz Golec-Biernat <mojedokumenty+openwrt@gmail.com>
    SVN-Revision: 31377

--- package/e2fsprogs/files/e2fsck.sh
+++ package/e2fsprogs/files/e2fsck.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Copyright 2010 Vertical Communications
+# Copyright 2012 OpenWrt.org
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 fsck_e2fsck() {
+	set -o pipefail
 	e2fsck -p "$device" 2>&1 | logger -t "fstab: e2fsck ($device)"
 	local status="$?"
+	set +o pipefail
 	case "$status" in
 		0|1) ;; #success
 		2) reboot;;
