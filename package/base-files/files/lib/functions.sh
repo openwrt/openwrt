d8d2b5d324c64c2744c617bb1d501e195a631461
Felix Fietkau <nbd@openwrt.org>
2012-04-21 20:34:45 +0000

    base-files: rename internal variables in config_foreach to make namespace conflicts more unlikely
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@31431 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -173,17 +173,17 @@ config_set() {
 }
 
 config_foreach() {
-	local function="$1"
+	local ___function="$1"
 	[ "$#" -ge 1 ] && shift
-	local type="$1"
+	local ___type="$1"
 	[ "$#" -ge 1 ] && shift
 	local section cfgtype
 
 	[ -z "$CONFIG_SECTIONS" ] && return 0
 	for section in ${CONFIG_SECTIONS}; do
 		config_get cfgtype "$section" TYPE
-		[ -n "$type" -a "x$cfgtype" != "x$type" ] && continue
-		eval "$function \"\$section\" \"\$@\""
+		[ -n "$___type" -a "x$cfgtype" != "x$___type" ] && continue
+		eval "$___function \"\$section\" \"\$@\""
 	done
 }
 
