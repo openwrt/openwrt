8a38c62c0702c5536d5807732343633eca518d62
Jonas Gorski <jogo@openwrt.org>
2012-04-24 11:49:27 +0000

    bcm63xx: drop linux 3.2 support
    SVN-Revision: 31464

--- target/linux/brcm63xx/modules.mk
+++ target/linux/brcm63xx/modules.mk
@@ -10,13 +10,8 @@ define KernelPackage/bcm63xx-spi
   TITLE:=Broadcom BCM63xx SPI driver
   DEPENDS:=@TARGET_brcm63xx +kmod-spi-bitbang
   KCONFIG:=CONFIG_SPI_BCM63XX
-  ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),ge,3.3)),1)
-    FILES:=$(LINUX_DIR)/drivers/spi/spi-bcm63xx.ko
-    AUTOLOAD:=$(call AutoLoad,92,spi-bcm63xx)
-  else
-    FILES:=$(LINUX_DIR)/drivers/spi/bcm63xx_spi.ko
-    AUTOLOAD:=$(call AutoLoad,92,bcm63xx_spi)
-  endif
+  FILES:=$(LINUX_DIR)/drivers/spi/spi-bcm63xx.ko
+  AUTOLOAD:=$(call AutoLoad,92,spi-bcm63xx)
 endef
 
 define KernelPackage/bcm63xx-spi/description
