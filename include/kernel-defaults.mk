3349cf269125525480485ee40bf30d7f0c8ae17a
Gabor Juhos <juhosg@openwrt.org>
2012-05-03 09:43:10 +0000

    Fix iptables abuse of kernel header files. Use exported headers instead.
    [juhosg: export xt_layer7.h for all kernel versions]
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    
    SVN-Revision: 31566

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -106,6 +106,7 @@ define Kernel/Configure/Default
 	$(call Kernel/SetInitramfs)
 	-$(_SINGLE)$(MAKE) $(KERNEL_MAKEOPTS) oldconfig prepare scripts
 	rm -rf $(KERNEL_BUILD_DIR)/modules
+	$(MAKE) $(KERNEL_MAKEOPTS) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
 	$(SH_FUNC) grep '=[ym]' $(LINUX_DIR)/.config | LC_ALL=C sort | md5s > $(LINUX_DIR)/.vermagic
 endef
 
