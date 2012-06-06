a53b084e497a9f1629a2caada833ebe14a6838b7
John Crispin <john@openwrt.org>
2012-06-06 13:27:11 +0000

    add ccache wrapper
    SVN-Revision: 32075

--- rules.mk
+++ rules.mk
@@ -188,8 +188,12 @@ INSTALL_DATA:=install -m0644
 INSTALL_CONF:=install -m0600
 
 ifneq ($(CONFIG_CCACHE),)
-  TARGET_CC:= ccache $(TARGET_CC)
-  TARGET_CXX:= ccache $(TARGET_CXX)
+  TARGET_CC_NOCACHE:=$(TARGET_CC)
+  TARGET_CXX_NOCACHE:=$(TARGET_CXX)
+  export TARGET_CC_NOCACHE
+  export TARGET_CXX_NOCACHE
+  TARGET_CC:= ccache_cc
+  TARGET_CXX:= ccache_cxx
   HOSTCC:= ccache $(HOSTCC)
 endif
 
