41e9131509c8ff5e54d3cec041d7c10f3db46840
Jo-Philipp Wich <jow@openwrt.org>
2012-06-15 12:57:29 +0000

    [include] uclibc++.mk: override the cc binary instead of the wrapper command when ccache is used
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@32383 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/uclibc++.mk
+++ include/uclibc++.mk
@@ -8,5 +8,9 @@ PKG_PREPARED_DEPENDS += CONFIG_USE_UCLIBCXX
 CXX_DEPENDS = +USE_UCLIBCXX:uclibcxx +USE_LIBSTDCXX:libstdcpp
 
 ifneq ($(CONFIG_USE_UCLIBCXX),)
+ ifneq ($(CONFIG_CCACHE),)
+  TARGET_CXX_NOCACHE="g++-uc"
+ else
   TARGET_CXX="g++-uc"
+ endif
 endif
