fdfffd0fbe8e6353b70ce84441aec99e6f50765e
Jo-Philipp Wich <jow@openwrt.org>
2012-06-18 23:08:18 +0000

    Add new package for configuring 6rd tunnels.
    This patch adds support for manually configuring 6rd tunnels. It depends on
    the netifd patches I sent earlier, which add 6rd support.
    
    A basic interface configuration looks like:
    
        config interface 'wan6'
          option proto '6rd'
          option peeraddr '192.0.2.1'
          option ip6prefix '2123::'
          option ip6prefixlen '16'
          option ip4prefixlen '0'
    
    Where ip4prefixlen is optional and actually defaults to 0, which would use all
    bits of the IPv4 in the calculated IPv6 subnet.
    
    I believe it should be possible to configure a regular 6to4 tunnel using this,
    and that we may want to merge the two eventually, but there are some larger
    
    differences between the two at the moment:
    
     - 6rd addresses can be more difficult to calculate. My ISP, for example, has
       a setup with a v6 mask of 43 bits, and a v4 mask of 19.
    
     - 6to4 has support for configuring radvd. This is something we want, of
       course, but it seems best to deal with this in a separate patch.
    
    Just creating a new package looked like the quickest way to get this in.
    
    This work is based on the 6in4 package, and work by Stijn Tintel.
    
    Signed-off-by: Stéphan Kochen <stephan@kochen.nl>
    
    SVN-Revision: 32431

---
+++ package/6rd/files/6rd.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+# 6rd.sh - IPv6-in-IPv4 tunnel backend
+# Copyright (c) 2010-2012 OpenWrt.org
+
+[ -n "$INCLUDE_ONLY" ] || {
+	. /lib/functions.sh
+	. /lib/functions/network.sh
+	. ../netifd-proto.sh
+	init_proto "$@"
+}
+
+tun_error() {
+	local cfg="$1"; shift;
+
+	[ -n "$1" ] && proto_notify_error "$cfg" "$@"
+	proto_block_restart "$cfg"
+}
+
+proto_6rd_setup() {
+	local cfg="$1"
+	local iface="$2"
+	local link="6rd-$cfg"
+
+	local mtu ttl ipaddr peeraddr ip6prefix ip6prefixlen ip4prefixlen
+	json_get_vars mtu ttl ipaddr peeraddr ip6prefix ip6prefixlen ip4prefixlen
+
+	[ -z "$ip6prefix" -o -z "$peeraddr" ] && {
+		tun_error "$cfg" "MISSING_ADDRESS"
+		return
+	}
+
+	[ -z "$ipaddr" ] && {
+		local wanif
+		if ! network_find_wan wanif || ! network_get_ipaddr ipaddr "$wanif"; then
+			tun_error "$cfg" "NO_WAN_LINK"
+			return
+		fi
+	}
+
+	# Determine the relay prefix.
+	local ip4prefixlen="${ip4prefixlen:-0}"
+	local ip4prefix=$(ipcalc.sh "$ipaddr/$ip4prefixlen" | grep NETWORK)
+	ip4prefix="${ip4prefix#NETWORK=}"
+
+	# Determine our IPv6 address.
+	local ip6subnet=$(6rdcalc "$ip6prefix/$ip6prefixlen" "$ipaddr/$ip4prefixlen")
+	local ip6addr="${ip6subnet%%::*}::1"
+
+	proto_init_update "$link" 1
+	proto_add_ipv6_address "$ip6addr" "$ip6prefixlen"
+	proto_add_ipv6_route "::" 0 "::$peeraddr"
+
+	proto_add_tunnel
+	json_add_string mode sit
+	json_add_int mtu "${mtu:-1280}"
+	json_add_int ttl "${ttl:-64}"
+	json_add_string local "$ipaddr"
+	json_add_string 6rd-prefix "$ip6prefix/$ip6prefixlen"
+	json_add_string 6rd-relay-prefix "$ip4prefix/$ip4prefixlen"
+	proto_close_tunnel
+
+	proto_send_update "$cfg"
+}
+
+proto_6rd_teardown() {
+	local cfg="$1"
+}
+
+proto_6rd_init_config() {
+	no_device=1
+	available=1
+
+	proto_config_add_int "mtu"
+	proto_config_add_int "ttl"
+	proto_config_add_string "peeraddr"
+	proto_config_add_string "ip6prefix"
+	proto_config_add_string "ip6prefixlen"
+	proto_config_add_string "ip4prefixlen"
+}
+
+[ -n "$INCLUDE_ONLY" ] || {
+	add_protocol 6rd
+}
