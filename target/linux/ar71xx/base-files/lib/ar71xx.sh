50527ff2b19c4b0e1e3612111c366eafcc892a36
Gabor Juhos <juhosg@openwrt.org>
2012-06-19 10:13:38 +0000

    ar71xx: correctly detect NETGEAR WNDRMAC
    This patch helps properly detect a WNDRMAC device. Before this
    patch the model is detected as "NETGEAR ?????????N".
    
    Signed-off-by: Roman A. aka BasicXP <x12ozmouse@ya.ru>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@32453 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -41,6 +41,8 @@ wndr3700_board_detect() {
 		model=$(ar71xx_get_mtd_offset_size_format art 56 10 %c)
 		if [ -z "$model" ] || [ "$model" = $'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff' ]; then
 			machine="NETGEAR WNDR3700v2"
+		elif [ -z "$model" ] || [ "$model" = $'\xff\xff\xff\xff\xff\xff\xff\xff\xffN' ]; then
+			machine="NETGEAR WNDRMAC"
 		else
 			machine="NETGEAR $model"
 		fi
