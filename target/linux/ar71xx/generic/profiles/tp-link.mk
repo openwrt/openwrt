7c87391c55a713a9ef9df169ab23e79177184b1a
Gabor Juhos <juhosg@openwrt.org>
2012-06-19 11:51:20 +0000

    ar71xx: create profile and build image for the TL-WDR4300 board
    SVN-Revision: 32461

--- target/linux/ar71xx/generic/profiles/tp-link.mk
+++ target/linux/ar71xx/generic/profiles/tp-link.mk
@@ -94,6 +94,17 @@ endef
 $(eval $(call Profile,TLWA901))
 
 
+define Profile/TLWDR4300
+	NAME:=TP-LINK TL-WDR4300
+	PACKAGES:=kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev
+endef
+
+define Profile/TLWDR4300/Description
+	Package set optimized for the TP-LINK TL-WDR4300.
+endef
+$(eval $(call Profile,TLWDR4300))
+
+
 define Profile/TLWR740
 	NAME:=TP-LINK TL-WR740N/ND
 	PACKAGES:=
