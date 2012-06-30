a8311020d66c4da2c9052c5c9cd8df178cae3671
Felix Fietkau <nbd@openwrt.org>
2012-06-30 21:43:59 +0000

    toolchain: fix up lib64 symlink to lib before copying the initial prefix dir to avoid errors on copying
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@32556 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/toolchain-build.mk
+++ include/toolchain-build.mk
@@ -16,3 +16,12 @@ TOOLCHAIN_JOBS?=$(if $(CONFIG_TOOLCHAIN_PARALLEL),-j$(CONFIG_PKG_BUILD_JOBS))
 include $(INCLUDE_DIR)/host-build.mk
 
 HOST_STAMP_PREPARED=$(HOST_BUILD_DIR)/.prepared
+
+define FixupLibdir
+	if [ -d $(1)/lib64 -a \! -L $(1)/lib64 ]; then \
+		mkdir -p $(1)/lib; \
+		mv $(1)/lib64/* $(1)/lib/; \
+		rm -rf $(1)/lib64; \
+	fi
+	ln -sf lib $(1)/lib64
+endef
