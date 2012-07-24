0a08a776fb34cfdedcb8f51fb9d92e2b6c3911e8
John Crispin <john@openwrt.org>
2012-07-24 20:37:56 +0000

    add support for Allnet ALL5002 to kernel
    support ALL5002 in ramips/rt305x branch of the kernel.
    
    Signed-off-by: Daniel Golle <dgolle@allnet.de>
    
    SVN-Revision: 32813

--- target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
+++ target/linux/ramips/files/arch/mips/include/asm/mach-ralink/machine.h
@@ -58,6 +58,9 @@ enum ramips_mach_type {
 	RAMIPS_MACH_WR6202,		/* Accton WR6202 */
 	RAMIPS_MACH_XDXRN502J,          /* unknown XDX-RN502J */
 	RAMIPS_MACH_UR_336UN,		/* UPVEL ROUTER */
+	
+	/* RT3352 based machines */
+	RAMIPS_MACH_ALL5002,		/* Allnet ALL5002 */
 
 	/* RT3662 based machines */
 	RAMIPS_MACH_RT_N56U,		/* Asus RT-N56U */
