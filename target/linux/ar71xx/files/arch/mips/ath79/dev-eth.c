11c3392b7fcbfb750b15f8ac825ac50f9b962dd5
Gabor Juhos <juhosg@openwrt.org>
2012-08-29 10:37:55 +0000

    Revert "ar71xx: only allow RGMII mode on the 2nd ethernet MAC of the AR7240"
    That was based on assumptions.
    
    SVN-Revision: 33310

--- target/linux/ar71xx/files/arch/mips/ath79/dev-eth.c
+++ target/linux/ar71xx/files/arch/mips/ath79/dev-eth.c
@@ -616,14 +616,7 @@ static int __init ath79_setup_phy_if_mode(unsigned int id,
 			break;
 
 		case ATH79_SOC_AR7242:
-			switch (pdata->phy_if_mode) {
-			case PHY_INTERFACE_MODE_RGMII:
-				break;
-			default:
-				/* Other PHY modes are not tested yet. */
-				return -EINVAL;
-			}
-			break;
+			/* FIXME */
 
 		case ATH79_SOC_AR9341:
 		case ATH79_SOC_AR9342:
