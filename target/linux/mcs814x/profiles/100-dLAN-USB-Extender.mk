c5fa46dc5f0cc9f973c75f20e6b20848ddc898c4
Florian Fainelli <florian@openwrt.org>
2012-09-03 20:25:40 +0000

    [mcs814x] add Generic and dLAN USB Extender profiles
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33313 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/mcs814x/profiles/100-dLAN-USB-Extender.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/dLAN_USB_Extender
+  NAME:=Devolo dLAN USB Extender
+  PACKAGES:=hpavcfg foot
+endef
+
+define Profile/dLAN_USB_Extender/Description
+	Package set specifically tuned for the Devolo dLAN USB Extender device.
+endef
+$(eval $(call Profile,dLAN_USB_Extender))
+
