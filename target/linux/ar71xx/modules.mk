488c801a0a96fdbe2c0a7b57a16176fdebc89edc
Gabor Juhos <juhosg@openwrt.org>
2012-09-13 07:26:23 +0000

    ar71xx: package the AR934x NAND driver module
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33386 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/modules.mk
+++ target/linux/ar71xx/modules.mk
@@ -37,6 +37,22 @@ endef
 $(eval $(call KernelPackage,leds-wndr3700-usb))
 
 
+define KernelPackage/nand-ar934x
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Atheros AR934x NAND flash controller driver
+  KCONFIG:=CONFIG_MTD_NAND_AR934X
+  DEPENDS:=@TARGET_ar71xx +kmod-nand
+  FILES:=$(LINUX_DIR)/drivers/mtd/nand/ar934x_nfc.ko
+  AUTOLOAD:=$(call AutoLoad,30,ar934x_nfc)
+endef
+
+define KernelPackage/nand-ar934x/description
+  Atheros AR934x NAND flash controller driver.
+endef
+
+$(eval $(call KernelPackage,nand-ar934x))
+
+
 define KernelPackage/spi-vsc7385
   SUBMENU:=$(SPI_MENU)
   TITLE:=Vitesse VSC7385 ethernet switch driver
@@ -67,3 +83,4 @@ define KernelPackage/wdt-ath79/description
 endef
 
 $(eval $(call KernelPackage,wdt-ath79))
+
