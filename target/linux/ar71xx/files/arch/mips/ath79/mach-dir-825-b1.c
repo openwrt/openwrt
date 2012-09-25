8ed8e51e74c4edf62ab23e2f5b97eec395af1f84
Gabor Juhos <juhosg@openwrt.org>
2012-09-25 12:57:38 +0000

    ar71xx: automatically find WLAN calibration data on DIR-825-B1
    SVN-Revision: 33538

--- target/linux/ar71xx/files/arch/mips/ath79/mach-dir-825-b1.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-dir-825-b1.c
@@ -40,11 +40,13 @@
 #define DIR825B1_KEYS_POLL_INTERVAL		20	/* msecs */
 #define DIR825B1_KEYS_DEBOUNCE_INTERVAL		(3 * DIR825B1_KEYS_POLL_INTERVAL)
 
-#define DIR825B1_CAL_LOCATION_0			0x1f661000
-#define DIR825B1_CAL_LOCATION_1			0x1f665000
+#define DIR825B1_CAL0_OFFSET			0x1000
+#define DIR825B1_CAL1_OFFSET			0x5000
+#define DIR825B1_MAC0_OFFSET			0xffa0
+#define DIR825B1_MAC1_OFFSET			0xffb4
 
-#define DIR825B1_MAC_LOCATION_0			0x1f66ffa0
-#define DIR825B1_MAC_LOCATION_1			0x1f66ffb4
+#define DIR825B1_CAL_LOCATION_0			0x1f660000
+#define DIR825B1_CAL_LOCATION_1			0x1f7f0000
 
 static struct gpio_led dir825b1_leds_gpio[] __initdata = {
 	{
@@ -111,10 +113,9 @@ static struct platform_device dir825b1_rtl8366s_device = {
 	}
 };
 
-static void dir825b1_read_ascii_mac(u8 *dest, unsigned int src_addr)
+static void dir825b1_read_ascii_mac(u8 *dest, u8 *src)
 {
 	int ret;
-	u8 *src = (u8 *)KSEG1ADDR(src_addr);
 
 	ret = sscanf(src, "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
 		     &dest[0], &dest[1], &dest[2],
@@ -124,23 +125,55 @@ static void dir825b1_read_ascii_mac(u8 *dest, unsigned int src_addr)
 		memset(dest, 0, ETH_ALEN);
 }
 
-static void __init dir825b1_setup(void)
+static bool __init dir825b1_is_caldata_valid(u8 *p)
 {
+	u16 *magic0, *magic1;
+
+	magic0 = (u16 *)(p + DIR825B1_CAL0_OFFSET);
+	magic1 = (u16 *)(p + DIR825B1_CAL1_OFFSET);
+
+	return (*magic0 == 0xa55a && *magic1 == 0xa55a);
+}
+
+static void __init dir825b1_wlan_init(void)
+{
+	u8 *caldata;
 	u8 mac1[ETH_ALEN], mac2[ETH_ALEN];
 
-	dir825b1_read_ascii_mac(mac1, DIR825B1_MAC_LOCATION_0);
-	dir825b1_read_ascii_mac(mac2, DIR825B1_MAC_LOCATION_1);
+	caldata = (u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_0);
+	if (!dir825b1_is_caldata_valid(caldata)) {
+		caldata = (u8 *)KSEG1ADDR(DIR825B1_CAL_LOCATION_1);
+		if (!dir825b1_is_caldata_valid(caldata)) {
+			pr_err("no calibration data found\n");
+			return;
+		}
+	}
+
+	dir825b1_read_ascii_mac(mac1, caldata + DIR825B1_MAC0_OFFSET);
+	dir825b1_read_ascii_mac(mac2, caldata + DIR825B1_MAC1_OFFSET);
+
+	ath79_init_mac(ath79_eth0_data.mac_addr, mac1, 2);
+	ath79_init_mac(ath79_eth1_data.mac_addr, mac1, 3);
+
+	ap9x_pci_setup_wmac_led_pin(0, 5);
+	ap9x_pci_setup_wmac_led_pin(1, 5);
+
+	ap94_pci_init(caldata + DIR825B1_CAL0_OFFSET, mac1,
+		      caldata + DIR825B1_CAL1_OFFSET, mac2);
+}
+
+static void __init dir825b1_setup(void)
+{
+	dir825b1_wlan_init();
 
 	ath79_register_mdio(0, 0x0);
 
-	ath79_init_mac(ath79_eth0_data.mac_addr, mac1, 2);
 	ath79_eth0_data.mii_bus_dev = &dir825b1_rtl8366s_device.dev;
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RGMII;
 	ath79_eth0_data.speed = SPEED_1000;
 	ath79_eth0_data.duplex = DUPLEX_FULL;
 	ath79_eth0_pll_data.pll_1000 = 0x11110000;
 
-	ath79_init_mac(ath79_eth1_data.mac_addr, mac1, 3);
 	ath79_eth1_data.mii_bus_dev = &dir825b1_rtl8366s_device.dev;
 	ath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_RGMII;
 	ath79_eth1_data.phy_mask = 0x10;
@@ -161,12 +194,6 @@ static void __init dir825b1_setup(void)
 	ath79_register_usb();
 
 	platform_device_register(&dir825b1_rtl8366s_device);
-
-	ap9x_pci_setup_wmac_led_pin(0, 5);
-	ap9x_pci_setup_wmac_led_pin(1, 5);
-
-	ap94_pci_init((u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_0), mac1,
-		      (u8 *) KSEG1ADDR(DIR825B1_CAL_LOCATION_1), mac2);
 }
 
 MIPS_MACHINE(ATH79_MACH_DIR_825_B1, "DIR-825-B1", "D-Link DIR-825 rev. B1",
