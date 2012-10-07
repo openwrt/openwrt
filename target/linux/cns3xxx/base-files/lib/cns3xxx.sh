bbb2b870022cff48dc79e3231ed2aed947f67e6a
Felix Fietkau <nbd@openwrt.org>
2012-10-07 23:24:02 +0000

    cns3xxx: add sysupgrade support
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>
    
     target/linux/cns3xxx/base-files/lib/cns3xxx.sh          |   22 ++
     target/linux/cns3xxx/base-files/lib/upgrade/platform.sh |  122 ++++++++++++++++
     target/linux/cns3xxx/image/Makefile                     |   19 +-
     3 files changed, 159 insertions(+), 4 deletions(-)
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33650 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/cns3xxx/base-files/lib/cns3xxx.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+
+cns3xxx_board_name() {
+        local machine
+        local name
+
+        machine=$(awk 'BEGIN{FS="[ \t]+:[ \t]"} /Hardware/ {print $2}' /proc/cpuinfo)
+
+        case "$machine" in
+		"Gateworks Corporation Laguna"*)
+			name="laguna"
+			;;
+		*)
+			name="generic";
+			;;
+	esac
+	
+	echo $name
+}
