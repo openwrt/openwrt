42c4a16172329bfe64ddd948b7999f9a82c62df9
Florian Fainelli <florian@openwrt.org>
2012-10-13 19:19:41 +0000

    gcc: update 4.7-linaro to 4.7-2012.10
    SVN-Revision: 33759

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -50,10 +50,10 @@ ifeq ($(findstring linaro, $(CONFIG_GCC_VERSION)),linaro)
       PKG_MD5SUM:=acd304caf055ccaaca4e3ef61da11e7d
     endif
     ifeq ($(CONFIG_GCC_VERSION),"4.7-linaro")
-      PKG_REV:=4.7-2012.04
-      PKG_VERSION:=4.7.1
+      PKG_REV:=4.7-2012.10
+      PKG_VERSION:=4.7.3
       PKG_VERSION_MAJOR:=4.7
-      PKG_MD5SUM:=6dab459c1177fc9ae2969e7a39549d44
+      PKG_MD5SUM:=a5ca87667350f1395d4da40c94ef059c
     endif
     PKG_SOURCE_URL:=http://launchpad.net/gcc-linaro/$(PKG_VERSION_MAJOR)/$(PKG_REV)/+download/
     PKG_SOURCE:=$(PKG_NAME)-linaro-$(PKG_REV).tar.bz2
