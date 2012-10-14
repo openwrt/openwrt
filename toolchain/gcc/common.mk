1b03aef75774dacd8a0dea2254bcc82a8c0c9f6c
Florian Fainelli <florian@openwrt.org>
2012-10-14 13:35:19 +0000

    [toolchain] gcc: add GCC 4.7.2
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33763 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -75,6 +75,9 @@ else
   ifeq ($(PKG_VERSION),4.7.0)
     PKG_MD5SUM:=2a0f1d99fda235c29d40b561f81d9a77
   endif
+  ifeq ($(PKG_VERSION),4.7.2)
+    PKG_MD5SUM:=cc308a0891e778cfda7a151ab8a6e762
+  endif
 endif
 endif
 
