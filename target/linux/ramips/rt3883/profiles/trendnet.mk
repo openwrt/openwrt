81787ca9d76e15c66a7a6ee6d181e085e0c8581f
Gabor Juhos <juhosg@openwrt.org>
2012-10-18 07:23:10 +0000

    ramips: add profile and build image for the TEW-692GR board
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33849 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/rt3883/profiles/trendnet.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/TEW692GR
+	NAME:=TRENDnet TEW-692GR
+	PACKAGES:=swconfig
+endef
+
+define Profile/TEW692GR/Description
+	Package set compatible with the TRENDnet TEW-692GR board.
+endef
+$(eval $(call Profile,TEW692GR))
