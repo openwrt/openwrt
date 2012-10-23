f237863aecbe5936790ad3f578b2a3d635dd977c
Jonas Gorski <jogo@openwrt.org>
2012-10-23 10:57:44 +0000

    [bcm63xx] add support for Comtrend AR5387un.
    Router: Comtrend AR5387un.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    [jogo: fix subject, indentation in patch]
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@33894 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/base-files/lib/brcm63xx.sh
+++ target/linux/brcm63xx/base-files/lib/brcm63xx.sh
@@ -22,6 +22,11 @@ brcm63xx_detect() {
 		status_led="96328avng::power"
 		ifname=eth0
 		;;
+	96328A-1441N1)
+		brcm63xx_has_reset_button="true"
+		status_led="96328A-1441N1:green:power"
+		ifname=eth0
+		;;
 	963281TAN)
 		status_led="963281TAN::power"
 		ifname=eth0
