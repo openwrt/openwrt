973eda60779017bef8fc1b94344dbe3b0eaec081
Gabor Juhos <juhosg@openwrt.org>
2012-10-27 07:57:57 +0000

    ar71xx: use mtd_read in tplinkpart
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 33950

--- target/linux/ar71xx/files/drivers/mtd/tplinkpart.c
+++ target/linux/ar71xx/files/drivers/mtd/tplinkpart.c
@@ -60,8 +60,8 @@ tplink_read_header(struct mtd_info *mtd, size_t offset)
 		goto err;
 
 	header_len = sizeof(struct tplink_fw_header);
-	ret = mtd->read(mtd, offset, header_len, &retlen,
-			(unsigned char *) header);
+	ret = mtd_read(mtd, offset, header_len, &retlen,
+		       (unsigned char *) header);
 	if (ret)
 		goto err_free_header;
 
@@ -91,8 +91,8 @@ static int tplink_check_rootfs_magic(struct mtd_info *mtd, size_t offset)
 	size_t retlen;
 	int ret;
 
-	ret = mtd->read(mtd, offset, sizeof(magic), &retlen,
-			(unsigned char *) &magic);
+	ret = mtd_read(mtd, offset, sizeof(magic), &retlen,
+		       (unsigned char *) &magic);
 	if (ret)
 		return ret;
 
