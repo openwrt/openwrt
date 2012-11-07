12d41e1668f606fc4ec74408bf81ccd0b2ef3b4e
John Crispin <blogic@openwrt.org>
2012-11-07 20:26:31 +0000

    [ramips] adds fon20n profile
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34112 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/rt305x/profiles/fon.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/FONERA20N
+	NAME:=Fonera 2.0N
+	PACKAGES:=\
+		kmod-usb-core kmod-usb-rt305x-dwc_otg \
+		kmod-ledtrig-usbdev
+endef
+
+define Profile/FONERA20N/Description
+	Package set for Fonera 2.0N
+endef
+
+$(eval $(call Profile,FONERA20N))
