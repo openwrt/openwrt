668234c1098da193745792e8a4685f8dd2dad86e
Florian Fainelli <florian@openwrt.org>
2012-11-11 16:11:29 +0000

    create generic subtarget
    Signed-off-by: Daniel Golle <dgolle@allnet.de>
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    SVN-Revision: 34147

---
+++ target/linux/ar7/generic/target.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+
+SUBTARGET:=generic
+BOARDNAME:=Texas Instruments AR7 boards
+
+define Target/Description
+	Build firmware images for TI AR7 based routers.
+endef
