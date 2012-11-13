5cbfaf71be0a9655916edda125467721ecf29e64
Gabor Juhos <juhosg@openwrt.org>
2012-11-13 14:48:02 +0000

    firmware-utils/mktplinkfw: add support for TL-WA7510N
    [juhosg: separated from the http://patchwork.openwrt.org/patch/2820/ patch]
    
    Signed-off-by: Stefan Helmert <helst_listen@aol.de>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34189 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -34,6 +34,7 @@
 #define HWID_TL_MR3220_V1	0x32200001
 #define HWID_TL_MR3420_V1	0x34200001
 #define HWID_TL_WA701N_V1	0x07010001
+#define HWID_TL_WA7510N_V1	0x75100001
 #define HWID_TL_WA901ND_V1	0x09010001
 #define HWID_TL_WA901ND_V2	0x09010002
 #define HWID_TL_WR703N_V1	0x07030101
@@ -195,6 +196,11 @@ static struct board_info boards[] = {
 	}, {
 		.id		= "TL-WA701Nv1",
 		.hw_id		= HWID_TL_WA701N_V1,
+		.hw_rev		= 1,
+		.layout_id	= "4M",
+	}, {
+		.id		= "TL-WA7510N",
+		.hw_id		= HWID_TL_WA7510N_V1,
 		.hw_rev		= 1,
 		.layout_id	= "4M",
 	}, {
