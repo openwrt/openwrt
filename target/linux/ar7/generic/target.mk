ae2cf98d8d7b44cad043196c63010b0cd66b034f
Florian Fainelli <florian@openwrt.org>
2012-11-16 13:00:55 +0000

    [ar7] move acx-mac80211 to the ar7 generic target
    None of the AC49x boards that we support actually have Wi-Fi.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34208 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar7/generic/target.mk
+++ target/linux/ar7/generic/target.mk
@@ -5,6 +5,8 @@
 SUBTARGET:=generic
 BOARDNAME:=Texas Instruments AR7 boards
 
+DEFAULT_PACKAGES+= kmod-mac80211 kmod-acx-mac80211 wpad-mini
+
 define Target/Description
 	Build firmware images for TI AR7 based routers.
 endef
