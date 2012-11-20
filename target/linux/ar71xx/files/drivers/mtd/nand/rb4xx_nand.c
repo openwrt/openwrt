46a3b818b97bd3314885e32c4e52663c2878fc24
Gabor Juhos <juhosg@openwrt.org>
2012-11-20 14:40:22 +0000

    ar71xx: remove buffer verification reference from rb4xx_nand.c
    It is a dead code.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 34276

--- target/linux/ar71xx/files/drivers/mtd/nand/rb4xx_nand.c
+++ target/linux/ar71xx/files/drivers/mtd/nand/rb4xx_nand.c
@@ -215,9 +215,6 @@ static int __devinit rb4xx_nand_probe(struct platform_device *pdev)
 	info->chip.read_byte	= rb4xx_nand_read_byte;
 	info->chip.write_buf	= rb4xx_nand_write_buf;
 	info->chip.read_buf	= rb4xx_nand_read_buf;
-#if 0
-	info->chip.verify_buf	= rb4xx_nand_verify_buf;
-#endif
 
 	info->chip.chip_delay	= 25;
 	info->chip.ecc.mode	= NAND_ECC_SOFT;
