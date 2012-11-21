811c89a11d84cdb91b9860f5f547cb851ae2b8e3
John Crispin <blogic@openwrt.org>
2012-11-21 06:06:48 +0000

    [lantiq] adds proper package selection for dgn3500
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34286 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/ar9/profiles/002-netgear.mk
+++ target/linux/lantiq/ar9/profiles/002-netgear.mk
@@ -1,6 +1,6 @@
 define Profile/DGN3500B
   NAME:=DGN3500B Netgear
-  PACKAGES:= kmod-usb-core kmod-usb-dwc-otg
+  PACKAGES:= kmod-usb-core kmod-usb-dwc-otg kmod-ath9k kmod-ltq-dsl-firmware-b-ar9
 endef
 
 define Profile/DGN3500B/Description
