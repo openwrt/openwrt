2975a8f5f6b36d1091dee96cbe1e98cf2807dba7
Hauke Mehrtens <hauke@openwrt.org>
2012-11-25 16:15:27 +0000

    brcm47xx: add profile for bcm4705 with wl driver
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34336 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm47xx/profiles/111-Bcm4705-wl.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Bcm4705-wl
+  NAME:=Broadcom BCM43xx WiFi (wl, proprietary)
+  PACKAGES:=-wpad-mini kmod-brcm-wl wlc nas kmod-tg3
+endef
+
+define Profile/Bcm4705-wl/Description
+	Package set compatible with hardware using Broadcom BCM43xx cards
+	using the proprietary broadcom wireless "wl" driver and tg3 Ethernet
+	driver.
+endef
+
+$(eval $(call Profile,Bcm4705-wl))
+
