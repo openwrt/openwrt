3ca51b539916a2692168fcb5bd0da4bde4296982
John Crispin <john@openwrt.org>
2012-12-15 11:01:16 +0000

    select hcd as a default package for the dgn3500
    SVN-Revision: 34702

--- target/linux/lantiq/xway/profiles/netgear.mk
+++ target/linux/lantiq/xway/profiles/netgear.mk
@@ -1,6 +1,7 @@
 define Profile/DGN3500A
   NAME:=Netgear DGN3500A
-  PACKAGES:=kmod-ath9k wpad-mini \
+  PACKAGES:=kmod-ltq-hcd-ar9 kmod-ledtrig-usbdev \
+	kmod-ath9k wpad-mini \
 	kmod-ltq-adsl-ar9-mei kmod-ltq-adsl-ar9 \
 	kmod-ltq-adsl-ar9-fw-a kmod-ltq-atm-ar9 \
 	ltq-adsl-app ppp-mod-pppoa \
@@ -11,7 +12,8 @@ $(eval $(call Profile,DGN3500A))
 
 define Profile/DGN3500B
   NAME:=Netgear DGN3500B
-  PACKAGES:=kmod-ath9k wpad-mini \
+  PACKAGES:=kmod-ltq-hcd-ar9 kmod-ledtrig-usbdev \
+	kmod-ath9k wpad-mini \
 	kmod-ltq-adsl-ar9-mei kmod-ltq-adsl-ar9 \
 	kmod-ltq-adsl-ar9-fw-b kmod-ltq-atm-ar9 \
 	ltq-adsl-app ppp-mod-pppoa \
