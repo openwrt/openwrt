6ebfe5fee2a1c0530c25b8e86beed09e79ea5f29
John Crispin <blogic@openwrt.org>
2012-12-15 02:00:50 +0000

    [lantiq] fix tapi dependency
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34696 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/ltqtapi.mk
+++ include/ltqtapi.mk
@@ -1,12 +1,12 @@
-LTQ_TAPI_BUILD_DEPENDS:=TARGET_lantiq_danube:kmod-ltq-tapi \
-	TARGET_lantiq_danube:kmod-ltq-vmmc \
+LTQ_TAPI_BUILD_DEPENDS:=TARGET_lantiq_xway:kmod-ltq-tapi \
+	TARGET_lantiq_xway:kmod-ltq-vmmc \
 	TARGET_lantiq_falcon:kmod-ltq-tapi \
 	TARGET_lantiq_falcon:kmod-ltq-vmmc
 
-LTQ_TAPI_TARGET:=@(TARGET_lantiq_falcon||TARGET_lantiq_danube)
+LTQ_TAPI_TARGET:=@(TARGET_lantiq_falcon||TARGET_lantiq_xway)
 
 LTQ_TAPI_DEPENDS:=$(LTQ_TAPI_TARGET) \
-	+TARGET_lantiq_danube:kmod-ltq-tapi \
-	+TARGET_lantiq_danube:kmod-ltq-vmmc \
+	+TARGET_lantiq_xway:kmod-ltq-tapi \
+	+TARGET_lantiq_xway:kmod-ltq-vmmc \
 	+TARGET_lantiq_falcon:kmod-ltq-tapi \
 	+TARGET_lantiq_falcon:kmod-ltq-vmmc
