33d3433d753496143976a71e2c8536bc27eb3ab4
John Crispin <john@openwrt.org>
2012-12-16 10:03:18 +0000

    fix i2c-lantiq module
    SVN-Revision: 34712

--- target/linux/lantiq/modules.mk
+++ target/linux/lantiq/modules.mk
@@ -5,18 +5,18 @@
 # See /LICENSE for more information.
 #
 
-I2C_FALCON_MODULES:= \
-  CONFIG_I2C_FALCON:drivers/i2c/busses/i2c-falcon
+I2C_LANTIQ_MODULES:= \
+  CONFIG_I2C_LANTIQ:drivers/i2c/busses/i2c-lantiq
 
-define KernelPackage/i2c-falcon-lantiq
-  TITLE:=Falcon I2C controller
-  $(call i2c_defaults,$(I2C_FALCON_MODULES),52)
-  DEPENDS:=kmod-i2c-core @(TARGET_lantiq_falcon||TARGET_lantiq_falcon_stable)
+define KernelPackage/i2c-lantiq
+  TITLE:=Lantiq I2C controller
+  $(call i2c_defaults,$(I2C_LANTIQ_MODULES),52)
+  DEPENDS:=kmod-i2c-core @TARGET_lantiq_falcon
 endef
 
-define KernelPackage/i2c-falcon-lantiq/description
-  Kernel support for the Falcon I2C controller
+define KernelPackage/i2c-lantiq/description
+  Kernel support for the Lantiq/Falcon I2C controller
 endef
 
-$(eval $(call KernelPackage,i2c-falcon-lantiq))
+$(eval $(call KernelPackage,i2c-lantiq))
 
