db831511d2091907eef52519ebcc47f54cfd6d33
Gabor Juhos <juhosg@openwrt.org>
2012-12-18 17:53:53 +0000

    goldfish: R.I.P.
    It is broken and it is not maintained by anyone since long time.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 34766

--- target/linux/goldfish/image/run-emulator.sh
+++
@@ -1,3 +0,0 @@
-PREFIX=openwrt-goldfish-
-touch ${PREFIX}data.bin
-./goldfish-qemu/emulator -skindir ./goldfish-qemu/skins -skin HVGA -sysdir . -ramdisk ${PREFIX}ramdisk.bin -kernel ${PREFIX}kernel.bin -system ${PREFIX}system.bin -data ${PREFIX}data.bin -show-kernel -partition-size 100 -qemu -append "console=ttyS0 ubi.mtd=0 root=/dev/mtdblock3 rootfstype=jffs2 bootdelay=1 init=/etc/preinit" -net nic,vlan=0,model=smc91c111 -net user,vlan=0,hostname=OpenWrt
