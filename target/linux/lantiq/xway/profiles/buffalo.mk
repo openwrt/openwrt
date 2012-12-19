5495ffbfdfeb5ec835ce902792b98a44f4ff75e3
John Crispin <john@openwrt.org>
2012-12-19 15:17:11 +0000

    more board file fixes
    SVN-Revision: 34788

--- target/linux/lantiq/xway/profiles/buffalo.mk
+++ target/linux/lantiq/xway/profiles/buffalo.mk
@@ -1,23 +1,23 @@
-define Profile/DGN3500
-  NAME:=Netgear DGN3500
+define Profile/WBMRA
+  NAME:=Buffalo WBMR-HP-G300H (A) - WBMR
   PACKAGES:=kmod-ltq-hcd-ar9 kmod-ledtrig-usbdev \
-	kmod-ath9k wpad-mini \
 	kmod-ltq-adsl-ar9-mei kmod-ltq-adsl-ar9 \
 	kmod-ltq-adsl-ar9-fw-a kmod-ltq-atm-ar9 \
 	ltq-adsl-app ppp-mod-pppoa \
-	kmod-ltq-deu-ar9
+  	kmod-ath9k wpad-mini \
+	swconfig
 endef
 
-$(eval $(call Profile,DGN3500))
+$(eval $(call Profile,WBMRA))
 
-define Profile/DGN3500B
-  NAME:=Netgear DGN3500B
+define Profile/WBMRB
+  NAME:=Buffalo WBMR-HP-G300H (B) - WBMR
   PACKAGES:=kmod-ltq-hcd-ar9 kmod-ledtrig-usbdev \
-	kmod-ath9k wpad-mini \
 	kmod-ltq-adsl-ar9-mei kmod-ltq-adsl-ar9 \
 	kmod-ltq-adsl-ar9-fw-b kmod-ltq-atm-ar9 \
 	ltq-adsl-app ppp-mod-pppoa \
-	kmod-ltq-deu-ar9
+  	kmod-ath9k wpad-mini \
+	swconfig
 endef
 
-$(eval $(call Profile,DGN3500B))
+$(eval $(call Profile,WBMRB))
