e975fbe3fb9495bb369c77045efd476e63254ed3
John Crispin <blogic@openwrt.org>
2012-12-21 13:47:55 +0000

    [lantiq] more devicetree fixes
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@34824 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/xway/profiles/avm.mk
+++ target/linux/lantiq/xway/profiles/avm.mk
@@ -4,7 +4,16 @@ define Profile/FRITZ7320
 	kmod-ltq-adsl-ar9-mei kmod-ltq-adsl-ar9 \
 	kmod-ltq-adsl-ar9-fw-b kmod-ltq-atm-ar9 \
 	ltq-adsl-app ppp-mod-pppoa \
-	kmod-ltq-deu-ar9
+	kmod-ltq-deu-ar9 kmod-ltq-hcd-ar9
 endef
 
 $(eval $(call Profile,FRITZ7320))
+
+define Profile/FRITZ3370
+  NAME:=Fritz!Box WLan - FRITZ3370
+  PACKAGES:=kmod-ath9k wpad-mini \
+	kmod-ltq-deu-vr9 kmod-ltq-hcd-vr9 \
+	ppp-mod-pppoa
+endef
+
+$(eval $(call Profile,FRITZ3370))
