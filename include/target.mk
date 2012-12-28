41a039f46450ffae9483d6216422098669da2900
Felix Fietkau <nbd@openwrt.org>
2012-12-28 19:17:19 +0000

    mips: use -mno-branch-likely for kernel and userspace, saves ~11k kernel size after lzma and ~12k squashfs size in the default configuration
    SVN-Revision: 34909

--- include/target.mk
+++ include/target.mk
@@ -193,7 +193,7 @@ ifeq ($(DUMP),1)
   DEFAULT_CFLAGS_i386=-O2 -pipe -march=i486 -fno-caller-saves
   DEFAULT_CFLAGS_x86_64=-O2 -pipe -march=athlon64 -fno-caller-saves
   DEFAULT_CFLAGS_m68k=-Os -pipe -mcfv4e -fno-caller-saves
-  DEFAULT_CFLAGS_mips=-Os -pipe -mips32 -mtune=mips32 -fno-caller-saves
+  DEFAULT_CFLAGS_mips=-Os -pipe -mips32 -mtune=mips32 -fno-caller-saves -mno-branch-likely
   DEFAULT_CFLAGS_mipsel=$(DEFAULT_CFLAGS_mips)
   DEFAULT_CFLAGS_mips64=-Os -pipe -mips64 -mtune=mips64 -mabi=64 -fno-caller-saves
   DEFAULT_CFLAGS_mips64el=$(DEFAULT_CFLAGS_mips64)
