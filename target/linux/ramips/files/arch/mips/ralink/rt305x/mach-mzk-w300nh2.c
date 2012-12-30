fb07876f2530fc27c0b4a160f9ddc01eb0eaa3a5
Gabor Juhos <juhosg@openwrt.org>
2012-12-30 08:23:48 +0000

    ramips: rt305x: fix garbled name in mach-mzk-w300nh2.c
    Signed-off-by: Samir Ibradžić <sibradzic@gmail.com>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 34934

--- target/linux/ramips/files/arch/mips/ralink/rt305x/mach-mzk-w300nh2.c
+++ target/linux/ramips/files/arch/mips/ralink/rt305x/mach-mzk-w300nh2.c
@@ -1,7 +1,7 @@
 /*
  *  Planex MZK-W300NH2 board support
  *
- *  Copyright (C) 2012 Samir IbradÅ¾iÄ‡ sibradzic@gmail.com
+ *  Copyright (C) 2012 Samir Ibradžić <sibradzic@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
