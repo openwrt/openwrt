4713aef3ffb8d0d9aa1eae624acb913e8568d230
Steven Barth <steven@midlink.org>
2013-01-15 13:08:19 +0000

    ipv6-support: Switch to native IPv6 support
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35174 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/network/ipv6/ipv6-support/files/firewall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ip6tables -N ipv6-site-border
-ip6tables -A forwarding_rule -s fc00::/7 -j ipv6-site-border
-ip6tables -A forwarding_rule -d fc00::/7 -j ipv6-site-border
-
-mkdir -p /var/etc/ipv6-firewall.d
-for i in /var/etc/ipv6-firewall.d/*; do
-	[ -f "$i" ] && . "$i"
-done
