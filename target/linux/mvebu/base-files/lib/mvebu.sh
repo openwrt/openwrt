a286a55c6f0c20c876be2e8c7a092fcb42965837
Florian Fainelli <florian@openwrt.org>
2013-01-17 22:28:48 +0000

    mvebu: add board detection scripts
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    SVN-Revision: 35208

---
+++ target/linux/mvebu/base-files/lib/mvebu.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+
+MVEBU_BOARD_NAME=
+MVEBU_MODEL=
+
+mvebu_board_detect() {
+	local machine
+	local name
+
+	machine=$(cat /proc/device-tree/model)
+
+	case "$machine" in
+	*"Marvell Armada 370 Evaluation Board")
+		name="armada-370-db"
+		;;
+	*"Globalscale Mirabox")
+		name="mirabox"
+		;;
+	*"Marvell Armada 370 Reference Design")
+		name="armada-370-rd"
+		;;
+	*"Marvell Armada XP Evaluation Board")
+		name="armada-xp-db"
+		;;
+	*"PlatHome OpenBlocks AX3-4 board")
+		name="openblocks-ax3-4"
+		;;
+	esac
+
+	[ -z "$name" ] && name="unknown"
+
+	[ -z "$MVEBU_BOARD_NAME" ] && MVEBU_BOARD_NAME="$name"
+	[ -z "$MVEBU_MODEL" ] && MVEBU_MODEL="$machine"
+
+	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
+
+	echo "$MVEBU_BOARD_NAME" > /tmp/sysinfo/board_name
+	echo "$MVEBU_MODEL" > /tmp/sysinfo/model
+}
+
+mvebu_board_name() {
+	local name
+
+	[ -f /tmp/sysinfo/board_name ] && name=$(cat /tmp/sysinfo/board_name)
+	[ -z "$name" ] && name="unknown"
+
+	echo "$name"
+}
