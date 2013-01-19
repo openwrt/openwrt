c2aebc74add42647a46c6047c38047594e690999
Gabor Juhos <juhosg@openwrt.org>
2013-01-19 17:05:07 +0000

    ramips: add profile and build image for the Tenda 3G300M
    Factory image should be used to flash from
    original firmware.
    
    [juhosg: use the GENERIC_4M template]
    
    Signed-off-by: Cezary Jackiewicz <cezary.jackiewicz@gmail.com>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35246 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/rt305x/profiles/tenda.mk
+++ target/linux/ramips/rt305x/profiles/tenda.mk
@@ -5,6 +5,19 @@
 # See /LICENSE for more information.
 #
 
+define Profile/3G300M
+	NAME:=Tenda 3G300M
+	PACKAGES:=\
+		kmod-usb-core kmod-usb-rt305x-dwc_otg \
+		kmod-ledtrig-usbdev
+endef
+
+define Profile/3G300M/Description
+	Package set for Tenda 3G300M board
+endef
+
+$(eval $(call Profile,3G300M))
+
 define Profile/W306R_V20
 	NAME:=Tenda W306R V2.0
 	PACKAGES:=
