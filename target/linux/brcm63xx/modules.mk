3e998d9cf9b157e0fc31e4b6dcc1345db7b4743c
Jonas Gorski <jogo@openwrt.org>
2013-01-23 10:12:13 +0000

    bcm63xx: move bcm63xx-spi into the kernel
    The SPI driver is needed for supporting to boot from SPI flash on BCM6358
    and BCM6368, and for SPI connected switches.
    
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    SVN-Revision: 35304

--- target/linux/brcm63xx/modules.mk
+++ target/linux/brcm63xx/modules.mk
@@ -5,22 +5,6 @@
 # See /LICENSE for more information.
 #
 
-define KernelPackage/bcm63xx-spi
-  SUBMENU:=$(SPI_MENU)
-  TITLE:=Broadcom BCM63xx SPI driver
-  DEPENDS:=@TARGET_brcm63xx
-  KCONFIG:=CONFIG_SPI_BCM63XX
-  FILES:=$(LINUX_DIR)/drivers/spi/spi-bcm63xx.ko
-  AUTOLOAD:=$(call AutoLoad,92,spi-bcm63xx)
-endef
-
-define KernelPackage/bcm63xx-spi/description
-  This package contains the Broadcom BCM63xx SPI Master driver
-endef
-
-$(eval $(call KernelPackage,bcm63xx-spi))
-
-
 define KernelPackage/pcmcia-bcm63xx
   SUBMENU:=$(PCMCIA_MENU)
   TITLE:=Broadcom BCM63xx PCMCIA support
