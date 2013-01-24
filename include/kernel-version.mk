1ccd35ab43fbca80eff9aad103948bb4a04f5e09
Gabor Juhos <juhosg@openwrt.org>
2013-01-24 17:08:37 +0000

    kernel: update linux 3.7 to 3.7.4
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35318 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,8 +8,8 @@ endif
 ifeq ($(LINUX_VERSION),3.6.11)
   LINUX_KERNEL_MD5SUM:=3d602ad7f7984509c3e923a5ae90bc54
 endif
-ifeq ($(LINUX_VERSION),3.7.3)
-  LINUX_KERNEL_MD5SUM:=6b35fb08d229d4c6d246f27c7d475790
+ifeq ($(LINUX_VERSION),3.7.4)
+  LINUX_KERNEL_MD5SUM:=b34c31c954bebdfb568244dd3897e7c6
 endif
 
 # disable the md5sum check for unknown kernel versions
