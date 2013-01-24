76c12ff797c49940741b107d7b09dc1bdd350670
Hauke Mehrtens <hauke@hauke-m.de>
2013-01-24 14:38:10 +0000

    brcm47xx: change generic profiles
    The default profile now contains Ethernet drivers for all the types
    found on supported boards. There are other profiles for each Ethernet
    driver.
    
    SVN-Revision: 35314

--- target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
+++ target/linux/brcm47xx/profiles/100-Broadcom-b43.mk
@@ -1,18 +1,19 @@
 #
-# Copyright (C) 2007-2010 OpenWrt.org
+# Copyright (C) 2007-2013 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Broadcom-b43
-  NAME:=Broadcom BCM43xx WiFi (b43, default)
-  PACKAGES:=kmod-b44 kmod-b43 kmod-b43legacy
+  NAME:=Broadcom SoC, all Ethernet, BCM43xx WiFi (b43, default)
+  PACKAGES:=kmod-b44 kmod-tg3 kmod-bgmac kmod-b43 kmod-b43legacy
 endef
 
 define Profile/Broadcom-b43/Description
-	Package set compatible with hardware using Broadcom BCM43xx cards
-	using the MAC80211 b43 and b43legacy drivers and b44 Ethernet driver.
+	Package set compatible with hardware any Broadcom BCM47xx or BCM535x 
+	SoC with Broadcom BCM43xx Wifi cards using the mac80211, b43 and 
+	b43legacy drivers and b44, tg3 or bgmac Ethernet driver.
 endef
 
 $(eval $(call Profile,Broadcom-b43))
