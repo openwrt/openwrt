26312e0ff332e438ec77c45e208334ce259e0fee
Steven Barth <steven@midlink.org>
2013-01-30 09:05:53 +0000

    6in4: work around hanging wget process
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35408 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/network/ipv6/6in4/files/6in4.sh
+++ package/network/ipv6/6in4/files/6in4.sh
@@ -65,8 +65,10 @@ proto_6in4_setup() {
 		local max=3
 
 		while [ $((++try)) -le $max ]; do
-			wget -qO/dev/null "$url" 2>/dev/null && break
-			sleep 1
+			( wget -qO/dev/null "$url" 2>/dev/null ) &
+			local pid=$!
+			( sleep 5; kill $pid 2>/dev/null ) &
+			wait $pid && break
 		done
 	}
 }
