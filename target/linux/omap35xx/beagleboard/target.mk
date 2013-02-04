591c58781f6324d16bfc4541429d6142d8a80ee0
Gabor Juhos <juhosg@openwrt.org>
2013-02-04 15:23:04 +0000

    omap35xx: R.I.P.
    It is broken since ~22 months and nobody takes care of that.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35485 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/omap35xx/beagleboard/target.mk
+++
@@ -1,6 +0,0 @@
-BOARDNAME:=Beagleboard
-
-define Target/Description
-	Build firmware images for Beagleboard
-endef
-
--- target/linux/omap35xx/gumstix/target.mk
+++
@@ -1,7 +0,0 @@
-BOARDNAME:=Gumstix Overo
-LINUX_VERSION:=3.0.3
-
-define Target/Description
-	Build firmware images for Gumstix Overo Boards
-endef
-
