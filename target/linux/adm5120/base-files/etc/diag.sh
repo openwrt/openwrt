d5e9696c490b1b1a55b215b82036b3a2687074ca
Gabor Juhos <juhosg@openwrt.org>
2013-02-18 09:56:26 +0000

    adm5120: migrate to common led helper functions
    [juhosg: migrate to status_led_blink_{preinit,failsafe} helpers]
    
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35649 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/adm5120/base-files/etc/diag.sh
+++ target/linux/adm5120/base-files/etc/diag.sh
@@ -1,45 +1,17 @@
 #!/bin/sh
-#
-# Copyright (C) 2007 OpenWrt.org
-#
-#
+# Copyright (C) 2007-2013 OpenWrt.org
 
+. /lib/functions/leds.sh
 . /lib/adm5120.sh
-
-led_set_attr() {
-	[ -f "/sys/class/leds/$1/$2" ] && echo "$3" > "/sys/class/leds/$1/$2"
-}
-
-status_led_set_timer() {
-	led_set_attr $status_led "trigger" "timer"
-	led_set_attr $status_led "delay_on" "$1"
-	led_set_attr $status_led "delay_off" "$2"
-}
-
-status_led_set_morse() {
-	led_set_attr $status_led "trigger" "morse"
-	led_set_attr $status_led "delay" "$1"
-	led_set_attr $status_led "message" "$2"
-}
-
-status_led_on() {
-	led_set_attr $status_led "trigger" "none"
-	led_set_attr $status_led "brightness" 255
-}
-
-status_led_off() {
-	led_set_attr $status_led "trigger" "none"
-	led_set_attr $status_led "brightness" 0
-}
 
 set_state() {
 	case "$1" in
 	preinit)
 		insmod leds-gpio
-		status_led_set_timer 200 200
+		status_led_blink_preinit
 		;;
 	failsafe)
-		status_led_set_timer 50 50
+		status_led_blink_failsafe
 		;;
 	done)
 		status_led_on
