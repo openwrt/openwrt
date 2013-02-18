f81f68336cc8b680dc33141f71d3e4ccad033720
Florian Fainelli <florian@openwrt.org>
2013-02-18 10:37:55 +0000

    brcm63xx: migrate platform to common led helper functions
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35661 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/base-files/etc/diag.sh
+++ target/linux/brcm63xx/base-files/etc/diag.sh
@@ -1,60 +1,13 @@
 #!/bin/sh
-#
-# Copyright (C) 2007 OpenWrt.org
-#
-#
+# Copyright (C) 2007-2013 OpenWrt.org
 
+. /lib/functions/leds.sh
 . /lib/brcm63xx.sh
-
-led_set_attr() {
-	[ -f "/sys/class/leds/$1/$2" ] && echo "$3" > "/sys/class/leds/$1/$2"
-}
-
-status_led_set_timer() {
-	led_set_attr $status_led "trigger" "timer"
-	led_set_attr $status_led "delay_on" "$1"
-	led_set_attr $status_led "delay_off" "$2"
-	[ -n "$status_led2" ] && {
-		led_set_attr $status_led2 "trigger" "timer"
-		led_set_attr $status_led2 "delay_on" "$1"
-		led_set_attr $status_led2 "delay_off" "$2"
-	}
-}
-
-status_led_set_morse() {
-	led_set_attr $status_led "trigger" "morse"
-	led_set_attr $status_led "delay" "$1"
-	led_set_attr $status_led "message" "$2"
-	[ -n "$status_led2" ] && {
-		led_set_attr $status_led2 "trigger" "morse"
-		led_set_attr $status_led2 "delay" "$1"
-		led_set_attr $status_led2 "message" "$2"
-	}
-}
-
-status_led_on() {
-	led_set_attr $status_led "trigger" "none"
-	led_set_attr $status_led "brightness" 255
-	[ -n "$status_led2" ] && {
-		led_set_attr $status_led2 "trigger" "none"
-		led_set_attr $status_led2 "brightness" 255
-	}
-}
-
-status_led_off() {
-	led_set_attr $status_led "trigger" "none"
-	led_set_attr $status_led "brightness" 0
-	[ -n "$status_led2" ] && {
-		led_set_attr $status_led2 "trigger" "none"
-		led_set_attr $status_led2 "brightness" 0
-	}
-}
 
 set_state() {
 	case "$1" in
 	preinit)
 		insmod leds-gpio
-		status_led_on
 		status_led_set_timer 200 200
 		;;
 	failsafe)
