275db5b4c4c78d3036cb56b3686650d62c1f5733
Gabor Juhos <juhosg@openwrt.org>
2013-02-25 21:28:38 +0000

    ramips: remove port_power_off field from ehci_platform_data
    It is removed in 3.8. Also add a revert patch for 3.7.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 35799

--- target/linux/ramips/files/arch/mips/ralink/rt3883/devices.c
+++ target/linux/ramips/files/arch/mips/ralink/rt3883/devices.c
@@ -177,7 +177,6 @@ static void rt3883_usb_power_off(struct platform_device *pdev)
 }
 
 static struct usb_ehci_pdata rt3883_ehci_data = {
-	.port_power_off	= 1,
 	.power_on	= rt3883_usb_power_on,
 	.power_off	= rt3883_usb_power_off,
 };
