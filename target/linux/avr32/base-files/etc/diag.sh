5d2ba776636f82adb6857aca84fe9b8146f340d0
Gabor Juhos <juhosg@openwrt.org>
2013-03-12 13:28:21 +0000

    avr32: add diag support
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@35980 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/avr32/base-files/etc/diag.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright (C) 2013 OpenWrt.org
+
+. /lib/functions/leds.sh
+
+set_state() {
+	status_led="sys"
+
+	case "$1" in
+	preinit)
+		insmod leds-gpio
+		status_led_blink_preinit
+		;;
+
+	failsafe)
+		status_led_blink_failsafe
+		;;
+
+	done)
+		status_led_on
+		;;
+	esac
+}
