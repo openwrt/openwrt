e3a6ea65e1a80bcfa3f05ea38a22853dd464944d
John Crispin <john@openwrt.org>
2013-03-12 13:52:54 +0000

    add support for AudioCodes AC-252
    user-space support for extracting the EEPROM for the RaLink WiFi is still
    missing. Besides this, it's all working.
    
    Signed-off-by: Daniel Golle <dgolle@allnet.de>
    
    SVN-Revision: 35990

---
+++ target/linux/lantiq/xway/profiles/audiocodes.mk
@@ -0,0 +1,9 @@
+define Profile/ACMP252
+  NAME:=AudioCodes MediaPack MP-252
+  PACKAGES:=kmod-rt2x00 wpad-mini \
+	kmod-ltq-adsl-danube-mei kmod-ltq-adsl-danube \
+	kmod-ltq-adsl-danube-fw-a kmod-ltq-ptm-danube \
+	ltq-adsl-app
+endef
+
+$(eval $(call Profile,ACMP252))
