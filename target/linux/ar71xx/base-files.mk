dcb02ff0806c752b039ceaac7990fdac0e96e909
John Crispin <blogic@openwrt.org>
2013-03-13 18:11:00 +0000

    [ar71xx] make sure the console works when using procd as our pid 1
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36000 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/base-files.mk
+++ target/linux/ar71xx/base-files.mk
@@ -1,5 +1,7 @@
 define Package/base-files/install-target
 	rm -f $(1)/etc/config/network
+	$(if $(CONFIG_PROCD_INIT),rm -f  $(1)/etc/uci-defaults/00_inittab-console-fixup)
+	$(if $(CONFIG_PROCD_INIT),echo "::askconsole:/bin/ash --login" >> $(1)/etc/inittab)
 endef
 
 
