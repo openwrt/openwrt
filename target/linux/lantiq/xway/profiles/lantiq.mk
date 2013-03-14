9b4a8c6d1e7a30ccce63d77b45e063af4453e366
John Crispin <john@openwrt.org>
2013-03-14 18:43:44 +0000

    select vdsl packages on vr9 boards
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 36028

--- target/linux/lantiq/xway/profiles/lantiq.mk
+++ target/linux/lantiq/xway/profiles/lantiq.mk
@@ -6,12 +6,22 @@ $(eval $(call Profile,EASY50712))
 
 define Profile/EASY80920NOR
   NAME:=Lantiq VR9 - EASY80920NOR
+  PACKAGES:=kmod-ath9k wpad-mini \
+	kmod-ltq-deu-vr9 kmod-ltq-hcd-vr9 \
+	kmod-ltq-vdsl-vr9-mei kmod-ltq-vdsl-vr9 \
+	kmod-ltq-atm-vr9 ltq-vdsl-vr9-fw-installer \
+	ltq-vdsl-app ppp-mod-pppoa
 endef
 
 $(eval $(call Profile,EASY80920NOR))
 
 define Profile/EASY80920NAND
   NAME:=Lantiq VR9 - EASY80920NAND
+  PACKAGES:=kmod-ath9k wpad-mini \
+	kmod-ltq-deu-vr9 kmod-ltq-hcd-vr9 \
+	kmod-ltq-vdsl-vr9-mei kmod-ltq-vdsl-vr9 \
+	kmod-ltq-atm-vr9 ltq-vdsl-vr9-fw-installer \
+	ltq-vdsl-app ppp-mod-pppoa
 endef
 
 $(eval $(call Profile,EASY80920NAND))
