dce4945972fad3072321f54d54036829431a137b
John Crispin <john@openwrt.org>
2013-04-03 09:59:16 +0000

    mark rt288x broken until the test hardware arrives in the mail
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 36164

--- target/linux/ramips/rt288x/target.mk
+++ target/linux/ramips/rt288x/target.mk
@@ -5,6 +5,7 @@
 SUBTARGET:=rt288x
 BOARDNAME:=RT288x based boards
 CFLAGS+= -mips32r2 -mtune=mips32r2
+FEATURES:=broken
 
 define Target/Description
 	Build firmware images for Ralink RT288x based boards.
