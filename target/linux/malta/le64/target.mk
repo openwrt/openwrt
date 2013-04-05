bc6e64b3260e9e41b8bb99dba5837fc7267b43fd
Florian Fainelli <florian@openwrt.org>
2013-04-05 12:36:48 +0000

    malta: add le64 and be64 sub targets
    These two subtargets are used to build a 64-bit malta kernel along with
    64-bit userland support.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36211 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/malta/le64/target.mk
@@ -0,0 +1,11 @@
+ARCH:=mips64el
+ARCH_PACKAGES:=malta_mipsel64
+SUBTARGET:=le64
+BOARDNAME:=Little Endian (64-bits)
+CFLAGS:=-Os -pipe -mips64 -mtune=mips64 -fno-caller-saves
+FEATURES:=ramdisk
+
+define Target/Description
+	Build LE firmware images for MIPS Malta CoreLV board running in
+	little-endian and 64-bits mode.
+endef
