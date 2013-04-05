46dfa04a083d6d08c31c9fd1f38bceb398f5dabe
Florian Fainelli <florian@openwrt.org>
2013-04-05 12:36:16 +0000

    malta: enable mips16 feature flag
    The malta platform supports mips16 advertise this feature flag.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    SVN-Revision: 36203

--- target/linux/malta/be/target.mk
+++ target/linux/malta/be/target.mk
@@ -2,7 +2,7 @@ ARCH:=mips
 ARCH_PACKAGES:=malta_mips
 SUBTARGET:=be
 BOARDNAME:=Big Endian
-FEATURES:=ramdisk
+FEATURES:=ramdisk mips16
 
 define Target/Description
 	Build BE firmware images for MIPS Malta CoreLV board running in
--- target/linux/malta/le/target.mk
+++ target/linux/malta/le/target.mk
@@ -2,7 +2,7 @@ ARCH:=mipsel
 ARCH_PACKAGES:=malta_mipsel
 SUBTARGET:=le
 BOARDNAME:=Little Endian
-FEATURES:=ramdisk
+FEATURES:=ramdisk mips16
 
 define Target/Description
 	Build LE firmware images for MIPS Malta CoreLV board running in
