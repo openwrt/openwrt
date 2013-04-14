9198c2f4ab51cd1909ba539fbc11a499e1479e79
Gabor Juhos <juhosg@openwrt.org>
2013-04-14 09:25:53 +0000

    kernel: update linux 3.8 to 3.8.7
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36332 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -11,8 +11,8 @@ endif
 ifeq ($(LINUX_VERSION),3.7.10)
   LINUX_KERNEL_MD5SUM:=09624c3702107076efcac5e1cd8a18ec
 endif
-ifeq ($(LINUX_VERSION),3.8.6)
-  LINUX_KERNEL_MD5SUM:=0934e6b31931c1832e48a28bd35ef27f
+ifeq ($(LINUX_VERSION),3.8.7)
+  LINUX_KERNEL_MD5SUM:=37ce9b5cc33551b45fea59c1c4c9da13
 endif
 
 # disable the md5sum check for unknown kernel versions
