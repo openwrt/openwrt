1780aae0a88de1b039db06b0b27b566a9813f76e
Jonas Gorski <jogo@openwrt.org>
2013-05-02 15:28:31 +0000

    bcm63xx: split into two subtargets, generic and SMP
    Make SMP a subtarget, since it only supports BCM6362 and BCM6368.
    
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36527 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm63xx/smp/profiles/101-Broadcom-wl.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2010 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Broadcom-wl
+  NAME:=Broadcom BCM43xx WiFi (wl, proprietary)
+  PACKAGES:= kmod-brcm-wl wlc nas
+endef
+
+define Profile/Broadcom-wl/Description
+	Package set compatible with hardware using Broadcom BCM43xx cards
+	using the proprietary broadcom wireless "wl" driver.
+endef
+
+$(eval $(call Profile,Broadcom-wl))
+
