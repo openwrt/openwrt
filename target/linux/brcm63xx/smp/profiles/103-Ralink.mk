24636fcabf64031a93a930ea1cd26f086acd30f8
Jonas Gorski <jogo@openwrt.org>
2013-05-02 15:28:31 +0000

    bcm63xx: split into two subtargets, generic and SMP
    Make SMP a subtarget, since it only supports BCM6362 and BCM6368.
    
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    SVN-Revision: 36527

---
+++ target/linux/brcm63xx/smp/profiles/103-Ralink.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2006 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Ralink
+  NAME:=Ralink WiFi
+  PACKAGES:=kmod-rt61-pci wpad-mini
+endef
+$(eval $(call Profile,Ralink))
+
