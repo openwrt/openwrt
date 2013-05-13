761238ce0aa9fbeff2fbceb10e5fb2c02ba6600c
Steven Barth <steven@midlink.org>
2013-05-13 17:12:20 +0000

    firewall3: Remove obsoleted ULA-border
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36624 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/network/config/firewall3/files/ipv6-ula-border.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ULA_PREFIX=$(uci -q get network.globals.ula_prefix)
-[ -n "$ULA_PREFIX" ] || exit 0
-
-ip6tables -I delegate_forward -s $ULA_PREFIX -m comment --comment "Enforce ULA-Border" -j zone_wan_dest_REJECT
-ip6tables -I delegate_forward -d $ULA_PREFIX -m comment --comment "Enforce ULA-Border" -j zone_wan_dest_REJECT
