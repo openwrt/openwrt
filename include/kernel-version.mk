6e2b85b4016a2db5f49fb7da1ef479a82979393c
Jonas Gorski <jogo@openwrt.org>
2013-05-19 18:31:51 +0000

    kernel: update 3.9 to 3.9.2
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    SVN-Revision: 36658

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -14,8 +14,8 @@ endif
 ifeq ($(LINUX_VERSION),3.8.12)
   LINUX_KERNEL_MD5SUM:=a4be720ca9e61445562565856bb5154d
 endif
-ifeq ($(LINUX_VERSION),3.9.1)
-  LINUX_KERNEL_MD5SUM:=913bbf85c65f3e587a5000763374b4aa
+ifeq ($(LINUX_VERSION),3.9.2)
+  LINUX_KERNEL_MD5SUM:=e406cc1bb5618a4ea88470f4d3d3d6e1
 endif
 
 # disable the md5sum check for unknown kernel versions
