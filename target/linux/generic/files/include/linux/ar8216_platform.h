267fc66f7716684b2220adc37201d1c59f493177
Gabor Juhos <juhosg@openwrt.org>
2013-05-30 17:38:26 +0000

    generic: ar8216: allow to specify SGMII config via platform data
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36798 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/generic/files/include/linux/ar8216_platform.h
+++ target/linux/generic/files/include/linux/ar8216_platform.h
@@ -63,6 +63,11 @@ struct ar8327_port_cfg {
 	int duplex:1;
 };
 
+struct ar8327_sgmii_cfg {
+	u32 sgmii_ctrl;
+	bool serdes_aen;
+};
+
 struct ar8327_led_cfg {
 	u32 led_ctrl0;
 	u32 led_ctrl1;
@@ -75,6 +80,7 @@ struct ar8327_platform_data {
 	struct ar8327_pad_cfg *pad0_cfg;
 	struct ar8327_pad_cfg *pad5_cfg;
 	struct ar8327_pad_cfg *pad6_cfg;
+	struct ar8327_sgmii_cfg *sgmii_cfg;
 	struct ar8327_port_cfg port0_cfg;
 	struct ar8327_port_cfg port6_cfg;
 	struct ar8327_led_cfg *led_cfg;
