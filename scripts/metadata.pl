982f0f338647610336671a268190ea7d98410bcd
Jonas Gorski <jogo@openwrt.org>
2013-06-05 20:58:03 +0000

    target: split jffs2 NAND out of jffs2
    Make jffs2_nand a separate option so this can be triggered without forcing
    jffs2 images for nor targets.
    
    Adds a new NAND_BLOCKSIZE variable that allows setting the generated layout
    in <page_size>:<block_size> pairs.
    
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36860 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/metadata.pl
+++ scripts/metadata.pl
@@ -165,7 +165,8 @@ sub target_config_features(@) {
 		/pcmcia/ and $ret .= "\tselect PCMCIA_SUPPORT\n";
 		/rtc/ and $ret .= "\tselect RTC_SUPPORT\n";
 		/squashfs/ and $ret .= "\tselect USES_SQUASHFS\n";
-		/jffs2/ and $ret .= "\tselect USES_JFFS2\n";
+		/jffs2$/ and $ret .= "\tselect USES_JFFS2\n";
+		/jffs2_nand/ and $ret .= "\tselect USES_JFFS2_NAND\n";
 		/ext4/ and $ret .= "\tselect USES_EXT4\n";
 		/targz/ and $ret .= "\tselect USES_TARGZ\n";
 		/cpiogz/ and $ret .= "\tselect USES_CPIOGZ\n";
