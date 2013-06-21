44b1688e6c7b4f16f7165fbd560e1183aef69090
John Crispin <john@openwrt.org>
2013-06-21 16:54:15 +0000

    lantiq: Tune the XWAY subtarget cflags
    This patch sets the cflags -mtune=34kc -mdsp only in XWAY subtarget, while the others remain with mtune=mips32r2.
    Although the Danube has a Mips 24KEc core performs well with the above cflags.
    
    Signed-off-by: José Vázquez Fernández <ppvazquezfer@gmail.com>
    
    SVN-Revision: 37006

--- target/linux/lantiq/ase/target.mk
+++ target/linux/lantiq/ase/target.mk
@@ -2,6 +2,7 @@ ARCH:=mips
 SUBTARGET:=ase
 BOARDNAME:=Amazon-SE
 FEATURES:=squashfs atm
+CFLAGS+= -mtune=mips32r2
 
 DEFAULT_PACKAGES+=kmod-pppoa ppp-mod-pppoa linux-atm atm-tools br2684ctl kmod-ltq-dsl-ase ltq-dsl-app
 
