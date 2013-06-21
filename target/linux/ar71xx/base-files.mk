ce0ccca1f72eede3873534cec54f152b3e277362
John Crispin <blogic@openwrt.org>
2013-06-21 16:53:11 +0000

    ar71xx: drop procd_init console hack
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@36992 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/base-files.mk
+++ target/linux/ar71xx/base-files.mk
@@ -1,7 +1,3 @@
 define Package/base-files/install-target
 	rm -f $(1)/etc/config/network
-	$(if $(CONFIG_PROCD_INIT),rm -f  $(1)/etc/uci-defaults/00_inittab-console-fixup)
-	$(if $(CONFIG_PROCD_INIT),echo "::askconsole:/bin/ash --login" >> $(1)/etc/inittab)
 endef
-
-
