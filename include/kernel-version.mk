565a1a3a6e5db789c2867851db86f42af9a6737c
Jonas Gorski <jogo@openwrt.org>
2013-07-01 13:21:06 +0000

    kernel: update linux 3.10 to 3.10 final
    3.10 final is out, so refresh patches and add md5sum.
    
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@37116 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -17,6 +17,9 @@ endif
 ifeq ($(LINUX_VERSION),3.9.8)
   LINUX_KERNEL_MD5SUM:=04d74575ef1c484ad00be61f32bb4557
 endif
+ifeq ($(LINUX_VERSION),3.10)
+  LINUX_KERNEL_MD5SUM:=4f25cd5bec5f8d5a7d935b3f2ccb8481
+endif
 
 # disable the md5sum check for unknown kernel versions
 LINUX_KERNEL_MD5SUM?=x
