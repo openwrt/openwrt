d20dabe8e8e100f06ade10e6c60b11564c17ff1e
John Crispin <blogic@openwrt.org>
2013-07-11 17:00:49 +0000

    procd: the delete ubus call was passed the wrong field name for services
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@37247 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -184,7 +184,7 @@ _procd_kill() {
 	local instance="$2"
 
 	json_init
-	[ -n "$service" ] && json_add_string service "$service"
+	[ -n "$service" ] && json_add_string name "$service"
 	[ -n "$instance" ] && json_add_string instance "$instance"
 	_procd_ubus_call delete
 }
