3ad136d9e3e65ac43db0eca40a7ead4111511468
John Crispin <john@openwrt.org>
2013-07-24 08:54:41 +0000

    ramips: Basic support for the AsiaRF RT5350 based AWM002 module and evaluation board.
    Basic support for the AsiaRF RT5350 based AWM002 module and evaluation board.
    http://www.asiarf.com/Smallest-Tiny-Ralink-802-11n-Wireless-AP-Router-Module-Board-AWM002-product-view-375.html
    
    Signed-off-by Jon Smirl <jonsmirl@gmail.com>
    
    SVN-Revision: 37521

---
+++ target/linux/ramips/rt305x/profiles/asiarf.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2012 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/AWM002-EVB
+	NAME:=AsiaRF AWM002-EVB
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev \
+	kmod-i2c-core kmod-i2c-gpio
+endef
+
+define Profile/AWM002-EVB/Description
+	Package set for AsiaRF AWM002 Evaluation Board
+endef
+
+$(eval $(call Profile,AWM002-EVB))
