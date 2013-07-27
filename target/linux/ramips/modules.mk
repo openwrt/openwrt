d9c021959b01d3314ec9570f0429c5c319014acb
John Crispin <blogic@openwrt.org>
2013-07-27 09:23:40 +0000

    ramips: add back dwc_otg driver
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@37566 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/modules.mk
+++ target/linux/ramips/modules.mk
@@ -5,6 +5,27 @@
 # See /LICENSE for more information.
 #
 
+define KernelPackage/usb-rt305x-dwc_otg
+  TITLE:=RT305X USB controller driver
+  DEPENDS:=@TARGET_ramips_rt305x
+  KCONFIG:= \
+	CONFIG_DWC_OTG \
+	CONFIG_DWC_OTG_HOST_ONLY=y \
+	CONFIG_DWC_OTG_DEVICE_ONLY=n \
+	CONFIG_DWC_OTG_DEBUG=n
+  FILES:=$(LINUX_DIR)/drivers/usb/dwc_otg/dwc_otg.ko
+  AUTOLOAD:=$(call AutoLoad,54,dwc_otg,1)
+  $(call AddDepends/usb)
+endef
+
+define KernelPackage/usb-rt305x-dwc_otg/description
+ This driver provides USB Device Controller support for the
+ Synopsys DesignWare USB OTG Core used in the Ralink RT305X SoCs.
+endef
+
+$(eval $(call KernelPackage,usb-rt305x-dwc_otg))
+
+
 I2C_RALINK_MODULES:= \
   CONFIG_I2C_RALINK:drivers/i2c/busses/i2c-ralink
 
