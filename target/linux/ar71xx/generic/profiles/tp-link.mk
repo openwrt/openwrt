ddee72b34d95e89fa8157910cd76a26de6a58d36
Gabor Juhos <juhosg@openwrt.org>
2013-07-28 10:23:31 +0000

    ar71xx: build image for the MERCURY MW4530R board
    Mercury MW4530R is a TP-Link TL-WDR4310 clone but
    with a much lower price tag (~ $40). This patch
    is tested to work at trunk r37525.
    
    Signed-off-by: Leon Xu <ylxu72@gmail.com>
    Patchwork: http://patchwork.openwrt.org/patch/3856/
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@37571 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/generic/profiles/tp-link.mk
+++ target/linux/ar71xx/generic/profiles/tp-link.mk
@@ -159,12 +159,12 @@ $(eval $(call Profile,TLWA901))
 
 
 define Profile/TLWDR4300
-	NAME:=TP-LINK TL-WDR3500/3600/4300/4310
+	NAME:=TP-LINK TL-WDR3500/3600/4300/4310/MW4350R
 	PACKAGES:=kmod-usb-core kmod-usb2 kmod-ledtrig-usbdev
 endef
 
 define Profile/TLWDR4300/Description
-	Package set optimized for the TP-LINK TL-WDR3500/3600/4300/4310.
+	Package set optimized for the TP-LINK TL-WDR3500/3600/4300/4310/MW4350R.
 endef
 $(eval $(call Profile,TLWDR4300))
 
