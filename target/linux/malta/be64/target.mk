18f807fbc53c99af441867c9de92bef7f91b1767
Luka Perkov <luka@openwrt.org>
2013-09-18 23:03:47 +0000

    malta: target.mk cosmetical fixup
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38047 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/malta/be64/target.mk
+++ target/linux/malta/be64/target.mk
@@ -2,8 +2,8 @@ ARCH:=mips64
 ARCH_PACKAGES:=malta_mips64
 SUBTARGET:=be64
 BOARDNAME:=Big Endian (64-bits)
-CFLAGS:=-Os -pipe -mips64 -mtune=mips64
 FEATURES:=ramdisk
+CFLAGS:=-Os -pipe -mips64 -mtune=mips64
 
 define Target/Description
 	Build BE firmware images for MIPS Malta CoreLV board running in
--- target/linux/malta/le64/target.mk
+++ target/linux/malta/le64/target.mk
@@ -2,10 +2,10 @@ ARCH:=mips64el
 ARCH_PACKAGES:=malta_mipsel64
 SUBTARGET:=le64
 BOARDNAME:=Little Endian (64-bits)
-CFLAGS:=-Os -pipe -mips64 -mtune=mips64
 FEATURES:=ramdisk
+CFLAGS:=-Os -pipe -mips64 -mtune=mips64
 
 define Target/Description
 	Build LE firmware images for MIPS Malta CoreLV board running in
-	little-endian and 64-bits mode.
+	little-endian and 64-bits mode
 endef
