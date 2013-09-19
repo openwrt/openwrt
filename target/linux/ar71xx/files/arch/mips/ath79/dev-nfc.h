83915a8d78db3446f05dcaad992afd53256f36ea
Gabor Juhos <juhosg@openwrt.org>
2013-09-19 18:43:42 +0000

    ar71xx: add ath79_nfc_set_ecc_mode helper
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 38070

--- target/linux/ar71xx/files/arch/mips/ath79/dev-nfc.h
+++ target/linux/ar71xx/files/arch/mips/ath79/dev-nfc.h
@@ -12,12 +12,14 @@
 #define _ATH79_DEV_NFC_H
 
 struct mtd_partition;
+enum ar934x_nfc_ecc_mode;
 
 #ifdef CONFIG_ATH79_DEV_NFC
 void ath79_nfc_set_parts(struct mtd_partition *parts, int nr_parts);
 void ath79_nfc_set_select_chip(void (*f)(int chip_no));
 void ath79_nfc_set_scan_fixup(int (*f)(struct mtd_info *mtd));
 void ath79_nfc_set_swap_dma(bool enable);
+void ath79_nfc_set_ecc_mode(enum ar934x_nfc_ecc_mode mode);
 void ath79_register_nfc(void);
 #else
 static inline void ath79_nfc_set_parts(struct mtd_partition *parts,
@@ -25,6 +27,7 @@ static inline void ath79_nfc_set_parts(struct mtd_partition *parts,
 static inline void ath79_nfc_set_select_chip(void (*f)(int chip_no)) {}
 static inline void ath79_nfc_set_scan_fixup(int (*f)(struct mtd_info *mtd)) {}
 static inline void ath79_nfc_set_swap_dma(bool enable) {}
+static inline void ath79_nfc_set_ecc_mode(enum ar934x_nfc_ecc_mode mode) {}
 static inline void ath79_register_nfc(void) {}
 #endif
 
