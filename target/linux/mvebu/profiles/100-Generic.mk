b5e56bd31fd50f9c099cf08788be70f46f82eb02
Luka Perkov <luka@openwrt.org>
2013-09-27 10:49:36 +0000

    mvebu: enable thermal module by default
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    SVN-Revision: 38232

--- target/linux/mvebu/profiles/100-Generic.mk
+++ target/linux/mvebu/profiles/100-Generic.mk
@@ -7,14 +7,15 @@
 
 define Profile/Generic
   NAME:=Generic (default)
-  PACKAGES:=kmod-mmc kmod-mvsdio kmod-usb2 kmod-usb-storage \
-	    kmod-of-i2c kmod-i2c-core kmod-i2c-mv64xxx \
-	    kmod-ata-core kmod-ata-marvell-sata \
-	    kmod-rtc-marvell
+  PACKAGES:= \
+	kmod-mmc kmod-mvsdio kmod-usb2 kmod-usb-storage \
+	kmod-of-i2c kmod-i2c-core kmod-i2c-mv64xxx \
+	kmod-ata-core kmod-ata-marvell-sata \
+	kmod-rtc-marvell kmod-thermal-armada
 endef
 
 define Profile/Generic/Description
-	Package set compatible with most Marvell Armada 370/XP based boards
+ Package set compatible with most Marvell Armada 370/XP based boards.
 endef
-$(eval $(call Profile,Generic))
 
+$(eval $(call Profile,Generic))
