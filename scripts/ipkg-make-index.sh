f578cfad56a4dac3b4e44198d6b49d34682ce734
Steven Barth <steven@midlink.org>
2013-10-04 05:56:20 +0000

    opkg: add support for SHA256 verification
    based on a patch by Evan Hunt <each@isc.org>
    Signed-off-by: Steven Barth <steven@midlink.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38302 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/ipkg-make-index.sh
+++ scripts/ipkg-make-index.sh
@@ -18,11 +18,13 @@ for pkg in `find $pkg_dir -name '*.ipk' | sort`; do
 	echo "Generating index for package $pkg" >&2
 	file_size=$(ls -l $pkg | awk '{print $5}')
 	md5sum=$(md5sum $pkg | awk '{print $1}')
+	sha256sum=$(openssl sha256 $pkg | awk '{print $2}')
 	# Take pains to make variable value sed-safe
 	sed_safe_pkg=`echo $pkg | sed -e 's/^\.\///g' -e 's/\\//\\\\\\//g'`
 	tar -xzOf $pkg ./control.tar.gz | tar xzOf - ./control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
 Size: $file_size\\
 MD5Sum: $md5sum\\
+SHA256sum: $sha256sum\\
 Description:/"
 	echo ""
 done
