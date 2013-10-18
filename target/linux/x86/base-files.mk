e855d498e34227ebd690c48aa5f4ae7df8807433
Jo-Philipp Wich <jow@openwrt.org>
2013-10-18 11:45:33 +0000

    x86: fix condition in base-files.mk
    TARGET_x86_generic_Soekris48xx is not defined,
    CONFIG_TARGET_x86_generic_Soekris48xx is
    
    This changes makes it possible to override the network config for soekris net48xx
    and net45xx targets.
    
    Signed-off-by: Bruno Randolf <br1@einfach.org>
    
    SVN-Revision: 38450

--- target/linux/x86/base-files.mk
+++ target/linux/x86/base-files.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_x86_generic_Soekris48xx)$(TARGET_x86_generic_Soekris45xx),)
+ifneq ($(CONFIG_TARGET_x86_generic_Soekris48xx)$(CONFIG_TARGET_x86_generic_Soekris45xx),)
 define Package/base-files/install-target
 	rm -f $(1)/etc/config/network
 endef
