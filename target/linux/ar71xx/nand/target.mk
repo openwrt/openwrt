3080df79e77d509ac76648ee50d362640722a2f0
Gabor Juhos <juhosg@openwrt.org>
2013-10-23 15:43:19 +0000

    ar71xx: change title and description for NAND subtarget
    The nand subtarget supports Mikrotik boards only. Change
    the title and the description to reflect that.
    
    Based on the patch by Stefan Agner:
    http://patchwork.openwrt.org/patch/4123/
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38508 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/nand/target.mk
+++ target/linux/ar71xx/nand/target.mk
@@ -1,8 +1,8 @@
-BOARDNAME:=Devices with NAND flash (mostly Mikrotik)
+BOARDNAME:=Mikrotik devices with NAND flash
 
 define Target/Description
-	Build firmware images for Atheros AR71xx/AR913x based boards with 
-	NAND flash, e.g. MikroTik RB-4xx or RB-750
+	Build firmware images for Atheros AR71xx/AR913x based Mikrotik boards.
+	e.g. MikroTik RB-4xx or RB-750
 endef
 
 
