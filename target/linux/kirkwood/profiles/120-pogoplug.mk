6e7a56d367d8fc43e91e4bf993acddcaddcaf3ea
Luka Perkov <luka@openwrt.org>
2013-10-31 12:04:04 +0000

    kirkwood: add userland support for Pogoplug E02
    This patch adds the userland support for the Pogoplug E02 by Cloud
    Engines, Inc.
    
    Signed-off-by: Felix Kaechele <heffer@fedoraproject.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38631 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/kirkwood/profiles/120-pogoplug.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/POGOE02
+  NAME:=Cloud Engines Pogoplug E02
+  PACKAGES:= \
+	kmod-usb2 kmod-usb-storage uboot-envtools
+endef
+
+define Profile/POGOE02/Description
+ Package set compatible with Cloud Engines Pogoplug E02 board.
+endef
+
+POGOE02_UBIFS_OPTS:="-m 2048 -e 128KiB -c 4096 -U"
+POGOE02_UBI_OPTS:="-m 2048 -p 128KiB -s 512"
+
+$(eval $(call Profile,POGOE02))
