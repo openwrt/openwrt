38671e769caad346f43f7a082dc099336f526b54
Gabor Juhos <juhosg@openwrt.org>
2013-11-04 18:37:08 +0000

    ar71xx: Add new subtarget nand for WNDR4300
    Add new subtarget nand with UBI/UBIFS including GLUEBI and squashfs.
    This target will be used for NAND based router which make use of
    UBI (and maybe UBIFS later on) in order to provide proper NAND
    support.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    [juhosg:
      - tweak target.mk:
         change BOARDNAME variable,
         improve Target/Description,
         remove targz and ubifs from FEATURES and add broken flag,
      - optimize kernel configuration:
         disable ubifs, it is not used yet,
         disable unused MTD options,
         disable SPI support
         disable unused network drivers,
         tune kernel command line,
      - remove image/ubinize.cfg, it will be in a follow-up patch
      - move profile modifications into a separate patch]
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38646 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ar71xx/nand/target.mk
@@ -0,0 +1,7 @@
+BOARDNAME := Generic devices with NAND flash
+FEATURES += broken squashfs
+
+define Target/Description
+	Build firmware images for Atheros AR71xx/AR913x based boards with
+	NAND flash, e.g. Netgear WNDR4300.
+endef
