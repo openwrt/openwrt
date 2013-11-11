0b335e2ac296020601ff31dadb7cd9ed66a7c686
Zoltan HERPAI <wigyori@uid0.hu>
2013-11-11 22:03:35 +0000

    kernel: of_i2c is moved into i2c-core in 3.12, add kernel version check into modules/i2c.mk
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38760 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/linux/modules/i2c.mk
+++ package/kernel/linux/modules/i2c.mk
@@ -24,8 +24,10 @@ I2C_CORE_MODULES:= \
   CONFIG_I2C:drivers/i2c/i2c-core \
   CONFIG_I2C_CHARDEV:drivers/i2c/i2c-dev
 
-ifeq ($(CONFIG_OF),y)
-  I2C_CORE_MODULES+=CONFIG_OF_I2C:drivers/of/of_i2c
+ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),lt,3.12.0)),1)
+  ifeq ($(CONFIG_OF),y)
+    I2C_CORE_MODULES+=CONFIG_OF_I2C:drivers/of/of_i2c
+  endif
 endif
 
 define KernelPackage/i2c-core
