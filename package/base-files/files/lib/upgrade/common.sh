f82ba9565701eb3c9fb2e8db69b756d1e31c6bff
Hauke Mehrtens <hauke@openwrt.org>
2013-11-11 22:12:58 +0000

    base-files: don't kill nas for sysupgrade
    Signed-off-by: Nathan Hintz <nlhintz@hotmail.com>
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@38766 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -99,7 +99,7 @@ kill_remaining() { # [ <signal> ]
 
 		case "$name" in
 			# Skip essential services
-			*procd*|*ash*|*init*|*watchdog*|*ssh*|*dropbear*|*telnet*|*login*|*hostapd*|*wpa_supplicant*) : ;;
+			*procd*|*ash*|*init*|*watchdog*|*ssh*|*dropbear*|*telnet*|*login*|*hostapd*|*wpa_supplicant*|*nas*) : ;;
 
 			# Killable process
 			*)
