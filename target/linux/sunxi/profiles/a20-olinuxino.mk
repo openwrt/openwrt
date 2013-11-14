f58bfd1df4e4a9b84a0e94691986c38bab6d8730
Zoltan Herpai <wigyori@uid0.hu>
2013-11-14 23:12:52 +0000

    sunxi: rework target - update kernel to 3.12 - add patches for clocks, i2c, usb, sid, rtc - support common image for A10/A13/A20 - add support for a couple boards - most drivers are configured into the kernel as of now
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    SVN-Revision: 38811

---
+++ target/linux/sunxi/profiles/a20-olinuxino.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/A20-OLinuXino_MICRO
+	NAME:=A20 OLinuXino Micro
+	PACKAGES:=\
+		uboot-sunxi-A20-OLinuXino_MICRO
+endef
+
+define Profile/A20-OLinuXino_MICRO/Description
+	Package set optimized for the Olimex A20 OLinuXino micro
+endef
+
+$(eval $(call Profile,A20-OLinuXino_MICRO))
---
+++ target/linux/sunxi/profiles/cubieboard.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Cubieboard
+	NAME:=Cubieboard
+	PACKAGES:=\
+		uboot-sunxi-Cubieboard
+endef
+
+define Profile/Cubieboard/Description
+	Package set optimized for the Cubieboard
+endef
+
+$(eval $(call Profile,Cubieboard))
