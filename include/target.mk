5de876bc78b7b47db6dd369067e87c254fd467e7
Imre Kaloz <kaloz@openwrt.org>
2013-11-29 10:25:43 +0000

    there's no such thing as a soft fpu, this target should be fine without these
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    
    SVN-Revision: 38940

--- include/target.mk
+++ include/target.mk
@@ -233,7 +233,6 @@ ifeq ($(DUMP),1)
     CPU_CFLAGS_xscale = -march=armv5te -mtune=xscale
     CPU_CFLAGS_vfp = -mfpu=vfp -mfloat-abi=softfp
     CPU_CFLAGS_vfpv3 = -mfpu=vfpv3-d16 -mfloat-abi=softfp
-    CPU_CFLAGS_soft = -mfloat-abi=softfp
   endif
   ifeq ($(ARCH),powerpc)
     CPU_CFLAGS_603e:=-mcpu=603e
