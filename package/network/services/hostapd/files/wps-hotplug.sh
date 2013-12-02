a26242cb6390ec0b5b4b69a9d35ecc2023f68bb5
Felix Fietkau <nbd@openwrt.org>
2013-12-02 13:08:14 +0000

    hostapd: change the wildcard for the hostapd control socket directory
    prepare for using /var/run/hostapd instead of /var/run/hostapd-phy*
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 38986

--- package/network/services/hostapd/files/wps-hotplug.sh
+++ package/network/services/hostapd/files/wps-hotplug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$ACTION" = "pressed" -a "$BUTTON" = "wps" ]; then
-	for dir in /var/run/hostapd-*; do
+	for dir in /var/run/hostapd*; do
 		[ -d "$dir" ] || continue
 		hostapd_cli -p "$dir" wps_pbc
 	done
