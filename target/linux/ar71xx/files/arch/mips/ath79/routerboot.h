6d5b1475361030a88421d80e9875355687154588
Gabor Juhos <juhosg@openwrt.org>
2013-12-16 10:11:01 +0000

    ar71xx: add some RouterBOOT specific helper functions
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39091 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ath79/routerboot.h
+++ target/linux/ar71xx/files/arch/mips/ath79/routerboot.h
@@ -11,11 +11,35 @@
 #ifndef _ATH79_ROUTERBOOT_H_
 #define _ATH79_ROUTERBOOT_H_
 
+struct rb_info {
+	unsigned int hard_cfg_offs;
+	unsigned int hard_cfg_size;
+	void *hard_cfg_data;
+	unsigned int soft_cfg_offs;
+
+	const char *board_name;
+	u32 hw_options;
+};
+
 #ifdef CONFIG_ATH79_ROUTERBOOT
+const struct rb_info *rb_init_info(void *data, unsigned int size);
+void *rb_get_wlan_data(void);
+
 int routerboot_find_tag(u8 *buf, unsigned int buflen, u16 tag_id,
 			u8 **tag_data, u16 *tag_len);
 int routerboot_find_magic(u8 *buf, unsigned int buflen, u32 *offset, bool hard);
 #else
+static inline const struct rb_info *
+rb_init_info(void *data, unsigned int size)
+{
+	return NULL;
+}
+
+static inline void *rb_get_wlan_data(void)
+{
+	return NULL;
+}
+
 static inline int
 routerboot_find_tag(u8 *buf, unsigned int buflen, u16 tag_id,
 		    u8 **tag_data, u16 *tag_len)
