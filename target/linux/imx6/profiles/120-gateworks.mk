b27f84034b0c742e38b2668a6614feca8ab9223b
Luka Perkov <luka@openwrt.org>
2013-12-17 02:47:20 +0000

    imx6: ventana: add canbus utils
    Some of the Gateworks Ventana boards have CAN bus support, therefore
    we will install the userspace utils.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>
    
    SVN-Revision: 39113

--- target/linux/imx6/profiles/120-gateworks.mk
+++ target/linux/imx6/profiles/120-gateworks.mk
@@ -10,7 +10,8 @@ define Profile/VENTANA
   PACKAGES:= \
 	kmod-thermal-imx kmod-sky2 kmod-usb-chipidea kmod-usb-mxs-phy \
 	kmod-sound-soc-imx kmod-sound-soc-imx-sgtl5000 \
-	kmod-can kmod-can-flexcan \
+	kmod-can kmod-can-flexcan canutils canutils-canconfig \
+	canutils-candump canutils-canecho canutils-cansend canutils-cansequence \
 	kmod-rtc-ds1672 kmod-gpio-pca953x kmod-hwmon-gsc kmod-eeprom-at24 \
 	kobs-ng
 endef
