a6622ee6bfec7eb173d5b3bf7cb009b98551238c
Gabor Juhos <juhosg@openwrt.org>
2013-12-17 22:14:10 +0000

    ar71xx: implement callback in mdio reset
    This enables us to add fixups to the board specific code for boards that
    require special treatment of PHYs on mdio bus reset.
    
    Signed-off-by: Felix Kaechele <heffer@fedoraproject.org>
    http://patchwork.openwrt.org/patch/4614/
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39127 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/include/asm/mach-ath79/ag71xx_platform.h
+++ target/linux/ar71xx/files/arch/mips/include/asm/mach-ath79/ag71xx_platform.h
@@ -55,6 +55,8 @@ struct ag71xx_mdio_platform_data {
 	u8		is_ar934x:1;
 	unsigned long	mdio_clock;
 	unsigned long	ref_clock;
+
+	void		(*reset)(struct mii_bus *bus);
 };
 
 #endif /* __ASM_MACH_ATH79_PLATFORM_H */
