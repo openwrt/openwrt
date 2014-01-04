51d5e458a3ae59fc8ff1c06b4a91c31f72cb9f95
Zoltan Herpai <wigyori@uid0.hu>
2014-01-04 10:06:17 +0000

    sunxi: add support for pcDuino board
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    SVN-Revision: 39190

---
+++ target/linux/sunxi/profiles/pcduino.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/pcDuino
+	NAME:=pcDuino
+	PACKAGES:=\
+		uboot-sunxi-pcDuino
+endef
+
+define Profile/pcDuino/Description
+	Package set optimized for the pcDuino
+endef
+
+$(eval $(call Profile,pcDuino))
