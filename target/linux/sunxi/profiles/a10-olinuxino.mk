1ae42098e9aaaf04b096d595f9a5525fa741c4ea
Zoltan Herpai <wigyori@uid0.hu>
2014-01-11 17:19:04 +0000

    sunxi: add support for Olinuxino A10 LIME
    Signed-off-by: Zalan Blenessy <zalan.blenessy@gmail.com>
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    SVN-Revision: 39222

---
+++ target/linux/sunxi/profiles/a10-olinuxino.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/A10-OLinuXino-Lime
+	NAME:=A10 OLinuXino LIME
+	PACKAGES:=\
+		uboot-sunxi-A10-OLinuXino-Lime kmod-ata-sunxi kmod-sun4i-emac
+endef
+
+define Profile/A10-OLinuXino-Lime/Description
+	Package set optimized for the Olimex A10 OLinuXino LIME
+endef
+
+$(eval $(call Profile,A10-OLinuXino-Lime))
