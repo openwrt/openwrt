bcdea80b0bc58376d0347179941aa78a6ff7d8a2
Hauke Mehrtens <hauke@hauke-m.de>
2014-01-12 18:56:36 +0000

    brcm47xx: fix led indication
    Use the new led api also on brcm47xx.
    The led for failsafe indication is searched for.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    SVN-Revision: 39255

--- target/linux/brcm47xx/base-files/etc/diag.sh
+++ target/linux/brcm47xx/base-files/etc/diag.sh
@@ -1,28 +1,38 @@
 #!/bin/sh
 # Copyright (C) 2006 OpenWrt.org
 
-set_led() {
-	local led="$1"
-	local state="$2"
-	[ -f "/proc/diag/led/$1" ] && echo "$state" > "/proc/diag/led/$1"
+. /lib/functions/leds.sh
+
+get_status_led() {
+	status_led_file=$(find /sys/class/leds/ -name "*power*" |head -n1)
+	if [ ! -f $status_led_file ]; then
+		status_led=$(basename $status_led_file)
+		return
+	fi;
+	status_led_file=$(find /sys/class/leds/ -name "*diag*" |head -n1)
+	if [ ! -f $status_led_file ]; then
+		status_led=$(basename $status_led_file)
+		return
+	fi;
+	status_led_file=$(find /sys/class/leds/ -name "*wps*" |head -n1)
+	if [ ! -f $status_led_file ]; then
+		status_led=$(basename $status_led_file)
+		return
+	fi;
 }
 
 set_state() {
+	get_status_led
+
 	case "$1" in
-		preinit)
-			set_led dmz 1
-			set_led diag 1
-			set_led power 0
+	preinit)
+		status_led_blink_preinit
 		;;
-		failsafe)
-			set_led diag f
-			set_led power f
-			set_led dmz f
+	failsafe)
+		status_led_blink_failsafe
 		;;
-		done)
-			set_led dmz 0
-			set_led diag 0
-			set_led power 1
+	done)
+		status_led_on
 		;;
 	esac
 }
