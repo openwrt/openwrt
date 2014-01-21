4d26250db14675a693b1c3620749c68c2db669c2
Zoltan Herpai <wigyori@uid0.hu>
2014-01-21 14:24:45 +0000

    modules: update swconfig.c to compile on 3.13 (compile-tested)
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    SVN-Revision: 39359

--- target/linux/generic/files/drivers/net/phy/swconfig.c
+++ target/linux/generic/files/drivers/net/phy/swconfig.c
@@ -24,6 +24,7 @@
 #include <linux/skbuff.h>
 #include <linux/switch.h>
 #include <linux/of.h>
+#include <linux/version.h>
 
 #define SWCONFIG_DEVNAME	"switch%d"
 
@@ -1109,6 +1110,8 @@ swconfig_init(void)
 	int i, err;
 
 	INIT_LIST_HEAD(&swdevs);
+	
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0))
 	err = genl_register_family(&switch_fam);
 	if (err)
 		return err;
@@ -1120,6 +1123,11 @@ swconfig_init(void)
 	}
 
 	return 0;
+#else
+	err = genl_register_family_with_ops(&switch_fam, swconfig_ops);
+	if (err)
+		goto unregister;
+#endif
 
 unregister:
 	genl_unregister_family(&switch_fam);
