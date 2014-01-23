d66d2ad104751b30d5d09e3feac4e5dff12429b1
Luka Perkov <luka@openwrt.org>
2014-01-23 11:33:41 +0000

    base-files: add ucidef_add_switch_port function
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    SVN-Revision: 39372

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -234,3 +234,13 @@ set network.@switch_vlan[-1].ports='$ports'
 EOF
 }
 
+ucidef_add_switch_port() {
+	local device=$1
+	local port=$2
+	uci batch <<EOF
+add network switch_port
+set network.@switch_port[-1].device='$device'
+set network.@switch_port[-1].port='$port'
+EOF
+}
+
