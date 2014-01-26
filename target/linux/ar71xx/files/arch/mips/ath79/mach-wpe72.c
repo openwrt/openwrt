2c12615b7bfcc360acb737782092dafa89f20026
Gabor Juhos <juhosg@openwrt.org>
2014-01-26 14:55:34 +0000

    ar71xx: wpe72: set active_low flag for the reset button
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39401 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/files/arch/mips/ath79/mach-wpe72.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-wpe72.c
@@ -59,6 +59,7 @@ static struct gpio_keys_button wpe72_gpio_keys[] __initdata = {
 		.code           = KEY_RESTART,
 		.debounce_interval = WPE72_KEYS_DEBOUNCE_INTERVAL,
 		.gpio           = WPE72_GPIO_RESET,
+		.active_low	= 1,
 	}
 };
 
