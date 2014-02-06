2949dd0cfeb17796ec3a6f78d38766313e30e675
Imre Kaloz <kaloz@openwrt.org>
2014-02-06 18:30:50 +0000

    add md5sum for 3.13.1
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39507 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -23,6 +23,9 @@ endif
 ifeq ($(LINUX_VERSION),3.12.5)
   LINUX_KERNEL_MD5SUM:=5b5330f657f562f7bef6da100a7133b1
 endif
+ifeq ($(LINUX_VERSION),3.13.1)
+  LINUX_KERNEL_MD5SUM:=18f528e5632f0096e642c511722630dc
+endif
 
 # disable the md5sum check for unknown kernel versions
 LINUX_KERNEL_MD5SUM?=x
