0da7d381f092e97ba334ce01beeef928fee271da
John Crispin <john@openwrt.org>
2014-02-18 13:34:16 +0000

    odhcp6c: add validation rules to proto handler
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 39624

--- package/network/ipv6/odhcp6c/files/dhcpv6.sh
+++ package/network/ipv6/odhcp6c/files/dhcpv6.sh
@@ -5,19 +5,17 @@
 init_proto "$@"
 
 proto_dhcpv6_init_config() {
-	proto_config_add_string "reqaddress"
-	proto_config_add_string "reqprefix"
-	proto_config_add_string "clientid"
-	proto_config_add_string "reqopts"
-	proto_config_add_string "noslaaconly"
-	proto_config_add_string "forceprefix"
-	proto_config_add_string "norelease"
-	proto_config_add_string "ip6prefix"
-	proto_config_add_string "iface_dslite"
-	proto_config_add_string "ifaceid"
-	proto_config_add_string "sourcerouting"
-	proto_config_add_string "userclass"
-	proto_config_add_string "vendorclass"
+	proto_config_add_string 'reqaddress:or("try","force","none")'
+	proto_config_add_string 'reqprefix:or("auto","no",range(0, 64))'
+	proto_config_add_string clientid
+	proto_config_add_string 'reqopts:list(uinteger)'
+	proto_config_add_string 'noslaaconly:bool'
+	proto_config_add_string 'forceprefix:bool'
+	proto_config_add_string 'norelease:bool'
+	proto_config_add_string 'ip6prefix:ip6addr'
+	proto_config_add_string iface_dslite
+	proto_config_add_string 'ifaceid:ip6addr'
+	proto_config_add_string 'sourcerouting:bool'
 }
 
 proto_dhcpv6_setup() {
