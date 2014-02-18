47e0e9ed2f0e0b1d14ef64b8d75e3b19d3a14735
John Crispin <blogic@openwrt.org>
2014-02-18 13:33:52 +0000

    comgt: add validation rules to proto handler
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39618 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/network/utils/comgt/files/3g.sh
+++ package/network/utils/comgt/files/3g.sh
@@ -9,7 +9,7 @@ proto_3g_init_config() {
 	no_device=1
 	available=1
 	ppp_generic_init_config
-	proto_config_add_string "device"
+	proto_config_add_string "device:device"
 	proto_config_add_string "apn"
 	proto_config_add_string "service"
 	proto_config_add_string "pincode"
