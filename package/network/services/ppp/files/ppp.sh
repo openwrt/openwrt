4aff5a4e3df635e83decd87fd44c88a4a5bc30a2
John Crispin <blogic@openwrt.org>
2014-02-18 13:33:56 +0000

    ppp: add validation rules to proto handler
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@39619 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/network/services/ppp/files/ppp.sh
+++ package/network/services/ppp/files/ppp.sh
@@ -9,16 +9,16 @@
 }
 
 ppp_generic_init_config() {
-	proto_config_add_string "username"
-	proto_config_add_string "password"
-	proto_config_add_string "keepalive"
-	proto_config_add_int "demand"
-	proto_config_add_string "pppd_options"
-	proto_config_add_string "connect"
-	proto_config_add_string "disconnect"
-	proto_config_add_boolean "ipv6"
-	proto_config_add_boolean "authfail"
-	proto_config_add_int "mtu"
+	proto_config_add_string username
+	proto_config_add_string password
+	proto_config_add_string keepalive
+	proto_config_add_int demand
+	proto_config_add_string pppd_options
+	proto_config_add_string 'connect:file'
+	proto_config_add_string 'disconnect:file'
+	proto_config_add_boolean ipv6
+	proto_config_add_boolean authfail
+	proto_config_add_int mtu
 }
 
 ppp_generic_setup() {
