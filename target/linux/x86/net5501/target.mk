3488372fa7942b1d517902e710d1d09e11037b71
Gabor Juhos <juhosg@openwrt.org>
2014-02-23 18:09:32 +0000

    x86/net5501: switch to 3.10 and add broken flag
    Compile tested only.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    
    SVN-Revision: 39728

--- target/linux/x86/net5501/target.mk
+++ target/linux/x86/net5501/target.mk
@@ -23,6 +23,8 @@ DEFAULT_PACKAGES += kmod-crypto-hw-geode kmod-crypto-ocf \
 CS5535_MASK:=0x01000040
 
 CPU_TYPE := geode
+LINUX_VERSION:=3.10.32
+FEATURES += broken
 
 define Target/Description
 	Build firmware images for Soekris net5501 board
