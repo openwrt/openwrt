522d86d5bbae0b5f1d0fd9ea48607523a3aa6fd9
John Crispin <blogic@openwrt.org>
2014-04-22 08:08:51 +0000

    ramips: Add support for the 7Links PX-4885 wireless router.
    The 7Links PX-4885 is a small battery-powered wireless router.
    It is based on a RT5350F WiSoC and features one ethernet port and one
    USB port. It is a Hame MPR-A1 clone, except it has different GPIOs,
    a different battery and more RAM (32 MB).
    This patch adds a profile for this board, the corresponding device tree
    file and the adequate base-files changes (incl. sysupgrade).
    
    Signed-off-by: Hugo Grostabussiat <bonstra@bonstra.fr.eu.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40554 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/rt305x/profiles/sevenlinks.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/PX4885
+	NAME:=7Links PX-4885
+	PACKAGES:=\
+		kmod-usb-core kmod-usb-rt305x-dwc_otg kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev kmod-leds-gpio
+endef
+
+define Profile/PX4885/Description
+	Package set for 7Links PX-4885 wireless router.
+endef
+
+$(eval $(call Profile,PX4885))
+
