2b5864b2a53a1a1b234665498fa14167fff82a9a
Zoltan HERPAI <wigyori@uid0.hu>
2014-04-30 19:50:15 +0000

    sunxi: add board support for LinkSprite pcDuino v3
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40606 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/sunxi/profiles/pcduino3.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/pcDuino3
+	NAME:=pcDuino3
+	PACKAGES:=\
+		uboot-sunxi-pcDuino3 kmod-sun4i-emac kmod-rtc-sunxi
+endef
+
+define Profile/pcDuino3/Description
+	Package set optimized for the pcDuino3
+endef
+
+$(eval $(call Profile,pcDuino3))
