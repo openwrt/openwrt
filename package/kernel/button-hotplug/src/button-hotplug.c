e5c9380c4d76b5dfa3ab8c519a7b3a283e9ed15e
Felix Fietkau <nbd@openwrt.org>
2014-05-23 22:13:13 +0000

    button-hotplug: remove #ifdef CONFIG_HOTPLUG, it is gone in newer kernels
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40839 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/button-hotplug/src/button-hotplug.c
+++ package/kernel/button-hotplug/src/button-hotplug.c
@@ -208,7 +208,6 @@ static int button_hotplug_create_event(const char *name, unsigned long seen,
 
 /* -------------------------------------------------------------------------*/
 
-#ifdef	CONFIG_HOTPLUG
 static int button_get_index(unsigned int code)
 {
 	int i;
@@ -239,12 +238,6 @@ static void button_hotplug_event(struct input_handle *handle,
 			(seen - priv->seen[btn]) / HZ, value);
 	priv->seen[btn] = seen;
 }
-#else
-static void button_hotplug_event(struct input_handle *handle,
-			   unsigned int type, unsigned int code, int value)
-{
-}
-#endif	/* CONFIG_HOTPLUG */
 
 static int button_hotplug_connect(struct input_handler *handler,
 		struct input_dev *dev, const struct input_device_id *id)
