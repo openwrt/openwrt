d5c281f5b432137b8ae039d3eeab589bbc145691
Luka Perkov <luka@openwrt.org>
2014-05-28 21:43:39 +0000

    kirkwood: add support for Linksys EA4500
    Support is added only for kernel 3.14 - do not show it until
    the switch is made.
    
    Following uboot env changes are needed to make it work on stock uboot:
    
    ---
    setenv owrt_bootargs_root ubi.mtd=3 root=ubi0:rootfs rootfstype=ubifs rw
    setenv owrt_mtdparts mtdparts=orion_nand:0x80000@0x0(uboot),0x20000@0x80000(uboot_env),0x200000@0x200000(kernel),-@0x400000(root)
    setenv owrt_boot nand read.e \${loadaddr} 0x200000 0x200000 \; setenv bootargs \$(console) \$(owrt_mtdparts) \${owrt_bootargs_root} \; bootm \${loadaddr}
    setenv bootcmd run owrt_boot
    saveenv
    ---
    
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40871 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/kirkwood/profiles/115-router.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/EA4500
+  NAME:=Linksys EA4500
+  PACKAGES:= \
+	kmod-mwl8k kmod-usb2 kmod-usb-storage \
+	uboot-envtools
+endef
+
+define Profile/EA4500/Description
+ Package set compatible with Linksys EA4500 board.
+endef
+
+EA4500_UBIFS_OPTS:="-m 2048 -e 126KiB -c 4096"
+EA4500_UBI_OPTS:="-m 2048 -p 128KiB -s 512"
+
+# $(eval $(call Profile,EA4500))
