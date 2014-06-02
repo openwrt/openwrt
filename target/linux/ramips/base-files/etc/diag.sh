efd09832febde11e9d4bc6fca6e471f94933867f
John Crispin <john@openwrt.org>
2014-06-02 12:42:48 +0000

    ramips: Support for Teltonika RUT5XX
    Hi Hartmut,
    
    Thanks for your feedback, I have reformated the patch accordingly.
    
    Added support for Teltonika RUT5XX hardware.
    
    Signed-off-by: Steffen Weinreich <steve@weinreich.org>
    
    SVN-Revision: 40907

--- target/linux/ramips/base-files/etc/diag.sh
+++ target/linux/ramips/base-files/etc/diag.sh
@@ -104,6 +104,9 @@ get_status_led() {
 	rt-n56u | wl-330n | wl-330n3g)
 		status_led="asus:blue:power"
 		;;
+	rut5xx)
+		status_led="rut5xx:green:status"
+		;;
 	sl-r7205)
 		status_led="sl-r7205:green:status"
 		;;
