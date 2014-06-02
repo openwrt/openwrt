5fd7e00d9d8e624e460342e40a450566f2b70907
John Crispin <john@openwrt.org>
2014-06-02 12:44:07 +0000

    image: ext4: allow creation of a journaling filesystem
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>
    
    SVN-Revision: 40925

--- include/image.mk
+++ include/image.mk
@@ -190,7 +190,7 @@ ifneq ($(CONFIG_TARGET_ROOTFS_EXT4FS),)
 # generate an ext2 fs
 	$(STAGING_DIR_HOST)/bin/genext2fs -U -B $(CONFIG_TARGET_EXT4_BLOCKSIZE) -b $(E2SIZE) -N $(CONFIG_TARGET_ROOTFS_MAXINODE) -d $(TARGET_DIR)/ $(KDIR)/root.ext4 -m $(CONFIG_TARGET_ROOTFS_RESERVED_PCT) $(MKFS_DEVTABLE_OPT)
 # convert it to ext4
-	$(STAGING_DIR_HOST)/bin/tune2fs -O extents,uninit_bg,dir_index $(KDIR)/root.ext4
+	$(STAGING_DIR_HOST)/bin/tune2fs $(if $(CONFIG_TARGET_EXT4_JOURNAL),-j) -O extents,uninit_bg,dir_index $(KDIR)/root.ext4
 # fix it up
 	$(STAGING_DIR_HOST)/bin/e2fsck -fy $(KDIR)/root.ext4
 	$(call Image/Build,ext4)
