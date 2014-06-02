ef5f37a08dfc405b2e9281f8482d32645d8a4760
John Crispin <john@openwrt.org>
2014-06-02 13:20:52 +0000

    ar71xx: add kernel support for the OpenMesh OM2Pv2 board
    Signed-off-by: Marek Lindner <marek@open-mesh.com>
    [sven@open-mesh.com: Rebased]
    Signed-off-by: Sven Eckelmann <sven@open-mesh.com>
    
    SVN-Revision: 40955

--- target/linux/ar71xx/files/arch/mips/ath79/mach-om2p.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-om2p.c
@@ -174,6 +174,7 @@ static void __init om2p_lc_setup(void)
 }
 
 MIPS_MACHINE(ATH79_MACH_OM2P_LC, "OM2P-LC", "OpenMesh OM2P LC", om2p_lc_setup);
+MIPS_MACHINE(ATH79_MACH_OM2Pv2, "OM2Pv2", "OpenMesh OM2Pv2", om2p_lc_setup);
 
 static void __init om2p_hs_setup(void)
 {
