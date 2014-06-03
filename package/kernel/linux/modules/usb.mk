3c1eb056fbc2cb0c25d1fdcacbc2c93fdfacd669
John Crispin <blogic@openwrt.org>
2014-06-03 05:49:34 +0000

    kernel: fix kernel build for chipidea w/o usb-gadget
    This resolves a module dependency kernel regression in r40905 with the
    usb-chipidea module if usb-gadget was not selected.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40992 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1373,10 +1373,10 @@ ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),lt,3.11.0)),1)
 else
   FILES:=\
 	$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc.ko \
-	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/gadget/udc-core.ko) \
+	$(if $(CONFIG_USB_GADGET),$(LINUX_DIR)/drivers/usb/gadget/udc-core.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc_imx.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/usbmisc_imx.ko)
-  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imxudc-core),1)
+  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imx),1)
 endif
   $(call AddDepends/usb)
 endef
