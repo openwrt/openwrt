b5d42bc1118d50e527eeb5b8e43763470a24e4db
Hauke Mehrtens <hauke@hauke-m.de>
2014-06-05 21:23:58 +0000

    brcm47xx: move current target to "generic" subtarget
    This allows creating more subtargets and optimize builds per family.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    SVN-Revision: 41024

---
+++ target/linux/brcm47xx/generic/target.mk
@@ -0,0 +1,6 @@
+BOARDNAME:=Generic
+
+define Target/Description
+	Build generic firmware for all Broadcom BCM47xx and BCM53xx MIPS
+	devices. It runs on both architectures BMIPS3300 and MIPS 74K.
+endef
