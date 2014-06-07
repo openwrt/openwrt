ed7c73fbefe240a6e3bcee4ebcbbe333f718b762
Hauke Mehrtens <hauke@hauke-m.de>
2014-06-07 17:36:02 +0000

    brcm47xx: activate some compiler optimizations for 74K CPUs
    With this patch the mips74k subtarget will be compiled with optimized
    compiler options to generated smaller and faster code. This currently
    breaks broadcom-wl, because the binary blob is only compiled with
    mipsr1 support.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    SVN-Revision: 41050

--- target/linux/brcm47xx/generic/target.mk
+++ target/linux/brcm47xx/generic/target.mk
@@ -1,4 +1,5 @@
 BOARDNAME:=Generic
+FEATURES+=pcmcia
 
 define Target/Description
 	Build generic firmware for all Broadcom BCM47xx and BCM53xx MIPS
--- target/linux/brcm47xx/legacy/target.mk
+++ target/linux/brcm47xx/legacy/target.mk
@@ -1,5 +1,6 @@
 FEATURES += low_mem
 BOARDNAME:=Legacy (802.11g)
+FEATURES+=pcmcia
 
 define Target/Description
 	Build generic firmware for legacy 802.11g Broadcom BCM47xx
