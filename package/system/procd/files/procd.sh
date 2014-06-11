5a1be6890b6bf1185928e86134fca266855a2020
Felix Fietkau <nbd@openwrt.org>
2014-06-11 17:05:46 +0000

    procd: allow passing multiple config files to procd_add_reload_trigger
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@41146 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -198,9 +198,12 @@ _procd_add_config_trigger() {
 _procd_add_reload_trigger() {
 	local script=$(readlink "$initscript")
 	local name=$(basename ${script:-$initscript})
+	local file
 
 	_procd_open_trigger
-	_procd_add_config_trigger "config.change" $1 /etc/init.d/$name reload
+	for file in "$@"; do
+		_procd_add_config_trigger "config.change" "$file" /etc/init.d/$name reload
+	done
 	_procd_close_trigger
 }
 
