a6e15f806007d56125f6309a874137d0389afa00
Luka Perkov <luka@openwrt.org>
2014-06-11 23:39:54 +0000

    kernel: update 3.14 to 3.14.7
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@41150 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -23,8 +23,8 @@ endif
 ifeq ($(LINUX_VERSION),3.13.7)
   LINUX_KERNEL_MD5SUM:=370adced5e5c1cb1d0d621c2dae2723f
 endif
-ifeq ($(LINUX_VERSION),3.14.5)
-  LINUX_KERNEL_MD5SUM:=db2f2b8a83239852b72e560811eb80de
+ifeq ($(LINUX_VERSION),3.14.7)
+  LINUX_KERNEL_MD5SUM:=046c448e477d534780ff7dad369b499f
 endif
 
 # disable the md5sum check for unknown kernel versions
