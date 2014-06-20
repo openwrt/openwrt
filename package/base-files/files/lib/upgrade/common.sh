09273f5c0acdfefdcab4eb44fdfbfea11ce2921a
John Crispin <john@openwrt.org>
2014-06-20 13:41:08 +0000

    base-files: regenerate the /lib64 symlink when switching to a ramdisk
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 41284

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -76,6 +76,8 @@ run_ramfs() { # <command> [...]
 	done
 	install_file /etc/resolv.conf /lib/functions.sh /lib/functions/*.sh /lib/upgrade/*.sh $RAMFS_COPY_DATA
 
+	[ -L "/lib64" ] && ln -s /lib $RAM_ROOT/lib64
+
 	supivot $RAM_ROOT /mnt || {
 		echo "Failed to switch over to ramfs. Please reboot."
 		exit 1
