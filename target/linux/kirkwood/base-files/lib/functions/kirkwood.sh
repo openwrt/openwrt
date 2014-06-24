e6aea925d334827e463ecdf875773fd3355b1563
Luka Perkov <luka@openwrt.org>
2014-06-24 18:18:51 +0000

    kirkwood: base-files: add script to detect board name and populate /tmp/sysinfo
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    SVN-Revision: 41335

---
+++ target/linux/kirkwood/base-files/lib/functions/kirkwood.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+
+KIRKWOOD_BOARD_NAME=
+KIRKWOOD_MODEL=
+
+kirkwood_board_detect() {
+	local machine
+	local name
+
+	machine=$(cat /proc/device-tree/model)
+
+	case "$machine" in
+	"Seagate FreeAgent Dockstar")
+		name="dockstar"
+		;;
+
+	"Iomega Iconnect")
+		name="iconnect"
+		;;
+
+	"RaidSonic ICY BOX IB-NAS62x0 (Rev B)")
+		name="ib62x0"
+		;;
+
+	"Cloud Engines Pogoplug E02")
+		name="pogo_e02"
+		;;
+
+	"Linksys EA4500")
+		name="ea4500"
+		;;
+
+	*)
+		name="generic"
+		;;
+	esac
+
+	[ -z "$KIRKWOOD_BOARD_NAME" ] && KIRKWOOD_BOARD_NAME="$name"
+	[ -z "$KIRKWOOD_MODEL" ] && KIRKWOOD_MODEL="$machine"
+
+	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
+
+	echo "$KIRKWOOD_BOARD_NAME" > /tmp/sysinfo/board_name
+	echo "$KIRKWOOD_MODEL" > /tmp/sysinfo/model
+}
+
+kirkwood_board_name() {
+	local name
+
+	[ -f /tmp/sysinfo/board_name ] || kirkwood_board_detect
+	[ -f /tmp/sysinfo/board_name ] && name=$(cat /tmp/sysinfo/board_name)
+	[ -z "$name" ] && name="unknown"
+
+	echo "$name"
+}
