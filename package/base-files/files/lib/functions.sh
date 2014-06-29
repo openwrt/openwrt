ba967cd842e5d7425d6cc4e7e1584b7f000a5d86
Luka Perkov <luka@openwrt.org>
2014-06-29 23:05:05 +0000

    base-files: define yes/no as valid boolean options
    ubox validate_data defines yes/no as valid boolean options, do the same in
    config_get_bool too.
    
    Signed-off-by: Luka Perkov <luka@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@41405 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -102,8 +102,8 @@ config_get_bool() {
 	local _tmp
 	config_get _tmp "$2" "$3" "$4"
 	case "$_tmp" in
-		1|on|true|enabled) _tmp=1;;
-		0|off|false|disabled) _tmp=0;;
+		1|on|true|yes|enabled) _tmp=1;;
+		0|off|false|no|disabled) _tmp=0;;
 		*) _tmp="$4";;
 	esac
 	export ${NO_EXPORT:+-n} "$1=$_tmp"
