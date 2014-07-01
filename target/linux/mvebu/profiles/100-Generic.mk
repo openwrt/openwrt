6fa01eab809a4d88fbc9b0262e6f7329576c33b1
Imre Kaloz <kaloz@openwrt.org>
2014-07-01 09:43:38 +0000

    adjust default profile, and add an evalboard one
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    
    SVN-Revision: 41435

--- target/linux/mvebu/profiles/100-Generic.mk
+++ target/linux/mvebu/profiles/100-Generic.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 OpenWrt.org
+# Copyright (C) 2013-2014 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,14 +8,17 @@
 define Profile/Generic
   NAME:=Generic (default)
   PACKAGES:= \
-	kmod-mmc kmod-mvsdio kmod-usb2 kmod-usb-storage \
+	kmod-usb2 kmod-usb3 kmod-usb-storage \
 	kmod-of-i2c kmod-i2c-core kmod-i2c-mv64xxx \
 	kmod-ata-core kmod-ata-marvell-sata \
-	kmod-rtc-marvell kmod-thermal-armada
+	kmod-rtc-marvell kmod-thermal-armada \
+	kmod-gpio-button-hotplug kmod-hwmon-tmp421 \
+	kmod-hwmon-gpiofan kmod-leds-tlc59116 \
+	kmod-ledtrig-usbdev
 endef
 
 define Profile/Generic/Description
- Package set compatible with most Marvell Armada 370/XP based boards.
+ Package set compatible with most supported Marvell Armada 370/XP based boards.
 endef
 
 $(eval $(call Profile,Generic))
