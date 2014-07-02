e2915c6981a4ba141d8ce4dc3f39e187890fb9f6
John Crispin <john@openwrt.org>
2014-07-02 16:33:11 +0000

    lantiq: add support for /tmp/sysinfo
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 41472

--- target/linux/lantiq/base-files/lib/functions/lantiq.sh
+++ target/linux/lantiq/base-files/lib/functions/lantiq.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-lantiq_board_id() {
-	grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/\(.*\) - .*/\1/g"             
+lantiq_board_detect() {
+	name=`grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/\(.*\) - .*/\1/g"`
+	model=`grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/.* - \(.*\)/\1/g"`
+	[ -z "$name" ] && name="unknown"
+	[ -z "$model" ] && model="unknown"
+	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
+	echo $name > /tmp/sysinfo/board_name
+	echo $model > /tmp/sysinfo/model
+}
+
+lantiq_board_model() {
+	local model
+
+	[ -f /tmp/sysinfo/model ] && model=$(cat /tmp/sysinfo/model)
+	[ -z "$model" ] && model="unknown"
+
+	echo "$model"
 }
 
 lantiq_board_name() {
-	grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/.* - \(.*\)/\1/g"
+	local name
+
+	[ -f /tmp/sysinfo/board_name ] && name=$(cat /tmp/sysinfo/board_name)
+	[ -z "$name" ] && name="unknown"
+
+	echo "$name"
 }
