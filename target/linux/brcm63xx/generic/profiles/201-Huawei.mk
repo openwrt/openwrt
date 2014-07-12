e5cadabb58808a0a65ca4a7530befc176b085646
Jonas Gorski <jogo@openwrt.org>
2014-07-12 12:31:39 +0000

    brcm63xx: Fix HW553 support
    - adsl and lan leds are swapped.
    - internetkey is labeled as hspa.
    - enable status led and reset button for failsafe.
    - use new leds script.
    - fix switch configuration.
    - remove enet0 from board_info.
    - add profile.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    SVN-Revision: 41588

--- target/linux/brcm63xx/generic/profiles/201-Huawei.mk
+++ target/linux/brcm63xx/generic/profiles/201-Huawei.mk
@@ -1,9 +1,19 @@
 #
-# Copyright (C) 2012 OpenWrt.org
+# Copyright (C) 2012-2014 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+
+define Profile/HW553
+  NAME:=Huawei HG553
+  PACKAGES:=kmod-b43 wpad-mini \
+	kmod-usb2 kmod-usb-ohci kmod-ledtrig-usbdev
+endef
+define Profile/HW553/Description
+	Package set optimized for Huawei HG553 using open-source b43 WiFi driver
+endef
+$(eval $(call Profile,HW553))
 
 define Profile/HW556
   NAME:=HW556 WiFi
@@ -14,4 +24,3 @@ define Profile/HW556/Description
 	Package set compatible with hardware using Atheros or Ralink WiFi cards (Huawei HG556a)
 endef
 $(eval $(call Profile,HW556))
-
