4cf06a885e8fa62a08f50d5a032a102652fb5cf4
Felix Fietkau <nbd@openwrt.org>
2014-07-29 10:25:43 +0000

    BB: remove all kernel versions except for 3.10
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/barrier_breaker@41877 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,26 +2,8 @@
 
 LINUX_RELEASE?=1
 
-ifeq ($(LINUX_VERSION),3.3.8)
-  LINUX_KERNEL_MD5SUM:=f1058f64eed085deb44f10cee8541d50
-endif
-ifeq ($(LINUX_VERSION),3.6.11)
-  LINUX_KERNEL_MD5SUM:=3d602ad7f7984509c3e923a5ae90bc54
-endif
-ifeq ($(LINUX_VERSION),3.8.13)
-  LINUX_KERNEL_MD5SUM:=2af19d06cd47ec459519159cdd10542d
-endif
-ifeq ($(LINUX_VERSION),3.9.11)
-  LINUX_KERNEL_MD5SUM:=edbf88eb7f7d34dbd5d3887726790755
-endif
 ifeq ($(LINUX_VERSION),3.10.49)
   LINUX_KERNEL_MD5SUM:=9774e12764e740d49c80eda77d0ef3eb
-endif
-ifeq ($(LINUX_VERSION),3.13.7)
-  LINUX_KERNEL_MD5SUM:=370adced5e5c1cb1d0d621c2dae2723f
-endif
-ifeq ($(LINUX_VERSION),3.14.12)
-  LINUX_KERNEL_MD5SUM:=7e76da2910683fe4dfbcec6ebfdd9a32
 endif
 
 # disable the md5sum check for unknown kernel versions
