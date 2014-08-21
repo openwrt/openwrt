89f205b1984b709631553731d9129e72a2712624
Jonas Gorski <jogo@openwrt.org>
2014-08-21 11:28:10 +0000

    brcm63xx: Add DT support for CT-5365
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    [jogo: make linux partition writable]
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@42237 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/profiles/comtrend.mk
+++ target/linux/brcm63xx/profiles/comtrend.mk
@@ -13,3 +13,12 @@ define Profile/CT536_CT5621/Description
   Package set optimized for CT-536+/CT-5621.
 endef
 $(eval $(call Profile,CT536_CT5621))
+
+define Profile/CT5365
+  NAME:=Comtrend CT-5365
+  PACKAGES:=kmod-b43 wpad-mini
+endef
+define Profile/CT5365/Description
+  Package set optimized for CT-5365.
+endef
+$(eval $(call Profile,CT5365))
