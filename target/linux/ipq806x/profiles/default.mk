3c1f6e358d4f1da4cf79083996544ce909f21b5f
John Crispin <john@openwrt.org>
2014-08-30 09:32:58 +0000

    ipq806x: Add support for IPQ806x chip family
    Patches are generated using the "format-patch" command from the
    following location:
    *https://www.codeaurora.org/cgit/quic/kernel/galak-msm/log/?h=apq_ipq_base
    *rev=0771849495b4128cac2faf7d49c85c729fc48b20
    Patches numbered 76/77/102/103 have already been integrated in 3.14.12,
    so they're not in this list.
    
    All these patches are either integrated are pending integration into
    kernel.org, therefore these patches should go away once the kernel
    gets upgraded to 3.16.
    
    Support is currently limited to AP148 board but can be extended to other
    platforms in the future.
    
    These changes do not cover ethernet connectivity.
    
    Signed-off-by: Mathieu Olivari <mathieu@codeaurora.org>
    
    SVN-Revision: 42334

---
+++ target/linux/ipq806x/profiles/default.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2014 The Linux Foundation. All rights reserved.
+# Copyright (C) 2009 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Default
+	NAME:=Default Profile (minimum package set)
+	PACKAGES:= \
+		kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
+endef
+
+define Profile/Default/Description
+	Default package set compatible with most boards.
+endef
+$(eval $(call Profile,Default))
