fb523872e123a6e70a1c3cd6023e70aa61ecc4c6
John Crispin <blogic@openwrt.org>
2014-09-01 13:22:04 +0000

    ar71xx: add Support for RB2011UiAS-2HnD
    Backport of r42317
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/barrier_breaker@42387 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -496,6 +496,9 @@ ar71xx_board_detect() {
 	*"RouterBOARD 2011UAS-2HnD")
 		name="rb-2011uas-2hnd"
 		;;
+	*"RouterBOARD 2011UiAS-2HnD")
+		name="rb-2011uias-2hnd"
+		;;
 	*"RouterBOARD SXT Lite2")
 		name="rb-sxt2n"
 		;;
