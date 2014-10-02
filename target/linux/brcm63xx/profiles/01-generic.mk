f51292a0a7a2fe77bcffb7a1c79dd25153127b1b
Jonas Gorski <jogo@openwrt.org>
2014-10-02 12:24:34 +0000

    brcm63xx: Add DT support for 96345GW2
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@42728 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm63xx/profiles/01-generic.mk
+++ target/linux/brcm63xx/profiles/01-generic.mk
@@ -22,3 +22,12 @@ define Profile/96328avng/Description
   Package set optimized for 96328avng.
 endef
 $(eval $(call Profile,96328avng))
+
+define Profile/96345GW2
+  NAME:=Generic 96345GW2
+  PACKAGES:=
+endef
+define Profile/96345GW2/Description
+  Package set optimized for 96345GW2.
+endef
+$(eval $(call Profile,96345GW2))
