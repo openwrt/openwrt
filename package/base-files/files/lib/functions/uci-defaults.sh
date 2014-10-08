c118e5edf4301dc303024603dd34d369d9926d6d
Steven Barth <cyrus@openwrt.org>
2014-10-08 13:08:05 +0000

    network: Unbind DHCPv6 from underlying interfaces
    This allows IPv6 to set up without IPv4 being up thus
    IPv6-only or IPv6+DS-Lite working with the default config.
    
    Signed-off-by: Steven Barth <steven@midlink.org>
    
    SVN-Revision: 42848

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -190,7 +190,7 @@ set network.wan='interface'
 set network.wan.ifname='$ifname'
 set network.wan.proto='dhcp'
 set network.wan6='interface'
-set network.wan6.ifname='@wan'
+set network.wan6.ifname='$ifname'
 set network.wan6.proto='dhcpv6'
 EOF
 }
