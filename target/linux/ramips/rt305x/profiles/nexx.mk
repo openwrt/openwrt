af1207b58c61eed1eaa86f5e670347b84fd75a09
John Crispin <john@openwrt.org>
2014-10-10 08:56:43 +0000

    ramips: add support for Nexx WT1520
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>
    
    SVN-Revision: 42867

---
+++ target/linux/ramips/rt305x/profiles/nexx.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/WT1520
+	NAME:=Nexx WT1520
+	PACKAGES:=kmod-usb2
+endef
+
+define Profile/WT1520/Description
+	Package set for WT1520
+endef
+
+$(eval $(call Profile,WT1520))
