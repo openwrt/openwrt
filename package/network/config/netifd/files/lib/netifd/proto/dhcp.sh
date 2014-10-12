9106cc0af9ff078bfc8b4b4f277252cdbcc8a849
Steven Barth <cyrus@openwrt.org>
2014-10-12 12:27:21 +0000

    netifd: Make mtu configurable of dynamic 6rd tunnel interface
    Patch allows to configure the mtu of the dynamic 6rd tunnel interface when created by dhcp script.
    In some setups it's desirable to have config control over the 6rd tunnel mtu to maximize the traffic throughput
    
    Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>
    
    SVN-Revision: 42871

--- package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+++ package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
@@ -18,14 +18,15 @@ proto_dhcp_init_config() {
 	proto_config_add_boolean delegate
 	proto_config_add_string zone6rd
 	proto_config_add_string zone
+	proto_config_add_string mtu6rd
 }
 
 proto_dhcp_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local ipaddr hostname clientid vendorid broadcast reqopts iface6rd sendopts delegate zone6rd zone
-	json_get_vars ipaddr hostname clientid vendorid broadcast reqopts iface6rd sendopts delegate zone6rd zone
+	local ipaddr hostname clientid vendorid broadcast reqopts iface6rd sendopts delegate zone6rd zone mtu6rd
+	json_get_vars ipaddr hostname clientid vendorid broadcast reqopts iface6rd sendopts delegate zone6rd zone mtu6rd
 
 	local opt dhcpopts
 	for opt in $reqopts; do
@@ -42,6 +43,7 @@ proto_dhcp_setup() {
 	[ -n "$iface6rd" ] && append dhcpopts "-O 212"
 	[ -n "$zone6rd" ] && proto_export "ZONE6RD=$zone6rd"
 	[ -n "$zone" ] && proto_export "ZONE=$zone"
+	[ -n "$mtu6rd" ] && proto_export "MTU6RD=$mtu6rd"
 	[ "$delegate" = "0" ] && proto_export "IFACE6RD_DELEGATE=0"
 
 	proto_export "INTERFACE=$config"
