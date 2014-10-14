28d57f707d09d0bd085f82d2bb4188ea3ee7a13c
John Crispin <john@openwrt.org>
2014-10-14 10:30:56 +0000

    mpc85xx: use wpad-mini in WDR4900 profile instead of wpad
    This change makes the profile of WDR4900 more consistent with other router
    specific profiles.
    
    Signed-off-by: Philipp Borgers <borgers@mi.fu-berlin.de>
    
    SVN-Revision: 42896

--- target/linux/mpc85xx/profiles/tp-link.mk
+++ target/linux/mpc85xx/profiles/tp-link.mk
@@ -9,7 +9,7 @@ define Profile/TLWDR4900
 	NAME:=TP-Link TL-WDR4900
 	PACKAGES:=\
 		kmod-usb-core kmod-usb2 kmod-usb2-fsl \
-		kmod-ath9k wpad
+		kmod-ath9k wpad-mini
 endef
 
 define Profile/TLWDR4900/Description
