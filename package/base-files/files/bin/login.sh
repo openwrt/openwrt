6ae004207b1581f79b980e36c7f556aec964dc16
Felix Fietkau <nbd@openwrt.org>
2014-10-20 15:07:19 +0000

    base-files: fix telnet login on musl
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43006 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/bin/login.sh
+++ package/base-files/files/bin/login.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Copyright (C) 2006-2011 OpenWrt.org
 
-if ( ! grep -qs '^root:[!x]\?:' /etc/shadow || \
-     ! grep -qs '^root:[!x]\?:' /etc/passwd ) && \
+if ( ! grep -qsE '^root:[!x]?:' /etc/shadow || \
+     ! grep -qsE '^root:[!x]?:' /etc/passwd ) && \
    [ -z "$FAILSAFE" ]
 then
 	echo "Login failed."
