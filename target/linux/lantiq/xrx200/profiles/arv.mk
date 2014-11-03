258b5fe8e43f0959a4c525c0c8cac951a721339b
John Crispin <blogic@openwrt.org>
2014-11-03 08:33:07 +0000

    BB: lantiq: vr9: vgv7519: load vmmc and tapi module into firmware image
    (required not-distributable firmware blob - dump it by yourself from original firmware)
    
    Signed-off-by: Eddi De Pieri <eddi@depieri.net>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/barrier_breaker@43166 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/xrx200/profiles/arv.mk
+++ target/linux/lantiq/xrx200/profiles/arv.mk
@@ -20,7 +20,8 @@ define Profile/VGV7519NOR
 	kmod-ltq-deu-vr9 kmod-ltq-hcd-vr9 \
 	kmod-ltq-vdsl-vr9-mei kmod-ltq-vdsl-vr9 \
 	kmod-ltq-atm-vr9 ltq-vdsl-vr9-fw-installer \
-	ltq-vdsl-app ppp-mod-pppoa
+	ltq-vdsl-app ppp-mod-pppoa \
+	kmod-ltq-tapi kmod-ltq-vmmc
 endef
 
 $(eval $(call Profile,VGV7519NOR))
@@ -31,7 +32,8 @@ define Profile/VGV7519BRN
 	kmod-ltq-deu-vr9 kmod-ltq-hcd-vr9 \
 	kmod-ltq-vdsl-vr9-mei kmod-ltq-vdsl-vr9 \
 	kmod-ltq-atm-vr9 ltq-vdsl-vr9-fw-installer \
-	ltq-vdsl-app ppp-mod-pppoa
+	ltq-vdsl-app ppp-mod-pppoa \
+	kmod-ltq-tapi kmod-ltq-vmmc
 endef
 
 $(eval $(call Profile,VGV7519BRN))
