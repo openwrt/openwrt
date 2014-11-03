0ceece4c82490197d5cefe11fa640c3a51159322
Jo-Philipp Wich <jow@openwrt.org>
2014-11-03 18:12:42 +0000

    px5g: generate unique serial numbers
    Generate a random serial from /dev/urandom when creating selfsigned certs.
    Fixes "sec_error_reused_issuer_and_serial" with Firefox.
    
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 43168

--- package/utils/px5g/px5g.c
+++ package/utils/px5g/px5g.c
@@ -143,7 +143,7 @@ int selfsigned(char **arg)
 	char *keypath = NULL, *certpath = NULL;
 	bool pem = true;
 	time_t from = time(NULL), to;
-	char fstr[20], tstr[20];
+	char fstr[20], tstr[20], sstr[17];
 	int len;
 
 	while (*arg && **arg == '-') {
@@ -222,8 +222,12 @@ int selfsigned(char **arg)
 	x509write_crt_set_subject_key_identifier(&cert);
 	x509write_crt_set_authority_key_identifier(&cert);
 
+	_urandom(NULL, buf, 8);
+	for (len = 0; len < 8; len++)
+		sprintf(sstr + len*2, "%02x", (unsigned char) buf[len]);
+
 	mpi_init(&serial);
-	mpi_read_string(&serial, 10, "1");
+	mpi_read_string(&serial, 16, sstr);
 	x509write_crt_set_serial(&cert, &serial);
 
 	if (pem) {
