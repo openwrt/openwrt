4fbb94f514ce1b0c3e23ef57fa3c9fcebe5739ed
John Crispin <blogic@openwrt.org>
2014-11-14 16:54:23 +0000

    scripts/om-fwupgradecfg-gen.sh: add support for the MR900
    Signed-off-by: Sven Eckelmann <sven@open-mesh.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43260 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/om-fwupgradecfg-gen.sh
+++ scripts/om-fwupgradecfg-gen.sh
@@ -7,7 +7,7 @@
 #
 
 usage() {
-	echo "Usage: $0 <OM2P|OM5P|MR600> <out file path> <kernel path> <rootfs path>"
+	echo "Usage: $0 <OM2P|OM5P|MR600|MR900> <out file path> <kernel path> <rootfs path>"
 	rm -f $CFG_OUT
 	exit 1
 }
@@ -26,7 +26,7 @@ case $CE_TYPE in
 		FLASH_BS=262144
 		MD5_SKIP_BLOCKS=1
 		;;
-	OM5P|MR600)
+	OM5P|MR600|MR900)
 		MAX_PART_SIZE=7808
 		KERNEL_FLASH_ADDR=0xb0000
 		FLASH_BS=65536
