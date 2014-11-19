c3d6194c74744adabfa83a02dc98319662a3e898
John Crispin <blogic@openwrt.org>
2014-11-19 14:09:10 +0000

    gemini: add basic nas4220 board support
    Few notes:
    - redboot partitions are unusable
    - redboot declares only 16M of RAM, hence the mem arg in cmdline and separate kernel config
    - boots fine but only ethernet works for now
    - split wiliboard and raidsonic boards into different subtargets
    
    Signed-off-by: Roman Yeryomin <roman@advem.lv>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43319 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/gemini/raidsonic/target.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2014 OpenWrt.org
+#
+
+SUBTARGET:=raidsonic
+BOARDNAME:=Raidsonic NAS42x0
+FEATURES+=jffs2
+
+define Target/Description
+	Build firmware images for Raidsonic NAS4220.
+endef
