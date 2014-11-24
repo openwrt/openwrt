13d6fa008189f8c5a6b9c68ffc9afb665e7e878a
Jonas Gorski <jogo@openwrt.org>
2014-11-24 14:44:19 +0000

    brcm63xx: Add DT support for A226G/M
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    SVN-Revision: 43359

--- target/linux/brcm63xx/profiles/pirelli.mk
+++ target/linux/brcm63xx/profiles/pirelli.mk
@@ -5,6 +5,25 @@
 # See /LICENSE for more information.
 #
 
+define Profile/A226G
+  NAME:=Pirelli A226G
+  PACKAGES:=kmod-b43 wpad-mini\
+	kmod-usb2 kmod-usb-ohci
+endef
+define Profile/A226G/Description
+  Package set optimized for A226G.
+endef
+$(eval $(call Profile,A226G))
+
+define Profile/A226M
+  NAME:=Pirelli A226M/A226M-FWB
+  PACKAGES:=kmod-usb2 kmod-usb-ohci
+endef
+define Profile/A226M/Description
+  Package set optimized for A226M/A226M-FWB.
+endef
+$(eval $(call Profile,A226M))
+
 define Profile/AGPF_S0
   NAME:=Pirelli Alice Gate VoIP 2 Plus Wi-Fi AGPF-S0
   PACKAGES:=kmod-b43 wpad-mini\
