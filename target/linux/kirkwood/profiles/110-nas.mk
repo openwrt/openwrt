8dfb87bebc2c64863d75c0248b73a7144c3c00c9
John Crispin <john@openwrt.org>
2014-11-26 09:00:48 +0000

    kirkwood: Add support for Iomega StorCenter ix2-200
    Signed-off-by: Richard Kunze <richard.kunze@web.de>
    
    SVN-Revision: 43391

--- target/linux/kirkwood/profiles/110-nas.mk
+++ target/linux/kirkwood/profiles/110-nas.mk
@@ -84,3 +84,22 @@ POGOE02_UBIFS_OPTS:="-m 2048 -e 126KiB -c 4096"
 POGOE02_UBI_OPTS:="-m 2048 -p 128KiB -s 512"
 
 $(eval $(call Profile,POGOE02))
+
+
+define Profile/IOMEGA_IX2_200
+  NAME:=Iomega StorCenter ix2-200
+  PACKAGES:= \
+	kmod-ata-core kmod-ata-marvell-sata \
+	kmod-usb2 kmod-usb-storage \
+	uboot-envtools
+endef
+
+define Profile/IOMEGA_IX2_200/Description
+ Package set compatible with Iomega StorCenter ix2-200 board.
+endef
+
+IOMEGA_IX2_200_UBIFS_OPTS:="-m 512 -e 15872 -c 2048"
+IOMEGA_IX2_200_UBI_OPTS:="-m 512 -p 16384 -s 256"
+
+$(eval $(call Profile,IOMEGA_IX2_200))
+
