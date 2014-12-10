40d94a3ef026e9e9f27203f744af405fb2e5c82b
Rafał Miłecki <zajec5@gmail.com>
2014-12-10 21:50:57 +0000

    bcm53xx: backport NVRAM driver sent upstream
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    
    SVN-Revision: 43612

--- target/linux/bcm53xx/files/include/linux/bcm47xx_nvram.h
+++ target/linux/bcm53xx/files/include/linux/bcm47xx_nvram.h
@@ -1,8 +1,4 @@
 /*
- *  Copyright (C) 2005, Broadcom Corporation
- *  Copyright (C) 2006, Felix Fietkau <nbd@openwrt.org>
- *  Copyright (C) 2014 Hauke Mehrtens <hauke@hauke-m.de>
- *
  *  This program is free software; you can redistribute  it and/or modify it
  *  under  the terms of  the GNU General  Public License as published by the
  *  Free Software Foundation;  either version 2 of the  License, or (at your
@@ -15,20 +11,24 @@
 #include <linux/types.h>
 #include <linux/kernel.h>
 
-#ifdef CONFIG_BCM47XX
+#ifdef CONFIG_BCM47XX_NVRAM
+int bcm47xx_nvram_init_from_mem(u32 base, u32 lim);
 int bcm47xx_nvram_getenv(const char *name, char *val, size_t val_len);
-
 int bcm47xx_nvram_gpio_pin(const char *name);
 #else
+static inline int bcm47xx_nvram_init_from_mem(u32 base, u32 lim)
+{
+	return -ENOTSUPP;
+};
 static inline int bcm47xx_nvram_getenv(const char *name, char *val,
 				       size_t val_len)
 {
-	return -ENXIO;
-}
-
+	return -ENOTSUPP;
+};
 static inline int bcm47xx_nvram_gpio_pin(const char *name)
 {
-	return -ENXIO;
-}
+	return -ENOTSUPP;
+};
 #endif
+
 #endif /* __BCM47XX_NVRAM_H */
