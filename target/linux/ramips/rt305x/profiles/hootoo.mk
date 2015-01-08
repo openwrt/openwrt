b8f3eecf556c27a3344dd096152248d6bffa77f6
John Crispin <blogic@openwrt.org>
2015-01-08 20:26:20 +0000

    ramips: Fix HooToo HT-TM02 support.
    - Fix LED definitions.
    - Add mode-switch slider definition (at GPIO 14).
    - Remove unneeded VLAN now that the ethernet driver is fixed and TCP packets get passed correctly now. (This router only has one port.)
    - Fix LAN port MAC address, which is defined in the factory partition as WLAN MAC address + 1.
    - Fix board name, as the company name is HooToo and not HOOTOO.
    - Remove unnecessary packages from the profile. (The end-user is supposed to use mechanisms such as the ImageBuilder in order to add his own set of packages to his own images... while the precompiled images should just contain the core set of packages needed to run the base functions of a wireless router.)
    
    Signed-off-by: Vittorio Gambaletta <openwrt@vittgam.net>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43883 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/rt305x/profiles/hootoo.mk
+++ target/linux/ramips/rt305x/profiles/hootoo.mk
@@ -1,27 +1,17 @@
 #
-# Copyright (C) 2013 OpenWrt.org
+# Copyright (C) 2014 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
-
 define Profile/HT-TM02
-	NAME:=HOOTOO HT-TM02
-	PACKAGES:=\
-		wpad-mini \
-		kmod-ledtrig-netdev kmod-ledtrig-timer kmod-leds-gpio kmod-ledtrig-default-on \
-		kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb-net usbutils \
-		kmod-scsi-core kmod-scsi-generic kmod-fs-ext4 \
-		kmod-usb-storage kmod-usb-storage-extras block-mount \
-		kmod-usb-serial kmod-usb-serial-ftdi kmod-gpio-button-hotplug \
-		kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 luci luci-mod-admin-full \
-		kmod-app-samba luci-theme-openwrt luci-proto-relay relayd nano \
-		fstools 
+	NAME:=HooToo HT-TM02
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-ledtrig-usbdev
 endef
 
 define Profile/HT-TM02/Description
-	Package set for HOOTOO HT-TM02 board
+	Package set optimized for the HooToo HT-TM02.
 endef
 
 $(eval $(call Profile,HT-TM02))
