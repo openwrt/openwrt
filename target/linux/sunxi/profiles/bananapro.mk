1e0c22507adb4724ca25de994bcdbe69b8502900
Zoltan Herpai <wigyori@uid0.hu>
2015-01-09 19:35:14 +0000

    sunxi: bump kernel to 3.18 - refresh patches - add bananapro, cubietruck, mele_m9 support
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    
    SVN-Revision: 43900

---
+++ target/linux/sunxi/profiles/bananapro.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/Bananapro
+	NAME:=Bananapro
+	PACKAGES:=\
+		uboot-sunxi-Bananapro kmod-rtc-sunxi
+endef
+
+define Profile/Bananapro/Description
+	Package set optimized for the Bananapro
+endef
+
+$(eval $(call Profile,Bananapro))
