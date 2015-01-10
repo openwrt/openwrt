c6d03d1503346df167c37fb0348c2246531805b3
Jo-Philipp Wich <jow@openwrt.org>
2015-01-10 11:17:11 +0000

    version.mk: explicitely filter "generic" subtarget for "%s" placeholder (#18710)
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@43905 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/version.mk
+++ include/version.mk
@@ -79,7 +79,7 @@ VERSION_SED:=$(SED) 's,%U,$(VERSION_REPO),g' \
 	-e 's,%R,$(REVISION),g' \
 	-e 's,%T,$(BOARD),g' \
 	-e 's,%S,$(BOARD)/$(if $(SUBTARGET),$(SUBTARGET),generic),g' \
-	-e 's,%s,$(BOARD)$(if $(SUBTARGET),.$(SUBTARGET)),g' \
+	-e 's,%s,$(BOARD)$(if $(filter-out generic,$(SUBTARGET)),.$(SUBTARGET)),g' \
 	-e 's,%t,$(VERSION_TAINTS),g' \
 	-e 's,%M,$(VERSION_MANUFACTURER),g' \
 	-e 's,%P,$(VERSION_PRODUCT),g' \
