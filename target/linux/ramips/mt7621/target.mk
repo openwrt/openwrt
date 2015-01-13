bc78885e70c2e8eacfc3cab87f17c5ee5ade6bc0
Felix Fietkau <nbd@openwrt.org>
2015-01-13 00:34:49 +0000

    ramips: add missing -Os cflag for mt7621
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 43954

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -8,7 +8,7 @@ ARCH_PACKAGES:=ramips_1004kc
 FEATURES+=usb
 CPU_TYPE:=1004kc
 CPU_SUBTYPE:=dsp
-CFLAGS:=-pipe -mmt -mips32r2 -mtune=1004kc
+CFLAGS:=-Os -pipe -mmt -mips32r2 -mtune=1004kc
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
