75b65571c9d1442c9ca80a82c1d848c0e9886e16
John Crispin <blogic@openwrt.org>
2015-01-17 14:06:13 +0000

    ramips: add support for Intenso Memory 2 Move USB 3.0
    This adds support for a rt5350-based "portable nas" solution
    from Intenso. The board comes with 32M RAM and 8M Flash, the
    built-in HDD is connected/accessible via a usb3.0<->sata
    bridge VLI VL701.
    
    The device has 1 Ethernet port (100M/10M), 1 micro b usb 3.0
    socket (for charging the battery, or accessing the hdd directly).
    Wireless connectivity is provided by the rt5350 SoC [i.e.:
    802.11n 1x1 2.4 GHz with a pcb antenna.]
    
    Serial, leds, wifi, ethernet and usb are tested and
    as far as I can tell: they are working fine (tm).
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44001 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/rt305x/profiles/m2m.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/M2M
+	NAME:=Intenso Memory 2 Move
+	PACKAGES:=\
+		kmod-ledtrig-netdev kmod-ledtrig-timer kmod-leds-gpio \
+		kmod-usb-core kmod-usb2 kmod-usb-storage kmod-scsi-core \
+		kmod-fs-ext4 kmod-fs-vfat block-mount
+endef
+
+define Profile/M2M/Description
+	Package set for Intenso Memory 2 Move USB 3.0
+endef
+
+$(eval $(call Profile,M2M))
