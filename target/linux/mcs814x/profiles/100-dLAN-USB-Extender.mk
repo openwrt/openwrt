fd6358859e241b4f2957e079d2cc7df9fcef6a09
Florian Fainelli <florian@openwrt.org>
2015-01-21 23:55:11 +0000

    mcs814x: document how to include 3rd party binary packages
    The dLAN USB Extender profile was referecing binary packages which are not to
    be included in any of the source-only feeds. Instead remove these references
    and document how to use the 3rd party feed.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44071 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/mcs814x/profiles/100-dLAN-USB-Extender.mk
+++ target/linux/mcs814x/profiles/100-dLAN-USB-Extender.mk
@@ -7,12 +7,19 @@
 
 define Profile/dLAN_USB_Extender
   NAME:=Devolo dLAN USB Extender
-  PACKAGES:=hpavcfg foot kmod-usb-serial kmod-usb-serial-ftdi \
-	    wmbus-repeater
+  PACKAGES:=kmod-usb-serial kmod-usb-serial-ftdi
 endef
 
 define Profile/dLAN_USB_Extender/Description
 	Package set specifically tuned for the Devolo dLAN USB Extender device.
+
+	Binary packages are required to enable the power line portion of the
+	device, these packages can be found in the following package feed:
+
+	src-git dlan https://github.com/ffainelli/dlan-usb-extender-3rd-party.git
+
+	Instructions are available here:
+	http://ffainelli.github.io/dlan-usb-extender-3rd-party/
 endef
 $(eval $(call Profile,dLAN_USB_Extender))
 
