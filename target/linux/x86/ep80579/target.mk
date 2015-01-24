386f05f1e11833bc12dc2ffdfd2233f3a2d21e45
Felix Fietkau <nbd@openwrt.org>
2015-01-24 20:01:40 +0000

    x86: mark the ep80579 subtarget as broken
    It still uses the long obsolete 3.3 kernel version
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 44107

--- target/linux/x86/ep80579/target.mk
+++ target/linux/x86/ep80579/target.mk
@@ -2,6 +2,7 @@ BOARDNAME:=Intel EP80579
 KERNEL_PATCHVER:=3.3
 DEFAULT_PACKAGES += kmod-ep80579-can kmod-ep80579-eth kmod-ep80579-misc \
 			kmod-usb-core kmod-usb2 kmod-usb-hid kmod-usb-uhci
+FEATURES += broken
 
 define Target/Description
 	Build firmware images for Intel EP80579 based boards
