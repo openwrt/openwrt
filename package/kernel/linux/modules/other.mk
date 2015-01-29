3609351bdee061722deea92460c17bbda70c7851
Jo-Philipp Wich <jow@openwrt.org>
2015-01-29 21:59:03 +0000

    linux: kmod-zram requires kmod-lib-lz4 for >= 3.15
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44205 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -748,7 +748,7 @@ $(eval $(call KernelPackage,ikconfig))
 define KernelPackage/zram
   SUBMENU:=$(OTHER_MENU)
   TITLE:=ZRAM
-  DEPENDS:=+kmod-lib-lzo @!TARGET_ep93xx
+  DEPENDS:=+kmod-lib-lzo @!TARGET_ep93xx +(!LINUX_3_8&&!LINUX_3_10&&!LINUX_3_13&&!LINUX_3_14):kmod-lib-lz4
   KCONFIG:= \
 	CONFIG_ZSMALLOC \
 	CONFIG_ZRAM \
