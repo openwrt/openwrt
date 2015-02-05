9886084b94568328481aaa20c3c3501f78da335e
Jo-Philipp Wich <jow@openwrt.org>
2015-02-05 16:57:12 +0000

    include: prereq-build.mk: use TestHostCommand and SetupHostCommand
    Also move tests from tools/Makefile and include/host.mk here to have all tests
    in a central place.
    
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 44271

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -12,191 +12,164 @@ include $(INCLUDE_DIR)/host-build.mk
 
 PKG_NAME:=Build dependency
 
-define Require/non-root
-	[ "$$(shell whoami)" != "root" ]
-endef
-$(eval $(call Require,non-root, \
-	Please do not compile as root. \
-))
 
 # Required for the toolchain
-define Require/working-make
-	$(MAKE) -v | awk '($$$$1 == "GNU") && ($$$$2 == "Make") && ($$$$3 >= "3.81") { print "ok" }' | grep ok > /dev/null
-endef
+$(eval $(call TestHostCommand,working-make, \
+	Please install GNU make v3.81 or later. (This version has bugs), \
+	$(MAKE) -v | grep -E 'Make (3\.8[1-9]|3\.9[0-9]|[4-9]\.)'))
 
-$(eval $(call Require,working-make, \
-	Please install GNU make v3.81 or later. (This version has bugs) \
-))
+$(eval $(call TestHostCommand,case-sensitive-fs, \
+	OpenWrt can only be built on a case-sensitive filesystem, \
+	rm -f $(TMP_DIR)/test.*; touch $(TMP_DIR)/test.fs; \
+		test ! -f $(TMP_DIR)/test.FS))
 
-define Require/case-sensitive-fs
-	rm -f $(TMP_DIR)/test.*
-	touch $(TMP_DIR)/test.fs
-	[ \! -f $(TMP_DIR)/test.FS ]
-endef
+$(eval $(call SetupHostCommand,gcc, \
+	Please install the GNU C Compiler (gcc), \
+	$(CC) --version | grep gcc, \
+	gcc --version | grep gcc, \
+	gcc49 --version | grep gcc, \
+	gcc48 --version | grep gcc, \
+	gcc47 --version | grep gcc, \
+	gcc46 --version | grep gcc))
 
-$(eval $(call Require,case-sensitive-fs, \
-	OpenWrt can only be built on a case-sensitive filesystem \
-))
-
-define Require/getopt
-	gnugetopt --help 2>&1 | grep long >/dev/null || \
-	/usr/local/bin/getopt --help 2>&1 | grep long >/dev/null || \
-	getopt --help 2>&1 | grep long >/dev/null
-endef
-$(eval $(call Require,getopt, \
-	Please install GNU getopt \
-))
-
-define Require/fileutils
-	gcp --help || cp --help
-endef
-$(eval $(call Require,fileutils, \
-	Please install GNU fileutils \
-))
-
-define Require/working-gcc
+$(eval $(call TestHostCommand,working-gcc, \
+	Please reinstall the GNU C Compiler - it appears to be broken, \
 	echo 'int main(int argc, char **argv) { return 0; }' | \
-		gcc -x c -o $(TMP_DIR)/a.out -
-endef
+		gcc -x c -o $(TMP_DIR)/a.out -))
 
-$(eval $(call Require,working-gcc, \
-	Please install the GNU C Compiler (gcc). \
-))
+$(eval $(call SetupHostCommand,g++, \
+	Please install the GNU C++ Compiler (g++), \
+	$(CXX) --version | grep g++, \
+	g++ --version | grep g++, \
+	g++49 --version | grep g++, \
+	g++48 --version | grep g++, \
+	g++47 --version | grep g++, \
+	g++46 --version | grep g++))
 
-define Require/working-g++
+$(eval $(call TestHostCommand,working-g++, \
+	Please reinstall the GNU C++ Compiler - it appears to be broken, \
 	echo 'int main(int argc, char **argv) { return 0; }' | \
 		g++ -x c++ -o $(TMP_DIR)/a.out - -lstdc++ && \
-		$(TMP_DIR)/a.out
-endef
+		$(TMP_DIR)/a.out))
 
-$(eval $(call Require,working-g++, \
-	Please install the GNU C++ Compiler (g++). \
-))
+$(eval $(call TestHostCommand,ncurses, \
+	Please install ncurses. (Missing libncurses.so or ncurses.h), \
+	echo 'int main(int argc, char **argv) { initscr(); return 0; }' | \
+		gcc -include ncurses.h -x c -o $(TMP_DIR)/a.out - -lncurses))
+
+$(eval $(call TestHostCommand,zlib, \
+	Please install zlib. (Missing libz.so or zlib.h), \
+	echo 'int main(int argc, char **argv) { gzdopen(0, "rb"); return 0; }' | \
+		gcc -include zlib.h -x c -o $(TMP_DIR)/a.out - -lz))
+
+$(eval $(call TestHostCommand,libssl, \
+	Please install the openssl library (with development headers), \
+	echo 'int main(int argc, char **argv) { SSL_library_init(); return 0; }' | \
+		gcc -include openssl/ssl.h -x c -o $(TMP_DIR)/a.out - -lcrypto -lssl))
 
 ifneq ($(HOST_STATIC_LINKING),)
-  define Require/working-gcc-static
+  $(eval $(call TestHostCommand,working-gcc-static, \
+	Please install the static libc development package (glibc-static on CentOS/Fedora/RHEL)., \
 	echo 'int main(int argc, char **argv) { return 0; }' | \
-		gcc -x c $(HOST_STATIC_LINKING) -o $(TMP_DIR)/a.out -
-  endef
+		gcc -x c $(HOST_STATIC_LINKING) -o $(TMP_DIR)/a.out -))
 
-  $(eval $(call Require,working-gcc-static, \
-    Please install the static libc development package (glibc-static on CentOS/Fedora/RHEL). \
-  ))
-
-  define Require/working-g++-static
+  $(eval $(call TestHostCommand,working-g++-static, \
+	Please install the static libstdc++ development package (libstdc++-static on CentOS/Fedora/RHEL)., \
 	echo 'int main(int argc, char **argv) { return 0; }' | \
 		g++ -x c++ $(HOST_STATIC_LINKING) -o $(TMP_DIR)/a.out - -lstdc++ && \
-		$(TMP_DIR)/a.out
-  endef
+		$(TMP_DIR)/a.out))
 
-  $(eval $(call Require,working-g++-static, \
-	Please install the static libstdc++ development package (libstdc++-static on CentOS/Fedora/RHEL). \
-  ))
+  $(eval $(call TestHostCommand,zlib-static, \
+	Please install a static zlib. (zlib-static on CentOS/Fedora/RHEL)., \
+	echo 'int main(int argc, char **argv) { gzdopen(0, "rb"); return 0; }' | \
+		gcc -include zlib.h -x c $(HOST_STATIC_LINKING) -o $(TMP_DIR)/a.out - -lz))
 endif
 
-define Require/ncurses
-	echo 'int main(int argc, char **argv) { initscr(); return 0; }' | \
-		gcc -include ncurses.h -x c -o $(TMP_DIR)/a.out - -lncurses
-endef
 
-$(eval $(call Require,ncurses, \
-	Please install ncurses. (Missing libncurses.so or ncurses.h) \
-))
+$(eval $(call SetupHostCommand,tar,Please install GNU 'tar', \
+	gtar --version 2>&1 | grep GNU, \
+	gnutar --version 2>&1 | grep GNU, \
+	tar --version 2>&1 | grep GNU))
+
+$(eval $(call SetupHostCommand,find,Please install GNU 'find', \
+	gfind --version 2>&1 | grep GNU, \
+	find --version 2>&1 | grep GNU))
+
+$(eval $(call SetupHostCommand,bash,Please install GNU 'bash', \
+	bash --version 2>&1 | grep GNU))
+
+$(eval $(call SetupHostCommand,patch,Please install GNU 'patch', \
+	gpatch --version 2>&1 | grep 'Free Software Foundation', \
+	patch --version 2>&1 | grep 'Free Software Foundation'))
+
+$(eval $(call SetupHostCommand,diff,Please install diffutils, \
+	gdiff --version 2>&1 | grep diff, \
+	diff --version 2>&1 | grep diff))
+
+$(eval $(call SetupHostCommand,cp,Please install GNU fileutils, \
+	gcp --help, \
+	cp --help))
+
+$(eval $(call SetupHostCommand,seq,, \
+	gseq --version, \
+	seq --version))
+
+$(eval $(call SetupHostCommand,awk,Please install GNU 'awk', \
+	gawk --version 2>&1 | grep GNU, \
+	awk --version 2>&1 | grep GNU))
+
+$(eval $(call SetupHostCommand,grep,Please install GNU 'grep', \
+	ggrep --version 2>&1 | grep GNU, \
+	grep --version 2>&1 | grep GNU))
+
+$(eval $(call SetupHostCommand,getopt, \
+	Please install an extended getopt version that supports --long, \
+	gnugetopt -o t --long test -- --test | grep '^ *--test *--', \
+	/usr/local/bin/getopt -o t --long test -- --test | grep '^ *--test *--', \
+	getopt -o t --long test -- --test | grep '^ *--test *--'))
+
+$(eval $(call SetupHostCommand,stat,Cannot find a file stat utility, \
+	gnustat -c%s $(TMP_DIR)/.host.mk, \
+	gstat -c%s $(TMP_DIR)/.host.mk, \
+	stat -c%s $(TMP_DIR)/.host.mk))
+
+$(eval $(call SetupHostCommand,md5sum,, \
+	gmd5sum /dev/null | grep d41d8cd98f00b204e9800998ecf8427e, \
+	md5sum /dev/null | grep d41d8cd98f00b204e9800998ecf8427e, \
+	$(SCRIPT_DIR)/md5sum /dev/null | grep d41d8cd98f00b204e9800998ecf8427e))
+
+$(eval $(call SetupHostCommand,unzip,Please install 'unzip', \
+	unzip 2>&1 | grep zipfile, \
+	unzip))
+
+$(eval $(call SetupHostCommand,bzip2,Please install 'bzip2', \
+	bzip2 --version </dev/null))
+
+$(eval $(call SetupHostCommand,wget,Please install GNU 'wget', \
+	wget --version | grep GNU))
+
+$(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
+	perl --version | grep "perl 5"))
+
+$(eval $(call SetupHostCommand,python,Please install Python 2.x, \
+	python2.7 -V 2>&1 | grep Python, \
+	python2 -V 2>&1 | grep Python, \
+	python -V 2>&1 | grep Python))
+
+$(eval $(call SetupHostCommand,svn,Please install the Subversion client, \
+	svn --version | grep Subversion))
+
+$(eval $(call SetupHostCommand,git,Please install Git (git-core) >= 1.6.5, \
+	git clone --help | grep -- --recursive))
+
+$(eval $(call SetupHostCommand,file,Please install the 'file' package, \
+	file --version | grep file))
+
+$(eval $(call SetupHostCommand,openssl,Please install the 'openssl' utility, \
+	openssl version | grep OpenSSL))
 
 
-define Require/zlib
-	echo 'int main(int argc, char **argv) { gzdopen(0, "rb"); return 0; }' | \
-		gcc -include zlib.h -x c -o $(TMP_DIR)/a.out - -lz
-endef
-
-$(eval $(call Require,zlib, \
-	Please install zlib. (Missing libz.so or zlib.h) \
-))
-
-ifneq ($(HOST_STATIC_LINKING),)
-  define Require/zlib-static
-	echo 'int main(int argc, char **argv) { gzdopen(0, "rb"); return 0; }' | \
-		gcc -include zlib.h -x c $(HOST_STATIC_LINKING) -o $(TMP_DIR)/a.out - -lz
-  endef
-
-  $(eval $(call Require,zlib-static, \
-	Please install a static zlib. (zlib-static on CentOS/Fedora/RHEL). \
-  ))
-endif
-
-$(eval $(call RequireCommand,gawk, \
-	Please install GNU awk. \
-))
-
-$(eval $(call RequireCommand,unzip, \
-	Please install unzip. \
-))
-
-$(eval $(call RequireCommand,bzip2, \
-	Please install bzip2. \
-))
-
-$(eval $(call RequireCommand,perl, \
-	Please install perl. \
-))
-
-$(eval $(call RequireCommand,$(PYTHON), \
-	Please install python. \
-))
-
-$(eval $(call RequireCommand,wget, \
-	Please install wget. \
-))
-
-define Require/git
-	git --version | awk '($$$$1 == "git") && ($$$$2 == "version") && ($$$$3 >= "1.6.5") { print "ok" }' | grep ok > /dev/null
-endef
-
-$(eval $(call Require,git, \
-	Please install git (git-core) v1.6.5 or later. \
-))
-
-define Require/gnutar
-	$(TAR) --version 2>&1 | grep GNU > /dev/null
-endef
-
-$(eval $(call Require,gnutar, \
-	Please install GNU tar. \
-))
-
-$(eval $(call RequireCommand,svn, \
-	Please install the subversion client. \
-))
-
-define Require/libssl
-	echo 'int main(int argc, char **argv) { SSL_library_init(); return 0; }' | \
-		gcc -include openssl/ssl.h -x c -o $(TMP_DIR)/a.out - -lcrypto -lssl
-endef
-
-$(eval $(call Require,libssl, \
-	Please install the openssl library (with development headers) \
-))
-
-$(eval $(call RequireCommand,openssl, \
-       Please install openssl. \
-))
-
-define Require/gnu-find
-	$(FIND) --version 2>/dev/null
-endef
-
-$(eval $(call Require,gnu-find, \
-	Please install GNU find \
-))
-
-define Require/getopt-extended
-	getopt --long - - >/dev/null
-endef
-
-$(eval $(call Require,getopt-extended, \
-	Please install an extended getopt version that supports --long \
-))
-
-$(eval $(call RequireCommand,file, \
-	Please install the 'file' package. \
-))
+# Install ldconfig stub
+$(eval $(call TestHostCommand,ldconfig-stub,Failed to install stub, \
+	touch $(STAGING_DIR_HOST)/bin/ldconfig && \
+	chmod +x $(STAGING_DIR_HOST)/bin/ldconfig))
