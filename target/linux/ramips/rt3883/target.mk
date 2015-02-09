52a82a73ae6d700f03cd8435f9070f154de30918
John Crispin <blogic@openwrt.org>
2015-02-09 19:34:32 +0000

    ralink: change rt3883 cpu type to 74kc
    Signed-off-by: michael lee <igvtee@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44370 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/rt3883/target.mk
+++ target/linux/ramips/rt3883/target.mk
@@ -6,7 +6,7 @@ SUBTARGET:=rt3883
 BOARDNAME:=RT3662/RT3883 based boards
 ARCH_PACKAGES:=ramips_24kec
 FEATURES+=usb pci
-CPU_TYPE:=24kec
+CPU_TYPE:=74kc
 CPU_SUBTYPE:=dsp2
 
 DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc
