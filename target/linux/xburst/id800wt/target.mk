464e7a7f10e634612a70793a46c38bcaaf58737a
Lars-Peter Clausen <lars@metafoo.de>
2015-03-01 15:56:43 +0000

    xburst: Remove n516, n526 and id800wt subtargets
    The driver code for supporting these subtargets was already removed a while ago,
    so
    the current images wont actually boot.
    
    It is also quite unlikely that there are any users of them at this point, so
    remove them.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    
    SVN-Revision: 44586

--- target/linux/xburst/id800wt/target.mk
+++
@@ -1,8 +0,0 @@
-BOARDNAME:=Sungale Pictureframe (id800wt)
-DEFAULT_PACKAGES += \
-	kmod-sound-core \
-	kmod-sound-soc-core \
-	kmod-sound-soc-jz4740 \
-	kmod-sound-soc-jz4740-codec \
-	kmod-sound-soc-id800wt \
-
--- target/linux/xburst/n516/target.mk
+++
@@ -1,8 +0,0 @@
-BOARDNAME:=Hanvon N516 e-book reader
-DEFAULT_PACKAGES += \
-	kmod-sound-core \
-	kmod-sound-soc-core \
-	kmod-sound-soc-jz4740 \
-	kmod-sound-soc-jz4740-codec \
-	kmod-sound-soc-n516 \
-
