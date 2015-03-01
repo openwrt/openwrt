02d68b34418dd3709d1aa57a06429feac8ada353
Michael Büsch <mb@bu3sch.de>
2015-03-01 20:44:33 +0000

    Fix download URL of the 4.x kernel.
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44589 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/kernel.mk
+++ include/kernel.mk
@@ -52,7 +52,7 @@ else
   LINUX_SOURCE:=linux-$(LINUX_VERSION).tar.xz
   TESTING:=$(if $(findstring -rc,$(LINUX_VERSION)),/testing,)
   ifeq ($(call qstrip,$(CONFIG_EXTERNAL_KERNEL_TREE))$(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI)),)
-      LINUX_SITE:=@KERNEL/linux/kernel/v3.x$(TESTING)
+      LINUX_SITE:=@KERNEL/linux/kernel/v$(word 1,$(subst ., ,$(KERNEL_BASE))).x$(TESTING)
   endif
 
   ifneq ($(TARGET_BUILD),1)
