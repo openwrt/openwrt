e151d056229202ad1451281f249b725c52492071
Steven Barth <cyrus@openwrt.org>
2015-03-10 08:43:54 +0000

    6rd: make host dependency more specific
    Signed-off-by: Steven Barth <steven@midlink.org>
    
    SVN-Revision: 44636

--- package/network/ipv6/6rd/files/6rd.sh
+++ package/network/ipv6/6rd/files/6rd.sh
@@ -23,7 +23,7 @@ proto_6rd_setup() {
 		return
 	}
 
-	( proto_add_host_dependency "$cfg" 0.0.0.0 "$tunlink" )
+	( proto_add_host_dependency "$cfg" "$peeraddr" "$tunlink" )
 
 	[ -z "$ipaddr" ] && {
 		local wanif="$tunlink"
