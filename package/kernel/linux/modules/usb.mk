e995ed50602bc5c5b6831c1e6805811c0e739ecf
John Crispin <john@openwrt.org>
2015-03-12 10:06:01 +0000

    modules: set dwc2 gadget dependency for at91
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 44688

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -440,7 +440,7 @@ $(eval $(call KernelPackage,usb2-pci))
 
 define KernelPackage/usb-dwc2
   TITLE:=DWC2 USB controller driver
-  DEPENDS:=@!LINUX_3_8 @!LINUX_3_10 +TARGET_brcm2708:kmod-usb-gadget
+  DEPENDS:=@!LINUX_3_8 @!LINUX_3_10 +(TARGET_brcm2708||TARGET_at91):kmod-usb-gadget
   KCONFIG:= \
 	CONFIG_USB_DWC2 \
 	CONFIG_USB_DWC2_PCI \
