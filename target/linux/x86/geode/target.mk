eb03a1d7c2c25e52e69086d0b0c8abb3bfe2f954
Felix Fietkau <nbd@openwrt.org>
2015-03-16 07:05:30 +0000

    x86: merge all geode based subtargets into one
    Build all geode platform drivers into the kernel
    Remove kmod-* packages for these drivers
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44809 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/x86/geode/target.mk
@@ -0,0 +1,14 @@
+BOARDNAME:=AMD Geode based systems
+FEATURES:=squashfs ext4 pci usb gpio
+DEFAULT_PACKAGES += \
+			kmod-crypto-hw-geode kmod-crypto-ocf kmod-crypto-aes kmod-crypto-cbc \
+			kmod-ath5k kmod-ath9k \
+			kmod-button-hotplug \
+			kmod-ledtrig-heartbeat kmod-ledtrig-gpio \
+			kmod-ledtrig-netdev hwclock wpad-mini
+
+CPU_TYPE := geode
+
+define Target/Description
+	Build firmware images for AMD Geode GX/LX based systems (net5501, alix, geos)
+endef
