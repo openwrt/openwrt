efedd48f33eb9c8068009c5cf883ba3c93e87b10
John Crispin <blogic@openwrt.org>
2015-03-17 09:43:13 +0000

    ar71xx: detect GL.iNet model
    There are two versions of the GL.iNet, the 6408A and the 6416A. The only
    difference is the flash size.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44860 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -274,6 +274,19 @@ tplink_pharos_board_detect() {
 	[ -n "$model" ] && AR71XX_MODEL="$model v$2"
 }
 
+gl_inet_board_detect() {
+	local size="$(mtd_get_part_size 'firmware')"
+
+	case "$size" in
+	8192000)
+		AR71XX_MODEL='GL-iNet 6408A v1'
+		;;
+	16580608)
+		AR71XX_MODEL='GL-iNet 6416A v1'
+		;;
+	esac
+}
+
 ar71xx_board_detect() {
 	local machine
 	local name
@@ -401,6 +414,7 @@ ar71xx_board_detect() {
 		;;
 	*"GL-CONNECT INET v1")
 		name="gl-inet"
+		gl_inet_board_detect
 		;;
 	*"EnGenius ESR1750")
 		name="esr1750"
