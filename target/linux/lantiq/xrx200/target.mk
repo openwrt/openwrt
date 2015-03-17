29cf72ac0768a01b65a54b4503d2643713c8cd66
John Crispin <john@openwrt.org>
2015-03-17 09:44:06 +0000

    lantiq/xrx200: cleanup default packages
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 44867

--- target/linux/lantiq/xrx200/target.mk
+++ target/linux/lantiq/xrx200/target.mk
@@ -5,7 +5,18 @@ FEATURES:=squashfs atm mips16 jffs2 nand ubifs
 CPU_TYPE:=34kc
 CPU_SUBTYPE:=dsp
 
-DEFAULT_PACKAGES+=kmod-leds-gpio kmod-gpio-button-hotplug swconfig atm-esi
+DEFAULT_PACKAGES+=kmod-leds-gpio \
+	kmod-gpio-button-hotplug \
+	ltq-vdsl-vr9-fw-installer \
+	kmod-ltq-vdsl-vr9-mei \
+	kmod-ltq-vdsl-vr9 \
+	kmod-ltq-atm-vr9 \
+	kmod-ltq-ptm-vr9 \
+	kmod-ltq-deu-vr9 \
+	ltq-vdsl-app \
+	ppp-mod-pppoa \
+	swconfig \
+	atm-esi
 
 define Target/Description
 	Lantiq XRX200
