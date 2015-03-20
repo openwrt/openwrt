1773aa5163cc3039ae29f3261d84f98c14836937
John Crispin <john@openwrt.org>
2015-03-20 08:49:08 +0000

    octeon: add er8 sysupgrade support
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 44902

--- target/linux/octeon/base-files/lib/upgrade/platform.sh
+++ target/linux/octeon/base-files/lib/upgrade/platform.sh
@@ -35,36 +35,50 @@ platform_copy_config() {
 	esac
 }
 
+platform_do_flash() {
+	local tar_file=$1
+	local board=$2
+	local kernel=$3
+	local rootfs=$4
+
+	mkdir -p /boot
+	mount -t vfat /dev/$kernel /boot
+
+	[ -f /boot/vmlinux.64 -a ! -L /boot/vmlinux.64 ] && {
+		mv /boot/vmlinux.64 /boot/vmlinux.64.previous
+		mv /boot/vmlinux.64.md5 /boot/vmlinux.64.md5.previous
+	}
+
+	echo "flashing kernel to /dev/$kernel"
+	tar xf $tar_file sysupgrade-$board/kernel -O > /boot/vmlinux.64
+	md5sum /boot/vmlinux.64 | cut -f1 -d " " > /boot/vmlinux.64.md5
+	echo "flashing rootfs to ${rootfs}"
+	tar xf $tar_file sysupgrade-$board/root -O | dd of="${rootfs}" bs=4096
+	sync
+	umount /boot
+}
+
 platform_do_upgrade() {
+	local tar_file="$1"
 	local board=$(octeon_board_name)
 	local rootfs="$(platform_get_rootfs)"
+	local kernel=
 
 	[ -b "${rootfs}" ] || return 1
-
 	case "$board" in
 	erlite)
-		local tar_file="$1"
-		local kernel_length=`(tar xf $tar_file sysupgrade-erlite/kernel -O | wc -c) 2> /dev/null`
-		local rootfs_length=`(tar xf $tar_file sysupgrade-erlite/root -O | wc -c) 2> /dev/null`
-
-		mkdir -p /boot
-		mount -t vfat /dev/sda1 /boot
-
-		[ -f /boot/vmlinux.64 -a ! -L /boot/vmlinux.64 ] && {
-			mv /boot/vmlinux.64 /boot/vmlinux.64.previous
-			mv /boot/vmlinux.64.md5 /boot/vmlinux.64.md5.previous
-		}
-
-		tar xf $tar_file sysupgrade-erlite/kernel -O > /boot/vmlinux.64
-		md5sum /boot/vmlinux.64 | cut -f1 -d " " > /boot/vmlinux.64.md5
-		tar xf $tar_file sysupgrade-erlite/root -O | dd of="${rootfs}" bs=4096
-		sync
-		umount /boot
-		return 0
+		kernel=sda1
 		;;
+	er)
+		kernel=mmcblk0p1
+		;;
+	*)
+		return 1
 	esac
 
-	return 1
+	platform_do_flash $tar_file $board $kernel $rootfs
+
+	return 0
 	
 }
 
@@ -72,10 +86,11 @@ platform_check_image() {
 	local board=$(octeon_board_name)
 
 	case "$board" in
-	erlite)
+	erlite | \
+	er)
 		local tar_file="$1"
-		local kernel_length=`(tar xf $tar_file sysupgrade-erlite/kernel -O | wc -c) 2> /dev/null`
-		local rootfs_length=`(tar xf $tar_file sysupgrade-erlite/root -O | wc -c) 2> /dev/null`
+		local kernel_length=`(tar xf $tar_file sysupgrade-$board/kernel -O | wc -c) 2> /dev/null`
+		local rootfs_length=`(tar xf $tar_file sysupgrade-$board/root -O | wc -c) 2> /dev/null`
 		[ "$kernel_length" = 0 -o "$rootfs_length" = 0 ] && {
 			echo "The upgarde image is corrupt."
 			return 1
