3a52795531485ca861f9bf33f99b5d9b4b275621
Felix Fietkau <nbd@openwrt.org>
2015-03-24 10:07:07 +0000

    uClibc: remove avr32 support
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 44961

--- toolchain/uClibc/common.mk
+++ toolchain/uClibc/common.mk
@@ -26,7 +26,6 @@ UCLIBC_TARGET_ARCH:=$(shell echo $(ARCH) | sed -e s'/-.*//' \
 		-e 's/i.86/i386/' \
 		-e 's/sparc.*/sparc/' \
 		-e 's/arm.*/arm/g' \
-		-e 's/avr32.*/avr32/g' \
 		-e 's/m68k.*/m68k/' \
 		-e 's/ppc/powerpc/g' \
 		-e 's/v850.*/v850/g' \
