10cdfd04e68f540760eee9a3ac45babf620bc2f2
Jonas Gorski <jogo@openwrt.org>
2015-03-25 15:36:12 +0000

    brcm63xx: move RTA770(B)W to dts
    Signed-off-by: Jonas Gorski <jogo@openwrt.org>
    
    SVN-Revision: 44992

--- target/linux/brcm63xx/profiles/dynalink.mk
+++ target/linux/brcm63xx/profiles/dynalink.mk
@@ -5,6 +5,24 @@
 # See /LICENSE for more information.
 #
 
+define Profile/RTA770BW
+  NAME:=Dynalink RTA770BW (Siemens SE 515)
+  PACKAGES:=kmod-b43 wpad-mini
+endef
+define Profile/RTA770BW/Description
+  Package set optimized for RTA770BW.
+endef
+$(eval $(call Profile,RTA770BW))
+
+define Profile/RTA770W
+  NAME:=Dynalink RTA770W
+  PACKAGES:=kmod-b43 wpad-mini
+endef
+define Profile/RTA770W/Description
+  Package set optimized for RTA770W.
+endef
+$(eval $(call Profile,RTA770W))
+
 define Profile/RTA1025W
   NAME:=Dynalink RTA1025W
   PACKAGES:=kmod-b43 wpad-mini
