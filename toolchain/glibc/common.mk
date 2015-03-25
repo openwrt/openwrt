360ae62034e160f3218fc353252d445f4f71f99b
Felix Fietkau <nbd@openwrt.org>
2015-03-25 14:29:23 +0000

    toolchain/glibc: remove broken eglibc 2.15 support
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@44976 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -17,12 +17,6 @@ PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)-r$(PKG_REVISION)
 PKG_SOURCE:=$(PKG_SOURCE_SUBDIR).tar.bz2
 
 GLIBC_PATH:=
-ifneq ($(CONFIG_EGLIBC_VERSION_2_15),)
-  GLIBC_PATH:=libc/
-  PKG_SOURCE_PROTO:=svn
-  PKG_SOURCE:=$(PKG_SOURCE_SUBDIR).tar.bz2
-  PKG_SOURCE_URL:=svn://svn.eglibc.org/branches/eglibc-2_15
-endif
 ifneq ($(CONFIG_EGLIBC_VERSION_2_19),)
   GLIBC_PATH:=libc/
   PKG_SOURCE_PROTO:=svn
@@ -105,9 +99,6 @@ define Host/Prepare
 	ln -snf $(PKG_SOURCE_SUBDIR) $(BUILD_DIR_TOOLCHAIN)/$(PKG_NAME)
 ifeq ($(CONFIG_GLIBC_VERSION_2_21),)
 	$(SED) 's,y,n,' $(HOST_BUILD_DIR)/libc/option-groups.defaults
-ifneq ($(CONFIG_EGLIBC_VERSION_2_15),)
-	ln -sf ../ports $(HOST_BUILD_DIR)/libc/
-endif
 endif
 endef
 
