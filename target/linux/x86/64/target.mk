ebf1e2461ae5796fb9d98ce0cadd56cd4f0716c7
Imre Kaloz <kaloz@openwrt.org>
2015-03-29 12:17:39 +0000

    x86_64: add description, maintainership
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    
    SVN-Revision: 45138

--- target/linux/x86/64/target.mk
+++ target/linux/x86/64/target.mk
@@ -1,3 +1,8 @@
 ARCH:=x86_64
 BOARDNAME:=x86_64
 DEFAULT_PACKAGES += kmod-e1000 kmod-r8169
+MAINTAINER:=Imre Kaloz <kaloz@openwrt.org>
+
+define Target/Description
+        Build images for 64 bit systems including virtualized guests.
+endef
