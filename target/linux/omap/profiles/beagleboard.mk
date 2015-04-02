69aa85a0d26cc465ab74b1411f5805bb8a3596db
John Crispin <john@openwrt.org>
2015-04-02 14:31:51 +0000

    omap: add profile for BeagleBoard
    original BeagleBoard has no Ethernet
    provide a profile with all available USB Ethernet adapters
    EBVBeagle comes with a kmod-usb-net-mcs7830 compatible device
    
    tested with BeagleBoard C4
    
    Signed-off-by: Dirk Neukirchen <dirkneukirchen@web.de>
    
    SVN-Revision: 45237

---
+++ target/linux/omap/profiles/beagleboard.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/BEAGLEBOARD
+	NAME:=EBV BeagleBoard
+	FEATURES:= usb ext4 targz
+	DEFAULT_PACKAGES += kmod-usb2 kmod-usb2-omap \
+			kmod-usb-net kmod-usb-net-asix \
+			kmod-usb-net-asix-ax88179 kmod-usb-net-hso \
+			kmod-usb-net-kaweth kmod-usb-net-pegasus \
+			kmod-usb-net-mcs7830 kmod-usb-net-smsc95xx \
+			kmod-usb-net-dm9601-ether
+endef
+
+define Profile/BEAGLEBOARD/Description
+	Package set for the BEAGLEBOARD and similar devices.
+	Contains various USB-NET drivers for boards without Ethernet.
+endef
+
+$(eval $(call Profile,BEAGLEBOARD))
