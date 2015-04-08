705651884d6ad3ae598ce59d3750fd3e53234739
Rafał Miłecki <zajec5@gmail.com>
2015-04-08 05:55:32 +0000

    brcm47xx: update legacy subtarget description
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    
    SVN-Revision: 45310

--- target/linux/brcm47xx/legacy/target.mk
+++ target/linux/brcm47xx/legacy/target.mk
@@ -1,7 +1,8 @@
 FEATURES += low_mem pcmcia
-BOARDNAME:=Legacy (802.11g)
+BOARDNAME:=Legacy (BMIPS3300)
 
 define Target/Description
-	Build generic firmware for legacy 802.11g Broadcom BCM47xx
-	and BCM53xx MIPS devices.
+	Build firmware for Broadcom BCM47xx and BCM53xx devices with
+	BMIPS3300 CPU except for BCM4705 SoC.
+	Supported SoCs: BCM5352E, BCM5354, BCM5365?, BCM4712, BCM4704.
 endef
