9cfa923660364f8389353538e08180ad02c7029e
Felix Fietkau <nbd@openwrt.org>
2015-04-11 10:51:55 +0000

    netifd: fix start_disabled 'sh: bad number' in mac80211 setup (bug #19345)
    Two errors "netifd: radio0: sh: bad number" have recently surfaced in system
    log in trunk when wifi interfaces come up. I tracked the errors to checking
    numerical values of some config options without ensuring that the option has
    any value.
    
    The errors I see have apparently been introduced by r45051 (ieee80211r in
    hostapd) and r45326 (start_disabled in mac80211). My patches fix two
    instances of "bad number", but there may be a third one, as the original
    report in bug 19345 pre-dates r45326 and already has two "bad number" errors
    for radio0.
    
    https://dev.openwrt.org/ticket/19345
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@45379 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+++ package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -318,6 +318,7 @@ mac80211_hostapd_setup_bss() {
 	json_get_vars wds dtim_period max_listen_int start_disabled
 
 	set_default wds 0
+	set_default start_disabled 0
 
 	[ "$wds" -gt 0 ] && append hostapd_cfg "wds_sta=1" "$N"
 	[ "$staidx" -gt 0 -o "$start_disabled" -eq 1 ] && append hostapd_cfg "start_disabled=1" "$N"
