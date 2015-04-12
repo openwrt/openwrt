df1cdff437b0170024f656daba797df38dfd6638
Imre Kaloz <kaloz@openwrt.org>
2015-04-12 15:02:43 +0000

    modules: package the mvebu ahci driver
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    
    
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@45394 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/kernel/linux/modules/block.mk
+++ package/kernel/linux/modules/block.mk
@@ -63,7 +63,7 @@ define KernelPackage/ata-ahci-platform
     $(LINUX_DIR)/drivers/ata/ahci_platform.ko \
     $(LINUX_DIR)/drivers/ata/libahci_platform.ko
   AUTOLOAD:=$(call AutoLoad,40,libahci_platform ahci_platform,1)
-  $(call AddDepends/ata,@TARGET_ipq806x +kmod-ata-ahci)
+  $(call AddDepends/ata,@TARGET_ipq806x||TARGET_mvebu +kmod-ata-ahci)
 endef
 
 define KernelPackage/ata-ahci-platform/description
@@ -120,6 +120,22 @@ define KernelPackage/ata-marvell-sata/description
 endef
 
 $(eval $(call KernelPackage,ata-marvell-sata))
+
+
+define KernelPackage/ata-mvebu-ahci
+  TITLE:=Marvell EBU AHCI support
+  DEPENDS:=@TARGET_mvebu +kmod-ata-ahci-platform
+  KCONFIG:=CONFIG_AHCI_MVEBU
+  FILES:=$(LINUX_DIR)/drivers/ata/ahci_mvebu.ko
+  AUTOLOAD:=$(call AutoLoad,41,ahci_mvebu,1)
+  $(call AddDepends/ata)
+endef
+
+define KernelPackage/ata-mvebu-ahci/description
+ AHCI support for Marvell EBU SoCs
+endef
+
+$(eval $(call KernelPackage,ata-mvebu-ahci))
 
 
 define KernelPackage/ata-nvidia-sata
