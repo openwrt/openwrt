f19757215be1f4a661c4b29839dd74b25f21a78c
Jo-Philipp Wich <jow@openwrt.org>
2015-05-29 11:03:55 +0000

    au1000: fix kmod-pcmcia-au1000
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 45817

--- target/linux/au1000/modules.mk
+++ target/linux/au1000/modules.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2010 OpenWrt.org
+# Copyright (C) 2010-2015 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,9 +8,10 @@
 define KernelPackage/pcmcia-au1000
   SUBMENU:=$(PCMCIA_MENU)
   TITLE:=RMI/AMD Au1000 PCMCIA support
-  DEPENDS:=@TARGET_au1000 kmod-pcmcia-core
-  FILES:=$(LINUX_DIR)/drivers/pcmcia/au1x00_ss.ko
-  AUTOLOAD:=$(call AutoLoad,41,au1x00_ss)
+  DEPENDS:=@TARGET_au1000 +kmod-pcmcia-core
+  FILES:=$(LINUX_DIR)/drivers/pcmcia/db1xxx_ss.ko
+  KCONFIG:=CONFIG_PCMCIA_ALCHEMY_DEVBOARD
+  AUTOLOAD:=$(call AutoLoad,41,db1xxx_ss)
 endef
 
 $(eval $(call KernelPackage,pcmcia-au1000))
