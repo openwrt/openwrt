1cc4a10d2c77bfcdc113c5db90e49b7b546b3eab
John Crispin <john@openwrt.org>
2015-05-31 17:46:24 +0000

    kernel: bluetooth: Add dependency to kmod-crypto-ecb
    Add dependency of kmod-crypto-ecb to kmod-bluetooth to avoid the kernel warning
    "Bluetooth: Unable to create crypto context".
    
    Signed-off-by: Bruno Randolf <br1@einfach.org>
    
    SVN-Revision: 45860

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -28,7 +28,7 @@ $(eval $(call KernelPackage,6lowpan))
 define KernelPackage/bluetooth
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Bluetooth support
-  DEPENDS:=@USB_SUPPORT +kmod-usb-core +kmod-crypto-hash +kmod-lib-crc16 +kmod-hid
+  DEPENDS:=@USB_SUPPORT +kmod-usb-core +kmod-crypto-hash +kmod-crypto-ecb +kmod-lib-crc16 +kmod-hid
   KCONFIG:= \
 	CONFIG_BLUEZ \
 	CONFIG_BLUEZ_L2CAP \
