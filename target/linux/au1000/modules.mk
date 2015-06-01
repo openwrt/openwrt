c0c8d3a95fb260f386bc0cacb79018dff2eac9b4
John Crispin <john@openwrt.org>
2015-06-01 06:03:52 +0000

    au1000: add a missing dependency to the pcmcia module
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 45864

--- target/linux/au1000/modules.mk
+++ target/linux/au1000/modules.mk
@@ -8,7 +8,7 @@
 define KernelPackage/pcmcia-au1000
   SUBMENU:=$(PCMCIA_MENU)
   TITLE:=RMI/AMD Au1000 PCMCIA support
-  DEPENDS:=@TARGET_au1000 +kmod-pcmcia-core
+  DEPENDS:=@TARGET_au1000 +kmod-pcmcia-core +kmod-pcmcia-rsrc
   FILES:=$(LINUX_DIR)/drivers/pcmcia/db1xxx_ss.ko
   KCONFIG:=CONFIG_PCMCIA_ALCHEMY_DEVBOARD
   AUTOLOAD:=$(call AutoLoad,41,db1xxx_ss)
