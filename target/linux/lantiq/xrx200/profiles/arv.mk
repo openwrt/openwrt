ca1db6b5ce5675d88b45b23f576e8c59f7f4fc48
John Crispin <john@openwrt.org>
2015-06-05 14:11:36 +0000

    lantiq: Add support for Arcadyan VGV7510KW22 (known as o2 Box 6431)
    SVN-Revision: 45897

--- target/linux/lantiq/xrx200/profiles/arv.mk
+++ target/linux/lantiq/xrx200/profiles/arv.mk
@@ -10,6 +10,20 @@ endef
 
 $(eval $(call Profile,VG3503J_V2))
 
+define Profile/VGV7510KW22NOR
+  NAME:=o2 Box 6431 / Arcadyan VGV7510KW22 (NOR)
+  PACKAGES:=kmod-rt2800-pci wpad-mini kmod-usb-dwc2
+endef
+
+$(eval $(call Profile,VGV7510KW22NOR))
+
+define Profile/VGV7510KW22BRN
+  NAME:=o2 Box 6431 / Arcadyan VGV7510KW22 (BRN)
+  PACKAGES:=kmod-rt2800-pci wpad-mini kmod-usb-dwc2
+endef
+
+$(eval $(call Profile,VGV7510KW22BRN))
+
 define Profile/VGV7519NOR
   NAME:=Experiabox 8 VGV7519
   PACKAGES:=kmod-rt2800-pci wpad-mini kmod-usb-dwc2
