3146134687cbd0a49243d623063b1563013c12a7
John Crispin <blogic@openwrt.org>
2015-06-18 18:20:56 +0000

    ramips: Add Support for the Linksys RE6500
    This patch adds support for the Linksys RE6500 Range Extender
    
    http://wiki.openwrt.org/toh/linksys/re6500
    
    Signed-off-by: L. D. Pinney <ldpinney@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@46043 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/mt7621/profiles/linksys.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/RE6500
+	NAME:=Linksys RE6500
+	PACKAGES:= kmod-mt76
+endef
+
+define Profile/RE6500/Description
+	Package set compatible with the Linksys RE6500 board.
+endef
+$(eval $(call Profile,RE6500))
