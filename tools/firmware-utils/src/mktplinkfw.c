3df0da9819e0e5bb9faa1afec6d5eb0be8e156e9
John Crispin <blogic@openwrt.org>
2015-06-18 20:24:15 +0000

    ar71xx: Add Support for the Bitmain Antminer S1
    This patch adds support for the Bitmain Antminer S1 Cryptocurrency Miner
    
    http://wiki.openwrt.org/toh/bitmain/s1
    
    Signed-off-by: L. D. Pinney <ldpinney@gmail.com>
    Acked-by: James Hilliard <james.hilliard1@gmail.com>
    
    Backport of r46044
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/chaos_calmer@46054 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -30,6 +30,7 @@
 #define ALIGN(x,a) ({ typeof(a) __a = (a); (((x) + __a - 1) & ~(__a - 1)); })
 
 #define HEADER_VERSION_V1	0x01000000
+#define HWID_ANTMINER_S1	0x04440001
 #define HWID_GL_INET_V1		0x08000001
 #define HWID_GS_OOLITE_V1	0x3C000101
 #define HWID_TL_MR10U_V1	0x00100101
@@ -411,6 +412,11 @@ static struct board_info boards[] = {
 		.hw_id		= HWID_GS_OOLITE_V1,
 		.hw_rev		= 1,
 		.layout_id	= "16Mlzma",
+	}, {
+		.id		= "ANTMINER-S1",
+		.hw_id		= HWID_ANTMINER_S1,
+		.hw_rev		= 1,
+		.layout_id	= "8Mlzma",
 	}, {
 		/* terminating entry */
 	}
