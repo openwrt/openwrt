e85f3471bad00a51711e7d7c7ab84e4ba64c1b93
John Crispin <john@openwrt.org>
2015-06-18 18:21:23 +0000

    ar71xx: add support for TL-WR720N-v4.
    The hardware should be almost the same as TL-WR720N-v3.  WiFi and LAN networks
    were tested by "Lo Yuk Fai <loyukfai@gmail.com>".  Failsafe and slider switch
    were tested by "Wong min <alpha080@gmail.com>".
    
    Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>
    
    SVN-Revision: 46046

--- tools/firmware-utils/src/mktplinkfw.c
+++ tools/firmware-utils/src/mktplinkfw.c
@@ -54,6 +54,7 @@
 #define HWID_TL_WDR4900_V1	0x49000001
 #define HWID_TL_WR703N_V1	0x07030101
 #define HWID_TL_WR720N_V3	0x07200103
+#define HWID_TL_WR720N_V4	0x07200104
 #define HWID_TL_WR741ND_V1	0x07410001
 #define HWID_TL_WR741ND_V4	0x07410004
 #define HWID_TL_WR740N_V1	0x07400001
@@ -400,6 +401,11 @@ static struct board_info boards[] = {
 	}, {
 		.id		= "TL-WR720Nv3",
 		.hw_id		= HWID_TL_WR720N_V3,
+		.hw_rev		= 1,
+		.layout_id	= "4Mlzma",
+	}, {
+		.id		= "TL-WR720Nv4",
+		.hw_id		= HWID_TL_WR720N_V4,
 		.hw_rev		= 1,
 		.layout_id	= "4Mlzma",
 	}, {
