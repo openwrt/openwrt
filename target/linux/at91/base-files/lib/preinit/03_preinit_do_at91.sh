ad576d262d5e954a322525675352ecce79a932a0
Florian Fainelli <florian@openwrt.org>
2015-07-04 01:25:15 +0000

    at91: add Device Tree based board detection
    Set up eth0 as LAN interface by default.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    
    SVN-Revision: 46174

--- target/linux/at91/base-files/lib/preinit/03_preinit_do_at91.sh
+++ target/linux/at91/base-files/lib/preinit/03_preinit_do_at91.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 do_at91() {
-	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
-	echo $(strings /proc/device-tree/compatible | head -1) > /tmp/sysinfo/board_name
-	echo $(cat /proc/device-tree/model) > /tmp/sysinfo/model
+	. /lib/at91.sh
+
+	at91_board_detect
 }
 
 boot_hook_add preinit_main do_at91
