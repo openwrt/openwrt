4a1021de873d13f46b265cdd1116907f3f2be791
John Crispin <john@openwrt.org>
2015-07-14 16:24:02 +0000

    x86: add kmod-acpi-button and kmod-button-hotplug to default packages
    Since r40909 openwrt can automatically shutdown when you press the
    power button or try to shutdown a kvm virtual machine.
    When booting a recent CC-rc2 or trunk image, it doesn't work,
    as two kernel modules are missing.
    
    Add these two kernel modules to the default packages, so users
    don't need to install them manually.
    
    Signed-off-by: Stefan Hellermann <stefan@the2masters.de>
    
    SVN-Revision: 46366

--- target/linux/x86/kvm_guest/target.mk
+++ target/linux/x86/kvm_guest/target.mk
@@ -1,4 +1,4 @@
 BOARDNAME:=KVM Guest
 FEATURES:=ext4 pci usb
-DEFAULT_PACKAGES += kmod-virtio-balloon kmod-virtio-net kmod-virtio-random
+DEFAULT_PACKAGES += kmod-acpi-button kmod-button-hotplug kmod-virtio-balloon kmod-virtio-net kmod-virtio-random
 CPU_TYPE := pentium4
