52bfee77e47fab1ca2a6be8712b00619bccc2d13
Jo-Philipp Wich <jow@openwrt.org>
2015-07-14 17:23:56 +0000

    linux: enable kmod-acpi-button for x86_64 (#20080)
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 46368

--- package/kernel/linux/modules/input.mk
+++ package/kernel/linux/modules/input.mk
@@ -176,7 +176,7 @@ $(eval $(call KernelPackage,input-matrixkmap))
 define KernelPackage/acpi-button
   SUBMENU:=$(INPUT_MODULES_MENU)
   TITLE:=ACPI Button Support
-  DEPENDS:=@(TARGET_x86_generic||TARGET_x86_kvm_guest||TARGET_x86_xen_domu) +kmod-input-evdev
+  DEPENDS:=@(TARGET_x86_generic||TARGET_x86_kvm_guest||TARGET_x86_xen_domu||TARGET_x86_64) +kmod-input-evdev
   KCONFIG:=CONFIG_ACPI_BUTTON
   FILES:=$(LINUX_DIR)/drivers/acpi/button.ko
   AUTOLOAD:=$(call AutoLoad,06,button)
