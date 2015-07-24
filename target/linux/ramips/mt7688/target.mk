7b4ff671b7c416297d8be63b5eba44800f45f1bd
John Crispin <blogic@openwrt.org>
2015-07-24 09:11:56 +0000

    ramips: add subtarget for mt7688
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@46473 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/ramips/mt7688/target.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+
+SUBTARGET:=mt7688
+BOARDNAME:=MT7688 based boards
+ARCH_PACKAGES:=ramips_24kec
+FEATURES+=usb
+CPU_TYPE:=24kec
+CPU_SUBTYPE:=dsp
+
+define Target/Description
+	Build firmware images for Ralink MT7688 based boards.
+endef
+
