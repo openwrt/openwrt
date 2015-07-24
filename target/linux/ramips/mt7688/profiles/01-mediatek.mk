e04e44a2234d1bf25fa406e57b38aee2c05344e2
John Crispin <john@openwrt.org>
2015-07-24 09:11:56 +0000

    ramips: add subtarget for mt7688
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    SVN-Revision: 46473

---
+++ target/linux/ramips/mt7688/profiles/01-mediatek.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2015 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/mt7688
+	NAME:=mt7688
+	PACKAGES:=\
+		kmod-usb-core kmod-usb2 kmod-usb-ohci \
+		kmod-ledtrig-usbdev
+endef
+
+define Profile/mt7688/Description
+	Default package set compatible with most boards.
+endef
+$(eval $(call Profile,mt7688))
