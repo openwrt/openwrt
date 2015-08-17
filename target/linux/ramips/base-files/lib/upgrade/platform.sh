a5165329a942d4eb4c221bdf9b3bf3342500c37b
John Crispin <blogic@openwrt.org>
2015-08-17 06:01:49 +0000

    ramips: be consistent with case statement in base-files/lib/upgrade/platform.sh
    Always use "|\" (without spaces) and keep boards in alphabetical order.
    
    Signed-off-by: Piotr Dymacz <pepe2k@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@46633 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -14,120 +14,120 @@ platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
 	case "$board" in
-	3g-6200n | \
-	3g-6200nl | \
-	3g150b | \
-	3g300m | \
-	a5-v11 | \
-	air3gii | \
-	ai-br100 |\
-	all0239-3g | \
-	all0256n | \
-	all5002 | \
-	all5003 | \
-	ar725w | \
-	asl26555 | \
-	awapn2403 | \
-	awm002-evb | \
-	awm003-evb | \
-	bc2 | \
-	broadway | \
-	carambola | \
-	cf-wr800n | \
-	d105 | \
-	dap-1350 | \
-	dcs-930 | \
-	dcs-930l-b1 | \
-	dir-300-b1 | \
-	dir-300-b7 | \
-	dir-320-b1 | \
-	dir-600-b1 | \
-	dir-600-b2 | \
-	dir-615-d | \
-	dir-615-h1 | \
-	dir-620-a1 | \
-	dir-620-d1 | \
-	dir-810l | \
-	e1700 | \
-	esr-9753 | \
-	f7c027 | \
-	fonera20n | \
-	freestation5 | \
-	firewrt |\
-	pbr-m1 |\
-	hg255d | \
-	hlk-rm04 | \
-	ht-tm02 | \
-	hw550-3g | \
-	ip2202 | \
-	m2m | \
-	m3 | \
-	m4 | \
-	microwrt | \
-	mlw221 | \
-	mlwg2 | \
-	mofi3500-3gn | \
-	mpr-a1 | \
-	mpr-a2 | \
-	mr-102n | \
-	mzk-w300nh2 | \
-	mzk-dp150n | \
-	nbg-419n | \
-	nw718 | \
-	miniembwifi | \
-	hpm | \
-	miniembplug | \
-	rt5350f-olinuxino | \
-	rt5350f-olinuxino-evb | \
-	psr-680w | \
-	px-4885 | \
-	re6500 | \
-	rp-n53 | \
-	rt-g32-b1 | \
-	rt-n10-plus | \
-	rt-n13u | \
-	rt-n14u | \
-	rt-n15 | \
-	rt-n56u | \
-	rut5xx | \
-	sap-g3200u3 | \
-	sl-r7205 | \
-	tew-691gr | \
-	tew-692gr | \
-	ur-326n4g |\
-	ur-336un |\
-	v22rw-2x2 | \
-	vocore | \
-	w150m | \
-	w306r-v20 |\
-	w502u |\
-	whr-g300n |\
-	whr-300hp2 |\
-	whr-600d |\
-	whr-1166d |\
-	wsr-600 |\
-	wl-330n | \
-	wl-330n3g | \
-	wl-351 | \
-	wl-341v3 | \
-	wli-tx4-ag300n | \
-	wzr-agl300nh | \
-	wmr-300 |\
-	wnce2001 | \
-	wr512-3gn |\
-	wr6202 |\
-	zbt-wr8305rt |\
-	wrtnode |\
-	oy-0001 |\
-	wt1520 |\
-	wt3020 |\
-	x5 |\
-	x8 |\
-	miwifi-mini |\
-	y1 |\
-	y1s |\
-	zte-q7 |\
-	zbt-wa05)
+	3g150b|\
+	3g300m|\
+	3g-6200n|\
+	3g-6200nl|\
+	a5-v11|\
+	ai-br100|\
+	air3gii|\
+	all0239-3g|\
+	all0256n|\
+	all5002|\
+	all5003|\
+	ar725w|\
+	asl26555|\
+	awapn2403|\
+	awm002-evb|\
+	awm003-evb|\
+	bc2|\
+	broadway|\
+	carambola|\
+	cf-wr800n|\
+	d105|\
+	dap-1350|\
+	dcs-930|\
+	dcs-930l-b1|\
+	dir-300-b1|\
+	dir-300-b7|\
+	dir-320-b1|\
+	dir-600-b1|\
+	dir-600-b2|\
+	dir-615-d|\
+	dir-615-h1|\
+	dir-620-a1|\
+	dir-620-d1|\
+	dir-810l|\
+	e1700|\
+	esr-9753|\
+	f7c027|\
+	firewrt|\
+	fonera20n|\
+	freestation5|\
+	hg255d|\
+	hlk-rm04|\
+	hpm|\
+	ht-tm02|\
+	hw550-3g|\
+	ip2202|\
+	m2m|\
+	m3|\
+	m4|\
+	microwrt|\
+	miniembplug|\
+	miniembwifi|\
+	miwifi-mini|\
+	mlw221|\
+	mlwg2|\
+	mofi3500-3gn|\
+	mpr-a1|\
+	mpr-a2|\
+	mr-102n|\
+	mzk-dp150n|\
+	mzk-w300nh2|\
+	nbg-419n|\
+	nw718|\
+	oy-0001|\
+	pbr-m1|\
+	psr-680w|\
+	px-4885|\
+	re6500|\
+	rp-n53|\
+	rt5350f-olinuxino|\
+	rt5350f-olinuxino-evb|\
+	rt-g32-b1|\
+	rt-n10-plus|\
+	rt-n13u|\
+	rt-n14u|\
+	rt-n15|\
+	rt-n56u|\
+	rut5xx|\
+	sap-g3200u3|\
+	sl-r7205|\
+	tew-691gr|\
+	tew-692gr|\
+	ur-326n4g|\
+	ur-336un|\
+	v22rw-2x2|\
+	vocore|\
+	w150m|\
+	w306r-v20|\
+	w502u|\
+	whr-1166d|\
+	whr-300hp2|\
+	whr-600d|\
+	whr-g300n|\
+	wl-330n|\
+	wl-330n3g|\
+	wl-341v3|\
+	wl-351|\
+	wli-tx4-ag300n|\
+	wmr-300|\
+	wnce2001|\
+	wr512-3gn|\
+	wr6202|\
+	wrtnode|\
+	wsr-600|\
+	wt1520|\
+	wt3020|\
+	wzr-agl300nh|\
+	x5|\
+	x8|\
+	y1|\
+	y1s|\
+	zbt-wa05|\
+	zbt-wr8305rt|\
+	zte-q7)
 		[ "$magic" != "27051956" ] && {
 			echo "Invalid image type."
 			return 1
@@ -148,9 +148,9 @@ platform_check_image() {
 		}
 		return 0
 		;;
-	cy-swr1100 |\
-	dir-610-a1 |\
-	dir-645 |\
+	cy-swr1100|\
+	dir-610-a1|\
+	dir-645|\
 	dir-860l-b1)
 		[ "$magic" != "5ea3a417" ] && {
 			echo "Invalid image type."
