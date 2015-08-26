f87990840db36a8a9e5fac8044c4f3a3a213b849
Rafał Miłecki <zajec5@gmail.com>
2015-08-26 15:21:14 +0000

    bcm53xx: support sysupgrade with Netgear R7000 original firmware
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    
    SVN-Revision: 46732

--- target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
+++ target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
@@ -26,6 +26,7 @@ platform_expected_image() {
 	case "$machine" in
 		"netgear,r6250v1")	echo "chk U12H245T00_NETGEAR"; return;;
 		"netgear,r6300v2")	echo "chk U12H240T00_NETGEAR"; return;;
+		"netgear,r7000")	echo "chk U12H270T00_NETGEAR"; return;;
 		"netgear,r8000")	echo "chk U12H315T00_NETGEAR"; return;;
 	esac
 }
