9e670fcf9d436abe28df552eb7bb4cb325e354a9
Felix Fietkau <nbd@openwrt.org>
2015-10-05 14:11:25 +0000

    build: use host cflags/ldflags for openssl prereq check
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 47138

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -74,7 +74,7 @@ $(eval $(call TestHostCommand,zlib, \
 $(eval $(call TestHostCommand,libssl, \
 	Please install the openssl library (with development headers), \
 	echo 'int main(int argc, char **argv) { SSL_library_init(); return 0; }' | \
-		gcc -include openssl/ssl.h -x c -o $(TMP_DIR)/a.out - -lcrypto -lssl))
+		gcc $(HOST_CFLAGS) -include openssl/ssl.h -x c -o $(TMP_DIR)/a.out - -lcrypto -lssl $(HOST_LDFLAGS)))
 
 $(eval $(call TestHostCommand,perl-thread-queue, \
 	Please install the Perl Thread::Queue module, \
