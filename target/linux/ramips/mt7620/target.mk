5defa2bf520df6bff023480fcbb2312b7129375a
Felix Fietkau <nbd@openwrt.org>
2015-10-06 12:32:31 +0000

    ramips: enable mt76 by default for the mt7620 subtarget (for devices with mt7620 + mt7612)
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47146 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -9,7 +9,7 @@ FEATURES+=usb
 CPU_TYPE:=24kec
 CPU_SUBTYPE:=dsp
 
-DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc
+DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc kmod-mt76
 
 define Target/Description
 	Build firmware images for Ralink MT7620 based boards.
