ebff32ce9e793005ff6a54037bf02ea067a41569
John Crispin <blogic@openwrt.org>
2015-10-19 10:19:22 +0000

    ramips: add LinkIt Smart7688 support
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    Backport of r47203
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/chaos_calmer@47227 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/base-files/lib/ramips.sh
+++ target/linux/ramips/base-files/lib/ramips.sh
@@ -418,6 +418,15 @@ ramips_board_detect() {
 	*"Mediatek MT7628AN evaluation board")
 		name="mt7628"
 		;;
+	*"MediaTek LinkIt Smart7688")
+		linkit="$(dd bs=1 skip=1024 count=12 if=/dev/mtd2 2> /dev/null)"
+		if [ "${linkit}" = "LINKITS7688D" ]; then
+			name="linkits7688d"
+			RAMIPS_MODEL="${machine} DUO"
+		else
+			name="linkits7688"
+		fi
+		;;
 	*)
 		name="generic"
 		;;
