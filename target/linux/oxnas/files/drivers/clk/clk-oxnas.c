392b93f579a88954e1e9747c4a8ec113f22a383c
John Crispin <john@openwrt.org>
2015-10-19 10:09:16 +0000

    oxnas: fix duplicate include in clk-oxnas
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    
    SVN-Revision: 47215

--- target/linux/oxnas/files/drivers/clk/clk-oxnas.c
+++ target/linux/oxnas/files/drivers/clk/clk-oxnas.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <linux/clk-provider.h>
+#include <linux/clk.h>
 #include <linux/clkdev.h>
 #include <linux/clk-provider.h>
 #include <linux/of.h>
