5c338cb0d82ace6b43988dc95925b0d541e7cbfa
John Crispin <john@openwrt.org>
2015-10-19 10:10:41 +0000

    kernel: add mvsw61xx module package
    Adds the package to select the mvsw61xx module present in the 3.18/4.0/4.1
    kernels.
    
    Signed-off-by: Pushpal Sidhu <psidhu@gateworks.com>
    
    SVN-Revision: 47224

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -180,6 +180,20 @@ endef
 
 $(eval $(call KernelPackage,swconfig))
 
+define KernelPackage/switch-mvsw61xx
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Marvell 88E61xx switch support
+  DEPENDS:=+kmod-swconfig
+  KCONFIG:=CONFIG_MVSW61XX_PHY
+  FILES:=$(LINUX_DIR)/drivers/net/phy/mvsw61xx.ko
+  AUTOLOAD:=$(call AutoLoad,42,mvsw61xx)
+endef
+
+define KernelPackage/switch-mvsw61xx/description
+ Marvell 88E61xx switch support
+endef
+
+$(eval $(call KernelPackage,switch-mvsw61xx))
 
 define KernelPackage/switch-ip17xx
   SUBMENU:=$(NETWORK_DEVICES_MENU)
