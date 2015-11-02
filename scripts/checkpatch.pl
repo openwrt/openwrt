8f72985cf913f2da42e4cf7c522d0649aa10affe
John Crispin <blogic@openwrt.org>
2015-11-02 10:17:03 +0000

    scripts/checkpatch.pl: Fix deprecation warnings
    In perl version v5.22, use of unescaped opening braces is deprecated
    
    Signed-off-by: Javier Domingo Cansino <javierdo1@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47342 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2354,8 +2354,8 @@ sub process {
 
 # function brace can't be on same line, except for #defines of do while,
 # or if closed on same line
-		if (($line=~/$Type\s*$Ident\(.*\).*\s{/) and
-		    !($line=~/\#\s*define.*do\s{/) and !($line=~/}/)) {
+		if (($line=~/$Type\s*$Ident\(.*\).*\s\{/) and
+		    !($line=~/\#\s*define.*do\s\{/) and !($line=~/}/)) {
 			ERROR("OPEN_BRACE",
 			      "open brace '{' following function declarations go on the next line\n" . $herecurr);
 		}
@@ -2623,8 +2623,8 @@ sub process {
 ##		}
 
 #need space before brace following if, while, etc
-		if (($line =~ /\(.*\){/ && $line !~ /\($Type\){/) ||
-		    $line =~ /do{/) {
+		if (($line =~ /\(.*\)\{/ && $line !~ /\($Type\)\{/) ||
+		    $line =~ /do\{/) {
 			ERROR("SPACING",
 			      "space required before the open brace '{'\n" . $herecurr);
 		}
@@ -2914,7 +2914,7 @@ sub process {
 			    $dstat !~ /^for\s*$Constant$/ &&				# for (...)
 			    $dstat !~ /^for\s*$Constant\s+(?:$Ident|-?$Constant)$/ &&	# for (...) bar()
 			    $dstat !~ /^do\s*{/ &&					# do {...
-			    $dstat !~ /^\({/)						# ({...
+			    $dstat !~ /^\(\{/)						# ({...
 			{
 				$ctx =~ s/\n*$//;
 				my $herectx = $here . "\n";
