b0d9e644648a4e8cf427ed0ea91ed3d5320a3914
Felix Fietkau <nbd@openwrt.org>
2015-11-03 12:00:53 +0000

    scripts/config: fix handling of CONFDEFAULT on oldconfig
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 47372

--- scripts/config/confdata.c
+++ scripts/config/confdata.c
@@ -1166,6 +1166,8 @@ bool conf_set_all_new_symbols(enum conf_def_mode mode)
 	}
 	bool has_changed = false;
 
+	sym_clear_all_valid();
+
 	for_all_symbols(i, sym) {
 		if (sym_has_value(sym) || (sym->flags & SYMBOL_VALID))
 			continue;
@@ -1208,8 +1210,6 @@ bool conf_set_all_new_symbols(enum conf_def_mode mode)
 		}
 
 	}
-
-	sym_clear_all_valid();
 
 	/*
 	 * We have different type of choice blocks.
