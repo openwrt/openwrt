b9a7089f75bf40a4d2cc43eb445eddf99a7b1c9f
John Crispin <blogic@openwrt.org>
2015-11-05 10:01:23 +0000

    ramips: minor tweak to the lks7688 baord name
    Signed-off-by: John Crispin <blogic@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47388 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/ramips/base-files/lib/ramips.sh
+++ target/linux/ramips/base-files/lib/ramips.sh
@@ -205,7 +205,7 @@ ramips_board_detect() {
 	*"M4")
 		name="m4"
 		;;
-	*"MediaTek LinkIt Smart7688")
+	*"MediaTek LinkIt Smart 7688")
 		linkit="$(dd bs=1 skip=1024 count=12 if=/dev/mtd2 2> /dev/null)"
 		if [ "${linkit}" = "LINKITS7688D" ]; then
 			name="linkits7688d"
