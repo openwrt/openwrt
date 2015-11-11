c98a034ef3826eb04528bb9c73a93e50bb0abdf3
Luka Perkov <luka@openwrt.org>
2015-11-11 12:48:30 +0000

    kirkwood: add Linksys routers to default profile
    Signed-off-by: Claudio Leite <leitec@staticky.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47458 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/kirkwood/profiles/100-generic.mk
+++ target/linux/kirkwood/profiles/100-generic.mk
@@ -11,7 +11,8 @@ define Profile/Generic
 	kmod-mmc kmod-mvsdio kmod-usb2 kmod-usb-storage \
 	kmod-i2c-core kmod-i2c-mv64xxx \
 	kmod-ata-core kmod-ata-marvell-sata \
-	kmod-rtc-marvell kmod-thermal-kirkwood
+	kmod-rtc-marvell kmod-thermal-kirkwood \
+	kmod-mwl8k swconfig wpad-mini
 endef
 
 define Profile/Generic/Description
