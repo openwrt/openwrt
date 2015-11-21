31d7b4fc95b36931f8ea0889fe7ceab40b54c8a0
Hauke Mehrtens <hauke@hauke-m.de>
2015-11-21 17:40:46 +0000

    kernel: make kmod-usb-dwc2 depend on kmod-usb-gadget on omap target
    This dependency is missing for the omap target.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    SVN-Revision: 47551

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -476,7 +476,7 @@ $(eval $(call KernelPackage,usb2-pci))
 
 define KernelPackage/usb-dwc2
   TITLE:=DWC2 USB controller driver
-  DEPENDS:=+(TARGET_brcm2708||TARGET_at91||TARGET_brcm63xx||TARGET_mxs||TARGET_imx6):kmod-usb-gadget
+  DEPENDS:=+(TARGET_brcm2708||TARGET_at91||TARGET_brcm63xx||TARGET_mxs||TARGET_imx6||TARGET_omap):kmod-usb-gadget
   KCONFIG:= \
 	CONFIG_USB_DWC2 \
 	CONFIG_USB_DWC2_PCI \
