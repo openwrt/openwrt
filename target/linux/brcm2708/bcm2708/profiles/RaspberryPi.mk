a71f631a0043bd8d57f9d21d8ce54c73049b7be5
John Crispin <blogic@openwrt.org>
2015-11-21 21:25:04 +0000

    brcm2708: add support for multi-device images
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47561 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/brcm2708/bcm2708/profiles/RaspberryPi.mk
+++ target/linux/brcm2708/bcm2708/profiles/RaspberryPi.mk
@@ -5,34 +5,10 @@
 # See /LICENSE for more information.
 #
 
-define Profile/Default
-  NAME:=Raspberry Pi
+define Profile/RaspberryPi
+  NAME:=Raspberry Pi Models B/B+/CM
 endef
-define Profile/Default/Description
-  Raspberry Pi
+define Profile/RaspberryPi/Description
+  Raspberry Pi Models B/B+/CM
 endef
-$(eval $(call Profile,Default))
-
-define Profile/RaspberryPi_B
-  NAME:=Raspberry Pi Model B
-endef
-define Profile/RaspberryPi_B/Description
-  Raspberry Pi Model B
-endef
-$(eval $(call Profile,RaspberryPi_B))
-
-define Profile/RaspberryPi_BPlus
-  NAME:=Raspberry Pi Model B+
-endef
-define Profile/RaspberryPi_BPlus/Description
-  Raspberry Pi Model B+
-endef
-$(eval $(call Profile,RaspberryPi_BPlus))
-
-define Profile/RaspberryPi_CM
-  NAME:=Raspberry Pi Compute Module
-endef
-define Profile/RaspberryPi_CM/Description
-  Raspberry Pi Model Compute Module
-endef
-$(eval $(call Profile,RaspberryPi_CM))
+$(eval $(call Profile,RaspberryPi))
