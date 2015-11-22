f5c790a4729f35709212e71fda7c5781549efb02
Felix Fietkau <nbd@openwrt.org>
2015-11-22 19:06:33 +0000

    build: add support for choosing a different url filename part than the output file
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47591 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/download.mk
+++ include/download.mk
@@ -42,11 +42,11 @@ define DownloadMethod/unknown
 endef
 
 define DownloadMethod/default
-	$(SCRIPT_DIR)/download.pl "$(DL_DIR)" "$(FILE)" "$(MD5SUM)" $(foreach url,$(URL),"$(url)")
+	$(SCRIPT_DIR)/download.pl "$(DL_DIR)" "$(FILE)" "$(MD5SUM)" "$(URL_FILE)" $(foreach url,$(URL),"$(url)")
 endef
 
 define wrap_mirror
-	$(if $(if $(MIRROR),$(filter-out x,$(MIRROR_MD5SUM))),@$(SCRIPT_DIR)/download.pl "$(DL_DIR)" "$(FILE)" "$(MIRROR_MD5SUM)" || ( $(1) ),$(1))
+	$(if $(if $(MIRROR),$(filter-out x,$(MIRROR_MD5SUM))),@$(SCRIPT_DIR)/download.pl "$(DL_DIR)" "$(FILE)" "" "$(MIRROR_MD5SUM)" || ( $(1) ),$(1))
 endef
 
 define DownloadMethod/cvs
@@ -155,6 +155,7 @@ Validate/darcs=VERSION SUBDIR
 define Download/Defaults
   URL:=
   FILE:=
+  URL_FILE:=
   PROTO:=
   MD5SUM:=
   SUBDIR:=
