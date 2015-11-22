718fe053cb6faee6bb92ce49e9548404ceadfa81
John Crispin <blogic@openwrt.org>
2015-11-22 08:04:37 +0000

    brcm2708: add device detection and use it for network, leds and preinit
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47572 3c298f89-4303-0410-b956-a3cf2f4a3e73

---
+++ target/linux/brcm2708/base-files/lib/brcm2708.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright (C) 2015 OpenWrt.org
+
+ifname=""
+
+brcm2708_detect() {
+	local board_name model
+
+	model=$(cat /proc/device-tree/model)
+	case "$model" in
+	"Raspberry Pi Model B Rev"*)
+		board_name="rpi-b"
+		;;
+	"Raspberry Pi Model B+ Rev"*)
+		board_name="rpi-b-plus"
+		;;
+	"Raspberry Pi Compute Module Rev"*)
+		board_name="rpi-cm"
+		;;
+	"Raspberry Pi 2 Model B Rev"*)
+		board_name="rpi-2-b"
+		;;
+	*)
+		board_name="unknown"
+		;;
+	esac
+
+	[ -e "/tmp/sysinfo" ] || mkdir -p "/tmp/sysinfo"
+
+	echo "$board_name" > /tmp/sysinfo/board_name
+	echo "$model" > /tmp/sysinfo/model
+}
+
+brcm2708_board_name() {
+	local name
+
+	[ -f /tmp/sysinfo/board_name ] && name=$(cat /tmp/sysinfo/board_name)
+	[ -n "$name" ] || name="unknown"
+
+	echo $name
+}
