8ef96b3e6f23de09c5db4bd33cdc5279ed84c457
John Crispin <john@openwrt.org>
2015-11-24 18:28:05 +0000

    ar71xx: Build initramfs by default for Mikrotik devices
    Signed-off-by: Baptiste Jonglez <git@bitsofnetworks.org>
    
    SVN-Revision: 47612

--- target/linux/ar71xx/mikrotik/target.mk
+++ target/linux/ar71xx/mikrotik/target.mk
@@ -1,5 +1,5 @@
 BOARDNAME:=Mikrotik devices with NAND flash
-FEATURES += targz
+FEATURES += targz ramdisk
 
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x based Mikrotik boards.
