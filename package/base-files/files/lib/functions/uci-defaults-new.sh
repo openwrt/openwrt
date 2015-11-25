212feb9ccfc66e555bc917a76e5cabfb86ddedfe
Jo-Philipp Wich <jow@openwrt.org>
2015-11-25 15:43:15 +0000

    base-files: uci-defaults-new: don't store switch attr numbers as strings
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 47643

--- package/base-files/files/lib/functions/uci-defaults-new.sh
+++ package/base-files/files/lib/functions/uci-defaults-new.sh
@@ -91,11 +91,14 @@ ucidef_add_switch_attr() {
 	local val=$3
 
 	json_select_object switch
-
 	json_select_object $name
-	json_add_string $key $val
-	json_select ..
 
+	case "$val" in
+		[0-9]) json_add_int "$key" "$val" ;;
+		*) json_add_string "$key" "$val" ;;
+	esac
+
+	json_select ..
 	json_select ..
 }
 
