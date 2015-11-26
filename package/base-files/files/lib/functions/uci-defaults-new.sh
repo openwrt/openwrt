1e347d1d169bd93fd8dcf4294541bcae8d36ab28
Jo-Philipp Wich <jow@openwrt.org>
2015-11-26 13:53:06 +0000

    base-files: uci-defaults-new, config_generate: support for gpio leds
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47660 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/functions/uci-defaults-new.sh
+++ package/base-files/files/lib/functions/uci-defaults-new.sh
@@ -321,6 +321,27 @@ ucidef_set_led_default() {
 	json_select ..
 }
 
+ucidef_set_led_gpio() {
+	local cfg="led_$1"
+	local name="$2"
+	local sysfs="$3"
+	local gpio="$4"
+	local inverted="$5"
+
+	json_select_object led
+
+	json_select_object "$1"
+	json_add_string type gpio
+	json_add_string name "$name"
+	json_add_string sysfs "$sysfs"
+	json_add_string trigger "$trigger"
+	json_add_int gpio "$gpio"
+	json_add_boolean inverted "$inverted"
+	json_select ..
+
+	json_select ..
+}
+
 ucidef_set_led_rssi() {
 	local cfg="led_$1"
 	local name="$2"
