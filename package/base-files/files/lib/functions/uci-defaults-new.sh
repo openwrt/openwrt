fd83873d6440d699fee4277151729e2c0ebbdc6d
Jo-Philipp Wich <jow@openwrt.org>
2015-12-03 22:27:02 +0000

    base-files: board.d: support timer leds
    Add ucidef_set_led_timer() and handling for it to config_generate.
    
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47731 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/base-files/files/lib/functions/uci-defaults-new.sh
+++ package/base-files/files/lib/functions/uci-defaults-new.sh
@@ -422,6 +422,26 @@ ucidef_set_led_ide() {
 	json_select ..
 }
 
+ucidef_set_led_timer() {
+	local cfg="led_$1"
+	local name="$2"
+	local sysfs="$3"
+	local delayon="$4"
+	local delayoff="$5"
+
+	json_select_object led
+
+	json_select_object "$1"
+	json_add_string type timer
+	json_add_string name "$name"
+	json_add_string sysfs "$sysfs"
+	json_add_int delayon "$delayon"
+	json_add_int delayoff "$delayoff"
+	json_select ..
+
+	json_select ..
+}
+
 ucidef_set_led_rssi() {
 	local cfg="led_$1"
 	local name="$2"
