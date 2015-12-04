61a99da4f9219237a25bba37244307f803f2e1e1
Jo-Philipp Wich <jow@openwrt.org>
2015-12-04 11:06:57 +0000

    base-files: board.d: auto-set model id and name if /tmp/sysinfo is available
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>
    
    SVN-Revision: 47752

--- package/base-files/files/lib/functions/uci-defaults-new.sh
+++ package/base-files/files/lib/functions/uci-defaults-new.sh
@@ -543,6 +543,16 @@ ucidef_set_rssimon() {
 board_config_update() {
 	json_init
 	[ -f ${CFG} ] && json_load "$(cat ${CFG})"
+
+	# auto-initialize model id and name if applicable
+	if ! json_is_a model object; then
+		json_select_object model
+			[ -f "/tmp/sysinfo/board_name" ] && \
+				json_add_string id "$(cat /tmp/sysinfo/board_name)"
+			[ -f "/tmp/sysinfo/model" ] && \
+				json_add_string name "$(cat /tmp/sysinfo/model)"
+		json_select ..
+	fi
 }
 
 board_config_flush() {
