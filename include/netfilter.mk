d9fbe33d708d19c77ad37b26534ea81e0c22248a
Felix Fietkau <nbd@openwrt.org>
2015-12-13 18:33:11 +0000

    include/netfilter.mk: fix kmod-ipt-tee build with 4.3/4.4
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@47890 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- include/netfilter.mk
+++ include/netfilter.mk
@@ -255,6 +255,8 @@ $(eval $(call nf_add,IPT_LED,CONFIG_NETFILTER_XT_TARGET_LED, $(P_XT)xt_LED))
 # tee
 
 $(eval $(call nf_add,IPT_TEE,CONFIG_NETFILTER_XT_TARGET_TEE, $(P_XT)xt_TEE))
+$(eval $(if $(NF_KMOD),$(call nf_add,IPT_TEE,CONFIG_NF_DUP_IPV4, $(P_V4)nf_dup_ipv4, ge 4.3),))
+$(eval $(if $(NF_KMOD),$(call nf_add,IPT_TEE,CONFIG_NF_DUP_IPV6, $(P_V6)nf_dup_ipv6, ge 4.3),))
 
 # u32
 
