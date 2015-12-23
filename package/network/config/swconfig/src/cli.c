8536afae6f8ff3196aa8457cc0a53c5c0ea9fbac
John Crispin <john@openwrt.org>
2015-12-23 19:24:45 +0000

    swconfig: support receiving SWITCH_TYPE_LINK from kernel
    When using cli, print link state the same way kernel used to do it.
    This will allow kernel switching PORT_LINK from SWITCH_TYPE_STRING.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    
    SVN-Revision: 47998

--- package/network/config/swconfig/src/cli.c
+++ package/network/config/swconfig/src/cli.c
@@ -84,9 +84,27 @@ list_attributes(struct switch_dev *dev)
 	print_attrs(dev->port_ops);
 }
 
+static const char *
+speed_str(int speed)
+{
+	switch (speed) {
+	case 10:
+		return "10baseT";
+	case 100:
+		return "100baseT";
+	case 1000:
+		return "1000baseT";
+	default:
+		break;
+	}
+
+	return "unknown";
+}
+
 static void
 print_attr_val(const struct switch_attr *attr, const struct switch_val *val)
 {
+	struct switch_port_link *link;
 	int i;
 
 	switch (attr->type) {
@@ -103,6 +121,21 @@ print_attr_val(const struct switch_attr *attr, const struct switch_val *val)
 				(val->value.ports[i].flags &
 				 SWLIB_PORT_FLAG_TAGGED) ? "t" : "");
 		}
+		break;
+	case SWITCH_TYPE_LINK:
+		link = val->value.link;
+		if (link->link)
+			printf("port:%d link:up speed:%s %s-duplex %s%s%s%s%s",
+				val->port_vlan,
+				speed_str(link->speed),
+				link->duplex ? "full" : "half",
+				link->tx_flow ? "txflow " : "",
+				link->rx_flow ? "rxflow " : "",
+				link->eee & SWLIB_LINK_FLAG_EEE_100BASET ? "eee100 " : "",
+				link->eee & SWLIB_LINK_FLAG_EEE_1000BASET ? "eee1000 " : "",
+				link->aneg ? "auto" : "");
+		else
+			printf("port:%d link:down", val->port_vlan);
 		break;
 	default:
 		printf("?unknown-type?");
--- package/network/config/swconfig/src/swlib.h
+++ package/network/config/swconfig/src/swlib.h
@@ -105,11 +105,16 @@ enum swlib_port_flags {
 	SWLIB_PORT_FLAG_TAGGED = (1 << 0),
 };
 
+enum swlib_link_flags {
+	SWLIB_LINK_FLAG_EEE_100BASET = (1 << 0),
+	SWLIB_LINK_FLAG_EEE_1000BASET = (1 << 1),
+};
 
 struct switch_dev;
 struct switch_attr;
 struct switch_port;
 struct switch_port_map;
+struct switch_port_link;
 struct switch_val;
 struct uci_package;
 
@@ -138,6 +143,7 @@ struct switch_val {
 		char *s;
 		int i;
 		struct switch_port *ports;
+		struct switch_port_link *link;
 	} value;
 };
 
@@ -159,6 +165,17 @@ struct switch_port {
 struct switch_portmap {
 	unsigned int virt;
 	char *segment;
+};
+
+struct switch_port_link {
+	int link:1;
+	int duplex:1;
+	int aneg:1;
+	int tx_flow:1;
+	int rx_flow:1;
+	int speed;
+	/* in ethtool adv_t format */
+	uint32_t eee;
 };
 
 /**
