c75c8ab6a637831425f2bc0610231a1afc8168de
John Crispin <john@openwrt.org>
2015-12-23 19:26:26 +0000

    libnl: fix warning with poll.h include on musl
    Warning is:
      #warning redirecting incorrect #include <sys/poll.h> to <poll.
    
    Not a big issue.
    But it can be annoying when building with -Werror set.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>
    
    SVN-Revision: 48004

---
+++ package/libs/libnl/patches/001-fix-poll-h-include-warning-on-musl.h
@@ -0,0 +1,13 @@
+diff --git a/include/netlink/netlink.h b/include/netlink/netlink.h
+index 5f68548..3bf3ed5 100644
+--- a/include/netlink/netlink.h
++++ b/include/netlink/netlink.h
+@@ -16,7 +16,7 @@
+ #include <stdint.h>
+ #include <string.h>
+ #include <stdlib.h>
+-#include <sys/poll.h>
++#include <poll.h>
+ #include <sys/socket.h>
+ #include <sys/types.h>
+ #include <sys/time.h>
