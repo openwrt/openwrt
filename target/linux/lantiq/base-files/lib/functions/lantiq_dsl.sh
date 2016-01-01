436fdee2725704063f10bce698821d1185ca58ec
John Crispin <john@openwrt.org>
2016-01-01 21:19:21 +0000

    lantiq: add the current profile to the stats output
    Signed-off-by: Mathias Kresin <openwrt@kresin.me>
    
    SVN-Revision: 48045

--- target/linux/lantiq/base-files/lib/functions/lantiq_dsl.sh
+++ target/linux/lantiq/base-files/lib/functions/lantiq_dsl.sh
@@ -631,10 +631,40 @@ line_state() {
 	fi
 }
 
+#
+# Which profile is used?
+#
+profile() {
+	local bpstg=$(dsl_cmd bpstg)
+	local profile=$(dsl_val "$bpstg" nProfile);
+	local s;
+
+	case "$profile" in
+		"0")	s="8a" ;;
+		"1")	s="8b" ;;
+		"2")	s="8c" ;;
+		"3")	s="8d" ;;
+		"4")	s="12a" ;;
+		"5")	s="12b" ;;
+		"6")	s="17a" ;;
+		"7")	s="30a" ;;
+		"8")	s="17b" ;;
+		"")		s="";;
+		*)		s="unknown" ;;
+	esac
+
+	if [ "$action" = "lucistat" ]; then
+		echo "dsl.profile=${profile:-nil}"
+	else
+		echo "Profile:                                  $s"
+	fi
+}
+
 status() {
 	vendor
 	chipset
 	xtse
+	profile
 	line_state
 	errors
 	power_mode
