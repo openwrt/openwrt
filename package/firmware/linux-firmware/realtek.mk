b6fcd06204798afe2e11c094e3206dae7c4b7788
Felix Fietkau <nbd@openwrt.org>
2016-01-10 16:35:46 +0000

    linux-firmware: remove duplication in package metadata
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 48174

--- package/firmware/linux-firmware/realtek.mk
+++ package/firmware/linux-firmware/realtek.mk
@@ -1,32 +1,17 @@
-define Package/r8169-firmware
-  SECTION:=firmware
-  CATEGORY:=Firmware
-  URL:=http://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git
-  TITLE:=RealTek r8169 firmware
-endef
-
+Package/r8169-firmware = $(call Package/firmware-default,RealTek RTL8169 firmware)
 define Package/r8169-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/rtl_nic
 	$(CP) \
 		$(PKG_BUILD_DIR)/rtl_nic/* \
 		$(1)/lib/firmware/rtl_nic
 endef
-
 $(eval $(call BuildPackage,r8169-firmware))
 
-
-define Package/r8188eu-firmware
-  SECTION:=firmware
-  CATEGORY:=Firmware
-  URL:=http://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git
-  TITLE:=RealTek r8188eu firmware
-endef
-
+Package/r8188eu-firmware = $(call Package/firmware-default,RealTek RTL8188EU firmware)
 define Package/r8188eu-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/rtlwifi
 	$(CP) \
 		$(PKG_BUILD_DIR)/rtlwifi/rtl8188eufw.bin \
 		$(1)/lib/firmware/rtlwifi
 endef
-
 $(eval $(call BuildPackage,r8188eu-firmware))
