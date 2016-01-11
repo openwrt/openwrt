6489c5fa2c1d0061f11bcf10277711153bbf4012
Felix Fietkau <nbd@openwrt.org>
2016-01-11 20:51:19 +0000

    linux-firmware: fix mediatek/ralink package names
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@48204 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/firmware/linux-firmware/mediatek.mk
+++ package/firmware/linux-firmware/mediatek.mk
@@ -1,13 +1,13 @@
-Package/mt7601u = $(call Package/firmware-default,MediaTek MT7601U firmware)
+Package/mt7601u-firmware = $(call Package/firmware-default,MediaTek MT7601U firmware)
 define Package/mt7601u-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware
 	$(INSTALL_DATA) \
 		$(PKG_BUILD_DIR)/mt7601u.bin \
 		$(1)/lib/firmware
 endef
-$(eval $(call BuildPackage,mt7601u))
+$(eval $(call BuildPackage,mt7601u-firmware))
 
-Package/rt2800-pci = $(call Package/firmware-default,Ralink RT28xx/3xxx PCI/SoC firmware)
+Package/rt2800-pci-firmware = $(call Package/firmware-default,Ralink RT28xx/3xxx PCI/SoC firmware)
 define Package/rt2800-pci-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware
 	$(INSTALL_DATA) \
@@ -15,16 +15,16 @@ define Package/rt2800-pci-firmware/install
 		$(PKG_BUILD_DIR)/rt3290.bin \
 		$(1)/lib/firmware
 endef
-$(eval $(call BuildPackage,rt2800-pci))
+$(eval $(call BuildPackage,rt2800-pci-firmware))
 
-Package/rt2800-usb = $(call Package/firmware-default,Ralink RT28xx/3xxx USB firmware)
+Package/rt2800-usb-firmware = $(call Package/firmware-default,Ralink RT28xx/3xxx USB firmware)
 define Package/rt2800-usb-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware
 	$(INSTALL_DATA) $(PKG_BUILD_DIR)/rt2870.bin $(1)/lib/firmware/
 endef
-$(eval $(call BuildPackage,rt2800-usb))
+$(eval $(call BuildPackage,rt2800-usb-firmware))
 
-Package/rt61-pci = $(call Package/firmware-default,Ralink RT2561 firmware)
+Package/rt61-pci-firmware = $(call Package/firmware-default,Ralink RT2561 firmware)
 define Package/rt61-pci-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware
 	$(INSTALL_DATA) \
@@ -33,11 +33,11 @@ define Package/rt61-pci-firmware/install
 		$(PKG_BUILD_DIR)/rt2661.bin \
 		$(1)/lib/firmware/
 endef
-$(eval $(call BuildPackage,rt61-pci))
+$(eval $(call BuildPackage,rt61-pci-firmware))
 
-Package/rt73-pci = $(call Package/firmware-default,Ralink RT2573 firmware)
+Package/rt73-pci-firmware = $(call Package/firmware-default,Ralink RT2573 firmware)
 define Package/rt73-usb-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware
 	$(INSTALL_DATA) $(PKG_BUILD_DIR)/rt73.bin $(1)/lib/firmware/
 endef
-$(eval $(call BuildPackage,rt73-pci))
+$(eval $(call BuildPackage,rt73-pci-firmware))
