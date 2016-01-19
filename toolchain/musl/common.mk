89ca7c5d0ed1da6fb4feb5973e5cb21d335dd36c
Felix Fietkau <nbd@openwrt.org>
2016-01-19 12:41:59 +0000

    toolchain/musl: enable debug information
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    SVN-Revision: 48363

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -37,7 +37,8 @@ MUSL_CONFIGURE:= \
 		--prefix=/ \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
-		--disable-gcc-wrapper
+		--disable-gcc-wrapper \
+		--enable-debug
 
 ifeq ($(CONFIG_MUSL_ENABLE_DEBUG),y)
 MUSL_CONFIGURE+= \
