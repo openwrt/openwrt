42eb04666165c474c083e0b19dce7732fb167e3a
Felix Fietkau <nbd@openwrt.org>
2016-02-29 20:02:34 +0000

    build: add support for choosing a different url filename part than the output file
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    
    Backport of r47591
    
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/chaos_calmer@48823 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- scripts/download.pl
+++ scripts/download.pl
@@ -11,14 +11,18 @@ use warnings;
 use File::Basename;
 use File::Copy;
 
-@ARGV > 2 or die "Syntax: $0 <target dir> <filename> <md5sum> [<mirror> ...]\n";
+@ARGV > 2 or die "Syntax: $0 <target dir> <filename> <md5sum> <url filename> [<mirror> ...]\n";
 
+my $url_filename;
 my $target = shift @ARGV;
 my $filename = shift @ARGV;
 my $md5sum = shift @ARGV;
+$url_filename = shift @ARGV unless $ARGV[0] =~ /:\/\//;
 my $scriptdir = dirname($0);
 my @mirrors;
 my $ok;
+
+$url_filename or $url_filename = $filename;
 
 sub localmirrors {
 	my @mlist;
@@ -106,7 +110,7 @@ sub download
 			return;
 		}
 	} else {
-		open WGET, "wget -t5 --timeout=20 --no-check-certificate $options -O- '$mirror/$filename' |" or die "Cannot launch wget.\n";
+		open WGET, "wget -t5 --timeout=20 --no-check-certificate $options -O- '$mirror/$url_filename' |" or die "Cannot launch wget.\n";
 		open MD5SUM, "| $md5cmd > '$target/$filename.md5sum'" or die "Cannot launch md5sum.\n";
 		open OUTPUT, "> $target/$filename.dl" or die "Cannot create file $target/$filename.dl: $!\n";
 		my $buffer;
