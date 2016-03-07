4ac99abeb3c6301f921ad844bd70b9b53516cee4
Felix Fietkau <nbd@openwrt.org>
2016-03-07 11:03:18 +0000

    lantiq: do not set default firmware file
    We are now shipping a DSL firmware, do not set it by default to some
    file name.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    SVN-Revision: 48941

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -335,15 +335,13 @@ ucidef_add_adsl_modem() {
 
 ucidef_add_vdsl_modem() {
 	local annex="$1"
-	local firmware="$2"
-	local tone="$3"
-	local xfer_mode="$4"
+	local tone="$2"
+	local xfer_mode="$3"
 
 	json_select_object dsl
 		json_select_object modem
 			json_add_string type "vdsl"
 			json_add_string annex "$annex"
-			json_add_string firmware "$firmware"
 			json_add_string tone "$tone"
 			json_add_string xfer_mode "$xfer_mode"
 		json_select ..
