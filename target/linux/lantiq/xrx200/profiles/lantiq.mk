4d7902b3a28051a5b2b612c2218236a553194772
Felix Fietkau <nbd@openwrt.org>
2016-03-07 11:03:21 +0000

    lantiq: add dsl firmware to default packages
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@48942 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/xrx200/profiles/lantiq.mk
+++ target/linux/lantiq/xrx200/profiles/lantiq.mk
@@ -1,13 +1,13 @@
 define Profile/EASY80920NOR
   NAME:=Lantiq VR9 - EASY80920NOR
-  PACKAGES:=kmod-ath9k wpad-mini kmod-usb-dwc2
+  PACKAGES:=kmod-ath9k wpad-mini kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 
 $(eval $(call Profile,EASY80920NOR))
 
 define Profile/EASY80920NAND
   NAME:=Lantiq VR9 - EASY80920NAND
-  PACKAGES:=kmod-ath9k wpad-mini kmod-usb-dwc2
+  PACKAGES:=kmod-ath9k wpad-mini kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 
 $(eval $(call Profile,EASY80920NAND))
