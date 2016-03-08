cd2114e19ceffb17b0f093718334148859f1adac
John Crispin <john@openwrt.org>
2016-03-08 18:11:49 +0000

    brcm2708: update linux 4.4 patches to latest version
    As usual these patches were extracted from the raspberry pi repo:
    https://github.com/raspberrypi/linux/tree/rpi-4.4.y
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    
    SVN-Revision: 48966

--- target/linux/brcm2708/modules.mk
+++ target/linux/brcm2708/modules.mk
@@ -289,6 +289,25 @@ endef
 
 $(eval $(call KernelPackage,spi-bcm2835))
 
+define KernelPackage/spi-bcm2835-aux
+  SUBMENU:=$(SPI_MENU)
+  TITLE:=BCM2835 Aux SPI controller driver
+  KCONFIG:=\
+    CONFIG_BCM2708_SPIDEV=n \
+    CONFIG_SPI=y \
+    CONFIG_SPI_BCM2835AUX \
+    CONFIG_SPI_MASTER=y
+  FILES:=$(LINUX_DIR)/drivers/spi/spi-bcm2835aux.ko
+  AUTOLOAD:=$(call AutoLoad,89,spi-bcm2835aux)
+  DEPENDS:=@TARGET_brcm2708
+endef
+
+define KernelPackage/spi-bcm2835-aux/description
+  This package contains the Broadcom 2835 Aux SPI master controller driver
+endef
+
+$(eval $(call KernelPackage,spi-bcm2835-aux))
+
 
 define KernelPackage/hwmon-bcm2835
   TITLE:=BCM2835 HWMON driver
