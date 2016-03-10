6a39f979354f562a597679e451d6dc9da6324458
John Crispin <blogic@openwrt.org>
2016-03-10 19:11:17 +0000

    procd: support pidfile writing.
    procd from revision b12bb150ed38a4409bef5127c77b060ee616b860 supports
    writing a pidfile.  This adds support for setting that parameter with
    standard init script hooks:
    
       procd_set_param pidfile /var/run/someprocess.pid
    
    Signed-off-by: Karl Palsson <karlp@etactica.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@48984 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -19,6 +19,7 @@
 #     netdev: bound network device (detects ifindex changes)
 #     limits: resource limits (passed to the process)
 #     user info: array with 1 values $username
+#     pidfile: file name to write pid into
 #
 #   No space separation is done for arrays/tables - use one function argument per command line argument
 #
@@ -195,7 +196,7 @@ _procd_set_param() {
 		nice)
 			json_add_int "$type" "$1"
 		;;
-		user|seccomp|capabilities)
+		pidfile|user|seccomp|capabilities)
 			json_add_string "$type" "$1"
 		;;
 		stdout|stderr|no_new_privs)
