46d73c210c2e136813a7ec685a18f4ec434b993a
David Antliff <david.antliff@imgtec.com>
2016-03-31 10:47:50 +1300

    Fix process exit code detection with TravisCI.
    Signed-off-by: David Antliff <david.antliff@imgtec.com>

---
+++ .travis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# marking duration in seconds
+MARK_DURATION=300
+
+# marking process
+do_mark () {
+    while true; do
+        sleep $MARK_DURATION
+        echo "MARK" >&2
+    done
+}
+
+# main process
+do_main () {
+    make -j 20
+    return $?
+}
+
+# start marking process
+do_mark &
+PID=$!
+
+# run main process
+do_main
+EXIT_CODE=$?
+
+# terminate marking process
+kill $PID
+
+# return main process error code
+exit $EXIT_CODE
