cfc45775e041f6bb8cf3cd96e08ef42dfbe3a042
Rafał Miłecki <zajec5@gmail.com>
2016-04-09 09:59:21 +0000

    bcm53xx: add basic support for Netgear R7900
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    
    git-svn-id: svn://svn.openwrt.org/openwrt/trunk@49122 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
+++ target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
@@ -28,6 +28,7 @@ platform_expected_image() {
 		"netgear,r6250v1")	echo "chk U12H245T00_NETGEAR"; return;;
 		"netgear,r6300v2")	echo "chk U12H240T00_NETGEAR"; return;;
 		"netgear,r7000")	echo "chk U12H270T00_NETGEAR"; return;;
+		"netgear,r7900")	echo "chk U12H315T30_NETGEAR"; return;;
 		"netgear,r8000")	echo "chk U12H315T00_NETGEAR"; return;;
 		"netgear,r8500")	echo "chk U12H334T00_NETGEAR"; return;;
 	esac
