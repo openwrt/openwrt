e68a90bf3a3b852ea54e623db9d6a48181cfedf7
Mayank Sirotiya <Mayank.Sirotiya@imgtec.com>
2016-04-21 12:44:10 +0530

    Adding sqlite3 download link to openwrt's default download mirrors
    sqlite3 comes with openwrt's packages, and its version needs to be
    bumped up to 3.12.2, as older versions have been removed from its
    downloading site. Hence adding another mirror until version gets
    updated in openwrt packages.

--- scripts/download.pl
+++ scripts/download.pl
@@ -226,6 +226,7 @@ foreach my $mirror (@ARGV) {
 #push @mirrors, 'http://mirror1.openwrt.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
+push @mirrors, 'http://ftp.osuosl.org/pub/blfs/conglomeration/sqlite';
 
 while (!$ok) {
 	my $mirror = shift @mirrors;
