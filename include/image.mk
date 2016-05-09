60fc6610bef5843e212e759423a49d80b6af40d3
Felix Fietkau <nbd@nbd.name>
2016-05-09 18:27:44 +0200

    image.mk: add support for limiting images to specific subtargets
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/image.mk
+++ include/image.mk
@@ -20,6 +20,7 @@ DTS_DIR:=$(LINUX_DIR)/arch/$(LINUX_KARCH)/boot/dts
 
 sanitize = $(call tolower,$(subst _,-,$(1)))
 
+SUBTARGET ?= default
 DIST_SANITIZED:=$(call sanitize,$(VERSION_DIST))
 EXTRA_NAME_SANITIZED=$(call sanitize,$(EXTRA_IMAGE_NAME))
 
@@ -438,6 +439,7 @@ endef
 
 define Device/Init
   PROFILES := $(PROFILE)
+  SUBTARGETS := $(SUBTARGET)
   DEVICE_NAME := $(1)
   DEVICE_TITLE :=
   DEVICE_PACKAGES :=
@@ -473,7 +475,7 @@ define Device/Export
 endef
 
 define Device/Check
-  _TARGET = $$(if $$(filter $(PROFILE),$$(PROFILES)),install,install-disabled)
+  _TARGET = $$(if $$(and $$(filter $(SUBTARGET),$$(SUBTARGETS)),$$(filter $(PROFILE),$$(PROFILES))),install,install-disabled)
   _COMPILE_TARGET = $$(if $(if $(IB),,$(CONFIG_IB)$$(filter $(PROFILE),$$(PROFILES))),compile,compile-disabled)
 endef
 
@@ -561,8 +563,10 @@ $(DEVICE_DESCRIPTION)
 
 endef
 
+DEVICE_PROFILE_CHECK=$(and $(DEVICE_TITLE),$(filter $(SUBTARGET),$(SUBTARGETS)))
+
 define Device/Dump
-$$(eval $$(if $$(DEVICE_TITLE),$$(info $$(call Device/DumpInfo,$(1)))))
+$$(eval $$(if $$(DEVICE_PROFILE_CHECK),$$(info $$(call Device/DumpInfo,$(1)))))
 endef
 
 define Device
