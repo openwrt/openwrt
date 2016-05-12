cc831e23e18c593be2629b77321c077def828bec
John Crispin <john@phrozen.org>
2016-05-12 15:52:55 +0200

    ar71xx: add proper support for Archer-C7 V2
    This has minor differences to the V1
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- target/linux/ar71xx/files/arch/mips/ath79/machtypes.h
+++ target/linux/ar71xx/files/arch/mips/ath79/machtypes.h
@@ -42,6 +42,7 @@ enum ath79_mach_type {
 	ATH79_MACH_AP96,		/* Atheros AP96 */
 	ATH79_MACH_ARCHER_C5,		/* TP-LINK Archer C5 board */
 	ATH79_MACH_ARCHER_C7,		/* TP-LINK Archer C7 board */
+	ATH79_MACH_ARCHER_C7_V2,	/* TP-LINK Archer C7 V2 board */
 	ATH79_MACH_AW_NR580,		/* AzureWave AW-NR580 */
 	ATH79_MACH_BHU_BXU2000N2_A1,	/* BHU BXU2000n-2 A1 */
 	ATH79_MACH_BSB,			/* Smart Electronics Black Swift board */
