111285e742d075956a149dce5a817fad3de2c850
Felix Fietkau <nbd@nbd.name>
2016-05-12 23:00:40 +0200

    download.pl: use http://sources.lede-project.org as download mirror
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- scripts/download.pl
+++ scripts/download.pl
@@ -225,6 +225,7 @@ foreach my $mirror (@ARGV) {
 }
 
 #push @mirrors, 'http://mirror1.openwrt.org';
+push @mirrors, 'http://sources.lede-project.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
 
