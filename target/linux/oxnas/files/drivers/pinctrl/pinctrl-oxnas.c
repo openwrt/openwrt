97ce23c3477b856faae17d7bfebb6a4bbb35030b
Daniel Golle <daniel@makrotopia.org>
2016-05-20 05:39:04 +0200

    oxnas: drop compatibility with old kernels from pinctrl
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/oxnas/files/drivers/pinctrl/pinctrl-oxnas.c
+++ target/linux/oxnas/files/drivers/pinctrl/pinctrl-oxnas.c
@@ -27,7 +27,6 @@
 /* Since we request GPIOs from ourself */
 #include <linux/pinctrl/consumer.h>
 #include <linux/spinlock.h>
-#include <linux/version.h>
 
 #include "core.h"
 
@@ -1200,11 +1199,7 @@ static struct irq_chip gpio_irqchip = {
 	.irq_set_type	= gpio_irq_type,
 };
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,2,0)
-static void gpio_irq_handler(unsigned irq, struct irq_desc *desc)
-#else
 static void gpio_irq_handler(struct irq_desc *desc)
-#endif
 {
 	struct irq_chip *chip = irq_desc_get_chip(desc);
 	struct irq_data *idata = irq_desc_get_irq_data(desc);
@@ -1245,9 +1240,6 @@ static int oxnas_gpio_irq_map(struct irq_domain *h, unsigned int virq,
 	irq_set_lockdep_class(virq, &gpio_lock_class);
 
 	irq_set_chip_and_handler(virq, &gpio_irqchip, handle_edge_irq);
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,2,0)
-	set_irq_flags(virq, IRQF_VALID);
-#endif
 	irq_set_chip_data(virq, oxnas_gpio);
 
 	return 0;
