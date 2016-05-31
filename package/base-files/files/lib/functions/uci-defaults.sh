07f03d08335aca0914059425a29d708e5490846e
Jo-Philipp Wich <jo@mein.io>
2016-05-31 14:35:24 +0200

    base-files: support port_state LED types in board.d
    Add support for handling port_state LEDs as used by ADM5120.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -432,6 +432,25 @@ ucidef_set_led_switch() {
 	json_select ..
 }
 
+ucidef_set_led_portstate() {
+	local cfg="led_$1"
+	local name="$2"
+	local sysfs="$3"
+	local port_state="$4"
+
+	json_select_object led
+
+	json_select_object "$1"
+	json_add_string name "$name"
+	json_add_string type portstate
+	json_add_string sysfs "$sysfs"
+	json_add_string trigger port_state
+	json_add_string port_state "$port_state"
+	json_select ..
+
+	json_select ..
+}
+
 ucidef_set_led_default() {
 	local cfg="led_$1"
 	local name="$2"
