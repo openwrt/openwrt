1001b5d77c5dd7adc7bb2fcce8778364b88e5ea0
Jonas Gorski <jonas.gorski@gmail.com>
2016-06-05 20:45:25 +0200

    scripts/getver.sh: allow conversion between git hash and revision
    Add code allowing easy conversion between git commit ids and revisions.
    
    Example:
    
    $ ./scripts/getver.sh
    r792
    $ ./scripts/getver.sh r123
    b7fc892eb56b52d25bc00578af9d1ee058dcf6b3
    $ ./scrpts/getver.sh b7fc892eb56b52d25bc00578af9d1ee058dcf6b3
    r123
    
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -3,6 +3,8 @@ export LANG=C
 export LC_ALL=C
 [ -n "$TOPDIR" ] && cd $TOPDIR
 
+GET_REV=$1
+
 try_version() {
 	[ -f version ] || return 1
 	REV="$(cat version)"
@@ -11,8 +13,22 @@ try_version() {
 
 try_git() {
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
-	REV="$(git rev-list reboot..HEAD --count)"
-	REV="${REV:+r$REV}"
+
+	[ -n "$GET_REV" ] || GET_REV="HEAD"
+
+	case "$GET_REV" in
+	r*)
+		GET_REV="$(echo $GET_REV | tr -d 'r')"
+		BASE_REV="$(git rev-list reboot..HEAD --count)"
+		REV="$(git rev-parse HEAD~$((BASE_REV - GET_REV)))"
+		;;
+	*)
+
+		REV="$(git rev-list reboot..$GET_REV --count)"
+		REV="${REV:+r$REV}"
+		;;
+	esac
+
 	[ -n "$REV" ]
 }
 
