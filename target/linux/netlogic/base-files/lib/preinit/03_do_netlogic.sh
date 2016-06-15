137b1ac5e8df6110a9ecea8a505ff61b64203ced
John Crispin <john@phrozen.org>
2016-06-15 20:28:21 +0200

    netlogic: R.I.P
    I asked florian and he is ok with this. i think no one apart from him has
    access to these boards.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- target/linux/netlogic/base-files/lib/preinit/03_do_netlogic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-do_netlogic() {
-	. /lib/netlogic.sh
-
-	netlogic_board_detect
-}
-
-boot_hook_add preinit_main do_netlogic
--- target/linux/netlogic/xlr/target.mk
+++
@@ -1,7 +0,0 @@
-CPU_TYPE:=xlr
-CPU_FLAGS_xlr:=-march=xlr -mabi=64
-BOARDNAME:=xlr
-
-define Target/Description
-	Build firmware images for Broadcom/Netlogic XLR boards.
-endef
