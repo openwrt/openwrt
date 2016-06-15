2e360bb7af770aa03e05f66fe63b5f1742b2d0ef
Mayank Sirotiya <Mayank.Sirotiya@imgtec.com>
2016-06-15 13:37:12 +0530

    pistachio: add kernel config and patches for 4.4
    Signed-off-by: Mayank Sirotiya <Mayank.Sirotiya@imgtec.com>
    Signed-off-by: Shraddha Chaudhari <Shraddha.Chaudhari@imgtec.com>

--- target/linux/pistachio/modules.mk
+++ target/linux/pistachio/modules.mk
@@ -7,7 +7,8 @@ define KernelPackage/serial-sc16is7xx
     CONFIG_SPI_MASTER=y \
     CONFIG_SERIAL_SC16IS7XX \
     CONFIG_SERIAL_SC16IS7XX_CORE \
-    CONFIG_SERIAL_SC16IS7XX_SPI=y
+    CONFIG_SERIAL_SC16IS7XX_SPI=y \
+    CONFIG_SERIAL_SC16IS7XX_I2C=n
   FILES:= $(LINUX_DIR)/drivers/tty/serial/sc16is7xx.ko
   AUTOLOAD:= $(call AutoProbe, sc16is7xx.ko)
   DEPENDS:=@TARGET_pistachio_marduk
@@ -20,12 +21,17 @@ $(eval $(call KernelPackage,serial-sc16is7xx))
 define KernelPackage/sound-pistachio-soc
   TITLE:=SoC Audio support for the Pistachio SOC
   KCONFIG:= \
-    CONFIG_SND_SOC_IMG=y \
     CONFIG_SND_MIPS=y \
     CONFIG_SND_SOC_IMG=y \
-    CONFIG_SND_SOC_IMG_PISTACHIO \
+    CONFIG_SND_SOC_IMG_I2S_IN \
+    CONFIG_SND_SOC_IMG_I2S_OUT \
     CONFIG_SND_SOC_IMG_PARALLEL_OUT \
-    CONFIG_SND_SOC_IMG_PISTACHIO_EVENT_TIMER \
+    CONFIG_SND_SOC_IMG_SPDIF_IN \
+    CONFIG_SND_SOC_IMG_SPDIF_OUT \
+    CONFIG_SND_SOC_IMG_PISTACHIO_INTERNAL_DAC \
+    CONFIG_SND_SOC_IMG_PISTACHIO_BUB \
+    CONFIG_SND_SOC_IMG_PISTACHIO \
+    CONFIG_SND_SOC_IMG_PISTACHIO_EVENT_TIMER=y \
     CONFIG_SND_SOC_IMG_PISTACHIO_EVENT_TIMER_ATU=y
   FILES:= \
     $(LINUX_DIR)/sound/soc/codecs/snd-soc-tpa6130a2.ko \
@@ -34,10 +40,9 @@ define KernelPackage/sound-pistachio-soc
     $(LINUX_DIR)/sound/soc/img/img-parallel-out.ko \
     $(LINUX_DIR)/sound/soc/img/img-spdif-in.ko \
     $(LINUX_DIR)/sound/soc/img/img-spdif-out.ko \
-    $(LINUX_DIR)/sound/soc/img/pistachio-event-timer-module.ko \
     $(LINUX_DIR)/sound/soc/img/pistachio-internal-dac.ko \
     $(LINUX_DIR)/sound/soc/img/pistachio.ko
-  AUTOLOAD:=$(call AutoProbe,img-i2s-in.ko img-i2s-out.ko img-parallel-out.ko img-spdif-in.ko img-spdif-out.ko pistachio-event-timer-module.ko pistachio-internal-dac.ko snd-soc-tpa6130a2.ko pistachio.ko)
+  AUTOLOAD:=$(call AutoProbe,img-i2s-in.ko img-i2s-out.ko img-parallel-out.ko img-spdif-in.ko img-spdif-out.ko pistachio-internal-dac.ko snd-soc-tpa6130a2.ko pistachio.ko)
   DEPENDS:=@TARGET_pistachio_marduk +kmod-sound-soc-core +kmod-serial-sc16is7xx
   $(call AddDepends/sound)
 endef
