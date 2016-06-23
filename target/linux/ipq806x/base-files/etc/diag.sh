880fddf86bfb0bb2452a33da6622d9d29c71d9bd
Zoltan HERPAI <wigyori@uid0.hu>
2016-06-23 18:51:35 +0200

    ipq806x: update bomb
    ipq806x: Add support for linux-4.4
    ipq806x: Add Archer C2600 to image/Makefile
    ipq806x/base-files: Add Archer C2600 LEDs and board
    ipq806x/base-files: Add support for Archer C2600
    ipq806x/base-files: extract ath10k caldata
    ipq806x/dts: Add Archer C2600 DTS
    ipq806x: enable PM support
    ipq806x: add a default profile
    ipq806x: add diag.sh script
    ipq806x: qcom rpm fix support for smb208
    mtd: add linksys_bootcount for ipq806x
    uboot-envtools: add ipq806x support
    
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>

---
+++ target/linux/ipq806x/base-files/etc/diag.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. /lib/functions/leds.sh
+. /lib/ipq806x.sh
+
+get_status_led() {
+	case $(ipq806x_board_name) in
+	c2600)
+		status_led="status:blue"
+		;;
+	esac
+}
+
+set_state() {
+	get_status_led
+
+	case "$1" in
+	preinit)
+		status_led_blink_preinit
+		;;
+	failsafe)
+		status_led_blink_failsafe
+		;;
+	preinit_regular)
+		status_led_blink_preinit_regular
+		;;
+	done)
+		status_led_on
+		;;
+	esac
+}
