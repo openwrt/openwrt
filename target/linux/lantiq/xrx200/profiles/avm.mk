6dc517dd7d8867813469141f0b9fbbed512b8315
John Crispin <john@phrozen.org>
2016-06-24 16:27:25 +0200

    lantiq: Add Support for Fritz!Box 7360 SL
    Signed-off-by: Sebastian Ortwein <krone@animeland.de>
    Tested-by: Guido Lipke <lipkegu@gmail.com>

--- target/linux/lantiq/xrx200/profiles/avm.mk
+++ target/linux/lantiq/xrx200/profiles/avm.mk
@@ -4,3 +4,10 @@ define Profile/FRITZ3370
 endef
 
 $(eval $(call Profile,FRITZ3370))
+
+define Profile/FRITZ7360SL
+  NAME:=1&1 HomeServer - FRITZ7360SL
+  PACKAGES:=kmod-ath9k wpad-mini kmod-usb-dwc2
+endef
+
+$(eval $(call Profile,FRITZ7360SL))
