35228f63b33c9929eb762ba37fb40a382689b983
Álvaro Fernández Rojas <noltari@gmail.com>
2016-06-27 15:52:10 +0200

    brcm63xx: move profiles definitions to image/Makefile
    This switches brcm63xx to the new profile definition system in image/Makefile
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/brcm63xx/profiles/default.mk
+++ target/linux/brcm63xx/profiles/default.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 OpenWrt.org
+# Copyright (C) 2016 LEDE project
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -8,8 +8,11 @@
 define Profile/Default
   NAME:=Default Profile
   PACKAGES:=kmod-b43 wpad-mini
+  PRIORITY:=1
 endef
+
 define Profile/Default/description
   Package set compatible with most boards.
 endef
+
 $(eval $(call Profile,Default))
