608058804fb6ee4d962e312336bf3d848b8c38d2
Abhijit Mahajani <Abhijit.Mahajani@imgtec.com>
2016-08-03 20:00:08 +0530

    Add luci, tar, letmecreate in default packages
    Change kmod-i2c to kmod-i2c-core since it gives error in imagebuilder
    
    Signed-off-by: Abhijit Mahajani <Abhijit.Mahajani@imgtec.com>

--- target/linux/pistachio/marduk/target.mk
+++ target/linux/pistachio/marduk/target.mk
@@ -25,10 +25,11 @@
 
 BOARDNAME:=marduk
 
-DEFAULT_PACKAGES+=kmod-leds-gpio kmod-ledtrig-heartbeat kmod-i2c i2c-tools \
+DEFAULT_PACKAGES+=kmod-leds-gpio kmod-ledtrig-heartbeat kmod-i2c-core i2c-tools \
                   kmod-sound-pistachio-soc alsa-lib alsa-utils alsa-utils-tests \
-                  kmod-uccp420wlan kmod-cfg80211 fping iperf3 iw hostapd wpa-cli wpa-supplicant \
-                  uhttpd uboot-envtools tcpdump board-test proddata
+                  kmod-uccp420wlan kmod-cfg80211 fping iw hostapd wpa-cli wpa-supplicant \
+                  uhttpd uboot-envtools tcpdump board-test proddata \
+                  luci tar letmecreate
 
 define Profile/marduk/default
 	$(1)_DEVICE_DTS:=$(2)
