2180b715c1dcef30cb8e734c42b7e24f5f6555e5
Felix Fietkau <nbd@nbd.name>
2016-08-04 11:12:27 +0200

    image: fix per-device rootfs build error when not all opkg package files are found
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/image.mk
+++ include/image.mk
@@ -294,7 +294,7 @@ target-dir-%: FORCE
 	$(CP) $(TARGET_DIR) $(mkfs_cur_target_dir)
 	mv $(mkfs_cur_target_dir)/etc/opkg $(mkfs_cur_target_dir).opkg
 	echo 'src default file://$(PACKAGE_DIR_ALL)' > $(mkfs_cur_target_dir).conf
-	$(if $(mkfs_packages_add), \
+	$(if $(call opkg_package_files,$(mkfs_packages_add)), \
 		$(opkg_target) update && \
 		$(opkg_target) install \
 			$(call opkg_package_files,$(mkfs_packages_add)))
