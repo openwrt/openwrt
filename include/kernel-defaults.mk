57ef81d78f590c86aaebeb41c14528b33b73db94
Josua Mayer <josua.mayer97@gmail.com>
2016-08-17 16:18:04 +0200

    kernel: prevent adding custom string to localversion
    When the kernel build picks up a localversion file in the source tree,
    that string is unconditionally appended to LOCALVERSION and affects the
    uname string.
    Make sure to delete any such file.
    
    Signed-off-by: Josua Mayer <josua.mayer97@gmail.com>

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -111,6 +111,7 @@ define Kernel/SetNoInitramfs
 endef
 
 define Kernel/Configure/Default
+	rm -f $(LINUX_DIR)/localversion
 	$(LINUX_CONF_CMD) > $(LINUX_DIR)/.config.target
 # copy CONFIG_KERNEL_* settings over to .config.target
 	awk '/^(#[[:space:]]+)?CONFIG_KERNEL/{sub("CONFIG_KERNEL_","CONFIG_");print}' $(TOPDIR)/.config >> $(LINUX_DIR)/.config.target
