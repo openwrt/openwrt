334fdea08d71079d6e0f5bc8dc79790c0402ae2a
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2016-08-19 13:58:36 +0300

    archs38: Merge sd and ramfs subtargets in generic again
    As it turned out we may actually build both initramfs and
    SD-based images in one run with just a small tweak in
    existing image/Makefile and so we do here.
    
    We're back with one "generic" target for all ARC HS38-based bords.
    
    Kudos to John for his patience and very helpful tips!
    
    Signed-off-by: John Crispin <john@phrozen.org>
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Jo-Philipp Wich <jo@mein.io>

--- target/linux/archs38/ramdisk/target.mk
+++
@@ -1,8 +0,0 @@
-BOARDNAME:=Ramdisk
-FEATURES += ramdisk
-
-define Target/Description
-	Build firmware images for generic ARC HS38 based boards that use built-in initramfs
-endef
-
-
--- target/linux/archs38/sd/target.mk
+++
@@ -1,8 +0,0 @@
-BOARDNAME:=SD
-FEATURES += ext4 usb
-
-define Target/Description
-	Build firmware images for ARC HS38 based boards that use SD-card for booting.
-endef
-
-
