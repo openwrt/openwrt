9c69ba83e2d0c19959fed81cf4fa038fde3e518d
Daniel Golle <daniel@makrotopia.org>
2016-09-04 03:34:19 +0200

    oxnas: add method to extract mac_adr from legacy cmdline
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/oxnas/base-files/lib/oxnas.sh
+++ target/linux/oxnas/base-files/lib/oxnas.sh
@@ -6,6 +6,30 @@
 OXNAS_BOARD_NAME=
 OXNAS_MODEL=
 
+bootloader_cmdline_var() {
+	local param
+	local pval
+	for arg in $(cat /proc/device-tree/chosen/bootloader-args); do
+		param="$(echo $arg | cut -d'=' -f 1)"
+		pval="$(echo $arg | cut -d'=' -f 2-)"
+
+		if [ "$param" = "$1" ]; then
+			echo "$pval"
+		fi
+	done
+}
+
+legacy_boot_mac_adr() {
+	local macstr
+	local oIFS
+	macstr="$(bootloader_cmdline_var mac_adr)"
+	oIFS="$IFS"
+	IFS=","
+	set -- $macstr
+	printf "%02x:%02x:%02x:%02x:%02x:%02x" $1 $2 $3 $4 $5 $6
+	IFS="$oIFS"
+}
+
 oxnas_board_detect() {
 	local machine
 	local name
