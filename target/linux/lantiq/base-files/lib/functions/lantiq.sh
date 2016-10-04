e07c80dfc881daed67c495d99885cc045f70d89a
Mathias Kresin <dev@kresin.me>
2016-10-04 20:34:03 +0200

    lantiq: use aliases device tree node for leds
    The chosen node is clearly the wrong place for aliases.
    
    Signed-off-by: Mathias Kresin <dev@kresin.me>

--- target/linux/lantiq/base-files/lib/functions/lantiq.sh
+++ target/linux/lantiq/base-files/lib/functions/lantiq.sh
@@ -32,7 +32,7 @@ lantiq_get_dt_led() {
 	local label
 	local ledpath
 	local basepath="/sys/firmware/devicetree/base"
-	local nodepath="$basepath/chosen/leds/$1"
+	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")
 	[ -n "$ledpath" ] && label=$(cat "$basepath$ledpath/label")
