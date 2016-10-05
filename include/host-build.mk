4ada2fd2768cba671c3289ce9eb8f5c9c728da1f
Matthias Schiffer <mschiffer@universe-factory.net>
2016-10-06 00:13:33 +0200

    include/host-build.mk: fix ACLOCAL_INCLUDE
    It seems the intention was to add both $(STAGING_DIR_HOST)/... and
    $(STAGING_DIR)/host/... instead of passing $(STAGING_DIR_HOST) twice. This
    makes the definition match HOST_CPPFLAGS and HOST_LDFLAGS.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- include/host-build.mk
+++ include/host-build.mk
@@ -133,7 +133,7 @@ define Download/default
 endef
 
 define Host/Exports/Default
-  $(1) : export ACLOCAL_INCLUDE=$$(foreach p,$$(wildcard $$(STAGING_DIR_HOST)/share/aclocal $$(STAGING_DIR_HOST)/share/aclocal-* $(if $(IS_PACKAGE_BUILD),$$(STAGING_DIR_HOST)/share/aclocal $$(STAGING_DIR_HOST)/share/aclocal-*)),-I $$(p))
+  $(1) : export ACLOCAL_INCLUDE=$$(foreach p,$$(wildcard $$(STAGING_DIR_HOST)/share/aclocal $$(STAGING_DIR_HOST)/share/aclocal-* $(if $(IS_PACKAGE_BUILD),$$(STAGING_DIR)/host/share/aclocal $$(STAGING_DIR)/host/share/aclocal-*)),-I $$(p))
   $(1) : export STAGING_PREFIX=$$(HOST_BUILD_PREFIX)
   $(1) : export PKG_CONFIG_PATH=$$(STAGING_DIR_HOST)/lib/pkgconfig:$$(HOST_BUILD_PREFIX)/lib/pkgconfig
   $(1) : export PKG_CONFIG_LIBDIR=$$(HOST_BUILD_PREFIX)/lib/pkgconfig
