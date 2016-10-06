7d7858f6f1b95f1f11e02bb0e6e386f4fa93bcf5
Matthias Schiffer <mschiffer@universe-factory.net>
2016-10-06 03:07:04 +0200

    include/host-build.mk: pass HOST_BUILD_PREFIX to Host/install
    Makes it more consistent with package builds.
    
    Based-on-patch-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- include/host-build.mk
+++ include/host-build.mk
@@ -111,7 +111,7 @@ define Host/Install/Default
 endef
 
 define Host/Install
-  $(call Host/Install/Default)
+  $(call Host/Install/Default,$(HOST_BUILD_PREFIX))
 endef
 
 
@@ -173,7 +173,7 @@ ifndef DUMP
 		touch $$@
 
   $(HOST_STAMP_INSTALLED): $(HOST_STAMP_BUILT) $(if $(FORCE_HOST_INSTALL),FORCE)
-		$(call Host/Install)
+		$(call Host/Install,$(HOST_BUILD_PREFIX))
 		$(foreach hook,$(Hooks/HostInstall/Post),$(call $(hook))$(sep))
 		mkdir -p $$(shell dirname $$@)
 		touch $(HOST_STAMP_BUILT)
