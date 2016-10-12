336e277e8b46394d4b5a6747d3d5e93be383a923
Dirk Neukirchen <dirkneukirchen@web.de>
2016-10-12 16:14:44 +0200

    autotools: use correct version for gettext FIXUP
    0.19.8.1 gettext-full uses "archive-version" of 0.19.8
    to replace makros, leading to breakage of PKG_FIXUP:=gettext-version
    
    Signed-off-by: Dirk Neukirchen <dirkneukirchen@web.de>

--- include/autotools.mk
+++ include/autotools.mk
@@ -85,7 +85,7 @@ endef
 
 define gettext_version_target
 	(cd $(PKG_BUILD_DIR) && \
-		GETTEXT_VERSION=$(shell $(STAGING_DIR)/host/bin/gettext -V | $(STAGING_DIR_HOST)/bin/sed -ne '1s/.* //p') && \
+		GETTEXT_VERSION=$(shell $(STAGING_DIR)/host/bin/gettext -V | $(STAGING_DIR_HOST)/bin/sed -ne '1s/.*\([0-9]\.[0-9]\{2\}\.[0-9]\).*/\1/p' ) && \
 		$(STAGING_DIR_HOST)/bin/sed \
 			-i $(PKG_BUILD_DIR)/configure.ac \
 			-e "s/AM_GNU_GETTEXT_VERSION(.*)/AM_GNU_GETTEXT_VERSION(\[$$$$GETTEXT_VERSION\])/g" && \
