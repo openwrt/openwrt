23e46f1c2ae2bbeb874ce4990cbfeb4cf39a747a
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2016-10-14 16:19:24 +0200

    kernel: kmod-crypto-iv dropped in Linux 4.8
    Commit 3a01d0ee2b991c8c267620e63a4ab47cd8c30cc4 upstream
    
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- package/kernel/linux/modules/crypto.mk
+++ package/kernel/linux/modules/crypto.mk
@@ -125,8 +125,8 @@ define KernelPackage/crypto-iv
   DEPENDS:=+kmod-crypto-manager +kmod-crypto-rng +kmod-crypto-wq
   KCONFIG:= CONFIG_CRYPTO_BLKCIPHER2
   FILES:= \
-	$(LINUX_DIR)/crypto/eseqiv.ko \
-	$(LINUX_DIR)/crypto/chainiv.ko
+	$(LINUX_DIR)/crypto/eseqiv.ko@lt4.8 \
+	$(LINUX_DIR)/crypto/chainiv.ko@lt4.8
   AUTOLOAD:=$(call AutoLoad,10,eseqiv chainiv)
   $(call AddDepends/crypto)
 endef
