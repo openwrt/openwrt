2beab73fad3d4e7889f05e696ba4f033981293fa
Imre Kaloz <kaloz@openwrt.org>
2016-10-14 13:50:41 +0200

    mvebu: add missing status LEDs for Linksys WRT1200AC and WRT1900ACv2
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>

--- target/linux/mvebu/base-files/etc/diag.sh
+++ target/linux/mvebu/base-files/etc/diag.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-# Copyright (C) 2014 OpenWrt.org
+# Copyright (C) 2014-2016 OpenWrt.org
+# Copyright (C) 2016 LEDE-Project.org
 
 . /lib/functions/leds.sh
 . /lib/mvebu.sh
 
 get_status_led() {
 	case $(mvebu_board_name) in
+	armada-385-linksys-caiman)
+		status_led="caiman:white:power"
+		;;
+	armada-385-linksys-cobra)
+		status_led="cobra:white:power"
+		;;
 	armada-385-linksys-shelby)
 		status_led="shelby:white:power"
 		;;
