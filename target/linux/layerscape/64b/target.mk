c6c731fe311f7da42777ffd31804a4f6aa3f8e19
Yutang Jiang <yutang.jiang@nxp.com>
2016-10-29 00:14:32 +0800

    layerscape: add 64b/32b target for ls1043ardb device
    Add support for NXP layerscape ls1043ardb 64b/32b Dev board.
    
    LS1043a is an SoC with 4x64-bit up to 1.6 GHz ARMv8 A53 cores.
    ls1043ardb support features as: 2GB DDR4, 128MB NOR/512MB NAND, USB3.0, eSDHC,
    I2C, GPIO, PCIe/Mini-PCIe, 6x1G/1x10G network port, etc.
    
    64b/32b ls1043ardb target is using 4.4 kernel, and rcw/u-boot/fman images from
    NXP QorIQ SDK release.
    
    All of 4.4 kernel patches porting from SDK release or upstream.
    
    QorIQ SDK ISOs can be downloaded from this location:
    http://www.nxp.com/products/software-and-tools/run-time-software/linux-sdk/linux-sdk-for-qoriq-processors:SDKLINUX
    
    Signed-off-by: Yutang Jiang <yutang.jiang@nxp.com>

---
+++ target/linux/layerscape/64b/target.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2016 Jiang Yutang <jiangyutang1978@gmail.com>
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ARCH:=aarch64
+BOARDNAME:=layerscape 64b boards
+CFLAGS:=-Os -pipe -fno-caller-saves
+
+define Target/Description
+	Build firmware images for $(BOARDNAME) SoC devices.
+endef
+
