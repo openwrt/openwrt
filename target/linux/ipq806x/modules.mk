70434c3f948749690de24effbfd98f7a64e0991f
Pavel Kubelun <be.dissent@gmail.com>
2016-11-12 14:35:56 +0300

    ipq806x: switch to upstream usb driver and backport fixes
    Also removing fifo-resize property drom DT as it has been removed from the driver.
    
    Signed-off-by: Pavel Kubelun <be.dissent@gmail.com>

--- target/linux/ipq806x/modules.mk
+++ target/linux/ipq806x/modules.mk
@@ -29,20 +29,3 @@ define KernelPackage/usb-phy-qcom-dwc3/description
 endef
 
 $(eval $(call KernelPackage,usb-phy-qcom-dwc3))
-
-
-define KernelPackage/usb-dwc3-qcom
-  TITLE:=DWC3 USB QCOM controller driver
-  DEPENDS:=@TARGET_ipq806x +kmod-usb-dwc3 +kmod-usb-phy-qcom-dwc3
-  KCONFIG:= CONFIG_USB_DWC3_QCOM
-  FILES:= $(LINUX_DIR)/drivers/usb/dwc3/dwc3-qcom.ko
-  AUTOLOAD:=$(call AutoLoad,53,dwc3-qcom,1)
-  $(call AddDepends/usb)
-endef
-
-define KernelPackage/usb-dwc3-qcom/description
- This driver provides support for the integrated DesignWare
- USB3 IP Core within the QCOM SoCs.
-endef
-
-$(eval $(call KernelPackage,usb-dwc3-qcom))
