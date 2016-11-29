62e4c915ee867b5c3610fcc150d3a6bf90cbb130
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2016-11-29 12:53:13 -0500

    mvebu: fix sysupgrade for Linksys WRT3200ACM
    sysupgrade command fails due to missing U-Boot environment-processing
    binaries on sysupgrade ramdisk. The missing binaries result in the
    following output:
    
    Switching to ramdisk...
    	Performing system upgrade...
    	ash: /usr/sbin/fw_printenv: not found
    	ash: fw_setenv: not found
    	ash: touch: not found
    	cannot find target partition
    
    Signed-off-by: Vignesh Balasubramaniam <vigneshb.hp@gmail.com>

--- target/linux/mvebu/base-files/lib/upgrade/linksys.sh
+++ target/linux/mvebu/base-files/lib/upgrade/linksys.sh
@@ -77,7 +77,7 @@ linksys_preupgrade() {
 	local board=$(mvebu_board_name)
 
 	case "$board" in
-	armada-385-linksys-caiman|armada-385-linksys-cobra|armada-385-linksys-shelby|armada-xp-linksys-mamba)
+	armada-385-linksys-caiman|armada-385-linksys-cobra|armada-385-linksys-shelby|armada-xp-linksys-mamba|armada-385-linksys-rango)
 		export RAMFS_COPY_BIN="${RAMFS_COPY_BIN} /usr/sbin/fw_printenv /usr/sbin/fw_setenv"
 		export RAMFS_COPY_BIN="${RAMFS_COPY_BIN} /bin/mkdir /bin/touch"
 		export RAMFS_COPY_DATA="${RAMFS_COPY_DATA} /etc/fw_env.config /var/lock/fw_printenv.lock"
