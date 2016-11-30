0d5097036e527b3f15680f4736ee393570703315
Florian Fainelli <f.fainelli@gmail.com>
2016-11-29 17:10:02 -0800

    orion: Set appropriate DEVICE_TYPE for harddisk target
    The harddisk target is for NAS boxes, so set the DEVICE_TYPE accordingly so we
    get a sensible default package selection.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- target/linux/orion/harddisk/target.mk
+++ target/linux/orion/harddisk/target.mk
@@ -7,6 +7,7 @@
 
 BOARDNAME:=Internal Hard-Disk
 FEATURES+=targz
+DEVICE_TYPE:=nas
 
 define Target/Description
 	Build firmware images for Marvell Orion based boards that boot directly from internal disk storage.
