ad907e1c03a3a1a236f4836cee085d085f280c8d
Yutang Jiang <yutang.jiang@nxp.com>
2016-12-08 00:07:42 +0800

    layerscape: add 64b/32b target for ls1046ardb device
    Add support for NXP layerscape ls1046ardb 64b/32b Dev board.
    
    LS1046ARDB Specification:
    -------------------------
    Memory subsystem:
    * 8GByte DDR4 SDRAM (64bit bus)
    * 512 Mbyte NAND flash
    * Two 64 Mbyte high-speed SPI flash
    * SD connector to interface with the SD memory card
    * On-board 4G eMMC
    Ethernet:
    * Two XFI 10G ports
    * Two SGMII ports
    * Two RGMII ports
    PCIe:
    * PCIe1 (SerDes2 Lane0) to miniPCIe slot
    * PCIe2 (SerDes2 Lane1) to x2 PCIe slot
    * PCIe3 (SerDes2 Lane2) to x4 PCIe slot
    
    * USB 3.0: one super speed USB 3.0 type A port, one Micro-AB port
    * UART: supports two UARTs up to 115200 bps for console
    
    Signed-off-by: Yutang Jiang <yutang.jiang@nxp.com>

--- target/linux/layerscape/64b/profiles/00-default.mk
+++ target/linux/layerscape/64b/profiles/00-default.mk
@@ -10,6 +10,8 @@ endef
 DEFAULT_PACKAGES+= \
 	rcw-layerscape-ls1043ardb uboot-layerscape-$(SUBTARGET)-ls1043ardb \
 	fman-layerscape-ls1043ardb \
+	rcw-layerscape-ls1046ardb uboot-layerscape-$(SUBTARGET)-ls1046ardb \
+	fman-layerscape-ls1046ardb \
 	rcw-layerscape-ls1012ardb uboot-layerscape-$(SUBTARGET)-ls1012ardb \
 	kmod-ppfe ppfe-ls1012ardb
 
