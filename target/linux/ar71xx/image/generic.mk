70a6bbd53d7fad461dfed305ad9f338b40702841
FUKAUMI Naoki <naobsd@gmail.com>
2016-12-09 17:53:15 +0900

    ar71xx: prepare jffs2 partition properly in factory.bin for BHR-4GRV2
    "pad-rootfs" is required just after "append-rootfs".
    
    Signed-off-by: FUKAUMI Naoki <naobsd@gmail.com>

--- target/linux/ar71xx/image/generic.mk
+++ target/linux/ar71xx/image/generic.mk
@@ -722,6 +722,6 @@ define Device/bhr-4grv2
   MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env)ro,14528k(rootfs),1472k(kernel),64k(art)ro,16000k@0x50000(firmware)
   IMAGES := sysupgrade.bin factory.bin
   IMAGE/sysupgrade.bin = append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_SIZE) | append-kernel | check-size $$$$(IMAGE_SIZE)
-  IMAGE/factory.bin = append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | mkbuffaloimg
+  IMAGE/factory.bin = append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | mkbuffaloimg
 endef
 TARGET_DEVICES += bhr-4grv2
