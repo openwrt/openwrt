34c2b3de6602205c625e9d16e40ef826fcf693d2
Felix Fietkau <nbd@nbd.name>
2016-12-13 15:08:54 +0100

    ipkg-make-index.sh: drop md5sum from package index
    We have switched opkg to sha256 a long time ago, and shrinking package
    lists is useful for systems that are running low on RAM
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- scripts/ipkg-make-index.sh
+++ scripts/ipkg-make-index.sh
@@ -8,7 +8,6 @@ if [ -z $pkg_dir ] || [ ! -d $pkg_dir ]; then
 	exit 1
 fi
 
-which md5sum >/dev/null 2>&1 || alias md5sum=md5
 empty=1
 
 for pkg in `find $pkg_dir -name '*.ipk' | sort`; do
@@ -19,13 +18,11 @@ for pkg in `find $pkg_dir -name '*.ipk' | sort`; do
 	[[ "$name" = "libc" ]] && continue
 	echo "Generating index for package $pkg" >&2
 	file_size=$(ls -l $pkg | awk '{print $5}')
-	md5sum=$(md5sum $pkg | awk '{print $1}')
 	sha256sum=$(openssl dgst -sha256 $pkg | awk '{print $2}')
 	# Take pains to make variable value sed-safe
 	sed_safe_pkg=`echo $pkg | sed -e 's/^\.\///g' -e 's/\\//\\\\\\//g'`
 	tar -xzOf $pkg ./control.tar.gz | tar xzOf - ./control | sed -e "s/^Description:/Filename: $sed_safe_pkg\\
 Size: $file_size\\
-MD5Sum: $md5sum\\
 SHA256sum: $sha256sum\\
 Description:/"
 	echo ""
