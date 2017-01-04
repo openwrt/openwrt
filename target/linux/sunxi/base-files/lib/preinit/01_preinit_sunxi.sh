0ac00c931cc6f0c8a1b9a546f6d9aaa275e8959a
Yousong Zhou <yszhou4tech@gmail.com>
2017-01-04 22:19:30 +0800

    sunxi: use fwtool for checking sdcard images
    To achieve this, device tree compatible string was used as boardname and
    the value of it will be checked against supported_devices list.
    
    It should be noted that we do not distinguish between
    sun5i-a13-olimex-som and sun5i-a13-olinuxino as they share the same dts
    file.
    
    The other thing is that we need to gunzip the generated firmware to do
    fwtool check.
    
    Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>

--- target/linux/sunxi/base-files/lib/preinit/01_preinit_sunxi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-do_sunxi() {
-        . /lib/sunxi.sh
-
-        sunxi_board_detect
-}
-
-boot_hook_add preinit_main do_sunxi
--- target/linux/sunxi/base-files/lib/preinit/02_b53_hack.sh
+++ target/linux/sunxi/base-files/lib/preinit/02_b53_hack.sh
@@ -4,7 +4,7 @@
 do_b53_hack() {
 	# hack: enable switch on Lamobo R1 and reset counters
 	case "$( sunxi_board_name )" in
-	"lamobo-r1")
+	"lamobo,lamobo-r1")
 		ifconfig eth0 up
 		sleep 1
 		swconfig dev switch0 set reset 1
