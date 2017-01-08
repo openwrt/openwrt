21591a336198beaffa11d40e3581215ef0fe67ec
Felix Fietkau <nbd@nbd.name>
2017-01-08 18:40:59 +0100

    adm5120: mark the rb1xx subtarget as broken
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/adm5120/rb1xx/target.mk
+++ target/linux/adm5120/rb1xx/target.mk
@@ -1,7 +1,7 @@
 ARCH:=mipsel
 SUBTARGET:=rb1xx
 BOARDNAME:=MikroTik RB-1xx boards
-FEATURES+=tgz
+FEATURES+=tgz broken
 
 define Target/Description
 	Build firmware images for Mikrotik RB-1xx series.
