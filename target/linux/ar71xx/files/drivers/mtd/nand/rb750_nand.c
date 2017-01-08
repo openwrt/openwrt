9db9072d6767e5910e01b8962171c89359ab7a14
Felix Fietkau <nbd@nbd.name>
2017-01-08 14:27:38 +0100

    ar71xx: convert mikrotik routerboard support to UBI
    Remove the wget2nand script, drop the need for manual installation,
    use sysupgrade instead.
    
    There are now two different NAND images, one for 64 MiB flashes, the
    other for >= 128 MiB
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/ar71xx/files/drivers/mtd/nand/rb750_nand.c
+++ target/linux/ar71xx/files/drivers/mtd/nand/rb750_nand.c
@@ -71,7 +71,7 @@ static struct mtd_partition rb750_nand_partitions[] = {
 		.offset	= (256 * 1024),
 		.size	= (4 * 1024 * 1024) - (256 * 1024),
 	}, {
-		.name	= "rootfs",
+		.name	= "ubi",
 		.offset	= MTDPART_OFS_NXTBLK,
 		.size	= MTDPART_SIZ_FULL,
 	},
