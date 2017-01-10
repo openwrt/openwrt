78de59f15aa4468c2c0c6e39627310895fe250aa
Felix Fietkau <nbd@nbd.name>
2017-01-10 10:20:52 +0100

    kernel: fix dwc2 gadget dependency
    Use USB_GADGET_SUPPORT feature flag instead of hardcoding the target
    list.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -518,7 +518,7 @@ $(eval $(call KernelPackage,usb2-pci))
 
 define KernelPackage/usb-dwc2
   TITLE:=DWC2 USB controller driver
-  DEPENDS:=+(TARGET_brcm2708||TARGET_at91||TARGET_brcm63xx||TARGET_mxs||TARGET_imx6||TARGET_omap):kmod-usb-gadget
+  DEPENDS:=+USB_GADGET_SUPPORT:kmod-usb-gadget
   KCONFIG:= \
 	CONFIG_USB_DWC2 \
 	CONFIG_USB_DWC2_PCI \
