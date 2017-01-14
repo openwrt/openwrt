0d9f922230aa02821c538a87c1d10584cff6c61c
Chris Blake <chrisrblake93@gmail.com>
2017-01-14 10:20:25 -0600

    x86: add kernel module for sp5100_tco watchdog
    This change adds the sp5100_tco driver as a kernel module for the x86
    target. Specifically, this can be used by the PCEngines APU2/APU3. The
    reason for having this as a kernel module is to allow users to
    load/unload it on demand, as the I2C interface on the APU2/APU3 will not
    work while this module is loaded. More info can be found on GitHub at
    https://github.com/riptidewave93/LEDE-APU2/pull/5#issuecomment-255667736
    
    Signed-off-by: Chris Blake <chrisrblake93@gmail.com>

--- target/linux/x86/modules.mk
+++ target/linux/x86/modules.mk
@@ -19,3 +19,18 @@ define KernelPackage/sound-cs5535audio/description
 endef
 
 $(eval $(call KernelPackage,sound-cs5535audio))
+
+define KernelPackage/sp5100_tco
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=SP5100 Watchdog Support
+  DEPENDS:=@TARGET_x86
+  KCONFIG:=CONFIG_SP5100_TCO
+  FILES:=$(LINUX_DIR)/drivers/watchdog/sp5100_tco.ko
+  AUTOLOAD:=$(call AutoLoad,50,sp5100_tco,1)
+endef
+
+define KernelPackage/sp5100_tco/description
+ Kernel module for the SP5100_TCO hardware watchdog.
+endef
+
+$(eval $(call KernelPackage,sp5100_tco))
