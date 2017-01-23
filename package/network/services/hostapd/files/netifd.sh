633c35aaa46d96721ea11462a510ae7c41d18378
Jo-Philipp Wich <jo@mein.io>
2017-01-23 14:52:27 +0100

    hostapd: fix stray "out of range" shell errors in hostapd.sh
    The hostapd_append_wpa_key_mgmt() procedure uses the possibly uninitialized
    $ieee80211r and $ieee80211w variables in a numerical comparisation, leading
    to stray "netifd: radio0 (0000): sh: out of range" errors in logread when
    WPA-PSK security is enabled.
    
    Ensure that those variables are substituted with a default value in order to
    avoid emitting this (harmless) shell error.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- package/network/services/hostapd/files/netifd.sh
+++ package/network/services/hostapd/files/netifd.sh
@@ -40,8 +40,8 @@ hostapd_append_wpa_key_mgmt() {
 	local auth_type="$(echo $auth_type | tr 'a-z' 'A-Z')"
 
 	append wpa_key_mgmt "WPA-$auth_type"
-	[ "$ieee80211r" -gt 0 ] && append wpa_key_mgmt "FT-${auth_type}"
-	[ "$ieee80211w" -gt 0 ] && append wpa_key_mgmt "WPA-${auth_type}-SHA256"
+	[ "${ieee80211r:-0}" -gt 0 ] && append wpa_key_mgmt "FT-${auth_type}"
+	[ "${ieee80211w:-0}" -gt 0 ] && append wpa_key_mgmt "WPA-${auth_type}-SHA256"
 }
 
 hostapd_add_log_config() {
