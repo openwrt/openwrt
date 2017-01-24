59a310818817614a5ab5fe3ff20200748306107c
Felix Fietkau <nbd@nbd.name>
2017-01-24 12:41:19 +0100

    malta: move FEATURES to the target makefile
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/malta/be64/target.mk
+++ target/linux/malta/be64/target.mk
@@ -2,7 +2,6 @@ ARCH:=mips64
 CPU_TYPE:=mips64
 SUBTARGET:=be64
 BOARDNAME:=Big Endian (64-bits)
-FEATURES:=ramdisk
 
 define Target/Description
 	Build BE firmware images for MIPS Malta CoreLV board running in
--- target/linux/malta/le64/target.mk
+++ target/linux/malta/le64/target.mk
@@ -2,7 +2,6 @@ ARCH:=mips64el
 CPU_TYPE:=mips64
 SUBTARGET:=le64
 BOARDNAME:=Little Endian (64-bits)
-FEATURES:=ramdisk
 
 define Target/Description
 	Build LE firmware images for MIPS Malta CoreLV board running in
