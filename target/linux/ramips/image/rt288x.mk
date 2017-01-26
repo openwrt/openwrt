a66623639af5ea120a522565f4b9ad7f9a3f6c18
Yo Abe <abe.geel@gmail.com>
2017-01-26 22:28:09 +0900

    ramips: add ip17xx support to WLI-TX4-AG300N
    ramips/rt288x WLI-TX4-AG300N was missing support for its 100Mbit switch which
    should be included by default.
    
    Signed-off-by: Yo Abe <abe.geel@gmail.com>
    [Jo-Philipp Wich: picked from OpenWrt PR#359, rewrap commit msg, fix Sob]
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- target/linux/ramips/image/rt288x.mk
+++ target/linux/ramips/image/rt288x.mk
@@ -61,6 +61,7 @@ define Device/wli-tx4-ag300n
   BLOCKSIZE := 64k
   IMAGE_SIZE := $(ralink_default_fw_size_4M)
   DEVICE_TITLE := Buffalo WLI-TX4-AG300N
+  DEVICE_PACKAGES := kmod-switch-ip17xx
 endef
 TARGET_DEVICES += wli-tx4-ag300n
 
