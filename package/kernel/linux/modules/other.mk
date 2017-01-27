9f7fc2376d0749c401b726bcdbf188067cb27c1c
Felix Fietkau <nbd@nbd.name>
2017-01-27 16:53:05 +0100

    kernel: mark nvmem as hidden, it is only used as a library for other modules
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -129,6 +129,7 @@ define KernelPackage/nvmem
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Non Volatile Memory support
   KCONFIG:=CONFIG_NVMEM
+  HIDDEN:=1
   FILES:=$(LINUX_DIR)/drivers/nvmem/nvmem_core.ko@ge4.9
 endef
 
