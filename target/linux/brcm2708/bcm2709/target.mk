77a00027c3b2172ee1779e298ebe206dde02b87a
Álvaro Fernández Rojas <noltari@gmail.com>
2017-02-08 00:31:09 +0100

    brcm2708: bcm2709: add support for Raspberry Pi 3
    This adds support for Raspberry Pi 3 on the bcm2709 subtarget, in order to
    keep 32 bit image support when 64 bit support is added on bcm2710 subtarget.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/brcm2708/bcm2709/target.mk
+++ target/linux/brcm2708/bcm2709/target.mk
@@ -1,12 +1,13 @@
 #
 # Copyright (C) 2015 OpenWrt.org
+# Copyright (C) 2017 LEDE project
 #
 
 SUBTARGET:=bcm2709
-BOARDNAME:=BCM2709 based boards
+BOARDNAME:=BCM2709/BCM2710 32 bit based boards
 CPU_TYPE:=cortex-a7
 CPU_SUBTYPE:=neon-vfpv4
 
 define Target/Description
-	Build firmware image for Broadcom BCM2709 SoC devices.
+	Build firmware image for Broadcom BCM2709/BCM2710 32 bit SoC devices.
 endef
