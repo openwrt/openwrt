a3408a5271ccb149fbbfdc249a4ff6ea031f8bcf
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2017-02-10 22:01:10 +0300

    toolchain/uclibc: Bump version to 1.0.22
    Important change was made in 1.0.18: all sub-libs were merged
    in one and only libc similarly to musl.
    
    See [1] for more details.
    
    To support that we had to remove refences to those sub-libs like
    libpthread, libcrypt, libdl, libm, libutil etc.
    
    [1] http://cgit.uclibc-ng.org/cgi/cgit/uclibc-ng.git/commit/?id=29ff9055c80efe77a7130767a9fcb3ab8c67e8ce
    
    Signed-off-by: Alexey Brodkin <Alexey.Brodkin@synopsys.com>

--- toolchain/uClibc/common.mk
+++ toolchain/uClibc/common.mk
@@ -7,7 +7,7 @@
 include $(TOPDIR)/rules.mk
 include $(INCLUDE_DIR)/target.mk
 
-PKG_VERSION:=1.0.14
+PKG_VERSION:=1.0.22
 
 PKG_NAME:=uClibc-ng
 PKG_SOURCE_URL = http://downloads.uclibc-ng.org/releases/$(PKG_VERSION)/
@@ -16,7 +16,7 @@ CONFIG_DIR:=$(PATH_PREFIX)/config
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION).tar.xz
 LIBC_SO_VERSION:=$(PKG_VERSION)
 
-PKG_HASH:=3c63d9f8c8b98b65fa5c4040d1c8ab1b36e99a16e1093810cedad51ac15c9a9e
+PKG_HASH:=f2004c85db8e07e9f1c2e8b7c513fa7c237bc9f9685d8e1bfc89535b8a85449b
 
 HOST_BUILD_DIR:=$(BUILD_DIR_TOOLCHAIN)/$(PKG_NAME)-$(PKG_VERSION)
 
