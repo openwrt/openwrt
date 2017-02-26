b24d91faf1551cfe993c1c755f75d947f4ff74b0
Felix Fietkau <nbd@nbd.name>
2017-02-26 10:38:51 +0100

    kernel: enable hwmon-k10temp in the x86/64 kernel config instead of packaging it
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- package/kernel/linux/modules/hwmon.mk
+++ package/kernel/linux/modules/hwmon.mk
@@ -386,19 +386,3 @@ define KernelPackage/hwmon-pwmfan/description
 endef
 
 $(eval $(call KernelPackage,hwmon-pwmfan))
-
-
-define KernelPackage/hwmon-k10temp
-  TITLE:=AMD Family 10h+ temperature sensor
-  KCONFIG:=CONFIG_SENSORS_K10TEMP
-  FILES:=$(LINUX_DIR)/drivers/hwmon/k10temp.ko
-  AUTOLOAD:=$(call AutoLoad,60,k10temp)
-  $(call AddDepends/hwmon,@PCI_SUPPORT @TARGET_x86)
-endef
-
-define KernelPackage/hwmon-k10temp/description
-  Thermal sensor support for AMD 10h, 11h, 12h (Llano), 14h (Brazos),
-  15h (Bulldozer/Trinity/Kaveri) and 16h (Kabini/Mullins) CPUs
-endef
-
-$(eval $(call KernelPackage,hwmon-k10temp))
