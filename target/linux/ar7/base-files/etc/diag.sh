5cc96c0953e2d829aebc84256dd29a9579193083
Piotr Dymacz <pepe2k@gmail.com>
2017-03-09 15:03:18 +0100

    ar7: diag.sh: use common status_led_* functions
    Switch to the common LED related status_led_blink_* functions in
    set_state() as it's done in most of other targets.
    
    Also, use "status_led_on" instead of "status_led_set_heartbeat"
    and incorrect "status_led_set_on" in the "done" state.
    
    Moreover, remove outdated comment from the script.
    
    Signed-off-by: Piotr Dymacz <pepe2k@gmail.com>

--- target/linux/ar7/base-files/etc/diag.sh
+++ target/linux/ar7/base-files/etc/diag.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 # Copyright (C) 2007-2013 OpenWrt.org
 
-# This setup gives us 4.5 distinguishable states:
-#
-# (1-LED) Solid OFF:    Bootloader running, or kernel hung (timer task stalled)
-# (1-LED) Solid ON:     Kernel hung (timer task stalled)
-# (2-LED) Solid RED:    Bootloader running, or kernel hung (timer task stalled)
-# (2-LED) Solid YELLOW: Kernel hung (timer task stalled)
-# 5Hz blink:            preinit
-# 10Hz blink:           failsafe
-# (1-LED) Heartbeat:    normal operation
-# (2-LED) Solid GREEN:  normal operation
-
 . /lib/functions/leds.sh
 
 get_status_led() {
@@ -24,20 +13,18 @@ set_state() {
 
 	case "$1" in
 	preinit)
-		status_led_set_timer 100 100
+		status_led_blink_preinit
 		;;
 	failsafe)
-		status_led_set_timer 50 50
+		status_led_blink_failsafe
 		;;
 	preinit_regular)
 		status_led_blink_preinit_regular
 		;;
 	done)
-		[ "$status_led" = "status" ] && {
-			status_led_set_heartbeat
-		}
+		status_led_on
+
 		[ "$status_led" = "power:green" ] && {
-			status_led_set_on
 			led_off "power:red"
 		}
 		;;
