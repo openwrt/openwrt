589b37e8b5d9c34ed7a292a127e681123c841261
Piotr Dymacz <pepe2k@gmail.com>
2017-03-10 22:23:46 +0100

    ar71xx: base-files: fix indentation in lib/ar71xx.sh
    Signed-off-by: Piotr Dymacz <pepe2k@gmail.com>

--- target/linux/ar71xx/base-files/lib/ar71xx.sh
+++ target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -74,25 +74,25 @@ ubnt_xm_board_detect() {
 
 	magic="$(ubnt_get_mtd_part_magic)"
 	case ${magic:0:3} in
-		"e00"|\
-		"e01"|\
-		"e80")
-			model="Ubiquiti NanoStation M"
-			;;
-		"e0a")
-			model="Ubiquiti NanoStation loco M"
-			;;
-		"e1b"|\
-		"e1d")
-			model="Ubiquiti Rocket M"
-			;;
-		"e20"|\
-		"e2d")
-			model="Ubiquiti Bullet M"
-			;;
-		"e30")
-			model="Ubiquiti PicoStation M"
-			;;
+	"e00"|\
+	"e01"|\
+	"e80")
+		model="Ubiquiti NanoStation M"
+		;;
+	"e0a")
+		model="Ubiquiti NanoStation loco M"
+		;;
+	"e1b"|\
+	"e1d")
+		model="Ubiquiti Rocket M"
+		;;
+	"e20"|\
+	"e2d")
+		model="Ubiquiti Bullet M"
+		;;
+	"e30")
+		model="Ubiquiti PicoStation M"
+		;;
 	esac
 
 	[ -z "$model" ] || AR71XX_MODEL="${model}${magic:3:1}"
