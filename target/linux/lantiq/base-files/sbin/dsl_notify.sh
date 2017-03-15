6a82f4006cb1651bc62e1eb23ca81f1bd19214c0
Oswald Buddenhagen <oswald.buddenhagen@gmx.de>
2017-03-15 11:40:12 +0100

    lantiq: Shortcut non-pppoa interfaces in dsl_notify
    No need to query 'up' and 'auto' when they are not going to be used.
    
    Signed-off-by: Oswald Buddenhagen <oswald.buddenhagen@gmx.de>

--- target/linux/lantiq/base-files/sbin/dsl_notify.sh
+++ target/linux/lantiq/base-files/sbin/dsl_notify.sh
@@ -31,22 +31,24 @@ interfaces=`ubus list network.interface.\* | cut -d"." -f3`
 for ifc in $interfaces; do
 
 	json_load "$(ifstatus $ifc)"
-	json_get_var up up
-
-	config_get_bool auto "$ifc" auto 1
 
 	json_get_var proto proto
+	if [ "$proto" != "pppoa" ]; then
+		continue
+	fi
 
+	json_get_var up up
+	config_get_bool auto "$ifc" auto 1
 	if [ "$DSL_INTERFACE_STATUS" = "UP" ]; then
-		if [ "$proto" = "pppoa" ] && [ "$up" != 1 ] && [ "$auto" = 1 ]; then
+		if [ "$up" != 1 ] && [ "$auto" = 1 ]; then
 			( sleep 1; ifup "$ifc" ) &
 		fi
 	else
-		if [ "$proto" = "pppoa" ] && [ "$up" = 1 ] && [ "$auto" = 1 ]; then
+		if [ "$up" = 1 ] && [ "$auto" = 1 ]; then
 			( sleep 1; ifdown "$ifc" ) &
 		else
 			json_get_var autostart autostart
-			if [ "$proto" = "pppoa" ] && [ "$up" != 1 ] && [ "$autostart" = 1 ]; then
+			if [ "$up" != 1 ] && [ "$autostart" = 1 ]; then
 				( sleep 1; ifdown "$ifc" ) &
 			fi
 		fi
