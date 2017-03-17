aa95d6cd20f7e08420562b9747c197c2eac1d2e2
Felix Fietkau <nbd@nbd.name>
2017-03-17 16:17:42 +0100

    lantiq: move lantiq_board_detect() to 03_preinit_board.sh
    It is only used there
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/lantiq/base-files/lib/functions/lantiq.sh
+++ target/linux/lantiq/base-files/lib/functions/lantiq.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-lantiq_board_detect() {
-	name=`grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/\(.*\) - .*/\1/g"`
-	model=`grep "^machine" /proc/cpuinfo | sed "s/machine.*: \(.*\)/\1/g" | sed "s/.* - \(.*\)/\1/g"`
-	[ -z "$name" ] && name="unknown"
-	[ -z "$model" ] && model="unknown"
-	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
-	echo $name > /tmp/sysinfo/board_name
-	echo $model > /tmp/sysinfo/model
-}
-
 lantiq_board_model() {
 	local model
 
