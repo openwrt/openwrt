f62f4b3c5c9d059a2e6a1e80ce7b4267ef0c236b
Felix Fietkau <nbd@nbd.name>
2017-05-03 15:01:03 +0200

    build: stop overriding STAGING_DIR_HOST for toolchain build
    This causes various issues in other places that assume that host
    binaries are staged in STAGING_DIR_HOST.
    Since all the right places use HOST_BUILD_PREFIX, override that instead.
    This fixes some issues with quilt on toolchain dirs
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -66,7 +66,7 @@ endif
 HOST_STAMP_PREPARED:=$(HOST_BUILD_DIR)/.prepared
 HOST_STAMP_BUILT:=$(GCC_BUILD_DIR)/.built
 HOST_STAMP_CONFIGURED:=$(GCC_BUILD_DIR)/.configured
-HOST_STAMP_INSTALLED:=$(STAGING_DIR_HOST)/stamp/.gcc_$(GCC_VARIANT)_installed
+HOST_STAMP_INSTALLED:=$(HOST_BUILD_PREFIX)/stamp/.gcc_$(GCC_VARIANT)_installed
 
 SEP:=,
 TARGET_LANGUAGES:="c,c++$(if $(CONFIG_INSTALL_LIBGCJ),$(SEP)java)$(if $(CONFIG_INSTALL_GFORTRAN),$(SEP)fortran)$(if $(CONFIG_INSTALL_GCCGO),$(SEP)go)"
@@ -86,7 +86,7 @@ ifdef CONFIG_INSTALL_GCCGO
 endif
 
 ifdef CONFIG_GCC_USE_GRAPHITE
-  GRAPHITE_CONFIGURE=--with-isl=$(REAL_STAGING_DIR_HOST)
+  GRAPHITE_CONFIGURE=--with-isl=$(HOST_BUILD_PREFIX)
 else
   GRAPHITE_CONFIGURE=--without-isl --without-cloog
 endif
@@ -213,8 +213,8 @@ endef
 
 define Host/Clean
 	rm -rf $(if $(GCC_PREPARE),$(HOST_SOURCE_DIR)) \
-		$(STAGING_DIR_HOST)/stamp/.gcc_* \
-		$(STAGING_DIR_HOST)/stamp/.binutils_* \
+		$(HOST_BUILD_PREFIX)/stamp/.gcc_* \
+		$(HOST_BUILD_PREFIX)/stamp/.binutils_* \
 		$(GCC_BUILD_DIR) \
 		$(BUILD_DIR_TOOLCHAIN)/$(PKG_NAME) \
 		$(TOOLCHAIN_DIR)/$(REAL_GNU_TARGET_NAME) \
