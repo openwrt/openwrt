4053c4f0fefe6bc4bee0b65cb8a187a75267293c
Alexander Couzens <lynxis@fe80.eu>
2017-06-07 23:56:19 +0200

    include/toplevel: set env GIT_ASKPASS=/bin/true
    When git-https request a service (e.g. github) which ask for credentials
    git will pass this request to the user resulting download.pl to wait for
    user input. Set GIT_ASKPASS to stop asking.
    
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -23,6 +23,7 @@ HOSTCC ?= $(CC)
 export REVISION
 export SOURCE_DATE_EPOCH
 export GIT_CONFIG_PARAMETERS='core.autocrlf=false'
+export GIT_ASKPASS:=/bin/true
 export MAKE_JOBSERVER=$(filter --jobserver%,$(MAKEFLAGS))
 
 # prevent perforce from messing with the patch utility
