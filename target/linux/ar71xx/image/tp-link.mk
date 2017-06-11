8b556fb056bdbdaf62bebdb59efa56a187b603b0
Piotr Dymacz <pepe2k@gmail.com>
2017-06-11 19:41:23 +0200

    ar71xx: image: fix TP-Link TL-WR710N v2/v2.1 BOARDNAME
    All TP-Link TL-WR710N versions share the same machine code.
    
    This has been working since the beginning as we don't use double-quotes
    to protect spaces inside command line values. Thus, kernel interprets
    'board=TL-WR710N v2' as 'board=TL-WR710N' and separate parameter 'v2'.
    
    Signed-off-by: Piotr Dymacz <pepe2k@gmail.com>

--- target/linux/ar71xx/image/tp-link.mk
+++ target/linux/ar71xx/image/tp-link.mk
@@ -754,7 +754,7 @@ define Device/tl-wr710n-v2
   $(Device/tplink-4mlzma)
   DEVICE_TITLE := TP-LINK TL-WR710N v2
   DEVICE_PACKAGES := kmod-usb-core kmod-usb2
-  BOARDNAME := TL-WR710N v2
+  BOARDNAME := TL-WR710N
   DEVICE_PROFILE := TLWR710
   TPLINK_HWID := 0x07100002
   CONSOLE := ttyATH0,115200
@@ -764,7 +764,7 @@ define Device/tl-wr710n-v2.1
   $(Device/tplink-8mlzma)
   DEVICE_TITLE := TP-LINK TL-WR710N v2.1
   DEVICE_PACKAGES := kmod-usb-core kmod-usb2
-  BOARDNAME := TL-WR710N v2.1
+  BOARDNAME := TL-WR710N
   DEVICE_PROFILE := TLWR710
   TPLINK_HWID := 0x07100002
   TPLINK_HWREV := 0x00000002
