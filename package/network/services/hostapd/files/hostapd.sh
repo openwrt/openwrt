4ed40be3e32643560874688d34b6fc69e6c4d20d
Kevin Darbyshire-Bryant <kevin@darbyshire-bryant.me.uk>
2017-06-15 12:48:56 +0100

    hostapd: add support for acs_chan_bias option
    During auto channel selection we may wish to prefer certain channels
    over others.
    
    e.g. we can just squeeze 4 channels into europe so '1:0.8 5:0.8 9:0.8
    13:0.8' does that.
    
    Signed-off-by: Kevin Darbyshire-Bryant <kevin@darbyshire-bryant.me.uk>

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -66,6 +66,8 @@ hostapd_common_add_device_config() {
 	config_add_string require_mode
 	config_add_boolean legacy_rates
 
+	config_add_string acs_chan_bias
+
 	hostapd_add_log_config
 }
 
@@ -76,7 +78,7 @@ hostapd_prepare_device_config() {
 	local base="${config%%.conf}"
 	local base_cfg=
 
-	json_get_vars country country_ie beacon_int:100 doth require_mode legacy_rates
+	json_get_vars country country_ie beacon_int:100 doth require_mode legacy_rates acs_chan_bias
 
 	hostapd_set_log_options base_cfg
 
@@ -92,6 +94,8 @@ hostapd_prepare_device_config() {
 		[ "$country_ie" -gt 0 ] && append base_cfg "ieee80211d=1" "$N"
 		[ "$hwmode" = "a" -a "$doth" -gt 0 ] && append base_cfg "ieee80211h=1" "$N"
 	}
+
+	[ -n "$acs_chan_bias" ] && append base_cfg "acs_chan_bias=$acs_chan_bias" "$N"
 
 	local brlist= br
 	json_get_values basic_rate_list basic_rate
