551a335956ee69aaa56520ab06ec17e783a76549
Chen Minqiang <ptpt52@gmail.com>
2017-07-04 12:13:34 +0800

    custom: ap152/ap147 add qca988x/qca9887 support

--- target/linux/ar71xx/image/generic-legacy-devices.mk
+++ target/linux/ar71xx/image/generic-legacy-devices.mk
@@ -105,13 +105,13 @@ LEGACY_DEVICES += AP143_16M
 
 define LegacyDevice/AP147_010
   DEVICE_TITLE := Qualcomm Atheros AP147-010 reference board
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-ath10k ath10k-firmware-qca9887
 endef
 LEGACY_DEVICES += AP147_010
 
 define LegacyDevice/AP152_16M
   DEVICE_TITLE := Qualcomm Atheros AP152 reference board (16MB flash)
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-ath10k ath10k-firmware-qca988x
 endef
 LEGACY_DEVICES += AP152_16M
 
