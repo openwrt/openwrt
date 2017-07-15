e9aec1591116cb3f6bd74eef9066c5a03cb799f2
Hauke Mehrtens <hauke@hauke-m.de>
2017-07-15 22:52:21 +0200

    brcm2708: bcm2710: do not activate neon-vfpv4 manually
    Neon and vfpv4 support are activated by GCC on all ARMv8 CPUs because
    this is now a mandatory part of the architecture. There is not need to
    activate is manually.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/brcm2708/bcm2710/target.mk
+++ target/linux/brcm2708/bcm2710/target.mk
@@ -7,7 +7,6 @@ ARCH:=aarch64
 SUBTARGET:=bcm2710
 BOARDNAME:=BCM2710 64 bit based boards
 CPU_TYPE:=cortex-a53
-CPU_SUBTYPE:=neon-vfpv4
 
 define Target/Description
 	Build firmware image for Broadcom BCM2710 64 bit SoC devices.
