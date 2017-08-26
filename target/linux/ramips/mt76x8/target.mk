3e36ca4b436510a6e411484db47f7df35ebaf86d
L. D. Pinney <ldpinney@gmail.com>
2017-08-26 09:49:45 +0800

    ramips: merge mt7628/mt7688 into mt76x8
    The ramips subtargets of mt7628 and mt7688 dts files all #include "mt7628an.dtsi"
    
    They are essentially a single subtarget.
    
    This patch merges the ramips subtargets mt7628 and mt7688 into a single subtarget mt76x8.
    
    Signed-off-by: L. D. Pinney <ldpinney@gmail.com>

--- target/linux/ramips/mt76x8/target.mk
+++ target/linux/ramips/mt76x8/target.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2009 OpenWrt.org
 #
 
-SUBTARGET:=mt7628
-BOARDNAME:=MT7628 based boards
+SUBTARGET:=mt76x8
+BOARDNAME:=MT76x8 based boards
 FEATURES+=usb
 CPU_TYPE:=24kc
 
 DEFAULT_PACKAGES += kmod-mt76 wpad-mini
 
 define Target/Description
-	Build firmware images for Ralink MT7628 based boards.
+	Build firmware images for Ralink MT76x8 based boards.
 endef
 
