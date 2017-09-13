f8a5ac1a8835b9c826286469379a143ff9e532d5
Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>
2017-09-13 11:51:13 -0700

    at91: Renaming subtarget sama5d3 to sama5
    Renaming at91 subtarget sama5d3 to sama5 and using at91-sama5d3_xplained
    as a target device in sama5 subtarget.This will enable to add other
    sama5d2 & sama5d4 target devices in sama5 subtraget.This will avoid
    code duplication when sama5d2 & sama5d4 added as different subtarget.
    
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>

--- target/linux/at91/image/sama5.mk
+++ target/linux/at91/image/sama5.mk
@@ -1,8 +1,7 @@
 define Device/at91-sama5d3_xplained
   $(Device/evaluation-dtb)
-  DEVICE_TITLE := Atmel AT91SAMA5D3XPLAINED
+  DEVICE_TITLE := Microchip(Atmel AT91) SAMA5D3 Xplained
   KERNEL_SIZE := 6144k
   SUBPAGESIZE := 2048
 endef
 TARGET_DEVICES += at91-sama5d3_xplained
-
--- target/linux/at91/sama5/target.mk
+++ target/linux/at91/sama5/target.mk
@@ -1,9 +1,9 @@
-BOARDNAME:=SAMA5D3 (Cortex-A5)
+BOARDNAME:=SAMA5 Xplained boards(Cortex-A5)
 CPU_TYPE:=cortex-a5
 
 DEFAULT_PACKAGES += kmod-usb2
 
 define Target/Description
-	Build generic firmware for SAMA5D3 AT91 platforms
+	Build generic firmware for Microchip(Atmel AT91) SAMA5 MPU's
 	using the ARMv7 instruction set.
 endef
