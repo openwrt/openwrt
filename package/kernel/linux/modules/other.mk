344fde35e3331dd999b38298b4743dce9a88583d
Philip Prindeville <philipp@redfish-solutions.com>
2017-09-15 11:03:52 -0600

    kernel: add packaging for Xeon iTCO watchdog timer
    Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1044,3 +1044,20 @@ define KernelPackage/w83627hf-wdt/description
 endef
 
 $(eval $(call KernelPackage,w83627hf-wdt))
+
+
+define KernelPackage/itco-wdt
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Intel iTCO Watchdog Timer
+  KCONFIG:=CONFIG_ITCO_WDT \
+           CONFIG_ITCO_VENDOR_SUPPORT=y
+  FILES:=$(LINUX_DIR)/drivers/$(WATCHDOG_DIR)/iTCO_wdt.ko \
+         $(LINUX_DIR)/drivers/$(WATCHDOG_DIR)/iTCO_vendor_support.ko
+  AUTOLOAD:=$(call AutoLoad,50,iTCO_vendor_support iTCO_wdt,1)
+endef
+
+define KernelPackage/itco-wdt/description
+  Kernel module for Intel iTCO Watchdog Timer
+endef
+
+$(eval $(call KernelPackage,itco-wdt))
