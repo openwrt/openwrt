0aa3f9bf00cf48354ff443d26fe4acca3b16060a
Chen Minqiang <ptpt52@gmail.com>
2017-10-16 23:03:30 +0800

    ramips: rename devices TITLE

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -972,7 +972,7 @@ define Device/phicomm_psg1208
   SOC := mt7620a
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := Phicomm
-  DEVICE_MODEL := PSG1208
+  DEVICE_MODEL := PSG1208 (K1)
   DEVICE_PACKAGES := kmod-mt76x2
   SUPPORTED_DEVICES += psg1208
 endef
@@ -982,7 +982,7 @@ define Device/phicomm_psg1218b
   SOC := mt7620a
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := Phicomm
-  DEVICE_MODEL := PSG1218
+  DEVICE_MODEL := PSG1218 (K2C)
   DEVICE_VARIANT := Bx
   DEVICE_PACKAGES := kmod-mt76x2
   SUPPORTED_DEVICES += psg1218 psg1218b
