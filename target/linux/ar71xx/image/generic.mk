cde71a543c629f63e6f01850fd8b6f3f94cdd25d
Felix Fietkau <nbd@nbd.name>
2017-11-08 18:40:37 +0100

    build: replace uses of OpenWrt with $(VERSION_DIST)
    This makes the distribution name more configurable.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/ar71xx/image/generic.mk
+++ target/linux/ar71xx/image/generic.mk
@@ -28,7 +28,7 @@ define Build/mkwrggimg
 	$(STAGING_DIR_HOST)/bin/mkwrggimg -b \
 		-i $@ -o $@.imghdr -d /dev/mtdblock/1 \
 		-m $(BOARDNAME) -s $(DAP_SIGNATURE) \
-		-v OpenWrt -B $(REVISION)
+		-v $(VERSION_DIST) -B $(REVISION)
 	mv $@.imghdr $@
 endef
 
@@ -46,7 +46,7 @@ define Build/netgear-squashfs
 		-A mips -O linux -T filesystem -C none \
 		-M $(NETGEAR_KERNEL_MAGIC) \
 		-a 0xbf070000 -e 0xbf070000 \
-		-n 'MIPS OpenWrt Linux-$(LINUX_VERSION)' \
+		-n 'MIPS $(VERSION_DIST) Linux-$(LINUX_VERSION)' \
 		-d $@.squashfs $@
 	rm -rf $@.squashfs $@.fs
 endef
@@ -93,7 +93,7 @@ define Build/uImageHiWiFi
 	mkimage -A $(LINUX_KARCH) \
 		-O linux -T kernel \
 		-C $(1) -a $(KERNEL_LOADADDR) -e $(if $(KERNEL_ENTRY),$(KERNEL_ENTRY),$(KERNEL_LOADADDR)) \
-		-n 'tw150v1 $(call toupper,$(LINUX_KARCH)) OpenWrt Linux-$(LINUX_VERSION)' -d $@ $@.new
+		-n 'tw150v1 $(call toupper,$(LINUX_KARCH)) $(VERSION_DIST) Linux-$(LINUX_VERSION)' -d $@ $@.new
 	@mv $@.new $@
 endef
 
