e79b096ee175219c072aeb8d228a07f3361c14ea
Christian Lamparter <chunkeey@gmail.com>
2017-12-17 15:18:46 +0100

    brcm2708: convert to metadata
    This patch converts all the raspberrypi images to utilize
    the common metadata-based image verification.
    
    Note: the CM1 and CM3 currently use the same "rpi-cm"
    boardname.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/brcm2708/base-files/lib/upgrade/platform.sh
+++ target/linux/brcm2708/base-files/lib/upgrade/platform.sh
@@ -1,20 +1,7 @@
-get_magic_at() {
-	local file="$1"
-	local pos="$2"
-	get_image "$file" | dd bs=1 count=2 skip="$pos" 2>/dev/null | hexdump -v -n 2 -e '1/1 "%02x"'
-}
+REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
-	local file="$1"
-	local magic
-
-	magic=$(get_magic_at "$file" 510)
-	[ "$magic" != "55aa" ] && {
-		echo "Failed to verify MBR boot signature."
-		return 1
-	}
-
-	return 0;
+	return 0
 }
 
 platform_do_upgrade() {
