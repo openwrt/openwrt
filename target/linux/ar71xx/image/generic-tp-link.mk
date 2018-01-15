18be1e63a2842004add99fcce2d8ce0b8af1bbf5
Henryk Heisig <hyniu@o2.pl>
2018-01-15 06:44:14 +0100

    ar71xx: add support for Archer C60v2
    TP-Link Archer C60v2 is a dual-band AC1350 router, based on Qualcomm/Atheros
    QCA9561+QCA9886.
    
    Specification:
    
    775/650/258 MHz (CPU/DDR/AHB)
    64 MB of RAM (DDR2)
    8 MB of FLASH (SPI NOR)
    3T3R 2.4 GHz
    2T2R 5 GHz
    5x 10/100 Mbps Ethernet
    7x LED, 2x button
    UART header on PCB
    
    Flash instruction:
    Web:
    Download lede-ar71xx-generic-archer-c60-v2-squashfs-factory.bin
    and use OEM System Tools - Firmware Upgrade site.
    
    Recovery:
    Set PC to fixed ip address 192.168.0.66
    Download lede-ar71xx-generic-archer-c60-v2-squashfs-factory.bin
    and rename it to tp_recovery.bin
    Start a tftp server with the file tp_recovery.bin in its root directory
    Turn off the router
    Press and hold Reset button
    Turn on router with the reset button pressed and wait ~15 seconds
    Release the reset button and after a short time
    the firmware should be transferred from the tftp server
    Wait ~30 second to complete recovery.
    
    Flash instruction under U-Boot, using UART:
    tftp 0x81000000 lede-ar71xx-...-sysupgrade.bin
    erase 0x9f030000 +$filesize
    cp.b $fileaddr 0x9f030000 $filesize
    reset
    
    Signed-off-by: Henryk Heisig <hyniu@o2.pl>

--- target/linux/ar71xx/image/generic-tp-link.mk
+++ target/linux/ar71xx/image/generic-tp-link.mk
@@ -62,6 +62,18 @@ define Device/archer-c60-v1
 endef
 TARGET_DEVICES += archer-c60-v1
 
+define Device/archer-c60-v2
+  $(Device/archer-c60-v1)
+  DEVICE_TITLE := TP-LINK Archer C60 v2
+  BOARDNAME := ARCHER-C60-V2
+  TPLINK_BOARD_ID := ARCHER-C60-V2
+  DEVICE_PROFILE := ARCHERC60V2
+  IMAGE_SIZE := 7808k
+  MTDPARTS := spi0.0:192k(u-boot)ro,7808k(firmware),128k(tplink)ro,64k(art)ro
+  SUPPORTED_DEVICES := archer-c60-v2
+endef
+TARGET_DEVICES += archer-c60-v2
+
 define Device/archer-c5-v1
   $(Device/tplink-16mlzma)
   DEVICE_TITLE := TP-LINK Archer C5 v1
