ab4584426eb5ca3034bc54927fa2056dd80a4b58
Michal Hrusecky <Michal@Hrusecky.net>
2018-02-19 22:02:58 +0100

    kernel: Include kernel magic in all kmods
    Kernel modules can greatly depend on configuration of the kernel and might not
    work with different kernel, so always put kernel magic as part of the package
    version.
    
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- include/kernel.mk
+++ include/kernel.mk
@@ -196,7 +196,7 @@ define KernelPackage
     CATEGORY:=Kernel modules
     DESCRIPTION:=$(DESCRIPTION)
     EXTRA_DEPENDS:=kernel (=$(LINUX_VERSION)-$(LINUX_RELEASE)-$(LINUX_VERMAGIC))
-    VERSION:=$(LINUX_VERSION)$(if $(PKG_VERSION),+$(PKG_VERSION))-$(if $(PKG_RELEASE),$(PKG_RELEASE),$(LINUX_RELEASE))
+    VERSION:=$(LINUX_VERSION)$(if $(PKG_VERSION),+$(PKG_VERSION))-$(if $(PKG_RELEASE),$(PKG_RELEASE),$(LINUX_RELEASE))-$(LINUX_VERMAGIC)
     PKGFLAGS:=$(PKGFLAGS)
     $(call KernelPackage/$(1))
     $(call KernelPackage/$(1)/$(BOARD))
