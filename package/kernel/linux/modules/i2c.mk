97c624631a9a48a02f7fe51d377f2798a82256e6
Felix Fietkau <nbd@nbd.name>
2018-02-22 12:16:19 +0100

    kernel: remove an old kernel compatibility line from module packaging
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- package/kernel/linux/modules/i2c.mk
+++ package/kernel/linux/modules/i2c.mk
@@ -24,10 +24,6 @@ I2C_CORE_MODULES:= \
   CONFIG_I2C:drivers/i2c/i2c-core \
   CONFIG_I2C_CHARDEV:drivers/i2c/i2c-dev
 
-ifeq ($(CONFIG_OF),y)
-  I2C_CORE_MODULES+=CONFIG_OF_I2C:drivers/of/of_i2c@lt3.12
-endif
-
 define KernelPackage/i2c-core
   $(call i2c_defaults,$(I2C_CORE_MODULES),51)
   TITLE:=I2C support
