ce49ac77bbfba7aeb4ef39ce62da6da78f1f1fa3
Sven Eckelmann <sven@narfation.org>
2018-03-05 09:51:47 +0100

    build: Allow to change the FIT config section name
    Some devices only boot when a special config is found in the image and
    completely ignore the default entry during the selection. These devices can
    now use the variable DEVICE_DTS_CONFIG in their device image definition.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -118,6 +118,7 @@ define Build/fit
 		-D $(DEVICE_NAME) -o $@.its -k $@ \
 		$(if $(word 2,$(1)),-d $(word 2,$(1))) -C $(word 1,$(1)) \
 		-a $(KERNEL_LOADADDR) -e $(if $(KERNEL_ENTRY),$(KERNEL_ENTRY),$(KERNEL_LOADADDR)) \
+		-c $(if $(DEVICE_DTS_CONFIG),$(DEVICE_DTS_CONFIG),"config@1") \
 		-A $(LINUX_KARCH) -v $(LINUX_VERSION)
 	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $@.its $@.new
 	@mv $@.new $@
