87c42101cfb001b4bd418d1201fa4d8c822dc77b
Christian Lamparter <chunkeey@gmail.com>
2018-03-07 09:13:10 +0100

    ipq40xx: add support for ASUS RT-AC58U/RT-ACRH13
    This patch adds support for ASUS RT-AC58U/RT-ACRH13.
    
    hardware highlights:
    
    SOC:	IPQ4018 / QCA Dakota
    CPU:	Quad-Core ARMv7 Processor rev 5 (v7l) Cortex-A7
    DRAM:	128 MiB DDR3L-1066 @ 537 MHz (1074?) NT5CC64M16GP-DI
    NOR:	2 MiB Macronix MX25L1606E (for boot, QSEE)
    NAND:   128 MiB Winbond W25NO1GVZE1G (cal + kernel + root, UBI)
    ETH:    Qualcomm Atheros QCA8075 Gigabit Switch (4 x LAN, 1 x WAN)
    USB:    1 x 3.0 (via Synopsys DesignWare DWC3 controller in the SoC)
    WLAN1:  Qualcomm Atheros QCA4018 2.4GHz 802.11bgn 2:2x2
    WLAN2:  Qualcomm Atheros QCA4018 5GHz 802.11a/n/ac 2:2x2
    INPUT:	one Reset and one WPS button
    LEDS:	Status, WAN, WIFI1/2, USB and LAN (one blue LED for each)
    Serial:
    	WARNING: The serial port needs a TTL/RS-232 3V3 level converter!
    	The Serial setting is 115200-8-N-1. The board has an unpopulated
    	1x4 0.1" header. The pinout (VDD, RX, GND, TX) is printed on the
    	PCB right next to the connector.
    
    U-Boot Note: The ethernet driver isn't always reliable and can sometime
    time out... Don't worry, just retry.
    
    Access via the serial console is required. As well as a working
    TFTP-server setup and the initramfs image. (If not provided, it
    has to be built from the OpenWrt source. Make sure to enable
    LZMA as the compression for the INITRAMFS!)
    
    To install the image permanently, you have to do the following
    steps in the listed order.
    
    1. Open up the router.
       There are four phillips screws hiding behind the four plastic
       feets on the underside.
    
    2. Connect the serial cable (See notes above)
    
    3. Connect your router via one of the four LAN-ports (yellow)
       to a PC which can set the IP-Address and ssh and scp from.
    
       If possible set your PC's IPv4 Address to 192.168.1.70
       (As this is the IP-Address the Router's bootloader expects
       for the tftp server)
    
    4. power up the router and enter the u-boot
       choose option 1 to upload the initramfs image. And follow
       through the ipv4 setup.
    
    Wait for your router's status LED to stop blinking rapidly and
    glow just blue. (The LAN LED should also be glowing blue).
    
    3. Connect to the OpenWrt running in RAM
    
       The default IPv4-Address of your router will be 192.168.1.1.
    
       1. Copy over the openwrt-sysupgrade.bin image to your router's
          temporary directory
    
       # scp openwrt-sysupgrade.bin root@192.168.1.1:/tmp
    
       2. ssh from your PC into your router as root.
    
       # ssh root@192.168.1.1
    
       The default OpenWrt-Image won't ask for a password. Simply hit the Enter-Key.
    
       Once connected...: run the following commands on your temporary installation
    
       3. delete the "jffs2" ubi partition to make room for your new root partition
    
       # ubirmvol /dev/ubi0 --name=jffs2
    
       4. install OpenWrt on the NAND Flash.
    
       # sysupgrade -v /tmp/openwrt-sysupgrade.bin
    
       - This will will automatically reboot the router -
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -5,17 +5,52 @@ RAMFS_COPY_BIN='fw_printenv fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
 platform_check_image() {
+	case "$(board_name)" in
+	asus,rt-ac58u)
+		CI_UBIPART="UBI_DEV"
+		local ubidev=$(nand_find_ubi $CI_UBIPART)
+		local asus_root=$(nand_find_volume $ubidev jffs2)
+
+		[ -n "$asus_root" ] || return 0
+
+		cat << EOF
+jffs2 partition is still present.
+There's probably no space left
+to install the filesystem.
+
+You need to delete the jffs2 partition first:
+# ubirmvol /dev/ubi0 --name=jffs2
+
+Once this is done. Retry.
+EOF
+		return 1
+		;;
+	esac
 	return 0;
 }
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	asus,rt-ac58u)
+		CI_UBIPART="UBI_DEV"
+		CI_KERNPART="linux"
+		nand_do_upgrade "$1"
+		;;
 	openmesh,a42)
 		PART_NAME="inactive"
 		platform_do_upgrade_openmesh "$ARGV"
 		;;
 	*)
 		default_do_upgrade "$ARGV"
+		;;
+	esac
+}
+
+platform_nand_pre_upgrade() {
+	case "$(board_name)" in
+	asus,rt-ac58u)
+		CI_UBIPART="UBI_DEV"
+		CI_KERNPART="linux"
 		;;
 	esac
 }
