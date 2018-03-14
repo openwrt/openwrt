9847ed7acd50c62adc21b795865189884e1e0cd9
Chen Minqiang <ptpt52@gmail.com>
2018-03-15 06:22:17 +0800

    ipq40xx: easy install for ASUS RT-AC58U/RT-ACRH13
    This patch make it easy to flash via web upgrade from factory firmware.
     * build flash-factory.trx for web ui upgrade from stock system.
     * fixup macaddress for eth0
     * rewrite the initramfs building command: initramfs image ready to upgrade from stock
    
     - install from stock to openwrt:
       1. upgrade via stock web ui, using file: initramfs-factory.trx
       2. sysupgrade via openwrt web ui, use the sysupgrade.bin
    
     - revert back to stock:
       1. copy RT-ACXXX.trx to /tmp/ via ssh
       2. do sysupgrade -F /tmp/RT-ACXXX.trx
    
    idea from 981213
    
    Signed-off-by: X Wrt <dev@x-wrt.com>
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -9,6 +9,11 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
 		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
 		;;
+	asus,rt-ac58u)
+		CI_UBIPART=UBI_DEV
+		mac=$(mtd_get_mac_binary_ubi Factory 4102)
+		ifconfig eth0 hw ether $mac 2>/dev/null
+		;;
 	meraki,mr33)
 		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 102)
 		[ -n "$mac_lan" ] && ip link set dev eth0 address "$mac_lan"
