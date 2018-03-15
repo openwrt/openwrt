9899ffcfd35120f7a4aa1d401b85653e56b775b0
Stijn Segers <foss@volatilesystems.org>
2018-03-15 22:00:08 +0100

    kernel: bump 4.14 to 4.14.27
    * Refreshed patches.
    * Deleted 812-pci-dwc-fix-enumeration.patch (was accepted upstream)
    
    Compile-tested: ramips/mt7621, x86/64
    Run-tested: ramips/mt7621, x86/64
    
    Signed-off-by: Stijn Segers <foss@volatilesystems.org>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,12 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .71
 LINUX_VERSION-4.4 = .121
 LINUX_VERSION-4.9 = .87
-LINUX_VERSION-4.14 = .25
+LINUX_VERSION-4.14 = .27
 
 LINUX_KERNEL_HASH-3.18.71 = 5abc9778ad44ce02ed6c8ab52ece8a21c6d20d21f6ed8a19287b4a38a50c1240
 LINUX_KERNEL_HASH-4.4.121 = 44a88268b5088dc326b30c9b9133ac35a9a200b636b7268d08f32abeae6ca729
 LINUX_KERNEL_HASH-4.9.87 = 7ac9f6af69dc5a7e38bf35cc3fa889e3a4b22504a85f57fdc87734a8abe4c917
-LINUX_KERNEL_HASH-4.14.25 = 6dcfbf79c068e51c1b06edb1ce58ddc9ca351f862bf2a144e96106ec3f21e587
+LINUX_KERNEL_HASH-4.14.27 = 0911a449c71a06c7232ece07dfcc041bd07efaf7e67799b9e2d0cf4bd9a028ea
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
