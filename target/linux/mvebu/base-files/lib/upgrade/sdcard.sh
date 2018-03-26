e1fe4a93debcd6e14b0167f21f2329e275f8fb11
Rosen Penev <rosenp@gmail.com>
2018-03-25 19:14:20 -0700

    mvebu: Get rid of RTC hack for Turris Omnia.
    As Solidrun's RTC patch got merged, this hack is no longer needed.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- target/linux/mvebu/base-files/lib/upgrade/sdcard.sh
+++ target/linux/mvebu/base-files/lib/upgrade/sdcard.sh
@@ -28,7 +28,7 @@ platform_do_upgrade_sdcard() {
 		fw_setenv openwrt_bootargs 'earlyprintk console=ttyS0,115200 root=/dev/mmcblk0p2 rootfstype=auto rootwait'
 		fw_setenv openwrt_mmcload 'setenv bootargs "$openwrt_bootargs cfg80211.freg=$regdomain"; fatload mmc 0 0x01000000 zImage; fatload mmc 0 0x02000000 armada-385-turris-omnia.dtb'
 		fw_setenv factory_mmcload 'setenv bootargs "$bootargs cfg80211.freg=$regdomain"; btrload mmc 0 0x01000000 boot/zImage @; btrload mmc 0 0x02000000 boot/dtb @'
-		fw_setenv mmcboot 'mw 0xf10184a0 0xfd4d4cfa; run openwrt_mmcload || run factory_mmcload; bootz 0x01000000 - 0x02000000'
+		fw_setenv mmcboot 'run openwrt_mmcload || run factory_mmcload; bootz 0x01000000 - 0x02000000'
 		;;
 	esac
 
