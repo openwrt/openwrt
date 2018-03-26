3097487a2ef26fce2607e44f8771b84cb4d88412
Damir Samardzic <damir.samardzic@sartura.hr>
2018-03-26 09:37:58 +0000

    mvebu: add support for MACCHIATObin (cortex-a72)
    Add initial support for Marvell MACCHIATObin, cortex-a72 based Marvell
    ARMADA 8040 Community board. Comes in two forms: Single Shot and Double
    Shot.
    
    Specifications:
    - Quad core Cortex-A72 (up to 2GHz)
    - DDR4 DIMM slot with optional ECC and single/dual chip select support
    - Dual 10GbE (1/2.5/10GbE) via copper or SFP
      2.5GbE (1/2.5GbE) via SFP
      1GbE via copper
    - SPI Flash
    - 3 X SATA 3.0 connectors
    - MicroSD connector
    - eMMC
    - PCI x4 3.0 slot
    - USB 2.0 Headers (Internal)
    - USB 3.0 connector
    - Console port (UART) over microUSB connector
    - 20-pin Connector for CPU JTAG debugger
    - 2 X UART Headers
    - 12V input via DC Jack
    - ATX type power connector
    - Form Factor: Mini-ITX (170 mm x 170 mm)
    
    More details at http://macchiatobin.net
    
    Booting from micro SD card:
     1. reset U-Boot environment:
          env default -a
          saveenv
    
     2. prepare U-Boot with boot script:
          setenv bootcmd "load mmc 1:1 0x4d00000 boot.scr; source 0x4d00000"
          saveenv
    
       or manually:
          setenv fdt_name armada-8040-mcbin.dtb
          setenv image_name Image
          setenv bootcmd 'mmc dev 1; ext4load mmc 1:1 $kernel_addr $image_name;ext4load mmc 1:1 $fdt_addr $fdt_name;setenv   bootargs $console root=/dev/mmcblk1p2 rw rootwait; booti $kernel_addr - $fdt_addr'
          saveenv
    
    Signed-off-by: Damir Samardzic <damir.samardzic@sartura.hr>

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -16,7 +16,7 @@ platform_do_upgrade() {
 	armada-385-linksys-caiman|armada-385-linksys-cobra|armada-385-linksys-rango|armada-385-linksys-shelby|armada-xp-linksys-mamba)
 		platform_do_upgrade_linksys "$ARGV"
 		;;
-	armada-385-turris-omnia|armada-388-clearfog-base|armada-388-clearfog-pro|globalscale,espressobin)
+	armada-385-turris-omnia|armada-388-clearfog-base|armada-388-clearfog-pro|globalscale,espressobin|marvell,armada8040-mcbin)
 		platform_do_upgrade_sdcard "$ARGV"
 		;;
 	*)
@@ -29,7 +29,7 @@ platform_copy_config() {
 	armada-385-linksys-caiman|armada-385-linksys-cobra|armada-385-linksys-rango|armada-385-linksys-shelby|armada-xp-linksys-mamba)
 		platform_copy_config_linksys
 		;;
-	armada-385-turris-omnia|armada-388-clearfog-base|armada-388-clearfog-pro|globalscale,espressobin)
+	armada-385-turris-omnia|armada-388-clearfog-base|armada-388-clearfog-pro|globalscale,espressobin|marvell,armada8040-mcbin)
 		platform_copy_config_sdcard "$ARGV"
 		;;
 	esac
