openwrt/openwrt

commit 4f4fc993db4cd074fb2b51586dadd10e882604ec

base-files: add more name source to get_dt_led helper function
Not all LED driver are using the label devicetree property for the led
name. Add support for the TI/National Semiconductor LP55xx Led Drivers,
which are using the chan-name property for the led name, as fallback.

Signed-off-by: Mathias Kresin <dev@kresin.me>

package/base-files/files/lib/functions/leds.sh
@@ -8,7 +8,10 @@ get_dt_led() {
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")
-	[ -n "$ledpath" ] && label=$(cat "$basepath$ledpath/label")
+	[ -n "$ledpath" ] && \
+		label=$(cat "$basepath$ledpath/label" 2>/dev/null) || \
+		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null) || \
+		echo "led label not found" 1>&2
 
 	echo "$label"
 }

commit 3a4bf25fd6828fcc7172c36601774f8a279d7692

base-files: get_dt_led: don't warn about missing led
It's intentional that some boards within a target don't have all LEDs
which are tried to be setup in a common script. Don't show a warning in
such cases.

Fixes: 4f4fc993db4c ("base-files: add more name source to get_dt_led helper function")
Signed-off-by: Mathias Kresin <dev@kresin.me>

package/base-files/files/lib/functions/leds.sh
@@ -10,8 +10,7 @@ get_dt_led() {
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")
 	[ -n "$ledpath" ] && \
 		label=$(cat "$basepath$ledpath/label" 2>/dev/null) || \
-		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null) || \
-		echo "led label not found" 1>&2
+		label=$(cat "$basepath$ledpath/chan-name" 2>/dev/null)
 
 	echo "$label"
 }