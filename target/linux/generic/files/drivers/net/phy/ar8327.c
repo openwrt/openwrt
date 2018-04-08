cb7ac151cd13ca510588b80c24645f2add1b3737
Felix Fietkau <nbd@nbd.name>
2018-04-08 09:27:09 +0200

    ar8216: reduce latency even further for ar8327 chips
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- target/linux/generic/files/drivers/net/phy/ar8327.c
+++ target/linux/generic/files/drivers/net/phy/ar8327.c
@@ -1083,8 +1083,10 @@ ar8327_wait_atu_ready(struct ar8xxx_priv *priv, u16 r2, u16 r1)
 {
 	int timeout = 20;
 
-	while (ar8xxx_mii_read32(priv, r2, r1) & AR8327_ATU_FUNC_BUSY && --timeout)
-                udelay(10);
+	while (ar8xxx_mii_read32(priv, r2, r1) & AR8327_ATU_FUNC_BUSY && --timeout) {
+		udelay(10);
+		cond_resched();
+	}
 
 	if (!timeout)
 		pr_err("ar8327: timeout waiting for atu to become ready\n");
