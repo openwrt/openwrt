60750ab505bae8d1cb70c6fe495b755b213a1ebf
Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>
2018-05-04 10:27:50 -0700

    at91: reorganize at91 subtargets
    reorganizing at91 subtargets based on sama5 soc features and this fix
    below problems.
      1. able to set neon flags to sama5d2 & sama5d4 subtargets.
      2. fix the make clean which removes all the subtargets in bin folder.
      3. able to configure kernel specific to subtarget.
      4. able to set vfpu4 flags to samad3 subtargets.
    
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>

--- target/linux/at91/sama5/target.mk
+++
@@ -1,9 +0,0 @@
-BOARDNAME:=SAMA5 boards(Cortex-A5)
-CPU_TYPE:=cortex-a5
-
-DEFAULT_PACKAGES += kmod-usb2
-
-define Target/Description
-	Build generic firmware for Microchip(Atmel AT91) SAMA5 MPU's
-	using the ARMv7 instruction set.
-endef
