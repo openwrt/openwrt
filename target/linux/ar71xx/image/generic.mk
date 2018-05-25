312d7a4a5aaac8df714da5ad25d8b83a09c9e68c
Luo chongjun <luochongjun@gl-inet.com>
2018-05-25 18:06:37 +0800

    ar71xx: add support for GL.iNet GL-AR750S
    This patch adds supports for GL-AR750S.
    
    Specification:
    - SOC: QCA9563 (775MHz)
    - Flash: 16 MiB (W25Q128FVSG)
    - RAM: 128 MiB DDR2
    - Ethernet: 2x 1Gbps LAN + 1x 1Gbps WAN
    - Wireless: 2.4GHz (bgn) and 5GHz (ac)
    - USB: 1x USB 2.0 port
    - Button: 1x switch button, 1x reset button
    - LED: 3x LEDS (green)
    
    Flash instruction:
    Apply factory image via web-gui.
    
    Signed-off-by: Luo chongjun <luochongjun@gl-inet.com>

--- target/linux/ar71xx/image/generic.mk
+++ target/linux/ar71xx/image/generic.mk
@@ -461,6 +461,19 @@ define Device/gl-ar750
 endef
 TARGET_DEVICES += gl-ar750
 
+define Device/gl-ar750s
+  DEVICE_TITLE := GL.iNet GL-AR750S
+  DEVICE_PACKAGES := kmod-ath10k ath10k-firmware-qca9887 kmod-usb-core \
+	kmod-usb2 kmod-usb-storage
+  BOARDNAME := GL-AR750S
+  SUPPORTED_DEVICES := gl-ar750s
+  IMAGE_SIZE := 16000k
+  MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env),64k(art)ro,-(firmware)
+  IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs | append-metadata | check-size $$$$(IMAGE_SIZE)
+endef
+TARGET_DEVICES += gl-ar750s
+
 define Device/gl-domino
   DEVICE_TITLE := GL.iNet Domino Pi
   DEVICE_PACKAGES := kmod-usb-core kmod-usb2
