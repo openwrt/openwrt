974a2b1aa9b0f8bd7a7442efa069d954e6b354ce
LoveSy <shana@zju.edu.cn>
2018-05-28 09:47:19 +0800

    ramips: mmc: Add SD card support to HC5661A
    Tested on HC5661A and it now fixes the issue that when enabling sd card
    in HC5661A, the wan and 3 lan ports will down.
    
    Known issue:
    - When enabling SD card support, the led light of system will down and the rest 2 lights keep working.
    
    Signed-off-by: LoveSy <shana@zju.edu.cn>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -54,6 +54,7 @@ define Device/hc5661a
   DTS := HC5661A
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_TITLE := HiWiFi HC5661A
+  DEVICE_PACKAGES := kmod-sdhci-mt7620
 endef
 TARGET_DEVICES += hc5661a
 
