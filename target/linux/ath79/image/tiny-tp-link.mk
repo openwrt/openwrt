ba5b6c619b21c36a0bc1c1ed02c56f3cc05ddf60
Alex Maclean <monkeh@monkeh.net>
2018-05-30 14:18:54 +0100

    ath79: add TP-Link TL-WR703N port
    Signed-off-by: Alex Maclean <monkeh@monkeh.net>

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -1,6 +1,16 @@
 include ./common-tp-link.mk
 
 
+define Device/tl-wr703n
+  $(Device/tplink-4mlzma)
+  ATH_SOC := ar9331
+  DEVICE_TITLE := TP-Link TL-WR703N
+  DEVICE_PACKAGES := kmod-usb-chipidea2
+  TPLINK_HWID := 0x07030101
+  SUPPORTED_DEVICES := tplink,tl-wr703n tl-wr703n
+endef
+TARGET_DEVICES += tl-wr703n
+
 define Device/tl-wr740n-v2
   $(Device/tplink-4m)
   ATH_SOC := ar7240
