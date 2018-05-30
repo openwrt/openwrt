d215d0f9ae201d1ca6a50b809337a08e0813b9c9
Alex Maclean <monkeh@monkeh.net>
2018-05-30 14:18:53 +0100

    ath79: add TP-Link TL-WR740N/ND v2 port
    Signed-off-by: Alex Maclean <monkeh@monkeh.net>

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -1,2 +1,11 @@
 include ./common-tp-link.mk
 
+
+define Device/tl-wr740n-v2
+  $(Device/tplink-4m)
+  ATH_SOC := ar7240
+  DEVICE_TITLE := TP-Link TL-WR740N/ND v2
+  TPLINK_HWID := 0x07400001
+  SUPPORTED_DEVICES := tplink,tl-wr740n-v2 tl-wr740n-v2
+endef
+TARGET_DEVICES += tl-wr740n-v2
