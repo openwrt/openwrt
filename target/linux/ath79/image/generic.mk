1e0aeb2b55d59a6944e2f1cac1cca71f5d79ce83
Zoltan HERPAI <wigyori@uid0.hu>
2018-06-13 10:51:51 +0200

    ath79: move Netgear build code into a common makefile
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1,26 +1,4 @@
-define Build/netgear-squashfs
-	rm -rf $@.fs $@.squashfs
-	mkdir -p $@.fs/image
-	cp $@ $@.fs/image/uImage
-	$(STAGING_DIR_HOST)/bin/mksquashfs-lzma \
-		$@.fs $@.squashfs \
-		-noappend -root-owned -be -b 65536 \
-		$(if $(SOURCE_DATE_EPOCH),-fixed-time $(SOURCE_DATE_EPOCH))
-
-	dd if=/dev/zero bs=1k count=1 >> $@.squashfs
-	mkimage \
-		-A mips -O linux -T filesystem -C none \
-		-M $(NETGEAR_KERNEL_MAGIC) \
-		-a 0xbf070000 -e 0xbf070000 \
-		-n 'MIPS $(VERSION_DIST) Linux-$(LINUX_VERSION)' \
-		-d $@.squashfs $@
-	rm -rf $@.squashfs $@.fs
-endef
-
-define Build/netgear-uImage
-	$(call Build/uImage,$(1) -M $(NETGEAR_KERNEL_MAGIC))
-endef
-
+include ./common-netgear.mk
 
 define Device/avm_fritz300e
   ATH_SOC := ar7242
