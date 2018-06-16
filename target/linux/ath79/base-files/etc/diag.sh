25079bdb2e355be58db049dd62cdb82f9ca68ddc
Johann Neuhauser <johann@it-neuhauser.de>
2018-06-16 08:13:09 +0200

    ath79: clean up led config script files for all devices
    1. Move all status led definitions out of diag.sh into device dt
       as we use here a generic script hook.
    
    2. Encode "phy0ptp" led trigger in dt of device for all ath79 devices.
    
    Signed-off-by: Johann Neuhauser <johann@it-neuhauser.de>

--- target/linux/ath79/base-files/etc/diag.sh
+++ target/linux/ath79/base-files/etc/diag.sh
@@ -4,32 +4,7 @@
 
 status_led="$(get_dt_led status)"
 
-get_status_led() {
-	local board=$(board_name)
-	local boardname="${board##*,}"
-
-	case $board in
-	"avm,fritz300e")
-		status_led="${boardname}:green:power"
-		;;
-	"embeddedwireless,dorin")
-		status_led="dorin:green:status"
-		;;
-	"glinet,ar150")
-		status_led="gl-ar150:orange:wlan"
-		;;
-	"tplink,tl-wr1043nd-v1")
-		status_led="tp-link:green:system"
-		;;
-	"ubnt,unifi")
-		status_led="ubnt:green:dome"
-		;;
-	esac
-}
-
 set_state() {
-	[ -z "$status_led" ] && get_status_led
-
 	case "$1" in
 	preinit)
 		status_led_blink_preinit
