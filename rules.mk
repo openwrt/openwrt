da9d760ea1c0450e9e6b1c0cdd5331e633c75887
Syrone Wong <wong.syrone@gmail.com>
2018-06-21 23:08:11 +0800

    rules.mk: replace iremap when using GCC 8
    The original -iremap is replaced by -fmacro-prefix-map in GCC 8
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- rules.mk
+++ rules.mk
@@ -141,6 +141,10 @@ endif
 
 ifeq ($(or $(CONFIG_EXTERNAL_TOOLCHAIN),$(CONFIG_GCC_VERSION_4_8),$(CONFIG_TARGET_uml)),)
   iremap = -iremap$(1):$(2)
+  # just overwrite iremap for GCC 8.1 and higher to keep backward compatibility
+  ifeq ($(CONFIG_GCC_VERSION_8),y)
+    iremap = -fmacro-prefix-map=$(1)=$(2)
+  endif
 endif
 
 PACKAGE_DIR:=$(BIN_DIR)/packages
