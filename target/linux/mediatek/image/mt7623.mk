94118334574ae886da2ee97e3f2c895885f9b552
Nikolay Amiantov <ab@fmap.me>
2018-06-28 12:56:05 +0000

    mediatek: build SD images for BPi-R2
    As mentioned in kernel patches only SD image is supported for now, however most
    needed code for EMMC images is added.
    
    Signed-off-by: Nikolay Amiantov <ab@fmap.me>

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -12,6 +12,9 @@ TARGET_DEVICES += 7623a-unielec-u7623-02-emmc-512m
 define Device/7623n-bananapi-bpi-r2
   DEVICE_TITLE := MTK7623n BananaPi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
+  SUPPORTED_DEVICES := bananapi,bpi-r2
+  IMAGES := sysupgrade-sd.bin.gz
+  IMAGE/sysupgrade-sd.bin.gz := sysupgrade-bpi-r2-sd | gzip | append-metadata
 endef
 
 TARGET_DEVICES += 7623n-bananapi-bpi-r2
