1c47b7a9f9054de7594e268906675db66f4220c6
Bartvz <bartvanzoest@gmail.com>
2018-07-03 18:55:48 +0200

    mt7621 CPU_TYPE=1004kc

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -5,7 +5,7 @@
 SUBTARGET:=mt7621
 BOARDNAME:=MT7621 based boards
 FEATURES+=nand ramdisk rtc usb minor
-CPU_TYPE:=24kc
+CPU_TYPE:=1004kc
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
