b123921a92a957f08abb186e041aa38aa9328f3e
Matthias Schiffer <mschiffer@universe-factory.net>
2018-07-14 15:44:47 +0200

    include/prereq-build.mk: explicitly check for -f flag when using busybox time
    On Debian, busybox does have a time applet, but it does not support the -f
    flag. Catch this in prereq check to give users to proper error message.
    
    Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -138,10 +138,10 @@ $(eval $(call SetupHostCommand,bzip2,Please install 'bzip2', \
 $(eval $(call SetupHostCommand,wget,Please install GNU 'wget', \
 	wget --version | grep GNU))
 
-$(eval $(call SetupHostCommand,time,Please install GNU 'time' or BusyBox 'time', \
+$(eval $(call SetupHostCommand,time,Please install GNU 'time' or BusyBox 'time' that supports -f, \
 	gtime --version 2>&1 | grep GNU, \
 	time --version 2>&1 | grep GNU, \
-	busybox time 2>&1 | grep BusyBox))
+	busybox time 2>&1 | grep -- '-f FMT'))
 
 $(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
 	perl --version | grep "perl.*v5"))
