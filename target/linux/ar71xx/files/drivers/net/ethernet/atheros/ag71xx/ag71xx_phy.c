2655fbe8efb2a596aa8db92cffa817e3bf0c64ea
Daniel F. Dickinson <cshored@thecshore.com>
2018-07-25 19:14:26 -0400

    ar71xx: ag71xx_phy: Fix compilation for debug messages
    NB: Error only appears with ag71xx debug messages and dynamic printk
    enabled.  This is probably why no one has caught it before.
    
    Previously phy probe debug messages used old (now wrong) functions
    to get the phy name for printing.  There was also the chance of
    a NULL pointer in the event no phy_device was found.
    
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_phy.c
+++ target/linux/ar71xx/files/drivers/net/ethernet/atheros/ag71xx/ag71xx_phy.c
@@ -119,7 +119,8 @@ static int ag71xx_phy_connect_multi(struct ag71xx *ag)
 		DBG("%s: PHY found at %s, uid=%08x\n",
 			dev_name(dev),
 			dev_name(&ag->mii_bus->phy_map[phy_addr]->dev),
-			ag->mii_bus->phy_map[phy_addr]->phy_id);
+			&ag->mii_bus->phy_map[phy_addr]->phy_id),
+			&ag->mii_bus->phy_map[phy_addr]->phy_id : 0);
 
 		if (phydev == NULL)
 			phydev = ag->mii_bus->phy_map[phy_addr];
@@ -130,7 +131,8 @@ static int ag71xx_phy_connect_multi(struct ag71xx *ag)
 		DBG("%s: PHY found at %s, uid=%08x\n",
 			dev_name(dev),
 			dev_name(&ag->mii_bus->mdio_map[phy_addr]->dev),
-			ag->mii_bus->mdio_map[phy_addr]->phy_id);
+			mdiobus_get_phy(ag->mii_bus, phy_addr) ?
+			mdiobus_get_phy(ag->mii_bus, phy_addr)->phy_id : 0);
 
 		if (phydev == NULL)
 			phydev = mdiobus_get_phy(ag->mii_bus, phy_addr);
