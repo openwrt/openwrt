e3ab280e47000aee3dc0ea916bcb656490e9839d
Koen Vandeputte <koen.vandeputte@ncentric.com>
2018-08-08 09:54:51 +0200

    kernel: remove linux 3.18 support
    No targets are using it anymore
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/target.mk
+++ include/target.mk
@@ -51,7 +51,7 @@ else
   endif
 endif
 
-ifneq ($(filter 3.18 4.4 4.9,$(KERNEL_PATCHVER)),)
+ifneq ($(filter 4.4 4.9,$(KERNEL_PATCHVER)),)
   DEFAULT_PACKAGES.router:=$(filter-out kmod-ipt-offload,$(DEFAULT_PACKAGES.router))
 endif
 
