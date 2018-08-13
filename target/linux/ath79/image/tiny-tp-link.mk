cc35c91f56339a46138de84b680e33f0c633acb7
Johann Neuhauser <johann@it-neuhauser.de>
2018-08-13 15:07:55 +0200

    ath79: fix sysupgrade TP-Link WR841v9
    Remove SUPPORTED_DEVICES from wr841-v9 because it´s not needed and
    for consistency rename everything to tl-wr841-v9.
    
    Signed-off-by: Johann Neuhauser <johann@it-neuhauser.de>

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -123,7 +123,6 @@ define Device/tplink_tl-wr841-v9
   ATH_SOC := qca9533
   DEVICE_TITLE := TP-LINK TL-WR841N/ND v9
   TPLINK_HWID := 0x08410009
-  SUPPORTED_DEVICES += tl-wr841n-v9
 endef
 TARGET_DEVICES += tplink_tl-wr841-v9
 
