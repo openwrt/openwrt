8db6522531531348952b161fd4de46a7eca481d6
Johann Neuhauser <johann@it-neuhauser.de>
2018-08-13 15:28:51 +0200

    ath79: add support for TP-Link WR841N/ND v11
    Specification:
    - SoC: Atheros AR9533
    - Flash: 4 MB
    - RAM: 32 MB
    - Ethernet: 4x LAN (100M) / 1x WAN (100M)
    - WiFi: 2.4G 300M
    
    Flash instruction:
      Flash factory image from stock WebUI.
    
    Signed-off-by: Johann Neuhauser <johann@it-neuhauser.de>

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -126,6 +126,17 @@ define Device/tplink_tl-wr841-v9
 endef
 TARGET_DEVICES += tplink_tl-wr841-v9
 
+define Device/tplink_tl-wr841-v11
+  $(Device/tplink-4mlzma)
+  ATH_SOC := qca9533
+  DEVICE_TITLE := TP-LINK TL-WR841N/ND v11
+  TPLINK_HWID := 0x08410011
+  IMAGES += factory-us.bin factory-eu.bin
+  IMAGE/factory-us.bin := append-rootfs | mktplinkfw factory -C US
+  IMAGE/factory-eu.bin := append-rootfs | mktplinkfw factory -C EU
+endef
+TARGET_DEVICES += tplink_tl-wr841-v11
+
 define Device/tplink_tl-wr941-v4
   $(Device/tplink-4m)
   ATH_SOC := ar7240
