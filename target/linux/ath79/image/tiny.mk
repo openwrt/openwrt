b3fbc88a192ba0aa41e1f0c9e612b1ddd17eddfe
INAGAKI Hiroshi <musashino.open@gmail.com>
2018-08-14 15:01:05 +0900

    ath79: add support for Buffalo WHR-G301N
    Buffalo WHR-G301N is a 2.4 GHz 11n router, based on Atheros AR7240.
    Ported from ar71xx target.
    
    Specification:
    
    - Atheros AR7240
    - 32 MB of RAM
    - 4 MB of Flash
    - 2.4 GHz 2T2R wifi
    - 5x 10/100 Mbps Ethernet
    - 9x LEDs, 4x keys
      - LED: 8x gpio-leds, 1x ath9k-leds
      - key: 2x buttons, 1x slide switch
    - UART header on PCB
      - Vcc, GND, TX, RX from LEDs side
      - 115200n8
    
    Flash instruction using factory image:
    
    1. Connect the computer to the LAN port of WHR-G301N
    2. Connect power cable to WHR-G301N and turn on it
    3. Access to "http://192.168.11.1/" and open firmware update page
    ("ファーム更新")
    4. Select the OpenWrt factory image and click execute ("実行") button
    5. Wait ~150 seconds to complete flashing
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>

--- target/linux/ath79/image/tiny.mk
+++ target/linux/ath79/image/tiny.mk
@@ -1,5 +1,24 @@
 DEVICE_VARS += ROOTFS_SIZE
 
+define Build/buffalo-tftp-header
+	( \
+		echo -n -e "# Airstation Public Fmt1" | dd bs=32 count=1 conv=sync; \
+		dd if=$@; \
+	) > $@.new
+  mv $@.new $@
+endef
+
+define Build/buffalo-tag
+	$(eval product=$(word 1,$(1)))
+	$(STAGING_DIR_HOST)/bin/buffalo-tag \
+		-c 0x80041000 -d 0x801e8000 -w 3 \
+		-a ath -v 1.99 -m 1.01 -f 1 \
+		-b $(product) -p $(product) \
+		-r M_ -l mlang8 \
+		-i $@ -o $@.new
+	mv $@.new $@
+endef
+
 define Device/buffalo_bhr-4grv2
   ATH_SOC := qca9558
   DEVICE_TITLE := Buffalo BHR-4GRV2
@@ -16,3 +35,15 @@ define Device/buffalo_bhr-4grv2
   SUPPORTED_DEVICES += bhr-4grv2
 endef
 TARGET_DEVICES += buffalo_bhr-4grv2
+
+define Device/buffalo_whr-g301n
+  ATH_SOC := ar7240
+  DEVICE_TITLE := Buffalo WHR-G301N
+  IMAGE_SIZE := 3712k
+  IMAGES += factory.bin tftp.bin
+  IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | check-size $$$$(IMAGE_SIZE)
+  IMAGE/factory.bin := $$(IMAGE/default) | buffalo-enc WHR-G301N 1.99 | buffalo-tag WHR-G301N
+  IMAGE/tftp.bin := $$(IMAGE/default) | buffalo-tftp-header
+  SUPPORTED_DEVICES += WHRG301N
+endef
+TARGET_DEVICES += buffalo_whr-g301n
