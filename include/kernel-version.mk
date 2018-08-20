705262f4d305145136b9d7c6bab486714040c1c0
Koen Vandeputte <koen.vandeputte@ncentric.com>
2018-08-20 10:45:32 +0200

    kernel: bump 4.14 to 4.14.65
    Refreshed all patches.
    
    Compile-tested on: cns3xxx, imx6, x86_64
    Runtime-tested on: cns3xxx, imx6, x86_64
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,11 +2,11 @@
 
 LINUX_RELEASE?=1
 
-LINUX_VERSION-4.14 = .63
 LINUX_VERSION-4.9 = .122
+LINUX_VERSION-4.14 = .65
 
-LINUX_KERNEL_HASH-4.14.63 = cd2e52f0e7ba861afa91cf487b2f45e5174115870f256a1d65996647b7bcc6d3
 LINUX_KERNEL_HASH-4.9.122 = a46f90876b2cdb8ca1626e6489e76f8706dfd593a9d44fc91e68df47db6cf86c
+LINUX_KERNEL_HASH-4.14.65 = 12980c406bb9be670db58a88464d1c36cafd3c1d4d5ee09e8ecfa71e5eb5a5ec
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
