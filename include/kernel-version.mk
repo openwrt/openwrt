b4bd6c2c95bf1365338697e77c6a3001506a7e4b
Koen Vandeputte <koen.vandeputte@ncentric.com>
2018-09-06 14:21:05 +0200

    kernel: bump 3.18 to 3.18.121
    Refreshed all patches.
    
    Compile-tested on: adm5120
    Runtime-tested on: none
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,13 +2,13 @@
 
 LINUX_RELEASE?=1
 
-LINUX_VERSION-3.18 = .119
 LINUX_VERSION-4.9 = .124
 LINUX_VERSION-4.14 = .67
+LINUX_VERSION-3.18 = .121
 
-LINUX_KERNEL_HASH-3.18.119 = 2bab623ed868b679eac224f62212cc285264061bedf1e32897f72e35aa26160d
 LINUX_KERNEL_HASH-4.9.124 = 5ca6b0d48c8dd1265005169f1b97149a4e893668c0d8aa566a73c7663c8e4311
 LINUX_KERNEL_HASH-4.14.67 = 3f4b056dc27233a78f7a4a35ed6fdcfd0a9680ec40b611a898bb6c8b905070ba
+LINUX_KERNEL_HASH-3.18.121 = 7a8a996433e4af1abd2903f91c390295745d90dd178607b3af82f2df5f95c9b0
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
