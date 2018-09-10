265dcb05ea4e4ca7fd780f496f20b17327598338
Hartmut Knaack <knaack.h@gmx.de>
2018-09-11 01:33:36 +0200

    kernel: add TSL4531 ambient light sensor driver
    Add support for the TAOS TSL4531x family of ambient light sensors using
    the I2C bus.
    
    Signed-off-by: Hartmut Knaack <knaack.h@gmx.de>

--- package/kernel/linux/modules/iio.mk
+++ package/kernel/linux/modules/iio.mk
@@ -155,3 +155,21 @@ define KernelPackage/iio-htu21/description
 endef
 
 $(eval $(call KernelPackage,iio-htu21))
+
+
+define KernelPackage/iio-tsl4531
+  SUBMENU:=$(IIO_MENU)
+  DEPENDS:=+kmod-i2c-core +kmod-iio-core
+  TITLE:=TAOS TSL4531 ambient light sensor
+  KCONFIG:= CONFIG_TSL4531
+  FILES:=$(LINUX_DIR)/drivers/iio/light/tsl4531.ko
+  AUTOLOAD:=$(call AutoLoad,56,tsl4531)
+endef
+
+define KernelPackage/iio-tsl4531/description
+ Support for TAOS TSL4531x family of ambient light sensors
+ connected via I2C. Following models are usable:
+ TSL45311, TSL45313, TSL45315, TSL45317.
+endef
+
+$(eval $(call KernelPackage,iio-tsl4531))
