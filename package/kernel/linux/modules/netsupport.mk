8b1bf2da583f0140cc93db98105394ffbb6ca4bb
Tony Ambardar <itugrok@yahoo.com>
2018-10-30 05:50:51 -0700

    kernel/modules: kmod-sched-core: add missing dependency, useful module
    All tc ematch modules, including those in kmod-sched-core and kmod-sched,
    use cls_basic as a core dependency. Relocate cls_basic from kmod-sched to
    kmod-sched-core to avoid requiring kmod-sched unnecessarily.
    
    This change is also backwards compatible since any past tc ematch users
    will have had to install both kmod-sched-core and kmod-sched anyway.
    
    Add the matchall kernel module cls_matchall introduced in kernel 4.8. The
    matchall classifier matches every packet and allows the user to apply
    actions on it. It is a simpler, more efficient replacement for the common
    but cryptic tc classifier idiom "u32 match u32 0 0".
    
    Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -716,7 +716,7 @@ $(eval $(call KernelPackage,mppe))
 
 
 SCHED_MODULES = $(patsubst $(LINUX_DIR)/net/sched/%.ko,%,$(wildcard $(LINUX_DIR)/net/sched/*.ko))
-SCHED_MODULES_CORE = sch_ingress sch_fq_codel sch_hfsc sch_htb sch_tbf cls_fw cls_route cls_flow cls_tcindex cls_u32 em_u32 act_mirred act_skbedit
+SCHED_MODULES_CORE = sch_ingress sch_fq_codel sch_hfsc sch_htb sch_tbf cls_basic cls_fw cls_route cls_flow cls_tcindex cls_u32 em_u32 act_mirred act_skbedit cls_matchall
 SCHED_MODULES_FILTER = $(SCHED_MODULES_CORE) act_connmark sch_netem
 SCHED_MODULES_EXTRA = $(filter-out $(SCHED_MODULES_FILTER),$(SCHED_MODULES))
 SCHED_FILES = $(patsubst %,$(LINUX_DIR)/net/sched/%.ko,$(filter $(SCHED_MODULES_CORE),$(SCHED_MODULES)))
@@ -725,6 +725,7 @@ SCHED_FILES_EXTRA = $(patsubst %,$(LINUX_DIR)/net/sched/%.ko,$(SCHED_MODULES_EXT
 define KernelPackage/sched-core
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=Traffic schedulers
+  DEPENDS:=@!LINUX_3_18
   KCONFIG:= \
 	CONFIG_NET_SCHED=y \
 	CONFIG_NET_SCH_HFSC \
@@ -734,6 +735,7 @@ define KernelPackage/sched-core
 	CONFIG_NET_SCH_FQ_CODEL \
 	CONFIG_NET_CLS=y \
 	CONFIG_NET_CLS_ACT=y \
+	CONFIG_NET_CLS_BASIC \
 	CONFIG_NET_CLS_FLOW \
 	CONFIG_NET_CLS_FW \
 	CONFIG_NET_CLS_ROUTE4 \
@@ -741,6 +743,7 @@ define KernelPackage/sched-core
 	CONFIG_NET_CLS_U32 \
 	CONFIG_NET_ACT_MIRRED \
 	CONFIG_NET_ACT_SKBEDIT \
+	CONFIG_NET_CLS_MATCHALL \
 	CONFIG_NET_EMATCH=y \
 	CONFIG_NET_EMATCH_U32
   FILES:=$(SCHED_FILES)
@@ -780,7 +783,6 @@ define KernelPackage/sched
 	CONFIG_NET_SCH_TEQL \
 	CONFIG_NET_SCH_FQ \
 	CONFIG_NET_SCH_PIE \
-	CONFIG_NET_CLS_BASIC \
 	CONFIG_NET_ACT_POLICE \
 	CONFIG_NET_ACT_IPT \
 	CONFIG_NET_EMATCH_CMP \
