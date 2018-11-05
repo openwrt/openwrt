860de2e1aa9deac7dce76b7f85cb5fcfbba00468
Koen Vandeputte <koen.vandeputte@ncentric.com>
2018-11-05 12:53:55 +0100

    ar71xx: disable some tiny targets with kernel 4.14
    The bump to kernel 4.14 caused a massive increase in kernel size.
    
    For most targets, switching them to dynamic partitioning allowed
    to cope with this.
    
    On some targets, the kernel partition is located behind the rootfs,
    which disallows switching to dynamic partitioning as the boot location
    would be altered, requiring a u-boot change.
    
    Also within the tiny section, which disables kernel symbols etc
    to decrease the image size,  the partition size is still too small.
    
    Disable these targets for now, fixing image generation:
    
    - Buffalo BHR-4GRV2
    - Zbtlink ZBT-WE1526
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- target/linux/ar71xx/image/tiny.mk
+++ target/linux/ar71xx/image/tiny.mk
@@ -9,7 +9,6 @@ define Device/bhr-4grv2
   IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_SIZE) | append-kernel | check-size $$$$(IMAGE_SIZE)
   IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | mkbuffaloimg
 endef
-TARGET_DEVICES += bhr-4grv2
 
 define Device/zbt-we1526
   DEVICE_TITLE := Zbtlink ZBT-WE1526
@@ -21,4 +20,3 @@ define Device/zbt-we1526
   MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env)ro,14528k(rootfs),1472k(kernel),64k(art)ro,16000k@0x50000(firmware)
   IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_SIZE) | append-kernel | check-size $$$$(IMAGE_SIZE)
 endef
-TARGET_DEVICES += zbt-we1526
