ff70f400c59297d14244f504a9a1193d2589e5f6
Chuanhong Guo <gch981213@gmail.com>
2018-12-05 23:54:00 +0800

    ramips: disable sdhc for HC5661A
    Currently OpenWrt doesn't support switching MT7628 into AP mode
    (which is done by writing some undocumented registers in MTK SDK)
    Without doing so, enabling SD breaks 4 FE ports and the SD controller
    doesn't work since SD pins aren't configured correctly.
    
    Disable SDHC on HC5661A to recover the 4 FE ports.
    
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -65,7 +65,6 @@ define Device/hc5661a
   DTS := HC5661A
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_TITLE := HiWiFi HC5661A
-  DEVICE_PACKAGES := kmod-sdhci-mt7620
 endef
 TARGET_DEVICES += hc5661a
 
