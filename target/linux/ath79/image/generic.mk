3dfc07d186e10ddfd17be302e76ab5e5b1397b93
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2018-12-10 15:44:56 +0100

    ath79: add support for COMFAST CF-E110N
    This patch adds support for the COMFAST CF-E110N, an outdoor wireless
    CPE with two Ethernet ports and a 802.11bgn radio.
    
    Specifications:
    
     - 650/400/216 MHz (CPU/DDR/AHB)
     - 2x 10/100 Mbps Ethernet, both with PoE-in support
     - 64 MB of RAM (DDR2)
     - 16 MB of FLASH
     - 2T2R 2.4 GHz, up to 26 dBm
     - 11 dBi built-in antenna
     - POWER/LAN/WAN/WLAN green LEDs
     - 4x RSSI LEDs (2x red, 2x green)
     - UART (115200 8N1) and GPIO (J9) headers on PCB
    
    Flashing instructions:
    
     The original firmware is based on OpenWrt so a sysupgrade image can be
     installed via the stock web GUI. Settings from the original firmware
     will be saved and restored on the new want, so a factory reset will be
     needed: once the new firmware is flashed, perform the factory reset by
     pushing the reset button several times during the boot process, while the
     WAN LED flashes, until it starts flashing quicker.
    
     The U-boot bootloader contains a recovery HTTP server to upload the
     firmware. Push the reset button while powering the device on and keep it
     pressed for >10 seconds. The recovery page will be at http://192.168.1.1
    
    Notes:
    
     The device is advertised, sold and labeled as "CF-E110N", but the
     bootloader and the stock firmware identify it as "v2".
    
    Acknowledgments:
    
     Petr Štetiar <ynezz@true.cz>
     Sebastian Kemper <sebastian_ml@gmx.net>
     Chuanhong Guo <gch981213@gmail.com>
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>
    [drop unused labels from devicetree source file]
    Signed-off-by: Mathias Kresin <dev@kresin.me>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -156,6 +156,14 @@ define Device/buffalo_wzr-hp-g450h
 endef
 TARGET_DEVICES += buffalo_wzr-hp-g450h
 
+define Device/comfast_cf-e110n-v2
+  ATH_SOC := qca9533
+  DEVICE_TITLE := COMFAST CF-E110N v2
+  DEVICE_PACKAGES := rssileds kmod-leds-gpio -swconfig -uboot-envtools
+  IMAGE_SIZE := 16192k
+endef
+TARGET_DEVICES += comfast_cf-e110n-v2
+
 define Device/devolo_dvl1200e
   ATH_SOC := qca9558
   DEVICE_TITLE := devolo WiFi pro 1200e
