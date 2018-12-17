11945174cf182a526158f0831b7153fff4d3ec6b
Petr Štetiar <ynezz@true.cz>
2018-12-18 00:19:30 +0100

    kernel: Fix usb-chipidea dependency on ulpi.ko for 4.19
    It seems, that since Linux 4.18-rc2 ci_hdrc depends on ulpi.
    
     commit a930d8bd94d8db7715d1af74299f710b1fb22fc8
     Author: Fabio Estevam <fabio.estevam@nxp.com>
     Date:   Wed Jul 4 10:09:58 2018 -0300
    
        usb: chipidea: Always build ULPI code
    
        Commit 03e6275ae381 ("usb: chipidea: Fix ULPI on imx51") causes a kernel
        hang on imx51 systems that use the ULPI interface and do not select the
        CONFIG_USB_CHIPIDEA_ULPI option.
    
        In order to avoid such potential misuse, let's always build the
        chipidea ULPI code into the final ci_hdrc object.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1565,7 +1565,8 @@ define KernelPackage/usb-chipidea
   FILES:= \
 	$(LINUX_DIR)/drivers/extcon/extcon.ko@lt4.9 \
 	$(LINUX_DIR)/drivers/extcon/extcon-core.ko@ge4.9 \
-	$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc.ko
+	$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc.ko \
+	$(LINUX_DIR)/drivers/usb/common/ulpi.ko@ge4.18
   AUTOLOAD:=$(call AutoLoad,39,ci_hdrc,1)
   $(call AddDepends/usb)
 endef
