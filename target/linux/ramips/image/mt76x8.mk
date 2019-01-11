69ae8d6c8e39481a3a47c3d37a2b1028e0a16176
Tobias Welz <tw@wiznet.eu>
2019-01-12 00:37:43 +0100

    ramips: add support for WIZnet WizFi630S board
    The WIZnet WizFi630S board is in the miniPCIe form factor.
    
    SoC: Mediatek MT7688AN
    RAM: 128MB
    Flash: 32Mb
    WiFi: 2.4GHz
    Ethernet: 3x 100Mbit
    USB: 1 (USB 2.0)
    serial ports: 2 (1x full, 1xlite)
    
    Flash and recovery instructions: Use the factory installed u-boot boot
    loader. It is available on UART2 (115200,8,n,1).  Then get the
    sysupgrade image from a tftp server.
    
    Signed-off-by: Tobias Welz <tw@wiznet.eu>
    [whitespace and device name in makefile fixes]
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -408,6 +408,13 @@ define Device/widora_neo-32m
 endef
 TARGET_DEVICES += widora_neo-32m
 
+define Device/wiznet_wizfi630s
+  DTS := WIZFI630S
+  IMAGE_SIZE := $(ralink_default_fw_size_32M)
+  DEVICE_TITLE := WIZnet WizFi630S
+endef
+TARGET_DEVICES += wiznet_wizfi630s
+
 define Device/wrtnode2p
   DTS := WRTNODE2P
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
