1c2f7b511514d43d8a826b26e09817b2750a2bf4
Django Armstrong <iamdjango@hotmail.com>
2019-01-20 16:46:42 +0000

    ath79: add support for Devolo WiFi Pro 1750e
    Hardware
    --------
    CPU:   Qualcomm Atheros QCA9558
    RAM:   128M DDR2
    FLASH: 16MiB
    ETH:   1x Atheros AR8035 (PoE in)
           1x Atheros AR8033
    WiFi2: QCA9558 3T3R (SiGE SE2565T 2.4 GHz power amp x3)
    WiFi5: QCA9880 3T3R (Skyworks 5003L1 5 GHz power amp x3)
    BTN:   1x Reset
           1x WPS
           1x USB eject
    LED:   1x LED blue
           1x LED red
    BEEP:  1x GPIO attached piezo beeper
    UART:  3.3V GND TX RX (115200-N-8) (3.3V is pin closest to rear ports)
           Dupont 4 pin header
           Rear RJ45 serial port non-functional
    USB:   1x v2.0
    
    Installation
    ------------
    Make sure you set a password for the root user as prompted on first
    setup!
    
    1. Upload OpenWRT sysupgrade image via SSH to the device.
    Use /tmp as the destination folder on the device.
    User is root, password previously set in the web interface.
    
    2. Install OpenWRT with
    
    > sysupgrade -n -F /tmp/<openwrt-image-name>
    
    Signed-off-by: Django Armstrong <iamdjango@hotmail.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -186,6 +186,14 @@ define Device/devolo_dvl1750c
 endef
 TARGET_DEVICES += devolo_dvl1750c
 
+define Device/devolo_dvl1750e
+  ATH_SOC := qca9558
+  DEVICE_TITLE := devolo WiFi pro 1750e
+  DEVICE_PACKAGES := kmod-usb2 kmod-ath10k-ct ath10k-firmware-qca988x-ct
+  IMAGE_SIZE := 15936k
+endef
+TARGET_DEVICES += devolo_dvl1750e
+
 define Device/devolo_dvl1750i
   ATH_SOC := qca9558
   DEVICE_TITLE := devolo WiFi pro 1750i
