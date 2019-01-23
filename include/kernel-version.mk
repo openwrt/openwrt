11bfb39e394b80dce5de28d16083ac0f59f4002d
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-01-23 14:10:59 +0100

    kernel: bump 4.14 to 4.14.95
    Refreshed all patches.
    
    Removed superseded patches:
    - 0400-Revert-MIPS-smp-mt-Use-CPU-interrupt-controller-IPI-.patch
    
    Add upstream patch to fix build error on ipq806x
    - 420-enable-CONFIG_MMC_SDHCI_IO_ACCESSORS.patch
    
    Compile-tested on: cns3xxx, imx6, ipq806x
    Runtime-tested on: cns3xxx, imx6
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -3,10 +3,10 @@
 LINUX_RELEASE?=1
 
 LINUX_VERSION-4.9 = .152
-LINUX_VERSION-4.14 = .94
+LINUX_VERSION-4.14 = .95
 
 LINUX_KERNEL_HASH-4.9.152 = 90e47b85c09af47eefafe851685ee731538f640b0650a6a9cfa0234436708e39
-LINUX_KERNEL_HASH-4.14.94 = e728518bb024209acc222e803cdc00d0ea2b1f4ebf28a8ed17a639171a1c23f1
+LINUX_KERNEL_HASH-4.14.95 = ce6729e3fca312520e3cb4f27993852dbb019d94c59c0b35cedab571f9cb58e4
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
