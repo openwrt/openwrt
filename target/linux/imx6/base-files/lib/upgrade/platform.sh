e0566a12808167de007e9edaa939026f99995319
Petr Štetiar <ynezz@true.cz>
2019-01-26 17:07:37 +0100

    fixup! imx6: Add support for Toradex Apalis family of computer on modules
    Replace hardcoded sysupgrade-apalis dirname with more generic name based
    on board_name to support more boards using Apalis SoCs.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- target/linux/imx6/base-files/lib/upgrade/platform.sh
+++ target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -23,9 +23,12 @@ apalis_copy_config() {
 }
 
 apalis_do_upgrade() {
+	local board_name=$(board_name)
+	board_name=${board_name/,/_}
+
 	apalis_mount_boot
-	get_image "$1" | tar Oxf - sysupgrade-apalis/kernel > /boot/uImage
-	get_image "$1" | tar Oxf - sysupgrade-apalis/root > $(rootpart_from_uuid)
+	get_image "$1" | tar Oxf - sysupgrade-${board_name}/kernel > /boot/uImage
+	get_image "$1" | tar Oxf - sysupgrade-${board_name}/root > $(rootpart_from_uuid)
 	sync
 	umount /boot
 }
