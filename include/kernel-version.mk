f003d732d726758f11f0c308758637c434c37c1a
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-01-28 13:07:54 +0100

    kernel: bump 4.19 to 4.19.18
    Refreshed all patches.
    
    Removed upstreamed:
    - 031-v5.0-MIPS-BCM47XX-Setup-struct-device-for-the-SoC.patch
    - 142-jffs2-Fix-use-of-uninitialized-delayed_work-lockdep-.patch
    
    Removed upstreamed hunk in:
    - 800-bcma-get-SoC-device-struct-copy-its-DMA-params-to-th.patch
    
    Compile-tested on: cns3xxx
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,12 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .133
 LINUX_VERSION-4.9 = .153
 LINUX_VERSION-4.14 = .96
-LINUX_VERSION-4.19 = .16
+LINUX_VERSION-4.19 = .18
 
 LINUX_KERNEL_HASH-3.18.133 = 3ec7f47365a8a050e629a5016e90e38a800e840c844901c979e9e796f8dc6711
 LINUX_KERNEL_HASH-4.9.153 = 9066929ec2550794ae107350a5f3c5b648438aa915cfc62bac5b7a54b9d7731a
 LINUX_KERNEL_HASH-4.14.96 = 110daeae1a416b7e0ec8dce5e86d67552deeb4567f696c3869389be239f0ecb5
-LINUX_KERNEL_HASH-4.19.16 = d8a088381fe3e7e5484c060dabcdda4b053ef7114f91cfd56db003a89bb11bdf
+LINUX_KERNEL_HASH-4.19.18 = 5ffdc068f9bf768b7cd5a8a2271d1370c2326e967848b13e21dfdbb1ef0ff3f1
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
