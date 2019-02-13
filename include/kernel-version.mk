6b6f238b82d1875a04f2dd6b7071dc429b66a5f7
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-02-13 11:38:41 +0100

    kernel: bump 4.19 to 4.19.21
    Refreshed all patches.
    
    Remove upstreamed:
    - 0007-ARM-dts-Fix-up-the-D-Link-DIR-685-MTD-partition-info.patch
    
    Compile-tested on: cns3xxx
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,12 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .134
 LINUX_VERSION-4.9 = .156
 LINUX_VERSION-4.14 = .99
-LINUX_VERSION-4.19 = .20
+LINUX_VERSION-4.19 = .21
 
 LINUX_KERNEL_HASH-3.18.134 = 36bdd04cab3b6c824a4b7e32ae02503f437e0916d5a4ff04c90aa22da2749c2f
 LINUX_KERNEL_HASH-4.9.156 = 5eb1b9ba43370512ab637452089bb93f8c0fdd7d5399e99561d382f74517a816
 LINUX_KERNEL_HASH-4.14.99 = 8ff98caed5b20b733dedcbe99559d71a0e09e239c0c2488b3fd799c96489eb0a
-LINUX_KERNEL_HASH-4.19.20 = dc7d2776dad4bf738e741ed05e7d1bea685855cfb7a62d1706f5f7aeabfa04a4
+LINUX_KERNEL_HASH-4.19.21 = db08ede726fe410da274e1e98f0de2aed9ff4b0e0b63a89e5928bef46998bbc1
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
