1da5478d1f540db6ec8ba1bbb8f34c8f5193ccee
Tomasz Maciej Nowak <tomek_n@o2.pl>
2019-03-04 15:14:18 +0100

    ath79: routerstation: prepare to use sysupgrade-tar format image
    In PR [1] introducing initial support for Ubiquiti RouterStation boards,
    Mathias Kresin suggested to replace the combined sysupgrade image with
    tarball generated by sysupgrade-tar.sh. This would simplify deployment
    of sysupgrade as the kernel size (needed to update FIS partition) could
    be simply calculated on the fly instead of reading value from combined
    image header. Unfortunately this would break sysupgrade compatibility
    between ar71xx image and ath79 image. Therefore this commit creates
    migration path to use new sysuprade image, it adds code to accept both
    of them at this moment. The plan is to keep it until new stable version
    is released. Then the image recipe should be changed to new format and
    compatibility code for old image removed.
    
    1. https://github.com/openwrt/openwrt/pull/1237
    
    Signed-off-by: Tomasz Maciej Nowak <tomek_n@o2.pl>

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -195,7 +195,8 @@ define Device/ubnt_routerstation_common
   IMAGE_SIZE := 16128k
   IMAGES += factory.bin
   IMAGE/factory.bin := append-rootfs | pad-rootfs | mkubntimage | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | combined-image | check-size $$$$(IMAGE_SIZE) | append-metadata
+  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | combined-image | check-size $$$$(IMAGE_SIZE)
+#  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | check-size $$$$(IMAGE_SIZE) | sysupgrade-tar rootfs=$$$$@ | append-metadata
   KERNEL := kernel-bin | append-dtb | lzma | pad-to $$(BLOCKSIZE)
   KERNEL_INITRAMFS := kernel-bin | append-dtb
 endef
