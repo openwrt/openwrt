d6fa04a437033d4bdfa1b0d90a4786bfe492f749
Daniel Golle <daniel@makrotopia.org>
2019-03-11 05:32:29 +0100

    IB: include SUPPORTED_DEVICES in 'make info' output
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- include/image.mk
+++ include/image.mk
@@ -558,6 +558,7 @@ Target-Profile: DEVICE_$(1)
 Target-Profile-Name: $(DEVICE_TITLE)
 Target-Profile-Packages: $(DEVICE_PACKAGES)
 Target-Profile-hasImageMetadata: $(if $(foreach image,$(IMAGES),$(findstring append-metadata,$(IMAGE/$(image)))),1,0)
+Target-Profile-SupportedDevices: $(SUPPORTED_DEVICES)
 Target-Profile-Description:
 $(DEVICE_DESCRIPTION)
 @@
