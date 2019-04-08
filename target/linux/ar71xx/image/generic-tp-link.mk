f8a578be85be99b86cb58c35fe27386281054e71
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-04-08 14:59:48 +0200

    ar71xx: Fix IMAGE_SIZE for TP-Link Archer C7 v5
    IMAGE_SIZE for C7v5 is wrong in openwrt-18.06, looks like it
    was just copied from C7v4. In master, this got fixed with the
    introduction of dynamic partitioning in
    https://github.com/openwrt/openwrt/commit/7c78be1b747eb0c8d64da67deb3a8aec75bd7b9c
    
    However, this is not connected to the changes introduced there,
    but also applies to the static partitioning in openwrt-18.06.
    It appears to be simply wrong at the moment ...
    
    Tested-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ar71xx/image/generic-tp-link.mk
+++ target/linux/ar71xx/image/generic-tp-link.mk
@@ -146,7 +146,7 @@ define Device/archer-c7-v5
   DEVICE_TITLE := TP-LINK Archer C7 v5
   BOARDNAME := ARCHER-C7-V5
   TPLINK_BOARD_ID := ARCHER-C7-V5
-  IMAGE_SIZE := 15104k
+  IMAGE_SIZE := 15360k
   MTDPARTS := spi0.0:128k(factory-uboot)ro,128k(u-boot)ro,64k@0x50000(art)ro,128k@0x60000(info)ro,1536k@0xc0000(kernel),13824k(rootfs),15360k@0xc0000(firmware)
   SUPPORTED_DEVICES := archer-c7-v5
 endef
