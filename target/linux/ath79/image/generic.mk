64493d42b41253aafe1538e325e0692db4201046
Rytis Zigmantavičius <rytis.z@8devices.com>
2019-04-11 19:31:23 +0300

    ath79: add support for 8devices Carambola2 development board
    Specifications:
    - Atheros AR9331 (400 MHz)
    - 64 MB of RAM (DDR2)
    - 16 MB of Flash (SPI)
    - 1T1R 2.4 Wlan (AR9331)
    - 2x 10/100 Mbps Ethernet
    - 3x LEDs, 1x gpio button
    - 1x USB 2.0, 5V
    - UART over usb, 115200n8
    
    Upgrading from ar71xx target:
    - Put image into board:
        scp openwrt-ath79-generic-8dev_carambola2-squashfs-sysupgrade.bin \
            root@192.168.1.1/tmp/
    - Run sysupgrade
        sysupgrade /tmp/sysupgrade.bin
    
    Upgrading from u-boot:
    - Set up tftp server with sysupgrade.bin image
    - Go to u-boot (reboot and press ESC when prompted)
    - Set TFTP server IP
        setenv serverip 192.168.1.254
    - Set device ip from same subnet
        setenv ipaddr 192.168.1.1
    - Copy new firmware to board
        tftpboot 0x81000000 sysupgrade.bin
    - erase flash
        erase 0x9f050000 +${filesize}
    - flash firmware
        cp.b 0x81000000 0x9f050000 ${filesize}
    - Reset board
        reset
    
    Signed-off-by: Rytis Zigmantavičius <rytis.z@8devices.com>
    [wrapped long line in commit description, whitespace and art address
     fix in DTS, keep default lan/wan setup, removed -n in sysupgrade]
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -75,6 +75,15 @@ define Device/seama
   SEAMA_SIGNATURE :=
 endef
 
+define Device/8dev_carambola2
+  ATH_SOC := ar9331
+  DEVICE_TITLE := 8devices Carambola2
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-chipidea2
+  IMAGE_SIZE := 16000k
+  SUPPORTED_DEVICES += carambola2
+endef
+TARGET_DEVICES += 8dev_carambola2
+
 define Device/aruba_ap-105
   ATH_SOC := ar7161
   DEVICE_TITLE := Aruba AP-105
