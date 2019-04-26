5b128234178728d3131c1028c185e4caf99db06c
Paul Spooren <mail@aparcar.org>
2019-04-26 19:15:34 +0200

    x86/64,arvirt/64: install kmod-fs-9p per default
    Installing kmod-fs-9p allows OpenWrt running as guest to mount folders
    shared by the host machine.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- target/linux/armvirt/64/target.mk
+++ target/linux/armvirt/64/target.mk
@@ -2,6 +2,7 @@ ARCH:=aarch64
 SUBTARGET:=64
 BOARDNAME:=ARMv8 multiplatform
 KERNELNAME:=Image
+DEFAULT_PACKAGES += kmod-fs-9p
 
 define Target/Description
 	Build multi-platform images for the ARMv8 instruction set architecture
