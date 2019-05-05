974d6958a7c6332438ab4cda98e72156c8d944ed
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-05-05 18:46:09 +0200

    ath79: Add support for TP-Link Archer C60 v2
    TP-Link Archer C60 v2 is a dual-band AC1350 router,
    based on Qualcomm/Atheros QCA9561 + QCA9886.
    
    Specification:
    
    - 775/650/258 MHz (CPU/DDR/AHB)
    - 64 MB of RAM (DDR2)
    - 8 MB of FLASH (SPI NOR)
    - 3T3R 2.4 GHz
    - 2T2R 5 GHz
    - 5x 10/100 Mbps Ethernet
    - 7x LED, 2x button
    - UART header on PCB
    
    Flash instruction (WebUI):
    Download *-factory.bin image and upload it via the firmwary upgrade
    function of the stock firmware WebUI.
    
    Flash instruction (TFTP):
    1. Set PC to fixed IP address 192.168.0.66
    2. Download *-factory.bin image and rename it to tp_recovery.bin
    3. Start a tftp server with the file tp_recovery.bin in its root
       directory
    4. Turn off the router
    5. Press and hold reset button
    6. Turn on router with the reset button pressed and wait ~15 seconds
    7. Release the reset button and after a short time the firmware should
       be transferred from the tftp server
    8. Wait ~30 second to complete recovery
    
    Flash instruction (under U-Boot, using UART):
    tftp 0x81000000 ...-sysupgrade.bin
    erase 0x9f030000 +$filesize
    cp.b $fileaddr 0x9f030000 $filesize
    reset
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -72,6 +72,18 @@ define Device/tplink_archer-c60-v1
 endef
 TARGET_DEVICES += tplink_archer-c60-v1
 
+define Device/tplink_archer-c60-v2
+  $(Device/tplink-safeloader-uimage)
+  ATH_SOC := qca9561
+  IMAGE_SIZE := 7808k
+  DEVICE_MODEL := Archer C60
+  DEVICE_VARIANT := v2
+  TPLINK_BOARD_ID := ARCHER-C60-V2
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct
+  SUPPORTED_DEVICES += archer-c60-v2
+endef
+TARGET_DEVICES += tplink_archer-c60-v2
+
 define Device/tplink_archer-c6-v2
   $(Device/tplink-safeloader-uimage)
   ATH_SOC := qca9563
