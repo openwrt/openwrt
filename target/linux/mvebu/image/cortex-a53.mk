de3387654b5b65dcd9029d68c8c2fab29f6fef84
Tomasz Maciej Nowak <tomek_n@o2.pl>
2019-05-11 19:37:35 +0200

    mvebu: add images for additional ESPRESSObin boards
    This commit adds support for different iterations of ESPRESSObin.
    The added variants are:
    
    ESPRESSObin with soldered eMMC,
    
    ESPRESSObin V7, compared to V5 some passive elements changed and ethernet
    ports labels positions have been reversed,
    
    ESPRESSObin V7 with soldered eMMC.
    
    Please refer to:
    584d7c5 ("mvebu: new subtarget cortex A53")
    for instruction how to boot OpenWrt image placed on SD card. It is
    advised for owners of V5 and previous with bootloader based on U-Boot
    2015.01, to upgrade the latest version available at:
    http://espressobin.net/tech-spec.
    
    Signed-off-by: Tomasz Maciej Nowak <tomek_n@o2.pl>

--- target/linux/mvebu/image/cortex-a53.mk
+++ target/linux/mvebu/image/cortex-a53.mk
@@ -7,6 +7,27 @@ define Device/globalscale_espressobin
 endef
 TARGET_DEVICES += globalscale_espressobin
 
+define Device/globalscale_espressobin-emmc
+  $(call Device/Default-arm64)
+  DEVICE_TITLE := ESPRESSObin eMMC (Marvell Armada 3700 Community Board)
+  DEVICE_DTS := armada-3720-espressobin-emmc
+endef
+TARGET_DEVICES += globalscale_espressobin-emmc
+
+define Device/globalscale_espressobin-v7
+  $(call Device/Default-arm64)
+  DEVICE_TITLE := ESPRESSObin V7 (Marvell Armada 3700 Community Board)
+  DEVICE_DTS := armada-3720-espressobin-v7
+endef
+TARGET_DEVICES += globalscale_espressobin-v7
+
+define Device/globalscale_espressobin-v7-emmc
+  $(call Device/Default-arm64)
+  DEVICE_TITLE := ESPRESSObin V7 eMMC (Marvell Armada 3700 Community Board)
+  DEVICE_DTS := armada-3720-espressobin-v7-emmc
+endef
+TARGET_DEVICES += globalscale_espressobin-v7-emmc
+
 define Device/marvell_armada-3720-db
   $(call Device/Default-arm64)
   DEVICE_TITLE := Marvell Armada 3720 Development Board DB-88F3720-DDR3
