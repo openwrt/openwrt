405e08bee63d030956aeb77a1f2b09d0d35a867d
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-05-28 12:28:11 +0200

    kernel: bump 4.19 to 4.19.46
    Refreshed all patches.
    
    Compile-tested on: cns3xxx, imx6
    Runtime-tested on: cns3xxx, imx6
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .179
 LINUX_VERSION-4.14 = .122
-LINUX_VERSION-4.19 = .45
+LINUX_VERSION-4.19 = .46
 
 LINUX_KERNEL_HASH-4.9.179 = 532e4b7b0103806dad9124fd09d15d19594eca08e7f900e94ff805d3b4171ee3
 LINUX_KERNEL_HASH-4.14.122 = c8a3f40ef877e18b3d2890e0954f2d81b6d012a7321836ca4e5b036ef93f68f5
-LINUX_KERNEL_HASH-4.19.45 = eb31e62fbb30aaeedb892cecdd8ac9746d42cbbef2ef2356f9ca8e13514b9d2b
+LINUX_KERNEL_HASH-4.19.46 = 097b52fe8a872259f4a3dba571b2eaf7b9863d9cde5399c6b316dec0ef57e67a
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
