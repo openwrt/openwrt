55e6c903ae20bd8e694f4626166766e59602f87c
Jeff Kletsky <git-commits@allycomm.com>
2019-06-01 17:51:17 -0700

    ath79: GL-AR300M: provide NAND support; increase to 4 MB kernel
    The GL.iNet GL-AR300M has been supported by the ar71xx and ath79
    platforms with access to its 16 MB NOR flash, but not its 128 MB
    SPI NAND flash.
    
    This commit provides support for the NAND through the upstream
    SPI-NAND framework. Devices with both NOR and NAND flash can support
    independent firmware on each, with U-Boot able to boot from either.
    The OEM U-Boot will fall back to the NOR firmware after three
    "unsuccessful" boots.
    
    The family of GL-AR300M devices on the ath79 platform now includes:
    
      * glinet,gl-ar300m-lite       "generic" target, NOR-only board
      * glinet,gl-ar300m-nand       "nand" target
      * glinet,gl-ar300m-nor        "nand" target (NAND-aware)
    
    NB: This commit increases the kernel size from 2 MB to 4 MB
    
    "Force-less" sysupgrade is presently supported from the current
    versions of following NOR-based firmwre images to the version of
    glinet,gl-ar300m-nor firmware produced by this commit:
    
      * gl-ar300m            -- OEM v3 NOR    ar71xx (openwrt-ar300m16-*.bin)
      * gl-ar300m            -- OpenWrt 18.06 ar71xx
      * gl-ar300m            -- OpenWrt 19.07 ar71xx
    
    Other upgrades to these images should be performed through U-Boot.
    
    The GL-AR300M OEM U-Boot allows upload and flashing of either NOR
    firmware (sysupgrade.bin) or NAND firmware (factory.img) through its
    HTTP-based GUI. Serial connectivity is not required.
    
    The glinet,gl-ar300m-nand and glinet,gl-ar300m-nor images generated
    after this commit should safely flash each other using sysupgrade.
    
    The boot counter is implemented by the OEM using u-boot-env. At this
    time, it does not appear that the switch on the side of the unit can
    be used to select NOR vs. NAND boot and the fail-over is only from
    NAND to NOR. To save flash wear, it is only reset when running the
    glinet,gl-ar300m-nand firmware.
    
    NAND-specific base-files are used to remove impact on existing
    generic and tiny targets.
    
    As there is now no "generic" build appropriate for the GL-AR300M16,
    (or for users of the GL-AR300M that do not need access to NAND)
    it will be introduced in a subsequent commit.
    
    Note: `mtd_get_mac_binary art 0x6` does not return the proper MAC
    and the GL.iNet source indicates that only the 0x0 offset is valid
    
    The ar71xx targets are unmodified.
    
    Signed-off-by: Jeff Kletsky <git-commits@allycomm.com>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -45,21 +45,35 @@ define Device/aerohive_hiveap-121
 endef
 TARGET_DEVICES += aerohive_hiveap-121
 
-define Device/glinet_gl-ar300m-nand
+define Device/glinet_gl-ar300m-common-nand
   ATH_SOC := qca9531
   DEVICE_VENDOR := GL.iNet
   DEVICE_MODEL := GL-AR300M
-  DEVICE_VARIANT := NAND
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport
-  KERNEL_SIZE := 2048k
-  BLOCKSIZE := 128k
+  DEVICE_PACKAGES := kmod-usb2
+  KERNEL_SIZE := 4096k
+  IMAGE_SIZE := 16000k
   PAGESIZE := 2048
-  VID_HDR_OFFSET := 512
-  IMAGES += factory.ubi
-  IMAGE/sysupgrade.bin := sysupgrade-tar
-  IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  VID_HDR_OFFSET := 2048
+endef
+
+define Device/glinet_gl-ar300m-nand
+  $(Device/glinet_gl-ar300m-common-nand)
+  DEVICE_VARIANT := NAND
+  BLOCKSIZE := 128k
+  IMAGES += factory.img
+  IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  SUPPORTED_DEVICES += glinet,gl-ar300m-nor
 endef
 TARGET_DEVICES += glinet_gl-ar300m-nand
+
+define Device/glinet_gl-ar300m-nor
+  $(Device/glinet_gl-ar300m-common-nand)
+  DEVICE_VARIANT := NOR
+  BLOCKSIZE := 64k
+  SUPPORTED_DEVICES += glinet,gl-ar300m-nand gl-ar300m
+endef
+TARGET_DEVICES += glinet_gl-ar300m-nor
 
 # fake rootfs is mandatory, pad-offset 129 equals (2 * uimage_header + 0xff)
 define Device/netgear_ath79_nand
