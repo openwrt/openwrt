24e09bac48b6ce90c11407676dbfa42d069cff24
Kevin Darbyshire-Bryant <ldir@darbyshire-bryant.me.uk>
2019-06-06 10:45:15 +0100

    Revert "kernel: backport act_ctinfo"
    This reverts commit 7c50182e0cdce0366715082872a2afbcf208bbf8.
    
    Produces build error:
    Package kmod-sched is missing dependencies for the following libraries:
    nf_conntrack.ko
    
    Signed-off-by: Kevin Darbyshire-Bryant <ldir@darbyshire-bryant.me.uk>

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -803,6 +803,7 @@ endef
 
 $(eval $(call KernelPackage,sched-mqprio))
 
+
 define KernelPackage/sched-connmark
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=Traffic shaper conntrack mark support
@@ -813,15 +814,6 @@ define KernelPackage/sched-connmark
 endef
 $(eval $(call KernelPackage,sched-connmark))
 
-define KernelPackage/sched-ctinfo
-  SUBMENU:=$(NETWORK_SUPPORT_MENU)
-  TITLE:=Traffic shaper ctinfo support
-  DEPENDS:=+kmod-sched-core +kmod-ipt-core +kmod-ipt-conntrack-extra
-  KCONFIG:=CONFIG_NET_ACT_CTINFO
-  FILES:=$(LINUX_DIR)/net/sched/act_ctinfo.ko
-  AUTOLOAD:=$(call AutoLoad,71, act_ctinfo)
-endef
-$(eval $(call KernelPackage,sched-ctinfo))
 
 define KernelPackage/sched-ipset
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
