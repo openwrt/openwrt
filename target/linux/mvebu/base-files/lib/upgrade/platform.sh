bc47285cb3c0125424e628521f905f1f0d7b4cef
Vladimir Vid <vladimir.vid@sartura.hr>
2019-06-07 15:50:50 +0200

    mvebu: fix regression for non-generic ESPRESSObin versions
    When targets for multiple ESPRESSObin devices were added, not all
    files were updated which means any ESPRESSObin version beside generic
    won't have proper networking, sysupgrade and uboot-env. This patch
    fixes the issue.
    
    * fixup network detection
    * fixup uboot-env
    * fixup platform.sh for sysupgrade
    
    Signed-off-by: Vladimir Vid <vladimir.vid@sartura.hr>

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -23,7 +23,8 @@ platform_do_upgrade() {
 	linksys,caiman|linksys,cobra|linksys,mamba|linksys,rango|linksys,shelby|linksys,venom)
 		platform_do_upgrade_linksys "$ARGV"
 		;;
-	cznic,turris-omnia|globalscale,espressobin|marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
+	cznic,turris-omnia|globalscale,espressobin|globalscale,espressobin-emmc|globalscale,espressobin-v7|globalscale,espressobin-v7-emmc|\
+	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
 		platform_do_upgrade_sdcard "$ARGV"
 		;;
 	*)
