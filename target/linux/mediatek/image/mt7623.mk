65b1eaa018394e4564e0fd582150b7fa26b0538a
Alexey Loukianov <lx2@lexa2.ru>
2019-06-07 12:23:13 +0300

    mediatek: include SATA, USB and preloader package for mt7623n-bananapi-bpi-r2
        Signed-off-by: Alexey Loukianov <lx2@lexa2.ru>
    
    Signed-off-by: Cristian Ciobanu <cioby.service@gmail.com>

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -12,6 +12,10 @@ TARGET_DEVICES += 7623a-unielec-u7623-02-emmc-512m
 define Device/7623n-bananapi-bpi-r2
   DEVICE_TITLE := MTK7623n BananaPi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 \
+                     kmod-ata-core kmod-ata-ahci-mtk \
+                     mt7623n-preloader
+  SUPPORTED_DEVICES := bananapi,bpi-r2
 endef
 
 TARGET_DEVICES += 7623n-bananapi-bpi-r2
