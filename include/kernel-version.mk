aa3f9736ea67200dd840093b848606ced27d388e
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-06-18 13:19:59 +0200

    kernel: bump 4.19 to 4.19.52
    Refreshed all patches.
    
    Fixes:
    
    - CVE-2019-11479
    - CVE-2019-11478
    - CVE-2019-11477
    
    Also fix a malformed patch issue caught during refresh.
    
    It was caused by removing a whitespace without altering
    the index values in a patch which alters a patch.
    
    Compile-tested on: cns3xxx
    Runtime-tested on: cns3xxx
    
    Fixes: cf6526249298 ("kernel: bump 4.19 to 4.19.51")
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .182
 LINUX_VERSION-4.14 = .127
-LINUX_VERSION-4.19 = .51
+LINUX_VERSION-4.19 = .52
 
 LINUX_KERNEL_HASH-4.9.182 = b16e12681a0638368479d73a9b1b8e9407c1ae4b7ae52fdf236d9e5657999695
 LINUX_KERNEL_HASH-4.14.127 = d7464c2379cdee46548ae9f848671f4669d4003af4034f3f738f2f0511ec75b3
-LINUX_KERNEL_HASH-4.19.51 = 7afa920d776f8ca3fc3ced5026cc02c5ccc960c58314050982c307589b5a8d70
+LINUX_KERNEL_HASH-4.19.52 = 266ee655620c3689ae94761970d6827fffc919760caf815427ad22730ba4381e
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
