5ef6c201833a476f65a8a0cb4468118886228638
Konstantin Demin <rockdrilla@gmail.com>
2019-06-28 21:30:50 +0300

    netsupport: move out mqprio from kmod-sched
    Currently, there's unable to install "kmod-sched-mqprio" after
    "kmod-sched" (or vice versa), because "sch_mqprio.ko" is
    shipped in both packages.
    
    Fixes: f83522fa6361 ("linux: Add kmod-sched-mqprio")
    Fixes: 6af639e0bf78 ("linux: Add kmod-sched-act-vlan")
    Fixes: 72c7e2dc467c ("linux: Add kmod-sched-flower")
    Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>
    [Add cls_flower and act_vlan]
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -733,7 +733,7 @@ $(eval $(call KernelPackage,mppe))
 
 SCHED_MODULES = $(patsubst $(LINUX_DIR)/net/sched/%.ko,%,$(wildcard $(LINUX_DIR)/net/sched/*.ko))
 SCHED_MODULES_CORE = sch_ingress sch_fq_codel sch_hfsc sch_htb sch_tbf cls_basic cls_fw cls_route cls_flow cls_tcindex cls_u32 em_u32 act_mirred act_skbedit cls_matchall
-SCHED_MODULES_FILTER = $(SCHED_MODULES_CORE) act_connmark act_ctinfo sch_netem em_ipset cls_bpf act_bpf
+SCHED_MODULES_FILTER = $(SCHED_MODULES_CORE) act_connmark act_ctinfo sch_netem sch_mqprio em_ipset cls_bpf cls_flower act_bpf act_vlan
 SCHED_MODULES_EXTRA = $(filter-out $(SCHED_MODULES_FILTER),$(SCHED_MODULES))
 SCHED_FILES = $(patsubst %,$(LINUX_DIR)/net/sched/%.ko,$(filter $(SCHED_MODULES_CORE),$(SCHED_MODULES)))
 SCHED_FILES_EXTRA = $(patsubst %,$(LINUX_DIR)/net/sched/%.ko,$(SCHED_MODULES_EXTRA))
