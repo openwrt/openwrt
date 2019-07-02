ebef2c27eb8a2840ae64ff51785a61f9752e59b1
Rafał Miłecki <rafal@milecki.pl>
2019-07-02 13:16:08 +0200

    base-files: drop unused jffs2_copy_config()
    Its last usage was dropped back in 2013 in the commit b95bdc8ab56e
    ("kernel/base-files: clean up old code related to refreshing mtd
    partitions, it is no longer used anywhere").
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    (cherry picked from commit f58ca6ee57b77350c69639defdceacb2e77e2249)

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -207,16 +207,6 @@ get_partitions() { # <device> <filename>
 	fi
 }
 
-jffs2_copy_config() {
-	if grep rootfs_data /proc/mtd >/dev/null; then
-		# squashfs+jffs2
-		mtd -e rootfs_data jffs2write "$CONF_TAR" rootfs_data
-	else
-		# jffs2
-		mtd jffs2write "$CONF_TAR" rootfs
-	fi
-}
-
 indicate_upgrade() {
 	. /etc/diag.sh
 	set_state upgrade
