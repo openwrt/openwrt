f557dafa390b115958c5c9ea835aea61fb036fc2
Chen Minqiang <ptpt52@gmail.com>
2019-07-04 15:40:46 +0800

    ramips: init support for xwrt_t-cpe1200k-v01

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1714,6 +1714,18 @@ define Device/xzwifi_creativebox-v1
 endef
 TARGET_DEVICES += xzwifi_creativebox-v1
 
+define Device/xwrt_t-cpe1200k-v01
+  $(Device/uimage-lzma-loader)
+  SOC := mt7621
+  IMAGE_SIZE := 16000k
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := T-CPE1200K
+  DEVICE_VARIANT := V01
+  DEVICE_PACKAGES := \
+	kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport rssileds
+endef
+TARGET_DEVICES += xwrt_t-cpe1200k-v01
+
 define Device/youhua_wr1200js
   $(Device/dsa-migration)
   IMAGE_SIZE := 16064k
