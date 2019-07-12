20452a8db9f90e5f1d0733363a488be522cd5c0f
Rafał Miłecki <rafal@milecki.pl>
2019-07-12 14:05:47 +0200

    ar71xx: get rid of platform_pre_upgrade()
    The only step between platform_pre_upgrade() and platform_do_upgrade()
    is switching to ramdisk. It should be fine to "mtd erase firmware" from
    the later callback and get rid of the first one.
    
    This change wasn't tested on affected target but identical code logic
    was verified to work as expected on brcm47xx with initramfs firmware.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -746,33 +746,6 @@ platform_check_image() {
 	return 1
 }
 
-platform_pre_upgrade() {
-	local board=$(board_name)
-
-	case "$board" in
-	rb-750-r2|\
-	rb-750p-pbr2|\
-	rb-750up-r2|\
-	rb-911-2hn|\
-	rb-911-5hn|\
-	rb-931-2nd|\
-	rb-941-2nd|\
-	rb-951ui-2nd|\
-	rb-952ui-5ac2nd|\
-	rb-962uigs-5hact2hnt|\
-	rb-lhg-5nd|\
-	rb-map-2nd|\
-	rb-mapl-2nd|\
-	rb-sxt-2nd-r3|\
-	rb-wap-2nd|\
-	rb-wapg-5hact2hnd|\
-	rb-wapr-2nd)
-		# erase firmware if booted from initramfs
-		[ -z "$(rootfs_type)" ] && mtd erase firmware
-		;;
-	esac
-}
-
 platform_nand_pre_upgrade() {
 	local board=$(board_name)
 
@@ -807,6 +780,29 @@ platform_nand_pre_upgrade() {
 
 platform_do_upgrade() {
 	local board=$(board_name)
+
+	case "$board" in
+	rb-750-r2|\
+	rb-750p-pbr2|\
+	rb-750up-r2|\
+	rb-911-2hn|\
+	rb-911-5hn|\
+	rb-931-2nd|\
+	rb-941-2nd|\
+	rb-951ui-2nd|\
+	rb-952ui-5ac2nd|\
+	rb-962uigs-5hact2hnt|\
+	rb-lhg-5nd|\
+	rb-map-2nd|\
+	rb-mapl-2nd|\
+	rb-sxt-2nd-r3|\
+	rb-wap-2nd|\
+	rb-wapg-5hact2hnd|\
+	rb-wapr-2nd)
+		# erase firmware if booted from initramfs
+		[ -z "$(rootfs_type)" ] && mtd erase firmware
+		;;
+	esac
 
 	case "$board" in
 	all0258n)
