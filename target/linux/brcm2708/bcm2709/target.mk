f781582828475f23abc478a07422db94f1bcda16
Álvaro Fernández Rojas <noltari@gmail.com>
2019-07-13 13:33:44 +0200

    brcm2708: Add Raspberry Pi 4 support
    - Support added to bcm2709 subtarget (32 bit).
    - 64 bit kernel still requires many hacks on RPi 4B.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/brcm2708/bcm2709/target.mk
+++ target/linux/brcm2708/bcm2709/target.mk
@@ -4,11 +4,11 @@
 #
 
 SUBTARGET:=bcm2709
-BOARDNAME:=BCM2709/BCM2710 boards (32 bit)
+BOARDNAME:=BCM2709/BCM2710/BCM2711 boards (32 bit)
 CPU_TYPE:=cortex-a7
 CPU_SUBTYPE:=neon-vfpv4
 
 define Target/Description
-	Build firmware image for BCM2709/BCM2710 devices.
+	Build firmware image for BCM2709/BCM2710/BCM2711 devices.
 	This firmware features a 32 bit kernel.
 endef
