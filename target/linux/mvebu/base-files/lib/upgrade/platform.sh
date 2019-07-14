670ce65a44db4263ee21a3fce962c47378ef042b
Rafał Miłecki <rafal@milecki.pl>
2019-07-14 22:08:58 +0200

    mvebu: sysupgrade: drop platform_copy_config_sdcard() argument
    That function doesn't take any arguments so don't pass anything when
    calling it.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -40,7 +40,7 @@ platform_copy_config() {
 		;;
 	cznic,turris-omnia|globalscale,espressobin|globalscale,espressobin-emmc|globalscale,espressobin-v7|globalscale,espressobin-v7-emmc|\
 	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
-		platform_copy_config_sdcard "$ARGV"
+		platform_copy_config_sdcard
 		;;
 	esac
 }
