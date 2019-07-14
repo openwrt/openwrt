76e43c82b55518b794ac24f9acfb3888c638f99b
Rafał Miłecki <rafal@milecki.pl>
2019-07-14 18:59:59 +0200

    mvebu: sysupgrade: don't use $ARGV in platform_check_image()
    sysupgrade passes image path to platform_check_image() as an argument so
    it can be simply accessed using $1
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -11,7 +11,7 @@ platform_check_image() {
 	case "$(board_name)" in
 	cznic,turris-omnia|globalscale,espressobin|globalscale,espressobin-emmc|globalscale,espressobin-v7|globalscale,espressobin-v7-emmc|\
 	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
-		platform_check_image_sdcard "$ARGV"
+		platform_check_image_sdcard "$1"
 		;;
 	*)
 		return 0
