openwrt/openwrt

commit 2f1a11d0089c9d4b675b0e1e8806e1e8e3be5a02

imx6: apalis: fix config survival after sysupgrade -n
This patch fixes `sysupgrade -n` when flashed with rootfs of the same
size as currently running, so the rootfs_data wouldn't get destroyed and
thus survive reboot. So let's fix it by always cleaning up the content
of the rootfs_data during sysupgrade.

Signed-off-by: Petr Štetiar <ynezz@true.cz>

target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -72,3 +72,16 @@ platform_copy_config() {
 		;;
 	esac
 }
+
+platform_pre_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	apalis*)
+		[ "$SAVE_CONFIG" -eq 0 ] && {
+			jffs2reset -y
+			umount /overlay
+		}
+		;;
+	esac
+}

commit 0711b515f635bc67e5096caaac1cb1386dd69e08

imx6: apalis: add missing jffs2reset to ramfs during sysupgrade
jffs2reset is used to cleanup the rootfs_data overlay, but it's
currently missing in the ramfs during sysupgrade leading to the
following error:

 Switching to ramdisk...
 Performing system upgrade...
 /lib/upgrade/do_stage2: line 27: jffs2reset: not found

Fixes: 2f1a11d0089c ("imx6: apalis: fix config survival after sysupgrade -n")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -4,7 +4,7 @@
 
 . /lib/imx6.sh
 
-RAMFS_COPY_BIN='blkid'
+RAMFS_COPY_BIN='blkid jffs2reset'
 
 enable_image_metadata_check() {
 	case "$(board_name)" in