be14b1feaee95d3c4fd3a4743b790b9fc34a16f0
Tomislav Požega <pozega.tomislav@gmail.com>
2019-07-18 17:59:37 +0200

    ar71xx: fix system LED names on Archer C5/C7
    Move system LED board definitions of Archer C5/C7 to reflect
    actual system LED colour used
    
    Signed-off-by: Tomislav Požega <pozega.tomislav@gmail.com>
    (cherry picked from commit a73934fc9ad05207e79a3e58abc3d14eee28b6f2)

--- target/linux/ar71xx/base-files/etc/diag.sh
+++ target/linux/ar71xx/base-files/etc/diag.sh
@@ -85,8 +85,6 @@ get_status_led() {
 	tl-wr902ac-v1)
 		status_led="$board:green:power"
 		;;
-	archer-c5|\
-	archer-c7|\
 	tl-mr10u|\
 	tl-mr12u|\
 	tl-mr13u|\
@@ -450,6 +448,8 @@ get_status_led() {
 	tl-mr6400)
 		status_led="tp-link:white:power"
 		;;
+	archer-c5|\
+	archer-c7|\
 	tl-mr3220|\
 	tl-mr3220-v2|\
 	tl-mr3420|\
