3d8667aaedc3c1da13942a008772e81626d5e77b
Vladimir Vid <vladimir.vid@sartura.hr>
2019-07-22 13:08:18 +0200

    mvebu: uDPU: add sysupgrade support
    This patch adds sysupgrade, uboot-env and networking support
    for Methode uDPU device.
    
    Device features 4 partitions:
    
    -----------------------------------------
    |  boot   | recovery  | rootfs |  misc  |
    | (ext4)  |  (ext4)   | (fsf2) | (f2fs) |
    _________________________________________
    
    Idea was to use f2fs only but the u-boot currently lacks support
    so first 2 partition are ext4 to be u-boot readable, and this was
    a reason why custom build and sysupgrade sections were required.
    
    On the sysupgrade, boot and rootfs partitions are updated, firmare
    image and user configuration is saved on the misc partition and if
    the upgrade was successfull, recovery partition will be updated on
    after the reboot from preinit script. If the sysupgrade fails for any
    reason, device will fallback to recovery initramfs image.
    
    Signed-off-by: Vladimir Vid <vladimir.vid@sartura.hr>

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -28,6 +28,9 @@ platform_do_upgrade() {
 	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
 		platform_do_upgrade_sdcard "$1"
 		;;
+	methode,udpu)
+		platform_do_upgrade_uDPU "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
 		;;
@@ -41,6 +44,9 @@ platform_copy_config() {
 	cznic,turris-omnia|globalscale,espressobin|globalscale,espressobin-emmc|globalscale,espressobin-v7|globalscale,espressobin-v7-emmc|\
 	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
 		platform_copy_config_sdcard
+		;;
+	methode,udpu)
+		platform_copy_config_uDPU
 		;;
 	esac
 }
