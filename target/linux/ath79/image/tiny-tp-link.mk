c8c4f7216e31916d50aecaf4585a07652e22cb27
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-07-24 20:38:36 +0200

    ath79: specify N and ND subversions of TL-WR941 with ALT0_MODEL
    TP-Link's TL-WR941 is sold with detachable antennas
    internationally (ND version), but with fixed antennas in China
    (N version). While hardware and images are similar for both
    variants of v2 and v4, they are different for v6.
    
    Having both explicitly will make it easier for user to identify
    the correct image, and most importantly will raise awareness
    that N and ND are not necessarily always the same as for
    TL-WR841 series.
    
    With an image selection webpage, using ALT0_MODEL as in this
    patch will provide the option to list versions for N and ND
    separately.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -243,8 +243,11 @@ TARGET_DEVICES += tplink_tl-wr940n-v4
 define Device/tplink_tl-wr941-v2
   $(Device/tplink-4m)
   ATH_SOC := ar9132
-  DEVICE_MODEL := TL-WR941N/ND
+  DEVICE_MODEL := TL-WR941ND
   DEVICE_VARIANT := v2/v3
+  DEVICE_ALT0_VENDOR := TP-Link
+  DEVICE_ALT0_MODEL := TL-WR941N
+  DEVICE_ALT0_VARIANT := v2/v3
   TPLINK_HWID := 0x09410002
   TPLINK_HWREV := 2
 endef
@@ -253,8 +256,11 @@ TARGET_DEVICES += tplink_tl-wr941-v2
 define Device/tplink_tl-wr941-v4
   $(Device/tplink-4m)
   ATH_SOC := ar7240
-  DEVICE_MODEL := TL-WR941N/ND
+  DEVICE_MODEL := TL-WR941ND
   DEVICE_VARIANT := v4
+  DEVICE_ALT0_VENDOR := TP-Link
+  DEVICE_ALT0_MODEL := TL-WR941N
+  DEVICE_ALT0_VARIANT := v4
   TPLINK_HWID := 0x09410004
 endef
 TARGET_DEVICES += tplink_tl-wr941-v4
