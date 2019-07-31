6226a5387fb050ceb27430d746368a6b3a519db0
Paul Spooren <mail@aparcar.org>
2019-07-31 16:34:38 +0200

    x86: verbose profile titles
    So far (nearly) all x86 profiles are called "Generic" which makes it
    hard to distinguish them in special cases, like searching for a specific
    profile (without pre-selecting target/subtarget).
    
    To make this change locally working, remove the tmp/ folder to force
    reload of menuconfig.
    
    As these files are infrequently touched, the Copyright was updated as
    well.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- target/linux/x86/legacy/profiles/000-Generic.mk
+++ target/linux/x86/legacy/profiles/000-Generic.mk
@@ -1,18 +1,18 @@
 #
-# Copyright (C) 2006-2009 OpenWrt.org
+# Copyright (C) 2006-2019 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Generic
-  NAME:=Generic
+  NAME:=Generic x86/legacy
   PACKAGES:=kmod-3c59x kmod-e100 kmod-e1000 kmod-natsemi kmod-ne2k-pci \
 	kmod-pcnet32 kmod-8139too kmod-r8169 kmod-sis900 kmod-tg3 \
 	kmod-via-rhine kmod-via-velocity
 endef
 
 define Profile/Generic/Description
-	Generic Profile
+	Generic Profile for x86 legacy architecutre
 endef
 $(eval $(call Profile,Generic))
