14d17306bc73a2ef512758c72af0f0f99aef7a2a
Paul Spooren <mail@aparcar.org>
2019-07-31 16:34:38 +0200

    x86: make profile titles more verbose
    So far (nearly) all x86 profiles are called "Generic" which makes
    it hard to distinguish them in special cases, like searching for a
    specific profile (without pre-selecting target/subtarget).
    
    To make this change locally working, remove the tmp/ folder to
    force reload of menuconfig.
    
    As these files are infrequently touched, the Copyright was updated
    as well.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>
    [changed commit title]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/x86/64/profiles/000-Generic.mk
+++ target/linux/x86/64/profiles/000-Generic.mk
@@ -1,15 +1,15 @@
 #
-# Copyright (C) 2006-2015 OpenWrt.org
+# Copyright (C) 2006-2019 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
 define Profile/Generic
-  NAME:=Generic
+  NAME:=Generic x86/64
 endef
 
 define Profile/Generic/Description
-	Generic Profile
+	Generic Profile for x86/64 architecture
 endef
 $(eval $(call Profile,Generic))
