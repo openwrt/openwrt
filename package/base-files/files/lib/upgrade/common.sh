b534ba96110012d2697d19d71b7dcd60bd4cd375
Rafał Miłecki <rafal@milecki.pl>
2019-08-16 16:54:38 +0200

    base-files: pass "save_config" option to the "sysupgrade" method
    This explicitly lets stage2 know if config should be preserved.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -218,7 +218,7 @@ indicate_upgrade() {
 # $(2): (optional) pipe command to extract firmware, e.g. dd bs=n skip=m
 default_do_upgrade() {
 	sync
-	if [ "$SAVE_CONFIG" -eq 1 ]; then
+	if [ "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ]; then
 		get_image "$1" "$2" | mtd $MTD_ARGS $MTD_CONFIG_ARGS -j "$CONF_TAR" write - "${PART_NAME:-image}"
 	else
 		get_image "$1" "$2" | mtd $MTD_ARGS write - "${PART_NAME:-image}"
