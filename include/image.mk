3934da48d209898a4102eee443c8360fe5f335a2
Paul Spooren <mail@aparcar.org>
2019-08-18 09:56:45 -1000

    build: create JSON files containing image info
    The JSON info files contain details about the created firmware images
    per device and are stored next to the created images.
    
    The JSON files are stored as "$(IMAGE_PREFIX).json" and contain some
    device/image meta data as well as a list of created firmware images.
    
    An example of openwrt-ramips-rt305x-aztech_hw550-3g.json
    
        {
          "id": "aztech_hw550-3g",
          "image_prefix": "openwrt-ramips-rt305x-aztech_hw550-3g",
          "images": [
            {
              "name": "openwrt-ramips-rt305x-aztech_hw550-3g-squashfs-sysupgrade.bin",
              "sha256": "db2b34b0ec4a83d9bf612cf66fab0dc3722b191cb9bedf111e5627a4298baf20",
              "type": "sysupgrade"
            }
          ],
          "metadata_version": 1,
          "supported_devices": [
            "aztech,hw550-3g",
            "hw550-3g"
          ],
          "target": "ramips/rt305x",
          "titles": [
            {
              "model": "HW550-3G",
              "vendor": "Aztech"
            },
            {
              "model": "ALL0239-3G",
              "vendor": "Allnet"
            }
          ],
          "version_commit": "r10920+123-0cc87b3bac",
          "version_number": "SNAPSHOT"
        }
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/image.mk
+++ include/image.mk
@@ -571,7 +571,32 @@ define Device/Build/image
 
   $(BIN_DIR)/$(call IMAGE_NAME,$(1),$(2)): $(KDIR)/tmp/$(call IMAGE_NAME,$(1),$(2))
 	cp $$^ $$@
-
+	$(if $(CONFIG_JSON_ADD_IMAGE_INFO), \
+		DEVICE_ID="$(DEVICE_NAME)" \
+		BIN_DIR="$(BIN_DIR)" \
+		IMAGE_NAME="$(IMAGE_NAME)" \
+		IMAGE_TYPE=$(word 1,$(subst ., ,$(2))) \
+		IMAGE_PREFIX="$(IMAGE_PREFIX)" \
+		DEVICE_VENDOR="$(DEVICE_VENDOR)" \
+		DEVICE_MODEL="$(DEVICE_MODEL)" \
+		DEVICE_VARIANT="$(DEVICE_VARIANT)" \
+		DEVICE_ALT0_VENDOR="$(DEVICE_ALT0_VENDOR)" \
+		DEVICE_ALT0_MODEL="$(DEVICE_ALT0_MODEL)" \
+		DEVICE_ALT0_VARIANT="$(DEVICE_ALT0_VARIANT)" \
+		DEVICE_ALT1_VENDOR="$(DEVICE_ALT1_VENDOR)" \
+		DEVICE_ALT1_MODEL="$(DEVICE_ALT1_MODEL)" \
+		DEVICE_ALT1_VARIANT="$(DEVICE_ALT1_VARIANT)" \
+		DEVICE_ALT2_VENDOR="$(DEVICE_ALT2_VENDOR)" \
+		DEVICE_ALT2_MODEL="$(DEVICE_ALT2_MODEL)" \
+		DEVICE_ALT2_VARIANT="$(DEVICE_ALT2_VARIANT)" \
+		DEVICE_TITLE="$(DEVICE_TITLE)" \
+		TARGET="$(BOARD)" \
+		SUBTARGET="$(SUBTARGET)" \
+		VERSION_NUMBER="$(VERSION_NUMBER)" \
+		VERSION_CODE="$(VERSION_CODE)" \
+		SUPPORTED_DEVICES="$(SUPPORTED_DEVICES)" \
+		$(TOPDIR)/scripts/json_add_image_info.py \
+	)
 endef
 
 define Device/Build/artifact
@@ -589,6 +614,8 @@ define Device/Build/artifact
 endef
 
 define Device/Build
+  $(shell rm -f $(BIN_DIR)/$(IMG_PREFIX)-$(1).json)
+
   $(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),$(call Device/Build/initramfs,$(1)))
   $(call Device/Build/kernel,$(1))
 
