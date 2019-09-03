ee76bd11bbe75bf57cccd84bbeea245ee7a2893b
Jonas Gorski <jonas.gorski@gmail.com>
2019-09-03 14:16:12 +0200

    images: fix boot failures on NAND with small sub pages
    SquashFS has a minimum block size of at least 1k, so we need to make
    sure the last data block is also at least that big.
    
    This is not an issue on NOR or SD CARD devices, since their rootfs
    partitions go all the way to the end of the usable space.
    
    But on NAND with ubiblock, the rootfs partition will be the exact space,
    rounded up to LEB size. Unfortunately, some NAND chips with small sub
    pages have a LEB size of x.5 kiB. This can cause the the last data block
    to be less than 1k, which will cause the last block to be inaccessible,
    causing boot failures as seen on MR24:
    
    [    1.532960] block ubiblock0_3: created from ubi0:3(rootfs)
    [    1.538457] ubiblock: device ubiblock0_3 (rootfs) set to be root filesystem
    [    1.552847] SQUASHFS error: squashfs_read_data failed to read block 0x621472
    [    1.559896] squashfs: SQUASHFS error: unable to read id index table
    [    1.566474] VFS: Cannot open root device "(null)" or unknown-block(254,0): error -5
    
    Since on most NOR devices, the start of the squashfs partition is not
    aligned. Since the start of the rootfs_data partition there is dependend
    on the SquashFS size, we cannot just always pad it, as the padding could
    creep into the rootfs_data partition, breaking jffs2.
    
    So fix this by ensuring a squashfs rootfs is always a multiple of 1k
    only for UBI and NAND sysupgrade images.
    
    Fixes #2460 without affecting NOR devices.
    
    Tested-by: Russell Senior <russell@personaltelco.net>
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

--- scripts/ubinize-image.sh
+++ scripts/ubinize-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. $TOPDIR/scripts/functions.sh
+
 part=""
 ubootenv=""
 ubinize_param=""
@@ -7,16 +9,6 @@ kernel=""
 rootfs=""
 outfile=""
 err=""
-
-get_magic_word() {
-	dd if=$1 bs=2 count=1 2>/dev/null | od -A n -N 2 -t x1 | tr -d ' '
-}
-
-is_ubifs() {
-	if [ "$( get_magic_word $1 )" = "3118" ]; then
-		echo "1"
-	fi
-}
 
 ubivol() {
 	volid=$1
@@ -31,7 +23,7 @@ ubivol() {
 	echo "vol_name=$name"
 	if [ "$image" ]; then
 		echo "image=$image"
-		[ -n "$size" ] && echo "vol_size=${size}MiB"
+		[ -n "$size" ] && echo "vol_size=${size}"
 	else
 		echo "vol_size=1MiB"
 	fi
@@ -42,7 +34,10 @@ ubivol() {
 
 ubilayout() {
 	local vol_id=0
-	local root_is_ubifs="$( is_ubifs "$2" )"
+	local rootsize=
+	local autoresize=
+	local rootfs_type="$( get_fs_type "$2" )"
+
 	if [ "$1" = "ubootenv" ]; then
 		ubivol $vol_id ubootenv
 		vol_id=$(( $vol_id + 1 ))
@@ -62,16 +57,28 @@ ubilayout() {
 
 		size="$part"
 
-		ubivol $vol_id "$name" "$image" "" "$size"
+		ubivol $vol_id "$name" "$image" "" "${size}MiB"
 		vol_id=$(( $vol_id + 1 ))
 	done
 	if [ "$3" ]; then
 		ubivol $vol_id kernel "$3"
 		vol_id=$(( $vol_id + 1 ))
 	fi
-	ubivol $vol_id rootfs "$2" $root_is_ubifs
+
+	case "$rootfs_type" in
+	"ubifs")
+		autoresize=1
+		;;
+	"squashfs")
+		# squashfs uses 1k block size, ensure we do not
+		# violate that
+		rootsize="$( round_up "$( stat -c%s "$2" )" 1024 )"
+		;;
+	esac
+	ubivol $vol_id rootfs "$2" "$autoresize" "$rootsize"
+
 	vol_id=$(( $vol_id + 1 ))
-	[ "$root_is_ubifs" ] || ubivol $vol_id rootfs_data "" 1
+	[ "$rootfs_type" = "ubifs" ] || ubivol $vol_id rootfs_data "" 1
 }
 
 while [ "$1" ]; do
