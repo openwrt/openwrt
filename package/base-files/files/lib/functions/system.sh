45600124fcbd14ece6e289cb59b318ea44c598fe
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-09-05 13:29:37 +0200

    base-files: use get_mac_binary() in mtd_get_mac_binary_ubi()
    The actual retrieval of the MAC address in mtd_get_mac_binary_ubi()
    is the same as in get_mac_binary(). Thus, use the latter function
    in the former to reduce duplicate code.
    
    This will also allow to benefit from the enhanced path check there
    and bring mtd_get_mac_binary_ubi() more in line with the similar
    mtd_get_mac_binary().
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- package/base-files/files/lib/functions/system.sh
+++ package/base-files/files/lib/functions/system.sh
@@ -79,12 +79,7 @@ mtd_get_mac_binary_ubi() {
 	local ubidev=$(nand_find_ubi $CI_UBIPART)
 	local part=$(nand_find_volume $ubidev $1)
 
-	if [ -z "$part" ]; then
-		echo "mtd_get_mac_binary: ubi volume $mtdname not found!" >&2
-		return
-	fi
-
-	hexdump -v -n 6 -s $offset -e '5/1 "%02x:" 1/1 "%02x"' /dev/$part 2>/dev/null
+	get_mac_binary "/dev/$part" "$offset"
 }
 
 mtd_get_part_size() {
