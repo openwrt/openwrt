d483f4e8c9a84f4e3ce1cfddcb03f3a929d258a8
Paul Spooren <mail@aparcar.org>
2019-09-04 19:41:04 -1000

    squashfs: switch to squashfs-tools-ng
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/image.mk
+++ include/image.mk
@@ -239,10 +239,7 @@ $(eval $(foreach S,$(JFFS2_BLOCKSIZE),$(call Image/mkfs/jffs2/template,$(S))))
 $(eval $(foreach S,$(NAND_BLOCKSIZE),$(call Image/mkfs/jffs2-nand/template,$(S))))
 
 define Image/mkfs/squashfs
-	$(STAGING_DIR_HOST)/bin/mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
-		-noappend -root-owned \
-		-comp $(SQUASHFSCOMP) $(SQUASHFSOPT) \
-		-processors 1
+	$(STAGING_DIR_HOST)/bin/gensquashfs -f --pack-dir $(call mkfs_target_dir,$(1)) $@
 endef
 
 # $(1): board name
