0bb9de0174549986370ee6a4005f218ace4fdbc3
Manuel Giganto <mgigantoregistros@gmail.com>
2019-09-13 13:40:05 +0000

    kernel: cfg80211: enable secure wifi with mac as key
    Allow the system to create a default wifi configuration
    with wifi enabled using PSK2 and the interface mac as
    password. This will allow access to some devices after
    firmware installation, when no ethernet interface is
    available such as some wifi extenders/plc devices, and
    accessing through WAN is firewall blocked...
    
    The wifi configuration will use device ssid, key, country,
    txpower and default (g/a/*) if they are defined in the
    configuration.
    
    The key for the wifi will be configured with the first
    available value of the following: key, label_mac, eth0 mac.
    When label-mac or eth0-mac s used as wifi key, the value
    will be transformed from AA:BB:CC:DD to aa_bb_cc_dd_ee_ff
    and a random BSSID will be used.
    
    A warning message will be displayed at ssh login and luci.
    Luci hint is implemented in this PR:
    https://github.com/openwrt/luci/pull/3204
    
    If SSID value is not configured, it will use Openwrt-YYYYYY
    where YYYYYY are random values.
    
    If country is not defined, it will be set to the safe
    worldwide domain, 00 by default.
    
    If txpower is configured, radios will be configured with that
    value, if txpower and country are not defined, the txpower will be
    reduced to 5, to allow connections within the same room so
    the user can connect and configure properly.
    
    If the device has a button configured with RFKILL, then radios
    will be disabled until the button is pressed. If no button is
    available and default mode is defined, only that wifi band will go up,
    when it is not defined the G band will go up, A band will go
    up only if key and country are defined. If default mode is configured
    with a value other than "g" or "b", both radios will be disabled.
    
    The wifi configuration reads /etc/board.json values in order to
    configure some attributes, so will check if the board.json doesn't
    exist, and in that case, it will wait for it up 20 seconds. If we
    don't give up eventually, it may causea soft brick... Right after
    we give up waiting, or after board_detect is done generating the
    file, the wifi config will continue as usual.
    
    Signed-off-by: Manuel Giganto <mgigantoregistros@gmail.com>

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -3,6 +3,8 @@
 
 append DRIVERS "mac80211"
 
+. /lib/functions/system.sh
+
 lookup_phy() {
 	[ -n "$phy" ] && {
 		[ -d /sys/class/ieee80211/$phy ] && return
@@ -60,6 +62,20 @@ check_mac80211_device() {
 detect_mac80211() {
 	devidx=0
 	config_load wireless
+	CFG=/etc/board.json
+
+	[ -s "$CFG" ] || touch "/tmp/wifi.update"
+	json_load "$(cat ${CFG})"
+	if json_is_a system object; then
+		json_select system
+			local wifi_ssid wifi_key wifi_country wifi_txpower wifi_default
+			json_get_vars wifi_ssid wifi_key wifi_country wifi_txpower wifi_default
+		json_select ..
+	fi
+
+	encryption="psk2"
+	key=$(< /dev/urandom tr -dc ".,+:;%_A-Z-0-9" | head -c${1:-40};echo;)
+
 	while :; do
 		config_get type "radio$devidx" type
 		[ -n "$type" ] || break
@@ -76,9 +92,13 @@ detect_mac80211() {
 		[ "$found" -gt 0 ] && continue
 
 		mode_band="g"
-		channel="11"
+		channel="1"
 		htmode=""
 		ht_capab=""
+		[ -n "$wifi_txpower" ] && tx_power="set wireless.radio${devidx}.txpower=$wifi_txpower" \
+			|| [ -n "$wifi_country" ] || tx_power="set wireless.radio${devidx}.txpower=5"
+		[ -n "$wifi_country" ] && country="$wifi_country" || country="00"
+		[ -n "$wifi_ssid" ] && ssid=$wifi_ssid || ssid=OpenWrt-$(hexdump -ve '"%02X"' -n4 /dev/urandom)
 
 		iw phy "$dev" info | grep -q 'Capabilities:' && htmode=HT20
 
@@ -89,6 +109,15 @@ detect_mac80211() {
 		}
 
 		[ -n "$htmode" ] && ht_capab="set wireless.radio${devidx}.htmode=$htmode"
+		disable_legacy=$( [ "$mode_band" = "g" ] && echo "set wireless.radio${devidx}.legacy_rates='0'" )
+
+		disabled=1
+		grep -q $'^\xf7$' $(find /sys/firmware/devicetree -name linux,code) || \
+			if [ -n "$wifi_default" ] ; then
+				[ "$wifi_default" = "$mode_band" ] && disabled="0"
+			else
+				[[ -n "$wifi_key" && -n "$wifi_country" || "$mode_band" = "g" ]] && disabled=0
+			fi
 
 		path="$(mac80211_phy_to_path "$dev")"
 		if [ -n "$path" ]; then
@@ -102,19 +131,25 @@ detect_mac80211() {
 			set wireless.radio${devidx}.type=mac80211
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.hwmode=11${mode_band}
+			${disable_legacy}
+			${tx_power}
 			${dev_id}
 			${ht_capab}
-			set wireless.radio${devidx}.disabled=1
+			set wireless.radio${devidx}.country=${country}
+			set wireless.radio${devidx}.disabled=${disabled}
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.ssid=${ssid}
+			set wireless.default_radio${devidx}.encryption=${encryption}
+			set wireless.default_radio${devidx}.key=${key}
 EOF
 		uci -q commit wireless
 
 		devidx=$(($devidx + 1))
 	done
+
+	uci -q set system.@system[0].default_key=$key && uci -q commit system
 }
