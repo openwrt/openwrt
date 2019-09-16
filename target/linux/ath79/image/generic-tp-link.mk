deef85e0a41493fde338e525270c0d6efe491707
Keith Maika <keithm@aoeex.com>
2019-09-16 12:45:02 -0400

    ath79: add support for TP-Link Archer C59v2
    Add ath79 support for Archer C59v2, previously supported by ar71xx.
    
    TP-Link Archer C59v2 is a dual-band AC1350 router based on
    Qualcomm/Atheros QCA9561+QCA9886 chips.
    
    Specification:
    
    - 775/650/258 MHz (CPU/DDR/AHB)
    - 128 MB of RAM (DDR2)
    - 16 MB of FLASH (SPI NOR)
    - 3T3R 2.4 GHz
    - 2T2R 5 GHz
    - 5x 10/100 Mbps Ethernet
    - USB 2.0 port
    - UART header on PCB
    
    Flash instruction:
      - via web UI:
        1. Download openwrt-ath79-generic-tplink_archer-c59-v2-squashfs-factory.bin
        2. Login to router and open the Advanced tab
        3. Navigate to System Tools -> Firmware Upgrade
        4. Upload firmware using the Manual Upgrade form
      - via TFTP:
        1. Set PC to fixed ip address 192.168.0.66
        2. Download openwrt-ath79-generic-tplink_archer-c59-v2-squashfs-factory.bin
           and rename it to tp_recovery.bin
        3. Start a tftp server with the file tp_recovery.bin in its root directory
        4. Turn off the router
        5. Press and hold Reset button
        6. Turn on router with the reset button pressed and wait ~15 seconds
        7. Release the reset button and after a short time
           the firmware should be transferred from the tftp server
        8. Wait ~30 second to complete recovery.
    
    Signed-off-by: Keith Maika <keithm@aoeex.com>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -60,6 +60,18 @@ define Device/tplink_archer-c59-v1
 endef
 TARGET_DEVICES += tplink_archer-c59-v1
 
+define Device/tplink_archer-c59-v2
+  $(Device/tplink-safeloader-uimage)
+  ATH_SOC := qca9561
+  IMAGE_SIZE := 14400k
+  DEVICE_MODEL := Archer C59
+  DEVICE_VARIANT := v2
+  TPLINK_BOARD_ID := ARCHER-C59-V2
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport kmod-ath10k-ct ath10k-firmware-qca9888-ct
+  SUPPORTED_DEVICES += archer-c59-v2
+endef
+TARGET_DEVICES += tplink_archer-c59-v2
+
 define Device/tplink_archer-c60-v1
   $(Device/tplink-safeloader-uimage)
   ATH_SOC := qca9561
