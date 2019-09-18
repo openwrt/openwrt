c79cb9fda71a49590dc785d7c6e1472eee134e02
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-09-18 12:50:22 +0200

    kernel: bump 4.19 to 4.19.73
    Refreshed all patches.
    
    Remove upstreamed:
    - 840-media-i2c-tda1997x-select-V4L2_FWNODE.patch
    - 079-v4.20-ARM-dts-qcom-ipq4019-fix-PCI-range.patch
    - 083-ARM-dts-qcom-ipq4019-enlarge-PCIe-BAR-range.patch
    - 084-ARM-dts-qcom-ipq4019-Fix-MSI-IRQ-type.patch
    
    Fixes:
    - CVE-2019-15030
    
    Compile-tested on: cns3xxx
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .193
 LINUX_VERSION-4.14 = .144
-LINUX_VERSION-4.19 = .72
+LINUX_VERSION-4.19 = .73
 
 LINUX_KERNEL_HASH-4.9.193 = 9be5081cc0fa6b720e6b3e5cb90296f3d857469bb3cda09ff93db00f9e16fd01
 LINUX_KERNEL_HASH-4.14.144 = cb8b84675ba060b249ffa62de2366d16e842e3edfcb355ebbfb06bf3de03ca54
-LINUX_KERNEL_HASH-4.19.72 = f9fcb6b3bd29115ac55fc154e300c3dce2044502732f6842ad6c25e6f9f51f6d
+LINUX_KERNEL_HASH-4.19.73 = 7db5e77ef2a2d8da4b128b7d47d7dd645ebcde2d19c1d191793e1eeefc275fab
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
