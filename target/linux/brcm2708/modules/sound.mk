openwrt/openwrt

commit ce536aeb3958dd305f4156f5ed55fd02f2e49736

brcm2708: add support for Hifiberry DAC+DSP
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

target/linux/brcm2708/modules/sound.mk
@@ -512,6 +512,26 @@ endef
 $(eval $(call KernelPackage,sound-soc-hifiberry-dacplusadc))
 
 
+define KernelPackage/sound-soc-hifiberry-dacplusdsp
+  TITLE:=Support for HifiBerry DAC+DSP
+  KCONFIG:= \
+    CONFIG_SND_BCM2708_SOC_HIFIBERRY_DACPLUSDSP
+  FILES:= \
+    $(LINUX_DIR)/sound/soc/bcm/snd-soc-hifiberry-dacplusdsp.ko \
+  AUTOLOAD:=$(call AutoLoad,68,snd-soc-hifiberry-dacplusdsp)
+  DEPENDS:= \
+    kmod-sound-soc-bcm2835-i2s \
+    +kmod-sound-soc-rpi-simple-soundcard
+  $(call AddDepends/sound)
+endef
+
+define KernelPackage/sound-soc-hifiberry-dacplusdsp/description
+  This package contains support for HifiBerry DAC+DSP
+endef
+
+$(eval $(call KernelPackage,sound-soc-hifiberry-dacplusdsp))
+
+
 define KernelPackage/sound-soc-hifiberry-dacplusadc-pro
   TITLE:=Support for HifiBerry DAC+ADC PRO
   KCONFIG:= \

commit 2e2fe82d1e74794a2fe78861ed43627798d5ae4b

brcm2708: fix Hifiberry DAC+DSP package definition
Fixes: ce536ae ("brcm2708: add support for Hifiberry DAC+DSP")
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

target/linux/brcm2708/modules/sound.mk
@@ -517,7 +517,7 @@ define KernelPackage/sound-soc-hifiberry-dacplusdsp
   KCONFIG:= \
     CONFIG_SND_BCM2708_SOC_HIFIBERRY_DACPLUSDSP
   FILES:= \
-    $(LINUX_DIR)/sound/soc/bcm/snd-soc-hifiberry-dacplusdsp.ko \
+    $(LINUX_DIR)/sound/soc/bcm/snd-soc-hifiberry-dacplusdsp.ko
   AUTOLOAD:=$(call AutoLoad,68,snd-soc-hifiberry-dacplusdsp)
   DEPENDS:= \
     kmod-sound-soc-bcm2835-i2s \