38bdfcdb87e9b3a1232e9820bd6534da4c28282b
Sungbo Eo <mans0n@gorani.run>
2019-09-27 00:16:33 +0900

    ramips: add support for ipTIME A104ns
    ipTIME A104ns is a 2.4/5GHz band AC750 router, based on MediaTek MT7620A.
    
    Specifications:
    - SoC: MT7620A
    - RAM: DDR2 64MB
    - Flash: SPI NOR 8MB
    - WiFi:
      - 2.4GHz: SoC internal
      - 5GHz: MT7610EN
    - Ethernet: 5x 10/100Mbps
      - Switch: SoC internal
    - USB: 1x 2.0
    - UART:
      - J2: 3.3V, TX, RX, GND (3.3V is the square pad) / 57600 8N1
    
    Installation via web interface:
    1.  Flash **initramfs** image through the stock web interface.
    2.  Boot into OpenWrt and perform sysupgrade with sysupgrade image.
    
    Revert to stock firmware:
    1.  Perform sysupgrade with stock image.
    
    In contrast to to-be-supported A1004ns, the A104ns has no usable
    value in 0x1fc40 (uboot), so wan_mac needs to be calculated.
    Also note that GPIOs for the LEDs really are inverted compared to
    the A1004ns.
    
    Signed-off-by: Sungbo Eo <mans0n@gorani.run>
    [moved state_default to device DTS, reordered properties in wmac,
    added comment about wan_mac and LED GPIOs]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -494,6 +494,16 @@ define Device/iodata_wn-ac733gr3
 endef
 TARGET_DEVICES += iodata_wn-ac733gr3
 
+define Device/iptime_a104ns
+  MTK_SOC := mt7620a
+  IMAGE_SIZE := 8000k
+  UIMAGE_NAME := a104ns
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := A104ns
+  DEVICE_PACKAGES := kmod-mt76x0e kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += iptime_a104ns
+
 define Device/kimax_u25awf-h1
   MTK_SOC := mt7620a
   IMAGE_SIZE := 16064k
