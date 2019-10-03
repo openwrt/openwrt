ccf8248681ae98434382a978f1840cb5874160fb
Cioby Moi <cioby.service@gmail.com>
2019-10-04 00:18:11 +0300

    mediatek: implement sysupgrade support part 02
    Signed-off-by: Cristian Ciobanu <cioby.service@gmail.com>

--- target/linux/mediatek/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/base-files/lib/upgrade/platform.sh
@@ -1,68 +1,89 @@
-platform_do_upgrade() {                 
-	local board=$(board_name)
-	case "$board" in
-	"unielec,u7623"*)
-		#Keep the persisten random mac address (if it exists)
-		mkdir -p /tmp/recovery
-		mount -o rw,noatime /dev/mmcblk0p1 /tmp/recovery
-		[ -f "/tmp/recovery/mac_addr" ] && \
-			mv -f /tmp/recovery/mac_addr /tmp/
-		umount /tmp/recovery
+platform_check_image() {
+	local diskdev partdev diff
 
-		#1310720 is the offset in bytes from the start of eMMC and to
-		#the location of the kernel (2560 512 byte sectors)
-		get_image "$1" | dd of=/dev/mmcblk0 bs=1310720 seek=1 conv=fsync
+	export_bootdevice && export_partdevice diskdev -2 || {
+		echo "Unable to determine upgrade device"
+		return 1
+	}
 
-		mount -o rw,noatime /dev/mmcblk0p1 /tmp/recovery
-		[ -f "/tmp/mac_addr" ] && mv -f /tmp/mac_addr /tmp/recovery
-		sync
-		umount /tmp/recovery
-		;;
-	*)
-		default_do_upgrade "$1"
-		;;
-	esac
-}
+	get_partitions "/dev/$diskdev" bootdisk
 
-PART_NAME=firmware
+	#extract the boot sector from the image
+	get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b 2>/dev/null
 
-platform_check_image() {                                                         
-	local board=$(board_name)                                                
-	local magic="$(get_magic_long "$1")"                                     
+	get_partitions /tmp/image.bs image
 
-	[ "$#" -gt 1 ] && return 1                                               
+	#compare tables
+	diff="$(grep -F -x -v -f /tmp/partmap.bootdisk /tmp/partmap.image)"
 
-	case "$board" in                                                       
-	bananapi,bpi-r2|\
-	"unielec,u7623"*)
-		[ "$magic" != "27051956" ] && {   
-			echo "Invalid image type."
-			return 1                                     
-		}                                                    
-		return 0                                             
-		;;                                                   
+	rm -f /tmp/image.bs /tmp/partmap.bootdisk /tmp/partmap.image
 
-	*)                                                           
-		echo "Sysupgrade is not supported on your board yet."
-		return 1                                             
-		;;                                
-	esac                                      
-
-	return 0                                                                                         
-}
-
-platform_copy_config_emmc() {
-	mkdir -p /recovery
-	mount -o rw,noatime /dev/mmcblk0p1 /recovery
-	cp -af "$UPGRADE_BACKUP" "/recovery/$BACKUP_FILE"
-	sync
-	umount /recovery
+	if [ -n "$diff" ]; then
+		echo "Partition layout has changed. Full image will be written."
+		ask_bool 0 "Abort" && exit 1
+		return 0
+	fi
 }
 
 platform_copy_config() {
-	case "$(board_name)" in
-	"unielec,u7623"*)
-		platform_copy_config_emmc
-		;;
-	esac
+	local partdev
+
+	if export_partdevice partdev -1; then
+		mount -t vfat -o rw,noatime "/dev/$partdev" /mnt
+		cp -af "$CONF_TAR" /mnt/
+		umount /mnt
+	fi
+}
+
+platform_do_upgrade() {
+	local diskdev partdev diff
+
+	export_bootdevice && export_partdevice diskdev -2 || {
+		echo "Unable to determine upgrade device"
+		return 1
+	}
+
+	sync
+
+	if [ "$SAVE_PARTITIONS" = "1" ]; then
+		get_partitions "/dev/$diskdev" bootdisk
+
+		#extract the boot sector from the image
+		get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b
+
+		get_partitions /tmp/image.bs image
+
+		#compare tables
+		diff="$(grep -F -x -v -f /tmp/partmap.bootdisk /tmp/partmap.image)"
+	else
+		diff=1
+	fi
+
+	if [ -n "$diff" ]; then
+		get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
+
+		# Separate removal and addtion is necessary; otherwise, partition 1
+		# will be missing if it overlaps with the old partition 2
+		partx -d - "/dev/$diskdev"
+		partx -a - "/dev/$diskdev"
+
+		return 0
+	fi
+
+	#write uboot image
+	get_image "$@" | dd of="$diskdev" bs=1024 skip=320 seek=320 count=700 conv=fsync
+	#iterate over each partition from the image and write it to the boot disk
+	while read part start size; do
+		part="$(($part - 2))"
+		if export_partdevice partdev $part; then
+			echo "Writing image to /dev/$partdev..."
+			get_image "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
+		else
+			echo "Unable to find partition $part device, skipped."
+		fi
+	done < /tmp/partmap.image
+
+	#copy partition uuid
+	echo "Writing new UUID to /dev/$diskdev..."
+	get_image "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
 }
