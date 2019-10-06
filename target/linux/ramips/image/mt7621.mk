ac36cca012dd1bbeea0fc4c2dc7a00941de34b52
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2019-10-06 21:07:33 +0200

    ramips: mt7621: Alternative name KE 2P for Phicomm K2P
    The Phicomm KE 2P is identical to the already supported Phicomm K2P,
    renamed for the European market. Use the ALT0 buildroot tags to show
    both devices.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -548,6 +548,8 @@ define Device/phicomm_k2p
   IMAGE_SIZE := 15744k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2P
+  DEVICE_ALT0_VENDOR := Phicomm
+  DEVICE_ALT0_MODEL := KE 2P
   SUPPORTED_DEVICES += k2p
   DEVICE_PACKAGES := kmod-mt7615e wpad-basic
 endef
