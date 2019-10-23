99835e09992bb411519fda1f8d16a6d07df7f598
André Valentin <avalentin@marcant.net>
2019-10-23 11:30:30 +0200

    ath79: add support for ZyXEL NBG6716
    Attention: Kernel partition size has been enlarged to 4MB.
    To switch, you must update to latest ar71xx-nand snapshort and flash the
    sysupgrade-4M-Kernel.bin:
    
    zcat openwrt-ath79-nand-zyxel_nbg6716-squashfs-sysupgrade-4M-Kernel.bin | mtd -r -e ubi write - firmware; reboot -f
    You will end up with a fresh config if you do not inject config into the image.
    
    The NBG6716 may come with 128MB or 256MB NAND. ar71xx was able to use all, but
    ath79 can only use the first 128MB. Therefore the complete NAND needs to be
    overwritten. If not, the old UBI may make problems and lead to reboot loop.
    
    Access the real u-boot shell:
    ZyXEL uses a proprietary loader/shell on top of u-boot: "ZyXEL zloader v2.02"
    When the device is starting up, the user can enter the the loader shell
    by simply pressing a key within the 3 seconds once the following string
    appears on the serial console:
    
    |   Hit any key to stop autoboot:  3
    
    The user is then dropped to a locked shell.
    
    |NBG6716> HELP
    |ATEN    x[,y]     set BootExtension Debug Flag (y=password)
    |ATSE    x         show the seed of password generator
    |ATSH              dump manufacturer related data in ROM
    |ATRT    [x,y,z,u] RAM read/write test (x=level, y=start addr, z=end addr, u=iterations)
    |ATGO              boot up whole system
    |ATUR    x         upgrade RAS image (filename)
    |NBG6716>
    
    In order to escape/unlock a password challenge has to be passed.
    Note: the value is dynamic! you have to calculate your own!
    
    First use ATSE $MODELNAME (MODELNAME is the hostname in u-boot env)
    to get the challange value/seed.
    
    |NBG6716> ATSE NBG6716
    |012345678901
    
    This seed/value can be converted to the password with the help of this
    bash script (Thanks to http://www.adslayuda.com/Zyxel650-9.html authors):
    
    - tool.sh -
    ror32() {
      echo $(( ($1 >> $2) | (($1 << (32 - $2) & (2**32-1)) ) ))
    }
    v="0x$1"
    a="0x${v:2:6}"
    b=$(( $a + 0x10F0A563))
    c=$(( 0x${v:12:14} & 7 ))
    p=$(( $(ror32 $b $c) ^ $a ))
    printf "ATEN 1,%X\n" $p
    - end of tool.sh -
    
    |# bash ./tool.sh 012345678901
    |
    |ATEN 1,879C711
    
    copy and paste the result into the shell to unlock zloader.
    
    |NBG6716> ATEN 1,0046B0017430
    
    If the entered code was correct the shell will change to
    use the ATGU command to enter the real u-boot shell.
    
    |NBG6716> ATGU
    |NBG6716#
    
    Signed-off-by: André Valentin <avalentin@marcant.net>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -1,3 +1,31 @@
+# attention: only zlib compression is allowed for the boot fs
+define  Build/zyxel-buildkerneljffs
+        rm -rf  $(KDIR_TMP)/zyxelnbg6716
+        mkdir -p $(KDIR_TMP)/zyxelnbg6716/image/boot
+        cp $@ $(KDIR_TMP)/zyxelnbg6716/image/boot/vmlinux.lzma.uImage
+        $(STAGING_DIR_HOST)/bin/mkfs.jffs2 \
+                --big-endian --squash-uids -v -e 128KiB -q -f -n -x lzma -x rtime \
+                -o $@ \
+                -d $(KDIR_TMP)/zyxelnbg6716/image
+        rm -rf $(KDIR_TMP)/zyxelnbg6716
+endef
+
+define Build/zyxel-factory
+	let \
+		maxsize="$(subst k,* 1024,$(RAS_ROOTFS_SIZE))"; \
+		let size="$$(stat -c%s $@)"; \
+		if [ $$size -lt $$maxsize ]; then \
+			$(STAGING_DIR_HOST)/bin/mkrasimage \
+				-b $(RAS_BOARD) \
+				-v $(RAS_VERSION) \
+				-r $@ \
+				-s $$maxsize \
+				-o $@.new \
+				-l 131072 \
+			&& mv $@.new $@ ; \
+		fi
+endef
+
 define Device/glinet_gl-ar300m-nand
   ATH_SOC := qca9531
   DEVICE_VENDOR := GL.iNet
@@ -13,3 +41,24 @@ define Device/glinet_gl-ar300m-nand
   IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
 endef
 TARGET_DEVICES += glinet_gl-ar300m-nand
+
+define Device/zyxel_nbg6716
+  ATH_SOC := qca9558
+  DEVICE_VENDOR := ZyXEL
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport kmod-ath10k-ct ath10k-firmware-qca988x-ct
+  RAS_BOARD := NBG6716
+  RAS_ROOTFS_SIZE := 29696k
+  RAS_VERSION := "OpenWrt Linux-$(LINUX_VERSION)"
+  KERNEL_SIZE := 4096k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL := kernel-bin | append-dtb | uImage none | \
+	zyxel-buildkerneljffs | check-size 4096k
+  IMAGES := sysupgrade.tar sysupgrade-4M-Kernel.bin factory.bin
+  IMAGE/sysupgrade.tar/squashfs := append-rootfs | pad-to $$$${BLOCKSIZE} | sysupgrade-tar rootfs=$$$$@ | append-metadata
+  IMAGE/sysupgrade-4M-Kernel.bin/squashfs := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | pad-to 263192576 | gzip
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | zyxel-factory
+  UBINIZE_OPTS := -E 5
+endef
+TARGET_DEVICES += zyxel_nbg6716
+DEVICE_VARS += RAS_ROOTFS_SIZE RAS_BOARD RAS_VERSION
