19ff3f5105791a31b27932e1bf2ad9e150604b6e
Andrew Cameron <apcameron@softhome.net>
2019-10-25 20:29:36 -0500

    ath79: add support for the TP-LINK CPE220 V3
    This adds support for a popular low-cost 2.4GHz N based AP
    
    Specifications:
    SoC: Qualcomm Atheros QCA9533 (650MHz)
    RAM: 64MB
    Storage: 8 MB SPI NOR
    Wireless: 2.4GHz N based built into SoC 2x2
    Ethernet: 2x 100/10 Mbps, integrated into SoC
    
    Installation:
    Flash factory image through stock firmware WEB UI
    or through TFTP
    To get to TFTP recovery just hold reset button while powering on for
    around 4-5 seconds and release.
    Rename factory image to recovery.bin
    Stock TFTP server IP:192.168.0.100
    Stock device TFTP adress:192.168.0.254
    
    This also applies some minor changes to the common DTSI:
    - use &wmac for label-mac-device, as this one is actually set up in
      common DTSI
    - move &eth0 to parent DTSI
    - fix several leading spaces, added/removed newlines
    
    Signed-off-by: Andrew Cameron <apcameron@softhome.net>
    [DTS style fixes/improvements, updated commit message/title,
    backport to 19.07]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -168,6 +168,17 @@ define Device/tplink_cpe210-v3
 endef
 TARGET_DEVICES += tplink_cpe210-v3
 
+define Device/tplink_cpe220-v3
+  $(Device/tplink-safeloader)
+  ATH_SOC := qca9533
+  IMAGE_SIZE := 7680k
+  DEVICE_TITLE := TP-Link CPE220 v3
+  DEVICE_PACKAGES := rssileds
+  TPLINK_BOARD_ID := CPE220V3
+  LOADER_TYPE := elf
+endef
+TARGET_DEVICES += tplink_cpe220-v3
+
 define Device/tplink_archer-d50-v1
   ATH_SOC := qca9531
   DEVICE_TITLE := TP-Link Archer D50 v1
