f85d56bb034d8017bdb6b4bb493eb076a21d11b3
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2019-10-30 10:07:27 +0100

    ath79: add support for Netgear WNR2200
    This patch adds ath79 support for Netgear WNR2200.
    Router was previously supported by ar71xx target only (8 MiB variant).
    Netgear WNR2200 has two flash versions - 8MiB sold in EU, US etc. and
    16 MiB for Russia and China markets. Apart from flash size both variants
    share the same hardware specification.
    
    Specification
    =============
      * Description: Netgear WNR2200
      * Loader: U-boot
      * SOC: Atheros AR7241 (360 MHz)
      * RAM: 64 MiB
      * Flash: 8 MiB or 16 MiB (SPI NOR)
    	- U-boot binary: 256 KiB
    	- U-boot environment: 64 KiB
    	- Firmware: 7808 KiB or 16000 KiB
    	- ART: 64 KiB
      * Ethernet: 4 x 10/100 LAN + 1 x 10/100 WAN
      * Wireless: 2.4 GHz b/g/n (Atheros AR9287)
      * USB: yes, 1 x USB 2.0
      * Buttons:
    	- Reset
    	- WiFi (rfkill)
    	- WPS
      * LEDs:
    	- Power (amber/green)
    	- WAN (amber/green)
    	- WLAN (blue)
    	- 4 x LAN (amber/green)
    	- WPS (green)
      * UART: 4-pin connector JP1, 3.3V (Vcc, TX, RX, GND), 115200 8N1
      * Power supply: DC 12V 1.5A
      * MAC addresses: LAN on case label, WAN +1, WLAN +2
    
    Installation
    ============
      * TFTP recovery
      * TFTP via U-boot prompt
      * sysupgrade
      * Web interface
    
    Test build configuration
    ========================
    CONFIG_TARGET_ath79=y
    CONFIG_TARGET_ath79_generic=y
    CONFIG_TARGET_ath79_generic_DEVICE_netgear_wnr2200-8m=y
    CONFIG_ALL_KMODS=y
    CONFIG_DEVEL=y
    CONFIG_CCACHE=y
    CONFIG_COLLECT_KERNEL_DEBUG=y
    CONFIG_IMAGEOPT=y
    CONFIG_KERNEL_DEBUG_INFO=y
    CONFIG_KERNEL_DEBUG_KERNEL=y
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -794,6 +794,40 @@ define Device/netgear_wndr3800ch
 endef
 TARGET_DEVICES += netgear_wndr3800ch
 
+define Device/netgear_wnr2200_common
+  ATH_SOC := ar7241
+  DEVICE_MODEL := WNR2200
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport
+  NETGEAR_KERNEL_MAGIC := 0x32323030
+  NETGEAR_BOARD_ID := wnr2200
+  IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | netgear-squashfs | \
+	append-rootfs | pad-rootfs
+  $(Device/netgear_ath79)
+endef
+
+define Device/netgear_wnr2200-8m
+  $(Device/netgear_wnr2200_common)
+  DEVICE_VARIANT := 8M
+  NETGEAR_HW_ID := 29763600+08+64
+  IMAGE_SIZE := 7808k
+  IMAGES += factory-NA.img
+  IMAGE/factory-NA.img := $$(IMAGE/default) | netgear-dni NA | \
+	check-size $$$$(IMAGE_SIZE)
+  SUPPORTED_DEVICES += wnr2200
+endef
+TARGET_DEVICES += netgear_wnr2200-8m
+
+define Device/netgear_wnr2200-16m
+  $(Device/netgear_wnr2200_common)
+  DEVICE_VARIANT := 16M
+  DEVICE_ALT0_VENDOR := NETGEAR
+  DEVICE_ALT0_MODEL := WNR2200
+  DEVICE_ALT0_VARIANT := CN/RU
+  NETGEAR_HW_ID :=
+  IMAGE_SIZE := 16000k
+endef
+TARGET_DEVICES += netgear_wnr2200-16m
+
 define Device/ocedo_koala
   ATH_SOC := qca9558
   DEVICE_VENDOR := Ocedo
