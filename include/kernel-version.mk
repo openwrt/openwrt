6ac1aa19579ddbee922ea5b89d7398f527baabd8
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-11-06 17:14:43 +0100

    kernel: bump 4.14 to 4.14.152
    Refreshed all patches.
    
    Altered patches:
    - 301-arch-support-layerscape.patch
    
    Remove upstreamed:
    - 950-0311-sc16is7xx-Fix-for-Unexpected-interrupt-8.patch
    
    Compile-tested on: ar71xx, cns3xxx, imx6, x86_64
    Runtime-tested on: ar71xx, cns3xxx, imx6
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .151
+LINUX_VERSION-4.14 = .152
 
-LINUX_KERNEL_HASH-4.14.151 = ff519c428ee9bbb513a84db5ec32a7e3705cd8c23a57104b25b944cb79583fae
+LINUX_KERNEL_HASH-4.14.152 = a9239fd9d341aae07b48a8e7a2afd528fe5cde2617d783454c71e6f3dae2b0aa
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
