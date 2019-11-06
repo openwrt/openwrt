06cdbcae201f173af1f4a1aa69b7ef41a9a31c7c
Chen Minqiang <ptpt52@gmail.com>
2019-11-06 22:30:12 +0800

    ramips: thunder_timecloud support sdcard

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1313,7 +1313,7 @@ define Device/thunder_timecloud
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Thunder
   DEVICE_MODEL := Timecloud
-  DEVICE_PACKAGES := kmod-usb3 -wpad-basic-wolfssl
+  DEVICE_PACKAGES := kmod-usb3 kmod-sdhci-mt7620 -wpad-basic-wolfssl
   SUPPORTED_DEVICES += timecloud
 endef
 TARGET_DEVICES += thunder_timecloud
