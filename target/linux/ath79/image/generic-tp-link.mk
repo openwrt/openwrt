ab29ffbb105ab7655b05871248c89d09e7e38261
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-11-11 19:03:20 +0100

    ath79: fix SUPPORTED_DEVICES for TP-Link TL-WR1043ND v3
    In ar71xx, the board name for the TL-WR1043ND v3 is equal to v2:
    tl-wr1043nd-v2
    
    Fix SUPPORTED_DEVICES for v3 in ath79 accordingly.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    (cherry picked from commit b5791118ccd3039cc9ef6fd3b1c4efcad6ab154f)

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -303,7 +303,7 @@ define Device/tplink_tl-wr1043nd-v3
   DEVICE_TITLE := TP-Link TL-WR1043N/ND v3
   DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport
   TPLINK_HWID := 0x10430003
-  SUPPORTED_DEVICES += tl-wr1043nd-v3
+  SUPPORTED_DEVICES += tl-wr1043nd-v2
 endef
 TARGET_DEVICES += tplink_tl-wr1043nd-v3
 
