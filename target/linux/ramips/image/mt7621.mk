ca17f4a0a0721d7fc447be4f6d535d49c7380156
Sungbo Eo <mans0n@gorani.run>
2019-11-12 01:25:31 +0900

    ramips: add support for ipTIME A6ns-M
    ipTIME A6ns-M is a 2.4/5GHz band AC1900 router, based on MediaTek MT7621A.
    
    Specifications:
    - SoC: MT7621AT
    - RAM: DDR3 128MB
    - Flash: SPI NOR 16MB
    - WiFi:
      - 2.4GHz: MT7615
      - 5GHz: MT7615
    - Ethernet: 5x 10/100/1000Mbps
      - Switch: SoC internal
    - UART:
      - J4: 3.3V, TX, RX, GND (3.3V is the square pad) / 57600 8N1
    
    Installation via web interface:
    1.  Flash **initramfs** image through the stock web interface.
    2.  Boot into OpenWrt and perform sysupgrade with sysupgrade image.
    
    Revert to stock firmware:
    1.  Perform sysupgrade with stock image.
    
    Signed-off-by: Sungbo Eo <mans0n@gorani.run>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -361,6 +361,16 @@ define Device/iodata_wnpr2600g
 endef
 TARGET_DEVICES += iodata_wnpr2600g
 
+define Device/iptime_a6ns-m
+  MTK_SOC := mt7621
+  IMAGE_SIZE := 16128k
+  UIMAGE_NAME := a6nm
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := A6ns-M
+  DEVICE_PACKAGES := kmod-mt7615e kmod-usb3 kmod-usb-ledtrig-usbport wpad-basic
+endef
+TARGET_DEVICES += iptime_a6ns-m
+
 define Device/lenovo_newifi-d1
   MTK_SOC := mt7621
   IMAGE_SIZE := 32448k
