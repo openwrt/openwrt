7533087565005ecb41954790d4991cb7c32bb066
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-11-18 10:17:50 +0100

    kernel: bump 4.19 to 4.19.84
    Refreshed all patches.
    
    Altered patches:
    - 950-0748-net-bcmgenet-Workaround-for-Pi-4B-network-issue.patch
    - 950-0754-Revert-net-bcmgenet-Workaround-for-Pi-4B-network-iss.patch
    - 902-debloat_proc.patch
    
    Remove upstreamed:
    - 150-MIPS-bmips-mark-exception-vectors-as-char-arrays.patch
    
    New symbols:
    X86_INTEL_MPX
    X86_INTEL_MEMORY_PROTECTION_KEYS
    X86_INTEL_TSX_MODE_OFF
    X86_INTEL_TSX_MODE_ON
    X86_INTEL_TSX_MODE_AUTO
    
    Compile-tested on: cns3xxx, x86_64
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .202
 LINUX_VERSION-4.14 = .154
-LINUX_VERSION-4.19 = .82
+LINUX_VERSION-4.19 = .84
 
 LINUX_KERNEL_HASH-4.9.202 = 8108ec1cd10fc40821c84e9f087dba10b1767aad66596f4a36925faef55e4ebf
 LINUX_KERNEL_HASH-4.14.154 = 86de4a24379a78d67fc08b2e249ccf7b689eb6c37fbe329fc3dfbe8ab4300603
-LINUX_KERNEL_HASH-4.19.82 = 58d96d6c2c5ee8288fe9714891e4037a18f457b008e369e33fc744afc2cb595d
+LINUX_KERNEL_HASH-4.19.84 = 868b4a92619cb00ab142a20a67f000525b9605820d1b66faa4a183133eac0660
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
