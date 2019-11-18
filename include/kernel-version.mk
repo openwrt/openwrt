0a9147be6a5e920ea190c9a2bc35fe8e5d772693
Koen Vandeputte <koen.vandeputte@ncentric.com>
2019-11-18 10:24:38 +0100

    kernel: bump 4.14 to 4.14.154
    Refreshed all patches.
    
    Altered patches:
    - 902-debloat_proc.patch
    - 040-dmaengine-qcom-bam-Process-multiple-pending-descript.patch
    
    Remove upstreamed:
    - 303-spi-nor-enable-4B-opcodes-for-mx66l51235l.patch
    
    New symbols:
    X86_INTEL_MPX
    X86_INTEL_MEMORY_PROTECTION_KEYS
    X86_INTEL_TSX_MODE_OFF
    X86_INTEL_TSX_MODE_ON
    X86_INTEL_TSX_MODE_AUTO
    SGL_ALLOC
    
    Compile-tested on: cns3xxx
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -3,10 +3,10 @@
 LINUX_RELEASE?=1
 
 LINUX_VERSION-4.9 = .202
-LINUX_VERSION-4.14 = .152
+LINUX_VERSION-4.14 = .154
 
 LINUX_KERNEL_HASH-4.9.202 = 8108ec1cd10fc40821c84e9f087dba10b1767aad66596f4a36925faef55e4ebf
-LINUX_KERNEL_HASH-4.14.152 = a9239fd9d341aae07b48a8e7a2afd528fe5cde2617d783454c71e6f3dae2b0aa
+LINUX_KERNEL_HASH-4.14.154 = 86de4a24379a78d67fc08b2e249ccf7b689eb6c37fbe329fc3dfbe8ab4300603
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
