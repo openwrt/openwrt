84b74bcb0d962dc192dc6c3f0ada3e0eba6fb593
Yorkie Liu <yorkiefixer@gmail.com>
2019-11-19 13:41:10 +0800

    toolchain/gcc: correct the check expr for newer clang
    This fixes gcc build error within clang 11.0, it tweaks the version
    string from LLVM to clang.
    
    Signed-off-by: Yorkie Liu <yorkiefixer@gmail.com>
    (cherry picked from commit 65a561fd0919eafff2363ae8324db64be2a57f77)

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -96,7 +96,7 @@ endif
 
 GCC_CONFIGURE:= \
 	SHELL="$(BASH)" \
-	$(if $(shell gcc --version 2>&1 | grep LLVM), \
+	$(if $(shell gcc --version 2>&1 | grep -E "Apple.(LLVM|clang)"), \
 		CFLAGS="-O2 -fbracket-depth=512 -pipe" \
 		CXXFLAGS="-O2 -fbracket-depth=512 -pipe" \
 	) \
