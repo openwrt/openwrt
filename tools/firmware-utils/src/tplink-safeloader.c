abaf329dad058545cfd1e05d52990d7b9d3a58f8
Anderson Vulczak <andi@andi.com.br>
2019-11-27 21:31:52 -0200

    tools: tplink-safeloader: update soft_ver for TP-Link Archer C6 v2 (EU)
    This patch updates "soft_ver" for TP-Link Archer C6 v2 (EU).
    
    It makes possible to upload OpenWrt on lastest vendor's firmware
    as the web-based updater checks for major.minor version during upload.
    
    Due to that on next major/minor version update TP-Link will stop
    us from using the web-based firmware update tool, so it will
    require a new patch on soft_ver to match major and minor version.
    Up to today's latest stock firmware the patch (major.minor.patch)
    version does not matters, that allows downgrade from 1.1.4 to 1.1.1
    but do not allow downgrade from 1.1.X to 1.0.X.
    
    Signed-off-by: Anderson Vulczak <andi@andi.com.br>
    (cherry picked from commit 7a8bfbf0ffaea14e15ae2016a54adf48337c6251)
    (cherry picked from commit eeb93e5c2b23518e372737ecf8fdde4b9f139a49)

--- tools/firmware-utils/src/tplink-safeloader.c
+++ tools/firmware-utils/src/tplink-safeloader.c
@@ -703,7 +703,7 @@ static struct device_info boards[] = {
 			"{product_name:Archer C6,product_ver:2.0.0,special_id:52550000}\r\n"
 			"{product_name:Archer C6,product_ver:2.0.0,special_id:4A500000}\r\n",
 		.support_trail = '\x00',
-		.soft_ver = "soft_ver:1.0.0\n",
+		.soft_ver = "soft_ver:1.1.1\n",
 
 		.partitions = {
 			{"fs-uboot", 0x00000, 0x20000},
