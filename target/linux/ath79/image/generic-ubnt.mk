b6946542f85014096c08f58c3c0acbbfe623a734
Tobias Schramm <tobleminer@gmail.com>
2019-12-03 22:48:35 +0100

    ath79: add support for Ubiquiti ToughSwitch/EdgeSwitch 8XP
    Flash:    8 MB
    RAM:      64 MB
    SoC:      AR7242
    Switch:   bcm53128
    USB:      1x USB 2.0
    Ethernet: 8x GbE, 1x FE
    
    The Ubiquiti ToughSwitch 8XP is a 8-port PoE Gigabit switch with a single
    Fast-Ethernet management port. It supports both 24V passive PoE and 48V
    802.11af/at PoE out on all eight ports.
    
    By default the single Fast-Ethernet port labeled "MGMT" is configured as the
    WAN port. Thus access to the device is only possible via the eight switch
    ports.
    
    Installation of the firware is possible either via serial + tftpboot or
    the factory firmware update function via webinterface.
    
    Serial: 3v3 115200 8n1
    
    The serial header is located in the lower left corner of the switches PCB:
    
    |
    |
    |
    | o
    | o RX
    | o TX
    | o GND
    |
    |
    ++  +-++-+  ++  ++  +
     +--+ ++ +--++--++--+
    
    Signed-off-by: Tobias Schramm <tobleminer@gmail.com>
    [fix whitespace issue]
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -129,6 +129,13 @@ define Device/ubnt_bullet-m-xw
 endef
 TARGET_DEVICES += ubnt_bullet-m-xw
 
+define Device/ubnt_edgeswitch-8xp
+  $(Device/ubnt-sw)
+  DEVICE_MODEL := EdgeSwitch 8XP
+  DEVICE_PACKAGES += switch-bcm53xx-mdio
+endef
+TARGET_DEVICES += ubnt_edgeswitch-8xp
+
 define Device/ubnt_lap-120
   $(Device/ubnt-wa)
   DEVICE_MODEL := LiteAP ac
