c640370939d725238aa6200423eb8aabf59ef23e
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-12-16 00:21:33 +0100

    lantiq: use soc_vendor_device scheme on DTS file
    This renames lantiq DTS(I) files to follow soc_vendor_device scheme.
    This will make DTS files easier to maintain.
    
    As a side effect, DTS file name can be derived from device node
    names now, only having to specify a SOC variable in Makefiles.
    
    While at it, move files to arch/mips/boot/dts/lantiq subfolder.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/lantiq/image/amazonse.mk
+++ target/linux/lantiq/image/amazonse.mk
@@ -2,7 +2,7 @@ define Device/allnet_all0333cj
   DEVICE_VENDOR := Allnet
   DEVICE_MODEL := ALL0333CJ
   IMAGE_SIZE := 3700k
-  DEVICE_DTS := ALL0333CJ
+  SOC := amazonse
   DEVICE_PACKAGES := kmod-ltq-adsl-ase kmod-ltq-adsl-ase-mei \
 	kmod-ltq-adsl-ase-fw-b kmod-ltq-atm-ase \
 	ltq-adsl-app ppp-mod-pppoe
@@ -13,7 +13,7 @@ define Device/netgear_dgn1000b
   DEVICE_VENDOR := NETGEAR
   DEVICE_MODEL := DGN1000B
   IMAGE_SIZE := 6000k
-  DEVICE_DTS := DGN1000B
+  SOC := amazonse
   DEVICE_PACKAGES := kmod-ltq-adsl-ase kmod-ltq-adsl-ase-mei \
 	kmod-ltq-adsl-ase-fw-b kmod-ltq-atm-ase \
 	ltq-adsl-app ppp-mod-pppoe
