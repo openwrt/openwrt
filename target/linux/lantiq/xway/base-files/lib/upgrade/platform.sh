355f0444f3622347650776dd8f1e388dc5d000ca
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-12-17 01:15:35 +0100

    lantiq: split base-files into subtargets
    This splits the device-dependent base-files into subtarget directories,
    like done recently for ath79 and ramips. While this increases the
    overall lines of codes, it will make the code per subtarget smaller
    and easier to keep track of features and devices.
    
    While at it, several variables at the top of 02_network are removed,
    as they were never changed. The values are put directly into the
    function calls where they are used.
    
    Remove unneeded LED setup from 01_leds, and remove 01_leds entirely
    for falcon subtarget (as it is not used there).
    
    Applies alphabetic reordering to device cases in base-files.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/lantiq/xway/base-files/lib/upgrade/platform.sh
+++ target/linux/lantiq/xway/base-files/lib/upgrade/platform.sh
@@ -9,15 +9,8 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	avm,fritz3370-rev2-hynix|\
-	avm,fritz3370-rev2-micron|\
-	avm,fritz7362sl|\
-	avm,fritz7412|\
 	bt,homehub-v2b|\
-	bt,homehub-v3a|\
-	bt,homehub-v5a|\
-	zyxel,p-2812hnu-f1|\
-	zyxel,p-2812hnu-f3)
+	bt,homehub-v3a)
 		nand_do_upgrade $1
 		;;
 	*)
