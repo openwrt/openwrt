121ebbfe8efa4ab1cba0a14a9dd12c93573d3b88
Josua Mayer <josua.mayer@jm0.eu>
2019-12-26 15:09:28 +0100

    mvebu: switch clearfog pro to using DSA
    The Clearfog Pro came with a DSA compatible switch from the start. Since
    OpenWRT has finally started using DSA, e.g. for the Turris Omnia, this
    seems like an appropriate time to finally make the change.
    
    Signed-off-by: Josua Mayer <josua.mayer@jm0.eu>

--- target/linux/mvebu/image/cortexa9.mk
+++ target/linux/mvebu/image/cortexa9.mk
@@ -223,7 +223,7 @@ define Device/solidrun_clearfog-pro-a1
   DEVICE_MODEL := ClearFog Pro
   KERNEL_INSTALL := 1
   KERNEL := kernel-bin
-  DEVICE_PACKAGES := mkf2fs e2fsprogs partx-utils swconfig
+  DEVICE_PACKAGES := mkf2fs e2fsprogs partx-utils
   IMAGES := sdcard.img.gz
   IMAGE/sdcard.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   DEVICE_DTS := armada-388-clearfog-pro armada-388-clearfog-base
