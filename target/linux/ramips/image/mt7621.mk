6e80df5e3344092a391468b1718e412f0dd151c1
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-12-27 19:05:06 +0100

    ramips: add support for NETGEAR R6700v2/AC2400
    SoC: MediaTek MT7621AT
    RAM: 256M DDR3
    FLASH: 128M NAND
    WiFi: MediaTek MT7615N an+ac
    MediaTek MT7615N bgn
    ETH: MediaTek MT7621AT
    BTN: 1x Connect (WPS), 1x WLAN, 1x Reset
    LED: Power (white/amber), WAN(white/amber), 2.4G(white), 5G(white),
    USB(white) , GuestWifi(white) 4x LAN(white/amber), Wifi Button(white),
    WPS Button(white)
    
    Installation:
    
    Login to netgear webinterface and flash factory.img
    
    Based on a discontinued GitHub Pull Request by
    kuyokushin <codenamezero@protonmail.com>
    
    https://github.com/openwrt/openwrt/pull/2545
    
    NOTE: Netgear R6700 v2 have five clones: R6900 v2, R7450, Nighthawk
    AC2400, Nighthawk AC2100 and already added R6800. Rest of them  should
    be really easy supportable. Image for R6700v2 should work perfectly with
    them. Please refer:
    
    https://github.com/openwrt/openwrt/pull/2614
    
    Tested-by: Víctor Gibrán <victorgibranmz@hotmail.com> [R6700v2]
    Tested-by: John Landrum <jl31m10@yahoo.com> [AC2400]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    [add guest led to mt7621_netgear_r6700-v2.dts end edit commit message]
    Signed-off-by: Pawel Dembicki <paweldembicki@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -595,6 +595,21 @@ define Device/netgear_r6350
 endef
 TARGET_DEVICES += netgear_r6350
 
+define Device/netgear_r6700-v2
+  $(Device/netgear_sercomm_nand)
+  DEVICE_MODEL := R6700
+  DEVICE_VARIANT := v2
+  DEVICE_ALT0_VENDOR := NETGEAR
+  DEVICE_ALT0_MODEL := Nighthawk AC2400
+  SERCOMM_HWNAME := R6950
+  SERCOMM_HWID := BZV
+  SERCOMM_HWVER := A001
+  SERCOMM_SWVER := 0x1032
+  IMAGE_SIZE := 40960k
+  DEVICE_PACKAGES += kmod-mt7615e
+endef
+TARGET_DEVICES += netgear_r6700-v2
+
 define Device/netgear_r6800
   $(Device/netgear_sercomm_nand)
   DEVICE_MODEL := R6800
