0a3350d908ec466206f58b9e6b300c49e4fb3b13
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2019-12-30 21:58:34 +0100

    brcm63xx: use compatible instead of manually setting board name
    This patch removes the translation of device model name into a
    board name in lib/brcm63xx.sh. The latter has been actually totally
    useless as we have the compatible which can be used instead of
    the board name (and actually is at other targets like ath79 or ramips).
    
    The change requires updating the base-files with the new
    identifiers based on compatible.
    
    With all "board names" replaced by the compatible, we do not need
    the old scripts to detect board name anymore and can also remove
    the obsolete treatment of "legacy" devices without DTS, as there
    are none of those left. So, this patch removes the target-specific
    board detection and uses the standard procedure in
    package/base-files/files/lib/preinit/02_sysinfo
    
    This also fixes several cases where the board name was not set or
    evaluated correctly:
    - asmax,ar1004g in 02_network
    - telsey,magic in 09_fix_crc/02_network
    - brcm,bcm96338gw in 02_network
    - brcm,bcm96338w in 02_network
    - brcm,bcm96348gw in 02_network
    - dynalink,rta1025w in 02_network
    - huawei,echolife-hg520v in 02_network
    - several cases in diag.sh
    
    The following orphaned identifiers are removed from board.d files:
    - dmv-s0
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/brcm63xx/base-files/etc/diag.sh
+++ target/linux/brcm63xx/base-files/etc/diag.sh
@@ -6,141 +6,143 @@
 
 set_state() {
 	case "$(board_name)" in
-	a4001n1)
-		status_led="A4001N1:green:power"
+	actiontec,r1000h)
+		status_led="R1000H:green:power"
 		;;
-	a4001n)
+	adb,a4001n)
 		status_led="A4001N:green:power"
 		;;
-	ad1018-nor)
-		status_led="AD1018:green:power"
+	adb,a4001n1)
+		status_led="A4001N1:green:power"
 		;;
-	ar-5315u)
-		status_led="AR-5315u:green:power"
-		;;
-	ar-5381u)
-		status_led="AR-5381u:green:power"
-		;;
-	ar-5387un)
-		status_led="AR-5387un:green:power"
-		;;
-	av4202n)
+	adb,av4202n)
 		status_led="AV4202N:white:power"
 		;;
-	bcm96348gw)
-		status_led="96348GW:green:power"
-		;;
-	bcm963281tan)
-		status_led="963281TAN::power"
-		;;
-	bcm96328avng)
-		status_led="96328avng::power"
-		;;
-	bcm96348gw-11)
-		status_led="96348GW-11:green:power"
-		;;
-	spw303v)
-		status_led="spw303v:green:power+adsl"
-		;;
-	vh4032n)
-		status_led="VH4032N:blue:power"
-		;;
-	vr-3025un)
-		status_led="VR-3025un:green:power"
-		;;
-	vr-3025u)
-		status_led="VR-3025u:green:power"
-		;;
-	vr-3026e)
-		status_led="VR-3026e:green:power"
-		;;
-	wap-5813n)
-		status_led="WAP-5813n:green:power"
-		;;
-	ar1004g)
+	asmax,ar-1004g)
 		status_led="AR1004G:green:power"
 		;;
-	dsl-274xb-c|\
-	dsl-274xb-f)
-		status_led="dsl-274xb:green:power"
+	brcm,bcm963281tan)
+		status_led="963281TAN::power"
 		;;
-	dsl-275xb-d)
-		status_led="dsl-275xb:green:power"
+	brcm,bcm96328avng)
+		status_led="96328avng::power"
 		;;
-	cpva642)
-		status_led="CPVA642:green:power:"
+	brcm,bcm96348gw)
+		status_led="96348GW:green:power"
 		;;
-	ct-536p_5621t)
-		status_led="CT536_CT5621:green:power"
+	brcm,bcm96348gw-11)
+		status_led="96348GW-11:green:power"
 		;;
-	cvg834g)
-		status_led="CVG834G:green:power"
-		;;
-	dsl-2640b-b)
-		status_led="D-4P-W:green:power"
-		;;
-	dgnd3700v1_dgnd3800b)
-		status_led="DGND3700v1_3800B:green:power"
-		;;
-	evg2000)
-		status_led="EVG2000:green:power"
-		;;
-	fast2504n)
-		status_led="fast2504n:green:ok"
-		;;
-	fast2704n)
-		status_led2="F@ST2704N:red:power"
-		;;
-	fast2704v2)
-		status_led="F@ST2704V2:green:power"
-		;;
-	homehub2a)
+	bt,home-hub-2-a)
 		status_led="HOMEHUB2A:green:upgrading"
 		status_led2="HOMEHUB2A:blue:upgrading"
 		;;
-	gw6200)
-		status_led="GW6200:green:line1"
-		status_led2="GW6200:green:tel"
+	bt,voyager-2110)
+		status_led="V2110:power:green"
 		;;
-	hg553)
-		status_led="HW553:blue:power"
+	comtrend,ar-5315u)
+		status_led="AR-5315u:green:power"
 		;;
-	hg556a_*)
-		status_led="HW556:red:power"
+	comtrend,ar-5381u)
+		status_led="AR-5381u:green:power"
 		;;
-	hg520)
-		status_led="HW520:green:net"
+	comtrend,ar-5387un)
+		status_led="AR-5387un:green:power"
 		;;
-	hg655b)
-		status_led="HW65x:green:power"
+	comtrend,ct-536plus)
+		status_led="CT536_CT5621:green:power"
 		;;
-	livebox1)
-		status_led="Livebox1:red:adsl-fail-power"
+	comtrend,vr-3025u)
+		status_led="VR-3025u:green:power"
 		;;
-	p870hw-51a_v2)
-		status_led="P870HW-51a:green:power"
+	comtrend,vr-3025un)
+		status_led="VR-3025un:green:power"
 		;;
-	r1000h)
-		status_led="R1000H:green:power"
+	comtrend,vr-3026e)
+		status_led="VR-3026e:green:power"
 		;;
-	r5010un_v2)
-		status_led="R5010UNv2:green:power"
+	comtrend,wap-5813n)
+		status_led="WAP-5813n:green:power"
 		;;
-	rta770bw)
+	d-link,dsl-2640b-b)
+		status_led="D-4P-W:green:power"
+		;;
+	d-link,dsl-274xb-c2|\
+	d-link,dsl-274xb-f1)
+		status_led="dsl-274xb:green:power"
+		;;
+	d-link,dsl-275xb-d1)
+		status_led="dsl-275xb:green:power"
+		;;
+	dynalink,rta770bw)
 		status_led="RTA770BW:green:diag"
 		;;
-	rta770w)
+	dynalink,rta770w)
 		status_led="RTA770W:green:diag"
 		;;
-	sr102)
+	huawei,echolife-hg520v)
+		status_led="HW520:green:net"
+		;;
+	huawei,echolife-hg553)
+		status_led="HW553:blue:power"
+		;;
+	huawei,echolife-hg556a-a|\
+	huawei,echolife-hg556a-b|\
+	huawei,echolife-hg556a-c)
+		status_led="HW556:red:power"
+		;;
+	huawei,echolife-hg655b)
+		status_led="HW65x:green:power"
+		;;
+	inventel,livebox-1)
+		status_led="Livebox1:red:adsl-fail-power"
+		;;
+	netgear,cvg834g)
+		status_led="CVG834G:green:power"
+		;;
+	netgear,dgnd3700-v1)
+		status_led="DGND3700v1_3800B:green:power"
+		;;
+	netgear,evg2000)
+		status_led="EVG2000:green:power"
+		;;
+	nucom,r5010un-v2)
+		status_led="R5010UNv2:green:power"
+		;;
+	observa,vh4032n)
+		status_led="VH4032N:blue:power"
+		;;
+	sagem,fast-2504n)
+		status_led="fast2504n:green:ok"
+		;;
+	sagem,fast-2704n)
+		status_led2="F@ST2704N:red:power"
+		;;
+	sagem,fast-2704-v2)
+		status_led="F@ST2704V2:green:power"
+		;;
+	sercomm,ad1018-nor)
+		status_led="AD1018:green:power"
+		;;
+	sky,sr102)
 		status_led="SR102:white:power"
 		status_led2="SR102:red:power"
 		;;
-	spw500v)
+	t-com,speedport-w-303v)
+		status_led="spw303v:green:power+adsl"
+		;;
+	t-com,speedport-w-500v)
 		status_led="SPW500V:green:power"
 		;;
-	v2110)
-		status_led="V2110:power:green"
+	tecom,gw6200)
+		status_led="GW6200:green:line1"
+		status_led2="GW6200:green:tel"
+		;;
+	telsey,cpva642)
+		status_led="CPVA642:green:power:"
+		;;
+	zyxel,p870hw-51a-v2)
+		status_led="P870HW-51a:green:power"
 		;;
 	esac
 
