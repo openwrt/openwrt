330046922b060aae8577f500e68b74838e4416e5
Hauke Mehrtens <hauke@hauke-m.de>
2020-01-04 19:58:43 +0100

    kernel: bump 4.14 to 4.14.162
    Refreshed all patches.
    
    Compile-tested on: ipq40xx, ramips
    Runtime-tested on: ipq40xx
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .161
+LINUX_VERSION-4.14 = .162
 
-LINUX_KERNEL_HASH-4.14.161 = 9d23629f50e6c7313c4f1277455137b7727a6117df931c4c4fb7fab7e98f81c9
+LINUX_KERNEL_HASH-4.14.162 = f65170224cd4359ce8b2793b492bd8127abdd0b91350484e001bce13f0c98b4b
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
