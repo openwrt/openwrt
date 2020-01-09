6a2855212096d2c486961a0841b037bae4b75de7
Daniel Golle <daniel@makrotopia.org>
2020-01-09 15:32:12 +0200

    dnsmasq: add uci-defaults script for config migration
    When running sysupgrade from an existing configuration, UCI option
    dhcp.@dnsmasq[0].resolvfile needs to be modified in case it has not
    been changed from it's original value.
    Accomplish that using a uci-defaults script.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

---
+++ package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+[ "$(uci get dhcp.@dnsmasq[0].resolvfile)" = "/tmp/resolv.conf.auto" ] && {
+	uci set dhcp.@dnsmasq[0].resolvfile="/tmp/resolv.conf.d/resolv.conf.auto"
+	uci commit dhcp
+}
