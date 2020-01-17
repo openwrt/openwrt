d4009d7985500b0f4ec26dee9b91bdeb3ea16da5
Rosen Penev <rosenp@gmail.com>
2020-01-16 20:43:59 -0800

    base-files/system.sh: remove $ in $(())
    Not needed.
    
    https://github.com/koalaman/shellcheck/wiki/Sc2004
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- package/base-files/files/lib/functions/system.sh
+++ package/base-files/files/lib/functions/system.sh
@@ -140,7 +140,7 @@ macaddr_add() {
 	local oui=${mac%:*:*:*}
 	local nic=${mac#*:*:*:}
 
-	nic=$(printf "%06x" $((0x${nic//:/} + $val & 0xffffff)) | sed 's/^\(.\{2\}\)\(.\{2\}\)\(.\{2\}\)/\1:\2:\3/')
+	nic=$(printf "%06x" $((0x${nic//:/} + val & 0xffffff)) | sed 's/^\(.\{2\}\)\(.\{2\}\)\(.\{2\}\)/\1:\2:\3/')
 	echo $oui:$nic
 }
 
