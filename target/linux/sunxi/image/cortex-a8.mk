d0d8584b4187da38b484da3290fbb23ca472abad
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-01-23 14:21:19 +0100

    sunxi: tidy up and sort alphabetically in image Makefiles
    This tidies up the image Makefiles for the sunxi target by:
    - Move the if-condition for the subtarget to the parent Makefile
    - Remove lots of unnecessary empty lines
    - Sort device definitions alphabetically
    - Harmonize line wrapping for DEVICE_PACKAGES
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/sunxi/image/cortex-a8.mk
+++ target/linux/sunxi/image/cortex-a8.mk
@@ -5,7 +5,31 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
-ifeq ($(SUBTARGET),cortexa8)
+
+define Device/cubietech_a10-cubieboard
+  DEVICE_VENDOR := Cubietech
+  DEVICE_MODEL := Cubieboard
+  DEVICE_PACKAGES:=kmod-ata-core kmod-ata-sunxi kmod-sun4i-emac kmod-rtc-sunxi
+  SOC := sun4i
+endef
+TARGET_DEVICES += cubietech_a10-cubieboard
+
+define Device/linksprite_a10-pcduino
+  DEVICE_VENDOR := LinkSprite
+  DEVICE_MODEL := pcDuino
+  DEVICE_PACKAGES:=kmod-sun4i-emac kmod-rtc-sunxi kmod-rtl8192cu
+  SOC := sun4i
+endef
+TARGET_DEVICES += linksprite_a10-pcduino
+
+define Device/marsboard_a10-marsboard
+  DEVICE_VENDOR := HAOYU Electronics
+  DEVICE_MODEL := MarsBoard A10
+  DEVICE_PACKAGES:=mod-ata-core kmod-ata-sunxi kmod-sun4i-emac kmod-rtc-sunxi \
+	sound-soc-sunxi
+  SOC := sun4i
+endef
+TARGET_DEVICES += marsboard_a10-marsboard
 
 define Device/olimex_a10-olinuxino-lime
   DEVICE_VENDOR := Olimex
@@ -13,9 +37,7 @@ define Device/olimex_a10-olinuxino-lime
   DEVICE_PACKAGES:=kmod-ata-core kmod-ata-sunxi kmod-sun4i-emac kmod-rtc-sunxi
   SOC := sun4i
 endef
-
 TARGET_DEVICES += olimex_a10-olinuxino-lime
-
 
 define Device/olimex_a13-olimex-som
   DEVICE_VENDOR := Olimex
@@ -25,9 +47,7 @@ define Device/olimex_a13-olimex-som
   SOC := sun5i-a13
   SUNXI_DTS := sun5i-a13-olinuxino
 endef
-
 TARGET_DEVICES += olimex_a13-olimex-som
-
 
 define Device/olimex_a13-olinuxino
   DEVICE_VENDOR := Olimex
@@ -35,37 +55,4 @@ define Device/olimex_a13-olinuxino
   DEVICE_PACKAGES:=kmod-rtl8192cu
   SOC := sun5i
 endef
-
 TARGET_DEVICES += olimex_a13-olinuxino
-
-
-define Device/cubietech_a10-cubieboard
-  DEVICE_VENDOR := Cubietech
-  DEVICE_MODEL := Cubieboard
-  DEVICE_PACKAGES:=kmod-ata-core kmod-ata-sunxi kmod-sun4i-emac kmod-rtc-sunxi
-  SOC := sun4i
-endef
-
-TARGET_DEVICES += cubietech_a10-cubieboard
-
-
-define Device/linksprite_a10-pcduino
-  DEVICE_VENDOR := LinkSprite
-  DEVICE_MODEL := pcDuino
-  DEVICE_PACKAGES:=kmod-sun4i-emac kmod-rtc-sunxi kmod-rtl8192cu
-  SOC := sun4i
-endef
-
-TARGET_DEVICES += linksprite_a10-pcduino
-
-
-define Device/marsboard_a10-marsboard
-  DEVICE_VENDOR := HAOYU Electronics
-  DEVICE_MODEL := MarsBoard A10
-  DEVICE_PACKAGES:=mod-ata-core kmod-ata-sunxi kmod-sun4i-emac kmod-rtc-sunxi sound-soc-sunxi
-  SOC := sun4i
-endef
-
-TARGET_DEVICES += marsboard_a10-marsboard
-
-endif
