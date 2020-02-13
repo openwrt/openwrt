085f38351feb2dcae0a2c7cace0e60452928615d
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-02-13 14:46:52 +0100

    ath79: enable forceless sysupgrade from ar71xx on fritz300e
    This adds the ar71xx board name to the SUPPORTED_DEVICES on ath79,
    so forceless sysupgrade on this device becomes possible.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    (cherry picked from commit 49ff00db340ea017c266ba37546c71209cfe3e26)

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -103,6 +103,7 @@ define Device/avm_fritz300e
 	append-squashfs-fakeroot-be | pad-to 256 | \
 	append-rootfs | pad-rootfs | append-metadata | check-size $$$$(IMAGE_SIZE)
   DEVICE_PACKAGES := fritz-tffs rssileds -swconfig
+  SUPPORTED_DEVICES += fritz300e
 endef
 TARGET_DEVICES += avm_fritz300e
 
