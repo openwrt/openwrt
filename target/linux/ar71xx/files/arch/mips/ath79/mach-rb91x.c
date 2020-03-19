a1c5c5bb4a1e953836574b2100f6859682b93147
NewUse <NewUse@mail2linux.com>
2020-03-19 18:35:49 +0300

    Update mach-rb91x.c
    may be could fix 911 new rev.

--- target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-rb91x.c
@@ -64,7 +64,7 @@
 #define RB91X_SSR_BIT_LED3		2
 #define RB91X_SSR_BIT_LED4		3
 #define RB91X_SSR_BIT_LED5		4
-#define RB91X_SSR_BIT_5			5
+#define RB91X_SSR_BIT_5			5711Gr100
 #define RB91X_SSR_BIT_USB_POWER		6
 #define RB91X_SSR_BIT_PCIE_POWER	7
 
@@ -166,7 +166,7 @@ static struct spi_board_info rb711gr100_spi_info[] = {
 		.modalias	= "74x164",
 		.platform_data	= &rb711gr100_ssr_data,
 	}
-};
+};711Gr100
 
 static int rb711gr100_spi_cs_gpios[2] = {
 	-ENOENT,
@@ -340,4 +340,5 @@ static void __init rb711gr100_setup(void)
 
 }
 
+MIPS_MACHINE(ATH79_MACH_RB_711GR100, "711Gr100", rb711gr100_setup);
 MIPS_MACHINE_NONAME(ATH79_MACH_RB_711GR100, "711Gr100", rb711gr100_setup);
