a39c7399ffd4cc400f2b903219b1caa4eea138a3
Chen Minqiang <ptpt52@gmail.com>
2020-04-13 22:52:45 +0800

    ramips: add swconfig for mt7621
    We could use ralink-eth for mt7621 so swconfig is needed

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -8,7 +8,7 @@ FEATURES+=nand ramdisk rtc usb minor
 CPU_TYPE:=24kc
 
 KERNEL_PATCHVER:=5.4
-DEFAULT_PACKAGES += wpad-basic
+DEFAULT_PACKAGES += wpad-basic swconfig
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
