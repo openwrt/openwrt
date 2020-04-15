0232f57e1af6580542c0ed1ce1d76c7cd4084613
Koen Vandeputte <koen.vandeputte@ncentric.com>
2020-04-15 14:55:36 +0200

    kernel: bump 4.14 to 4.14.176
    Refreshed all patches.
    
    Remove upstreamed:
    - 0001-net-thunderx-workaround-BGX-TX-Underflow-issue.patch
    - 600-ipv6-addrconf-call-ipv6_mc_up-for-non-Ethernet-inter.patch
    - 003-ARM-dts-oxnas-Fix-clear-mask-property.patch
    
    Fixes:
    - CVE-2020-8647
    - CVE-2020-8648 (potentially)
    - CVE-2020-8649
    
    Compile-tested on: cns3xxx, octeontx
    Runtime-tested on: cns3xxx
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .172
+LINUX_VERSION-4.14 = .176
 
-LINUX_KERNEL_HASH-4.14.172 = 2318a1ab937580a079351ed20557c336a3d95b664f667b14e3ba49e3271b217a
+LINUX_KERNEL_HASH-4.14.176 = bcae0956baaeb55dab5bad0401873fbc5baaa7fbe957ea6d27a5ab241cec5ca2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
