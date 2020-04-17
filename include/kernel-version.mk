23916bca61435ee80236f6aef70a3f0acacd4a93
Petr Štetiar <ynezz@true.cz>
2020-04-17 14:29:33 +0200

    kernel: bump 5.4 to 5.4.33
    Refreshed patches, removed upstreamed patches:
    
     oxnas: 001-irqchip-versatile-fpga-Handle-chained-IRQs-properly.patch
     oxnas: 002-irqchip-versatile-fpga-Apply-clear-mask-earlier.patch
    
    Run tested: qemu-x86-64, apalis
    Build tested: x86/64, imx6, sunxi/a53
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .176
 LINUX_VERSION-4.19 = .115
-LINUX_VERSION-5.4 = .32
+LINUX_VERSION-5.4 = .33
 
 LINUX_KERNEL_HASH-4.14.176 = bcae0956baaeb55dab5bad0401873fbc5baaa7fbe957ea6d27a5ab241cec5ca2
 LINUX_KERNEL_HASH-4.19.115 = 11b2d97c8ea5ceb40c5e1d0bb87ad5b2b8c84560181bc60c0d28ec3a3e3801c2
-LINUX_KERNEL_HASH-5.4.32 = 192d99fab7a47a537493b6d7eddb52892b98f8ada655c0bbb419b360a995f2b3
+LINUX_KERNEL_HASH-5.4.33 = b5579621302a6169b389b64dd9ef084df3bb2f11d91dd73273f76e2323223861
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
