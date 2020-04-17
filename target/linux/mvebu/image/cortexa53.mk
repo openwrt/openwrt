050c24f05c85268431515fb118a0710941473f2c
Li Zhang <li.zhang@gl-inet.com>
2020-04-17 14:39:32 +0800

    mvebu: add support for GL.iNet GL-MV1000
    This patch adds supports for GL-MV1000.
    
    Specification:
    	- SOC: Marvell Armada 88F3720 (1GHz)
    	- Flash: 16MB (W25Q128FWSIG)
    	- RAM: 1GB DDR4
    	- Ethernet: 3x GE (1 WAN + 2 LAN)
    	- EMMC: 8GB EMMC (KLM8G1GETF-B041)
    	- MicroSD: 1x microSD slot
    	- USB: 1x USB 2.0 port(TypeA),1x USB 3.0 port(TypeC)
    	- Button: 1x reset button,1x slide switch
    	- LED: 3x greed LED
    	- UART: 1x UART on PCB (JP1: 3.3V, RX, TX, GND)
    
    	Update firmware instructions
    	============================
    	In the compiled,please gzip -d xxx.img.gz,then update firmware on uboot web.
    
    Signed-off-by: Li Zhang <li.zhang@gl-inet.com>
    [Copied dts file to files-5.4]
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mvebu/image/cortexa53.mk
+++ target/linux/mvebu/image/cortexa53.mk
@@ -69,3 +69,12 @@ define Device/methode_udpu
   BOOT_SCRIPT := udpu
 endef
 TARGET_DEVICES += methode_udpu
+
+define Device/glinet_gl-mv1000
+  $(call Device/Default-arm64)
+  DEVICE_TITLE := GL.iNet GL-MV1000
+  DEVICE_DTS := armada-gl-mv1000
+  SUPPORTED_DEVICES := glinet,gl-mv1000
+endef
+TARGET_DEVICES += glinet_gl-mv1000
+
