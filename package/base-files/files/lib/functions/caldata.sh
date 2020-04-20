02a9d3d6a932cdf707f8bcf4555d095839ce8d24
Thibaut VARÈNE <hacks@slashdirt.org>
2020-04-20 13:48:02 +0200

    package/base-files: add caldata_sysfsload_from_file()
    This routine enables loading caldata binary via the kernel sysfs loader
    
    See https://www.kernel.org/doc/html/v4.19/driver-api/firmware/fallback-mechanisms.html
    
    Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

--- package/base-files/files/lib/functions/caldata.sh
+++ package/base-files/files/lib/functions/caldata.sh
@@ -68,6 +68,21 @@ caldata_from_file() {
 		caldata_die "failed to extract calibration data from $source"
 }
 
+caldata_sysfsload_from_file() {
+	local source=$1
+	local offset=$(($2))
+	local count=$(($3))
+
+	# test extract to /dev/null first
+	dd if=$source of=/dev/null iflag=skip_bytes bs=$count skip=$offset count=1 2>/dev/null || \
+		caldata_die "failed to extract calibration data from $source"
+
+	# can't fail now
+	echo 1 > /sys/$DEVPATH/loading
+	dd if=$source of=/sys/$DEVPATH/data iflag=skip_bytes bs=$count skip=$offset count=1 2>/dev/null
+	echo 0 > /sys/$DEVPATH/loading
+}
+
 caldata_valid() {
 	local expected="$1"
 	local target=$2
