be6543c53994b873a61689261f54ef2353165453
Daniel Golle <daniel@makrotopia.org>
2020-04-20 19:21:50 +0100

    x86: really remove packages already enabled in kconfig
    This commit really removes packages in geode profiles already enabled
    in kernel config.
    
    Fixes: 9c23ecee57 ("x86: move packages selection to profiles")
    Reported-by: Tomasz Maciej Nowak <tomek_n@o2.pl>
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/x86/image/geode.mk
+++ target/linux/x86/image/geode.mk
@@ -1,7 +1,7 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/Geode
   DEVICE_PACKAGES += kmod-crypto-cbc kmod-crypto-hw-geode kmod-ledtrig-gpio \
-	kmod-ledtrig-heartbeat kmod-ledtrig-netdev kmod-via-rhine
+	kmod-ledtrig-heartbeat kmod-ledtrig-netdev
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic
@@ -9,8 +9,7 @@ TARGET_DEVICES += generic
 define Device/geos
   $(call Device/generic)
   DEVICE_TITLE := Traverse Technologies Geos
-  DEVICE_PACKAGES += br2684ctl flashrom kmod-8139cp kmod-hwmon-lm90 kmod-mppe \
-	kmod-pppoa kmod-usb-ohci-pci linux-atm ppp-mod-pppoa pppdump pppstats \
-	soloscli tc
+  DEVICE_PACKAGES += br2684ctl flashrom kmod-hwmon-lm90 kmod-mppe kmod-pppoa \
+	kmod-usb-ohci-pci linux-atm ppp-mod-pppoa pppdump pppstats soloscli tc
 endef
 TARGET_DEVICES += geos
