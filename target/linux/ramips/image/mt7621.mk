57dac3c2e012c1a9377f4d10d65bc108a60b68d9
Chen Minqiang <ptpt52@gmail.com>
2020-04-25 09:27:38 +0800

    ramips: init support xwrt_x-sdwan-1200

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1781,6 +1781,17 @@ define Device/xwrt_t-cpe1201k-v01
 endef
 TARGET_DEVICES += xwrt_t-cpe1201k-v01
 
+define Device/xwrt_x-sdwan-1200
+  $(Device/uimage-lzma-loader)
+  SOC := mt7621
+  IMAGE_SIZE := 16000k
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := X-SDWAN-1200
+  DEVICE_PACKAGES := \
+	kmod-mt7603 kmod-mt76x2
+endef
+TARGET_DEVICES += xwrt_x-sdwan-1200
+
 define Device/youhua_wr1200js
   $(Device/dsa-migration)
   IMAGE_SIZE := 16064k
