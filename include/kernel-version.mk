5ea3ea154f9cc8a6f1d3425fa4763614e0e51f8d
Petr Štetiar <ynezz@true.cz>
2020-04-29 23:24:49 +0200

    kernel: bump 5.4 to 5.4.36
    Refreshed patches, removed upstreamed patch:
    
     generic/hack: 551-loop-Better-discard-support-for-block-devices.patch
    
    Added generic config symbol `ARM64_ERRATUM_1542419` due to Fixes:
    f2791551cedb ("arm64: errata: Hide CTR_EL0.DIC on systems affected by
    Neoverse-N1 #1542419").
    
    Run tested: qemu-x86-64, apalis, nbg6617
    Build tested: x86/64, imx6, ipq40xx, sunxi/a53
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .176
 LINUX_VERSION-4.19 = .115
-LINUX_VERSION-5.4 = .35
+LINUX_VERSION-5.4 = .36
 
 LINUX_KERNEL_HASH-4.14.176 = bcae0956baaeb55dab5bad0401873fbc5baaa7fbe957ea6d27a5ab241cec5ca2
 LINUX_KERNEL_HASH-4.19.115 = 11b2d97c8ea5ceb40c5e1d0bb87ad5b2b8c84560181bc60c0d28ec3a3e3801c2
-LINUX_KERNEL_HASH-5.4.35 = e16bd5f7284a80a41328bf712e1136b0adf5b71cc0bd263efa7cac75539806d4
+LINUX_KERNEL_HASH-5.4.36 = b9faea98122e8316af8fb428c942e81797b5d28a8fc59a24a4e47959e3765b8d
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
