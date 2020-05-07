17531f1a45808a6c64223a8d6a313b4c2f86aeb0
Natalie Kagelmacher <nataliek@pm.me>
2020-05-07 23:36:41 +0200

    ath79: add support for AVM FRITZ!WLAN Repeater DVB-C
    This commit adds support for the AVM FRITZ!WLAN Repeater DVB-C
    
    SOC: Qualcomm Atheros QCA9556 720/600/200MHz MIPS 74Kc
    RAM: 64 MiB (0x4000000)
    FLASH: 16 MB NOR (WINBOND Uniform-Flash, 256 Bytes WriteBuffer)
    WLAN: 2.4 GHZ b/g/n and 5 GHz n/ac
    Ethernet: Gigabit
    DVB-C: em28174 with MxL251 tuner
    INPUT: 1x WPS Button (used for reset in OEM firmware by 15 seconds hold)
    LED: Power, WLAN, TV, RSSI0-4
    Serial: Yes
    
    Tested and working:
     - Ethernet (correct MAC, gigabit, iperf3 about 200 Mbit/s)
     - 2.4 GHz Wi-Fi (correct MAC)
     - 5 GHz Wi-Fi (correct MAC)
     - WPS Button (tested using wifitoggle)
     - Installation via EVA bootloader (FTP recovery)
     - OpenWrt sysupgrade (both CLI and LuCI)
     - Download of "urlader" (mtd0)
    
    Not working:
     - Internal USB
     - DVB-C em28174+MxL251 (depends on internal USB)
     - LEDs (SPI part implemented, no individual control yet, all on)
    
    Installation via EVA bootloader (FTP recovery):
    Set NIC to 192.168.178.3/24 gateway 192.168.178.1 and power on the device,
    connect to 192.168.178.1 through FTP and sign in with adam2/adam2:
    
      ftp> quote USER adam2
      ftp> quote PASS adam2
      ftp> binary
      ftp> debug
      ftp> passive
      ftp> quote MEDIA FLSH
      ftp> put openwrt-sysupgrade.bin mtd1
    
    Wait for "Transfer complete" together with the transfer details.
    Wait two minutes to make sure flash is complete (just to be safe).
    Then restart the device (power off and on) to boot into OpenWrt.
    Revert your NIC settings to reach OpenWrt at 192.168.1.1
    
    Signed-off-by: Natalie Kagelmacher <nataliek@pm.me>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -182,6 +182,16 @@ define Device/avm_fritz1750e
 endef
 TARGET_DEVICES += avm_fritz1750e
 
+define Device/avm_fritzdvbc
+  $(Device/avm)
+  SOC := qca9556
+  IMAGE_SIZE := 15232k
+  DEVICE_MODEL := FRITZ!WLAN Repeater DVB-C
+  DEVICE_PACKAGES += rssileds kmod-ath9k kmod-ath10k-ct-smallbuffers \
+	ath10k-firmware-qca988x-ct -swconfig
+endef
+TARGET_DEVICES += avm_fritzdvbc
+
 define Device/avm_fritz300e
   $(Device/avm)
   SOC := ar7242
