05afbcd14e1ea9fd25454c1c2fcc0ead4e46af53
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-05-10 16:47:48 +0200

    mac80211: replace backticks by $(...)
    This replaces deprecated backticks by more versatile $(...) syntax.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+++ package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -1040,7 +1040,7 @@ list_phy_interfaces() {
 drv_mac80211_teardown() {
 	wireless_process_kill_all
 
-	for phy in `ls /sys/class/ieee80211/`; do
+	for phy in $(ls /sys/class/ieee80211/); do
 		mac80211_interface_cleanup "$phy"
 		uci -q -P /var/state revert wireless._${phy}
 	done
