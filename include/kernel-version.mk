2efcfb1d1ab7b331b078aae3d6eed8b125cea866
Koen Vandeputte <koen.vandeputte@ncentric.com>
2020-05-11 12:08:05 +0200

    kernel: bump 4.14 to 4.14.180
    Refreshed all patches.
    
    Compile-tested on: ar71xx, cns3xxx, imx6
    Runtime-tested on: ar71xx, cns3xxx, imx6
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .179
+LINUX_VERSION-4.14 = .180
 
-LINUX_KERNEL_HASH-4.14.179 = 5da7eb5c94904ee31db7e25eb7be53e27a34c9a7fde2ee4bb0b3af7b87cc5981
+LINUX_KERNEL_HASH-4.14.180 = 444ef973d9b6a6ea174e4a9086f0aea980d8575d13302e431ad688f22e27ed0e
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
