8e81957180a55934c45aa46cbe949a5e9db82104
Thibaut VARÈNE <hacks@slashdirt.org>
2020-05-21 17:20:55 +0200

    packages/boot: remove rbcfg
    The new sysfs soft_config driver makes buggy rbcfg obsolete and
    entirely replaces it.
    
    Signed-off-by: Thibaut VARÈNE <hacks@slashdirt.org>

--- target/linux/ar71xx/image/mikrotik.mk
+++ target/linux/ar71xx/image/mikrotik.mk
@@ -43,7 +43,7 @@ TARGET_DEVICES += nand-large-ac
 define Device/rb-nor-flash-16M
   $(Device/mikrotik)
   DEVICE_TITLE := MikroTik RouterBoard (16 MB SPI NOR)
-  DEVICE_PACKAGES := rbcfg rssileds -nand-utils kmod-ledtrig-gpio
+  DEVICE_PACKAGES := rssileds -nand-utils kmod-ledtrig-gpio
   IMAGE_SIZE := 16000k
   KERNEL_INSTALL := 1
   SUPPORTED_DEVICES := rb-750-r2 rb-750up-r2 rb-750p-pbr2 rb-911-2hn rb-911-5hn rb-931-2nd rb-941-2nd rb-951ui-2nd rb-952ui-5ac2nd rb-962uigs-5hact2hnt rb-lhg-5nd rb-map-2nd rb-mapl-2nd rb-wap-2nd rb-wapr-2nd rb-sxt-2nd-r3
