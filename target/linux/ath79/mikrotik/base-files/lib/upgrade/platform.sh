6e343abfbcb23542e3d1f875f93fda2201b277fd
Christopher Hill <ch6574@gmail.com>
2020-05-25 21:10:00 -0400

    ath79: add support for MikroTik RouterBOARD 493G (rb4xx series)
    This patch adds support for the MikroTik RouterBOARD RB493G, ported
    from the ar71xx target.
    
    See https://routerboard.com/RB493G for details
    
    Specification:
    - SoC Qualcomm Atheros AR7161
    - RAM: 256 MiB
    - Storage: 128MiB NAND
    - Ethernet: 9x 1000/100/10 Mbps
    - USB 1x 2.0 / 1.0 type A
    - PCIe: 3x Mini slot
    - MicroSD slot
    
    Working:
    - Board/system detection
    - Ethernet
    - SPI
    - NAND
    - LEDs
    - USB
    - Sysupgrade
    
    Enabled (but untested due to lack of hardware):
    - PCIe - ath79_pci_irq struct has the slot/pin/IRQ mappings if needed
    
    Installation methods:
    - tftp boot initramfs image, scp then flash via "sysupgrade -n"
    - nand boot existing OpenWrt, scp then flash via "sysupgrade -n"
    
    Notes:
    - initramfs image will not work if uncompressed image size over ~8.5Mb
    - The "rb4xx" drivers have been enabled
    
    Signed-off-by: Christopher Hill <ch6574@gmail.com>

--- target/linux/ath79/mikrotik/base-files/lib/upgrade/platform.sh
+++ target/linux/ath79/mikrotik/base-files/lib/upgrade/platform.sh
@@ -31,6 +31,7 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	mikrotik,routerboard-493g|\
 	mikrotik,routerboard-922uags-5hpacd)
 		platform_do_upgrade_mikrotik_nand "$1"
 		;;
