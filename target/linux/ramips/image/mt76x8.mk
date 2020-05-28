686d74a50863839abfc64cb0177956a2362cc949
frol1805 <60575664+frol1805@users.noreply.github.com>
2020-05-28 16:07:03 +0500

    zyxel_keenetic-kn-1210
    zyxel_keenetic-kn-1210

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -618,3 +618,15 @@ define Device/zyxel_keenetic-extra-ii
 	check-size | zyimage -d 6162 -v "ZyXEL Keenetic Extra II"
 endef
 TARGET_DEVICES += zyxel_keenetic-extra-ii
+
+define Device/zyxel_keenetic-kn-1210
+  DTS := kn-1210
+  IMAGE_SIZE := 16064k
+  BLOCKSIZE := 64k
+  DEVICE_TITLE := Keenetic 4G (KN-1210)
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  IMAGES += factory.bin
+  IMAGE/factory.bin := $$(sysupgrade_bin) | pad-to $$$$(BLOCKSIZE) | \
+	check-size $$$$(IMAGE_SIZE) | zyimage -d 4624 -v "KN-1210"
+endef
+TARGET_DEVICES += zyxel_keenetic-kn-1210
