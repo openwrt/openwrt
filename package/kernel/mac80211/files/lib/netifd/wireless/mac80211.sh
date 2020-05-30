02f08056bcbc5f014894b7edcea43f2ff822fd90
John Crispin <john@phrozen.org>
2020-05-30 12:01:08 +0200

    mac80211: fix wifi teardown
    reverts part of the recent wifi reconf patch.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+++ package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -1041,10 +1041,12 @@ list_phy_interfaces() {
 drv_mac80211_teardown() {
 	wireless_process_kill_all
 
-	for phy in $(ls /sys/class/ieee80211/); do
-		mac80211_interface_cleanup "$phy"
-		uci -q -P /var/state revert wireless._${phy}
-	done
+	json_select data
+	json_get_vars phy
+	json_select ..
+
+	mac80211_interface_cleanup "$phy"
+	uci -q -P /var/state revert wireless._${phy}
 }
 
 add_driver mac80211
