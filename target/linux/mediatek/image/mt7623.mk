7adc29f59ede5112b94b3df4688b47a88f84f544
David Woodhouse <dwmw2@infradead.org>
2020-06-12 11:06:38 +0100

    mediatek: add SD card image creation for Banana Pi R2
    Based on work by Alexey Loukianov <lx2@lexa2.ru> and others.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -1,5 +1,30 @@
 KERNEL_LOADADDR := 0x80008000
 
+ifneq ($(CONFIG_BANANA_PI_BOOT_PARTSIZE),)
+BOOTFS_BLOCK_SIZE := 1024
+BOOTFS_BLOCKS := $(shell echo $$(($(CONFIG_BANANA_PI_BOOT_PARTSIZE)*1024*1024/$(BOOTFS_BLOCK_SIZE))))
+endif
+
+define Build/banana-pi-sdcard
+	rm -f $@.boot
+	mkfs.fat -C $@.boot $(BOOTFS_BLOCKS)
+
+	./gen_banana_pi_img.sh emmc $@.emmc \
+		$(STAGING_DIR_IMAGE)/$(UBOOT_TARGET)-preloader.bin
+
+	mkenvimage -s 0x2000 -o $(STAGING_DIR_IMAGE)/$(UBOOT_TARGET)-uboot.env $(UBOOT_TARGET)-uEnv.txt
+	mcopy -i $@.boot $(STAGING_DIR_IMAGE)/$(UBOOT_TARGET)-uboot.env ::uboot.env
+	mcopy -i $@.boot $(IMAGE_KERNEL) ::uImage
+	mcopy -i $@.boot $@.emmc ::eMMCboot.bin
+	./gen_banana_pi_img.sh sd $@ \
+		$(STAGING_DIR_IMAGE)/$(UBOOT_TARGET)-preloader.bin \
+		$(STAGING_DIR_IMAGE)/$(UBOOT_TARGET)-u-boot.bin \
+		$@.boot \
+		$(IMAGE_ROOTFS) \
+		$(CONFIG_BANANA_PI_BOOT_PARTSIZE) \
+		$(CONFIG_TARGET_ROOTFS_PARTSIZE)
+endef
+
 define Device/bpi_bananapi-r2
   DEVICE_VENDOR := Bpi
   DEVICE_MODEL := Banana Pi R2
@@ -7,6 +32,11 @@ define Device/bpi_bananapi-r2
   SUPPORTED_DEVICES := bananapi,bpi-r2
   DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
 	mkf2fs e2fsprogs kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-ata-ahci-mtk
+  UBOOT_TARGET := mt7623n_bpir2
+  IMAGES := img.gz
+  IMAGE/img.gz := banana-pi-sdcard | gzip | append-metadata
+  KERNEL := kernel-bin | fit none $$(DTS_DIR)/$$(DEVICE_DTS).dtb
+  KERNEL_INITRAMFS :=
 endef
 TARGET_DEVICES += bpi_bananapi-r2
 
