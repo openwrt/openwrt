c848bc6cd576baf1d538d97662e410289214f0b7
David Woodhouse <dwmw2@infradead.org>
2020-06-15 09:47:09 +0100

    mediatek/mt7623: unify features and packages, add ext4 and usb
    The supported MT7623 boards are mostly identical (what with being a
    System-on-Chip and all), so unify the DEVICE_PACKAGES for them, and add
    ext4 and usb support for them.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -5,6 +5,8 @@ define Device/bpi_bananapi-r2
   DEVICE_MODEL := Banana Pi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
   SUPPORTED_DEVICES := bananapi,bpi-r2
+  DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
+	mkf2fs e2fsprogs kmod-usb-ohci kmod-usb2 kmod-usb3
 endef
 TARGET_DEVICES += bpi_bananapi-r2
 
@@ -13,9 +15,9 @@ define Device/unielec_u7623-02-emmc-512m
   DEVICE_MODEL := U7623-02
   DEVICE_VARIANT := eMMC/512MB RAM
   DEVICE_DTS := mt7623a-unielec-u7623-02-emmc-512m
-  DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
-	mkf2fs e2fsprogs
   IMAGES := sysupgrade-emmc.bin.gz
   IMAGE/sysupgrade-emmc.bin.gz := sysupgrade-emmc | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
+	mkf2fs e2fsprogs kmod-usb-ohci kmod-usb2 kmod-usb3
 endef
 TARGET_DEVICES += unielec_u7623-02-emmc-512m
