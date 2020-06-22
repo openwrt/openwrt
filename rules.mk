557d4695ce7647539fc5b3723f75c3e7a5ec25a0
Felix Fietkau <nbd@nbd.name>
2020-06-22 21:44:37 +0200

    build: make prefix mapping of debug information optional
    Remapping the local build path in debug information makes debugging
    using ./scripts/remote-gdb harder, because files no longer refer to the full
    path on the build host.
    
    For local builds, debug information does not need to be reproducible,
    since it will be stripped out of packages anyway.
    
    For buildbot builds, it makes sense to keep debug information reproducible,
    since the full path is not needed (nor desired) anywhere.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- rules.mk
+++ rules.mk
@@ -144,7 +144,7 @@ ifeq ($(or $(CONFIG_EXTERNAL_TOOLCHAIN),$(CONFIG_TARGET_uml)),)
   ifeq ($(CONFIG_GCC_USE_IREMAP),y)
     iremap = -iremap$(1):$(2)
   else
-    iremap = -ffile-prefix-map=$(1)=$(2)
+    iremap = -f$(if $(CONFIG_REPRODUCIBLE_DEBUG_INFO),file,macro)-prefix-map=$(1)=$(2)
   endif
 endif
 
