99dae00fa86203effc3e7e41cbc8d89b9518848c
Rui Salvaterra <rsalvaterra@gmail.com>
2020-06-25 15:20:23 +0100

    kernel: kmod-zram: break the strict dependency from lz4
    Zram is only strictly dependent on lzo, not lz4. Break this dependency and
    make the lz4 module visible in the configuration, in order for the user to
    have the choice of enabling/disabling it, if (s)he sees fit.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -930,15 +930,14 @@ $(eval $(call KernelPackage,ikconfig))
 define KernelPackage/zram
   SUBMENU:=$(OTHER_MENU)
   TITLE:=ZRAM
-  DEPENDS:=+kmod-lib-lzo +kmod-lib-lz4
+  DEPENDS:=+kmod-lib-lzo
   KCONFIG:= \
 	CONFIG_ZSMALLOC \
 	CONFIG_ZRAM \
 	CONFIG_ZRAM_DEBUG=n \
 	CONFIG_PGTABLE_MAPPING=n \
 	CONFIG_ZRAM_WRITEBACK=n \
-	CONFIG_ZSMALLOC_STAT=n \
-	CONFIG_ZRAM_LZ4_COMPRESS=y
+	CONFIG_ZSMALLOC_STAT=n
   FILES:= \
 	$(LINUX_DIR)/mm/zsmalloc.ko \
 	$(LINUX_DIR)/drivers/block/zram/zram.ko
