c43bab0fbfcf0bd8a8bef9dc200f5712fe7b921a
Rowan Border <rowanjborder@gmail.com>
2020-06-30 14:35:56 +0100

    ramips: add support for TP-Link RE220 v2
    TP-Link RE220 v2 is a wireless range extender with Ethernet and 2.4G and 5G
    WiFi with internal antennas. It's based on MediaTek MT7628AN+MT7610EN.
    
    This port of OpenWRT leverages work done by Andreas Böhler <dev@aboehler.at>
    for the TP-Link RE200 v2 as both devices share the same SoC, flash layout
    and GPIO pinout.
    
    Specifications
    
    MediaTek MT7628AN (580 Mhz)
    64 MB of RAM
    8 MB of FLASH
    2T2R 2.4 GHz and 1T1R 5 GHz
    1x 10/100 Mbps Ethernet
    UART header on PCB (57600 8n1)
    8x LED (GPIO-controlled), 2x button
    There are 2.4G and 5G LEDs in red and green which are controlled separately.
    
    Web Interface Installation
    
    It is possible to upgrade to OpenWrt via the web interface. Simply flash
    the -factory.bin from OEM. In contrast to a stock firmware, this will not
    overwrite U-Boot.
    
    Signed-off-by: Rowan Border <rowanjborder@gmail.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -367,6 +367,16 @@ define Device/tplink_re200-v2
 endef
 TARGET_DEVICES += tplink_re200-v2
 
+define Device/tplink_re220-v2
+  $(Device/tplink-safeloader)
+  IMAGE_SIZE := 7808k
+  DEVICE_MODEL := RE220
+  DEVICE_VARIANT := v2
+  DEVICE_PACKAGES := kmod-mt76x0e
+  TPLINK_BOARD_ID := RE220-V2
+endef
+TARGET_DEVICES += tplink_re220-v2
+
 define Device/tplink_re305-v1
   $(Device/tplink-safeloader)
   IMAGE_SIZE := 6016k
