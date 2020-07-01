71346dc62dc34941b2165dd5c6dd3e59b4ec7eba
John Audia <graysky AT archlinux DOT us>
2020-07-01 17:05:22 -0400

    kernel: bump 5.4 to 5.4.50 update via update_kernel.sh -v -u 5.4
    Resolved merge conflict in the following patches:
     701-net-0234-enetc-WA-for-MDIO-register-access-issue.patch
     834-ledtrig-libata.patch
    
    Removed upstreamed patches:
     396-5.8-sch_cake-don-t-try-to-reallocate-or-unshare-skb-unco.patch
     397-5.8-sch_cake-don-t-call-diffserv-parsing-code-when-it-is.patch
     398-5.8-sch_cake-fix-a-few-style-nits.patch
     812-v5.8-i2c-pxa-fix-i2c_pxa_scream_blue_murder-debug-output.patch
     819-v5.8-i2c-pxa-clear-all-master-action-bits-in-i2c_pxa_stop.patch
    
    All others refreshed
    
    build tested: ipq806x (netgear R7800)
    
    Signed-off-by: John Audia graysky@archlinux.us

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .180
 LINUX_VERSION-4.19 = .123
-LINUX_VERSION-5.4 = .48
+LINUX_VERSION-5.4 = .50
 
 LINUX_KERNEL_HASH-4.14.180 = 444ef973d9b6a6ea174e4a9086f0aea980d8575d13302e431ad688f22e27ed0e
 LINUX_KERNEL_HASH-4.19.123 = a79914d31a8d8c6b0e2bb0f2b143d615fe8a6c4dd2e0f36e97aa20efd69a993f
-LINUX_KERNEL_HASH-5.4.48 = bf20ddafcd04c114d34654bb10d1eb74f1864f3d14b676c6f0d42d60bbcf1d53
+LINUX_KERNEL_HASH-5.4.50 = ad10f4c1e900f4e3eb4903b65dbcb4ca74250de63aa9fa7105b9b3c3f9a8a6e2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
