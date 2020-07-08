d0fbc7c637b12906be7a8cd2786985d5f324bfaa
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2020-07-08 22:01:30 +0800

    openwrt

--- scripts/update_package.sh
+++ scripts/update_package.sh
@@ -5,6 +5,7 @@
 #!/bin/bash
 clear
 rm -rf ./package/new
+rm -rf ./package/lean/trojan
 git clone https://github.com/NateLol/luci-app-beardropper package/new/luci-app-beardropper
 sed -i 's/"luci.fs"/"luci.sys".net/g' package/new/luci-app-beardropper/luasrc/model/cbi/beardropper/setting.lua
 sed -i '/firewall/d' package/new/luci-app-beardropper/root/etc/uci-defaults/luci-beardropper
@@ -12,7 +13,6 @@ mv package/new/luci-app-beardropper/po/zh_Hans   package/new/luci-app-beardroppe
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/luci-theme-opentomcat
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/new/luci-app-ssr-plus
-##simple-obfs
 #git clone -b master --single-branch https://github.com/aa65535/openwrt-simple-obfs package/new/simple-obfs
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/new/smartdns 
@@ -27,6 +27,8 @@ git clone -b master --single-branch https://github.com/frainzy1477/luci-app-clas
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-clash package/new/luci-app-clash
 sed -i 's/), 5)/), 48)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/new/luci-app-passwall
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/new/trojan-go
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan package/new/trojan
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ package/new/luci-app-jd-dailybonus
 chmod -R 755 ./
