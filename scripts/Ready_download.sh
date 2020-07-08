501c22019540dae475710d9bb4cde7ab4175d78b
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2020-07-08 20:17:26 +0800

    set

--- scripts/Ready_download.sh
+++ scripts/Ready_download.sh
@@ -10,10 +10,8 @@ git clone https://github.com/siropboy/mypackages package/mypackages
 git clone https://github.com/siropboy/mysmall package/mysmall
 ./scripts/feeds update -a
 mkdir  ../bak
-mv .feeds/luci/applications/luci-app-mwan3/luasrc/model/cbi/mwan/policyconfig.lua  ../bak
 mv ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm  ../bak
 mv ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua   ../bak
-mv .feeds/luci/applications/luci-app-mwan3/luasrc/model/cbi/mwan/policyconfig.lua  ../bak
 mv ./package/lean/default-settings/files/zzz-default-settings   ../bak
 mv ./package/lean/autocore/files/index.htm  ../bak
 mv ./package/lean/autocore/files/cpuinfo  ../bak
@@ -27,8 +25,9 @@ sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
 sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+sed -i 's/天翼家庭云//g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
 #sed -i '/filter_aaaa/d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d-%H%M%S)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 wget -P package/lean/autocore/files  https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/index.htm
 wget -P package/lean/autocore/files https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/sbin/cpuinfo
 ./scripts/feeds install -a
