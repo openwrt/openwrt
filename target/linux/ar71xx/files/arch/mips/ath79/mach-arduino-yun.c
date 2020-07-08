openwrt/openwrt

commit 05d73a2a73794f8a978300eac75e25deae6d0626

ar71xx: Arduino Yun board 'WLAN RST' button support
This patch adds support for Arduino Yun board "WLAN RST" button.

Signed-off-by: Hirokazu MORIKAWA <morikw2@gmail.com>

SVN-Revision: 48983

target/linux/ar71xx/files/arch/mips/ath79/mach-arduino-yun.c
@@ -60,6 +60,17 @@ static struct gpio_led ds_leds_gpio[] __initdata = {
 	},
 };
 
+static struct gpio_keys_button ds_gpio_keys[] __initdata = {
+	{
+		.desc		= "configuration button",
+		.type		= EV_KEY,
+		.code		= KEY_WPS_BUTTON,
+		.debounce_interval = DS_KEYS_DEBOUNCE_INTERVAL,
+		.gpio		= DS_GPIO_CONF_BTN,
+		.active_low	= 1,
+	},
+};
+
 static void __init ds_common_setup(void)
 {
 	static u8 mac[6];
@@ -97,7 +108,17 @@ static void __init ds_setup(void)
 
 	ath79_register_leds_gpio(-1, ARRAY_SIZE(ds_leds_gpio),
 				 ds_leds_gpio);
+	ath79_register_gpio_keys_polled(-1, DS_KEYS_POLL_INTERVAL,
+					ARRAY_SIZE(ds_gpio_keys),
+					ds_gpio_keys);
 	ath79_register_usb();
+
+	/* use the swtich_led directly form sysfs */
+	ath79_gpio_function_disable(AR933X_GPIO_FUNC_ETH_SWITCH_LED0_EN |
+								AR933X_GPIO_FUNC_ETH_SWITCH_LED1_EN |
+								AR933X_GPIO_FUNC_ETH_SWITCH_LED2_EN |
+								AR933X_GPIO_FUNC_ETH_SWITCH_LED3_EN |
+								AR933X_GPIO_FUNC_ETH_SWITCH_LED4_EN);
 
 	//Disable the Function for some pins to have GPIO functionality active
 	// GPIO6-7-8 and GPIO11

commit 2da06a981fb3e6076583ce971bdeb2d3ed66e9f9

ar71xx: enable ethernet LED of Arduino Yun
Commit 05d73a2a7379 enabled GPIO on ethernet LED, but proper LED setup was
not added then. This commit fixes it by reverting the change on the LED.

Fixes: 05d73a2a7379 ("ar71xx: Arduino Yun board 'WLAN RST' button support")
Signed-off-by: Sungbo Eo <mans0n@gorani.run>

target/linux/ar71xx/files/arch/mips/ath79/mach-arduino-yun.c
@@ -117,8 +117,7 @@ static void __init ds_setup(void)
 	ath79_gpio_function_disable(AR933X_GPIO_FUNC_ETH_SWITCH_LED0_EN |
 								AR933X_GPIO_FUNC_ETH_SWITCH_LED1_EN |
 								AR933X_GPIO_FUNC_ETH_SWITCH_LED2_EN |
-								AR933X_GPIO_FUNC_ETH_SWITCH_LED3_EN |
-								AR933X_GPIO_FUNC_ETH_SWITCH_LED4_EN);
+								AR933X_GPIO_FUNC_ETH_SWITCH_LED3_EN);
 
 	//Disable the Function for some pins to have GPIO functionality active
 	// GPIO6-7-8 and GPIO11