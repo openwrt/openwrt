92d7cedcad73ff9845ea3f635ea82bd925078c1f
Petr Štetiar <ynezz@true.cz>
2020-07-11 13:24:17 +0200

    scripts/gen_image_generic.sh: fix more shellcheck warnings
    Fixes following shellcheck warnings:
    
     In scripts/gen_image_generic.sh line 20:
     cyl=$(( (KERNELSIZE + ROOTFSSIZE) * 1024 * 1024 / (head * sect * 512)))
     ^-^ SC2034: cyl appears unused. Verify use (or export if used externally).
     --
     In scripts/gen_image_generic.sh line 34:
         [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$(($ROOTFSOFFSET + $ROOTFSSIZE))" conv=notrunc count="$sect"
                                                                           ^-----------^ SC2004: $/${} is unnecessary on arithmetic variables.
     --
     In scripts/gen_image_generic.sh line 35:
         mkfs.fat -n kernel -C "$OUTPUT.kernel" -S 512 "$(($KERNELSIZE / 1024))"
                                                           ^---------^ SC2004: $/${} is unnecessary on arithmetic variables.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- scripts/gen_image_generic.sh
+++ scripts/gen_image_generic.sh
@@ -17,10 +17,9 @@ rm -f "$OUTPUT"
 
 head=16
 sect=63
-cyl=$(( (KERNELSIZE + ROOTFSSIZE) * 1024 * 1024 / (head * sect * 512)))
 
 # create partition table
-set $(ptgen -o "$OUTPUT" -h $head -s $sect ${GUID:+-g} -p ${KERNELSIZE}m -p ${ROOTFSSIZE}m ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${GUID:+-G $GUID})
+set $(ptgen -o "$OUTPUT" -h $head -s $sect ${GUID:+-g} -p "${KERNELSIZE}m" -p "${ROOTFSSIZE}m" ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${GUID:+-G $GUID})
 
 KERNELOFFSET="$(($1 / 512))"
 KERNELSIZE="$2"
@@ -31,8 +30,8 @@ ROOTFSSIZE="$(($4 / 512))"
 dd if="$ROOTFSIMAGE" of="$OUTPUT" bs=512 seek="$ROOTFSOFFSET" conv=notrunc
 
 if [ -n "$GUID" ]; then
-    [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$(($ROOTFSOFFSET + $ROOTFSSIZE))" conv=notrunc count="$sect"
-    mkfs.fat -n kernel -C "$OUTPUT.kernel" -S 512 "$(($KERNELSIZE / 1024))"
+    [ -n "$PADDING" ] && dd if=/dev/zero of="$OUTPUT" bs=512 seek="$((ROOTFSOFFSET + ROOTFSSIZE))" conv=notrunc count="$sect"
+    mkfs.fat -n kernel -C "$OUTPUT.kernel" -S 512 "$((KERNELSIZE / 1024))"
     mcopy -s -i "$OUTPUT.kernel" "$KERNELDIR"/* ::/
 else
     make_ext4fs -J -L kernel -l "$KERNELSIZE" "$OUTPUT.kernel" "$KERNELDIR"
