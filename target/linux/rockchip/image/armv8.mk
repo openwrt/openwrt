382feaa34b87816461adbb654ba3150202066529
David Bauer <mail@david-bauer.net>
2020-07-16 10:08:57 +0200

    rockchip: add NanoPi R2S support
    Hardware
    --------
    RockChip RK3328 ARM64 (4 cores)
    1GB DDR4 RAM
    2x 1000 Base-T
    3 LEDs (LAN / WAN / SYS)
    1 Button (Reset)
    Micro-SD slot
    USB 2.0 Port
    
    Installation
    ------------
    Uncompress the OpenWrt sysupgrade and write it to a micro SD card using
    dd.
    
    MAC-address
    -----------
    The vendor code supports reading a MAC address from an EEPROM connected
    via i2c0 of the SoC. The EEPROM (address 0x51) should contain the MAC
    address in binary at offset 0xfa. However, my two units didn't come with
    such an EEPROM soldered on. The EEPROM should be placed between the SoC
    and the GPIO pins on the board. (U10)
    
    Generating rendom MAC addresses works around this issue. Otherwise, all
    boards running the same image have identical MAC addresses.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -4,6 +4,17 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+
+define Device/friendlyarm_nanopi-r2s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R2S
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-usb-net kmod-usb-net-rtl8152
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r2s
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
