76ca5bf2181de9a5e5fa4494d3a8bc280bcf8103
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-07-20 15:34:04 +0200

    layerscape: harmonize device strings
    OpenWrt lately has harmonized device (definition) names to the
    pattern vendor_model to improve overall consistency, also with
    other values like the DTS compatible.
    
    This patch applies that scheme to the layerscape target.
    
    Since this (intentionally) creates a bigger overlap between DTS names,
    compatible, and device definition name, it also moves DEVICE_DTS and
    SUPPORTED_DEVICES definitions to the Device/Default blocks.
    
    Apart from that, it also modifies several packages to use consistent
    naming in order to keep the $(1) file references working.
    
    While at it, remove one layer of complexity for the setup in
    tfa-layerscape package.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/layerscape/image/armv7.mk
+++ target/linux/layerscape/image/armv7.mk
@@ -13,18 +13,19 @@ define Device/Default
   KERNEL_NAME := zImage
   KERNEL_LOADADDR := 0x80008000
   KERNEL_ENTRY_POINT := 0x80008000
+  DEVICE_DTS = $(lastword $(subst _, ,$(1)))
+  SUPPORTED_DEVICES = $(subst _,$(comma),$(1))
   IMAGE_SIZE := 64m
   IMAGE/sysupgrade.bin := append-kernel | pad-to 16M | \
     append-rootfs | pad-rootfs | \
     check-size $(LS_SYSUPGRADE_IMAGE_SIZE) | append-metadata
 endef
 
-define Device/ls1021atwr
+define Device/fsl_ls1021a-twr
   DEVICE_VENDOR := NXP
   DEVICE_MODEL := TWR-LS1021A
   DEVICE_VARIANT := Default
   DEVICE_PACKAGES += layerscape-rcw
-  DEVICE_DTS := ls1021a-twr
   IMAGE/firmware.bin := \
     ls-clean | \
     ls-append $(1)-rcw.bin | pad-to 1M | \
@@ -33,11 +34,10 @@ define Device/ls1021atwr
     ls-append-dtb $$(DEVICE_DTS) | pad-to 16M | \
     append-kernel | pad-to 32M | \
     append-rootfs | pad-rootfs | check-size
-  SUPPORTED_DEVICES := fsl,ls1021a-twr
 endef
-TARGET_DEVICES += ls1021atwr
+TARGET_DEVICES += fsl_ls1021a-twr
 
-define Device/ls1021atwr-sdboot
+define Device/fsl_ls1021a-twr-sdboot
   DEVICE_VENDOR := NXP
   DEVICE_MODEL := TWR-LS1021A
   DEVICE_VARIANT := SD Card Boot
@@ -57,16 +57,16 @@ define Device/ls1021atwr-sdboot
     ls-append-sdhead $(1) | pad-to 16M | \
     append-kernel | pad-to $(LS_SD_ROOTFSPART_OFFSET)M | \
     append-rootfs | check-size $(LS_SD_IMAGE_SIZE) | append-metadata
-  SUPPORTED_DEVICES := fsl,ls1021a-twr-sdboot
 endef
-TARGET_DEVICES += ls1021atwr-sdboot
+TARGET_DEVICES += fsl_ls1021a-twr-sdboot
 
-define Device/ls1021aiot-sdboot
+define Device/fsl_ls1021a-iot-sdboot
   DEVICE_VENDOR := NXP
   DEVICE_MODEL := LS1021A-IoT
   DEVICE_VARIANT := SD Card Boot
   DEVICE_DTS := ls1021a-iot
   FILESYSTEMS := ext4
+  SUPPORTED_DEVICES :=
   IMAGES := sdcard.img
   IMAGE/sdcard.img := \
     ls-clean | \
@@ -77,4 +77,4 @@ define Device/ls1021aiot-sdboot
     append-kernel | pad-to $(LS_SD_ROOTFSPART_OFFSET)M | \
     append-rootfs | check-size $(LS_SD_IMAGE_SIZE)
 endef
-TARGET_DEVICES += ls1021aiot-sdboot
+TARGET_DEVICES += fsl_ls1021a-iot-sdboot
