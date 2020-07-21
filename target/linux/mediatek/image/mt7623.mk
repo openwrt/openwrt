739d282c2fc83906c1cd75d01e86fc2ab8dd625c
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-07-21 17:28:54 +0200

    mediatek: remove condition in Device/Default
    The current condition with part of the variables set dependent on
    the subtarget in Device/Default isn't really nice to read and also
    defeats the purpose of having a default node.
    
    This removes the special settings for mt7623 and moves them to the
    individual devices, which is not much of a problem as there are
    actually just two of them and they partly use different settings
    anyway.
    
    While at it, slightly adjust the order of variables and wrap some
    long lines.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/mediatek/image/mt7623.mk
+++ target/linux/mediatek/image/mt7623.mk
@@ -29,14 +29,15 @@ define Device/bpi_bananapi-r2
   DEVICE_VENDOR := Bpi
   DEVICE_MODEL := Banana Pi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
-  SUPPORTED_DEVICES := bananapi,bpi-r2
+  KERNEL_NAME := zImage
+  KERNEL := kernel-bin | fit none $$(DTS_DIR)/$$(DEVICE_DTS).dtb
+  KERNEL_INITRAMFS :=
   DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
 	mkf2fs e2fsprogs kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-ata-ahci-mtk
   UBOOT_TARGET := mt7623n_bpir2
   IMAGES := img.gz
   IMAGE/img.gz := banana-pi-sdcard | gzip | append-metadata
-  KERNEL := kernel-bin | fit none $$(DTS_DIR)/$$(DEVICE_DTS).dtb
-  KERNEL_INITRAMFS :=
+  SUPPORTED_DEVICES := bananapi,bpi-r2
 endef
 TARGET_DEVICES += bpi_bananapi-r2
 
@@ -45,9 +46,12 @@ define Device/unielec_u7623-02-emmc-512m
   DEVICE_MODEL := U7623-02
   DEVICE_VARIANT := eMMC/512MB RAM
   DEVICE_DTS := mt7623a-unielec-u7623-02-emmc-512m
-  IMAGES := sysupgrade-emmc.bin.gz
-  IMAGE/sysupgrade-emmc.bin.gz := sysupgrade-emmc | gzip | append-metadata
+  KERNEL_NAME := zImage
+  KERNEL := kernel-bin | append-dtb | uImage none
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | uImage none
   DEVICE_PACKAGES := kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc \
 	mkf2fs e2fsprogs kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-ata-ahci-mtk
+  IMAGES := sysupgrade-emmc.bin.gz
+  IMAGE/sysupgrade-emmc.bin.gz := sysupgrade-emmc | gzip | append-metadata
 endef
 TARGET_DEVICES += unielec_u7623-02-emmc-512m
