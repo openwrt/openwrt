80dcd14abeed8cd808b92bb307964dbaeb252144
Yangbo Lu <yangbo.lu@nxp.com>
2020-07-30 13:12:43 +0800

    layerscape: add LX2160ARDB (Rev2.0 silicon) board support
    The QorIQ LX2160A reference design board provides a comprehensive platform
    that enables design and evaluation of the LX2160A processor.
    
    - Enables network intelligence with the next generation Datapath (DPPA2)
      which provides differentiated offload and a rich set of IO, including
      10GE, 25GE, 40GE, and PCIe Gen4
    
    - Delivers unprecedented efficiency and new virtualized networks
    
    - Supports designs in 5G packet processing, network function
      virtualization, storage controller, white box switching, network
      interface cards, and mobile edge computing
    
    - Supports all three LX2 family members (16-core LX2160A; 12-core LX2120A;
      and 8-core LX2080A)
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>
    [use AUTORELEASE, add dtb to firmware part]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/layerscape/image/armv8_64b.mk
+++ target/linux/layerscape/image/armv8_64b.mk
@@ -323,6 +323,58 @@ define Device/fsl_ls2088a-rdb
 endef
 TARGET_DEVICES += fsl_ls2088a-rdb
 
+define Device/fsl_lx2160a-rdb
+  DEVICE_VENDOR := NXP
+  DEVICE_MODEL := LX2160A-RDB
+  DEVICE_VARIANT := Rev2.0 silicon
+  DEVICE_PACKAGES += \
+    layerscape-mc \
+    layerscape-dpl \
+    layerscape-ddr-phy \
+    tfa-lx2160a-rdb \
+    restool
+  IMAGE/firmware.bin := \
+    ls-clean | \
+    ls-append $(1)-bl2.pbl | pad-to 1M | \
+    ls-append $(1)-fip.bin | pad-to 5M | \
+    ls-append $(1)-uboot-env.bin | pad-to 8M | \
+    ls-append $(1)-fip_ddr_all.bin | pad-to 10M | \
+    ls-append $(1)-mc.itb | pad-to 13M | \
+    ls-append $(1)-dpl.dtb | pad-to 14M | \
+    ls-append $(1)-dpc.dtb | pad-to 15M | \
+    ls-append-dtb $$(DEVICE_DTS) | pad-to 16M | \
+    append-kernel | pad-to 32M | \
+    append-rootfs | pad-rootfs | check-size
+endef
+TARGET_DEVICES += fsl_lx2160a-rdb
+
+define Device/fsl_lx2160a-rdb-sdboot
+  $(Device/fsl-sdboot)
+  DEVICE_VENDOR := NXP
+  DEVICE_MODEL := LX2160A-RDB
+  DEVICE_VARIANT := Rev2.0 silicon SD Card Boot
+  DEVICE_PACKAGES += \
+    layerscape-mc \
+    layerscape-dpl \
+    layerscape-ddr-phy \
+    tfa-lx2160a-rdb-sdboot \
+    restool
+  DEVICE_DTS := freescale/fsl-lx2160a-rdb
+  IMAGE/sdcard.img.gz := \
+    ls-clean | \
+    ls-append-sdhead $(1) | pad-to 4K | \
+    ls-append $(1)-bl2.pbl | pad-to 1M | \
+    ls-append $(1)-fip.bin | pad-to 5M | \
+    ls-append $(1)-uboot-env.bin | pad-to 8M | \
+    ls-append fsl_lx2160a-rdb-fip_ddr_all.bin | pad-to 10M | \
+    ls-append fsl_lx2160a-rdb-mc.itb | pad-to 13M | \
+    ls-append fsl_lx2160a-rdb-dpl.dtb | pad-to 14M | \
+    ls-append fsl_lx2160a-rdb-dpc.dtb | pad-to 16M | \
+    ls-append-kernel | pad-to $(LS_SD_ROOTFSPART_OFFSET)M | \
+    append-rootfs | pad-to $(LS_SD_IMAGE_SIZE)M | gzip
+endef
+TARGET_DEVICES += fsl_lx2160a-rdb-sdboot
+
 define Device/traverse_ls1043
   DEVICE_VENDOR := Traverse
   DEVICE_MODEL := LS1043 Boards
