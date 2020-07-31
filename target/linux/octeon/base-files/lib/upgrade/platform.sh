e3a7ee51979424a5a40d7431097757ccfca44c19
Jacob Bernhardt <31599460+bernhardtj@users.noreply.github.com>
2020-07-31 14:54:19 -0500

    octeon: add support for Unifi Security Gateway 3P
    The Unifi Security Gateway is almost identical to the Ubiquiti EdgeRouter
    Lite, which is already supported.
    
    Specifications:
    - 2x 500MHz Cavium CN5020 cores
    - 4MB Flash / 512MB RAM
    - 3 Gbit Ethernet Ports
    - Blue/white Unifi Dome LEDs
    - USB 2.0 port (internal)
    
    As the USG boots off of an internal 4G usb stick, flashing procedure is
    the same as for the ERL:
    1. Take out the internal usb stick (as /dev/sdc)
    2. mkfs.vfat /dev/sdc1; mount /dev/sdc1 /mnt
    3. tar xf openwrt-octeon-usg3p-sysupgrade.tar --strip 1
    4. cp ./kernel /mnt/vmlinux.64
    5. dd if=./root of=/dev/sdc2
    
    Signed-off-by: Jacob Bernhardt <31599460+bernhardtj@users.noreply.github.com>

--- target/linux/octeon/base-files/lib/upgrade/platform.sh
+++ target/linux/octeon/base-files/lib/upgrade/platform.sh
@@ -19,7 +19,8 @@ platform_get_rootfs() {
 
 platform_copy_config() {
 	case "$(board_name)" in
-	erlite)
+	erlite | \
+	ubnt,unifi-security-gateway-3p)
 		mount -t vfat /dev/sda1 /mnt
 		cp -af "$UPGRADE_BACKUP" "/mnt/$BACKUP_FILE"
 		umount /mnt
@@ -81,7 +82,8 @@ platform_do_upgrade() {
 	er)
 		kernel=mmcblk0p1
 		;;
-	erlite)
+	erlite | \
+	ubnt,unifi-security-gateway-3p)
 		kernel=sda1
 		;;
 	itus,shield-router)
@@ -107,7 +109,8 @@ platform_check_image() {
 	case "$board" in
 	er | \
 	erlite | \
-	itus,shield-router)
+	itus,shield-router \
+	ubnt,unifi-security-gateway-3p)
 		local kernel_length=$(tar xf $tar_file $board_dir/kernel -O | wc -c 2> /dev/null)
 		local rootfs_length=$(tar xf $tar_file $board_dir/root -O | wc -c 2> /dev/null)
 		[ "$kernel_length" = 0 -o "$rootfs_length" = 0 ] && {
