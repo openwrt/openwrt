009e62c92c35142c5be3b375d6c393e63d2d27eb
Richard Fröhning <misanthropos@gmx.de>
2020-08-02 16:22:50 +0200

    ramips: add support for TP-Link RE200 v3
    TP-Link RE200 v3 is a wireless range extender with Ethernet and 2.4G and 5G
    WiFi with internal antennas. It's based on MediaTek MT7628AN+MT7610EN like the v2.
    
    Specifications
    --------------
    
    - MediaTek MT7628AN (580 Mhz)
    - 64 MB of RAM
    - 8 MB of FLASH
    - 2T2R 2.4 GHz and 1T1R 5 GHz
    - 1x 10/100 Mbps Ethernet
    - 8x LED (GPIO-controlled), 2x button
    
    Unverified:
    - UART header on PCB (57600 8n1)
    
    There are 2.4G and 5G LEDs in red and green which are controlled
    separately.
    
    MAC addresses
    -------------
    
    MAC address assignment has been done according to the RE200 v2.
    
    The label MAC address matches the OpenWrt ethernet address.
    
    Installation
    ------------
    
    Web Interface
    -------------
    
    It is possible to upgrade to OpenWrt via the web interface. Simply flash
    the -factory.bin from OEM. In contrast to a stock firmware, this will not
    overwrite U-Boot.
    
    Recovery
    --------
    
    Unfortunately, this devices does not offer a recovery mode or a tftp
    installation method. If the web interface upgrade fails, you have to open
    your device and attach serial console.
    
    The device has not been opened for adding support. However, it is expected
    that the behavior is similar to the RE200 v2. Instructions for serial console
    and recovery may be checked out in commit 6d6f36ae787c ("ramips: add support
    for TP-Link RE200 v2") or on the device's Wiki page.
    
    Signed-off-by: Richard Fröhning <misanthropos@gmx.de>
    [adjust commit title/message, sort support list]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -404,6 +404,16 @@ define Device/tplink_re200-v2
 endef
 TARGET_DEVICES += tplink_re200-v2
 
+define Device/tplink_re200-v3
+  $(Device/tplink-safeloader)
+  IMAGE_SIZE := 7808k
+  DEVICE_MODEL := RE200
+  DEVICE_VARIANT := v3
+  DEVICE_PACKAGES := kmod-mt76x0e
+  TPLINK_BOARD_ID := RE200-V3
+endef
+TARGET_DEVICES += tplink_re200-v3
+
 define Device/tplink_re220-v2
   $(Device/tplink-safeloader)
   IMAGE_SIZE := 7808k
