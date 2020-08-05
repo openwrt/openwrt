4c73c34ec4215deb690bf03faea2a0fe725476f0
Alexey Dobrovolsky <dobrovolskiy.alexey@gmail.com>
2020-08-05 22:50:53 +0300

    ramips: switch rt305x subtarget to kernel 5.4
    RT3x5x seems to work fine with kernel 5.4. Set the default kernel
    version to 5.4 to bring this to a broader audience.
    
    Since 4 of 6 targets are on kernel 5.4 now, invert the kernel
    version setup logic in Makefile/target.mk files.
    
    Tested on ZyXEL Keenetic.
    
    Signed-off-by: Alexey Dobrovolsky <dobrovolskiy.alexey@gmail.com>
    [invert version setup logic]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ramips/rt288x/target.mk
+++ target/linux/ramips/rt288x/target.mk
@@ -9,6 +9,8 @@ CPU_TYPE:=24kc
 
 DEFAULT_PACKAGES += kmod-rt2800-soc wpad-mini swconfig
 
+KERNEL_PATCHVER:=4.14
+
 define Target/Description
 	Build firmware images for Ralink RT288x based boards.
 endef
