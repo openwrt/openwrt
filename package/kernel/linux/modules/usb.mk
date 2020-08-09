87e92d50e577851743663763bc749fe18aa294c7
Ansuel Smith <ansuelsmth@gmail.com>
2020-08-09 16:31:05 +0200

    kernel: usb: move phy-qcom-ipq806x-usb to ipq806x modules.mk
    This driver is only used by ipq806x SoCs. Move it there and drop
    dependency from ipq40xx since it's not used anywere.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>
    [rebase on changes to previous patches]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Tested-by: Paul Blazejowski <paulb@blazebox.homeip.net> [R7800]

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -70,24 +70,6 @@ endef
 $(eval $(call KernelPackage,usb-phy-nop))
 
 
-define KernelPackage/phy-qcom-ipq806x-usb
-  TITLE:=Qualcomm IPQ806x DWC3 USB PHY driver
-  DEPENDS:=@(TARGET_ipq40xx||TARGET_ipq806x)
-  KCONFIG:= CONFIG_PHY_QCOM_IPQ806X_USB
-  FILES:= \
-    $(LINUX_DIR)/drivers/phy/qualcomm/phy-qcom-ipq806x-usb.ko
-  AUTOLOAD:=$(call AutoLoad,45,phy-qcom-ipq806x-usb,1)
-  $(call AddDepends/usb)
-endef
-
-define KernelPackage/phy-qcom-ipq806x-usb/description
- This driver provides support for the integrated DesignWare
- USB3 IP Core within the QCOM SoCs.
-endef
-
-$(eval $(call KernelPackage,phy-qcom-ipq806x-usb))
-
-
 define KernelPackage/phy-ath79-usb
   TITLE:=Support for ATH79 USB PHY
   KCONFIG:=CONFIG_PHY_AR7100_USB \
