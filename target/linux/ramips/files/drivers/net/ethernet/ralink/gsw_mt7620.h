b4f7a8da491cd18fe2eec655232757c8ce180033
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-08-21 16:18:23 +0200

    ramips: remove further mt7621 code from local ethernet driver
    While commit 734a8c46e703 focussed on removing stuff directly
    selected by the NET_RALINK_* symbols, this patch removes additional
    unused mt7621-specific code from the ethernet driver.
    
    As with the previous patch, the main reason is to reduce the amount
    of code we have to maintain and care about.
    
    Note that this patch still keeps a few lines with
    IS_ENABLED(CONFIG_SOC_MT7621) in mtk_eth_soc.h/.c, as this file is
    still selected for the mt7621 subtarget.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- target/linux/ramips/files/drivers/net/ethernet/ralink/gsw_mt7620.h
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/gsw_mt7620.h
@@ -17,11 +17,7 @@
 
 #define GSW_REG_PHY_TIMEOUT	(5 * HZ)
 
-#ifdef CONFIG_SOC_MT7621
-#define MT7620A_GSW_REG_PIAC	0x0004
-#else
 #define MT7620A_GSW_REG_PIAC	0x7004
-#endif
 
 #define GSW_NUM_VLANS		16
 #define GSW_NUM_VIDS		4096
@@ -65,11 +61,7 @@
 
 #define PORT_IRQ_ST_CHG		0x7f
 
-#ifdef CONFIG_SOC_MT7621
-#define ESW_PHY_POLLING		0x0000
-#else
 #define ESW_PHY_POLLING		0x7000
-#endif
 
 #define	PMCR_IPG		BIT(18)
 #define	PMCR_MAC_MODE		BIT(16)
