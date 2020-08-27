e79df3516d3e2931a2a2964cadfed0af99acef49
Paul Spooren <mail@aparcar.org>
2020-08-27 11:47:06 -1000

    build: add libustream and certs to default pkgs
    To allow HTTPS usage on a router it requires both certificates
    (ca-bundle) and a fitting libustream library (libustream-wolfssl)
    
    By adding both, uclient-fetch and wget can connect to encrypted HTTP.
    
    This allows opkg to update package lists in a more secure fashion.
    
    Suggested-by: Petr Štetiar <ynezz@true.cz>
    Suggested-by: Baptiste Jonglez <baptiste@bitsofnetworks.org>
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/target.mk
+++ include/target.mk
@@ -13,7 +13,7 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd
+DEFAULT_PACKAGES:=base-files busybox ca-bundle dropbear fstools libc libgcc logd libustream-wolfssl mtd netifd opkg uci uclient-fetch urandom-seed urngd
 # For the basic set
 DEFAULT_PACKAGES.basic:=
 # For nas targets
