e8762024f5c57cf8a4a8b74cae7c6c4915e177b6
Pawel Dembicki <paweldembicki@gmail.com>
2020-08-30 22:26:14 +0200

    kirkwood: add support for NETGEAR ReadyNAS Duo v2
    NETGEAR ReadyNAS Duo v2 is a NAS based on Marvell kirkwood SoC.
    
    Specification:
     - Processor Marvell 88F6282 (1.6 GHz)
     - 256MB RAM
     - 128MB NAND
     - 1x GBE LAN port (PHY: Marvell 88E1318)
     - 1x USB 2.0
     - 2x USB 3.0
     - 2x SATA
     - 3x button
     - 5x leds
     - serial on J5 connector accessible from rear panel
       (115200 8N1) (VCC,TX,RX,GND) (3V3 LOGIC!)
    
    Installation by USB + serial:
      - Copy initramfs image to fat32 usb drive
      - Connect pendrive to USB 2.0 front socket
      - Connect serial console
      - Stop booting in u-boot
      - Do:
    	usb reset
    	fatload usb 0:1 0x1200000 openwrt-kirkwood-netgear_readynas-duo-v2-initramfs-uImage
    	setenv bootargs 'console=ttyS0,115200n8 earlyprintk'
    	bootm 0x1200000
      - copy sysupgrade image via ssh.
      - run sysupgrade
    
    Installation by TFTP + serial:
      - Setup TFTP server and copy initramfs image
      - Connect serial console
      - Stop booting in u-boot
      - Do:
    	setenv serverip 192.168.1.1
    	setenv ipaddr 192.168.1.2
    	setenv bootargs 'console=ttyS0,115200n8 earlyprintk'
    	tftpboot 0x1200000 openwrt-kirkwood-netgear_readynas-duo-v2-initramfs-uImage
    	bootm 0x1200000
      - copy sysupgrade image via ssh.
      - run sysupgrade
    
    Known issues:
    gpio-poweroff from upstream dts is buggy. It isn't work and causes only warning.
    After that is impossible to start device without unplugging the cord.
    According to GPL sources of NETGEAR, poweroff should be done as reboot with
    some registers configuration. To proper poweroff is needed to clear bit in one
    of registers:
      - PHY register: Page 3 Reg 17 Bit 4
      - RTC register: SL2(BIT5) of RS5C372a control 1 register (0xe0)
    
    For more info please look at file:
    RND_5.3.13_WW.src/u-boot/board/mv_feroceon/mv_hal/usibootup/usibootup.c
    from Netgear GPL sources.
    
    Tested-By: Raylynn Knight <rayknight@me.com>
    Tested-By: Lech Perczak <lech.perczak@gmail.com>
    Signed-off-by: Pawel Dembicki <paweldembicki@gmail.com>

--- target/linux/kirkwood/base-files/lib/upgrade/platform.sh
+++ target/linux/kirkwood/base-files/lib/upgrade/platform.sh
@@ -4,7 +4,18 @@ RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
-	return 0
+	local board="$(board_name)"
+
+	case "$board" in
+	netgear,readynas-duo-v2)
+		# let's store how rootfs is mounted
+		cp /proc/mounts /tmp/mounts
+		return 0
+		;;
+	*)
+		return 0
+		;;
+	esac
 }
 
 platform_do_upgrade() {
@@ -15,6 +26,16 @@ platform_do_upgrade() {
 	linksys,ea3500|\
 	linksys,ea4500)
 		platform_do_upgrade_linksys "$1"
+		;;
+	netgear,readynas-duo-v2)
+		local root_mtd=$(cat /tmp/mounts | grep "/dev/root")
+		if [ -z "$root_mtd" ]; then
+			mkdir -p /var/lock
+			touch /var/lock/fw_printenv.lock
+			fw_setenv bootargs 'console=ttyS0,115200n8 earlyprintk'
+			fw_setenv bootcmd 'nand read.e 0x1200000 0x200000 0x600000;bootm 0x1200000'
+		fi
+		nand_do_upgrade "$1"
 		;;
 	*)
 		nand_do_upgrade "$1"
