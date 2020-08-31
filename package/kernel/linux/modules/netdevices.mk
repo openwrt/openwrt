f48d6035698437197d26f4db5c0909eed87dee95
Chen Minqiang <ptpt52@gmail.com>
2020-08-31 22:35:03 +0800

    kernel: Make kmod-sfp depend on kmod-libphy
    With kernel 5.4 kmod-sfp is depending on kmod-libphy, add this
    missing dependency.
    
    This also make phylink tristate so that kmod build work
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -111,6 +111,7 @@ $(eval $(call KernelPackage,libphy))
 define KernelPackage/phylink
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Model for MAC to optional PHY connection
+  DEPENDS:=+LINUX_5_4:kmod-libphy
   KCONFIG:=CONFIG_PHYLINK
   FILES:=$(LINUX_DIR)/drivers/net/phy/phylink.ko
   AUTOLOAD:=$(call AutoLoad,15,phylink,1)
@@ -1375,7 +1376,7 @@ $(eval $(call KernelPackage,qlcnic))
 define KernelPackage/sfp
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=SFP cage support
-  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core +kmod-phylink
+  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core +kmod-phylink +LINUX_5_4:kmod-libphy
   KCONFIG:= \
 	CONFIG_SFP \
 	CONFIG_MDIO_I2C
