b186ce5c5d8b1601535c74c0f54e0e82681d1174
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2020-09-08 16:00:02 +0800

    x86/efi: add FAT32 esp mounting support
    Adds a new function get_magic_fat32() in base-files to read FAT32 magic.
    Now FAT32 EFI system partition can be handled in the same way as FAT12/FAT16.
    
    Signed-off-by: Kagurazaka Kotori <kagurazakakotori@gmail.com>

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -110,6 +110,10 @@ get_magic_vfat() {
 	(get_image "$@" | dd bs=1 count=3 skip=54) 2>/dev/null
 }
 
+get_magic_fat32() {
+	(get_image "$@" | dd bs=1 count=5 skip=82) 2>/dev/null
+}
+
 part_magic_efi() {
 	local magic=$(get_magic_gpt "$@")
 	[ "$magic" = "EFI PART" ]
@@ -117,7 +121,8 @@ part_magic_efi() {
 
 part_magic_fat() {
 	local magic=$(get_magic_vfat "$@")
-	[ "$magic" = "FAT" ]
+	local magic_fat32=$(get_magic_fat32 "$@")
+	[ "$magic" = "FAT" -o "$magic_fat32" = "FAT32" ]
 }
 
 export_bootdevice() {
