9d84b33e23da5938c573b8625a64eb13bdd308f7
John Audia <graysky@archlinux.us>
2020-09-17 09:51:25 -0400

    kernel: bump 5.4 to 5.4.66
    All modifications made by update_kernel.sh/no manual intervention needed
    
    Run-tested: ipq806x (R7800), ath79 (Archer C7v5), x86/64
    
    No dmesg regressions, everything appears functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    [add run test from PR]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-4.19 = .138
-LINUX_VERSION-5.4 = .65
+LINUX_VERSION-5.4 = .66
 
 LINUX_KERNEL_HASH-4.19.138 = d15c27d05f6c527269b75b30cc72972748e55720e7e00ad8abbaa4fe3b1d5e02
-LINUX_KERNEL_HASH-5.4.65 = f514834417d09de1667836e443e085bf37952603f23572b69ef0fcfda16cac69
+LINUX_KERNEL_HASH-5.4.66 = ff1930376774f4c1fc884d82184b5ebea6628f0a37ed9be781c0b119c4cfdab2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
