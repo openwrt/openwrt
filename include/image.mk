96d1dc5ebfbbf68f960610407a83423915e5e966
Paul Spooren <mail@aparcar.org>
2020-09-27 22:37:53 -1000

    policycoreutils: install to host/bin not hostpkg
    By installing policycoreutils to host/bin it is also available within
    the ImageBuilder and SDK, allowing to correctly label both filesystems
    and packages.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/image.mk
+++ include/image.mk
@@ -243,7 +243,7 @@ endef
 
 ifeq ($(CONFIG_TARGET_ROOTFS_SECURITY_LABELS),y)
 define Image/mkfs/squashfs
-	echo "$(STAGING_DIR_HOSTPKG)/bin/setfiles -r" \
+	echo "$(STAGING_DIR_HOST)/bin/setfiles -r" \
 	     "$(call mkfs_target_dir,$(1))" \
 	     "$(call mkfs_target_dir,$(1))/etc/selinux/targeted/contexts/files/file_contexts " \
 	     "$(call mkfs_target_dir,$(1))" > $@.fakeroot-script
