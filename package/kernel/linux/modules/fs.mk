6542615e41a109d944730cabd91441cdb3a692c2
Hauke Mehrtens <hauke@hauke-m.de>
2020-10-10 12:10:10 +0200

    kernel: Move CONFIG_F2FS_FS_SECURITY to generic kernel config
    Move the CONFIG_F2FS_FS_SECURITY kernel configuration option to the
    generic kernel configuration.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/fs.mk
+++ package/kernel/linux/modules/fs.mk
@@ -209,7 +209,6 @@ define KernelPackage/fs-f2fs
   KCONFIG:= \
 	CONFIG_F2FS_FS \
 	CONFIG_F2FS_FS_POSIX_ACL=n \
-	CONFIG_F2FS_FS_SECURITY=n \
 	CONFIG_F2FS_CHECK_FS=n
   FILES:=$(LINUX_DIR)/fs/f2fs/f2fs.ko
   AUTOLOAD:=$(call AutoLoad,30,f2fs,1)
