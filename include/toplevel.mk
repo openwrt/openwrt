f344f9d066485dc65f747b0d610f3c9d1d395b32
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-10-16 22:58:44 +0200

    Revert "build: process variable exports from toplevel.mk only once"
    This reverts commit ef7c34c1d1beac6bca4a683a3a161dd12a81f7e8.
    
    The commit seems to break all buildbots with messages like:
    
    /builder/shared-workdir/build/include/toplevel.mk:15:
      /builder/shared-workdir/build/include/toplevel-vars.mk: No such file or directory
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -11,13 +11,61 @@ PREP_MK= OPENWRT_BUILD= QUIET=0
 export IS_TTY=$(shell tty -s && echo 1 || echo 0)
 
 include $(TOPDIR)/include/verbose.mk
-ifeq ($(OPENWRT_VARS),)
-  include $(TOPDIR)/include/toplevel-vars.mk
+
+ifeq ($(SDK),1)
+  include $(TOPDIR)/include/version.mk
+else
+  REVISION:=$(shell $(TOPDIR)/scripts/getver.sh)
+  SOURCE_DATE_EPOCH:=$(shell $(TOPDIR)/scripts/get_source_date_epoch.sh)
+endif
+
+HOSTCC ?= $(CC)
+export REVISION
+export SOURCE_DATE_EPOCH
+export GIT_CONFIG_PARAMETERS='core.autocrlf=false'
+export GIT_ASKPASS:=/bin/true
+export MAKE_JOBSERVER=$(filter --jobserver%,$(MAKEFLAGS))
+export GNU_HOST_NAME:=$(shell $(TOPDIR)/scripts/config.guess)
+export HOST_OS:=$(shell uname)
+export HOST_ARCH:=$(shell uname -m)
+
+# prevent perforce from messing with the patch utility
+unexport P4PORT P4USER P4CONFIG P4CLIENT
+
+# prevent user defaults for quilt from interfering
+unexport QUILT_PATCHES QUILT_PATCH_OPTS
+
+unexport C_INCLUDE_PATH CROSS_COMPILE ARCH
+
+# prevent distro default LPATH from interfering
+unexport LPATH
+
+# make sure that a predefined CFLAGS variable does not disturb packages
+export CFLAGS=
+export LDFLAGS=
+
+empty:=
+space:= $(empty) $(empty)
+path:=$(subst :,$(space),$(PATH))
+path:=$(filter-out .%,$(path))
+path:=$(subst $(space),:,$(path))
+export PATH:=$(path)
+
+unexport TAR_OPTIONS
+
+ifneq ($(shell $(HOSTCC) 2>&1 | grep clang),)
+  export HOSTCC_REAL?=$(HOSTCC)
+  export HOSTCC_WRAPPER:=$(TOPDIR)/scripts/clang-gcc-wrapper
+else
+  export HOSTCC_WRAPPER:=$(HOSTCC)
 endif
 
 ifeq ($(FORCE),)
   .config scripts/config/conf scripts/config/mconf: staging_dir/host/.prereq-build
 endif
+
+SCAN_COOKIE?=$(shell echo $$$$)
+export SCAN_COOKIE
 
 SUBMAKE:=umask 022; $(SUBMAKE)
 
