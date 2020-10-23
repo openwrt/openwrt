d1f1e5269ed992e6fc30d575b43b0913c9e58ca2
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2020-10-23 16:05:28 +0200

    ipq40xx: add support for MikroTik SXTsq 5 ac
    This commit adds support for the MikroTik SXTsq 5 ac (RBSXTsqG-5acD),
    an outdoor 802.11ac wireless CPE with one 10/100/1000 Mbps Ethernet
    port.
    
    Specifications:
     - SoC: Qualcomm Atheros IPQ4018
     - RAM: 256 MB
     - Storage: 16 MB NOR
     - Wireless: IPQ4018 (SoC) 802.11a/n/ac 2x2:2, 16 dBi antennae
     - Ethernet: IPQ4018 (SoC) 1x 10/100/1000 port, 10-28 Vdc PoE in
     - 1x Ethernet LED (green)
     - 7x user-controllable LEDs
      · 1x power (blue)
      · 1x user (green)
      · 5x rssi (green)
    
    Note:
     Serial UART is probably available on the board, but it has not been
     tested.
    
    Flashing:
     Boot via TFTP the initramfs image. Then, upload a sysupgrade image
     via SSH and flash it normally. More info at the "Common procedures
     for MikroTik products" page https://openwrt.org/toh/mikrotik/common.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -111,7 +111,8 @@ platform_do_upgrade() {
 		CI_KERNPART="part.safe"
 		nand_do_upgrade "$1"
 		;;
-	mikrotik,hap-ac2)
+	mikrotik,hap-ac2|\
+	mikrotik,sxtsq-5-ac)
 		[ "$(rootfs_type)" = "tmpfs" ] && mtd erase firmware
 		default_do_upgrade "$1"
 		;;
