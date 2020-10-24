2cc55fa1ddb0b00f6f1ad884ce8630c3a686bdc2
Felix Fietkau <nbd@nbd.name>
2020-10-24 21:15:20 +0200

    mediatek: mt7622: add Linux 5.10 support
    Switch mt7622 subtarget to Linux 5.10, it has been tested by many of us
    on several devices for a couple of weeks already.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- target/linux/mediatek/mt7622/target.mk
+++ target/linux/mediatek/mt7622/target.mk
@@ -4,6 +4,7 @@ BOARDNAME:=MT7622
 CPU_TYPE:=cortex-a53
 DEFAULT_PACKAGES += kmod-mt7615e kmod-mt7615-firmware wpad-basic-wolfssl
 KERNELNAME:=Image dtbs
+KERNEL_PATCHVER:=5.10
 
 define Target/Description
 	Build firmware images for MediaTek MT7622 ARM based boards.
