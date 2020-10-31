25a3e7fc27bded1fb03bc4456c8ea24683fd6832
John Audia <graysky@archlinux.us>
2020-10-31 07:35:12 -0400

    kernel: bump 5.4 to 5.4.73
    Removed upstreamed patches:
     generic/pending-5.4
       445-mtd-spinand-gigadevice-Only-one-dummy-byte-in-QUA.patch
       446-mtd-spinand-gigadevice-Add-QE-Bit.patch
     pistachio/patches-5.4
       150-pwm-img-Fix-null-pointer-access-in-probe.patch
    
    Manually rebased:
     layerscape/patches-5.4
       801-audio-0011-Revert-ASoC-fsl_sai-add-of_match-data.patch
       801-audio-0039-MLK-16224-6-ASoC-fsl_sai-fix-DSD-suspend-resume.patch
       801-audio-0073-MLK-21957-3-ASoC-fsl_sai-add-bitcount-and-timestamp-.patch
       820-usb-0009-usb-dwc3-Add-workaround-for-host-mode-VBUS-glitch-wh.patch
    
    All modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, ath79/generic, bcm27xx/bcm2711,
                  mvebu (mamba, rango), x86_64, ramips/mt7621
    Run-tested: ipq806x/R7800, mvebu (mamba, rango), x86_64, ramips (RT-AC57U)
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    [alter 820-usb-0009-usb-dwc3-Add-workaround-for-host-mode-VBUS-glitch-wh]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .72
+LINUX_VERSION-5.4 = .73
 
-LINUX_KERNEL_HASH-5.4.72 = 0e24645bd56fe5b55a7a662895f5562c103d71b54d097281f0c9c71ff22c1172
+LINUX_KERNEL_HASH-5.4.73 = 5a424b403b726bbe7dfa1d1524e431676c4a64f22a8db524534ed678bfe576b2
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
