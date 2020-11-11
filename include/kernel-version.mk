0e5547abc66637533904911aa482d148d8e99a8d
John Audia <graysky@archlinux.us>
2020-11-11 15:30:36 -0500

    kernel: bump 5.4 to 5.4.77
    Manually rebased patches:
     bcm27xx/patches-5.4/950-0135-spi-spi-bcm2835-Disable-forced-software-CS.patch
     generic-backport/744-v5.5-net-sfp-soft-status-and-control-support.patch
     layerscape/patches-5.4/819-uart-0005-tty-serial-fsl_lpuart-enable-dma-mode-for-imx8qxp.patch
     mvebu/patches-5.4/521-arm64-dts-marvell-espressobin-Add-ethernet-switch-al.patch
    
    Removed:
     layerscape/patches-5.4/819-uart-0012-tty-serial-lpuart-add-LS1028A-support.patch
    
    All modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, ath79/generic, bcm27xx/bcm2711,
                  lantiq/Easybox 904 xDSL, x86_64
    Run-tested: ipq806x/R7800, lantiq/Easybox 904 xDSL, x86_64
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Co-developed-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .75
+LINUX_VERSION-5.4 = .77
 
-LINUX_KERNEL_HASH-5.4.75 = d2466fd6eb5433e7bf287b617b11b2640c65a7ea93a57eb7a80d7f537cbc1470
+LINUX_KERNEL_HASH-5.4.77 = a3e03e6970240dddc8174bf9f49b56d774c40125eabe1582d2ebe85b01addbf7
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
