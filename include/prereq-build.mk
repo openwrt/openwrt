cc16f5d73e770136eb0f3cf92ed518292d626bac
Felix Fietkau <nbd@nbd.name>
2020-11-14 07:35:11 +0100

    build: try to find getopt in macOS homebrew's standard location
    It is not symlinked to /usr/local/bin by default
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -125,7 +125,8 @@ $(eval $(call SetupHostCommand,egrep,Please install GNU 'grep', \
 $(eval $(call SetupHostCommand,getopt, \
 	Please install an extended getopt version that supports --long, \
 	gnugetopt -o t --long test -- --test | grep '^ *--test *--', \
-	getopt -o t --long test -- --test | grep '^ *--test *--'))
+	getopt -o t --long test -- --test | grep '^ *--test *--', \
+	/usr/local/opt/gnu-getopt/bin/getopt -o t --long test -- --test | grep '^ *--test *--'))
 
 $(eval $(call SetupHostCommand,stat,Cannot find a file stat utility, \
 	gnustat -c%s $(TOPDIR)/Makefile, \
