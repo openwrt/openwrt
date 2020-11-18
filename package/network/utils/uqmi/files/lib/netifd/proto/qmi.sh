ce293cd3ac3a594d536dc4243e85434fa503265b
Filip Moc <lede@moc6.cz>
2020-11-18 20:37:17 +0100

    uqmi: set device-operating-mode to online
    This is required for LTE module MR400 (in TL-MR6400 v4).
    Otherwise LTE module won't register to GSM network.
    
    Signed-off-by: Filip Moc <lede@moc6.cz>

--- package/network/utils/uqmi/files/lib/netifd/proto/qmi.sh
+++ package/network/utils/uqmi/files/lib/netifd/proto/qmi.sh
@@ -174,6 +174,9 @@ proto_qmi_setup() {
 	# Cleanup current state if any
 	uqmi -s -d "$device" --stop-network 0xffffffff --autoconnect > /dev/null 2>&1
 
+	# Go online
+	uqmi -s -d "$device" --set-device-operating-mode online > /dev/null 2>&1
+
 	# Set IP format
 	uqmi -s -d "$device" --set-data-format 802.3 > /dev/null 2>&1
 	uqmi -s -d "$device" --wda-set-data-format 802.3 > /dev/null 2>&1
