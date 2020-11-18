2c513e2427d9addd0cd3e1dddf795266b689b3f5
Filip Moc <lede@moc6.cz>
2020-11-18 20:38:21 +0100

    ramips: add support for TP-Link MR6400 v4
    You can flash via tftp recovery:
     - serve tftp-recovery image as /tp_recovery.bin on 192.168.0.225/24
     - connect to any ethernet port
     - power on the device while holding the reset button
     - wait at least 8 seconds before releasing reset button
    
    Flashing via OEM web interface does not work.
    
    LTE module does not support DHCP so it must be configured via QMI.
    
    Hardware Specification (v4.0 EU):
     - SoC: MT7628NN
     - Flash: Winbond W25Q64JVS (8MiB)
     - RAM: ESMT M14D5121632A (64MiB)
     - Wireless: SoC platform only (2.4GHz b/g/n, 2x internal antenna)
     - Ethernet: 1NIC (4x100M)
     - WWAN: TP-LINK LTE MODULE (2x external detachable antenna)
     - Power: DC 9V 0.85A
    
    Signed-off-by: Filip Moc <lede@moc6.cz>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -482,6 +482,22 @@ define Device/tplink_tl-mr3420-v5
 endef
 TARGET_DEVICES += tplink_tl-mr3420-v5
 
+define Device/tplink_tl-mr6400-v4
+  $(Device/tplink-v2)
+  IMAGE_SIZE := 7808k
+  DEVICE_MODEL := TL-MR6400
+  DEVICE_VARIANT := v4
+  TPLINK_FLASHLAYOUT := 8Mmtk
+  TPLINK_HWID := 0x64000004
+  TPLINK_HWREV := 0x4
+  TPLINK_HWREVADD := 0x4
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport \
+	kmod-usb-serial kmod-usb-serial-option kmod-usb-net-qmi-wwan uqmi
+  IMAGES := sysupgrade.bin tftp-recovery.bin
+  IMAGE/tftp-recovery.bin := pad-extra 128k | $$(IMAGE/factory.bin)
+endef
+TARGET_DEVICES += tplink_tl-mr6400-v4
+
 define Device/tplink_tl-wa801nd-v5
   $(Device/tplink-v2)
   IMAGE_SIZE := 7808k
