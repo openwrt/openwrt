2be31f5d73edada2f8c560a6050dea0e71d7bbaa
Sven Eckelmann <sven@narfation.org>
2020-11-23 13:41:34 +0100

    ath79: add support for OpenMesh OM2P-HS v1
    Device specifications:
    ======================
    
    * Qualcomm/Atheros AR9341 rev 1
    * 535/400/200 MHz (CPU/DDR/AHB)
    * 64 MB of RAM
    * 16 MB of SPI NOR flash
      - 2x 7 MB available; but one of the 7 MB regions is the recovery image
    * 2x 10/100 Mbps Ethernet
    * 2T2R 2.4 GHz Wi-Fi
    * 6x GPIO-LEDs (3x wifi, 2x ethernet, 1x power)
    * 1x GPIO-button (reset)
    * external h/w watchdog (enabled by default)
    * TTL pins are on board (arrow points to VCC, then follows: GND, TX, RX)
    * 2x fast ethernet
      - eth0
        + 802.3af POE
        + builtin switch port 1
        + used as LAN interface
      - eth1
        + 18-24V passive POE (mode B)
        + used as WAN interface
    * 12-24V 1A DC
    * internal antennas
    
    Flashing instructions:
    ======================
    
    Various methods can be used to install the actual image on the flash.
    Two easy ones are:
    
    ap51-flash
    ----------
    
    The tool ap51-flash (https://github.com/ap51-flash/ap51-flash) should be
    used to transfer the image to the u-boot when the device boots up.
    
    initramfs from TFTP
    -------------------
    
    The serial console must be used to access the u-boot shell during bootup.
    It can then be used to first boot up the initramfs image from a TFTP server
    (here with the IP 192.168.1.21):
    
       setenv serverip 192.168.1.21
       setenv ipaddr 192.168.1.1
       tftpboot 0c00000 <filename-of-initramfs-kernel>.bin && bootm $fileaddr
    
    The actual sysupgrade image can then be transferred (on the LAN port) to
    the device via
    
      scp <filename-of-squashfs-sysupgrade>.bin root@192.168.1.1:/tmp/
    
    On the device, the sysupgrade must then be started using
    
      sysupgrade -n /tmp/<filename-of-squashfs-sysupgrade>.bin
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    [drop redundant status from eth1]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/generic/base-files/lib/upgrade/platform.sh
+++ target/linux/ath79/generic/base-files/lib/upgrade/platform.sh
@@ -51,6 +51,7 @@ platform_do_upgrade() {
 		redboot_fis_do_upgrade "$1" linux
 		;;
 	openmesh,om2p-v4|\
+	openmesh,om2p-hs-v1|\
 	openmesh,om2p-hs-v4)
 		PART_NAME="inactive"
 		platform_do_upgrade_openmesh "$1"
