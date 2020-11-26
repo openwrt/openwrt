e8f321403422546bc5c27d5ea375d9a209ef649d
Petr Štetiar <ynezz@true.cz>
2020-11-26 10:23:35 +0100

    kernel: sfc,sfc-falcon: fix kernel config symbols
    I've just noticed on i.mx6 target, that there are missing kernel symbols
    so I'm fixing it.
    
    Fixes: 3c5d70ad26ed ("kernel: add module support Solarflare network adapter")
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1196,10 +1196,10 @@ define KernelPackage/sfc
   DEPENDS:=@PCI_SUPPORT +kmod-mdio +kmod-lib-crc32c +kmod-ptp +kmod-hwmon-core
   KCONFIG:= \
 	CONFIG_SFC \
-	CONFIG_SFC_MTD \
-	CONFIG_SFC_MCDI_MON \
-	CONFIG_SFC_MCDI_LOGGING \
-	CONFIG_SFC_SRIOV
+	CONFIG_SFC_MTD=y \
+	CONFIG_SFC_MCDI_MON=y \
+	CONFIG_SFC_MCDI_LOGGING=y \
+	CONFIG_SFC_SRIOV=y
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/sfc/sfc.ko
   AUTOLOAD:=$(call AutoProbe,sfc)
 endef
@@ -1217,7 +1217,7 @@ define KernelPackage/sfc-falcon
   DEPENDS:=@PCI_SUPPORT +kmod-mdio +kmod-lib-crc32c +kmod-i2c-algo-bit
   KCONFIG:= \
 	CONFIG_SFC_FALCON \
-	CONFIG_SFC_FALCON_MTD
+	CONFIG_SFC_FALCON_MTD=y
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/sfc/falcon/sfc-falcon.ko
   AUTOLOAD:=$(call AutoProbe,sfc-falcon)
 endef
