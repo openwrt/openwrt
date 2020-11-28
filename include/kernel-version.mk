c72b7a4f0d8d476df5fb0f9d6a2ad8fadb223025
Hauke Mehrtens <hauke@hauke-m.de>
2020-11-28 23:02:21 +0100

    kernel: bump 4.14 to 4.14.209
    Refreshed all patches.
    
    Altered patches:
    - 804-i2c-support-layerscape.patch
    
    Compile-tested on: ipq40xx, ath79, layerscape/armv8_64b
    Runtime-tested on: ipq40xx, ath79
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .206
+LINUX_VERSION-4.14 = .209
 
-LINUX_KERNEL_HASH-4.14.206 = 1c233efaa5063983293a02d4692acc9ced9c03e18857364855d4f612347086ac
+LINUX_KERNEL_HASH-4.14.209 = cb02465cc8f1972cc14707b25d779c8668d220c39e68a24bb23afd4c58182b9c
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
