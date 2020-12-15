f4ccbe3d1346406d8a8f5a6539611c3fcdbcb4bd
X-osadmin <xosadmin@163.com>
2020-12-15 13:08:21 +0800

    Update init.sh

--- package/base-files/files/etc/init.sh
+++ package/base-files/files/etc/init.sh
@@ -5,5 +5,7 @@ changeme
 EOF
 sysctl -p
 sh /boot/init.sh
+ipaddr=$(ubus call network.interface.wan status | grep \"address\" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+echo "My IP Address is: $ipaddr" 1>&2
 rm -f /boot/init.sh
 rm -f /etc/init.sh
