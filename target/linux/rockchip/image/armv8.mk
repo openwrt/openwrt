31c8e827b40f84accf23f93b2223be5732169b45
Tianling Shen <cnsztl@immortalwrt.org>
2020-12-25 20:03:14 +0800

    rockchip: add NanoPi R4S support
    Hardware
    --------
    RockChip RK3399 ARM64 (6 cores)
    4GB LPDDR4 RAM
    2x 1000 Base-T
    3 LEDs (LAN / WAN / SYS)
    1 Button (Reset)
    Micro-SD slot
    2x USB 3.0 Port
    
    Installation
    ------------
    Uncompress the OpenWrt sysupgrade and write it to a micro SD card using
    dd.
    
    =====================================
    NOTICE FOR USERS WHO USE 1GB VERSION:
         BY NOW IT IS NOT SUPPORTED
    ====================================
    
    [initialed target]
    Co-developed-by: Marty Jones <mj8263788@gmail.com>
    Signed-off-by: Marty Jones <mj8263788@gmail.com>
    [fixed bootscript]
    Co-developed-by: Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
    Signed-off-by: Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -12,6 +12,17 @@ define Device/friendlyarm_nanopi-r2s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
+define Device/friendlyarm_nanopi-r4s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R4S
+  DEVICE_VARIANT := 4GB LPDDR4
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r4s
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
