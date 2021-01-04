431ec6e2f8552bef84157ba8fc2d91d1701bbed6
Sungbo Eo <mans0n@gorani.run>
2021-01-04 22:52:20 +0900

    kernel: drop empty kmod-ledtrig-* packages
    The following four led triggers are enabled in generic config.
    
    * kmod-ledtrig-default-on
    * kmod-ledtrig-heartbeat
    * kmod-ledtrig-netdev
    * kmod-ledtrig-timer
    
    Drop the packages and remove them from DEVICE_PACKAGES.
    There's no other package depending on them in this repo.
    
    Signed-off-by: Sungbo Eo <mans0n@gorani.run>

--- target/linux/mxs/profiles/01-duckbill.mk
+++ target/linux/mxs/profiles/01-duckbill.mk
@@ -10,7 +10,7 @@ define Profile/duckbill
   FEATURES+=usbgadget
   PACKAGES+= \
 	-dnsmasq -firewall -ppp -ip6tables -iptables -6relayd -mtd uboot-envtools \
-	kmod-leds-gpio kmod-ledtrig-timer -kmod-ipt-nathelper uboot-mxs-duckbill
+	kmod-leds-gpio -kmod-ipt-nathelper uboot-mxs-duckbill
 endef
 
 define Profile/duckbill/Description
