b2ccef2c0f5de117ff3ce79f42ad0d7a0179c0d9
John Audia <graysky@archlinux.us>
2021-01-06 12:37:16 -0500

    kernel: bump 5.4 to 5.4.87
    All modification by update_kernel.sh.
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, bcm27xx/bcm2711
    Run-tested: ipq806x/R7800
    
    No dmesg regressions, everything functional
    
    Compile-tested [*]: ath79/{generic,tiny}, ipq40xx, octeon,
                        ramips/mt7621, realtek, x86/64.
    Run-tested [*]:     ramips/mt7621 (DIR-878 A1, R6800, RT-AC57U),
                        octeon (EdgeRouter Lite).
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Tested-by: Stijn Segers <foss@volatilesystems.org> [*]

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .86
+LINUX_VERSION-5.4 = .87
 
-LINUX_KERNEL_HASH-5.4.86 = eb36b5fc6ef7b953acba0a3e62d872e0330c4d34b38d58f5714493a4fe3b0e8b
+LINUX_KERNEL_HASH-5.4.87 = 6a34e93e2e84bb645155124962307ad9d3646124f43838d087209ed4ea595c31
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
