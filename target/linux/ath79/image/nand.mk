7cb91a0b3bac775daa54368f9c5a5fe6bdd1fb7a
GL.iNet-Hongjian.Zhang <hongjian.zhang@gl-inet.com>
2021-01-06 14:13:07 +0800

    add support for usb150

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -7,6 +7,15 @@ define Device/glinet_gl-ar150
 endef
 TARGET_DEVICES += glinet_gl-ar150
 
+define Device/glinet_gl-usb150
+  ATH_SOC := qca9331
+  DEVICE_TITLE := GL.iNet GL-USB150
+  DEVICE_PACKAGES := kmod-usb2  block-mount
+  IMAGE_SIZE := 16000k
+  SUPPORTED_DEVICES += gl-usb150 glinet,gl-usb150
+endef
+TARGET_DEVICES += glinet_gl-usb150
+
 define Device/glinet_gl-mifi
   ATH_SOC := qca9331
   DEVICE_TITLE := GL.iNet GL-MIFI
