aa4ad4e2eb62240794e9092f80f473c186edda10
Marty Jones <mj8263788@gmail.com>
2021-01-11 18:35:29 -0500

    uboot-rockchip: update to v2021.01
     Update the U-Boot to version v2021.01
     Refresh patches for the NanoPi R2S
    
     Run-tested: FriendlyARM NanoPi R2S
                 Radxa Rock Pi 4
                 Pine64 RockPro64
    
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- package/boot/uboot-rockchip/src/of-platdata/nanopi-r2s-rk3328/dt-structs-gen.h
+++ package/boot/uboot-rockchip/src/of-platdata/nanopi-r2s-rk3328/dt-structs-gen.h
@@ -6,6 +6,18 @@
 
 #include <stdbool.h>
 #include <linux/libfdt.h>
+struct dtd_ns16550_serial {
+	fdt32_t		clock_frequency;
+	struct phandle_1_arg clocks[2];
+	const char *	dma_names[2];
+	fdt32_t		dmas[4];
+	fdt32_t		interrupts[3];
+	fdt32_t		pinctrl_0;
+	const char *	pinctrl_names;
+	fdt64_t		reg[2];
+	fdt32_t		reg_io_width;
+	fdt32_t		reg_shift;
+};
 struct dtd_regulator_fixed {
 	fdt32_t		gpio[3];
 	fdt32_t		pinctrl_0;
@@ -22,15 +34,7 @@ struct dtd_rockchip_gpio_bank {
 	fdt32_t		interrupts[3];
 	fdt64_t		reg[2];
 };
-struct dtd_rockchip_rk3328_cru {
-	fdt64_t		reg[2];
-	fdt32_t		rockchip_grf;
-};
-struct dtd_rockchip_rk3328_dmc {
-	fdt64_t		reg[12];
-	fdt32_t		rockchip_sdram_params[196];
-};
-struct dtd_rockchip_rk3328_dw_mshc {
+struct dtd_rockchip_rk3288_dw_mshc {
 	fdt32_t		bus_width;
 	bool		cap_mmc_highspeed;
 	bool		cap_sd_highspeed;
@@ -46,6 +50,14 @@ struct dtd_rockchip_rk3328_dw_mshc {
 	fdt32_t		vmmc_supply;
 	fdt32_t		vqmmc_supply;
 };
+struct dtd_rockchip_rk3328_cru {
+	fdt64_t		reg[2];
+	fdt32_t		rockchip_grf;
+};
+struct dtd_rockchip_rk3328_dmc {
+	fdt64_t		reg[12];
+	fdt32_t		rockchip_sdram_params[196];
+};
 struct dtd_rockchip_rk3328_grf {
 	fdt64_t		reg[2];
 };
@@ -53,20 +65,3 @@ struct dtd_rockchip_rk3328_pinctrl {
 	bool		ranges;
 	fdt32_t		rockchip_grf;
 };
-struct dtd_rockchip_rk3328_uart {
-	fdt32_t		clock_frequency;
-	struct phandle_1_arg clocks[2];
-	const char *	dma_names[2];
-	fdt32_t		dmas[4];
-	fdt32_t		interrupts[3];
-	fdt32_t		pinctrl_0;
-	const char *	pinctrl_names;
-	fdt64_t		reg[2];
-	fdt32_t		reg_io_width;
-	fdt32_t		reg_shift;
-};
-#define dtd_syscon dtd_rockchip_rk3328_cru
-#define dtd_simple_mfd dtd_rockchip_rk3328_grf
-#define dtd_snps_dw_apb_uart dtd_rockchip_rk3328_uart
-#define dtd_rockchip_cru dtd_rockchip_rk3328_cru
-#define dtd_rockchip_rk3288_dw_mshc dtd_rockchip_rk3328_dw_mshc
