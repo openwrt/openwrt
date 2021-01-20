78e67291b9fac00245be3044b7a7235975a1ffef
Bjørn Mork <bjorn@mork.no>
2021-01-20 18:36:49 +0100

    kernel: mtdsplit_uimage: replace "allnet, uimage" parser
    Convert users to the generic "openwrt,uimage" using device specific
    "openwrt,ih-magic" properties, and remove "allnet,uimage".
    
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_uimage.c
+++ target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_uimage.c
@@ -333,54 +333,6 @@ static struct mtd_part_parser uimage_netgear_parser = {
 
 };
 
-
-/**************************************************
- * ALLNET
- **************************************************/
-
-#define FW_MAGIC_SG8208M	0x00000006
-#define FW_MAGIC_SG8310PM	0x83000006
-
-static ssize_t uimage_verify_allnet(u_char *buf, size_t len, u32 ih_magic)
-{
-	struct uimage_header *header = (struct uimage_header *)buf;
-
-	switch (be32_to_cpu(header->ih_magic)) {
-	case FW_MAGIC_SG8208M:
-	case FW_MAGIC_SG8310PM:
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	if (header->ih_os != IH_OS_LINUX)
-		return -EINVAL;
-
-	return 0;
-}
-
-static int
-mtdsplit_uimage_parse_allnet(struct mtd_info *master,
-			      const struct mtd_partition **pparts,
-			      struct mtd_part_parser_data *data)
-{
-	return __mtdsplit_parse_uimage(master, pparts, data,
-				      uimage_verify_allnet);
-}
-
-static const struct of_device_id mtdsplit_uimage_allnet_of_match_table[] = {
-	{ .compatible = "allnet,uimage" },
-	{},
-};
-
-static struct mtd_part_parser uimage_allnet_parser = {
-	.owner = THIS_MODULE,
-	.name = "allnet-fw",
-	.of_match_table = mtdsplit_uimage_allnet_of_match_table,
-	.parse_fn = mtdsplit_uimage_parse_allnet,
-};
-
-
 /**************************************************
  * Edimax
  **************************************************/
@@ -438,7 +390,6 @@ static int __init mtdsplit_uimage_init(void)
 {
 	register_mtd_parser(&uimage_generic_parser);
 	register_mtd_parser(&uimage_netgear_parser);
-	register_mtd_parser(&uimage_allnet_parser);
 	register_mtd_parser(&uimage_edimax_parser);
 
 	return 0;
