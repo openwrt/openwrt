5901f66b0faf9ede106432fdae1346c491f48d5e
John Audia <graysky@archlinux.us>
2021-01-23 11:09:03 -0500

    kernel: bump 5.4 to 5.4.92
    All modification made by update_kernel.sh in a fresh clone without
    existing toolchains.
    
    Build system:       x86_64
    Build-tested:       ipq806x/R7800, bcm27xx/bcm2711
    Run-tested:         ipq806x/R7800
    Compile-tested [*]: ath79/{tiny,generic}, ipq40xx, octeon,
                        ramips/mt7621, realtek, x86/64.
    Run-tested [*]:     ath79/generic, octeon, ramips/mt7621, realtek.
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Tested-by: Stijn Segers <foss@volatilesystems.org> [*]

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .91
+LINUX_VERSION-5.4 = .92
 
-LINUX_KERNEL_HASH-5.4.91 = 0e0161bb034b9ba59e58a20985e49ecfb38104586602f53f37b382f508fc5c17
+LINUX_KERNEL_HASH-5.4.92 = c0937ff98824c4b14cfea68a04340e0beb3c00f1cc02984daf2f3bdf542394fd
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
