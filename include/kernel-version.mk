d6997f0850f392b825293817d3755fe4386e5b9e
John Audia <graysky@archlinux.us>
2021-01-27 09:41:12 -0500

    kernel: bump 5.4 to 5.4.93
    All modification made by update_kernel.sh in a fresh clone without
    existing toolchains.
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, bcm27xx/bcm2711
    Run-tested: ipq806x/R7800
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .92
+LINUX_VERSION-5.4 = .93
 
-LINUX_KERNEL_HASH-5.4.92 = c0937ff98824c4b14cfea68a04340e0beb3c00f1cc02984daf2f3bdf542394fd
+LINUX_KERNEL_HASH-5.4.93 = d37449403664cc3b1bac96d0d9a199dbe619885cd899c0ae3108843f42e3d522
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
