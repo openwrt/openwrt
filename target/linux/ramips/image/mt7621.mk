1c37751de194760a452f4a4ba78d0f99d8266fdd
CYJason2000 <42707013+CYJason2000@users.noreply.github.com>
2021-01-28 10:53:21 +0800

    ramips: add support for HiLink HLK-7621A
    Specifications:
    SoC: MediaTek MT7621AT
    RAM: 256 MB (DDR3)
    Flash: 32 MB SPI NOR 44MHz
    Switch: 1 WAN, 4 LAN (Gigabit)
    LEDs: 1 WAN, 4 LAN (controlled by PHY)
    
    UART Serial:
    UART1 as console : 57600 baud
    
    Installation:
    Update firmware by internal GNUBEE uboot: https://github.com/gnubee-git/GnuBee-MT7621-uboot
    By HTTP: Initial uboot address is http://10.10.10.123, your address needs to be 10.10.10.x, and mask 255.255.255.0.
    By TFTP: Uboot is in client mode, the address of the firmware must be tftp://10.10.10.3/uboot.bin
    
    Recovery:
    Manufacturer provides MTK OpenWrt 14.07 source code, compile then flash it by uboot.
    HLK-7621A is a stamp hole package module for embedded development, users have to design IO boards to use it. So only basic IOs are defined in the DTS file.
    
    Signed-off-by: Chen Yijun cyjason@bupt.edu.cn

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -529,6 +529,14 @@ define Device/gnubee_gb-pc2
 endef
 TARGET_DEVICES += gnubee_gb-pc2
 
+define Device/hilink_hlk-7621a
+  $(Device/dsa-migration)
+  IMAGE_SIZE := 32448k
+  DEVICE_VENDOR := HiLink
+  DEVICE_MODEL := HLK-7621A
+endef
+TARGET_DEVICES += hilink_hlk-7621a
+
 define Device/hiwifi_hc5962
   $(Device/dsa-migration)
   BLOCKSIZE := 128k
