b430f5f6d6930ce4ffcdebd9b1faf02dc649cefe
Florian Eckert <fe@dev.tdt.de>
2021-02-03 10:25:38 +0100

    base-files: remove unused vn and _vn function
    The remaining vn calls have been ported to v.
    Therefore, these functions are no longer needed and will be removed.
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -65,17 +65,9 @@ _v() {
 	[ -n "$VERBOSE" ] && [ "$VERBOSE" -ge 1 ] && echo "$*" >&2
 }
 
-_vn() {
-	[ -n "$VERBOSE" ] && [ "$VERBOSE" -ge 1 ] && echo -n "$*" >&2
-}
-
 v() {
 	_v "$(date) upgrade: $@"
 	logger -p info -t upgrade "$@"
-}
-
-vn() {
-	_vn "$(date) upgrade: $@"
 }
 
 json_string() {
