a544c9760779a6d30366339aa71970035de5faf5
John Audia <graysky@archlinux.us>
2021-02-07 12:36:29 -0500

    kernel: bump 5.4 to 5.4.96
    Ran update_kernel.sh in a fresh clone without any existing toolchains.
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, bcm27xx/bcm2711
    Run-tested: ipq806x/R7800
    Run-tested [*]: ramips/mt7621 (R6800, DIR-878 A1, EAP235-Wall)
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Tested-by: Stijn Segers <foss@volatilesystems.org> [*]

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .95
+LINUX_VERSION-5.4 = .96
 
-LINUX_KERNEL_HASH-5.4.95 = 030ae544f346bfa2ce619dd9e17e93d10ec393632d3b6d6cf5d1fc84b914d449
+LINUX_KERNEL_HASH-5.4.96 = f728de695ec5eb17efa15acaecc48fcd7a6c4a912b51704ed137cccf93f9f5e0
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
