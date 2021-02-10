037f4f34f2c4a3dc6162aae3eaa628c3c38cbfa6
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2021-02-10 14:52:34 +0100

    target: use SPDX license identifiers on Makefiles
    Use SPDX license tags to allow machines to check licenses.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- target/linux/layerscape/image/armv8_64b.mk
+++ target/linux/layerscape/image/armv8_64b.mk
@@ -1,9 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
 #
 # Copyright 2018-2020 NXP
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
-#
 
 define Device/Default
   PROFILES := Default
