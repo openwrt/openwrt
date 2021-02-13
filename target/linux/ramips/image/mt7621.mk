43690fb5f3f3e376b5e87ab0b854984bb924c7e7
Stephen Sun <js2597@cam.ac.uk>
2021-02-13 13:46:16 +0800

    ramips: fix image size for hc5962
    This commit corrects an error in a previous commit on the
    master branch [1] where the author forgot to change IMAGE_SIZE
    accordingly after modifying the partition table and increasing
    the kernel size to 4096k in preparation for a kernel upgrade.
    
    Referring to the partitions in dts, this parameter indicates the size of the concatenated virtual ubi partition.
    
    [1] https://github.com/openwrt/openwrt/commit/d74fb0088c5bc89ba080816921699a980966d015
    
    Signed-off-by: Stephen Sun js2597@cam.ac.uk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -559,7 +559,7 @@ define Device/hiwifi_hc5962
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
   UBINIZE_OPTS := -E 5
-  IMAGE_SIZE := 32768k
+  IMAGE_SIZE := 124672k
   IMAGES += factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
