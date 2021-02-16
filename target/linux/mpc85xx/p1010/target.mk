5bd6d9377d625f6445caf75bee2ecb16fd45971d
David Bauer <mail@david-bauer.net>
2021-02-16 22:05:13 +0100

    mpc85xx-p1010: add Kernel 5.10 support
    Tested on: Sophos RED 15W
    
    The TP-Link WL-WDR4900 needs to be disabled when 5.10 becomes the
    default kernel.
    
    When building with all kmods enabled, the resulting kernel image
    exceeds the maximum size the bootloader reads from the flash.
    
    For more information, see GitHub issue #1773
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/mpc85xx/p1010/target.mk
+++ target/linux/mpc85xx/p1010/target.mk
@@ -2,6 +2,8 @@ BOARDNAME:=P1010
 FEATURES+=nand
 KERNELNAME:=simpleImage.tl-wdr4900-v1
 
+KERNEL_TESTING_PATCHVER:=5.10
+
 define Target/Description
 	Build firmware images for P1010 based boards.
 endef
