8c7a61f9a1555a62b7eebc7610be101525a740a4
Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
2021-02-19 08:21:24 -0800

    kernel: 5.10: fix kmod build failures
    Modify existing modules to reflect their new location in Linux 5.10. Add
    missing dependenices.
    
    Signed-off-by: Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
    [enable CRYPTO_USER_API_ENABLE_OBSOLETE; add kmod-crypto-hash dependency
    to usb-net-rtl8152]
    Signed-off-by: David Bauer <mail@david-bauer.net>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1126,7 +1126,9 @@ define KernelPackage/keys-trusted
   TITLE:=TPM trusted keys on kernel keyring
   DEPENDS:=@KERNEL_KEYS +kmod-crypto-hash +kmod-crypto-hmac +kmod-crypto-sha1 +kmod-tpm
   KCONFIG:=CONFIG_TRUSTED_KEYS
-  FILES:=$(LINUX_DIR)/security/keys/trusted.ko
+  FILES:= \
+	  $(LINUX_DIR)/security/keys/trusted.ko@lt5.10 \
+	  $(LINUX_DIR)/security/keys/trusted-keys/trusted.ko@ge5.10
   AUTOLOAD:=$(call AutoLoad,01,trusted-keys,1)
 endef
 
