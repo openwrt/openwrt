0e22b05276e779eab634a514bce332c762babda0
David Bauer <mail@david-bauer.net>
2021-02-20 21:17:26 +0100

    download: add mirror alias for Debian
    Add an alias for Debian packages and download them from the Debian
    mirror redirector.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- include/download.mk
+++ include/download.mk
@@ -27,7 +27,7 @@ define dl_method
 $(strip \
   $(if $(filter git,$(2)),$(call dl_method_git,$(1),$(2)),
     $(if $(2),$(2), \
-      $(if $(filter @OPENWRT @APACHE/% @GITHUB/% @GNOME/% @GNU/% @KERNEL/% @SF/% @SAVANNAH/% ftp://% http://% https://% file://%,$(1)),default, \
+      $(if $(filter @OPENWRT @APACHE/% @DEBIAN/% @GITHUB/% @GNOME/% @GNU/% @KERNEL/% @SF/% @SAVANNAH/% ftp://% http://% https://% file://%,$(1)),default, \
         $(if $(filter git://%,$(1)),$(call dl_method_git,$(1),$(2)), \
           $(if $(filter svn://%,$(1)),svn, \
             $(if $(filter cvs://%,$(1)),cvs, \
