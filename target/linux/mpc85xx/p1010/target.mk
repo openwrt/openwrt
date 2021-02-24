a22a941cd1f64987c9d39922b1b70c5d5597b226
Pawel Dembicki <paweldembicki@gmail.com>
2021-02-24 14:30:54 +0100

    mpc85xx: add testing kernel 5.10
    Subtargets p1010 and p1020 have already added.
    
    Compile and run tested on: P2020RDB
    
    Signed-off-by: Pawel Dembicki <paweldembicki@gmail.com>

--- target/linux/mpc85xx/p1010/target.mk
+++ target/linux/mpc85xx/p1010/target.mk
@@ -2,8 +2,6 @@ BOARDNAME:=P1010
 FEATURES+=nand
 KERNELNAME:=simpleImage.tl-wdr4900-v1
 
-KERNEL_TESTING_PATCHVER:=5.10
-
 define Target/Description
 	Build firmware images for P1010 based boards.
 endef
