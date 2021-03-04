2e58e62a25b72d46515f95eb04ab5fabe1d08288
Robert Marko <robert.marko@sartura.hr>
2021-03-04 12:37:22 +0100

    build: use config-1 instead of config@1 as default
    U-boot will reject all nodes with @ since commit:
    https://gitlab.denx.de/u-boot/u-boot/-/commit/79af75f7776fc20b0d7eb6afe1e27c00fdb4b9b4
    
    This will cause the OpenWrt images to fail booting,
    to rectify use the config-1 as default.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- include/image.mk
+++ include/image.mk
@@ -139,7 +139,7 @@ endef
 define Image/BuildKernel/MkFIT
 	$(TOPDIR)/scripts/mkits.sh \
 		-D $(1) -o $(KDIR)/fit-$(1).its -k $(2) $(if $(3),-d $(3)) -C $(4) -a $(5) -e $(6) \
-		-c $(if $(DEVICE_DTS_CONFIG),$(DEVICE_DTS_CONFIG),"config@1") \
+		-c $(if $(DEVICE_DTS_CONFIG),$(DEVICE_DTS_CONFIG),"config-1") \
 		-A $(LINUX_KARCH) -v $(LINUX_VERSION)
 	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $(KDIR)/fit-$(1).its $(KDIR)/fit-$(1)$(7).itb
 endef
