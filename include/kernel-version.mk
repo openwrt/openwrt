1cd098784eeb0e3ba0d05cd3e71ebf1a106138e9
Kabuli Chana <newtownBuild@gmail.com>
2021-03-09 19:19:12 -0700

    kernel: bump 5.10 to 5.10.23
    update kernel to 5.10.23, rebase patches, deleted upstreamed patch:
    
    target/linux/generic/backport-5.10/830-v5.12-0001-net-usb-qmi_wwan-support-ZTE-P685M-modem.patch
    
    compile / test - mvebu / mamba, rango
    
    Signed-off-by: Kabuli Chana <newtownBuild@gmail.com>
    [refresh again]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .105
-LINUX_VERSION-5.10 = .20
+LINUX_VERSION-5.10 = .23
 
 LINUX_KERNEL_HASH-5.4.105 = 244e4cd16184285df55ec5a9501daba011aa8b85c5527ee05eab4592e70fb8b6
-LINUX_KERNEL_HASH-5.10.20 = 9be37146feba42be05137cf900a7d9012990b5a1d5e59bc0c8da1f86952930a3
+LINUX_KERNEL_HASH-5.10.23 = f6e21c03ec6ff85b26d77c59fdab81e64707792a57593643307df192749edb6a
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
