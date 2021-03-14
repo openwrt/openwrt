f82e7e96a00e964649c809cc7a57578121ad998d
Hauke Mehrtens <hauke@hauke-m.de>
2021-03-14 15:51:56 +0100

    kernel: bump 5.4 to 5.4.105
    Refreshed all patches.
    
    The following patches were applied upstream:
    * 755-v5.8-net-dsa-add-GRO-support-via-gro_cells.patch
    * 831-v5.9-usbip-tools-fix-build-error-for-multiple-definition.patch
    
    Compile-tested on: x86_64, ipq40xx, ath79
    Runtime-tested on: x86_64, ipq40xx, ath79
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .102
+LINUX_VERSION-5.4 = .105
 
-LINUX_KERNEL_HASH-5.4.102 = fd697ce1c3f6024d4ae77d4eb5a1552199407b60cb8e90bc621e23cbce639aed
+LINUX_KERNEL_HASH-5.4.105 = 244e4cd16184285df55ec5a9501daba011aa8b85c5527ee05eab4592e70fb8b6
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
