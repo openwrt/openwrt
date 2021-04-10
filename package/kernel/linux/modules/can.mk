dc61c32fdc5ec2c47a3857a17a6fb2dbd43c711a
Hauke Mehrtens <hauke@hauke-m.de>
2021-04-10 23:06:11 +0200

    kernel: Adapt renamed can-dev.ko
    The can-dev.ko kernel module was moved in kernel 5.4.110 and 5.10.28.
    
    Fixes: a1311df95587 ("kernel: bump 5.10 to 5.10.28")
    Fixes: b0a34e886da1 ("kernel: bump 5.4 to 5.4.110")
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/can.mk
+++ package/kernel/linux/modules/can.mk
@@ -28,7 +28,7 @@ define KernelPackage/can
 	CONFIG_CAN_SOFTING=n \
 	CONFIG_NET_EMATCH_CANID=n \
 	CONFIG_CAN_DEBUG_DEVICES=n
-  FILES:=$(LINUX_DIR)/drivers/net/can/can-dev.ko \
+  FILES:=$(LINUX_DIR)/drivers/net/can/dev/can-dev.ko \
 	 $(LINUX_DIR)/net/can/can.ko
   AUTOLOAD:=$(call AutoProbe,can can-dev)
 endef
