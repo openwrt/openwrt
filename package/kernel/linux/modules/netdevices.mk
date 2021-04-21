5a6fbdbb96dd849986a7dcbc92fb89f4dcb26b5c
Chen Minqiang <ptpt52@gmail.com>
2021-04-21 13:27:42 +0800

    kernel: add kmod-ipvlan support
    This kmod is similar to macvlan with the difference being that the
    endpoints have the same mac address.
    
    It is useful on cloud where only one mac address allowed on port,
    where macvlan not works but ipvlan would.
    
    One use case is where multiple IPs and gateways assign on one net port
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -885,6 +885,22 @@ endef
 $(eval $(call KernelPackage,macvlan))
 
 
+define KernelPackage/ipvlan
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=IP-VLAN support
+  KCONFIG:=CONFIG_IPVLAN
+  FILES:=$(LINUX_DIR)/drivers/net/ipvlan/ipvlan.ko
+  AUTOLOAD:=$(call AutoProbe,ipvlan)
+endef
+
+define KernelPackage/ipvlan/description
+ A kernel module which allows one to create virtual interfaces that
+ map packets to or from specific IP addresses to a particular interface
+endef
+
+$(eval $(call KernelPackage,ipvlan))
+
+
 define KernelPackage/tulip
   TITLE:=Tulip family network device support
   DEPENDS:=@PCI_SUPPORT +kmod-mii
