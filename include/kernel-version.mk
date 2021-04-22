4398a3506786715f3b8532714a840d2a212c0fac
Koen Vandeputte <koen.vandeputte@ncentric.com>
2021-04-22 18:48:08 +0200

    kernel: bump 4.14 to 4.14.231
    Refreshed all patches.
    
    Fixes:
    - CVE-2020-25672
    - CVE-2020-25671
    - CVE-2020-25670
    
    Compile-tested on: ar71xx, cns3xxx, imx6, x86_64
    Runtime-tested on: ar71xx, cns3xxx, imx6
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .230
+LINUX_VERSION-4.14 = .231
 
-LINUX_KERNEL_HASH-4.14.230 = 9c95f354db7d35c8b8767844d0f5bf2fc615cb235a2cb0add56e11ad8366c5be
+LINUX_KERNEL_HASH-4.14.231 = 573b79adee3490a8dd82f663bb187218288c99cd2613fd9f69edf915f91e6682
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
