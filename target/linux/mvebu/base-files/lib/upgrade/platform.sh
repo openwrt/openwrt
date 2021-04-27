7beffa498714f68b23e4ead6a38da91ec9117734
Li Zhang <li.zhang@gl-inet.com>
2021-04-27 15:24:52 +0800

    fix: Support GL.iNet GL-MV1000 EMMC upgrade with sysupgrade

--- target/linux/mvebu/base-files/lib/upgrade/platform.sh
+++ target/linux/mvebu/base-files/lib/upgrade/platform.sh
@@ -14,6 +14,9 @@ platform_check_image() {
 	marvell,armada8040-mcbin|solidrun,clearfog-base-a1|solidrun,clearfog-pro-a1)
 		platform_check_image_sdcard "$ARGV"
 		;;
+	gl-mv1000)
+		platform_check_image_sdcard "$1"
+		;;
 	*)
 		return 0
 		;;
