04579a527a07b9d3fc543d84828b607bed1b3732
Hauke Mehrtens <hauke@hauke-m.de>
2021-05-07 22:49:56 +0200

    kernel: bump 5.4 to 5.4.117
    All updated automatically.
    
    Compile-tested on: x86/64, armvirt/32, ath79/generic
    Runtime-tested on: x86/64, armvirt/32, ath79/generic
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .114
+LINUX_VERSION-5.4 = .117
 
-LINUX_KERNEL_HASH-5.4.114 = be98f087cb58f23bc5edb8ee80a4794a569d48049d75b1a8eaef9f80fede9557
+LINUX_KERNEL_HASH-5.4.117 = 4e989b5775830092e5c76b5cca65ebff862ad0c87d0b58c3a20d415c3d4ec770
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
