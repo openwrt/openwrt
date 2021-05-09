c782412537fee962f04a90aa1353b03200e1904a
Leonardo Mörlein <me@irrelefant.net>
2021-05-09 22:28:15 +0200

    build: introduce $(MKHASH)
    Before this commit, it was assumed that mkhash is in the PATH. While
    this was fine for the normal build workflow, this led to some issues if
    
        make TOPDIR="$(pwd)" -C "$pkgdir" compile
    
    was called manually. In most of the cases, I just saw warnings like this:
    
        make: Entering directory '/home/.../package/gluon-status-page'
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        bash: line 1: mkhash: command not found
        [...]
    
    While these were only warnings and the package still compiled sucessfully,
    I also observed that some package even fail to build because of this.
    
    After applying this commit, the variable $(MKHASH) is introduced. This
    variable points to $(STAGING_DIR_HOST)/bin/mkhash, which is always the
    correct path.
    
    Signed-off-by: Leonardo Mörlein <me@irrelefant.net>

--- include/depends.mk
+++ include/depends.mk
@@ -11,7 +11,7 @@
 
 DEP_FINDPARAMS := -x "*/.svn*" -x ".*" -x "*:*" -x "*\!*" -x "* *" -x "*\\\#*" -x "*/.*_check" -x "*/.*.swp" -x "*/.pkgdir*"
 
-find_md5=find $(wildcard $(1)) -type f $(patsubst -x,-and -not -path,$(DEP_FINDPARAMS) $(2)) -printf "%p%T@\n" | sort | mkhash md5
+find_md5=find $(wildcard $(1)) -type f $(patsubst -x,-and -not -path,$(DEP_FINDPARAMS) $(2)) -printf "%p%T@\n" | sort | $(MKHASH) md5
 
 define rdep
   .PRECIOUS: $(2)
