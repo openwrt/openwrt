d5b1140f8e916bcefad803f7a7128615523eb447
kestrel1974 <kestrel1974@github.com>
2021-05-12 11:10:40 +0200

    Add renesas dependency for 5490 - 5490 is using the renesas usb too, it was missing in the usb kernel modules definition - now added

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1663,6 +1663,7 @@ define KernelPackage/usb3
 	+TARGET_ramips_mt7621:kmod-usb-xhci-mtk \
 	+(TARGET_apm821xx_nand&&LINUX_5_10):kmod-usb-xhci-pci-renesas \
 	+(TARGET_lantiq_xrx200_DEVICE_avm_fritz3490):kmod-usb-xhci-pci-renesas \
+	+(TARGET_lantiq_xrx200_DEVICE_avm_fritz5490):kmod-usb-xhci-pci-renesas \
 	+(TARGET_lantiq_xrx200_DEVICE_avm_fritz7490):kmod-usb-xhci-pci-renesas
   KCONFIG:= \
 	CONFIG_USB_PCI=y \
