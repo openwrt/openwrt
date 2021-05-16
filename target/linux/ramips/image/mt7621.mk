9fa5b3afc90b1dc2a69f90897f39a4e593b42b11
Chukun Pan <amadeus@jmu.edu.cn>
2021-05-16 20:10:05 +0800

    ramips: add support for JCG Q20
    JCG Q20 is an AX 1800M router.
    
    Hardware specs:
      SoC: MediaTek MT7621AT
      Flash: Winbond W29N01HV 128 MiB
      RAM: Winbond W632GU6NB-11 256 MiB
      WiFi: MT7915 2.4/5 GHz 2T2R
      Ethernet: 10/100/1000 Mbps x3
      LED: Status (red / blue)
      Button: Reset, WPS
      Power: DC 12V,1A
    
    Flash instructions:
      Upload factory.bin in stock firmware's upgrade page,
      do not preserve settings.
    
    MAC addresses map:
      0x00004 *:3e wlan2g/wlan5g
      0x3fff4 *:3c lan/label
      0x3fffa *:3c wan
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
    (cherry picked from commit 57cb387cfeee2a07902a2f1383ca471ef47265f3)

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -720,6 +720,23 @@ define Device/jcg_jhr-ac876m
 endef
 TARGET_DEVICES += jcg_jhr-ac876m
 
+define Device/jcg_q20
+  $(Device/dsa-migration)
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBINIZE_OPTS := -E 5
+  KERNEL_SIZE := 4096k
+  IMAGE_SIZE := 91136k
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
+	check-size
+  DEVICE_VENDOR := JCG
+  DEVICE_MODEL := Q20
+  DEVICE_PACKAGES := kmod-mt7915e uboot-envtools
+endef
+TARGET_DEVICES += jcg_q20
+
 define Device/jcg_y2
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
