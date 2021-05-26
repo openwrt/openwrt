390709688360ff450234d1fc881c85f6aa2af0ee
Rui Salvaterra <rsalvaterra@gmail.com>
2021-05-26 12:58:06 +0100

    kernel: bump 5.10 to 5.10.40
    Automatically refreshed:
    generic/hack-5.10/902-debloat_proc.patch
    generic/pending-5.10/110-perf-jevents-fix-getting-maximum-number-of-fds.patch
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .121
-LINUX_VERSION-5.10 = .39
+LINUX_VERSION-5.10 = .40
 
 LINUX_KERNEL_HASH-5.4.121 = 9d9327fca397e114bcc59d69fa5ae1ac8bac76b170ed811d1b1645df7456375f
-LINUX_KERNEL_HASH-5.10.39 = 5738a515ca97853481767360c568eae46c8d777d98a69e018a3299baa6b3f614
+LINUX_KERNEL_HASH-5.10.40 = 7480803acd7152b1e8248954e219ca9d8d95afa73ec2d8973170939ac44b2f24
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
