a395657b7f23131240c948812ade7b54d994cf6d
Michael Pratt <mcpratt@pm.me>
2021-06-11 14:54:43 -0400

    ramips: mt7620: remove useless GMAC nodes
    These nodes are used for configuring a GMAC interface
    and for defining external PHYs to be accessed with MDIO.
    
    None of this is possible on MT7620N, only MT7620A,
    so remove them from all MT7620N DTS.
    
    When the mdio-bus node is missing, the driver returns -NODEV
    which causes the internal switch to not initialize.
    Replace that return so that everything works without the DTS node.
    
    Also, an extra kernel message to indicate for all error conditions
    that mdio-bus is disabled.
    
    Fixes: d482356322c9 ("ramips: mt7620n: add mdio node and disable port4 by default")
    Fixes: aa5014dd1a58 ("ramips: mt7620n: enable port 4 as EPHY by default")
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- target/linux/ramips/files/drivers/net/ethernet/ralink/mdio.c
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/mdio.c
@@ -232,7 +232,8 @@ int fe_mdio_init(struct fe_priv *priv)
 	mii_np = of_get_child_by_name(priv->dev->of_node, "mdio-bus");
 	if (!mii_np) {
 		dev_err(priv->dev, "no %s child node found", "mdio-bus");
-		return -ENODEV;
+		err = 0;
+		goto err_no_bus;
 	}
 
 	if (!of_device_is_available(mii_np)) {
@@ -264,6 +265,8 @@ err_free_bus:
 	kfree(priv->mii_bus);
 err_put_node:
 	of_node_put(mii_np);
+err_no_bus:
+	dev_err(priv->dev, "%s disabled", "mdio-bus");
 	priv->mii_bus = NULL;
 	return err;
 }
