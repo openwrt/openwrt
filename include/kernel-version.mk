85cef1cf221a4786fb8b1531748a5a0790101a3a
Hauke Mehrtens <hauke@hauke-m.de>
2021-06-26 21:08:19 +0200

    kernel: bump 5.4 to 5.4.128
    Manually rebased
      bcm27xx/patches-5.4/950-0089-cgroup-Disable-cgroup-memory-by-default.patch
        Replaced with patch from Raspberry Pi kernel 5.10 patches
      mvebu/patches-5.4/002-PCI-aardvark-Don-t-rely-on-jiffies-while-holding-spi.patch
        Applied upstream
    
    All others updated automatically.
    
    Compile-tested on: ath79/generic, lantiq/xrx200, brcm27xx
    Runtime-tested on: ath79/generic, lantiq/xrx200
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .124
+LINUX_VERSION-5.4 = .128
 
-LINUX_KERNEL_HASH-5.4.124 = f7f29dda2b042d7b5986d18274413131cf70e17288c05e9a683df1f46c507d82
+LINUX_KERNEL_HASH-5.4.128 = 3b54aebb816b9e628cb9ba3055a6aca58ce0ddeec49366c0da86ced9a7be39ab
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
