bce7777222887ac0356b2a2311dd16486cc2cc05
Daniel Golle <daniel@makrotopia.org>
2021-07-12 18:13:37 +0100

    mediatek: mt7623: build with Linux 5.10
    Update Kernel config and set Linux 5.10 for mediatek/mt7623.
    (patches have already been updated to 5.10 when mt7622 was bumped)
    Tested on Bananapi BPi-R2.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/mt7623/target.mk
+++ target/linux/mediatek/mt7623/target.mk
@@ -9,6 +9,8 @@ CPU_TYPE:=cortex-a7
 CPU_SUBTYPE:=neon-vfpv4
 KERNELNAME:=Image dtbs zImage
 FEATURES+=ext4 usb
+KERNEL_PATCHVER:=5.10
+DEFAULT_PACKAGES+=blockdev uboot-envtools
 
 define Target/Description
 	Build firmware images for MediaTek mt7623 ARM based boards.
