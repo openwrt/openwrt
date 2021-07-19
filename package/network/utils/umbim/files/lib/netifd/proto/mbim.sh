c84bf89b3a8f6c93ad30a906b514edcf0077bb51
Lech Perczak <lech.perczak@gmail.com>
2021-07-19 19:15:53 +0200

    umbim: inherit "peerdns" option from parent interface
    MBIM protocol handler should intherit "peerdns" options from parent
    interface on sub-interfaces, otherwise upstream DNS servers are applied
    regardless of configuration.
    
    Signed-off-by: Lech Perczak <lech.perczak@gmail.com>

--- package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
+++ package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
@@ -187,7 +187,7 @@ _proto_mbim_setup() {
 			json_close_array
 			json_add_string gateway "$ipv4gateway"
 			json_add_array dns
-			json_add_string "" "$ipv4dnsserver"
+			[ "$peerdns" = 0 ] || json_add_string "" "$ipv4dnsserver"
 			json_close_array
 			proto_add_dynamic_defaults
 			json_close_object
@@ -204,7 +204,7 @@ _proto_mbim_setup() {
 			json_close_array
 			json_add_string ip6gw "$ipv6gateway"
 			json_add_array dns
-			json_add_string "" "$ipv6dnsserver"
+			[ "$peerdns" = 0 ] || json_add_string "" "$ipv6dnsserver"
 			json_close_array
 			proto_add_dynamic_defaults
 			json_close_object
