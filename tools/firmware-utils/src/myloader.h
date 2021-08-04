5e5e72b6b8d1517180265016827ec8293a31b55a
Rafał Miłecki <rafal@milecki.pl>
2021-08-04 13:06:24 +0200

    firmware-utils: replace GPL 2.0+ boilerplate/reference with SPDX
    This uses "GPL-2.0-or-later" header for files identified using scancode
    license scanner with 100% score as GPL 2.0 or later.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

--- tools/firmware-utils/src/myloader.h
+++ tools/firmware-utils/src/myloader.h
@@ -1,11 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *  Copyright (C) 2006-2008 Gabor Juhos <juhosg@openwrt.org>
- *
- *  This program is free software; you can redistribute  it and/or modify it
- *  under  the terms of  the GNU General  Public License as published by the
- *  Free Software Foundation;  either version 2 of the  License, or (at your
- *  option) any later version.
- *
  */
 
 #ifndef _MYLOADER_H_
