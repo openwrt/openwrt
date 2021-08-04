7efe10421498bc3f485d214dabac25226d545f4d
Rafał Miłecki <rafal@milecki.pl>
2021-08-04 12:30:38 +0200

    firmware-utils: replace GPL 2.0 boilerplate/reference with SPDX
    This uses "GPL-2.0-only" header for files identified using scancode
    license scanner with 100% score as GPL 2.0.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Stijn Tintel <stijn@linux-ipv6.be>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- tools/firmware-utils/src/pc1crypt.c
+++ tools/firmware-utils/src/pc1crypt.c
@@ -1,9 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *  Copyright (C) 2009 Gabor Juhos <juhosg@openwrt.org>
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 2 as published
- *  by the Free Software Foundation.
  *
  *  This code was based on:
  *	PC1 Cipher Algorithm ( Pukall Cipher 1 )
