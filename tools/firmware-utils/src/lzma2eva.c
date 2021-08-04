fac5278c749ad53ad6423bb68d20bc0e87406c5d
Rafał Miłecki <rafal@milecki.pl>
2021-08-04 13:06:24 +0200

    firmware-utils: replace GPL 2.0+ boilerplate/reference with SPDX
    This uses "GPL-2.0-or-later" header for files identified using scancode
    license scanner with 100% score as GPL 2.0 or later.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- tools/firmware-utils/src/lzma2eva.c
+++ tools/firmware-utils/src/lzma2eva.c
@@ -1,20 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
     lzma2eva - convert lzma-compressed file to AVM EVA bootloader format
     Copyright (C) 2007  Enrik Berkhan <Enrik.Berkhan@inka.de>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 */
 
 #include <stdint.h>
--- tools/firmware-utils/src/mkcsysimg.c
+++ tools/firmware-utils/src/mkcsysimg.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *
  *  Copyright (C) 2007-2009 Gabor Juhos <juhosg@openwrt.org>
@@ -5,21 +6,6 @@
  *  This program was based on the code found in various Linux
  *  source tarballs released by Edimax for it's devices.
  *  Original author: David Hsu <davidhsu@realtek.com.tw>
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the
- *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA  02110-1301, USA.
  */
 
 #include <stdio.h>
