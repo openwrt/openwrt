3a58cc4f3f03f2559cdd46af06538d990cbbc958
Rafał Miłecki <rafal@milecki.pl>
2021-08-04 12:30:38 +0200

    firmware-utils: replace GPL 2.0 boilerplate/reference with SPDX
    This uses "GPL-2.0-only" header for files identified using scancode
    license scanner with 100% score as GPL 2.0.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Stijn Tintel <stijn@linux-ipv6.be>

--- tools/firmware-utils/src/zynos.h
+++ tools/firmware-utils/src/zynos.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *
  *  Copyright (C) 2007-2008 OpenWrt.org
@@ -6,11 +7,6 @@
  *  This code was based on the information of the ZyXEL's firmware
  *  image format written by Kolja Waschk, can be found at:
  *  http://www.ixo.de/info/zyxel_uclinux
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 2 as published
- *  by the Free Software Foundation.
- *
  */
 
 #ifndef _ZYNOS_H
--- tools/firmware-utils/src/zytrx.c
+++ tools/firmware-utils/src/zytrx.c
@@ -8,10 +8,6 @@
  * Copyright (C) 2008 Imre Kaloz  <kaloz@openwrt.org>
  *                    Gabor Juhos <juhosg@openwrt.org>
  * Copyright (C) 2021 Bjørn Mork <bjorn@mork.no>
-
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2 as published by the Free Software Foundation.
  */
 
 #include <stdio.h>
