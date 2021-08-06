eb2d10666322df94bcaaacc679579ed2adf3895e
Rafał Miłecki <rafal@milecki.pl>
2021-08-06 12:59:23 +0200

    firmware-utils: nand_ecc: replace GPL boilerplate with SPDX
    Cc: yajin <yajin@vm-kernel.org>
    Cc: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Signed-off-by: maurerr <mariusd84@gmail.com>

--- tools/firmware-utils/src/nand_ecc.c
+++ tools/firmware-utils/src/nand_ecc.c
@@ -1,23 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
 /*
  * calculate ecc code for nand flash
  *
  * Copyright (C) 2008 yajin <yajin@vm-kernel.org>
  * Copyright (C) 2009 Felix Fietkau <nbd@nbd.name>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 or
- * (at your option) version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
  */
 
 
