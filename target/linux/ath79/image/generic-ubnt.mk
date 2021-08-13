0f170b1f9026692721ecb407104f34e434f893fb
Russell Senior <russell@personaltelco.net>
2021-08-12 22:57:41 -0700

    ath79: add support for Ubiquiti PowerBeam M2 (XW)
    This patch adds support for the Ubiquiti PowerBeam M2 (XW), e.g. PBE-M2-400,
    a 802.11n wireless with a feed+dish form factor. This device was previously
    supported by the ar71xx loco-m-xw firmware.
    
    Specifications:
     - Atheros AR9342 SoC
     - 64 MB RAM
     - 8 MB SPI flash
     - 1x 10/100 Mbps Ethernet port, 24 Vdc PoE-in
     - Power and LAN green LEDs
     - 4x RSSI LEDs (red, orange, green, green)
     - UART (115200 8N1)
    
    Flashing via stock GUI:
     - Downgrade to AirOS v5.5.x (latest available is 5.5.10-u2) first (see
       https://openwrt.org/toh/ubiquiti/powerbeam installation instructions)
     - Upload the factory image via AirOS web GUI.
    
    Flashing via TFTP:
     - Use a pointy tool (e.g., unbent paperclip) to keep the
       reset button pressed.
     - Power on the device (keep reset button pressed).
     - Keep pressing until LEDs flash alternatively LED1+LED3 =>
       LED2+LED4 => LED1+LED3, etc.
     - Release reset button.
     - The device starts a TFTP server at 192.168.1.20.
     - Set a static IP on the computer (e.g., 192.168.1.21/24).
     - Upload via tftp the factory image:
        $ tftp 192.168.1.20
        tftp> bin
        tftp> trace
        tftp> put openwrt-ath79-generic-ubnt_powerbeam-m2-xw-squashfs-factory.bin
    
    WARNING: so far, no non-destructive method has been discovered for
    opening the enclosure to reach the serial console. Internal photos
    are available here: https://fcc.io/SWX-NBM2HP
    
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -328,6 +328,14 @@ define Device/ubnt_powerbeam-5ac-gen2
 endef
 TARGET_DEVICES += ubnt_powerbeam-5ac-gen2
 
+define Device/ubnt_powerbeam-m2-xw
+  $(Device/ubnt-xw)
+  DEVICE_MODEL := PowerBeam M2
+  DEVICE_PACKAGES += rssileds
+  SUPPORTED_DEVICES += loco-m-xw
+endef
+TARGET_DEVICES += ubnt_powerbeam-m2-xw
+
 define Device/ubnt_powerbeam-m5-xw
   $(Device/ubnt-xw)
   DEVICE_MODEL := PowerBeam M5
