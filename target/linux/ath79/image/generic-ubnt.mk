d4d03dda3475d4b798991da9efab0db3f92cc187
Russell Senior <russell@personaltelco.net>
2021-08-12 22:57:39 -0700

    ath79: rename Ubiquiti PowerBeam M (XW) to PowerBeam M5 (XW)
    The commit [1] added support for Ubiquiti PowerBeam M (XW), tested
    on the PBE-M5-400. But, it turns out the PBE-M2-400 has a different
    ethernet configuration, so make the support specific to the m5 version
    in anticipation of adding specific support for the m2 in a separate
    commit.
    
    [1] 12eb5b2384a6 ("ath79: add support for Ubiquiti PowerBeam M (XW)")
    
    Signed-off-by: Russell Senior <russell@personaltelco.net>
    [fix model name in DTS, format commit reference in commit message]
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -328,13 +328,13 @@ define Device/ubnt_powerbeam-5ac-gen2
 endef
 TARGET_DEVICES += ubnt_powerbeam-5ac-gen2
 
-define Device/ubnt_powerbeam-m-xw
+define Device/ubnt_powerbeam-m5-xw
   $(Device/ubnt-xw)
-  DEVICE_MODEL := PowerBeam M
+  DEVICE_MODEL := PowerBeam M5
   DEVICE_PACKAGES += rssileds
   SUPPORTED_DEVICES += loco-m-xw
 endef
-TARGET_DEVICES += ubnt_powerbeam-m-xw
+TARGET_DEVICES += ubnt_powerbeam-m5-xw
 
 define Device/ubnt_powerbridge-m
   $(Device/ubnt-xm)
