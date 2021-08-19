378769b5551714ccaa821b481bfeecbf362f351e
David Bauer <mail@david-bauer.net>
2021-08-19 20:40:29 +0200

    kernel: bump 5.4 to 5.4.142
    Compile-tested: ath79-generic ipq40xx-generic
    Run-tested: ipq40xx-generic
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .137
+LINUX_VERSION-5.4 = .142
 
-LINUX_KERNEL_HASH-5.4.137 = f09e5e366ce5d8bde887cda229ef17138fd1653706a702221f934f99aaa31f7c
+LINUX_KERNEL_HASH-5.4.142 = 99785728968564ba27c7e552d024b560072dcbc885540912eabb5c021e231451
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
