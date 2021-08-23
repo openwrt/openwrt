27efa6784c038930ff1493e34b0ea8a8e4264dc7
Jan-Niklas Burfeind <git@aiyionpri.me>
2021-08-23 15:07:38 +0200

    ath79: add support for onion omega
    The Onion Omega is a hardware development platform with built-in WiFi.
    
    https://onioniot.github.io/wiki/
    
    Specifications:
     - QCA9331 @ 400 MHz (MIPS 24Kc Big-Endian Processor)
     - 64MB of DDR2 RAM running at 400 MHz
     - 16MB of on-board flash storage
     - Support for USB 2.0
     - Support for Ethernet at 100 Mbps
     - 802.11b/g/n WiFi at 150 Mbps
     - 18 digital GPIOs
     - A single Serial UART
     - Support for SPI
     - Support for I2S
    
    Flash instructions:
    The device is running OpenWrt upon release using the ar71xx target.
    Both a sysupgrade
    and uploading the factory image using u-boots web-UI do work fine.
    
    Depending on the ssh client, it might be necessary to enable outdated
    KeyExchange methods e.g. in the clients ssh-config:
    
    Host 192.168.1.1
            KexAlgorithms +diffie-hellman-group1-sha1
    
    The stock credentials are: root onioneer
    
    For u-boots web-UI manually configure `192.168.1.2/24` on your computer,
    connect to `192.168.1.1`.
    
    MAC addresses as verified by OEM firmware:
    2G       phy0      label
    LAN      eth0      label - 1
    
    LAN is only available in combination with an optional expansion dock.
    
    Based on vendor acked commit:
    commit 5cd49bb067ca ("ar71xx: add support for Onion Omega")
    
    Partly reverts:
    commit fc553c7e4c8e ("ath79: drop unused/incomplete dts")
    
    Signed-off-by: Jan-Niklas Burfeind <git@aiyionpri.me>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1688,6 +1688,19 @@ define Device/ocedo_ursus
 endef
 TARGET_DEVICES += ocedo_ursus
 
+define Device/onion_omega
+  $(Device/tplink-16mlzma)
+  SOC := ar9331
+  DEVICE_VENDOR := Onion
+  DEVICE_MODEL := Omega
+  DEVICE_PACKAGES := kmod-usb-chipidea2
+  SUPPORTED_DEVICES += onion-omega
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma
+  IMAGE_SIZE := 16192k
+  TPLINK_HWID := 0x04700001
+endef
+TARGET_DEVICES += onion_omega
+
 define Device/openmesh_common_64k
   DEVICE_VENDOR := OpenMesh
   DEVICE_PACKAGES := uboot-envtools
