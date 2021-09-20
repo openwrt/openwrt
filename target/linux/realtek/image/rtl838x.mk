6168475d03af70fc36d47150fc5dd8f6822c9df3
INAGAKI Hiroshi <musashino.open@gmail.com>
2021-09-20 22:28:51 +0900

    realtek: add support for APRESIA ApresiaLightGS120GT-SS
    APRESIA ApresiaLightGS120GT-SS (APLGS120GTSS) is a 16 + 4 ports gigabit
    switch, based on RTL8382M.
    
    Specifications:
    
    - SoC          : Realtek RTL8382M
    - RAM          : DDR3 256 MiB (Nanya NT5CC256M8JQ-EK)
    - Flash        : SPI-NOR 32 MiB (Macronix MX25L25635FMI-10G)
    - Ethernet     : 10/100/1000 Mbps x16 + 4
      - port  1-8  : RTL8218B
      - port  9-16 : RTL8382M, TP (SoC, RTL8218B)
      - port 17-20 : RTL8214FC, TP/SFP (Combo)
    - LEDs/Keys    : 3x/1x
    - UART         : through-hole on PCB
      - J6: 3.3V, TX, RX, GND from tri-angle marking side
      - 115200n8
    - Power        : 100-120/200-240 VAC, 50/60 Hz
                     Max. 16 W, Avg 14 W (100 VAC)
      - Plug       : IEC 60320-C13
    
    Flash instruction using factory image:
    
    1. Boot ApresiaLightGS120GT-SS normally
    2. Login to WebUI and open firmware page ("ファームウェア")
    3. If the device is booted from image1, set active image for next
       booting ("起動イメージ選択") to image2("イメージ2"), press apply
       ("適用") button and reboot the device to make booting from image2
    4. On the WebUI, set active image to image1
    5. Select the OpenWrt factory image and press update button ("更新")
    6. Open reboot page ("再起動") and press reboot button ("再起動実行")
    
    Notes:
    
    - "ApresiaLightGS120GT-SS" is a model name and "APLGS120GTSS" is a model
      number
    
    - this device has 3x GPIO-controlled LEDs on PCB, but 1x LED
      ("green:unused") has no hole on the case
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>

--- target/linux/realtek/image/rtl838x.mk
+++ target/linux/realtek/image/rtl838x.mk
@@ -12,6 +12,22 @@ define Device/allnet_all-sg8208m
 endef
 TARGET_DEVICES += allnet_all-sg8208m
 
+define Device/apresia_aplgs120gtss
+  $(Device/cameo-fw)
+  SOC := rtl8382
+  IMAGE_SIZE := 14848k
+  DEVICE_VENDOR := APRESIA
+  DEVICE_MODEL := ApresiaLightGS120GT-SS
+  UIMAGE_MAGIC := 0x12345000
+  CAMEO_KERNEL_PART_SIZE := 1572864
+  CAMEO_KERNEL_PART := 3
+  CAMEO_ROOTFS_PART := 4
+  CAMEO_CUSTOMER_SIGNATURE := 2
+  CAMEO_BOARD_MODEL := APLGS120GTSS
+  CAMEO_BOARD_VERSION := 4
+endef
+TARGET_DEVICES += apresia_aplgs120gtss
+
 define Device/d-link_dgs-1210-10mp-f
   $(Device/d-link_dgs-1210)
   SOC := rtl8380
