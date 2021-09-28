0e3027663247153a39b7f0d21d031acbbcd66155
Stijn Tintel <stijn@linux-ipv6.be>
2021-09-29 01:28:06 +0300

    kernel: add missing symbol to kmod-kvm
    Even though TRACEPOINTS is not enabled in my kernel config, my build
    fails due to KVM_MMU_AUDIT being missing. Add this symbol to kmod-kvm to
    fix this.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- package/kernel/linux/modules/virt.mk
+++ package/kernel/linux/modules/virt.mk
@@ -21,6 +21,7 @@ define KernelPackage/kvm-x86
   DEPENDS:=@TARGET_x86_generic||TARGET_x86_64 +kmod-irqbypass
   KCONFIG:=\
 	  CONFIG_KVM \
+	  CONFIG_KVM_MMU_AUDIT=n \
 	  CONFIG_VIRTUALIZATION=y
   FILES:= $(LINUX_DIR)/arch/$(LINUX_KARCH)/kvm/kvm.ko
   AUTOLOAD:=$(call AutoProbe,kvm.ko)
