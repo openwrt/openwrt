3ad229db0b0ed8538eadcdc2a1c3faf964f239be
Robert Marko <robimarko@gmail.com>
2021-10-09 20:13:25 +0200

    ipq40xx: add support for MikroTik hAP ac3
    This adds support for the MikroTik RouterBOARD RBD53iG-5HacD2HnD
    (hAP ac³), a  indoor dual band, dual-radio 802.11ac
    wireless AP with external omnidirectional antennae, USB port, five
    10/100/1000 Mbps Ethernet ports and PoE passthrough.
    
    See https://mikrotik.com/product/hap_ac3 for more info.
    
    Specifications:
     - SoC: Qualcomm Atheros IPQ4019
     - RAM: 256 MB
     - Storage: 16 MB NOR + 128 MB NAND
     - Wireless:
       · Built-in IPQ4019 (SoC) 802.11b/g/n 2x2:2, 3 dBi antennae
       · Built-in IPQ4019 (SoC) 802.11a/n/ac 2x2:2, 5.5 dBi antennae
     - Ethernet: Built-in IPQ4019 (SoC, QCA8075) , 5x 1000/100/10 port,
                 passive PoE in, PoE passtrough on port 5
    - 1x USB Type A port
    
    Installation:
    1. Boot the initramfs image via TFTP
    2. Run "cat /proc/mtd" and look for "ubi" partition mtd device number, ex. "mtd1"
    3. Use ubiformat to remove MikroTik specific UBI volumes
    * Detach the UBI partition by running: "ubidetach -d 0"
    * Format the partition by running: "ubiformat /dev/mtdN -y"
    Replace mtdN with the correct mtd index from step 2.
    3. Flash the sysupgrade image using "sysupgrade -n"
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    Tested-by: Mark Birss <markbirss@gmail.com>
    Tested-by: Michael Büchler <michael.buechler@posteo.net>
    Tested-by: Alex Tomkins <tomkins@darkzone.net>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -55,6 +55,24 @@ zyxel_do_upgrade() {
 	fi
 }
 
+platform_do_upgrade_mikrotik_nand() {
+	local fw_mtd=$(find_mtd_part kernel)
+	fw_mtd="${fw_mtd/block/}"
+	[ -n "$fw_mtd" ] || return
+
+	local board_dir=$(tar tf "$1" | grep -m 1 '^sysupgrade-.*/$')
+	board_dir=${board_dir%/}
+	[ -n "$board_dir" ] || return
+
+	local kernel_len=$(tar xf "$1" ${board_dir}/kernel -O | wc -c)
+	[ -n "$kernel_len" ] || return
+
+	tar xf "$1" ${board_dir}/kernel -O | ubiformat "$fw_mtd" -y -S $kernel_len -f -
+
+	CI_KERNPART="none"
+	nand_do_upgrade "$1"
+}
+
 platform_do_upgrade() {
 	case "$(board_name)" in
 	8dev,jalapeno |\
@@ -118,6 +136,9 @@ platform_do_upgrade() {
 	mikrotik,sxtsq-5-ac)
 		[ "$(rootfs_type)" = "tmpfs" ] && mtd erase firmware
 		default_do_upgrade "$1"
+		;;
+	mikrotik,hap-ac3)
+		platform_do_upgrade_mikrotik_nand "$1"
 		;;
 	netgear,rbr50 |\
 	netgear,rbs50 |\
