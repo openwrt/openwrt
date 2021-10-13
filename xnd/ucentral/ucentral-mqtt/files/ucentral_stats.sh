66b7e0d8e81cf7872f12a0ae0acf69ce741c9d74
jason <jason@wallystech.com>
2021-10-13 23:21:07 +0800

    delete

--- xnd/ucentral/ucentral-mqtt/files/ucentral_stats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ucode -m ubus -i /usr/share/ucentral/state.uc -E cfg=/etc/ucentral/ucentral.active > /tmp/ucentral.stats
-
--- xnd/ucentral/ucentral-schema/files/usr/libexec/ucentral/ucentral_led_blink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-timeout=$1
-
-/etc/init.d/led blink
-sleep $1
-/etc/init.d/led turnon
-exit 0
