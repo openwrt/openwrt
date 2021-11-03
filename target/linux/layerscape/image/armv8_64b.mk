8084fd122efc0a989a07faba0c6bf95c631520f5
Martin Schiller <ms@dev.tdt.de>
2021-11-03 10:18:39 +0100

    layerscape: remove unneeded KERNEL_ENTRY_POINT definition
    As KERNEL_ENTRY_POINT is identical to KERNEL_LOADADDR, drop it.
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>

--- target/linux/layerscape/image/armv8_64b.mk
+++ target/linux/layerscape/image/armv8_64b.mk
@@ -9,7 +9,6 @@ define Device/Default
   KERNEL := kernel-bin | gzip | uImage gzip
   KERNEL_INITRAMFS = kernel-bin | gzip | fit gzip $$(DTS_DIR)/$$(DEVICE_DTS).dtb
   KERNEL_LOADADDR := 0x80080000
-  KERNEL_ENTRY_POINT := 0x80080000
   DEVICE_DTS = freescale/$(subst _,-,$(1))
   IMAGE_SIZE := 64m
   IMAGE/sysupgrade.bin = \
