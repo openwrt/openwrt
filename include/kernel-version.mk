1d94f72439034c112bfa4b694fc736123f4890a5
Petr Štetiar <ynezz@true.cz>
2021-12-12 08:19:37 +0100

    kernel: bump 4.14 to 4.14.258
    Rebased patches:
    
     * generic: 273-batman-adv-Convert-packet.h-to-uapi-header.patch
     * ipq806x: 0065-arm-override-compiler-flags.patch
     * mvebu: 513-arm64-dts-marvell-armada37xx-Add-emmc-sdio-pinctrl-d.patch
    
    Removed patches:
    
     Fixed upstream:
    
       * ar71xx: 821-serial-core-add-support-for-boot-console-with-arbitr.patch
       * ath79: 921-serial-core-add-support-for-boot-console-with-arbitr.patch
         - in 4.14.256 via 9112e7ef87149b3d8093e7446d784117f6e18d69
    
       * mvebu: 527-PCI-aardvark-allow-to-specify-link-capability.patch
         - in 4.14.257 via 62a3dc9b65a2b24800fc4267b8cf590fad135034
    
       * mvebu: 524-PCI-aardvark-set-host-and-device-to-the-same-MAX-payload-size.patch
         - should be hopefully fixed by the bunch of changes in .256 and .257
    
    Run tested on ipq40xx/glinet-b1300 and mvebu/turris-omnia.
    
    Fixes: CVE-2021-3640
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .254
+LINUX_VERSION-4.14 = .258
 
-LINUX_KERNEL_HASH-4.14.254 = dfd95c1bc3af5b0e57e53608f8356037510b19cb55cac520e43c3d499ea60d46
+LINUX_KERNEL_HASH-4.14.258 = 71af29d042e4ce6cf0f8cdb5ce2fc4790cca642ee4a80ae6bb71a2092afc4b98
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
