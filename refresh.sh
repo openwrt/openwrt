79fd2bc9a3d6b14eede176ce7d8b7eaa04ad727e
Miguel Andrade <rmandrad@gmail.com>
2021-12-18 07:12:30 +0000

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -2,10 +2,7 @@ git stash
 git config --global user.email "rmandrad@gmail.com"
 git config --global user.name "rmandrad"
 
-git remote -v
-
 git remote add upstream https://github.com/openwrt/openwrt.git
 
-git remote -v
-
+git checkout master
 git fetch upstream
