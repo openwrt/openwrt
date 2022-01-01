3282a818e7b8f5725709146ae11fdfc9966b3266
Zhijun You <hujy652@gmail.com>
2022-01-01 23:12:51 +0100

    ipq807x: add Redmi AX6
    Redmi AX6 is a budget 802.11ax dual-band router/AP
    
    Specifications:
    * CPU: Qualcomm IPQ8071A Quad core Cortex-A53 1.4GHz
    * RAM: 512MB of DDR3
    * Storage: 128MB NAND
    * Ethernet: 4x1G RJ45 ports (QCA8075)
    * WLAN:
    	* 2.4GHz: Qualcomm QCN5024 2x2 802.11b/g/n/ax 574 Mbps PHY rate
    	* 5GHz: Qualcomm QCN5054 4x4@80MHz or 2x2@160MHz 802.11a/b/g/n/ac/ax 2402 PHY rate
    * LEDs:
    	* System (Blue/Yellow)
    	* Network (Blue/Yellow)
    *Buttons: 1x soft reset
    *Power: 12V DC jack
    
    Installation instructions:
    
    Must obtain SSH access first.
    
    For first installlation, using ubiformat is mandatory:
    * Copy ubi file to the router's /tmp
    * cat /proc/mtd and find the rootfs and rootfs_1 mtd index
    * Use ubiformat to flash the UBI image:
    	* if you are on rootfs (mtd12)
    		ubiformat /dev/mtd13 -y -f /tmp/openwrt-ipq807x-generic-redmi_ax6-squashfs-nand-factory.ubi
    		nvram set flag_last_success=1
    		nvram set flag_boot_rootfs=1
    		nvram commit
    	* if you are on rootfs_1 (mtd13)
    		ubiformat /dev/mtd12 -y -f /tmp/openwrt-ipq807x-generic-redmi_ax6-squashfs-nand-factory.ubi
    		nvram set flag_last_success=0
    		nvram set flag_boot_rootfs=0
    		nvram commit
    * reboot
    * ubiformat the other rootfs
    * Now the regualr sysupgrade can be used
    
    Signed-off-by: Zhijun You <hujy652@gmail.com>

--- target/linux/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq807x/base-files/lib/upgrade/platform.sh
@@ -10,6 +10,7 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	redmi,ax6|\
 	xiaomi,ax3600)
 		part_num="$(fw_printenv -n flag_boot_rootfs)"
 		if [ "$part_num" -eq "0" ]; then
