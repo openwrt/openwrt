2fd930aeb4f5e0db2961a22d64c7e3e946600cd6
Robert Marko <robimarko@gmail.com>
2022-01-07 19:01:36 +0100

    ipq807x: Add Xiaomi AX9000
    Xiaomi AX9000 is a premium 802.11ax "tri"-band router/AP.
    Specifications:
    * CPU: Qualcomm IPQ8072A Quad core Cortex-A53 2.2GHz
    * RAM: 1024MB of DDR3
    * Storage: 256MB of parallel NAND
    * Ethernet:
    	* 4x1G RJ45 ports (QCA8075) with 1x status LED per port
    	* 1x2.5G RJ45 port (QCA8081) with 1x status LED
    * WLAN:
    	* PCI based Qualcomm QCA9889 1x1 802.11ac Wawe 2 for IoT
    	* 2.4GHz: Qualcomm QCN5024 4x4@40MHz 802.11b/g/n/ax 1147 Mbps PHY rate
    	* 5.8GHz: Qualcomm QCN5054 4x4@80MHz or 2x2@160MHz 802.11a/b/g/n/ac/ax 2402Mbps PHY rate
    	* 5GHz: PCI based Qualcomm QCN9024 4x4@160MHz 802.11a/b/g/n/ac/ax 4804Mbps PHY rate
    * USB: 1x USB3.0 Type-A port
    * LED-s:
    	* System (Blue and Yellow)
    	* Network (Blue and Yellow)
    	* RGB light bar on top in X shape
    * Buttons:
    	* 1x Power switch
    	* 1x Soft reset
    	* 1x Mesh button
    * Power: 12V DC Jack
    
    Installation instructions:
    
    Obtaining SSH access is mandatory
    https://openwrt.org/inbox/toh/xiaomi/ax9000#obtain_ssh_access
    
    For first installation, using ubiformat is mandatory:
    * Copy openwrt-ipq807x-generic-xiaomi_ax9000-squashfs-nand-factory.ubi to /tmp
    * cat /proc/mtd and find the rootfs and rootfs_1 mtd index
    * Use ubiformat to flash the UBI image:
    ubiformat /dev/mtd21 -y -f /tmp/openwrt-ipq807x-generic-xiaomi_ax9000-squashfs-nand-factory.ubi
    ubiformat /dev/mtd22 -y	-f /tmp/openwrt-ipq807x-generic-xiaomi_ax9000-squashfs-nand-factory.ubi
    
    Please confirm that mtd21 and mtd22 are the correct indexes.
    
    * reboot
    
    After that regular sysupgrade can be used.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq807x/base-files/lib/upgrade/platform.sh
@@ -19,7 +19,8 @@ platform_do_upgrade() {
 		mmc_do_upgrade "$1"
 		;;
 	redmi,ax6|\
-	xiaomi,ax3600)
+	xiaomi,ax3600|\
+	xiaomi,ax9000)
 		part_num="$(fw_printenv -n flag_boot_rootfs)"
 		if [ "$part_num" -eq "0" ]; then
 			CI_UBIPART="rootfs_1"
