cbce6c6d9519ab86e11c9752910a4c72dd52cb58
Ansuel Smith <ansuelsmth@gmail.com>
2022-01-10 17:02:30 +0100

    kernel: split kernel version to dedicated files
    Move the kernel versions and hash to dedicated files.
    This makes kernel bump quicker and fix some annoying
    problem with rebasing when multiple kernel bump are proposed.
    
    Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>
    [Rebased on top of current master]
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>
    (cherry picked from commit 0765466a42f46f7357e260866a4284ed567bb7ad)
    Signed-off-by: Josef Schlehofer <pepe.schlehofer@gmail.com>
    [Rebased on top of current openwrt-21.02]

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,12 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .225
+KERNEL_DETAILS_FILE=$(INCLUDE_DIR)/kernel-$(KERNEL_PATCHVER)
+ifeq ($(wildcard $(KERNEL_DETAILS_FILE)),)
+  $(error Missing kernel version/hash file for $(KERNEL_PATCHVER). Please create $(KERNEL_DETAILS_FILE))
+endif
 
-LINUX_KERNEL_HASH-5.4.225 = 59f596f6714317955cf481590babcf015aff2bc1900bd8e8dc8f7af73bc560aa
+include $(KERNEL_DETAILS_FILE)
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
