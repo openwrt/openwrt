63ddac31ffb9470336425ea946d8da15811bdfab
suyuan168 <175338101@qq.com>
2022-01-11 00:41:28 +0800

    add tempinfo setmac_all

--- package/base-files/files/lib/functions/system.sh
+++ package/base-files/files/lib/functions/system.sh
@@ -154,6 +154,16 @@ macaddr_add() {
 	echo $oui:$nic
 }
 
+macaddr_sub() {
+    local mac=$1
+    local val=$2
+    local oui=${mac%:*:*:*}
+    local nic=${mac#*:*:*:}
+
+    nic=$(printf "%06x" $((0x${nic//:/} - $val & 0xffffff)) | sed 's/^\(.\{2\}\)\(.\{2\}\)\(.\{2\}\)/\1:\2:\3/')
+    echo $oui:$nic
+}
+
 macaddr_geteui() {
 	local mac=$1
 	local sep=$2
