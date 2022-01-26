80edacc8a697b5aaacf3a26d462c1fd9e1d5a639
Florian Eckert <fe@dev.tdt.de>
2022-01-26 16:12:12 +0100

    target/mxs: replace gpio-mcp23s08 with pinctrl-mcp23s08
    The dependency on the kernel module gpio-mcp23s08 is replaced by
    pinctrl-mcp23s08-spi and pinctrl-mcp23s08-i2c, as the gpio-mpc23s08 kernel
    module no longer exists.
    
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>

--- target/linux/mxs/profiles/03-olinuxino-micro.mk
+++ target/linux/mxs/profiles/03-olinuxino-micro.mk
@@ -5,7 +5,8 @@
 define Profile/olinuxino-micro
   NAME:=Olimex OLinuXino Micro/Nano boards
   PACKAGES += imx-bootlets uboot-mxs-mx23_olinuxino \
-	  kmod-gpio-mcp23s08 kmod-leds-gpio
+	  kmod-pinctrl-mcp23s08-spi kmod-pinctrl-mcp23s08-i2c \
+	  kmod-leds-gpio
 endef
 
 define Profile/olinuxino-micro/Description
