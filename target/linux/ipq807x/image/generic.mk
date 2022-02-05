b5f32064ed0c3a8b897377ec2fc895dc7a5f1bc8
Robert Marko <robimarko@gmail.com>
2022-02-05 23:40:51 +0100

    ipq807x: add Qualcomm Atheros IPQ807x target
    Qualcomm Atheros IPQ807x is a modern WiSoC featuring:
    * Quad Core ARMv8 Cortex A-53
    	* @ 2.2 GHz (IPQ8072A/4A/6A/8A) Codename Hawkeye
    	* @ 1.4 GHz (IPQ8070A/1A) Codename Acorn
    * Dual Band simultaneaous IEEE 802.11ax
    	* 5G: 8x8/80 or 4x4/160MHz (IPQ8074A/8A)
    	* 5G: 4x4/80 or 2x2/160MHz (IPQ8071A/2A/6A)
    	* 5G: 2x2/80MHz (IPQ8070A)
    	* 2G: 4x4/40MHz (IPQ8072A/4A/6A/8A)
    	* 2G: 2x2/40MHz (IPQ8070A/1A)
    * 1x PSGMII via QCA8072/5 (Max 5x 1GbE ports)
    * 2x SGMII/USXGMII (1/2.5/5/10 GbE) on Hawkeye
    * 2x SGMII/USXGMII (1/2.5/5 GbE) on Acorn
    * DDR3L/4 32/16 bit up to 2400MT/s
    * SDIO 3.0/SD card 3.0/eMMC 5.1
    * Dual USB 3.0
    * One PCIe Gen2.1 and one PCIe Gen3.0 port (Single lane)
    * Parallel NAND (ONFI)/LCD
    * 6x QUP BLSP SPI/I2C/UART
    * I2S, PCM, and TDMA
    * HW PWM
    * 1.8V configurable GPIO
    * Companion PMP8074 PMIC via SPMI (GPIOS, RTC etc)
    
    Note that only v2 SOC models aka the ones ending with A suffix are
    supported, v1 models do not comply to the final 802.11ax and have
    lower clocks, lack the Gen3 PCIe etc.
    
    SoC itself has two UBI32 cores for the NSS offloading system, however
    currently no offloading is supported.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

---
+++ target/linux/ipq807x/image/generic.mk
@@ -0,0 +1,18 @@
+define Device/FitImage
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | libdeflate-gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/FitImageLzma
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/UbiFit
+	KERNEL_IN_UBI := 1
+	IMAGES := factory.ubi sysupgrade.bin
+	IMAGE/factory.ubi := append-ubi
+	IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
