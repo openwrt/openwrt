606106fb295e2770af4df7c04fc9fcc95428a0f4
Petr Štetiar <ynezz@true.cz>
2022-02-10 13:27:06 +0100

    kernel: bump 4.14 to 4.14.265
    All patches refreshed automagically without conflicts.
    
    Run tested on ipq40xx/glinet-b1300 and mvebu/turris-omnia.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .264
+LINUX_VERSION-4.14 = .265
 
-LINUX_KERNEL_HASH-4.14.264 = 55b04bc00df0282d5e7d23028d12f7d1d29aaadbf52199661b09e70f1c4225b4
+LINUX_KERNEL_HASH-4.14.265 = 1a643d551d9578005663fcc0ce70bffc43904aa9371d6aa80e0f0584be7892c7
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
