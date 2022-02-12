c6ddf8d5021d5262ecac8648d6ae849a47e1f0fb
Hauke Mehrtens <hauke@hauke-m.de>
2022-02-12 16:01:04 +0000

    kernel: bump 5.4 to 5.4.179
    Many changes were done in drivers/pinctrl/bcm/pinctrl-bcm2835.c between
    5.4.171 and 5.4.179.
    The following 3 patches do not apply any more:
    * target/linux/bcm27xx/patches-5.4/950-0316-pinctrl-bcm2835-Add-support-for-BCM2711-pull-up-func.patch
      This was already integrated in kernel v5.4-rc1, it was never needed.
    
    * target/linux/bcm27xx/patches-5.4/950-0328-Revert-pinctrl-bcm2835-Pass-irqchip-when-adding-gpio.patch
    * target/linux/bcm27xx/patches-5.4/950-0362-pinctrl-bcm2835-Change-init-order-for-gpio-hogs.patch
    I think these were done to fix the problem which was really fixed in
    commit 75278f1aff5e ("pinctrl: bcm2835: Change init order for gpio
    hogs") from v5.4.175
    
    target/linux/generic/backport-5.4/716-v5.5-net-sfp-move-fwnode-parsing-into-sfp-bus-layer.patch
    Move fwnode_device_is_available to the same position as in kernel 5.10.
    
    target/linux/layerscape/patches-5.4/302-dts-0083-arm64-ls1028a-qds-correct-bus-of-rtc.patch
    Applied in commit 65816c1034769e714edb70f59a33bc5472d9e55f ("arm64: dts:
    ls1028a-qds: move rtc node to the correct i2c bus")
    
    Compile-tested: lantiq/xrx200, bcm27xx/bcm2710
    Run-tested: lantiq/xrx200
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .171
+LINUX_VERSION-5.4 = .179
 
-LINUX_KERNEL_HASH-5.4.171 = afbcc8ed7d85485af299567f307b03057ffff3ec7bee02a21c33933d435b4958
+LINUX_KERNEL_HASH-5.4.179 = 2c9bdec0922a95aff34e8d53d2e0ecf7e842033cd908d2959a43d34afb5d897d
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
