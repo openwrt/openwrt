0fd433100d9f9c666452ef846dcbd20471be8db6
Sungbo Eo <mans0n@gorani.run>
2022-02-12 17:01:31 +0900

    tools: zip: fetch SOURCE_DATE_EPOCH directly
    Remove "--mtime" option introduced in commit 18c9faa03211 ("tools: zip:
    add option for reproducible archives") and instead fetch SOURCE_DATE_EPOCH
    environment variable directly in the code.
    
    Ref: https://sourceforge.net/p/infozip/patches/25/
    Signed-off-by: Sungbo Eo <mans0n@gorani.run>

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -542,7 +542,6 @@ define Build/zip
 	mkdir $@.tmp
 	mv $@ $@.tmp/$(word 1,$(1))
 	TZ=UTC $(STAGING_DIR_HOST)/bin/zip -j -X \
-		$(if $(SOURCE_DATE_EPOCH),--mtime="$(SOURCE_DATE_EPOCH)") \
 		$(wordlist 2,$(words $(1)),$(1)) \
 		$@ $@.tmp/$(if $(word 1,$(1)),$(word 1,$(1)),$$(basename $@))
 	rm -rf $@.tmp
