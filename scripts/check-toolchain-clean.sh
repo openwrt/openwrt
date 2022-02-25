9116096c0f1abe3cdb07ad9c9340dd4ca65ce797
Petr Štetiar <ynezz@true.cz>
2022-02-25 11:15:45 +0100

    check-toolchain-clean.sh: fix shellcheck warnings
    Fixes following complaints and suggestions:
    
     In scripts/check-toolchain-clean.sh line 2:
     eval `grep CONFIG_GCC_VERSION .config`
          ^-- SC2046 (warning): Quote this to prevent word splitting.
          ^-- SC2006 (style): Use $(...) notation instead of legacy backticks `...`.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- scripts/check-toolchain-clean.sh
+++ scripts/check-toolchain-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-eval `grep CONFIG_GCC_VERSION .config`
+eval "$(grep CONFIG_GCC_VERSION .config)"
 CONFIG_TOOLCHAIN_BUILD_VER="$CONFIG_GCC_VERSION-$(cat toolchain/build_version)"
 touch .toolchain_build_ver
 [ "$CONFIG_TOOLCHAIN_BUILD_VER" = "$(cat .toolchain_build_ver)" ] && exit 0
