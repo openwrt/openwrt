3c3aa8be2f84404d19ff6e595acebbf83ac68f8d
Paul Maruhn <paulmaruhn@posteo.de>
2022-03-22 21:06:42 +0100

    ath79: support for TP-Link EAP225-Outdoor v3
    This model is almost identical to the EAP225-Outdoor v1.
    Major difference is the RTL8211FS PHY Chipset.
    
    Device specifications:
    * SoC: QCA9563 @ 775MHz
    * Memory: 128MiB DDR2
    * Flash: 16MiB SPI-NOR
    * Wireless 2.4GHz (SoC): b/g/n 2x2
    * Wireless 5GHz (QCA9886): a/n/ac 2x2 MU-MIMO
    * Ethernet (RTL8211FS): 1× 1GbE, PoE
    
    Flashing instructions:
    * ssh into target device with recent (>= v1.6.0) firmware
    * run `cliclientd stopcs` on target device
    * upload factory image via web interface
    
    Debricking:
    To recover the device, you need access to the serial port. This requires
    fine soldering to test points, or the use of probe pins.
    * Open the case and solder wires to the test points: RXD, TXD and TPGND4
      * Use a 3.3V UART, 115200 baud, 8n1
    * Interrupt bootloader by holding ctrl+B during boot
    * upload initramfs via built-in tftp client and perform sysupgrade
        setenv ipaddr 192.168.1.1 # default, change as required
        setenv serverip 192.168.1.10 # default, change as required
        tftp 0x80800000 initramfs.bin
        bootelf $fileaddr
    
    MAC addresses:
    MAC address (as on device label) is stored in device info partition at
    an offset of 8 bytes. ath9k device has same address as ethernet, ath10k
    uses address incremented by 1.
    From stock ifconfig:
    
        ath0      Link encap:Ethernet  HWaddr D8:...:2E
        ath10     Link encap:Ethernet  HWaddr D8:...:2F
        br0       Link encap:Ethernet  HWaddr D8:...:2E
        eth0      Link encap:Ethernet  HWaddr D8:...:2E
    
    Signed-off-by: Paul Maruhn <paulmaruhn@posteo.de>
    Co-developed-by: Philipp Rothmann <philipprothmann@posteo.de>
    Signed-off-by: Philipp Rothmann <philipprothmann@posteo.de>
    [Add pre-calibraton nvme-cells]
    Tested-by: Tido Klaassen <tido_ff@4gh.eu>
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -401,6 +401,17 @@ define Device/tplink_eap225-outdoor-v1
 endef
 TARGET_DEVICES += tplink_eap225-outdoor-v1
 
+define Device/tplink_eap225-outdoor-v3
+  $(Device/tplink-eap2x5)
+  SOC := qca9563
+  IMAGE_SIZE := 13824k
+  DEVICE_MODEL := EAP225-Outdoor
+  DEVICE_VARIANT := v3
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct
+  TPLINK_BOARD_ID := EAP225-V3
+endef
+TARGET_DEVICES += tplink_eap225-outdoor-v3
+
 define Device/tplink_eap225-v1
   $(Device/tplink-eap2x5)
   SOC := qca9563
