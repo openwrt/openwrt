8da3de825a90405351519c3a886959aac42c8364
Koen Vandeputte <koen.vandeputte@ncentric.com>
2022-04-05 14:53:01 +0200

    kernel: package mhi pci driver
    This driver provides MHI PCI controller driver for devices
    such as Qualcomm SDX55 based PCIe modems
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1354,3 +1354,18 @@ define KernelPackage/mhi-bus/description
 endef
 
 $(eval $(call KernelPackage,mhi-bus))
+
+define KernelPackage/mhi-pci-generic
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=MHI PCI controller driver
+  DEPENDS:=@LINUX_5_15 +kmod-mhi-bus
+  KCONFIG:=CONFIG_MHI_BUS_PCI_GENERIC
+  FILES:=$(LINUX_DIR)/drivers/bus/mhi/mhi_pci_generic.ko
+  AUTOLOAD:=$(call AutoProbe,mhi_pci_generic)
+endef
+
+define KernelPackage/mhi-pci-generic/description
+  Kernel module for the MHI PCI controller driver.
+endef
+
+$(eval $(call KernelPackage,mhi-pci-generic))
