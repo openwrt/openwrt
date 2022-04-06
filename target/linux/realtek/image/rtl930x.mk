cbcca06543e821fd30d78c7fb26babdb8816594f
Birger Koblitz <git@birger-koblitz.de>
2022-04-06 15:33:18 +0200

    realtek: add support for Zyxel XGS1210-12
    The Zyxel XGS1210-12 is an 8x 1 GBit, 2x 2.5 GBit NBaseT ports and 2 SFP+ ports switch
    
    Hardware:
    	- RTL9302 SoC: MIPS 34Kc @ 800MHz
    	- 128 MByte DRAM
    	- 16MBit NOR Flash MX25L12833F
    	- RTL8318D 8x GBit PHY
    	- 2x RTL8226 2.5 GBit PHYs
    	- 2x SFP+ Slots
    	- 1 Reset button
    	- Externally accessible UART header at the right side:
    		Top-down: Vcc (3.3V), TX, RX, GND 115200 8N1
    	- System LED
    
    Installation:
    	Connect to the Serial Port, power on the device
    	Interrupt boot by pressing <ESC>
    	Enable network by entering
    	 > rtk network on
    	Upload initramfs via tftp into RAM:
    	 > tftpboot 0x84f00000 192.168.1.100:openwrt-realtek-rtl930x-zyxel_xgs1210-12-initramfs-kernel.bin
    	Start initramfs
    	 > bootm
    	Once OpenWRT is running, install the squashfs image to flash.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -8,6 +8,19 @@ define Device/ubiquiti_usw-aggregation
 endef
 TARGET_DEVICES += ubiquiti_usw-aggregation
 
+define Device/zyxel_xgs1210-12
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93001210
+  ZYXEL_VERS := ABTY
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := XGS1210-12
+    IMAGE_SIZE := 13312k
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | gzip | zyxel-vers $$$$(ZYXEL_VERS) | \
+        uImage gzip
+
+endef
+TARGET_DEVICES += zyxel_xgs1210-12
+
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
   UIMAGE_MAGIC := 0x93001250
