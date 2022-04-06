83b241bd7bf2743a30258f705e7e381fe012229f
Birger Koblitz <git@birger-koblitz.de>
2022-04-06 16:32:32 +0200

    realtek: add support for EdgeCore ECS4125-10p
    The EdgeCore ECS4125-10p is a PoE capable 8x2.5GBit NBaseT + 2 SFP+ port switch
    
    Hardware:
    	- RTL9313 SoC
    	- 1GByte DDR RAM
    	- 32MByte Flash
    	- 4 x RTL8218D dual 2.5 GBit NBaseT PHYs
    	- 2 SFP+ ports
    	- Cisco-Style RJ45 console port
    	- System LED
    	- PoE LED (controllted by PoE controller)
    
    Installation:
    	No idea.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/edgecore_ecs4125-10p
+  KERNEL_LOADADDR = 0x80220000
+  KERNEL_ENTRY = 0x80220000
+  SOC := rtl9313
+  IMAGE_SIZE := 14336k
+  DEVICE_VENDOR := Edgecore
+  DEVICE_MODEL := ECS4125-10P
+  DEVICE_PACKAGES := ip-bridge ethtool
+  DEVICE_PACKAGES += lua-rs232
+endef
+TARGET_DEVICES += edgecore_ecs4125-10p
