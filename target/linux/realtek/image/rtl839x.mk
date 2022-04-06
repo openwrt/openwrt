8e43cb9eb8d0921de0d2cd0af9c9edc65f93b419
Birger Koblitz <git@birger-koblitz.de>
2022-04-06 15:45:50 +0200

    realtek: add support for the Netgear GS728TPv2 switch
    The Netgear GS728TPv2 switch is a 24 GBit + 4 SFP port switch
    
    Hardware:
    	- RTL8391 SoC @700MHz
    	- 128 MB DRAM
    	- 32MB Flash memory MX25L25635E
    	- 3x RTL8218B 8x GBit PHYs
    	- 4 SFP ports
    	- 3 Buttons (Reset, Factory Reset, LED mode)
    	- LED mode LED, Power LED
    	- PoE green/yellow LED and fan green/yellow LED HW controlled
    	- MSP430 muC for control of fans via I2C
    	- 2 fans
    	- 190W active PoE controlled via UART 1
    	- external UART port at back of device (RX, TX, GND, 3.3V) from left to right
    
    Installation:
    	- Upload initramfs image via serial access, then install sysupgrade image.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>

--- target/linux/realtek/image/rtl839x.mk
+++ target/linux/realtek/image/rtl839x.mk
@@ -1,5 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+define Device/netgear_gs728tpv2
+  SOC := rtl8391
+  IMAGE_SIZE := 13952k
+  DEVICE_VENDOR := Netgear
+  DEVICE_MODEL := GS728TP
+  DEVICE_VARIANT := v2
+  UIMAGE_MAGIC := 0x174e4742
+  DEVICE_PACKAGES += lua-rs232
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | gzip | uImage gzip
+endef
+TARGET_DEVICES += netgear_gs728tpv2
+
 define Device/zyxel_gs1900-48
   SOC := rtl8393
   IMAGE_SIZE := 13952k
