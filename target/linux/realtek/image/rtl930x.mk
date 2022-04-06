edfb46a1309218108056416d6f059e28a76b4e76
Birger Koblitz <git@birger-koblitz.de>
2022-04-06 15:33:18 +0200

    realtek/rtl930x: add support for Zyxel XGS1210-12
    The Zyxel XGS1210-12 is an 8x 1 GBit, 2x 2.5 GBit NBaseT ports and
    2 SFP+ ports switch.
    
    Hardware:
    	- RTL9302 SoC: MIPS 34Kc @ 800MHz
    	- 128 MByte DRAM
    	- 16MBit NOR Flash MX25L12833F
    	- RTL8218D 8x GBit PHY
    	- 2x RTL8226 2.5 GBit PHYs
    	- 2x SFP+ Slots
    	- 1 Reset button
    	- Externally accessible UART header at the right side:
    		Top-down: Vcc (3.3V), TX, RX, GND 115200 8N1
    	- System LED
    
    Installation:
    	Connect to the Serial Port, power on the device
    	Interrupt boot by pressing <ESC>
    	Enable network by entering
    	 > rtk network on
    	Upload initramfs via tftp into RAM:
    	 > tftpboot 0x84f00000 192.168.1.100:openwrt-realtek-rtl930x-zyxel_xgs1210-12-initramfs-kernel.bin
    	Start initramfs
    	 > bootm
    	Once OpenWRT is running, install the squashfs image to flash.
    
    Two images are available, an 'ideal' and preferred one, which does
    require a custom U-Boot binary (available from source), and one that
    keeps with the vendor partition layout, which does not offer use, but
    does cost space. The later is preferred for users who do not wish to
    re-flash their bootloader and wish to keep backwards compatibility at
    the cost of about ~2 MiB worth of flash.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -1,5 +1,29 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+define Device/zyxel_xgs1210-12
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93001210
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := XGS1210-12
+  IMAGE_SIZE := 15424k
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma
+endef
+TARGET_DEVICES += zyxel_xgs1210-12
+
+define Device/zyxel_xgs1210-12_vendor
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93001210
+  ZYXEL_VERS := ABTY
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := XGS1210-12_vendor
+  IMAGE_SIZE := 13312k
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | zyxel-vers $$$$(ZYXEL_VERS) | \
+                      uImage lzma
+endef
+TARGET_DEVICES += zyxel_xgs1210-12_vendor
+
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
   UIMAGE_MAGIC := 0x93001250
