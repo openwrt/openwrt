3579ee63bccc74f0d9b8761a7da091e5ecc9cf86
David Bauer <mail@david-bauer.net>
2022-04-25 01:00:49 +0200

    mpc85xx: set WS-AP3825i mac-address in preinit
    The bootloader does seem to not correctly patch in the MAC address for
    eth0 / eth1 in some cases. While the root cause is not known, manually
    applying the MAC-Address in preinit does not hurt.
    
    Reported-by: Tom Herbers <freifunk@tomherbers.de>
    Signed-off-by: David Bauer <mail@david-bauer.net>
    (cherry picked from commit c6d52515e009d96e0afbe77310bf172f113d0eef)

---
+++ target/linux/mpc85xx/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -0,0 +1,12 @@
+. /lib/functions.sh
+
+preinit_set_mac_address() {
+	case $(board_name) in
+	extreme-networks,ws-ap3825i)
+		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
+		ip link set dev eth1 address $(mtd_get_mac_ascii cfg1 eth1addr)
+		;;
+	esac
+}
+
+boot_hook_add preinit_main preinit_set_mac_address
