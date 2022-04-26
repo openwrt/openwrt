575ec7a4b1a085f243961ef13dd23f228a60e67e
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2022-04-26 18:51:32 -0300

    ath79: use rtl8366rb as a module
    It looks like rtl8366rb is used only by tplink_tl-wr1043nd-v1 and
    buffalo_wzr-hp-g300nh-rb. There is no need to have it built-in as it
    works as a loadable module.
    
    Tested both failsafe and normal boot on tl-wr1043nd-v1.
    buffalo_wzr-hp-g300nh-rb was not tested.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -581,6 +581,7 @@ TARGET_DEVICES += buffalo_wzr-hp-g300nh-rb
 define Device/buffalo_wzr-hp-g300nh-s
   $(Device/buffalo_wzr-hp-g300nh)
   DEVICE_MODEL := WZR-HP-G300NH (RTL8366S switch)
+  DEVICE_PACKAGES += kmod-switch-rtl8366rb
 endef
 TARGET_DEVICES += buffalo_wzr-hp-g300nh-s
 
