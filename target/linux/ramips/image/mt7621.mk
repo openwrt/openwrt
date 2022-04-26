4d0afbeafb8ec5dd3de3e78410f4ac2d6ae66312
Chen Minqiang <ptpt52@gmail.com>
2022-04-27 03:10:11 +0800

    independent: add lte-modem-xwrt-fm10-ax-nand

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2898,7 +2898,7 @@ define Device/xwrt_fm10-ax-nand
   DEVICE_VENDOR := XWRT
   DEVICE_MODEL := FM10-AX
   DEVICE_VARIANT := NAND
-  DEVICE_PACKAGES := kmod-mt7915-firmware kmod-usb3 kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-mt7915-firmware kmod-usb3 kmod-usb-ledtrig-usbport lte-modem-xwrt-fm10-ax-nand
 endef
 TARGET_DEVICES += xwrt_fm10-ax-nand
 
