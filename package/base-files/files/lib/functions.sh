openwrt/openwrt

commit 6d7272852e0b2634b2fa93a131ea8659ec87f079

base-files: add missing $IPKG_INSTROOT to restorecon call
Update to overlooked v2 version of Dominick Grift's patch.

Fixes: 5109bd164c ("base-files: address sed in-place without SELinux awareness")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/base-files/files/lib/functions.sh
@@ -386,7 +386,9 @@ group_add_user() {
 	echo "$grp" | grep -q ":$" && delim=""
 	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
 	sed -i "s/$grp/$grp$delim$2/g" ${IPKG_INSTROOT}/etc/group
-	selinuxenabled 2>/dev/null && restorecon /etc/group
+	if [ -x /usr/sbin/selinuxenabled ] && selinuxenabled; then
+		selinuxenabled 2>/dev/null && restorecon ${IPKG_INSTROOT}/etc/group
+	fi
 	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
 }
 

commit 7b07c3cff57f057d6780d34adeb23c06123732db

base-files: simplify restorecon logic
Remove forgotten redundant selinuxenabled call and skip the whole
thing in case $IPKG_INSTROOT is set as labels are anyway applied only
later on in fakeroot when squashfs is created.

Fixes: 6d7272852e ("base-files: add missing $IPKG_INSTROOT to restorecon call")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/base-files/files/lib/functions.sh
@@ -386,8 +386,8 @@ group_add_user() {
 	echo "$grp" | grep -q ":$" && delim=""
 	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
 	sed -i "s/$grp/$grp$delim$2/g" ${IPKG_INSTROOT}/etc/group
-	if [ -x /usr/sbin/selinuxenabled ] && selinuxenabled; then
-		selinuxenabled 2>/dev/null && restorecon ${IPKG_INSTROOT}/etc/group
+	if [ -z "$IPKG_INSTROOT" ] && [ -x /usr/sbin/selinuxenabled ] && selinuxenabled; then
+		restorecon /etc/group
 	fi
 	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
 }