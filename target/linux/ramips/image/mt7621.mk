064e7e57b483e6879de0facef4f1fce86ec4ad47
Piotr Dymacz <pepe2k@gmail.com>
2022-05-13 13:35:59 +0200

    ramips: fix booting on Samknows SK-WB8
    This fixes a well known "LZMA ERROR 1" error, reported previously on
    numerous of similar devices.
    
    Fixes: #9824
    Signed-off-by: Piotr Dymacz <pepe2k@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1461,6 +1461,7 @@ TARGET_DEVICES += renkforce_ws-wn530hp3-a
 
 define Device/samknows_whitebox-v8
   $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := SamKnows
   DEVICE_MODEL := Whitebox 8
