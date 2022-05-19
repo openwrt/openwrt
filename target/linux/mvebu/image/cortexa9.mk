89b7714cf65e432997ca3c738afbffbd73469956
Stefan Kalscheuer <stefan@stklcode.de>
2022-05-19 21:10:34 +0200

    kernel: add kmod-leds-turris-omnia
    Add support for LEDs of the CZ.NIC Turris Omnia using the upstream
    driver.
    
    There is no generic way to control the LEDs in UCI manner, however
    the kernel module is the first step to actually use the RGB LEDs in
    custom logic.
    
    Signed-off-by: Stefan Kalscheuer <stefan@stklcode.de>
    (removed DMARC notice, added driver to Turris Omnia, moved module
    recipe to target/linux/mvebu/modules.mk)
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>
    (cherry picked from commit f8fa38c13fcc3b4ce9a4dfc56d98e5188353afac)
    Reviewed-by: Robert Marko <robimarko@gmail.com>

--- target/linux/mvebu/image/cortexa9.mk
+++ target/linux/mvebu/image/cortexa9.mk
@@ -59,7 +59,7 @@ define Device/cznic_turris-omnia
   DEVICE_PACKAGES :=  \
     mkf2fs e2fsprogs kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 \
     wpad-basic-wolfssl kmod-ath9k kmod-ath10k-ct ath10k-firmware-qca988x-ct \
-    partx-utils kmod-i2c-mux-pca954x
+    partx-utils kmod-i2c-mux-pca954x kmod-leds-turris-omnia
   IMAGES := $$(DEVICE_IMG_PREFIX)-sysupgrade.img.gz omnia-medkit-$$(DEVICE_IMG_PREFIX)-initramfs.tar.gz
   IMAGE/$$(DEVICE_IMG_PREFIX)-sysupgrade.img.gz := boot-scr | boot-img | sdcard-img | gzip | append-metadata
   IMAGE/omnia-medkit-$$(DEVICE_IMG_PREFIX)-initramfs.tar.gz := omnia-medkit-initramfs | gzip
