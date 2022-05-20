fc11935209f43698fd8d87eb929ba1b144786d83
Birger Koblitz <git@birger-koblitz.de>
2022-05-20 14:09:36 +0200

    realtek: add support for Zyxel XS1930-10
    The Zyxel XS1900-10 is an 8 port 10G/5G/2.5G/1G/100M multi-gigabit switch.
    In addition it has 2 SFP+ slots for uplink.
    
    Hardware:
    	- RTL9313 SoC: InterAptiv MIPS dual core at 1000MHz
    	- RAM: Winbond W632GU6MB 256MiB DDR3
    	- Flash: Macronix MX25L25645G, 32MiB SPI NOR
    	- AQR813 8x Multi-GBit PHY
    	- 2x SFP+ Slots
    	- RTL8231 LED controller
    	- 74x052 dual channel 4x IO multiplexer
    	- 1 Reset button (hard reset, not configurable)
            - 1 Restore button
    	- Externally accessible UART header at the right side:
    		Front to back: GND RX, TX, Vcc (3.3V) 115200 8N1
    	- LEDs: PWR(green, not software configurable), SYS (green), Cloud (blue), Location (red)
    
    Installation:
    	Connect to the Serial Port using e.g picocom: "picocom -b 115200 /dev/ttyUSB0"
    	Power on the device
    	Interrupt boot by pressing <ESC>
    	Enable extended u-boot commands by entering
    	 > aten 1 C621E14A [or correct code for your MAC]
    	 > loady 0x84f00000 460800
    	   CTRL-a-x, change baud:
    	   picocom -b 460800 /dev/ttyUSB0
    	   <enter>
    	   CTRL-a-s
    	   enter initframfs image file name  <enter>
    	   wait for transfer done
    	   CTRL-a-x, change baud back:
    	   picocom -b 115200 /dev/ttyUSB0
    	   <ESC>
    	 > bootm 0x84f00000
    
             Instructions for permanent installation once OpenWRT is running, need to follow.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>
    
    fix

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -11,3 +11,16 @@ define Device/edgecore_ecs4125-10p
   DEVICE_PACKAGES += lua-rs232
 endef
 TARGET_DEVICES += edgecore_ecs4125-10p
+
+define Device/zyxel_xs1930-10
+  KERNEL_LOADADDR = 0x80220000
+  KERNEL_ENTRY = 0x80220000
+  ZYXEL_VERS := ABQE
+  SOC := rtl9313
+  IMAGE_SIZE := 14336k
+  DEVICE_MODEL := XS1930-10
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | gzip | zyxel-vers $$$$(ZYXEL_VERS) | \
+	uImage gzip
+  DEVICE_PACKAGES := ip-bridge ethtool
+endef
+TARGET_DEVICES += zyxel_xs1930-10
