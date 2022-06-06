c27279dc26a26b8ad054882d4c5f86a85526fd16
Yoonji Park <koreapyj@dcmys.kr>
2022-06-06 07:44:48 +0000

    mediatek: add support for ipTIME A6004MX Add basic support for ipTIME A6004MX.
    Hardware:
    SoC: MediaTek MT7629 Cortex-A7 (ARMv7 1.25GHz, Dual-Core)
    RAM: DDR3 128MB
    Flash: Macronix MX35LF1GE4AB (SPI-NAND 128MB)
    WiFi: MediaTek MT7761N (2.4GHz) / MediaTek MT7762N (5GHz) - no driver
    Ethernet: SoC (WAN) / MediaTek MT7531 (LAN x4)
    UART: [GND, RX, TX, 3.3V] (115200)
    
    Installation:
    - Flash recovery image with TFTP recovery
    
    Revert to stock firmware:
    - Flash stock firmware with TFTP recovery
    
    TFTP Recovery method:
    1. Unplug the router
    2. Hold the reset button and plug in
    3. Release when the power LED stops flashing and go off
    4. Set your computer IP address manually to 192.168.0.x / 255.255.255.0
    5. Flash image with TFTP client to 192.168.0.1
    
    Signed-off-by: Yoonji Park <koreapyj@dcmys.kr>

--- target/linux/mediatek/image/mt7629.mk
+++ target/linux/mediatek/image/mt7629.mk
@@ -7,3 +7,20 @@ define Device/mediatek_mt7629-rfb
   DEVICE_PACKAGES := swconfig
 endef
 TARGET_DEVICES += mediatek_mt7629-rfb
+
+define Device/iptime_a6004mx
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := A6004MX
+  DEVICE_DTS := mt7629-iptime-a6004mx
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 uboot-envtools
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL := $$(KERNEL) | pad-offset 128k 2048 | iptime-crc32 a6004mx
+  KERNEL_INITRAMFS := $$(KERNEL_INITRAMFS) | pad-offset 128k 2048 | iptime-crc32 a6004mx
+  IMAGES += recovery.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/recovery.bin := append-kernel | pad-to 128k | append-ubi | append-metadata
+endef
+TARGET_DEVICES += iptime_a6004mx
