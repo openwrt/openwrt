37d26efce22d966c82e66eb91b8e938f080e061f
Michael Pratt <mcpratt@pm.me>
2022-06-09 03:51:48 -0400

    build: move mkhash to tools and set as prerequisite
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -70,6 +70,7 @@ prereq-build: staging_dir/host/.prereq-build FORCE ;
 
 prepare-clean: FORCE
 	$(Q)rm -rf $(TOPDIR)/staging_dir/host/.prereq-build
+	$(Q)$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r tools/mkhash/clean
 
 tmpinfo-clean: FORCE
 	$(Q)rm -rf $(TOPDIR)/tmp/.*info $(TOPDIR)/tmp/info
@@ -186,6 +187,7 @@ kernel_xconfig: prepare_kernel_conf FORCE
 
 staging_dir/host/.prereq-build: include/prereq-build.mk
 	mkdir -p tmp
+	$(Q)$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r tools/mkhash/compile
 	$(Q)$(_SINGLE)$(NO_TRACE_MAKE) -j1 -r -f $(TOPDIR)/include/prereq-build.mk prereq 2>/dev/null || { \
 		echo "Prerequisite check failed. Use FORCE=1 to override."; \
 		false; \
