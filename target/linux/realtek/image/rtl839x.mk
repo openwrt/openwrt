b16f982cbcf39fda287e99231f911118231cd1b7
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2022-06-27 19:49:37 -0300

    realtek: add support for DGS-1210-52
    Hardware specification
    ----------------------
    
    * RTL8393M SoC, 1 MIPS 34Kc core @ 700MHz
    * 128MB DRAM
    * 32MB NOR Flash
    * 48 x 10/100/1000BASE-T ports
      - 6 x External PHY with 8 ports (RTL8218D)
    * 4 x Gigabit RJ45/SFP Combo ports
      - External PHY with 4 SFP ports (RTL8214FC) (accoding to u-boot)
    * Power LED
    * Reset button on front panel
    * UART (115200 8N1) via unpopulated standard 0.1" pin header marked J14
    
    The gpio-restart node is not required but it does reset the switch.
    
    The 4 combo ports were not stable and were commented out. The RTL8214FC
    is what the u-boot reported. However, the Linux kernel reported 49 and
    50 as "External RTL8393 SERDES" and 51 and 52 as "RTL8218B (external)".
    Those ports only work if u-boot initialize it (for example, loading
    initramfs image using one of those ports).
    
    The firmware recovery using U-Boot is broken for all DGS-1210 tested
    devices as pressing RESET does not trigger it (only if pressed from a
    running stock image)
    
    UART pinout
    -----------
    
    [o]ooo|J14
     | ||`------ GND
     | |`------- RX
     | `-------- TX
     `---------- Vcc (3V3)
    
    Installation using OEM upgrade
    ------------------------------
    
    1. Make sure you are running OEM firmware in image2 slot (logged as admin):
       - > config firmware image_id 2 boot_up
       - > reboot
    2. Install squashfs-factory_image1.bin to image1 using (logged as admin):
       - > download firmware_fromTFTP <tftpserver> factory_image1.bin
       - > config firmware image_id 1 boot_up
       - > reboot
    
    Installation using serial interface
    -----------------------------------
    
    1. Press Escape key during `Hit Esc key to stop autoboot` prompt
    2. Press CTRL+C keys to get into real U-Boot prompt
    3. Init network with `rtk network on` command
    4. Load image with `tftpboot 0x8f000000 openwrt-realtek-rtl839x-d-link_dgs-1210-52-initramfs-kernel.bin` command
    5. Boot the image with `bootm` command
    
    Once booted the initramfs, install the squashfs-sysupgrade.bin as a
    normal OpenWrt system.
    
    Dual-boot with stock firmware using writable u-boot-env
    -------------------------------------------------------
    
    From stock to OpenWrt / boot image 1 (CLI as admin):
       - > config firmware image_id 1 boot_up
       - > reboot
    
    From OpenWrt to stock / boot image 2: (shell as root)
       - # fw_setenv bootcmd 'run addargs ; bootm 0xb4e80000'
       - # fw_setenv image '/dev/mtdblock7'
       - # reboot
    
    Debrick using serial interface
    ------------------------------
    
    1. Press Escape key during `Hit Esc key to stop autoboot` prompt
    2. In a Windows PC, run 'D-Link Network Assistant v2.0.2.4'. It should
       detect the switch
    3. Flash the firmware.
    
    Back to stock firmware using dual-boot
    --------------------------------------
    
    If you have serial interface, you can change u-boot env vars
    interrupting the boot process. If not but you are running OpenWrt, you
    can dual-boot (as mentioned eariler) and skip to step 4:
    
    1. Press Escape key during `Hit Esc key to stop autoboot` prompt
    2. Press CTRL+C keys to get into real U-Boot prompt
    3. Boot the image 2:
       - set image /dev/mtdblock7; run addargs; bootm 0xb4e80000
    4. Once booted, log as admin and change the boot image to 2
       - > config firmware image_id 2 boot_up
       - > reboot
    5. After the boot, flash image1 with the vendor image
    
    Back to stock firmware using DNA
    --------------------------------
    
    1. From an OpenWrt:
       - # fw_setenv bootstop on
       - # reboot
    2. In a Windows PC, run 'D-Link Network Assistant v2.0.2.4'. It should
       detect the switch
    3. Flash the firmware.
    
    It has been developed and tested on device with F3 revision.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/realtek/image/rtl839x.mk
+++ target/linux/realtek/image/rtl839x.mk
@@ -1,5 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+define Device/d-link_dgs-1210-52
+  $(Device/d-link_dgs-1210)
+  SOC := rtl8393
+  DEVICE_MODEL := DGS-1210-52
+  DEVICE_PACKAGES += uboot-envtools
+endef
+TARGET_DEVICES += d-link_dgs-1210-52
+
 define Device/zyxel_gs1900-48
   SOC := rtl8393
   IMAGE_SIZE := 13952k
