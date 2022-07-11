8e5b473bc1f7f1a8ad796e8b8cc7587fedbad9f5
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2022-07-11 16:09:16 -0300

    realtek: move common DGS-1210 def to Makefile
    The same parameters for DGS-1210 based on rtl838x SoC are shared with
    models with rtl839x SoC.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/realtek/image/rtl838x.mk
+++ target/linux/realtek/image/rtl838x.mk
@@ -11,24 +11,9 @@ define Device/allnet_all-sg8208m
 endef
 TARGET_DEVICES += allnet_all-sg8208m
 
-define Device/d-link_dgs-1210
-  SOC := rtl8382
-  IMAGE_SIZE := 13824k
-  DEVICE_VENDOR := D-Link
-  DLINK_KERNEL_PART_SIZE := 1572864
-  KERNEL := kernel-bin | append-dtb | gzip | uImage gzip | dlink-cameo
-  CAMEO_KERNEL_PART := 2
-  CAMEO_ROOTFS_PART := 3
-  CAMEO_CUSTOMER_SIGNATURE := 2
-  CAMEO_BOARD_VERSION := 32
-  IMAGES += factory_image1.bin
-  IMAGE/factory_image1.bin := append-kernel | pad-to 64k | \
-	append-rootfs | pad-rootfs | pad-to 16 | check-size | \
-	dlink-version | dlink-headers
-endef
-
 define Device/d-link_dgs-1210-10p
   $(Device/d-link_dgs-1210)
+  SOC := rtl8382
   DEVICE_MODEL := DGS-1210-10P
   DEVICE_PACKAGES += lua-rs232
 endef
@@ -36,18 +21,21 @@ TARGET_DEVICES += d-link_dgs-1210-10p
 
 define Device/d-link_dgs-1210-16
   $(Device/d-link_dgs-1210)
+  SOC := rtl8382
   DEVICE_MODEL := DGS-1210-16
 endef
 TARGET_DEVICES += d-link_dgs-1210-16
 
 define Device/d-link_dgs-1210-20
   $(Device/d-link_dgs-1210)
+  SOC := rtl8382
   DEVICE_MODEL := DGS-1210-20
 endef
 TARGET_DEVICES += d-link_dgs-1210-20
 
 define Device/d-link_dgs-1210-28
   $(Device/d-link_dgs-1210)
+  SOC := rtl8382
   DEVICE_MODEL := DGS-1210-28
 endef
 TARGET_DEVICES += d-link_dgs-1210-28
