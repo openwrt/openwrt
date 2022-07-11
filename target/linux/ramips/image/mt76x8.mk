63b93e135c1ee87b9fe5793bbd47acb68b9bec42
Russ Innes <russ.innes@gmail.com>
2022-07-11 10:29:30 -0600

    ramips: add support for Asus RT-N600
    The ASUS RT-N600 Wireless Router is based on the MT7628AN SoC.
    
    This is a config "clone" of the ASUS RT-AC1200v1, with a change to the model name definition basically for the end-user. This came via some trial and error with the AC1200 image discussed in #10083. The RT-N600 functions perfectly using the image from the AC1200, less the incorrect model definition.
    
    CPU: MediaTek MT7628AN @580MHz WiSoC (2T2R 802.11bgn)
    WLAN: MediaTek MT7612EN (2T2R 802.11an -> 802.11an/ac)
    RAM: 64MB (DDR2), Flash 16MB (SPI, 16-pin)
    USB 2.0
    LED (ETHx4, WLANx2, USB, Power)
    PORTS: 4x 100M RJ45 LAN, 1x 100M WAN
    Initial Flashing:
    
    Download the "Asus Firmware Recovery Tool" from the Asus website
    Set a static IP of 192.168.1.10 on a wired connection (Netmask 255.255.255.0)
    Load the desired firmware file in the recovery tool (.bin, .trx, etc)
    Power on the device while holding the reset button (Power LED will begin flashing when ready)
    Update the firmware (recovery) in the software
    
    Signed-off-by: Russ Innes russ.innes@gmail.com

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -38,6 +38,15 @@ define Device/alfa-network_awusfree1
 endef
 TARGET_DEVICES += alfa-network_awusfree1
 
+define Device/asus_rt-n600
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := Asus
+  DEVICE_MODEL := RT-N600
+  DEVICE_PACKAGES := kmod-mt76x2 kmod-usb2 kmod-usb-ohci \
+	kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += asus_rt-n600
+
 define Device/asus_rt-ac1200
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Asus
