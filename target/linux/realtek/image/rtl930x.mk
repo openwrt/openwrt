fb2552c385b5162c56d55bcf04d00fcf58ece137
Olliver Schinagl <oliver@schinagl.nl>
2022-07-26 09:27:24 +0200

    realtek/rtl930x: Migrate XGS1250-12 to common devicetree
    With the newly introduced shared devicetree for the XGS12xx series, also
    switch the XGS1250-12 to use the shared devicetree.
    
    While here, clean up a small ugliness, without changing actual
    functionality.
    
    Note, that due to the weird I2C muxing, there's some overlap there that
    will be addressed in the future with a proper I2C driver that properly
    handles this.
    
    This change also includes a build to keep vendor U-Boot/flash layout
    compatibility. The preferred method is to use the update U-Boot SPL.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -51,12 +51,23 @@ TARGET_DEVICES += zyxel_xgs1210-12_vendor
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
   UIMAGE_MAGIC := 0x93001250
-  ZYXEL_VERS := ABWE
   DEVICE_VENDOR := Zyxel
   DEVICE_MODEL := XGS1250-12
+  IMAGE_SIZE := 15424k
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma
+endef
+TARGET_DEVICES += zyxel_xgs1250-12
+
+define Device/zyxel_xgs1250-12_vendor
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93001250
+  ZYXEL_VERS := ABWE
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := XGS1250-12_vendor
   IMAGE_SIZE := 13312k
   KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
   KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | zyxel-vers $$$$(ZYXEL_VERS) | \
                       uImage lzma
 endef
-TARGET_DEVICES += zyxel_xgs1250-12
+TARGET_DEVICES += zyxel_xgs1250-12_vendor
