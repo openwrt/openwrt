2b03f207e038ec2ba8f57f55cc93e211af2e1a75
Leo Soares <leo@hyper.ag>
2022-07-31 15:21:42 +0100

    kernel: add kmod-usb-hid-mcp2221
    "The MCP2221 is a USB-to-UART/I2C serial converter which enables
    USB connectivity in applications that have a UART and I2C interfaces."
    <https://www.microchip.com/en-us/product/MCP2221>
    
    Signed-off-by: Leo Soares <leo@hyper.ag>
    (replaced GPIOLIB KConfig with @GPIO_SUPPORT)
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1555,6 +1555,23 @@ endef
 $(eval $(call KernelPackage,usb-hid-cp2112))
 
 
+define KernelPackage/usb-hid-mcp2221
+  SUBMENU:=$(USB_MENU)
+  TITLE:=Microchip USB 2.0 to I2C/UART Protocol Converter with GPIO
+  KCONFIG:=CONFIG_HID_MCP2221
+  DEPENDS:=@GPIO_SUPPORT +kmod-usb-hid +kmod-i2c-core
+  FILES:=$(LINUX_DIR)/drivers/hid/hid-mcp2221.ko
+  AUTOLOAD:=$(call AutoProbe,hid-mcp2221)
+endef
+
+define KernelPackage/usb-hid-mcp2221/description
+ HID device driver which registers as an i2c adapter and gpiochip to expose
+ these functions of the MCP2221.
+endef
+
+$(eval $(call KernelPackage,usb-hid-mcp2221))
+
+
 define KernelPackage/usb-yealink
   TITLE:=USB Yealink VOIP phone
   DEPENDS:=+kmod-input-evdev
