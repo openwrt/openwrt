6133424f1675595b174fb43ca317543078983571
Lu Jiale <jialex.lu@intel.com>
2022-08-03 08:26:45 +0000

    x86/64: Add Intel SOC Arizona Beach platform.
    Arizona Beach is a derivative of the Alder Lake-M series product targeted specifically at networking applications.
    Nexsec MT110 is a uCPE power by Intel Atom SOC Arizona Beach
    
    Signed-off-by: Lu Jiale <jialex.lu@intel.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -7,4 +7,15 @@ define Device/generic
 	kmod-tg3
   GRUB2_VARIANT := generic
 endef
+
 TARGET_DEVICES += generic
+
+define Device/AZB_MT110
+  DEVICE_VENDOR := Intel Arizona Beach (NEXSEC MT110)
+  DEVICE_PACKAGES += \
+	kmod-e1000e kmod-e1000 kmod-fs-vfat kmod-igb kmod-ixgbe kmod-igc kmod-i40e \
+	kmod-iwlwifi kmod-usb-net-rndis kmod-usb-net-cdc-mbim kmod-i2c-i801
+  GRUB2_VARIANT := generic
+endef
+
+TARGET_DEVICES += AZB_MT110
