719f12d0f3fc26b986b84bfba1ecdb2befdc8e89
Oscar Molnar <oscar@tymscar.com>
2022-08-06 16:13:41 +0100

    build: add support for python3.11 and higher
    python3.11 beta is out but fails to run the makefile currently
    this supports python versions from 3.6 to 3.99 with the python3 binary
    it also checks specifically for 3.11 as it is the latest version out
    
    Signed-off-by: Oscar Molnar <oscar@tymscar.com>
    (cherry picked from commit a9e8eec221874e5840d659aed3f68850ff1d9982)
    Signed-off-by: Michal Vasilek <michal.vasilek@nic.cz> [rebase for 21.02]

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -153,22 +153,24 @@ $(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
 $(eval $(call CleanupPython2))
 
 $(eval $(call SetupHostCommand,python,Please install Python >= 3.5, \
+	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
 	python3.8 -V 2>&1 | grep 'Python 3', \
 	python3.7 -V 2>&1 | grep 'Python 3', \
 	python3.6 -V 2>&1 | grep 'Python 3', \
 	python3.5 -V 2>&1 | grep 'Python 3', \
-	python3 -V 2>&1 | grep -E 'Python 3\.([5-9]|10)\.?'))
+	python3 -V 2>&1 | grep -E 'Python 3\.([5-9]|[0-9][0-9])\.?'))
 
 $(eval $(call SetupHostCommand,python3,Please install Python >= 3.5, \
+	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
 	python3.8 -V 2>&1 | grep 'Python 3', \
 	python3.7 -V 2>&1 | grep 'Python 3', \
 	python3.6 -V 2>&1 | grep 'Python 3', \
 	python3.5 -V 2>&1 | grep 'Python 3', \
-	python3 -V 2>&1 | grep -E 'Python 3\.([5-9]|10)\.?'))
+	python3 -V 2>&1 | grep -E 'Python 3\.([5-9]|[0-9][0-9])\.?'))
 
 $(eval $(call TestHostCommand,python3-distutils, \
 	Please install the Python3 distutils module, \
