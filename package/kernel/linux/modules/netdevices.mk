openwrt/openwrt

commit 2519190fec9b6e16e307f88e0825f4080c399886

kernel: package mhi wwan ctrl driver
MHI WWAN CTRL allows QCOM-based PCIe modems to expose different modem
control protocols/ports to userspace, including AT, MBIM, QMI, DIAG
and FIREHOSE. These protocols can be accessed directly from userspace
(e.g. AT commands) or via libraries/tools (e.g. libmbim, libqmi, libqcdm)

Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

package/kernel/linux/modules/netdevices.mk
@@ -1330,3 +1330,19 @@ define KernelPackage/mhi-net/description
 endef
 
 $(eval $(call KernelPackage,mhi-net))
+
+define KernelPackage/mhi-wwan-ctrl
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=MHI WWAN Control
+  DEPENDS:=@LINUX_5_15 @PCI_SUPPORT +kmod-mhi-bus
+  KCONFIG:=CONFIG_MHI_WWAN_CTRL
+  FILES:=$(LINUX_DIR)/drivers/net/mhi_wwan_ctrl.ko
+  AUTOLOAD:=$(call AutoProbe,mhi_wwan_ctrl)
+endef
+
+define KernelPackage/mhi-wwan-ctrl/description
+ Driver for MHI WWAN Control
+ This exposes all modem control ports like AT, MBIM, QMI, DIAG, ..
+endef
+
+$(eval $(call KernelPackage,mhi-wwan-ctrl))

commit badb0cfb561e3676c309d8fb98f262bac66fc07e

kernel: kmod-wwan: Fix path of wwan drivers
The wwan drivers are in the wwan sub folder, add this missing folder
name.

Fixes: 2519190fec9b "(kernel: package mhi wwan ctrl driver")
Fixes: 6af46796faf9 "(kernel: package mhi mbim driver")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/netdevices.mk
@@ -1381,7 +1381,7 @@ define KernelPackage/mhi-wwan-ctrl
   TITLE:=MHI WWAN Control
   DEPENDS:=@LINUX_5_15 @PCI_SUPPORT +kmod-mhi-bus
   KCONFIG:=CONFIG_MHI_WWAN_CTRL
-  FILES:=$(LINUX_DIR)/drivers/net/mhi_wwan_ctrl.ko
+  FILES:=$(LINUX_DIR)/drivers/net/wwan/mhi_wwan_ctrl.ko
   AUTOLOAD:=$(call AutoProbe,mhi_wwan_ctrl)
 endef
 
@@ -1397,7 +1397,7 @@ define KernelPackage/mhi-wwan-mbim
   TITLE:=MHI MBIM
   DEPENDS:=@LINUX_5_15 @PCI_SUPPORT +kmod-mhi-bus
   KCONFIG:=CONFIG_MHI_WWAN_MBIM
-  FILES:=$(LINUX_DIR)/drivers/net/mhi_wwan_mbim.ko
+  FILES:=$(LINUX_DIR)/drivers/net/wwan/mhi_wwan_mbim.ko
   AUTOLOAD:=$(call AutoProbe,mhi_wwan_mbim)
 endef
 