431526be7c3a9a1ee981c99496f610c23d6ea73d
Nick Hainke <vincent@systemli.org>
2022-08-23 19:14:47 +0200

    ath79: move 5.15 testing kernel to common Makefile
    All subtargets are using now 5.15 as testing kernel.
    Move KERNEL_TESTING_PATCHVER:=5.15 to the common Makefile.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- target/linux/ath79/nand/target.mk
+++ target/linux/ath79/nand/target.mk
@@ -4,8 +4,6 @@ FEATURES += nand
 
 DEFAULT_PACKAGES += wpad-basic-wolfssl
 
-KERNEL_TESTING_PATCHVER:=5.15
-
 define Target/Description
 	Firmware for boards using Qualcomm Atheros, MIPS-based SoCs
 	in the ar72xx and subsequent series, with support for NAND flash
--- target/linux/ath79/tiny/target.mk
+++ target/linux/ath79/tiny/target.mk
@@ -3,8 +3,6 @@ FEATURES += low_mem small_flash
 
 DEFAULT_PACKAGES += wpad-basic-wolfssl
 
-KERNEL_TESTING_PATCHVER:=5.15
-
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x/AR934x based boards with small flash
 endef
