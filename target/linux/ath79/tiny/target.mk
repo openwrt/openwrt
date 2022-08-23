ae6bfb7d67c12a4e9d2c1e3f6366de5cef7b61e3
Nick Hainke <vincent@systemli.org>
2022-08-23 18:44:21 +0200

    ath79: tiny: add 5.15 support for tiny subtarget
    Tested on Ubiquiti Nanostation M5 XM with low_mem.
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- target/linux/ath79/tiny/target.mk
+++ target/linux/ath79/tiny/target.mk
@@ -3,6 +3,8 @@ FEATURES += low_mem small_flash
 
 DEFAULT_PACKAGES += wpad-basic-wolfssl
 
+KERNEL_TESTING_PATCHVER:=5.15
+
 define Target/Description
 	Build firmware images for Atheros AR71xx/AR913x/AR934x based boards with small flash
 endef
