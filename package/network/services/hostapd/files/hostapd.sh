2984a0420649733662ff95b0aff720b8c2c19f8a
Felix Fietkau <nbd@nbd.name>
2022-08-30 10:57:26 +0200

    mac80211: disable ft-over-ds by default
    Testing has shown it to be very unreliable in variety of configurations.
    It is not mandatory, so let's disable it by default until we have a better
    solution.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -889,7 +889,7 @@ hostapd_set_bss_options() {
 			json_get_vars mobility_domain ft_psk_generate_local ft_over_ds reassociation_deadline
 
 			set_default mobility_domain "$(echo "$ssid" | md5sum | head -c 4)"
-			set_default ft_over_ds 1
+			set_default ft_over_ds 0
 			set_default reassociation_deadline 1000
 
 			case "$auth_type" in
