787abcd6b491cd422773cd1896900db69df4ca02
Shiji Yang <yangshiji66@qq.com>
2022-09-16 01:10:52 +0800

    ramips: add support for SIM SIMAX1800T and Haier HAR-20S2U1
    SIM AX18T and Haier HAR-20S2U1 Wi-Fi6 AX1800 routers are designed based
    on Tenbay WR1800K. They have the same hardware circuits and u-boot.
    SIM AX18T has three carrier customized models: SIMAX1800M (China Mobile),
    SIMAX1800T (China Telecom) and SIMAX1800U (China Unicom). All of these
    models run the same firmware.
    
    Specifications:
     SOC:      MT7621 + MT7905 + MT7975
     ROM:      128 MiB
     RAM:      256 MiB
     LED:      status *3 R/G/B
     Button:   reset *1 + wps/mesh *1
     Ethernet:      lan *3 + wan *1 (10/100/1000Mbps)
     TTL Baudrate:  115200
     TFTP Server:   192.168.1.254
     TFTP IP:       192.168.1.28 or 192.168.1.160 (when envs is broken)
    
    MAC Address:
     use        address               source
     label      30:xx:xx:xx:xx:62     wan
     lan        30:xx:xx:xx:xx:65     factory.0x8004
     wan        30:xx:xx:xx:xx:62     factory.0x8004 -3
     wlan2g     30:xx:xx:xx:xx:64     factory.0x0004
     wlan5g     32:xx:xx:xx:xx:64     factory.0x0004 set 7th bit
    
    TFTP Installation (initramfs image only & recommend):
    1. Set local tftp server IP: 192.168.1.254 and NetMask: 255.255.255.0
    2. Rename initramfs-kernel.bin to "factory.bin" and put it in the root
       directory of the tftp server. (tftpd64 is a good choice for Windows)
    3. Start the TFTP server, plug in the power supply, and wait for the
       system to boot.
    4. Backup "firmware" partition and rename it to "firmware.bin", we need
       it to back to stock firmware.
    5. Use "fw_printenv" command to list envs.
       If "firmware_select=2" is observed then set u-boot enviroment:
       /# fw_setenv firmware_select 1
    6. Apply sysupgrade.bin in OpenWrt LuCI.
    
    Web UI Installation:
    1. Apply update by uploading initramfs-factory.bin to the web UI.
    2. Use "fw_printenv" command to list envs.
       If "firmware_select=2" is observed then set u-boot enviroment:
       /# fw_setenv firmware_select 1
    3. Apply squashfs-sysupgrade.bin in OpenWrt LuCI.
    
    Recovery to stock firmware:
    a. Upload "firmware.bin" to OpenWrt /tmp, then execute:
       /# mtd -r write /tmp/firmware.bin firmware
    b. We can also write factory image "UploadBrush-bin.img" to firmware
       partition to recovery. Upload image file to /tmp, then execute:
       /# mtd erase firmware
       /# mtd -r write /tmp/UploadBrush-bin.img firmware
    
    How to extract stock firmware image:
      Download stock firmware, then use openssl:
      openssl aes-256-cbc -d -salt -in [Downloaded_Firmware] \
      -out "firmware.tar.tgz" -k QiLunSmartWL
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -65,6 +65,7 @@ platform_do_upgrade() {
 	h3c,tx1800-plus|\
 	h3c,tx1801-plus|\
 	h3c,tx1806|\
+	haier,har-20s2u1|\
 	hiwifi,hc5962|\
 	iptime,a3004t|\
 	iptime,ax2004m|\
@@ -95,6 +96,7 @@ platform_do_upgrade() {
 	raisecom,msg1500-x-00|\
 	sercomm,na502|\
 	sercomm,na502s|\
+	sim,simax1800t|\
 	xiaomi,mi-router-3g|\
 	xiaomi,mi-router-3-pro|\
 	xiaomi,mi-router-4|\
