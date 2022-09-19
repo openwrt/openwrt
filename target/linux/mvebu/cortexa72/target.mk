da458ee631b828114f3b9bc845f83b4ca2f0189a
Eneas U de Queiroz <cotequeiroz@gmail.com>
2022-09-19 18:01:39 -0300

    mvebu/cortexa72: default to libwolfsslcpu-crypto
    This adds libwolfsslcpu-crypto to DEFAULT_PACKAGES.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- target/linux/mvebu/cortexa72/target.mk
+++ target/linux/mvebu/cortexa72/target.mk
@@ -8,6 +8,6 @@ ARCH:=aarch64
 BOARDNAME:=Marvell Armada 7k/8k (ARM64)
 CPU_TYPE:=cortex-a72
 FEATURES+=ext4
-DEFAULT_PACKAGES+=e2fsprogs ethtool mkf2fs partx-utils
+DEFAULT_PACKAGES+=e2fsprogs ethtool libwolfsslcpu-crypto mkf2fs partx-utils
 
 KERNELNAME:=Image dtbs
