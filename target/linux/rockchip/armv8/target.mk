79f727dda34abf657226b8aa94ac1af5e6340f32
Eneas U de Queiroz <cotequeiroz@gmail.com>
2022-09-19 18:01:39 -0300

    rockchip/armv8: default to libwolfsslcpu-crypto
    This adds libwolfsslcpu-crypto to DEFAULT_PACKAGES.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- target/linux/rockchip/armv8/target.mk
+++ target/linux/rockchip/armv8/target.mk
@@ -1,6 +1,7 @@
 ARCH:=aarch64
 SUBTARGET:=armv8
 BOARDNAME:=RK33xx boards (64 bit)
+DEFAULT_PACKAGES+=libwolfsslcpu-crypto
 
 define Target/Description
 	Build firmware image for Rockchip RK33xx devices.
