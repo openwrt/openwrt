af028ed393ee6e137010adb296effeb62c25a4ad
Szabolcs Hubai <szab.hu@gmail.com>
2022-09-28 17:40:12 +0200

    ramips: enable ramdisk for rt3883
    To test the new seama-lzma-loader's ramdisk on D-Link DIR-645.
    
    Tested-by: Glenn Fowler <gfowler1@outlook.com>
    Signed-off-by: Szabolcs Hubai <szab.hu@gmail.com>

--- target/linux/ramips/rt3883/target.mk
+++ target/linux/ramips/rt3883/target.mk
@@ -4,7 +4,7 @@
 
 SUBTARGET:=rt3883
 BOARDNAME:=RT3662/RT3883 based boards
-FEATURES+=usb pci small_flash
+FEATURES+=usb pci ramdisk small_flash
 CPU_TYPE:=74kc
 
 DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc wpad-basic-wolfssl swconfig
