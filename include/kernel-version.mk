b1c9eb42203aa3e637a68377a4942be2f760212f
mikrotik295 <114218195+mikrotik295@users.noreply.github.com>
2022-10-08 19:10:29 +0700

    Update kernel-version.mk
    Signed-off-by: mikrotik295 <114218195+mikrotik295@users.noreply.github.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-5.4 = .188
+LINUX_VERSION-5.4 = .215
 
-LINUX_KERNEL_HASH-5.4.188 = 3c5b1ea4df7d3b7efcc7bf88f6631102
+LINUX_KERNEL_HASH-5.4.215 = 3c5b1ea4df7d3b7efcc7bf88f6631102
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
