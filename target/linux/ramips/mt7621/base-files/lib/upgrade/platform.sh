8ec34abd7cbfe9ad2eb16c6faf84fdde28c11e65
Oleg S <remittor@gmail.com>
2022-10-09 17:21:36 +0300

    ramips: mt7621: Add support revert to stock firmware for Xiaomi routers
    This patch allows using Luci do revert to stock firmware for few Xiaomi
    routers based on mt7621.
    This patch also allows you to install OpenWrt factory firmware
    (example: mi router 3 pro).
    Also, this patch allows you to completely refuse to edit the parameters
    that are responsible for the Dual Boot.
    
    Signed-off-by: Oleg S <remittor@gmail.com>

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -95,14 +95,9 @@ platform_do_upgrade() {
 	raisecom,msg1500-x-00|\
 	sercomm,na502|\
 	sercomm,na502s|\
-	xiaomi,mi-router-3g|\
-	xiaomi,mi-router-3-pro|\
-	xiaomi,mi-router-4|\
-	xiaomi,mi-router-ac2100|\
 	xiaomi,mi-router-cr6606|\
 	xiaomi,mi-router-cr6608|\
 	xiaomi,mi-router-cr6609|\
-	xiaomi,redmi-router-ac2100|\
 	zyxel,nwa50ax|\
 	zyxel,nwa55axe)
 		nand_do_upgrade "$1"
@@ -121,6 +116,13 @@ platform_do_upgrade() {
 	ubnt,edgerouter-x|\
 	ubnt,edgerouter-x-sfp)
 		platform_upgrade_ubnt_erx "$1"
+		;;
+	xiaomi,mi-router-3g|\
+	xiaomi,mi-router-3-pro|\
+	xiaomi,mi-router-4|\
+	xiaomi,mi-router-ac2100|\
+	xiaomi,redmi-router-ac2100)
+		xiaomi_do_upgrade "$board" "$1"
 		;;
 	zyxel,lte3301-plus|\
 	zyxel,nr7101)
