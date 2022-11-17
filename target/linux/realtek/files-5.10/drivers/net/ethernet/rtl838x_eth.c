7c25cdca7b4bab86d779c05b7f93cac16895564b
Markus Stockhausen <markus.stockhausen@gmx.de>
2022-11-17 21:19:30 +0100

    realtek: Improve SMI polling configuration based on .dts
    Improves error detection of the association of smi addresses to ports
    on RTL93xx. Allows empty smi addresses for ports using a PHY linked to an
    internal SerDes. This patch is very important as it makes the ethernet
    interface of the switch available from outside world.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

--- target/linux/realtek/files-5.10/drivers/net/ethernet/rtl838x_eth.c
+++ target/linux/realtek/files-5.10/drivers/net/ethernet/rtl838x_eth.c
@@ -118,7 +118,7 @@ struct rtnc_priv {
 	u32			lastEvent;
 	u16			rxrings;
 	u16			rxringlen;
-	u8			smi_bus[RTNC_MAX_PORTS];
+	int			smi_bus[RTNC_MAX_PORTS];
 	u8			smi_addr[RTNC_MAX_PORTS];
 	u32			sds_id[RTNC_MAX_PORTS];
 	bool			smi_bus_isc45[RTNC_MAX_SMI_BUSSES];
@@ -455,7 +455,7 @@ static int rtmd_930x_reset(struct mii_bus *bus)
 	/* Mapping of port to phy-addresses on an SMI bus */
 	poll_sel[0] = poll_sel[1] = 0;
 	for (i = 0; i < RTL930X_CPU_PORT; i++) {
-		if (priv->smi_bus[i] > 3)
+		if ((priv->smi_bus[i] < 0) || (priv->smi_bus[i] > RTNC_MAX_SMI_BUSSES))
 			continue;
 		pos = (i % 6) * 5;
 		sw_w32_mask(0x1f << pos, priv->smi_addr[i] << pos,
@@ -567,7 +567,9 @@ static int rtmd_931x_reset(struct mii_bus *bus)
 	mdc_on[0] = mdc_on[1] = mdc_on[2] = mdc_on[3] = false;
 	/* Mapping of port to phy-addresses on an SMI bus */
 	poll_sel[0] = poll_sel[1] = poll_sel[2] = poll_sel[3] = 0;
-	for (i = 0; i < 56; i++) {
+	for (i = 0; i < RTL931X_CPU_PORT; i++) {
+		if ((priv->smi_bus[i] < 0) || (priv->smi_bus[i] > RTNC_MAX_SMI_BUSSES))
+			continue;
 		pos = (i % 6) * 5;
 		sw_w32_mask(0x1f << pos, priv->smi_addr[i] << pos, RTL931X_SMI_PORT_ADDR + (i / 6) * 4);
 		pos = (i * 2) % 32;
@@ -676,13 +678,17 @@ static int rtmd_init(struct rtnc_priv *priv)
 	for_each_node_by_name(dn, "ethernet-phy") {
 		u32 smi_addr[2];
 
-		if (of_property_read_u32(dn, "reg", &pn))
+		if (of_property_read_u32(dn, "reg", &pn)) {
+			pr_err("%s: missing reg property on port %d\n", __func__, pn);
 			continue;
-
-		if (of_property_read_u32_array(dn, "rtl9300,smi-address", &smi_addr[0], 2)) {
-			smi_addr[0] = 0;
-			smi_addr[1] = pn;
 		}
+
+		if (pn >= RTNC_MAX_PORTS) {
+			pr_err("%s: illegal port number %d\n", __func__, pn);
+			continue;
+		}
+
+		priv->phy_is_internal[pn] = of_property_read_bool(dn, "phy-is-integrated");
 
 		if (of_property_read_u32(dn, "sds", &priv->sds_id[pn]))
 			priv->sds_id[pn] = -1;
@@ -690,19 +696,28 @@ static int rtmd_init(struct rtnc_priv *priv)
 			pr_info("set sds port %d to %d\n", pn, priv->sds_id[pn]);
 		}
 
-		if (pn < RTNC_MAX_PORTS) {
+		if (of_property_read_u32_array(dn, "rtl9300,smi-address", &smi_addr[0], 2)) {
+			/* Integrated PHYs associated to a SerDes do not have an smi_bus */
+			if (priv->phy_is_internal[pn] && priv->sds_id[pn] >= 0) {
+				priv->smi_bus[pn] = -1;
+			/* PHYs whether integrated or not, not associated to an SDS use an smi_bus */
+			} else {
+				/* For RTL83xx, PHY-id is port ID on smi_bus 0 */
+				priv->smi_bus[pn] = 0;
+				priv->smi_addr[pn] = pn;
+			}
+		} else {
 			priv->smi_bus[pn] = smi_addr[0];
 			priv->smi_addr[pn] = smi_addr[1];
-		} else {
-			pr_err("%s: illegal port number %d\n", __func__, pn);
 		}
 
-		if (of_device_is_compatible(dn, "ethernet-phy-ieee802.3-c45"))
-			priv->smi_bus_isc45[smi_addr[0]] = true;
-
-		if (of_property_read_bool(dn, "phy-is-integrated")) {
-			priv->phy_is_internal[pn] = true;
+		if (priv->smi_bus[pn] > RTNC_MAX_SMI_BUSSES) {
+			pr_err("%s: illegal SMI bus number %d\n", __func__, priv->smi_bus[pn]);
+			continue;
 		}
+
+		if (priv->smi_bus[pn] >= 0)
+			priv->smi_bus_isc45[priv->smi_bus[pn]] = !!of_device_is_compatible(dn, "ethernet-phy-ieee802.3-c45");
 	}
 
 	dn = of_find_compatible_node(NULL, NULL, "realtek,rtl83xx-switch");
