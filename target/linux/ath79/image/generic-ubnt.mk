844de50b7bdef52a596ae1328c59ef3dbc6d0dfc
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2022-11-18 16:39:27 +0100

    ath79: disable image building for Ubiquiti EdgeSwitch 8XP
    The downstream OpenWrt driver for the BCM53128 switch ceased to work,
    rendering the 8 LAN ports of the device unusable. This commit disables
    image building while the problem is being solved.
    
    See issue #10374 for more details.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>
    (cherry picked from commit 5a1d7d8c1b422827673b13a034473683f5af3d6f)

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -43,6 +43,7 @@ define Device/ubnt_edgeswitch-8xp
   $(Device/ubnt-sw)
   DEVICE_MODEL := EdgeSwitch 8XP
   DEVICE_PACKAGES += kmod-switch-bcm53xx-mdio
+  DEFAULT := n
 endef
 TARGET_DEVICES += ubnt_edgeswitch-8xp
 
