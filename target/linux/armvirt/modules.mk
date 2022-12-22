3eb25435af354b84eaac35de9bae851857ff27ce
Anton Antonov <Anton.Antonov@arm.com>
2022-12-22 12:00:12 +0000

    armvirt: 64: Add NXP i.MX 8M Mini/Nano/Quad/Plus EVK support
    Also includes Advantech RSB-3720 (iMX8 Plus) support.
    
    Signed-off-by: Anton Antonov <Anton.Antonov@arm.com>
    Signed-off-by: Mathew McBride <matt@traverse.com.au>
    [Re-sort into kernel config, move network into modules]
    (23.05/5.15 version of commit 3efb3b801bb1393897ff58b9af3753157f28f441)

--- target/linux/armvirt/modules.mk
+++ target/linux/armvirt/modules.mk
@@ -25,6 +25,29 @@ endef
 
 $(eval $(call KernelPackage,fsl-pcs-lynx))
 
+define KernelPackage/pcs-xpcs
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Synopsis DesignWare PCS driver
+  DEPENDS:=@(TARGET_armvirt_64)
+  KCONFIG:=CONFIG_PCS_XPCS
+  FILES:=$(LINUX_DIR)/drivers/net/pcs/pcs_xpcs.ko
+  AUTOLOAD:=$(call AutoLoad,20,pcs_xpcs)
+endef
+
+$(eval $(call KernelPackage,pcs-xpcs))
+
+define KernelPackage/fsl-fec
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio \
+    +kmod-ptp +kmod-net-selftests
+  TITLE:=NXP (Freescale) FEC Ethernet controller (i.MX)
+  KCONFIG:=CONFIG_FEC
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/freescale/fec.ko
+  AUTOLOAD:=$(call AutoLoad,35,fec)
+endef
+
+$(eval $(call KernelPackage,fsl-fec))
+
 define KernelPackage/fsl-xgmac-mdio
   SUBMENU=$(NETWORK_DEVICES_MENU)
   DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio +kmod-acpi-mdio
@@ -161,6 +184,54 @@ define KernelPackage/mvpp2
 endef
 
 $(eval $(call KernelPackage,mvpp2))
+
+define KernelPackage/imx2-wdt
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=NXP (Freescale) i.MX2+ and Layerscape watchdog driver
+  KCONFIG:=CONFIG_IMX2_WDT
+  FILES=$(LINUX_DIR)/drivers/watchdog/imx2_wdt.ko
+  AUTOLOAD=$(call AutoLoad,60,imx2_wdt)
+endef
+
+$(eval $(call KernelPackage,imx2-wdt))
+
+define KernelPackage/imx7-ulp-wdt
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=NXP (Freescale) i.MX7ULP and later watchdog
+  KCONFIG:=CONFIG_IMX7ULP_WDT
+  FILES=$(LINUX_DIR)/drivers/watchdog/imx7ulp_wdt.ko
+  AUTOLOAD=$(call AutoLoad,60,imx7ulp_wdt)
+endef
+
+$(eval $(call KernelPackage,imx7-ulp-wdt))
+
+define KernelPackage/stmmac-core
+  SUBMENU=$(NETWORK_DEVICES_MENU)
+  TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)
+  DEPENDS:=@(TARGET_armvirt_64) +kmod-phylink +kmod-pcs-xpcs +kmod-ptp
+  KCONFIG:=CONFIG_STMMAC_ETH \
+    CONFIG_STMMAC_SELFTESTS=n \
+    CONFIG_STMMAC_PLATFORM \
+    CONFIG_CONFIG_DWMAC_DWC_QOS_ETH=n \
+    CONFIG_DWMAC_GENERIC
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/stmmac.ko \
+    $(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/stmmac-platform.ko \
+    $(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-generic.ko
+  AUTOLOAD=$(call AutoLoad,40,stmmac stmmac-platform dwmac-generic)
+endef
+
+$(eval $(call KernelPackage,stmmac-core))
+
+define KernelPackage/dwmac-imx
+  SUBMENU=$(NETWORK_DEVICES_MENU)
+  TITLE:=NXP i.MX8 Ethernet controller
+  DEPENDS:=+kmod-stmmac-core
+  KCONFIG:=CONFIG_DWMAC_IMX8
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-imx.ko
+  AUTOLOAD=$(call AutoLoad,45,dwmac-imx)
+endef
+
+$(eval $(call KernelPackage,dwmac-imx))
 
 define KernelPackage/wdt-sp805
   SUBMENU:=$(OTHER_MENU)
