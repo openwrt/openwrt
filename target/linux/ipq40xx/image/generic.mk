4a3acb0cf684c9a28f83015a11aa2a5b9f02e28a
Vincent Tremblay <vincent@vtremblay.dev>
2022-12-23 12:30:36 -0500

    ipq40xx: add support for Linksys WHW03 V2
    SOC:             Qualcomm IPQ4019
    WiFi 1:          QCA4019 IEEE 802.11b/g/n
    WiFi 2:          QCA4019 IEEE 802.11a/n/ac
    WiFi 3:          QCA8888 IEEE 802.11a/n/ac
    Bluetooth:       Qualcomm CSR8811 (A12U)
    Zigbee:          Silicon Labs EM3581 NCP + Skyworks SE2432L
    Ethernet:        Qualcomm Atheros QCA8072 (2-port)
    Flash 1:         Mactronix MX30LF4G18AC-XKI
    RAM (NAND):      SK hynix H5TC4G63CFR-PBA (512MB)
    LED Controller:  NXP PCA9633 (I2C)
    Buttons:         Single reset button (GPIO).
    
    - The three WiFis were fully tested and are configured with the same settings as in the vendor firmware.
    - The specific board files were submitted to the ATH10k mailing list but I'm still waiting for a reply. They can be removed once they are approved upstream.
    - Two ethernet ports are accessible on the device. By default one is configured as WAN and the other one is LAN. They are fully working.
    
    Bluetooth:
    ========
    - Fully working with the following caveats:
      - RFKILL need to be enabled in the kernel.
      - An older version of bluez is needed as bccmd is needed to configure the chip.
    
    Zigbee:
    ======
    - The spidev device is available in the /dev directory.
    - GPIOs are configured the same way as in the vendor firmware.
    - Tests are on-going. I am working on getting access to the Silicon Labs stack to validate that it is fully working.
    
    Installation:
    =========
    The squash-factory image can be installed via the Linksys Web UI:
    1. Open "http://192.168.1.1/ca" (Change the IP with the IP of your device).
    2. Login with your admin password.
    3. To enter into the support mode, click on the "CA" link and the bottom of the page.
    4. Open the "Connectivity" menu and upload the squash-factory image with the "Choose file" button.
    5. Click start. Ignore all the prompts and warnings by click "yes" in all the popups.
    
    The device uses a dual partition mechanism. The device automatically revert to the previous partition after 3 failed boot attempts.
    If you want to force the previous firmware to load, you can turn off and then turn on the device for 2 seconds, 3 times in a row.
    
    It can also be done via TFTP:
    1. Setup a local TFTP server and configure its IP to 192.168.1.100.
    2. Rename your image to "nodes_v2.img" and put it to the TFTP root of your server.
    3. Connect to the device through the serial console.
    4. Power on device and press enter when prompted to drop into U-Boot.
    5. Flash the partition of your choice by typing "run flashimg" or "run flashimg2".
    6. Once flashed, enter "reset" to reboot the device.
    
    Reviewed-by: Robert Marko <robimarko@gmail.com>
    Signed-off-by: Vincent Tremblay <vincent@vtremblay.dev>

--- target/linux/ipq40xx/image/generic.mk
+++ target/linux/ipq40xx/image/generic.mk
@@ -720,6 +720,23 @@ define Device/linksys_mr8300
 endef
 TARGET_DEVICES += linksys_mr8300
 
+define Device/linksys_whw03v2
+	$(call Device/FitzImage)
+	DEVICE_VENDOR := Linksys
+	DEVICE_MODEL := WHW03
+	DEVICE_VARIANT := V2
+	SOC := qcom-ipq4019
+	KERNEL_SIZE := 6144k
+	IMAGE_SIZE := 158720k
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	UBINIZE_OPTS := -E 5    # EOD marks to "hide" factory sig at EOF
+	IMAGES += factory.bin
+	IMAGE/factory.bin  := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | linksys-image type=WHW03v2
+	DEVICE_PACKAGES := ath10k-firmware-qca9888-ct ipq-wifi-linksys_whw03v2 kmod-leds-pca963x kmod-spi-dev kmod-bluetooth
+endef
+TARGET_DEVICES += linksys_whw03v2
+
 define Device/linksys_whw01
 	$(call Device/FitzImage)
 	DEVICE_VENDOR := Linksys
