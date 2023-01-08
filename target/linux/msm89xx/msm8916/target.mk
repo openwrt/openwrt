82d63d666d7cf4a27a250b9ad754c2049afc5e33
Nikita Travkin <nikita@trvn.ru>
2023-01-08 15:07:58 +0500

    msm89xx: Introduce a new target
    This commit introduces support for various Snapdragon 210/410/615 based
    devices. These chipsets usually have integrated wifi and cellular
    connectivity on the same chip.
    
    Among with the platform a few devices are introduced. These
    include Longcheer L8150 smartphone design (Wileyfox Swift / Google
    Android One 2nd gen) and a few snapdragon based USB modem sticks.
    
    Signed-off-by: Nikita Travkin <nikita@trvn.ru>

---
+++ target/linux/msm89xx/msm8916/target.mk
@@ -0,0 +1,8 @@
+SUBTARGET:=msm8916
+BOARDNAME:=Snapdragon 410 (msm8916) based boards
+ARCH:=aarch64
+
+define Target/Description
+	Build firmware images for msm8916 based boards.
+	This firmware features a 64 bit kernel.
+endef
