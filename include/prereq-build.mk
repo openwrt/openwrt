6362933606f8a054f9468f68ed72c9fa4f7433cf
Andre Heider <a.heider@gmail.com>
2023-01-09 15:17:42 +0100

    prereq-build: don't whitelist future python version
    That's guaranteed to fail at some point in time, as python itself can
    contain breaking changes - like the upcoming v3.12 release, which doesn't
    have the distutils module any longer.
    
    Signed-off-by: Andre Heider <a.heider@gmail.com>

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -181,7 +181,7 @@ $(eval $(call SetupHostCommand,python3,Please install Python >= 3.7, \
 	python3.9 -V 2>&1 | grep 'Python 3', \
 	python3.8 -V 2>&1 | grep 'Python 3', \
 	python3.7 -V 2>&1 | grep 'Python 3', \
-	python3 -V 2>&1 | grep -E 'Python 3\.([7-9]|[0-9][0-9])\.?'))
+	python3 -V 2>&1 | grep -E 'Python 3\.([7-9]|1[0-1])\.?'))
 
 $(eval $(call TestHostCommand,python3-distutils, \
 	Please install the Python3 distutils module, \
