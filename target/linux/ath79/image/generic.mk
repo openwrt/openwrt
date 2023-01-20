c7059c56a85567ce6bc943960168b19e2572508b
Shiji Yang <yangshiji66@qq.com>
2023-01-20 03:16:33 +0000

    ath79: improve support for Letv LBA-047-CH
    1. Convert wireless calibration data to NVMEM.
    2. Enable control green status LED and change default LED behaviors.
       The three LEDs of LBA-047-CH are in the same position, and the green
       LED will be completely covered by the other two LEDs. So don's use
       green LED as WAN indicator to ensure that only one LED is on at a time.
       LED     Factory          OpenWrt
       blue    internet fail    failsafe && upgrade
       green   internet okay    run
       red     boot             boot
    3. Reduce the SPI clock to 30 MHz because the ath79 target does not
       support 50 MHz SPI operation well. Keep the fast-read support to
       ensure the spi-mem feature (b3f9842330) is enabled.
    4. Remove unused package "uboot-envtools".
    5. Split the factory image into two parts: rootfs and kernel.
       This change can reduce the factory image size and allow users to
       upgrade the OpenWrt kernel loader uImage (OKLI) independently.
    
       The new installation method: First, rename "squashfs-kernel.bin" to
       "openwrt-ar71xx-generic-ap147-16M-kernel.bin" and rename "rootfs.bin"
       to "openwrt-ar71xx-generic-ap147-16M-rootfs-squashfs.bin". Then we
       can press reset button for about 5 seconds to enter tftp download mode.
       Finally, set IP address to 192.168.67.100 and upload the above two
       parts via tftp server.
    
    Tested on Letv LBA-047-CH
    
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1645,13 +1645,15 @@ define Device/letv_lba-047-ch
   SOC := qca9531
   DEVICE_VENDOR := Letv
   DEVICE_MODEL := LBA-047-CH
+  DEVICE_PACKAGES := -uboot-envtools
+  FACTORY_SIZE := 14528k
   IMAGE_SIZE := 15936k
   LOADER_FLASH_OFFS := 0x50000
   KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -M 0x4f4b4c49
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
-	append-rootfs | pad-rootfs | check-size | pad-to 14528k | \
-	append-loader-okli-uimage $(1) | pad-to 64k
+  IMAGES += kernel.bin rootfs.bin
+  IMAGE/kernel.bin := append-loader-okli-uimage $(1) | pad-to 64k
+  IMAGE/rootfs.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs | check-size $$$$(FACTORY_SIZE)
 endef
 TARGET_DEVICES += letv_lba-047-ch
 
