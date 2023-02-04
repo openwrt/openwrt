efbb623debef3c63eebc9b849f6ca57130efa5f4
Arınç ÜNAL <arinc.unal@arinc9.com>
2023-02-04 10:52:29 +0300

    ramips: add support for Huasifei WS1208V2
    The Huasifei WS1208V2 is an AC1200 router featuring 5 Ethernet ports with a
    Quectel RM520N-GL cellular modem which supports QMI and MBIM modes.
    
    Specifications:
    - MT7621AT, 256 MiB RAM, 16 MiB SPI Flash
    - MT7603EN 2.4 GHz & MT7612EN 5 GHz WLAN
    - Quectel RM520N-GL Cellular Modem
    - 2 WLAN & 4 Cellular Antennas
    - 5 Gigabit Ethernet Ports
    - 1 USB 2.0 port
    - 1 PCI-E Slot
    - 1 M.2 slot
    - 1 SIM card slot
    - 1 SD card slot
    
    Installation:
    - Install sysupgrade image via ROOter OS.
    
    TFTP Recovery:
    - Connect to serial console.
    - Boot initramfs image by choosing option 1 when U-Boot prompts.
    - Install sysupgrade image via OpenWrt.
    
    Link: https://www.huasifei.com/a/Products/5G%20CPE/240.html
    Signed-off-by: Arınç ÜNAL <arinc.unal@arinc9.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1068,6 +1068,18 @@ define Device/humax_e10
 endef
 TARGET_DEVICES += humax_e10
 
+define Device/huasifei_ws1208v2
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := Huasifei
+  DEVICE_MODEL := WS1208V2
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-mt7603 kmod-mt76x2 kmod-sdhci-mt7620 \
+	kmod-usb3 kmod-usb-net-cdc-mbim kmod-usb-net-qmi-wwan \
+	kmod-usb-serial-option
+endef
+TARGET_DEVICES += huasifei_ws1208v2
+
 define Device/iodata_wn-ax1167gr
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
