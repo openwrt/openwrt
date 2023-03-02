a39a49e323e6731215bd2654e68d91a2df4672c7
Daniel Linjama <daniel@dev.linjama.com>
2023-03-02 11:05:22 +0200

    ath79: add support for D-Link COVR-P2500 A1
    Specifications:
    * QCA9563, 16 MiB flash, 128 MiB RAM, 2T2R 802.11n
    * QCA9886 2T2R 801.11ac Wave 2
    * QCA7550 Homeplug AV2 1300
    * AR8337, 3 Gigabit ports (1, 2: LAN; 3: WAN)
    
    To make use of PLC functionality, firmware needs to be
    provided via plchost (QCA7550 comes without SPI NOR),
    patched with the Network Password and MAC.
    
    Flashing via OEM Web Interface
    * Flash 'factory.bin' using web-interface
    * Wait until firmware succesfully installed and device booted
    * Hold down reset button to reset factory defaults (~10 seconds)
    
    Flashing via Recovery Web Interface:
    * Hold down reset button during power-on (~10 seconds)
    * Recovery Web UI is at 192.168.0.50, no DHCP.
    * Flash 'recovery.bin' with
      scripts/flashing/dlink_recovery_upload.py
      (Recovery Web UI does not work with modern OSes)
    
    Return to stock
    * Hold down reset button during power-on (~10 seconds)
    * Recovery Web UI is at 192.168.0.50, no DHCP.
    * Flash unencrypted stock firmware with
      scripts/flashing/dlink_recovery_upload.py
      (Recovery Web UI does not work with modern OSes)
    
    Co-developed-by: Sebastian Schaper <openwrt@sebastianschaper.net>
    Signed-off-by: Sebastian Schaper <openwrt@sebastianschaper.net>
    Signed-off-by: Daniel Linjama <daniel@dev.linjama.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -33,6 +33,17 @@ define Build/cybertan-trx
 	-rm $@-empty.bin
 endef
 
+define Build/dlink-sge-signature
+	( \
+		crc=$$(gzip -c $@ | tail -c 8 | od -An -tx4 --endian little | cut -d " " -f2); \
+		cat $@; \
+		$(MKHASH) md5 $@ ; \
+		echo $(1); \
+		echo -n $$crc; \
+	) > $@.new
+	mv $@.new $@
+endef
+
 define Build/edimax-headers
 	$(eval edimax_magic=$(word 1,$(1)))
 	$(eval edimax_model=$(word 2,$(1)))
@@ -994,6 +1005,26 @@ define Device/devolo_magic-2-wifi
   IMAGE_SIZE := 15872k
 endef
 TARGET_DEVICES += devolo_magic-2-wifi
+
+define Device/dlink_covr-p2500-a1
+  $(Device/loader-okli-uimage)
+  SOC := qca9563
+  DEVICE_VENDOR := D-Link
+  DEVICE_MODEL := COVR-P2500
+  DEVICE_VARIANT := A1
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct
+  LOADER_FLASH_OFFS := 0x050000
+  LOADER_KERNEL_MAGIC := 0x68737173
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -M 0x68737173
+  IMAGE_SIZE := 14528k
+  IMAGES += factory.bin recovery.bin
+  IMAGE/recovery.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs | check-size | pad-to 14528k | \
+	append-loader-okli-uimage $(1) | pad-to 15616k
+  IMAGE/factory.bin := $$(IMAGE/recovery.bin) | \
+	dlink-sge-image COVR-P2500 | dlink-sge-signature COVR-P2500
+endef
+TARGET_DEVICES += dlink_covr-p2500-a1
 
 define Device/dlink_dap-13xx
   SOC := qca9533
