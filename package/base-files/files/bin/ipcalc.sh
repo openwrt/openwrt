b63476e337322aa00635bd38b9e9d3fc34a8b486
Marius Dinu <m95d+git@psihoexpert.ro>
2023-03-06 12:28:30 +0200

    Revert "base-files: ipcalc.sh: use shebang to invoke awk"
    This reverts commit 6ce9f42b9819b93717d7425ca4135afec6ea43af.
    It breaks compatibility with BUSYBOX_CONFIG_INSTALL_NO_USR.
    https://github.com/openwrt/openwrt/issues/12089
    
    Signed-off-by: Marius Dinu <m95d+git@psihoexpert.ro>

--- package/base-files/files/bin/ipcalc.sh
+++ package/base-files/files/bin/ipcalc.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/awk -f
+#!/bin/sh
 
+awk -f - $* <<EOF
 function bitcount(c) {
 	c=and(rshift(c, 1),0x55555555)+and(c,0x55555555)
 	c=and(rshift(c, 2),0x33333333)+and(c,0x33333333)
@@ -87,3 +88,4 @@ BEGIN {
 	print "START="int2ip(start)
 	print "END="int2ip(end)
 }
+EOF
