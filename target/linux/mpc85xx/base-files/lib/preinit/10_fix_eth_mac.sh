765f66810a3324cc35fa6471ee8eeee335ba8c2b
David Bauer <mail@david-bauer.net>
2023-03-18 02:28:23 +0100

    mpc85xx: add support for Enterasys WS-AP3715i
    Hardware
    --------
    
    SoC:   NXP P1010 (1x e500 @ 800MHz)
    RAM:   256M DDR3 (2x Samsung K4B1G1646G-BCH9)
    FLASH: 32M NOR (Spansion S25FL256S)
    BTN:   1x Reset
    WiFi:  1x Atheros AR9590 2.4 bgn 3x3
           2x Atheros AR9590 5.0 an 3x3
    ETH:   2x Gigabit Ethernet (Atheros AR8033 / AR8035)
    UART:  115200 8N1 (RJ-45 Cisco)
    
    Installation
    ------------
    1. Grab the OpenWrt initramfs, rename it to ap3715.bin. Place it in
       the root directory of a TFTP server and serve it at
       192.168.1.66/24.
    
    2. Connect to the serial port and boot the AP. Stop autoboot in U-Boot
       by pressing Enter when prompted. Credentials are identical to the one
       in the APs interface. By default it is admin / new2day.
    
    3. Alter the bootcmd in U-Boot:
    
     $ setenv ramboot_openwrt "setenv ipaddr 192.168.1.1;
       setenv serverip 192.168.1.66; tftpboot 0x2000000 ap3715.bin; bootm"
    
     $ setenv boot_openwrt "sf probe 0; sf read 0x2000000 0x140000 0x1000000;
       bootm 0x2000000"
    
     $ setenv bootcmd "run boot_openwrt"
    
     $ saveenv
    
    4. Boot the initramfs image
    
     $ run ramboot_openwrt
    
    5. Transfer the OpenWrt sysupgrade image to the AP using SCP. Install
       using sysupgrade.
    
     $ sysupgrade -n <path-to-sysupgrade.bin>
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/mpc85xx/base-files/lib/preinit/10_fix_eth_mac.sh
+++ target/linux/mpc85xx/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -3,6 +3,7 @@
 
 preinit_set_mac_address() {
 	case $(board_name) in
+	enterasys,ws-ap3715i|\
 	extreme-networks,ws-ap3825i)
 		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
 		ip link set dev eth1 address $(mtd_get_mac_ascii cfg1 eth1addr)
--- target/linux/mpc85xx/p1010/target.mk
+++ target/linux/mpc85xx/p1010/target.mk
@@ -1,5 +1,5 @@
 BOARDNAME:=P1010
-KERNEL_IMAGES:=simpleImage.tl-wdr4900-v1
+KERNEL_IMAGES:=simpleImage.tl-wdr4900-v1 simpleImage.ws-ap3715i
 
 define Target/Description
 	Build firmware images for P1010 based boards.
