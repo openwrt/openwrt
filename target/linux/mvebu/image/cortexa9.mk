caeeedc280f6d5e8927c4a2bc174c54f27f17327
INAGAKI Hiroshi <musashino.open@gmail.com>
2023-03-21 15:23:56 +0900

    mvebu: add support for Fortinet FortiWiFi 30E
    Fortinet FortiWiFi 30E (FWF-30E) is a UTM with 1x WLAN, based on Armada
    385 (88F6820).
    
    Specification:
    
    - SoC          : Marvell Armada 385 88F6820
    - RAM          : DDR3 1 GiB (4x Nanya NT5CC256M8IN-D1)
    - Flash        : SPI-NOR 128 MiB (Macronix MX66L1G45GMI-10G)
    - Ethernet     : 5x 10/100/1000 Mbps
      - Switch     : Marvell 88E6176
    - WLAN         : Fortinet EMP7618-FT (Atheros AR9382 (2T2R))
      - interface  : MiniPCIe
    - LEDs/Keys    : 16x/1x
    - UART         : "CONSOLE" port (RJ-45, RS-232C level)
      - port       : ttyS0
      - settings   : 9600bps(default) 8n1
      - assignment : 1:NC , 2:NC , 3:TXD, 4:GND,
                    5:GND, 6:RXD, 7:NC , 8:NC
      - note       : compatible with Cisco console cable
    - HW Monitoring: nuvoTon NCT7802Y
    - Power        : 12 VDC, 2 A
      - plug       : Modex 5557-02R
    
    Flash instruction using initramfs image:
    
     1. Power on FWF-30E and interrupt to show bootmenu
     2. Call "[I]: System information." -> "[S]: Set serial port baudrate."
        and set baudrate to 9600 bps
     3. Call "[R]: Review TFTP parameters.", check TFTP parameters and
        connect computer to "Image download port" in the parameters
     4. Prepare TFTP server with the parameters obtained above
     5. Rename OpenWrt initramfs image to "image.out" and put to TFTP
        directory
     6. Call "[T]: Initiate TFTP firmware transfer." to download initramfs
        image from TFTP server
     7. Type "r" key when the following message is showed, to boot initramfs
        image without flashing to spi-nor flash
    
        "Save as Default firmware/Backup firmware/Run image without saving:[D/B/R]?"
    
     8. On initramfs image, backup mtd if needed
    
        minimum:
    
        - "firmware-info"
        - "kernel"
        - "rootfs"
    
     9. On initramfs image, upload sysupgrade image to the device and perform
        sysupgrade
    10. Wait ~200 seconds to complete flashing and rebooting.
        If the device is booted with stock firmware, login to bootmenu and
        call "[B]: Boot with backup firmware and set as default." to set the
        first OS image as default and boot it.
    
    Notes:
    
    - Both colors of Bi-color LEDs on the front panel cannot be turned on at
      the same time.
    
    - "PWR" and "Logo" LEDs are connected to power source directly.
    
    - The following partitions are added for OpenWrt.
      These partitions are contained in "uboot" partition (0x0-0x1fffff) on
      stock firmware.
    
      - "firmware-info"
      - "dtb"
      - "u-boot-env"
      - "board-info"
    
    Image header for bootmenu tftp:
    
      0x0 - 0xf  : ?
     0x10 - 0x2f : Image Name
     0x30 - 0x17f: ?
    0x180 - 0x183: Kernel Offset*
    0x184 - 0x187: Kernel Length*
    0x188 - 0x18b: RootFS Offset (ext2)*
    0x18c - 0x18f: RootFS Length (ext2)*
    0x190 - 0x193: DTB Offset
    0x194 - 0x197: DTB Length
    0x198 - 0x19b: Data Offset (jffs2)
    0x19c - 0x19f: Data Length (jffs2)
    0x1a0 - 0x1ff: ?
    
    *: required for initramfs image
    
    MAC addresses:
    
    (eth0): 90:6C:AC:xx:xx:CE (board-info, 0xd880 (hex))
    WAN   : 90:6C:AC:xx:xx:CF
    LAN 1 : 90:6C:AC:xx:xx:D0
    LAN 2 : 90:6C:AC:xx:xx:D1
    LAN 3 : 90:6C:AC:xx:xx:D2
    LAN 4 : 90:6C:AC:xx:xx:D3
    WLAN  : 88:DC:96:xx:xx:xx (MiniPCIe Card)
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>
    Tested-by: Raylynn Knight <rayknight@me.com>

--- target/linux/mvebu/image/cortexa9.mk
+++ target/linux/mvebu/image/cortexa9.mk
@@ -158,6 +158,16 @@ define Device/fortinet_fg-52e
 endef
 TARGET_DEVICES += fortinet_fg-52e
 
+define Device/fortinet_fwf-30e
+  $(Device/fortinet)
+  DEVICE_MODEL := FortiWiFi 30E
+  DEVICE_DTS := armada-385-fortinet-fwf-30e
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | fortigate-header | \
+    gzip-filename FWF30E
+  DEVICE_PACKAGES += kmod-ath9k wpad-basic-mbedtls
+endef
+TARGET_DEVICES += fortinet_fwf-30e
+
 define Device/globalscale_mirabox
   $(Device/NAND-512K)
   DEVICE_VENDOR := Globalscale
