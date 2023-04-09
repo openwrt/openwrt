6e081e1778b728796f7d0461bbcc2606a18281ca
Álvaro Fernández Rojas <noltari@gmail.com>
2023-04-09 10:34:05 +0200

    bmips: add LED kernel modules
    Add BCM6328 and BCM6358 LED kernel modules.
    This allows selecting the LED controllers only for those devices using them.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/bmips/modules.mk
+++ target/linux/bmips/modules.mk
@@ -1,5 +1,35 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+define KernelPackage/leds-bcm6328
+  SUBMENU:=$(LEDS_MENU)
+  TITLE:=BCM6328 LED support
+  KCONFIG:=CONFIG_LEDS_BCM6328
+  FILES:=$(LINUX_DIR)/drivers/leds/leds-bcm6328.ko
+  DEPENDS:=@TARGET_bmips
+  AUTOLOAD:=$(call AutoLoad,60,leds-bcm6328,1)
+endef
+
+define KernelPackage/leds-bcm6328/description
+  Kernel support for the BCM6328 LED controller.
+endef
+
+$(eval $(call KernelPackage,leds-bcm6328))
+
+define KernelPackage/leds-bcm6358
+  SUBMENU:=$(LEDS_MENU)
+  TITLE:=BCM6358 LED support
+  KCONFIG:=CONFIG_LEDS_BCM6358
+  FILES:=$(LINUX_DIR)/drivers/leds/leds-bcm6358.ko
+  DEPENDS:=@TARGET_bmips
+  AUTOLOAD:=$(call AutoLoad,60,leds-bcm6358,1)
+endef
+
+define KernelPackage/leds-bcm6358/description
+  Kernel support for the BCM6358 LED controller.
+endef
+
+$(eval $(call KernelPackage,leds-bcm6358))
+
 define KernelPackage/leds-sercomm-msp430
   SUBMENU:=$(LEDS_MENU)
   TITLE:=Sercomm MSP430G2513 LED support
