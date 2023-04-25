ce0c77e5ed2d393f517ac88327616293c9267977
Phinex Hung <phinex@realtek.com>
2023-04-25 11:09:52 +0800

    rtd1xxx: image: add 1319 Pymparticles eMMC 2GB board support
    add rtd1319 pymparticles eMMC 2GB board DT file
    
    Signed-off-by: Phinex Hung <phinex@realtek.com>

--- target/linux/rtd1xxx/image/rtd1319.mk
+++ target/linux/rtd1xxx/image/rtd1319.mk
@@ -4,10 +4,21 @@ define Device/rtd1319
   DEVICE_NAME := realtek
   DEVICE_VENDOR := Realtek
   DEVICE_DTS_DIR := $(DTS_DIR)/realtek
-  DEVICE_DTS := $(1)
+  DEVICE_DTS := rtd1319-$(1)
   DEVICE_PACKAGES := nas
   PROFILES := Default $$(DEVICE_NAME)
   SOC := hank
   BOARD_NAME :=
   FLASH_SIZE :=
 endef
+
+define Device/pymparticles-emmc-2gb
+  $(Device/rtd1319)
+  DEVICE_MODEL := Pymparticles eMMC board
+  DEVICE_VARIANT := 2GB
+  DEVICE_PACKAGES +=
+  BOARD_NAME := emmc
+  FLASH_SIZE := 8gb
+endef
+
+TARGET_DEVICES += pymparticles-emmc-2gb
