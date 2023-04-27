29128b0bd43532404d965d22338554fce624db6a
Nick Hainke <vincent@systemli.org>
2023-04-27 23:03:14 +0200

    toolchain: gcc: add support for GCC 13
    Release Notes:
    https://gcc.gnu.org/pipermail/gcc-announce/2023/000175.html
    
    Manually Refreshed:
    - 910-mbsd_multi.patch
    - 970-macos_arm64-building-fix.patch
    
    Automatically Refreshed:
    - 010-documentation.patch
    - 230-musl_libssp.patch
    - 300-mips_Os_cpu_rtx_cost_model.patch
    - 820-libgcc_pic.patch
    - 840-armv4_pass_fix-v4bx_to_ld.patch
    - 850-use_shared_libgcc.patch
    - 870-ppc_no_crtsavres.patch
    - 920-specs_nonfatal_getenv.patch
    
    Signed-off-by: Nick Hainke <vincent@systemli.org>

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -37,6 +37,10 @@ ifeq ($(PKG_VERSION),12.2.0)
   PKG_HASH:=e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff
 endif
 
+ifeq ($(PKG_VERSION),13.1.0)
+  PKG_HASH:=61d684f0aa5e76ac6585ad8898a2427aade8979ed5e7f85492286c4dfc13ee86
+endif
+
 PATCH_DIR=../patches-$(GCC_MAJOR_VERSION).x
 
 BUGURL=http://bugs.openwrt.org/
@@ -187,10 +191,10 @@ define Host/SetToolchainInfo
 endef
 
 
-ifeq ($(GCC_MAJOR_VERSION),12)
-	GCC_VERSION_FILE:=gcc/genversion.cc
-else
+ifeq ($(GCC_MAJOR_VERSION),11)
 	GCC_VERSION_FILE:=gcc/version.c
+else
+	GCC_VERSION_FILE:=gcc/genversion.cc
 endif
 
 ifneq ($(GCC_PREPARE),)
