048023133d31b9d7d62ea9646c2b6bb51a82c03b
Daniel Kestrel <kestrel1974@t-online.de>
2023-05-03 15:05:49 +0200

    lantiq: add support for AVM Fritzbox 3490
    This adds support for the Fritzbox 3490 device. It contains two
    SoCs, one Lantiq with a 5GHz WiFi and one QCA9558 with 2.4GHz
    and 5 GHz WiFi. Only the Lantiq has access to the flash memory,
    the Atheros runs fully from RAM and is booted by using a remoteproc
    kernel module which is not supported with this commit.
    The devices were manufactured with varying NAND chips which
    requires Micron and non-Micron versions of the images.
    
    Specifications:
     - SoC: Lantiq 500 MHz
     - RAM: 256 MB
     - Storage: 512 MB NAND, 1MB FLASH
     - Wireless, separate SOC QCA9558 with 128MB RAM (not supported yet):
       · Qualcomm-QCA9558 w/ 3×3 MIMO for 2.4GHz 802.11b/g/n
       · Qualcomm-QCA9880 w/ 3×3 MIMO for 5GHz 802.11a/ac
       · AG71xx ethernet
     - Ethernet: Built-in AR 803x, 7 port 4 phy switch,
       4x 1000/100/10 port, Port 5 is fixed and connected to the WASP SOC
     - Renesas µPD720202 USB3 PCIe, requires firmware binary on the device
     - VDSL2 modem
     - Without telephony or ISDN
    
    Installation:
    Check which NAND the device has by using the following procedure with
    stock firmware:
    Go to to http://<fritzbox_ip>/support.lua, download the support data
    file and search for string "NAND device" to get the manufacturer kernel
    output.
    Use Micron image if Micron is displayed otherwise the non-Micron image.
    Use the eva_ramboot.py script to boot the initramfs image. Follow the
    procedure to interrupt booting by ftp into 192.168.178.1 within
    5 seconds after poweron.
    Then transfer the sysupgrade image to the device and run sysupgrade to
    flash it to the NAND.
    For making USB work, an renesas xhci firmware file (e.g. v2026) is
    needed and it should be copied to /lib/firmware/ (file name
    renesas_usb_fw.mem).
    
    Signed-off-by: Daniel Kestrel <kestrel1974@t-online.de>

--- target/linux/lantiq/xrx200/base-files/lib/upgrade/platform.sh
+++ target/linux/lantiq/xrx200/base-files/lib/upgrade/platform.sh
@@ -12,6 +12,8 @@ platform_do_upgrade() {
 	avm,fritz3370-rev2-hynix|\
 	avm,fritz3370-rev2-micron|\
 	avm,fritz3390|\
+	avm,fritz3490|\
+	avm,fritz3490-micron|\
 	avm,fritz5490|\
 	avm,fritz5490-micron|\
 	avm,fritz7362sl|\
