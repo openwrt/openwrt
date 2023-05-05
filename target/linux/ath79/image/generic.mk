0ffbef9317a1dc049ad259c1ec1530355efc0552
Shiji Yang <yangshiji66@qq.com>
2023-05-05 01:50:36 +0000

    ath79: add support for D-Link DIR-859 A3
    Specifications:
      SOC:      QCA9563 775 MHz + QCA9880
      Switch:   QCA8337N-AL3C
      RAM:      Winbond W9751G6KB-25 64 MiB
      Flash:    Winbond W25Q128FVSG 16 MiB
      WLAN:     Wi-Fi4 2.4 GHz 3*3 + 5 GHz 3*3
      LAN:      LAN ports *4
      WAN:      WAN port *1
      Buttons:  reset *1 + wps *1
      LEDs: ethernet *5, power, wlan, wps
    
    MAC Address:
      use      address               source1          source2
      label    40:9b:xx:xx:xx:3c     lan && wlan      u-boot,env@ethaddr
      lan      40:9b:xx:xx:xx:3c     devdata@0x3f     $label
      wan      40:9b:xx:xx:xx:3f     devdata@0x8f     $label + 3
      wlan2g   40:9b:xx:xx:xx:3c     devdata@0x5b     $label
      wlan5g   40:9b:xx:xx:xx:3e     devdata@0x76     $label + 2
    
    Install via Web UI:
      Apply factory image in the stock firmware's Web UI.
    
    Install via Emergency Room Mode:
      DIR-859 A1 will enter recovery mode when the system fails to boot
      or press reset button for about 10 seconds.
    
      First, set computer IP to 192.168.0.5 and Gateway to 192.168.0.1.
      Then we can open http://192.168.0.1 in the web browser to upload
      OpenWrt factory image or stock firmware. Some modern browsers may
      need to turn on compatibility mode.
    
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1143,17 +1143,27 @@ define Device/dlink_dir-842-c3
 endef
 TARGET_DEVICES += dlink_dir-842-c3
 
-define Device/dlink_dir-859-a1
+define Device/dlink_dir-859-ax
   $(Device/seama)
   SOC := qca9563
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := DIR-859
-  DEVICE_VARIANT := A1
   IMAGE_SIZE := 15872k
   DEVICE_PACKAGES := kmod-usb2 kmod-ath10k-ct-smallbuffers ath10k-firmware-qca988x-ct
   SEAMA_SIGNATURE := wrgac37_dlink.2013gui_dir859
 endef
+
+define Device/dlink_dir-859-a1
+  $(Device/dlink_dir-859-ax)
+  DEVICE_VARIANT := A1
+endef
 TARGET_DEVICES += dlink_dir-859-a1
+
+define Device/dlink_dir-859-a3
+  $(Device/dlink_dir-859-ax)
+  DEVICE_VARIANT := A3
+endef
+TARGET_DEVICES += dlink_dir-859-a3
 
 define Device/dlink_dir-869-a1
   $(Device/seama)
