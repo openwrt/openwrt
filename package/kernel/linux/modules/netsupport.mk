06e64f9b364abe15c27bf0a7225fcac740819668
Tianling Shen <cnsztl@immortalwrt.org>
2023-05-23 15:43:14 +0800

    kernel: modules: add xdp-sockets-diag support
    Support for PF_XDP sockets monitoring interface used by the ss tool.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/kernel/linux/modules/netsupport.mk
+++ package/kernel/linux/modules/netsupport.mk
@@ -1434,6 +1434,22 @@ endef
 $(eval $(call KernelPackage,inet-diag))
 
 
+define KernelPackage/xdp-sockets-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=PF_XDP sockets monitoring interface support for ss utility
+  DEPENDS:=@KERNEL_XDP_SOCKETS
+  KCONFIG:=CONFIG_XDP_SOCKETS_DIAG
+  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
+  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
+endef
+
+define KernelPackage/xdp-sockets-diag/description
+ Support for PF_XDP sockets monitoring interface used by the ss tool
+endef
+
+$(eval $(call KernelPackage,xdp-sockets-diag))
+
+
 define KernelPackage/wireguard
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=WireGuard secure network tunnel
