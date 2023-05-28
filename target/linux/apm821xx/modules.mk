55fbcad20a2d9572966ddbb8856d3b01f68be23a
Christian Lamparter <chunkeey@gmail.com>
2023-05-28 22:13:52 +0200

    apm821xx: make crypto4xx as a standalone module
    Make the crypto4xx crypto accelerator support as a standalone module.
    This saves 90kb on a gzipped kernel.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/apm821xx/modules.mk
+++ target/linux/apm821xx/modules.mk
@@ -11,3 +11,23 @@ define KernelPackage/ata-dwc/description
 endef
 
 $(eval $(call KernelPackage,ata-dwc))
+
+define KernelPackage/hw-crypto-4xx
+  TITLE:=Driver AMCC PPC4xx crypto accelerator
+  KCONFIG:= \
+	CONFIG_CRYPTO_HW=y \
+	CONFIG_HW_RANDOM=y \
+	CONFIG_CRYPTO_DEV_PPC4XX \
+	CONFIG_HW_RANDOM_PPC4XX=y
+  DEPENDS:=+kmod-random-core +kmod-crypto-manager \
+	   +kmod-crypto-ccm +kmod-crypto-gcm \
+	   +kmod-crypto-sha1 +kmod-crypto-sha256 +kmod-crypto-sha512
+  FILES:=$(LINUX_DIR)/drivers/crypto/amcc/crypto4xx.ko
+  AUTOLOAD:=$(call AutoLoad,09,sata_dwc_460ex,1)
+endef
+
+define KernelPackage/hw-crypto-4xx/description
+  Platform support for the on-chip crypto acceleration.
+endef
+
+$(eval $(call KernelPackage,hw-crypto-4xx))
