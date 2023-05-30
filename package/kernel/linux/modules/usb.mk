0f6b8e65161a840c57d86dfb8fb61d018798c0a1
Hauke Mehrtens <hauke@hauke-m.de>
2023-05-30 22:04:20 +0200

    kernel: Make kmod-usb-net-smsc95xx depend on kmod-net-selftests
    The smsc95xx driver got selftest support with kernel 5.18, add the new
    dependency fixing the all kernel modules build on MIPS malta with kernel
    6.1.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1285,7 +1285,7 @@ $(eval $(call KernelPackage,usb-net-smsc75xx))
 
 define KernelPackage/usb-net-smsc95xx
   TITLE:=SMSC LAN95XX based USB 2.0 10/100 ethernet devices
-  DEPENDS:=+kmod-libphy +kmod-phy-smsc
+  DEPENDS:=+kmod-libphy +kmod-phy-smsc +LINUX_6_1:kmod-net-selftests
   KCONFIG:=CONFIG_USB_NET_SMSC95XX
   FILES:=$(LINUX_DIR)/drivers/$(USBNET_DIR)/smsc95xx.ko
   AUTOLOAD:=$(call AutoProbe,smsc95xx)
