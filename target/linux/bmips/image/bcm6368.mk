725319ad3857e0d6b2f5f900e9ab2f779ad53410
Daniel González Cabanelas <dgcbueu@gmail.com>
2023-06-09 00:09:33 +0200

    bmips: add support for Observa VH4032N
    The Observa VH4032N is an xDSL wifi router with a vertical white casing
    and two internal antennas connected via UFL.
    
    Hardware:
     - SoC: Broadcom BCM6368
     - CPU: dual core BMIPS4350 V3.1 @400MHz
     - RAM: 128 MB DDR
     - Flash: 32 MB parallel NOR
     - Ethernet LAN: 4x 100Mbit
     - Wifi 2.4/5 GHz: onboard Broadcom BCM43222 802.11abgn
     - USB: 3x 2.0
     - Buttons: 2x, 1 reset
     - LEDs: 8x, blue and red
     - UART: 1x
    
    Installation via OEM web UI:
      1. Use the admin credentials to login via web UI
      2. Go to Managament->Update firmware and select the OpenWrt CFE firmware
      3. Press "Update Firmware" button and wait some minutes until it finish
    
    Signed-off-by: Daniel González Cabanelas <dgcbueu@gmail.com>

--- target/linux/bmips/image/bcm6368.mk
+++ target/linux/bmips/image/bcm6368.mk
@@ -13,3 +13,18 @@ define Device/comtrend_vr-3025u
     kmod-leds-gpio
 endef
 TARGET_DEVICES += comtrend_vr-3025u
+
+define Device/observa_vh4032n
+  $(Device/bcm63xx-cfe)
+  DEVICE_VENDOR := Observa
+  DEVICE_MODEL := VH4032N
+  IMAGES += sysupgrade.bin
+  CFE_BOARD_ID := 96368VVW
+  CHIP_ID := 6368
+  BLOCKSIZE := 0x20000
+  FLASH_MB := 32
+  DEVICE_PACKAGES += $(USB2_PACKAGES) \
+    $(B43_PACKAGES) broadcom-43222-sprom \
+    kmod-leds-gpio
+endef
+TARGET_DEVICES += observa_vh4032n
