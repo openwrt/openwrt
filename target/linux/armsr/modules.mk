90f40d85938b1fd6e3815fda9b135398637ed9a9
Mathew McBride <matt@traverse.com.au>
2023-06-09 00:43:33 +0000

    armsr: rename from armvirt
    Now that the armvirt target supports real hardware, not just
    VMs, thanks to the addition of EFI, rename it to something
    more appropriate.
    
    'armsr' (Arm SystemReady) was chosen after the name of
    the Arm standards program.
    
    The 32 and 64 bit targets have also been renamed
    armv7 and armv8 respectively, to allow future profiles
    where required (such as armv9).
    
    See https://developer.arm.com/documentation/102858/0100/Introduction
    for more information.
    
    Signed-off-by: Mathew McBride <matt@traverse.com.au>
    (23.05 version of commit 40b02a230167626def69389452f19b7109aaeac1)

--- target/linux/armsr/modules.mk
+++ target/linux/armsr/modules.mk
@@ -1,7 +1,7 @@
 define KernelPackage/acpi-mdio
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=ACPI MDIO support
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-mdio-devres
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-mdio-devres
   KCONFIG:=CONFIG_ACPI_MDIO
   FILES:=$(LINUX_DIR)/drivers/net/mdio/acpi_mdio.ko
   AUTOLOAD:=$(call AutoLoad,11,acpi_mdio)
@@ -15,7 +15,7 @@ $(eval $(call KernelPackage,acpi-mdio))
 
 define KernelPackage/fsl-pcs-lynx
   SUBMENU=$(NETWORK_DEVICES_MENU)
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio +kmod-phylink
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-of-mdio +kmod-phylink
   TITLE:=NXP (Freescale) Lynx PCS
   HIDDEN:=1
   KCONFIG:=CONFIG_PCS_LYNX
@@ -28,7 +28,7 @@ $(eval $(call KernelPackage,fsl-pcs-lynx))
 define KernelPackage/pcs-xpcs
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis DesignWare PCS driver
-  DEPENDS:=@(TARGET_armvirt_64)
+  DEPENDS:=@(TARGET_armsr_armv8)
   KCONFIG:=CONFIG_PCS_XPCS
   FILES:=$(LINUX_DIR)/drivers/net/pcs/pcs_xpcs.ko
   AUTOLOAD:=$(call AutoLoad,20,pcs_xpcs)
@@ -38,7 +38,7 @@ $(eval $(call KernelPackage,pcs-xpcs))
 
 define KernelPackage/fsl-fec
   SUBMENU:=$(NETWORK_DEVICES_MENU)
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio \
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-of-mdio \
     +kmod-ptp +kmod-net-selftests
   TITLE:=NXP (Freescale) FEC Ethernet controller (i.MX)
   KCONFIG:=CONFIG_FEC
@@ -50,7 +50,7 @@ $(eval $(call KernelPackage,fsl-fec))
 
 define KernelPackage/fsl-xgmac-mdio
   SUBMENU=$(NETWORK_DEVICES_MENU)
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio +kmod-acpi-mdio
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-of-mdio +kmod-acpi-mdio
   TITLE:=NXP (Freescale) MDIO bus
   KCONFIG:=CONFIG_FSL_XGMAC_MDIO
   FILES=$(LINUX_DIR)/drivers/net/ethernet/freescale/xgmac_mdio.ko
@@ -74,7 +74,7 @@ $(eval $(call KernelPackage,fsl-mc-dpio))
 define KernelPackage/fsl-enetc-net
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=:NXP ENETC (LS1028A) Ethernet
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-phylink +kmod-fsl-pcs-lynx
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-phylink +kmod-fsl-pcs-lynx
   KCONFIG:= \
     CONFIG_FSL_ENETC \
     CONFIG_FSL_ENETC_VF \
@@ -92,7 +92,7 @@ $(eval $(call KernelPackage,fsl-enetc-net))
 define KernelPackage/fsl-dpaa1-net
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=NXP DPAA1 (LS1043/LS1046) Ethernet
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-fsl-xgmac-mdio +kmod-libphy +kmod-crypto-crc32
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-fsl-xgmac-mdio +kmod-libphy +kmod-crypto-crc32
   KCONFIG:= \
     CONFIG_FSL_DPAA=y \
     CONFIG_FSL_DPAA_ETH \
@@ -112,7 +112,7 @@ $(eval $(call KernelPackage,fsl-dpaa1-net))
 define KernelPackage/fsl-dpaa2-net
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=NXP DPAA2 Ethernet
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-fsl-xgmac-mdio +kmod-phylink \
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-fsl-xgmac-mdio +kmod-phylink \
     +kmod-fsl-pcs-lynx +kmod-fsl-mc-dpio
   KCONFIG:= \
     CONFIG_FSL_MC_UAPI_SUPPORT=y \
@@ -127,7 +127,7 @@ $(eval $(call KernelPackage,fsl-dpaa2-net))
 define KernelPackage/fsl-dpaa2-console
   SUBMENU:=$(OTHER_MENU)
   TITLE:=NXP DPAA2 Debug console
-  DEPENDS:=@(TARGET_armvirt_64)
+  DEPENDS:=@(TARGET_armsr_armv8)
   KCONFIG:=CONFIG_DPAA2_CONSOLE
   FILES=$(LINUX_DIR)/drivers/soc/fsl/dpaa2-console.ko
   AUTOLOAD=$(call AutoLoad,40,dpaa2-console)
@@ -143,7 +143,7 @@ $(eval $(call KernelPackage,fsl-dpaa2-console))
 define KernelPackage/marvell-mdio
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell Armada platform MDIO driver
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy +kmod-of-mdio +kmod-acpi-mdio
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-of-mdio +kmod-acpi-mdio
   KCONFIG:=CONFIG_MVMDIO
   FILES=$(LINUX_DIR)/drivers/net/ethernet/marvell/mvmdio.ko
   AUTOLOAD=$(call AutoLoad,30,marvell-mdio)
@@ -154,7 +154,7 @@ $(eval $(call KernelPackage,marvell-mdio))
 define KernelPackage/phy-marvell-10g
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell Alaska 10G PHY driver
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-libphy
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy
   KCONFIG:=CONFIG_MARVELL_10G_PHY
   FILES=$(LINUX_DIR)/drivers/net/phy/marvell10g.ko
   AUTOLOAD=$(call AutoLoad,35,marvell10g)
@@ -165,7 +165,7 @@ $(eval $(call KernelPackage,phy-marvell-10g))
 define KernelPackage/mvneta
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell Armada 370/38x/XP/37xx network driver
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-marvell-mdio +kmod-phylink
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-marvell-mdio +kmod-phylink
   KCONFIG:=CONFIG_MVNETA
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/marvell/mvneta.ko
   AUTOLOAD=$(call AutoLoad,40,mvneta)
@@ -176,7 +176,7 @@ $(eval $(call KernelPackage,mvneta))
 define KernelPackage/mvpp2
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell Armada 375/7K/8K network driver
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-marvell-mdio +kmod-phylink
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-marvell-mdio +kmod-phylink
   KCONFIG:=CONFIG_MVPP2 \
     CONFIG_MVPP2_PTP=n
   FILES=$(LINUX_DIR)/drivers/net/ethernet/marvell/mvpp2/mvpp2.ko
@@ -208,7 +208,7 @@ $(eval $(call KernelPackage,imx7-ulp-wdt))
 define KernelPackage/stmmac-core
   SUBMENU=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-phylink +kmod-pcs-xpcs +kmod-ptp
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-phylink +kmod-pcs-xpcs +kmod-ptp
   KCONFIG:=CONFIG_STMMAC_ETH \
     CONFIG_STMMAC_SELFTESTS=n \
     CONFIG_STMMAC_PLATFORM \
@@ -258,7 +258,7 @@ $(eval $(call KernelPackage,dwmac-rockchip))
 define KernelPackage/thunderx-net
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell (Cavium) ThunderX/2 network drivers
-  DEPENDS:=@(TARGET_armvirt_64) +kmod-phylink
+  DEPENDS:=@(TARGET_armsr_armv8) +kmod-phylink
   KCONFIG:=CONFIG_NET_VENDOR_CAVIUM \
     CONFIG_THUNDER_NIC_PF \
     CONFIG_THUNDER_NIC_VF \
