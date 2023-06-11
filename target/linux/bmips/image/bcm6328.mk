bcdf861519b8fddad1abc5d87c434252815a3a6d
Álvaro Fernández Rojas <noltari@gmail.com>
2023-06-11 18:44:36 +0200

    bmips: add support for Comtrend AR-5381u
    The Comtrend AR-5381u is a wifi fast ethernet router, 2.4 GHz single band with
    two internal antennas.
    
    Hardware:
     - SoC: Broadcom BCM6328
     - CPU: single core BMIPS4350 @ 320Mhz
     - RAM: 64 MB DDR
     - Flash: 16 MB SPI NOR
     - Ethernet LAN: 4x 100Mbit
     - Wifi 2.4 GHz: miniPCI Broadcom BCM43225 802.11bgn
     - USB: 1x 2.0
     - Buttons: 1x (reset)
     - LEDs: yes
     - UART: yes
    
    Installation via CFE web UI:
      1. Power off the router.
      2. Press reset button near the power switch.
      3. Keep it pressed while powering up during ~20+ seconds.
      4. Browse to http://192.168.1.1 and upload the firmware.
      5. Wait a few minutes for it to finish.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/bmips/image/bcm6328.mk
+++ target/linux/bmips/image/bcm6328.mk
@@ -1,5 +1,18 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+define Device/comtrend_ar-5381u
+  $(Device/bcm63xx-cfe)
+  DEVICE_VENDOR := Comtrend
+  DEVICE_MODEL := AR-5381u
+  CHIP_ID := 6328
+  CFE_BOARD_ID := 96328A-1241N
+  FLASH_MB := 16
+  DEVICE_PACKAGES += $(USB2_PACKAGES) \
+    $(B43_PACKAGES) broadcom-43225-sprom \
+    kmod-leds-bcm6328
+endef
+TARGET_DEVICES += comtrend_ar-5381u
+
 define Device/comtrend_ar-5387un
   $(Device/bcm63xx-cfe)
   DEVICE_VENDOR := Comtrend
