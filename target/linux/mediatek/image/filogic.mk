626344c9926dcf2db2e10681c19aab0328fee160
Chukun Pan <amadeus@jmu.edu.cn>
2023-06-16 23:16:30 +0800

    mediatek: filogic: add JCG Q30 PRO support
    Hardware specification:
      SoC: MediaTek MT7981B 2x A53
      Flash: Winbond 128MB
      RAM: DDR3 256MB
      Ethernet: 4x 10/100/1000 Mbps
      Switch: MediaTek MT7531AE
      WiFi: MediaTek MT7976C
      Button: Reset
      Power: DC 12V 1A
    
    Flash instructions:
      1. Connect to your PC via the Gigabit port of the router,
         set a static ip on the ethernet interface of your PC.
         (ip 192.168.1.254, gateway 192.168.1.1)
      2. Attach UART, pause at u-boot menu.
      3. Select "Upgrade ATF BL2", then use preloader.bin
      4. Select "Upgrade ATF FIP", then use bl31-uboot.fip
      5. Download the initramfs image, and type "reset",
         waiting for tftp recovery to complete.
      6. After openwrt boots up, perform sysupgrade.
    
    Note:
      1. Since NMBM is disabled, we must back up all partitions.
      2. Although we can upgrade new firmware in the stock firmware,
         we need the special fit image signature of MediaTek and
         dual boot (hack kernel) to make u-boot boot it. So just
         abandon these hacks and flash it via the serial port.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -330,6 +330,30 @@ define Device/h3c_magic-nx30-pro
 endef
 TARGET_DEVICES += h3c_magic-nx30-pro
 
+define Device/jcg_q30-pro
+  DEVICE_VENDOR := JCG
+  DEVICE_MODEL := Q30 PRO
+  DEVICE_DTS := mt7981b-jcg-q30-pro
+  DEVICE_DTS_DIR := ../dts
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+        fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
+  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot jcg_q30-pro
+endef
+TARGET_DEVICES += jcg_q30-pro
+
 define Device/netgear_wax220
   DEVICE_VENDOR := NETGEAR
   DEVICE_MODEL := WAX220
