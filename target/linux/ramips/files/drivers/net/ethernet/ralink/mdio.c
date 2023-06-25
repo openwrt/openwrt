openwrt/openwrt

commit a2acdf9607045e5669c305c57dd7c77be8351ba0

ramips: mt7620: remove useless GMAC nodes
These nodes are used for configuring a GMAC interface
and for defining external PHYs to be accessed with MDIO.

None of this is possible on MT7620N, only MT7620A,
so remove them from all MT7620N DTS.

When the mdio-bus node is missing, the driver returns -NODEV
which causes the internal switch to not initialize.
Replace that return so that everything works without the DTS node.

Also, an extra kernel message to indicate for all error conditions
that mdio-bus is disabled.

Fixes: d482356322c9 ("ramips: mt7620n: add mdio node and disable port4 by default")
Fixes: aa5014dd1a58 ("ramips: mt7620n: enable port 4 as EPHY by default")
Signed-off-by: Michael Pratt <mcpratt@pm.me>

target/linux/ramips/files/drivers/net/ethernet/ralink/mdio.c
@@ -232,7 +232,8 @@ int fe_mdio_init(struct fe_priv *priv)
 	mii_np = of_get_child_by_name(priv->dev->of_node, "mdio-bus");
 	if (!mii_np) {
 		dev_err(priv->dev, "no %s child node found", "mdio-bus");
-		return -ENODEV;
+		err = 0;
+		goto err_no_bus;
 	}
 
 	if (!of_device_is_available(mii_np)) {
@@ -264,6 +265,8 @@ err_free_bus:
 	kfree(priv->mii_bus);
 err_put_node:
 	of_node_put(mii_np);
+err_no_bus:
+	dev_err(priv->dev, "%s disabled", "mdio-bus");
 	priv->mii_bus = NULL;
 	return err;
 }

target/linux/ramips/dts/mt7620n.dtsi
@@ -322,20 +322,6 @@
 		reset-names = "fe", "esw";
 
 		mediatek,switch = <&gsw>;
-
-		mdio-bus {
-			#address-cells = <1>;
-			#size-cells = <0>;
-
-			status = "disabled";
-		};
-
-		port@4 {
-			compatible = "mediatek,mt7620a-gsw-port", "mediatek,eth-port";
-			reg = <4>;
-
-			status = "disabled";
-		};
 	};
 
 	gsw: gsw@10110000 {

target/linux/ramips/dts/mt7620n_dlink_dwr-921-c1.dts
@@ -133,12 +133,6 @@
 	status = "okay";
 };
 
-&ethernet {
-	port@4 {
-		status = "okay";
-	};
-};
-
 &state_default {
 	default {
 		groups = "spi refclk", "i2c", "ephy", "wled";

target/linux/ramips/dts/mt7620n_dlink_dwr-922-e2.dts
@@ -136,12 +136,6 @@
 	status = "okay";
 };
 
-&ethernet {
-	port@4 {
-		status = "okay";
-	};
-};
-
 &state_default {
 	default {
 		groups = "spi refclk", "i2c", "ephy", "wled";

commit 285f0668f4058a935389985eb80353c8a5adbc7d

ramips: do not print error log when mdio bus is disabled
The mdio bus is used to control externel switch. In most cases, they are
disabled, which is the normal behavior. Treating this as an error makes
no sense, so we need to change the notification level from error to info.

Fixes: a2acdf960704 ("ramips: mt7620: remove useless GMAC nodes")
Signed-off-by: Shiji Yang <yangshiji66@qq.com>

target/linux/ramips/files/drivers/net/ethernet/ralink/mdio.c
@@ -257,7 +257,7 @@ err_free_bus:
 err_put_node:
 	of_node_put(mii_np);
 err_no_bus:
-	dev_err(priv->dev, "%s disabled", "mdio-bus");
+	dev_info(priv->dev, "%s disabled", "mdio-bus");
 	priv->mii_bus = NULL;
 	return err;
 }