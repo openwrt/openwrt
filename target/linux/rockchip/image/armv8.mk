ac9a2d6a3575f7d2864d5013bb2dbf8cb40eeece
Lu jicong <jiconglu58@gmail.com>
2023-07-24 15:22:42 +0800

    rockchip: add FriendlyARM NanoPC T4 support
    Hardware
    --------
    RockChip RK3399 ARM64 (6 cores)
    4GB LPDDR3 RAM
    1x 1000 Base-T
    1 GPIO LED (status)
    HDMI 2.0
    3.5mm TRRS AV jack
    Micro-SD slot
    16GB eMMC
    1x USB 3.0 Port
    2x USB 2.0 Port
    1x USB Type-C Port
    1x M.2 PCI-E Port
    AP6356S (BCM4356) SDIO WiFi & Bluetooth adapter
    --------
    Note: AP6356S is not supported yet due to the lack of firmware and NVRAM
    
    Signed-off-by: Lu jicong <jiconglu58@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -15,6 +15,14 @@ define Device/firefly_roc-rk3328-cc
 endef
 TARGET_DEVICES += firefly_roc-rk3328-cc
 
+define Device/friendlyarm_nanopc-t4
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPC T4
+  SOC := rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += friendlyarm_nanopc-t4
+
 define Device/friendlyarm_nanopi-r2c
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R2C
