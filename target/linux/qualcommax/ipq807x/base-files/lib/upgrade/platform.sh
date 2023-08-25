fbcda3616e2c18b533a552fad9b724a636bcae5e
Chukun Pan <amadeus@jmu.edu.cn>
2023-08-25 23:10:35 +0800

    ipq807x: add Arcadyan AW1000 support
    Hardware specification:
      SoC: Qualcomm IPQ8072A
      Flash: Toshiba NAND 1GiB
      RAM: 1 GiB of DDR3 466 MHz
      Ethernet: 4x 1Gbps + 1x 2.5Gbps
      WiFi1: QCN5024 2.4GHz ax 4x4
      WiFi2: QCN5054 5GHz ax 4x4
      Button: WiFi, WPS, Reset
      Modem: RG500Q-EA
      USB: 1 x USB 3.0
      Power: DC 12V 4A
    
    Flash instructions:
      1. Download the initramfs image, rename it to
         initramfs.bin, and host it with tftp server.
      2. Interrupt U-Boot and run these commands:
         tftpboot initramfs.bin
         bootm
      3. After openwrt boots up, use scp or luci web
         to upload sysupgrade.bin to upgrade.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
    Reviewed-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -43,14 +43,7 @@ platform_pre_upgrade() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
-	buffalo,wxr-5950ax12)
-		CI_KERN_UBIPART="rootfs"
-		CI_ROOT_UBIPART="user_property"
-		buffalo_upgrade_prepare
-		nand_do_flash_file "$1" || nand_do_upgrade_failed
-		nand_do_restore_config || nand_do_upgrade_failed
-		buffalo_upgrade_optvol
-		;;
+	arcadyan,aw1000|\
 	compex,wpq873|\
 	dynalink,dl-wrx36|\
 	edimax,cax1800|\
@@ -59,6 +52,14 @@ platform_do_upgrade() {
 	netgear,wax620|\
 	netgear,wax630)
 		nand_do_upgrade "$1"
+		;;
+	buffalo,wxr-5950ax12)
+		CI_KERN_UBIPART="rootfs"
+		CI_ROOT_UBIPART="user_property"
+		buffalo_upgrade_prepare
+		nand_do_flash_file "$1" || nand_do_upgrade_failed
+		nand_do_restore_config || nand_do_upgrade_failed
+		buffalo_upgrade_optvol
 		;;
 	edgecore,eap102)
 		active="$(fw_printenv -n active)"
