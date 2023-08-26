b61ecb7d756b9302fa4e1463f47a591f0a81028e
Chukun Pan <amadeus@jmu.edu.cn>
2023-08-26 23:29:20 +0800

    ipq807x: add support for CMCC RM2-6
    Hardware specifications:
      SoC: Qualcomm IPQ8070A
      RAM: 512MB of DDR3
      Flash: 256MB Micron NAND
      Ethernet: 2x 1G RJ45 port
      WiFi1: QCN5024 2x2 2.4GHz
      WiFi2: QCN5054 2x2 5GHz
      Fan: 1x GPIO controlled
      Button: Reset, WPS
    
    Flash instructions:
      Upload factory.bin in stock firmware's upgrade page.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
    Reviewed-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -36,6 +36,21 @@ define Device/buffalo_wxr-5950ax12
 endef
 TARGET_DEVICES += buffalo_wxr-5950ax12
 
+define Device/cmcc_rm2-6
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := CMCC
+	DEVICE_MODEL := RM2-6
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac02
+	SOC := ipq8070
+	IMAGES := factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-ubi | qsdk-ipq-factory-nand
+	DEVICE_PACKAGES := ipq-wifi-cmcc_rm2-6 kmod-hwmon-gpiofan
+endef
+TARGET_DEVICES += cmcc_rm2-6
+
 define Device/compex_wpq873
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -44,6 +44,7 @@ platform_pre_upgrade() {
 platform_do_upgrade() {
 	case "$(board_name)" in
 	arcadyan,aw1000|\
+	cmcc,rm2-6|\
 	compex,wpq873|\
 	dynalink,dl-wrx36|\
 	edimax,cax1800|\
