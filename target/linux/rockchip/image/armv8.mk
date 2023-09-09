6b01d24aa2768bdb329455d414306dde43e3e4c6
TahomaSoft <bacon@tahomasoft.com>
2023-09-09 19:35:23 -0400

    needed to fix bootscript reference

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -94,7 +94,7 @@ define Device/linkstar_common
   DEVICE_VENDOR := SeeedLinkStar
   SOC := rk3568
   UBOOT_DEVICE_NAME := opc-h68k-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | rk35xx-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | rk35xx-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
 endef
 
