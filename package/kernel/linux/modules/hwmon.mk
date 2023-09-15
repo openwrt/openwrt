76713f4b8247e9f0a9631a7b25c476cf8b0ca0a6
Til Kaiser <til.kaiser@gmx.de>
2023-09-15 12:03:07 +0200

    packages: add needed packages for Mellanox Spectrum Switches
    Add needed kernel and firmware packages for Mellanox Spectrum
    SN2000 Series Switches.
    
    Signed-off-by: Til Kaiser <til.kaiser@gmx.de>

--- package/kernel/linux/modules/hwmon.mk
+++ package/kernel/linux/modules/hwmon.mk
@@ -77,6 +77,23 @@ endef
 $(eval $(call KernelPackage,hwmon-adt7475))
 
 
+define KernelPackage/hwmon-coretemp
+  TITLE:=Intel Core/Core2/Atom temperature sensor
+  KCONFIG:=CONFIG_SENSORS_CORETEMP
+  FILES:=$(LINUX_DIR)/drivers/hwmon/coretemp.ko
+  AUTOLOAD:=$(call AutoProbe,coretemp)
+  $(call AddDepends/hwmon,)
+endef
+
+define KernelPackage/hwmon-coretemp/description
+  Kernel module for Intel Core/Core2/Atom temperature monitoring support.
+  Most of the family 6 CPUs are supported.
+  Check Documentation/hwmon/coretemp.rst for details.
+endef
+
+$(eval $(call KernelPackage,hwmon-coretemp))
+
+
 define KernelPackage/hwmon-dme1737
   TITLE:=SMSC DME1737 and compatible monitoring support
   KCONFIG:=CONFIG_SENSORS_DME1737
