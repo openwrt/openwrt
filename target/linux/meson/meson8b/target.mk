70ac353d0a4e723a22349191fa5343c7e987a3a9
晓东 <1052672227@qq.com>
2023-09-19 07:59:15 +0800

    meson: add support for onecloud
    Specifications:
    
    SoC: Amlogic S805 ARM Cortex-A53 1.5 GHz
    RAM: 1 GB RAM
    Flash: 8 GB eMMC
    LAN: 1x RTL8211F Gigabit Ethernet/at support
    LEDs: 1x blue and 1x red
    USB：Two USB 2.0
    Buttons: 1x reset button
    
    Flashing instructions:
    Standard onecloud SD card installation procedure - copy image to SD card,
    insert into SD card slot on the device and boot.
    
    u-boot
    https://github.com/shiyu1314/u-boot-onecloud/releases
    
    Signed-off-by: 晓东 <1052672227@qq.com>

---
+++ target/linux/meson/meson8b/target.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2020 shiyu1314 <1052672227@qq.com>
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+ARCH:=arm
+SUBTARGET:=meson8b
+BOARDNAME:=Amlogic S805 boards (32 bit)
+CPU_TYPE:=cortex-a5
+CPU_SUBTYPE:=vfpv4
+MAINTAINER:=shiyu1314 <1052672227@qq.com>
+
+define Target/Description
+	Build firmware image for Amlogic S805 SoC devices.
+	This firmware features a 32 bit kernel.
+endef
