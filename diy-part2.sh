ba489ef7762be79a03a0671a7490ff881560d617
Thanh Phat <85094758+InBomBi@users.noreply.github.com>
2023-10-03 20:07:22 +0700

    Create diy-part2.sh
    Signed-off-by: Thanh Phat <85094758+InBomBi@users.noreply.github.com>

---
+++ diy-part2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
