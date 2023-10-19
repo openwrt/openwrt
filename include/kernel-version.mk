7592df4c5117bad3d60cebb310048b530cc77e5b
Linus Lüssing <linus.luessing@c0d3.blue>
2023-10-19 20:52:16 +0200

    kernel: bump 4.14 to 4.14.327
    The following four new config symbols were added due to the update
    and were set to their default values:
    
    * CONFIG_GDS_FORCE_MITIGATION=n
    * CONFIG_INET_TABLE_PERTURB_ORDER=16
    * CONFIG_RANDOM_TRUST_CPU=y
    * CONFIG_RANDOM_TRUST_BOOTLOADER=y
    
    The following patches were initially rejected and needed manual, but
    small and straightforward adjustments:
    
    * .../generic/.../289-v4.16-netfilter-add-defines-for-arp-decnet-max-hooks.patch
    * .../generic/.../303-v4.16-netfilter-nf_tables-remove-multihook-chains-and-fami.patch
    * .../generic/.../344-v4.16-netfilter-nf_tables-allocate-handle-and-delete-objec.patch
    * .../ath79/.../403-mtd_fix_cfi_cmdset_0002_status_check.patch
    * .../ar71xx/.../403-mtd_fix_cfi_cmdset_0002_status_check.patch
    * .../ar71xx/.../930-chipidea-pullup.patch
    * .../oxnas/.../100-oxnas-clk-plla-pllb.patch
    * .../lantiq/.../0028-NET-lantiq-various-etop-fixes.patch
    * .../layerscape/.../202-core-linux-support-layerscape.patch
    * .../layerscape/.../707-dpaa-ethernet-support-layerscape.patch
    * .../layerscape/.../807-usb-support-layerscape.patch
    
    All other patches were updated automatically via
    "make target/linux/refresh".
    
    Runtime tested on an x86 VM and ath79 UniFi AP. All other targets were
    build tested only.
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .275
+LINUX_VERSION-4.14 = .327
 
-LINUX_KERNEL_HASH-4.14.275 = 100a9960fb2d8e079c9feeef640715a7fb749ed728a57e427f9e2443212e58f9
+LINUX_KERNEL_HASH-4.14.327 = bff085d8832dd5c3d717beeebdff78a0b678f0a2307f83acf22764dabdcb0a5a
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
