8208bea24a2df8c857eb878a1a0cfcd7fe7de5e1
Andrew Sim <andrewsimz@gmail.com>
2023-10-21 13:46:21 +0200

    netifd: update packet-steering.sh
    update the packet-steering script fixing all warnings and recommendations
    detected by shellcheck
    
    Signed-off-by: Andrew Sim <andrewsimz@gmail.com>

--- package/network/config/netifd/files/usr/libexec/network/packet-steering.sh
+++ package/network/config/netifd/files/usr/libexec/network/packet-steering.sh
@@ -1,70 +1,75 @@
 #!/bin/sh
+
 NPROCS="$(grep -c "^processor.*:" /proc/cpuinfo)"
 [ "$NPROCS" -gt 1 ] || exit
 
-PROC_MASK="$(( (1 << $NPROCS) - 1 ))"
+PROC_MASK="$(( (1 << NPROCS) - 1 ))"
 
 find_irq_cpu() {
-	local dev="$1"
-	local match="$(grep -m 1 "$dev\$" /proc/interrupts)"
-	local cpu=0
+    dev="$1"
+    match="$(grep -m 1 "$dev\$" /proc/interrupts)"
+    cpu=0
 
-	[ -n "$match" ] && {
-		set -- $match
-		shift
-		for cur in $(seq 1 $NPROCS); do
-			[ "$1" -gt 0 ] && {
-				cpu=$(($cur - 1))
-				break
-			}
-			shift
-		done
-	}
-
-	echo "$cpu"
+    [ -n "$match" ] && {
+        set -- "$match"
+        shift
+        for cur in $(seq 1 "$NPROCS"); do
+            [ "$1" -gt 0 ] && {
+                cpu=$((cur - 1))
+                break
+            }
+            shift
+        done
+    }
+    echo "$cpu"
 }
 
 set_hex_val() {
-	local file="$1"
-	local val="$2"
-	val="$(printf %x "$val")"
-	[ -n "$DEBUG" ] && echo "$file = $val"
-	echo "$val" > "$file"
+    file="$1"
+    val="$2"
+    val="$(printf %x "$val")"
+    [ -n "$DEBUG" ] && echo "$file = $val"
+    echo "$val" > "$file"
 }
 
 packet_steering="$(uci get "network.@globals[0].packet_steering")"
 [ "$packet_steering" != 1 ] && exit 0
 
-exec 512>/var/lock/smp_tune.lock
-flock 512 || exit 1
+exec 9>/var/lock/smp_tune.lock
+flock 9 || exit 1
 
 [ -e "/usr/libexec/platform/packet-steering.sh" ] && {
-	/usr/libexec/platform/packet-steering.sh
-	exit 0
+    /usr/libexec/platform/packet-steering.sh
+    exit 0
 }
 
 for dev in /sys/class/net/*; do
-	[ -d "$dev" ] || continue
+    [ -d "$dev" ] || continue
 
-	# ignore virtual interfaces
-	[ -n "$(ls "${dev}/" | grep '^lower_')" ] && continue
-	[ -d "${dev}/device" ] || continue
+    # Ignore virtual interfaces
+    for file in "${dev}/lower_"*; do
+        if [ -e "$file" ]; then
+            continue
+        fi
+    done
 
-	device="$(readlink "${dev}/device")"
-	device="$(basename "$device")"
-	irq_cpu="$(find_irq_cpu "$device")"
-	irq_cpu_mask="$((1 << $irq_cpu))"
+    [ -d "${dev}/device" ] || continue
 
-	for q in ${dev}/queues/tx-*; do
-		set_hex_val "$q/xps_cpus" "$PROC_MASK"
-	done
+    device="$(readlink "${dev}/device")"
+    device="$(basename "$device")"
+    irq_cpu="$(find_irq_cpu "$device")"
+    irq_cpu="$((1 << irq_cpu))"
 
-	# ignore dsa slave ports for RPS
-	subsys="$(readlink "${dev}/device/subsystem")"
-	subsys="$(basename "$subsys")"
-	[ "$subsys" = "mdio_bus" ] && continue
+    for q in "${dev}/queues/tx-"*; do
+        set_hex_val "$q/xps_cpus" "$PROC_MASK"
+    done
 
-	for q in ${dev}/queues/rx-*; do
-		set_hex_val "$q/rps_cpus" "$PROC_MASK"
-	done
+    # ignore DSA slave ports for RPS
+    subsys="$(readlink "${dev}/device/subsystem")"
+    subsys="$(basename "$subsys")"
+    [ "$subsys" = "mdio_bus" ] && continue
+
+    for q in "${dev}/queues/rx-"*; do
+        set_hex_val "$q/rps_cpus" "$PROC_MASK"
+    done
 done
