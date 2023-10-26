3d0a78add22754aa891529b6702b5e4c7b837446
Florian Maurer <f.maurer@outlook.de>
2023-10-26 08:40:59 +0200

    qualcommax: only build initramfs if CONFIG_TARGET_ROOTFS_INITRAMFS is set
    This makes it possible to build the ipq807x netgear-wax218 without initramfs - which is required for downstream projects (gluon)
    
    Signed-off-by: Florian Maurer <f.maurer@outlook.de>
    (cherry picked from commit b3d2008f92f15ff776ad6c4f6d113b0cda1bd2d0)

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -80,15 +80,17 @@ TARGET_DEVICES += edimax_cax1800
 define Device/netgear_wax218
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
-	ARTIFACTS := web-ui-factory.fit
 	DEVICE_VENDOR := Netgear
 	DEVICE_MODEL := WAX218
 	DEVICE_DTS_CONFIG := config@hk07
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
 	SOC := ipq8072
+ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
+	ARTIFACTS := web-ui-factory.fit
 	ARTIFACT/web-ui-factory.fit := append-image initramfs-uImage.itb | \
 		ubinize-kernel | qsdk-ipq-factory-nand
+endif
 	DEVICE_PACKAGES := kmod-spi-gpio kmod-spi-bitbang kmod-gpio-nxp-74hc164 \
 		ipq-wifi-netgear_wax218
 endef
