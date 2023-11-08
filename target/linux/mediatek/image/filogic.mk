f2a7151ae6e1ae96e29bd2f48b00b69058e6eb88
Ian Ishmael C. Oderon <ianoderon@gmail.com>
2023-11-08 23:31:18 +0800

    mediatek: add support for ZBT Z8103AX
    Specifications:
    SoC: MediaTek MT7981B
    RAM: 256MiB
    Flash: SPI-NAND 128 MiB
    Switch: 1 WAN, 3 LAN (Gigabit)
    Buttons: Reset, Mesh
    Power: DC 12V 1A
    WiFi: WiFi6 (AX/AC/N)
    UART: 115200n8
      --------------------------
      |         Layout         |
      |   -----------------    |
      | 4 | VCC RX TX GND | <= |
      |   -----------------    |
      --------------------------
    
    Installation:
    A. Through OpenWrt Dashboard:
    If your router comes with OpenWrt preinstalled (modified by the seller),
    you can easily upgrade it by going to the dashboard (192.168.1.1) and then upload
    the latest OpenWrt firmware.

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -755,3 +755,20 @@ define Device/zyxel_nwa50ax-pro
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += zyxel_nwa50ax-pro
+
+define Device/zbtlink_zbt-z8103ax
+  DEVICE_VENDOR := ZBT
+  DEVICE_MODEL := Z8103AX
+  DEVICE_DTS := mt7981b-zbtlink-zbt-z8103ax
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+  KERNEL_IN_UBI := 1
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += zbtlink_zbt-z8103ax
