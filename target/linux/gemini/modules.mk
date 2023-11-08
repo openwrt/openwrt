469577886551d9d941c5144295e4b80238d16d41
Robert Marko <robimarko@gmail.com>
2023-11-08 13:31:12 +0100

    gemini: usb-fotg210: enable OTG support
    The usb-fotg210 does not currently select CONFIG_USB_FOTG210_UDC which
    enable OTG support, but it was previously selected directly in the target
    kernel config so lets enable it to keep the functionality identical.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/gemini/modules.mk
+++ target/linux/gemini/modules.mk
@@ -1,8 +1,10 @@
 define KernelPackage/usb-fotg210
   TITLE:=Support for FOTG210 USB host and device controllers
   DEPENDS:=@USB_SUPPORT @TARGET_gemini
-  KCONFIG:=CONFIG_USB_FOTG210 \
-	CONFIG_USB_FOTG210_HCD=y
+  KCONFIG:= \
+  CONFIG_USB_FOTG210 \
+  CONFIG_USB_FOTG210_HCD=y \
+  CONFIG_USB_FOTG210_UDC=y
   FILES:=$(LINUX_DIR)/drivers/usb/fotg210/fotg210.ko
   AUTOLOAD:=$(call AutoLoad,50,1)
   $(call AddDepends/usb)
