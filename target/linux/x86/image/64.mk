04b11cb7df6c5e02c1bcccd44e80bbeba0d5f439
Tan Zien <nabsdh9@gmail.com>
2023-11-10 01:58:28 +0800

    x86/64: decomposition nvidia spectrum platform image profile
    There are complaints that the x86/64 generic profile after add
    NVIDIA Spectrum support results in generated images that are too large.
    
    Also change the image name as the driver apparently supports
    more than just spectrum-1 platforms.
    
    Signed-off-by: Tan Zien <nabsdh9@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -4,11 +4,21 @@ define Device/generic
   DEVICE_PACKAGES += \
 	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-e1000e kmod-e1000 \
 	kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 \
-	kmod-tg3 kmod-mlxsw-core kmod-mlxsw-pci kmod-mlxsw-i2c \
+	kmod-tg3
+  GRUB2_VARIANT := generic
+endef
+TARGET_DEVICES += generic
+
+define Device/nvidia_spectrum
+  DEVICE_VENDOR := NVIDIA
+  DEVICE_MODEL := Spectrum Platform
+  DEVICE_PACKAGES += \
+  	kmod-e1000e kmod-igb kmod-ixgbe kmod-mlx5-core kmod-fs-vfat \
+  	kmod-mlxsw-core kmod-mlxsw-pci kmod-mlxsw-i2c \
   	kmod-mlxsw-spectrum kmod-mlxsw-minimal kmod-mlxfw \
   	kmod-leds-mlxcpld kmod-lib-objagg kmod-lib-parman \
   	kmod-hwmon-coretemp kmod-hwmon-drivetemp kmod-hwmon-jc42 \
   	kmod-i2c-i801 mlxsw_spectrum-firmware
   GRUB2_VARIANT := generic
 endef
-TARGET_DEVICES += generic
+TARGET_DEVICES += nvidia_spectrum
