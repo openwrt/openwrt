6b7c4739c407588865aaa0d6868cf6bfe216a987
Rafał Miłecki <rafal@milecki.pl>
2023-11-26 21:24:28 +0100

    base-files: execute package's "postinst" after executing uci-defaults
    Allow "postinst" scripts to perform extra actions after applying all
    kind of fixups implemented using uci-defaults.
    
    This is needed e.g. by uhttpd-mod-ubus which after installation in a
    running systems needs to:
    1. Update uhttpd config using its uci-defaults script
    2. Reload uhttpd
    
    While this approach makes sense there is a risk it'll blow up some
    corner case postinst usages. There is only 1 way to find out.
    
    Cc: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    (cherry picked from commit b799dd3c705dfd95745cdd94b13d1cd2ad2367a6)

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -262,11 +262,6 @@ default_postinst() {
 
 	add_group_and_user "${pkgname}"
 
-	if [ -f "$root/usr/lib/opkg/info/${pkgname}.postinst-pkg" ]; then
-		( . "$root/usr/lib/opkg/info/${pkgname}.postinst-pkg" )
-		ret=$?
-	fi
-
 	if [ -d "$root/rootfs-overlay" ]; then
 		cp -R $root/rootfs-overlay/. $root/
 		rm -fR $root/rootfs-overlay/
@@ -290,6 +285,11 @@ default_postinst() {
 		fi
 
 		rm -f /tmp/luci-indexcache
+	fi
+
+	if [ -f "$root/usr/lib/opkg/info/${pkgname}.postinst-pkg" ]; then
+		( . "$root/usr/lib/opkg/info/${pkgname}.postinst-pkg" )
+		ret=$?
 	fi
 
 	local shell="$(command -v bash)"
