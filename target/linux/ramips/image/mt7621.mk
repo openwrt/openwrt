96580500d2806136a98cf38657e7c6082dab7e1c
Mikhail Zhilkin <csharper2005@gmail.com>
2023-12-03 15:05:40 +0000

    ramips: add alternative name to Etisalat (Sercomm) S3
    This commit adds alternative name to Etisalat (Sercomm) S3 router.
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1093,6 +1093,8 @@ define Device/etisalat_s3
   SERCOMM_SWVER := 4009
   DEVICE_VENDOR := Etisalat
   DEVICE_MODEL := S3
+  DEVICE_ALT0_VENDOR := Sercomm
+  DEVICE_ALT0_MODEL := S3
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615-firmware kmod-usb3
 endef
 TARGET_DEVICES += etisalat_s3
