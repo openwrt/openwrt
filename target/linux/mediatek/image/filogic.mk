4300bc668819a77226b6e8ea079d3067f216e7bb
Ian Oderon <ianoderon@gmail.com>
2023-12-09 12:48:35 +0800

    mediatek: add support for Zbtlink ZBT-Z8103AX
    Specifications:
    SoC: MediaTek MT7981B
    RAM: 256MiB
    Flash: SPI-NAND 128 MiB
    Switch: 1 WAN, 3 LAN (Gigabit)
    Buttons: Reset, Mesh
    Power: DC 12V 1A
    WiFi: MT7976CN
    UART: 115200n8
    UART Layout:
    VCC-RX-TX-GND
    
    No. of Antennas: 6
    Note: Upon opening the router, only 5 antennas were connected
    to the mainboard.
    
    Led Layout:
    Power-Mesh-5gwifi-WAN-LAN3-LAN2-LAN1-2gWiFi
    
    Buttons:
    Reset-Mesh
    
    Installation:
    A. Through OpenWrt Dashboard:
    If your router comes with OpenWrt preinstalled (modified by the seller),
    you can easily upgrade by going to the dashboard (192.168.1.1) and then
    navigate to System -> Backup/Flash firmware, then flash the firmware
    
    B. Through TFTP
    Standard installation via UART:
    
    1. Connect USB Serial Adapter to the UART, (NOTE: Don't connect the VCC pin).
    2. Power on the router. Make sure that you can access your router via UART.
    3. Restart the router then repeatedly press ctrl + c to skip default boot.
    4. Type > bootmenu
    5. Press '2' to select upgrade firmware
    6. Press 'Y' on 'Run image after upgrading?'
    7. Press '0' and hit 'enter' to select TFTP client (default)
    8. Fill the U-Boot's IP address and TFTP server's IP address.
    9. Finally, enter the 'firmware' filename.
    
    Signed-off-by: Ian Oderon <ianoderon@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -831,6 +831,23 @@ define Device/zbtlink_zbt-z8102ax
 endef
 TARGET_DEVICES += zbtlink_zbt-z8102ax
 
+define Device/zbtlink_zbt-z8103ax
+  DEVICE_VENDOR := Zbtlink
+  DEVICE_MODEL := ZBT-Z8103AX
+  DEVICE_DTS := mt7981b-zbtlink-zbt-z8103ax
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+  KERNEL_IN_UBI := 1
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += zbtlink_zbt-z8103ax
+
 define Device/zyxel_ex5601-t0-stock
   DEVICE_VENDOR := Zyxel
   DEVICE_MODEL := EX5601-T0
