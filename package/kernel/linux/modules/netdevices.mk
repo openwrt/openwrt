openwrt/openwrt

commit 4b4c940fbc8f87b67c6b8c5cca035cfb88478af5

x86: Add kmod-dwmac-intel
This adds the Intel Ethernet driver for the Intel Quark/EHL/TGL chips.

Fixes: #13994
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/netdevices.mk
@@ -1546,7 +1546,7 @@ $(eval $(call KernelPackage,sfp))
 define KernelPackage/stmmac-core
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)
-  DEPENDS:=@(TARGET_armsr_armv8) +kmod-pcs-xpcs +kmod-ptp \
+  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8 +kmod-pcs-xpcs +kmod-ptp \
     +kmod-of-mdio
   KCONFIG:=CONFIG_STMMAC_ETH \
     CONFIG_STMMAC_SELFTESTS=n \

target/linux/x86/modules.mk
@@ -18,6 +18,18 @@ endef
 $(eval $(call KernelPackage,amd-xgbe))
 
 
+define KernelPackage/dwmac-intel
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Intel GMAC support
+  DEPENDS:=@TARGET_x86_64 +kmod-stmmac-core
+  KCONFIG:=CONFIG_DWMAC_INTEL
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-intel.ko
+  AUTOLOAD=$(call AutoLoad,45,dwmac-intel)
+endef
+
+$(eval $(call KernelPackage,dwmac-intel))
+
+
 define KernelPackage/f71808e-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Fintek F718xx/F818xx Watchdog Timer

target/linux/x86/image/64.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_VENDOR := Generic
   DEVICE_MODEL := x86/64
   DEVICE_PACKAGES += \
-	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-e1000e kmod-e1000 \
+	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel kmod-e1000e kmod-e1000 \
 	kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 \
 	kmod-tg3 kmod-mlxsw-core kmod-mlxsw-pci kmod-mlxsw-i2c \
   	kmod-mlxsw-spectrum kmod-mlxsw-minimal kmod-mlxfw \

commit b09e060e2c12535fb71f18bf0b47a0fc3e267896

package: kernel: move pcs-xpcs from armsr to generic
Move pcs-xpcs kmod from armsr modules.mk to generic modules package.

Also add additional dependency to x86_64 as stmmac-core it's now used
by x86_64 target and depends on this package.

Fixes: 4b4c940fbc8f ("x86: Add kmod-dwmac-intel")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

package/kernel/linux/modules/netdevices.mk
@@ -1543,6 +1543,18 @@ endef
 $(eval $(call KernelPackage,sfp))
 
 
+define KernelPackage/pcs-xpcs
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Synopsis DesignWare PCS driver
+  DEPENDS:=@(TARGET_x86_64||TARGET_armsr_armv8) +kmod-phylink
+  KCONFIG:=CONFIG_PCS_XPCS
+  FILES:=$(LINUX_DIR)/drivers/net/pcs/pcs_xpcs.ko
+  AUTOLOAD:=$(call AutoLoad,20,pcs_xpcs)
+endef
+
+$(eval $(call KernelPackage,pcs-xpcs))
+
+
 define KernelPackage/stmmac-core
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)

target/linux/armsr/modules.mk
@@ -47,17 +47,6 @@ endef
 
 $(eval $(call KernelPackage,fsl-pcs-lynx))
 
-define KernelPackage/pcs-xpcs
-  SUBMENU:=$(NETWORK_DEVICES_MENU)
-  TITLE:=Synopsis DesignWare PCS driver
-  DEPENDS:=@(TARGET_armsr_armv8) +kmod-phylink
-  KCONFIG:=CONFIG_PCS_XPCS
-  FILES:=$(LINUX_DIR)/drivers/net/pcs/pcs_xpcs.ko
-  AUTOLOAD:=$(call AutoLoad,20,pcs_xpcs)
-endef
-
-$(eval $(call KernelPackage,pcs-xpcs))
-
 define KernelPackage/fsl-fec
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   DEPENDS:=@(TARGET_armsr_armv8) +kmod-libphy +kmod-of-mdio \