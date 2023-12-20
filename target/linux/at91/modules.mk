376473ab8c125f76b5d8a7db924eeeeaa5b7b17b
Elliott Mitchell <ehem+openwrt@m5p.com>
2023-12-20 09:55:02 -0800

    at91/modules: assume udc subdirectory for USB modules
    This appears to be compatibility with an older kernel which didn't
    feature as many USB drivers, hence these were directly in
    drivers/usb.  Both appear to have permanently moved to
    drivers/usb/udc.
    
    Signed-off-by: Elliott Mitchell <ehem+openwrt@m5p.com>

--- target/linux/at91/modules.mk
+++ target/linux/at91/modules.mk
@@ -52,11 +52,7 @@ define KernelPackage/at91-udc
   TITLE:=USB Device Controller on atmel SoC
   DEPENDS:=@TARGET_at91 +kmod-usb-gadget +kmod-regmap-core
   KCONFIG:=CONFIG_USB_AT91
-ifneq ($(wildcard $(LINUX_DIR)/drivers/usb/gadget/udc/at91_udc.ko),)
   FILES:=$(LINUX_DIR)/drivers/usb/gadget/udc/at91_udc.ko
-else
-  FILES:=$(LINUX_DIR)/drivers/usb/gadget/at91_udc.ko
-endif
   AUTOLOAD:=$(call AutoLoad,51,at91_udc)
 endef
 
@@ -71,11 +67,7 @@ define KernelPackage/atmel-usba-udc
   TITLE:=High-speed USB Device Controller on atmel SoC
   DEPENDS:=@TARGET_at91 +kmod-usb-gadget
   KCONFIG:=CONFIG_USB_ATMEL_USBA
-ifneq ($(wildcard $(LINUX_DIR)/drivers/usb/gadget/udc/atmel_usba_udc.ko),)
   FILES:=$(LINUX_DIR)/drivers/usb/gadget/udc/atmel_usba_udc.ko
-else
-  FILES:=$(LINUX_DIR)/drivers/usb/gadget/atmel_usba_udc.ko
-endif
   AUTOLOAD:=$(call AutoLoad,51,atmel_usba_udc)
 endef
 
