18ed67bd36529bbbb4f90f67f2e2c3781a9c18fe
Sergey Sharshunov <s.sharshunov@gmail.com>
2023-12-23 15:49:46 +0500

    ramips: New device support zte,mf286c3 (mt7621)
    MF286C3 LTE CPE with GSM, WCDMA and WiFi
    
    Specifications:
    SoC: MediaTek MT7621 (MIPS1004Kc 32-bit 880MHz)
    RAM: 256MiB
    Flash: SPI-NAND 128 MiB
    Switch: 1 WAN (LAN), 3 LAN (Gigabit)
    Buttons: Reset, WPS, WLAN
    Power: DC 12V 1A
    WiFi: MT7603 and MT7663
    UART: 115200n8
    UART Layout:
    VCC-RX-TX-GND
    
    Led Layout:
    Power-WLAN-PHONE-3G/4G-SIGNAL(5 LEDS)
    
    Buttons:
    Reset-WPS-WLAN
    
    Original mtd paritions:
    Creating 14 MTD partitions on "MT7621-NAND":
    0x000000000000-0x000007f80000 : "ALL"
    0x000000000000-0x000000080000 : "Bootloader"
    0x000000080000-0x000000100000 : "Config"
    0x000000100000-0x000000200000 : "Factory"
    0x000000200000-0x000000300000 : "inventory"
    0x000000300000-0x000000400000 : "customer"
    0x000000400000-0x000000500000 : "zone"
    0x000000500000-0x000000a00000 : "reserved0"
    0x000000a00000-0x000000f00000 : "kernel0"
    0x000000f00000-0x000001400000 : "kernel1"
    0x000001400000-0x000003800000 : "ubi0"
    0x000003800000-0x000005c00000 : "ubi1"
    0x000005c00000-0x000006600000 : "reserved1"
    0x000006600000-0x000007500000 : "reserved2"
    
    Installation:
    A. Through OpenWrt Dashboard:
    If your router comes with OpenWrt preinstalled (modified by the seller), you can easily upgrade by going to the dashboard (192.168.1.1) and then navigate to System -> Backup/Flash firmware, then flash the firmware
    
    B. Through TFTP
    Standard installation via UART:
    
    Connect USB Serial Adapter to the UART, (NOTE: Don't connect the VCC pin).
    Power on the router. Make sure that you can access your router via UART.
    Restart the router then repeatedly press ctrl + c to skip default boot.
    Type > bootmenu
    Press '2' to select upgrade firmware
    Press 'Y' on 'Run image after upgrading?'
    Press '0' and hit 'enter' to select TFTP client (default)
    Fill the U-Boot's IP address and TFTP server's IP address.
    Finally, enter the 'firmware' filename.
    Signed-off-by: Sergey Sharshunov <s.sharshunov@gmail.com>
    
    Update target/linux/ramips/dts/mt7621_zte_mf286c3.dts
    
    Co-authored-by: DragonBluep <70847398+DragonBluep@users.noreply.github.com>
    Signed-off-by: SSharshunov <s.sharshunov@gmail.com>

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -156,6 +156,7 @@ platform_do_upgrade() {
 	xiaomi,mi-router-cr6609|\
 	xiaomi,redmi-router-ac2100|\
 	z-router,zr-2660|\
+	zte,mf286c3|\
 	zyxel,nwa50ax|\
 	zyxel,nwa55axe)
 		nand_do_upgrade "$1"
