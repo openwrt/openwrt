fd46680966e9b9bc282b665282e2e6a22a27c26e
Tomasz Maciej Nowak <tmn505@gmail.com>
2024-01-10 16:59:48 +0100

    ramips: mt76x8: enable small_flash feature
    Some of devices in this target have only 8 MiB space and are closing to
    borders of usable space. Particularly, TP-Link RE305 v1 already suffers
    from this issue[1], where with current partition layout, on release
    images, there's not enough space for overlay. So activate small_flash
    feature, which will remove some userspace hardening but will gain almost
    1 MiB additional flash memory space. Here is small size comparison of
    similar device (RE365 v1) with default config + LuCI:
    
    		kernel		rootfs		sysupgrade
    current:	2305728		3635044		5964584
    small_flash:	1713571		3320132		5047080
    
    1. https://github.com/openwrt/openwrt/issues/14215
    
    Suggested-by: Sander Vanheule <sander@svanheule.net>
    Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>

--- target/linux/ramips/mt76x8/target.mk
+++ target/linux/ramips/mt76x8/target.mk
@@ -4,7 +4,7 @@
 
 SUBTARGET:=mt76x8
 BOARDNAME:=MT76x8 based boards
-FEATURES+=usb ramdisk
+FEATURES+=usb ramdisk small_flash
 CPU_TYPE:=24kc
 
 DEFAULT_PACKAGES += kmod-mt7603 wpad-basic-mbedtls swconfig
