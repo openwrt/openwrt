20e4a18feb3f766b0f6ebec1afc154b345398a7a
Leon M. Busch-George <leon@georgemail.eu>
2024-01-12 16:10:40 +0100

    mediatek: filogic: add support for Cudy M3000 v1
    Hardware:
      SoC:     MT7981b
      RAM:     256 MB
      Flash:   128 MB SPI NAND
      Ethernet:
        1x 2.5Gbps (rtl8221b)
        1x 1Gbps (integrated phy)
      WiFi:    2x2 MT7981
      Buttons: Reset, WPS
      LED:     1x multicolor
    
    Solder on UART:
      - remove rubber ring on the bottom
      - remove screws
      - pull up the cylinder, maybe help by push on an ethernet socket with a screwdriver
      - remove the (3) screws holding the board in the frame
      - remove the board from the frame to get to the screws for the silver, flat heat shield
      - remove the (3) screws holding the heat shield
      - solder UART pins to the back of the board
        - make sure to have the pins point out on side with the black, finned heat spread
        - the markings for the pins are going to be below the silver heat shield
        - Vcc is not needed
    
    If you don't intend on using the UART outside of the installation process, you might not
    want to solder:
    
      - carefully scrape off the thin layer of epoxy on the holes (not the copper)
      - place your pin header with the UART attached in the holes
      - the pins, starting with the one closest to the socket:
        - Vcc (not required)
        - GND
        - RX
        - TX
      - either wedge the header or hold it with your fingers so that the pins stay in contact with the board
    
    Installation (UART):
      - attach an Ethernet cable to the 1Gbps port (black) on the router
      - hold the reset button while powering the router
      - press CTRL-C or wait for the timeout to get to the U-Boot prompt
      - prepare a TFTP server on the network to supply ..-initramfs-kernel.bin
      - use 'tftpboot' in the U-Boot shell to pull the image
      - boot the image using 'bootm'
      - push the ..-sysupgrade to the router using your preferred method
      - perform the upgrade with 'sysupgrade -n'
    
    There is a recovery mechanism that involves fetching a file called 'recovery.bin' but that is not understood yet.
    
    Signed-off-by: Leon M. Busch-George <leon@georgemail.eu>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -466,6 +466,28 @@ define Device/confiabits_mt7981
 endef
 TARGET_DEVICES += confiabits_mt7981
 
+define Device/cudy_m3000-v1
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := M3000
+  DEVICE_VARIANT := v1
+  DEVICE_DTS := mt7981b-cudy-m3000-v1
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += R37
+  DEVICE_DTS_LOADADDR := 0x44000000
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  KERNEL := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := sysupgrade.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+endef
+TARGET_DEVICES += cudy_m3000-v1
+
 define Device/cudy_re3000-v1
   DEVICE_VENDOR := Cudy
   DEVICE_MODEL := RE3000
