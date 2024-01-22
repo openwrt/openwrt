44168fda7800dfdd28c8d51fa92b4863f4fd6ea9
Mantas Pucka <mantas@8devices.com>
2024-01-22 11:28:32 +0200

    qualcommax: ipq60xx: Add 8devices Mango DVK
    8devices Mango DVK is a single board computer / devkit for 8devices Mango
    system-on-module (SoM).
    
    Specifications:
    * CPU: Qualcomm IPQ6010 Quad core Cortex-A53 1.8GHz
    * RAM: 512 MB
    * Storage:
        * 32 MB serial NOR flash (on SoM)
        * 256 MB parallel NAND flash (on DVK)
    * Ethernet:
        * 2x1G RJ45 ports(QCA8072 or QCA8075)
        * 1x2.5G RJ45 port (QCA8081)
        * 1xSFP (shares SGMII with QCA8081)
    * Switch: Qualcomm Atheros IPQ6010
    * WLAN:
        * 2.4GHz: QCN5121 2x2 802.11b/g/n/ax 574 Mbps PHY rate
        * 5GHz: QCN5152 2x2 802.11a/n/ac/ax 1201 Mbps PHY rate
    * USB:
        * 1x USB3.0 Type-A port
        * 1x USB2.0 available at mini PCIe slot
    * PCIe: 1x mini PCIe slot 1xLane Gen3 (8GT/s)
    * SD/eMMC (on a single shared bus - only one can be active):
        * micro SD slot
        * eMMC module connector
    * LEDs:
        * Green power led (not controllable)
        * Green 2.4GHz radio led (GPIO 67)
        * Green 5GHz radio led (GPIO 66)
    * Buttons:
        * 1x (WPS GPIO79) button
    * GPIOs: 2.54mm header brings out 18 GPIOs (1.8V level)
    * UART: 4-pin UART header (3.3V level)
        * 115200 8N1, 3.3V-Tx-Rx-GND (3.3V is pin 1 close to boot-switch SW2)
    * Power:
        * PoE IN on 2.5G port (passive 24-48V)
        * DC power terminal (12-58V)
    
    Installation instructions:
    
    Vendor image format is compatible with squashfs-sysupgrade image. Run:
    
    sysupgrade -n -F openwrt-qualcommax-ipq60xx-8devices_mango-dvk-squashfs-sysupgrade.bin
    
    Signed-off-by: Mantas Pucka <mantas@8devices.com>

---
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -0,0 +1,12 @@
+define Device/8devices_mango-dvk
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := 8devices
+	DEVICE_MODEL := Mango-DVK
+	IMAGE_SIZE := 27776k
+	BLOCKSIZE := 64k
+	SOC := ipq6010
+	SUPPORTED_DEVICES += 8devices,mango
+	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
+	DEVICE_PACKAGES := ipq-wifi-8devices_mango
+endef
+TARGET_DEVICES += 8devices_mango-dvk
