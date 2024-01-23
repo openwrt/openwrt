aed723e40ec8f009ff39453dfa6b799a521a3235
Tim Harvey <tharvey@gateworks.com>
2024-01-22 17:02:06 -0800

    kernel: add support for HCIUART_BCM
    Enable support for the Bluetooth Broadcom HCI protocol supporting
    various broadcom chip BT HCI devices we must enable:
     - BT_HCIUART_BCM=y
     - BT_HCIUART_SERDEV=y (dependency of BT_HCIUART_BCM)
     - SERIAL_DEV_CTRL_TTYPORT=y (dependency of BT_HCIUART_BCM)
     - SERIAL_DEV_BUS=y (dependency of SERIAL_DEV_CTRL_TTYPORT)
     - RAVE_SP_CORE=n (selected by above and we do not need)
     - MFD_IEI_WT61P803_PUZZLE=n (selected by above and we do not need)
     - X86_ANDROID_TABLETS=n (selected by above and we do not need)
    
    The size of the resulting btbcm.ko is 88KB on aarch64.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>
    ---
    v2:
     - undef CONFIG_MFD_IEI_WT61P803_PUZZLE which depends on SERIAL_DEV_BUS
     - undef CONFIG_X86_ANDROID_TABLETS which depends on SERIAL_DEV_BUS

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -43,7 +43,13 @@ define KernelPackage/bluetooth
 	CONFIG_BT_HCIBTUSB_MTK=y \
 	CONFIG_BT_HCIBTUSB_RTL=y \
 	CONFIG_BT_HCIUART \
-	CONFIG_BT_HCIUART_BCM=n \
+	CONFIG_BT_HCIUART_BCM=y \
+	CONFIG_BT_HCIUART_SERDEV=y \
+	CONFIG_SERIAL_DEV_CTRL_TTYPORT=y \
+	CONFIG_SERIAL_DEV_BUS=y \
+	CONFIG_RAVE_SP_CORE=n \
+	CONFIG_MFD_IEI_WT61P803_PUZZLE=n \
+	CONFIG_X86_ANDROID_TABLETS=n \
 	CONFIG_BT_HCIUART_INTEL=n \
 	CONFIG_BT_HCIUART_H4 \
 	CONFIG_BT_HCIUART_NOKIA=n \
@@ -58,6 +64,7 @@ define KernelPackage/bluetooth
 	$(LINUX_DIR)/drivers/bluetooth/btusb.ko \
 	$(LINUX_DIR)/drivers/bluetooth/btintel.ko \
 	$(LINUX_DIR)/drivers/bluetooth/btrtl.ko \
+	$(LINUX_DIR)/drivers/bluetooth/btbcm.ko \
 	$(LINUX_DIR)/drivers/bluetooth/btmtk.ko@ge5.17
   AUTOLOAD:=$(call AutoProbe,bluetooth rfcomm bnep hidp hci_uart btusb)
 endef
