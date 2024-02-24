eb65421c523f93b586bad7a2ca5f85202f7189de
Paweł Owoc <frut3k7@gmail.com>
2024-02-24 20:47:58 +0100

    qualcommax: ipq807x: Add ability to perform upgrade on current partition for MX4200/MX5300
    Dual firmware devices like Linksys MX4200 and MX5300 use separate rootfs partition.
    This option helps to keep configuration during upgrade.
    
    Signed-off-by: Paweł Owoc <frut3k7@gmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -77,14 +77,21 @@ platform_do_upgrade() {
 	linksys,mx4200v1|\
 	linksys,mx4200v2|\
 	linksys,mx5300)
+		CI_UBIPART="rootfs"
 		boot_part="$(fw_printenv -n boot_part)"
-		if [ "$boot_part" -eq "1" ]; then
-			fw_setenv boot_part 2
-			CI_KERNPART="alt_kernel"
-			CI_UBIPART="alt_rootfs"
+		if [ -n "$UPGRADE_OPT_CURR_PARTITION" ]; then
+			if [ "$boot_part" -eq "2" ]; then
+				CI_KERNPART="alt_kernel"
+				CI_UBIPART="alt_rootfs"
+			fi
 		else
-			fw_setenv boot_part 1
-			CI_UBIPART="rootfs"
+			if [ "$boot_part" -eq "1" ]; then
+				fw_setenv boot_part 2
+				CI_KERNPART="alt_kernel"
+				CI_UBIPART="alt_rootfs"
+			else
+				fw_setenv boot_part 1
+			fi
 		fi
 		fw_setenv boot_part_ready 3
 		fw_setenv auto_recovery yes
