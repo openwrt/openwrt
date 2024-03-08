65df33fc76f722701f161dd88828f61c9e31f1d1
Daniel Golle <daniel@makrotopia.org>
2024-03-08 20:20:23 +0000

    ramips: add support for YunCore G720
    The YunCore G720 is a dual band 802.11ax router with 5 GbE ports.
    
    Specs:
    - SoC: MediaTek MT7621
    - Ethernet: 5x GbE ports (built-in MT7530)
    - Wireless 2.4GHz / 5GHz: MediaTek MT7915E
    - RAM: 256MiB
    - ROM: 16MiB (W25Q128)
    - 1 Button (reset)
    - 8 LEDs (1x system, 2x wifi, 5x switch ports)
    
    Flash instructions:
    The vendor firmware is based on OpenWrt, the sysupgrade image can be
    flashed using the '-F' (force) option on the CLI.
    Make sure not to keep settings when doing so.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2918,6 +2918,16 @@ define Device/yuncore_fap690
 endef
 TARGET_DEVICES += yuncore_fap690
 
+define Device/yuncore_g720
+  $(Device/dsa-migration)
+  DEVICE_VENDOR := YunCore
+  DEVICE_MODEL := G720
+  DEVICE_PACKAGES := kmod-mt7915-firmware
+  IMAGE_SIZE := 15808k
+  SUPPORTED_DEVICES += mt7621-rfb-ax-nor
+endef
+TARGET_DEVICES += yuncore_g720
+
 define Device/zbtlink_zbt-we1326
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
