7753b143cde79a048cdda003b9e2825f50399b2c
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2024-03-13 02:24:11 +0100

    ramips: 6.6: fix net drivers compatibility
    Fix compatibility of ralink net drivers with kernel 6.6.
    It follows the kernel patch: u64_stats: Streamline the implementation (https://github.com/torvalds/linux/commit/44b0c2957adc62b86fcd51adeaf8e993171b)
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- target/linux/ramips/files/drivers/net/ethernet/ralink/ethtool.c
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/ethtool.c
@@ -199,12 +199,20 @@ static void fe_get_ethtool_stats(struct net_device *dev,
 	do {
 		data_src = &hwstats->tx_bytes;
 		data_dst = data;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 0)
+		start = u64_stats_fetch_begin(&hwstats->syncp);
+#else
 		start = u64_stats_fetch_begin_irq(&hwstats->syncp);
+#endif
 
 		for (i = 0; i < ARRAY_SIZE(fe_gdma_str); i++)
 			*data_dst++ = *data_src++;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 0)
+	} while (u64_stats_fetch_retry(&hwstats->syncp, start));
+#else
 	} while (u64_stats_fetch_retry_irq(&hwstats->syncp, start));
+#endif
 }
 
 static struct ethtool_ops fe_ethtool_ops = {
