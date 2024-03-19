60061b5c26b65004377a886f917bcd3005e97dad
Shiji Yang <yangshiji66@qq.com>
2024-03-19 21:25:52 +0800

    ath79: disable building small Flash EnGenius devices
    The buildbot has not successfully built them for a long time due
    to small Flash size.
    
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- target/linux/ath79/image/tiny.mk
+++ target/linux/ath79/image/tiny.mk
@@ -40,6 +40,7 @@ define Device/engenius_eap350-v1
   IMAGE_SIZE := 4928k
   LOADER_FLASH_OFFS := 0x1a0000
   SENAO_IMGNAME := senao-eap350
+  DEFAULT := n
 endef
 TARGET_DEVICES += engenius_eap350-v1
 
@@ -53,6 +54,7 @@ define Device/engenius_ecb350-v1
   IMAGE_SIZE := 4928k
   LOADER_FLASH_OFFS := 0x1a0000
   SENAO_IMGNAME := senao-ecb350
+  DEFAULT := n
 endef
 TARGET_DEVICES += engenius_ecb350-v1
 
@@ -66,6 +68,7 @@ define Device/engenius_enh202-v1
   IMAGE_SIZE := 4928k
   LOADER_FLASH_OFFS := 0x1a0000
   SENAO_IMGNAME := senao-enh202
+  DEFAULT := n
 endef
 TARGET_DEVICES += engenius_enh202-v1
 
