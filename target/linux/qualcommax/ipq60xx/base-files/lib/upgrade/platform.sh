6be5a883bf5642c03eeaa539737ab2faa859b866
Chukun Pan <amadeus@jmu.edu.cn>
2024-03-20 23:05:07 +0800

    qualcommax: ipq60xx: add GL.iNet GL-AX1800/AXT1800 support
    Specifications:
      SoC:     Qualcomm IPQ6000
      RAM:     512 MiB
      Flash:   128 MiB NAND
      ETH:     Qualcomm QCA8075
      WLAN1:   2.4GHz 802.11b/g/n/ax 2x2
      WLAN2:   5GHz 802.11a/n/ac/ax 2x2
      Button:  Reset, Switch
      USB:     1x 3.0
      SD slot: 1 in GL-AXT1800
    
    Install via stock firmware:
      Upload factory.bin in stock firmware's upgrade page,
      do not preserve settings.
    
    Install via uboot web failsafe:
      Push the reset button for 5 seconds, then use broswer
      to access http://192.168.1.1, and upload factory.ubi.
    
    Afterwards upgrade can use sysupgrade image.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
    Link: https://github.com/openwrt/openwrt/pull/14950
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -110,6 +110,12 @@ platform_do_upgrade() {
 		fw_setenv bootcount 0
 		nand_do_upgrade "$1"
 		;;
+	glinet,gl-ax1800|\
+	glinet,gl-axt1800|\
+	netgear,wax214|\
+	qihoo,360v6)
+		nand_do_upgrade "$1"
+		;;
 	linksys,mr7350)
 		boot_part="$(fw_printenv -n boot_part)"
 		if [ "$boot_part" -eq "1" ]; then
@@ -122,10 +128,6 @@ platform_do_upgrade() {
 		fi
 		fw_setenv boot_part_ready 3
 		fw_setenv auto_recovery yes
-		nand_do_upgrade "$1"
-		;;
-	netgear,wax214|\
-	qihoo,360v6)
 		nand_do_upgrade "$1"
 		;;
 	tplink,eap610-outdoor)
