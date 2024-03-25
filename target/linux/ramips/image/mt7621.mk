927a77ecffd791aa096faff609d6897f99673603
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-03-25 21:39:30 +0900

    ramips: add support for ELECOM WMC-M1267GST2
    ELECOM WMC-M1267GST2 is a 2.4/5 GHz band 11ac (Wi-Fi 5) mesh router,
    based on MT7621A.
    
    Specification:
    
    - SoC          : MediaTek MT7621A
    - RAM          : DDR3 256 MiB (Nanya NT5CC128M16JR-EK)
    - Flash        : SPI-NOR 32 MiB (Winbond W25Q256JVFIQ)
    - WLAN         : 2.4/5 GHz 2T2R (MediaTek MT7615D)
    - Ethernet     : 10/100/1000 Mbps x5
      - switch     : MediaTek MT7530 (SoC)
    - LEDs/Keys    : 6x/6x
    - UART         : through-hole on PCB ("J4")
      - arrangement: 3.3V, GND, TX, RX from tri-angle marking
      - settings   : 57600n8
    - Power        : 12 VDC, 1 A
    
    Flash instruction using factory image:
    
    1. Boot WMC-M1267GST2 normally with "Router" mode
    2. Access to "http://192.168.2.1/" and open firmware update page
       ("ファームウェア更新")
    3. Select the OpenWrt factory image and click apply ("適用") button
    4. Wait ~120 seconds to complete flashing
    
    MAC addresses:
    
    LAN    : 04:AB:18:xx:xx:61 (Factory, 0xFFF4 (hex))
    WAN    : 04:AB:18:xx:xx:62 (Factory, 0xFFFA (hex))
    2.4 GHz: 04:AB:18:xx:xx:63
    5 GHz  : 04:AB:18:xx:xx:64 (Factory, 0x4 (hex))
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -979,6 +979,25 @@ define Device/edimax_rg21s
 endef
 TARGET_DEVICES += edimax_rg21s
 
+define Device/elecom_wrc-gs
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  DEVICE_VENDOR := ELECOM
+  IMAGES += factory.bin
+  IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | \
+	elecom-wrc-gs-factory $$$$(ELECOM_HWNAME) 0.00 -N | \
+	append-string MT7621_ELECOM_$$$$(ELECOM_HWNAME)
+  DEVICE_PACKAGES := kmod-mt7615-firmware -uboot-envtools
+endef
+
+define Device/elecom_wmc-m1267gst2
+  $(Device/elecom_wrc-gs)
+  IMAGE_SIZE := 24576k
+  DEVICE_MODEL := WMC-M1267GST2
+  ELECOM_HWNAME := WMC-DLGST2
+endef
+TARGET_DEVICES += elecom_wmc-m1267gst2
+
 define Device/elecom_wrc-1167ghbk2-s
   $(Device/dsa-migration)
   IMAGE_SIZE := 15488k
@@ -990,17 +1009,6 @@ define Device/elecom_wrc-1167ghbk2-s
   DEVICE_PACKAGES := kmod-mt7615-firmware -uboot-envtools
 endef
 TARGET_DEVICES += elecom_wrc-1167ghbk2-s
-
-define Device/elecom_wrc-gs
-  $(Device/dsa-migration)
-  $(Device/uimage-lzma-loader)
-  DEVICE_VENDOR := ELECOM
-  IMAGES += factory.bin
-  IMAGE/factory.bin := $$(sysupgrade_bin) | check-size | \
-	elecom-wrc-gs-factory $$$$(ELECOM_HWNAME) 0.00 -N | \
-	append-string MT7621_ELECOM_$$$$(ELECOM_HWNAME)
-  DEVICE_PACKAGES := kmod-mt7615-firmware -uboot-envtools
-endef
 
 define Device/elecom_wrc-1167gs2-b
   $(Device/elecom_wrc-gs)
