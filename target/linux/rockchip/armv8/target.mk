cf6e5970ffdba9ba6151610cccb2daa5b06b12ef
Han Pengfei <pengphei@foxmail.com>
2024-04-01 15:46:30 +0800

    rockchip: enable cortexa7 sub target
    Signed-off-by: Han Pengfei <pengphei@foxmail.com>

--- target/linux/rockchip/armv8/target.mk
+++ target/linux/rockchip/armv8/target.mk
@@ -1,5 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2017 Hauke Mehrtens
+
+include $(TOPDIR)/rules.mk
+
 ARCH:=aarch64
-SUBTARGET:=armv8
 BOARDNAME:=RK33xx boards (64 bit)
 
 define Target/Description
