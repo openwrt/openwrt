9052c80ef33050322dac601b33ca14f6038ad108
Han Pengfei <pengphei@foxmail.com>
2024-04-01 17:35:50 +0800

    rockchip:cortexa7: fix image generation for arm
    Signed-off-by: Han Pengfei <pengphei@foxmail.com>

--- target/linux/rockchip/image/cortexa7.mk
+++ target/linux/rockchip/image/cortexa7.mk
@@ -5,7 +5,7 @@
 define Device/Default-emmc
   $(Device/Default)
   FILESYSTEMS += squashfs ext4
-  DEVICE_DTS := rockchip/$(1)
+  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
   IMAGE/boot.img := resource-img $(1)
@@ -14,7 +14,7 @@ endef
 define Device/Default-spiflash
   $(Device/Default)
   FILESYSTEMS += squashfs jffs2
-  DEVICE_DTS := rockchip/$(1)
+  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
   IMAGE/boot.img := resource-img $(1)
@@ -23,7 +23,7 @@ endef
 define Device/Default-nandflash
   $(Device/Default)
   FILESYSTEMS += squashfs ubifs
-  DEVICE_DTS := rockchip/$(1)
+  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
   IMAGE/boot.img := resource-img $(1)
@@ -31,13 +31,11 @@ endef
 
 
 define Device/luckfox_pico-max
-  $(Device/Default-nandflash)
+  $(Device/Default-emmc)
   DEVICE_TITLE := Luckfox Pico Max
   SUPPORTED_DEVICES := luckfox,pico-max
   SOC := rv1106g
   DEVICE_DTS := rv1106g-luckfox-pico-pro-max
   UBOOT_DEVICE_NAME := luckfox-pico-max-rv1106
-  IMAGES := sysupgrade.img.gz
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += luckfox_pico-max
