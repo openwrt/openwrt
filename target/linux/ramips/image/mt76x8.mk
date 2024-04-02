2a2f53415ed8ed79778edb8135bdbb5e819e587b
Lea Teuberth <lea.teuberth@outlook.com>
2024-04-02 11:30:32 +0200

    ramips: add support for TP-Link Archer MR200v5
    Specifications:
    	CPU:     MT7628AN 580MHz
    	RAM:     64MB DDR2
    	FLASH:   8MB EN25QH64 NOR SPI
    	WIFI:    2.4GHz 2x2 MT7628 b/g/n internal
    	WIFI:    5GHz 1x1 MT7610E ac/n PCI
    	LTE:     Qualcomm MDM9207
    	ETH:     4xLAN 100base-T integrated
    	SWITCH:  RT3050-ESW Port 0,1,2,3: LAN, Port 6: CPU
    	LEDS:    LAN, WAN, Power, 3x signal strength, WiFi
    	BTNS:    Reset, WiFi toggle
    	UART:    Near ETH ports, Vcc-GND-RX-TX, 115200, 8N1
    
    Installation:
    	1. Update using recovery mode
            - set your IP to 192.168.0.225, subnet mask: 255.255.255.0
            - start tftp server, rename tftp-recovery.bin to
              tp_recovery.bin and place it into the server's directory
            - while holdig the "reset" button, power on the device
            - keep holding "reset" until the file is being transferred
    
    Notes:
    	This board has only one MAC address programmed
            in the "romfile" partition:
            - MAC for phy0 (2.4GHz) at romfile 0xf100 (0)
            - MAC for phy1 (5GHz) at romfile 0xf100 (-1)
            - stock firmware re-uses phy0 MAC for ethernet
            - stock firmware uses romfile 0xf100 (1) for WWAN;
              not used since QMI interface is raw IP
    
    Signed-off-by: Lea Teuberth <lea.teuberth@outlook.com>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -606,6 +606,21 @@ define Device/tplink_archer-c50-v6
 endef
 TARGET_DEVICES += tplink_archer-c50-v6
 
+define Device/tplink_archer-mr200-v5
+  $(Device/tplink-v2)
+  IMAGE_SIZE := 7872k
+  DEVICE_MODEL := Archer MR200
+  DEVICE_VARIANT := v5
+  TPLINK_FLASHLAYOUT := 8MLmtk
+  TPLINK_HWID := 0x20000005
+  TPLINK_HWREV := 0x5
+  TPLINK_HWREVADD := 0x5
+  DEVICE_PACKAGES := kmod-mt76x0e uqmi kmod-usb2 kmod-usb-serial-option
+  IMAGES := sysupgrade.bin tftp-recovery.bin
+  IMAGE/tftp-recovery.bin := pad-extra 128k | $$(IMAGE/factory.bin)
+endef
+TARGET_DEVICES += tplink_archer-mr200-v5
+
 define Device/tplink_re200-v2
   $(Device/tplink-safeloader)
   IMAGE_SIZE := 7808k
