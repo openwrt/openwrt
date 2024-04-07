dac45178812ece0ecc008c50933b6604c40dd329
Han Pengfei <pengphei@foxmail.com>
2024-04-07 17:08:34 +0800

    rockchip: fix cortexa7 image generation
    Signed-off-by: Han Pengfei <pengphei@foxmail.com>

--- target/linux/rockchip/image/cortexa7.mk
+++ target/linux/rockchip/image/cortexa7.mk
@@ -3,39 +3,56 @@
 # Copyright (C) 2020 Tobias Maedel
 
 define Device/Default-emmc
-  $(Device/Default)
+  $(Device/Default-arm32)
   FILESYSTEMS += squashfs ext4
-  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
-  IMAGE/boot.img := resource-img $(1)
+  IMAGE/boot.img := resource-img | boot-arm-bin
+endef
+
+define Device/Default-sdcard
+  $(Device/Default-arm32)
+  FILESYSTEMS += squashfs ext4
+  IMAGES := boot.img rootfs.img
+  IMAGE/rootfs.img := append-rootfs | pad-extra 128k
+  IMAGE/boot.img := resource-img | boot-arm-bin
 endef
 
 define Device/Default-spiflash
-  $(Device/Default)
+  $(Device/Default-arm32)
   FILESYSTEMS += squashfs jffs2
-  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
-  IMAGE/boot.img := resource-img $(1)
+  IMAGE/boot.img := resource-img | boot-arm-bin
 endef
 
 define Device/Default-nandflash
-  $(Device/Default)
+  $(Device/Default-arm32)
   FILESYSTEMS += squashfs ubifs
-  DEVICE_DTS := $(1)
   IMAGES := boot.img rootfs.img
   IMAGE/rootfs.img := append-rootfs | pad-extra 128k
-  IMAGE/boot.img := resource-img $(1)
+  IMAGE/boot.img := resource-img | boot-arm-bin
 endef
-
 
 define Device/luckfox_pico-max
   $(Device/Default-emmc)
   DEVICE_TITLE := Luckfox Pico Max
   SUPPORTED_DEVICES := luckfox,pico-max
-  SOC := rv1106g
+  SOC := rv1106
   DEVICE_DTS := rv1106g-luckfox-pico-pro-max
-  UBOOT_DEVICE_NAME := luckfox-pico-max-rv1106
+  UBOOT_DEVICE_NAME := rv1106-sfc
 endef
 TARGET_DEVICES += luckfox_pico-max
+
+define Device/luckfox_pico
+  $(Device/Default-emmc)
+  DEVICE_TITLE := Luckfox Pico
+  SUPPORTED_DEVICES := luckfox,pico
+  SOC := rv1103
+  DEVICE_DTS := rv1103g-luckfox-pico
+  UBOOT_DEVICE_NAME := rv1106-emmc
+  DEFAULT_PACKAGES += kmod-rknpu-rockchip
+  IMAGES += sysupgrade.img.gz
+  IMAGE/sysupgrade.img.gz := env-sd-img | rockchip32-legacy-bin | append-rootfs | pad-extra 128k | gzip | append-metadata
+endef
+TARGET_DEVICES += luckfox_pico
