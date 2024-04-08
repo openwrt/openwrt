fa1292cdf52a9ebff15b5829b4faf7559bc390c8
Paul Spooren <mail@aparcar.org>
2024-04-08 08:05:29 +0200

    treewide: remove implicit SUBTARGET
    Historically it's possible to leave the `SUBTARGETS` undefined and
    automatically fallback to a "generic" subtarget. This however breaks
    various downstream scripts which may have expectations around filenames:
    
    While some targets with an explicit generic subtarget contain `generic`
    in the filenames of artifacts, implicit "subtargets" don't.
    
    Right now this breaks the CI[1], possibly also scripts using the ImageBuilders.
    
    This commit removes all code that support implicit handling of
    subtargets and instead requires every target to define "SUBTARGETS".
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/kernel.mk
+++ include/kernel.mk
@@ -43,7 +43,7 @@ else
     PATCH_DIR ?= $(CURDIR)/patches$(if $(wildcard ./patches-$(KERNEL_PATCHVER)),-$(KERNEL_PATCHVER))
     FILES_DIR ?= $(foreach dir,$(wildcard $(CURDIR)/files $(CURDIR)/files-$(KERNEL_PATCHVER)),"$(dir)")
   endif
-  KERNEL_BUILD_DIR ?= $(BUILD_DIR)/linux-$(BOARD)$(if $(SUBTARGET),_$(SUBTARGET))
+  KERNEL_BUILD_DIR ?= $(BUILD_DIR)/linux-$(BOARD)_$(SUBTARGET)
   LINUX_DIR ?= $(KERNEL_BUILD_DIR)/linux-$(LINUX_VERSION)
   LINUX_UAPI_DIR=uapi/
   LINUX_VERMAGIC:=$(strip $(shell cat $(LINUX_DIR)/.vermagic 2>/dev/null))
@@ -204,7 +204,7 @@ define KernelPackage
   $(eval $(call KernelPackage/Defaults))
   $(eval $(call KernelPackage/$(1)))
   $(eval $(call KernelPackage/$(1)/$(BOARD)))
-  $(eval $(call KernelPackage/$(1)/$(BOARD)/$(if $(SUBTARGET),$(SUBTARGET),generic)))
+  $(eval $(call KernelPackage/$(1)/$(BOARD)/$(SUBTARGET)))
 
   define Package/kmod-$(1)
     TITLE:=$(TITLE)
@@ -216,7 +216,7 @@ define KernelPackage
     PKGFLAGS:=$(PKGFLAGS)
     $(call KernelPackage/$(1))
     $(call KernelPackage/$(1)/$(BOARD))
-    $(call KernelPackage/$(1)/$(BOARD)/$(if $(SUBTARGET),$(SUBTARGET),generic))
+    $(call KernelPackage/$(1)/$(BOARD)/$(SUBTARGET))
   endef
 
   ifdef KernelPackage/$(1)/conffiles
