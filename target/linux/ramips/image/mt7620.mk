c08f8a4a4913d80ed3fb261dadc711e94b8428dd
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2024-04-12 16:44:23 +0200

    ramips: add support for TP-Link EC220-G5 v2
    TP-Link EC220-G5 v2 is a dual band router with 4 GbE ports
    Advertised as AC1200 for its 867Mbps (2x2) 5GHz band
    and 300 Mbps (2x2) 2.4GHz band.
    
    Specs:
    - SoC: MediaTek MT7620A
    - Ethernet: 4x GbE ports (Realtek RTL8367S)
    - Wireless 2.4GHz: MediaTek MT7620A
    - Wireless 5GHz: MediaTek MT7612E
    - RAM: 64MiB
    - ROM: 8MiB (W25Q64BV)
    - 2 Buttons (WPS and reset)
    - 7 LEDs
    
    Flash instructions via serial console:
    1. Rename the factory.bin to to test.bin
    2. start a TFTP server from IP address 192.168.0.225 and serve the image named test.bin
    3. connect your device to the LAN port
    4. power up the router and press 4 on the console to stop the boot process.
    5. enter the following commands on the router console
    	tftp 0x80060000 test.bin
    	erase tplink 0x20000 0x7a0000
    	cp.b 0x80060000 0x20000 0x7a0000
    	reset
    
    Flash instructions via TFTP:
    1. Update orginal firmware of the router to the latest one.
    2. Rename openwrt-ramips-mt7620-tplink_ec220-g5-v2-squashfs-tftp-recovery.bin to tp_recovery.bin
    3. Change computer IP to 192.168.0.66
    4. Run TFTP serwer
    5. Start the router with the reset button pressed, the file will be automatically downloaded and after a while the router will restart.
    6. After updating, set your computer's IP to DHCP
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -1273,6 +1273,22 @@ define Device/tplink_archer-mr200
 endef
 TARGET_DEVICES += tplink_archer-mr200
 
+define Device/tplink_ec220-g5-v2
+  $(Device/tplink-v2)
+  SOC := mt7620a
+  IMAGE_SIZE := 7808k
+  TPLINK_FLASHLAYOUT := 8Mmtk
+  TPLINK_HWID := 0x02015a15
+  TPLINK_HWREV := 0x55000600
+  TPLINK_HWREVADD := 0x03000000
+  IMAGES += tftp-recovery.bin
+  IMAGE/tftp-recovery.bin := pad-extra 128k | $$(IMAGE/factory.bin)
+  DEVICE_MODEL := EC220-G5
+  DEVICE_VARIANT := v2
+  DEVICE_PACKAGES := kmod-mt76x2 kmod-switch-rtl8367b
+endef
+TARGET_DEVICES += tplink_ec220-g5-v2
+
 define Device/tplink_re200-v1
   $(Device/tplink-v1)
   SOC := mt7620a
