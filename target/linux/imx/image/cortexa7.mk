368cd88c44dbec3783099550cade33d5d36974f1
Tim Harvey <tharvey@gateworks.com>
2024-05-20 12:28:52 -0700

    imx: fix cortexa9/cortexa7 image builds due to dts move
    The IMX device-tree's for arm moved from arch/arm/boot/dts to
    arch/arm/boot/dts/nxp/imx. Use that if using the 6.6 kernel.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- target/linux/imx/image/cortexa7.mk
+++ target/linux/imx/image/cortexa7.mk
@@ -10,6 +10,9 @@ define Device/Default
   KERNEL_NAME := zImage
   KERNEL := kernel-bin | uImage none
   KERNEL_LOADADDR := 0x80008000
+ifdef CONFIG_LINUX_6_6
+  DTS_DIR := $(DTS_DIR)/nxp/imx
+endif
   IMAGES :=
 endef
 
