eebe2d1a832ddec67727acc2fd16b9cf5af7b0ba
Tim Harvey <tharvey@gateworks.com>
2024-05-20 15:19:37 -0700

    kernel: modules: video: fix imx-ldb
    Commit ae8bf1a26e15 ("imx: add imx8m support") configured the
    drm-imx-ldb kmod for imx_cortexa9 and imx_cortexa7 however it is only
    applicable to imx_cortexa9 (imx6).
    
    Fix this so that we can avoid a missing module config for cortexa7 when
    moving to the 6.6 kernel.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -460,7 +460,7 @@ $(eval $(call KernelPackage,drm-imx-hdmi))
 define KernelPackage/drm-imx-ldb
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Freescale i.MX LVDS DRM support
-  DEPENDS:=@(TARGET_imx&&!TARGET_imx_cortexa53) +kmod-backlight kmod-drm-imx
+  DEPENDS:=@(TARGET_imx&&TARGET_imx_cortexa9) +kmod-backlight kmod-drm-imx
   KCONFIG:=CONFIG_DRM_IMX_LDB \
 	CONFIG_DRM_PANEL_SIMPLE \
 	CONFIG_DRM_PANEL=y \
