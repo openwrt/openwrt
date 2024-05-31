7d768a9ba671e8fb0be6f3b226dc286133009d1f
David Bauer <mail@david-bauer.net>
2024-05-31 18:47:25 +0200

    mpc85xx: add compressed zImage for mpc85xx
    Add a universal zImage which can be loaded by mpc85xx boards at
    load address 0x3000000. This allows boards to boot kernels larger than
    16MB even if the image is loaded temporarily from NAND at offset
    0x1000000 which some bootloaders do by default.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/mpc85xx/p1020/target.mk
+++ target/linux/mpc85xx/p1020/target.mk
@@ -1,5 +1,5 @@
 BOARDNAME:=P1020
-KERNEL_IMAGES:=simpleImage.ws-ap3710i simpleImage.ws-ap3825i simpleImage.hiveap-330
+KERNEL_IMAGES:=simpleImage.ws-ap3710i simpleImage.ws-ap3825i simpleImage.hiveap-330 zImage.la3000000
 
 define Target/Description
 	Build firmware images for Freescale P1020 based boards.
