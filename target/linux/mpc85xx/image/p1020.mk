3001d3c5d446bf53fc222640d49a4cc79e04a2f3
David Bauer <mail@david-bauer.net>
2024-06-09 19:34:46 +0200

    mpc85xx: fix WS-AP3710i boot
    On master, the bootwrapper link-address for all simpleImage targets was
    relocated to 0x15000000 due to growing kernel size.
    
    This was not done on OpenWrt 23.05, as the decompressed kernel still
    fits. However, with the wrapper for the WS-AP3710i, the bootloader
    attempts execute in-place with the uImage load-address of 0x1000000. As
    the image is compiled without the uImage header in mind, this naturally
    fails.
    
    In order to fix this, link the WS-AP3715i simpleImage at 0x15000000 as
    done in master. This will force the bootloader to relocate the code to
    the proper address and skip XIP.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/mpc85xx/image/p1020.mk
+++ target/linux/mpc85xx/image/p1020.mk
@@ -63,8 +63,8 @@ define Device/enterasys_ws-ap3710i
   DEVICE_MODEL := WS-AP3710i
   BLOCKSIZE := 128k
   KERNEL_NAME := simpleImage.ws-ap3710i
-  KERNEL_ENTRY := 0x1000000
-  KERNEL_LOADADDR := 0x1000000
+  KERNEL_ENTRY := 0x1500000
+  KERNEL_LOADADDR := 0x1500000
   KERNEL = kernel-bin | uImage none
   KERNEL_INITRAMFS := kernel-bin | uImage none
   IMAGES := sysupgrade.bin
