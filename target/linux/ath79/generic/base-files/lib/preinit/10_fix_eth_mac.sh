a15e5186b4a9944fce99e10b32f774d0436fd090
Rosen Penev <rosenp@gmail.com>
2024-06-14 11:30:31 -0700

    ath79: engenius,eXXX: use nvmem
    Userspace handling is deprecated.
    
    Move calibration for pcie node out of dtsi. calibration size for ath10k
    and ath9k differs.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
+++ target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -12,12 +12,6 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(mtd_get_mac_ascii bdcfg "lanmac")
 		ip link set dev eth1 address $(mtd_get_mac_ascii bdcfg "wanmac")
 		;;
-	engenius,epg5000|\
-	engenius,esr1200|\
-	engenius,esr1750|\
-	engenius,esr900)
-		ip link set dev eth0 address $(mtd_get_mac_ascii u-boot-env ethaddr)
-		;;
 	siemens,ws-ap3610)
 		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
 		;;
