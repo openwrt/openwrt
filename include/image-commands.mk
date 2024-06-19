69eda95e879f022a1bf78226d8ed71f65bb01eab
Sebastien Delafond <sdelafond@gmail.com>
2024-06-19 07:08:42 +0200

    vmdk: revert all our ESX/flat-related changes in order
    Comment: MFW-5037
    
    - a2a7a0152d871d92545ef7c7e1831531ea204735: MFW-2264 More clarifying comment
    - 6bf8282e7e883e0c4008b165ff43d50318ef828f: MFW-2264 Add force flag for move
    - 133774c725eb1e9f6d4c080234ed5097beedda3e: MFW-2264 Add comment
    - 6466692ab456b3030dee0665732ab02bc284e708: MFW-2264 Rename image to .image extension then build .vmdk from that to properly build both .vmdk and its flat file
    - 5b110a6c08a6d7affdeffe4b82c47259c780ee3b: esxi: raise `-jN` timeout for IMAGE/*flat* to 15 minute
    - bf48789b1850373421125f08ef87aedc67a9aeda: esxi: raise `-jN` timeout for IMAGE/*flat* to 5 minute
    - 0d4b917bb21fbd894b41f2903457e57542abdfab: esxi: define IMAGE/*flat* to avoid systematically failing -j32 build
    - 0c4444751b90baa5cf32fb5ac6f703a58a0971f5: build: make esxi build config compatible with new x86 build process
    - 2c4d79feebeef784ae8df2e151be775aae971eb8: build: build proper VMDK formats for ESXI 6.

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -338,27 +338,6 @@ define Build/qemu-image
 	fi
 endef
 
-# Convert a raw image into an ESXi $1 type image.
-# NOTE: We build the image with the .vmdk extension which causes qemu-img to create a file
-# of the same name, meaning there is nothing for the resulting flat file.
-# Renaming the .vmdk file to .image before running qemu-image creates the .vmdk and its
-# companion flat file correct.
-# E.g. | qemu-image-esxi vdi
-define Build/qemu-image-esxi
-	if command -v qemu-img; then \
-		mv -f $@ $@.image; \
-		qemu-img convert -f raw -o adapter_type=lsilogic,subformat=monolithicFlat -O $1 $@.image $@; \
-	else \
-		echo "WARNING: Install qemu-img to create VDI/VMDK images" >&2; exit 1; \
-	fi
-endef
-
-# Wait for an image created by another target, to avoid failing on
-# -flat.vmdk in -j32 scenarios
-define Build/wait
-	timeout $1 bash -c "while [ ! -f $@ ] ; do sleep 1 ; done"
-endef
-
 define Build/qsdk-ipq-factory-nand
 	$(TOPDIR)/scripts/mkits-qsdk-ipq-image.sh \
 		$@.its ubi $@
