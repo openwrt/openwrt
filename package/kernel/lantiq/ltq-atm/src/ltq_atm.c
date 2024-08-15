f6f1b4ddf49db962b1d8a6bf2ad36c9729397863
Rosen Penev <rosenp@gmail.com>
2024-08-14 19:40:00 -0700

    ltq-atm: propagate EPROBE_DEFER to probe
    Instead of ignoring errors, let the linux infrastructure handle it.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/16262
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    (cherry picked from commit 42a763ef0460d634a0bd744214617a99255b5e3a)
    Signed-off-by: Tom Herbers <mail@tomherbers.de>

--- package/kernel/lantiq/ltq-atm/src/ltq_atm.c
+++ package/kernel/lantiq/ltq-atm/src/ltq_atm.c
@@ -1784,7 +1784,10 @@ static int ltq_atm_probe(struct platform_device *pdev)
 		goto INIT_PRIV_DATA_FAIL;
 	}
 
-	ops->init(pdev);
+	ret = ops->init(pdev);
+	if (ret)
+		return ret;
+
 	init_rx_tables();
 	init_tx_tables();
 
