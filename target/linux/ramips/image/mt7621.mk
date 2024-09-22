4d4d260f5b3d95c6c89f03c2a04243efd65d2eb3
Hauke Mehrtens <hauke@hauke-m.de>
2024-09-22 14:03:22 +0200

    ramips: tp-link er605-v2: Remove sysupgrade-tar image
    The sysupgrade-tar image build is not defined for this target, do not
    add a build instruction for it. The build system will use the definition
    from the dna_valokuitu-plus-ex400 board and the build will fail.
    
    This fixes the build of the ramips target.
    
    Fixes: 665c2154ef12 ("ramips: add basic support for tp-link er605-v2")
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    (cherry picked from commit 746e894877e5130a7d5227cd90f90dfe18cc274b)

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2328,7 +2328,6 @@ define Device/tplink_er605-v2
   KERNEL_IN_UBI := 1
   KERNEL_LOADADDR := 0x82000000
   KERNEL := kernel-bin | relocate-kernel 0x80001000 | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  IMAGES += sysupgrade.tar
   IMAGE_SIZE := 127744k
 endef
 TARGET_DEVICES += tplink_er605-v2
