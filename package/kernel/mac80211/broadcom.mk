aa1aef57608aef9de330685c7afb5113293009a2
Thomas Richard <thomas.richard@bootlin.com>
2024-09-24 18:06:28 +0200

    mac80211: enable CONFIG_BRCMFMAC_SDIO for stm32
    All stm32 devices use BCM43430 Wi-Fi interface in SDIO mode.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- package/kernel/mac80211/broadcom.mk
+++ package/kernel/mac80211/broadcom.mk
@@ -437,6 +437,7 @@ define KernelPackage/brcmfmac/config
 		default y if TARGET_starfive
 		default y if TARGET_rockchip
 		default y if TARGET_sunxi
+		default y if TARGET_stm32
 		default n
 		help
 		  Enable support for cards attached to an SDIO bus.
