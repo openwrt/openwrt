openwrt/openwrt

commit bdd2d685db3620298928b1b0b2a565475ce16b2e

kernel: netdevices: add dsa support
Add support for building DSA switch drivers as kernel modules.

Signed-off-by: Oskari Lemmela <oskari@lemmela.net>
Link: https://github.com/openwrt/openwrt/pull/15926
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/netdevices.mk
@@ -523,13 +523,27 @@ endef
 
 $(eval $(call KernelPackage,phy-aquantia))
 
+define KernelPackage/dsa
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Distributed Switch Architecture support
+  DEPENDS:=+kmod-mdio-devres +kmod-net-selftests +kmod-phylink
+  KCONFIG:=CONFIG_NET_DSA
+  FILES:=$(LINUX_DIR)/net/dsa/dsa_core.ko
+endef
+
+define KernelPackage/dsa/description
+  Kernel module support for Distributed Switch Architecture
+endef
+
+$(eval $(call KernelPackage,dsa))
+
 define KernelPackage/dsa-tag-dsa
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell DSA type DSA and EDSA taggers
+  DEPENDS:=+kmod-dsa
   KCONFIG:= CONFIG_NET_DSA_TAG_DSA_COMMON \
 	CONFIG_NET_DSA_TAG_DSA \
-	CONFIG_NET_DSA_TAG_EDSA \
-	CONFIG_NET_DSA=y
+	CONFIG_NET_DSA_TAG_EDSA
   FILES:=$(LINUX_DIR)/net/dsa/tag_dsa.ko
   AUTOLOAD:=$(call AutoLoad,40,tag_dsa,1)
 endef
@@ -543,10 +557,9 @@ $(eval $(call KernelPackage,dsa-tag-dsa))
 define KernelPackage/dsa-mv88e6xxx
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Marvell MV88E6XXX DSA Switch
-  DEPENDS:=+kmod-ptp +kmod-phy-marvell +kmod-dsa-tag-dsa
+  DEPENDS:=+kmod-dsa +kmod-ptp +kmod-phy-marvell +kmod-dsa-tag-dsa
   KCONFIG:=CONFIG_NET_DSA_MV88E6XXX \
-	CONFIG_NET_DSA_MV88E6XXX_PTP=y \
-	CONFIG_NET_DSA=y
+	CONFIG_NET_DSA_MV88E6XXX_PTP=y
   FILES:=$(LINUX_DIR)/drivers/net/dsa/mv88e6xxx/mv88e6xxx.ko
   AUTOLOAD:=$(call AutoLoad,41,mv88e6xxx,1)
 endef
@@ -557,6 +570,25 @@ endef
 
 $(eval $(call KernelPackage,dsa-mv88e6xxx))
 
+define KernelPackage/dsa-qca8k
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Qualcomm Atheros QCA8xxx switch family DSA support
+  DEPENDS:=+kmod-dsa +kmod-regmap-core
+  KCONFIG:= \
+	CONFIG_NET_DSA_QCA8K \
+	CONFIG_NET_DSA_QCA8K_LEDS_SUPPORT=y \
+	CONFIG_NET_DSA_TAG_QCA
+  FILES:= \
+	$(LINUX_DIR)/drivers/net/dsa/qca/qca8k.ko \
+	$(LINUX_DIR)/net/dsa/tag_qca.ko
+  AUTOLOAD:=$(call AutoLoad,42,qca8k,1)
+endef
+
+define KernelPackage/dsa-qca8k/description
+  DSA based kernel modules for the Qualcomm Atheros QCA8xxx switch family
+endef
+
+$(eval $(call KernelPackage,dsa-qca8k))
 
 define KernelPackage/swconfig
   SUBMENU:=$(NETWORK_DEVICES_MENU)

commit 1816d2a2c8710c0d742d97095069912e3030bbf3

kernel: remove unneeded dependency from kmod-dsa
It doesn't need kmod-mdio-devres.

Fixes: bdd2d68 ("kernel: netdevices: add dsa support")

Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

package/kernel/linux/modules/netdevices.mk
@@ -527,7 +527,7 @@ $(eval $(call KernelPackage,phy-aquantia))
 define KernelPackage/dsa
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Distributed Switch Architecture support
-  DEPENDS:=+kmod-mdio-devres +kmod-net-selftests +kmod-phylink
+  DEPENDS:=+kmod-net-selftests +kmod-phylink
   KCONFIG:=CONFIG_NET_DSA
   FILES:=$(LINUX_DIR)/net/dsa/dsa_core.ko
 endef