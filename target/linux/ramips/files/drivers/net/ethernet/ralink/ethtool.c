73a920c6e0522c7b38270ac07b40d31f80a828df
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2024-10-07 21:11:41 +0200

    kernel: update drivers to be compatible with kernel 6.12
    Update drivers to be compatible with kernel 6.12.
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- target/linux/ramips/files/drivers/net/ethernet/ralink/ethtool.c
+++ target/linux/ramips/files/drivers/net/ethernet/ralink/ethtool.c
@@ -13,6 +13,7 @@
  */
 
 #include "mtk_eth_soc.h"
+#include "ethtool.h"
 
 static const char fe_gdma_str[][ETH_GSTRING_LEN] = {
 #define _FE(x...)	# x,
@@ -70,9 +71,9 @@ static void fe_get_drvinfo(struct net_device *dev,
 	struct fe_priv *priv = netdev_priv(dev);
 	struct fe_soc_data *soc = priv->soc;
 
-	strlcpy(info->driver, priv->dev->driver->name, sizeof(info->driver));
-	strlcpy(info->version, MTK_FE_DRV_VERSION, sizeof(info->version));
-	strlcpy(info->bus_info, dev_name(priv->dev), sizeof(info->bus_info));
+	strscpy(info->driver, priv->dev->driver->name, sizeof(info->driver));
+	strscpy(info->version, MTK_FE_DRV_VERSION, sizeof(info->version));
+	strscpy(info->bus_info, dev_name(priv->dev), sizeof(info->bus_info));
 
 	if (soc->reg_table[FE_REG_FE_COUNTER_BASE])
 		info->n_stats = ARRAY_SIZE(fe_gdma_str);
