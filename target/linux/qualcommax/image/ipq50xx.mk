0de458c63ddd4bec1a231bfcea041193828c3b3e
George Moussalem <george.moussalem@outlook.com>
2024-10-07 16:09:40 +0400

    qualcommax: ipq50xx: add support for Linksys MX2000 and MX5500
    Add support for Linksys MX2000 (Atlas 6) and MX5500 (Atlas 6 Pro).
    These devices are completely identical except for the secondary wifi
    chip used for 5Ghz: QCN6102 is used on MX2000 while QCN9024 is used
    on MX5500
    
    Speficiations:
    * SoC: Qualcomm IPQ5018 (64-bit dual-core ARM Cortex-A53 @ 1.0Ghz)
    * Memory: Winbond W634GU6NB-11 (512 MiB DDR3-933)
    * Serial Port: 3v3 TTL 115200n8
    * Wi-Fi: IPQ5018 (2x2 2.4 Ghz 802.11b/g/n/ax)
    * Wi-Fi: MX2000: QCN6102 (2x2:2 5 Ghz 802.11an/ac/ax)
             MX5500: QCN9024 (4x4:4 5 Ghz 802.11an/ac/ax)
    * Ethernet: IPQ5018 integrated virtual switch connected to an external
                QCA8337 switch (4 Ports 10/100/1000 GBASE-T)
    * Flash: Macronix MX35UF2GE4AD (256 MiB)
    * LEDs: 1x multi-color PWM LED
    * Buttons: 1x WPS (GPIO 27 Active Low)
               1x Reset (GPIO 28 Acive Low)
    
    Flash instructions (in case of MX2000, else replace with MX5500 images):
    1. On OEM firmware, login to the device (typically at http://192.168.1.1) and click 'CA'
    in the bottom right corner -> Connectivity -> Manual Upgrade. Alternatively, browse to
    http://<router IP>/fwupdate.html.
    Upgrade firmware using openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin image.
    Optionally install on second partition, after first boot check actual partition:
    fw_printenv -n boot_part
    and install firmware on second partition using command in case of 2:
    mtd -r -e kernel -n write openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin kernel
    and in case of 1:
    mtd -r -e alt_kernel -n write openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin alt_kernel
    2. Installation using serial connection from OEM firmware (default login: root, password: admin):
    fw_printenv -n boot_part
    In case of 2:
    flash_erase /dev/mtd12 0 0
    nandwrite -p /dev/mtd12 openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin
    or in case of 1:
    flash_erase /dev/mtd14 0 0
    nandwrite -p /dev/mtd14 openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin
    After first boot install firmware on second partition:
    mtd -r -e kernel -n write openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin kernel
    or:
    mtd -r -e alt_kernel -n write openwrt-qualcommax-ipq50xx-linksys_mx2000-squashfs-factory.bin alt_kernel
    3. Back to the OEM firmware.
    Download firmware from OEM website:
    MX2000: https://support.linksys.com/kb/article/585-en/
    MX5500: https://support.linksys.com/kb/article/587-en/
    From serial or SSH:
    fw_printenv boot_part
    in case of 1:
    mtd -r -e alt_kernel -n write FW_MX2000_1.1.7.210469_prod.img alt_kernel
    else in case of 2:
    mtd -r -e kernel -n write FW_MX2000_1.1.7.210469_prod.img kernel
    
    Signed-off-by: George Moussalem <george.moussalem@outlook.com>
    Link: https://github.com/openwrt/openwrt/pull/17182
    Signed-off-by: Robert Marko <robimarko@gmail.com>

---
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -0,0 +1,31 @@
+define Device/linksys_ipq50xx_mx_base
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := Linksys
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	KERNEL_SIZE := 8192k
+	IMAGE_SIZE := 83968k
+	NAND_SIZE := 256m
+	SOC := ipq5018
+	IMAGES += factory.bin
+	IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | linksys-image type=$$$$(DEVICE_MODEL)
+endef
+
+define Device/linksys_mx2000
+	$(call Device/linksys_ipq50xx_mx_base)
+	DEVICE_MODEL := MX2000
+	DEVICE_DTS_CONFIG := config@mp03.5-c1
+	DEVICE_PACKAGES := ath11k-firmware-qcn6122 \
+		ipq-wifi-linksys_mx2000
+endef
+TARGET_DEVICES += linksys_mx2000
+
+define Device/linksys_mx5500
+	$(call Device/linksys_ipq50xx_mx_base)
+	DEVICE_MODEL := MX5500
+	DEVICE_DTS_CONFIG := config@mp03.1
+	DEVICE_PACKAGES := kmod-ath11k-pci \
+		ath11k-firmware-qcn9074 \
+		ipq-wifi-linksys_mx5500
+endef
+TARGET_DEVICES += linksys_mx5500
