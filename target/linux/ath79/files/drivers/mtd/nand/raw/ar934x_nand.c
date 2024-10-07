5feca81ccdf1b7f800162bc9c7a4f02b5380479e
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2024-10-07 21:11:41 +0200

    ramips,generic: update drivers to be compatible with kernel 6.12
    Update drivers to be compatible with kernel 6.12.
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- target/linux/ath79/files/drivers/mtd/nand/raw/ar934x_nand.c
+++ target/linux/ath79/files/drivers/mtd/nand/raw/ar934x_nand.c
@@ -1459,7 +1459,11 @@ err_free_buf:
 	return ret;
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,11,0)
 static int ar934x_nfc_remove(struct platform_device *pdev)
+#else
+static void ar934x_nfc_remove(struct platform_device *pdev)
+#endif
 {
 	struct ar934x_nfc *nfc;
 
@@ -1470,7 +1474,9 @@ static int ar934x_nfc_remove(struct platform_device *pdev)
 		ar934x_nfc_free_buf(nfc);
 	}
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,11,0)
 	return 0;
+#endif
 }
 
 static const struct of_device_id ar934x_nfc_match[] = {
