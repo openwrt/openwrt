19620e1495d6936fb3299c4778b557375e5d7320
Rhnn Hur <hurrhnn@icmp.kr>
2024-10-20 20:14:28 +0900

    mediatek: filogic: add support for ipTIME AX3000M
    ipTIME AX3000M is an 802.11ax (Wi-Fi 6) router, based on MediaTek
    MT7981B. (Filogic 820)
    
    Specifications:
    * SoC: MetiaTek MT7981B
    * RAM: 256MiB
    * Flash: ESMT SPI-NAND F50L1G41LB 128MiB
    * Wi-Fi:
      * MediaTek MT7915E: 2.4GHz and 5GHz
    * Ethernet: 5x 1GbE
      * Switch: MediaTek MT7531
    * USB: 1x 3.0
    * UART: J4 (115200 baud)
    * LED:
      * PWR: VCC
      * CPU, 2.4G, 5G: GPIO
      * LAN 1-4, WAN: Controlled by Switch
    
    MAC Addresses:
    * 2.4G, 5G: B0:XX:XX:04:2A:60 (factory 0x4)
    * WAN: B0:XX:XX:04:2A:61 (factory 0x4, +1)
    * LAN: B0:XX:XX:04:2A:63 (factory 0x4, +3)
    
    MTD Partitions:
    * 0x000000000000-0x000000100000 : "BL2"
    * 0x000000100000-0x000000180000 : "u-boot-env"
    * 0x000000180000-0x000000380000 : "Factory"
    * 0x000000380000-0x000000580000 : "FIP"
    * 0x000000580000-0x000007380000 : "ubi"
    
    UBI Partitions (Dynamic):
    * id: 0, kernel2
    * id: 1, kernel
    * id: 2, rootfs
    * id: 3, rootfs_data
    * id: 4, rootfs2
    
    Installation:
    * Upload factory image through the tftp recovery mode.
    
    Notes:
    * This device has a dual-boot partition scheme, if installing with the
    stock web interface method will flash only on the inactive ubi partitions
    which are kernel and rootfs, newly flashed kernel didn't know the proper
    rootfs partition so the booted kernel will panic.
    
    Tested-by: Rhnn Hur <hurrhnn@icmp.kr>
    Signed-off-by: Rhnn Hur <hurrhnn@icmp.kr>
    Link: https://github.com/openwrt/openwrt/pull/16643
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1273,6 +1273,26 @@ define Device/iptime_ax3000sm
 endef
 TARGET_DEVICES += iptime_ax3000sm
 
+define Device/iptime_ax3000m
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := AX3000M
+  DEVICE_DTS := mt7981b-iptime-ax3000m
+  DEVICE_DTS_DIR := ../dts
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 32768k
+  KERNEL := kernel-bin | lzma | \
+	    fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := factory.bin sysupgrade.bin
+  IMAGE/factory.bin := sysupgrade-tar | append-metadata | check-size | iptime-crc32 ax3000m
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  SUPPORTED_DEVICES += mediatek,mt7981-spim-snand-rfb
+endef
+TARGET_DEVICES += iptime_ax3000m
+
 define Device/jcg_q30-pro
   DEVICE_VENDOR := JCG
   DEVICE_MODEL := Q30 PRO
