fb4b471669d449205d9b08c53ae8df796a40b879
Kevin Zhang <kevin@kevinzhang.me>
2024-10-21 10:45:42 -0400

    rockchip: Add support for RK3566 FriendlyElec NanoPi R3S
    This commit adds support for the FriendlyElec NanoPi R3S.
    
    CPU: Rockchip RK3566, Quad-core Cortex-A55
    RAM: 2GB LPDDR4X
    Ethernet: GMAC RTL8211F GbE, PCIe R8111H GbE
    USB3.0 Host: Type-A x1
    Storage: MicroSD Slot x 1, and optional on-board 32GB eMMC
    Debug Serial Port: 3.3V TTL, 3-pin 2.54mm pitch connector, 1500000 bauds
    LED: LED x 3
    RTC: One low-power RTC, supports backup battery input
    
    Both GbE controllers are working (WAN eth0, LAN eth1).
    Appropriate LAN/WAN interface assignments and MAC address generation.
    All three LEDs are working.
    USB appears to be working and has been tested with mass storage.
    
    Installation - microSD:
    
    -Uncompress the OpenWRT sysupgrade.img.gz
    -Write image to microSD card using dd or similar tool
    
    Installation - eMMC:
    
    -Boot from microSD
    -Uncompress the OpenWRT sysupgrade.img.gz
    -Flash to eMMC : dd if=x.img of=/dev/mmcblk0
    -sync
    -Remove microSD card
    -Reboot
    
    Signed-off-by: Kevin Zhang <kevin@kevinzhang.me>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -65,6 +65,14 @@ define Device/friendlyarm_nanopi-r2s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
+define Device/friendlyarm_nanopi-r3s
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R3S
+  SOC := rk3566
+  DEVICE_PACKAGES := kmod-r8169
+endef
+TARGET_DEVICES += friendlyarm_nanopi-r3s
+
 define Device/friendlyarm_nanopi-r4s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R4S
