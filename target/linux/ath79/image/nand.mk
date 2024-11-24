b60d241f2d5e451b785d368d0dffa3f9a6fa8a36
Lech Perczak <lech.perczak@gmail.com>
2024-11-25 00:22:27 +0100

    ath79: ZTE MF286A: use specific board definition file for qca9888
    Using board definition file extracted from stock firmware yields 50%
    throughput improvement in RX direction under iperf3 test.
    Make the device use temporary files from firmware_qca-wireless.git
    temporarily, as well as select the specific variant in the device tree
    files.
    
    Signed-off-by: Lech Perczak <lech.perczak@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/17620
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -474,6 +474,7 @@ define Device/zte_mf286
   $(Device/zte_mf28x_common)
   DEVICE_MODEL := MF286
   DEVICE_PACKAGES += ath10k-firmware-qca988x-ct ath10k-firmware-qca9888-ct \
+	-ath10k-board-qca9888 ipq-wifi-zte_mf286ar \
 	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi
 endef
 TARGET_DEVICES += zte_mf286
@@ -482,6 +483,7 @@ define Device/zte_mf286a
   $(Device/zte_mf28x_common)
   DEVICE_MODEL := MF286A
   DEVICE_PACKAGES += ath10k-firmware-qca9888-ct kmod-usb-net-qmi-wwan \
+	-ath10k-board-qca9888 ipq-wifi-zte_mf286ar \
 	kmod-usb-serial-option uqmi
 endef
 TARGET_DEVICES += zte_mf286a
