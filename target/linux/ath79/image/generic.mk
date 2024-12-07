11ad73f97d35518b3b4def26a62fa78ae1b3dfe1
Lech Perczak <lech.perczak@gmail.com>
2024-12-07 04:39:13 +0100

    ath79: teltonika-rut230: include kmod-usb-serial-option in image
    Newer modems used in RUT240 (Quectel EC25 and MeiG SLM750) use the
    "option" driver instead of CDC-ACM. Include it in the image too.
    
    Signed-off-by: Lech Perczak <lech.perczak@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/17503
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -3064,7 +3064,7 @@ define Device/teltonika_rut230-v1
   DEVICE_MODEL := RUT230
   DEVICE_VARIANT := v1
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-chipidea2 kmod-usb-acm \
-	kmod-usb-net-qmi-wwan uqmi -uboot-envtools
+	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi -uboot-envtools
   IMAGE_SIZE := 15552k
   TPLINK_HWID := 0x32200002
   TPLINK_HWREV := 0x1
