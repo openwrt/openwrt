openwrt/openwrt

commit 5fd86d66c15fc4d6f5076f7d07c91e0690d91531

kernel: modules: add support for led-group-multicolor
This option enables support for monochrome LEDs that are grouped into multicolor
LEDs which is useful in the case where LEDs of different colors are physically
grouped in a single multi-color LED and driven by a controller that doesn't have
multi-color support.

Signed-off-by: Jonathan Brophy <professor_jonny@hotmail.com>

Link: https://github.com/openwrt/openwrt/pull/16397
Signed-off-by: John Crispin <john@phrozen.org>

package/kernel/linux/modules/leds.mk
@@ -22,6 +22,25 @@ endef
 
 $(eval $(call KernelPackage,leds-gpio))
 
+define KernelPackage/led-group-multicolor
+	 SUBMENU:=$(LEDS_MENU)
+	 TITLE:=LEDs group multi-color support
+	 KCONFIG:= \
+	     CONFIG_LEDS_CLASS_MULTICOLOR \
+	     CONFIG_LEDS_GROUP_MULTICOLOR
+	  FILES:=$(LINUX_DIR)/drivers/leds/rgb/leds-group-multicolor.ko
+	 AUTOLOAD:=$(call AutoProbe,led-group-multi-color)
+endef
+
+define KernelPackage/led-group-multi-color/description
+	 This option enables support for monochrome LEDs that are grouped
+	 into multicolor LEDs which is useful in the case where LEDs of
+	 different colors are physically grouped in a single multi-color LED
+	 and driven by a controller that does not have multi-color support.
+endef
+
+$(eval $(call KernelPackage,led-group-multi-color))
+
 LED_TRIGGER_DIR=$(LINUX_DIR)/drivers/leds/trigger
 
 define KernelPackage/ledtrig-activity

commit 1cea889c96a8df4e91236e269539b086d519a60a

kernel: modules: fix led-group-multi-color patch
Fixes: 5fd86d6 ("kernel: modules: add support for led-group-multicolor")
Signed-off-by: John Crispin <john@phrozen.org>

package/kernel/linux/modules/leds.mk
@@ -22,21 +22,20 @@ endef
 
 $(eval $(call KernelPackage,leds-gpio))
 
-define KernelPackage/led-group-multicolor
-	 SUBMENU:=$(LEDS_MENU)
-	 TITLE:=LEDs group multi-color support
-	 KCONFIG:= \
-	     CONFIG_LEDS_CLASS_MULTICOLOR \
-	     CONFIG_LEDS_GROUP_MULTICOLOR
-	  FILES:=$(LINUX_DIR)/drivers/leds/rgb/leds-group-multicolor.ko
-	 AUTOLOAD:=$(call AutoProbe,led-group-multi-color)
+define KernelPackage/led-group-multi-color
+  SUBMENU:=$(LEDS_MENU)
+  TITLE:=LEDs group multi-color support
+  KCONFIG:=CONFIG_LEDS_CLASS_MULTICOLOR \
+     CONFIG_LEDS_GROUP_MULTICOLOR
+  FILES:=$(LINUX_DIR)/drivers/leds/rgb/leds-group-multicolor.ko
+  AUTOLOAD:=$(call AutoProbe,led-group-multi-color)
 endef
 
 define KernelPackage/led-group-multi-color/description
-	 This option enables support for monochrome LEDs that are grouped
-	 into multicolor LEDs which is useful in the case where LEDs of
-	 different colors are physically grouped in a single multi-color LED
-	 and driven by a controller that does not have multi-color support.
+ This option enables support for monochrome LEDs that are grouped
+ into multicolor LEDs which is useful in the case where LEDs of
+ different colors are physically grouped in a single multi-color LED
+ and driven by a controller that does not have multi-color support.
 endef
 
 $(eval $(call KernelPackage,led-group-multi-color))