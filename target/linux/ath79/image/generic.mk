ba87c0560563fb912ae711b7833eb4bb8ac21056
Edward Chow <equu@openmail.cc>
2024-12-13 13:38:57 +0800

    ath79: convert buffalo WZR-450HP2 to DSA
    Reimplement switch handling of WZR-450HP2 with DSA.
    
    Signed-off-by: Edward Chow <equu@openmail.cc>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -763,9 +763,13 @@ define Device/buffalo_wzr-450hp2
   $(Device/buffalo_common)
   SOC := qca9558
   DEVICE_MODEL := WZR-450HP2/WZR-450HP2D
+  DEVICE_PACKAGES := kmod-ag71xx kmod-dsa-qca8k kmod-phy-qca83xx \
+	-kmod-ag71xx-legacy -kmod-switch-ip17xx -kmod-switch-ar8xxx -kmod-swconfig -swconfig
   BUFFALO_PRODUCT := WZR-450HP2
   IMAGE_SIZE := 15936k
   SUPPORTED_DEVICES += wzr-450hp2
+  DEVICE_COMPAT_VERSION := 1.1
+  DEVICE_COMPAT_MESSAGE := Config cannot be migrated from swconfig to DSA
 endef
 TARGET_DEVICES += buffalo_wzr-450hp2
 
