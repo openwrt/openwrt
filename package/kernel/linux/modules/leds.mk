58795c9096e7d1a09007bdcce863c37f1b021871
jonathan brophy <professor_jonny@hotmail.com>
2024-12-15 13:36:29 +1300

    kernel: modules: reenable led-group-multi-color patch
    This reverts commit 1375a7bae0d5e8dddb6b6cfe0584a399cd6c20b4.
    
    This version does not activate the CONFIG_LEDS_CLASS_MULTICOLOR option,
    this option is controlled by the generic target configuration.
    
    Signed-off-by: jonathan brophy <professor_jonny@hotmail.com>
    Link: https://github.com/openwrt/openwrt/pull/17277
    [Reword commit message]
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/leds.mk
+++ package/kernel/linux/modules/leds.mk
@@ -24,6 +24,23 @@ $(eval $(call KernelPackage,leds-gpio))
 
 LED_TRIGGER_DIR=$(LINUX_DIR)/drivers/leds/trigger
 
+define KernelPackage/led-group-multi-color
+  SUBMENU:=$(LEDS_MENU)
+  TITLE:=LEDs group multi-color support
+  KCONFIG:=CONFIG_LEDS_GROUP_MULTICOLOR
+  FILES:=$(LINUX_DIR)/drivers/leds/rgb/leds-group-multicolor.ko
+  AUTOLOAD:=$(call AutoProbe,led-group-multi-color)
+endef
+
+define KernelPackage/led-group-multi-color/description
+ This option enables support for monochrome LEDs that are grouped
+ into multicolor LEDs which is useful in the case where LEDs of
+ different colors are physically grouped in a single multi-color LED
+ and driven by a controller that does not have multi-color support.
+endef
+
+$(eval $(call KernelPackage,led-group-multi-color))
+
 define KernelPackage/ledtrig-activity
   SUBMENU:=$(LEDS_MENU)
   TITLE:=LED Activity Trigger
