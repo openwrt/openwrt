ae406cab9bc1af4e7bfa41426a9ce3fb65f285a2
Jin Yijie <14980985+grepmorecat@users.noreply.github.com>
2024-12-28 15:00:28 +0800

    ramips: mt7621: add support for JDCloud RE-SP-01B
    JDCloud RE-SP-01B is a dual-band WiFi 5 router based on the MT7621AT.
    
    Specifications:
    - SoC: MediaTek MT7621AT
    - RAM: 512MB DDR3
    - Flash: 32MB SPI NOR
    - WiFi: MediaTek MT7603EN (2.4GHz), MediaTek MT7615N (5GHz)
    - Ethernet: 1x WAN, 2x LAN (Gigabit Ethernet)
    - LEDs: red, blue, green (GPIO controlled)
    - Button: Reset (GPIO controlled)
    - eMMC: Single onboard (32GB/64GB/128GB)
    - USB: 1x USB 2.0 port
    
    MAC Address Structure:
    The MAC address shares the structure DC:D8:7C:XX:XX:XX, where:
    - WAN, LAN, and 2.4GHz WiFi interfaces use the same MAC as the label.
    - 5GHz WiFi uses an offset of the label MAC + 0x800000.
    
    (The label MAC address is located in the config partition at offset
    0x4429 and it is hexadecimal encoded. Thanks to commit 7db87d7, no
    additional patch work is needed to support this format)
    
    Flash Instruction:
    A third-party bootloader is required to boot the image.
    (I use a "Clip" tool to flash the image through SPI for testing.)
    
    note:
    - The official bootloader does provide a web interface for
    recovering an official image though. To access it:
      1. Hold the reset button and power on the device.
      2. Set a manual IP address 192.168.68.2 on your computer.
      3. Visit http://192.168.68.1 in a web browser.
    
    Signed-off-by: Jin Yijie <14980985+grepmorecat@users.noreply.github.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1787,6 +1787,16 @@ define Device/jdcloud_re-cp-02
 endef
 TARGET_DEVICES += jdcloud_re-cp-02
 
+define Device/jdcloud_re-sp-01b
+  $(Device/dsa-migration)
+  IMAGE_SIZE := 27328k
+  DEVICE_VENDOR := JDCloud
+  DEVICE_MODEL := RE-SP-01B
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615e \
+	kmod-mt7615-firmware kmod-mmc-mtk kmod-usb3
+endef
+TARGET_DEVICES += jdcloud_re-sp-01b
+
 define Device/keenetic_kn-3010
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
