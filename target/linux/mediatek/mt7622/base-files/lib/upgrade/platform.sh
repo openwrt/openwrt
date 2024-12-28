7657a1c5317a9d41189912da60c0359e762d5d96
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-12-28 17:48:02 +0900

    mediatek: mt7622: sync buffalo.sh with ramips/mt7621
    Sync buffalo.sh with the one in ramips/mt7621 to improve handling of the
    TRX magic numbers of Buffalo WSR devices and switching sysupgrade method
    between buffalo_upgrade_ubinized() and nand_do_upgrade().
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>

--- target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
@@ -16,16 +16,7 @@ platform_do_upgrade() {
 		;;
 	buffalo,wsr-2533dhp2|\
 	buffalo,wsr-3200ax4s)
-		local magic="$(get_magic_long "$1")"
-
-		# use "mtd write" if the magic is "DHP2 (0x44485032)"
-		# or "DHP3 (0x44485033)"
-		if [ "$magic" = "44485032" -o "$magic" = "44485033" ]; then
-			buffalo_upgrade_ubinized "$1"
-		else
-			CI_KERNPART="firmware"
-			nand_do_upgrade "$1"
-		fi
+		buffalo_do_upgrade "$1"
 		;;
 	dlink,eagle-pro-ai-m32-a1|\
 	dlink,eagle-pro-ai-r32-a1|\
