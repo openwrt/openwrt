31ee49b8db8c46f09dcffcc8162675ae4843bfd5
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2025-01-14 16:38:36 -0300

    ath79: convert swconfig drivers into modules
    In a first step to gradually adopt DSA switch, all swconfig modules must
    be optional, loaded as modules when needed.
    
    AT803X_PHY was also converted to drop it from swconfig devices as
    swconfig uses AR8216_PHY/ar8xxx. Once we convert a device to DSA, we
    can do the opposite and drop ar8xxx and include at803x.
    
    AG71XX (ethernet driver) was also converted into a module to postpone
    mdio bus initialization. If not, mdio.0 would be registered with the
    first compatible driver, normally "Generic PHY".
    
    All modules are still installed by default maintainig their previous
    availability. However, they should be removed from default selections
    and included only on the group of device they are needed.
    
    There are still other PHY drivers builtin that could follow the same
    path as AT803X_PHY and AR8216_PHY. The /sys/bus/mdio_bus/drivers/
    directory might give a hint of what to convert into modules (and
    selectively require on each needed devices).
    
    Tested on TP-Link TL-WDR3600v1, TL-WD4300v1 and Archer C7v5, both in
    normal and failsafe mode.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/ath79/generic/target.mk
+++ target/linux/ath79/generic/target.mk
@@ -1,6 +1,6 @@
 BOARDNAME:=Generic
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls
+DEFAULT_PACKAGES += wpad-basic-mbedtls kmod-switch-ip17xx
 
 define Target/Description
 	Build firmware images for generic Atheros AR71xx/AR913x/AR934x based boards.
