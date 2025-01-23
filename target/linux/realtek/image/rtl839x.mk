8f3b24643b40e14be1c9c092c74030c29fa9d8c2
Evan Jobling <evan@jobling.au>
2025-01-23 00:13:35 +0000

    realtek: HPE 1920-48G-PoE: allow fan speed control
    The JG928A has an RTL8231 on the aux mdio bus. Add it to dts to expose
    the GPIO pins used to control and monitor the fan speed. To enable speed
    control, add the appropriate kernel driver module to DEVICE_PACKAGES.
    
    Of note, this does not control all fans for the unit. The power supply
    fans are not controlled.
    
    Signed-off-by: Evan Jobling <evan@jobling.au>
    Link: https://github.com/openwrt/openwrt/pull/17699
    Signed-off-by: Sander Vanheule <sander@svanheule.net>
    (cherry picked from commit cbd1acbad3448280a180640e2a1a37a7144e1fb3)

--- target/linux/realtek/image/rtl839x.mk
+++ target/linux/realtek/image/rtl839x.mk
@@ -21,7 +21,7 @@ define Device/hpe_1920-48g-poe
   $(Device/hpe_1920)
   SOC := rtl8393
   DEVICE_MODEL := 1920-48G-PoE (JG928A)
-  DEVICE_PACKAGES += realtek-poe
+  DEVICE_PACKAGES += realtek-poe kmod-hwmon-gpiofan
   H3C_DEVICE_ID := 0x0001002b
 endef
 TARGET_DEVICES += hpe_1920-48g-poe
