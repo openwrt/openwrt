ca5875851c086ed43c489bdda5b1d81dcf05d6a9
Tianling Shen <cnsztl@immortalwrt.org>
2025-01-24 16:15:22 +0800

    mediatek: add ubootmod layout for cudy tr3000 v1
    This allows us to use the full size of nand, which increases ubi size
    from 64M to 122.25M.
    
    If you are at factory firmware, please refer commit 63b8d98dd0d2 ("mediatek: add support for Cudy TR3000 v1")
    to boot into OpenWrt initramfs (stock layout).
    
    Flash instructions:
    1. Login into the device and backup everything, especially 'Factory' part.
    1. Unlock mtd partitions:
       apk update && apk add kmod-mtd-rw
       insmod mtd-rw i_want_a_brick=1
    3. Write new BL2 and FIP
       mtd write openwrt-mediatek-filogic-cudy_tr3000-v1-ubootmod-preloader.bin BL2
       mtd write openwrt-mediatek-filogic-cudy_tr3000-v1-ubootmod-bl31-uboot.fip FIP
    4. Set static IP on your PC:
       IP 192.168.1.254/24, GW 192.168.1.1
    5. Serve OpenWrt initramfs image using TFTP server.
    6. Cut off the power and re-engage, wait for TFTP recovery to complete.
    7. After OpenWrt has booted, perform sysupgrade.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -74,6 +74,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r4-poe|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
+	cudy,tr3000-v1-ubootmod|\
 	gatonetworks,gdsp|\
 	h3c,magic-nx30-pro|\
 	jcg,q30-pro|\
@@ -204,6 +205,7 @@ platform_check_image() {
 	bananapi,bpi-r4-poe|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
+	cudy,tr3000-v1-ubootmod|\
 	gatonetworks,gdsp|\
 	h3c,magic-nx30-pro|\
 	jcg,q30-pro|\
