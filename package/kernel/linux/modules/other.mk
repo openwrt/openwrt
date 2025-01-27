c40840da3a03d9abdf849f9be653e137ced81b2f
Tony Ambardar <itugrok@yahoo.com>
2025-01-27 06:20:36 -0800

    package/zram: fix up Kconfig behaviour for kernel 6.12
    The initial 6.12 merge leaves package/zram menuconfig behaviour confusing.
    Selecting "kmod-zram" opens a submenu of the available algorithms to build
    and the actual algo to use. However, when "lzo-rle" is shown as the default
    used, it still remains missing from the available list. Only enabling a
    _different_ compression algo to build will also reveal "lzo-rle" as
    available.
    
    Update the Kconfig to show "lzo-rle" as available if used/referenced.
    
    Fixes: 4708057e27 ("package/zram: update for kernel 6.12")
    Signed-off-by: Tony Ambardar <itugrok@yahoo.com>
    Link: https://github.com/openwrt/openwrt/pull/18712
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -714,10 +714,7 @@ define KernelPackage/zram/config
   if PACKAGE_kmod-zram
     if !LINUX_6_6
         config KERNEL_ZRAM_BACKEND_LZO
-                bool "lzo and lzo-rle compression support" if KERNEL_ZRAM_BACKEND_LZ4 || \
-                    KERNEL_ZRAM_BACKEND_LZ4HC || KERNEL_ZRAM_BACKEND_ZSTD
-                default !KERNEL_ZRAM_BACKEND_LZ4 && \
-                    !KERNEL_ZRAM_BACKEND_LZ4HC && !KERNEL_ZRAM_BACKEND_ZSTD
+                bool "lzo and lzo-rle compression support"
 
         config KERNEL_ZRAM_BACKEND_LZ4
                 bool "lz4 compression support"
@@ -727,6 +724,12 @@ define KernelPackage/zram/config
 
         config KERNEL_ZRAM_BACKEND_ZSTD
                 bool "zstd compression support"
+
+        config KERNEL_ZRAM_BACKEND_FORCE_LZO
+                def_bool !KERNEL_ZRAM_BACKEND_LZ4 && \
+                         !KERNEL_ZRAM_BACKEND_LZ4HC && \
+                         !KERNEL_ZRAM_BACKEND_ZSTD
+                select KERNEL_ZRAM_BACKEND_LZO
 
     endif
     choice
