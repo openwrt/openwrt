47ae0960ae523f73c76b6a99b67606b5fdae605f
Andrew LaMarche <andrewjlamarche@gmail.com>
2025-01-28 02:52:16 +0000

    airoha: add support for Gemtek W1700K
    This commit adds support for Gemtek (Centurylink/Lumen/Quantum Fiber)
    W1700K.
    
    Device specification
    --------------------
    SoC Type:	Airoha AN7581
    RAM:		ESMT M16U8G16512A (2GB)
    Flash:		Winbond 25N04KVZEIR (512MB)
    Ethernet:	2x gigabit via AN7581, 2x 10g via RTL8261N
    Wi-Fi:		MT7996 - BE19000
    LEDs:		1 LED, power/status
    Button:		Reset
    USB ports:	None
    Bootloader:	U-Boot 2014.04-rc1 (Jun 12 2024 - 08:14:34) AXON 2.0
    Fan:		1x controlled by Nuvoton NCT7511Y
    
    This device is pretty useless with the stock firmware and wastes a lot
    of space on the flash. Therefore, the partitions have been restructured
    to make better use of the space and also utilizes UBI for the rootfs and
    overlay. As a result, reverting to the stock firmware is not documented
    here, though it is possible by writing back the vendor firmware to
    0x600000 and restoring the default bootcmd to `flash imgread 2048`.
    
    The following steps can be used to install OpenWrt.
    
    Connect to serial console. There is a Torx T10 screw underneath the QR
    code printed onto the label. Then, pry between the gray and white
    plastic, starting by the ports on the back. There are clips arount the
    entire device. Starting closest to the screw next to the UART header,
    TX - GND - VCC - N/A - RX. The bootloader can be interrupted by
    pressing any key.
    
    If you do not wish to make a backup, skip to Configuring Bootloader for
    Openwrt.
    
    Rooting Stock FW (for making backups, recommended):
    - Boot the router and watch serial console until presented with failsafe
      mode. Enter it (f + enter).
    - mount_root
    - Change the root password (passwd).
    - Open /etc/config/axon_platform_manager and set sshServerEnable,
      localAccessEnable and remoteAccessEnable to 1.
    - Search for "SSH". You'll find a long string with 3 matches such as
      Enabled%25252c1%25252cSSH%Drop. Change any instances of "Disabled"
      preceeding SSH to "Enabled" and any instances of "Drop" to "Accept"
      that follow SSH. Same for "Local SSH" and "Remote SSH".
    - Set /etc/config/dropbear to:
    
    config dropbear
    	option PasswordAuth 'on'
    	option RootPasswordAuth 'on'
    	option Port '22'
    
    - Reboot.
    - Connect 10g WAN port to existing network and SSH in with the password
      you set.
    - SSH into rooted stock fw.
    - dd off each /dev/mtdX partition and save to your computer
    
    Configuring Bootloader for OpenWrt:
    The bootloader's default bootcmd will only run a signed image. However,
    we can still bootm our own image from flash.
    - setenv bootflag 0
    - setenv one flash read 0x600000 0x1000000 \$loadaddr
    - setenv two "; bootm"
    - setenv bootcmd "$one$two"
    - setenv one
    - setenv two
    - saveenv
    
    Booting OpenWrt initramfs and installing:
    - Set your computer's IP address to 192.168.0.205.
    - Run a TFTP server providing the initramfs image.
    - Power on the device and enter U-Boot menu.
    - tftpboot 0x89000000 openwrt-airoha-an7581-airoha_an7581-gemtek-w1700k-initramfs-uImage.itb
    - bootm 0x89000000
    * Note: The ethernet driver is flaky and may drop packets before loading
      the initramfs. A small initramfs is recommended, and it may take
      several tries to successfully load the image via TFTP.
    - The sysupgrade image can be flashed now, via LuCI or the cli.
    
    Signed-off-by: Andrew LaMarche <andrewjlamarche@gmail.com>

--- target/linux/airoha/an7581/target.mk
+++ target/linux/airoha/an7581/target.mk
@@ -2,6 +2,7 @@ ARCH:=aarch64
 SUBTARGET:=an7581
 BOARDNAME:=AN7581 / AN7566 / AN7551
 CPU_TYPE:=cortex-a53
+DEFAULT_PACKAGES += kmod-gpio-button-hotplug wpad-basic-mbedtls uboot-envtools
 KERNELNAME:=Image dtbs
 FEATURES+=pwm source-only
 
