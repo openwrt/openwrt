0254d6579af06af9f82ae99fac66c45a08b3a84d
FUKAUMI Naoki <naoki@radxa.com>
2025-01-29 07:33:59 +0000

    rockchip: Add support for Radxa ROCK 4C+
    The Radxa ROCK 4C+[1] is a single board computer with dual HDMI using
    the Rockchip RK3399-T.
    
    Hardware
    --------
    
    - Dual-core Cortex-A72 and quad-core Cortex-A53 CPU
    - Mali-T860MP4 GPU
    - LPDDR4 4GB RAM
    - eMMC connector
    - microSD card slot
    - Wi-Fi 5 (not supported)
    - Gigabit Ethernet with PoE support (additional PoE HAT required)
    - USB 3.0 Type-A OTG port
    - USB 3.0 Type-A HOST port
    - 2x USB 2.0 Type-A HOST ports
    - USB Type-C power port (5V only)
    - 40 Pin GPIO header
    
    [1] https://radxa.com/products/rock4/4cp
    
    Installation
    ------------
    Uncompress the OpenWrt sysupgrade and write it to the micro SD card or
    internal eMMC using dd.
    
    Signed-off-by: FUKAUMI Naoki <naoki@radxa.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -183,6 +183,13 @@ define Device/radxa_rock-3c
 endef
 TARGET_DEVICES += radxa_rock-3c
 
+define Device/radxa_rock-4c-plus
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK 4C+
+  SOC := rk3399
+endef
+TARGET_DEVICES += radxa_rock-4c-plus
+
 define Device/radxa_rock-5a
   DEVICE_VENDOR := Radxa
   DEVICE_MODEL := ROCK 5A
