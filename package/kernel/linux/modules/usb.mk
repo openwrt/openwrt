e471e60a741b9cbd041f7fec0d7b5f7462bbcd7b
Rui Salvaterra <rsalvaterra@gmail.com>
2025-02-01 18:48:29 +0000

    kernel: usb: simplify usb-net kconfig
    Now that CONFIG_USB_NET_DRIVERS is unconditionally enabled, remove it from the
    usb-net kconfig symbol list.
    
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1178,8 +1178,7 @@ $(eval $(call KernelPackage,usb-atm-cxacru))
 define KernelPackage/usb-net
   TITLE:=Kernel modules for USB-to-Ethernet convertors
   DEPENDS:=+kmod-mii
-  KCONFIG:=CONFIG_USB_USBNET \
-	CONFIG_USB_NET_DRIVERS
+  KCONFIG:=CONFIG_USB_USBNET
   AUTOLOAD:=$(call AutoProbe,usbnet)
   FILES:=$(LINUX_DIR)/drivers/$(USBNET_DIR)/usbnet.ko
   $(call AddDepends/usb)
