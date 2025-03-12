1358c2c8dbe8118300067865fe0af43d439431b5
Roland Reinl <reinlroland+github@gmail.com>
2025-03-12 19:11:08 +0100

    ramips: Add support for Cudy M1200 v1
    The M1200 v1 is similar to the TR1200 series from Cudy. Differences:
    - Only 1 LAN port
    - No USB
    
    Specifications:
    - MT7628
    - MT7628AN (2.4G b/g/n) and MT7613BE (5G ac/n) wifi
    - 128 MB RAM
    - 16 MB flash
    
    MAC Addresses:
    - There is one on the label, e.g. xx:xx:xx:xx:xx:A4
    - LAN (bottom connector) is the same as the label, e.g. xx:xx:xx:xx:xx:A4
    - WAN (top connector) is label + 1, e.g. xx:xx:xx:xx:xx:A5
    - WLAN (2.4G) is the same as the label, e.g. xx:xx:xx:xx:xx:A4
    - WLAN (5G) is label + 2, e.g. xx:xx:xx:xx:xx:A6
    
    UART:
    - is available via the pin holes on the board
    - The pinout is printed to the board: P: VCC, G: GND, R: RX, T:TX
    - RX and TX require solder bridges to be installed
    - Do NOT connect VCC
    - Settings: 3.3V, 115200, 8N1
    
    GPIO:
    - There are two LEDs: Red (GPIO 4) and White (GPIO 0)
    - There are two buttons: Reset (GPIO 11) and WPS (GPIO 5)
    
    Migration to OpenWrt:
    - Download the migration image from the Cudy website (it should be available as soon as OpenWrt officially supports the device)
    - Connect computer to LAN (bottom connector) and flash the migration image via OEM web interface
    - OpenWrt is now accessible via 192.168.1.1
    
    Revert back to OEM firmware:
    - Set up a TFTP server on IP 192.168.1.88 and connect to the WAN port (upper port)
    - Provide the Cudy firmware as recovery.bin in the TFTP server
    - Press the reset button while powering on the device
    - Recovery process is started now
    - When recovery process is done, OEM firmware is accessible via 192.168.10.1 again
    
    General information:
    - No possibility to load a initramfs image via U-Boot because there is no option to interrupt U-Boot
    
    Signed-off-by: Roland Reinl <reinlroland+github@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/18233
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    (cherry picked from commit c13a050d5ad55ab090a592823a9f964af8614fc2)

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -167,6 +167,17 @@ define Device/comfast_cf-wr758ac-v2
 endef
 TARGET_DEVICES += comfast_cf-wr758ac-v2
 
+define Device/cudy_m1200-v1
+  IMAGE_SIZE := 15872k
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := M1200
+  DEVICE_VARIANT := v1
+  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7663-firmware-ap
+  UIMAGE_NAME := R22
+  SUPPORTED_DEVICES += R22
+endef
+TARGET_DEVICES += cudy_m1200-v1
+
 define Device/cudy_tr1200-v1
   IMAGE_SIZE := 15872k
   DEVICE_VENDOR := Cudy
