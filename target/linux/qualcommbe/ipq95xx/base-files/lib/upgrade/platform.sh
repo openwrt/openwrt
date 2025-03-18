d324d6f07e687c3d89b0737bef091f9e1d151486
Mantas Pucka <mantas@8devices.com>
2025-03-18 10:23:55 +0200

    qualcommbe: ipq95xx: add 8devices Kiwi-DVK support
    Specifications:
      SoC:     Qualcomm IPQ9570
      RAM:     1 GiB DDR4
      Flash:   4 GiB eMMC
      ETH1:    2.5G Maxlinear GPY211C (48V POE-in)
      ETH2:    10G Realtek RTL8261N
      SFP:     10G SFP+ cage
      WLAN1:   QCN9274 2.4GHz 802.11b/g/n/ax/be 2x2
      WLAN2:   QCN9274 5-6GHz 802.11a/n/ac/ax/be 2x2
      PCIE0:   Gen3x1 mPCIe or M.2(B+M)
      PCIE1:   Gen3x1 M.2(A+E) or M.2(B+M)
      PCIE2:   Gen3x2 M.2(A+E)
      USB:     1x 3.0
      Console: USB-C via FTDI
      Button:  Reset
    
    Install using sysupgrade:
    
      sysupgrade openwrt-qualcommbe-ipq95xx-8devices_kiwi-dvk-squashfs-sysupgrade.bin
    
    Install using U-Boot:
    
     * Put factory.bin at TFTP server(192.168.2.254) as fwupdate.bin
     * Run 'run do_recovery' in U-Boot shell
    
    Signed-off-by: Mantas Pucka <mantas@8devices.com>
    Link: https://github.com/openwrt/openwrt/pull/18435
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommbe/ipq95xx/base-files/lib/upgrade/platform.sh
@@ -3,10 +3,27 @@ PART_NAME=firmware
 RAMFS_COPY_BIN='fw_printenv fw_setenv head'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
+platform_check_image() {
+	return 0;
+}
+
 platform_do_upgrade() {
 	case "$(board_name)" in
+	8devices,kiwi-dvk)
+		CI_KERNPART="0:HLOS"
+		CI_ROOTPART="rootfs"
+		emmc_do_upgrade "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
+		;;
+	esac
+}
+
+platform_copy_config() {
+	case "$(board_name)" in
+	8devices,kiwi-dvk)
+		emmc_copy_config
 		;;
 	esac
 }
