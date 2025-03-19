ffd4703872704dae860b9b32e8e2bbb8715fbc53
Yonghyu Ban <yhban@silicon.moe>
2025-03-19 17:29:04 +0900

    mediatek: filogic: add support for TP-Link Festa FR365
    Hardware
    --------
    SOC     : MediaTek MT7981B 2x A53
    RAM     : NT5AD256M16E4-JR 512MiB
    Flash   : ESMT F50L1G41LB 128MB
    ETH     : 5x 1GbE (1x WAN, 4x LAN)
              1x SFP+ (2.5 GbE)
    Switch  : MediaTek MT7531AE
    WiFi    : MediaTek MT7976D
    Buttons : Reset
    Power   : DC 12V 2A
    
    Installation
    ------------
    1. Disassemble the device
    2. Create solder bridge on R85, R86, Solder 4-pin header on J4.
    3. Connect UART console.
    4. In U-Boot shell, enter the following commands to prepare flashing
    ```
    enable_phy
    extra_para set extra-para score 100
    extra_para set extra-para fwFlag good
    ```
    5. Use `mtkupgrade` command to TFTP flash openwrt-mediatek-filogic-tplink_fr365-v1-squashfs-factory.bin
    6. Reboot the device
    
    Signed-off-by: Yonghyu Ban <yhban@silicon.moe>

--- target/linux/mediatek/filogic/base-files/lib/preinit/10_fix_eth_mac.sh
+++ target/linux/mediatek/filogic/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -33,6 +33,16 @@ preinit_set_mac_address() {
 		addr=$(get_mac_binary "/tmp/tp_data/default-mac" 0)
 		ip link set dev eth1 address "$(macaddr_add $addr 1)"
 		;;
+	tplink,fr365-v1)
+		lan_mac=$(strings /dev/mtd11 | grep 'option macaddr' | awk -F"'" '{print $2}')
+		wan_mac="$(macaddr_add $lan_mac 1)"
+		ip link set dev port2 address "$wan_mac"
+		ip link set dev port1 address "$lan_mac"
+		ip link set dev port3 address "$lan_mac"
+		ip link set dev port4 address "$lan_mac"
+		ip link set dev port5 address "$lan_mac"
+		ip link set dev port6 address "$lan_mac"
+		;;
 	*)
 		;;
 	esac
