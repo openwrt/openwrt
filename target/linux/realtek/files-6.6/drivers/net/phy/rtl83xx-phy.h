1c88c6979549cf466b15ddb7fc663e836e9441ce
Antanas Bruzas <antanas.bruzas@protonmail.com>
2025-03-19 19:34:47 +0200

    realtek: add support for RTL8218E
    Signed-off-by: Antanas Bruzas <antanas.bruzas@protonmail.com>

--- target/linux/realtek/files-6.6/drivers/net/phy/rtl83xx-phy.h
+++ target/linux/realtek/files-6.6/drivers/net/phy/rtl83xx-phy.h
@@ -27,6 +27,7 @@ struct __attribute__ ((__packed__)) fw_header {
 #define PHY_ID_RTL8218B_E			0x001cc980
 #define PHY_ID_RTL8214_OR_8218			0x001cc981
 #define PHY_ID_RTL8218D				0x001cc983
+#define PHY_ID_RTL8218E				0x001cc984
 #define PHY_ID_RTL8218B_I			0x001cca40
 #define PHY_ID_RTL8221B				0x001cc849
 #define PHY_ID_RTL8226				0x001cc838
