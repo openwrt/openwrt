7e37ed564635972517be80416ac3cf849aa35973
Scott Mercer <TheRootEd24@gmail.com>
2025-03-22 12:46:18 -0400

    qualcommax: ipq50xx: add support for GL.iNET GL-B3000 Add support for GL.iNET (AX3000) B3000.
    Speficiations:
    * SoC: Qualcomm IPQ5018 (64-bit dual-core ARM Cortex-A53 @ 1.0Ghz)
    * Memory: Winbond W634GU6NQB-11 (512 MiB DDR3-933)
    * Serial Port: 3v3 TTL 115200n8
    * Wi-Fi: IPQ5018 (2x2 2.4 Ghz 802.11b/g/n/ax)
    * Wi-Fi: QCN6102 (2x2:2 5 Ghz 802.11an/ac/ax)
    * Ethernet: IPQ5018 integrated virtual switch connected to an external
                QCA8337 switch (3 Ports 10/100/1000 GBASE-T)
    * Flash: Winbond W25N01GWZEIG (128 MiB)
    * LEDs: 1x single-color blue LED (GPIO 24 Active High)
            1x single-color white LED (GPIO 23 Active High)
    * Buttons: 1x Reset (GPIO 27 Active Low)
    
    Flash Instructions:
    
    *** The .img files are now universal ! ***
    
    Openwrt		--> openwrt-qualcommax-ipq50xx-glinet_gl-b3000-squashfs-factory.img
    
    GL.iNet OEM	--> openwrt-b3000-4.5.18-0731-1722397535.img
    
    Either file can be flashed, in any of the available upgrade options, in both Firmwares.
    
    Pick a file .. pick a method .. and SEND IT !!
    
    Signed-off-by: Scott Mercer <TheRootEd24@gmail.com>

--- scripts/mkits-qsdk-ipq-image.sh
+++ scripts/mkits-qsdk-ipq-image.sh
@@ -14,13 +14,27 @@
 # to include as many sections as needed.
 #
 
+node_type="firmware"
+has_script=
+
 usage() {
+
+	echo ""; echo "** QSDK format (Default) ** "
 	echo "Usage: `basename $0` output img0_name img0_file [[img1_name img1_file] ...]"
+	echo ""; echo "** QSDK format with booscript (UBOOT_SCRIPT) **"
+	echo "Usage: `basename $0` output_name img_name img_file [ device_name ].bootscript"
 	exit 1
 }
 
 # We need at least 3 arguments
 [ "$#" -lt 3 ] && usage
+
+# if $# === 4 check for valid bootscript
+if [ "$#" -eq 4 ] && [ "${4##*.}" = "bootscript" ]; then
+	has_script=true && node_type="script"
+elif [ "$#" -eq 4 ] && [ "${4##*.}" != "bootscript" ]; then
+	usage
+fi
 
 # Target output file
 OUTPUT="$1"; shift
@@ -35,23 +49,40 @@ echo "\
 
 	images {" > ${OUTPUT}
 
-while [ -n "$1" -a -n "$2" ]; do
-	[ -f "$2" ] || usage
+while [ -n "$1" -a -n "$2" ] || [ $has_script ]; do
+	[ -f "$2" ] || [ $has_script ] || usage
 
-	name="$1"; shift
-	file="$1"; shift
+	case "$node_type" in
+		script)
+			name="$node_type"
+			file="$3"
+			desc="${3%.*} uboot ${3##*.}"
+			type="$node_type"
+		;;
+		firmware)
+			name="$1"; shift
+			file="$1"; shift
+			desc="$name"
+			type="$node_type"
+		;;
+	esac
 
 	echo \
 "		${name} {
-			description = \"${name}\";
+			description = \"${desc}\";
 			data = /incbin/(\"${file}\");
-			type = \"Firmware\";
+			type = \"${type}\";
 			arch = \"ARM\";
 			compression = \"none\";
 			hash@1 {
 				algo = \"crc32\";
 			};
 		};" >> ${OUTPUT}
+
+	if [ "$node_type" = "script" ]; then
+		node_type="firmware"
+		has_script=
+	fi
 done
 
 echo \
