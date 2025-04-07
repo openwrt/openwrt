1859f722fe1a326927d356a0be16b82e77aaae5d
tristan <lipanfeng@asterfusion.com>
2025-04-07 10:02:07 +0800

    Fixed: kernel fingerprint not matching the official OpenWRT, then cannot install any software Get official fingerprint here:     -> https://downloads.openwrt.org/releases/23.05.5/targets/mvebu/cortexa72/kmods/         -> 5.15.167-1-929d60da47a7932cac2a52ce60a4eee0             -> fingerprint is: 929d60da47a7932cac2a52ce60a4eee0

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -118,7 +118,8 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) $(if $(findstring uml,$(BOARD)),ARCH=$(ARCH)) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	#grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	echo 929d60da47a7932cac2a52ce60a4eee0 > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
