0b5650f76783b215a6bfa22bde45973474a0d04f
Christian Marangi <ansuelsmth@gmail.com>
2025-04-11 11:38:55 +0200

    generic: add missing of.h header to ar8327
    It seems new kernel linux version reorganized the header include and now
    of.h needs to be explicitly included. This should have been done from
    when the driver was introduced.
    
    Add the missing of.h header to fix compilation error in later kernel
    version.
    
    Co-authored-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>
    Link: https://github.com/openwrt/openwrt/pull/18455
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- target/linux/generic/files/drivers/net/phy/ar8327.c
+++ target/linux/generic/files/drivers/net/phy/ar8327.c
@@ -23,6 +23,7 @@
 #include <linux/lockdep.h>
 #include <linux/ar8216_platform.h>
 #include <linux/workqueue.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/leds.h>
 #include <linux/mdio.h>
