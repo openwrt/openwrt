openwrt/openwrt

commit 65de1e0f78707695238382ace6a78695acbb8ed8

kernel: add missing symbols for lxc
These symbols are needed to satisfy lxc dependencies.

Signed-off-by: John Audia <therealgraysky@proton.me>
Link: https://github.com/openwrt/openwrt/pull/17553
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/netsupport.mk
@@ -1649,3 +1649,21 @@ define KernelPackage/qrtr-mhi/description
 endef
 
 $(eval $(call KernelPackage,qrtr-mhi))
+
+define KernelPackage/unix-diag
+  TITLE:=UNIX socket monitoring interface
+  KCONFIG:=CONFIG_UNIX_DIAG
+  FILES:= $(LINUX_DIR)/net/unix/unix_diag.ko
+  AUTOLOAD:=$(call AutoProbe,unix_diag)
+endef
+
+$(eval $(call KernelPackage,unix-diag))
+
+define KernelPackage/packet-diag
+  TITLE:=Packet sockets monitoring interface
+  KCONFIG:=CONFIG_PACKET_DIAG
+  FILES:= $(LINUX_DIR)/net/packet/af_packet_diag.ko
+  AUTOLOAD:=$(call AutoProbe,af_packet_diag)
+endef
+
+$(eval $(call KernelPackage,packet-diag))

commit e086bb951cad12510c0ee747ced3349504204f62

kernel: add missing submenu for diag modules
The submenu of two diag modules is missing, fix it.

Fixes: 65de1e0 ("kernel: add missing symbols for lxc")
Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>
Link: https://github.com/openwrt/openwrt/pull/18480
Signed-off-by: Robert Marko <robimarko@gmail.com>

package/kernel/linux/modules/netsupport.mk
@@ -1651,6 +1651,7 @@ endef
 $(eval $(call KernelPackage,qrtr-mhi))
 
 define KernelPackage/unix-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=UNIX socket monitoring interface
   KCONFIG:=CONFIG_UNIX_DIAG
   FILES:= $(LINUX_DIR)/net/unix/unix_diag.ko
@@ -1660,6 +1661,7 @@ endef
 $(eval $(call KernelPackage,unix-diag))
 
 define KernelPackage/packet-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=Packet sockets monitoring interface
   KCONFIG:=CONFIG_PACKET_DIAG
   FILES:= $(LINUX_DIR)/net/packet/af_packet_diag.ko