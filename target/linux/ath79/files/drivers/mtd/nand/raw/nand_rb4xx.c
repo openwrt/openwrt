45743a3637aa265e80f9049e0dfdf8d0dc4511da
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2025-04-19 12:23:14 +0200

    ath79: convert driver to .remove_new
    Convert driver to .remove_new in preparation for kernel 6.12 support.
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>
    Link: https://github.com/openwrt/openwrt/pull/18535
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/ath79/files/drivers/mtd/nand/raw/nand_rb4xx.c
+++ target/linux/ath79/files/drivers/mtd/nand/raw/nand_rb4xx.c
@@ -214,14 +214,12 @@ static int rb4xx_nand_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int rb4xx_nand_remove(struct platform_device *pdev)
+static void rb4xx_nand_remove(struct platform_device *pdev)
 {
 	struct rb4xx_nand *nand = platform_get_drvdata(pdev);
 
 	mtd_device_unregister(nand_to_mtd(&nand->chip));
 	nand_cleanup(&nand->chip);
-
-	return 0;
 }
 
 static const struct platform_device_id rb4xx_nand_id_table[] = {
@@ -232,7 +230,7 @@ MODULE_DEVICE_TABLE(platform, rb4xx_nand_id_table);
 
 static struct platform_driver rb4xx_nand_driver = {
 	.probe = rb4xx_nand_probe,
-	.remove = rb4xx_nand_remove,
+	.remove_new = rb4xx_nand_remove,
 	.id_table = rb4xx_nand_id_table,
 	.driver = {
 		.name = "rb4xx-nand",
