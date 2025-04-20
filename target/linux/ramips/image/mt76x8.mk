c3b8108a2be4df3a114ac6d964c664dffd2d5bbf
Olgun Demir <olgun.demir@mail.com.tr>
2025-04-20 07:04:33 +0200

    ramips: Add support for Xiaomi MiWiFi 3A
    The Xiaomi MiWiFi 3A wireless router has a similar system architecture as the Xiaomi Mi 4A router, which is already officially supported by OpenWrt.
    
    Product website: https://www.mi.com/miwifi3a
    
    Device specification
    --------------------
    SoC:      MT7628AN MIPS_24KEc @ 580 MHz 2.4G-bgn 2x2
    WiFi:     MT7612EN 5G-an, ac 80 MHz 2T2R
    Flash:    16 MB
    DRAM:     64 MB
    Switch:   MT7628AN (integrated in SoC)
    Ethernet: 1 x 10 /100 Mbps
    USB:      None
    Antennas: 2 x 2,4 GHz and 2 x 5 GHz (all are external and non-detachable)
    LEDs:     blue/red/amber
    Buttons:  Reset
    Serial:   115200,8n1
    
    MAC addresses as verified by OEM firmware:
    ------------------------------------------
    use address source
    LAN *:DD factory 0x28
    WAN *:DD factory 0x28
    2g *:DE factory 0x4
    5g *:DF factory 0x8004
    
    OEM firmware uses VLAN's to create the network interface for WAN and LAN.
    
    Bootloader info:
    ----------------
    The stock bootloader uses a "Dual ROM Partition System".
    OS1 is a deep copy of OS2.
    The bootloader starts OS2 by default.
    To force start OS1 it is needed to set "flag_try_sys2_failed=1".
    
    How to install:
    ---------------
    1- Use OpenWRTInvasion to gain Telnet, SSH and FTP access: https://github.com/acecilia/OpenWRTInvasion
    [IP: 192.168.31.1 | Username: root | Password: root | FTP-Port: 21]
    2- Connect to router using telnet or ssh.
    3- Backup all partitions. Use command "dd if=/dev/mtd0 of=/tmp/mtd0". Copy /tmp/mtd0 to computer using ftp.
    4- Copy openwrt-ramips-mt76x8-xiaomi_miwifi-3a-squashfs-sysupgrade.bin to /tmp in router using ftp.
    5- Enable UART access and change start image to OS1.
    
    nvram set uart_en=1
    nvram set flag_last_success=1
    nvram set boot_wait=on
    nvram set flag_try_sys2_failed=1
    nvram commit
    
    6- Erase OS1 & OS2 and install OpenWrt
    
    mtd erase OS1
    mtd erase OS2
    mtd -r write /tmp/openwrt-ramips-mt76x8-xiaomi_miwifi-3a-squashfs-sysupgrade.bin OS1
    
    Credits:
    --------
    This PR is based on the work of Zehao Zhang (Github: @ZZH-Finalize) that he had published in the PR: #15698
    
    Signed-off-by: Olgun Demir <olgun.demir@mail.com.tr>
    Link: https://github.com/openwrt/openwrt/pull/18427
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -1213,6 +1213,14 @@ define Device/xiaomi_mi-router-4c
 endef
 TARGET_DEVICES += xiaomi_mi-router-4c
 
+define Device/xiaomi_miwifi-3a
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := MiWiFi 3A
+  DEVICE_PACKAGES := kmod-mt76x2
+endef
+TARGET_DEVICES += xiaomi_miwifi-3a
+
 define Device/xiaomi_miwifi-3c
   IMAGE_SIZE := 15104k
   DEVICE_VENDOR := Xiaomi
