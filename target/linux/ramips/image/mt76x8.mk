ebd5e5fb5359d5afb9b0cbda8727830592615a81
Steffen Förster <nemesis@chemnitz.freifunk.net>
2025-04-25 18:27:05 +0200

    ramips: switched TP-Link RE305 v1 to new partition layout
    After trying to implement the gluon support for this device I ended up in a boot loop due to the usable amount of flash left. With this patch layout it uses the unused and empty flash space in the original partiton layout.
    The version 3 of this device the RE365 share the same approach to have more usable space.
    
    Signed-off-by: Steffen Förster <nemesis@chemnitz.freifunk.net>
    Link: https://github.com/openwrt/openwrt/pull/18639
    Signed-off-by: Sander Vanheule <sander@svanheule.net>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -889,10 +889,16 @@ TARGET_DEVICES += tplink_re220-v2
 
 define Device/tplink_re305-v1
   $(Device/tplink-safeloader)
-  IMAGE_SIZE := 6016k
+  IMAGE_SIZE := 7680k
+  KERNEL_SIZE := 6016k
   DEVICE_MODEL := RE305
   DEVICE_VARIANT := v1
   DEVICE_PACKAGES := kmod-mt76x2
+  DEVICE_COMPAT_VERSION := 2.0
+  DEVICE_COMPAT_MESSAGE := Partition design has changed compared to older versions due to size restrictions and unsused flash. \
+	Upgrade via sysupgrade mechanism is not possible, so new installation via TFTP is required.
+  IMAGES := sysupgrade.bin
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | check-size | append-metadata
   TPLINK_BOARD_ID := RE305-V1
 endef
 TARGET_DEVICES += tplink_re305-v1
