0842e793381a1e60f8acdcbea0e8c59be1ac9a88
David Bauer <mail@david-bauer.net>
2025-05-04 04:47:32 +0200

    ramips: add Semtech SX9512 touch input driver
    This driver is required for the touch-inputs as well as some LEDs on the
    Genexis Pulse EX400.
    
    The driver was also sent upstream. As the EX400 is currently the only
    consumer, the driver is added target-specific for ramips.
    
    Once the driver has been accepted upstream and is provided in a kernel
    release used by OpenWrt, the package should be moved to the global input
    drivers.
    
    Link: https://www.spinics.net/lists/kernel/msg5669349.html
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- target/linux/ramips/modules.mk
+++ target/linux/ramips/modules.mk
@@ -152,3 +152,21 @@ define KernelPackage/sound-mt7620/description
 endef
 
 $(eval $(call KernelPackage,sound-mt7620))
+
+
+define KernelPackage/keyboard-sx951x
+  SUBMENU:=Other modules
+  TITLE:=Semtech SX9512/SX9513
+  DEPENDS:=@TARGET_ramips_mt7621 +kmod-input-core
+  KCONFIG:= \
+	CONFIG_KEYBOARD_SX951X \
+	CONFIG_INPUT_KEYBOARD=y
+  FILES:=$(LINUX_DIR)/drivers/input/keyboard/sx951x.ko
+  AUTOLOAD:=$(call AutoProbe,sx951x)
+endef
+
+define KernelPackage/keyboard-sx951x/description
+ Enable support for SX9512/SX9513 capacitive touch controllers
+endef
+
+$(eval $(call KernelPackage,keyboard-sx951x))
