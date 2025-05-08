465249f2b3c946437ee846c86c7ceb72a4c53f15
Daniel Golle <daniel@makrotopia.org>
2025-05-08 17:51:44 +0100

    mediatek: filogic: adapt BananaPi-R4 for use with upstream DT
     * the variant with 2.5G PHY instead of LAN SFP is called '2p5'
       upstream and 'poe' in our downstream Linux 6.6 DT. Use the right
       DTS depending on the kernel version and set an additional
       compatible.
    
     * drop additional DT overlay for WiFi.
       The final version of the board uses a physical switch for the 12V
       power of the WiFi module and the I2C EEPROM of the module always
       comes empty (instead of with a MAC address).
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -71,6 +71,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r3|\
 	bananapi,bpi-r3-mini|\
 	bananapi,bpi-r4|\
+	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
@@ -203,6 +204,7 @@ platform_check_image() {
 	bananapi,bpi-r3|\
 	bananapi,bpi-r3-mini|\
 	bananapi,bpi-r4|\
+	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
@@ -254,6 +256,7 @@ platform_copy_config() {
 	bananapi,bpi-r3|\
 	bananapi,bpi-r3-mini|\
 	bananapi,bpi-r4|\
+	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	cmcc,rax3000m)
 		if [ "$CI_METHOD" = "emmc" ]; then
