openwrt/openwrt

commit 5048c9d501ad8547f8bd88cc910ef0aa2f92cc25

kernel: modules: adjust the object files of kmod-fb
kmod-fb no longer depends on fb_io_fops.ko since 6.8 kernel.

Link: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=linux-6.8.y&id=8813e86f6d82a7931446c3cbc5d596f77d0f1ba6
Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/18661
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/video.mk
@@ -129,7 +129,7 @@ define KernelPackage/fb
 	CONFIG_VT_HW_CONSOLE_BINDING=y
   FILES:=$(LINUX_DIR)/drivers/video/fbdev/core/fb.ko \
 	$(LINUX_DIR)/lib/fonts/font.ko \
-	$(LINUX_DIR)/drivers/video/fbdev/core/fb_io_fops.ko
+	$(LINUX_DIR)/drivers/video/fbdev/core/fb_io_fops.ko@lt6.8
   AUTOLOAD:=$(call AutoLoad,06,fb font)
 endef
 

commit 1863908daabae14dae907bda27a902e77bb4bc6b

kernel/drm-radeon: 6.12: fix missing dependency
Fixes:
Package kmod-drm-radeon is missing dependencies for the following libraries:
fb_io_fops.ko
make[2]: *** [modules/video.mk:620: /__w/openwrt/openwrt/openwrt/bin/targets/x86/geode/packages/kmod-drm-radeon-6.12.28-r1.apk] Error 1
time: package/kernel/linux/compile#45.35#40.01#75.34

Fixes: 5048c9d ("kernel: modules: adjust the object files of kmod-fb ")

Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

package/kernel/linux/modules/video.mk
@@ -626,7 +626,8 @@ define KernelPackage/drm-radeon
 	+kmod-drm-ttm +kmod-drm-ttm-helper +kmod-i2c-algo-bit +radeon-firmware \
 	+kmod-drm-display-helper +kmod-acpi-video +kmod-drm-suballoc-helper
   KCONFIG:=CONFIG_DRM_RADEON
-  FILES:=$(LINUX_DIR)/drivers/gpu/drm/radeon/radeon.ko
+  FILES:=$(LINUX_DIR)/drivers/gpu/drm/radeon/radeon.ko \
+	$(LINUX_DIR)/drivers/video/fbdev/core/fb_io_fops.ko@ge6.8
   AUTOLOAD:=$(call AutoProbe,radeon)
 endef
 