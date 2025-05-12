0d6ba33181ef8846f1259e51ddc79115a4a0def4
Christoph Krapp <achterin@gmail.com>
2025-05-12 21:19:27 +0200

    mediatek: filogic: fix wifi on ASUS RT-AX52
    Add missing kmod-7915e package to get both wireless interfaces working.
    
    Link: https://github.com/openwrt/openwrt/issues/18690
    
    Reviewed-by: Steffen Förster <nemesis@chemnitz.freifunk.net>
    Signed-off-by: Christoph Krapp <achterin@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -256,12 +256,12 @@ define Device/asus_rt-ax52
   DEVICE_MODEL := RT-AX52
   DEVICE_DTS := mt7981b-asus-rt-ax52
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
   IMAGES := sysupgrade.bin
   KERNEL := kernel-bin | lzma | \
 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb  with-initrd | pad-to 64k
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   ARTIFACTS := initramfs.trx
   ARTIFACT/initramfs.trx := append-image-stage initramfs-kernel.bin | \
