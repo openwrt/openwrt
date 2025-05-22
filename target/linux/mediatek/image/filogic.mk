53ebda4e8278c313f2d0260d488ad64163cc0c4e
Mateusz Krzak <kszaquitto+github@gmail.com>
2025-05-22 18:39:06 +0200

    mediatek: filogic: D-Link M30: add OpenWrt partition layout
    This commit adds OpenWrt partition layout support for D-Link
    Aquila M30 A1. The aim is to concatenate ubi and ubi1 for
    additional 50MB space for user data.
    
    Suffix 'ubootmod' is added because it is not possible to
    directly sysupgrade to this version. Stock version is kept for
    people who want to keep stock 50/50 layout.
    
    Recovery image is built with initramfs, so that OpenWrt can
    boot even if current partition layout is different. Then,
    on sysupgrade partition layout is updated in u-boot-env
    and system can boot normally.
    
    Setting mupgrade_en to 0 is required to avoid u-boot running
    'aup' command (Auto Upgrade?), because it restores stock
    partition layout.
    
    Install:
    --------
    From any firmware (OEM or OpenWrt):
     - Set your IP address to 192.168.200.2, subnetmask 255.255.255.0
     - Press the reset button while powering on the device
     - Keep the reset button pressed until the LED blinks red
     - Open a Chromium based browser and goto http://192.168.200.1 (recovery web interface)
     - Download openwrt-mediatek-filogic-dlink_aquila-pro-ai-m30-a1-ubootmod-squashfs-recovery.bin
     - The recovery web interface always reports successful flashing, even if it fails
     - After flashing, the recovery web interface will try to forward the browser to 192.168.0.1
       (can be ignored)
     - OpenWrt initramfs should start - the LED will blink white and stay white at the end
     - Perform a sysupgrade using
       openwrt-mediatek-filogic-dlink_aquila-pro-ai-m30-a1-ubootmod-squashfs-sysupgrade.bin
    
    Return to stock:
    ----------------
    
    1. Prepare DECRYPTED OEM firmware.
    
    2. Start OpenWrt and run:
    ```
    fw_setenv sw_tryactive 0
    fw_setenv boot_by_tryactive "env default -a; saveenv; aup; reset"
    ```
    
    3. After a reboot, router will boot into OEM recovery,
       where you can flash a DECRYPTED OEM image.
    
    Signed-off-by: Mateusz Krzak <kszaquitto+github@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1746,6 +1746,13 @@ endif
 endef
 TARGET_DEVICES += dlink_aquila-pro-ai-m30-a1
 
+define Device/dlink_aquila-pro-ai-m30-a1-ubootmod
+  $(Device/dlink_aquila-pro-ai-m30-a1)
+  DEVICE_VARIANT := A1 (OpenWrt partition layout)
+  DEVICE_DTS := mt7981b-dlink-aquila-pro-ai-m30-a1-ubootmod
+endef
+TARGET_DEVICES += dlink_aquila-pro-ai-m30-a1-ubootmod
+
 define Device/dlink_aquila-pro-ai-m60-a1
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := AQUILA PRO AI M60
