5ab72cfc40555cecab7f289794da0e7d6bb71f63
Álvaro Fernández Rojas <noltari@gmail.com>
2025-06-07 12:01:54 +0200

    bmips: bcm6358: add support for Huawei HG556a ver A
    Specifications:
    - SoC: Broadcom BCM6358 dual 300MHz MIPS
    - Flash: 16MB NOR Spansion S29GL128P10
    - RAM: 64MB DDR
    - Ethernet: 4x 100M
    - Wifi: Atheros AR9223
    - 3x USB 2.0 port
    - 4x Button
    - 13x LED
    - RJ11 2x FXS VoIP (unsupported)
    - RJ11 xDSL (unsupported)
    
    Install instructions:
    - Assign static IP 192.168.1.100 to PC.
    - Unplug the power source.
    - Press the RESTART button at the router, don't release it yet!
    - Plug the power source and wait at least 15 seconds.
    - Release the RESTART button.
    - Browse to http://192.168.1.1 with your PC.
    - Upload the openwrt-bmips-bcm6358-huawei_hg556a-a-squashfs-cfe.bin file.
    - Wait some minutes until the firmware upgrade completes.
    
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- target/linux/bmips/image/bcm6358.mk
+++ target/linux/bmips/image/bcm6358.mk
@@ -1,5 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+define Device/huawei_hg556a-a
+  $(Device/bcm63xx-cfe-legacy)
+  DEVICE_VENDOR := Huawei
+  DEVICE_MODEL := EchoLife HG556a
+  DEVICE_VARIANT := A
+  CHIP_ID := 6358
+  CFE_BOARD_ID := HW556
+  CFE_EXTRAS += --rsa-signature "EchoLife_HG556a" --tag-version 8
+  IMAGE_OFFSET := 0x20000
+  DEVICE_PACKAGES += $(USB2_PACKAGES) $(ATH9K_PACKAGES) \
+    kmod-leds-gpio
+endef
+TARGET_DEVICES += huawei_hg556a-a
+
 define Device/huawei_hg556a-b
   $(Device/bcm63xx-cfe-legacy)
   DEVICE_VENDOR := Huawei
