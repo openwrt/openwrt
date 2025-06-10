6af9476b8a3c75ce65f17cfa89da7d35360a4756
Nicolas BERTRAND <nicolasbertrand89@gmail.com>
2025-06-10 11:09:06 +0200

    realtek: Add support for Zyxel XGS1210-12 Switch
    The Zyxel XGS1210-12 Switch is a 10 + 2 port multi-GBit switch with
    8 x 1000BaseT, 2 x 10/100/1000/2500BaseT Ethernet ports and
    2 SFP+ module slot.
    
    Hardware:
    - RTL9302B SoC
    - Macronix MX25L12833F (16MB flash)
    - Nanja NT5CC64M16GP-1 (128MB DDR3 SDRAM)
    - RTL8231 GPIO extender to control the port LEDs
    - RTL8218D 8x Gigabit PHY
    - RTL8226 2x 10m/100m/1/2.5 Gigabit PHY
    - SFP+ 2x 10GBit slot
    
    Power is supplied via a 12V 1.5A standard barrel connector. At the
    right side behind the grid is UART serial connector. A Serial
    header can be connected to from the outside of the switch trough
    the airvents with a standard 2.54mm header.
    Pins are from top to bottom Vcc(3.3V), TX, RX and GND. Serial
    connection is via 115200 baud, 8N1.
    
    A reset button is accessble through a hole in the front panel
    
    At the time of this commit, all ethernet ports work under OpenWrt,
    including the various NBaseT modes, SFP+ slots are supported with i2c bus.
    
    Installation
    --------------
    * Connect serial as per the layout above. Connection parameters: 115200 8N1.
    * Navigate to 'Management' in the OEM web interface and click on 'Firmware upgrade'
      to the left.
    * Upload the OpenWrt initramfs image, and wait till the switch reboots.
    * Connect to the device through serial and change the U-boot boot command.
      > fw_setenv bootcmd 'rtk network on; boota'
    * Reboot, scp the sysupgrade image to /tmp, verify the checksum and flash it:
      > sysupgrade openwrt-realtek-rtl930x-Zyxel_xgs1210-12-squashfs-sysupgrade.bin
    * Upon reboot, you have a functional OpenWrt installation. Leave the bootcmd
      value as is - without 'rtk network on' the switch will fail to initialise
      the network.
    
    Debug
    ------------
    * Connect serial as per the layout above. Connection parameters: 115200 8N1.
    * A tftp server is requiered, tftpd-hpa works well.
    * Power the device, at U-Boot start rapidly hit Esc key to stop autoboot
    * Enable network:
      > rtk network on
    * Change ip address (default is 192.168.1.1):
      > setenv ipaddr 192.168.1.6
    * Download initramfs:
      > tftpboot 0x84f00000 192.168.1.111:openwrt-realtek-rtl930x-Zyxel_xgs1210-12-initramfs-kernel.bin
    * Boot loaded file:
      > bootm 0x84f00000
    
    This prodecudre also apply to the sock firmware with the file XGS1210-12_V2.00(ABTY.1)C0.bix.
    
    More information can be found on the page of XGS1250-12 as they share the same base.
    
    Signed-off-by: Nicolas BERTRAND <nicolasbertrand89@gmail.com>
    [fixed white space error]
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -31,6 +31,22 @@ define Device/xikestor_sks8300-8x
 endef
 TARGET_DEVICES += xikestor_sks8300-8x
 
+define Device/zyxel_xgs1210-12
+  SOC := rtl9302
+  UIMAGE_MAGIC := 0x93001210
+  ZYXEL_VERS := ABTY
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := XGS1210-12
+  IMAGE_SIZE := 13312k
+  KERNEL_INITRAMFS := \
+        kernel-bin | \
+        append-dtb | \
+        gzip | \
+        zyxel-vers | \
+        uImage gzip
+endef
+TARGET_DEVICES += zyxel_xgs1210-12
+
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
   UIMAGE_MAGIC := 0x93001250
