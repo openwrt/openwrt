c9c63e32efa2dcc6cd78df0fc2640aa6ca50e2de
Sharadanand Karanjkar <sk@simonwunderlich.de>
2025-06-11 14:35:14 +0000

    rtl93xx: i2c: Fix i2c multi byte read and add support for BLOCK transfers
    Signed-off-by: Sharadanand Karanjkar <sk@simonwunderlich.de>
    Co-authored-by: Harshal Gohel <hg@simonwunderlich.de>

--- target/linux/realtek/files-6.6/drivers/i2c/busses/i2c-rtl9300.c
+++ target/linux/realtek/files-6.6/drivers/i2c/busses/i2c-rtl9300.c
@@ -148,10 +148,14 @@ static int i2c_write(void __iomem *r0, u8 *buf, int len)
 
 		if (! (i % 4))
 			v = 0;
-		v <<= 8;
-		v |= buf[i];
-		if (i % 4 == 3 || i == len - 1)
+		/* Write from left to right */
+		v |= (buf[i] << 24);
+		if (i % 4 == 3 || i == len - 1) {
+			v >>= (8 * (3 - i % 4));
 			writel(v, r0 + (i / 4) * 4);
+		}
+		else
+			v >>= 8;
 	}
 
 	return len;
@@ -212,7 +216,7 @@ static int rtl9300_execute_xfer(struct rtl9300_i2c *i2c, char read_write,
 		} else if (size == I2C_SMBUS_WORD_DATA) {
 			data->word = readl(REG(i2c, RTL9300_I2C_DATA_WORD0));
 		} else if (len > 0) {
-			rtl9300_i2c_read(i2c, &data->block[0], len);
+			rtl9300_i2c_read(i2c, &data->block[1], len);
 		}
 	}
 
@@ -243,7 +247,7 @@ static int rtl9310_execute_xfer(struct rtl9300_i2c *i2c, char read_write,
 		} else if (size == I2C_SMBUS_WORD_DATA) {
 			data->word = readl(REG(i2c, RTL9310_I2C_DATA));
 		} else if (len > 0) {
-			rtl9310_i2c_read(i2c, &data->block[0], len);
+			rtl9310_i2c_read(i2c, &data->block[1], len);
 		}
 	}
 
@@ -295,8 +299,8 @@ static int rtl9300_i2c_smbus_xfer(struct i2c_adapter * adap, u16 addr,
 		break;
 
 	case I2C_SMBUS_BLOCK_DATA:
-		pr_debug("I2C_SMBUS_BLOCK_DATA %02x, read %d, len %d\n",
-			addr, read_write, data->block[0]);
+		pr_debug("I2C_SMBUS_BLOCK_DATA %02x, read %d, len %d data->block: %*ph\n",
+			addr, read_write, data->block[0], data->block[0]+1, &data->block[0]);
 		drv_data->reg_addr_set(i2c, command, 1);
 		drv_data->config_xfer(i2c, addr, data->block[0]);
 		if (read_write == I2C_SMBUS_WRITE)
@@ -304,6 +308,15 @@ static int rtl9300_i2c_smbus_xfer(struct i2c_adapter * adap, u16 addr,
 		len = data->block[0];
 		break;
 
+	case I2C_SMBUS_I2C_BLOCK_DATA:
+		pr_debug("I2C_SMBUS_I2C_BLOCK_DATA %02x, read %d, len %d data->block: %*ph\n", addr, read_write, data->block[0], data->block[0]+1, &data->block[0]);
+
+		drv_data->reg_addr_set(i2c, command, 1);
+		drv_data->config_xfer(i2c, addr, data->block[0]);
+		if (read_write == I2C_SMBUS_WRITE)
+			drv_data->write(i2c, &data->block[1], data->block[0]);
+		len = data->block[0];
+		break;
 	default:
 		dev_warn(&adap->dev, "Unsupported transaction %d\n", size);
 		return -EOPNOTSUPP;
@@ -320,7 +333,8 @@ static u32 rtl9300_i2c_func(struct i2c_adapter *a)
 {
 	return I2C_FUNC_SMBUS_QUICK | I2C_FUNC_SMBUS_BYTE |
 	       I2C_FUNC_SMBUS_BYTE_DATA | I2C_FUNC_SMBUS_WORD_DATA |
-	       I2C_FUNC_SMBUS_BLOCK_DATA;
+	       I2C_FUNC_SMBUS_BLOCK_DATA | I2C_FUNC_SMBUS_READ_I2C_BLOCK |
+		   I2C_FUNC_SMBUS_WRITE_I2C_BLOCK;
 }
 
 static const struct i2c_algorithm rtl9300_i2c_algo = {
@@ -336,6 +350,7 @@ struct i2c_adapter_quirks rtl9300_i2c_quirks = {
 
 static int rtl9300_i2c_probe(struct platform_device *pdev)
 {
+	struct resource *res;
 	struct rtl9300_i2c *i2c;
 	struct i2c_adapter *adap;
 	struct i2c_drv_data *drv_data;
@@ -350,13 +365,15 @@ static int rtl9300_i2c_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+
 	drv_data = (struct i2c_drv_data *) device_get_match_data(&pdev->dev);
 
 	i2c = devm_kzalloc(&pdev->dev, sizeof(struct rtl9300_i2c), GFP_KERNEL);
 	if (!i2c)
 		return -ENOMEM;
 
-	i2c->base = devm_platform_ioremap_resource(pdev, 0);
+	i2c->base = devm_ioremap_resource(&pdev->dev, res);
 	i2c->mst2_offset = drv_data->mst2_offset;
 	if (IS_ERR(i2c->base))
 		return PTR_ERR(i2c->base);
@@ -448,7 +465,7 @@ struct i2c_drv_data rtl9300_i2c_drv_data = {
 struct i2c_drv_data rtl9310_i2c_drv_data = {
 	.scl0_pin = 13,
 	.scl1_pin = 14,
-	.sda0_pin = 0,
+	.sda0_pin = 15,
 	.read = rtl9310_i2c_read,
 	.write = rtl9310_i2c_write,
 	.reg_addr_set = rtl9310_i2c_reg_addr_set,
