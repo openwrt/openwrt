2365240cce46395b51665005ee3bcceb67e7ffba
Mario Andrés Pérez <mapb_@outlook.com>
2025-06-12 15:49:59 +0200

    base-files: handle packages alternatives when apk removes packages
    On commit 3010ab8 ("base-files: add update_alternatives function") was
    implemented the function to handle ALTERNATIVES when using APK (OPKG
    handle it internally) but in commit bcc6415 ("base-files: add
    compatibility for APK and OPKG") was only called when adding a package,
    so call it also when removing packages.
    
    Fixes: bcc6415 ("base-files: add compatibility for APK and OPKG")
    Fixes: https://github.com/openwrt/openwrt/issues/19090
    Fixes: https://github.com/openwrt/openwrt/issues/16991
    Reported-and-tested-by: Eric Fahlgren <ericfahlgren@gmail.com>
    Signed-off-by: Mario Andrés Pérez <mapb_@outlook.com>

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -216,6 +216,10 @@ default_prerm() {
 	local filelist="${root}/usr/lib/opkg/info/${pkgname}.list"
 	[ -f "$root/lib/apk/packages/${pkgname}.list" ] && filelist="$root/lib/apk/packages/${pkgname}.list"
 
+	if [ -e "$root/lib/apk/packages/${pkgname}.alternatives" ]; then
+		update_alternatives remove "${pkgname}"
+	fi
+
 	if [ -f "$root/usr/lib/opkg/info/${pkgname}.prerm-pkg" ]; then
 		( . "$root/usr/lib/opkg/info/${pkgname}.prerm-pkg" )
 		ret=$?
@@ -352,8 +356,7 @@ default_postinst() {
 		add_group_and_user "${pkgname}"
 	fi
 
-	if [ -e "${root}/lib/apk/packages/${pkgname}.list" ]; then
-		filelist="${root}/lib/apk/packages/${pkgname}.list"
+	if [ -e "${root}/lib/apk/packages/${pkgname}.alternatives" ]; then
 		update_alternatives install "${pkgname}"
 	fi
 
