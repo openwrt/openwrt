d9780a27a6d96068210e5c4bcfc4debbae05b6e3
Milan Krstic <milan.krstic@gmail.com>
2025-06-13 14:49:18 +0000

    qualcommax: ipq807x: add support for TCL LINKHUB HH500V
    TCL LINKHUB HH500V also known as Vodafone Gigacube B157 is a dual band
    802.11ax 5G NR CPE with an FXS port.
    
    Specifications:
    * SoC: Qualcomm IPQ8072A
    * RAM: 1024 MB
    * NAND flash: 256MB NAND (MT29F2G08ABBGAH4)
    * NOR flash: 64MB NOR (MX25U25635F) - only in some variants
    * WiFi 5G: Qualcomm QCN5054
    * WiFi 2.4G: Qualcomm QCN5024
    * Ethernet 2.5G: Qualcomm QCA8081
    * Ethernet 1G: Qualcomm QCA8075
    * Modem: GosunCn GM800 (Qualcomm Snapdragon X55)
    * SIM: 1 nano-SIM card slot
    * Buttons: Power, Reset, WPS
    * LEDs: Power (B/R), WiFi (B), 4G (B/Y/R), 5G (B/Y/R)
    * VoIP: 1 FXS RJ11 port (not supported in OpenWrt)
    * Power: 12V, 3A
    
    UART serial console:
    
    * 115200,8N1,1.8V
    * Three unmarked test points next to QJ2012A:
    
     +---------+
     | QJ2012A |
     +---------+
     o TX
     o GND
     o RX
     o
     o
     o
    
    Installation via OEM firmware:
    
    * Use the following process to obtain root ssh access
    https://forum.openwrt.org/t/150371/24
    * Connect using root with no password on port 42000
    * Optionally for serial bootloader access run :
    fw_setenv bootdelay=3
    * SCP factory.bin to /tmp
    * Run:
    sysupgrade -n /tmp/factory.bin
    
    Installation via serial console and OEM firmware::
    
    * Use console to access OEM firmware shell
    * Proceed with SCP & sysupgrade as described above
    
    Other notes:
    
    * This device uses active partition rotation
    * Some versions (TCL branded?) have a NOR chip in addition to NAND
    * The above is supported by u-boot patching DT partitions
    * DT patching does NOT occur on tftpboot/bootm
    * Modem is detected as foxconn-sdx55 by kernel (same VID/PID)
    * This works OK-ish and should be improved if we can get OEM modem details
    
    Signed-off-by: Milan Krstic <milan.krstic@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/19182
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    (cherry picked from commit ac8b5526bccabd26ec39df26b0cd437a664b82d0)

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -263,6 +263,10 @@ platform_do_upgrade() {
 		CI_DATAPART="rootfs_data"
 		emmc_do_upgrade "$1"
 		;;
+	tcl,linkhub-hh500v)
+		tcl_upgrade_prepare
+		nand_do_upgrade "$1"
+		;;
 	tplink,deco-x80-5g|\
 	tplink,eap620hd-v1|\
 	tplink,eap660hd-v1)
