46d34b6cf68cab54460f42839a647832e6d4315e
Sharadanand Karanjkar <sk@simonwunderlich.de>
2025-06-17 07:21:41 +0000

    rtl8224: Add initial probing support for rtl8224
    Disable low power mode on phy to start, this removes requirement of "rtk
    network on" before boot, however "rtk init" is still required
    
    Signed-off-by: Sharadanand Karanjkar <sk@simonwunderlich.de>
    Co-athored-by: Harshal Gohel <hg@simonwunderlich.de>
    Co-athored-by: Simon Wunderlich <sw@simonwunderlich.de>

--- target/linux/realtek/files-6.12/drivers/net/phy/rtl83xx-phy.c
+++ target/linux/realtek/files-6.12/drivers/net/phy/rtl83xx-phy.c
@@ -3649,6 +3649,14 @@ static int rtl8214c_phy_probe(struct phy_device *phydev)
 	return 0;
 }
 
+static int rtl8224_phy_probe(struct phy_device *phydev)
+{
+	int value;
+	value = phy_read_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_CTRL1);
+	value &= (~BMCR_PDOWN);
+	return phy_write_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_CTRL1, value);
+}
+
 static int rtl8218b_ext_phy_probe(struct phy_device *phydev)
 {
 	struct device *dev = &phydev->mdio.dev;
@@ -3917,6 +3925,21 @@ static struct phy_driver rtl83xx_phy_driver[] = {
 		.resume		= genphy_resume,
 		.read_status	= rtl9300_read_status,
 	},
+	{
+		PHY_ID_MATCH_MODEL(PHY_ID_RTL8224),
+		.name		= "REALTEK RTL8224",
+		.features	= PHY_10GBIT_FEATURES,
+		.flags		= PHY_POLL_CABLE_TEST,
+		.suspend	= genphy_suspend,
+		.resume		= genphy_resume,
+		.set_loopback	= genphy_loopback,
+		.read_status = rtl8226_read_status,
+		.config_aneg = rtl8226_config_aneg,
+		.set_eee = rtl8226_set_eee,
+		.get_eee = rtl8226_get_eee,
+		.probe		= rtl8224_phy_probe,
+	},
+
 };
 
 module_phy_driver(rtl83xx_phy_driver);
