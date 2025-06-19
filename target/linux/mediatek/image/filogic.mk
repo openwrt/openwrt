95c9f6e32666bf6fb8a9b16303b6f09bd6a9d83a
Tianling Shen <cnsztl@immortalwrt.org>
2025-06-19 22:16:34 +0800

    mediatek: add ubootmod layout for cudy tr3000 v1
    This allows us to use the full size of nand, which increases ubi size
    from 64M to 122.25M.
    
    If you are at factory firmware, please refer commit 63b8d98dd0d2 ("mediatek: add support for Cudy TR3000 v1")
    to boot into OpenWrt initramfs (stock layout).
    
    Flash instructions:
    1. Login into the device and backup everything, especially 'Factory' part.
    1. Unlock mtd partitions:
       opkg update && opkg install kmod-mtd-rw
       insmod mtd-rw i_want_a_brick=1
    3. Write new BL2 and FIP
       mtd write openwrt-mediatek-filogic-cudy_tr3000-v1-ubootmod-preloader.bin BL2
       mtd write openwrt-mediatek-filogic-cudy_tr3000-v1-ubootmod-bl31-uboot.fip FIP
    4. Set static IP on your PC:
       IP 192.168.1.254/24, GW 192.168.1.1
    5. Serve OpenWrt initramfs image using TFTP server.
    6. Cut off the power and re-engage, wait for TFTP recovery to complete.
    7. After OpenWrt has booted, perform sysupgrade.
    
    Tested-by: Yangyu Chen <cyy@cyyself.name>
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 6f8c58bfd8f380dfdc1d89aab29fdcdfde0ee65b)
    [sync uboot defconfigs with 24.10 branch, change apk to opkg]
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    Link: https://github.com/openwrt/openwrt/pull/18945
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -737,6 +737,31 @@ define Device/cudy_tr3000-v1
 endef
 TARGET_DEVICES += cudy_tr3000-v1
 
+define Device/cudy_tr3000-v1-ubootmod
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := TR3000
+  DEVICE_VARIANT := v1 (OpenWrt U-Boot layout)
+  DEVICE_DTS := mt7981b-cudy-tr3000-v1-ubootmod
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7981-bl2 cudy-tr3000-v1
+  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot cudy_tr3000-v1
+endef
+TARGET_DEVICES += cudy_tr3000-v1-ubootmod
+
 define Device/cudy_wr3000-v1
   DEVICE_VENDOR := Cudy
   DEVICE_MODEL := WR3000
