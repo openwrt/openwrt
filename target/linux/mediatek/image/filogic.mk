150f181700395e5a26bb11ff7eb7da0b7118f0b0
Fil Dunsky <filipp.dunsky@gmail.com>
2025-07-06 12:08:27 +0300

    mediatek: add Huasifei WH3000 Pro support
    **Huasifei WH3000 Pro**
    Portable Wi-Fi 6 travel router based on MediaTek MT7981A SoC. MT7981B+MT7976CN+RTL8221B Dual Core 1.3GHZ with 5G modems module and PWM Fan.
    
    **Specifications**
    SoC: Filogic 820 MT7981A (1.3GHz)
    RAM: DDR4 1GB
    Flash: eMMC 8GB
    WiFi: 2.4GHz and 5GHz with 3 antennas
    Ethernet:
    1x WAN (10/100/1000M)
    1x LAN (10/100/1000/2500M)
    USB: 1x USB 3.0 port
    Two buttons: power/reset and mode (BTN_0)
    LEDS: blue, red, blue+red=pink
    UART: 3.3V, TX, RX, GND / 115200 8N1
    
    **Installation via U-Boot rescue**
    1. Set static IP 192.168.1.2 on your computer and default route as 192.168.1.1
    2. Connect to the WAN port and hold the reset button while booting the device.
    3. Wait for the LED to blink 5 times, and release the reset button.
    4. Open U-boot web page on your browser at http://192.168.1.1
    5. Select the OpenWRT sysupgrade image, upload it, and start the upgrade.
    6. Wait for the router to flash the new firmware.
    7. Wait for the router to reboot itself.
    
    **Installation via sysupgrade**
    Just flash sysupgrade file via [LuCI upgrade page](http://192.168.1.1/cgi-bin/luci/admin/system/flash) without saving the settings.
    
    **Installation via SSH**
    Upload the file to the router `/tmp` directory, `ssh root@192.168.1.1` and issue a command:
    ```
    sysupgrade -n /tmp/openwrt-mediatek-filogic-huasifei_wh3000-pro-squashfs-sysupgrade.bin
    ```
    
    **Factory MAC**
    You can find your Factory MAC which is mentioned on the box at `/dev/mmcblck0p2` partition `factory` starting from `0x4`
    ```
    dd if=/dev/mmcblk0p2 bs=1 skip=4 count=6 | hexdump -C
    ```
    
    Cherry-picked from https://github.com/coolsnowwolf/lede/commit/949d0bd77a2e97c2d982531f72b76c575fb28839
    
    Fixed `green` to `blue` LED in dts, added `SUPPORTED_DEVICES += huasifei,fudy-pro` - to make sysupgrade compatible with factory QWRT/Lede fork firmware.
    
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/19315
    (cherry picked from commit db1de8d21fd7f115cc1a6274f031119c9f36eefd)
    Link: https://github.com/openwrt/openwrt/pull/19391
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1031,6 +1031,19 @@ define Device/huasifei_wh3000
 endef
 TARGET_DEVICES += huasifei_wh3000
 
+define Device/huasifei_wh3000-pro
+  DEVICE_VENDOR := Huasifei
+  DEVICE_MODEL := WH3000 Pro
+  DEVICE_DTS := mt7981b-huasifei-wh3000-pro
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware kmod-hwmon-pwmfan kmod-usb3 f2fsck mkf2fs
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += huasifei_wh3000-pro
+
 define Device/iptime_ax3000sm
   DEVICE_VENDOR := ipTIME
   DEVICE_MODEL := AX3000SM
