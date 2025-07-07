b778277162cf060578d74a3c1c6222334550125e
Sharadanand Karanjkar <sk@simonwunderlich.de>
2025-07-07 14:15:30 +0000

    realtek: add support for plasma cloud - psx28/esx28
    Device specification -
    
    SoC - Realtek RTL9312
    
    CPU Model - MIPS interAptiv (multi) V2.0.
    
    Memory - 256 MB.
    
    Flash memory - 32 MB NOR.
    
    Installation procedure -
    
    Via U-boot:
    
     1. Switch ON the hardware using - rtk network on
     2. Set server IP address - setenv serverip 192.168.1.1
     3. Set IP address - setenv ipaddr 192.168.1.20
     4. Get the RAMfs image into image - tftpboot 0x84000000 openwrt-realtek-rtl931x-plasmacloud_psx28-initramfs-kernel.bin
     5. Boot from the RAM - bootm 0x84000000
    
    Image will be booted and can be flashed by using sysupgrade.
    
    Via sysupgrade:
     1. Transfer the sysupgrade image to the switch -
     	scp openwrt-realtek-rtl931x-plasmacloud_psx28-squashfs-sysupgrade.bin root@[IP V6 address]:/tmp/
    
     2. Perform system update using sysupgrade command -
     	sysupgrade -n /tmp/openwrt-realtek-rtl931x-plasmacloud_psx28-squashfs-sysupgrade.bin
    
    Signed-off-by: Sharadanand Karanjkar <sk@simonwunderlich.de>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,54 @@
 # SPDX-License-Identifier: GPL-2.0-only
+define Device/plasmacloud_psx28
+  SOC := rtl9312
+  UIMAGE_MAGIC := 0x93100000
+  DEVICE_VENDOR := Plasma Cloud
+  DEVICE_MODEL := PSX28
+  IMAGE_SIZE := 13376k
+  BLOCKSIZE := 64k
+  SUPPORTED_DEVICES = plasmacloud,psx28
+  DEVICE_PACKAGES := \
+		     lm-sensors \
+		     kmod-hwmon-adt7475 \
+		     kmod-sched-act-police \
+		     kmod-sched-flower \
+		     poemgr \
+		     i2c-tools
+  KERNEL_INITRAMFS := \
+    kernel-bin | \
+    append-dtb | \
+    lzma | \
+    uImage lzma
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma | pad-to $$(BLOCKSIZE)
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | append-rootfs | pad-rootfs
+  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+TARGET_DEVICES = plasmacloud_psx28
+
+define Device/plasmacloud_esx28
+  SOC := rtl9312
+  UIMAGE_MAGIC := 0x93100000
+  DEVICE_VENDOR := Plasma Cloud
+  DEVICE_MODEL := ESX28
+  IMAGE_SIZE := 13376k
+  BLOCKSIZE := 64k
+  SUPPORTED_DEVICES = plasmacloud,esx28
+  DEVICE_PACKAGES := \
+		     lm-sensors \
+		     kmod-hwmon-adt7475 \
+		     kmod-sched-act-police \
+		     kmod-sched-flower \
+		     poemgr \
+		     i2c-tools
+  KERNEL_INITRAMFS := \
+    kernel-bin | \
+    append-dtb | \
+    lzma | \
+    uImage lzma
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma | pad-to $$(BLOCKSIZE)
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | append-rootfs | pad-rootfs
+  IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+TARGET_DEVICES += plasmacloud_esx28
