aea6d1bf5eb579614dcc12c3b2c7215b7cd985ac
Donghyun Ko <nyankosoftware@gmail.com>
2025-07-10 21:24:18 +0900

    mediatek: filogic: add support for ipTIME AX3000Q
    Specification
    -------------
    - SoC       : MediaTek MT7981BA dual-core ARM Cortex-A53 1.3GHz
    - RAM       : DDR3 256Mbytes, ESMT M15T2G16128A
    - Flash     : 128Mbytes NAND Flash, ESMT F50L1G41LB
    - WLAN      : MediaTek MT7976CN dual-band Wi-Fi 6
      - 2.4GHz  : b/g/n/ax, MU-MIMO
      - 5GHz    : a/n/ac/ax, MU-MIMO
    - Ethernet  : MediaTek MT7531AE
      - LAN     : 10/100/1000 Mbps x4
      - WAN     : 10/100/1000 Mbps x1
    - UART      : 1x4 pin header on PCB
      - [J6] TX, RX, GND, 3.3V (115200, 8N1)
    - Buttons   : WPS, Reset
    - LEDs      : 1x CPU (Amber)
                  1x Wi-Fi 5GHz (Amber)
                  1x Wi-Fi 2.4GHz (Amber)
                  1x WAN activity (Amber)
                  4x LAN activity (Amber)
    - Power     : 12VDC, 1A (Center positive polarity)
    
    MAC address
    -----------
    +-----------+-------------------+-----------------------+
    | Interface | MAC               | Algorithm             |
    +-----------+-------------------+-----------------------+
    | WLAN 2.4G | B0:38:6C:48:xx:xx | label                 |
    | WLAN 5G   | B2:38:6C:48:xx:xx | label with LA Bit Set |
    | WAN       | B0:38:6C:48:xx:xx | label + 1             |
    | LAN       | B0:38:6C:48:xx:xx | label + 3             |
    +-----------+-------------------+-----------------------+
    The WLAN 2.4G MAC was found in 'Factory' partition, 0x4
    
    Installation
    ------------
    1. Download the OEM recovery software from the manufacturer's website
    2. Download the *squashfs-factory.bin file from the OpenWrt website
    3. Press a reset button, and power up the router(keep pressing the reset button)
    4. Wait more than 10 seconds until the CPU LED stop blinking
    5. Connect the router(LAN port) to the PC
    6. Replace a file in the OEM recovery software with the file from step 2
    7. Run the OEM recovery software and follow the instructions
    8. Wait for the router to boot from *squashfs-factory.bin
    
    Signed-off-by: Donghyun Ko <nyankosoftware@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/19368
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1156,6 +1156,25 @@ define Device/huasifei_wh3000-pro
 endef
 TARGET_DEVICES += huasifei_wh3000-pro
 
+define Device/iptime_ax3000q
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := AX3000Q
+  DEVICE_DTS := mt7981b-iptime-ax3000q
+  DEVICE_DTS_DIR := ../dts
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 32768k
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := factory.bin sysupgrade.bin
+  IMAGE/factory.bin := sysupgrade-tar | append-metadata | check-size | iptime-crc32 ax3000q
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  SUPPORTED_DEVICES += mediatek,mt7981-spim-snand-rfb
+endef
+TARGET_DEVICES += iptime_ax3000q
+
 define Device/iptime_ax3000sm
   DEVICE_VENDOR := ipTIME
   DEVICE_MODEL := AX3000SM
