f8f96bf1117108f3164beb4ce6e335f30b025038
andros-ua <andros.ua@gmail.com>
2025-07-16 12:56:13 +0300

    platform.sh: add CreatLentem CLT-R30B1
    Signed-off-by: andros-ua <andros.ua@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -241,6 +241,8 @@ platform_check_image() {
 		fit_check_image "$1"
 		return $?
 		;;
+	creatlentem,clt-r30b1|\
+	creatlentem,clt-r30b1-112m|\
 	nradio,c8-668gl)
 		# tar magic `ustar`
 		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
