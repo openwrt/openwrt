53d606dd7801a2eb27783b15d9302e34299b764a
Andrew LaMarche <andrewjlamarche@gmail.com>
2025-08-06 02:35:19 +0000

    airoha: an7581: add common default packages
    These are the common packages for an7581 devices.
    
    Signed-off-by: Andrew LaMarche <andrewjlamarche@gmail.com>

--- target/linux/airoha/an7581/target.mk
+++ target/linux/airoha/an7581/target.mk
@@ -4,6 +4,7 @@ BOARDNAME:=AN7581 / AN7566 / AN7551
 CPU_TYPE:=cortex-a53
 KERNELNAME:=Image dtbs
 FEATURES+=pwm source-only
+DEFAULT_PACKAGES += airoha-en7581-npu-firmware
 
 define Target/Description
 	Build firmware images for Airoha an7581 ARM based boards.
