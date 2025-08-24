16dae693471c20a3db08a078c7bee7014a012a39
Qing W <ses1er@gmail.com>
2025-08-24 01:16:27 -0400

    mediatek: Add support for Acer Predator Connect W6x Ubootmod
    Product name: Acer Predator Connect W6x
    Product link: https://www.acer.com/us-en/predator/networking/wi-fi/predator-connect-w6x/pdp/FF.G2TTA.001
    
    * Specifications:
    
    SOC: MT7986AV
    RAM: 1024MB
    Flash: 256 MB SPI NAND
    Ports: 4 LAN (1G) & 1 WAN (2.5G)
    WIFI: MT7976GN + MT7976AN
    LED: 1, ws2812b controller
    
    ** This commit includes a fix for the GPIO pins for factory/reset and WPS. It was reversed and is now fixed.
    
    * U-Boot Mod Openwrt Installation via UART:
    
    Openwrt Stock Layout Commit: https://github.com/openwrt/openwrt/commit/6e04dccb7ad3191e9a48597a1b354bf548ead1d8
    NOTE: Stock Openwrt is not necessary. You can go straight to ubootmod version of the firmware. However, it is recommended to follow stock layout instructions to backup NAND.
    
    1. Configure TFTP server with IP 192.168.1.66. Copy `openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-initramfs-recovery.itb` to TFTP root and rename to `predator.bin`
    2. Interrupt boot by pressing 0 on startup or select `U-Boot Console` in U-Boot Boot Menu.
    3. Run setenv `serverip 192.168.1.66; setenv ipaddr 192.168.1.1; tftpboot 0x46000000 predator.bin; bootm` in uboot console.
    4. Wait for boot complete on Openwrt initramfs env.
    5. Transfer `openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-initramfs-recovery.itb`,
    		`openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-squashfs-sysupgrade.itb`,
    		`openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-bl31-uboot.fip`,
    		`openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-preloader.bin` to router's /tmp/ directory.
    6. run `ubidetach -p /dev/mtd5; ubiformat /dev/mtd5 -y; ubiattach -p /dev/mtd5`
    7. run `ubimkvol /dev/ubi0 -n 0 -N ubootenv -s 128KiB`
    8. run `ubimkvol /dev/ubi0 -n 1 -N ubootenv2 -s 128KiB`
    9. run `ubimkvol /dev/ubi0 -n 2 -N recovery -s 10MiB`
    10. run `ubiupdatevol /dev/ubi0_2 /tmp/openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-initramfs-recovery.itb`
    11. install kmod-mtd-rw via opkg or apk.
    12. run `insmod /lib/modules/$(uname -r)/mtd-rw.ko i_want_a_brick=1`
    13. run `mtd write /tmp/openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-preloader.bin bl2`
    14. run `mtd write /tmp/openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-bl31-uboot.fip fip`
    15. run `sysupgrade -n /tmp/openwrt-mediatek-filogic-acer_predator-w6x-ubootmod-squashfs-sysupgrade.itb`
    16. reboot to ubootmod layout
    
    MTD layout before ubootmod:
    ```
    dev:    size   erasesize  name
    mtd0: 00100000 00020000 "bl2"
    mtd1: 00080000 00020000 "u-boot-env"
    mtd2: 00200000 00020000 "factory"
    mtd3: 00200000 00020000 "fip"
    mtd4: 00020000 00020000 "prod"
    mtd5: 00100000 00020000 "dual"
    mtd6: 00100000 00020000 "pot"
    mtd7: 06400000 00020000 "ubi"
    mtd8: 06400000 00020000 "ubi1"
    mtd9: 00800000 00020000 "storage"
    ```
    
    MTD layout after ubootmod:
    ```
    dev:    size   erasesize  name
    mtd0: 00100000 00020000 "bl2"
    mtd1: 00080000 00020000 "u-boot-env"
    mtd2: 00200000 00020000 "factory"
    mtd3: 00200000 00020000 "fip"
    mtd4: 00020000 00020000 "prod"
    mtd5: 0d200000 00020000 "ubi"
    ```
    
    Signed-off-by: Qing W <ses1er@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -79,6 +79,7 @@ platform_do_upgrade() {
 
 	case "$board" in
 	abt,asr3000|\
+	acer,predator-w6x-ubootmod|\
 	asus,zenwifi-bt8-ubootmod|\
 	bananapi,bpi-r3|\
 	bananapi,bpi-r3-mini|\
@@ -242,6 +243,7 @@ platform_check_image() {
 
 	case "$board" in
 	abt,asr3000|\
+	acer,predator-w6x-ubootmod|\
 	asus,zenwifi-bt8-ubootmod|\
 	bananapi,bpi-r3|\
 	bananapi,bpi-r3-mini|\
