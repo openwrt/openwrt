b082f9f60e0009f44a43b0b046a0c8952f0c7761
Jonas Jelonek <jelonek.jonas@gmail.com>
2025-09-02 09:38:51 +0200

    realtek: fix model for TP-Link TL-ST1008F v2.0
    Fix the model name in DTS compatible, Makefiles and board scripts by
    using dash instead of comma or underscore. This aligns it with other
    examples in OpenWrt and makes in consistent in all places where the
    board model is used.
    
    'tplink,tl-st1008f,v2' --> 'tplink,tl-st1008f-v2'
    'tplink,tl-st1008f_v2' --> 'tplink,tl-st1008f-v2'
    
    Fixes: 39b9b491bb ("realtek: add support for TP-Link TL-ST1008F v2.0")
    Fixes: #19930
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/19934
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -40,17 +40,18 @@ define Device/plasmacloud_psx10
 endef
 TARGET_DEVICES += plasmacloud_psx10
 
-define Device/tplink_tl-st1008f_v2
+define Device/tplink_tl-st1008f-v2
   SOC := rtl9303
   UIMAGE_MAGIC := 0x93030000
   DEVICE_VENDOR := TP-Link
   DEVICE_MODEL := TL-ST1008F
   DEVICE_VARIANT := v2.0
   DEVICE_PACKAGES := kmod-gpio-pca953x
+  SUPPORTED_DEVICES += tplink,tl-st1008f,v2
   IMAGE_SIZE := 31808k
   $(Device/kernel-lzma)
 endef
-TARGET_DEVICES += tplink_tl-st1008f_v2
+TARGET_DEVICES += tplink_tl-st1008f-v2
 
 define Device/vimin_vm-s100-0800ms
   SOC := rtl9303
