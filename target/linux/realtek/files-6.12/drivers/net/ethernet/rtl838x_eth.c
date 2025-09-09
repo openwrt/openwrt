0d6b7fb56e06490caf70daad2e6e5386190569c3
Issam Hamdi <ih@simonwunderlich.de>
2025-09-09 15:32:29 +0200

    realtek: rtl93xx: Ignore STP for per port TX
    If transmissions are done outside of the DSA switch (directly from the CPU
    port), the STP state must not block the transmission. Otherwise, STP frames
    are not correctly submitted and the STP frames cannot correctly detect
    loops before switching a port in the forwarding state.
    
    The same applies for the LLDP frames. These must be submitted independent
    of the STP state to identify neighbors or configure POE limits.
    
    It is not necessary to filter specific destination mac addresses because
    the transmission was done outside the bridge/switch in the first place. The
    transmission is therefore forced.
    
    Signed-off-by: Issam Hamdi <ih@simonwunderlich.de>
    Co-developed-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Link: https://github.com/openwrt/openwrt/pull/20184
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/realtek/files-6.12/drivers/net/ethernet/rtl838x_eth.c
+++ target/linux/realtek/files-6.12/drivers/net/ethernet/rtl838x_eth.c
@@ -131,7 +131,10 @@ static void rtl839x_create_tx_header(struct p_hdr *h, unsigned int dest_port, in
 static void rtl930x_create_tx_header(struct p_hdr *h, unsigned int dest_port, int prio)
 {
 	h->cpu_tag[0] = 0x8000;  /* CPU tag marker */
-	h->cpu_tag[1] = 0x0200; /* Set FWD_TYPE to LOGICAL (2) */
+
+	h->cpu_tag[1] = FIELD_PREP(RTL93XX_CPU_TAG1_FWD_MASK,
+				   RTL93XX_CPU_TAG1_FWD_LOGICAL);
+	h->cpu_tag[1] |= FIELD_PREP(RTL93XX_CPU_TAG1_IGNORE_STP_MASK, 1);
 	h->cpu_tag[2] = 0;
 	h->cpu_tag[3] = 0;
 	h->cpu_tag[4] = 0;
@@ -147,7 +150,10 @@ static void rtl930x_create_tx_header(struct p_hdr *h, unsigned int dest_port, in
 static void rtl931x_create_tx_header(struct p_hdr *h, unsigned int dest_port, int prio)
 {
 	h->cpu_tag[0] = 0x8000;  /* CPU tag marker */
-	h->cpu_tag[1] = 0x0200; /* Set FWD_TYPE to LOGICAL (2) */
+
+	h->cpu_tag[1] = FIELD_PREP(RTL93XX_CPU_TAG1_FWD_MASK,
+				   RTL93XX_CPU_TAG1_FWD_LOGICAL);
+	h->cpu_tag[1] |= FIELD_PREP(RTL93XX_CPU_TAG1_IGNORE_STP_MASK, 1);
 	h->cpu_tag[2] = 0;
 	h->cpu_tag[3] = 0;
 	h->cpu_tag[4] = h->cpu_tag[5] = h->cpu_tag[6] = h->cpu_tag[7] = 0;
