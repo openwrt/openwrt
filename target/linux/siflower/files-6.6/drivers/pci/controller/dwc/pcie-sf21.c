6555321eb216b582ca9473b8355e14401640da5c
Rosen Penev <rosenp@gmail.com>
2025-09-17 18:44:21 -0700

    treewide: remove of_gpio.h
    of_gpio.h is deprecated in upstream Linux and may be removed soon. Get
    ahead of things and remove it. Most of these drivers already use the
    gpiod API.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/20076
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/siflower/files-6.6/drivers/pci/controller/dwc/pcie-sf21.c
+++ target/linux/siflower/files-6.6/drivers/pci/controller/dwc/pcie-sf21.c
@@ -13,7 +13,6 @@
 #include <linux/kernel.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
-#include <linux/of_gpio.h>
 #include <linux/of_device.h>
 #include <linux/of_address.h>
 #include <linux/pci.h>
