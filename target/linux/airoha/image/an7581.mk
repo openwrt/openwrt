9fb65c90ce137729c7870b313be27d4016ff91b6
Chukun Pan <amadeus@jmu.edu.cn>
2025-09-25 23:18:39 +0800

    airoha: add support for Gemtek W1701K
    This commit adds support for Quantum Fiber W1701K.
    
    Hardware specification:
      SoC:     Airoha AN7581
      RAM:     2GB DDR4 2666MHz
      Flash:   512MB W25N04KV
      ETH:     2x 2.5G EN8811H
      WLAN:    MT7996AV BE19000
      WLAN1:   MT7975N 2.4GHz 4x4
      WLAN2:   MT7977BN 5GHz 4x5
      WLAN3:   MT7977AN 6GHz 4x5
      PWM FAN: Nuvoton NCT7511Y
      Power:   100-240V AC 1.2A
      Button:  Reset
    
    Flash instructions:
      1. Download the initramfs image, rename it to
         initramfs.itb, host it with the tftp server.
    
      2. Connect to the router through the right Lan port,
         set a static ip of your PC. (ip 192.168.0.205)
    
      3. Interrupt U-Boot and run these commands:
    
          setenv bootflag 0
          setenv one flash read 0x600000 0x1000000 \$loadaddr
          setenv two "; bootm"
          setenv bootcmd "$one$two"
          setenv one
          setenv two
          saveenv
    
      4. Reboot and interrupt U-Boot again, then run:
    
          tftpboot 0x89000000 initramfs.itb
          bootm
    
      5. After openwrt boots up, use scp or luci web
         to upload sysupgrade.bin to upgrade.
    
    Note:
      1. Flash partition is based on pr 17869.
      2. Flash instructions is based on pr 17869.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -42,3 +42,17 @@ define Device/airoha_an7581-evb-emmc
   ARTIFACTS := preloader.bin bl31-uboot.fip
 endef
 TARGET_DEVICES += airoha_an7581-evb-emmc
+
+define Device/gemtek_w1701k
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Gemtek
+  DEVICE_MODEL := W1701K
+  DEVICE_ALT0_VENDOR := Quantum Fiber
+  DEVICE_ALT0_MODEL := W1701K
+  DEVICE_PACKAGES := kmod-i2c-an7581 kmod-phy-airoha-en8811h \
+		     kmod-mt7996-firmware wpad-basic-mbedtls
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  KERNEL_LOADADDR := 0x80088000
+  SOC := an7581
+endef
+TARGET_DEVICES += gemtek_w1701k
