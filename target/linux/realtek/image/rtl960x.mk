5095eb1c81b612dc2c0a518fee5c37b363782ce8
Rustam Adilov <adilov@tutamail.com>
2025-09-29 02:00:22 +0500

    realtek: add a new device BT-PON BT-G711AX
    The BT-PON BT-G711AX is a fiber modem (ONT) based on RTL9607C.
    
    Specifications:
    
    - SoC               : Realtek RTL9607C
    - RAM               : DDR3 256 MiB
    - Flash             : SPI-NAND 128 MiB (Windbond)
    - Ethernet          : 4 port GbE Switch
    - Fiber             : 1x XPON SC/UPC (Semtech GN25L95)
    - Phone             : 1x RJ11 (Si32192)
    - WIFI              : RTL8192XBR + RTL8832BR
    - LED               : 12x LEDs
        1x Power
        1x PON
        1x LOS
        4x LAN
        1x TEL
        1x USB
        1x WPS
        2x Wifi
    - UART              : 1x4 pin holes on PCB
        - settings      : 115200 8N1
    - Button            : 3x (WPS, WLAN, Reset)
    - USB               : 1x USB 2.0
    - Power             : 12V DC, 1.5A
    
    This device, and many other devices of this platform, use a
    dual-image layout inside a single ubi_device partition.
    
    Intructions to boot initramfs can be found in the previous
    commit message.
    
    Signed-off-by: Rustam Adilov <adilov@tutamail.com>

--- target/linux/realtek/image/rtl960x.mk
+++ target/linux/realtek/image/rtl960x.mk
@@ -1,1 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/bt-pon_bt-g711ax
+  SOC := rtl9607
+  DEVICE_VENDOR := BT-PON
+  DEVICE_MODEL := BT-G711AX
+  UIMAGE_MAGIC := 0x27051956
+  $(Device/kernel-lzma)
+endef
+TARGET_DEVICES += bt-pon_bt-g711ax
