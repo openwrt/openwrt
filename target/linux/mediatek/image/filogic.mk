23a0c62fa3a1dc9810049db9225218dc3922e763
Rani Hod <rani.hod@gmail.com>
2025-10-07 17:54:55 +0300

    mediatek: filogic: add support for Widelantech WAP430X
    This AX3000 flying saucer-shaped device is sold under a few names:
    - Widelantech WAP430X
      http://www.widelantech.com/?m=home&c=View&a=index&aid=131
    - Felicomm
    - UeeVii UAP200 (sold on amazon.com)
    True name, as marked on the PCB and as appears in the DTS, is WAP430X.
    
    Hardware:
    - SoC: Mediatek MT7981B
    - RAM: 256MiB DDR3
    - ROM: 16MiB NOR flash (Winbond 25Q128JVSQ)
    - Wired: one GbE RJ45 port
    - Wireless: Mediatek MT7976CN DBDC
    - Antennas: Internal (two 2.4 GHz 4.5 dBi, three 5 GHz 6 dBi)
    - GPIO: two blue LEDs (status, WLAN), one button (reset)
    - Power: DC5521 (12V) or 802.3af PoE (48V 0.3A)
    - UART: J500 on PCB [3v3] (Rx) (Tx) (Gnd), 115200n
    
    Original firmware is GECOOS 8.0_2024081000, a fork of OpenWrt 21.02
    created by Widelantech. All ports are closed except 443 (web management
    interface) and 80 (redirects to 443). UART is locked down, but U-Boot
    bootloader is friendly.
    
    Installation is done via UART (see note below).
    
    Method 1: press any key during boot to enter MTK U-Boot bootloader,
    then choose option 2 (Upgrade firmware) and upload sysupgrade binary
    via TFTP (or via Xmodem).
    
    Method 2: put sysupgrade binary on a webserver (http, not https); after
    GECOOS loads, choose option 4 (固件在线升级 = online firmware upgrade)
    in the menu and enter the URL to the sysupgrade binary.
    
    Note: GECOOS has a slick web management, and firmware can be uploaded
    at https://6.6.6.6/#/system/sysupgrade but it's still not clear how to
    convert the sysupgrade binary to a factory binary accepted by the web
    interface (WIP).
    
    Signed-off-by: Rani Hod <rani.hod@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2239,6 +2239,17 @@ define Device/wavlink_wl-wn573hx3
 endef
 TARGET_DEVICES += wavlink_wl-wn573hx3
 
+define Device/widelantech_wap430x
+  DEVICE_VENDOR := Widelantech
+  DEVICE_MODEL := WAP430X
+  DEVICE_DTS := mt7981b-widelantech-wap430x
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  IMAGE_SIZE := 14784k
+  SUPPORTED_DEVICES += UAP200
+endef
+TARGET_DEVICES += widelantech_wap430x
+
 define Device/xiaomi_mi-router-ax3000t
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := Mi Router AX3000T
