f22ca3e64f3c019fb43bcbaf7dc335d6da94f083
Sidhant Goel <sidhant@hexcod.in>
2025-10-21 15:24:03 +0000

    qualcommbe: ipq9574: Add support for TP-Link BE800 v1
    - Updated target.mk to include default packages for squashfs, nand, and ramdisk.
    - Introduced a patch to retrieve the board ID from the device tree for ath12k.
    - Intriduced a patch to defer Aquantia PHY firmware loading during probe.
    - Created a new Python script for manipulating tplink2023 images, supporting extraction and creation.
    - Added device tree source for the TP-Link BE800 v1, defining hardware components and configurations.
    - Implemented a hotplug script for ath12k calibration data extraction based on firmware.
    - Added a preinit script to mount factory data for the TP-Link BE800 v1.
    - Introduced a patch to limit QPIC SPI NAND controller to a maximum of 4 bytes ID.
    - Enabled USX Auto-Negotiation for USXGMII in the Aquantia PHY driver.
    - Enabled qca8084 switch support in the target configuration.
    - Added target to build with SFP support (hotplug is not supported yet).
    - Added patches on top of new PCS driver to make it work
    
    Signed-off-by: Sidhant Goel <sidhant@hexcod.in>

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -1,4 +1,5 @@
 DTS_DIR := $(DTS_DIR)/qcom
+DEVICE_VARS += TPLINK_SUPPORT_STRING
 
 define Device/8devices_kiwi-dvk
 	$(call Device/FitImage)
@@ -27,3 +28,50 @@ define Device/qcom_rdp433
 	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += qcom_rdp433
+
+define Device/tplink_archer-be800-common
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := TP-Link
+	DEVICE_MODEL := BE800
+	DEVICE_DTS_CONFIG := config@al02-c4
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	IMAGE_SIZE := 51200k
+	UBINIZE_OPTS := -E 5
+	UBIFS_OPTS := -m 2048 -e 126976 -c 4096
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := 6144k
+	SOC := ipq9574
+	DEVICE_PACKAGES := kmod-ath12k ipq-wifi-tplink_archer-be800 ipq-wifi-firmware-tplink_archer-be800 f2fsck mkf2fs
+	IMAGES += web-ui-factory.bin
+	IMAGE/web-ui-factory.bin := append-ubi | tplink-image-2023
+	TPLINK_SUPPORT_STRING := SupportList:\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:55530000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:43410000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:45550000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:484B0000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:41530000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:41550000}\n$\
+		{product_name:Archer BE800,product_ver:1.0.0,special_id:53470000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:55530000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:45550000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:43410000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:41530000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:41550000}\n$\
+		{product_name:Archer BE800,product_ver:1.20,special_id:53470000}\n
+endef
+
+define Device/tplink_archer-be800
+	$(call Device/tplink_archer-be800-common)
+	DEVICE_VARIANT := v1
+	SUPPORTED_DEVICES += tplink,archer-be800-sfp
+endef
+TARGET_DEVICES += tplink_archer-be800
+
+define Device/tplink_archer-be800-sfp
+	$(call Device/tplink_archer-be800-common)
+	DEVICE_VARIANT := v1-sfp
+	SUPPORTED_DEVICES += tplink,archer-be800
+endef
+TARGET_DEVICES += tplink_archer-be800-sfp
