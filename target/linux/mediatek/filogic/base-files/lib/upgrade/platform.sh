d44132e22fdbc664b5f8e31d2d37fd80bd1d0ab8
Ayra Hikari <ayrahikari@linuxmail.org>
2025-10-30 19:08:50 +0700

    mediatek/filogic: fix Totolink X6000R sysupgrade failed
    Fix Totolink X6000R image failing to upgrade via sysupgrade due to an
    incorrect upgrade case. The fallback upgrade path used the NAND upgrade
    routine, which caused the "layout volume not found" error on NOR flash
    devices. By adding a specific case for this board, sysupgrade now uses
    `default_do_upgrade`, which is compatible with the NOR flash layout.
    
    Fixes: 7cd10ad
    Signed-off-by: Ayra Hikari <ayrahikari@linuxmail.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -154,7 +154,8 @@ platform_do_upgrade() {
 	cudy,re3000-v1|\
 	cudy,wr3000-v1|\
 	yuncore,ax835|\
-	wavlink,wl-wn573hx3)
+	wavlink,wl-wn573hx3|\
+	totolink,x6000r)
 		default_do_upgrade "$1"
 		;;
 	dlink,aquila-pro-ai-m30-a1|\
