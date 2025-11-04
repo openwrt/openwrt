17c723ea611972b57d648dbe6e44fe46188e7d30
dingjie <DW22965391@outlook.com>
2025-11-04 15:59:51 +0800

    mediatek: filogic: add support for Comfast CF-WR630AX
    This commit add support for Comfast CF-WR630AX models.
    
    Hardware
    SOC: MediaTek MT7981B
    RAM: 256MB
    FLASH: 128MB SPI-NAND (Winbond)
    WIFI 2.4G: (Embedded in SOC) b/g/n/ax, MIMO 4x4
    WIFI 5G: (Embedded in SOC) a/n/ac/ax, MIMO 4x4
    ETHERNET: 1x 2.5GbE MT7981 (eth1: WAN)
    ETHERNET: MediaTek MT7531AE 3xGbE (lan1 lan2 lan3)
    UART: 3.3V 115200 8N1
    
    Signed-off-by: dingjie <DW22965391@outlook.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -806,6 +806,30 @@ define Device/comfast_cf-wr633ax
 endef
 TARGET_DEVICES += comfast_cf-wr633ax
 
+define Device/comfast_cf-wr630ax
+  DEVICE_VENDOR := COMFAST
+  DEVICE_MODEL := CF-WR630AX
+  DEVICE_DTS := mt7981b-comfast-cf-wr630ax
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_DTS_LOADADDR := 0x43f00000
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  KERNEL_LOADADDR := 0x44000000
+  KERNEL = kernel-bin | lzma | \
+       fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS = kernel-bin | lzma | \
+       fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES := sysupgrade.bin factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += comfast_cf-wr630ax
+
 define Device/confiabits_mt7981
   DEVICE_VENDOR := Confiabits
   DEVICE_MODEL := MT7981
