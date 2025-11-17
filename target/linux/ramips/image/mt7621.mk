openwrt/openwrt

commit 8c00fd9b4519bf0ef8fb3470a6df421b9f38c03c

ramips: add support for ASUS RT-AX53U
Specifications:
- Device: ASUS RT-AX53U
- SoC: MT7621AT
- Flash: 128MB
- RAM: 256MB
- Switch: 1 WAN, 3 LAN (10/100/1000 Mbps)
- WiFi: MT7905 2x2 2.4G + MT7975 2x2 5G
- Ports: USB 3.0
- LEDs: 1x POWER (blue, configurable)
        3x LAN (blue, configurable)
        1x WAN (blue, configurable)
        1x USB (blue, not configurable)
	1x 2.4G (blue, not configurable)
	1x 5G (blue, not configurable)

Flash by U-Boot TFTP method:
- Configure your PC with IP 192.168.1.2
- Set up TFTP server and put the factory.bin image on your PC
- Connect serial port(rate:115200) and turn on AP, then interrupt "U-Boot Boot Menu" by hitting any key
   Select "2. Upgrade firmware"
   Press enter when show "Run firmware after upgrading? (Y/n):"
   Select 0 for TFTP method
   Input U-Boot's IP address: 192.168.1.1
   Input TFTP server's IP address: 192.168.1.2
   Input IP netmask: 255.255.255.0
   Input file name: openwrt-ramips-mt7621-asus_rt-ax53u-squashfs-factory.bin
- Restart AP aftre see the log "Firmware upgrade completed!"

Signed-off-by: Chuncheng Chen <ccchen1984@gmail.com>
(replaced led label, added key-* prefix to buttons, added note about
BBT)
Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

target/linux/ramips/dts/mt7621_asus_rt-ax53u.dts
@@ -1,0 +1,164 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+
+#include "mt7621.dtsi"
+
+#include <dt-bindings/gpio/gpio.h>
+#include <dt-bindings/input/input.h>
+#include <dt-bindings/leds/common.h>
+
+/ {
+	compatible = "asus,rt-ax53u", "mediatek,mt7621-soc";
+	model = "ASUS RT-AX53U";
+
+	aliases {
+		led-boot = &led_power;
+		led-failsafe = &led_power;
+		led-running = &led_power;
+		led-upgrade = &led_power;
+		label-mac-device = &gmac0;
+	};
+
+	chosen {
+		bootargs = "console=ttyS0,115200";
+		bootargs-override = "console=ttyS0,115200";
+	};
+
+	leds {
+		compatible = "gpio-leds";
+
+		led_power: led-0 {
+			color = <LED_COLOR_ID_BLUE>;
+                        function = LED_FUNCTION_POWER;
+			gpios = <&gpio 13 GPIO_ACTIVE_LOW>;
+		};
+	};
+
+	keys {
+		compatible = "gpio-keys";
+
+		key-restart {
+			label = "reset";
+			gpios = <&gpio 16 GPIO_ACTIVE_LOW>;
+			linux,code = <KEY_RESTART>;
+		};
+
+		key-wps {
+			label = "wps";
+			gpios = <&gpio 15 GPIO_ACTIVE_LOW>;
+			linux,code = <KEY_WPS_BUTTON>;
+		};
+	};
+};
+
+&nand {
+	status = "okay";
+
+	partitions {
+		compatible = "fixed-partitions";
+		#address-cells = <1>;
+		#size-cells = <1>;
+
+		partition@0 {
+			label = "u-boot";
+			reg = <0x0 0xe0000>;
+			read-only;
+		};
+
+		partition@e0000 {
+			label = "u-boot-env";
+			reg = <0xe0000 0x100000>;
+			read-only;
+		};
+
+		factory: partition@1e0000 {
+			label = "factory";
+			reg = <0x1e0000 0x100000>;
+			read-only;
+
+			compatible = "nvmem-cells";
+			#address-cells = <1>;
+			#size-cells = <1>;
+
+			macaddr_factory_4: macaddr@4 {
+				reg = <0x4 0x6>;
+			};
+		};
+
+		factory2: partition@2e0000 {
+			label = "factory2";
+			reg = <0x2e0000 0x100000>;
+			read-only;
+		};
+
+		partition@3e0000 {
+			label = "kernel";
+			reg = <0x3e0000 0x400000>;
+		};
+
+		partition@7e0000 {
+			label = "ubi";
+			reg = <0x7e0000 0x2e00000>;
+		};
+
+		partition@35e0000 {
+			label = "firmware2";
+			reg = <0x35e0000 0x3200000>;
+		};
+
+		/* Last 8M possibly store the bad block table */
+	};
+};
+
+&pcie {
+	status = "okay";
+};
+
+&pcie1 {
+	wifi@0,0 {
+		compatible = "mediatek,mt76";
+		reg = <0x0000 0 0 0 0>;
+		mediatek,mtd-eeprom = <&factory 0x0000>;
+	};
+};
+
+&gmac0 {
+	nvmem-cells = <&macaddr_factory_4>;
+	nvmem-cell-names = "mac-address";
+};
+
+&switch0 {
+	ports {
+		port@0 {
+			status = "okay";
+			label = "wan";
+		};
+
+		port@1 {
+			status = "okay";
+			label = "lan1";
+		};
+
+		port@2 {
+			status = "okay";
+			label = "lan2";
+		};
+
+		port@3 {
+			status = "okay";
+			label = "lan3";
+		};
+
+		port@4 {
+			status = "okay";
+			label = "lan4";
+		};
+	};
+};
+
+&state_default {
+	gpio {
+		groups = "i2c", "uart2", "uart3", "jtag", "wdt";
+		function = "gpio";
+	};
+};
+

target/linux/ramips/image/mt7621.mk
@@ -232,6 +232,23 @@ define Device/asus_rt-n56u-b1
 endef
 TARGET_DEVICES += asus_rt-n56u-b1
 
+define Device/asus_rt-ax53u
+  $(Device/dsa-migration)
+  DEVICE_VENDOR := ASUS
+  DEVICE_MODEL := RT-AX53U
+  IMAGE_SIZE := 51200k
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 4096k
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
+	check-size
+  DEVICE_PACKAGES := kmod-mt7915e kmod-usb3 uboot-envtools
+endef
+TARGET_DEVICES += asus_rt-ax53u
+
 define Device/beeline_smartbox-flash
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)

target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -53,6 +53,7 @@ platform_do_upgrade() {
 	ampedwireless,ally-r1900k|\
 	asus,rt-ac65p|\
 	asus,rt-ac85p|\
+	asus,rt-ax53u|\
 	beeline,smartbox-flash|\
 	dlink,dir-1960-a1|\
 	dlink,dir-2640-a1|\

commit 74394d5c5411c57282757432be49006e3cb0cb35

ramips: use whole flash on Asus RT-AX53U
The Asus RT-AX53U router has a 128MB flash, but currently only around
62MB of it are used with OpenWRT. The rest consists of an alternative
Asus firmware image (`firmware2`) and Asus firmware configuration
(`jffs2`). These two partitions are useless with OpenWRT (even for
restoring the original Asus firmware), so remove them and increase the
size of the `firmware` and `ubi` partitions accordingly. This gives
more space for the user.

Remove an entry from the NMBM remap list to avoid conflicts with UBI.

There are two good ways to flash the modified firmware:

1. TFTP the image to the router. This does not require opening the case
   and can even be done easily with the ASUS Firmware Restoration
   utility (available on the ASUS website):

   1. Connect LAN1 on the router with an Ethernet cable.
   2. Set computer's static IP to 192.168.1.10.
   3. Remove power; while pressing the RESET button apply power to the
      router.
   4. TFTP (as client) the initramfs-kernel image to the router on
      192.168.1.1.
   5. Wait a while, use the squashfs-sysupgrade image to sysupgrade in
      the usual way.

   Sysupgrade in the last step will format the UBI partition anew and
   remove any old signatures.

2. Do:

       mtd erase firmware2
       mtd erase jffs2
       sysupgrade --force openwrt-ramips-mt7621-asus_rt-ax53u-squashfs-sysupgrade.bin
       <reboot>
       # if preserving configuration with sysupgrade
       uci set system.@system[0].compat_version='2.0'
       uci commit
       # once again:
       sysupgrade openwrt-ramips-mt7621-asus_rt-ax53u-squashfs-sysupgrade.bin

   Erasing `firmware2` and `jffs2` is necessary to remove any old UBI
   signatures (there would be some remnants if the user had flashed a
   patched firmware and then downgraded). These old UBI signatures would
   cause UBI attach failures - likely with a message like:

       bad image sequence number %d in PEB %d, expected %d

   After the first sysupgrade UBI will automatically expand to the new
   space, but the UBIFS for `rootfs_data` will not automatically claim
   the newly available blocks. This space will be claimed during the
   second sysupgrade, when `rootfs_data` is removed and created again.

   This is a similar approach to
commit 983a753f389e ("ath79: linksys_ea4500-v3: take over the space for user data")
Link: https://github.com/openwrt/openwrt/pull/14791

   The second upgrade method allows keeping the OpenWRT configuration.

Because of the need to erase the partitions manually with step 2 (in
some cases), increase DEVICE_COMPAT_VERSION to 2.0.

I have been running OpenWRT with modifications similar to this patch
since 23.05 and have done numerous trials while testing it.

Previous PRs:
Link: https://github.com/openwrt/openwrt/pull/10502
Link: https://github.com/openwrt/openwrt/pull/10685
Link: https://github.com/openwrt/openwrt/pull/12741

Fixes: 8c00fd9b4519 ("ramips: add support for ASUS RT-AX53U")

target/linux/ramips/dts/mt7621_asus_rt-ax53u.dts
@@ -63,8 +63,7 @@
 
 	mediatek,nmbm;
 	mediatek,bmt-remap-range =
-		<0x000000 0x7e0000>,
-		<0x35e0000 0x7800000>;
+		<0x000000 0x7e0000>;
 
 	partitions {
 		compatible = "fixed-partitions";
@@ -124,7 +123,7 @@
 
 		partition@3e0000 {
 			label = "firmware";
-			reg = <0x3e0000 0x3200000>;
+			reg = <0x3e0000 0x7420000>;
 
 			compatible = "fixed-partitions";
 			#address-cells = <1>;
@@ -137,18 +136,8 @@
 
 			partition@400000 {
 				label = "ubi";
-				reg = <0x400000 0x2e00000>;
+				reg = <0x400000 0x7020000>;
 			};
-		};
-
-		partition@35e0000 {
-			label = "firmware2";
-			reg = <0x35e0000 0x3200000>;
-		};
-
-		partition@67e0000 {
-			label = "jffs2";
-			reg = <0x67e0000 0x1020000>;
 		};
 
 		/* Last 8M are reserved for NMBM management (bad blocks) */

target/linux/ramips/image/mt7621.mk
@@ -474,6 +474,10 @@ define Device/asus_rt-ax53u
   DEVICE_MODEL := RT-AX53U
   DEVICE_ALT0_VENDOR := ASUS
   DEVICE_ALT0_MODEL := RT-AX1800U
+  DEVICE_COMPAT_VERSION := 2.0
+  DEVICE_COMPAT_MESSAGE := Partition table has been changed. \
+	Follow instructions in the OpenWRT Wiki to migrate to the new layout: \
+	https://openwrt.org/toh/asus/rt-ax53u
   IMAGE_SIZE := 51200k
   IMAGES += factory.bin
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \

target/linux/ramips/mt7621/base-files/etc/board.d/05_compat-version
@@ -8,6 +8,7 @@
 board_config_update
 
 case "$(board_name)" in
+	asus,rt-ax53u|\
 	ubnt,edgerouter-x|\
 	ubnt,edgerouter-x-sfp|\
 	iptime,ax2004m)