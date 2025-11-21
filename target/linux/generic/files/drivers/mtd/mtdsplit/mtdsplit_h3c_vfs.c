openwrt/openwrt

commit 2acf18bf4deb7d431e4ef3858ae8969a481fe196

kernel: mtdsplit_h3c_vfs: use -ENOENT instead of -ENODEV
New linux version will check the return code of parser on subpartitions.
The only valid case for skipping a parser with an error is -ENOENT.

Change the relevant entry to -ENOENT.

Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_h3c_vfs.c
@@ -99,7 +99,9 @@ static int mtdsplit_h3c_vfs_parse(struct mtd_info *mtd,
 		return -EIO;
 
 	if (format_flag != FORMAT_FLAG)
-		return -EINVAL;
+		pr_debug("mtdsplit_h3c_vfs: unexpected format flag %08x\n",
+			 format_flag);
+		return -ENOENT;
 
 	/* Check file entry */
 	err = mtd_read(mtd, FILE_ENTRY_OFFSET, sizeof(file_entry), &retlen,
@@ -111,19 +113,19 @@ static int mtdsplit_h3c_vfs_parse(struct mtd_info *mtd,
 		return -EIO;
 
 	if (file_entry.flags != FILE_ENTRY_FLAGS)
-		return -EINVAL;
+		return -ENOENT;
 
 	if (file_entry.parent_block != FILE_ENTRY_PARENT_BLOCK)
-		return -EINVAL;
+		return -ENOENT;
 
 	if (file_entry.parent_index != FILE_ENTRY_PARENT_INDEX)
-		return -EINVAL;
+		return -ENOENT;
 
 	if (file_entry.data_block != FILE_ENTRY_DATA_BLOCK)
-		return -EINVAL;
+		return -ENOENT;
 
 	if (strncmp(file_entry.name, FILE_ENTRY_NAME, sizeof(file_entry.name)) != 0)
-		return -EINVAL;
+		return -ENOENT;
 
 	/* Find rootfs offset */
 	kernel_size = block_offset(file_entry.data_block +

commit 4f8f0bfeeff48cddd64d87093f7702d1e308adbf

kernel: mtdsplit_h3c_vfs: Fix build
Fix a recently introduces compile problem.

Fixes: 2acf18bf4deb ("kernel: mtdsplit_h3c_vfs: use -ENOENT instead of -ENODEV")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_h3c_vfs.c
@@ -98,10 +98,11 @@ static int mtdsplit_h3c_vfs_parse(struct mtd_info *mtd,
 	if (retlen != sizeof(format_flag))
 		return -EIO;
 
-	if (format_flag != FORMAT_FLAG)
+	if (format_flag != FORMAT_FLAG) {
 		pr_debug("mtdsplit_h3c_vfs: unexpected format flag %08x\n",
 			 format_flag);
 		return -ENOENT;
+	}
 
 	/* Check file entry */
 	err = mtd_read(mtd, FILE_ENTRY_OFFSET, sizeof(file_entry), &retlen,