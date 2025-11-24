5629e06cf211cffb19f370b5b098f3d46e1e80bb
Til Kaiser <mail@tk154.de>
2025-11-24 15:50:33 +0100

    ramips: mt76x8: add Teltonika RUT976 support
    This commit adds support for the Teltonika RUT976, part
    of the RUTE series, which is very similar to the RUT956.
    The RUT976 now has a 5G modem and the flash size has doubled.
    
    Specification:
    - MediaTek MT7628AN SoC
    - 128 MB RAM
    - 32MB Flash
    - 4x 10/100 Mbps Ethernet, with passive PoE support on LAN1
    - MediaTek MT7628AN 2.4 GHz 802.11n WiFi
    - Quectel RG255C 5G modem
    - GNSS
    - RS232, DB9 connector, Cypress ACM via USB, /dev/ttyACM0
    - RS485 /dev/ttyS1
    - microSD card slot
    - 2.0 USB Type-A HOST port
    - analog 0-24V input (MCP3221)
    - Relay
    - 2x Digital input
    - 2x Digital output
    - 2x SIM slot (can be swapped via GPIO)
    - eSIM (depends on hardware configuration, can be swapped via GPIO)
    
    GPIO:
     - 1 button (Reset)
     - 7 LEDs (power, status green/red, RSSI 1,2,3,4,5)
     - 5 Modem control (power button, reset, status, SIM select, eSIM select)
     - 2 Digital input
     - 2 Digital output
     - 1 Isolated input
     - 1 RS485 tx enable
     - 1 RS485 rx enable
     - 1 Relay
     - 1 Data Carrier Detect (DCD)
    
    Flashing via OEM WebUI:
    1. Download the firmware image *-squashfs-factory.bin
    2. Upload firmware image via OEM WebUI firmware update, do not keep settings
    
    To revert back to OEM firmware:
    https://wiki.teltonika-networks.com/view/Bootloader_menu
    
    Mobile data connection:
    Create proto qmi interface.
    
    Signed-off-by: Til Kaiser <mail@tk154.de>
    Link: https://github.com/openwrt/openwrt/pull/20933
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -752,6 +752,23 @@ define Device/teltonika_rut9x6
 endef
 TARGET_DEVICES += teltonika_rut9x6
 
+define Device/teltonika_rut976
+  DEVICE_VENDOR := Teltonika
+  DEVICE_MODEL := RUT976
+  SUPPORTED_TELTONIKA_DEVICES := teltonika,rut976
+  SUPPORTED_TELTONIKA_HW_MODS := 2c7c_6005 TLA2021 CH343 esim ala440
+  IMAGE_SIZE := 31552k
+  BLOCKSIZE := 64k
+  DEVICE_PACKAGES := uqmi kmod-mt76x2 kmod-usb2 kmod-usb-ohci \
+	kmod-usb-serial-option kmod-spi-gpio kmod-gpio-nxp-74hc164 kmod-i2c-mt7628 \
+	kmod-hwmon-mcp3021 kmod-scsi-core kmod-usb-storage kmod-usb-acm kmod-usb-net-cdc-ether
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs | check-size | append-teltonika-metadata
+  IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | check-size | append-metadata
+endef
+TARGET_DEVICES += teltonika_rut976
+
 define Device/totolink_a3
   IMAGE_SIZE := 7936k
   UIMAGE_NAME := za3
