openwrt/openwrt

commit 87fa17a91e9a3300321833a53add8fd20258bbcb

odhcp6c: change dhcpv6.sh config option "ifaceid" to "ip6ifaceid"
With this change, the interface identifier can be configured via LuCI.

Signed-off-by: Peter Meiser <peter.meiser@gmx.com>
Link: https://github.com/openwrt/openwrt/pull/19318
Signed-off-by: Robert Marko <robimarko@gmail.com>

package/network/ipv6/odhcp6c/files/dhcpv6.sh
@@ -30,7 +30,7 @@ proto_dhcpv6_init_config() {
 	proto_config_add_string iface_464xlat
 	proto_config_add_string zone_464xlat
 	proto_config_add_string zone
-	proto_config_add_string 'ifaceid:ip6addr'
+	proto_config_add_string 'ip6ifaceid:ip6addr'
 	proto_config_add_string "userclass"
 	proto_config_add_string "vendorclass"
 	proto_config_add_array "sendopts:list(string)"
@@ -56,8 +56,8 @@ proto_dhcpv6_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease noserverunicast noclientfqdn noacceptreconfig ip6prefix ip6prefixes iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass sendopts delegate zone_dslite zone_map zone_464xlat zone encaplimit_dslite encaplimit_map skpriority soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff verbose
-	json_get_vars reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease noserverunicast noclientfqdn noacceptreconfig iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone encaplimit_dslite encaplimit_map skpriority soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff verbose
+	local reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease noserverunicast noclientfqdn noacceptreconfig ip6prefix ip6prefixes iface_dslite iface_map iface_464xlat ip6ifaceid userclass vendorclass sendopts delegate zone_dslite zone_map zone_464xlat zone encaplimit_dslite encaplimit_map skpriority soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff verbose
+	json_get_vars reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease noserverunicast noclientfqdn noacceptreconfig iface_dslite iface_map iface_464xlat ip6ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone encaplimit_dslite encaplimit_map skpriority soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff verbose
 	json_for_each_item proto_dhcpv6_add_prefix ip6prefix ip6prefixes
 
 	# Configure
@@ -84,7 +84,8 @@ proto_dhcpv6_setup() {
 
 	[ "$noacceptreconfig" = "1" ] && append opts "-a"
 
-	[ -n "$ifaceid" ] && append opts "-i$ifaceid"
+	[ -z "$ip6ifaceid" ] && json_get_var ip6ifaceid ifaceid
+	[ -n "$ip6ifaceid" ] && append opts "-i$ip6ifaceid"
 
 	[ -n "$vendorclass" ] && append opts "-V$vendorclass"
 

commit 3ac20837f3eaa499aca6a70e125496d8a741c707

odhcp6c: fix ip6ifaceid parameter
Commit 87fa17a91e9a started passing ip6ifaceid as a parameter to odhcpd6c,
but ip6ifaceid can also contain special values such as "eui64" and "random",
which aren't supported since odhcp6c expects an IPv6 address.

Fixes: 87fa17a91e9a ("odhcp6c: change dhcpv6.sh config option "ifaceid" to "ip6ifaceid"")
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

package/network/ipv6/odhcp6c/Makefile
@@ -8,7 +8,7 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=odhcp6c
-PKG_RELEASE:=1
+PKG_RELEASE:=2
 
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_URL=$(PROJECT_GIT)/project/odhcp6c.git

package/network/ipv6/odhcp6c/files/dhcpv6.sh
@@ -46,6 +46,24 @@ proto_dhcpv6_init_config() {
 	proto_config_add_boolean dynamic
 }
 
+proto_dhcpv6_add_ifaceid() {
+	local ifaceid="$1"
+	local ip6addr
+
+	if [ "$ifaceid" = "random" ]; then
+		local loop=1
+
+		while [ $loop -eq 1 ]; do
+			ip6addr=$(hexdump -vn 8 -e '"::" 3/2 "%x:" 1/2 "%x"' /dev/urandom)
+			( echo $ip6addr | grep -qE "::[0-9a-f]{0,4}:[0-9a-f]{0,2}ff:fe[0-9a-f]{2}:[0-9a-f]{0,4}" ) || loop=0
+		done
+	elif [ "$ifaceid" != "eui64" ]; then
+		ip6addr="$ifaceid"
+	fi
+
+	[ -n "$ip6addr" ] && append opts "-i$ip6addr"
+}
+
 proto_dhcpv6_add_prefix() {
 	append "$3" "$1"
 }
@@ -104,7 +122,7 @@ proto_dhcpv6_setup() {
 	[ "$noacceptreconfig" = "1" ] && append opts "-a"
 
 	[ -z "$ip6ifaceid" ] && json_get_var ip6ifaceid ifaceid
-	[ -n "$ip6ifaceid" ] && append opts "-i$ip6ifaceid"
+	[ -n "$ip6ifaceid" ] && proto_dhcpv6_add_ifaceid "$ip6ifaceid"
 
 	[ -n "$vendorclass" ] && append opts "-V$vendorclass"
 