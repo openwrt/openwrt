openwrt/openwrt

commit 8d30e07180367cdeb4affd79adead6e1025355c9

mediatek: filogic: fix for new GL.iNet GL-MT2500/GL-MT2500A hardware revision
GL.iNet shipped a hardware change of the WAN PHY going from the MaxLinear
GPY211C to the Airoha EN8811H.

Signed-off-by: Matthew Bilker <me@mbilker.us>
Link: https://github.com/openwrt/openwrt/pull/18799
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500-v1.dts
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+#include "mt7981b-glinet-gl-mt2500.dtsi"
+
+&gmac0 {
+	phy-handle = <&phy5>;
+};
+
+&mdio_bus {
+	phy5: ethernet-phy@5 {
+		reg = <5>;
+		compatible = "ethernet-phy-ieee802.3-c45";
+	};
+};

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500-v2.dts
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+#include "mt7981b-glinet-gl-mt2500.dtsi"
+
+&gmac0 {
+	phy-handle = <&phy13>;
+};
+
+&mdio_bus {
+	phy13: ethernet-phy@13 {
+		reg = <13>;
+		compatible = "ethernet-phy-ieee802.3-c45";
+	};
+};

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500.dtsi
@@ -108,7 +108,6 @@
 		reg = <0>;
 
 		phy-mode = "2500base-x";
-		phy-handle = <&phy5>;
 	};
 
 	gmac1: mac@1 {
@@ -125,11 +124,6 @@
 	reset-gpios = <&pio 14 GPIO_ACTIVE_LOW>;
 	reset-delay-us = <600>;
 	reset-post-delay-us = <20000>;
-
-	phy5: ethernet-phy@5 {
-		reg = <5>;
-		compatible = "ethernet-phy-ieee802.3-c45";
-	};
 };
 
 &usb_phy {

target/linux/mediatek/image/filogic.mk
@@ -927,15 +927,30 @@ TARGET_DEVICES += gatonetworks_gdsp
 define Device/glinet_gl-mt2500
   DEVICE_VENDOR := GL.iNet
   DEVICE_MODEL := GL-MT2500
-  DEVICE_DTS := mt7981b-glinet-gl-mt2500
+  DEVICE_VARIANT := MaxLinear PHY
+  DEVICE_DTS := mt7981b-glinet-gl-mt2500-v1
   DEVICE_DTS_DIR := ../dts
   DEVICE_DTS_LOADADDR := 0x47000000
   DEVICE_PACKAGES := -wpad-basic-mbedtls e2fsprogs f2fsck mkf2fs kmod-usb3
-  SUPPORTED_DEVICES += glinet,mt2500-emmc
+  SUPPORTED_DEVICES += glinet,mt2500-emmc glinet,gl-mt2500-airoha
   IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-gl-metadata
 endef
 TARGET_DEVICES += glinet_gl-mt2500
+
+define Device/glinet_gl-mt2500-airoha
+  DEVICE_VENDOR := GL.iNet
+  DEVICE_MODEL := GL-MT2500
+  DEVICE_VARIANT := Airoha PHY
+  DEVICE_DTS := mt7981b-glinet-gl-mt2500-v2
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS_LOADADDR := 0x47000000
+  DEVICE_PACKAGES := -wpad-basic-mbedtls e2fsprogs f2fsck mkf2fs kmod-usb3 kmod-phy-airoha-en8811h airoha-en8811h-firmware
+  SUPPORTED_DEVICES += glinet,mt2500-emmc glinet,gl-mt2500
+  IMAGES := sysupgrade.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-gl-metadata
+endef
+TARGET_DEVICES += glinet_gl-mt2500-airoha
 
 define Device/glinet_gl-mt3000
   DEVICE_VENDOR := GL.iNet

commit 215fa5c80b237daeccf0d08c0a679e44dfca57ec

mediatek: filogic: gl-mt2500 fix compatibles
Alternative fix for [1] if we want to mantain the two profiles to be
compatible with each other.[2]
They shared the same "compatible" in device tree causing some
incompatibilities (sysupgrades, ASU profile identification), assign a
unique "compatible" and "model" to each variant.

A related fix for allow sysupgrade between the two profiles is also
included in this series PR.

[1]: https://github.com/openwrt/openwrt/pull/20632
[2]: https://github.com/openwrt/openwrt/pull/18799#discussion_r2115720739
Fixes: 8d30e07 ("mediatek: filogic: fix for new GL.iNet GL-MT2500/GL-MT2500A hardware revision")
Fixes: https://github.com/openwrt/openwrt/issues/20566
Fixes(partially): https://github.com/openwrt/asu/issues/1525

Signed-off-by: Mario Andrés Pérez <mapb_@outlook.com>

package/boot/uboot-tools/uboot-envtools/files/mediatek_filogic
@@ -54,6 +54,7 @@ acer,predator-w6|\
 acer,predator-w6d|\
 acer,vero-w6m|\
 glinet,gl-mt2500|\
+glinet,gl-mt2500-airoha|\
 glinet,gl-mt6000|\
 glinet,gl-x3000|\
 glinet,gl-xe3000|\

target/linux/mediatek/base-files/lib/preinit/05_set_preinit_iface
@@ -5,6 +5,7 @@ set_preinit_iface() {
 	cudy,tr3000-v1|\
 	cudy,tr3000-v1-ubootmod|\
 	glinet,gl-mt2500|\
+	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt3000|\
 	openembed,som7981|\
 	wavlink,wl-wn573hx3)

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500-v1.dts
@@ -3,6 +3,11 @@
 /dts-v1/;
 #include "mt7981b-glinet-gl-mt2500.dtsi"
 
+/ {
+	model = "GL.iNet GL-MT2500 v1 Maxlinear PHY";
+	compatible = "glinet,gl-mt2500", "mediatek,mt7981";
+};
+
 &gmac0 {
 	phy-handle = <&phy5>;
 };

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500-v2.dts
@@ -3,6 +3,11 @@
 /dts-v1/;
 #include "mt7981b-glinet-gl-mt2500.dtsi"
 
+/ {
+	model = "GL.iNet GL-MT2500 v2 Airoha PHY";
+	compatible = "glinet,gl-mt2500-airoha", "mediatek,mt7981";
+};
+
 &gmac0 {
 	phy-handle = <&phy13>;
 };

target/linux/mediatek/dts/mt7981b-glinet-gl-mt2500.dtsi
@@ -5,9 +5,6 @@
 #include <dt-bindings/pinctrl/mt65xx.h>
 
 / {
-	model = "GL.iNet GL-MT2500";
-	compatible = "glinet,gl-mt2500", "mediatek,mt7981";
-
 	aliases {
 		label-mac-device = &gmac0;
 		led-boot = &led_sys_white;

target/linux/mediatek/filogic/base-files/etc/board.d/02_network
@@ -120,6 +120,7 @@ mediatek_setup_interfaces()
 	cudy,tr3000-v1|\
 	cudy,tr3000-v1-ubootmod|\
 	glinet,gl-mt2500|\
+	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt3000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\

target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -123,6 +123,7 @@ platform_do_upgrade() {
 	acer,vero-w6m|\
 	arcadyan,mozart|\
 	glinet,gl-mt2500|\
+	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\
@@ -340,6 +341,7 @@ platform_copy_config() {
 	acer,vero-w6m|\
 	arcadyan,mozart|\
 	glinet,gl-mt2500|\
+	glinet,gl-mt2500-airoha|\
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\