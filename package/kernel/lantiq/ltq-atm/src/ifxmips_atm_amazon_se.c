openwrt/openwrt

commit 5b263f3360bfc1c74a17d3ce1710d5c528084156

treewide: add const to static arrays
Allows more compiler optimization.

Signed-off-by: Rosen Penev <rosenp@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/19932
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/mvebu/patches-6.12/902-drivers-mfd-Add-a-driver-for-IEI-WT61P803-PUZZLE-MCU.patch
@@ -820,7 +820,7 @@ Cc: Robert Marko <robert.marko@sartura.hr>
 +static IEI_WT61P803_PUZZLE_ATTR_RW(power_loss_recovery, IEI_WT61P803_PUZZLE_POWER_LOSS_RECOVERY, 0);
 +static IEI_WT61P803_PUZZLE_ATTR_RO(power_status, IEI_WT61P803_PUZZLE_POWER_STATUS, 0);
 +
-+static struct attribute *iei_wt61p803_puzzle_attrs[] = {
++static const struct attribute *iei_wt61p803_puzzle_attrs[] = {
 +	&dev_attr_version.dev_attr.attr,
 +	&dev_attr_build_info.dev_attr.attr,
 +	&dev_attr_bootloader_mode.dev_attr.attr,

package/kernel/button-hotplug/src/button-hotplug.c
@@ -72,7 +72,7 @@ extern u64 uevent_next_seqnum(void);
 		.name = (_name),	\
 	}
 
-static struct bh_map button_map[] = {
+static const struct bh_map button_map[] = {
 	BH_MAP(BTN_0,		"BTN_0"),
 	BH_MAP(BTN_1,		"BTN_1"),
 	BH_MAP(BTN_2,		"BTN_2"),

package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -72,7 +72,7 @@ extern u64 uevent_next_seqnum(void);
 		.name = (_name),	\
 	}
 
-static struct bh_map button_map[] = {
+static const struct bh_map button_map[] = {
 	BH_MAP(BTN_0,			"BTN_0"),
 	BH_MAP(BTN_1,			"BTN_1"),
 	BH_MAP(BTN_2,			"BTN_2"),

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_fw_amazon_se.h
@@ -34,7 +34,7 @@
 #define ATM_FW_VER_MINOR        16
 
 
-static unsigned int firmware_binary_code[] = {
+static const unsigned int firmware_binary_code[] = {
  0x800004b8, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
  0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x8000ffe0, 0x00000000, 0x00000000, 0x00000000,
  0xc1000002, 0xd90c00f8, 0xc2000002, 0xda0800f9, 0x80004cc8, 0xc2000000, 0xda0800f9, 0x80004330,
@@ -450,7 +450,7 @@ static unsigned int firmware_binary_code[] = {
  0x9d000000, 0xcd4000f8, 0x00000000, 0x00000000, 0x9d000000, 0x4158a000, 0xcd4000f8, 0x00000000,
 };
 
-static unsigned int firmware_binary_data[] = {
+static const unsigned int firmware_binary_data[] = {
 };
 
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_fw_ar9.h
@@ -32,7 +32,7 @@
 #define ATM_FW_VER_MINOR        16
 
 
-static unsigned int ar9_fw_bin[] = {
+static const unsigned int ar9_fw_bin[] = {
  0x800004b8, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
  0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x8000ffe0, 0x00000000, 0x00000000, 0x00000000,
  0xc1000002, 0xd90c00f8, 0xc2000002, 0xda0800f9, 0x80004980, 0xc2000000, 0xda0800f9, 0x80003fe8,
@@ -432,7 +432,7 @@ static unsigned int ar9_fw_bin[] = {
  0xcd4000f8, 0x00000000,
 };
 
-static unsigned int ar9_fw_data[] = {
+static const unsigned int ar9_fw_data[] = {
 };
 
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_fw_ar9_retx.h
@@ -32,7 +32,7 @@
 #define ATM_FW_VER_MINOR        15
 
 
-static unsigned int firmware_binary_code[] = {
+static const unsigned int firmware_binary_code[] = {
     0x800004B8, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
     0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x8000FFE0, 0x00000000, 0x00000000, 0x00000000,
     0xC1000002, 0xD90C00F8, 0xC2000002, 0xDA0800F9, 0xC0001B50, 0x8C100000, 0x00000000, 0x00000000,
@@ -604,7 +604,7 @@ static unsigned int firmware_binary_code[] = {
     0xCDC000F8, 0xC1C00000, 0xC0004734, 0x9CC00000, 0xCDC000F8, 0xC0004732, 0xCD8000F8,
 };
 
-static unsigned int firmware_binary_data[] = {
+static const unsigned int firmware_binary_data[] = {
 };
 
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_fw_danube.h
@@ -35,7 +35,7 @@
 //  add multiple queue per PVC feature
 
 
-static unsigned int danube_fw_bin[] = {
+static const unsigned int danube_fw_bin[] = {
     0x800004A0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
     0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x8000FFC8, 0x00000000, 0x00000000, 0x00000000,
     0xC1000002, 0xD90C0000, 0xC2000002, 0xDA080001, 0x80004968, 0xC2000000, 0xDA080001, 0x80003FD0,
@@ -435,7 +435,7 @@ static unsigned int danube_fw_bin[] = {
     0xCD400000, 0x00000000,
 };
 
-static unsigned int danube_fw_data[] = {
+static const unsigned int danube_fw_data[] = {
 };
 
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_fw_danube_retx.h
@@ -33,7 +33,7 @@
 #define ATM_FW_VER_MINOR        15
 
 
-static unsigned int firmware_binary_code[] = {
+static const unsigned int firmware_binary_code[] = {
     0x800004A0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
     0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x8000FFC8, 0x00000000, 0x00000000, 0x00000000,
     0xC1000002, 0xD90C0000, 0xC2000002, 0xDA080001, 0xC0001B50, 0x8C100000, 0x00000000, 0x00000000,
@@ -605,7 +605,7 @@ static unsigned int firmware_binary_code[] = {
     0xCDC00000, 0xC1C00000, 0xC0004734, 0x9CC00000, 0xCDC00000, 0xC0004732, 0xCD800000,
 };
 
-static unsigned int firmware_binary_data[] = {
+static const unsigned int firmware_binary_data[] = {
 };
 
 

commit cd9229d06126e40226c01a7f354072bdf777f697

treewide: Fix const compile errors
Fixes compile errors caused by static const arrays;

The lantiq firmware is const now.

ATTRIBUTE_GROUPS() does not take const.

Fixes: 5b263f3360b ("treewide: add const to static arrays")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_amazon_se.c
@@ -220,7 +220,7 @@ static inline void clear_share_buffer(void)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     volatile u32 *dest;
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_ar9.c
@@ -159,7 +159,7 @@ static inline void clear_share_buffer(void)
         IFX_REG_W32(0, p++);
 }
 
-static inline int pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     volatile u32 *dest;
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_danube.c
@@ -106,7 +106,7 @@ static inline void reset_ppe(struct platform_device *pdev)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int danube_pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int danube_pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
 	volatile u32 *dest;
 

package/kernel/lantiq/ltq-atm/src/ifxmips_atm_vr9.c
@@ -91,7 +91,7 @@ static inline int vr9_reset_ppe(struct platform_device *pdev)
 	return 0;
 }
 
-static inline int vr9_pp32_download_code(int pp32, u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int vr9_pp32_download_code(int pp32, const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
 	unsigned int clr, set;
 	volatile u32 *dest;

package/kernel/lantiq/ltq-ptm/src/ifxmips_ptm_amazon_se.c
@@ -219,7 +219,7 @@ static inline void clear_share_buffer(void)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     volatile u32 *dest;
 

package/kernel/lantiq/ltq-ptm/src/ifxmips_ptm_ar9.c
@@ -242,7 +242,7 @@ static inline void clear_share_buffer(void)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     volatile u32 *dest;
 

package/kernel/lantiq/ltq-ptm/src/ifxmips_ptm_danube.c
@@ -214,7 +214,7 @@ static inline void clear_share_buffer(void)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int pp32_download_code(u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     volatile u32 *dest;
 

package/kernel/lantiq/ltq-ptm/src/ifxmips_ptm_vr9.c
@@ -202,7 +202,7 @@ static inline void clear_share_buffer(void)
  *    int       --- 0:    Success
  *                  else:           Error Code
  */
-static inline int pp32_download_code(int pp32, u32 *code_src, unsigned int code_dword_len, u32 *data_src, unsigned int data_dword_len)
+static inline int pp32_download_code(int pp32, const u32 *code_src, unsigned int code_dword_len, const u32 *data_src, unsigned int data_dword_len)
 {
     unsigned int clr, set;
     volatile u32 *dest;

target/linux/mvebu/patches-6.12/902-drivers-mfd-Add-a-driver-for-IEI-WT61P803-PUZZLE-MCU.patch
@@ -820,7 +820,7 @@ Cc: Robert Marko <robert.marko@sartura.hr>
 +static IEI_WT61P803_PUZZLE_ATTR_RW(power_loss_recovery, IEI_WT61P803_PUZZLE_POWER_LOSS_RECOVERY, 0);
 +static IEI_WT61P803_PUZZLE_ATTR_RO(power_status, IEI_WT61P803_PUZZLE_POWER_STATUS, 0);
 +
-+static const struct attribute *iei_wt61p803_puzzle_attrs[] = {
++static struct attribute *iei_wt61p803_puzzle_attrs[] = {
 +	&dev_attr_version.dev_attr.attr,
 +	&dev_attr_build_info.dev_attr.attr,
 +	&dev_attr_bootloader_mode.dev_attr.attr,