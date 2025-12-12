72fa75243a238f95f710daea7810ddd3b0525c0c
Fil Dunsky <filipp.dunsky@gmail.com>
2025-12-12 16:16:50 +0300

    mediatek: add support for Cudy WBR3000UAX v1
    mediatek: add support for Cudy WBR3000UAX v1
    
    Another OEM variation of a Cudy WR3000 series device made for Russian market.
    
    Hardware:
     - SoC: MediaTek MT7981B
     - CPU: 2x 1.3 GHz Cortex-A53
     - Flash: 128 MiB GigaDevice SPI NAND. Flash Model: F50L1G41LB, ID: C801
     - RAM: DDR3, 512 MiB
     - WLAN: 2.4 GHz, 5 GHz (MediaTek MT7976CN, 802.11ax)
     - 1x WAN MT7531 (JXD 2531Z) 10/100/1000 Mbps
     - 4x LAN 2x MT7530 (JXD 2529S) 10/100/1000 Mbps
     - USB 3.0 port
     - Buttons: Reset, WPS
     - 8x LEDs: 2x Red, 6x Blue
     - Serial console: no need to solder, just open the case and unskrew the radiator, TX-RX, RX-TX, GND-GND, VCC do not connect, 115200 8n1
     - Power: 12 VDC, 1.5 A
    
    +---------+-------------------+-----------+
    |         | MAC               | Algorithm |
    +---------+-------------------+-----------+
    | WAN     | 80:AF:CA:xx:xx:x1 | label+1   |
    | LAN     | 80:AF:CA:xx:xx:x0 | label     |
    | WLAN 2g | 80:AF:CA:xx:xx:x0 | label     |
    | WLAN 5g | 82:AF:CA:xx:xx:x1 | label+1   |
    +---------+-------------------+-----------+
    
    Based on a factory layout with only 64mb partition for easier transition from factory to OpenWrt for users if the "intermediate" RSA signed firmware will be provided by Cudy.
    
    **Installation**
    The installation must be done via UART & TFTP by disassembling the router. On other occasions Cudy has distributed intermediate firmware and dts to make installation easier, but since this router is OEM special WB order for local RU market there is a possibility they will not provide it.
    
    **Install using UART & TFTP**
    1. Connect to UART.
    2. Since the factory BL is locked and the boot process can not be stopped, you have to use mtkuartboot, hold reset, engage the power, boot into your payloaded bl2 & fip.
    3. Connect to LAN and set your IP to 192.168.1.254.
    4. Configure a TFTP server to serve openwrt-mediatek-filogic-cudy_wbr3000uax-v1-initramfs-kernel.bin file.
    5. Run these steps in u-boot using the name of your file:
    
    ```
    setenv bootfile openwrt-mediatek-filogic-cudy_wbr3000uax-v1-initramfs-kernel.bin
    setenv ipaddr 192.168.1.1
    setenv serverip 192.168.1.254
    tftpboot
    bootm
    ```
    
    6. Router will boot into OpenWrt initramfs recovery, just open your browser `192.168.1.1` and sysupgrade with the `Keep settings` option turned off.
    
    Tested-by: many 4pda users
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1231,6 +1231,23 @@ define Device/cudy_wr3000p-v1
 endef
 TARGET_DEVICES += cudy_wr3000p-v1
 
+define Device/cudy_wbr3000uax-v1
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := WBR3000UAX
+  DEVICE_VARIANT := v1
+  DEVICE_DTS := mt7981b-cudy-wbr3000uax-v1
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += R120
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+endef
+TARGET_DEVICES += cudy_wbr3000uax-v1
+
 define Device/dlink_aquila-pro-ai-m30-a1
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := AQUILA PRO AI M30
