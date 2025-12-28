b8fe7b5c6582d4fad4214e0f2e0b847dceb43762
Fil Dunsky <filipp.dunsky@gmail.com>
2025-12-28 20:58:05 +0300

    mediatek: add support for netis NX32U
    This PR adds support for netis NX32U router.
    
    Specification
    -------------
    - SoC       : MediaTek MT7981BA dual-core ARM Cortex-A53 1.3 GHz
    - RAM       : 256 MiB DDR3
    - Flash     : SPI-NAND 128 MiB (ESMT)
    - WLAN      : MediaTek MT7976CN dual-band WiFi 6
      - 2.4 GHz : b/g/n/ax, MIMO 2x2
      - 5 GHz   : a/n/ac/ax, MIMO 2x2
    - Ethernet  : 10/100/1000 Mbps x3 (LAN, MediaTek MT7531AE)
                  10/100/1000 Mbps x1 (WAN, SoC internal phy)
    - USB       : 3.0
    - Buttons   : Mesh, Reset
    - LEDs      : 1x Power (green), unmanaged
                  1x Internet (green), gpio-controlled
                  1x WPS (green), gpio-controlled
                  1x WiFi 2.4 GHz (green), gpio-controlled
                  1x WiFi 5 GHz (green), gpio-controlled
                  1x LAN activity (green), switch-controlled
                  1x WAN activity (green), switch-controlled
                  1x USB (green), gpio-controlled
    - Power     : 12 VDC, 1 A
    
    Installation
    ------------
    1. Connect to the router using ssh (user: admin, pass: web interface
       password)
    2. Backup:
    ```
    cat /dev/mtd0 | gzip -1 -c > /tmp/mtd0_spi0.0.bin.gz
    cat /dev/mtd1 | gzip -1 -c > /tmp/mtd1_BL2.bin.gz
    cat /dev/mtd2 | gzip -1 -c > /tmp/mtd2_u-boot-env.bin.gz
    cat /dev/mtd3 | gzip -1 -c > /tmp/mtd3_Factory.bin.gz
    cat /dev/mtd4 | gzip -1 -c > /tmp/mtd4_FIP.bin.gz
    cat /dev/mtd5 | gzip -1 -c > /tmp/mtd5_ubi.bin.gz
    ```
    3. Download mtd backup from the /tmp dir of the router to your PC using
       scp protocol
    4. Upload OpenWrt 'bl31-uboot.fip', 'preloader.bin' images to the /tmp
       dir of the router using scp protocol
    5. Write FIP and BL2 (replace bootloader):
    ```
    mtd write /tmp/openwrt-mediatek-filogic-netis_nx32u-bl31-uboot.fip FIP
    mtd write /tmp/openwrt-mediatek-filogic-netis_nx32u-preloader.bin BL2
    ```
    6. Place OpenWrt
       'openwrt-mediatek-filogic-netis_nx32u-initramfs-recovery.itb' image on
       the tftp server (IP: 192.168.1.254)
    7. Erase 'ubi' partition and reboot the router:
       mtd erase ubi
       reboot
    8. U-Boot automatically boot OpenWrt recovery image from tftp server to
       the RAM
    9. Upload OpenWrt 'sysupgrade.itb' image to the /tmp dir of the router
       (IP: 192.168.1.1) using scp protocol
    10. Connect to the router using ssh and run:
    ```
    ubidetach -p /dev/mtd4; ubiformat /dev/mtd4 -y; ubiattach -p /dev/mtd4
    ubimkvol /dev/ubi0 -n 0 -N ubootenv -s 128KiB
    ubimkvol /dev/ubi0 -n 1 -N ubootenv2 -s 128KiB
    sysupgrade -n openwrt-mediatek-filogic-netis_nx32u-squashfs-sysupgrade.itb
    ```
    
    Recovery
    --------
    1. Place OpenWrt
       'openwrt-mediatek-filogic-netis_nx32u-initramfs-recovery.itb' image on
       the tftp server (IP: 192.168.1.254)
    2. Press “Reset” button and power on the router. After ~10 sec release
       the button.
    3. Use OpenWrt initramfs system for recovery
    
    +---------+-------------------+-----------+
    |         | MAC               | Algorithm |
    +---------+-------------------+-----------+
    | LAN     | dc:xx:xx:d1:xx:18 | label     |
    | WAN     | dc:xx:xx:d1:xx:1a | label+2   |
    | WLAN 2g | de:xx:xx:11:xx:19 |           |
    | WLAN 5g | de:xx:xx:71:xx:19 |           |
    +---------+-------------------+-----------+
    The LAN MAC was found in 'Factory', 0x1fef20
    The WAN MAC was found in 'Factory', 0x1fef26
    The WLAN 2g/5g MAC prototype was found in 'Factory', 0x4
    
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21368
    (cherry picked from commit 0f713d5d112389fab497269c78c64cc69c930065)
    Link: https://github.com/openwrt/openwrt/pull/22004
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -104,6 +104,7 @@ platform_do_upgrade() {
 	mercusys,mr90x-v1-ubi|\
 	netis,nx30v2|\
 	netis,nx31|\
+	netis,nx32u|\
 	nokia,ea0326gmp|\
 	openwrt,one|\
 	netcore,n60|\
@@ -295,6 +296,7 @@ platform_check_image() {
 	mediatek,mt7988a-rfb|\
 	mercusys,mr90x-v1-ubi|\
 	nokia,ea0326gmp|\
+	netis,nx32u|\
 	openwrt,one|\
 	netcore,n60|\
 	qihoo,360t7|\
