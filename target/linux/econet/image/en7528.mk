143f0cd1ce1f8d4deb74fd9dfcae63a1ff822c8f
Ahmed Naseef <naseefkm@gmail.com>
2025-12-30 12:24:43 +0400

    econet: en7528: add support for DASAN H660GM-A
    DASAN H660GM-A is a GPON ONT.
    
    Specification:
    - SoC:      EcoNet EN7528
    - RAM:      256 MB
    - Flash:    256 MB SPI NAND
    - Ethernet: 4x 1GbE
    - FXS:      1 port
    - GPON:     1 port
    - USB:      1x USB 2.0
    
    Install via OEM web UI:
    1. Login to the factory web UI with username "superuser"
       and password "Dz$!A!r7".
    2. Rename the OpenWrt image
       openwrt-econet-en7528-dasan_h660gm-a-squashfs-tclinux.trx
       to G_ONU_openwrt.bin.
    3. Upload the renamed image via the firmware upgrade page
       at Maintenance > Firmware Upgrade in the factory web UI.
    
    Signed-off-by: Ahmed Naseef <naseefkm@gmail.com>

--- target/linux/econet/image/en7528.mk
+++ target/linux/econet/image/en7528.mk
@@ -6,3 +6,13 @@ define Device/en7528_generic
   DEVICE_DTS := en7528_generic
 endef
 TARGET_DEVICES += en7528_generic
+
+define Device/dasan_h660gm-a
+  DEVICE_VENDOR := DASAN
+  DEVICE_MODEL := H660GM-A
+  DEVICE_DTS := en7528_dasan_h660gm-a
+  TRX_MODEL := Dewberry
+  IMAGES := tclinux.trx
+  IMAGE/tclinux.trx := append-kernel | lzma | tclinux-trx
+endef
+TARGET_DEVICES += dasan_h660gm-a
