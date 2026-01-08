34e2a354b5090d62b7e8f2586d4b19ff449aee76
John Christoforidis <github@yanny.dev>
2026-01-08 22:13:27 +0200

    qualcommax: ipq60xx: add support for TP-Link EAP620HD v3
    Specifications:
      * SoC: Qualcomm IPQ6018/AP-CP03-C1 (64-bit Quad-core Arm Cortex-A53 @ 1.2 GHz)
      * RAM: 2x Zentel A3T4GF40BBF-HP (1 GiB DDR3-1866 (13-13-13))
      * Serial Port: 1v8 TTL 115200n8
      * Wi-Fi: Qualcomm QCN5022 (802.11ax/b/g/n)
      * Wi-Fi: Qualcomm QCN5052 (802.11ac/ax/n)
      * Ethernet: RTL8211FS (10/100/1000BASE-T)
      * Flash: GigaDevice PSR1GA30DT (128 MiB)
      * LEDs: 1x Blue Status (GPIO 35 Active High)
      * Buttons: 1x Reset (GPIO 9 Active Low)
      * FCCID: 2AXJ4EAP620HDV3
      * UART: 4-pin unpopulated header by the J1 footprint
    
    Installation Instructions (Serial+TFTP):
      1. Locate the J1 footprint which contains 4 unpopulated pins
      where starting from the arrow, the pins are TX, RX, VCC and GND
      respectively.
      2. Connect a 1v8 TTL port to the 4 pins. Ensure RX and TX are crossed
         over.
      3. Copy openwrt-qualcommax-ipq60xx-tplink_eap620-hd-v3-initramfs-uImage.itb
      to your TFTP server.
      4. Power up the AP and hold Ctrl+B in the serial console (115200n8) until autoboot is halted.
      5. Run the following commands in the U-boot prompt:
         IPQ6018# setenv serverip <TFTP server addr>
         IPQ6018# setenv ipaddr <addr of the AP>
         IPQ6018# tftpboot 0x44000000 openwrt-qualcommax-ipq60xx-tplink_eap620-hd-v3-initramfs-uImage.itb
         IPQ6018# bootm 0x44000000
         You may need to type Ctrl+C and Enter before running these commands
         to clear invisible characters from the buffer.
      6. Run the following command in a terminal to copy the sysupgrade image
         to be installed (check IP address):
         $ scp -O openwrt-qualcommax-ipq60xx-tplink_eap620-hd-v3-squashfs-sysupgrade.bin root@192.168.1.1:/tmp/
      7. Activate the OpenWrt serial console and run the following commands:
         # cd /tmp
         # sysupgrade -n openwrt-qualcommax-ipq60xx-tplink_eap620-hd-v3-squashfs-sysupgrade.bin
      8. The AP will reboot and OpenWrt will be successfully installed.
    
    Installation Instructions (Web UI method):
      1. Set up the device using the vendor's web UI. Navigate to
         Management->SSH and enable the "SSH Login" checkbox. Select "Save".
    
      2. Connect to the machine via SSH:
         $ ssh -o hostkeyalgorithms=ssh-rsa <ip_of_device>
    
      3. Disable signature verification:
         $ cliclientd stopcs
      4. Rename the "-web-ui-factory" image to something less than 63
         characters, maintaining the ".bin" suffix.
    
      5. Go to System -> Firmware Update.
         Under "New Firmware File", click "Browse" and select the image.
         Select "Update" and confirm by clicking "OK".
    
      6. If the update fails, the web UI should show an error message.
         Otherwise, the device should reboot into OpenWrt.
    
    Device support followed from EAP620HD v2 and eap6xx-outdoor for DTS.
    Links: openwrt/openwrt#18227
    
    Signed-off-by: John Christoforidis <github@yanny.dev>
    Link: https://github.com/openwrt/openwrt/pull/21467
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -202,6 +202,7 @@ platform_do_upgrade() {
 		nand_do_upgrade "$1"
 		;;
 	tplink,eap610-outdoor|\
+	tplink,eap620-hd-v3|\
 	tplink,eap623od-hd-v1|\
 	tplink,eap625-outdoor-hd-v1)
 		tplink_do_upgrade "$1"
