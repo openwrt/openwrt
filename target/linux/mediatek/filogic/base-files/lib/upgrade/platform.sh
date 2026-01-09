50042b88d4c650b7d9cb889b6a59c866cd3b24a3
Fil Dunsky <filipp.dunsky@gmail.com>
2026-01-09 20:16:30 +0300

    mediatek: add support for Hiveton H5000M
    mediatek: add support for Hiveton H5000M
    Hardware specification:
    SoC: MediaTek MT7987A
    Flash: 8GB eMMC
    RAM: 1GB DDR4
    Ethernet: 2x 2.5GbE (RTL8221B PHY + Internal PHY)
    WiFi: MediaTek MT7992 / 2+3 antenna variant
    Interface: M.2 USB for 5G module
    LED: Power (hw-controlled), 2.5G LAN (hw-controlled),
    5G Module (hw-controlled), LED-3 (user-definable),
    LED-4 (user-definable)
    Button: Reset, WPS
    Power: USB Type-C PD
    Other: PWM Fan control
    
    Flash instructions:
    
    Factory `mmcblk0p2` partition is empty,
    stock ImmortalWrt-798x-mt799x-6.6-mtwifi from PadavanOnly firmware
    uses `MT7991_MT7976_EEPROM_BE5040_iPAiLNA.bin`.
    You can flash it as eeprom before flashing OpenWrt:
    ```
    dd if=/lib/firmware/MT7991_MT7976_EEPROM_BE5040_iPAiLNA.bin of=/dev/mmcblk0p2 bs=1 count=7680
    sync
    ```
    Power off the device.
    Press and hold the Reset button.
    Power on the device while keeping the Reset button pressed.
    Wait for the device to enter U-Boot recovery mode.
    Connect to the device via Ethernet (default IP: 192.168.1.1).
    Set your PC's IP to 192.168.1.x (e.g., 192.168.1.100).
    Open a web browser and navigate to http://192.168.1.1
    Upload and flash the OpenWrt firmware
    Wait for the flashing process to complete and the device to reboot.
    
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -173,6 +173,7 @@ platform_do_upgrade() {
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\
+	hiveton,h5000m|\
 	huasifei,wh3000|\
 	huasifei,wh3000-pro-emmc|\
 	smartrg,sdg-8612|\
@@ -363,6 +364,7 @@ platform_check_image() {
 		;;
 	creatlentem,clt-r30b1|\
 	creatlentem,clt-r30b1-112m|\
+	hiveton,h5000m|\
 	nradio,c8-668gl)
 		# tar magic `ustar`
 		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
@@ -408,6 +410,7 @@ platform_copy_config() {
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\
+	hiveton,h5000m|\
 	huasifei,wh3000|\
 	huasifei,wh3000-pro-emmc|\
 	jdcloud,re-cp-03|\
