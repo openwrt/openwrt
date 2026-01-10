openwrt/openwrt

commit 9151c7015ed2116b92a82b27c122310f1a91a426

netifd: use the global DHCP DUID for DHCPv4
This makes the DHCPv4 client use the global DHCP DUID to generate a
RFC4361-style client identifier.

Signed-off-by: David Härdeman <david@hardeman.nu>
Link: https://github.com/openwrt/openwrt/pull/20359
Signed-off-by: Robert Marko <robimarko@gmail.com>

package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
@@ -4,6 +4,7 @@
 
 . /lib/functions.sh
 . ../netifd-proto.sh
+. /lib/config/uci.sh
 init_proto "$@"
 
 proto_dhcp_init_config() {
@@ -31,6 +32,18 @@ proto_dhcp_add_sendopts() {
 	[ -n "$1" ] && append "$3" "-x $1"
 }
 
+proto_dhcp_get_default_clientid() {
+	[ -z "$1" ] && return
+
+	local iface="$1"
+	local duid
+	local iaid="0"
+
+        [ -e "/sys/class/net/$iface/ifindex" ] && iaid="$(cat "/sys/class/net/$iface/ifindex")"
+        duid="$(uci_get network @globals[0] dhcp_default_duid)"
+        [ -n "$duid" ] && printf "ff%08x%s" "$iaid" "$duid"
+}
+
 proto_dhcp_setup() {
 	local config="$1"
 	local iface="$2"
@@ -51,6 +64,7 @@ proto_dhcp_setup() {
 	[ "$defaultreqopts" = 0 ] && defaultreqopts="-o" || defaultreqopts=
 	[ "$broadcast" = 1 ] && broadcast="-B" || broadcast=
 	[ "$norelease" = 1 ] && norelease="" || norelease="-R"
+	[ -z "$clientid" ] && clientid="$(proto_dhcp_get_default_clientid "$iface")"
 	[ -n "$clientid" ] && clientid="-x 0x3d:${clientid//:/}"
 	[ -n "$iface6rd" ] && proto_export "IFACE6RD=$iface6rd"
 	[ "$iface6rd" != 0 -a -f /lib/netifd/proto/6rd.sh ] && append dhcpopts "-O 212"

commit 91ebb875ab665a3d815f9069614ef766f544298a

netifd: use stable IAID for DHCPv4
Commit 9151c7015ed2 introduced support for the global DHCP DUID to
generate a RFC4361-style client identifier.
However, the IAID introduced in those changes is based on ifindex, which
is subject to changes and causes issues on environments requiring a stable
IAID.

This commit switches the IAID to a stable one based on the same algorithm
used in odhcp6c:
https://github.com/openwrt/odhcp6c/commit/9a4d6fe802d21e4fc1b84f7d55b5c3c23e71d6ba

Fixes: 9151c7015ed2 ("netifd: use the global DHCP DUID for DHCPv4")
Link: https://github.com/openwrt/openwrt/pull/21489
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

package/network/config/netifd/Makefile
@@ -1,7 +1,7 @@
 include $(TOPDIR)/rules.mk
 
 PKG_NAME:=netifd
-PKG_RELEASE:=2
+PKG_RELEASE:=3
 
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_URL=$(PROJECT_GIT)/project/netifd.git

package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
@@ -32,16 +32,34 @@ proto_dhcp_add_sendopts() {
 	[ -n "$1" ] && append "$3" "-x $1"
 }
 
+proto_dhcp_get_iaid() {
+	local iface="$1"
+
+	echo $iface | awk '
+	BEGIN {
+		FS=""
+		for(n=0;n<256;n++)
+			ord[sprintf("%c",n)]=n
+	}
+	{
+		iaid=0
+		for (i=1;i<=NF;i++) {
+			iaid=and(ord[$i]+iaid*31,0xFFFFFFFF)
+		}
+		printf("%08x\n",iaid)
+	}'
+}
+
 proto_dhcp_get_default_clientid() {
 	[ -z "$1" ] && return
 
 	local iface="$1"
 	local duid
-	local iaid="0"
+	local iaid
 
-        [ -e "/sys/class/net/$iface/ifindex" ] && iaid="$(cat "/sys/class/net/$iface/ifindex")"
-        duid="$(uci_get network @globals[0] dhcp_default_duid)"
-        [ -n "$duid" ] && printf "ff%08x%s" "$iaid" "$duid"
+	iaid="$(proto_dhcp_get_iaid $iface)"
+	duid="$(uci_get network @globals[0] dhcp_default_duid)"
+	[ -n "$duid" ] && printf "ff%s%s" "$iaid" "$duid"
 }
 
 proto_dhcp_setup() {