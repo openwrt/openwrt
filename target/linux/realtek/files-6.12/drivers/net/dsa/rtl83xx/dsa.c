b631b1900b9980a32686c5856d9b019290996f32
Markus Stockhausen <markus.stockhausen@gmx.de>
2026-01-13 12:53:17 +0100

    realtek: dsa: convert stp_get signature
    The device specific stp_get() function returns a device specific
    state to the generic caller. So the caller must use a if/else
    statement depending on the family to look into the retrieved data.
    
    Change the signature of the function so it can return a device
    independent state of a given port in the future. Additionally
    use the new function prefix.
    
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

--- target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
+++ target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
@@ -1899,7 +1899,7 @@ static void rtldsa_port_xstp_state_set(struct rtl838x_switch_priv *priv, int por
 	index = n - (pos >> 4) - 1;
 	bit = (pos << 1) % 32;
 
-	priv->r->stp_get(priv, mst_slot, port_state);
+	priv->r->stp_get(priv, mst_slot, port, port_state);
 
 	pr_debug("Current state, port %d: %d\n", port, (port_state[index] >> bit) & 3);
 	port_state[index] &= ~(3 << bit);
