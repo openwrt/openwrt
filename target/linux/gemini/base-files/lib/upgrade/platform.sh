dd8727dd67a91d3fd2a80b8893225d8815556328
Linus Walleij <linusw@kernel.org>
2026-01-21 22:24:46 +0100

    gemini: support upgrade on reference designs
    The Gemini reference design-derived devices uses a partition
    format which is predictable and we can exploit this to offer
    some proper upgrade path.
    
    The kernel for these contains a hack to use this partition
    format unaltered by combining the partitions "Kern" and "Ramdisk"
    to one image with all of the kernel+ramdisk in memory.
    
    Then the "Application" which is used for the rootfs go into its
    own partition.
    
    Standard flash layout:
    Kern         2048k |
    Ramdisk      6144k | = 9216k
    Application  6144k | = 15360k
    
    Following the pattern of the factory image we create three
    images named zImage, rd.gz and hddapp.tgz (these filenames
    are misleading! They are just required by the old firmware.)
    and flash each individually with "mtd" during upgrades.
    
    Since the IB-4220-V has a different layout with a bigger kernel
    space we parameterize this so we can handle this too. (More
    fixes are needed for that device though.)
    
    A way to upgrade older OpenWrt on these platforms to the latest
    and greatest will be to copy the file
    target/linux/gemini/base-files/lib/upgrade/platform.sh
    to /lib/upgrade/platform.sh
    on your running system and then run sysupgrade from the image
    produced after this patch.
    
    The script is picky to sanity check the partitions before
    commencing upgrade.
    
    This was tested with a full sysupgrade on the iTian SQ201.
    
    Link: https://github.com/openwrt/openwrt/pull/21680
    (cherry picked from commit 0b0cd4efe25d9ee73896c8fa3079074dbae2637d)
    Link: https://github.com/openwrt/openwrt/pull/21973
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- target/linux/gemini/base-files/lib/upgrade/platform.sh
+++ target/linux/gemini/base-files/lib/upgrade/platform.sh
@@ -1,10 +1,81 @@
 REQUIRE_IMAGE_METADATA=1
+MTDSYSFS=/sys/class/mtd
+
+gemini_do_platform_upgrade() {
+	ESZ=`cat ${MTDSYSFS}/mtd1/erasesize`
+	if test ${ESZ} == 131072 ; then
+		echo "MTD1 has 128kb EB size..."
+	else
+		echo "MTD1 has wrong EB size!"
+	fi
+	NAME=`cat ${MTDSYSFS}/mtd1/name`
+	SZ=`cat ${MTDSYSFS}/mtd1/size`
+	KSZ=$(($ESZ * $2))
+	if test "x${NAME}" == "xKern" ; then
+		if test ${SZ} == ${KSZ} ; then
+			echo "MTD1 OK..."
+		else
+			echo "MTD1 is wrong size, aborting" >&2
+			exit 1
+		fi
+	else
+		echo "MTD1 has wrong name, aborting" >&2
+		exit 1
+	fi
+	NAME=`cat ${MTDSYSFS}/mtd2/name`
+	SZ=`cat ${MTDSYSFS}/mtd2/size`
+	RSZ=$(($ESZ * $3))
+	if test "x${NAME}" == "xRamdisk" ; then
+		if test ${SZ} == ${RSZ} ; then
+			echo "MTD2 OK..."
+		else
+			echo "MTD2 is wrong size, aborting" >&2
+			exit 1
+		fi
+	else
+		echo "MTD2 has wrong name, aborting" >&2
+		exit 1
+	fi
+	NAME=`cat ${MTDSYSFS}/mtd3/name`
+	SZ=`cat ${MTDSYSFS}/mtd3/size`
+	ASZ=$(($ESZ * $4))
+	if test "x${NAME}" == "xApplication" ; then
+		if test ${SZ} == ${ASZ} ; then
+			echo "MTD3 OK..."
+		else
+			echo "MTD3 is wrong size, aborting" >&2
+			exit 1
+		fi
+	else
+		echo "MTD3 has wrong name, aborting" >&2
+		exit 1
+	fi
+	echo "Extract the three firmware parts"
+	tar xvfz "$1"; rm "$1"
+	sync
+	echo 3 > /proc/sys/vm/drop_caches
+	echo "COMMENCING UPGRADE. BE PATIENT, THIS IS NOT FAST!"
+	echo "Upgrade Kern partition (kernel part 1, $2 erase blocks)"
+	mtd write zImage Kern
+	[ $? -ne 0 ] && exit 1
+	echo "Upgrade Ramdisk partition (kernel part 2, $3 erase blocks)"
+	mtd write rd.gz Ramdisk
+	[ $? -ne 0 ] && exit 1
+	echo "Upgrade Application partition (rootfs, $4 erase blocks)"
+	mtd write hddapp.tgz Application
+	[ $? -ne 0 ] && exit 1
+}
 
 platform_check_image() {
 	local board=$(board_name)
 
 	case "$board" in
 	dlink,dir-685)
+		return 0
+		;;
+	raidsonic,ib-4220-b|\
+	itian,sq201|\
+	storlink,gemini324)
 		return 0
 		;;
 	esac
@@ -20,6 +91,13 @@ platform_do_upgrade() {
 	dlink,dir-685)
 		PART_NAME=firmware
 		default_do_upgrade "$1"
+		;;
+	itian,sq201|\
+	storlink,gemini324)
+		gemini_do_platform_upgrade "$1" 16 48 48
+		;;
+	raidsonic,ib-4220-b)
+		gemini_do_platform_upgrade "$1" 24 48 48
 		;;
 	esac
 }
