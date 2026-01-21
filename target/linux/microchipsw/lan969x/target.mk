38de99b956d6c79c563a32c44cc4b606d4a7269b
Robert Marko <robert.marko@sartura.hr>
2026-01-21 15:27:45 +0100

    microchipsw: lan969x: use network driver as a module
    Due to PTP conflicts with CONFIG_ALL_KMODS which causes the end images
    to end up with no switchdev driver included, lets switch to using the
    switchdev and PHY drivers as modules instead.
    
    Fixes: 88a404a2d177 ("microchipsw: add support for Microchip LAN969x switches")
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- target/linux/microchipsw/lan969x/target.mk
+++ target/linux/microchipsw/lan969x/target.mk
@@ -2,7 +2,8 @@ SUBTARGET:=lan969x
 BOARDNAME:=Microchip LAN969x switches
 FEATURES+= boot-part rootfs-part
 
-DEFAULT_PACKAGES += kmod-usb3 kmod-usb-dwc3 \
+DEFAULT_PACKAGES += kmod-sparx5-switch kmod-sfp kmod-phy-micrel \
+	kmod-usb3 kmod-usb-dwc3 \
 	e2fsprogs kmod-fs-ext4 losetup \
 	kmod-fs-f2fs f2fs-tools
 
