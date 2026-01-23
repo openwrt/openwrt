67e6baf05e3922568e366915233bd63f3884256d
Michael Lotz <mmlr@mlotz.ch>
2026-01-23 15:38:50 +0100

    qualcommax: ipq60xx: add Netgear RBx350 support
    Netgear RBx350 are dual band 4 stream 802.11ax mesh devices from the Orbi
    series. The RBR350 is a router with a WAN and 3 LAN ports. The RBS350 is a
    satellite without WAN port, only 2 LAN ports and half the flash. The
    hardware is otherwise identical. They were sold in kits as RBK352, RBK353,
    RBK354 or RBK355, with one router and 1-4 satellites.
    
    Hardware:
    * SoC: Qualcomm IPQ6018
    * RAM: 512MiB 1x Nanya NT5CC256M16ER-EK
    * Flash: 512MiB Winbond W29N04GZ or 256MiB Winbond W29N02GZ
    * WLAN 2.4GHz: QCN5022 2x2:2 b/g/n/ax
    * WLAN 5GHz: QCN5052 2x2:2 a/n/ac/ax
    * Ethernet: QCA8075 switch with 1 WAN and 3 LAN ports or 2 LAN ports
    * Serial Config: 3.3V TTL 115200-8-N-1, internal populated header
    * Serial Layout: 3.3V (don't connect, marked with dot) RX TX GND
    * LEDs: green/red power, white/red/green/blue status
    * Buttons: 1x Reset, 1x WPS
    
    MAC addresses:
    * LAN1: base address on label, stored in boarddata partition at 0x8
    * LAN2: base + 1
    * LAN3: base + 2
    * WAN: base + 3
    * 2.4GHz WLAN: base + 1
    * 5GHz WLAN: base + 2
    
    Flashing Notes:
    The stock firmware images are signed. Both the bootloader and the stock
    web interface check the signature and will fail to boot/flash.
    The bootloader automatically does NMRP when a gigabit LAN connection is
    present. The stock and factory images contain a U-Boot script that is
    executed when flashing using NMRP. This is used to alter and persist the
    U-Boot env with a boot command that works with unsigned firmware.
    
    Install OpenWrt:
    * Get the nmrpflash utility [0] and OpenWrt factory image
    * Find network interface to use: nmrpflash -L
    * Start nmrpflash: nmrpflash -i interface -f openwrt-...-factory.img
    * Connect the device LAN port closest to the power jack to the same
      network using gigabit
    * Plug the device in and wait for the bootloader to flash
    * Unplug and replug the device once the power LED blinks amber
    
    Revert to Stock:
    The boot command needs to be reverted before flashing the stock firmware,
    otherwise it will fail to boot and get stuck in recovery mode (red power
    LED flashing).
    
    * Run: fw_setenv bootcmd bootipq
    * Restart the device
    * Flash the stock firmware RBx350-Va.b.c.d.img using nmrpflash
    
    [0]: https://github.com/jclehner/nmrpflash
    
    Signed-off-by: Michael Lotz <mmlr@mlotz.ch>
    Link: https://github.com/openwrt/openwrt/pull/21656
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -10,6 +10,17 @@ define Build/wax610-netgear-tar
 	rm -rf $@.tmp
 endef
 
+define Build/netgear-rbx350-qsdk-ipq-factory
+	$(CP) $(FLASH_SCRIPT) $(KDIR_TMP)/
+
+	echo "VERSION : V5.0.0.0_$(LINUX_VERSION)" > $@.metadata
+	echo "MODEL_ID : $(DEVICE_MODEL)" >> $@.metadata
+
+	$(TOPDIR)/scripts/mkits-qsdk-ipq-image.sh $@.its $(FLASH_SCRIPT) txt $@.metadata ubi $@
+	PATH=$(LINUX_DIR)/scripts/dtc:$(PATH) mkimage -f $@.its $@.new
+	@mv $@.new $@
+endef
+
 define Device/8devices_mango-dvk
 	$(call Device/FitImageLzma)
 	DEVICE_VENDOR := 8devices
@@ -144,6 +155,31 @@ define Device/linksys_mr7500
 		kmod-leds-pwm kmod-phy-aquantia
 endef
 TARGET_DEVICES += linksys_mr7500
+
+define Device/netgear_rbx350
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	SOC := ipq6018
+	DEVICE_VENDOR := Netgear
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_PACKAGES := ipq-wifi-netgear_rbk350
+	FLASH_SCRIPT := netgear_rbx350.bootscript
+	IMAGES += factory.img
+	IMAGE/factory.img := append-ubi | netgear-rbx350-qsdk-ipq-factory
+endef
+
+define Device/netgear_rbr350
+	$(call Device/netgear_rbx350)
+	DEVICE_MODEL := RBR350
+endef
+TARGET_DEVICES += netgear_rbr350
+
+define Device/netgear_rbs350
+	$(call Device/netgear_rbx350)
+	DEVICE_MODEL := RBS350
+endef
+TARGET_DEVICES += netgear_rbs350
 
 define Device/netgear_wax214
 	$(call Device/FitImage)
