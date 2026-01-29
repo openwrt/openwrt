31f30f5a6cffa2046328a933ed8063e9f8fc30b2
Anton Danilov <frimen.c@gmail.com>
2026-01-29 14:43:17 +0300

    mediatek: add Motorcomm PHY support to Cudy AP3000 v1
    Newer Cudy AP3000 v1 routers feature a Motorcomm YT8821 PHY
    instead of the Realtek PHY [1]. Support for the YT8821
    was recently introduced for the Cudy WR3000H router [2].
    On the AP3000, the changes allow the PHY to be autodetected.
    
    [1]: https://forum.openwrt.org/t/cudy-ap3000-v1-indoor-wan-motorcomm-yt8821-support/245491
    [2]: https://github.com/openwrt/openwrt/pull/21399
    
    Signed-off-by: Anton Danilov <frimen.c@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21776
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1096,7 +1096,7 @@ define Device/cudy_ap3000-v1
   IMAGE_SIZE := 65536k
   KERNEL_IN_UBI := 1
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-phy-motorcomm
 endef
 TARGET_DEVICES += cudy_ap3000-v1
 
