41300fd88a96037fc1931b424a27618a2a4474ad
Markus Stockhausen <markus.stockhausen@gmx.de>
2026-01-30 13:58:05 +0100

    realtek: eth: refactor transmit function
    Provide a new transmit function. It is cleaner and closer to
    upstream than what we have now. The basic features are:
    
    - Avoid memory moving and keep data in the SKBs
    - Only protect really critcal parts by a lock as transmit
      queues will be only called once by the kernel
    
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>
    Link: https://github.com/openwrt/openwrt/pull/21778
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/realtek/files-6.12/drivers/net/ethernet/rtl838x_eth.h
+++ target/linux/realtek/files-6.12/drivers/net/ethernet/rtl838x_eth.h
@@ -435,7 +435,7 @@ struct rteth_config {
 	u32 (*get_mac_tx_pause_sts)(int port);
 	int mac;
 	int l2_tbl_flush_ctrl;
-	void (*create_tx_header)(struct p_hdr *h, unsigned int dest_port, int prio);
+	void (*create_tx_header)(struct rteth_packet *h, unsigned int dest_port, int prio);
 	bool (*decode_tag)(struct p_hdr *h, struct dsa_tag *tag);
 	void (*hw_reset)(struct rteth_ctrl *ctrl);
 	int (*init_mac)(struct rteth_ctrl *ctrl);
