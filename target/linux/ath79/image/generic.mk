609abd8f88a82c792008fbd43f323d19990691eb
Rosen Penev <rosenp@gmail.com>
2026-02-02 15:33:38 -0800

    ath79: convert Engenius devices to qca8k
    These devices do not use SGMII and are thus safe to convert to use
    qca8k.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -1535,7 +1535,10 @@ define Device/engenius_epg5000
   SOC := qca9558
   DEVICE_VENDOR := EnGenius
   DEVICE_MODEL := EPG5000
-  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2
+  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2 \
+	kmod-dsa-qca8k kmod-phy-qca83xx \
+	kmod-fixed-phy kmod-phylink kmod-phy-at803x \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -kmod-swconfig -swconfig
   IMAGE_SIZE := 14656k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := append-kernel | pad-to $$$$(BLOCKSIZE) | \
@@ -1549,7 +1552,10 @@ define Device/engenius_esr1200
   SOC := qca9557
   DEVICE_VENDOR := EnGenius
   DEVICE_MODEL := ESR1200
-  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2
+  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2 \
+	kmod-dsa-qca8k kmod-phy-qca83xx \
+	kmod-fixed-phy kmod-phylink kmod-phy-at803x \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -kmod-swconfig -swconfig
   IMAGE_SIZE := 14656k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := append-kernel | pad-to $$$$(BLOCKSIZE) | \
@@ -1563,7 +1569,10 @@ define Device/engenius_esr1750
   SOC := qca9558
   DEVICE_VENDOR := EnGenius
   DEVICE_MODEL := ESR1750
-  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2
+  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2 \
+	kmod-dsa-qca8k kmod-phy-qca83xx \
+	kmod-fixed-phy kmod-phylink kmod-phy-at803x \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -kmod-swconfig -swconfig
   IMAGE_SIZE := 14656k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := append-kernel | pad-to $$$$(BLOCKSIZE) | \
@@ -1577,7 +1586,10 @@ define Device/engenius_esr900
   SOC := qca9558
   DEVICE_VENDOR := EnGenius
   DEVICE_MODEL := ESR900
-  DEVICE_PACKAGES := kmod-usb2
+  DEVICE_PACKAGES := kmod-usb2 \
+	kmod-dsa-qca8k kmod-phy-qca83xx \
+	kmod-fixed-phy kmod-phylink kmod-phy-at803x \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -kmod-swconfig -swconfig
   IMAGE_SIZE := 14656k
   IMAGES += factory.dlf
   IMAGE/factory.dlf := append-kernel | pad-to $$$$(BLOCKSIZE) | \
