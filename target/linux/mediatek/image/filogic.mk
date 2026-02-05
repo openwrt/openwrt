63dbdefcc3f421147b988de0476097fa193cd15c
Elwin Huang <s09289728096@gmail.com>
2026-02-05 14:30:40 +0800

    mediatek: filogic: Add support for AsiaRF AP7988-002
    >Specification:
    - SoC           : MediaTek MT7988A, Quad-core 1.8 GHz ARM Cortex-A73 CPU
    - RAM           : DDR4 1 GiB (2x Nanya NT5AD256M16E4-JRI)
    - Flash         : SPI-NAND 128 MiB (Macronix MX35LF1GE4AB-Z4I)
    - Ethernet      : 6 ports
        - LAN :
            4x 10/100/1000 Mbps RJ-45 Port
            1x 10/100/1000/2500/5000/10000 Mbps RJ-45 Port (Marvell ARQ113C)
        - WAN :
            1x 10/100/1000/2500 Mbps RJ-45 Port
    - LED           : 15x LEDs
        1x Power (Blue, GPIO)
        1x Status (Orange, GPIO)
        3x Wi-Fi (Orange)
        1x M.2 B Key (Red)
        1x M.2 M Key (Red)
        5x Ethernet LAN activity (White)
        3x Ethernet WAN activity (Green)
    - UART          : 1x4 pin header on PCB [J6538]
        - arrangement : 3.3V, TX, RX, GND
        - settings    : 115200, 8n1
    - Button        : 2x (Reset, WPS)
    - GPS           : 1x (Quectel L76-L)
    - WiFi          : 3x
        WiFi 7 2.4/5/6 Ghz (Mediatek MT7996AV)
    - Socket        :
        1x M.2 B key (USB 3.2)
        1x M.2 M key (PCIe Gen3)
        1x USB-A (USB 3.2)
        1x SIM Card
        1x SD Card
    - I2C           :
        1x iio (DPS368)
    - Power         : 12V DC, 5A
    
    MAC addresses :
        WLAN Band1 00:0A:52:xx:xx:xx (Factory, 0x4)
        WLAN Band2 00:0A:52:xx:xx:xx (Factory, 0xa)
        WLAN Band3 00:0A:52:xx:xx:xx (Factory, 0x2c0)
        LAN  00:0A:52:xx:xx:xx (Factory, 0xffff4)
        WAN  00:0A:52:xx:xx:xx (Factory, 0xffffa)
    
    Flash instruction through LuCI:
    
    This device is flashed OpenWRT base firmware with this target.
    The LuCI webpage is integrated in default for upgrading.
    
    Flash instruction through u-boot:
    
    1. Prepare the TFTP server on PC.
    2. Connect uart to PC, select "2. Upgrade firmware" in u-boot menu.
    3. input client IP, server IP, flashed bin file path
    4. Wait about 20 seconds to complete flashing
    
    Signed-off-by: Elwin Huang <s09289728096@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -393,6 +393,25 @@ define Device/arcadyan_mozart
 endef
 TARGET_DEVICES += arcadyan_mozart
 
+define Device/asiarf_ap7988-002
+  DEVICE_VENDOR := AsiaRF
+  DEVICE_MODEL := AP7988-002
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS := mt7988a-asiarf-ap7988-002
+  DEVICE_PACKAGES := kmod-usb3 fitblk kmod-mt7996-firmware kmod-phy-aquantia mt7988-2p5g-phy-firmware mt7988-wo-firmware
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_DTS_LOADADDR := 0x45f00000
+  KERNEL_LOADADDR := 0x46000000
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  KERNEL_INITRAMFS_SUFFIX := .itb
+  IMAGES := sysupgrade.itb
+  IMAGE_SIZE := 65536k
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += asiarf_ap7988-002
+
 define Device/asus_rt-ax52
   DEVICE_VENDOR := ASUS
   DEVICE_MODEL := RT-AX52
