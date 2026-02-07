a14d81d46580704e7fab178944a2a9fe580662d7
Ahmed Naseef <naseefkm@gmail.com>
2026-02-07 16:02:25 +0400

    econet: en7528: add GPIO and LED support for DASAN H660GM-A
    Enable the Airoha EN7523 GPIO driver for EN7528 and add GPIO
    controller nodes to the EN7528 DTSI. Add LED, button and GPIO
    definitions for the DASAN H660GM-A board.
    
    Signed-off-by: Ahmed Naseef <naseefkm@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21326
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/econet/en7528/target.mk
+++ target/linux/econet/en7528/target.mk
@@ -6,6 +6,8 @@ BOARDNAME:=EN7528 based boards
 CPU_TYPE:=24kc
 KERNELNAME:=vmlinuz.bin
 
+DEFAULT_PACKAGES += kmod-leds-gpio kmod-gpio-button-hotplug
+
 define Target/Description
 	Build firmware images for EcoNet EN7528 based boards.
 endef
