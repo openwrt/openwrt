e822ec274edbd2d6e0ca48c03141398ec41c2b4b
Ziyang Huang <hzyitc@outlook.com>
2026-02-08 18:28:13 +0800

    airoha: an7581: add support for Bell XG-140G-Y003
    This commit adds support for Bell XG-140G-Y003 models.
    
    Hardware
    --------
    SoC:            Airoha AN7581
    RAM:            TSEMIC TN4G16D3CSER-EK (512MB DDR3 1866MHz)
    Flash:          SkyHigh S35ML02G300 (256M)
    ETH:            Airoha AN7581 GbE Switch (eth0: LAN2, LAN3, LAN4)
                    EcoNet EN8811H 2.5GbE PHY (eth1: LAN1)
    PON Phy:        EcoNet EN7572AN
    SLIC:           MaxLinear DXS101 (SLLX3)
    USB:            USB2.0 x 1
                    USB3.0 x 1
    
    Installation
    ------------
    1. Connect uart to PC, press enter to login into U-Boot console.
            Username: telecomadmin
            Password: nE7jA%5m
    2. Run the following command to setup bootcmd.
            setenv bootcmd 'flash read 0x000c0000 0x880000 $loadaddr && bootm'
            saveenv
    3. Use a TFTP client to PUT "*-squashfs-factory.bin" to "192.168.1.1".
    4. Run the following command to flash.
            flash erase 0x000c0000 0x02880000
            flash write 0x000c0000 $filesize $loadaddr
    5. Reboot.
            reset
    
    Signed-off-by: Ziyang Huang <hzyitc@outlook.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -70,6 +70,27 @@ define Device/airoha_an7581-evb-emmc
 endef
 TARGET_DEVICES += airoha_an7581-evb-emmc
 
+define Device/bell_xg-140g-y003
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Bell
+  DEVICE_MODEL := Bell XG-140G-Y003
+  DEVICE_DTS := an7581-xg-140g-y003
+  KERNEL_SIZE := 8704k
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := \
+	kmod-phy-airoha-en8811h \
+	kmod-gpio-button-hotplug \
+	kmod-usb3 \
+	kmod-i2c-an7581 \
+	uboot-envtools
+endef
+TARGET_DEVICES += bell_xg-140g-y003
+
 define Device/gemtek_w1700k-ubi
   DEVICE_VENDOR := Gemtek
   DEVICE_MODEL := W1700K
