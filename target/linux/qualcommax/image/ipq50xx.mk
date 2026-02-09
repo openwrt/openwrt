3a39f682df5d064ffe2d56cf21bdf77abe2bd301
Mikhail Zhilkin <csharper2005@gmail.com>
2026-02-09 22:27:10 +0300

    qualcommax: ipq50xx: add support for CMCC MR3000D-CI
    CMCC MR3000D-CI is a 2.4/5 GHz band 11ax (Wi-Fi 6) router, based on
    IPQ5000.
    
    Device specification
    --------------------
    - SoC			: Qualcomm IPQ5018
    - RAM			: 512 MiB DDR3L
    - Flash			: 128 MiB SPI-NAND (GigaDevice GD5F1GQ5REYIG)
    - WLAN			: 2.4/5 GHz 2T2R
    - 	2.4 GHz		: Qualcomm IPQ5018 (SoC)
    - 	5 GHz		: Qualcomm Atheros QCN6102
    - Ethernet		: 4x 10/100/1000 Mbps
    - 	Switch		: Qualcomm Atheros QCA8337
    - LEDs/Keys (GPIO)	: 2x LEDs, 2x Buttons
    - UART			: Through-hole on PCB
    - 	Voltage		: 3.3 V
    - 	Assignment	: Silkscreened on PCB
    - 	Settings	: 115200n8
    - Power			: 12 VDC, 1.5 A
    
    Installation
    -----------------
    1. Telnet method
        a. Enable telnet
        Log in to http://192.168.10.1/ with the password on the sticker
        Modify URL according to example (keep your unique hash after ";stok=")
        and press Enter:
        http://192.168.10.1/cgi-bin/luci/;stok=78becad1b1490e45be2776025cde2b7d/api/NPCnetwork/ping?url=$(telnetd)
        You should get the following in the browser:
        {"link":0}
    
        b. Run tftp server on IP 192.168.10.254 and put factory image
        'openwrt-qualcommax-ipq50xx-cmcc_mr3000d-ci-squashfs-factory.ubi'
        in the tftp root dir.
    
        c. Login to 192.168.10.1 with telnet (user: root, pass: from the
        sticker).
    
        d. Download factory image from the tftp:
        tftp -l factory.ubi -r openwrt-qualcommax-ipq50xx-cmcc_mr3000d-ci-squashfs-factory.ubi -g 192.168.10.254
    
        e. Flash factory image:
        export rootfs=$(cat /proc/mtd | grep rootfs | grep -v _ | cut -d: -f1)
        ubidetach -f -p /dev/${rootfs}
        ubiformat /dev/${rootfs} -y -f /tmp/factory.ubi
    
        f. Reboot:
        reboot
    
    2. U-Boot Method using UBI Image (using UART)
        a. Place the factory.ubi file on your TFTP server, enter U-Boot CLI
        and exec these commands:
        tftpboot <your_tftp_server_ip>:factory.ubi
        flash rootfs
        reset
    
    3. U-Boot Method using initramfs Image (using UART)
        a. Place the openwrt-*-initramfs-fit-uImage.itb file on your TFTP
        server and rename it to initramfs.bin
    
        b. Enable serial console, enter to U-Boot CLI and exec these commands:
        tftpboot <your_tftp_server_ip>:initramfs.bin
        bootm
    
        c. Once boot completed, upload the sysupgrade.bin file to router's
        /tmp directory (using scp or wget) and execute the following command
        in openwrt shell:
        sysupgrade -n /tmp/sysupgrade.bin
    
    MAC Addresses
    -------------
    +--------------+-------------------+-------------+
    | Interface    | MAC example       | Location    |
    +--------------+-------------------+-------------+
    | LAN          | 84:7a:xx:xx:xx:dd | 0:ART, 0x6  |
    | WAN (label)  | 84:7a:xx:xx:xx:dc | 0:ART, 0x0  |
    | WLAN 2.4 GHz | 84:7a:xx:xx:xx:de | 0:ART, 0xc  |
    | WLAN 5       | 84:7a:xx:xx:xx:df | 0:ART, 0x12 |
    +--------------+-------------------+-------------+
    
    Notes
    -----
    1. U-Boot is protected by a password (pass: netpower).
    
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21952
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -19,6 +19,22 @@ define Build/mstc-header
 	rm -f $@.crclen
 endef
 
+define Device/cmcc_mr3000d-ci
+	$(call Device/FitImageLzma)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := CMCC
+	DEVICE_MODEL := MR3000D-CI
+	DEVICE_DTS_CONFIG := config@mp03.3-m1
+	SOC := ipq5018
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	IMAGE_SIZE := 59392k
+	NAND_SIZE := 128m
+	DEVICE_PACKAGES := ath11k-firmware-ipq5018-qcn6122 \
+		ipq-wifi-cmcc_mr3000d-ci
+endef
+TARGET_DEVICES += cmcc_mr3000d-ci
+
 define Device/cmcc_pz-l8
 	$(call Device/FitImageLzma)
 	$(call Device/UbiFit)
