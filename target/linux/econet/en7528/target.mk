be24a13ad5b2948e01cead1c93d45582d704f40c
Ahmed Naseef <naseefkm@gmail.com>
2026-02-09 17:57:35 +0400

    econet: en7528: add PCIe and WiFi support
    Add PCIe controller and PHY support for EN7528 SoC. This includes
    a new PCIe PHY driver, EN7528-specific startup in the MediaTek PCIe
    controller, and a fix for bogus prefetch window reads on bridges
    that do not implement the registers.
    
    Enable WiFi for the DASAN H660GM-A board with MT7603 (2.4 GHz) and
    MT7615/MT7663 (5 GHz).
    
    Signed-off-by: Ahmed Naseef <naseefkm@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21326
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- target/linux/econet/en7528/target.mk
+++ target/linux/econet/en7528/target.mk
@@ -6,7 +6,7 @@ BOARDNAME:=EN7528 based boards
 CPU_TYPE:=24kc
 KERNELNAME:=vmlinuz.bin
 
-DEFAULT_PACKAGES += kmod-leds-gpio kmod-gpio-button-hotplug
+DEFAULT_PACKAGES += kmod-leds-gpio kmod-gpio-button-hotplug wpad-basic-mbedtls
 
 define Target/Description
 	Build firmware images for EcoNet EN7528 based boards.
--- target/linux/econet/image/en7528.mk
+++ target/linux/econet/image/en7528.mk
@@ -11,6 +11,7 @@ define Device/dasan_h660gm-a
   DEVICE_VENDOR := DASAN
   DEVICE_MODEL := H660GM-A
   DEVICE_DTS := en7528_dasan_h660gm-a
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615e kmod-mt7663-firmware-ap
   TRX_MODEL := Dewberry
   IMAGES := tclinux.trx
   IMAGE/tclinux.trx := append-kernel | lzma | tclinux-trx
