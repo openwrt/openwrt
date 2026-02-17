f03bb44a08be2f1fb6bc82cfacff3fd08d7baa94
Robert Marko <robert.marko@sartura.hr>
2026-02-17 13:00:04 +0100

    mvebu: move FitImage recipe to generic image Makefile
    Move the Device/FitImage recipe to the generic image Makefile to avoid
    duplicating it for other subtargets.
    
    Will be used for uDPU/eDPU.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- target/linux/mvebu/image/cortexa72.mk
+++ target/linux/mvebu/image/cortexa72.mk
@@ -2,12 +2,6 @@ define Build/append-bootscript
 	cat $@-boot.scr >> $@
 endef
 
-define Device/FitImage
-  KERNEL_SUFFIX := -uImage.itb
-  KERNEL = kernel-bin | gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
-  KERNEL_NAME := Image
-endef
-
 define Device/UbiFit
   KERNEL_IN_UBI := 1
   IMAGES := factory.ubi sysupgrade.bin
