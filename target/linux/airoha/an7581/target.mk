a40f50b71064ed2d595165d05bf11ef781d396d2
Andrew LaMarche <andrewjlamarche@gmail.com>
2026-02-20 15:03:12 +0000

    airoha: an7581: add uboot-envtools to default packages

--- target/linux/airoha/an7581/target.mk
+++ target/linux/airoha/an7581/target.mk
@@ -6,7 +6,7 @@ KERNELNAME:=Image dtbs
 FEATURES+=pwm source-only
 
 DEFAULT_PACKAGES += \
-	airoha-en7581-npu-firmware
+	airoha-en7581-npu-firmware uboot-envtools
 
 define Target/Description
 	Build firmware images for Airoha an7581 ARM based boards.
