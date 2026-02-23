69730f603b7becca18a63ca4ef9356242dd698ab
Andrew LaMarche <andrewjlamarche@gmail.com>
2026-02-23 01:26:31 +0000

    mediatek: add support for BananaPi BPi-R4 Pro 8X
    The BPi-R4 Pro 8X is a Wi-Fi 7 router board based on the MT7988A SoC.
    
    Specification
    -------------
    
    - SoC:		Mediatek MT7988A (4x Cortex-A73 @1.8GHz)
    - RAM:		8GB DDR4
    - Flash: 	256MB SPI-NAND, 8GB eMMC, microSD Slot
    - Switch:	MT7530, MxL86252C
    - PHYs:		2x AS21010P
    - Ports:	9 ports, 7 usable simultaneously
    	* 1x 1G RJ45 LAN
    	* 4x 2.5G RJ45 LAN
    	* 1x 10G RJ45/SFP Combo
    	* 1x 10G RJ45/SFP Combo
    - Buttons:	Reset, WPS
    - LEDs:		Power
    - USB:		On-board VIA VL822 USB3.2/USB2.0 hub
    	* 1x USB-A 2.0 connector
    	* 1x USB-A 3.2 connector
    	* 3x NGFF-KEYB
    - PCIe:
    	* 2x mini-PCIe slots with PCIe 3.0 2-lane interface
    	* 2x M.2 M-Key slots with PCIe 3.0 1-lane interface (switchable
    	  PCIe signal with M.2 B-Key)
    - Debug:	USB Type-C console port
    - Power:	USB Type-C PD 20V or DC barrel connector
    
    Installation
    ------------
    
    Uncompress *sdcard.img.gz and write to microSD card, eg. using 'dd'. Use
    bootloader menu on the serial console to install SPI-NAND once installed
    to SPI-NAND you can used the bootloader menu to install to eMMC. See
    instructions for BananaPi R4 for details.
    
    Co-authored-by: Frank Wunderlich <frank-w@public-files.de>
    Signed-off-by: Andrew LaMarche <andrewjlamarche@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -282,6 +282,7 @@ platform_check_image() {
 	bananapi,bpi-r4-2g5|\
 	bananapi,bpi-r4-poe|\
 	bananapi,bpi-r4-lite|\
+	bananapi,bpi-r4-pro-8x|\
 	bazis,ax3000wm|\
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -745,6 +745,60 @@ define Device/bananapi_bpi-r4-poe
 endef
 TARGET_DEVICES += bananapi_bpi-r4-poe
 
+define Device/bananapi_bpi-r4-pro-common
+  DEVICE_VENDOR := Bananapi
+  DEVICE_DTS_DIR := $(DTS_DIR)/
+  DEVICE_DTS_LOADADDR := 0x45f00000
+  DEVICE_DTS_OVERLAY:= mt7988a-bananapi-bpi-r4-pro-emmc mt7988a-bananapi-bpi-r4-pro-sd \
+		       mt7988a-bananapi-bpi-r4-pro-cn15 mt7988a-bananapi-bpi-r4-pro-cn18
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_PACKAGES := kmod-dsa-mxl862xx kmod-hwmon-pwmfan kmod-i2c-mux-pca954x \
+		     kmod-eeprom-at24 kmod-mt7996-firmware kmod-mt7996-233-firmware \
+		     kmod-rtc-pcf8563 kmod-sfp kmod-usb3 e2fsprogs f2fsck mkf2fs mt7988-wo-firmware \
+		     kmod-gpio-pca953x kmod-nvme
+  IMAGES := sysupgrade.itb
+  KERNEL_LOADADDR := 0x46000000
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  ARTIFACTS := \
+	       emmc-gpt.bin emmc-preloader.bin emmc-bl31-uboot.fip \
+	       snand-preloader.bin sdcard.img.gz snand-bl31-uboot.fip
+  ARTIFACT/emmc-gpt.bin		:= mt798x-gpt emmc
+  ARTIFACT/emmc-preloader.bin	:= mt7988-bl2 emmc-$$(DEVICE_BL2)
+  ARTIFACT/emmc-bl31-uboot.fip	:= mt7988-bl31-uboot $$(DEVICE_NAME)-emmc
+  ARTIFACT/snand-preloader.bin	:= mt7988-bl2 spim-nand-ubi-$$(DEVICE_BL2)
+  ARTIFACT/snand-bl31-uboot.fip	:= mt7988-bl31-uboot $$(DEVICE_NAME)-snand
+  ARTIFACT/sdcard.img.gz	:= mt798x-gpt sdmmc |\
+				   pad-to 17k | mt7988-bl2 sdmmc-$$(DEVICE_BL2) |\
+				   pad-to 6656k | mt7988-bl31-uboot $$(DEVICE_NAME)-sdmmc |\
+				$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
+				   pad-to 12M | append-image-stage initramfs-recovery.itb | check-size 44m |\
+				) \
+				   pad-to 44M | mt7988-bl2 spim-nand-ubi-$$(DEVICE_BL2) |\
+				   pad-to 45M | mt7988-bl31-uboot $$(DEVICE_NAME)-snand |\
+				   pad-to 51M | mt7988-bl2 emmc-$$(DEVICE_BL2) |\
+				   pad-to 52M | mt7988-bl31-uboot $$(DEVICE_NAME)-emmc |\
+				   pad-to 56M | mt798x-gpt emmc |\
+				$(if $(CONFIG_TARGET_ROOTFS_SQUASHFS),\
+				   pad-to 64M | append-image squashfs-sysupgrade.itb | check-size |\
+				) \
+				  gzip
+  IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
+  KERNEL			:= kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+endef
+
+define Device/bananapi_bpi-r4-pro-8x
+  DEVICE_MODEL := BPi-R4-Pro-8X
+  DEVICE_DTS := mt7988a-bananapi-bpi-r4-pro-8x
+  DEVICE_DTS_CONFIG := config-mt7988a-bananapi-bpi-r4-pro-8x
+  DEVICE_BL2 := comb-4bg
+  $(call Device/bananapi_bpi-r4-pro-common)
+  DEVICE_PACKAGES += kmod-phy-aeonsemi-as21xxx
+endef
+TARGET_DEVICES += bananapi_bpi-r4-pro-8x
+
 define Device/bananapi_bpi-r4-lite
   DEVICE_VENDOR := Bananapi
   DEVICE_MODEL := BPi-R4 Lite
