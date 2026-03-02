f087204502363aece43715df43d4c4bfa9611bcc
Jakub Vaněk <linuxtardis@gmail.com>
2026-03-02 21:14:28 +0100

    mediatek: filogic: add support for Cudy M3000 w/ YT8821 PHY
    The hardware is very close the the Cudy M3000 v1 (see commit
    20e4a18feb3f). However, the Motorcomm YT8821 PHY is tricky
    to support because of a MDIO address collision within the router.
    
    Specification:
     - MT7981BA CPU: dual-core ARM Cortex-A53 @ 1.3 GHz
     - 256 MiB RAM
     - 128 MiB SPI NAND
     - Ethernet:
       - 1x 1GbE LAN port driven by the internal MT7981 PHY
       - 1x 2.5GbE WAN port driven by the Motorcomm YT8821
     - WiFi:
       - MT7981BA 2.4 GHz WiFi with 2x2:2 MIMO
       - MT7981BA 5 GHz WiFi with 2x3:2 MIMO
     - Buttons: Reset, WPS
     - LED: 1x combined red/white
    
    How to know if you have the a router with the YT8821 PHY:
     - Boot the router into the vendor's firmware. Go to Diagnostic Tools
       -> System Log. Try searching for "rtl8221b".
     - If there are some matches, you have the Cudy M3000 router with
       the Realtek PHY and you should NOT use the device defined in this
       commit. Instead, you should use the device defined in
       mt7981b-cudy-m3000-v1.dts.
     - If there are no matches, try searching for "yt8821". If that
       matches something, you have the Cudy M3000 with the Motorcomm PHY
       and you should use this device tree
       (mt7981b-cudy-m3000-v2-yt8821.dts).
     - If even the yt8821 string did not match anything, then something
       is wrong. Rebooting the router might help (the system log would
       be refreshed).
    
    Installation via the Cudy web UI:
     - Download the signed intermediary firmware from
       https://drive.google.com/drive/folders/1BKVarlwlNxf7uJUtRhuMGUqeCa5KpMnj
     - Flash the intermediary firmware using the Cudy web UI
     - Connect a PC/laptop to the "1Gbps LAN" port
     - Open http://192.168.1.1 in your browser, log in
       (the password should be empty)
     - Flash your desired OpenWrt firmware via LuCI
     - The router should reboot into the desired firmware
    
    How to access UART (citing from 20e4a18feb3f):
     - remove rubber ring on the bottom
     - remove screws
     - pull up the cylinder, maybe help by push on an ethernet socket
       with a screwdriver
     - remove the (3) screws holding the board in the frame
     - remove the board from the frame to get to the screws for the
       silver, flat heat shield
     - remove the (3) screws holding the heat shield
     - solder UART pins to the back of the board
       - make sure to have the pins point out on side with the black,
         finned heat spread
       - the markings for the pins are going to be below the silver heat
         shield
       - Vcc is not needed
     - the UART parameters are 115200 baud, 8n1
    
    Installation via UART (citing from 20e4a18feb3f):
     - attach an Ethernet cable to the "1Gbps LAN" port on the router
     - hold the reset button while powering the router
     - press CTRL-C or wait for the timeout to get to the U-Boot prompt
     - prepare a TFTP server on the network to supply ..-initramfs-kernel.bin
     - use 'tftpboot 0x46000000 ..-initramfs-kernel.bin' in the U-Boot
       shell to pull the image (change the file name accordingly)
     - boot the image using 'bootm 0x46000000'
     - push the ..-sysupgrade to the router using your preferred method
     - perform the upgrade with 'sysupgrade -n'
    
    Signed-off-by: Jakub Vaněk <linuxtardis@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/22259
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    (cherry picked from commit 45b51ebaff51d2a49d149e9011717abb1b032683)
    Signed-off-by: Jakub Vaněk <linuxtardis@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1122,6 +1122,28 @@ define Device/cudy_m3000-v1
 endef
 TARGET_DEVICES += cudy_m3000-v1
 
+define Device/cudy_m3000-v2-yt8821
+  DEVICE_VENDOR := Cudy
+  DEVICE_MODEL := M3000
+  DEVICE_VARIANT := v2 with Motorcomm YT8821
+  DEVICE_DTS := mt7981b-cudy-m3000-v2-yt8821
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += R37
+  DEVICE_DTS_LOADADDR := 0x44000000
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  KERNEL := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := sysupgrade.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-phy-motorcomm
+endef
+TARGET_DEVICES += cudy_m3000-v2-yt8821
+
 define Device/cudy_re3000-v1
   DEVICE_VENDOR := Cudy
   DEVICE_MODEL := RE3000
