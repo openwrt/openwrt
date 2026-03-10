openwrt/openwrt

commit 93173aee96e7496f3ff158046d3d19cd42c2e031

qualcommbe: ipq95xx: Add initial support for new target
Add initial support for new target with the initial patch for ethernet
support using pending upstream patches for PCS UNIPHY, PPE and EDMA.

Only initramfs currently working as support for new SPI/NAND
implementation, USB, CPUFreq and other devices is still unfinished and
needs to be evaluated.

Link: https://github.com/openwrt/openwrt/pull/17725
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

target/linux/qualcommbe/image/ipq95xx.mk
@@ -1,0 +1,14 @@
+define Device/qcom_rdp433
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := Qualcomm Technologies, Inc.
+	DEVICE_MODEL := RDP433
+	DEVICE_VARIANT := AP-AL02-C4
+	BOARD_NAME := ap-al02.1-c4
+	DEVICE_DTS_CONFIG := config@rdp433
+	SOC := ipq9574
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := 6096k
+	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
+endef
+TARGET_DEVICES += qcom_rdp433

commit bc6bd766d5b3a6d4354101b7f79186c79d0d62f2

qualcommbe: ipq95xx: rdp433 add dts compatible to SUPPORTED_DEVICES
The default derived SUPPORTED_DEVICES "qcom,rdp433" does not match the
actual dts compatible "qcom,ipq9574-ap-al02-c7" used by this board[1],
breaking sysupgrade and ASU support.
Add actual upstream device dts compatible to SUPPORTED_DEVICES.

[1]: https://elixir.bootlin.com/linux/v6.12.76/source/arch/arm64/boot/dts/qcom/ipq9574-rdp433.dts

Fixes: 93173aee96e7496f3ff158046d3d19cd42c2e031 ("qualcommbe: ipq95xx: Add initial support for new target")
Signed-off-by: Mario Andrés Pérez <mapb_@outlook.com>

target/linux/qualcommbe/image/ipq95xx.mk
@@ -25,5 +25,6 @@ define Device/qcom_rdp433
 	KERNEL_SIZE := 6096k
 	IMAGE_SIZE := 25344k
 	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
+	SUPPORTED_DEVICES += qcom,ipq9574-ap-al02-c7
 endef
 TARGET_DEVICES += qcom_rdp433