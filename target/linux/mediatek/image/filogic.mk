c79573a89ce03fe5ccce53400309926852e5ba89
Tianchen Tang <ttc0419@outlook.com>
2026-03-10 15:15:27 +0800

    mediatek/filogic: add support for beeconmini seed ac1
    The stock firmware uses a variant of luci, upload the firmware via web ui's system tab to install.
    
    Signed-off-by: Tianchen Tang <ttc0419@outlook.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -805,6 +805,16 @@ endif
 endef
 TARGET_DEVICES += bananapi_bpi-r4-lite
 
+define Device/beeconmini_seed-ac1
+  DEVICE_VENDOR := BeeconMini
+  DEVICE_MODEL := SEED AC1
+  DEVICE_DTS := mt7981b-beeconmini-seed-ac1
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-sfp kmod-usb3 kmod-fs-f2fs
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += beeconmini_seed-ac1
+
 define Device/bazis_ax3000wm
   DEVICE_VENDOR := Bazis
   DEVICE_MODEL := AX3000WM
