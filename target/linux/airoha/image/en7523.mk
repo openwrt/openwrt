0e27a08bfd3d01e06f8652f15f6dc8aea40e30c3
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2026-03-26 14:08:37 -0300

    airoha: an7523: add configs and memory reservation
    add nodes in dst that do not need patches
    enable some options in the linux kernel config
    
    Signed-off-by: Matheus Sampaio Queiroga <srherobrine20@gmail.com>

--- target/linux/airoha/image/en7523.mk
+++ target/linux/airoha/image/en7523.mk
@@ -1,8 +1,9 @@
-define Target/Description
-	Build firmware images for Airoha EN7523 ARM based boards.
-endef
+#
+# AN7523 Profile
+#
 
 define Device/airoha_en7523-evb
+  $(Device/FitImage)
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := EN7523 Evaluation Board
   DEVICE_DTS := en7523-evb
