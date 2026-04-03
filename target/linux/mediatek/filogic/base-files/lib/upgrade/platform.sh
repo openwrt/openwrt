323ddbecd3a1dccfa3fe0fd24d4f2400927bd08e
Fil Dunsky <filipp.dunsky@gmail.com>
2026-04-03 13:04:33 +0300

    mediatek: add support for Wavlink WL-WNT100X3 ubootmod
    This allows us to use the full size of nand, which increases ubi size
    from 90M to 122.25M.
    
    Flashing instructions:
    1. Login into the device and backup all your partitions,
    especially `Factory` and 'HW' to be able to come back to stock and use all
    Wavlink services.
    
    2. Unlock mtd partitions:
    ```
    apk update && apk add kmod-mtd-rw
    insmod mtd-rw i_want_a_brick=1
    ```
    3. Upload new `bl2` and `fip` to the router /tmp and write them:
    ```
    mtd write /tmp/openwrt-mediatek-filogic-wavlink_wl-wnt100x3-ubootmod-preloader.bin bl2
    mtd write /tmp/openwrt-mediatek-filogic-wavlink_wl-wnt100x3-ubootmod-bl31-uboot.fip fip
    mtd erase ubi
    ```
    4. Set static IP on your PC:
       IP 192.168.1.254/24, GW 192.168.1.1
    5. Serve OpenWrt initramfs image using TFTP server.
    6. Cut off the power and re-engage, wait for TFTP recovery to complete.
    7. After OpenWrt initramfs has booted, prepare ubi and envs:
    ```
    ubidetach -p /dev/mtd4 && ubiformat /dev/mtd4 -y && ubiattach -p /dev/mtd4 && ubimkvol /dev/ubi0 -n 0 -N ubootenv -s 128KiB && ubimkvol /dev/ubi0 -n 1 -N ubootenv2 -s 128KiB
    ```
    8. Perform sysupgrade.
    
    Signed-off-by: Fil Dunsky <filipp.dunsky@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/22753
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    (cherry picked from commit d2fabb974c57cc96c12f783f2197cc2639e1ed5a)

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -145,6 +145,7 @@ platform_do_upgrade() {
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
 	tplink,tl-xtr8488|\
+	wavlink,wl-wnt100x3-ubootmod|\
 	xiaomi,mi-router-ax3000t-ubootmod|\
 	xiaomi,redmi-router-ax6000-ubootmod|\
 	xiaomi,mi-router-wr30u-ubootmod|\
@@ -346,6 +347,7 @@ platform_check_image() {
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
 	tplink,tl-xtr8488|\
+	wavlink,wl-wnt100x3-ubootmod|\
 	xiaomi,mi-router-ax3000t-ubootmod|\
 	xiaomi,redmi-router-ax6000-ubootmod|\
 	xiaomi,mi-router-wr30u-ubootmod|\
