b05a5488228c2fe08d5bdd4163b0bb5bdcfb410a
Rosen Penev <rosenp@gmail.com>
2026-04-17 18:59:36 -0700

    irq-ath79-intc: add SPDX license
    License boilerplate was deprecated by upstream in
    d2912cb15bdda8ba4a5dd73396ad62641af2f520
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- target/linux/ath79/files/drivers/irqchip/irq-ath79-intc.c
+++ target/linux/ath79/files/drivers/irqchip/irq-ath79-intc.c
@@ -1,11 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *  Atheros AR71xx/AR724x/AR913x specific interrupt handling
  *
  *  Copyright (C) 2018 John Crispin <john@phrozen.org>
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 2 as published
- *  by the Free Software Foundation.
  */
 
 #include <linux/interrupt.h>
