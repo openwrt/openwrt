b495c24a2e1090300a6c86d852f0cfb65c349387
Sergey Shlukov <ichizakurain@gmail.com>
2026-04-27 06:51:08 +0300

    mediatek: filogic: Archer AX80v1: add U‑Boot (UBI) layout
    This commit adds U-Boot (UBI) layout support for Archer AX80v1.
    
    1. Installation (UART)
    
    Place OpenWrt initramfs image on tftp server with IP 192.168.1.2
    Attach UART, switch on the router
    Interrupt the boot process by pressing 'Ctrl-C'
    Load and run OpenWrt initramfs image:
    ```
    tftpboot ubi-initramfs-recovery.bin
    bootm
    ```
    2. Make backups:
    ```
    cat /dev/mtd0 > /tmp/boot.bin
    cat /dev/mtd5 > /tmp/tp_data.bin
    ```
    Copy /tp_data dir content, /tmp/boot.bin and /tmp/tp_data.bin and to
    your PC using scp. You can also backup the remaining partititons. Copy
    backups to a safe place, they are required for the next steps and stock
    firmware recovery.
    
    3. Reboot to OpenWrt initramfs:
    ```
    reboot
    ```
    
    4. Copy OpenWrt ubi-bl31-uboot.fip, ubi-preloader.bin,
       ubi-squashfs-sysupgrade.itb and MT7986_EEPROM.bin, default-mac (from
       /tp_data backup) to the /tmp folder of the router using scp.
    
    5. Prepare UBI:
    ```
    ubidetach -p /dev/mtd3; ubiformat /dev/mtd3 -y; ubiattach -p /dev/mtd3
    ubimkvol /dev/ubi0 -N fip -t static -s 1MiB
    ubiupdatevol /dev/ubi0_0 /tmp/ubi-bl31-uboot.fip
    ubimkvol /dev/ubi0 -N ubootenv -s 0x1f000
    ubimkvol /dev/ubi0 -N ubootenv2 -s 0x1f000
    ```
    
    6.  Install kmod-mtd-rw and unlock partitions:
    ```
    opkg update && opkg install kmod-mtd-rw
    insmod mtd-rw i_want_a_brick=1
    mtd unlock boot
    mtd unlock bl2
    mtd unlock factory
    ```
    
    7. Prepare "factory" partition:
    ```
    dd if=/dev/zero bs=32K count=1 | tr '\000' '\377' > /tmp/factory.bin
    dd if=/tmp/MT7986_EEPROM.bin of=/tmp/factory.bin conv=notrunc
    dd if=/tmp/default-mac >> /tmp/factory.bin
    ```
    
    8. Write "factory" partition:
    ```
    mtd erase factory
    mtd write /tmp/factory.bin factory
    ```
    
    9. Write preloader partition:
    ```
    mtd erase bl2
    mtd write /tmp/ubi-preloader.bin bl2
    ```
    
    10. Write OpenWrt sysupgrade image:
    ```
    sysupgrade -n /tmp/ubi-squashfs-sysupgrade.itb
    ```
    
    Recovery
    --------
    1. Place OpenWrt ubi-initramfs-recovery.bin image on the
       tftp server (IP: 192.168.1.254).
    2. Press "reset" button and power on the router. After ~10 sec release the
       button.
    3. Use OpenWrt initramfs system for recovery.
    
    BL2 and FIP recovery
    --------------------
    Use mtk_uartboot and UART connetion if BL2 or FIP in UBI is destroyed:
    Link: https://github.om/981213/mtk_uartboot
    
    Return to stock:
    ----------------
    1. Copy "boot" partition backup (boot.bin) to the /tmp dir of the router
       using scp.
    
    2. Install kmod-mtd-rw:
    ```
    opkg update && opkg install kmod-mtd-rw
    ```
    
    3. Restore stock U-Boot:
    ```
       insmod mtd-rw i_want_a_brick=1
       mtd unlock boot
       mtd erase boot
       mtd write /tmp/boot.bin boot
    ```
    
    4. Erase UBI and reboot:
    ```
    mtd erase ubi
    reboot
    ```
    
    5. Open U-Boot web recovery, upload stock firmware image and start
       upgrade.
    Link: http://192.168.1.1
    
    6. After reboot, upgrade with stock firmware image again via WebUI.
    
    7. Complete steps 1-4 of the Installation (UART).
    Link: https://github.com/openwrt/openwrt/pull/17753
    
    8. Upload "tp_data" partition backup (tp_data.bin) to the /tmp folder of
       the router using scp.
    
    9. Restore stock calibrations:
    ```
    mtd write /tmp/tp_data.bin tp_data
    reboot
    ```
    
    10. Perform "factory restore" via stock firmware web interface.
    
    Signed-off-by: Sergey Shlukov <ichizakurain@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -173,7 +173,8 @@ platform_do_upgrade() {
 	qihoo,360t7-ubi|\
 	routerich,ax3000-ubootmod|\
 	routerich,be7200|\
-	snr,snr-cpe-ax2|\
+ 	snr,snr-cpe-ax2|\
+	tplink,archer-ax80-v1-ubi|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
@@ -376,6 +377,7 @@ platform_check_image() {
 	qihoo,360t7|\
 	qihoo,360t7-ubi|\
 	routerich,ax3000-ubootmod|\
+	tplink,archer-ax80-v1-ubi|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
