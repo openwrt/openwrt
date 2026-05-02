d9f8f74fd8eccf54e09fe4dbbbf8bdabe7e33d17
Lukas Stockner <lukas@lukasstockner.de>
2026-05-02 19:31:43 +0200

    base-files: support rootfs_data on its own partition
    The current code assumes that the rootfs_data UBI volume is on the same MTD
    partition as the rootfs.
    Unfortunately, this does not work on the Aruba AP-325 (and variants), since
    the bootloader enforces a particular UBI volume layout.
    
    Therefore, this adds a separate variable to set the rootfs_data partition,
    and updates all existing devices with a non-default rootfs partition to also
    specify the new variable.
    
    Signed-off-by: Lukas Stockner <lukas@lukasstockner.de>
    Link: https://github.com/openwrt/openwrt/pull/20738
    Signed-off-by: Paul Spooren <mail@aparcar.org>
    (cherry picked from commit bf770bc4e3eb150b9496314992d8baed97067848)
    Signed-off-by: Florian Maurer <f.maurer@outlook.de>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -197,6 +197,7 @@ platform_do_upgrade() {
 	buffalo,wxr-5950ax12)
 		CI_KERN_UBIPART="rootfs"
 		CI_ROOT_UBIPART="user_property"
+		CI_DATA_UBIPART="user_property"
 		buffalo_upgrade_prepare
 		nand_do_flash_file "$1" || nand_do_upgrade_failed
 		nand_do_restore_config || nand_do_upgrade_failed
@@ -255,6 +256,7 @@ platform_do_upgrade() {
 		# Kernel and rootfs are placed in 2 different UBI
 		CI_KERN_UBIPART="ubi_kernel"
 		CI_ROOT_UBIPART="rootfs"
+		CI_DATA_UBIPART="rootfs"
 		nand_do_upgrade "$1"
 		;;
 	spectrum,sax1v1k)
@@ -295,6 +297,7 @@ platform_do_upgrade() {
 	zte,mf269)
 		CI_KERN_UBIPART="ubi_kernel"
 		CI_ROOT_UBIPART="rootfs"
+		CI_DATA_UBIPART="rootfs"
 		nand_do_upgrade "$1"
 		;;
 	zyxel,nbg7815)
