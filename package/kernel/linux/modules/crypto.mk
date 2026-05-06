1e203299f16a528a7e475d3c02280d6fe74298e5
Rosen Penev <rosenp@gmail.com>
2026-05-06 15:47:24 -0700

    kernel/crypto: user: remove AF_ALG
    It seems kmod-crypto-user includes AFALG for whatever reason. Since it's
    going away, remove it.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- package/kernel/linux/modules/crypto.mk
+++ package/kernel/linux/modules/crypto.mk
@@ -1153,23 +1153,11 @@ $(eval $(call KernelPackage,crypto-test))
 
 
 define KernelPackage/crypto-user
-  TITLE:=CryptoAPI userspace interface
+  TITLE:=CryptoAPI userspace configuration interface
   DEPENDS:=+kmod-crypto-hash +kmod-crypto-manager +kmod-crypto-rng
-  KCONFIG:= \
-	CONFIG_CRYPTO_USER \
-	CONFIG_CRYPTO_USER_API \
-	CONFIG_CRYPTO_USER_API_AEAD \
-	CONFIG_CRYPTO_USER_API_HASH \
-	CONFIG_CRYPTO_USER_API_RNG \
-	CONFIG_CRYPTO_USER_API_SKCIPHER
-  FILES:= \
-	$(LINUX_DIR)/crypto/af_alg.ko \
-	$(LINUX_DIR)/crypto/algif_aead.ko \
-	$(LINUX_DIR)/crypto/algif_hash.ko \
-	$(LINUX_DIR)/crypto/algif_rng.ko \
-	$(LINUX_DIR)/crypto/algif_skcipher.ko \
-	$(LINUX_DIR)/crypto/crypto_user.ko
-  AUTOLOAD:=$(call AutoLoad,09,af_alg algif_aead algif_hash algif_rng algif_skcipher crypto_user)
+  KCONFIG:=CONFIG_CRYPTO_USER
+  FILES:=$(LINUX_DIR)/crypto/crypto_user.ko
+  AUTOLOAD:=$(call AutoLoad,09,crypto_user)
   $(call AddDepends/crypto)
 endef
 
