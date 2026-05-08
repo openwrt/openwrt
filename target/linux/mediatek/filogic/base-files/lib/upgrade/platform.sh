65e0ee53d3c6352d1f1c869332c9a383203e76a8
Chukun Pan <amadeus@jmu.edu.cn>
2026-05-08 16:06:09 +0800

    mediatek: add TP-Link TL-7DR7230 v1 support
    Hardware specification:
      SoC: MediaTek MT7988D 3x A73
      Flash: ESMT F50L1G41LB 128MB
      RAM: GDQ2BFAA-CJ DDR4 512MB
      Ethernet: 1x 2.5G Wan, 4x 1G Lan
      WiFi1: MT7975N 2.4GHz 4T4R
      WiFi2: MT7979N 5GHz 4T5R
      Button: Reset, WPS
      Power: DC 12V 2A
    
    MAC addresses map:
       LAN 4C:xx:xx:xx:xx:A7
       WAN 4C:xx:xx:xx:xx:A8
       2G  4C:xx:xx:xx:xx:A7
       5G  4C:xx:xx:xx:xx:A9
    
    Flash instructions:
      1. Update 7dr7230mv1-middleware.bin in vendor uboot
    
      2. Update factory.bin in the middleware firmware
    
      3. After openwrt boots up, install kmod-mtd-rw and
         execute: insmod mtd-rw i_want_a_brick=1
    
      4. Replace the stock bootloader to OpenWrt's:
         mtd write openwrt-mediatek-filogic-tplink_tl-7dr7230-v1-preloader.bin bl2
         mtd write openwrt-mediatek-filogic-tplink_tl-7dr7230-v1-bl31-uboot.fip fip
    
      5. Run the following command to remove the OEM UBI volume:
         ubirmvol /dev/ubi0 -N kernel
         ubirmvol /dev/ubi0 -N rootfs
         ubimkvol /dev/ubi0 -N ubootenv -s 1MiB
         ubimkvol /dev/ubi0 -N ubootenv2 -s 1MiB
    
      6. Upload sysupgrade.bin to perform the upgrade.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -210,6 +210,7 @@ platform_do_upgrade() {
 	snr,snr-cpe-ax2|\
 	teralink,tl3020-256mb|\
 	tplink,be450-ubi|\
+	tplink,tl-7dr7230*|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
@@ -456,6 +457,7 @@ platform_check_image() {
 	routerich,ax3000-ubootmod|\
 	teralink,tl3020-256mb|\
 	tplink,be450-ubi|\
+	tplink,tl-7dr7230*|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
