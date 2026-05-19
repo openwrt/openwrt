abbf00c85d23f46cd6511320b0899ae6353a099c
Chukun Pan <amadeus@jmu.edu.cn>
2026-05-19 18:09:05 +0800

    mediatek: add TP-Link TL-7DR7250 v1 support
    Hardware specification:
      SoC: MediaTek MT7988D 3x A73
      Flash: ESMT F50L1G41LB 128MB
      RAM: NT5AD256M16E4 DDR4 512MB
      Ethernet: 2x 2.5G + 2x 1G Lan
      WiFi1: MT7975N 2.4GHz 4T4R
      WiFi2: MT7979N 5GHz 4T5R
      Button: Reset, WPS
      Power: DC 12V 2A
    
    MAC addresses map:
       LAN 4C:xx:xx:xx:xx:57
       WAN 4C:xx:xx:xx:xx:58
       2G  4C:xx:xx:xx:xx:57
       5G  4C:xx:xx:xx:xx:59
    
    Flash instructions:
      1. Connect to your PC via the Gigabit port of the router,
         set a static ip on the ethernet interface of your PC.
         (ip 192.168.1.254, gateway 192.168.1.1)
    
      2. Execute the following operation to open nc shell:
         https://forum.openwrt.org/t/tp-link-china-routers-rce-vuln
    
      3. Download the preloader.bin and uboot.fip, host them with
         the HTTP server: python3 -m http.server
    
      4. Replace the stock bootloader to OpenWrt's:
         ln -sf opt/game_acc/rootfs/bin/busybox wget && cd tmp
         /wget http://192.168.1.254:8000/openwrt-mediatek-filogic-tplink_tl-7dr7250-v1-preloader.bin
         /wget http://192.168.1.254:8000/openwrt-mediatek-filogic-tplink_tl-7dr7250-v1-bl31-uboot.fip
         dd bs=131072 conv=sync of=/dev/mtdblock15 if=openwrt-mediatek-filogic-tplink_tl-7dr7250-v1-preloader.bin
         dd bs=131072 conv=sync of=/dev/mtdblock15 seek=44 if=openwrt-mediatek-filogic-tplink_tl-7dr7250-v1-bl31-uboot.fip
    
      5. Download the initramfs image, host it with the tftp server,
         and restart the router, waiting for tftp recovery to complete.
    
      6. After openwrt boots up, upload sysupgrade.bin to upgrade.
    
    Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -211,6 +211,7 @@ platform_do_upgrade() {
 	teralink,tl3020-256mb|\
 	tplink,be450-ubi|\
 	tplink,tl-7dr7230*|\
+	tplink,tl-7dr7250*|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
@@ -458,6 +459,7 @@ platform_check_image() {
 	teralink,tl3020-256mb|\
 	tplink,be450-ubi|\
 	tplink,tl-7dr7230*|\
+	tplink,tl-7dr7250*|\
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
