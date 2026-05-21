455d73619c27db7dacc0ef2e32f1d8ed1849f5c0
Jan-Henrik Bruhn <git@jhbruhn.de>
2026-05-21 23:24:37 +0200

    realtek: dts,build: create common Linksys LGS328x DTSI and image-recipe
    This is in preparation for the addition of the LGS328MPC, which is
    based on the LGS328C.
    
    It also drops the unused UBINIZE_OPTS, as UBI is only used during runtime
    of the firmware, not during build.
    
    Signed-off-by: Jan-Henrik Bruhn <git@jhbruhn.de>
    Link: https://github.com/openwrt/openwrt/pull/23466
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/realtek/image/rtl930x_nand.mk
+++ target/linux/realtek/image/rtl930x_nand.mk
@@ -2,19 +2,16 @@
 
 include ./common.mk
 
-define Device/linksys_lgs328c
+define Device/linksys_lgs328
   $(Device/uimage-rt-loader)
   SOC := rtl9301
   IMAGE_SIZE := 29696k
   KERNEL_SIZE := 10240k
   DEVICE_VENDOR := Linksys
-  DEVICE_MODEL := LGS328C
   BELKIN_MODEL := BKS-RTL93xx
   BELKIN_HEADER := 0x07600001
-  LINKSYS_MODEL := 60412040
   PAGESIZE := 2048
   BLOCKSIZE := 128k
-  UBINIZE_OPTS := -E 5
   KERNEL := \
 	$$(KERNEL) | \
 	pad-to $$(BLOCKSIZE)
@@ -23,5 +20,11 @@ define Device/linksys_lgs328c
 	pad-rootfs | \
 	sysupgrade-tar rootfs=$$$$@ | \
 	append-metadata
+endef
+
+define Device/linksys_lgs328c
+  $(Device/linksys_lgs328)
+  DEVICE_MODEL := LGS328C
+  LINKSYS_MODEL := 60412040
 endef
 TARGET_DEVICES += linksys_lgs328c
