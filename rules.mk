1af72b4121f8d1e88d82dd279b1436f1e42ffd55
S. Hoot <spoot_hoot@protonmail.com>
2026-05-22 12:10:01 -0400

    build: enable shared-host build tool paths for worktrees

--- rules.mk
+++ rules.mk
@@ -270,7 +270,7 @@ else
 endif
 
 export ORIG_PATH:=$(if $(ORIG_PATH),$(ORIG_PATH),$(PATH))
-export PATH:=$(TARGET_PATH)
+export PATH:=$(STAGING_DIR_HOST)/bin:$(TARGET_PATH)
 export STAGING_DIR STAGING_DIR_HOST STAGING_DIR_HOSTPKG
 export SH_FUNC:=. $(INCLUDE_DIR)/shell.sh;
 
