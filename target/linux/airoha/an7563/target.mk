487eafb9196395320105154b90a85243ce3bc5ab
Ray Liu <ray.xy.liu@gmail.com>
2026-05-23 17:20:25 +0800

    airoha: an7563: enable LuCI debug access
    Default AN7563 LAN to eth0 and include LuCI/uhttpd with the required web runtime packages for debug images.
    
    Signed-off-by: Ray Liu <ray.xy.liu@gmail.com>

--- target/linux/airoha/an7563/target.mk
+++ target/linux/airoha/an7563/target.mk
@@ -5,7 +5,18 @@ CPU_TYPE:=cortex-a7
 KERNELNAME:=Image dtbs
 FEATURES+=source-only
 DEPENDS+=@+BUSYBOX_DEFAULT_DEVMEM \
-	@+KERNEL_DEVMEM
+	@+KERNEL_DEVMEM \
+	@+PACKAGE_rpcd \
+	@+PACKAGE_rpcd-mod-file \
+	@+PACKAGE_rpcd-mod-luci \
+	@+PACKAGE_rpcd-mod-rrdns \
+	@+PACKAGE_rpcd-mod-ucode \
+	@+PACKAGE_uhttpd \
+	@+PACKAGE_uhttpd-mod-ubus \
+	@+PACKAGE_uhttpd-mod-ucode \
+	@+PACKAGE_ucode-mod-html \
+	@+PACKAGE_ucode-mod-log \
+	@+PACKAGE_ucode-mod-math
 
 define Target/Description
 	Build firmware images for Airoha an7563 (ARMv8 Cortex-A53 running
--- target/linux/airoha/image/an7563.mk
+++ target/linux/airoha/image/an7563.mk
@@ -23,6 +23,7 @@ define Device/airoha_an7563-evb
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7563 Evaluation Board
   DEVICE_DTS := an7563-evb
+  DEVICE_PACKAGES += luci uhttpd
   KERNEL_LOADADDR := 0x80088000
   ARTIFACT/preloader.bin := an7563-preloader rfb
   ARTIFACT/bl2-bl31-uboot.bin := an7563-bl2-bl31-uboot rfb
@@ -34,6 +35,7 @@ define Device/xiaomi_be5000
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := BE5000
   DEVICE_DTS := an7563-evb
+  DEVICE_PACKAGES += luci uhttpd
   KERNEL_LOADADDR := 0x80088000
   ARTIFACT/preloader.bin := an7563-preloader xiaomi_be5000
   ARTIFACT/bl2-bl31-uboot.bin := an7563-bl2-bl31-uboot xiaomi_be5000
