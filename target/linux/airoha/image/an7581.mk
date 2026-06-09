acfec78ef83826da6c9ef83620fbf466d977b7db
YaleiZang <yalei.zang@airoha.com>
2026-06-09 10:03:05 +0800

    airoha: add dtso support for an7581
    Enable dtso handling for dynamic hardware configuration and flexible device adaptation.
    
    Example (U-Boot):
    setenv bootcmd "ubi read 81800000 kernel; bootm 0x81800000#config@1#an7581-evb-nand-kite"
    
    Signed-off-by: YaleiZang <yalei.zang@airoha.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -83,6 +83,22 @@ define Device/airoha_an7581-evb-emmc-kite
 endef
 TARGET_DEVICES += airoha_an7581-evb-emmc-kite
 
+define Device/airoha_an7581-multievb
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Airoha
+  DEVICE_MODEL := AN7581 Evaluation Board (multievb)
+  DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-an7581 kmod-pwm-airoha kmod-input-gpio-keys-polled
+  DEVICE_DTS := an7581-multievb
+  DEVICE_DTS_OVERLAY := an7581-evb-nand-kite
+  DEVICE_DTS_CONFIG := config@1
+  IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
+  ARTIFACT/preloader.bin := an7581-preloader rfb
+  ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot rfb
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  DEVICE_DTC_FLAGS := --pad 20480
+endef
+TARGET_DEVICES += airoha_an7581-multievb
+
 define Device/gemtek_w1700k-ubi
   DEVICE_VENDOR := Gemtek
   DEVICE_MODEL := W1700K
