eb8cd69f9121196a3c43acdf9af3c372a4a50c93
Andrii Kuiukoff <andros.ua@gmail.com>
2026-06-18 13:56:23 +0300

    mediatek: creatlentem clt-r30b1: add all-in-UBI layout
    This commit introduces OpenWrt U-Boot all-in-ubi layout support
    for the CreatLentem CLT-R30B1, enabling:
    - Fully-featured U-Boot
    - Effective recovery mechanisms
    - Prolonged device lifetime by allocating most of the flash
      to UBI (which takes care of wear-leveling)
    - Maximum available storage space for OpenWrt
    - Unified firmware across different device variants
    
    OpenWrt U-Boot UBI flash instructions
    -------------------------------------
    
    Using the installer image
    -------------------------
    To simplify the installation process, this method uses a fork
    of Daniel Golle's (@dangowrt) UBI Installer
    https://github.com/dangowrt/owrt-ubi-installer
    
    1. Ensure your router is running the latest generic OpenWrt firmware.
       Upgrade it if necessary.
    2. Obtain the installer image:
       Build the installer from source
       https://github.com/andros-ua/owrt-ubi-installer/tree/clt-r30b1
       or download a prebuilt image from the
       https://github.com/andros-ua/owrt-ubi-installer/releases
    3. Flash the openwrt*creatlentem_clt-r30b1-ubi-initramfs-recovery-installer.itb
       image using sysupgrade.
    4. Wait for installation: the green status LED will blink rapidly,
       indicating that the all-in-UBI installer is running.
    5. Once the installation finishes,
       the status LED will turn solid white (lime) for 5 seconds.
    6. After the device reboots, perform a final sysupgrade using the
       openwrt*creatlentem_clt-r30b1-ubi-squashfs-sysupgrade.itb image.
    
    Return to stock layout
    ----------------------
    1. Flash openwrt*creatlentem_clt-r30b1(-112m)-initramfs-kernel.bin
       via sysupgrade
    2. Copy files to /tmp on the device via SCP:
       BL2.bin
       u-boot-env.bin
       Factory.bin
       FIP.bin
       openwrt*creatlentem_clt-r30b1(-112m)-squashfs-sysupgrade.bin
    3. Restore stock MTD partitions:
       apk add kmod-mtd-rw
       insmod mtd-rw i_want_a_brick=1
       mtd write /tmp/BL2.bin BL2
       mtd write /tmp/u-boot-env.bin u-boot-env
       mtd write /tmp/Factory.bin Factory
       mtd write /tmp/FIP.bin FIP
    4. Install the system:
       sysupgrade /tmp/*sysupgrade.bin
    
    Recovery
    --------
    Use mtk_uartboot to recover corrupted BL2 or FIP via UART:
    https://github.com/981213/mtk_uartboot
    
    Stock layout
    ----------------------------------------
    | dev:    size   erasesize  name       |
    | mtd0: 00100000 00020000 "BL2"        |
    | mtd1: 00080000 00020000 "u-boot-env" |
    | mtd2: 00200000 00020000 "Factory"    |
    | mtd3: 00200000 00020000 "FIP"        |
    | mtd4: 07000000 00020000 "ubi"        |
    ----------------------------------------
    
    OpenWrt U-Boot UBI layout
    ----------------------------------
    | dev:    size   erasesize  name |
    | mtd0: 00100000 00020000 "bl2"  |
    | mtd1: 07f00000 00020000 "ubi"  |
    ----------------------------------
    
    Signed-off-by: Andrii Kuiukoff <andros.ua@gmail.com>
    Link: #20666
    (cherry picked from commit e414ca4)
    Signed-off-by: Andrii Kuiukoff <andros.ua@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/23862
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -91,6 +91,7 @@ platform_do_upgrade() {
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
 	comfast,cf-wr632ax-ubootmod|\
+	creatlentem,clt-r30b1-ubi|\
 	cudy,tr3000-v1-ubootmod|\
 	cudy,wbr3000uax-v1-ubootmod|\
 	cudy,wr3000e-v1-ubootmod|\
@@ -295,6 +296,7 @@ platform_check_image() {
 	cmcc,a10-ubootmod|\
 	cmcc,rax3000m|\
 	comfast,cf-wr632ax-ubootmod|\
+	creatlentem,clt-r30b1-ubi|\
 	cudy,tr3000-v1-ubootmod|\
 	cudy,wbr3000uax-v1-ubootmod|\
 	cudy,wr3000e-v1-ubootmod|\
