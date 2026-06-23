0b6f2f542508304f1189c0a7fa382ef07c978730
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2026-06-23 12:43:44 -0300

    Update dts and scripts for en7523 targets

--- target/linux/airoha/en7523/base-files/lib/upgrade/platform.sh
+++ target/linux/airoha/en7523/base-files/lib/upgrade/platform.sh
@@ -5,9 +5,20 @@ platform_do_upgrade() {
   local board=$(board_name)
 
   case "$board" in
+  gemtek,w1700k-ubi |\
+  genexis,arcee |\
+  genexis,e650 |\
+  genexis,laxy |\
+  genexis,pixly_r1 |\
+  genexis,rodimus_r1 |\
+  genexis,zephyr |\
   tplink,ex530v-v1 |\
   tplink,xx230v-v1)
     fit_do_upgrade "$1"
+    ;;
+  mikrotik,e60iugs)
+    PART_NAME=firmware
+    nand_do_upgrade "$1"
     ;;
   *)
     nand_do_upgrade "$1"
