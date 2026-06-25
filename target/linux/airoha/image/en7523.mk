2c84fd8904381c26c2701b69b56cee5f71d85e3c
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2026-06-25 18:21:48 -0300

    airoha: en7523: draft Heimgard/Genexis devices

--- target/linux/airoha/image/en7523.mk
+++ target/linux/airoha/image/en7523.mk
@@ -10,6 +10,72 @@ define Device/airoha_en7523-evb
 endef
 TARGET_DEVICES += airoha_en7523-evb
 
+define Device/genexis_arcee
+  $(Device/FitImage)
+  DEVICE_VENDOR := Heimgard
+  DEVICE_MODEL := CX20
+  DEVICE_ALT0_VENDOR := Genexis
+  DEVICE_ALT0_MODEL := CX20
+  DEVICE_ALT1_VENDOR := Genexis
+  DEVICE_ALT1_MODEL := Arcee
+  DEVICE_PACKAGES += hostapd-mbedtls wpad-mbedtls wpa-supplicant-mbedtls \
+                     kmod-mt7915e kmod-mt7916-firmware
+endef
+TARGET_DEVICES += genexis_arcee
+
+define Device/genexis_laxy
+  $(Device/FitImage)
+  DEVICE_VENDOR := Heimgard
+  DEVICE_MODEL := Laxy
+  DEVICE_ALT0_VENDOR := Genexis
+  DEVICE_ALT0_MODEL := Laxy
+  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2 \
+                     hostapd-mbedtls wpad-mbedtls wpa-supplicant-mbedtls \
+                     kmod-mt7915e kmod-mt7916-firmware
+endef
+TARGET_DEVICES += genexis_laxy
+
+define Device/genexis_pixly_r1
+  $(Device/Uboot-FitImage)
+  DEVICE_VENDOR := Heimgard
+  DEVICE_MODEL := CX30
+  DEVICE_ALT0_VENDOR := Genexis
+  DEVICE_ALT0_MODEL := CX30
+  DEVICE_ALT1_VENDOR := Heimgard
+  DEVICE_ALT1_MODEL := Pixly R1
+  DEVICE_PACKAGES += hostapd-mbedtls wpad-mbedtls wpa-supplicant-mbedtls \
+                     kmod-mt7915e kmod-mt7916-firmware
+endef
+TARGET_DEVICES += genexis_pixly_r1
+
+define Device/genexis_rodimus_r1
+  $(Device/FitImage)
+  DEVICE_VENDOR := Heimgard
+  DEVICE_MODEL := HX40
+  DEVICE_ALT0_VENDOR := Genexis
+  DEVICE_ALT0_MODEL := HX40
+  DEVICE_ALT1_VENDOR := Genexis
+  DEVICE_ALT1_MODEL := Rodimus R1
+  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2 \
+                     hostapd-mbedtls wpad-mbedtls wpa-supplicant-mbedtls \
+                     kmod-mt7915e kmod-mt7916-firmware
+endef
+TARGET_DEVICES += genexis_rodimus_r1
+
+define Device/genexis_zephyr
+  $(Device/FitImage)
+  DEVICE_VENDOR := Heimgard
+  DEVICE_MODEL := HX30
+  DEVICE_ALT0_VENDOR := Genexis
+  DEVICE_ALT0_MODEL := HX30
+  DEVICE_ALT1_VENDOR := Genexis
+  DEVICE_ALT1_MODEL := Zephyr
+  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2 \
+                     hostapd-mbedtls wpad-mbedtls wpa-supplicant-mbedtls \
+                     kmod-mt7915e kmod-mt7916-firmware
+endef
+TARGET_DEVICES += genexis_zephyr
+
 define Device/mikrotik_e60iugs
   $(Device/FitImage)
   $(Device/BootUboot)
