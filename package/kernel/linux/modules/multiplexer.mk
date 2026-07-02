openwrt/openwrt

commit ac39016898818b9e2d1b729ab6a397cc139f8b89

kernel: modules: package MMIO mux driver
Create a package for MMIO-based multiplexer driver, used as a backend by
video-mux on i.MX7 SoCs.

Signed-off-by: Lech Perczak <lech.perczak@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/17606
Signed-off-by: Robert Marko <robert.marko@sartura.hr>

package/kernel/linux/modules/multiplexer.mk
@@ -32,3 +32,18 @@ define KernelPackage/mux-gpio/description
 endef
 
 $(eval $(call KernelPackage,mux-gpio))
+
+define KernelPackage/mux-mmio
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=MMIO/Regmap register bitfield-controlled Multiplexer controller
+  DEPENDS:=+kmod-mux-core
+  KCONFIG:=CONFIG_MUX_MMIO
+  FILES:=$(LINUX_DIR)/drivers/mux/mux-mmio.ko
+  AUTOLOAD:=$(call AutoProbe,mux-mmio)
+endef
+
+define KernelPackage/mux-mmio/description
+  Kernel module for the MMIO/Regmap multiplexer driver.
+endef
+
+$(eval $(call KernelPackage,mux-mmio))

commit e0bcee08288ae17a59d3a812e171268d3c998c90

kernel: modules: mux-mmio: add missing regmap-mmio dependency
mux-mmio.ko depends on regmap-mmio.ko at runtime, but the kmod package
was missing the dependency, causing build failures when the module is
included.

Fixes: ac3901689881 ("kernel: modules: package MMIO mux driver")
Signed-off-by: Rosen Penev <rosenp@gmail.com>

package/kernel/linux/modules/multiplexer.mk
@@ -36,7 +36,7 @@ $(eval $(call KernelPackage,mux-gpio))
 define KernelPackage/mux-mmio
   SUBMENU:=$(OTHER_MENU)
   TITLE:=MMIO/Regmap register bitfield-controlled Multiplexer controller
-  DEPENDS:=+kmod-mux-core
+  DEPENDS:=+kmod-mux-core +kmod-regmap-mmio
   KCONFIG:=CONFIG_MUX_MMIO
   FILES:=$(LINUX_DIR)/drivers/mux/mux-mmio.ko
   AUTOLOAD:=$(call AutoProbe,mux-mmio)