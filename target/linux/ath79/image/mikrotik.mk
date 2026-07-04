88b4ac949d65fcfd91ba68ac8c572f9feb1fe7ba
Rosen Penev <rosenp@gmail.com>
2026-07-03 18:20:25 -0700

    ath79: mikrotik: convert to DSA
    Use qca8k to drive the external switches on these devices.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/24066
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/ath79/image/mikrotik.mk
+++ target/linux/ath79/image/mikrotik.mk
@@ -95,7 +95,11 @@ define Device/mikrotik_routerboard-951g-2hnd
   $(Device/mikrotik_nand)
   SOC := ar9344
   DEVICE_MODEL := RouterBOARD 951G-2HnD
-  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2
+  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2 kmod-dsa-qca8k kmod-phy-qca83xx \
+	-swconfig -kmod-switch-ar8xxx
+  DEVICE_COMPAT_MESSAGE := \
+       Switched to DSA. Cannot preserve the network config.
+  DEVICE_COMPAT_VERSION := 2.0
   SUPPORTED_DEVICES += rb-951g-2hnd
 endef
 TARGET_DEVICES += mikrotik_routerboard-951g-2hnd
@@ -143,8 +147,12 @@ define Device/mikrotik_routerboard-962uigs-5hact2hnt
   SOC := qca9558
   DEVICE_MODEL := RouterBOARD 962UiGS-5HacT2HnT (hAP ac)
   DEVICE_PACKAGES += kmod-ath10k-ct ath10k-firmware-qca988x-ct kmod-usb2 \
-	kmod-i2c-gpio kmod-sfp
+	kmod-i2c-gpio kmod-sfp kmod-dsa-qca8k kmod-phy-qca83xx \
+	-swconfig -kmod-switch-ar8xxx
   IMAGE_SIZE := 16256k
+  DEVICE_COMPAT_MESSAGE := \
+       Switched to DSA. Cannot preserve the network config.
+  DEVICE_COMPAT_VERSION := 2.0
   SUPPORTED_DEVICES += rb-962uigs-5hact2hnt
 endef
 TARGET_DEVICES += mikrotik_routerboard-962uigs-5hact2hnt
