fd1c10ec303d89ce307ce2ec9b25761e6287da8c
Aleksei Kosmakov <a.kosmakov@nagtech.ru>
2026-07-27 12:52:46 +0500

    mediatek: filogic: add support for BO-RT622-G31-G2
    -----------------------Specifications---------------------------------:
    SoC: Filogic 820 MT7981B (1.3GHz)
    RAM: DDR3 256MB
    Flash: 128MiB Winbond SPI NAND
    WiFi: Mediatek MT7981b DBDC 802.11ax 2.4/5 GHz
    Ethernet: MT7531: 3x 1GbE LAN + 1x 1GbE WAN
    Two buttons: reset and mesh
    UART: 3.3V, TX, RX, GND / 115200 8N1
    DC power interface
    
    -------------------Install openwrt image-------------------------------:
    Power off the device.
    Press and hold the Reset button.
    Power on the device while keeping the Reset button pressed.
    Wait for the device to enter U-Boot recovery mode.
    Connect to the device via Ethernet (default IP: 192.168.1.1).
    Set your PC's IP to 192.168.1.x (e.g., 192.168.1.131).
    Open a web browser and navigate to http://192.168.1.1
    Upload and flash the OpenWrt firmware
    Wait for the flashing process to complete and the device to reboot.
    
    Signed-off-by: Aleksei Kosmakov <a.kosmakov@nagtech.ru>
    Link: https://github.com/openwrt/openwrt/pull/24440

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -865,6 +865,29 @@ define Device/bazis_ax3000wm
 endef
 TARGET_DEVICES += bazis_ax3000wm
 
+define Device/bo_rt622-g31-g2
+  DEVICE_VENDOR := BO
+  DEVICE_MODEL := RT622-G31-G2
+  DEVICE_DTS := mt7981b-bo-rt622-g31-g2
+  DEVICE_DTS_DIR := ../dts
+  # "bo-rt622-g31-g2" matches the vendor U-Boot DTS model (lowercased) checked
+  # by verify_model_fit during the recovery/upgrade path.
+  SUPPORTED_DEVICES += bo-rt622-g31-g2
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware
+  KERNEL_LOADADDR := 0x48080000
+  KERNEL := kernel-bin | lzma | \
+	snr-fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 117248k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += bo_rt622-g31-g2
+
 define Device/buffalo_wsr-3000ax4p
   DEVICE_VENDOR := BUFFALO
   DEVICE_MODEL := WSR-3000AX4P
