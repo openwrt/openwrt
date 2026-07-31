5727a8acebd98a1b8d660e8c4b7cc56e75c7946f
Joshua Covington <joshuacov@gmail.com>
2026-07-31 13:47:27 +0000

    bcm27xx: move USB HID/sound to opt-in meta-packages
    kmod-usb-hid, kmod-sound-core, and kmod-sound-arm-bcm2835 are
    currently forced onto every bcm27xx image regardless of use case.
    Move them into two new opt-in meta-packages, kmod-bcm27xx-hid and
    kmod-bcm27xx-sound, filed under the input and sound submenus
    respectively, following the same pattern as kmod-bcm27xx-i2c/-spi.
    Still available via apk or menuconfig, just no longer default.
    
    Signed-off-by: Joshua Covington <joshuacov@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/24244
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/bcm27xx/modules/other.mk
+++ target/linux/bcm27xx/modules/other.mk
@@ -139,3 +139,35 @@ define KernelPackage/rp1-mailbox/description
 endef
 
 $(eval $(call KernelPackage,rp1-mailbox))
+
+
+define KernelPackage/bcm27xx-hid
+  SUBMENU:=$(INPUT_MODULES_MENU)
+  TITLE:=USB HID support for bcm27xx boards
+  DEPENDS:=@TARGET_bcm27xx \
+	+kmod-usb-hid
+endef
+
+define KernelPackage/bcm27xx-hid/description
+ Pulls in USB Human Input Device (keyboard, mouse) support. Not
+ installed by default -- headless/router deployments don't need
+ it; install this if you want to attach a USB keyboard or mouse.
+endef
+
+$(eval $(call KernelPackage,bcm27xx-hid))
+
+
+define KernelPackage/bcm27xx-sound
+  SUBMENU:=$(SOUND_MENU)
+  TITLE:=Onboard audio support for bcm27xx boards
+  DEPENDS:=@TARGET_bcm27xx \
+	+kmod-sound-core +kmod-sound-arm-bcm2835
+endef
+
+define KernelPackage/bcm27xx-sound/description
+ Pulls in onboard audio support. Not installed by default --
+ headless/router deployments don't need it; install this if you
+ want sound output on this board.
+endef
+
+$(eval $(call KernelPackage,bcm27xx-sound))
