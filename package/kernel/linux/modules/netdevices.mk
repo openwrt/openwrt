bee48059af013df4e3a0f95c8960e70ee8fb3fbb
Shiji Yang <yangshiji66@outlook.com>
2026-08-13 19:01:50 +0800

    kernel: fix kmod-mdio-devres dependencies for econet target
    Add missing dependent package kmod-of-mdio to fix build error:
    
    Package kmod-mdio-devres is missing dependencies for the following libraries:
    of_mdio.ko
    
    Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
    Link: https://github.com/openwrt/openwrt/pull/24710
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    (cherry picked from commit 943bfe441c10dd2821f5aec1c0258a13809f51d7)
    Link: https://github.com/openwrt/openwrt/pull/24703
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -201,7 +201,7 @@ $(eval $(call KernelPackage,mii))
 define KernelPackage/mdio-devres
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Supports MDIO device registration
-  DEPENDS:=+kmod-libphy +(TARGET_armsr||TARGET_bcm27xx_bcm2708||TARGET_loongarch64||TARGET_malta||TARGET_tegra):kmod-of-mdio
+  DEPENDS:=+kmod-libphy +(TARGET_armsr||TARGET_bcm27xx_bcm2708||TARGET_econet||TARGET_loongarch64||TARGET_malta||TARGET_tegra):kmod-of-mdio
   KCONFIG:=CONFIG_MDIO_DEVRES
   HIDDEN:=1
   FILES:=$(LINUX_DIR)/drivers/net/phy/mdio_devres.ko
