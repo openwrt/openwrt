e4de94870370e0948987a18f6409c353c891e28e
Matt Eaton <git@divinehawk.com>
2026-08-14 20:39:02 +0200

    kernel: add softpwm/gpio-pwm backlight support for UTR
    Enable PWM and GPIO PWM support in the kernel and switch the UTR LCD
    backlight from gpio-backlight to pwm-backlight.
    
    Add a GPIO PWM provider using GPIO2 and configure the LCD backlight
    with a nonlinear brightness table (extracted from vendor firmware).
    
    Signed-off-by: Matt Eaton <git@divinehawk.com>
    Link: https://github.com/openwrt/openwrt/pull/22740
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/ipq40xx/image/generic.mk
+++ target/linux/ipq40xx/image/generic.mk
@@ -1326,7 +1326,7 @@ define Device/ubnt_utr
 	PAGESIZE := 2048
 	KERNEL_IN_UBI :=
 	UBINIZE_PARTS = vol=$$(KDIR_KERNEL_IMAGE)
-	DEVICE_PACKAGES := ipq-wifi-ubnt_utr kmod-i2c-gpio kmod-iio-st_accel-i2c kmod-drm-panel-mipi-dbi kmod-backlight-gpio kmod-btusb mipi-dbi-ubnt-utr
+	DEVICE_PACKAGES := ipq-wifi-ubnt_utr kmod-i2c-gpio kmod-iio-st_accel-i2c kmod-drm-panel-mipi-dbi kmod-backlight-pwm kmod-gpio-pwm kmod-btusb mipi-dbi-ubnt-utr
 endef
 TARGET_DEVICES += ubnt_utr
 
