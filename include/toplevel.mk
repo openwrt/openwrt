openwrt/openwrt

commit 9c88ba80b9d566ab7e3dc752fa68ae82aed54212

more build system cleanup
SVN-Revision: 8206

include/toplevel.mk
@@ -1,0 +1,132 @@
+# Makefile for OpenWrt
+#
+# Copyright (C) 2007 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+RELEASE:=Kamikaze
+#VERSION:=2.0 # uncomment for final release
+
+all: world
+
+SHELL:=/usr/bin/env bash
+OPENWRTVERSION:=$(RELEASE)
+ifneq ($(VERSION),)
+  OPENWRTVERSION:=$(VERSION) ($(OPENWRTVERSION))
+else
+  REV:=$(shell LANG=C svn info | awk '/^Revision:/ { print$$2 }' )
+  ifneq ($(REV),)
+    OPENWRTVERSION:=$(OPENWRTVERSION)/r$(REV)
+  endif
+endif
+export OPENWRTVERSION
+
+ifeq ($(FORCE),)
+  .config scripts/config/conf scripts/config/mconf: tmp/.prereq-build
+  world: prereq
+endif
+
+SCAN_COOKIE?=$(shell echo $$$$)
+export SCAN_COOKIE
+
+tmp/.packageinfo tmp/.targetinfo prepare-tmpinfo:
+	@mkdir -p tmp/info
+	@$(NO_TRACE_MAKE) -s -f include/scan.mk SCAN_TARGET="packageinfo" SCAN_DIR="package" SCAN_NAME="package" SCAN_DEPS="$(TOPDIR)/include/package*.mk" SCAN_DEPTH=4 SCAN_EXTRA=""
+	@$(NO_TRACE_MAKE) -s -f include/scan.mk SCAN_TARGET="targetinfo" SCAN_DIR="target/linux" SCAN_NAME="target" SCAN_DEPS="profiles/*.mk $(TOPDIR)/include/kernel*.mk" SCAN_DEPTH=2 SCAN_EXTRA=""
+	@for type in package target; do \
+		f=tmp/.$${type}info; t=tmp/.config-$${type}.in; \
+		[ "$$t" -nt "$$f" ] || ./scripts/metadata.pl $${type}_config < "$$f" > "$$t" || { rm -f "$$t"; echo "Failed to build $$t"; false; break; }; \
+	done
+
+.config: ./scripts/config/conf prepare-tmpinfo
+	if [ \! -f .config ]; then \
+		[ -e $(HOME)/.openwrt/defconfig ] && cp $(HOME)/.openwrt/defconfig .config; \
+		$(NO_TRACE_MAKE) menuconfig; \
+	fi
+	$< -D .config Config.in &> /dev/null
+
+scripts/config/mconf:
+	$(MAKE) -C scripts/config all
+
+scripts/config/conf:
+	$(MAKE) -C scripts/config conf
+
+config: scripts/config/conf prepare-tmpinfo FORCE
+	$< Config.in
+
+config-clean: FORCE
+	$(NO_TRACE_MAKE) -C scripts/config clean
+
+defconfig: scripts/config/conf prepare-tmpinfo FORCE
+	touch .config
+	$< -D .config Config.in
+
+oldconfig: scripts/config/conf prepare-tmpinfo FORCE
+	$< -o Config.in
+
+menuconfig: scripts/config/mconf prepare-tmpinfo FORCE
+	if [ \! -f .config -a -e $(HOME)/.openwrt/defconfig ]; then \
+		cp $(HOME)/.openwrt/defconfig .config; \
+	fi
+	$< Config.in
+
+kernel_oldconfig: .config FORCE
+	$(NO_TRACE_MAKE) -C target/linux oldconfig
+
+kernel_menuconfig: .config FORCE
+	$(NO_TRACE_MAKE) -C target/linux menuconfig
+
+tmp/.prereq-build: include/prereq-build.mk
+	mkdir -p tmp
+	rm -f tmp/.host.mk
+	$(NO_TRACE_MAKE) -s -f $(TOPDIR)/include/prereq-build.mk prereq 2>/dev/null || { \
+		echo "Prerequisite check failed. Use FORCE=1 to override."; \
+		false; \
+	}
+	touch $@
+
+tmp/.prereq-package: tmp/.packageinfo
+tmp/.prereq-target: tmp/.targetinfo .config
+tmp/.prereq-package tmp/.prereq-target: include/prereq.mk 
+	mkdir -p tmp
+	rm -f tmp/.host.mk
+	$(NO_TRACE_MAKE) -s -C $(patsubst tmp/.prereq-%,%,$@) prereq 2>/dev/null || { \
+		echo "Prerequisite check failed. Use FORCE=1 to override."; \
+		false; \
+	}
+	touch $@
+
+prereq: tmp/.prereq-build tmp/.prereq-package tmp/.prereq-target .config FORCE ;
+
+download: .config FORCE
+	$(MAKE) -j1 tools/download
+	$(MAKE) -j1 toolchain/download
+	$(MAKE) -j1 package/download
+	$(MAKE) -j1 target/download
+
+clean dirclean distclean:
+	@$(MAKE) $@ 
+
+%::
+	@$(SUBMAKE) -s prereq QUIET=0 OPENWRT_BUILD=
+	@$(MAKE) $@ 
+
+help:
+	cat README
+
+docs docs/compile: FORCE
+	$(MAKE) -C docs compile
+
+docs/clean: FORCE
+	$(MAKE) -C docs clean
+
+symlinkclean:
+	-find package -type l | xargs rm -f
+	rm -rf tmp
+
+.SILENT: symlinkclean clean dirclean distclean config-clean download help tmpinfo-clean .config scripts/config/mconf scripts/config/conf menuconfig tmp/.prereq-build tmp/.prereq-package tmp/.prereq-target
+.PHONY: help FORCE
+.NOTPARALLEL:
+

Makefile
@@ -7,139 +7,24 @@
 # See /LICENSE for more information.
 #
 
-RELEASE:=Kamikaze
-#VERSION:=2.0 # uncomment for final release
-
-#--------------------------------------------------------------
-# Just run 'make menuconfig', configure stuff, then run 'make'.
-# You shouldn't need to mess with anything beyond this point...
-#--------------------------------------------------------------
-
 all: world
 
-SHELL:=/usr/bin/env bash
-export LC_ALL=C
-export LANG=C
-export TOPDIR=${CURDIR}
-export IS_TTY=$(shell tty -s && echo 1 || echo 0)
 
-include ./rules.mk 
-include $(INCLUDE_DIR)/depends.mk
-include $(INCLUDE_DIR)/subdir.mk
-include tools/Makefile
+TOPDIR:=${CURDIR}
+LC_ALL:=C
+LANG:=C
+IS_TTY:=${shell tty -s && echo 1 || echo 0}
+export TOPDIR LC_ALL LANG IS_TTY
 
-OPENWRTVERSION:=$(RELEASE)
-ifneq ($(VERSION),)
-  OPENWRTVERSION:=$(VERSION) ($(OPENWRTVERSION))
+include rules.mk
+
+ifneq ($(OPENWRT_BUILD),1)
+  export OPENWRT_BUILD:=1
+  include $(INCLUDE_DIR)/toplevel.mk
 else
-  REV:=$(shell LANG=C svn info | awk '/^Revision:/ { print$$2 }' )
-  ifneq ($(REV),)
-    OPENWRTVERSION:=$(OPENWRTVERSION)/r$(REV)
-  endif
-endif
-export OPENWRTVERSION
-
-ifeq ($(FORCE),)
-  .config scripts/config/conf scripts/config/mconf: tmp/.prereq-build
-  world: tmp/.prereq-package tmp/.prereq-target
-endif
-
-package/%/Makefile: ;
-target/%/Makefile: ;
-
-SCAN_COOKIE?=$(shell echo $$$$)
-export SCAN_COOKIE
-
-tmp/.packageinfo tmp/.targetinfo: FORCE
-	mkdir -p tmp/info
-	$(NO_TRACE_MAKE) -s -f include/scan.mk SCAN_TARGET="targetinfo" SCAN_DIR="target/linux" SCAN_NAME="target" SCAN_DEPS="profiles/*.mk $(TOPDIR)/include/kernel*.mk" SCAN_DEPTH=2 SCAN_EXTRA=""
-	$(NO_TRACE_MAKE) -s -f include/scan.mk SCAN_TARGET="packageinfo" SCAN_DIR="package" SCAN_NAME="package" SCAN_DEPS="$(TOPDIR)/include/package*.mk" SCAN_DEPTH=4 SCAN_EXTRA=""
-
-tmpinfo-clean: FORCE
-	-rm -rf tmp/.*info
-
-tmp/.config-%.in: tmp/.%info scripts/metadata.pl
-	./scripts/metadata.pl $*_config < $< > $@ || rm -f $@
-
-.config: ./scripts/config/conf tmp/.config-target.in tmp/.config-package.in
-	if [ \! -f .config ]; then \
-		[ -e $(HOME)/.openwrt/defconfig ] && cp $(HOME)/.openwrt/defconfig .config; \
-		$(NO_TRACE_MAKE) menuconfig; \
-	fi
-	$< -D .config Config.in &> /dev/null
-
-scripts/config/mconf:
-	$(MAKE) -C scripts/config all
-
-scripts/config/conf:
-	$(MAKE) -C scripts/config conf
-
-
-
-config: scripts/config/conf tmp/.config-target.in tmp/.config-package.in FORCE
-	$< Config.in
-
-config-clean: FORCE
-	$(NO_TRACE_MAKE) -C scripts/config clean
-
-defconfig: scripts/config/conf tmp/.config-target.in tmp/.config-package.in FORCE
-	touch .config
-	$< -D .config Config.in
-
-oldconfig: scripts/config/conf tmp/.config-target.in tmp/.config-package.in FORCE
-	$< -o Config.in
-
-menuconfig: scripts/config/mconf tmp/.config-target.in tmp/.config-package.in FORCE
-	if [ \! -f .config -a -e $(HOME)/.openwrt/defconfig ]; then \
-		cp $(HOME)/.openwrt/defconfig .config; \
-	fi
-	$< Config.in
-
-kernel_oldconfig: .config FORCE
-	$(NO_TRACE_MAKE) -C target/linux oldconfig
-
-kernel_menuconfig: .config FORCE
-	$(NO_TRACE_MAKE) -C target/linux menuconfig
-
-package/% target/%: tmp/.packageinfo
-toolchain/% package/% target/%: tmp/.targetinfo
-package/% target/% toolchain/%: FORCE
-	$(MAKE) -C $(patsubst %/$*,%,$@) $*
-
-
-tmp/.prereq-build: include/prereq-build.mk
-	mkdir -p tmp
-	rm -f tmp/.host.mk
-	$(NO_TRACE_MAKE) -s -f $(TOPDIR)/include/prereq-build.mk prereq 2>/dev/null || { \
-		echo "Prerequisite check failed. Use FORCE=1 to override."; \
-		false; \
-	}
-	touch $@
-
-tmp/.prereq-%: include/prereq.mk tmp/.%info .config
-	mkdir -p tmp
-	rm -f tmp/.host.mk
-	$(NO_TRACE_MAKE) -s -C $* prereq 2>/dev/null || { \
-		echo "Prerequisite check failed. Use FORCE=1 to override."; \
-		false; \
-	}
-	touch $@
-
-prereq: tmp/.prereq-build tmp/.prereq-package tmp/.prereq-target FORCE
-
-download: .config FORCE
-	$(MAKE) tools/download
-	$(MAKE) toolchain/download
-	$(MAKE) package/download
-	$(MAKE) target/download
-
-world: .config $(tools/stamp) FORCE
-	$(MAKE) toolchain/install
-	$(MAKE) target/compile
-	$(MAKE) package/compile
-	$(MAKE) package/install
-	$(MAKE) target/install
-	$(MAKE) package/index
+  include $(INCLUDE_DIR)/depends.mk
+  include $(INCLUDE_DIR)/subdir.mk
+  include tools/Makefile
 
 clean: FORCE
 	rm -rf build_* bin tmp
@@ -150,18 +35,15 @@ dirclean: clean
 distclean: dirclean config-clean symlinkclean docs/clean
 	rm -rf dl
 
-help:
-	cat README
+toolchain/% package/% target/%: FORCE
+	$(MAKE) -C $(patsubst %/$*,%,$@) $*
 
-docs docs/compile: FORCE
-	$(MAKE) -C docs compile
+world: .config $(tools/stamp) FORCE
+	$(MAKE) toolchain/install
+	$(MAKE) target/compile
+	$(MAKE) package/compile
+	$(MAKE) package/install
+	$(MAKE) target/install
+	$(MAKE) package/index
 
-docs/clean: FORCE
-	$(MAKE) -C docs clean
-
-symlinkclean:
-	-find package -type l | xargs rm -f
-	rm -rf tmp
-
-.SILENT: clean dirclean distclean symlinkclean config-clean download world help tmp/.packageinfo tmp/.targetinfo tmpinfo-clean tmp/.config-package.in tmp/.config-target.in .config scripts/config/mconf scripts/config/conf menuconfig tmp/.prereq-build tmp/.prereq-package tmp/.prereq-target
-.PHONY: help
+endif

include/scan.mk
@@ -68,3 +68,4 @@ tmp/.$(SCAN_TARGET): $(TARGET_STAMP) $(SCAN_STAMP)
 
 FORCE:
 .PHONY: FORCE
+.NOTPARALLEL:

include/subdir.mk
@@ -32,7 +32,7 @@ define subdir
     $(call warn,$(1),d,BD $(1)/$(bd))
     $(foreach target,$(SUBTARGETS),
       $(call warn_eval,$(1)/$(bd),t,T,$(1)/$(bd)/$(target): $($(1)/$(bd)/$(target)) $(call $(1)//$(target),$(1)/$(bd)))
-		@$$(MAKE) -j1 -C $(1)/$(bd) $(target)
+		@+$$(MAKE) -j1 -C $(1)/$(bd) $(target)
 
       # legacy targets
       $(call warn_eval,$(1)/$(bd),l,T,$(1)/$(bd)-$(target): $(1)/$(bd)/$(target))
@@ -49,7 +49,7 @@ define stampfile
   $(call rdep,$(1),$$($(1)/stamp))
 
   $$($(1)/stamp):
-	@$(MAKE) $(1)/install
+	@+$(MAKE) $(1)/install
 	@mkdir -p $$$$(dirname $$($(1)/stamp))
 	@touch $$($(1)/stamp)
   .PRECIOUS: $$($(1)/stamp) # work around a make bug

include/verbose.mk
@@ -8,7 +8,9 @@
 
 ifeq ($(NO_TRACE_MAKE),)
 NO_TRACE_MAKE := $(MAKE) V=99
+SUBMAKE := $(MAKE)
 export NO_TRACE_MAKE
+export SUBMAKE
 endif
 
 ifndef KBUILD_VERBOSE
@@ -29,18 +31,20 @@ endef
 
 ifneq ($(KBUILD_VERBOSE),99)
   ifeq ($(QUIET),1)
-    $(MAKECMDGOALS): trace
-    trace: FORCE
-	@[ -f "$(MAKECMDGOALS)" ] || { \
-		[ -z "$${PWD##$$TOPDIR}" ] || DIR=" -C $${PWD##$$TOPDIR/}"; \
-		$(call MESSAGE, "make[$$(($(MAKELEVEL)+1))]$$DIR $(MAKECMDGOALS)"); \
-	}
+    ifneq ($(CURDIR),$(TOPDIR))
+      _DIR:=$(patsubst $(TOPDIR)/%,%,${CURDIR})
+    else
+      _DIR:=
+    endif
+    _NULL:=$(if $(MAKECMDGOALS),$(shell \
+		$(call MESSAGE, "make[$(MAKELEVEL)]$(if $(_DIR), -C $(_DIR)) $(MAKECMDGOALS)"); \
+    ))
   else
-    export QUIET:=1
     ifeq ($(KBUILD_VERBOSE),0)
       MAKE:=&>/dev/null $(MAKE)
     endif
-    MAKE:=cmd() { $(MAKE) $$* || {  echo "Build failed. Please re-run make with V=99 to see what's going on"; false; } } 3>&1 4>&2; cmd
+    export QUIET:=1
+    MAKE:=cmd() { $(MAKE) $$* || {  echo "make $$*: build failed. Please re-run make with V=99 to see what's going on"; false; } } 3>&1 4>&2; cmd
   endif
 
   .SILENT: $(MAKECMDGOALS)

rules.mk
@@ -138,4 +138,3 @@ endef
 all:
 FORCE: ;
 .PHONY: FORCE
-%: ;

commit a9e882765278a75b8cd99511fcd24419f069f2fa

build: add build target to clean tmp info
Add a build target tmpclean, similar to the original
tmpinfo-clean which was removed instead of moved.

Fixes: 9c88ba80b9d5 ("more build system cleanup")
Signed-off-by: Michael Pratt <mcpratt@pm.me>

include/toplevel.mk
@@ -91,6 +91,17 @@ prepare-tmpinfo: FORCE
 	./scripts/package-metadata.pl usergroup tmp/.packageinfo > tmp/.packageusergroup || { rm -f tmp/.packageusergroup; false; }
 	touch $(TOPDIR)/tmp/.build
 
+tmpclean: FORCE
+	rm -f $(PREP_MK)
+	rm -f $(STAGING_DIR_HOST)/.prereq-build
+	rm -rf $(TOPDIR)/tmp/info
+	rm -f $(TOPDIR)/tmp/.config-package.in
+	rm -f $(TOPDIR)/tmp/.config-target.in
+	rm -f $(TOPDIR)/tmp/.config-feeds.in
+	rm -f $(TOPDIR)/tmp/.package*
+	rm -f $(TOPDIR)/tmp/.target*
+	rm -f $(TOPDIR)/tmp/.build
+
 .config: ./scripts/config/conf $(if $(CONFIG_HAVE_DOT_CONFIG),,prepare-tmpinfo)
 	@+if [ \! -e .config ] || ! grep CONFIG_HAVE_DOT_CONFIG .config >/dev/null; then \
 		[ -e $(HOME)/.openwrt/defconfig ] && cp $(HOME)/.openwrt/defconfig .config; \
@@ -114,7 +125,7 @@ config: scripts/config/conf prepare-tmpinfo FORCE
 	[ -L .config ] && export KCONFIG_OVERWRITECONFIG=1; \
 		$< $(KCONF_FLAGS) Config.in
 
-config-clean: FORCE
+config-clean: tmpclean FORCE
 	$(_SINGLE)$(NO_TRACE_MAKE) -C scripts/config clean
 
 defconfig: scripts/config/conf prepare-tmpinfo FORCE
@@ -265,7 +276,7 @@ distclean:
 	@$(_SINGLE)$(SUBMAKE) -C scripts/config clean
 
 ifeq ($(findstring v,$(DEBUG)),)
-  .SILENT: symlinkclean clean dirclean distclean config-clean download help tmpinfo-clean .config scripts/config/mconf scripts/config/conf menuconfig $(STAGING_DIR_HOST)/.prereq-build tmp/.prereq-package prepare-tmpinfo
+  .SILENT: symlinkclean clean dirclean distclean config-clean download help tmpclean .config scripts/config/mconf scripts/config/conf menuconfig $(STAGING_DIR_HOST)/.prereq-build tmp/.prereq-package prepare-tmpinfo
 endif
 .PHONY: help FORCE
 .NOTPARALLEL: