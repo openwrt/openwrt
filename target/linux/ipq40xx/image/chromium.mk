74dc557c34681eb793ad399844704a1e7e951d08
Matt Eaton <git@divinehawk.com>
2026-08-20 11:27:05 +0200

    ipq40xx: switch to kernel modules to reduce kernel size
    Multiple devices are limited on kernel size due to the kernel
    partition.
    
    Modularize multiple drivers to reduce kernel size.
    
    Signed-off-by: Matt Eaton <git@divinehawk.com>

--- target/linux/ipq40xx/image/chromium.mk
+++ target/linux/ipq40xx/image/chromium.mk
@@ -37,6 +37,6 @@ define Device/google_wifi
 	# kmod-ramoops.
 	DEVICE_PACKAGES := partx-utils mkf2fs e2fsprogs \
 			   kmod-fs-ext4 kmod-fs-f2fs kmod-google-firmware \
-			   kmod-ramoops
+			   kmod-leds-lp5523 kmod-ramoops
 endef
 TARGET_DEVICES += google_wifi
