openwrt/openwrt

commit 1446eff88786abf08f94a3939014618f5bc6b255

mediatek: filogic: bpi-r4-pro-8x: add aeonsemi phy to bpi-r4-pro-8x
Add the two external Aeonsemi AS21xxx PHYs used by the LAN and WAN
10GBASE-T ports of the BPI-R4 Pro 8x. Configure their firmware, reset
timing and LEDs, and connect them to the MxL switch and GMAC1.

Replace the fixed SFP GPIO hog configuration with LAN and WAN overlays
selecting either the copper PHY or the corresponding SFP cage. Build all
four overlays, include them in the device image and select the SFP
overlays by default through U-Boot `bootconf_extra`.

Make the AS21xxx firmware package pull in the PHY driver on Linux 6.18,
instead of making the driver depend on the firmware. Update existing
Airoha device package lists accordingly so they continue to install both
components through the firmware package.

Breaking change for users using the router with SFP hogs from PR #21083.

After flashing the new image, configure `bootconf_extra` to select
between SFP or PHY overlays:

fw_setenv bootconf_extra \
mt7988a-bananapi-bpi-r4-pro-8x-lan-phy#mt7988a-bananapi-bpi-r4-pro-8x-wan-phy

If bootconf_extra had additional overlays (CN13 or CN14) keep them appended

Signed-off-by: Pietro Ameruoso <p.ameruoso@live.it>
Signed-off-by: Frank Wunderlich <frank-w@public-files.de>

package/firmware/linux-firmware/aeonsemi.mk
@@ -1,4 +1,4 @@
-Package/aeonsemi-as21xxx-firmware = $(call Package/firmware-default,Aeonsemi AS21xxx Ethernet PHY firmware,,LICENSE.aeonsemi)
+Package/aeonsemi-as21xxx-firmware = $(call Package/firmware-default,Aeonsemi AS21xxx Ethernet PHY firmware, +LINUX_6_18:kmod-phy-aeonsemi-as21xxx,LICENSE.aeonsemi)
 define Package/aeonsemi-as21xxx-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/aeonsemi
 	$(CP) \

package/boot/uboot-mediatek/patches/472-add-bpi-r4-pro-8x.patch
@@ -474,7 +474,7 @@
 +bootcmd=if pstore check ; then run boot_recovery ; else run boot_emmc ; fi
 +bootconf=config-mt7988a-bananapi-bpi-r4-pro-8x
 +bootconf_emmc=mt7988a-bananapi-bpi-r4-pro-emmc
-+bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14
++bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14#mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp#mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp
 +bootdelay=0
 +bootfile=openwrt-mediatek-filogic-bananapi_bpi-r4-pro-8x-initramfs-recovery.itb
 +bootfile_bl2=openwrt-mediatek-filogic-bananapi_bpi-r4-pro-8x-emmc-preloader.bin
@@ -533,7 +533,7 @@
 +bootcmd=if pstore check ; then run boot_recovery ; else run boot_sdmmc ; fi
 +bootconf=config-mt7988a-bananapi-bpi-r4-pro-8x
 +bootconf_sd=mt7988a-bananapi-bpi-r4-pro-sd
-+bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14
++bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14#mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp#mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp
 +bootdelay=0
 +bootfile=openwrt-mediatek-filogic-bananapi_bpi-r4-pro-8x-initramfs-recovery.itb
 +bootfile_upg=openwrt-mediatek-filogic-bananapi_bpi-r4-pro-8x-squashfs-sysupgrade.itb
@@ -600,7 +600,7 @@
 +bootargs=console=ttyS0,115200n1 pci=pcie_bus_perf root=/dev/fit0 rootwait ubi.block=0,fit
 +bootconf=config-mt7988a-bananapi-bpi-r4-pro-8x
 +bootconf_emmc=mt7988a-bananapi-bpi-r4-pro-emmc
-+bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14
++bootconf_extra=mt7988a-bananapi-bpi-r4-pro-cn13#mt7988a-bananapi-bpi-r4-pro-cn14#mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp#mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp
 +bootcmd=if pstore check ; then run boot_recovery ; else run boot_ubi ; fi
 +bootdelay=0
 +bootfile=openwrt-mediatek-filogic-bananapi_bpi-r4-pro-8x-initramfs-recovery.itb

package/kernel/linux/modules/netdevices.mk
@@ -626,7 +626,7 @@ $(eval $(call KernelPackage,phy-vitesse))
 define KernelPackage/phy-aeonsemi-as21xxx
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Aeonsemi AS21xxx 10G Ethernet PHY
-  DEPENDS:=+aeonsemi-as21xxx-firmware +kmod-libphy
+  DEPENDS:=+kmod-libphy
   KCONFIG:=CONFIG_AS21XXX_PHY
   FILES:= \
    $(LINUX_DIR)/drivers/net/phy/as21xxx.ko

target/linux/airoha/image/an7581.mk
@@ -134,7 +134,7 @@ define Device/nokia_valyrian
   DEVICE_DTS := an7581-nokia-valyrian
   DEVICE_PACKAGES := kmod-spi-gpio kmod-gpio-nxp-74hc164 kmod-leds-gpio \
     kmod-i2c-gpio kmod-iio-richtek-rtq6056 \
-    kmod-sfp kmod-phy-aeonsemi-as21xxx \
+    kmod-sfp aeonsemi-as21xxx-firmware \
     kmod-mt7996-firmware airoha-en7581-mt7996-npu-firmware \
     kmod-usb3
   ARTIFACT/preloader.bin := an7581-preloader nokia_valyrian

target/linux/airoha/image/an7583.mk
@@ -24,7 +24,7 @@ define Device/airoha_an7583-evb
   $(call Device/FitImageLzma)
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7583 Evaluation Board (SNAND)
-  DEVICE_PACKAGES := kmod-phy-aeonsemi-as21xxx kmod-leds-pwm \
+  DEVICE_PACKAGES := aeonsemi-as21xxx-firmware kmod-leds-pwm \
 	kmod-pwm-airoha kmod-input-gpio-keys-polled
   DEVICE_DTS := an7583-evb
   DEVICE_DTS_CONFIG := config@1

target/linux/mediatek/image/filogic.mk
@@ -818,7 +818,9 @@ define Device/bananapi_bpi-r4-pro-8x
   DEVICE_DTS_CONFIG := config-mt7988a-bananapi-bpi-r4-pro-8x
   DEVICE_BL2 := comb-4bg
   $(call Device/bananapi_bpi-r4-pro-common)
-  DEVICE_PACKAGES += kmod-phy-aeonsemi-as21xxx
+  DEVICE_PACKAGES += aeonsemi-as21xxx-firmware
+  DEVICE_DTS_OVERLAY += mt7988a-bananapi-bpi-r4-pro-8x-lan-phy mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp \
+			mt7988a-bananapi-bpi-r4-pro-8x-wan-phy mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp
 endef
 TARGET_DEVICES += bananapi_bpi-r4-pro-8x
 

target/linux/mediatek/patches-6.18/968-arm64-dts-mediatek-mt7988a-bpi-r4-pro-add-lan-wan-overlays.patch
@@ -1,0 +1,159 @@
+--- a/arch/arm64/boot/dts/mediatek/Makefile
++++ b/arch/arm64/boot/dts/mediatek/Makefile
+@@ -27,6 +27,10 @@ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-b
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-emmc.dtbo
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-4e.dtb
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-8x.dtb
++dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-8x-lan-phy.dtbo
++dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp.dtbo
++dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-8x-wan-phy.dtbo
++dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp.dtbo
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-cn13.dtbo
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-cn14.dtbo
+ dtb-$(CONFIG_ARCH_MEDIATEK) += mt7988a-bananapi-bpi-r4-pro-cn15.dtbo
+--- /dev/null
++++ b/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro-8x-lan-phy.dtso
+@@ -0,0 +1,36 @@
++// SPDX-License-Identifier: (GPL-2.0 OR MIT)
++/*
++ * Copyright (C) 2025 MediaTek Inc.
++ * Author: Frank Wunderlich <frank-w@public-files.de>
++ */
++
++/* This enables LAN-PHY on BPI-R4-Pro */
++
++/dts-v1/;
++/plugin/;
++
++#include <dt-bindings/gpio/gpio.h>
++
++/ {
++	compatible = "bananapi,bpi-r4-pro", "mediatek,mt7988a";
++};
++
++&{/soc/ethernet@15100000/mdio-bus/switch@16/ports} {
++	#address-cells = <1>;
++	#size-cells = <0>;
++	port@13 {
++		reg = <13>;
++		managed = "in-band-status";
++		phy-mode = "usxgmii";
++		phy-handle = <&phy24>;
++		status = "okay";
++	};
++};
++
++&{/soc/pinctrl@1001f000} {
++	lan-phy-hog {
++		gpio-hog;
++		gpios = <54 GPIO_ACTIVE_HIGH>;
++		output-high; /* select phy */
++	};
++};
+--- /dev/null
++++ b/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro-8x-lan-sfp.dtso
+@@ -0,0 +1,36 @@
++// SPDX-License-Identifier: (GPL-2.0 OR MIT)
++/*
++ * Copyright (C) 2025 MediaTek Inc.
++ * Author: Frank Wunderlich <frank-w@public-files.de>
++ */
++
++/* This enables LAN-SFP on BPI-R4-Pro */
++
++/dts-v1/;
++/plugin/;
++
++#include <dt-bindings/gpio/gpio.h>
++
++/ {
++	compatible = "bananapi,bpi-r4-pro", "mediatek,mt7988a";
++};
++
++&{/soc/ethernet@15100000/mdio-bus/switch@16/ports} {
++	#address-cells = <1>;
++	#size-cells = <0>;
++	port@13 {
++		reg = <13>;
++		managed = "in-band-status";
++		phy-mode = "usxgmii";
++		sfp = <&sfp1>;
++		status = "okay";
++	};
++};
++
++&{/soc/pinctrl@1001f000} {
++	lan-sfp-hog {
++		gpio-hog;
++		gpios = <54 GPIO_ACTIVE_HIGH>;
++		output-low; /* select sfp */
++	};
++};
+--- /dev/null
++++ b/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro-8x-wan-phy.dtso
+@@ -0,0 +1,31 @@
++// SPDX-License-Identifier: (GPL-2.0 OR MIT)
++/*
++ * Copyright (C) 2025 MediaTek Inc.
++ * Author: Frank Wunderlich <frank-w@public-files.de>
++ */
++
++/* This enables WAN-PHY on BPI-R4-Pro */
++
++/dts-v1/;
++/plugin/;
++
++#include <dt-bindings/gpio/gpio.h>
++
++/ {
++	compatible = "bananapi,bpi-r4-pro", "mediatek,mt7988a";
++};
++
++&{/soc/ethernet@15100000/mac@1} {
++	managed = "in-band-status";
++	phy-handle = <&phy28>;
++	phy-mode = "usxgmii";
++	status = "okay";
++};
++
++&{/soc/pinctrl@1001f000} {
++	wan-phy-hog {
++		gpio-hog;
++		gpios = <3 GPIO_ACTIVE_HIGH>;
++		output-low; /* select phy */
++	};
++};
+--- /dev/null
++++ b/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro-8x-wan-sfp.dtso
+@@ -0,0 +1,31 @@
++// SPDX-License-Identifier: (GPL-2.0 OR MIT)
++/*
++ * Copyright (C) 2025 MediaTek Inc.
++ * Author: Frank Wunderlich <frank-w@public-files.de>
++ */
++
++/* This enables WAN-SFP on BPI-R4-Pro */
++
++/dts-v1/;
++/plugin/;
++
++#include <dt-bindings/gpio/gpio.h>
++
++/ {
++	compatible = "bananapi,bpi-r4-pro", "mediatek,mt7988a";
++};
++
++&{/soc/ethernet@15100000/mac@1} {
++	sfp = <&sfp2>;
++	managed = "in-band-status";
++	phy-mode = "usxgmii";
++	status = "okay";
++};
++
++&{/soc/pinctrl@1001f000} {
++	wan-sfp-hog {
++		gpio-hog;
++		gpios = <3 GPIO_ACTIVE_HIGH>;
++		output-high; /* select sfp */
++	};
++};

target/linux/mediatek/patches-6.18/968-arm64-dts-mediatek-mt7988a-bpi-r4-pro-add-sfp-hog.patch
@@ -1,62 +1,0 @@
-From: Frank Wunderlich <frank-w@public-files.de>
-Date: Sun, 15 Feb 2026 00:00:00 +0200
-Subject: [PATCH] mediatek: add gpio-hog for bananapi bpi r4 pro sfp
-
-Add gpio-hog for BananPi BPi R4 Pro SFP.
-
-Signed-off-by: Frank Wunderlich <frank-w@public-files.de>
----
---- a/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro.dtsi
-+++ b/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4-pro.dtsi
-@@ -191,6 +191,14 @@
- 	status = "okay";
- };
- 
-+&gmac1 {
-+	sfp = <&sfp2>;
-+	managed = "in-band-status";
-+	phy-mode = "usxgmii";
-+	openwrt,netdev-name = "wan";
-+	status = "okay";
-+};
-+
- &gmac2 {
- 	phy-mode = "10gbase-r";
- 	phy-connection-type = "10gbase-r";
-@@ -260,6 +268,15 @@
- 					full-duplex;
- 				};
- 			};
-+
-+			port@13 {
-+				reg = <13>;
-+				sfp = <&sfp1>;
-+				managed = "in-band-status";
-+				phy-mode = "usxgmii";
-+				label = "lan6";
-+				status = "okay";
-+			};
- 		};
- 
- 		mdio {
-@@ -285,6 +302,20 @@
- 	};
- };
- 
-+&pio {
-+	sfp-lan-hog {
-+		gpio-hog;
-+		gpios = <54 GPIO_ACTIVE_HIGH>;
-+		output-low; /* select sfp */
-+	};
-+
-+	sfp-wan-hog {
-+		gpio-hog;
-+		gpios = <3 GPIO_ACTIVE_HIGH>;
-+		output-high; /* select sfp */
-+	};
-+};
-+
- &gsw_phy0 {
- 	pinctrl-0 = <&gbe0_led0_pins>;
- 	pinctrl-names = "gbe-led";

commit 897533e4886e008e994980ea1339cff44e5a405e

linux-firmware: mark aeonsemi-as21xxx-firmware as 'nonshared'
In order for the firmware package to depend on the corresponding kmod it
needs to be built within the same builtbot phase for the dependency to
land in the package metadata. Select 'nonshared' to force the firmware to
also get built as part of phase1.

Fixes: 1446eff887 ("mediatek: filogic: bpi-r4-pro-8x: add aeonsemi phy to bpi-r4-pro-8x")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/firmware/linux-firmware/aeonsemi.mk
@@ -1,4 +1,4 @@
-Package/aeonsemi-as21xxx-firmware = $(call Package/firmware-default,Aeonsemi AS21xxx Ethernet PHY firmware, +LINUX_6_18:kmod-phy-aeonsemi-as21xxx,LICENSE.aeonsemi)
+Package/aeonsemi-as21xxx-firmware = $(call Package/firmware-default,Aeonsemi AS21xxx Ethernet PHY firmware, +LINUX_6_18:kmod-phy-aeonsemi-as21xxx,LICENSE.aeonsemi,,nonshared)
 define Package/aeonsemi-as21xxx-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/aeonsemi
 	$(CP) \