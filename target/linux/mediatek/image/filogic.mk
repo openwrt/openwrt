e3ef8dc942255ee8a711d1f0834cf0c4fc8a00e8
Andrii Kuiukoff <andros.ua@gmail.com>
2026-09-11 16:17:30 +0300

    mediatek: filogic: comfast cf-wr631ax: add all-in-UBI layout
    This commit introduces OpenWrt U-Boot all-in-UBI layout support
    for the COMFAST CF-WR631AX, enabling:
    - Prolonged device lifetime by allocating most of the flash
      to UBI (which takes care of wear-leveling)
    - Maximum available storage space for OpenWrt
    
    OpenWrt U-Boot UBI flash instructions
    -------------------------------------
    A device running stock firmware should be upgraded to the
    latest OpenWrt firmware
    (https://firmware-selector.openwrt.org/?target=mediatek%2Ffilogic&id=comfast_cf-wr631ax).
    
    Back up critical data
    ---------------------
    LuCI Web-UI:
    "System" -> "Backup / Flash Firmware" -> "Save mtdblock contents"
    
    Save mtdblock:
     - BL2
     - Factory
     - FIP
     - u-boot-env
    
    Make sure the files were successfully downloaded to your downloads directory.
    
    Using the installer image
    -------------------------
    To simplify the installation process, this method uses a fork
    of Daniel Golle's (@dangowrt) UBI Installer
    https://github.com/dangowrt/owrt-ubi-installer
    
    1. Assign ip 192.168.1.254 netmask 255.255.255.0 to your computer's
       Ethernet port.
    
    2. Ensure your router is running the latest generic OpenWrt firmware.
       Upgrade it if necessary.
    
    3. Obtain the installer image:
       Build the installer from source
       https://github.com/andros-ua/owrt-ubi-installer/tree/cf-wr631ax
       or download a prebuilt image from the
       https://github.com/andros-ua/owrt-ubi-installer/releases
    
    4. Flash the openwrt*-ubi-initramfs-recovery-installer.itb
       image using sysupgrade.
    
    5. Wait for installation: the green status LED will blink rapidly,
       indicating that the all-in-UBI installer is running.
       Once the installation finishes,
       the status LED will turn solid purple for 5 seconds.
    
    6. After the device reboots, perform a final sysupgrade using the
       openwrt*-ubi-squashfs-sysupgrade.itb image.
    
    Return to stock layout
    ----------------------
    You can use the files from the stock bootchain backup
    that the installer image created.
    
    To retrieve the files from the backup:
    1. Extract the backup:
       cat /dev/ubi0_6 | tar xzv -C /tmp
    
    2. Copy the files from /tmp/boot_backup to your PC via SCP.
    
    Restore stock layout
    --------------------
    1. Flash openwrt*-comfast_cf-wr631ax-initramfs-kernel.bin
       via sysupgrade and wait for the device to boot.
    
    2. Copy the following files to /tmp on the device via SCP:
       - BL2.bin
       - Factory.bin
       - FIP.bin
       - u-boot-env.bin
       - openwrt*-comfast_cf-wr631ax-squashfs-sysupgrade.bin
    
    3. Restore the stock MTD partitions:
       apk add kmod-mtd-rw
       insmod mtd-rw i_want_a_brick=1
       mtd write /tmp/BL2.bin BL2
       mtd write /tmp/Factory.bin Factory
       mtd write /tmp/FIP.bin FIP
       mtd write /tmp/u-boot-env.bin u-boot-env
    
    4. Install the system:
       sysupgrade /tmp/*sysupgrade.bin
    
    Stock layout
    ----------------------------------------
    | dev:    size   erasesize  name       |
    | mtd0: 00100000 00020000 "BL2"        |
    | mtd1: 00080000 00020000 "u-boot-env" |
    | mtd2: 00200000 00020000 "Factory"    |
    | mtd3: 00200000 00020000 "FIP"        |
    | mtd4: 04000000 00020000 "ubi"        |
    ----------------------------------------
    
    OpenWrt U-Boot UBI layout
    ----------------------------------
    | dev:    size   erasesize  name |
    | mtd0: 00100000 00020000 "bl2"  |
    | mtd1: 07f00000 00020000 "ubi"  |
    ----------------------------------
    
    Signed-off-by: Andrii Kuiukoff <andros.ua@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1144,6 +1144,24 @@ define Device/comfast_cf-wr631ax
 endef
 TARGET_DEVICES += comfast_cf-wr631ax
 
+define Device/comfast_cf-wr631ax-ubi
+  $(call Device/comfast_cf-wr631ax-common)
+  DEVICE_VARIANT := (UBI)
+  DEVICE_DTS := mt7981b-comfast-cf-wr631ax-ubi
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ubi-ddr3-1866
+  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot comfast_cf-wr631ax-ubi
+endef
+TARGET_DEVICES += comfast_cf-wr631ax-ubi
+
 define Device/comfast_cf-wr632ax-common
   DEVICE_VENDOR := COMFAST
   DEVICE_MODEL := CF-WR632AX
