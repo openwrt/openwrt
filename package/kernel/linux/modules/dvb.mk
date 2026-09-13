openwrt/openwrt

commit 1560c75fa250576b5d551a860d0928b2b1cee54a

kernel: add initial DVB support
Add kernel packages for DVB adapters, frontends and tuners. Not
everything is covered, but the list can grow over the time.

Userspace uses these through /dev/dvb, for example tvheadend from the
packages feed, which streams the tuners to network clients over HTTP
and HTSP.

Remote controller support (kmod-rc-core) goes into the Input modules
menu: rc-core depends only on INPUT and is not DVB specific.

Enabling DVB_CORE and RC_CORE makes parts of the media driver menu
visible which nothing in the tree has enabled so far, so kconfig has
no answer for them and stops for input. Add the 98 missing symbols to
the generic config. CONFIG_MEDIA_RC_SUPPORT, CONFIG_VIDEO_V4L2,
CONFIG_DVB_HD29L2, CONFIG_DVB_USB_FRIIO and CONFIG_IR_SIR do not exist
in kernel 6.18 anymore and are removed.

Tested on kernel 6.18.44, all 78 packages build with CONFIG_ALL_KMODS
and their module paths and kernel symbols were verified against the
kernel tree.

Signed-off-by: Michal Hrusecky <michal.hrusecky@nic.cz>
Signed-off-by: Karel Kočí <karel.koci@nic.cz>
Signed-off-by: Josef Schlehofer <pepe.schlehofer@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/24788
Signed-off-by: Robert Marko <robimarko@gmail.com>

package/kernel/linux/modules/input.mk
@@ -347,3 +347,26 @@ define KernelPackage/input-serio-libps2/description
 endef
 
 $(eval $(call KernelPackage,input-serio-libps2))
+
+define KernelPackage/rc-core
+  SUBMENU:=$(INPUT_MODULES_MENU)
+  TITLE:=Remote Controller support
+  KCONFIG:= \
+	CONFIG_RC_DEVICES=y \
+	CONFIG_RC_CORE
+  FILES:=$(LINUX_DIR)/drivers/media/rc/rc-core.ko
+  AUTOLOAD:=$(call AutoProbe,rc-core)
+  DEPENDS:=+kmod-input-core
+endef
+
+define KernelPackage/rc-core/description
+ Enable support for Remote Controllers on Linux. This is
+ needed in order to support several video capture adapters,
+ standalone IR receivers/transmitters, and RF receivers.
+
+ Enable this option if you have a video capture board even
+ if you don't need IR, as otherwise, you may not be able to
+ compile the driver for your adapter.
+endef
+
+$(eval $(call KernelPackage,rc-core))

target/linux/generic/config-6.18
@@ -762,6 +762,7 @@ CONFIG_BPF=y
 CONFIG_BPF_JIT=y
 # CONFIG_BPF_JIT_ALWAYS_ON is not set
 CONFIG_BPF_JIT_DEFAULT_ON=y
+# CONFIG_BPF_LIRC_MODE2 is not set
 # CONFIG_BPF_LSM is not set
 # CONFIG_BPF_PRELOAD is not set
 # CONFIG_BPF_STREAM_PARSER is not set
@@ -925,6 +926,7 @@ CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE=y
 # CONFIG_CC_OPTIMIZE_FOR_SIZE is not set
 # CONFIG_CDROM_PKTCDVD is not set
 # CONFIG_CDX_BUS is not set
+# CONFIG_CEC_SECO_RC is not set
 # CONFIG_CEPH_FS is not set
 # CONFIG_CEPH_LIB is not set
 # CONFIG_CFG80211 is not set
@@ -1832,9 +1834,14 @@ CONFIG_DUMMY_CONSOLE_ROWS=25
 # CONFIG_DVB_ATBM8830 is not set
 # CONFIG_DVB_AU8522_DTV is not set
 # CONFIG_DVB_AU8522_V4L is not set
+# CONFIG_DVB_AV7110 is not set
 # CONFIG_DVB_B2C2_FLEXCOP_PCI is not set
 # CONFIG_DVB_B2C2_FLEXCOP_USB is not set
 # CONFIG_DVB_BCM3510 is not set
+# CONFIG_DVB_BT8XX is not set
+# CONFIG_DVB_BUDGET_CI is not set
+# CONFIG_DVB_BUDGET_CORE is not set
+# CONFIG_DVB_C8SECTPFE is not set
 # CONFIG_DVB_CORE is not set
 # CONFIG_DVB_CX22700 is not set
 # CONFIG_DVB_CX22702 is not set
@@ -1855,6 +1862,7 @@ CONFIG_DUMMY_CONSOLE_ROWS=25
 # CONFIG_DVB_DIB7000P is not set
 # CONFIG_DVB_DIB8000 is not set
 # CONFIG_DVB_DIB9000 is not set
+# CONFIG_DVB_DM1105 is not set
 # CONFIG_DVB_DRX39XYJ is not set
 # CONFIG_DVB_DRXD is not set
 # CONFIG_DVB_DRXK is not set
@@ -1864,6 +1872,7 @@ CONFIG_DUMMY_CONSOLE_ROWS=25
 # CONFIG_DVB_EC100 is not set
 # CONFIG_DVB_FIREDTV is not set
 # CONFIG_DVB_HELENE is not set
+# CONFIG_DVB_HOPPER is not set
 # CONFIG_DVB_HORUS3A is not set
 # CONFIG_DVB_ISL6405 is not set
 # CONFIG_DVB_ISL6421 is not set
@@ -1882,6 +1891,7 @@ CONFIG_DUMMY_CONSOLE_ROWS=25
 # CONFIG_DVB_LNBP22 is not set
 # CONFIG_DVB_M88DS3103 is not set
 # CONFIG_DVB_M88RS2000 is not set
+# CONFIG_DVB_MANTIS is not set
 CONFIG_DVB_MAX_ADAPTERS=16
 # CONFIG_DVB_MB86A16 is not set
 # CONFIG_DVB_MB86A20S is not set
@@ -1916,6 +1926,7 @@ CONFIG_DVB_MAX_ADAPTERS=16
 # CONFIG_DVB_SI2165 is not set
 # CONFIG_DVB_SI2168 is not set
 # CONFIG_DVB_SI21XX is not set
+# CONFIG_DVB_SMIPCIE is not set
 # CONFIG_DVB_SP2 is not set
 # CONFIG_DVB_SP8870 is not set
 # CONFIG_DVB_SP887X is not set
@@ -1955,9 +1966,48 @@ CONFIG_DVB_MAX_ADAPTERS=16
 # CONFIG_DVB_TUNER_ITD1000 is not set
 # CONFIG_DVB_ULE_DEBUG is not set
 # CONFIG_DVB_USB is not set
+# CONFIG_DVB_USB_A800 is not set
+# CONFIG_DVB_USB_AF9005 is not set
+# CONFIG_DVB_USB_AF9015 is not set
+# CONFIG_DVB_USB_AF9035 is not set
+# CONFIG_DVB_USB_ANYSEE is not set
+# CONFIG_DVB_USB_AU6610 is not set
+# CONFIG_DVB_USB_AZ6007 is not set
+# CONFIG_DVB_USB_AZ6027 is not set
+# CONFIG_DVB_USB_CE6230 is not set
+# CONFIG_DVB_USB_CINERGY_T2 is not set
+# CONFIG_DVB_USB_CXUSB is not set
+# CONFIG_DVB_USB_CXUSB_ANALOG is not set
+# CONFIG_DVB_USB_DEBUG is not set
+# CONFIG_DVB_USB_DIB0700 is not set
+# CONFIG_DVB_USB_DIBUSB_MB is not set
+# CONFIG_DVB_USB_DIBUSB_MB_FAULTY is not set
+# CONFIG_DVB_USB_DIBUSB_MC is not set
+# CONFIG_DVB_USB_DIGITV is not set
+# CONFIG_DVB_USB_DTT200U is not set
+# CONFIG_DVB_USB_DTV5100 is not set
+# CONFIG_DVB_USB_DVBSKY is not set
+# CONFIG_DVB_USB_DW2102 is not set
+# CONFIG_DVB_USB_EC168 is not set
+# CONFIG_DVB_USB_GL861 is not set
+# CONFIG_DVB_USB_GP8PSK is not set
+# CONFIG_DVB_USB_LME2510 is not set
+# CONFIG_DVB_USB_M920X is not set
+# CONFIG_DVB_USB_MXL111SF is not set
+# CONFIG_DVB_USB_NOVA_T_USB2 is not set
+# CONFIG_DVB_USB_OPERA1 is not set
+# CONFIG_DVB_USB_PCTV452E is not set
+# CONFIG_DVB_USB_RTL28XXU is not set
+# CONFIG_DVB_USB_TECHNISAT_USB2 is not set
+# CONFIG_DVB_USB_TTUSB2 is not set
+# CONFIG_DVB_USB_UMT_010 is not set
 # CONFIG_DVB_USB_V2 is not set
+# CONFIG_DVB_USB_VP702X is not set
+# CONFIG_DVB_USB_VP7045 is not set
+# CONFIG_DVB_USB_ZD1301 is not set
 # CONFIG_DVB_VES1820 is not set
 # CONFIG_DVB_VES1X93 is not set
+# CONFIG_DVB_VIDTV is not set
 # CONFIG_DVB_ZD1301_DEMOD is not set
 # CONFIG_DVB_ZL10036 is not set
 # CONFIG_DVB_ZL10039 is not set
@@ -2548,6 +2598,7 @@ CONFIG_HARDEN_BRANCH_HISTORY=y
 # CONFIG_HID_PENMOUNT is not set
 # CONFIG_HID_PETALYNX is not set
 # CONFIG_HID_PICOLCD is not set
+# CONFIG_HID_PICOLCD_CIR is not set
 # CONFIG_HID_PID is not set
 # CONFIG_HID_PLANTRONICS is not set
 # CONFIG_HID_PLAYSTATION is not set
@@ -3113,24 +3164,52 @@ CONFIG_IP_VS_MH_TAB_INDEX=10
 # CONFIG_IRQ_POLL is not set
 # CONFIG_IRQ_TIME_ACCOUNTING is not set
 # CONFIG_IRSD200 is not set
+# CONFIG_IR_ENE is not set
+# CONFIG_IR_FINTEK is not set
 # CONFIG_IR_GPIO_CIR is not set
+# CONFIG_IR_GPIO_TX is not set
 # CONFIG_IR_HIX5HD2 is not set
 # CONFIG_IR_IGORPLUGUSB is not set
 # CONFIG_IR_IGUANA is not set
 # CONFIG_IR_IMG is not set
+# CONFIG_IR_IMG_HW is not set
+# CONFIG_IR_IMG_JVC is not set
+# CONFIG_IR_IMG_NEC is not set
+# CONFIG_IR_IMG_RAW is not set
+# CONFIG_IR_IMG_RC5 is not set
+# CONFIG_IR_IMG_RC6 is not set
+# CONFIG_IR_IMG_SANYO is not set
+# CONFIG_IR_IMG_SHARP is not set
+# CONFIG_IR_IMG_SONY is not set
 # CONFIG_IR_IMON is not set
+# CONFIG_IR_IMON_DECODER is not set
 # CONFIG_IR_IMON_RAW is not set
+# CONFIG_IR_ITE_CIR is not set
 # CONFIG_IR_JVC_DECODER is not set
 # CONFIG_IR_MCEUSB is not set
+# CONFIG_IR_MCE_KBD_DECODER is not set
+# CONFIG_IR_MESON is not set
+# CONFIG_IR_MESON_TX is not set
+# CONFIG_IR_MTK is not set
 # CONFIG_IR_NEC_DECODER is not set
+# CONFIG_IR_NUVOTON is not set
+# CONFIG_IR_PWM_TX is not set
 # CONFIG_IR_RC5_DECODER is not set
 # CONFIG_IR_RC6_DECODER is not set
+# CONFIG_IR_RCMM_DECODER is not set
 # CONFIG_IR_REDRAT3 is not set
+# CONFIG_IR_SANYO_DECODER is not set
 # CONFIG_IR_SERIAL is not set
+# CONFIG_IR_SERIAL_TRANSMITTER is not set
+# CONFIG_IR_SHARP_DECODER is not set
 # CONFIG_IR_SONY_DECODER is not set
+# CONFIG_IR_SPI is not set
 # CONFIG_IR_STREAMZAP is not set
+# CONFIG_IR_SUNXI is not set
 # CONFIG_IR_TOY is not set
 # CONFIG_IR_TTUSBIR is not set
+# CONFIG_IR_WINBOND_CIR is not set
+# CONFIG_IR_XMP_DECODER is not set
 # CONFIG_ISA_BUS is not set
 # CONFIG_ISA_BUS_API is not set
 # CONFIG_ISCSI_BOOT_SYSFS is not set
@@ -3515,6 +3594,7 @@ CONFIG_MAGIC_SYSRQ_SERIAL_SEQUENCE=""
 # CONFIG_MAILBOX is not set
 # CONFIG_MANAGER_SBS is not set
 # CONFIG_MANGLE_BOOTARGS is not set
+# CONFIG_MANTIS_CORE is not set
 # CONFIG_MARVELL_10G_PHY is not set
 # CONFIG_MARVELL_88Q2XXX_PHY is not set
 # CONFIG_MARVELL_88X2222_PHY is not set
@@ -3588,9 +3668,11 @@ CONFIG_MAX_SKB_FRAGS=17
 # CONFIG_MD_LLBITMAP is not set
 # CONFIG_MEDIATEK_GE_PHY is not set
 # CONFIG_MEDIATEK_MT6577_AUXADC is not set
+# CONFIG_MEDIA_ALTERA_CI is not set
 # CONFIG_MEDIA_ANALOG_TV_SUPPORT is not set
 # CONFIG_MEDIA_ATTACH is not set
 # CONFIG_MEDIA_CAMERA_SUPPORT is not set
+# CONFIG_MEDIA_CEC_RC is not set
 # CONFIG_MEDIA_CEC_SUPPORT is not set
 # CONFIG_MEDIA_CONTROLLER is not set
 # CONFIG_MEDIA_CONTROLLER_DVB is not set
@@ -5288,8 +5370,10 @@ CONFIG_RCU_STALL_COMMON=y
 # CONFIG_RC_ATI_REMOTE is not set
 # CONFIG_RC_CORE is not set
 # CONFIG_RC_DECODERS is not set
+# CONFIG_RC_DEVICES is not set
 # CONFIG_RC_LOOPBACK is not set
 # CONFIG_RC_MAP is not set
+# CONFIG_RC_ST is not set
 # CONFIG_RC_XBOX_DVD is not set
 # CONFIG_RDS is not set
 # CONFIG_RD_BZIP2 is not set
@@ -6201,6 +6285,7 @@ CONFIG_SLUB_CPU_PARTIAL=y
 # CONFIG_SMSC9420 is not set
 # CONFIG_SMSC_PHY is not set
 # CONFIG_SMS_SDIO_DRV is not set
+# CONFIG_SMS_SIANO_RC is not set
 # CONFIG_SMS_USB_DRV is not set
 # CONFIG_SM_CAMCC_8250 is not set
 # CONFIG_SM_FTL is not set
@@ -7824,6 +7909,7 @@ CONFIG_VHOST_MENU=y
 # CONFIG_VIDEO_ATMEL_ISC is not set
 # CONFIG_VIDEO_ATMEL_ISI is not set
 # CONFIG_VIDEO_AU0828 is not set
+# CONFIG_VIDEO_AU0828_RC is not set
 # CONFIG_VIDEO_BCM2835 is not set
 # CONFIG_VIDEO_BCM2835_UNICAM is not set
 # CONFIG_VIDEO_BCM2835_UNICAM_LEGACY is not set
@@ -7844,11 +7930,16 @@ CONFIG_VHOST_MENU=y
 # CONFIG_VIDEO_CS3308 is not set
 # CONFIG_VIDEO_CS5345 is not set
 # CONFIG_VIDEO_CS53L32A is not set
+# CONFIG_VIDEO_CX18 is not set
 # CONFIG_VIDEO_CX231XX is not set
+# CONFIG_VIDEO_CX231XX_DVB is not set
+# CONFIG_VIDEO_CX231XX_RC is not set
 # CONFIG_VIDEO_CX2341X is not set
+# CONFIG_VIDEO_CX23885 is not set
 # CONFIG_VIDEO_CX25821 is not set
 # CONFIG_VIDEO_CX25840 is not set
 # CONFIG_VIDEO_CX88 is not set
+# CONFIG_VIDEO_CX88_DVB is not set
 # CONFIG_VIDEO_DEV is not set
 # CONFIG_VIDEO_DS90UB913 is not set
 # CONFIG_VIDEO_DS90UB953 is not set
@@ -7859,6 +7950,10 @@ CONFIG_VHOST_MENU=y
 # CONFIG_VIDEO_DW9768 is not set
 # CONFIG_VIDEO_DW9807_VCM is not set
 # CONFIG_VIDEO_EM28XX is not set
+# CONFIG_VIDEO_EM28XX_ALSA is not set
+# CONFIG_VIDEO_EM28XX_DVB is not set
+# CONFIG_VIDEO_EM28XX_RC is not set
+# CONFIG_VIDEO_EM28XX_V4L2 is not set
 # CONFIG_VIDEO_ET8EK8 is not set
 # CONFIG_VIDEO_FIXED_MINOR_RANGES is not set
 # CONFIG_VIDEO_GC0308 is not set
@@ -7972,6 +8067,7 @@ CONFIG_VHOST_MENU=y
 # CONFIG_VIDEO_OV9650 is not set
 # CONFIG_VIDEO_OV9734 is not set
 # CONFIG_VIDEO_PVRUSB2 is not set
+# CONFIG_VIDEO_PVRUSB2_DVB is not set
 # CONFIG_VIDEO_RASPBERRYPI_PISP_BE is not set
 # CONFIG_VIDEO_RCAR_CSI2 is not set
 # CONFIG_VIDEO_RCAR_ISP is not set
@@ -7991,6 +8087,8 @@ CONFIG_VHOST_MENU=y
 # CONFIG_VIDEO_SAA711X is not set
 # CONFIG_VIDEO_SAA7127 is not set
 # CONFIG_VIDEO_SAA7134 is not set
+# CONFIG_VIDEO_SAA7134_DVB is not set
+# CONFIG_VIDEO_SAA7134_RC is not set
 # CONFIG_VIDEO_SAA7164 is not set
 # CONFIG_VIDEO_SAA717X is not set
 # CONFIG_VIDEO_SAA7185 is not set

commit ccbdf2098468ab965ae0b99a10d6385ff9e7ed4c

kernel: restrict DVB and rc-core modules to kernel 6.18
The new DVB and remote controller packages are kernel version agnostic,
but the media symbols they make reachable were only answered in
target/linux/generic/config-6.18. On the targets still building kernel
6.12 the buildbot selects the packages through CONFIG_ALL_KMODS,
kconfig has no answer for the newly visible symbols and syncconfig
stops waiting for input.

Restrict the three packages the others hang off. Everything DVB
depends on kmod-dvb-core and kmod-video-em28xx-dvb follows
kmod-video-em28xx, while kmod-rc-core is a root of its own: RC_DEVICES
opens the remote controller menu with no DVB package involved.

Fixes: 1560c75fa250 ("kernel: add initial DVB support")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/linux/modules/dvb.mk
@@ -23,7 +23,7 @@ define KernelPackage/dvb-core
 	CONFIG_DVB_DEMUX_SECTION_LOSS_LOG=y
   FILES:=$(LINUX_DIR)/drivers/media/dvb-core/dvb-core.ko
   AUTOLOAD:=$(call AutoProbe,dvb-core)
-  DEPENDS:=+kmod-i2c-core
+  DEPENDS:=@!LINUX_6_12 +kmod-i2c-core
 endef
 
 define KernelPackage/dvb-core/description
@@ -1046,7 +1046,7 @@ define KernelPackage/video-em28xx
 	CONFIG_VIDEO_EM28XX
   FILES:=$(LINUX_DIR)/drivers/media/usb/em28xx/em28xx.ko
   AUTOLOAD:=$(call AutoProbe,em28xx)
-  DEPENDS+=+kmod-usb-core +kmod-i2c-core +kmod-tveeprom +kmod-video-core
+  DEPENDS+=@!LINUX_6_12 +kmod-usb-core +kmod-i2c-core +kmod-tveeprom +kmod-video-core
   SUBMENU:=$(VIDEO_MENU)
 endef
 

package/kernel/linux/modules/input.mk
@@ -356,7 +356,7 @@ define KernelPackage/rc-core
 	CONFIG_RC_CORE
   FILES:=$(LINUX_DIR)/drivers/media/rc/rc-core.ko
   AUTOLOAD:=$(call AutoProbe,rc-core)
-  DEPENDS:=+kmod-input-core
+  DEPENDS:=@!LINUX_6_12 +kmod-input-core
 endef
 
 define KernelPackage/rc-core/description