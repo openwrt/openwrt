f5fb15d8698ffd2feff34d30842a619544450c38
John Crispin <john@phrozen.org>
2026-09-18 20:47:16 +0200

    build: list the available feeds only when needed
    feeds.mk runs "scripts/feeds list -n" when it is included, which
    happens in every package Makefile. The list is only needed to find the
    package directories of the feeds, which a package Makefile only does
    when it creates or cleans a package.
    
    Evaluate FEEDS_AVAILABLE on first use.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- include/feeds.mk
+++ include/feeds.mk
@@ -6,7 +6,7 @@
 -include $(TMP_DIR)/.packageauxvars
 
 FEEDS_INSTALLED:=$(notdir $(wildcard $(TOPDIR)/package/feeds/*))
-FEEDS_AVAILABLE:=$(sort $(FEEDS_INSTALLED) $(shell $(SCRIPT_DIR)/feeds list -n 2>/dev/null))
+FEEDS_AVAILABLE=$(eval FEEDS_AVAILABLE:=$(sort $(FEEDS_INSTALLED) $(shell $(SCRIPT_DIR)/feeds list -n 2>/dev/null)))$(FEEDS_AVAILABLE)
 
 PACKAGE_SUBDIRS=$(PACKAGE_DIR)
 ifneq ($(CONFIG_PER_FEED_REPO),)
