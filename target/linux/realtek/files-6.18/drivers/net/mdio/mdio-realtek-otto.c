openwrt/openwrt

commit f91c59650eef4dcf955077c01af8696fd03c04b4

realtek: mdio: RTL931x does not need ext_page set
Remove the register write. It is never used in the SDK.

Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>
Link: https://github.com/openwrt/openwrt/pull/23204
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/realtek/files-6.18/drivers/net/mdio/mdio-realtek-otto.c
@@ -480,7 +480,6 @@ static int rtmdio_931x_write_c22(struct mii_bus *bus, u32 pn, u32 page, u32 reg,
 {
 	struct rtmdio_931x_smi_access smi_access = {
 		.ctrl_0 = RTMDIO_931X_C22_DATA(page, reg),
-		.ctrl_1 = 0x1ff,
 		.ctrl_2 = (u32)(BIT_ULL(pn)),
 		.ctrl_x = (u32)(BIT_ULL(pn) >> 32),
 		.ctrl_3 = val,

commit 2193626398ce9cb16683695f9ce9f7ccffb273d2

realtek: mdio: fix RTL931x C22 extended page access
RTL8218E bring-up on the RTL931x-based switches exposed that private PHY
register accesses always hit extended page zero.

RTL931x indirect C22 commands include a controller-side extended page
field. Leaving it at zero forces every command to page zero, overriding
register 30 and making private registers inaccessible.

Set the field to the 0x1ff skip value for C22 reads and writes, matching
RTL839x behavior and leaving extended page selection to the PHY.

Fixes: f91c59650eef ("realtek: mdio: RTL931x does not need ext_page set")
Assisted-by: ChatGPT (OpenAI GPT-5.6 Sol)
Link: https://github.com/openwrt/openwrt/pull/25236
Signed-off-by: Jonas Jelonek <jonas@jonasjelonek.de>

target/linux/realtek/files-6.18/drivers/net/mdio/mdio-realtek-otto.c
@@ -185,6 +185,7 @@
 #define   RTMD_931X_CMD_WRITE_C45		(BIT(3) | BIT(4))
 #define   RTMD_931X_C22_DATA(page, reg)		((reg) << 6 | (page) << 11)
 #define RTMD_931X_SMI_INDRT_ACCESS_CTRL_1	0x0c04
+#define   RTMD_931X_SMI_INDRT_SKIP_EXT_PAGE	GENMASK(8, 0)
 #define RTMD_931X_SMI_INDRT_ACCESS_CTRL_2(x)	(0x0c08 + (x) * 4)
 #define RTMD_931X_SMI_INDRT_ACCESS_CTRL_3	0x0c10
 #define RTMD_931X_SMI_INDRT_ACCESS_MMD		0x0c18
@@ -484,6 +485,7 @@ static int rtmd_931x_read_c22(struct mii_bus *bus, u32 pn, u32 page, u32 reg, u3
 	struct rtmd_command_data cmd_data = {
 		.brdcast = RTMD_931X_SMI_INDRT_PORT(pn),
 		.c22_adr = RTMD_931X_C22_DATA(page, reg),
+		.ex_page = RTMD_931X_SMI_INDRT_SKIP_EXT_PAGE,
 	};
 
 	return rtmd_run_cmd(bus, RTMD_931X_CMD_READ_C22, &cmd_data, val);
@@ -493,6 +495,7 @@ static int rtmd_931x_write_c22(struct mii_bus *bus, u32 pn, u32 page, u32 reg, u
 {
 	struct rtmd_command_data cmd_data = {
 		.c22_adr = RTMD_931X_C22_DATA(page, reg),
+		.ex_page = RTMD_931X_SMI_INDRT_SKIP_EXT_PAGE,
 		.mask_lo = (u32)(BIT_ULL(pn)),
 		.mask_hi = (u32)(BIT_ULL(pn) >> 32),
 		.io_data = val,