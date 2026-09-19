1dae6bbcbdfea3cd39fb6653e83ee10cad1260f2
Paul Maruhn <paulmaruhn@posteo.de>
2026-09-19 14:51:19 +0200

    ath79: merge eap225-outdoor-v3.dts and f52-outdoor-v1.dts into .dtsi file
    and update image definition in generic-tp-link.mk
    as suggested by openwrt-ai bot.
    
    Signed-off-by: Paul Maruhn <paulmaruhn@posteo.de>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -441,7 +441,9 @@ endef
 TARGET_DEVICES += tplink_eap225-outdoor-v3
 
 define Device/tplink_f52-outdoor-v1
-  $(Device/tplink_eap225-outdoor-v3)
+  $(Device/tplink-eap2x5)
+  SOC := qca9563
+  IMAGE_SIZE := 13824k
   DEVICE_MODEL := F52-Outdoor
   DEVICE_VARIANT := v1
   DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct
