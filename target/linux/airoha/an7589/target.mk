cc99c30f41f7ba9a95cca561aea7cdcb51289290
Zhengping Zhang <zhengping.zhang@airoha.com>
2026-09-21 21:48:01 +0800

    airoha: introduce AN7589 SoC support
    AN7589 is a quad-core Arm Cortex-A73-based broadband gateway SoC
    designed for Wi-Fi 8 applications. It integrates a 16-core RISC-V
    network processor, a hardware packet-processing accelerator (UPS),
    and an EIP-197 security accelerator.
    
    This commit primarily adds the target definitions and clock driver
    for AN7589. The remaining drivers will be added in subsequent
    commits.
    
    Signed-off-by: Zhengping Zhang <zhengping.zhang@airoha.com>

---
+++ target/linux/airoha/an7589/target.mk
@@ -0,0 +1,10 @@
+ARCH:=aarch64
+SUBTARGET:=an7589
+BOARDNAME:=AN7589 / AN9550 / AN9520
+CPU_TYPE:=cortex-a73
+KERNELNAME:=Image dtbs
+
+define Target/Description
+	Build firmware images for Airoha an7589 ARM based boards.
+endef
+
